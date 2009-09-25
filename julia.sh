77ea45461e6ca16a1538cd6420f16ce886c9b089
Viral Shah <viral@mayin.org>
2009-09-25 10:21:51 -0700

    Start julia REPL directly.

---
+++ julia.sh
@@ -0,0 +1,1 @@
+gsi -e "(include \"utils.scm\")" -e "(include \"match.scm\")" -e "(include \"julia-parser.scm\")" -e "(include \"julia-syntax.scm\")" -e "(include \"julia-interpreter.scm\")" -e \(julia-repl\)
