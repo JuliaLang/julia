663dd4718c31093357be07d4e5f9d3d44cb4352d
Jeff Bezanson <bezanson@post.harvard.edu>
2010-03-21 23:14:19 -0400

    skeleton of code generation for all expression types handling rest argument in function preamble

--- codegen.cpp
+++ codegen.cpp
@@ -38,6 +38,7 @@ static std::map<const std::string, GlobalVariable*> stringConstants;
 // types
 static const Type *jl_value_llvmt;
 static const Type *jl_pvalue_llvmt;
+static const Type *jl_function_llvmt;
 static const FunctionType *jl_func_sig;
 static const Type *T_int8;
 static const Type *T_int32;
@@ -52,6 +53,17 @@ static GlobalVariable *jlsysmod_var;
 static Function *jlerror_func;
 static Function *jlgetbinding_func;
 static Function *jltuple_func;
+
+// head symbols for each expression type
+static jl_sym_t *goto_sym;    static jl_sym_t *goto_ifnot_sym;
+static jl_sym_t *label_sym;   static jl_sym_t *return_sym;
+static jl_sym_t *lambda_sym;  static jl_sym_t *call_sym;
+static jl_sym_t *assign_sym;  static jl_sym_t *quote_sym;
+static jl_sym_t *null_sym;    static jl_sym_t *top_sym;
+static jl_sym_t *boundp_sym;  static jl_sym_t *closure_ref_sym;
+static jl_sym_t *body_sym;    static jl_sym_t *list_sym;
+static jl_sym_t *locals_sym;  static jl_sym_t *colons_sym;
+static jl_sym_t *dots_sym;
 
 /*
   plan
@@ -68,6 +80,8 @@ static Function *jltuple_func;
   - gensyms from the front end might conflict with real variables, fix it
   - source location tracking, var name metadata
   - better error messages
+  - exceptions
+  - threads or other advanced control flow
 
   optimizations round 1:
   - constants, especially global. resolve functions statically.
@@ -132,11 +146,11 @@ extern "C" void jl_compile(jl_lambda_info_t *li)
 // get array of formal argument expressions
 static jl_buffer_t *lam_args(jl_expr_t *l)
 {
-    assert(l->head == jl_symbol("lambda"));
+    assert(l->head == lambda_sym);
     jl_value_t *ae = ((jl_value_t**)l->args->data)[0];
     if (ae == (jl_value_t*)jl_null) return jl_the_empty_buffer;
     assert(jl_is_expr(ae));
-    assert(((jl_expr_t*)ae)->head == jl_symbol("list"));
+    assert(((jl_expr_t*)ae)->head == list_sym);
     return ((jl_expr_t*)ae)->args;
 }
 
@@ -147,7 +161,7 @@ static jl_buffer_t *lam_locals(jl_expr_t *l)
     assert(jl_is_expr(le));
     jl_expr_t *lle = ((jl_expr_t**)((jl_expr_t*)le)->args->data)[0];
     assert(jl_is_expr(lle));
-    assert(lle->head == jl_symbol("locals"));
+    assert(lle->head == locals_sym);
     return lle->args;
 }
 
@@ -156,7 +170,7 @@ static jl_buffer_t *lam_body(jl_expr_t *l)
 {
     jl_value_t *be = ((jl_value_t**)l->args->data)[2];
     assert(jl_is_expr(be));
-    assert(((jl_expr_t*)be)->head == jl_symbol("body"));
+    assert(((jl_expr_t*)be)->head == body_sym);
     return ((jl_expr_t*)be)->args;
 }
 
@@ -170,36 +184,112 @@ static jl_sym_t *decl_var(jl_value_t *ex)
 static int is_rest_arg(jl_value_t *ex)
 {
     if (!jl_is_expr(ex)) return 0;
-    if (((jl_expr_t*)ex)->head != jl_symbol("::")) return 0;
+    if (((jl_expr_t*)ex)->head != colons_sym) return 0;
     jl_expr_t *atype = ((jl_expr_t**)((jl_expr_t*)ex)->args->data)[1];
     if (!jl_is_expr(atype)) return 0;
-    if (atype->head != jl_symbol("call") ||
+    if (atype->head != call_sym ||
         atype->args->length != 3)
         return 0;
-    if (((jl_sym_t**)atype->args->data)[1] != jl_symbol("..."))
+    if (((jl_sym_t**)atype->args->data)[1] != dots_sym)
         return 0;
     return 1;
 }
 
+// information about the context of a piece of code: its enclosing
+// function and module, and visible local variables and labels.
+typedef struct {
+    Function *f;
+    std::map<std::string, AllocaInst*> *vars;
+    std::map<std::string, BasicBlock*> *labels;
+    jl_module_t *module;
+    jl_expr_t *lam;
+    const Argument *envArg;
+    const Argument *argArray;
+    const Argument *argCount;
+} jl_codectx_t;
+
+static void emit_expr(jl_value_t *expr, jl_codectx_t *ctx)
+{
+    if (jl_is_symbol(expr)) {
+        // var
+    }
+    if (!jl_is_expr(expr)) {
+        // numeric literals
+        if (jl_is_int32(expr)) {
+        }
+        else if (jl_is_int64(expr)) {
+        }
+        else if (jl_is_uint64(expr)) {
+        }
+        else if (jl_is_float64(expr)) {
+        }
+        else if (jl_is_buffer(expr)) {
+            // string literal
+        }
+        assert(0);
+    }
+    jl_expr_t *ex = (jl_expr_t*)expr;
+    // this is object-disoriented.
+    // however, this is a good way to do it because it should *not* be easy
+    // to add new node types.
+    if (ex->head == goto_sym) {
+    }
+    else if (ex->head == goto_ifnot_sym) {
+    }
+    else if (ex->head == label_sym) {
+    }
+
+    else if (ex->head == return_sym) {
+    }
+    else if (ex->head == call_sym) {
+    }
+
+    else if (ex->head == assign_sym) {
+    }
+    else if (ex->head == top_sym) {
+    }
+    else if (ex->head == boundp_sym) {
+    }
+    else if (ex->head == closure_ref_sym) {
+    }
+
+    else if (ex->head == quote_sym) {
+    }
+    else if (ex->head == null_sym) {
+    }
+}
+
 static void emit_function(jl_expr_t *lam, Function *f)
 {
-    BasicBlock *b0 = BasicBlock::Create(jl_LLVMContext, "a", f);
+    BasicBlock *b0 = BasicBlock::Create(jl_LLVMContext, "top", f);
     builder.SetInsertPoint(b0);
     std::map<std::string, AllocaInst*> localVars;
+    std::map<std::string, BasicBlock*> labels;
     jl_buffer_t *largs = lam_args(lam);
     jl_buffer_t *lvars = lam_locals(lam);
     Function::arg_iterator AI = f->arg_begin();
     const Argument &envArg = *AI++;
     const Argument &argArray = *AI++;
     const Argument &argCount = *AI++;
+    jl_codectx_t ctx;
+    ctx.f = f;
+    ctx.vars = &localVars;
+    ctx.labels = &labels;
+    ctx.module = jl_system_module; //TODO
+    ctx.lam = lam;
+    ctx.envArg = &envArg;
+    ctx.argArray = &argArray;
+    ctx.argCount = &argCount;
 
     // check arg count
     size_t nreq = largs->length;
+    int va = 0;
     std::vector<Value *> zeros(0);
     zeros.push_back(ConstantInt::get(T_int32, 0));
     zeros.push_back(ConstantInt::get(T_int32, 0));
     if (nreq > 0 && is_rest_arg(((jl_value_t**)largs->data)[nreq-1])) {
         nreq--;
+        va = 1;
         Value *enough =
             builder.CreateICmpUGE((Value*)&argCount,
                                   ConstantInt::get(T_int32, nreq));
@@ -234,7 +324,7 @@ static void emit_function(jl_expr_t *lam, Function *f)
     // move args into local variables
     // (probably possible to avoid this step with a little redesign)
     size_t i;
-    for(i=0; i < largs->length; i++) {
+    for(i=0; i < nreq; i++) {
         char *argname = decl_var(((jl_value_t**)largs->data)[i])->name;
         AllocaInst *lv = builder.CreateAlloca(jl_pvalue_llvmt, 0, argname);
         Value *argPtr =
@@ -243,6 +333,42 @@ static void emit_function(jl_expr_t *lam, Function *f)
         LoadInst *theArg = builder.CreateLoad(argPtr, false);
         builder.CreateStore(theArg, lv);
         localVars[argname] = lv;
+    }
+    // allocate rest argument if necessary
+    if (va) {
+        // restarg = jl_f_tuple(NULL, &args[nreq], nargs-nreq)
+        Value *restTuple =
+            builder.CreateCall3(jltuple_func,
+                                Constant::getNullValue(jl_pvalue_llvmt),
+                                builder.CreateGEP((Value*)&argArray,
+                                                  ConstantInt::get(T_int32,nreq)),
+                                builder.CreateSub((Value*)&argCount,
+                                                  ConstantInt::get(T_int32,nreq)));
+        char *argname = decl_var(((jl_value_t**)largs->data)[nreq])->name;
+        AllocaInst *lv = builder.CreateAlloca(jl_pvalue_llvmt, 0, argname);
+        builder.CreateStore(restTuple, lv);
+        localVars[argname] = lv;
+    }
+
+    // allocate local variables
+    for(i=0; i < lvars->length; i++) {
+        char *argname = ((jl_sym_t**)lvars->data)[i]->name;
+        AllocaInst *lv = builder.CreateAlloca(jl_pvalue_llvmt, 0, argname);
+        localVars[argname] = lv;
+    }
+
+    jl_buffer_t *stmts = lam_body(lam);
+    // associate labels with basic blocks so forward jumps can be resolved
+    for(i=0; i < stmts->length; i++) {
+        jl_value_t *ex = ((jl_value_t**)stmts->data)[i];
+        if (jl_is_expr(ex) && ((jl_expr_t*)ex)->head == label_sym) {
+            char *lname = ((jl_sym_t**)((jl_expr_t*)ex)->args->data)[0]->name;
+            labels[lname] = BasicBlock::Create(getGlobalContext(), lname);
+        }
+    }
+    // compile body statements
+    for(i=0; i < stmts->length; i++) {
+        emit_expr(((jl_value_t**)stmts->data)[i], &ctx);
     }
 
     LoadInst *nullVal = builder.CreateLoad(jlnull_var, false);
@@ -275,6 +401,7 @@ static void init_julia_llvm_env(Module *m)
     jl_pvalue_llvmt = PointerType::get(jl_value_llvmt, 0);
     jl_func_sig = dynamic_cast<const FunctionType*>(jdefs->getTypeByName("jl_callable_t"));
     assert(jl_func_sig != NULL);
+    jl_function_llvmt = jdefs->getTypeByName("jl_function_t");
 
     jltrue_var = global_to_llvm("jl_true", (void*)&jl_true);
     jlfalse_var = global_to_llvm("jl_false", (void*)&jl_false);
@@ -314,4 +441,22 @@ extern "C" void jl_init_codegen()
     jl_ExecutionEngine = EngineBuilder(jl_Module).create();
 
     init_julia_llvm_env(jl_Module);
+
+    goto_sym = jl_symbol("goto");
+    goto_ifnot_sym = jl_symbol("goto-ifnot");
+    label_sym = jl_symbol("label");
+    return_sym = jl_symbol("return");
+    lambda_sym = jl_symbol("lambda");
+    call_sym = jl_symbol("call");
+    assign_sym = jl_symbol("assign");
+    quote_sym = jl_symbol("quote");
+    null_sym = jl_symbol("null");
+    top_sym = jl_symbol("top");
+    boundp_sym = jl_symbol("boundp");
+    closure_ref_sym = jl_symbol("closure-ref");
+    body_sym = jl_symbol("body");
+    list_sym = jl_symbol("list");
+    locals_sym = jl_symbol("locals");
+    colons_sym = jl_symbol("::");
+    dots_sym = jl_symbol("...");
 }
