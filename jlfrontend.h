386553796c64d4a076e4e7b000eff2085a4ab434
Jeff Bezanson <bezanson@post.harvard.edu>
2010-05-25 14:30:36 -0400

    moving old files to attic removing jlfrontend.h

--- jlfrontend.h
+++
@@ -1,6 +0,0 @@
-___SCMOBJ jl_scm_parse_string(const char *str);
-___SCMOBJ jl_scm_parse_file(const char *fname);
-
-uint64_t jl_scm_uint64(___SCMOBJ x);
-double jl_scm_float64(___SCMOBJ x);
-int jl_scm_integerp(___SCMOBJ x);
