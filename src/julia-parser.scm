caa96accc0a098a4d8adcde5c8ac2d93d0aac139
Jeff Bezanson <bezanson@post.harvard.edu>
2011-06-06 02:29:33 -0400

    allowing return with no value in parser

--- src/julia-parser.scm
+++ src/julia-parser.scm
@@ -636,7 +636,9 @@
 		    (expect-end s)
 		    (list 'try try-block var catch-block)))
 	 (else (error "improperly terminated try block")))))
-    ((return)          (list 'return (parse-eq s)))
+    ((return)          (if (memv (peek-token s) '(#\newline #\;))
+			   (list 'return '(null))
+			   (list 'return (parse-eq s))))
     ((break continue)  (list word))
     (else (error "unhandled reserved word")))))
 
