6c27dde12458a402039b237a51ff6471a932e6a2
Jeff Bezanson <bezanson@post.harvard.edu>
2011-06-15 21:14:26 -0400

    adding tgamma to fdlibm. fixes issue #39 updating some .gitignore files

--- external/fdlibm/math_private.h
+++ external/fdlibm/math_private.h
@@ -19,6 +19,8 @@
 
 #include <sys/types.h>
 
+#define	M_PI		3.14159265358979323846	/* pi */
+
 /* The original fdlibm code used statements like:
 	n0 = ((*(int*)&one)>>29)^1;		* index of high word *
 	ix0 = *(n0+(int*)&x);			* high word of x *
