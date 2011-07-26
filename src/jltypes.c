JuliaLang/julia

commit 8bcc0e59ffb2f5a513b54793d6ee780161915218

improving method sort rules not to count ... as less specific unless that's the only difference between two signatures

src/jltypes.c
@@ -1354,6 +1354,12 @@ static int jl_tuple_subtype_(jl_value_t **child, size_t cl,
         if (!jl_subtype_le(ce, pe, ta, morespecific, invariant))
             return 0;
 
+        if (morespecific) {
+            // stop as soon as one element is strictly more specific
+            if (!jl_types_equal(ce,pe))
+                return 1;
+        }
+
         if (cseq && pseq) return 1;
         if (!cseq) ci++;
         if (!pseq) pi++;

commit dc8b92d3776b4e4324d15983b9a3637a9194d173

fixing issue #157, caused by commit 8bcc0e59ffb2f5a513b54793d6ee780161915218

src/jltypes.c
@@ -1345,34 +1345,46 @@ static int jl_tuple_subtype_(jl_value_t **child, size_t cl,
                              int morespecific, int invariant)
 {
     size_t ci=0, pi=0;
+    int mode = 0;
     while(1) {
         int cseq = !ta && (ci<cl) && jl_is_seq_type(child[ci]);
         int pseq = (pi<pl) && jl_is_seq_type(parent[pi]);
         if (ci >= cl)
-            return (pi>=pl || pseq);
+            return mode || (pi>=pl || pseq);
         if (cseq && !pseq)
-            return 0;
+            return mode;
         if (pi >= pl)
-            return 0;
+            return mode;
         jl_value_t *ce = child[ci];
         jl_value_t *pe = parent[pi];
         if (cseq) ce = jl_tparam0(ce);
         if (pseq) pe = jl_tparam0(pe);
 
-        if (!jl_subtype_le(ce, pe, ta, morespecific, invariant))
-            return 0;
+        if (mode) {
+            if (jl_subtype_le(pe, ce, 0, morespecific, invariant) &&
+                !jl_types_equal(ce,pe))
+                return 0;
+        }
+        else {
+            if (!jl_subtype_le(ce, pe, ta, morespecific, invariant))
+                return 0;
+        }
 
-        if (morespecific) {
+        if (morespecific && 0) {
             // stop as soon as one element is strictly more specific
-            if (!jl_types_equal(ce,pe))
-                return 1;
+            if (!jl_types_equal(ce,pe)) {
+                mode = 1;
+                // here should go into a different mode where we return 1
+                // unless some element of the parent, pe, is strictly more
+                // specific than the corresponding child element, ce.
+            }
         }
 
         if (cseq && pseq) return 1;
         if (!cseq) ci++;
         if (!pseq) pi++;
     }
-    return 0;
+    return mode;
 }
 
 int jl_tuple_subtype(jl_value_t **child, size_t cl,