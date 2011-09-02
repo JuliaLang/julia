31dd30202f7af041b754279c8cfcec06a8883b10
Viral B. Shah <viral@mayin.org>
2011-09-02 11:30:34 +0530

    Add parse_bin to perf.cc.

--- test/perf.cc
+++ test/perf.cc
@@ -3,19 +3,19 @@
 
 #include <cstdio>
 #include <cstdlib>
-#include <iostream>
 #include <cassert>
-#include <ctime>
-#include <algorithm>
 #include <cmath>
 #include <complex>
 #include <sys/time.h>
+#include <algorithm>
 
 #ifdef __APPLE__
 #include <vecLib/cblas.h>
 #endif
 
 using namespace std;
+
+#define NITER 10
 
 double clock_now()
 {
@@ -26,8 +26,6 @@ double clock_now()
 }
 
 #define CLOCK() clock_now()
-
-#define NITER 10
 
 extern "C" void dgemm_(char, char, int, int, int, double, double *, int, double *, int, double, double *, int);
 
@@ -70,6 +68,24 @@ int mandelperf() {
     }
     return mandel_sum;
 }
+
+long parse_int(const char *s, long base) {
+    long n = 0;
+    
+    for (int i=0; i<strlen(s); ++i) {
+        char c = s[i];
+        long d = (long) '0' <= c <= '9' ? c-'0' :
+                        'A' <= c <= 'Z' ? c-'A' + (int) 10 :
+                        'a' <= c <= 'z' ? c-'a' + (int) 10 :
+                        printf("Error\n");
+        if (base <= d) {
+            printf("Error\n");
+        }
+        n = n*base + d;
+    }
+    return n;
+}
+
 int main() {
     double t1, t2;
     
@@ -83,6 +99,17 @@ int main() {
     t2 = CLOCK() - t1;
     printf("fib(20):\t %1.8lf\n", (t2/(double)NITER));
     
+    // parse_bin
+    assert(parse_int("1111000011110000111100001111", 2) == 252645135);
+    t1 = CLOCK();
+    for (int i=0; i<NITER; ++i) {
+        for (int k=0; k<1000; ++k) {
+            long n = parse_int("1111000011110000111100001111", 2);
+        }
+    }
+    t2 = CLOCK() - t1;
+    printf("parse_bin:\t %1.8lf\n", (t2/(double)NITER));
+
     // array constructor
     double *a;
     t1 = CLOCK();
