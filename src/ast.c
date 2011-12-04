3cce54008e2c374fd6bb4bca472ab18bdbc09cac
Jeff Bezanson <bezanson@post.harvard.edu>
2011-12-03 23:34:45 -0500

    fixing #278

--- src/ast.c
+++ src/ast.c
@@ -66,8 +66,8 @@ value_t fl_invoke_julia_macro(value_t *args, uint32_t nargs)
     }
     JL_CATCH {
         JL_GC_POP();
-        ios_putc('\n', jl_current_output_stream());
         jl_show(jl_exception_in_transit);
+        ios_putc('\n', jl_current_output_stream());
         return fl_cons(symbol("error"), FL_NIL);
     }
     // protect result from GC, otherwise it could be freed during future
