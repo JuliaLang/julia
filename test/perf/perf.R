80a175ad98bc7702bedd1212b033106b4c902209
Stefan Karpinski <stefan@karpinski.org>
2012-01-09 19:11:29 -0500

    Convert tabs to spaces in R benchmark script.

--- test/perf/perf.R
+++ test/perf/perf.R
@@ -1,26 +1,26 @@
 library(R.utils)
 
 assert = function(bool) {
-	if (!bool) stop('Assertion failed')
+    if (!bool) stop('Assertion failed')
 }
 
 timeit = function(name, f, ..., times=5) {
-	tmin = Inf
-	for (t in 1:times) {
-		t = system.time(f(...))["elapsed"]
-		if (t < tmin) tmin = t
-	}
-	cat(sprintf("r,%s,%.8f\n", name, tmin*1000))
+    tmin = Inf
+    for (t in 1:times) {
+        t = system.time(f(...))["elapsed"]
+        if (t < tmin) tmin = t
+    }
+    cat(sprintf("r,%s,%.8f\n", name, tmin*1000))
 }
 
 ## fib ##
 
 fib = function(n) {
-	if (n < 2) {
-		return(n)
-	} else {
-		return(fib(n-1) + fib(n-2))
-	}
+    if (n < 2) {
+        return(n)
+    } else {
+        return(fib(n-1) + fib(n-2))
+    }
 }
 
 assert(fib(20) == 6765)
@@ -29,13 +29,13 @@ timeit("fib", fib, 20)
 ## parse_int ##
 
 parseintperf = function(t) {
-	for (i in 1:t) {
-		# R doesn't support uint32 values
-		n = floor(2^31-1*runif(1))
-		s = intToHex(n)
-		m = as.numeric(paste("0x",s,sep=""))
-		assert(m == n)
-	}
+    for (i in 1:t) {
+        # R doesn't support uint32 values
+        n = floor(2^31-1*runif(1))
+        s = intToHex(n)
+        m = as.numeric(paste("0x",s,sep=""))
+        assert(m == n)
+    }
 }
 
 timeit("parse_int", parseintperf, 1000)
@@ -80,26 +80,26 @@ timeit('quicksort', sortperf, 5000)
 
 mandel = function(z) {
     c = z
-	maxiter = 80
+    maxiter = 80
     for (n in 1:maxiter) {
         if (Mod(z) > 2) return(n-1)
         z = z^2+c
-	}
-	return(maxiter)
+    }
+    return(maxiter)
 }
 
 mandelperf = function() {
-	re = seq(-2,0.5,.1)
-	im = seq(-1,1,.1)
-	M = matrix(0.0,nrow=length(re),ncol=length(im))
-	count = 1
-	for (r in re) {
-		for (i in im) {
-			M[count] = mandel(complex(real=r,imag=i))
-			count = count + 1
-		}
-	}
-	return(M)
+    re = seq(-2,0.5,.1)
+    im = seq(-1,1,.1)
+    M = matrix(0.0,nrow=length(re),ncol=length(im))
+    count = 1
+    for (r in re) {
+        for (i in im) {
+            M[count] = mandel(complex(real=r,imag=i))
+            count = count + 1
+        }
+    }
+    return(M)
 }
 
 assert(sum(mandelperf()) == 14660)
