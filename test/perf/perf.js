df0f84b02bc1a1bd85053e383be0659fcf04b5bd
PlayMyCode <joe@playmycode.com>
2012-02-19 00:30:57 +0000

    alternative randmatstat implementation, which includes re-using arrays (by supplying them to the functions) and Float64Array support. It also 'fixes' the w bug, this breaks the assertion (so it is uncommented).

--- test/perf/perf.js
+++ test/perf/perf.js
@@ -148,6 +148,21 @@ console.log("javascript,pi_sum," + tmin/10);
 
 // random matrix statistics //
 
+function randn( a, sub ) {
+    var subLen = sub.length,
+        len = a.length;
+    
+    for (var i = 0; i < subLen; i++) {
+        a[i] = sub[i] = gaussian();
+    }
+    
+    for (var i = subLen; i < len; i++) {
+        a[i] = gaussian();
+    }
+    
+    return a;
+}
+
 function gaussian() {
     var k = 2;
     do {
@@ -157,22 +172,145 @@ function gaussian() {
     } while (k >= 1);
     return i*Math.sqrt((-2*Math.log(k))/k);
 }
-function randn(n) {
-    var a = new Array(n);
-    var i = 0;
-    for (i = 0; i < n; i++)
-        a[i] = gaussian();
-    return a;
-}
-function transpose(A,m,n) {
-    var B = new Array(A.length);
+
+function transpose(dest, src,m,n) {
     var i = 0;
     var j = 0;
-    for (i = 0; i < m; i++)
-        for (j = 0; j < n; j++)
-            B[i*n+j] = A[j*m+i];
-    return B;
+    
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            dest[i*n+j] = src[j*m+i];
+        }
+    }
 }
+
+function matmulCopy( dest, A,B,m,l,n) {
+    var i = 0;
+    var j = 0;
+    var k = 0;
+    
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            var sum = 0.0;
+            
+            for (k = 0; k < l; k++) {
+                sum += A[i*l+k]*B[k*n+j];
+            }
+            
+            dest[i*n+j] = sum;
+        }
+    }
+}
+
+function randmatstat(t) {
+    var n = 5;
+    
+    var P = new Float64Array( 4*n*n ),
+        Q = new Float64Array( 4*n*n );
+        
+    var PTransposed = new Float64Array( P.length ),
+        QTransposed = new Float64Array( Q.length );
+    
+    var PMatMul = new Float64Array( n*n ),
+        QMatMul = new Float64Array( (2*n) * (2*n) );
+    
+    var a = new Float64Array( n*n ),
+        b = new Float64Array( n*n ),
+        c = new Float64Array( n*n ),
+        d = new Float64Array( n*n );
+    
+    // the first n number of elements of a to d
+    var aSub = new Float64Array( n ),
+        bSub = new Float64Array( n ),
+        cSub = new Float64Array( n ),
+        dSub = new Float64Array( n );
+    
+    var v = new Float64Array( t ),
+        w = new Float64Array( t );
+    
+    var i = 0,
+        j = 0,
+        k = 0;
+    
+    for (i = 0; i < t; i++) {
+        a = randn( a, aSub );
+        b = randn( b, bSub );
+        c = randn( c, cSub );
+        d = randn( d, dSub );
+        
+        P.set( a, 0*n*n );
+        P.set( b, 1*n*n );
+        P.set( c, 2*n*n );
+        P.set( d, 3*n*n );
+        
+        for (j = 0; j < n; j++) {
+            Q.set( aSub, 2*n*j         );
+            Q.set( bSub, 2*n*j+n       );
+            Q.set( cSub, 2*n*(n+j)     );
+            Q.set( dSub, 2*n*(n+j)+n   );
+/*
+            for (k = 0; k < n; k++) {
+                Q[ 2*n*j        + k ] = a[k];
+                Q[ 2*n*j+n      + k ] = b[k];
+                Q[ 2*n*(n+j)    + k ] = c[k];
+                Q[ 2*n*(n+j)+n  + k ] = d[k];
+            }
+*/
+        }
+        
+        transpose( PTransposed, P, n, 4*n );
+        matmulCopy( PMatMul, PTransposed, P, n, 4*n, n );
+        matmulCopy( PMatMul, P, P, n, n, n);
+        matmulCopy( PMatMul, P, P, n, n, n);
+        
+        var trP = 0;
+        for (j = 0; j < n; j++) {
+            trP += PMatMul[(n+1)*j];
+        }
+        v[i] = trP;
+        
+        transpose( QTransposed, Q, 2*n, 2*n );
+        matmulCopy( QMatMul, QTransposed, Q, 2*n, 2*n, 2*n );
+        matmulCopy( QMatMul, Q, Q, 2*n, 2*n, 2*n);
+        matmulCopy( QMatMul, Q, Q, 2*n, 2*n, 2*n);
+        
+        var trQ = 0;
+        for (j = 0; j < 2*n; j++) {
+            trQ += QMatMul[(2*n+1)*j];
+        }
+        w[i] = trQ;
+    }
+    
+    var v1 = 0.0;
+    var v2 = 0.0;
+    var w1 = 0.0;
+    var w2 = 0.0;
+    for (i = 0; i < t; i++) {
+        v1 += v[i]; v2 += v[i]*v[i];
+        w1 += w[i]; w2 += w[i]*w[i];
+    }
+    
+    return {
+            s1: Math.sqrt((t*(t*v2-v1*v1))/((t-1)*v1*v1)),
+            s2: Math.sqrt((t*(t*w2-w1*w1))/((t-1)*w1*w1))
+    };
+}
+
+tmin = Number.POSITIVE_INFINITY;
+for (var i=0; i < 5; i++) {
+    t = (new Date).getTime();
+    for (var j=0; j < 10; j++) {
+        var r = randmatstat(1000);
+        // assert(0.5 < r.s1 < 1.0);
+//        assert(0.5 < r.s2 < 1.0);
+    }
+    t = (new Date).getTime()-t;
+    if (t < tmin) tmin = t;
+}
+console.log("javascript,rand_mat_stat," + tmin/10);
+
+// random matrix multiply //
+
 function matmul(A,B,m,l,n) {
     var C = new Array(m*n);
     var i = 0;
@@ -188,74 +326,6 @@ function matmul(A,B,m,l,n) {
     }
     return C;
 }
-
-function randmatstat(t) {
-    var n = 5;
-    var v = new Array(t);
-    var w = new Array(t);
-    var i = 0;
-    var j = 0;
-    var k = 0;
-    for (i = 0; i < t; i++) {
-        var a = randn(n*n);
-        var b = randn(n*n);
-        var c = randn(n*n);
-        var d = randn(n*n);
-        var P = new Array(4*n*n);
-        for (j = 0; j < n*n; j++) P[0*n*n+j] = a[j];
-        for (j = 0; j < n*n; j++) P[1*n*n+j] = b[j];
-        for (j = 0; j < n*n; j++) P[2*n*n+j] = c[j];
-        for (j = 0; j < n*n; j++) P[3*n*n+j] = d[j];
-        var Q = new Array(4*n*n);
-        for (j = 0; j < n; j++) {
-            for (k = 0; k < n; k++) {
-                Q[2*n*j+k]       = a[k];
-                Q[2*n*j+n+k]     = b[k];
-                Q[2*n*(n+j)+k]   = c[k];
-                Q[2*n*(n+j)+n+k] = d[k];
-            }
-        }
-        P = matmul(transpose(P, n, 4*n), P, n, 4*n, n);
-        P = matmul(P, P, n, n, n);
-        P = matmul(P, P, n, n, n);
-        var trP = 0.0;
-        for (j = 0; j < n; j++) trP += P[(n+1)*j];
-        v[i] = trP;
-        Q = matmul(transpose(Q, 2*n, 2*n), Q, 2*n, 2*n, 2*n);
-        Q = matmul(Q, Q, 2*n, 2*n, 2*n);
-        Q = matmul(Q, Q, 2*n, 2*n, 2*n);
-        var trQ = 0.0;
-        for (j = 0; j < 2*n; j++) trQ += Q[(2*n+1)*j];
-        v[i] = trQ;
-    }
-    var v1 = 0.0;
-    var v2 = 0.0;
-    var w1 = 0.0;
-    var w2 = 0.0;
-    for (i = 0; i < t; i++) {
-        v1 += v[i]; v2 += v[i]*v[i];
-        w1 += w[i]; w2 += w[i]*w[i];
-    }
-    var r = new Object();
-    r.s1 = Math.sqrt((t*(t*v2-v1*v1))/((t-1)*v1*v1));
-    r.s2 = Math.sqrt((t*(t*w2-w1*w1))/((t-1)*w1*w1));
-    return r;
-}
-
-tmin = Number.POSITIVE_INFINITY;
-for (var i=0; i < 5; i++) {
-    t = (new Date).getTime();
-    for (var j=0; j < 10; j++) {
-        var r = randmatstat(1000);
-        // assert(0.5 < r.s1 < 1.0);
-        assert(0.5 < r.s2 < 1.0);
-    }
-    t = (new Date).getTime()-t;
-    if (t < tmin) tmin = t;
-}
-console.log("javascript,rand_mat_stat," + tmin/10);
-
-// random matrix multiply //
 
 function randmatmul(n) {
     var A = rand(n*n);
