0724564a24ebe540ddacfdc4f77f0bc9141fe1cd
Corey M. Hoffstein <corey@newfoundresearch.com>
2012-03-06 21:55:22 -0500

    Fixed bigfloat test

--- jl/bigfloat.jl
+++ jl/bigfloat.jl
@@ -13,11 +13,7 @@ type BigFloat <: Float
 		b
 	end
 
-<<<<<<< HEAD
-	function BigFloat(x::Float64)
-=======
 	function BigFloat(x::Float64) 
->>>>>>> a3ed8da3ff56d920d870beb25f68e6bfe33a3a85
 		z = _jl_BigFloat_init()
 		ccall(dlsym(_jl_libgmp_wrapper, :_jl_mpf_set_d), Void, (Ptr{Void}, Float), z, x)
 		b = new(z)
@@ -56,27 +52,6 @@ type BigFloat <: Float
 	end
 end
 
-<<<<<<< HEAD
-convert(::Type{BigFloat}, x::Int8) = BigFloat(int(x))
-convert(::Type{BigFloat}, x::Int16) = BigFloat(int(x))
-convert(::Type{BigFloat}, x::Int) = BigFloat(x)
-
-macro define_bigfloat_convert ()
-	if WORD_SIZE == 64
-		:(convert(::Type{BigFloat}, x::Int32) = BigFloat(int(x)))
-		:(convert(::Type{BigFloat}, x::Uint32) = BigFloat(int(x)))
-	else
-		:(convert(::Type{BigFloat}, x::Int64) = BigFloat(string(x)))
-		:(convert(::Type{BigFloat}, x::Uint64) = BigFloat(string(x)))
-	end
-end
-
-@define_bigfloat_convert
-
-convert(::Type{BigFloat}, x::Uint8) = BigFloat(int(x))
-convert(::Type{BigFloat}, x::Uint16) = BigFloat(int(x))
-convert(::Type{BigFloat}, x::Float32) = BigFloat(float64(x))
-=======
 convert(::Type{BigFloat}, x::Int8)   = BigFloat(int(x))
 convert(::Type{BigFloat}, x::Int16)  = BigFloat(int(x))
 convert(::Type{BigFloat}, x::Int)  = BigFloat(x)
@@ -93,12 +68,19 @@ end
 @define_bigfloat_convert
 convert(::Type{BigFloat}, x::Uint8)  = BigFloat(int(x))
 convert(::Type{BigFloat}, x::Uint16) = BigFloat(int(x))
->>>>>>> a3ed8da3ff56d920d870beb25f68e6bfe33a3a85
 convert(::Type{BigFloat}, x::Float64) = BigFloat(x)
 convert(::Type{BigFloat}, x::Float32) = BigFloat(float64(x))
 
 promote_rule(::Type{BigFloat}, ::Type{Float32}) = BigFloat
 promote_rule(::Type{BigFloat}, ::Type{Float64}) = BigFloat
+promote_rule(::Type{BigFloat}, ::Type{Int8}) = BigFloat
+promote_rule(::Type{BigFloat}, ::Type{Int16}) = BigFloat
+promote_rule(::Type{BigFloat}, ::Type{Int32}) = BigFloat
+promote_rule(::Type{BigFloat}, ::Type{Int64}) = BigFloat
+promote_rule(::Type{BigFloat}, ::Type{Uint8}) = BigFloat
+promote_rule(::Type{BigFloat}, ::Type{Uint16}) = BigFloat
+promote_rule(::Type{BigFloat}, ::Type{Uint32}) = BigFloat
+promote_rule(::Type{BigFloat}, ::Type{Uint64}) = BigFloat
 
 function +(x::BigFloat, y::BigFloat) 
 	z= _jl_BigFloat_init()
