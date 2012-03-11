f30047f433b99a4e7a625c86c0effe387316e899
Jeff Bezanson <bezanson@post.harvard.edu>
2012-03-11 18:51:42 -0400

    adding 'static inline' to allow murmur to build in debug mode

--- src/support/MurmurHash3.c
+++ src/support/MurmurHash3.c
@@ -31,12 +31,12 @@
 
 #define	FORCE_INLINE __attribute__((always_inline))
 
-inline uint32_t rotl32 ( uint32_t x, int8_t r )
+static inline uint32_t rotl32 ( uint32_t x, int8_t r )
 {
   return (x << r) | (x >> (32 - r));
 }
 
-inline uint64_t rotl64 ( uint64_t x, int8_t r )
+static inline uint64_t rotl64 ( uint64_t x, int8_t r )
 {
   return (x << r) | (x >> (64 - r));
 }
