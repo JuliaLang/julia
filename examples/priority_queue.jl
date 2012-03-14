c7302f6b74622a4d0fae0a8e192601e9ec56b3ad
Tim Holy <tim.holy@gmail.com>
2012-03-14 06:52:57 -0500

    Cleanup of function/variable names and added many more comments.
    Details:
     - Changed the comparator name from "lessthan" to "cmp"
     - Changed variable names to be more neutral ("iextreme" rather than "imin")
     - Added many more comments, both to explain the overall duty of each
       function and internal notes about their algorithms

--- examples/priority_queue.jl
+++ examples/priority_queue.jl
@@ -1,12 +1,13 @@
-# These functions define a min-heap
+# These functions define a heap
 # Example usage of the Heap object:
-# You can create an empty min-heap this way:
+# You can create an empty heap this way:
 #   h = Heap(Float64)
-# This defaults to using isless for comparison.
+# This defaults to using < for comparison, so is a min-heap.
+#
 # A more complex and complete example, yielding a max-heap:
-#   h = Heap(>,rand(3))
+#   h = Heap(>,rand(3))  # initialize heap with 3 random points
 #   for i = 1:10
-#     push!(h,rand())
+#     push!(h,rand())    # add more points
 #   end
 #   length(h)
 #   max_val = pop!(h)
@@ -18,96 +19,112 @@
 #   min_val = heap_pop!(z)
 #   heap2rsorted!(z)
 
-
 ## Function definitions for operating on vectors ##
 
-function _heapify!{T}(lessthan::Function,x::Vector{T},i::Int,len::Int)
-    # Among node i and its two children, find the smallest
+
+# This is a "percolate down" function, used by several other
+# functions. "percolate up" is implemented in heap_push.
+function _heapify!{T}(cmp::Function,x::Vector{T},i::Int,len::Int)
+    # Among node i and its two children, find the extreme value
+    #(e.g., the smallest when using < for comparison)
     il = 2*i;  # index of left child
     if il > len
         return
     end
-    ismallest = lessthan(x[il],x[i]) ? il : i  # index of smallest child
+    iextreme = cmp(x[il],x[i]) ? il : i  # index of extreme value
     if il < len
-        ismallest = lessthan(x[il+1],x[ismallest]) ? il+1 : ismallest
+        iextreme = cmp(x[il+1],x[iextreme]) ? il+1 : iextreme
     end
-    if ismallest != i
-        # Put the smallest at i
-        tmp = x[ismallest]
-        x[ismallest] = x[i]
+    if iextreme != i
+        # Put the extreme value at i via a swap
+        tmp = x[iextreme]
+        x[iextreme] = x[i]
         x[i] = tmp
-        # Recursively re-heapify
-        _heapify!(lessthan,x,ismallest,len)
+        # Recursively re-heapify, starting at the swapped child node
+        _heapify!(cmp,x,iextreme,len)
     end
 end
 
-function vector2heap!{T}(lessthan::Function, x::Vector{T})
+# Convert an arbitrary vector into heap storage format
+function vector2heap!{T}(cmp::Function, x::Vector{T})
     for i = convert(Int,ifloor(length(x)/2)):-1:1
-        _heapify!(lessthan,x,i,length(x))
+        _heapify!(cmp,x,i,length(x))
     end
 end
 function vector2heap!{T}(x::Vector{T})
-    vector2heap!(isless,x)
+    vector2heap!(<,x)
 end
 
-function isheap{T}(lessthan::Function, x::Vector{T})
+# Test whether a vector is a valid heap
+function isheap{T}(cmp::Function, x::Vector{T})
     for i = 1:convert(Int,ifloor(length(x)/2))
         i2 = 2*i
-        if !lessthan(x[i],x[i2])
+        if !cmp(x[i],x[i2])
             return false
         end
-        if i2 < length(x) && !lessthan(x[i],x[i2+1])
+        if i2 < length(x) && !cmp(x[i],x[i2+1])
             return false
         end
     end
     return true
 end
 function isheap{T}(x::Vector{T})
-    isheap(isless,x)
+    isheap(<,x)
 end
 
-function heap_push!{T}(lessthan::Function, x::Vector{T},item::T)
-    # append new element at the bottom
+# Add a new item to a heap
+function heap_push!{T}(cmp::Function, x::Vector{T},item::T)
+    # Append new element at the bottom
     push(x,item)
-    # let the new item percolate up until it has a smaller parent
+    # Let the new item percolate up until stopped by a more-extreme parent
     i = length(x)
     ip = convert(Int,ifloor(i/2))  # index of parent
-    while i > 1 && lessthan(x[i],x[ip])
-        # swap i and its parent
+    while i > 1 && cmp(x[i],x[ip])
+        # Swap i and its parent
         tmp = x[ip]
         x[ip] = x[i]
         x[i] = tmp
-        # traverse up the tree
+        # Traverse up the tree
         i = ip
-        ip = convert(Int,ifloor(i/2))  # index of parent
+        ip = convert(Int,ifloor(i/2))
     end
 end
 function heap_push!{T}(x::Vector{T},item::T)
-    heap_push!(isless,x,item)
+    heap_push!(<,x,item)
 end
 
-function heap_pop!{T}(lessthan::Function, x::Vector{T})
-    min_item = x[1]
+# Remove the root node from the heap, leaving the remaining values in
+# a valid heap
+function heap_pop!{T}(cmp::Function, x::Vector{T})
+    # Save the value we want to return
+    extreme_item = x[1]
+    # We need to shorten the list, so replace the former top with the
+    # last item, then let it percolate down
     x[1] = x[end]
     pop(x)
-    _heapify!(lessthan,x,1,length(x))
-    return min_item
+    _heapify!(cmp,x,1,length(x))
+    return extreme_item
 end
 function heap_pop!{T}(x::Vector{T})
-    min_item = heap_pop!(isless,x)
-    return min_item
+    extreme_item = heap_pop!(<,x)
+    return extreme_item
 end
 
-function heap2rsorted!{T}(lessthan::Function, x::Vector{T})
+# From a heap, return a sorted vector. This is implemented efficiently
+# if the sorting is in the reverse order of the comparison function.
+function heap2rsorted!{T}(cmp::Function, x::Vector{T})
     for i = length(x):-1:2
+        # Swap the root with i, the last unsorted position
         tmp = x[1]
         x[1] = x[i]
         x[i] = tmp
-        _heapify!(lessthan,x,1,i-1)
+        # The heap portion now has length i-1, but needs fixing up
+        # starting with the root
+        _heapify!(cmp,x,1,i-1)
     end
 end
 function heap2rsorted!{T}(x::Vector{T})
-    heap2rsorted!(isless,x)
+    heap2rsorted!(<,x)
 end
 
 
@@ -115,27 +132,27 @@ end
 ## Heap object ##
 
 type Heap{T}
-    lessthan::Function
+    cmp::Function
     data::Array{T,1}
 
-    function Heap(lessthan::Function,x::Array{T,1})
+    function Heap(cmp::Function,x::Array{T,1})
         data = copy(x)
-        vector2heap!(lessthan,data)
-        new(lessthan,data)
+        vector2heap!(cmp,data)
+        new(cmp,data)
     end
 end
-Heap{T}(lessthan::Function,x::Vector{T}) = Heap{T}(lessthan,x)
-Heap{T}(x::Vector{T}) = Heap{T}(isless,x)
-Heap{T}(lessthan::Function,::Type{T}) = Heap{T}(lessthan,zeros(T,0))
-Heap{T}(::Type{T}) = Heap{T}(isless,zeros(T,0))
+Heap{T}(cmp::Function,x::Vector{T}) = Heap{T}(cmp,x)
+Heap{T}(x::Vector{T}) = Heap{T}(<,x)
+Heap{T}(cmp::Function,::Type{T}) = Heap{T}(cmp,zeros(T,0))
+Heap{T}(::Type{T}) = Heap{T}(<,zeros(T,0))
 
 function push!{T}(h::Heap{T},item::T)
-    heap_push!(h.lessthan,h.data,item)
+    heap_push!(h.cmp,h.data,item)
 end
 
 function pop!{T}(h::Heap{T})
-    min_item = heap_pop!(h.lessthan,h.data)
-    return min_item
+    extreme_item = heap_pop!(h.cmp,h.data)
+    return extreme_item
 end
 
 function length{T}(h::Heap{T})
