468dfd742a12edb8d54dc3625ffb591132ce48b5
Stefan Karpinski <stefan@karpinski.org>
2012-03-15 23:58:20 -0400

    Implement enumerate like in Python.
    Note: for indexable objects, this is kind of silly to use.

--- jl/iterator.jl
+++ jl/iterator.jl
@@ -1,24 +1,40 @@
 ## various useful iterator constructs ##
 
+# iterator which uses a thunk to create the iterator and another to destroy it
+
 type ShivaIterator
     create::Function
     destroy::Function
 end
 
-function start(ci::ShivaIterator)
-    itr = ci.create()
+function start(si::ShivaIterator)
+    itr = si.create()
     itr, start(itr)
 end
 
-function next(ci::ShivaIterator, state)
+function next(si::ShivaIterator, state)
     v, s = next(state...)
     v, (state[1], s)
 end
 
-function done(ci::ShivaIterator, state)
+function done(si::ShivaIterator, state)
     if done(state...)
-        ci.destroy()
+        si.destroy()
         return true
     end
     false
 end
+
+# like python's enumerate
+
+type Enumerate
+    itr
+end
+
+enumerate(itr) = Enumerate(itr)
+start(en::Enumerate) = (start(en.itr), 1)
+function next(en::Enumerate, state)
+    v, s = next(en.itr, state[1])
+    (v,state[2]), (s,state[2]+1)
+end
+done(en::Enumerate, state) = done(en.itr, state[1])
