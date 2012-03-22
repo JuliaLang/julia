a2556568dd5e4dda43b87c8cdd606e095ebb0e61
Patrick O'Leary <patrick.oleary@gmail.com>
2012-03-21 23:44:26 -0500

    ODE function cleanup, continued
    Added Cash-Karp coefficient option for ode45 as ode45_ck.
    Cleaned up coefficient blocks.
    Made array operations in ode45() look more like those in oderosenbrock() so common structure is more apparent.

--- extras/ode.jl
+++ extras/ode.jl
@@ -184,7 +184,7 @@ function oderkf{T}(F::Function, tspan::AbstractVector, x0::AbstractVector{T}, is
     tout = [t]            # first output time
     xout = x.'            # first output solution
         
-    k = zeros(eltype(x), (length(x),length(c)))
+    k = zeros(eltype(x), (length(c),length(x)))
     
     while (t < tfinal) & (h >= hmin)
         if t + h > tfinal
@@ -195,24 +195,24 @@ function oderkf{T}(F::Function, tspan::AbstractVector, x0::AbstractVector{T}, is
         # notes: k needs to end up as an Nxs, a is 7x6, which is s by (s-1),
         #        s is the number of intermediate RK stages on [t (t+h)] (Dormand-Prince has s=7 stages)
         if !isDormandPrince || t == tspan[1]
-            k[:,1] = F(t,x) # first stage
+            k[1,:] = F(t,x) # first stage
         else
             # Assign the last stage for x(k) as the first stage for computing x[k+1].
             # This is part of the Dormand-Prince pair caveat.
 	    # k[:,7] has already been computed, so use it instead of recomputing it
 	    # again as k[:,1] during the next step.
-            k[:,1]=k[:,end]
+            k[1,:] = k[end,:]
         end
 
         for j = 2:length(c)
-            k[:,j] = F(t + h.*c[j], x + h.*(a[j,1:j-1]*k[:,1:j-1]).')
+            k[j,:] = F(t + h.*c[j], x + h.*(a[j,1:j-1]*k[1:j-1,:]).')
         end
                 
 	# compute the 4th order estimate
-        x4 = x + h.*(k*b4) # k is Nxs (or Nx7) and b4 is a 7x1
+        x4 = x + h.*(b4*k).'
                     
         # compute the 5th order estimate
-        x5 = x + h.*(k*b5) # k is Nxs (or Nx7) and b5 is a 7x1
+        x5 = x + h.*(b5*k).'
                 
         # estimate the local truncation error
         gamma1 = x5 - x4
@@ -255,34 +255,50 @@ random_stuff =     [    0           0          0         0         0        0   
 #
 # Dormand-Prince coefficients
 dp_coefficients = (true,
-                   [    0           0          0         0         0        0    0
-                        1/5         0          0         0         0        0    0
-                        3/40        9/40       0         0         0        0    0
-                       44/45      -56/15      32/9       0         0        0    0
-                    19372/6561 -25360/2187 64448/6561 -212/729     0        0    0
-                     9017/3168   -355/33   46732/5247   49/176 -5103/18656  0    0
-                       35/384       0        500/1113  125/192 -2187/6784  11/84 0],
+                   [    0           0          0         0         0        0
+                        1/5         0          0         0         0        0
+                        3/40        9/40       0         0         0        0
+                       44/45      -56/15      32/9       0         0        0
+                    19372/6561 -25360/2187 64448/6561 -212/729     0        0
+                     9017/3168   -355/33   46732/5247   49/176 -5103/18656  0
+                       35/384       0        500/1113  125/192 -2187/6784  11/84],
                    # 4th order b-coefficients
-                   [5179/57600; 0; 7571/16695; 393/640; -92097/339200; 187/2100; 1/40],
+                   [5179/57600 0 7571/16695 393/640 -92097/339200 187/2100 1/40],
                    # 5th order b-coefficients
-                   [35/384; 0; 500/1113; 125/192; -2187/6784; 11/84; 0],
+                   [35/384 0 500/1113 125/192 -2187/6784 11/84 0],
                    )
 ode45_dp(F, tspan, x0) = oderkf(F, tspan, x0, dp_coefficients...)
 
 # Fehlberg coefficients
 fb_coefficients = (false,
-                   [    0         0          0         0        0    0
-                       1/4        0          0         0        0    0
-                       3/32       9/32       0         0        0    0
-                    1932/2197 -7200/2197  7296/2197    0        0    0
-                     439/216     -8       3680/513  -845/4104   0    0
-                      -8/27       2      -3544/2565 1859/4104 -11/40 0],
+                   [    0         0          0         0        0
+                       1/4        0          0         0        0
+                       3/32       9/32       0         0        0
+                    1932/2197 -7200/2197  7296/2197    0        0
+                     439/216     -8       3680/513  -845/4104   0
+                      -8/27       2      -3544/2565 1859/4104 -11/40],
                    # 4th order b-coefficients
-                   [25/216; 0; 1408/2565; 2197/4104; -1/5; 0],
+                   [25/216 0 1408/2565 2197/4104 -1/5 0],
                    # 5th order b-coefficients
-                   [16/135; 0; 6656/12825; 28561/56430; -9/50; 2/55]
+                   [16/135 0 6656/12825 28561/56430 -9/50 2/55],
                    )
 ode45_fb(F, tspan, x0) = oderkf(F, tspan, x0, fb_coefficients...)
+
+# Cash-Karp coefficients
+# Numerical Recipes in Fortran 77
+ck_coefficients = (false,
+                   [   0         0       0           0          0
+                       1/5       0       0           0          0
+                       3/40      9/40    0           0          0
+                       3/10     -9/10    6/5         0          0
+                     -11/54      5/2   -70/27       35/27       0
+                    1631/55296 175/512 575/13824 44275/110592 253/4096],
+                   # 4th order b-coefficients
+                   [37/378 0 250/621 125/594 0 512/1771],
+                   # 5th order b-coefficients
+                   [2825/27648 0 18575/48384 13525/55296 277/14336 1/4],
+                   )
+ode45_ck(F, tspan, x0) = oderkf(F, tspan, x0, ck_coefficients...)
 
 # Use Dormand Prince version of ode45 by default
 ode45 = ode45_dp
