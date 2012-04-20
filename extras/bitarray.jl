b9c08f764ecb4476d3b73639458565cc80c1dd88
Carlo Baldassi <carlobaldassi@gmail.com>
2012-04-20 18:31:32 +0200

    bitarray: added dot, triu, tril
    also a tentative implementation of aTb, kept
    commented because it's slow

--- extras/bitarray.jl
+++ extras/bitarray.jl
@@ -1730,3 +1730,64 @@ function cumprod{T}(v::BitVector{T})
     end
     return c
 end
+
+## Linear algebra
+
+function dot{T,S}(x::BitVector{T}, y::BitVector{S})
+    # simplest way to mimic generic dot behavior
+    s = zero(one(T) * one(S))
+    for i = 1 : length(x.chunks)
+        s += count_ones(x.chunks[i] & y.chunks[i])
+    end
+    return s
+end
+
+## slower than the unpacked version, which is MUCH slower
+#  than blas'd (this one saves storage though, keeping it commented
+#  just in case)
+#function aTb{T,S}(A::BitMatrix{T}, B::BitMatrix{S})
+    #(mA, nA) = size(A)
+    #(mB, nB) = size(B)
+    #C = zeros(promote_type(T,S), nA, nB)
+    #z = zero(eltype(C))
+    #if mA != mB; error("*: argument shapes do not match"); end
+    #if mA == 0; return C; end
+    #col_ch = _jl_num_bit_chunks(mA)
+    ## TODO: avoid using aux chunks and copy (?)
+    #aux_chunksA = zeros(Uint64, col_ch)
+    #aux_chunksB = [zeros(Uint64, col_ch) | j=1:nB]
+    #for j = 1:nB
+        #_jl_copy_chunks(aux_chunksB[j], 1, B.chunks, (j-1)*mA+1, mA)
+    #end
+    #for i = 1:nA
+        #_jl_copy_chunks(aux_chunksA, 1, A.chunks, (i-1)*mA+1, mA)
+        #for j = 1:nB
+            #for k = 1:col_ch
+                #C[i, j] += count_ones(aux_chunksA[k] & aux_chunksB[j][k])
+            #end
+        #end
+    #end
+    #return C
+#end
+
+#aCb{T, S}(A::BitMatrix{T}, B::BitMatrix{S}) = aTb(A, B)
+
+function triu{T}(B::BitMatrix{T}, k)
+    m,n = size(B)
+    A = bitzeros(T, m,n)
+    for i = max(k+1,1):n
+        j = clamp((i - 1) * m + 1, 1, i * m)
+        _jl_copy_chunks(A.chunks, j, B.chunks, j, min(i-k, m))
+    end
+    return A
+end
+
+function tril{T}(B::BitMatrix{T}, k)
+    m,n = size(B)
+    A = bitzeros(T, m, n)
+    for i = 1:min(n, m+k)
+        j = clamp((i - 1) * m + i - k, 1, i * m)
+        _jl_copy_chunks(A.chunks, j, B.chunks, j, max(m-i+k+1, 0))
+    end
+    return A
+end
