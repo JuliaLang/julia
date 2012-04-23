49d10f9ae375efc75fb19b69ebb8614790b142e3
Carlo Baldassi <carlobaldassi@gmail.com>
2012-04-23 17:02:32 +0200

    Added sparse diff

--- extras/linalg_sparse.jl
+++ extras/linalg_sparse.jl
@@ -105,7 +105,7 @@ function triu{Tv,Ti}(S::SparseMatrixCSC{Tv,Ti}, k::Int)
     rowval = Array(Ti, nnz)
     nzval = Array(Tv, nnz)
     A = SparseMatrixCSC{Tv,Ti}(m, n, colptr, rowval, nzval)
-    for col = max(k+1,1) : S.n
+    for col = max(k+1,1) : n
         c1 = S.colptr[col]
         for c2 = A.colptr[col] : A.colptr[col+1]-1
             A.rowval[c2] = S.rowval[c1]
@@ -150,3 +150,149 @@ function tril{Tv,Ti}(S::SparseMatrixCSC{Tv,Ti}, k::Int)
     return A
 end
 tril{Tv,Ti}(S::SparseMatrixCSC{Tv,Ti}, k::Integer) = tril(S, int(k))
+
+## diff
+
+function _jl_sparse_diff1{Tv,Ti}(S::SparseMatrixCSC{Tv,Ti})
+    m,n = size(S)
+    if m <= 1
+        return SparseMatrixCSC{Tv,Ti}(0, n, ones(n+1), Ti[], Tv[])
+    end
+    colptr = Array(Ti, n+1)
+    numnz = 2 * nnz(S) # upper bound; will shrink later
+    rowval = Array(Ti, numnz)
+    nzval = Array(Tv, numnz)
+    numnz = 0
+    colptr[1] = 1
+    for col = 1 : n
+        last_row = 0
+        last_val = 0
+        for k = S.colptr[col] : S.colptr[col+1]-1
+            row = S.rowval[k]
+            val = S.nzval[k]
+            if row > 1
+                if row == last_row + 1
+                    nzval[numnz] += val
+                    if nzval[numnz] == zero(Tv)
+                        numnz -= 1
+                    end
+                else
+                    numnz += 1
+                    rowval[numnz] = row - 1
+                    nzval[numnz] = val
+                end
+            end
+            if row < m
+                numnz += 1
+                rowval[numnz] = row
+                nzval[numnz] = -val
+            end
+            last_row = row
+            last_val = val
+        end
+        colptr[col+1] = numnz+1
+    end
+    del(rowval, numnz+1:length(rowval))
+    del(nzval, numnz+1:length(nzval))
+    return SparseMatrixCSC{Tv,Ti}(m-1, n, colptr, rowval, nzval)
+end
+
+function _jl_sparse_diff2{Tv,Ti}(a::SparseMatrixCSC{Tv,Ti})
+
+    m,n = size(a)
+    colptr = Array(Ti, max(n,1))
+    numnz = 2 * nnz(a) # upper bound; will shrink later
+    rowval = Array(Ti, numnz)
+    nzval = Array(Tv, numnz)
+
+    z = zero(Tv)
+
+    colptr_a = a.colptr
+    rowval_a = a.rowval
+    nzval_a = a.nzval
+
+    ptrS = 1
+    colptr[1] = 1
+
+    if n == 0
+        return SparseMatrixCSC{Tv,Ti}(m, n, colptr, rowval, nzval)
+    end
+
+    startA = colptr_a[1]
+    stopA = colptr_a[2]
+
+    rA = startA : stopA - 1
+    rowvalA = rowval_a[rA]
+    nzvalA = nzval_a[rA]
+    lA = stopA - startA
+
+    for col = 1:n-1
+        startB, stopB = startA, stopA
+        startA = colptr_a[col+1]
+        stopA = colptr_a[col+2]
+
+        rowvalB = rowvalA
+        nzvalB = nzvalA
+        lB = lA
+
+        rA = startA : stopA - 1
+        rowvalA = rowval_a[rA]
+        nzvalA = nzval_a[rA]
+        lA = stopA - startA
+
+        ptrB = 1
+        ptrA = 1
+
+        while ptrA <= lA && ptrB <= lB
+            rowA = rowvalA[ptrA]
+            rowB = rowvalB[ptrB]
+            if rowA < rowB
+                rowval[ptrS] = rowA
+                nzval[ptrS] = nzvalA[ptrA]
+                ptrS += 1
+                ptrA += 1
+            elseif rowB < rowA
+                rowval[ptrS] = rowB
+                nzval[ptrS] = -nzvalB[ptrB]
+                ptrS += 1
+                ptrB += 1
+            else
+                res = nzvalA[ptrA] - nzvalB[ptrB]
+                if res != z
+                    rowval[ptrS] = rowA
+                    nzval[ptrS] = res
+                    ptrS += 1
+                end
+                ptrA += 1
+                ptrB += 1
+            end
+        end
+
+        while ptrA <= lA
+            rowval[ptrS] = rowvalA[ptrA]
+            nzval[ptrS] = nzvalA[ptrA]
+            ptrS += 1
+            ptrA += 1
+        end
+
+        while ptrB <= lB
+            rowval[ptrS] = rowvalB[ptrB]
+            nzval[ptrS] = -nzvalB[ptrB]
+            ptrS += 1
+            ptrB += 1
+        end
+
+        colptr[col+1] = ptrS
+    end
+    del(rowval, ptrS:length(rowval))
+    del(nzval, ptrS:length(nzval))
+    return SparseMatrixCSC{Tv,Ti}(m, n-1, colptr, rowval, nzval)
+end
+
+function diff(a::SparseMatrixCSC, dim::Integer)
+    if dim == 1
+        _jl_sparse_diff1(a)
+    else
+        _jl_sparse_diff2(a)
+    end
+end
