93563f6ca276475146950a63bac76765b0ba2e9e
Jameson Nash <jameson@mit.edu>
2012-04-28 00:05:52 -0400

    multiline input in basic mode

--- ui/repl-basic.c
+++ ui/repl-basic.c
@@ -8,15 +8,10 @@ char *stdin_buf = NULL;
 unsigned long stdin_buf_len = 0;
 unsigned long stdin_buf_maxlen = 128;
 
-
 void init_repl_environment(void)
 {
 	stdin_buf = LLT_ALLOC(stdin_buf_maxlen);
-	int pipefd[2];
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }}
+}
 
 DLLEXPORT void jl_enable_color(void)
 {
@@ -25,52 +20,23 @@ DLLEXPORT void jl_enable_color(void)
 
 void jl_input_line_callback(char *input)
 {
-    jl_value_t *ast;
-    int end=0, doprint=1;
-
-    if (!input || ios_eof(ios_stdin)) {
-        end = 1;
-        ast = jl_nothing;
-    }
-    else {
-        ast = jl_parse_input_line(input);
-		LLT_FREE(input);
-        // TODO
-        //if (jl_is_expr(ast) && ((jl_expr_t*)ast)->head == jl_continue_sym)
-        doprint = !ends_with_semicolon(input);
-    }
-    handle_input(ast, end, doprint);
-}
-
-char *read_expr(char *prompt)
-{
-	perror("read_expr not supported");
-	exit(EXIT_FAILURE);
+    if (input) {
+        jl_value_t *ast = jl_parse_input_line(input);
+		int line_done = !ast || !jl_is_expr(ast) ||
+        	(((jl_expr_t*)ast)->head != jl_continue_sym);
+		if (line_done) {
+			int doprint = !ends_with_semicolon(input);
+			stdin_buf[0] = 0; //input[0] == 0
+			stdin_buf_len = 0;
+    		handle_input(ast, 0, doprint);
+		}
+	}
 }
 
 void repl_callback_enable()
 {
     ios_printf(ios_stdout, prompt_string);
     ios_flush(ios_stdout);
-}
-
-void basic_stdin_callback(void)
-{
-	char *start = stdin_buf;
-	int linelen = 0;
-	if (stdin_buf_len == 0) return;
-	while (*start != '\n') {
-		if (*start == 0) return;
-		linelen++;
-		start++;
-		if (linelen > stdin_buf_len) return;
-	}
-	char *input = LLT_ALLOC(linelen);
-	memcpy(input, stdin_buf, linelen);
-	input[linelen] = '\0';
-	stdin_buf_len = 0;
-	stdin_buf[0] = 0;
-    jl_input_line_callback(input);
 }
 
 void stdin_buf_pushc(char c) {
@@ -86,19 +52,29 @@ void stdin_buf_pushc(char c) {
 	stdin_buf_len++;
 }
 
+void basic_stdin_callback(void)
+{
+	stdin_buf_pushc('\0');
+	stdin_buf_len--;
+	jl_input_line_callback(stdin_buf);
+}
+
 void jl_readBuffer(uv_stream_t* stream, ssize_t nread, char *base, int buflen)
 {
 	char *start = base;
 	int esc = 0;
+	int newline = 0;
     while(*start != 0 && nread > 0) {
 		if (*start < 32 || *start == 127) {
 			switch (*start) {
 			case '\n':
 			case '\r':
+#ifdef __WIN32__
+				ios_putc('\r', ios_stdout);
+#endif
 				ios_putc('\n', ios_stdout);
-				ios_flush(ios_stdout);
 				stdin_buf_pushc('\n');
-				basic_stdin_callback();
+				newline = 1;
 				break;
 			case '\x03':
 				raise(SIGINT);
@@ -114,9 +90,9 @@ void jl_readBuffer(uv_stream_t* stream, ssize_t nread, char *base, int buflen)
 				break;
 			case 127:
 			case '\b':
-				if (stdin_buf_len > 0) {
+				if (stdin_buf_len > 0 && stdin_buf[stdin_buf_len-1] != '\n') {
 					stdin_buf_len--;
-					ios_write(ios_stdout,"\e[D \e[D",7);
+					ios_write(ios_stdout,"\b \b",3);
 				}
 			}
 		} else if (esc == 1) {
@@ -136,6 +112,7 @@ void jl_readBuffer(uv_stream_t* stream, ssize_t nread, char *base, int buflen)
         nread--;
 	}
 	ios_flush(ios_stdout);
+	if (newline) basic_stdin_callback();
 }
 
 void rl_clear_input(void)
