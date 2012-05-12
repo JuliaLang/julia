4de04ddc5e32643f55068a491c2d9304dcd3bcce
Stefan Karpinski <stefan@karpinski.org>
2012-05-12 13:06:17 -0400

    wip: slightly different approach to package checkpointing.

--- base/pkg.jl
+++ base/pkg.jl
@@ -8,12 +8,49 @@ function pkg_init(dir::String, meta::String)
     @chdir dir begin
         run(`git init`)
         run(`git commit --allow-empty -m "[jul] empty package repo"`)
-        pkg_install({"METADATA" => meta})
+        head = chomp(readall(`git rev-parse HEAD`))
+        sha1 = chomp(readall(`echo [checkpoint] $head` | `git commit-tree $head^{tree}`))
+        pkg_write_checkpoint(dir, sha1)
+        pkg_install(dir, {"METADATA" => meta})
+    end
+end
+pkg_init(dir::String) = pkg_init(dir, PKG_DEFAULT_META)
+pkg_init()            = pkg_init(PKG_DEFAULT_DIR)
+
+# checkpoint a repo, recording submodule commits as parents
+
+function pkg_write_checkpoint(dir::String, sha1::String)
+    @chdir dir begin
+        file = open(".git/refs/heads/checkpoints","w")
+        try println(file, sha1)
+        catch err
+            close(file)
+            throw(err)
+        end
+        close(file)
     end
 end
 
-pkg_init(dir::String) = pkg_init(dir, PKG_DEFAULT_META)
-pkg_init()            = pkg_init(PKG_DEFAULT_DIR)
+function pkg_checkpoint(dir::String, msg::String)
+    @chdir dir begin
+        run(`git commit -m $msg`)
+        head = chomp(readall(`git rev-parse HEAD`))
+        parents = [chomp(readall(".git/refs/heads/checkpoints"))]
+        for line in each_line(`git ls-tree $head`)
+            m = match(r"^160000 commit ([0-9a-f]{40})\b", line)
+            if m != nothing
+                push(parents, m.captures[1])
+            end
+        end
+        commit_tree = `git commit-tree $head^{tree}`
+        for parent in parents
+            commit_tree = `$commit_tree -p $parent`
+        end
+        commit = chomp(readall((`echo "[checkpoint] $head"` | commit_tree)))
+        pkg_write_checkpoint(dir, commit)
+    end
+end
+pkg_checkpoint(msg::String) = pkg_checkpoint(PKG_DEFAULT_DIR, msg)
 
 # install packages by name and, optionally, git url
 
@@ -23,13 +60,14 @@ function pkg_install(dir::String, urls::Associative)
     @chdir dir begin
         for pkg in names
             url = urls[pkg]
-            run(`git clone $url $pkg`)
-            run(`git add $pkg/`) # the slash is crucial
+            head = "master"
+            run(`git fetch $url +$head:packages/$pkg`)
+            run(`git clone --reference . -b $head $url $pkg`)
+            run(`git add $pkg`) # submodule without .gitmodules
         end
-        run(`git commit -m "[jul] install $(join(names, ", "))"`)
+        pkg_checkpoint(dir, "[jul] install "*join(names, ", "))
     end
 end
-
 function pkg_install(dir::String, names::AbstractVector)
     urls = Dict()
     @chdir dir for pkg in names
@@ -37,7 +75,6 @@ function pkg_install(dir::String, names::AbstractVector)
     end
     pkg_install(dir, urls)
 end
-
 pkg_install(urls::Associative)     = pkg_install(PKG_DEFAULT_DIR, urls)
 pkg_install(names::AbstractVector) = pkg_install(PKG_DEFAULT_DIR, names)
 pkg_install(names::String...)      = pkg_install([names...])
@@ -49,12 +86,11 @@ function pkg_remove(dir::String, names::AbstractVector)
     sort!(names)
     @chdir dir begin
         for pkg in names
-            run(`git rm -rf $pkg`)
-            run(`rm -rf $pkg`) # clean up anything left
+            run(`git rm --cached $pkg`)
+            run(`rm -rf $pkg`)
         end
-        run(`git commit -m "[jul] remove $(join(names, ", "))"`)
+        pkg_checkpoint(dir, "[jul] remove "*join(names, ", "))
     end
 end
-
 pkg_remove(names::AbstractVector) = pkg_remove(PKG_DEFAULT_DIR, names)
 pkg_remove(names::String...)      = pkg_remove([names...])
