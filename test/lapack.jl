98f549767525c2d76c11fb66e5fe4a53dd39bd01
Douglas Bates <dmbates@gmail.com>
2012-05-16 13:29:31 -0500

    Additional checks.

--- test/lapack.jl
+++ test/lapack.jl
@@ -1,28 +1,5 @@
 # blas, lapack
 Eps = sqrt(eps())
-n = 10
-a = rand(n,n)
-asym = a+a'+n*eye(n)
-b = rand(n)
-r = chol(asym)
-@assert norm(r'*r - asym) < Eps
-(l,u,p) = lu(a)
-@assert norm(l*u - a[p,:]) < Eps
-(q,r,s) = qr(a)
-@assert norm(q*r - a[:,s]) < Eps
-(d,v) = eig(asym)
-@assert norm(asym*v[:,1]-d[1]*v[:,1]) < Eps
-(d,v) = eig(a)
-@assert norm(a*v[:,1]-d[1]*v[:,1]) < Eps
-(U,s,vt) = svd(a)
-@assert norm(U*diagm(s)*vt - a) < Eps
-x = a \ b
-@assert norm(a*x - b) < Eps
-x = triu(a) \ b
-@assert norm(triu(a)*x - b) < Eps
-x = tril(a) \ b
-@assert norm(tril(a)*x - b) < Eps
-
 isPerm{T <: Integer}(p::Vector{T}) = all(int(1:length(p)) == int(sort(p)))
 
 function invPerm{T <: Integer}(p::Vector{T})
@@ -35,5 +12,37 @@ function invPerm{T <: Integer}(p::Vector{T})
     ip
 end
 
-@assert all(p == invPerm(invPerm(p)))
+n = 10
+a = rand(n,n)
+asym = a+a'+n*eye(n)
+b = rand(n)
+r = chol(asym)
+@assert norm(r'*r - asym) < Eps
+
+(l,u,p) = lu(a)
+@assert norm(l*u - a[p,:]) < Eps
+@assert all(p == invPerm(invPerm(p)))  # make sure invPerm works
 @assert norm(l[invPerm(p),:]*u - a) < 1e-8
+
+(q,r,p) = qr(a)
+@assert norm(q*r - a[:,p]) < Eps
+@assert norm(q*r[:,invPerm(p)] - a) < 1e-8
+
+(d,v) = eig(asym)
+@assert norm(asym*v[:,1]-d[1]*v[:,1]) < Eps
+
+(d,v) = eig(a)
+@assert norm(a*v[:,1]-d[1]*v[:,1]) < Eps
+
+(u,s,vt) = svd(a)
+@assert norm(u*diagm(s)*vt - a) < Eps
+@assert norm(u*diagmm(s,vt) - a) < Eps   # slightly cleaner calculation
+
+x = a \ b
+@assert norm(a*x - b) < Eps
+
+x = triu(a) \ b
+@assert norm(triu(a)*x - b) < Eps
+
+x = tril(a) \ b
+@assert norm(tril(a)*x - b) < Eps
