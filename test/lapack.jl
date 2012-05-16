d04541746a39c8d0ca54c9b453e7e6f093ea59f5
Douglas Bates <dmbates@gmail.com>
2012-05-16 13:18:04 -0500

    Parameterize comparison value.  Create and check the inverse permutations.

--- test/lapack.jl
+++ test/lapack.jl
@@ -1,23 +1,39 @@
 # blas, lapack
+Eps = sqrt(eps())
 n = 10
 a = rand(n,n)
 asym = a+a'+n*eye(n)
 b = rand(n)
 r = chol(asym)
-@assert norm(r'*r - asym) < 1e-8
+@assert norm(r'*r - asym) < Eps
 (l,u,p) = lu(a)
-@assert norm(l*u - a[p,:]) < 1e-8
-(q,r,p) = qr(a)
-@assert norm(q*r - a[:,p]) < 1e-8
+@assert norm(l*u - a[p,:]) < Eps
+(q,r,s) = qr(a)
+@assert norm(q*r - a[:,s]) < Eps
 (d,v) = eig(asym)
-@assert norm(asym*v[:,1]-d[1]*v[:,1]) < 1e-8
+@assert norm(asym*v[:,1]-d[1]*v[:,1]) < Eps
 (d,v) = eig(a)
-@assert norm(a*v[:,1]-d[1]*v[:,1]) < 1e-8
-(u,s,vt) = svd(a)
-@assert norm(u*diagm(s)*vt - a) < 1e-8
+@assert norm(a*v[:,1]-d[1]*v[:,1]) < Eps
+(U,s,vt) = svd(a)
+@assert norm(U*diagm(s)*vt - a) < Eps
 x = a \ b
-@assert norm(a*x-b) < 1e-8
+@assert norm(a*x - b) < Eps
 x = triu(a) \ b
-@assert norm(triu(a)*x-b) < 1e-8
+@assert norm(triu(a)*x - b) < Eps
 x = tril(a) \ b
-@assert norm(tril(a)*x-b) < 1e-8
+@assert norm(tril(a)*x - b) < Eps
+
+isPerm{T <: Integer}(p::Vector{T}) = all(int(1:length(p)) == int(sort(p)))
+
+function invPerm{T <: Integer}(p::Vector{T})
+    if !isPerm(p)
+        error("p must be a permutation of 1:length(p)")
+    end
+    lp = length(p)
+    ip = Array(Int, size(p))
+    for i=1:lp; ip[p[i]] = i; end
+    ip
+end
+
+@assert all(p == invPerm(invPerm(p)))
+@assert norm(l[invPerm(p),:]*u - a) < 1e-8
