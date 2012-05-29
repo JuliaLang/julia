5d5048bc188bff06d176543e96e0cc402d4b0cf5
Viral B. Shah <viral@mayin.org>
2012-05-29 09:45:32 +0530

    Attic files were too old, even for attic.

--- attic/utf32.jl
+++
@@ -1,7 +0,0 @@
-type UTF32String
-    data::Array{Int32,1}
-end
-
-length(s::UTF32String) = length(s.data)
-ref(s::UTF32String, i::Int) = s.data[i]
-assign(s::UTF32String, c::Int32, i::Int) = (s.data[i] = c)
