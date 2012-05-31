a4359fbfb0d2e461b5c5cb22c6db6998dfec4611
Mike Nolta <mike@nolta.net>
2012-05-31 16:53:27 -0400

    fix gcc47 warning: always_inline function might not be inlinable

--- src/support/MurmurHash3.c
+++ src/support/MurmurHash3.c
@@ -29,7 +29,7 @@
 
 #else	// defined(_MSC_VER)
 
-#define	FORCE_INLINE __attribute__((always_inline))
+#define	FORCE_INLINE inline __attribute__((always_inline))
 
 static inline uint32_t rotl32 ( uint32_t x, int8_t r )
 {
