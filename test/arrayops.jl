b0208934571612914b15f45011aa2c609be2d48f
Harlan Harris <harlan.harris@kaplan.com>
2012-06-19 09:12:09 -0400

    test cases for two-arg find()

--- test/arrayops.jl
+++ test/arrayops.jl
@@ -92,12 +92,15 @@ Y = [2, 1, 4, 3]
 a = [0,1,2,3,0,1,2,3]
 @assert find(a) == [2,3,4,6,7,8]
 @assert find(a.==2) == [3,7]
+@assert find(a,2) == [3,7]
+@assert find(a,isodd) == [2,4,6,8]
 @assert findfirst(a) == 2
 @assert findfirst(a.==0) == 1
 @assert findfirst(a.==5) == 0
 @assert findfirst([1,2,4,1,2,3,4], 3) == 6
 @assert findfirst([2,4,6,3,9,2,0], isodd) == 4
 @assert findfirst([2,4,6,2,0], isodd) == 0
+
 
 ## findn ##
 
