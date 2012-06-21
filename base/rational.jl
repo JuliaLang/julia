ea6c9cfd8a15e981de23a41576c8a497b700a498
Jeff Bezanson <jeff.bezanson@gmail.com>
2012-06-21 18:54:16 -0400

    fix silly comment typo in last commit

--- base/rational.jl
+++ base/rational.jl
@@ -142,7 +142,7 @@ rational(z::Complex) = complex(rational(real(z)), rational(imag(z)))
 rational(z::Complex, tol::Real) =
     (tol /= sqrt(2); complex(rational(real(z), tol), rational(imag(z), tol)))
 
-## float to rational coercion ##
+## rational to int coercion ##
 
 for f in (:int8, :int16, :int32, :int64, :int128,
           :uint8, :uint16, :uint32, :uint64, :uint128,
