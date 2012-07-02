JuliaLang/julia

commit 173399e27871be6f2c5020a2b25a68246b82f66a

ensure 16-byte alignment for array data. should fix #993

src/gc.c
@@ -36,11 +36,11 @@
 #define GC_PAGE_SZ (1536*sizeof(void*)+8)//bytes
 
 typedef struct _gcpage_t {
+    char data[GC_PAGE_SZ - 8];
     union {
         struct _gcpage_t *next;
         char _pad[8];
     };
-    char data[GC_PAGE_SZ - 8];
 } gcpage_t;
 
 typedef struct _gcval_t {
@@ -469,11 +469,7 @@ static void gc_markval_(jl_value_t *v)
     else if (((jl_struct_type_t*)(vt))->name == jl_array_typename) {
         jl_array_t *a = (jl_array_t*)v;
         int ndims = jl_array_ndims(a);
-        int ndimwords = (ndims > 2 ? (ndims-2) : 0);
-#ifndef __LP64__
-        // on 32-bit, ndimwords must be odd to preserve 8-byte alignment
-        ndimwords += (~ndimwords)&1;
-#endif
+        int ndimwords = jl_array_ndimwords(ndims);
         void *data_area = &a->_space[0] + ndimwords*sizeof(size_t);
         if (a->reshaped) {
             GC_Markval(*((jl_value_t**)data_area));

src/array.c
@@ -38,11 +38,7 @@ static jl_array_t *_new_array(jl_type_t *atype,
         tot = sizeof(void*) * nel;
     }
 
-    int ndimwords = (ndims > 2 ? (ndims-2) : 0);
-#ifndef __LP64__
-    // on 32-bit, ndimwords must be odd to preserve 8-byte alignment
-    ndimwords += (~ndimwords)&1;
-#endif
+    int ndimwords = jl_array_ndimwords(ndims);
     if (tot <= ARRAY_INLINE_NBYTES) {
         a = allocobj(sizeof(jl_array_t) + tot + (ndimwords-1)*sizeof(size_t));
         a->type = atype;
@@ -93,11 +89,7 @@ jl_array_t *jl_reshape_array(jl_type_t *atype, jl_array_t *data,
     jl_array_t *a;
     size_t ndims = jl_tuple_len(dims);
 
-    int ndimwords = (ndims > 2 ? (ndims-2) : 0);
-#ifndef __LP64__
-    // on 32-bit, ndimwords must be odd to preserve 8-byte alignment
-    ndimwords += (~ndimwords)&1;
-#endif
+    int ndimwords = jl_array_ndimwords(ndims);
     a = allocobj(sizeof(jl_array_t) + ndimwords*sizeof(size_t));
     a->type = atype;
     *((jl_array_t**)(&a->_space[0] + ndimwords*sizeof(size_t))) = data;
@@ -184,11 +176,7 @@ jl_array_t *jl_ptr_to_array(jl_type_t *atype, void *data, jl_tuple_t *dims,
     else
         elsz = sizeof(void*);
 
-    int ndimwords = (ndims > 2 ? (ndims-2) : 0);
-#ifndef __LP64__
-    // on 32-bit, ndimwords must be odd to preserve 8-byte alignment
-    ndimwords += (~ndimwords)&1;
-#endif
+    int ndimwords = jl_array_ndimwords(ndims);
     a = allocobj(sizeof(jl_array_t) + ndimwords*sizeof(size_t));
     a->type = atype;
     a->data = data;

src/julia.h
@@ -75,11 +75,13 @@ typedef struct {
     JL_STRUCT_TYPE
     void *data;
     size_t length;
+
     unsigned short ndims:14;
     unsigned short reshaped:1;
     unsigned short ptrarray:1;  // representation is pointer array
     uint16_t elsize;
     uint32_t offset;  // for 1-d only. does not need to get big.
+
     size_t nrows;
     union {
         // 1d
@@ -87,11 +89,25 @@ typedef struct {
         // Nd
         size_t ncols;
     };
+    // other dim sizes go here for ndims > 2
+
     union {
         char _space[1];
         void *_pad;
     };
 } jl_array_t;
+
+// compute # of extra words needed to store dimensions
+static inline int jl_array_ndimwords(uint32_t ndims)
+{
+#ifdef __LP64__
+    // on 64-bit, ndimwords must be even to give 16-byte alignment
+    return (ndims == 0 ? 0 : ((ndims-1) & -2));
+#else
+    // on 32-bit, ndimwords must = 4k+1 to give 16-byte alignment
+    return (ndims & -4) + 1;
+#endif
+}
 
 typedef struct _jl_type_t {
     JL_STRUCT_TYPE

commit 9d98f7bb7014b245b025c2c42e183e7818a76ccb

fix regression on 32-bit caused by commit 173399e27871be6f2c5020a2b25a68246b82f66a

src/gc.c
@@ -33,10 +33,10 @@
 #define BVOFFS 2
 #endif
 
-#define GC_PAGE_SZ (1536*sizeof(void*)+8)//bytes
+#define GC_PAGE_SZ (1536*sizeof(void*))//bytes
 
 typedef struct _gcpage_t {
-    char data[GC_PAGE_SZ - 8];
+    char data[GC_PAGE_SZ];
     union {
         struct _gcpage_t *next;
         char _pad[8];
@@ -285,7 +285,7 @@ static void add_page(pool_t *p)
     if (pg == NULL)
         jl_raise(jl_memory_exception);
     gcval_t *v = (gcval_t*)&pg->data[0];
-    char *lim = (char*)pg + GC_PAGE_SZ - p->osize;
+    char *lim = (char*)v + GC_PAGE_SZ - p->osize;
     gcval_t *fl;
     gcval_t **pfl = &fl;
     while ((char*)v <= lim) {
@@ -328,8 +328,8 @@ static void sweep_pool(pool_t *p)
     size_t osize = p->osize;
 
     while (pg != NULL) {
-        char *lim = (char*)pg + GC_PAGE_SZ - osize;
         v = (gcval_t*)&pg->data[0];
+        char *lim = (char*)v + GC_PAGE_SZ - osize;
         //empty = 1;
         freedall = 1;
         prev_pfl = pfl;
@@ -781,8 +781,8 @@ static size_t pool_stats(pool_t *p, size_t *pwaste)
 
     while (pg != NULL) {
         npgs++;
-        char *lim = (char*)pg + GC_PAGE_SZ - osize;
         v = (gcval_t*)&pg->data[0];
+        char *lim = (char*)v + GC_PAGE_SZ - osize;
         while ((char*)v <= lim) {
             if (!v->marked) {
                 nfree++;