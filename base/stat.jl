aca6665366c782174eb79049fa749a378e40935d
Stefan Karpinski <stefan@karpinski.org>
2012-07-02 12:18:39 -0400

    stat: some consistency and completeness touch-ups.

--- base/stat.jl
+++ base/stat.jl
@@ -28,63 +28,48 @@ Stat(buf::Vector{Uint8}) = Stat(
     ccall(:jl_stat_ctime,   Float64, (Ptr{Uint8},), buf),
 )
 
-const _jl_stat_buf = Array(Uint8, ccall(:jl_sizeof_stat, Int, ()))
-
-function stat(path::String)
-    ccall(:jl_stat, Int32, (Ptr{Uint8}, Ptr{Uint8}), path, _jl_stat_buf)
-    Stat(_jl_stat_buf)
-end
-function stat(fd::Integer)
-    ccall(:jl_fstat, Int32, (Int, Ptr{Uint8}), fd, _jl_stat_buf)
-    Stat(_jl_stat_buf)
-end
-function lstat(path::String)
-    ccall(:jl_lstat, Int32, (Ptr{Uint8}, Ptr{Uint8}), path, _jl_stat_buf)
-    Stat(_jl_stat_buf)
-end
-
 show(io::IO, st::Stat) = printf(f"Stat(mode=%06o,size=%i)\n", st.mode, st.size)
 
-# accesssor functions
+# stat & lstat functions
 
-filemode(path::String) = stat(path).mode
-filesize(path::String) = stat(path).size
-   mtime(path::String) = stat(path).mtime
-   ctime(path::String) = stat(path).ctime
+const _jl_stat_buf = Array(Uint8, ccall(:jl_sizeof_stat, Int, ()))
+macro _jl_stat_call(sym)
+    quote
+    fill!(_jl_stat_buf,0)
+    ccall($(expr(:quote,sym)), Int32, (Ptr{Uint8}, Ptr{Uint8}), path, _jl_stat_buf)
+    Stat(_jl_stat_buf)
+    end
+end
 
-# type predicates
+stat(path::String)  = @_jl_stat_call jl_stat
+stat(fd::Integer)   = @_jl_stat_call jl_fstat
+lstat(path::String) = @_jl_stat_call jl_lstat
 
-    isfifo(st::Stat) = st.mode & 0xf000 == 0x1000
- ischardev(st::Stat) = st.mode & 0xf000 == 0x2000
-     isdir(st::Stat) = st.mode & 0xf000 == 0x4000
-isblockdev(st::Stat) = st.mode & 0xf000 == 0x6000
-    isfile(st::Stat) = st.mode & 0xf000 == 0x8000
-    islink(st::Stat) = st.mode & 0xf000 == 0xa000
-  issocket(st::Stat) = st.mode & 0xf000 == 0xc000
+# mode type predicates
 
-# permission predicates
+    isfifo(mode::Unsigned) = mode & 0xf000 == 0x1000
+ ischardev(mode::Unsigned) = mode & 0xf000 == 0x2000
+     isdir(mode::Unsigned) = mode & 0xf000 == 0x4000
+isblockdev(mode::Unsigned) = mode & 0xf000 == 0x6000
+    isfile(mode::Unsigned) = mode & 0xf000 == 0x8000
+    islink(mode::Unsigned) = mode & 0xf000 == 0xa000
+  issocket(mode::Unsigned) = mode & 0xf000 == 0xc000
 
-  isreadable(mode::Unsigned) = (mode & 0x0124) > 0
- iswriteable(mode::Unsigned) = (mode & 0x0092) > 0
-isexecutable(mode::Unsigned) = (mode & 0x0049) > 0
-
-  isreadable(st::Stat) =   isreadable(st.mode)
- iswriteable(st::Stat) =  iswriteable(st.mode)
-isexecutable(st::Stat) = isexecutable(st.mode)
-
- user(mode::Unsigned) = uint8(mode >> 6) & 0x7
-group(mode::Unsigned) = uint8(mode >> 3) & 0x7
-world(mode::Unsigned) = uint8(mode     ) & 0x7
-
- user(st::Stat) =  user(st.mode)
-group(st::Stat) = group(st.mode)
-world(st::Stat) = world(st.mode)
+# mode permission predicates
 
 issetuid(mode::Unsigned) = (mode & 0x800) > 0
 issetgid(mode::Unsigned) = (mode & 0x400) > 0
 issticky(mode::Unsigned) = (mode & 0x200) > 0
 
-# methods for file names
+  isreadable(mode::Unsigned) = (mode & 0x0124) > 0
+ iswriteable(mode::Unsigned) = (mode & 0x0092) > 0
+isexecutable(mode::Unsigned) = (mode & 0x0049) > 0
+
+uperm(mode::Unsigned) = uint8(mode >> 6) & 0x7
+gperm(mode::Unsigned) = uint8(mode >> 3) & 0x7
+wperm(mode::Unsigned) = uint8(mode     ) & 0x7
+
+# mode predicate methods for file names & stat objects
 
 for f in {
     :isfifo
@@ -92,15 +77,27 @@ for f in {
     :isdir
     :isblockdev
     :isfile
+    :islink
     :issocket
+    :issetuid
+    :issetgid
+    :issticky
     :isreadable
     :iswriteable
     :isexecutable
-    :user
-    :group
-    :world
+    :uperm
+    :gperm
+    :wperm
 }
     @eval ($f)(path::String) = ($f)(stat(path))
+    @eval ($f)(st::Stat)     = ($f)(st.mode)
 end
 
 islink(path::String) = islink(lstat(path))
+
+# some convenience functions
+
+filemode(path::String) = stat(path).mode
+filesize(path::String) = stat(path).size
+   mtime(path::String) = stat(path).mtime
+   ctime(path::String) = stat(path).ctime
