bb619c30c073057f8b98efeafa610dad80671fb9
Keno Fischer <kenof@stanford.edu>
2012-07-08 14:43:45 -0400

    Fix readline error reporting

--- deps/readline-win.h
+++ deps/readline-win.h
@@ -1,8 +1,11 @@
 #ifndef READLINE_WIN_H
 #define READLINE_WIN_H
 
+extern void *jl_uv_stderr;
 #include <stdio.h>
 #define fprintf jl_printf
+#undef stderr
+#define stderr jl_uv_stderr
 #define fflush (void)
 #define putc jl_putc
 #define fwrite(ptr,size,count,stream) jl_write(stream,ptr,size*count)
