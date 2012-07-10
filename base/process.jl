JuliaLang/julia

commit ca7d415fae62b4dbce4ac0385aa92fdc10a83ec0

Fix #991 (too many open files)
Also, fixed show(io, ::FileDes) and spawn() error reporting

base/process.jl
@@ -40,7 +40,7 @@ show(io, fd::FileDes) =
     fd == STDIN  ? print(io, "STDIN")  :
     fd == STDOUT ? print(io, "STDOUT") :
     fd == STDERR ? print(io, "STDERR") :
-    invoke(show, (Any,), fd)
+    invoke(show, (Any, Any), io, fd)
 
 type Pipe
     in::FileDes
@@ -379,7 +379,7 @@ function spawn(cmd::Cmd)
                 # dup2 manually inlined to avoid potential heap stomping
                 r = ccall(:dup2, Int32, (Int32, Int32), dup2_fds[i], dup2_fds[i+1])
                 if r == -1
-                    println(stderr, "dup2: ", strerror())
+                    println(stderr_stream, "dup2: ", strerror())
                     exit(0xff)
                 end
                 i += 2
@@ -390,14 +390,14 @@ function spawn(cmd::Cmd)
                 # close manually inlined to avoid potential heap stomping
                 r = ccall(:close, Int32, (Int32,), close_fds[i])
                 if r != 0
-                    println(stderr, "close: ", strerror())
+                    println(stderr_stream, "close: ", strerror())
                     exit(0xff)
                 end
                 i += 1
             end
             if !isequal(ptrs, nothing)
                 ccall(:execvp, Int32, (Ptr{Uint8}, Ptr{Ptr{Uint8}}), ptrs[1], ptrs)
-                println(stderr, "exec: ", strerror())
+                println(stderr_stream, "exec: ", strerror())
                 exit(0xff)
             end
             # other ways of execing (e.g. a julia function)
@@ -484,6 +484,7 @@ function _readall(ports::Ports, cmds::Cmds)
     if !wait(cmds)
         pipeline_error(cmds)
     end
+    close(r)
     return o
 end
 

commit 57aac970bd0e08effa5f7d0464b6e742c2116f8f

process: fix bug in _readall
This was introduced by ca7d415fae62b4dbce4ac0385aa92fdc10a83ec0
and caused some weird errors with file descriptors.

base/process.jl
@@ -483,8 +483,9 @@ run(cmds::Cmds) = success(cmds) ? nothing : pipeline_error(cmds)
 function _readall(ports::Ports, cmds::Cmds)
     r = read_from(ports)
     spawn(cmds)
-    o = readall(fdio(r.fd, true))
+    o = readall(fdio(r.fd, false))
     if !wait(cmds)
+        close(r)
         pipeline_error(cmds)
     end
     close(r)