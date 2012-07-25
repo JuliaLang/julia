8c3fc4de2571e8b9a00855df02825198c2ffc58e
Lars Buitinck <L.J.Buitinck@uva.nl>
2012-07-25 17:42:17 +0200

    use NumPy's quicksort in Python benchmark
    Two orders of magnitude speedup.

--- test/perf/perf.py
+++ test/perf/perf.py
@@ -18,24 +18,7 @@ def fib(n):
 ## quicksort ##
 
 def qsort_kernel(a, lo, hi):
-    i = lo
-    j = hi
-    while i < hi:
-        pivot = a[(lo+hi) // 2]
-        while i <= j:
-            while a[i] < pivot:
-                i += 1
-            while a[j] > pivot:
-                j -= 1
-            if i <= j:
-                a[i], a[j] = a[j], a[i]
-                i += 1
-                j -= 1
-        if lo < j:
-            qsort_kernel(a, lo, j)
-        lo = i
-        j = hi
-    return a
+    return numpy.sort(a[lo:hi], kind="quicksort")
 
 ## randmatstat ##
 
