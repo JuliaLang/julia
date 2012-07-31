8a691219e824d8b00148b10a101481e642152218
Keno Fischer <kfischer@college.harvard.edu>
2012-07-31 13:20:30 +0200

    Fix #1097

--- extras/julia_web_base.jl
+++ extras/julia_web_base.jl
@@ -197,11 +197,11 @@ function __eval_exprs(__parsed_exprs)
 end
 
 # print version info
-println("Julia ", _jl_version_string)
-println(_jl_commit_string, "\n")
+println("Julia ", Base._jl_version_string)
+println(Base._jl_commit_string, "\n")
 
 # work around bug displaying "\n "
-#print("  ",replace(_jl_banner_plain, "\n", "\n  "))
+#print("  ",replace(Base._jl_banner_plain, "\n", "\n  "))
 
 ###########################################
 # wait forever while asynchronous processing happens
