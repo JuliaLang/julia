49724c1a49e3a23bb3dd5eaf2cad9eb13ad39e60
Viral B. Shah <viral@mayin.org>
2012-08-21 09:48:43 +0530

    Remove the cruft out of the scientific python benchmark. Retaining the rest for comparisons.

--- test/perf2/laplace/__laplace.c
+++
@@ -1,6 +0,0 @@
-void c_update(double **u, unsigned long rows, unsigned long cols, double dx2, double dy2) {
-    unsigned long i, j;
-    for (i=1; i<rows-1; i++)
-        for (j=1; j<cols-1; j++)
-            u[i][j] = ((u[i+1][ j] + u[i-1][ j]) * dy2 + (u[i][ j+1] + u[i][ j-1]) * dx2) * (1.0 / (2*(dx2+dy2)));
-}
