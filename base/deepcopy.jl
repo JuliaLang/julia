c451871961e939fcf1c76ee06730aefba0516f1a
Jeff Bezanson <jeff.bezanson@gmail.com>
2012-09-02 16:01:06 -0400

    some tweaks to deepcopy

--- base/deepcopy.jl
+++ base/deepcopy.jl
@@ -2,8 +2,8 @@
 deepcopy(x) = _deepcopy(x, ObjectIdDict())
 
 _deepcopy(x::Union(Symbol,Function,LambdaStaticData,
-              TopNode,QuoteNode,BitsKind,CompositeKind,AbstractKind,
-              UnionKind), stackdict::ObjectIdDict) = x
+                   TopNode,QuoteNode,BitsKind,CompositeKind,AbstractKind,
+                   UnionKind), stackdict::ObjectIdDict) = x
 _deepcopy(x::Tuple, stackdict::ObjectIdDict) =
     ntuple(length(x), i->_deepcopy(x[i], stackdict))
 _deepcopy(x::Module, stackdict::ObjectIdDict) = error("deepcopy of Modules not supported")
@@ -18,13 +18,13 @@ end
 _deepcopy_t(x, T::BitsKind, stackdict::ObjectIdDict) = x
 function _deepcopy_t(x, T::CompositeKind, stackdict::ObjectIdDict)
     nf = length(T.names)
-    dc_field(i::Int, stackdict) = _deepcopy(x.(T.names[i]), stackdict)
 
     ret = ccall(:jl_new_struct_uninit, Any, (Any,), T)
     stackdict[x] = ret
     for i=1:nf
         try
-            ccall(:jl_set_nth_field, Any, (Any, Int, Any), ret, i-1, dc_field(i, stackdict))
+            ccall(:jl_set_nth_field, Any, (Any, Int, Any),
+                  ret, i-1, _deepcopy(x.(T.names[i]), stackdict))
         catch err
             # we ignore undefined references errors
             if !isa(err, UndefRefError)
@@ -49,19 +49,22 @@ _deepcopy_array_t(x, T::BitsKind, stackdict::ObjectIdDict) = copy(x)
 function _deepcopy_array_t(x, T, stackdict::ObjectIdDict)
     dest = similar(x)
     stackdict[x] = dest
-    for i=1:length(x)
+    i0 = 1; local i
+    while true
         try
-            # NOTE: this works around the performance problem caused by all
-            # the doubled definitions of assign()
-            arrayset(dest, i, _deepcopy(x[i], stackdict))
+            for i=i0:length(x)
+                # NOTE: this works around the performance problem caused by all
+                # the doubled definitions of assign()
+                arrayset(dest, i, _deepcopy(x[i], stackdict))
+            end
+            break
         catch err
             # we ignore undefined references errors
             if !isa(err, UndefRefError)
                 throw(err)
             end
+            i0 = i+1
         end
     end
     return dest
 end
-
-
