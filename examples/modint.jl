ea478f1e189e26f2940d1abdba928ebd0af608bc
Jeff Bezanson <bezanson@post.harvard.edu>
2012-09-23 12:44:56 -0400

    add promote to ModInt example

--- examples/modint.jl
+++ examples/modint.jl
@@ -6,6 +6,8 @@ end
 +{N}(a::ModInt{N}, b::ModInt{N}) = ModInt{N}(a.k+b.k)
 *{N}(a::ModInt{N}, b::ModInt{N}) = ModInt{N}(a.k*b.k)
 
-convert{N}(::Type{ModInt{N}}, i::Integer) = ModInt{N}(i)
+convert{N}(::Type{ModInt{N}}, i::Int) = ModInt{N}(i)
+promote_rule{N}(::Type{ModInt{N}}, ::Type{Int}) = ModInt{N}
+
 show{n}(io, k::ModInt{n}) = println(io, "$(k.k) mod $n")
 showcompact(io, k::ModInt) = print(io, k.k)
