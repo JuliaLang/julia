944130cbd8b63de4778ad38101b43109f9fec70e
Jameson Nash <jameson@mit.edu>
2012-10-16 00:16:02 -0400

    #1385, I got the single processor special case backwards

--- base/multi.jl
+++ base/multi.jl
@@ -1439,7 +1439,7 @@ function pmap(f, lsts...)
     next_idx() = (idx=i; i+=1; idx)
     @sync begin
         for p=1:np
-            if p != myid() && np > 1
+            if p != myid() || np == 1
                 @spawnat myid() begin
                     while true
                         idx = next_idx()
