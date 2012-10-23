1d2beb6b97e55b7dce77215ab01411cf8dafd800
Kevin Squire <kevin.squire@gmail.com>
2012-10-22 18:24:09 -0700

    Added seek_end, position, truncate, eof, close, and bytestring methods to IOString.

--- extras/iostring.jl
+++ extras/iostring.jl
@@ -36,6 +36,13 @@ read{T}(from::IOString, ::Type{Ptr{T}}) = convert(Ptr{T}, read(from, Uint))
 
 skip(io::IOString, n::Integer) = io.ptr += n
 seek(io::IOString, n::Integer) = io.ptr = n+1
+seek_end(io::IOString) = io.ptr = length(io.data)+1
+position(io::IOString) = io.ptr-1
+truncate(io::IOString, n::Integer) = (grow(io.data, n-length(io.data)); ptr = min(ptr, n+1); uint(0))
+eof(io::IOString) = io.ptr-1 == length(io.data)
+close(io::IOString) = (grow(io.data, 0); ptr = 1; nothing)
+
+bytestring(io::IOString) = bytestring(io.data)
 
 function write{T}(to::IOString, a::Array{T})
     if isa(T, BitsKind)
