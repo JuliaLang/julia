8e77427d9fbfd1b897e70e63a6f5bca7445464e3
Jeff Bezanson <jeff.bezanson@gmail.com>
2012-10-27 16:45:28 -0400

    add test case from issue #1438

--- test/corelib.jl
+++ test/corelib.jl
@@ -109,6 +109,29 @@ end
 _d = {"a"=>0}
 @assert isa([k for k in filter(x->length(x)==1, keys(_d))], Vector{Any})
 
+# issue #1438
+type I1438T
+    id
+end
+hash(x::I1438T) = x.id
+
+begin
+    local seq, xs, s
+    seq = [26,28,29,30,31,32,33,34,35,36,-32,-35,-34,-28,37,38,39,40,-30,
+           -31,41,42,43,44,-33,-36,45,46,47,48,-37,-38,49,50,51,52,-46,-50,53]
+    xs = [ I1438T(id) for id=1:53 ]
+    s = Set()
+    for id in seq
+        if id > 0
+            x = xs[id]
+            add(s, x)
+            @assert has(s, x)                 # check that x can be found
+        else
+            del(s, xs[-id])
+        end
+    end
+end
+
 # #################### set ####################
 
 # Is there any good reason why sets (and dicts) do not implement
