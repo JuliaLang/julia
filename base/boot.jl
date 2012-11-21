JuliaLang/julia

commit 6f8f7b01cc8f53027450dc5b7df996d734248dd1

remove exports made unnecessary by my last change

base/boot.jl
@@ -116,7 +116,6 @@
 # end
 
 import Main
-import Intrinsics.ccall
 
 export ..., ANY, ASCIIString, AbstractArray, AbstractKind, Any, Array,
     BitsKind, Bool, BoundsError, Box, ByteString, Char, CompositeKind,
@@ -139,8 +138,7 @@ export ..., ANY, ASCIIString, AbstractArray, AbstractKind, Any, Array,
     # constants
     JULIA_HOME, nothing,
     # intrinsics module
-    Intrinsics,
-    ccall
+    Intrinsics
     #ccall, abs_float, add_float, add_int, and_int, ashr_int,
     #box, bswap_int, checked_fptosi32,
     #checked_fptosi64, checked_fptoui32, checked_fptoui64, checked_sadd,

base/export.jl
@@ -548,8 +548,6 @@ export
     cartesian_map,
     cat,
     cell,
-    cell_1d,
-    cell_2d,
     circshift,
     colon,
     conj!,
@@ -646,9 +644,7 @@ export
     ref_shape,
     assign_shape_check,
     indices,
-    append_any,
     make_loop_nest,
-    trailingsize,
     check_bounds, 
 
 # linear algebra
@@ -1260,7 +1256,6 @@ export
     hasenv,
     pointer,
     pointer_to_array,
-    ptr_arg_convert,
     cfunction,
     setenv,
     strerror,

commit a9dd07217580f74df1f17ab3920c721f04797b3d

fix a regression in 6f8f7b01cc8f53027450dc5b7df996d734248dd1

base/boot.jl
@@ -116,6 +116,7 @@
 # end
 
 import Main
+import Intrinsics.ccall
 
 export ..., ANY, ASCIIString, AbstractArray, AbstractKind, Any, Array,
     BitsKind, Bool, BoundsError, Box, ByteString, Char, CompositeKind,