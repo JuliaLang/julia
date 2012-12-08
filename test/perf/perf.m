ff8ed2a835165d42173cdfe51e7617523bc40f4f
Steven Pav <shabbychef@gmail.com>
2012-12-07 16:03:02 -0800

    fix row/column mixup
    not that it matters for performance, but Matlab is column-major,
    so the extant code was filling rows into the columns. reversing
    the order of the for loops fixes this issue.

--- test/perf/perf.m
+++ test/perf/perf.m
@@ -82,8 +82,8 @@ mandel(complex(-.53,.68));
 function M = mandelperf(ignore)
   M = zeros(length(-2.0:.1:0.5), length(-1:.1:1));
   count = 1;
-  for r = -2:0.1:0.5
-    for i = -1:.1:1
+  for i = -1:.1:1
+    for r = -2:0.1:0.5
       M(count) = mandel(complex(r,i));
       count = count + 1;
     end
