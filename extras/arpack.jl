69be3905b971ec50fdb9db6184837912a70f5c82
Viral B. Shah <viral@mayin.org>
2012-12-13 13:04:28 +0530

    Remove dlopen/dlsym from extras/arpack.jl

--- extras/arpack.jl
+++ extras/arpack.jl
@@ -2,11 +2,11 @@ module ARPACK
 
 export eigs, svds
 
-libarpack = dlopen("libarpack")
+const libarpack = "libarpack"
 
 # For a dense matrix A is ignored and At is actually A'*A
-_jl_sarupdate{T}(A::StridedMatrix{T}, At::StridedMatrix{T}, X::StridedVector{T}) = BLAS.symv('U', one(T), At, X)
-_jl_sarupdate{Tv,Ti}(A::SparseMatrixCSC{Tv,Ti}, At::SparseMatrixCSC{Tv,Ti}, X::StridedVector{Tv}) = At*(A*X)
+sarupdate{T}(A::StridedMatrix{T}, At::StridedMatrix{T}, X::StridedVector{T}) = BLAS.symv('U', one(T), At, X)
+sarupdate{Tv,Ti}(A::SparseMatrixCSC{Tv,Ti}, At::SparseMatrixCSC{Tv,Ti}, X::StridedVector{Tv}) = At*(A*X)
 
 for (T, saupd, seupd, naupd, neupd) in
     ((:Float64, :dsaupd_, :dseupd_, :dnaupd_, :dneupd_),
@@ -44,14 +44,14 @@ for (T, saupd, seupd, naupd, neupd) in
 
             while true
                 if sym
-                    ccall(dlsym(libarpack, $(string(saupd))), Void,
+                    ccall(($(string(saupd)), libarpack), Void,
                           (Ptr{Int32}, Ptr{Uint8}, Ptr{Int32}, Ptr{Uint8}, Ptr{Int32},
                            Ptr{$T}, Ptr{$T}, Ptr{Int32}, Ptr{$T}, Ptr{Int32},
                            Ptr{Int32}, Ptr{Int32}, Ptr{$T}, Ptr{$T}, Ptr{Int32}, Ptr{Int32}),
                           ido, bmat, &n, evtype, &nev, tol, resid, &ncv, v, &n, 
                           iparam, ipntr, workd, workl, &lworkl, info)
                 else
-                    ccall(dlsym(libarpack, $(string(naupd))), Void,
+                    ccall(($(string(naupd)), libarpack), Void,
                           (Ptr{Int32}, Ptr{Uint8}, Ptr{Int32}, Ptr{Uint8}, Ptr{Int32},
                            Ptr{$T}, Ptr{$T}, Ptr{Int32}, Ptr{$T}, Ptr{Int32},
                            Ptr{Int32}, Ptr{Int32}, Ptr{$T}, Ptr{$T}, Ptr{Int32}, Ptr{Int32}),
@@ -68,7 +68,7 @@ for (T, saupd, seupd, naupd, neupd) in
             if sym
                 d = Array($T, nev)
                 sigma = zeros($T, 1)
-                ccall(dlsym(libarpack, $(string(seupd))), Void,
+                ccall(($(string(seupd)), libarpack), Void,
                       (Ptr{Int32}, Ptr{Uint8}, Ptr{Int32}, Ptr{$T}, Ptr{$T}, Ptr{Int32},
                        Ptr{$T}, Ptr{Uint8}, Ptr{Int32}, Ptr{Uint8}, Ptr{Int32},
                        Ptr{$T}, Ptr{$T}, Ptr{Int32}, Ptr{$T}, Ptr{Int32}, Ptr{Int32},
@@ -84,7 +84,7 @@ for (T, saupd, seupd, naupd, neupd) in
             sigmar = zeros($T, 1)
             sigmai = zeros($T, 1)
             workev = Array($T, 3*ncv)
-            ccall(dlsym(libarpack, $(string(neupd))), Void,
+            ccall(($(string(neupd)), libarpack), Void,
                   (Ptr{Int32}, Ptr{Uint8}, Ptr{Int32}, Ptr{$T}, Ptr{$T}, Ptr{$T},
                    Ptr{Int32}, Ptr{$T}, Ptr{$T}, Ptr{$T}, Ptr{Uint8}, Ptr{Int32},
                    Ptr{Uint8}, Ptr{Int32}, Ptr{$T}, Ptr{$T}, Ptr{Int32}, Ptr{$T},
@@ -146,7 +146,7 @@ for (T, TR, naupd, neupd) in
            zernm1 = 0:(n-1)
 
            while true
-               ccall(dlsym(libarpack, $(string(naupd))), Void,
+               ccall(($(string(naupd)), libarpack), Void,
                          (Ptr{Int32}, Ptr{Uint8}, Ptr{Int32}, Ptr{Uint8}, Ptr{Int32},
                           Ptr{$TR}, Ptr{$T}, Ptr{Int32}, Ptr{$T}, Ptr{Int32},
                           Ptr{Int32}, Ptr{Int32}, Ptr{$T}, Ptr{$T}, Ptr{Int32},
@@ -163,7 +163,7 @@ for (T, TR, naupd, neupd) in
            d = Array($T, nev+1)
            sigma = zeros($T, 1)
            workev = Array($T, 2ncv)
-           ccall(dlsym(libarpack, $(string(neupd))), Void,
+           ccall(($(string(neupd)), libarpack), Void,
                  (Ptr{Int32}, Ptr{Uint8}, Ptr{Int32}, Ptr{$T}, Ptr{$T}, Ptr{Int32},
                   Ptr{$T}, Ptr{$T}, Ptr{Uint8}, Ptr{Int32}, Ptr{Uint8}, Ptr{Int32},
                   Ptr{$TR}, Ptr{$T}, Ptr{Int32}, Ptr{$T}, Ptr{Int32}, Ptr{Int32},
@@ -185,8 +185,8 @@ eigs(A::AbstractMatrix) = eigs(A, 6, "LM", true)
 
 
 # For a dense matrix A is ignored and At is actually A'*A
-_jl_sarupdate{T}(A::StridedMatrix{T}, At::StridedMatrix{T}, X::StridedVector{T}) = BLAS.symv('U', one(T), At, X)
-_jl_sarupdate{Tv,Ti}(A::SparseMatrixCSC{Tv,Ti}, At::SparseMatrixCSC{Tv,Ti}, X::StridedVector{Tv}) = At*(A*X)
+sarupdate{T}(A::StridedMatrix{T}, At::StridedMatrix{T}, X::StridedVector{T}) = BLAS.symv('U', one(T), At, X)
+sarupdate{Tv,Ti}(A::SparseMatrixCSC{Tv,Ti}, At::SparseMatrixCSC{Tv,Ti}, X::StridedVector{Tv}) = At*(A*X)
 
 for (T, saupd, seupd) in ((:Float64, :dsaupd_, :dseupd_), (:Float32, :ssaupd_, :sseupd_))
    @eval begin
@@ -219,7 +219,7 @@ for (T, saupd, seupd) in ((:Float64, :dsaupd_, :dseupd_), (:Float32, :ssaupd_, :
            zernm1 = 0:(n-1)
 
            while true
-               ccall(dlsym(libarpack, $(string(saupd))), Void,
+               ccall(($(string(saupd)), libarpack), Void,
                      (Ptr{Int32}, Ptr{Uint8}, Ptr{Int32}, Ptr{Uint8}, Ptr{Int32},
                       Ptr{$T}, Ptr{$T}, Ptr{Int32}, Ptr{$T}, Ptr{Int32},
                       Ptr{Int32}, Ptr{Int32}, Ptr{$T}, Ptr{$T}, Ptr{Int32}, Ptr{Int32}),
@@ -227,13 +227,13 @@ for (T, saupd, seupd) in ((:Float64, :dsaupd_, :dseupd_), (:Float32, :ssaupd_, :
                      iparam, ipntr, workd, workl, &lworkl, info)
                if (info[1] < 0) error("error code $(info[1]) from ARPACK saupd") end
                if (ido[1] != -1 && ido[1] != 1) break end
-               workd[ipntr[2]+zernm1] = _jl_sarupdate(A, At, ref(workd, ipntr[1]+zernm1))
+               workd[ipntr[2]+zernm1] = sarupdate(A, At, ref(workd, ipntr[1]+zernm1))
            end
 
            d      = Array($T, nev)
            howmny = "A"
 
-           ccall(dlsym(libarpack, $(string(seupd))), Void,
+           ccall(($(string(seupd)), libarpack), Void,
                   (Ptr{Int32}, Ptr{Uint8}, Ptr{Int32}, Ptr{$T}, Ptr{$T}, Ptr{Int32}, Ptr{$T},
                    Ptr{Uint8}, Ptr{Int32}, Ptr{Uint8}, Ptr{Int32},
                    Ptr{$T}, Ptr{$T}, Ptr{Int32}, Ptr{$T}, Ptr{Int32}, Ptr{Int32},
