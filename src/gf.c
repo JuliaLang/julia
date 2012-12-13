JuliaLang/julia

commit ec56c3d89c8294782f7634549528e2545daa7ade

warning for overwriting a method from a different module. close #1737

src/gf.c
@@ -1078,6 +1078,23 @@ jl_methlist_t *jl_method_list_insert(jl_methlist_t **pml, jl_tuple_t *type,
         if (((l->tvars==jl_null) == (tvars==jl_null)) &&
             sigs_eq((jl_value_t*)type, (jl_value_t*)l->sig)) {
             // method overwritten
+            jl_module_t *newmod = method->linfo->module;
+            if (check_amb && l->func->linfo &&
+                (l->func->linfo->module != newmod) &&
+                // special case: allow adding Array() methods in Base
+                (pml != &((jl_methtable_t*)jl_array_type->env)->defs ||
+                 newmod != jl_base_module)) {
+                jl_value_t *errstream = jl_stderr_obj();
+                ios_t *s = JL_STDERR;
+                ios_printf(s, "Warning: Method definition %s", method->linfo->name->name);
+                jl_show(errstream, (jl_value_t*)type);
+                ios_printf(s, " in module %s", l->func->linfo->module->name->name);
+                print_func_loc(s, l->func->linfo);
+                ios_printf(s, " overwritten in module %s",
+                           newmod->name->name);
+                print_func_loc(s, method->linfo);
+                ios_printf(s, ".\n");
+            }
             JL_SIGATOMIC_BEGIN();
             l->sig = type;
             l->tvars = tvars;

base/math.jl
@@ -88,7 +88,7 @@ for f in (:cbrt, :sin, :cos, :tan, :sinh, :cosh, :tanh, :asin, :acos, :atan,
     end
 end
 
-for f in (:log1p, :logb, :expm1, :ceil, :floor, :trunc, :round, :significand) # :rint, :nearbyint
+for f in (:log1p, :logb, :expm1, :ceil, :trunc, :round, :significand) # :rint, :nearbyint
     @eval begin
         ($f)(x::Float64) = ccall(($(string(f)),:libopenlibm), Float64, (Float64,), x)
         ($f)(x::Float32) = ccall(($(string(f,"f")),:libopenlibm), Float32, (Float32,), x)
@@ -96,6 +96,10 @@ for f in (:log1p, :logb, :expm1, :ceil, :floor, :trunc, :round, :significand) # 
         @vectorize_1arg Real $f
     end
 end
+
+floor(x::Float32) = ccall((:floorf, :libopenlibm), Float32, (Float32,), x)
+floor(x::Real) = floor(float(x))
+@vectorize_1arg Real floor
 
 atan2(x::Real, y::Real) = atan2(float64(x), float64(y))
 

commit ccf7c3a1f6d7f849bd43d444f59f77822e87cee8

fix crash caused by ec56c3d

src/gf.c
@@ -1078,12 +1078,12 @@ jl_methlist_t *jl_method_list_insert(jl_methlist_t **pml, jl_tuple_t *type,
         if (((l->tvars==jl_null) == (tvars==jl_null)) &&
             sigs_eq((jl_value_t*)type, (jl_value_t*)l->sig)) {
             // method overwritten
-            jl_module_t *newmod = method->linfo->module;
-            if (check_amb && l->func->linfo &&
-                (l->func->linfo->module != newmod) &&
+            if (check_amb && l->func->linfo && method->linfo &&
+                (l->func->linfo->module != method->linfo->module) &&
                 // special case: allow adding Array() methods in Base
                 (pml != &((jl_methtable_t*)jl_array_type->env)->defs ||
-                 newmod != jl_base_module)) {
+                 method->linfo->module != jl_base_module)) {
+                jl_module_t *newmod = method->linfo->module;
                 jl_value_t *errstream = jl_stderr_obj();
                 ios_t *s = JL_STDERR;
                 ios_printf(s, "Warning: Method definition %s", method->linfo->name->name);