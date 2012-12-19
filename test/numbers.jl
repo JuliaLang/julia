JuliaLang/julia

commit 6e1f7ed9124de0024e56f80c7b6798dbc5b0dd9e

update show to use new Float32 literal syntax
Also, print NaN32/Inf32 instead of NaN/Inf when arg is Float32.

base/grisu.jl
@@ -50,20 +50,17 @@ function grisu_sig(x::Real, n::Integer)
 end
 
 function _show(io, x::FloatingPoint, mode::Int32, n::Int)
-    if isnan(x); return write(io, "NaN"); end
-    if isinf(x); return write(io, x < 0 ? "-Inf" : "Inf"); end
+    if isnan(x); return write(io, isa(x,Float32) ? "NaN32" : "NaN"); end
+    if x < 0 write(io,'-') end
+    if isinf(x); return write(io, isa(x,Float32) ? "Inf32" : "Inf"); end
     @grisu_ccall x mode n
     pdigits = pointer(DIGITS)
-    neg = NEG[1]
     len = LEN[1]
     pt  = POINT[1]
     if mode == PRECISION
         while len > 1 && DIGITS[len] == '0'
             len -= 1
         end
-    end
-    if neg
-        write(io, '-')
     end
     if pt <= -4 || pt > 6 # .00001 to 100000.
         # => #.#######e###
@@ -74,8 +71,9 @@ function _show(io, x::FloatingPoint, mode::Int32, n::Int)
         else
             write(io, '0')
         end
-        write(io, 'e')
+        write(io, isa(x,Float32) ? 'f' : 'e')
         write(io, dec(pt-1))
+        return
     elseif pt <= 0
         # => 0.00########
         write(io, "0.")
@@ -97,6 +95,7 @@ function _show(io, x::FloatingPoint, mode::Int32, n::Int)
         write(io, '.')
         write(io, pdigits+pt, len-pt)
     end
+    if isa(x,Float32) write(io, "f0") end
     nothing
 end
 
@@ -114,23 +113,21 @@ showcompact(io, x::FloatingPoint) = _show(io, x, PRECISION, 6)
 #   0 < pt              ########e###        len+k+1
 
 function _print_shortest(io, x::FloatingPoint, dot::Bool, mode::Int32)
-    if isnan(x); return write(io, "NaN"); end
-    if isinf(x); return write(io, x < 0 ? "-Inf" : "Inf"); end
+    if isnan(x); return write(io, isa(x,Float32) ? "NaN32" : "NaN"); end
+    if x < 0 write(io,'-') end
+    if isinf(x); return write(io, isa(x,Float32) ? "Inf32" : "Inf"); end
     @grisu_ccall x mode 0
     pdigits = pointer(DIGITS)
-    neg = NEG[1]
     len = LEN[1]
     pt  = POINT[1]
-    if neg
-        write(io, '-')
-    end
     e = pt-len
     k = -9<=e<=9 ? 1 : 2
     if -pt > k+1 || e+dot > k+1
         # => ########e###
         write(io, pdigits+0, len)
-        write(io, 'e')
+        write(io, isa(x,Float32) ? 'f' : 'e')
         write(io, dec(e))
+        return
     elseif pt <= 0
         # => .000########
         write(io, '.')
@@ -154,6 +151,7 @@ function _print_shortest(io, x::FloatingPoint, dot::Bool, mode::Int32)
         write(io, '.')
         write(io, pdigits+pt, len-pt)
     end
+    if isa(x,Float32) write(io, "f0") end
     nothing
 end
 

commit 6ebd23c1b1e2de9d42a5f45c21d27e9bbe06b2a5

negative zero: make sure we print -0.0 with a leading minus sign.
This apparently regressed in 6e1f7ed9124de0024e56f80c7b6798dbc5b0dd9e
and no one noticed since! I guess that shows how much people care
about -0.0, but now there are tests, which there should have been in
the first place.

base/grisu.jl
@@ -47,11 +47,15 @@ function grisu_sig(x::Real, n::Integer)
 end
 
 function _show(io, x::FloatingPoint, mode::Int32, n::Int)
-    if isnan(x); return write(io, isa(x,Float32) ? "NaN32" : "NaN"); end
-    if x < 0 write(io,'-') end
-    if isinf(x); return write(io, isa(x,Float32) ? "Inf32" : "Inf"); end
+    if isnan(x) return write(io, isa(x,Float32) ? "NaN32" : "NaN") end
+    if isinf(x)
+        if x < 0 write(io,'-') end
+        write(io, isa(x,Float32) ? "Inf32" : "Inf")
+        return
+    end
     @grisu_ccall x mode n
     pdigits = pointer(DIGITS)
+    neg = NEG[1]
     len = LEN[1]
     pt  = POINT[1]
     if mode == PRECISION
@@ -59,6 +63,7 @@ function _show(io, x::FloatingPoint, mode::Int32, n::Int)
             len -= 1
         end
     end
+    if neg write(io,'-') end
     if pt <= -4 || pt > 6 # .00001 to 100000.
         # => #.#######e###
         write(io, pdigits, 1)

test/numbers.jl
@@ -212,6 +212,35 @@ if WORD_SIZE > 32
 @test base(12,typemax(Int128)) == "2a695925806818735399a37a20a31b3534a7"
 end
 
+# floating-point printing
+@test repr(1.0) == "1.0"
+@test repr(-1.0) == "-1.0"
+@test repr(0.0) == "0.0"
+@test repr(-0.0) == "-0.0"
+@test repr(0.1) == "0.1"
+@test repr(0.2) == "0.2"
+@test repr(0.3) == "0.3"
+@test repr(0.1+0.2) != "0.3"
+@test repr(Inf) == "Inf"
+@test repr(-Inf) == "-Inf"
+@test repr(NaN) == "NaN"
+@test repr(-NaN) == "NaN"
+@test repr(pi) == "3.141592653589793"
+
+@test repr(1.0f0) == "1.0f0"
+@test repr(-1.0f0) == "-1.0f0"
+@test repr(0.0f0) == "0.0f0"
+@test repr(-0.0f0) == "-0.0f0"
+@test repr(0.1f0) == "0.1f0"
+@test repr(0.2f0) == "0.2f0"
+@test repr(0.3f0) == "0.3f0"
+@test repr(0.1f0+0.2f0) == "0.3f0"
+@test repr(Inf32) == "Inf32"
+@test repr(-Inf32) == "-Inf32"
+@test repr(NaN32) == "NaN32"
+@test repr(-NaN32) == "NaN32"
+@test repr(float32(pi)) == "3.1415927f0"
+
 # signs
 @test sign(1) == 1
 @test sign(-1) == -1