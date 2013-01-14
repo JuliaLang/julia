efd3ab9a2781930920c60e487cff09920fd2d83b
Stefan Karpinski <stefan@karpinski.org>
2013-01-14 15:24:25 -0500

    base/deprecated.jl: cosmetic whitespace touchup.

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -9,28 +9,28 @@ macro deprecate(oldf,newf)
           end))
 end
 
-@deprecate  push     push!
-@deprecate  pop      pop!
-@deprecate  grow     grow!
-@deprecate  enqueue  unshift!
-@deprecate  unshift  unshift!
-@deprecate  shift    shift!
-@deprecate  insert   insert!
-@deprecate  del      delete!
-@deprecate  del_all  empty!
+@deprecate  push        push!
+@deprecate  pop         pop!
+@deprecate  grow        grow!
+@deprecate  enqueue     unshift!
+@deprecate  unshift     unshift!
+@deprecate  shift       shift!
+@deprecate  insert      insert!
+@deprecate  del         delete!
+@deprecate  del_all     empty!
 
-@deprecate  load     require
-@deprecate  numel    length
-@deprecate  idump    xdump
-@deprecate  cwd      pwd
-@deprecate  strlen   length
+@deprecate  load        require
+@deprecate  numel       length
+@deprecate  idump       xdump
+@deprecate  cwd         pwd
+@deprecate  strlen      length
 
 # aliases
-@deprecate  chi2rnd  randchi2
-@deprecate  betarnd  randbeta
-@deprecate  exprnd   randexp
-@deprecate  islogical isbool
-@deprecate  csvread  readcsv
-@deprecate  dlmread  readdlm
-@deprecate  csvwrite writecsv
-@deprecate  dlmwrite writedlm
+@deprecate  chi2rnd     randchi2
+@deprecate  betarnd     randbeta
+@deprecate  exprnd      randexp
+@deprecate  islogical   isbool
+@deprecate  csvread     readcsv
+@deprecate  dlmread     readdlm
+@deprecate  csvwrite    writecsv
+@deprecate  dlmwrite    writedlm
