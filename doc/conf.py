cab139cf3bbfddfce82aba38614ab58231626f39
Mike Nolta <mike@nolta.net>
2013-01-22 01:36:04 -0500

    fiddle w/ the doc style a bit more

--- doc/conf.py
+++ doc/conf.py
@@ -161,7 +161,7 @@ html_domain_indices = False
 html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
