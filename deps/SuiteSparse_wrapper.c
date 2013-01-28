e2318cd464c5d3e2aa5d3b0fdd0bb6fe3d679d61
Douglas Bates <dmbates@gmail.com>
2013-01-28 10:53:30 -0600

    Add two simple functions to return information on the cholmod_common structure.

--- deps/SuiteSparse_wrapper.c
+++ deps/SuiteSparse_wrapper.c
@@ -1,6 +1,32 @@
 #include <string.h>
 #include <cholmod.h>
 
+extern size_t jl_cholmod_common_size(size_t x) {
+    return sizeof(cholmod_common);
+}
+
+extern void jl_cholmod_common_offsets(size_t *vv) {
+    vv[0] = offsetof(cholmod_common, dbound);
+    vv[1] = offsetof(cholmod_common, maxrank);
+    vv[2] = offsetof(cholmod_common, supernodal_switch);
+    vv[3] = offsetof(cholmod_common, supernodal);
+    vv[4] = offsetof(cholmod_common, final_asis);
+    vv[5] = offsetof(cholmod_common, final_super);
+    vv[6] = offsetof(cholmod_common, final_ll);
+    vv[7] = offsetof(cholmod_common, final_pack);
+    vv[8] = offsetof(cholmod_common, final_monotonic);
+    vv[9] = offsetof(cholmod_common, final_resymbol);
+    vv[10] = offsetof(cholmod_common, prefer_zomplex);
+    vv[11] = offsetof(cholmod_common, prefer_upper);
+    vv[12] = offsetof(cholmod_common, print);
+    vv[13] = offsetof(cholmod_common, precise);
+    vv[14] = offsetof(cholmod_common, nmethods);
+    vv[15] = offsetof(cholmod_common, selected);
+    vv[16] = offsetof(cholmod_common, postorder);
+    vv[17] = offsetof(cholmod_common, itype);
+    vv[18] = offsetof(cholmod_common, dtype);
+}
+
 extern void
 jl_cholmod_common(void **cm)
 {
