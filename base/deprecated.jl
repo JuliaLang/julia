2f40acbadf3bffdb70b0a5bbf580ccc5afc37298
Stefan Karpinski <stefan@karpinski.org>
2013-02-02 09:43:38 -0800

    Various deprecation improvements.
     - only warn once per unique deprecation message
     - support randi and randival with deprecation warnings
    
    Other rand* functions are not really deprecated – they're just not
    supported anymore. These warnings point the direction to where to go
    for the equivalent functionality.

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -1,10 +1,19 @@
+const depwarned = (ByteString=>Bool)[]
+
+function depwarn(msg::String...)
+    msg = bytestring(msg...)
+    has(depwarned,msg) && return
+    depwarned[msg] = true
+    warn(msg)
+end
+
 macro deprecate(oldf,newf)
     oldname = expr(:quote,oldf)
     newname = expr(:quote,newf)
     expr(:toplevel,
         expr(:export,esc(oldf)),
         :(function $(esc(oldf))(args...)
-              warn(string($oldname," is deprecated, use ",$newname," instead."))
+              depwarn(string($oldname," is deprecated, use ",$newname," instead."))
               $(esc(newf))(args...)
           end))
 end
@@ -36,12 +45,7 @@ end
 @deprecate  searchsorted  searchsortedfirst
 @deprecate  choose        first
 @deprecate  system        run
-#@deprecate  strcat        string
-
-export strcat
-strcat(xs...) = string(xs...)
-
-# compatibility aliases
+@deprecate  order         sortperm
 @deprecate  numel         length
 @deprecate  islogical     isbool
 @deprecate  csvread       readcsv
@@ -53,20 +57,38 @@ strcat(xs...) = string(xs...)
 @deprecate  exprnd        randexp
 @deprecate  rot90         rotl90
 
-export randi, randival, randexp, randg, randbeta, randchi2
+#@deprecate  strcat        string
+export strcat
+strcat(xs...) = string(xs...)
 
-randi(x...) = error("randi is deprecated. Instead use: rand(r::Range)")
+export randi
+function randi(n,x...)
+    depwarn("randi(n,...) is deprecated, use rand(1:n,...) instead.")
+    rand(1:n,x...)
+end
 
-randival(x...) = error("randival is deprecated. Instead use: rand(r::Range)")
+export randival
+function randival(lo,hi,x...)
+    depwarn("randival(lo,hi,...) is deprecated, use rand(lo:hi,...) instead.")
+    rand(lo:hi,x...)
+end
 
-randexp(x...) = error("randexp is deprecated. Instead use the Distributions package:
-    using Distributions; rand(Exponential())")
+# discontinued functions
 
-randg(x...) = error("randg is deprecated. Instead use the Distributions package:
-    using Distributions; rand(Gamma())")
+export randexp, randg, randbeta, randchi
 
-randbeta(x...) = error("randbeta is deprecated. Instead use the Distributions package:
-    using Distributions; rand(Beta)")
+randexp(x...) = error("randexp is no longer supported; use the Distributions package instead:
+    using Distributions
+    rand(Exponential())")
 
-randchi2(x...) = error("randchi2 is deprecated. Instead use the Distributions package:
-    using Distributions; rand(Chisq())")
+randg(x...) = error("randg is no longer supported; use the Distributions package instead:
+    using Distributions
+    rand(Gamma())")
+
+randbeta(x...) = error("randbeta is no longer supported; use the Distributions package instead:
+    using Distributions
+    rand(Beta)")
+
+randchi2(x...) = error("randchi2 is no longer supported; use the Distributions package instead:
+    using Distributions
+    rand(Chisq())")
