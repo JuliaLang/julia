13e31202457fba76ea00b833b06bd388b67f50d9
Jeff Bezanson <bezanson@post.harvard.edu>
2013-02-02 21:42:07 -0500

    Revert "Rewrite filter in terms of filter!"
    This reverts commit 38a0b410b5c7fd8baa6fad975f47308073bf70a7.

--- base/array.jl
+++ base/array.jl
@@ -1684,8 +1684,12 @@ end
 ## Filter ##
 
 # given a function returning a boolean and an array, return matching elements
-function filter!(f::Function, a::StridedArray)
-    a = reshape(a, length(a))
+function filter(f::Function, As::StridedArray)
+    boolmap::Array{Bool} = map(f, As)
+    As[boolmap]
+end
+
+function filter!(f::Function, a::Array)
     insrt = 1
     for curr = 1:length(a)
         if f(a[curr])
@@ -1696,8 +1700,6 @@ function filter!(f::Function, a::StridedArray)
     delete!(a, insrt:length(a))
     return a
 end
-
-filter(f::Function, a::StridedArray) = filter!(f, copy(a))
 
 ## Transpose ##
 
