faf0b91d6f7ab80f34c94b8869be59727ba804b4
Marcus P S <marcus.ps+github@gmail.com>
2013-02-06 19:56:30 -0500

    Added simple tests for vec

--- test/arrayops.jl
+++ test/arrayops.jl
@@ -41,12 +41,14 @@ b = reshape(a, (32,))
 @test b[1]  == 10
 @test b[19] == 20
 @test b[13] == 30
+@test b == vec(a)
 
 b = rand(32)
 a = reshape(b, (2, 2, 2, 2, 2))
 @test ndims(a) == 5
 @test a[2,1,2,2,1] == b[14]
 @test a[2,2,2,2,2] == b[end]
+@test b == vec(a)
 
 sz = (5,8,7)
 A = reshape(1:prod(sz),sz...)
