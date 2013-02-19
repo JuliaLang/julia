79917485c3bc64e81eaf405dfed1c86c4e3f3fd7
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-02-19 17:44:43 -0500

    localpart needs to be called localize in 0.1 (fix test)

--- test/parallel.jl
+++ test/parallel.jl
@@ -8,5 +8,5 @@ s = convert(Array, d[1:150, 1:150])
 a = convert(Array, d)
 @test a[1:150,1:150] == s
 
-@test fetch(@spawnat 1 localpart(d)[1,1]) == d[1,1]
-@test fetch(@spawnat 2 localpart(d)[1,1]) == d[1,101]
+@test fetch(@spawnat 1 localize(d)[1,1]) == d[1,1]
+@test fetch(@spawnat 2 localize(d)[1,1]) == d[1,101]
