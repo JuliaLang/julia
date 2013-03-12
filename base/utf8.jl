8b967b944c1e372a1dc5a1e8a9913a8a6b0b66fe
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-03-12 18:33:10 -0400

    deprecate a couple redundant string functions

--- base/utf8.jl
+++ base/utf8.jl
@@ -107,5 +107,5 @@ write(io::IO, s::UTF8String) = write(io, s.data)
 utf8(x) = convert(UTF8String, x)
 convert(::Type{UTF8String}, s::UTF8String) = s
 convert(::Type{UTF8String}, s::ASCIIString) = UTF8String(s.data)
-convert(::Type{UTF8String}, a::Array{Uint8,1}) = check_utf8(UTF8String(a))
+convert(::Type{UTF8String}, a::Array{Uint8,1}) = is_valid_utf8(a) ? UTF8String(a) : error("invalid UTF-8 sequence")
 convert(::Type{UTF8String}, s::String) = utf8(bytestring(s))
