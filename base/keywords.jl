9db9544cfe2253ceba36b4c1d45f017671dd4531
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-03-23 03:08:00 -0400

    update keyword sorters to evaluate default expressions lazily, and handle rest keywords

--- base/keywords.jl
+++
@@ -1,8 +0,0 @@
-# type to specify that a call is suffixed with n symbol-value keyword pairs
-immutable NKeywords
-    n::Int
-end
-
-# singleton used to select a version of a method with keywords pre-sorted
-type SortedKeywords
-end
