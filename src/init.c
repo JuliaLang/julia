20c916504298f73835b68b671070e664348143d0
KDr2 <killy.draw@gmail.com>
2013-03-30 08:44:59 +0800

    remove macro JL_PRINT_GC_STATS from julia.h

--- src/init.c
+++ src/init.c
@@ -267,7 +267,9 @@ static void jl_uv_exitcleanup_walk(uv_handle_t* handle, void *arg) {
 }
 DLLEXPORT void uv_atexit_hook()
 {
-    JL_PRINT_GC_STATS;
+#if defined(JL_GC_MARKSWEEP) || defined(GC_FINAL_STATS)
+    jl_print_gc_stats(JL_STDERR);
+#endif
     if (jl_base_module) {
         jl_value_t *f = jl_get_global(jl_base_module, jl_symbol("_atexit"));
         if (f!=NULL && jl_is_function(f)) {
