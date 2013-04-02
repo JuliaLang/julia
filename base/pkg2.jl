dfb7b653e96dcbcce122a6899a4881e2b58bd822
Stefan Karpinski <stefan@karpinski.org>
2013-04-02 16:47:58 -0400

    wip: Pkg rewrite -- version interval algebra.

--- base/pkg2.jl
+++ base/pkg2.jl
@@ -1,32 +1,68 @@
-module Pkg2
-import Base: Git, isequal, isless, hash
+# module Pkg2
+import Base: Git, isequal, isless, hash, intersect, contains
 
-@unix_only    const DIR_NAME = ".julia"
-@windows_only const DIR_NAME = "packages"
+### VersionSet ###
 
-function dir()
-    d = abspath(get(ENV, "JULIA_PKGDIR", joinpath(ENV["HOME"], DIR_NAME)))
-    x, y = VERSION.major, VERSION.minor
-    dxy = joinpath(d, "v$x.$y")
-    isdir(dxy) && return dxy
-    dx = joinpath(d, "v$x")
-    isdir(dx) && return dx
-    return d
+abstract VersionInterval
+immutable NoVersions <: VersionInterval end
+immutable AllVersions <: VersionInterval end
+immutable VersionsGreater <: VersionInterval
+    lo::VersionNumber
 end
-dir(pkg::String...) = joinpath(dir(), pkg...)
+immutable VersionsBetween <: VersionInterval
+    lo::VersionNumber
+    hi::VersionNumber
+end
 
-function cd_pkgdir(f::Function)
-    d = dir()
-    if !isdir(d)
-        if has(ENV, "JULIA_PKGDIR")
-            error("Package directory $d doesn't exist; run Pkg.init() to create it.")
-        else
-            info("Auto-initializing default package repository $d.")
-            init()
+interval(lo::VersionNumber) = VersionsGreater(lo)
+interval(lo::VersionNumber, hi::VersionNumber) = lo < hi ? VersionsBetween(lo, hi) : NoVersions()
+
+intersect(x::NoVersions,      y::VersionInterval) = x
+intersect(x::AllVersions,     y::VersionInterval) = y
+intersect(x::VersionInterval, y::VersionInterval) = intersect(y,x)
+intersect(x::VersionsGreater, y::VersionsGreater) = interval(max(x.lo,y.lo))
+intersect(x::VersionsGreater, y::VersionsBetween) = interval(max(x.lo,y.lo),y.hi)
+intersect(x::VersionsBetween, y::VersionsBetween) = interval(max(x.lo,y.lo),min(x.hi,y.hi))
+
+contains(i::NoVersions, v::VersionNumber) = false
+contains(i::AllVersions, v::VersionNumber) = true
+contains(i::VersionsGreater, v::VersionNumber) = i.lo <= v
+contains(i::VersionsBetween, v::VersionNumber) = i.lo <= v < i.hi
+
+immutable VersionSet
+    package::ByteString
+    versions::Vector{VersionNumber}
+    function VersionSet(pkg::ByteString, vers::Vector{VersionNumber})
+        if !issorted(vers)
+            error("version numbers must be sorted")
         end
+        new(pkg, vers)
     end
-    cd(f,d)
 end
+VersionSet(pkg::ByteString) = VersionSet(pkg, VersionNumber[])
+
+isequal(a::VersionSet, b::VersionSet) =
+    a.package == b.package && a.versions == b.versions
+isless(a::VersionSet, b::VersionSet) = a.package < b.package
+hash(s::VersionSet) = hash([s.(n) for n in VersionSet.names])
+
+function parse_requires(readable)
+    reqs = VersionSet[]
+    for line in eachline(readable)
+        if ismatch(r"^\s*(?:#|$)", line) continue end
+        line = replace(line, r"#.*$", "")
+        fields = split(line)
+        pkg = shift!(fields)
+        vers = [ convert(VersionNumber,x) for x in fields ]
+        if !issorted(vers)
+            error("invalid requires entry for $pkg: $vers")
+        end
+        # TODO: merge version sets instead of appending?
+        push!(reqs,VersionSet(pkg,vers))
+    end
+    sort!(reqs)
+end
+parse_requires(file::String) = open(parse_requires,file)
 
 each_installed() = @task begin
     for line in eachline(`ls`)
@@ -60,42 +96,4 @@ function requirements()
     sort!(reqs)
 end
 
-function parse_requires(readable)
-    reqs = VersionSet[]
-    for line in eachline(readable)
-        if ismatch(r"^\s*(?:#|$)", line) continue end
-        line = replace(line, r"#.*$", "")
-        fields = split(line)
-        pkg = shift!(fields)
-        vers = [ convert(VersionNumber,x) for x in fields ]
-        if !issorted(vers)
-            error("invalid requires entry for $pkg: $vers")
-        end
-        # TODO: merge version sets instead of appending?
-        push!(reqs,VersionSet(pkg,vers))
-    end
-    sort!(reqs)
-end
-parse_requires(file::String) = open(parse_requires,file)
-
-### VersionSet ###
-
-immutable VersionSet
-    package::ByteString
-    versions::Vector{VersionNumber}
-
-    function VersionSet(pkg::ByteString, vers::Vector{VersionNumber})
-        if !issorted(vers)
-            error("version numbers must be sorted")
-        end
-        new(pkg, vers)
-    end
-end
-VersionSet(pkg::ByteString) = VersionSet(pkg, VersionNumber[])
-
-isequal(a::VersionSet, b::VersionSet) =
-    a.package == b.package && a.versions == b.versions
-isless(a::VersionSet, b::VersionSet) = a.package < b.package
-hash(s::VersionSet) = hash([s.(n) for n in VersionSet.names])
-
-end # module
+# end # module
