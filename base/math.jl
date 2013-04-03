a6b065e56b301fb5a186269658be44ab43f45e98
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-04-03 14:27:35 -0400

    improve sinc and cosc of complex integer

--- base/math.jl
+++ base/math.jl
@@ -34,11 +34,11 @@ clamp{T<:Real}(x::AbstractArray{T}, lo::Real, hi::Real) =
 
 sinc(x::Number) = x==0 ? one(x)  : (pix=pi*x; oftype(x,sin(pix)/pix))
 sinc(x::Integer) = x==0 ? one(x) : zero(x)
-sinc{T<:Integer}(x::Complex{T}) = sinc(convert(Complex{FloatingPoint},x))
+sinc{T<:Integer}(x::Complex{T}) = sinc(complex(float(real(x)),float(imag(x))))
 @vectorize_1arg Number sinc
 cosc(x::Number) = x==0 ? zero(x) : (pix=pi*x; oftype(x,cos(pix)/x-sin(pix)/(pix*x)))
-cosc(x::Integer) = cosc(convert(FloatingPoint,x))
-cosc{T<:Integer}(x::Complex{T}) = cosc(convert(Complex{FloatingPoint},x))
+cosc(x::Integer) = cosc(float(x))
+cosc{T<:Integer}(x::Complex{T}) = cosc(complex(float(real(x)),float(imag(x))))
 @vectorize_1arg Number cosc
 
 radians2degrees(z::Real) = oftype(z, (180/pi) * z)
