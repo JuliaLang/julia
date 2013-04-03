e39e69ed511d369f51d4536d099eeb1d88a93c59
Stefan Karpinski <stefan@karpinski.org>
2013-04-03 00:54:14 -0400

    pkg2 wip: implement merging of requirements from different sources.

--- extras/pkg2.jl
+++ extras/pkg2.jl
@@ -14,8 +14,10 @@ intersect(a::VersionInterval, b::VersionInterval) = VersionInterval(max(a.lower,
 intersect(A::Vector{VersionInterval}, B::Vector{VersionInterval}) =
     sortby!(filter!(i->!isempty(i), vec([ intersect(a,b) for a in A, b in B ])), i->i.lower)
 
+typealias Requires Dict{ByteString,Vector{VersionInterval}}
+
 function parse_requires(readable)
-    reqs = Dict{ByteString,Vector{VersionInterval}}()
+    reqs = Requires()
     for line in eachline(readable)
         ismatch(r"^\s*(?:#|$)", line) && continue
         fields = split(replace(line, r"#.*$", ""))
@@ -41,6 +43,13 @@ function parse_requires(readable)
     return reqs
 end
 parse_requires(file::String) = open(parse_requires,file)
+
+function merge!(A::Requires, B::Requires)
+    for (pkg, ivals) in B
+        A[pkg] = has(A,pkg) ? intersect(A[pkg], ivals) : ivals
+    end
+    return A
+end
 
 each_installed() = @task begin
     for line in eachline(`ls`)
@@ -69,9 +78,9 @@ function requirements()
     for pkg in each_installed()
         isfixed(pkg) || continue
         isfile(pkg, "REQUIRE") || continue
-        append!(reqs, parse_requires(joinpath(pkg, "REQUIRE")))
+        merge!(reqs, parse_requires(joinpath(pkg, "REQUIRE")))
     end
-    sort!(reqs)
+    return reqs
 end
 
 # end # module
