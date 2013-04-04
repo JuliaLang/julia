a7bc7d31faf8ae8dd729176278a2a927ec493bf1
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-04-04 01:56:07 -0400

    fix mkpath. I don't think it handled relative paths right. fix formatting in file.jl

--- base/file.jl
+++ base/file.jl
@@ -57,19 +57,20 @@ mkdir(path::String, mode::Signed) = error("mkdir: mode must be an unsigned integ
 mkdir(path::String) = mkdir(path, 0o777)
 
 function mkpath(path::String, mode)
-  dparts = splitdrive(path)
-	@windows_only begin 
+    dparts = splitdrive(path)
+    isabs = isabspath(path)
     path = dparts[1]
-    path *= "\\"
-  end
-  @unix_only if(path[1]=='/'); path = "/"; end
-	parts = (split(dparts[2],Base.path_separator_re,false))
-	for x in parts
-		path=joinpath(path,x)
-		if(!isdir(path))
-			mkdir(path,mode)
-		end
+    if isabs
+        @windows_only path *= "\\"
+        @unix_only path = "/"
+    end
+    parts = split(dparts[2],Base.path_separator_re,false)
+    for x in parts
+        path = joinpath(path,x)
+	if !isdir(path)
+	    mkdir(path,mode)
 	end
+    end
 end
 mkpath(path::String) = mkpath(path,0o777)
 
@@ -93,12 +94,12 @@ touch(path::String) = run(`touch $path`)
 
 # Obtain a temporary filename.
 function tempname()
-  d = get(ENV, "TMPDIR", C_NULL) # tempnam ignores TMPDIR on darwin
-  @unix_only p = ccall(:tempnam, Ptr{Uint8}, (Ptr{Uint8},Ptr{Uint8}), d, "julia")
-  @windows_only p = ccall(:_tempnam, Ptr{Uint8}, (Ptr{Uint8},Ptr{Uint8}), d, "julia")
-  s = bytestring(p)
-  c_free(p)
-  s
+    d = get(ENV, "TMPDIR", C_NULL) # tempnam ignores TMPDIR on darwin
+    @unix_only p = ccall(:tempnam, Ptr{Uint8}, (Ptr{Uint8},Ptr{Uint8}), d, "julia")
+    @windows_only p = ccall(:_tempnam, Ptr{Uint8}, (Ptr{Uint8},Ptr{Uint8}), d, "julia")
+    s = bytestring(p)
+    c_free(p)
+    s
 end
 
 # Obtain a temporary directory's path.
@@ -106,56 +107,56 @@ tempdir() = dirname(tempname())
 
 # Create and return the name of a temporary file along with an IOStream
 @unix_only function mktemp()
-  b = joinpath(tempdir(), "tmpXXXXXX")
-  p = ccall(:mkstemp, Int32, (Ptr{Uint8}, ), b)
-  return (b, fdio(p, true))
+    b = joinpath(tempdir(), "tmpXXXXXX")
+    p = ccall(:mkstemp, Int32, (Ptr{Uint8}, ), b)
+    return (b, fdio(p, true))
 end
 
 @windows_only begin 
 function GetTempPath()
-  temppath = Array(Uint8,261)
-  lentemppath = ccall(:GetTempPathA,stdcall,Uint32,(Uint32,Ptr{Uint8}),length(temppath),temppath)
-  if lentemppath >= length(temppath) || lentemppath == 0
-      error("GetTempPath failed")
-end
-  resize!(temppath,lentemppath)
-  return convert(ASCIIString,temppath)
+    temppath = Array(Uint8,261)
+    lentemppath = ccall(:GetTempPathA,stdcall,Uint32,(Uint32,Ptr{Uint8}),length(temppath),temppath)
+    if lentemppath >= length(temppath) || lentemppath == 0
+        error("GetTempPath failed")
+    end
+    resize!(temppath,lentemppath)
+    return convert(ASCIIString,temppath)
 end
 GetTempFileName(uunique::Uint32) = GetTempFileName(GetTempPath(), uunique)
 function GetTempFileName(temppath::String,uunique::Uint32)
-  tname = Array(Uint8,261)
-  uunique = ccall(:GetTempFileNameA,stdcall,Uint32,(Ptr{Uint8},Ptr{Uint8},Uint32,Ptr{Uint8}),temppath,"julia",uunique,tname)
-  lentname = findfirst(tname,0)-1
-  if uunique == 0 || lentname <= 0
-      error("GetTempFileName failed")
-  end
-  resize!(tname,lentname)
-  return convert(ASCIIString, tname)
+    tname = Array(Uint8,261)
+    uunique = ccall(:GetTempFileNameA,stdcall,Uint32,(Ptr{Uint8},Ptr{Uint8},Uint32,Ptr{Uint8}),temppath,"julia",uunique,tname)
+    lentname = findfirst(tname,0)-1
+    if uunique == 0 || lentname <= 0
+        error("GetTempFileName failed")
+    end
+    resize!(tname,lentname)
+    return convert(ASCIIString, tname)
 end
 function mktemp()
-  filename = GetTempFileName(uint32(0))
-  return (filename, open(filename,"r+"))
+    filename = GetTempFileName(uint32(0))
+    return (filename, open(filename,"r+"))
 end
 end
 
 # Create and return the name of a temporary directory
 @unix_only function mktempdir()
-  b = joinpath(tempdir(), "tmpXXXXXX")
-  p = ccall(:mkdtemp, Ptr{Uint8}, (Ptr{Uint8}, ), b)
-  return bytestring(p)
+    b = joinpath(tempdir(), "tmpXXXXXX")
+    p = ccall(:mkdtemp, Ptr{Uint8}, (Ptr{Uint8}, ), b)
+    return bytestring(p)
 end
 
 @windows_only function mktempdir()
-  seed = rand(Uint32)
-  while true
-      filename = GetTempFileName(seed)
-      ret = ccall(:_mkdir, Int32, (Ptr{Uint8},), filename)
-      if ret == 0
-          return filename
-end
-      system_error(:mktempdir, errno()!=EEXIST)
-      seed += 1
-  end
+    seed = rand(Uint32)
+    while true
+        filename = GetTempFileName(seed)
+        ret = ccall(:_mkdir, Int32, (Ptr{Uint8},), filename)
+        if ret == 0
+            return filename
+        end
+        system_error(:mktempdir, errno()!=EEXIST)
+        seed += 1
+    end
 end
 
 downloadcmd = nothing
@@ -181,39 +182,39 @@ function download(url::String, filename::String)
     filename
 end
 function download(url::String)
-  filename = tempname()
-  download(url, filename)
+    filename = tempname()
+    download(url, filename)
 end
 
 function readdir(path::String)
-  # Allocate space for uv_fs_t struct
-  uv_readdir_req = zeros(Uint8, ccall(:jl_sizeof_uv_fs_t, Int32, ()))
+    # Allocate space for uv_fs_t struct
+    uv_readdir_req = zeros(Uint8, ccall(:jl_sizeof_uv_fs_t, Int32, ()))
 
-  # defined in sys.c, to call uv_fs_readdir
-  file_count = ccall(:jl_readdir, Int32, (Ptr{Uint8}, Ptr{Uint8}),
-                     bytestring(path), uv_readdir_req)
+    # defined in sys.c, to call uv_fs_readdir
+    file_count = ccall(:jl_readdir, Int32, (Ptr{Uint8}, Ptr{Uint8}),
+                       bytestring(path), uv_readdir_req)
 
-  if file_count < 0
-    error("Unable to read directory $path.")
-  end
+    if file_count < 0
+        error("Unable to read directory $path.")
+    end
 
-  # The list of dir entries is returned as a contiguous sequence of null-terminated
-  # strings, the first of which is pointed to by ptr in uv_readdir_req.
-  # The following lines extracts those strings into dirent
-  entries = String[]
-  offset = 0
+    # The list of dir entries is returned as a contiguous sequence of null-terminated
+    # strings, the first of which is pointed to by ptr in uv_readdir_req.
+    # The following lines extracts those strings into dirent
+    entries = String[]
+    offset = 0
 
-  for i = 1:file_count
-    entry = bytestring(ccall(:jl_uv_fs_t_ptr_offset, Ptr{Uint8}, 
-                             (Ptr{Uint8}, Int), uv_readdir_req, offset))
-    push!(entries, entry)
-    offset += length(entry) + 1   # offset to the next entry
-  end
+    for i = 1:file_count
+        entry = bytestring(ccall(:jl_uv_fs_t_ptr_offset, Ptr{Uint8},
+                                 (Ptr{Uint8}, Int), uv_readdir_req, offset))
+        push!(entries, entry)
+        offset += length(entry) + 1   # offset to the next entry
+    end
 
-  # Clean up the request string
-  ccall(:jl_uv_fs_req_cleanup, Void, (Ptr{Uint8},), uv_readdir_req)
+    # Clean up the request string
+    ccall(:jl_uv_fs_req_cleanup, Void, (Ptr{Uint8},), uv_readdir_req)
 
-  entries
+    entries
 end
 
 readdir(cmd::Cmd) = readdir(string(cmd)[2:end-1])
