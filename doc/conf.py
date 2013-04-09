1ee3fd71e83d4854e31e758f5c7e9bd2a89ca3b8
Mike Nolta <mike@nolta.net>
2013-04-09 00:54:47 -0400

    doc: add separate latex index
    Fixes pdf build failure caused by unicode chinese characters.

--- doc/conf.py
+++ doc/conf.py
@@ -198,7 +198,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'JuliaLanguage.tex', u'Julia Language Documentation',
+  ('latex', 'JuliaLanguage.tex', u'Julia Language Documentation',
    AUTHORS, 'manual'),
 ]
 
