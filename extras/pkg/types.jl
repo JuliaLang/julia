68017ababd56ac64d348d0e53813a03c05be39e5
Stefan Karpinski <stefan@karpinski.org>
2013-04-12 13:46:53 -0400

    pkg2: for now trying to minimize custom types.

---
+++ extras/pkg/types.jl
@@ -0,0 +1,14 @@
+import Base: Git, isequal, isless, hash, isempty, contains
+
+immutable VersionInterval
+    lower::VersionNumber
+    upper::VersionNumber
+end
+
+isempty(i::VersionInterval) = i.upper <= i.lower
+contains(i::VersionInterval, v::VersionNumber) = a.lower <= v < a.upper
+intersect(a::VersionInterval, b::VersionInterval) = VersionInterval(max(a.lower,b.lower), min(a.upper,b.upper))
+intersect(A::Vector{VersionInterval}, B::Vector{VersionInterval}) =
+    sortby!(filter!(i->!isempty(i), vec([ intersect(a,b) for a in A, b in B ])), i->i.lower)
+
+typealias Requires Dict{ByteString,Vector{VersionInterval}}
