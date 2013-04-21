JuliaLang/julia

commit 0228c1cab0222fc4684954842fd6e028601fc1ba

pool constants from compressed ASTs per-module, which is more efficient

src/dump.c
@@ -167,6 +167,7 @@ static void jl_serialize_module(ios_t *s, jl_module_t *m)
             jl_serialize_value(s, (jl_value_t*)m->usings.items[i]);
         }
     }
+    jl_serialize_value(s, m->constant_table);
 }
 
 static int is_ast_node(jl_value_t *v)
@@ -635,6 +636,7 @@ static jl_value_t *jl_deserialize_value(ios_t *s)
         for(size_t i=0; i < ni; i++) {
             arraylist_push(&m->usings, jl_deserialize_value(s));
         }
+        m->constant_table = (jl_array_t*)jl_deserialize_value(s);
         return (jl_value_t*)m;
     }
     else if (vtag == (jl_value_t*)SmallInt64_tag) {
@@ -814,7 +816,7 @@ jl_value_t *jl_ast_rettype(jl_lambda_info_t *li, jl_value_t *ast)
 {
     if (jl_is_expr(ast))
         return jl_lam_body((jl_expr_t*)ast)->etype;
-    tree_literal_values = li->def->roots;
+    tree_literal_values = li->module->constant_table;
     ios_t src;
     jl_array_t *bytes = (jl_array_t*)ast;
     ios_mem(&src, 0);
@@ -834,11 +836,9 @@ jl_value_t *jl_compress_ast(jl_lambda_info_t *li, jl_value_t *ast)
     int en = jl_gc_is_enabled();
     jl_gc_disable();
 
-    jl_lambda_info_t *def = li->def;
-    if (def->roots == NULL) {
-        def->roots = jl_alloc_cell_1d(0);
-    }
-    tree_literal_values = def->roots;
+    if (li->module->constant_table == NULL)
+        li->module->constant_table = jl_alloc_cell_1d(0);
+    tree_literal_values = li->module->constant_table;
     li->capt = (jl_value_t*)jl_lam_capt((jl_expr_t*)ast);
     if (jl_array_len(li->capt) == 0)
         li->capt = NULL;
@@ -849,7 +849,7 @@ jl_value_t *jl_compress_ast(jl_lambda_info_t *li, jl_value_t *ast)
 
     jl_value_t *v = (jl_value_t*)jl_takebuf_array(&dest);
     if (jl_array_len(tree_literal_values) == 0) {
-        def->roots = NULL;
+        li->module->constant_table = NULL;
     }
     tree_literal_values = last_tlv;
     if (en)
@@ -861,7 +861,7 @@ DLLEXPORT
 jl_value_t *jl_uncompress_ast(jl_lambda_info_t *li, jl_value_t *data)
 {
     jl_array_t *bytes = (jl_array_t*)data;
-    tree_literal_values = li->def->roots;
+    tree_literal_values = li->module->constant_table;
     ios_t src;
     ios_mem(&src, 0);
     ios_setbuf(&src, bytes->data, jl_array_len(bytes), 0);

base/serialize.jl
@@ -204,7 +204,7 @@ end
 function serialize(s, linfo::LambdaStaticData)
     writetag(s, LambdaStaticData)
     serialize(s, lambda_number(linfo))
-    serialize(s, linfo.ast)
+    serialize(s, uncompressed_ast(linfo))
     if isdefined(linfo.def, :roots)
         serialize(s, linfo.def.roots)
     else

base/show.jl
@@ -39,16 +39,12 @@ function show(io::IO, m::Module)
     end
 end
 
-uncompress_ast(l::LambdaStaticData) = ccall(:jl_uncompress_ast, Any, (Any,Any), l, l.ast)
+uncompressed_ast(l::LambdaStaticData) =
+    isa(l.ast,Expr) ? l.ast : ccall(:jl_uncompress_ast, Any, (Any,Any), l, l.ast)
 
 function show(io::IO, l::LambdaStaticData)
     print(io, "AST(")
-    if isa(l.ast,Expr)
-        show(io, l.ast)
-    else
-        ast = uncompress_ast(l)
-        show(io, ast)
-    end
+    show(io, uncompressed_ast(l))
     print(io, ")")
 end
 
@@ -341,10 +337,7 @@ function show(io::IO, m::Method)
         show_delim_array(io, tv, '{', ',', '}', false)
     end
     li = m.func.code
-    e = li.ast
-    if !isa(e,Expr)
-        e = uncompress_ast(li)
-    end
+    e = uncompressed_ast(li)
     argnames = e.args[1]
     decls = map(argtype_decl_string, argnames, {m.sig...})
     print(io, "(")

src/gc.c
@@ -527,6 +527,8 @@ static void gc_mark_module(jl_module_t *m)
                 gc_push_root(b->type);
         }
     }
+    if (m->constant_table)
+        gc_push_root(m->constant_table);
 }
 
 // for chasing down unwanted references

src/julia.h
@@ -286,6 +286,7 @@ typedef struct _jl_module_t {
     struct _jl_module_t *parent;
     htable_t bindings;
     arraylist_t usings;  // modules with all bindings potentially imported
+    jl_array_t *constant_table;
 } jl_module_t;
 
 typedef struct _jl_methlist_t {

src/module.c
@@ -14,6 +14,7 @@ jl_module_t *jl_new_module(jl_sym_t *name)
     jl_module_t *m = (jl_module_t*)allocobj(sizeof(jl_module_t));
     m->type = (jl_value_t*)jl_module_type;
     m->name = name;
+    m->constant_table = NULL;
     htable_new(&m->bindings, 0);
     jl_set_const(m, name, (jl_value_t*)m);
     arraylist_new(&m->usings, 0);

src/toplevel.c
@@ -60,7 +60,8 @@ jl_value_t *jl_eval_module_expr(jl_expr_t *ex)
         jl_old_base_module = jl_base_module;
         // pick up Base module during bootstrap
         jl_base_module = newm;
-    } else {
+    }
+    else {
         base_module_conflict = 1;
     }
     // export all modules from Main

commit 80eda6254d179fc400fda58c927590901ca05a49

fix #2905 - regression caused by 0228c1cab0222fc4684954842fd6e028601fc1ba re: AST compression efficiency

src/dump.c
@@ -848,7 +848,7 @@ jl_value_t *jl_compress_ast(jl_lambda_info_t *li, jl_value_t *ast)
     //JL_PRINTF(JL_STDERR, "%d bytes, %d values\n", dest.size, vals->length);
 
     jl_value_t *v = (jl_value_t*)jl_takebuf_array(&dest);
-    if (jl_array_len(tree_literal_values) == 0) {
+    if (jl_array_len(tree_literal_values) == 0 && last_tlv == NULL) {
         li->module->constant_table = NULL;
     }
     tree_literal_values = last_tlv;