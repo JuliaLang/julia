c9eda9ca40d6198b49cd6a35e19662309bfb21c2
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-04-27 19:10:55 -0400

    fix charwidth ccall

--- base/string.jl
+++ base/string.jl
@@ -277,7 +277,7 @@ isless(a::Symbol, b::Symbol) = cmp(a,b)<0
 
 ## character column width function ##
 
-charwidth(c::Char) = max(0,int(ccall(:wcwidth, Int32, (Char,), c)))
+charwidth(c::Char) = max(0,int(ccall(:wcwidth, Int32, (Uint32,), c)))
 strwidth(s::String) = (w=0; for c in s; w += charwidth(c); end; w)
 strwidth(s::ByteString) = ccall(:u8_strwidth, Int, (Ptr{Uint8},), s.data)
 # TODO: implement and use u8_strnwidth that takes a length argument
