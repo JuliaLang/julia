755a4fa1bade74a23eb3e0348801d8b1e57564f5
Simon Kornblith <simon@simonster.com>
2013-04-28 17:25:02 -0400

    Fix enum.jl, and convert to use immutables

--- extras/enum.jl
+++ extras/enum.jl
@@ -1,16 +1,13 @@
 macro enum(T,syms...)
-    N = int(Int.nbits)
-    T = esc(T)
-    sh = Expr(:block)
-    sh.args = {:(if x===$sym return print($(string(sym))) end) for sym in syms}
     blk = quote
-        bitstype ($N) ($T)
+        immutable ($T)
+            n::Int
+        end
         $(esc(:symbols))(_::Type{$T}) = $syms
-        $(esc(:isequal))(a::($T), b::($T)) = eq_int(unbox($T,a),unbox($T,b))
-        $(esc(:show))(io::IO, x::($T)) = $sh
+        Base.show(io::IO, x::($T)) = print($(esc(:symbols))(($T))[x.n+1])
     end
     for (i,sym) in enumerate(syms)
-        push!(blk.args, :(const $(esc(sym)) = box($T,unbox(Int,$(i-1)))))
+        push!(blk.args, :(const $(esc(sym)) = $(T)($(i-1))))
     end
     push!(blk.args, :nothing)
     blk.head = :toplevel
