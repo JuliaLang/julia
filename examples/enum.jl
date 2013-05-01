6db5a9365fbc764b629761a7aafc61f4d3978097
Jameson Nash <vtjnash@gmail.com>
2013-05-01 12:49:14 -0400

    enum bit masks -- primarily for ccall

--- examples/enum.jl
+++ examples/enum.jl
@@ -1,11 +1,14 @@
+module Enum
+export @enum, @enum_mask
+
 macro enum(T,syms...)
     blk = quote
         immutable $(esc(T))
-            n::Int32
+            n::Uint32
             $(esc(T))(n::Integer) = new(n)
         end
-        Base.show(io::IO, x::$(esc(T))) = print(io, $syms[x.n+1])
-        Base.show(io::IO, x::Type{$(esc(T))}) = print(io, $(string("enum ", T, ' ', '(', join(syms, ", "), ')')))
+        $(esc(:(Base.show)))(io::IO, x::$(esc(T))) = print(io, $syms[x.n+1])
+        $(esc(:(Base.show)))(io::IO, x::Type{$(esc(T))}) = print(io, $(string("enum ", T, ' ', '(', join(syms, ", "), ')')))
     end
     for (i,sym) in enumerate(syms)
         push!(blk.args, :(const $(esc(sym)) = $(esc(T))($(i-1))))
@@ -13,4 +16,49 @@ macro enum(T,syms...)
     push!(blk.args, :nothing)
     blk.head = :toplevel
     return blk
+end
+
+abstract EnumMask
+Base.(:&){T<:EnumMask}(a::T, b::T) = T(a.n & b.n)
+Base.(:|){T<:EnumMask}(a::T, b::T) = T(a.n | b.n)
+Base.(:~){T<:EnumMask}(a::T) = T(~a.n)
+Base.(:(==)){T<:EnumMask}(a::T, b::T) = T(a.n == b.n)
+Base.(:!=){T<:EnumMask}(a::T, b::T) = T(a.n != b.n)
+
+macro enum_mask(T,syms...)
+    @assert length(syms) <= 32
+    blk = quote
+        immutable $(esc(T)) <: EnumMask
+            n::Uint32
+            $(esc(T))(n::Integer) = new(n)
+        end
+        function $(esc(:(Base.show)))(io::IO, x::$(esc(T)))
+            print(io, $(string(T)), '(')
+            first = true
+            if x.n == 0
+                println("0")
+            else
+                for i = 0:$(length(syms)-1)
+                    if x.n & (1<<i) != 0
+                        if !first
+                            print(io, '|')
+                        else
+                            first = false
+                        end
+                        print(io, $syms[i+1])
+                    end
+                end
+            end
+            println(')')
+        end
+        $(esc(:(Base.show)))(io::IO, x::Type{$(esc(T))}) = print(io, $(string("enum mask ", T, ' ', '(', join(syms, ", "), ')')))
+    end
+    for (i,sym) in enumerate(syms)
+        push!(blk.args, :(const $(esc(sym)) = $(esc(T))($(1<<(i-1)))))
+    end
+    push!(blk.args, :nothing)
+    blk.head = :toplevel
+    return blk
+end
+
 end
