da670c49876a21eae50cbe0bb16ef86c4f462e26
Stefan Karpinski <stefan@karpinski.org>
2013-05-06 00:02:45 -0400

    base/primes.jl: faster primes(n) and isprime(n) functions.
    Sieve of Atkin for generating primes:
    
    	http://en.wikipedia.org/wiki/Sieve_of_Atkin
    
    Code very loosely based on this:
    
    	http://thomasinterestingblog.wordpress.com/2011/11/30/generating-primes-with-the-sieve-of-atkin-in-c/
    	http://dl.dropboxusercontent.com/u/29023244/atkin.cpp
    
    Miller-Rabin for primality testing:
    
    	http://en.wikipedia.org/wiki/Miller–Rabin_primality_test

---
+++ base/primes.jl
@@ -0,0 +1,84 @@
+function primesmask(n::Int)
+    s = falses(n)
+    n < 2 && return s; s[2] = true
+    n < 3 && return s; s[3] = true
+    r = ifloor(sqrt(n))
+    for x = 1:r
+        xx = x*x
+        for y = 1:r
+            yy = y*y
+            i, j, k = 4xx+yy, 3xx+yy, 3xx-yy
+            i <= n && (s[i] $= (i%12==1)|(i%12==5))
+            j <= n && (s[j] $= (j%12==7))
+            1 <= k <= n && (s[k] $= (x>y)&(k%12==11))
+        end
+    end
+    for i = 5:r
+        s[i] && (s[i*i:i*i:n] = false)
+    end
+    return s
+end
+primes(n::Int) = find(primesmask(n))
+
+function isprime(n::Int)
+    n == 2 && return true
+    n <= 2 | iseven(n) && return false
+    s = trailing_zeros(n-1)
+    d = (n-1) >>> s
+    for a in (2,3,5,7,11,13,17) # TODO: more checks for larger n
+        a < n || break
+        x = powermod(a,d,n)
+        x == 1 && continue
+        t = s
+        while x != n-1
+            (t-=1) <= 0 && return false
+            x = x*x % n
+            x == 1 && return false
+        end
+    end
+    return true
+end
+
+# TODO: replace this factorization routine
+
+function factor{T<:Integer}(n::T)
+    if n <= 0
+        error("factor: number to be factored must be positive")
+    end
+    h = (T=>Int)[]
+    if n == 1 return h end
+    local p::T
+    s = ifloor(sqrt(n))
+    P = primes(n)
+    for p in P
+        if p > s
+            break
+        end
+        if n % p == 0
+            while n % p == 0
+                h[p] = get(h,p,0)+1
+                n = div(n,p)
+            end
+            if n == 1
+                return h
+            end
+            s = ifloor(sqrt(n))
+        end
+    end
+    p = P[end]+2
+    while p <= s
+        if n % p == 0
+            while n % p == 0
+                h[p] = get(h,p,0)+1
+                n = div(n,p)
+            end
+            if n == 1
+                return h
+            end
+            s = ifloor(sqrt(n))
+        end
+        p += 2
+    end
+    h[n] = 1
+    return h
+end
