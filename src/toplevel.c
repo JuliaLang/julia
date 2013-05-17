c0e8da1edfd653742600c88f43cf0046321257bb
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-05-17 00:04:01 -0400

    add missing export of gradient() disallow subtypes of ...

--- src/toplevel.c
+++ src/toplevel.c
@@ -483,6 +483,7 @@ void jl_set_datatype_super(jl_datatype_t *tt, jl_value_t *super)
 {
     if (!jl_is_datatype(super) || super == (jl_value_t*)jl_undef_type ||
         !jl_is_abstracttype(super) ||
+        jl_subtype(super,(jl_value_t*)jl_vararg_type,0) ||
         jl_subtype(super,(jl_value_t*)jl_type_type,0)) {
         jl_errorf("invalid subtyping in definition of %s",tt->name->name->name);
     }
