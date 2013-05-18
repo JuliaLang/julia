dc16f4628e0971a4587b1cedb9e4568bde9e6829
Toivo Henningsson <toivo.h.h@gmail.com>
2013-05-18 07:52:27 +0200

    Add broadcast! function. Make broadcast(f) and broadcast!(f) do function wrapping.

--- test/broadcast.jl
+++ test/broadcast.jl
@@ -11,6 +11,11 @@
 @test [1, 0] .+ [1, 4] == [2, 4]
 @test [1] .+ [] == []
 
+A = eye(2); @test broadcast!(+, A, A, [1, 4]) == [2 1; 4 5]
+A = eye(2); @test broadcast!(+, A, A, [1  4]) == [2 4; 1 5]
+A = [1  0]; @test_fails broadcast!(+, A, A, [1, 4])
+A = [1  0]; @test broadcast!(+, A, A, [1  4]) == [2 4]
+
 M = [11 12; 21 22]
 @test broadcast_getindex(M, eye(2)+1,[1, 2]) == [21 11; 12 22]
 A = zeros(2,2); broadcast_setindex!(A, [21 11; 12 22], eye(2)+1,[1, 2])
