a1749ef124fd72f495e6bbd626dd9e9bdfac92ce
Toivo Henningsson <toivo.h.h@gmail.com>
2013-05-18 14:53:14 +0200

    Make broadcast.jl work with StridedArray arguments. Add some gensyms.

--- test/broadcast.jl
+++ test/broadcast.jl
@@ -1,24 +1,45 @@
-@test broadcast(+, eye(2), [1, 4]) == [2 1; 4 5]
-@test broadcast(+, eye(2), [1  4]) == [2 4; 1 5]
-@test broadcast(+, [1  0], [1, 4]) == [2 1; 5 4]
-@test broadcast(+, [1, 0], [1  4]) == [2 5; 1 4]
-@test broadcast(+, [1, 0], [1, 4]) == [2, 4]
-@test broadcast(+) == 0
-@test broadcast(*) == 1
+function as_sub(x::Vector)
+    y = Array(eltype(x), length(x)*2)
+    y = sub(y, 2:2:length(y))
+    y[:] = x[:]
+    y
+end
+function as_sub(x::Matrix)
+    y = Array(eltype(x), tuple(([size(x)...]*2)...))
+    y = sub(y, 2:2:size(y,1), 2:2:size(y,2))
+    for j=1:size(x,2)
+        for i=1:size(x,1)
+            y[i,j] = x[i,j]
+        end
+    end
+    y
+end
 
-@test eye(2) .+ [1, 4] == [2 1; 4 5]
-@test eye(2) .+ [1  4] == [2 4; 1 5]
-@test [1  0] .+ [1, 4] == [2 1; 5 4]
-@test [1, 0] .+ [1  4] == [2 5; 1 4]
-@test [1, 0] .+ [1, 4] == [2, 4]
-@test [1] .+ [] == []
-
-A = eye(2); @test broadcast!(+, A, A, [1, 4]) == [2 1; 4 5]
-A = eye(2); @test broadcast!(+, A, A, [1  4]) == [2 4; 1 5]
-A = [1  0]; @test_fails broadcast!(+, A, A, [1, 4])
-A = [1  0]; @test broadcast!(+, A, A, [1  4]) == [2 4]
-
-M = [11 12; 21 22]
-@test broadcast_getindex(M, eye(2)+1,[1, 2]) == [21 11; 12 22]
-A = zeros(2,2); broadcast_setindex!(A, [21 11; 12 22], eye(2)+1,[1, 2])
-@test A == M
+for arr in (identity, as_sub)
+    @test broadcast(+, arr(eye(2)), arr([1, 4])) == [2 1; 4 5]
+    @test broadcast(+, arr(eye(2)), arr([1  4])) == [2 4; 1 5]
+    @test broadcast(+, arr([1  0]), arr([1, 4])) == [2 1; 5 4]
+    @test broadcast(+, arr([1, 0]), arr([1  4])) == [2 5; 1 4]
+    @test broadcast(+, arr([1, 0]), arr([1, 4])) == [2, 4]
+    @test broadcast(+) == 0
+    @test broadcast(*) == 1
+    
+    @test arr(eye(2)) .+ arr([1, 4]) == arr([2 1; 4 5])
+    @test arr(eye(2)) .+ arr([1  4]) == arr([2 4; 1 5])
+    @test arr([1  0]) .+ arr([1, 4]) == arr([2 1; 5 4])
+    @test arr([1, 0]) .+ arr([1  4]) == arr([2 5; 1 4])
+    @test arr([1, 0]) .+ arr([1, 4]) == arr([2, 4])
+    @test arr([1]) .+ arr([]) == arr([])
+    
+    A = arr(eye(2)); @test broadcast!(+, A, A, arr([1, 4])) == arr([2 1; 4 5])
+    A = arr(eye(2)); @test broadcast!(+, A, A, arr([1  4])) == arr([2 4; 1 5])
+    A = arr([1  0]); @test_fails broadcast!(+, A, A, arr([1, 4]))
+    A = arr([1  0]); @test broadcast!(+, A, A, arr([1  4])) == arr([2 4])
+    
+    M = arr([11 12; 21 22])
+    @test broadcast_getindex(M, eye(Int, 2)+1,arr([1, 2])) == [21 11; 12 22]
+    
+    A = arr(zeros(2,2))
+    broadcast_setindex!(A, arr([21 11; 12 22]), eye(Int, 2)+1,arr([1, 2]))
+    @test A == M
+end
