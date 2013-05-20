6b526418b882f20d12061d2e02282c76e584d0ac
Stefan Karpinski <stefan@karpinski.org>
2013-05-20 19:00:00 -0400

    pkg2: add .julia directory finding and creation.
    I'm not sure that we still want .julia to always be a git repo now,
    but for the time being, it's easiest to just steal code from Pkg 1.
    In any case, the business of prompting users for their GitHub info
    is a complete annoyance and has to go. We should probably get rid
    or that in the current Pkg, but I'm loathe to change anything there.

--- extras/pkg2.jl
+++ extras/pkg2.jl
@@ -1,18 +1,19 @@
 module Pkg2
 
+include("pkg/dir.jl")
 include("pkg/types.jl")
 include("pkg/read.jl")
 include("pkg/query.jl")
 include("pkg/write.jl")
 
-function requirements()
+requirements() = Dir.cd() do
     avail = Read.available()
     reqs  = Read.parse_requires()
     fix   = Read.fixed(avail)
     Query.requirements(reqs,fix)
 end
 
-function dependencies()
+dependencies() = Dir.cd() do
     avail = Read.available()
     fix   = Read.fixed(avail)
     Query.dependencies(avail,fix)
