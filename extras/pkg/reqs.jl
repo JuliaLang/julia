6970b3462c599ee027554e8eb5fcc239a0c0403e
Stefan Karpinski <stefan@karpinski.org>
2013-05-21 15:09:47 -0400

    pkg2: Write.update_file(f, file, args...) filters and replaces a file.
    This can be used, e.g., to add or remove requirements to REQUIRE:
    
    	Write.update_file(Reqs.add, "REQUIRE", pkg, versions)
    	Write.update_file(Reqs.rm,  "REQUIRE", pkg)

--- extras/pkg/reqs.jl
+++ extras/pkg/reqs.jl
@@ -41,7 +41,7 @@ function parse(io::IO)
 end
 parse(file::String) = isfile(file) ? open(parse,file) : Requires()
 
-function add(input::IO, output::IO, pkg::String, versions::VersionSet)
+function add(input::IO, output::IO, pkg::String, versions::VersionSet=VersionSet())
     existed = false
     for r in process(input)
         if isa(r,Requirement) && r.package == pkg
