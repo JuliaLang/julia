5d9701f59f2752530005cb7b49dbd4c72db9c202
Stefan Karpinski <stefan@karpinski.org>
2013-05-22 14:15:32 -0400

    pkg2: formatting (convert tabs to spaces).

--- extras/pkg/write.jl
+++ extras/pkg/write.jl
@@ -3,20 +3,20 @@ module Write
 using ..Types, ..Reqs
 
 function update_file(f::Function, file::String, args...)
-	tmp = "$file.$(randstring()).tmp"
-	ispath(tmp) && error("tempfile $tmp already exists!?")
-	try
-		x = open(file) do input
-			open(tmp,"w") do output
-				f(input, output, args...)
-			end
-		end
-		run(`mv -f $tmp $file`)
-		return x
-	catch
-		ispath(tmp) && rm(tmp)
-		rethrow()
-	end
+    tmp = "$file.$(randstring()).tmp"
+    ispath(tmp) && error("tempfile $tmp already exists!?")
+    try
+        x = open(file) do input
+            open(tmp,"w") do output
+                f(input, output, args...)
+            end
+        end
+        run(`mv -f $tmp $file`)
+        return x
+    catch
+        ispath(tmp) && rm(tmp)
+        rethrow()
+    end
 end
 
 end # module
