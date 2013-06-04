e92157c77829a050fadc2d45db9c3ac701fdb682
Stefan Karpinski <stefan@karpinski.org>
2013-06-04 17:27:33 -0400

    pkg2: hook new query infrastructure up to old resolve mechanism.

--- extras/pkg2.jl
+++ extras/pkg2.jl
@@ -7,17 +7,44 @@ include("pkg/read.jl")
 include("pkg/query.jl")
 include("pkg/write.jl")
 
-requirements() = Dir.cd() do
-    reqs  = Reqs.parse("REQUIRE")
-    avail = Read.available()
-    fix   = Read.fixed(avail)
-    Query.requirements(reqs,fix)
+using .Types
+import Base.Pkg.Resolve
+
+function versions(vset::VersionSet)
+    vers = VersionNumber[]
+    vset == VersionSet() && return vers
+    for ival in vset.intervals
+        push!(vers, ival.lower)
+        ival.upper < typemax(VersionNumber) &&
+        push!(vers, ival.upper)
+    end
+    return vers
 end
 
-dependencies() = Dir.cd() do
-    avail = Read.available()
-    fix   = Read.fixed(avail)
-    Query.dependencies(avail,fix)
+resolve() = Dir.cd() do
+    reqs  = Reqs.parse("REQUIRE")           :: Dict{ByteString,VersionSet}
+    avail = Read.available()                :: Dict{ByteString,Dict{VersionNumber,Available}}
+    fix   = Read.fixed(avail)               :: Dict{ByteString,Fixed}
+    reqs  = Query.requirements(reqs,fix)    :: Dict{ByteString,VersionSet}
+    deps  = Query.dependencies(avail,fix)   :: Dict{ByteString,Dict{VersionNumber,Available}}
+
+    # adapt for old resolve code
+    vx = Resolve.Version[]
+    rx = Resolve.VersionSet[]
+    dx = Array((Resolve.Version,Resolve.VersionSet),0)
+
+    for (pkg,vset) in reqs
+        push!(rx, Resolve.VersionSet(pkg,versions(vset)))
+    end
+    for (pkg,vers) in deps, (v,a) in vers
+        ver = Resolve.Version(pkg,v)
+        push!(vx, ver)
+        for (p,vset) in a.requires
+            push!(dx, (ver,Resolve.VersionSet(p,versions(vset))))
+        end
+    end
+
+    Resolve.resolve(sort!(rx), sort!(vx), sort!(dx))
 end
 
 end # module
