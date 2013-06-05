31660394dcf2a130349ac9dd5db24897d2fc3f0f
Stefan Karpinski <stefan@karpinski.org>
2013-06-05 09:06:22 -0400

    pkg2: refactor adapter code for Pkg 1 resolve logic into submodule.

--- base/pkg2.jl
+++ base/pkg2.jl
@@ -5,46 +5,18 @@ include("pkg2/types.jl")
 include("pkg2/reqs.jl")
 include("pkg2/read.jl")
 include("pkg2/query.jl")
+include("pkg2/resolve.jl")
 include("pkg2/write.jl")
 
 using .Types
-import Base.Pkg.Resolve
-
-function versions(vset::VersionSet)
-    vers = VersionNumber[]
-    vset == VersionSet() && return vers
-    for ival in vset.intervals
-        push!(vers, ival.lower)
-        ival.upper < typemax(VersionNumber) &&
-        push!(vers, ival.upper)
-    end
-    return vers
-end
 
 resolve() = Dir.cd() do
-    reqs  = Reqs.parse("REQUIRE")           :: Dict{ByteString,VersionSet}
-    avail = Read.available()                :: Dict{ByteString,Dict{VersionNumber,Available}}
-    fix   = Read.fixed(avail)               :: Dict{ByteString,Fixed}
-    reqs  = Query.requirements(reqs,fix)    :: Dict{ByteString,VersionSet}
-    deps  = Query.dependencies(avail,fix)   :: Dict{ByteString,Dict{VersionNumber,Available}}
-
-    # adapt for old resolve code
-    vx = Resolve.Version[]
-    rx = Resolve.VersionSet[]
-    dx = Array((Resolve.Version,Resolve.VersionSet),0)
-
-    for (pkg,vset) in reqs
-        push!(rx, Resolve.VersionSet(pkg,versions(vset)))
-    end
-    for (pkg,vers) in deps, (v,a) in vers
-        ver = Resolve.Version(pkg,v)
-        push!(vx, ver)
-        for (p,vset) in a.requires
-            push!(dx, (ver,Resolve.VersionSet(p,versions(vset))))
-        end
-    end
-
-    Resolve.resolve(sort!(rx), sort!(vx), sort!(dx))
+    reqs  = Reqs.parse("REQUIRE")
+    avail = Read.available()
+    fix   = Read.fixed(avail)
+    reqs  = Query.requirements(reqs,fix)
+    deps  = Query.dependencies(avail,fix)
+    want  = Resolve.resolve(reqs,deps)
 end
 
 add(pkg::ByteString, vers::VersionSet) = Dir.cd() do
