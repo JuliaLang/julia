JuliaLang/julia

commit 5f65f568e6e6de92b3d514eec76d14971c9326a7

fix #3471

src/jltypes.c
@@ -106,12 +106,19 @@ int jl_has_typevars(jl_value_t *v)
     return jl_has_typevars_(v, 0);
 }
 
+static inline int is_unspec(jl_datatype_t *dt)
+{
+    return (jl_datatype_t*)dt->name->primary == dt;
+}
+
 DLLEXPORT int jl_is_leaf_type(jl_value_t *v)
 {
     if (jl_is_datatype(v)) {
         if (((jl_datatype_t*)v)->abstract) {
             if (jl_is_type_type(v)) {
-                return !jl_is_typevar(jl_tparam0(v));
+                jl_value_t *tp0 = jl_tparam0(v);
+                return (jl_is_datatype(tp0) && is_unspec((jl_datatype_t*)tp0)) ||
+                    jl_is_leaf_type(tp0);
             }
             return 0;
         }
@@ -277,11 +284,6 @@ static inline int is_bnd(jl_tvar_t *tv, cenv_t *env)
             return 1;
     }
     return 0;
-}
-
-static inline int is_unspec(jl_datatype_t *dt)
-{
-    return (jl_datatype_t*)dt->name->primary == dt;
 }
 
 static inline int is_btv(jl_value_t *v)

test/core.jl
@@ -852,3 +852,9 @@ function x3220()
     a::Vector{Int} += [1]
 end
 @test x3220() == [2]
+
+# issue #3471
+function f3471(y)
+    convert(Array{typeof(y[1]),1}, y)
+end
+@test isa(f3471({1.0,2.0}), Vector{Float64})

commit e288ff7320a49225bab392d85b11a3e5ea1ef85d

fix perf regression in 5f65f568e6e6de92b3d514eec76d14971c9326a7

src/jltypes.c
@@ -118,7 +118,7 @@ DLLEXPORT int jl_is_leaf_type(jl_value_t *v)
             if (jl_is_type_type(v)) {
                 jl_value_t *tp0 = jl_tparam0(v);
                 return (jl_is_datatype(tp0) && is_unspec((jl_datatype_t*)tp0)) ||
-                    jl_is_leaf_type(tp0);
+                    !jl_has_typevars_(tp0,1);
             }
             return 0;
         }