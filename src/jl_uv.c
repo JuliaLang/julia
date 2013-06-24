JuliaLang/julia

commit 9582f99671fccaec5bc225f0aa0a660d712bbb44

Building libjulia with NMake and Intel C++ Composer

src/jl_uv.c
@@ -1,18 +1,25 @@
+#include "platform.h"
+
+/*
+ * There is no need to define WINVER because it is already defined in Makefile.
+ */
+#if defined(_COMPILER_MINGW_)
 #define WINVER                 _WIN32_WINNT
 #define _WIN32_WINDOWS         _WIN32_WINNT
+#endif
+
 #include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
-#ifdef __WIN32__
-#include <w32api.h>
+#ifdef _OS_WINDOWS_
 #include <ws2tcpip.h>
 #include <malloc.h>
 #else
 #include "errno.h"
+#include <unistd.h>
 #include <sys/socket.h>
 #endif
 
@@ -69,20 +76,20 @@ DLLEXPORT void jl_get_uv_hooks()
 int base_module_conflict = 0; //set to 1 if Base is getting redefined since it means there are two place to try the callbacks
 // warning: this is defined without the standard do {...} while (0) wrapper, since I wanted ret to escape
 // warning: during bootstrapping, callbacks will be called twice if a MethodError occured at ANY time during callback call
-#define JULIA_CB(hook,args...) \
+#define JULIA_CB(hook, ...) \
     jl_value_t *ret; \
     if (!base_module_conflict) { \
-        ret = jl_callback_call(JULIA_HOOK(hook),args); \
+        ret = jl_callback_call(JULIA_HOOK(hook),__VA_ARGS__); \
     } else { \
         JL_TRY { \
-            ret = jl_callback_call(JULIA_HOOK(hook),args); \
+            ret = jl_callback_call(JULIA_HOOK(hook),__VA_ARGS__); \
             /* jl_puts(#hook " original succeeded\n",jl_uv_stderr); */ \
         } \
         JL_CATCH { \
             if (jl_typeof(jl_exception_in_transit) == (jl_value_t*)jl_methoderror_type) { \
                 /* jl_puts("\n" #hook " being retried with new Base bindings --> ",jl_uv_stderr); */ \
                 jl_function_t *cb_func = JULIA_HOOK_((jl_module_t*)jl_get_global(jl_main_module, jl_symbol("Base")), hook); \
-                ret = jl_callback_call(cb_func,args); \
+                ret = jl_callback_call(cb_func,__VA_ARGS__); \
                 /* jl_puts(#hook " succeeded\n",jl_uv_stderr); */ \
             } else { \
                 jl_rethrow(); \
@@ -318,7 +325,9 @@ DLLEXPORT int jl_listen(uv_stream_t* stream, int backlog)
 #ifdef __APPLE__
 #include <crt_externs.h>
 #else
+#if defined(_COMPILER_MINGW_)
 extern char **environ;
+#endif
 #endif
 
 DLLEXPORT int jl_spawn(char *name, char **argv, uv_loop_t *loop,
@@ -334,7 +343,7 @@ DLLEXPORT int jl_spawn(char *name, char **argv, uv_loop_t *loop,
     uv_stdio_container_t stdio[3];
     int error;
     opts.file = name;
-#ifndef __WIN32__
+#ifndef _OS_WINDOWS_
     opts.env = environ;
 #else
     opts.env = NULL;
@@ -358,7 +367,7 @@ DLLEXPORT int jl_spawn(char *name, char **argv, uv_loop_t *loop,
     return error;
 }
 
-#ifdef __WIN32__
+#ifdef _OS_WINDOWS_
 #include <time.h>
 DLLEXPORT struct tm* localtime_r(const time_t *t, struct tm *tm)
 {
@@ -519,7 +528,11 @@ int jl_vprintf(uv_stream_t *s, const char *format, va_list args)
     char *str=NULL;
     int c;
     va_list al;
+#if defined(_OS_WINDOWS_) && !defined(_COMPILER_MINGW_)
+    al = args;
+#else
     va_copy(al, args);
+#endif
 
     c = vasprintf(&str, format, al);
 
@@ -595,7 +608,7 @@ DLLEXPORT int jl_cwd(char *buffer, size_t size)
 
 DLLEXPORT int jl_getpid()
 {
-#ifdef __WIN32__
+#ifdef _OS_WINDOWS_
     return GetCurrentProcessId();
 #else
     return getpid();
@@ -631,7 +644,7 @@ DLLEXPORT void getlocalip(char *buf, size_t len)
         ifa = (ifAddrStruct+i)->address.address4;
         if (ifa.sin_family==AF_INET) { // check it is IP4
             // is a valid IP4 Address
-#ifndef __WIN32__
+#ifndef _OS_WINDOWS_
             tmpAddrPtr=&(ifa.sin_addr);
             inet_ntop(AF_INET, tmpAddrPtr, buf, len); //Not available on WinXP
 #else

Windows.inc
@@ -48,12 +48,9 @@ CFLAGS = $(CFLAGS) /D_AMD64_=1 /DWIN64 /D_WIN64
 !ifdef DEBUG
 CFLAGS = $(CFLAGS) /Zi /Od /DDEBUG /MDd
 LFLAGS = $(LFLAGS) /DEBUG
-DLL_LFLAGS = $(LFLAGS) /LDd
 !else
 CFLAGS = $(CFLAGS) /Ox /DNDEBUG /MD
 LFLAGS = $(LFLAGS) /RELEASE
-DLL_LFLAGS = $(LFLAGS) /LD
-
 !endif
 
 !endif # _MAKEFILE_INC_

src/.gitignore
@@ -1,6 +1,10 @@
 /*.o
 /*.do
 /*.exe
+/*.obj
+/*.lib
+/*.pdb
+/*.exp
 
 /boot.j.inc
 julia_flisp.boot

src/Windows.mk
@@ -1,0 +1,123 @@
+!INCLUDE <..\Windows.inc>
+
+.SUFFIXES: .c
+
+NAME = julia
+
+HEADERS = \
+	builtin_proto.h \
+	file_constants.h  \
+	jltypes_internal.h  \
+	julia.h  \
+	newobj_internal.h  \
+	os_detect.h  \
+	uv_constants.h
+
+OBJECTS = \
+	jltypes.obj \
+	gf.obj \
+	ast.obj \
+	builtins.obj \
+	module.obj \
+	codegen.obj \
+	interpreter.obj \
+	alloc.obj \
+	dlload.obj \
+	sys.obj \
+	init.obj \
+	task.obj \
+	array.obj \
+	dump.obj \
+	toplevel.obj \
+	jl_uv.obj \
+	jlapi.obj \
+	gc.obj
+
+LIBFLISP = flisp\libflisp.lib
+LIBSUPPORT = support\libsupport.lib
+LIBUV = ..\deps\libuv\libuv.lib
+FLISP = flisp\flisp.exe
+
+INCLUDE = $(INCLUDE);$(MAKEDIR)\..\deps\libuv\include;$(MAKEDIR)\flisp;$(MAKEDIR)\support;C:\Program Files\LLVM\include;
+LIB = $(LIB);C:\Program Files\LLVM\lib;
+
+CFLAGS = $(CFLAGS) -DCOPY_STACKS -D_CRT_SECURE_NO_WARNINGS
+CFLAGS = $(CFLAGS) -DJL_SYSTEM_IMAGE_PATH=\"../lib/julia/sys.ji\"
+
+LIBWINDOWS = \
+	kernel32.lib \
+	ws2_32.lib \
+	psapi.lib \
+	advapi32.lib \
+	iphlpapi.lib \
+	shell32.lib
+
+LIBLLVM = \
+	LLVMAnalysis.lib \
+	LLVMArchive.lib \
+	LLVMAsmParser.lib \
+	LLVMAsmPrinter.lib \
+	LLVMBitReader.lib \
+	LLVMBitWriter.lib \
+	LLVMCodeGen.lib \
+	LLVMCore.lib \
+	LLVMDebugInfo.lib \
+	LLVMExecutionEngine.lib \
+	LLVMInstCombine.lib \
+	LLVMInstrumentation.lib \
+	LLVMInterpreter.lib \
+	LLVMipa.lib \
+	LLVMipo.lib \
+	LLVMJIT.lib \
+	LLVMLinker.lib \
+	LLVMMC.lib \
+	LLVMMCDisassembler.lib \
+	LLVMMCJIT.lib \
+	LLVMMCParser.lib \
+	LLVMObject.lib \
+	LLVMRuntimeDyld.lib \
+	LLVMScalarOpts.lib \
+	LLVMSelectionDAG.lib \
+	LLVMSupport.lib \
+	LLVMTableGen.lib \
+	LLVMTarget.lib \
+	LLVMTransformUtils.lib \
+	LLVMVectorize.lib \
+	LLVMX86AsmParser.lib \
+	LLVMX86AsmPrinter.lib \
+	LLVMX86CodeGen.lib \
+	LLVMX86Desc.lib \
+	LLVMX86Disassembler.lib \
+	LLVMX86Info.lib \
+	LLVMX86Utils.lib
+
+default: lib$(NAME).dll
+
+lib$(NAME).dll: $(OBJECTS) flisp\libflisp.lib support\libsupport.lib ..\deps\libuv\libuv.lib
+	$(LINK) $(LFLAGS) /DLL $(LIBWINDOWS) $(LIBLLVM) /OUT:lib$(NAME).dll $**
+
+$(FLISP) $(LIBFLISP):
+	PUSHD flisp && $(MAKE) /NOLOGO /F Windows.mk && POPD
+
+$(LIBSUPPORT):
+	PUSHD support && $(MAKE) /NOLOGO /F Windows.mk && POPD
+
+$(LIBUV):
+	PUSHD ..\deps\libuv && $(MAKE) /NOLOGO /F Windows.mk && POPD
+
+ast.obj: julia_flisp.boot.inc flisp\*.h
+
+julia_flisp.boot.inc: julia_flisp.boot $(FLISP)
+	.\flisp\flisp .\bin2hex.scm <julia_flisp.boot >$@
+
+julia_flisp.boot: julia-parser.scm julia-syntax.scm match.scm utils.scm jlfrontend.scm mk_julia_flisp_boot.scm $(FLISP)
+	.\flisp\flisp .\mk_julia_flisp_boot.scm
+
+.c.obj:
+	$(CC) $(CFLAGS) $<
+
+.cpp.obj:
+	$(CC) $(CFLAGS) $<
+
+# vim: noexpandtab:ts=4:sw=4:
+

src/alloc.c
@@ -499,7 +499,7 @@ void jl_add_constructors(jl_datatype_t *t)
             size_t np = jl_tuple_len(t->parameters);
             jl_tuple_t *sparams = jl_alloc_tuple_uninit(np*2);
             jl_function_t *cfactory = NULL;
-            JL_GC_PUSH(&sparams, &cfactory);
+            JL_GC_PUSH2(&sparams, &cfactory);
             for(size_t i=0; i < np; i++) {
                 jl_tupleset(sparams, i*2+0,
                             jl_tupleref(((jl_datatype_t*)t->name->primary)->parameters, i));
@@ -572,7 +572,7 @@ jl_datatype_t *jl_new_datatype(jl_sym_t *name, jl_datatype_t *super,
 {
     jl_datatype_t *t=NULL;
     jl_typename_t *tn=NULL;
-    JL_GC_PUSH(&t, &tn);
+    JL_GC_PUSH2(&t, &tn);
 
     if (!jl_boot_file_loaded && jl_is_symbol(name)) {
         // hack to avoid making two versions of basic types needed
@@ -815,7 +815,7 @@ jl_value_t *jl_box_bool(int8_t x)
 jl_expr_t *jl_exprn(jl_sym_t *head, size_t n)
 {
     jl_array_t *ar = n==0 ? (jl_array_t*)jl_an_empty_cell : jl_alloc_cell_1d(n);
-    JL_GC_PUSH(&ar);
+    JL_GC_PUSH1(&ar);
     jl_expr_t *ex = (jl_expr_t*)alloc_4w();
     ex->type = (jl_value_t*)jl_expr_type;
     ex->head = head;
@@ -832,7 +832,7 @@ JL_CALLABLE(jl_f_new_expr)
     JL_NARGSV(Expr, 1);
     JL_TYPECHK(Expr, symbol, args[0]);
     jl_array_t *ar = jl_alloc_cell_1d(nargs-1);
-    JL_GC_PUSH(&ar);
+    JL_GC_PUSH1(&ar);
     for(size_t i=1; i < nargs; i++)
         jl_cellset(ar, i-1, args[i]);
     jl_expr_t *ex = (jl_expr_t*)alloc_4w();

src/array.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#ifdef __WIN32__
+#ifdef _OS_WINDOWS_
 #include <malloc.h>
 #endif
 #include "julia.h"
@@ -80,7 +80,7 @@ static jl_array_t *_new_array(jl_value_t *atype,
         }
         tsz = (tsz+15)&-16; // align whole object size 16
         a = allocobj(tsz);
-        JL_GC_PUSH(&a);
+        JL_GC_PUSH1(&a);
         a->type = atype;
         a->ismalloc = 1;
         a->isinline = 0;
@@ -140,7 +140,7 @@ jl_array_t *jl_reshape_array(jl_value_t *atype, jl_array_t *data,
         a->elsize = sizeof(void*);
         a->ptrarray = 1;
     }
-    JL_GC_PUSH(&a);
+    JL_GC_PUSH1(&a);
 
     char *d = data->data;
     if (data->ndims == 1) d -= data->offset*data->elsize;
@@ -352,7 +352,7 @@ jl_value_t *jl_array_to_string(jl_array_t *a)
 jl_value_t *jl_pchar_to_string(const char *str, size_t len)
 {
     jl_array_t *a = jl_pchar_to_array(str, len);
-    JL_GC_PUSH(&a);
+    JL_GC_PUSH1(&a);
     jl_value_t *s = jl_array_to_string(a);
     JL_GC_POP();
     return s;
@@ -405,7 +405,7 @@ JL_CALLABLE(jl_f_arraysize)
         JL_NARGS(arraysize, 1, 1);
     }
     jl_tuple_t *d = jl_alloc_tuple(nd);
-    JL_GC_PUSH(&d);
+    JL_GC_PUSH1(&d);
     size_t i;
     for(i=0; i < nd; i++)
         jl_tupleset(d, i, jl_box_long(jl_array_dim(a,i)));

src/ast.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
-#ifdef __WIN32__
+#ifdef _OS_WINDOWS_
 #include <malloc.h>
 #endif
 #include "julia.h"
@@ -420,7 +420,7 @@ DLLEXPORT jl_value_t *jl_parse_string(const char *str, int pos0, int greedy)
     value_t p = fl_applyn(3, symbol_value(symbol("jl-parse-one-string")),
                           s, fixnum(pos0), greedy?FL_T:FL_F);
     jl_value_t *expr=NULL, *pos1=NULL;
-    JL_GC_PUSH(&expr, &pos1);
+    JL_GC_PUSH2(&expr, &pos1);
 
     value_t e = car_(p);
     if (e == FL_EOF) {
@@ -505,7 +505,7 @@ jl_lambda_info_t *jl_wrap_expr(jl_value_t *expr)
     // `(lambda () (() () ()) ,expr)
     jl_expr_t *le=NULL, *bo=NULL; jl_value_t *vi=NULL;
     jl_value_t *mt = jl_an_empty_cell;
-    JL_GC_PUSH(&le, &vi, &bo);
+    JL_GC_PUSH3(&le, &vi, &bo);
     le = jl_exprn(lambda_sym, 3);
     jl_cellset(le->args, 0, mt);
     vi = (jl_value_t*)jl_alloc_cell_1d(3);
@@ -631,7 +631,7 @@ static jl_value_t *copy_ast(jl_value_t *expr, jl_tuple_t *sp, int do_sp)
         */
         // TODO: avoid if above condition is true and decls have already
         // been evaluated.
-        JL_GC_PUSH(&li);
+        JL_GC_PUSH1(&li);
         li = jl_add_static_parameters(li, sp);
         li->ast = jl_prepare_ast(li, li->sparams);
         JL_GC_POP();
@@ -640,7 +640,7 @@ static jl_value_t *copy_ast(jl_value_t *expr, jl_tuple_t *sp, int do_sp)
     else if (jl_typeis(expr,jl_array_any_type)) {
         jl_array_t *a = (jl_array_t*)expr;
         jl_array_t *na = jl_alloc_cell_1d(jl_array_len(a));
-        JL_GC_PUSH(&na);
+        JL_GC_PUSH1(&na);
         size_t i;
         for(i=0; i < jl_array_len(a); i++)
             jl_cellset(na, i, copy_ast(jl_cellref(a,i), sp, do_sp));
@@ -650,7 +650,7 @@ static jl_value_t *copy_ast(jl_value_t *expr, jl_tuple_t *sp, int do_sp)
     else if (jl_is_expr(expr)) {
         jl_expr_t *e = (jl_expr_t*)expr;
         jl_expr_t *ne = jl_exprn(e->head, jl_array_len(e->args));
-        JL_GC_PUSH(&ne);
+        JL_GC_PUSH1(&ne);
         if (e->head == lambda_sym) {
             jl_exprarg(ne, 0) = copy_ast(jl_exprarg(e,0), sp, 0);
             jl_exprarg(ne, 1) = copy_ast(jl_exprarg(e,1), sp, 0);
@@ -725,7 +725,7 @@ jl_value_t *jl_prepare_ast(jl_lambda_info_t *li, jl_tuple_t *sparams)
     jl_tuple_t *spenv = NULL;
     jl_value_t *ast = li->ast;
     if (ast == NULL) return NULL;
-    JL_GC_PUSH(&spenv, &ast);
+    JL_GC_PUSH2(&spenv, &ast);
     spenv = jl_tuple_tvars_to_symbols(sparams);
     if (!jl_is_expr(ast)) {
         ast = jl_uncompress_ast(li, ast);

src/builtins.c
@@ -1,6 +1,8 @@
 /*
   implementations of some built-in functions and utilities
 */
+#include "platform.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,12 +12,18 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <unistd.h>
-#ifdef __WIN32__
+#if defined(_OS_WINDOWS_)
 #include <malloc.h>
+#if defined(_COMPILER_INTEL_)
+#include <mathimf.h>
+#else
+#include <math.h>
+#endif
+#else
+#include <unistd.h>
+#include <math.h>
 #endif
 #include <ctype.h>
-#include <math.h>
 #include "julia.h"
 #include "builtin_proto.h"
 
@@ -28,7 +36,7 @@ void jl_error(const char *str)
         jl_exit(1);
     }
     jl_value_t *msg = jl_pchar_to_string((char*)str, strlen(str));
-    JL_GC_PUSH(&msg);
+    JL_GC_PUSH1(&msg);
     jl_throw(jl_new_struct(jl_errorexception_type, msg));
 }
 
@@ -44,7 +52,7 @@ void jl_errorf(const char *fmt, ...)
         jl_exit(1);
     }
     jl_value_t *msg = jl_pchar_to_string(buf, nc);
-    JL_GC_PUSH(&msg);
+    JL_GC_PUSH1(&msg);
     jl_throw(jl_new_struct(jl_errorexception_type, msg));
 }
 
@@ -63,7 +71,7 @@ void jl_type_error_rt(const char *fname, const char *context,
                       jl_value_t *ty, jl_value_t *got)
 {
     jl_value_t *ctxt=NULL;
-    JL_GC_PUSH(&ctxt, &got);
+    JL_GC_PUSH2(&ctxt, &got);
     ctxt = jl_pchar_to_string((char*)context, strlen(context));
     jl_value_t *ex = jl_new_struct(jl_typeerror_type, jl_symbol(fname),
                                    ctxt, ty, got);
@@ -226,7 +234,7 @@ JL_CALLABLE(jl_f_apply)
             if (jl_is_array(args[1])) {
                 size_t n = jl_array_len(args[1]);
                 jl_tuple_t *t = jl_alloc_tuple(n);
-                JL_GC_PUSH(&t);
+                JL_GC_PUSH1(&t);
                 for(size_t i=0; i < n; i++) {
                     jl_tupleset(t, i, jl_arrayref((jl_array_t*)args[1], i));
                 }
@@ -278,7 +286,7 @@ JL_CALLABLE(jl_f_apply)
 
  fancy_apply: ;
     jl_value_t *argarr = jl_apply(jl_append_any_func, &args[1], nargs-1);
-    JL_GC_PUSH(&argarr);
+    JL_GC_PUSH1(&argarr);
     assert(jl_typeis(argarr, jl_array_any_type));
     jl_value_t *result = jl_apply((jl_function_t*)args[0],
                                   jl_cell_data(argarr), jl_array_len(argarr));
@@ -526,7 +534,7 @@ JL_CALLABLE(jl_f_convert_tuple)
         return (jl_value_t*)x;
     size_t i, cl=jl_tuple_len(x), pl=jl_tuple_len(to);
     jl_tuple_t *out = jl_alloc_tuple(cl);
-    JL_GC_PUSH(&out);
+    JL_GC_PUSH1(&out);
     jl_value_t *ce, *pe=NULL;
     int pseq=0;
     jl_function_t *f = (jl_function_t*)args[2];
@@ -612,7 +620,11 @@ DLLEXPORT int jl_strtof(char *str, float *out)
 {
     char *p;
     errno = 0;
+#if defined(_OS_WINDOWS_) && !defined(_COMPILER_MINGW_)
+    *out = (float)strtod(str, &p);
+#else
     *out = strtof(str, &p);
+#endif
     if (p == str ||
         (errno==ERANGE && (*out==0 || *out==HUGE_VALF || *out==-HUGE_VALF)))
         return 1;
@@ -856,7 +868,7 @@ JL_CALLABLE(jl_f_union)
             jl_tupleset(argt, i, args[i]);
         }
     }
-    JL_GC_PUSH(&argt);
+    JL_GC_PUSH1(&argt);
     jl_value_t *u = jl_type_union(argt);
     JL_GC_POP();
     return u;

commit 812b033f65727da8872e14db97fdfe10d7bafd1b

fix freebsd regression caused by 9582f99671fccaec5bc225f0aa0a660d712bbb44

src/jl_uv.c
@@ -350,10 +350,8 @@ DLLEXPORT int jl_listen(uv_stream_t* stream, int backlog)
 
 #ifdef __APPLE__
 #include <crt_externs.h>
-#else
-#if defined(_COMPILER_MINGW_)
+#elif !defined(_OS_WINDOWS_) || defined(_COMPILER_MINGW_)
 extern char **environ;
-#endif
 #endif
 
 DLLEXPORT int jl_spawn(char *name, char **argv, uv_loop_t *loop,