bc57f118fc4687b5f579aae705772c1a8afe456d
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-07-07 16:05:25 -0400

    remove global state from shootout benchmarks so they can be run multiple times

--- test/perf/shootout/nbody.jl
+++ test/perf/shootout/nbody.jl
@@ -23,42 +23,6 @@ type Body
     mass::Float64
 end
 
-const jupiter = Body( 4.84143144246472090e+00,                   # x
-                     -1.16032004402742839e+00,                   # y
-                     -1.03622044471123109e-01,                   # z
-                      1.66007664274403694e-03 * days_per_year,   # vx
-                      7.69901118419740425e-03 * days_per_year,   # vy
-                     -6.90460016972063023e-05 * days_per_year,   # vz
-                      9.54791938424326609e-04 * solar_mass)      # mass
-
-const saturn = Body( 8.34336671824457987e+00,
-                     4.12479856412430479e+00,
-                    -4.03523417114321381e-01,
-                    -2.76742510726862411e-03 * days_per_year,
-                     4.99852801234917238e-03 * days_per_year,
-                     2.30417297573763929e-05 * days_per_year,
-                     2.85885980666130812e-04 * solar_mass)     
-
-const uranus = Body( 1.28943695621391310e+01,
-                    -1.51111514016986312e+01,
-                    -2.23307578892655734e-01,
-                     2.96460137564761618e-03 * days_per_year,
-                     2.37847173959480950e-03 * days_per_year,
-                    -2.96589568540237556e-05 * days_per_year,
-                     4.36624404335156298e-05 * solar_mass)
-
-const neptune = Body( 1.53796971148509165e+01,
-                     -2.59193146099879641e+01,
-                      1.79258772950371181e-01,
-                      2.68067772490389322e-03 * days_per_year,
-                      1.62824170038242295e-03 * days_per_year,
-                     -9.51592254519715870e-05 * days_per_year,
-                      5.15138902046611451e-05 * solar_mass)
-
-const sun = Body(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, solar_mass)
-
-bodies = [sun, jupiter, saturn, uranus, neptune]
-
 function offset_momentum(b::Body, px, py, pz)
     b.vx = -px / solar_mass
     b.vy = -py / solar_mass
@@ -122,6 +86,42 @@ end
 
 
 function nbody(N::Int=1000)
+    jupiter = Body( 4.84143144246472090e+00,                   # x
+                   -1.16032004402742839e+00,                   # y
+                   -1.03622044471123109e-01,                   # z
+                   1.66007664274403694e-03 * days_per_year,   # vx
+                   7.69901118419740425e-03 * days_per_year,   # vy
+                   -6.90460016972063023e-05 * days_per_year,   # vz
+                   9.54791938424326609e-04 * solar_mass)      # mass
+    
+    saturn = Body( 8.34336671824457987e+00,
+                  4.12479856412430479e+00,
+                  -4.03523417114321381e-01,
+                  -2.76742510726862411e-03 * days_per_year,
+                  4.99852801234917238e-03 * days_per_year,
+                  2.30417297573763929e-05 * days_per_year,
+                  2.85885980666130812e-04 * solar_mass)
+    
+    uranus = Body( 1.28943695621391310e+01,
+                  -1.51111514016986312e+01,
+                  -2.23307578892655734e-01,
+                  2.96460137564761618e-03 * days_per_year,
+                  2.37847173959480950e-03 * days_per_year,
+                  -2.96589568540237556e-05 * days_per_year,
+                  4.36624404335156298e-05 * solar_mass)
+    
+    neptune = Body( 1.53796971148509165e+01,
+                   -2.59193146099879641e+01,
+                   1.79258772950371181e-01,
+                   2.68067772490389322e-03 * days_per_year,
+                   1.62824170038242295e-03 * days_per_year,
+                   -9.51592254519715870e-05 * days_per_year,
+                   5.15138902046611451e-05 * solar_mass)
+    
+    sun = Body(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, solar_mass)
+    
+    bodies = [sun, jupiter, saturn, uranus, neptune]
+
     init_sun(bodies)
 #    @printf("%.9f\n", energy(bodies))
     for i = 1:N
