f693284a67a67cab5489cf48eac5a99efdbf5ccf
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-07-08 17:41:22 -0400

    remove redundant timeit macro in testdefs fix LOAD_PATH modification not to assume "." is in the array

--- test/testdefs.jl
+++ test/testdefs.jl
@@ -5,16 +5,6 @@ function runtests(name)
     Core.include("$name.jl")
 end
 
-macro timeit(ex,name)
-    quote
-        t = Inf
-        for i = 1:5
-            t = min(t, @elapsed $(esc(ex)))
-        end
-        println(rpad(string($name,":"), 20), t)
-    end
-end
-
 function propagate_errors(a,b)
     if isa(a,Exception)
         rethrow(a)
@@ -25,4 +15,5 @@ function propagate_errors(a,b)
     nothing
 end
 
-shift!(LOAD_PATH) # looking in . messes things up badly
+# looking in . messes things up badly
+filter!(x->x!=".", LOAD_PATH)
