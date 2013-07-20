0de11bcfde27810915fcf1829e00ebcab30451fd
Andreas Noack Jensen <andreasnoackjensen@gmail.com>
2013-07-20 15:28:37 +0200

    Fix matrix vector multiplication when dimension is zero

--- base/linalg/matmul.jl
+++ base/linalg/matmul.jl
@@ -229,6 +229,7 @@ function gemv{T<:BlasFloat}(y::StridedVector{T},
     if nA != length(x); error("*: argument shapes do not match"); end
     if mA != length(y); error("*: output size is incorrect"); end
     if mA == 0; return zeros(T, 0); end
+    if nA == 0; return zeros(T, mA); end
 
     BLAS.gemv!(tA, one(T), A, x, zero(T), y)
 end
