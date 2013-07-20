c07db1017702a01263659352fa3b3975dcfd5e1c
Keno Fischer <kfischer@college.harvard.edu>
2013-07-19 21:21:16 -0400

    Fix #2815

--- src/init.c
+++ src/init.c
@@ -112,6 +112,7 @@ void fpe_handler(int arg)
 }
 
 #ifndef _OS_WINDOWS_
+#if defined(_OS_LINUX_)
 void segv_handler(int sig, siginfo_t *info, void *context)
 {
     sigset_t sset;
@@ -142,6 +143,7 @@ void segv_handler(int sig, siginfo_t *info, void *context)
             raise(sig);
     }
 }
+#endif
 
 #endif
 
@@ -504,6 +506,114 @@ void init_stdio()
     JL_STDIN = init_stdio_handle(0,1);
 }
 
+static void *signal_stack;
+
+#ifdef _OS_DARWIN_
+
+#include <mach/mach_traps.h>
+#include <mach/task.h>
+#define UNW_LOCAL_ONLY
+#include <libunwind.h>
+static mach_port_t segv_port = 0;
+
+extern boolean_t exc_server(mach_msg_header_t *, mach_msg_header_t *);
+
+void * mach_segv_listener(void *arg)
+{
+    (void) arg;
+    while (1) {
+        int ret = mach_msg_server(exc_server,2048,segv_port,MACH_MSG_TIMEOUT_NONE);
+        printf("mach_msg_server: %s\n", mach_error_string(ret));
+        jl_exit(1);
+    }
+}
+
+extern ptrint_t bt_data[MAX_BT_SIZE+1];
+extern size_t bt_size;
+void NORETURN throw_internal(jl_value_t *e);
+
+void darwin_stack_overflow_handler(unw_context_t *uc)
+{
+    unw_word_t ip;
+    unw_cursor_t cursor;
+    unw_init_local(&cursor,uc);
+    size_t n=0;
+    while (unw_step(&cursor) > 0 && n < MAX_BT_SIZE) {
+        if (unw_get_reg(&cursor, UNW_REG_IP, &ip) < 0) {
+            break;
+        }
+        bt_data[n++] = ip;
+    }
+    bt_size = n;
+    throw_internal(jl_stackovf_exception);
+}
+
+void darwin_segfault_exit()
+{
+    JL_PRINTF(JL_STDERR,"Segmentation Fault.\n");
+    uv_tty_reset_mode();
+    jl_exit(1);
+}
+
+#define HANDLE_MACH_ERROR(msg, retval) \
+    if(retval!=KERN_SUCCESS) { mach_error(msg ":", (retval)); jl_exit(1); }
+
+//exc_server uses dlsym to find symbol
+DLLEXPORT kern_return_t catch_exception_raise
+                (mach_port_t                          exception_port,
+                 mach_port_t                                  thread,
+                 mach_port_t                                    task,
+                 exception_type_t                          exception,
+                 exception_data_t                               code,
+                 mach_msg_type_number_t                   code_count)
+{
+    unsigned int count = MACHINE_THREAD_STATE_COUNT;
+    unsigned int exc_count = X86_EXCEPTION_STATE64_COUNT;
+    x86_thread_state64_t state, old_state;
+    x86_exception_state64_t exc_state;
+    kern_return_t ret;
+    //memset(&state,0,sizeof(x86_thread_state64_t));
+    //memset(&exc_state,0,sizeof(x86_exception_state64_t));
+    ret = thread_get_state(thread,x86_EXCEPTION_STATE64,(thread_state_t)&exc_state,&exc_count);
+    uint64_t fault_addr = exc_state.__faultvaddr;
+    ret = thread_get_state(thread,x86_THREAD_STATE64,(thread_state_t)&state,&count);
+    HANDLE_MACH_ERROR("thread_get_state",ret);
+    old_state = state;
+    state.__rsp = (((uint64_t)signal_stack + SIGSTKSZ - 512)&-16)-sizeof(void*);
+    if (
+#ifdef COPY_STACKS
+        (char*)fault_addr > (char*)jl_stack_lo-3000000 &&
+        (char*)fault_addr < (char*)jl_stack_hi
+#else
+        (char*)fault_addr > (char*)jl_current_task->stack-8192 &&
+        (char*)fault_addr <
+        (char*)jl_current_task->stack+jl_current_task->ssize
+#endif
+        ) {
+        // Setup libunwind information
+        state.__rsp = (uint64_t)signal_stack + SIGSTKSZ;
+        state.__rsp -= sizeof(unw_context_t);
+        state.__rsp &= -16;
+        state.__rsp -= 512 + sizeof(void*);
+        state.__rsp &= -16;
+        unw_context_t *uc = (unw_context_t*)state.__rsp;
+        memset(uc,0,sizeof(x86_thread_state64_t));
+        memcpy(uc,&old_state,sizeof(x86_thread_state64_t));
+        state.__rdi = (uint64_t)uc;
+        state.__rip = (uint64_t)darwin_stack_overflow_handler;
+    }
+    else {
+        state.__rip = (uint64_t)darwin_segfault_exit;
+    }
+
+    state.__rbp = state.__rsp;
+    ret = thread_set_state(thread,x86_THREAD_STATE64,(thread_state_t)&state,count);
+    HANDLE_MACH_ERROR("thread_set_state",ret);
+    return KERN_SUCCESS;
+}
+
+#endif
+
 void julia_init(char *imageFile)
 {
     jl_page_size = jl_getpagesize();
@@ -600,6 +710,8 @@ void julia_init(char *imageFile)
     jl_module_import(jl_main_module, jl_core_module, jl_symbol("eval"));
     jl_current_module = jl_main_module;
 
+    signal_stack = malloc(SIGSTKSZ);
+
 #ifndef _OS_WINDOWS_
     struct sigaction actf;
     memset(&actf, 0, sizeof(struct sigaction));
@@ -610,11 +722,11 @@ void julia_init(char *imageFile)
         JL_PRINTF(JL_STDERR, "sigaction: %s\n", strerror(errno));
         jl_exit(1);
     }
-
+#if defined(_OS_LINUX_)
     stack_t ss;
     ss.ss_flags = 0;
     ss.ss_size = SIGSTKSZ;
-    ss.ss_sp = malloc(ss.ss_size);
+    ss.ss_sp = signal_stack;
     if (sigaltstack(&ss, NULL) < 0) {
         JL_PRINTF(JL_STDERR, "sigaltstack: %s\n", strerror(errno));
         jl_exit(1);
@@ -634,6 +746,33 @@ void julia_init(char *imageFile)
         JL_PRINTF(JL_STDERR, "Couldn't set SIGPIPE\n");
         jl_exit(1);
     }
+#elif defined (_OS_DARWIN_)
+    kern_return_t ret;
+    mach_port_t self = mach_task_self();
+    ret = mach_port_allocate(self,MACH_PORT_RIGHT_RECEIVE,&segv_port);
+    HANDLE_MACH_ERROR("mach_port_allocate",ret);
+    ret = mach_port_insert_right(self,segv_port,segv_port,MACH_MSG_TYPE_MAKE_SEND);
+    HANDLE_MACH_ERROR("mach_port_insert_right",ret);
+
+    // Alright, create a thread to serve as the listener for exceptions
+    pthread_t thread;
+    pthread_attr_t attr;
+    if (pthread_attr_init(&attr) != 0)
+    {
+        JL_PRINTF(JL_STDERR, "pthread_attr_init failed");
+        jl_exit(1);  
+    }
+    pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
+    if (pthread_create(&thread,&attr,mach_segv_listener,NULL) != 0)
+    {
+        JL_PRINTF(JL_STDERR, "pthread_create failed");
+        jl_exit(1);  
+    }     
+    pthread_attr_destroy(&attr);
+
+    ret = task_set_exception_ports(self,EXC_MASK_BAD_ACCESS,segv_port,EXCEPTION_DEFAULT,MACHINE_THREAD_STATE);
+    HANDLE_MACH_ERROR("task_set_exception_ports",ret);
+#endif
 #else
     if (signal(SIGFPE, (void (__cdecl *)(int))fpe_handler) == SIG_ERR) {
         JL_PRINTF(JL_STDERR, "Couldn't set SIGFPE\n");
