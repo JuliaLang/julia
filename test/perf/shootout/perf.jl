25c30ff8cec5da15afc5c7c199b76713b5faf9aa
Elliot Saba <staticfloat@gmail.com>
2013-07-24 10:25:21 -0700

    First draft of codespeedization of test/perf
    * Change webhook notification URLs to julia.mit.edu (Linux) and criid.ee.washington.edu (OSX)
    * Add `codespeed` target to test/perf/Makefile, which will run perfsuite and upload result to hardcoded codespeed installation location
      * Note that `make codespeed` will attempt to `Pkg.add()` the packages `JSON` and `Curl` if not already installed.
      * The sort subtests run a limited subset of 28 tests instead of almost 800
    * Annotate all @time macro calls with an extra "Description" field, used to populate the codespeed database for the first time
    * Until extra build-time information is provided, expect environment variables such as JULIA_FLAVOR, JULIA_BRANCH, and JULIA_COMMIT_DATE on the command line when `make codespeed` is invoked.

--- test/perf/shootout/perf.jl
+++ test/perf/shootout/perf.jl
@@ -1,10 +1,10 @@
 include("../perfutil.jl")
 
 include("binary_trees.jl")
-@timeit binary_trees(10) "binary_trees"
+@timeit binary_trees(10) "binary_trees" "Allocate and deallocate many many binary trees"
 
 include("fannkuch.jl")
-@timeit fannkuch(7) "fannkuch"
+@timeit fannkuch(7) "fannkuch" "Indexed-access to tiny integer-sequence"
 
 include("fasta.jl")
 n = 100
@@ -12,34 +12,34 @@ n = 100
     repeat_fasta(alu, 2n)
     random_fasta(iub[1], iub[2], 3n)
     random_fasta(homosapiens[1], homosapiens[2], 5n)
-end "fasta"
+end "fasta" "Generate and write random DNA sequences"
 
 include("k_nucleotide.jl")
-@timeit k_nucleotide("shootout/knucleotide-input.txt") "k_nucleotide"
+@timeit k_nucleotide("shootout/knucleotide-input.txt") "k_nucleotide" "Hashtable update and k-nucleotide strings"
 
 include("mandelbrot.jl")
-@timeit mandelbrot(200, "shootout/mandelbrot-output-julia.txt") "mandelbrot"
+@timeit mandelbrot(200, "shootout/mandelbrot-output-julia.txt") "mandelbrot" "Generate Mandelbrot set portable bitmap file"
 
 include("meteor_contest.jl")
-@timeit1 meteor_contest() "meteor_contest"
+@timeit1 meteor_contest() "meteor_contest" "Search for solutions to shape packing puzzle"
 
 include("nbody.jl")
 using NBody
-@timeit NBody.nbody() "nbody"
+@timeit NBody.nbody() "nbody" "Double-precision N-body simulation"
 
 include("nbody_vec.jl")
 using NBodyVec
-@timeit NBodyVec.nbody_vec() "nbody_vec"
+@timeit NBodyVec.nbody_vec() "nbody_vec" "A vectorized double-precision N-body simulation"
 
 include("pidigits.jl")
 @assert pidigits(1000) == 9216420198
-@timeit pidigits(1000) "pidigits"
+@timeit pidigits(1000) "pidigits" "Streaming arbitrary-precision arithmetic"
 
 include("regex_dna.jl")
-@timeit regex_dna("shootout/regexdna-input.txt") "regex_dna"
+@timeit regex_dna("shootout/regexdna-input.txt") "regex_dna" "Match DNA 8-mers and substitute nucleotides for IUB codes"
 
 include("revcomp.jl")
-@timeit revcomp("shootout/revcomp-input.txt") "revcomp"
+@timeit revcomp("shootout/revcomp-input.txt") "revcomp" "Read DNA sequences - write their reverse-complement"
 
 include("spectralnorm.jl")
-@timeit spectralnorm() "spectralnorm"
+@timeit spectralnorm() "spectralnorm" "Eigenvalue using the power method"
