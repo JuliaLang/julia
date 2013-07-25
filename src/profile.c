8247ac7321ea195dac7358902169234bb12fbcbc
Jameson Nash <vtjnash@gmail.com>
2013-07-25 00:51:45 -0400

    fix #2838, helps with #2597

--- src/profile.c
+++ src/profile.c
@@ -2,30 +2,91 @@
 #include <stddef.h>
 #include "julia.h"
 
-extern size_t rec_backtrace(ptrint_t *bt_data, size_t maxsize);
-
-static ptrint_t* bt_data_prof = NULL;
-static size_t bt_size_max = 0;
-static size_t bt_size_cur = 0;
-static u_int64_t nsecprof;
+static volatile ptrint_t* bt_data_prof = NULL;
+static volatile size_t bt_size_max = 0;
+static volatile size_t bt_size_cur = 0;
+static volatile u_int64_t nsecprof = 0;
 
 //
 // Timer section
 //
 #if defined(__WIN32__)
 //Need timer implementation for windows
+volatile HANDLE hBtThread = 0;
+volatile int running = 0;
+static DWORD WINAPI sprofile_bt( LPVOID lparam ) {
+    TIMECAPS tc;
+    if (MMSYSERR_NOERROR!=timeGetDevCaps(&tc, sizeof(tc))) {
+        fputs("failed to get get timer resulution",stderr);
+        hBtThread = 0;
+        return 0;
+    }
+    while (1) {
+        if (running && bt_size_cur < bt_size_max) {
+            DWORD timeout = nsecprof/1000000;
+            timeout = min(max(timeout,tc.wPeriodMin*2),tc.wPeriodMax/2);
+            Sleep(timeout);
+            if ((DWORD)-1 == SuspendThread(hMainThread)) {
+                fputs("failed to suspend main thread. aborting profiling.",stderr);
+                break;
+            }
+            CONTEXT ctxThread;
+            memset(&ctxThread,0,sizeof(CONTEXT));
+            ctxThread.ContextFlags = CONTEXT_CONTROL | CONTEXT_INTEGER;
+            if (!GetThreadContext(hMainThread, &ctxThread)) {
+                fputs("failed to get context from main thread. aborting profiling.",stderr);
+                break;
+            }
+            // Get backtrace data
+            bt_size_cur += rec_backtrace_ctx((ptrint_t*)bt_data_prof+bt_size_cur, bt_size_max-bt_size_cur-1, &ctxThread);
+            // Mark the end of this block with 0
+            bt_data_prof[bt_size_cur] = 0;
+            bt_size_cur++;
+            if ((DWORD)-1 == ResumeThread(hMainThread)) {
+                fputs("failed to resume main thread! aborting.",stderr);
+                abort();
+            }
+        } else {
+            SuspendThread(GetCurrentThread());
+        }
+    }
+    hBtThread = 0;
+    return 0;
+}
+DLLEXPORT int sprofile_start_timer(void) {
+    running = 1;
+    if (hBtThread == 0) {
+        hBtThread = CreateThread( 
+            NULL,                   // default security attributes
+            0,                      // use default stack size
+            sprofile_bt,            // thread function name
+            0,                      // argument to thread function
+            0,                      // use default creation flags
+            0);                     // returns the thread identifier
+        (void)SetThreadPriority(hBtThread,THREAD_PRIORITY_ABOVE_NORMAL);
+    } else {
+        if ((DWORD)-1 == ResumeThread(hBtThread)) {
+            fputs("failed to resume profiling thread.",stderr);
+            return -2;
+        }
+    }
+    return (hBtThread != NULL ? 0 : -1);
+}
+DLLEXPORT void sprofile_stop_timer(void) {
+    running = 0;
+}
 #else
 #include <signal.h>
 #if defined (__APPLE__) || defined(__FreeBSD___)
 #include <sys/time.h>
 struct itimerval timerprof;
-int running;
+volatile int running;
 
 // The handler function, called whenever the profiling timer elapses
 static void sprofile_bt(int dummy)
 {
     // Get backtrace data
-    bt_size_cur += rec_backtrace(bt_data_prof+bt_size_cur, bt_size_max-bt_size_cur-1);
+    bt_size_cur += rec_backtrace((ptrint_t*)bt_data_prof+bt_size_cur, bt_size_max-bt_size_cur-1);
     // Mark the end of this block with 0
     bt_data_prof[bt_size_cur] = 0;
     bt_size_cur++;
@@ -70,7 +131,7 @@ static void sprofile_bt(int signal, siginfo_t *si, void *uc)
 {
     if (si->si_value.sival_ptr == &timerprof && bt_size_cur < bt_size_max) {
         // Get backtrace data
-       bt_size_cur += rec_backtrace(bt_data_prof+bt_size_cur, bt_size_max-bt_size_cur-1);
+       bt_size_cur += rec_backtrace((ptrint_t*)bt_data_prof+bt_size_cur, bt_size_max-bt_size_cur-1);
         // Mark the end of this block with 0
         bt_data_prof[bt_size_cur] = 0;
         bt_size_cur++;
@@ -129,7 +190,7 @@ DLLEXPORT int sprofile_init(size_t maxsize, u_int64_t delay_nsec)
     bt_size_max = maxsize;
     nsecprof = delay_nsec;
     if (bt_data_prof != NULL)
-        free(bt_data_prof);
+        free((void*)bt_data_prof);
     bt_data_prof = (ptrint_t*) malloc(maxsize*sizeof(ptrint_t));
     if (bt_data_prof == NULL && maxsize > 0)
         return -1;
