ab2412e2270017f308b4ab01c820ecb6542ebebe
Keno Fischer <kfischer@college.harvard.edu>
2013-07-26 19:50:19 -0400

    Fix typo in src/uv_constants.h/add m4 files(uv)

--- src/uv_constants.h
+++ src/uv_constants.h
@@ -3,7 +3,7 @@
 #define YY(uc,lc) (:UV_##uc,UV__##uc),
 const uv_handle_types = [UV_HANDLE_TYPE_MAP(XX) :UV_FILE]
 const uv_req_types = [UV_REQ_TYPE_MAP(XX)]
-const uv_err_vals = [UV_ERRNO_MAP(XX)]
+const uv_err_vals = [UV_ERRNO_MAP(YY)]
 let 
     handles = [:UV_UNKNOWN_HANDLE, uv_handle_types, :UV_HANDLE_TYPE_MAX, :UV_RAW_FD, :UV_RAW_HANDLE]
     reqs = [:UV_UNKNOWN_REQ, uv_req_types, :UV_REQ_TYPE_PRIVATE,:UV_REQ_TYPE_MAX]
