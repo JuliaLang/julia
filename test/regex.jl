be65c96c5a3cb7fd6fa2dc3e09923ccd0f2fbb0a
Daniel Jones <dcjones@cs.washington.edu>
2013-08-09 01:01:36 -0700

    A faster matchall fuction that returns an array of string.

--- test/regex.jl
+++ test/regex.jl
@@ -1,7 +1,11 @@
-matches(args...) = map(m->m.match,matchall(args...))
 
-@test matches(r"a?b?", "asbd") == ["a","","b","",""]
-@test matches(r"\w+", "hello", true) == ["hello","ello","llo","lo","o"]
-@test matches(r".\s", "x \u2200 x \u2203 y") == ["x ", "∀ ", "x ", "∃ "]
-@test matches(r"(\w+)(\s*)", "The dark side of the moon") ==
+@test matchall(r"a?b?", "asbd") == ["a","","b","",""]
+@test matchall(r"a?b?", "asbd", true) == ["a","","b","",""]
+@test matchall(r"\w+", "hello", true) == ["hello","ello","llo","lo","o"]
+@test matchall(r".\s", "x \u2200 x \u2203 y") == ["x ", "∀ ", "x ", "∃ "]
+@test matchall(r"(\w+)(\s*)", "The dark side of the moon") ==
 	  ["The ", "dark ", "side ", "of ", "the ", "moon"]
+
+@test matchall(r"aa", "aaaa", true) == ["aa", "aa", "aa"]
+@test matchall(r"", "aaa") == ["", "", "", ""]
+@test matchall(r"", "aaa", true) == ["", "", "", ""]
