cae16209760d4726f3a1d3428483a236f0c8ef26
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-08-14 18:14:12 -0400

    faster convert(::Type{Float32}, val::Float16) faster Float16 isnan, isinf, == add < and isless for Float16

--- test/float16.jl
+++ test/float16.jl
@@ -21,7 +21,31 @@ g = float16(1.)
 @test_approx_eq sin(f) sin(2f0)
 
 @test isnan(NaN16)
+@test isnan(-NaN16)
+@test !isnan(Inf16)
+@test !isnan(-Inf16)
+@test !isnan(float16(2.6))
 @test NaN16 != NaN16
 @test isequal(NaN16, NaN16)
 @test sprint(showcompact, NaN16) == "NaN16"
+
+@test isinf(Inf16)
+@test isinf(-Inf16)
+@test !isinf(NaN16)
+@test !isinf(-NaN16)
+@test !isinf(float16(2.6))
+@test Inf16 == Inf16
+@test Inf16 != -Inf16
+@test -Inf16 < Inf16
+@test isequal(Inf16, Inf16)
 @test sprint(showcompact, Inf16) == "Inf16"
+
+@test float16(0.0) == float16(0.0)
+@test float16(-0.0) == float16(0.0)
+@test float16(0.0) == float16(-0.0)
+@test float16(-0.0) == float16(-0.0)
+@test float16(2.5) == float16(2.5)
+@test float16(2.5) != float16(2.6)
+@test isequal(float16(0.0), float16(0.0))
+@test !isequal(float16(-0.0), float16(0.0))
+@test !isequal(float16(0.0), float16(-0.0))
