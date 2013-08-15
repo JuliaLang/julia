f5c86e2eddadd7a1d36082e39dd1dfd2937574e1
Keno Fischer <kfischer@college.harvard.edu>
2013-08-15 03:52:37 -0400

    tpl -> &tpl and don't use return value from unreachable

--- src/gc.c
+++ src/gc.c
@@ -10,7 +10,7 @@
 
 // with MEMDEBUG, every object is allocated explicitly with malloc, and
 // filled with 0xbb before being freed.
-#define MEMDEBUG
+//#define MEMDEBUG
 
 // MEMPROFILE prints pool summary statistics after every GC
 //#define MEMPROFILE
