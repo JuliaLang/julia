fb4c88dddef0892a1c654dd644c78b12cac6738f
Stefan Karpinski <stefan@karpinski.org>
2013-08-15 20:47:44 -0400

    Pkg(2): rename runbuildscript => build. Yay, brevity.

--- base/pkg.jl
+++ base/pkg.jl
@@ -241,7 +241,7 @@ resolve(
     fixup(String[pkg for (pkg,_) in filter(x->x[2][2]!=nothing,changes)],avail)
 end
 
-function runbuildscript(pkg,args=[])
+function build(pkg,args=[])
     try 
         path = Dir.path(pkg,"deps","build.jl")
         if isfile(path)
@@ -302,7 +302,7 @@ function _fixup(
     end)
     for p in instlist
         contains(exclude,p) && continue
-        runbuildscript(p,["fixup"]) || return
+        build(p,["fixup"]) || return
     end
 end
 
