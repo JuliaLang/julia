61e0ff19dff34f61155fce2ad56485d96366e07b
Stefan Karpinski <stefan@karpinski.org>
2013-08-18 21:00:11 -0700

    Pkg: more work on scaffolding -- actually create package repo.

--- base/pkg/scaffold.jl
+++ base/pkg/scaffold.jl
@@ -1,25 +1,50 @@
 module Scaffold
 
-using Base.Git, ..Dir
+using Base.Git, ..Dir, ..Read
 
 function scaffold(
     pkg::String;
-    license::Symbol = :__unspecified__,
+    license = nothing,
     years::Union(Int,String) = readchomp(`date +%Y`),
     authors::String = readchomp(`git config --global --get user.name`),
 )
-    if license == :__unspecified__
-        error("""
-        No license chosen -- you must choose a license, e.g.:
+    license === nothing && error("""
+    No license chosen -- you must choose a license, e.g.:
 
-            julia> Pkg.scaffold("$pkg", license=:MIT)
-        """)
+        julia> Pkg.scaffold("$pkg", license="MIT")
+    """)
+    haskey(LICENSES,license) ||
+        error("$license is not a known license choice.")
+    avail = Dir.cd(Read.available)
+    haskey(avail,pkg) &&
+        error("$pkg is already a registered package name.")
+    d = Dir.path(pkg)
+    ispath(d) &&
+        error("$d exists, refusing to overwrite.")
+    mkpath(d)
+    try cd(d) do
+            info("Initializing $pkg repo")
+            Git.run(`init -q`)
+            info("Generating $pkg/LICENSE.md")
+            open("LICENSE.md","w") do f
+                print(f, LICENSES[license](pkg, string(years), authors))
+            end
+            info("Generating $pkg/README.md")
+            open("README.md","w") do f
+                # TODO: add some content?
+            end
+            mkdir("src")
+            open("src/$pkg.jl", "w") do f
+                # TODO: add some content?
+            end
+            info("Initial commit of $pkg")
+            Git.run(`add LICENSE.md README.md src/$pkg.jl`)
+            Git.run(`commit -q -m "$pkg.jl scaffold with $license license."`)
+        end
+    catch
+        run(`rm -rf $d`)
+        rethrow()
     end
-    if !haskey(LICENSES,license)
-        error("$license is not a known license option.")
-    end
-    license_text = LICENSES[license](pkg,string(years),authors)
-    # d = Dir.path(pkg)
 end
 
 mit(pkg::String, years::String, authors::String) = """
@@ -47,6 +72,6 @@ The $pkg.jl package is licensed under the MIT License:
 > WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-const LICENSES = [ :MIT => mit ]
+const LICENSES = [ "MIT" => mit ]
 
 end # module
