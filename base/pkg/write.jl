0e31970836481e31a0cd373aa4a536e0c7c200d9
Stefan Karpinski <stefan@karpinski.org>
2013-08-24 11:37:03 -0400

    Pkg.Write: factor common code for fetching, checking out commits.

--- base/pkg/write.jl
+++ base/pkg/write.jl
@@ -3,35 +3,43 @@ module Write
 using Base.Git, ..Cache, ..Read
 
 function prefetch(pkg::String, sha1::String)
-    isempty(Cache.prefetch(pkg, Read.url(pkg), sha1)) ||
-        error("$pkg: couldn't find commit $(sha1[1:10])")
+    isempty(Cache.prefetch(pkg, Read.url(pkg), sha1)) && return
+    error("$pkg: couldn't find commit $(sha1[1:10])")
 end
 
-begin
-local const pkg_refspec = "refs/heads/*:refs/remotes/cache/*"
-global install, update
-function install(pkg::String, sha1::String)
-    prefetch(pkg, sha1)
-    if !isdir(".trash/$pkg")
-        Git.run(`clone -q $(Cache.path(pkg)) $pkg`)
-    else
-        run(`mv .trash/$pkg ./`)
-        Git.run(`fetch -q $(Cache.path(pkg)) $(pkg_refspec)`, dir=pkg)
-    end
+function fetch(pkg::String, sha1::String)
+    refspec = "refs/heads/*:refs/remotes/cache/*"
+    Git.run(`fetch -q $(Cache.path(pkg)) $refspec`, dir=pkg)
+    Git.iscommit(sha1, dir=pkg) && return
+    f = Git.iscommit(sha1, dir=Cache.path(pkg)) ? "fetch" : "prefetch"
+    error("$pkg: $f failed to get commit $(sha1[1:10]), please file a bug")
+end
+
+function checkout(pkg::String, sha1::String)
     Git.run(`config remote.origin.url $(Read.url(pkg))`, dir=pkg)
     Git.run(`checkout -q $sha1`, dir=pkg)
+end
+
+function install(pkg::String, sha1::String)
+    prefetch(pkg, sha1)
+    if isdir(".trash/$pkg")
+        run(`mv .trash/$pkg ./`)
+    else
+        Git.run(`clone -q $(Cache.path(pkg)) $pkg`)
+    end
+    fetch(pkg, sha1)
+    checkout(pkg, sha1)
 end
 
 function update(pkg::String, sha1::String)
     prefetch(pkg, sha1)
-    Git.run(`fetch -q $(Cache.path(pkg)) $(pkg_refspec)`, dir=pkg)
-    Git.run(`config remote.origin.url $(Read.url(pkg))`, dir=pkg)
-    Git.run(`checkout -q $sha1`, dir=pkg)
-end
+    fetch(pkg, sha1)
+    checkout(pkg, sha1)
 end
 
 function remove(pkg::String)
     isdir(".trash") || mkdir(".trash")
+    # this shouldn't happen in the course of normal operation:
     ispath(".trash/$pkg") && run(`rm -rf .trash/$pkg`)
     run(`mv $pkg .trash/`)
 end
