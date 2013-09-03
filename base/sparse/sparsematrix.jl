967591ed31da56c7ee3dcb4bd23558e576c781d0
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-09-03 18:27:05 -0400

    fix tty_rows in showing sparse matrices ref #4198

--- base/sparse/sparsematrix.jl
+++ base/sparse/sparsematrix.jl
@@ -19,7 +19,7 @@ nnz(S::SparseMatrixCSC) = int(S.colptr[end]-1)
 function show(io::IO, S::SparseMatrixCSC)
     println(io, S.m, "x", S.n, " sparse matrix with ", nnz(S), " ", eltype(S), " nonzeros:")
 
-    half_screen_rows = div(tty_rows() - 8, 2)
+    half_screen_rows = div(Base.tty_rows() - 8, 2)
     pad = ndigits(max(S.m,S.n))
     k = 0
     for col = 1:S.n, k = S.colptr[col] : (S.colptr[col+1]-1)
