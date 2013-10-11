a9d78043e4ddef9d04ec4b3c7c3a1c7ee4d5a031
Stefan Karpinski <stefan@karpinski.org>
2013-10-11 12:17:46 -0400

    Pkg: reconsidered coupling versioning of Julia with metadata formats.

--- base/pkg.jl
+++ base/pkg.jl
@@ -4,7 +4,7 @@ export dir, init, rm, add, available, installed, status, clone, checkout,
        release, fix, update, resolve, register, tag, publish, generate
 
 const DEFAULT_META = "git://github.com/JuliaLang/METADATA.jl"
-const META_BRANCH = "release-0.2"
+const META_BRANCH = "metadata-v2"
 
 for file in split("dir types reqs cache read query resolve write generate entry")
     include("pkg/$file.jl")
