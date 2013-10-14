579a005c995df57cd92f0dc90e6bda2b25158de6
Jutho <jutho.haegeman@ugent.be>
2013-10-14 22:57:35 +0200

    Further changes to eigs
    (1) ARPACK info flag 1 now results in a warning (maximal number of
    iterations taken) rather then throwing an error.
    (2) Added the return value nconv, indicating the number of converged
    eigenvalues, which is relevant when the maximal number of iterations
    has been taken before all requested eigenvalues have converged (which
    no longer leads to an error thanks to (1) ).
    (3) Modified behavior of eigs such that any input type of matrix is
    allowed, as long as the type of A overloads the methods size, issym,
    eltype and can be multiplied with a vector of the same element type.
    (4) Updated test file accordingly

--- base/linalg/arpack.jl
+++ base/linalg/arpack.jl
@@ -49,7 +49,8 @@ function aupd_wrapper(T, linop::Function, n::Integer,
                   iparam, ipntr, workd, workl, lworkl, info)
         end
         if info[1] == 3; warn("Try eigs/svds with a larger value for ncv."); end
-        if info[1] != 0; throw(ARPACKException(info[1])); end
+        if info[1] == 1; warn("Maximum number of iterations taken. Check nconv for number of converged eigenvalues."); end
+        if info[1] < 0; throw(ARPACKException(info[1])); end
         if (ido[1] != -1 && ido[1] != 1); break; end
         workd[ipntr[2]+zernm1] = linop(getindex(workd, ipntr[1]+zernm1))
     end
@@ -77,7 +78,7 @@ function eupd_wrapper(T, n::Integer, sym::Bool, cmplx::Bool, bmat::ASCIIString,
               bmat, n, which, nev, tol, resid, ncv, v, ldv,
               iparam, ipntr, workd, workl, lworkl, rwork, info)
         if info[1] != 0; throw(ARPACKException(info[1])); end
-        return ritzvec ? (d[1:nev], v[1:n, 1:nev],iparam[3],iparam[9],resid) : (d[1:nev],iparam[3],iparam[9],resid)
+        return ritzvec ? (d[1:nev], v[1:n, 1:nev],iparam[5],iparam[3],iparam[9],resid) : (d[1:nev],iparam[5],iparam[3],iparam[9],resid)
 
     elseif sym
 
@@ -87,7 +88,7 @@ function eupd_wrapper(T, n::Integer, sym::Bool, cmplx::Bool, bmat::ASCIIString,
               bmat, n, which, nev, tol, resid, ncv, v, ldv,
               iparam, ipntr, workd, workl, lworkl, info) 
         if info[1] != 0; throw(ARPACKException(info[1])); end
-        return ritzvec ? (d, v[1:n, 1:nev],iparam[3],iparam[9],resid) : (d,iparam[3],iparam[9],resid)
+        return ritzvec ? (d, v[1:n, 1:nev],iparam[5],iparam[3],iparam[9],resid) : (d,iparam[5],iparam[3],iparam[9],resid)
 
     else
 
@@ -113,7 +114,7 @@ function eupd_wrapper(T, n::Integer, sym::Bool, cmplx::Bool, bmat::ASCIIString,
             j += 1
         end
         d = complex(dr[1:nev],di[1:nev])
-        return ritzvec ? (d, evec[1:n, 1:nev],iparam[3],iparam[9],resid) : (d,iparam[3],iparam[9],resid)
+        return ritzvec ? (d, evec[1:n, 1:nev],iparam[5],iparam[3],iparam[9],resid) : (d,iparam[5],iparam[3],iparam[9],resid)
     end
     
 end
