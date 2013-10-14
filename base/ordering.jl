25eb81e955a32cebb81eaa7e78e9651de6b561fa
Stefan Karpinski <stefan@karpinski.org>
2013-10-14 17:46:08 -0400

    sorting bounds checks: wrapping a function definition does nothing.
    Sortperm on Float64 arrays is now 37% faster than it used to be since
    I've actually removed the bounds checks in nans2left and nans2right.
    I tried various ways to wrap the lt(::Perm,a,b) call in `@inbounds`
    but all approaches caused significant slow downs.

--- base/ordering.jl
+++ base/ordering.jl
@@ -43,7 +43,7 @@ lt(o::ForwardOrdering, a, b) = isless(a,b)
 lt(o::ReverseOrdering, a, b) = lt(o.fwd,b,a)
 lt(o::By,              a, b) = isless(o.by(a),o.by(b))
 lt(o::Lt,              a, b) = o.lt(a,b)
-@inbounds lt(p::Perm,  a, b) = lt(p.order, p.data[a], p.data[b])
+lt(p::Perm,            a, b) = lt(p.order, p.data[a], p.data[b])
 
 # Map a bits-type to an unsigned int, maintaining sort order
 uint_mapping(::ForwardOrdering, x::Unsigned) = x
@@ -66,9 +66,9 @@ uint_mapping(::ReverseOrdering{ForwardOrdering}, x::Int128)   = ~uint128(x $ typ
 uint_mapping(::ReverseOrdering{ForwardOrdering}, x::Float32)  = (y = reinterpret(Int32, x); uint32(y < 0 ? y : ~(y $ typemin(Int32))))
 uint_mapping(::ReverseOrdering{ForwardOrdering}, x::Float64)  = (y = reinterpret(Int64, x); uint64(y < 0 ? y : ~(y $ typemin(Int64))))
 
-uint_mapping(o::By, x) = uint_mapping(Forward, o.by(x))
-uint_mapping(o::Lt, x) = error("uint_mapping does not work with general Lt Orderings")
-@inbounds uint_mapping(o::Perm, i::Int) = uint_mapping(o.order, o.data[i])
+uint_mapping(o::By,   x     ) = uint_mapping(Forward, o.by(x))
+uint_mapping(o::Perm, i::Int) = uint_mapping(o.order, o.data[i])
+uint_mapping(o::Lt,   x     ) = error("uint_mapping does not work with general Lt Orderings")
 
 ordtype(o::ReverseOrdering, vs::AbstractArray) = ordtype(o.fwd, vs)
 ordtype(o::Perm,            vs::AbstractArray) = ordtype(o.order, o.data)
