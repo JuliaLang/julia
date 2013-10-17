JuliaLang/julia

commit 4801de69755a9f5586ac5c0538e3122368d490f7

fix #4538

src/julia-syntax.scm
@@ -26,7 +26,7 @@
 	 (error (string "malformed function arguments " v)))
 	(else
 	 (case (car v)
-	   ((...)         (decl-var (cadr v)))
+	   ((... kw)      (decl-var (cadr v)))
 	   ((|::|)        (decl-var v))
 	   (else (error (string "malformed function argument " v)))))))
 
@@ -2968,7 +2968,7 @@ So far only the second case can actually occur.
   (if (symbol? e) e
       (cadr e)))
 
-(define (resolve-expansion-vars-with-new-env x env m)
+(define (resolve-expansion-vars-with-new-env x env m inkw)
   (resolve-expansion-vars-
    x
    (append!
@@ -2976,9 +2976,9 @@ So far only the second case can actually occur.
 	      (not (assq (car x) env)))
 	    (pair-with-gensyms (vars-introduced-by x)))
     env)
-   m))
+   m inkw))
 
-(define (resolve-expansion-vars- e env m)
+(define (resolve-expansion-vars- e env m inkw)
   (cond ((or (eq? e 'true) (eq? e 'false) (eq? e 'end))
 	 e)
 	((symbol? e)
@@ -2993,36 +2993,47 @@ So far only the second case can actually occur.
 	   ((escape) (cadr e))
 	   ((macrocall)
 	    `(macrocall ,.(map (lambda (x)
-				 (resolve-expansion-vars- x env m))
+				 (resolve-expansion-vars- x env m inkw))
 			       (cdr e))))
 	   ((type)
-	    `(type ,(cadr e) ,(resolve-expansion-vars- (caddr e) env m)
+	    `(type ,(cadr e) ,(resolve-expansion-vars- (caddr e) env m inkw)
 		   ;; type has special behavior: identifiers inside are
 		   ;; field names, not expressions.
 		   ,(map (lambda (x)
 			   (cond ((atom? x) x)
 				 ((and (pair? x) (eq? (car x) '|::|))
 				  `(|::| ,(cadr x)
-				    ,(resolve-expansion-vars- (caddr x) env m)))
+				    ,(resolve-expansion-vars- (caddr x) env m inkw)))
 				 (else
-				  (resolve-expansion-vars-with-new-env x env m))))
+				  (resolve-expansion-vars-with-new-env x env m inkw))))
 			 (cadddr e))))
+
+	   ((parameters)
+	    (cons 'parameters
+		  (map (lambda (x)
+			 ;; in keyword arg A=B, don't transform "A"
+			 (resolve-expansion-vars- x env m #t))
+		       (cdr e))))
+
 	   ((kw)
-	    ;; in keyword arg A=B, don't transform "A"
 	    (if (and (pair? (cadr e))
 		     (eq? (caadr e) '|::|))
 		`(kw (|::|
-		      ,(cadr (cadr e))
-		      ,(resolve-expansion-vars- (caddr (cadr e)) env m))
-		     ,(resolve-expansion-vars- (caddr e) env m))
-		`(kw ,(cadr e)
-		     ,(resolve-expansion-vars- (caddr e) env m))))
+		      ,(if inkw
+			   (cadr (cadr e))
+			   (resolve-expansion-vars- (cadr (cadr e)) env m inkw))
+		      ,(resolve-expansion-vars- (caddr (cadr e)) env m inkw))
+		     ,(resolve-expansion-vars- (caddr e) env m inkw))
+		`(kw ,(if inkw
+			  (cadr e)
+			  (resolve-expansion-vars- (cadr e) env m inkw))
+		     ,(resolve-expansion-vars- (caddr e) env m inkw))))
 
 	   ;; todo: trycatch
 	   (else
 	    (cons (car e)
 		  (map (lambda (x)
-			 (resolve-expansion-vars-with-new-env x env m))
+			 (resolve-expansion-vars-with-new-env x env m inkw))
 		       (cdr e))))))))
 
 ;; decl-var that also identifies f in f()=...
@@ -3079,7 +3090,7 @@ So far only the second case can actually occur.
   ;; expand binding form patterns
   ;; keep track of environment, rename locals to gensyms
   ;; and wrap globals in (getfield module var) for macro's home module
-  (resolve-expansion-vars- e (env-for-expansion e) m))
+  (resolve-expansion-vars- e (env-for-expansion e) m #f))
 
 ;; expander entry point
 

test/keywordargs.jl
@@ -102,3 +102,27 @@ kwf7{T}(x::T; k::T=1) = T
 # try to confuse it with quoted symbol
 kwf8{T}(x::MIME{:T};k::T=0) = 0
 @test kwf8(MIME{:T}()) === 0
+
+# issue #4538
+macro TEST4538()
+    quote
+        function $(esc(:test4538))(x=1)
+            return x
+        end
+    end
+end
+@TEST4538
+@test test4538() == 1
+@test test4538(2) == 2
+
+macro TEST4538_2()
+    quote
+        function $(esc(:test4538_2))(;x=1)
+            return x
+        end
+    end
+end
+@TEST4538_2
+@test test4538_2() == 1
+@test_throws test4538_2(2)
+@test test4538_2(x=2) == 2

commit 1888cd2706bfa17257e66f7a9f7df7c62048fd8f

fix a further bug in keyword args in macros caused by 4801de69755a9f5586ac5c0538e3122368d490f7

src/julia-syntax.scm
@@ -2968,7 +2968,7 @@ So far only the second case can actually occur.
   (if (symbol? e) e
       (cadr e)))
 
-(define (resolve-expansion-vars-with-new-env x env m inkw)
+(define (resolve-expansion-vars-with-new-env x env m inarg)
   (resolve-expansion-vars-
    x
    (append!
@@ -2976,9 +2976,9 @@ So far only the second case can actually occur.
 	      (not (assq (car x) env)))
 	    (pair-with-gensyms (vars-introduced-by x)))
     env)
-   m inkw))
+   m inarg))
 
-(define (resolve-expansion-vars- e env m inkw)
+(define (resolve-expansion-vars- e env m inarg)
   (cond ((or (eq? e 'true) (eq? e 'false) (eq? e 'end))
 	 e)
 	((symbol? e)
@@ -2993,47 +2993,59 @@ So far only the second case can actually occur.
 	   ((escape) (cadr e))
 	   ((macrocall)
 	    `(macrocall ,.(map (lambda (x)
-				 (resolve-expansion-vars- x env m inkw))
+				 (resolve-expansion-vars- x env m inarg))
 			       (cdr e))))
 	   ((type)
-	    `(type ,(cadr e) ,(resolve-expansion-vars- (caddr e) env m inkw)
+	    `(type ,(cadr e) ,(resolve-expansion-vars- (caddr e) env m inarg)
 		   ;; type has special behavior: identifiers inside are
 		   ;; field names, not expressions.
 		   ,(map (lambda (x)
 			   (cond ((atom? x) x)
 				 ((and (pair? x) (eq? (car x) '|::|))
 				  `(|::| ,(cadr x)
-				    ,(resolve-expansion-vars- (caddr x) env m inkw)))
+				    ,(resolve-expansion-vars- (caddr x) env m inarg)))
 				 (else
-				  (resolve-expansion-vars-with-new-env x env m inkw))))
+				  (resolve-expansion-vars-with-new-env x env m inarg))))
 			 (cadddr e))))
 
 	   ((parameters)
 	    (cons 'parameters
 		  (map (lambda (x)
-			 ;; in keyword arg A=B, don't transform "A"
-			 (resolve-expansion-vars- x env m #t))
+			 (resolve-expansion-vars- x env m #f))
 		       (cdr e))))
+
+	   ((= function)
+	    (if (and (pair? (cadr e)) (eq? (caadr e) 'call))
+		;; in (kw x 1) inside an arglist, the x isn't actually a kwarg
+		`(,(car e) (call ,(resolve-expansion-vars- (cadadr e) env m inarg)
+				 ,@(map (lambda (x)
+					  (resolve-expansion-vars- x env m #t))
+					(cddr (cadr e))))
+		  ,(resolve-expansion-vars- (caddr e) env m inarg))
+		`(,(car e) ,@(map (lambda (x)
+				    (resolve-expansion-vars- x env m inarg))
+				  (cdr e)))))
 
 	   ((kw)
 	    (if (and (pair? (cadr e))
 		     (eq? (caadr e) '|::|))
 		`(kw (|::|
-		      ,(if inkw
-			   (cadr (cadr e))
-			   (resolve-expansion-vars- (cadr (cadr e)) env m inkw))
-		      ,(resolve-expansion-vars- (caddr (cadr e)) env m inkw))
-		     ,(resolve-expansion-vars- (caddr e) env m inkw))
-		`(kw ,(if inkw
-			  (cadr e)
-			  (resolve-expansion-vars- (cadr e) env m inkw))
-		     ,(resolve-expansion-vars- (caddr e) env m inkw))))
+		      ,(if inarg
+			   (resolve-expansion-vars- (cadr (cadr e)) env m inarg)
+			   ;; in keyword arg A=B, don't transform "A"
+			   (cadr (cadr e)))
+		      ,(resolve-expansion-vars- (caddr (cadr e)) env m inarg))
+		     ,(resolve-expansion-vars- (caddr e) env m inarg))
+		`(kw ,(if inarg
+			  (resolve-expansion-vars- (cadr e) env m inarg)
+			  (cadr e))
+		     ,(resolve-expansion-vars- (caddr e) env m inarg))))
 
 	   ;; todo: trycatch
 	   (else
 	    (cons (car e)
 		  (map (lambda (x)
-			 (resolve-expansion-vars-with-new-env x env m inkw))
+			 (resolve-expansion-vars-with-new-env x env m inarg))
 		       (cdr e))))))))
 
 ;; decl-var that also identifies f in f()=...

test/keywordargs.jl
@@ -126,3 +126,12 @@ end
 @test test4538_2() == 1
 @test_throws test4538_2(2)
 @test test4538_2(x=2) == 2
+
+f4538_3(;x=1) = x
+macro TEST4538_3()
+    quote
+        x = 2
+        f4538_3(x=3)
+    end
+end
+@test (@TEST4538_3) == 3