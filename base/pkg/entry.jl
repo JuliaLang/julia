69f930e6c01d6ff049dd100cf12ca0ec7fd4aa41
Carlo Baldassi <carlobaldassi@gmail.com>
2013-10-21 00:40:44 +0200

    Pkg: improve "incompatible with fixed" analysis
    Addresses #4049
    
    Keeps track of what fixed packages determined the removal
    of some package; uses that information in case one of the
    required packages was removed in this way.
    
    Also, recursively remove packages from the available list,
    i.e. when a fixed package determines the removal of an available
    package because its requirements can't be satisfied, then also all
    other packages whose requirements can't be satisfied because of this
    removal are themselves removed, and so on. (In case of the error
    mentioned above, the blame still goes to the fixed packages.)

--- base/pkg/entry.jl
+++ base/pkg/entry.jl
@@ -282,15 +282,14 @@ function resolve(
     have  :: Dict = Read.free(instd),
 )
     reqs = Query.requirements(reqs,fixed,avail)
-    deps = Query.dependencies(avail,fixed)
+    deps, conflicts = Query.dependencies(avail,fixed)
 
-    incompatible = {}
     for pkg in keys(reqs)
-        haskey(deps,pkg) || push!(incompatible,pkg)
+        if !haskey(deps,pkg)
+            error("$pkg's requirements can't be satisfied because of the following fixed packages: ",
+                   join(conflicts[pkg], ", ", " and "))
+        end
     end
-    isempty(incompatible) ||
-        error("The following packages are incompatible with fixed requirements: ",
-              join(incompatible, ", ", " and "))
 
     deps = Query.prune_dependencies(reqs,deps)
     want = Resolve.resolve(reqs,deps)
