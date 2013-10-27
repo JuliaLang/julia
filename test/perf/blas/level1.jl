ea37433539bbcb11a27e8a85d0488385fd1ace8b
Jiahao Chen (陈家豪) <jiahao@mit.edu>
2013-10-27 18:39:19 -0400

    Consolidate blas, cat and lapack test drivers

--- test/perf/blas/level1.jl
+++ test/perf/blas/level1.jl
@@ -18,14 +18,7 @@ function axpytest(n, iter)
     y
 end
 
-for (testfunc, testname, longtestname) in [(dottest, "dot", "dot product"),
-                                           (axpytest, "axpy", "axpy")]
-    for (n, t, size) in [(2, 10^6, "tiny"),
-                         (2^4, 10^6, "small"),
-                         (2^6, 10^6, "medium"),
-                         (2^8, 10^5, "large"),
-                         (2^10, 10^5, "huge")]
-        @timeit apply(testfunc, n, t) string(testname, "_", size) string(uppercase(size[1]), size[2:end], " ", longtestname, " test")
-    end
-end
+problemsizes = [(2, 10^6, "tiny"), (2^4, 10^6, "small"), (2^6, 10^6, "medium"), (2^8, 10^5, "large"), (2^10, 10^5, "huge")]
+testdata = [(dottest, "dot", "dot product", problemsizes), (axpytest, "axpy", "axpy", problemsizes)]
+include("../perfgeneric.jl")
 
