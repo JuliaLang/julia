f32c37a018f58537047ee931c12c759a043d39af
Yuriy Skalko <yuriy.skalko@gmail.com>
2013-11-10 16:45:53 +0200

    Make SubArray logical indexing handle low-dimensional cases

--- test/arrayops.jl
+++ test/arrayops.jl
@@ -125,6 +125,9 @@ sA = sub(A, 1:2:3, 1:3:5, 1:2:8)
 # sub logical indexing #4763
 A = sub([1:10], 5:8)
 @test A[A.<7] == [5, 6]
+B = reshape(1:16, 4, 4)
+sB = sub(B, 2:3, 2:3)
+@test sB[sB.>8] == [10, 11]
 
 # slice
 A = reshape(1:120, 3, 5, 8)
