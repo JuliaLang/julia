2baa93126003675199961587e3003501f1f7554a
Steven G. Johnson <stevenj@mit.edu>
2013-11-14 13:13:27 -0500

    make gcd and lcm positive (fix #4810)

--- base/rational.jl
+++ base/rational.jl
@@ -6,7 +6,7 @@ immutable Rational{T<:Integer} <: Real
         if num == 0 && den == 0
             error("invalid rational: 0//0")
         end
-        g = gcd(den, num)
+        g = den < 0 ? -gcd(den,num) : gcd(den, num)
         new(div(num, g), div(den, g))
     end
 end
