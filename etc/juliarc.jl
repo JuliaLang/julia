fc4f44ac5f31a919065d45ca0997395f57f6f7a5
Yuriy Skalko <yuriy.skalko@gmail.com>
2013-11-18 20:58:03 +0200

    Update juliarc.jl comment

--- etc/juliarc.jl
+++ etc/juliarc.jl
@@ -1,3 +1,3 @@
 # This file should contain site-specific commands to be executed on Julia startup
-# Users should store their own personal commands in home(), in a file named .juliarc.jl
+# Users should store their own personal commands in homedir(), in a file named .juliarc.jl
 
