4ce45e69237e5cd0d8676aef8150375f6f984f2c
Jeff Bezanson <bezanson@post.harvard.edu>
2013-11-24 02:59:09 -0500

    don't use parseint for uint128 constants in random.jl. fixes #4910

--- base/random.jl
+++ base/random.jl
@@ -222,8 +222,8 @@ end
 
 @eval function uuid4()
     u = rand(Uint128)
-    u &= $(parseint(Uint128,"ffffffffffff0fff3fffffffffffffff",16))
-    u |= $(parseint(Uint128,"00000000000040008000000000000000",16))
+    u &= $(uint128(0xffffffffffff0fff)<<64 | uint128(0x3fffffffffffffff))
+    u |= $(uint128(0x0000000000004000)<<64 | uint128(0x8000000000000000))
     UUID(u)
 end
 
