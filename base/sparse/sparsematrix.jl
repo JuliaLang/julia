98d7bce73e5bd30b43f6ebc300302d4487b99d12
Viral B. Shah <viral@mayin.org>
2013-11-28 09:50:35 +0530

    Fix sparse matrix printing.

--- base/sparse/sparsematrix.jl
+++ base/sparse/sparsematrix.jl
@@ -17,18 +17,17 @@ size(S::SparseMatrixCSC) = (S.m, S.n)
 nnz(S::SparseMatrixCSC) = int(S.colptr[end]-1)
 
 function show(io::IO, S::SparseMatrixCSC)
-    print(io, S.m, "x", S.n, " sparse matrix with ", nnz(S), " ", eltype(S), " nonzeros:")
+    println(io, S.m, "x", S.n, " sparse matrix with ", nnz(S), " ", eltype(S), " nonzeros:")
 
     half_screen_rows = div(Base.tty_rows() - 8, 2)
     pad = ndigits(max(S.m,S.n))
     k = 0
     for col = 1:S.n, k = S.colptr[col] : (S.colptr[col+1]-1)
-        print("\n\t")
         if k < half_screen_rows || k > nnz(S)-half_screen_rows
-            print(io, "[", rpad(S.rowval[k], pad), ", ", lpad(col, pad), "]  =  ",
+            println(io, "[", rpad(S.rowval[k], pad), ", ", lpad(col, pad), "]  =  ",
                     sprint(showcompact, S.nzval[k]))
         elseif k == half_screen_rows
-            print(io, "\u22ee")
+            println(io, "\u22ee")
         end
         k += 1
     end
