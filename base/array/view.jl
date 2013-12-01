ae0ae6e180969eb6eb156ecc45b95d734cb703e7
Stefan Karpinski <stefan@karpinski.org>
2013-12-01 11:11:36 -0500

    base/array/view.jl: some very basic array view sketching.
    - only supports Vector{T} as the data array
    - multidimensional indexing via an arbitrary linear transform
    - linear indexing compatible with the multidimensional indexing

---
+++ base/array/view.jl
@@ -0,0 +1,41 @@
+importall Base
+
+isdefined(:ArrayView) || immutable ArrayView{T,n} <: AbstractArray
+    data::Vector{T}
+    offset::Int
+    strides::NTuple{n,Int}
+    size::NTuple{n,Int}
+end
+
+size(v::ArrayView) = v.size
+size(v::ArrayView, i::Int) = v.size[i]
+ndims{T,n}(v::ArrayView{T,n}) = n
+show(io::IO, v::ArrayView) = invoke(show,(IO,Any),io,v)
+
+function view2data{T,n}(v::ArrayView{T,n}, I::NTuple{n,Int})
+    d = v.offset
+    t = v.strides
+    # z = v.size
+    @inbounds for k = 1:n
+        j = I[k]
+        # 1 <= j <= z[k] || throw(BoundsError())
+        d += t[k]*(j-1)
+    end
+    return d+1
+end
+
+function view2data{T,n}(v::ArrayView{T,n}, i::Int)
+    i -= 1
+    z = v.size
+    I = ntuple(n) do k
+        j = i % z[k]
+        i = div(i,z[k])
+        return j+1
+    end
+    view2data(v,I)
+end
+
+getindex(v::ArrayView, i::Int) = @inbounds return v.data[view2data(v,i)]
+getindex(v::ArrayView, I::Int...) = @inbounds return v.data[view2data(v,I)]
+setindex!(v::ArrayView, x, i::Int) = (@inbounds v.data[view2data(v,i)] = x)
+setindex!(v::ArrayView, x, I::Int...) = (@inbounds v.data[view2data(v,I)] = x)
