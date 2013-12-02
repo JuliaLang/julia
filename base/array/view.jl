0a42dd34b32f693489fbe96ff00de1fdd799961a
Stefan Karpinski <stefan@karpinski.org>
2013-12-01 19:02:45 -0500

    base/array/view.jl: tweaking for beauty.

--- base/array/view.jl
+++ base/array/view.jl
@@ -14,14 +14,14 @@ show(io::IO, v::ArrayView) = invoke(show,(IO,Any),io,v)
 
 function view2data{T,n}(v::ArrayView{T,n}, I::NTuple{n,Int})
     d = v.offset
-    t = v.strides
     # z = v.size
+    t = v.strides
     @inbounds for k = 1:n
         j = I[k]
         # 1 <= j <= z[k] || throw(BoundsError())
-        d += t[k]*(j-1)
+        d += (j-1)*t[k]
     end
-    return d+1
+    d + 1
 end
 
 function view2data{T,n}(v::ArrayView{T,n}, i::Int)
@@ -30,10 +30,10 @@ function view2data{T,n}(v::ArrayView{T,n}, i::Int)
     z = v.size
     t = v.strides
     @inbounds for k = 1:n
-        d += t[k]*rem(i,z[k])
-        i = div(i,z[k])
+        d += rem(i,z[k])*t[k]
+        i =  div(i,z[k])
     end
-    return d+1
+    d + 1
 end
 
 getindex(v::ArrayView, i::Int) = @inbounds return v.data[view2data(v,i)]
