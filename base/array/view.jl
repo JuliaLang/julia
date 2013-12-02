68c241f41151f0902360cf70cbe7dc6866006b03
Stefan Karpinski <stefan@karpinski.org>
2013-12-02 03:27:02 +0000

    base/array/view.jl: factor out the really basic index computation.
    This version lets you see how tight the code gen can be without the
    gratuitous (now-removed) checks from the div intrinsic and the undef
    checks from loading the fields from the ArrayView structure:
    
    	julia> code_native(view2data,(Int,(Int,Int),(Int,Int)))
    		.section	__TEXT,__text,regular,pure_instructions
    	Filename: /Users/stefan/projects/julia/base/array/view.jl
    	Source line: 17
    		push	RBP
    		mov	RBP, RSP
    	Source line: 17
    		mov	R8, QWORD PTR [RSI + 16]
    		mov	RSI, QWORD PTR [RSI + 24]
    		mov	RAX, QWORD PTR [RDX + 16]
    		mov	RCX, QWORD PTR [RDX + 24]
    		mov	RCX, QWORD PTR [RCX + 8]
    		dec	RCX
    		imul	RCX, QWORD PTR [RSI + 8]
    		mov	RAX, QWORD PTR [RAX + 8]
    		dec	RAX
    		imul	RAX, QWORD PTR [R8 + 8]
    		add	RAX, RDI
    	Source line: 19
    		lea	RAX, QWORD PTR [RCX + RAX + 1]
    		pop	RBP
    		ret
    
    	julia> code_native(view2data,(Int,(Int,Int),(Int,Int),Int))
    		.section	__TEXT,__text,regular,pure_instructions
    	Filename: /Users/stefan/projects/julia/base/array/view.jl
    	Source line: 23
    		push	RBP
    		mov	RBP, RSP
    		mov	R8, RDX
    	Source line: 23
    		lea	RAX, QWORD PTR [RCX - 1]
    	Source line: 25
    		mov	RCX, QWORD PTR [RSI + 16]
    		mov	R9, QWORD PTR [RSI + 24]
    		cqo
    		idiv	QWORD PTR [RCX + 8]
    		mov	RCX, RDX
    		mov	RDX, QWORD PTR [R8 + 16]
    		mov	RSI, QWORD PTR [R8 + 24]
    		imul	RCX, QWORD PTR [RDX + 8]
    		add	RCX, RDI
    		cqo
    		idiv	QWORD PTR [R9 + 8]
    		imul	RDX, QWORD PTR [RSI + 8]
    	Source line: 28
    		lea	RAX, QWORD PTR [RDX + RCX + 1]
    		pop	RBP
    		ret

--- base/array/view.jl
+++ base/array/view.jl
@@ -12,29 +12,24 @@ size(v::ArrayView, i::Int) = v.size[i]
 ndims{T,n}(v::ArrayView{T,n}) = n
 show(io::IO, v::ArrayView) = invoke(show,(IO,Any),io,v)
 
-function view2data{T,n}(v::ArrayView{T,n}, I::NTuple{n,Int})
-    d = v.offset
-    # z = v.size
-    t = v.strides
+function view2data{n}(d::Int, strides::NTuple{n,Int}, I::NTuple{n,Int})
     @inbounds for k = 1:n
-        j = I[k]
-        # 1 <= j <= z[k] || throw(BoundsError())
-        d += (j-1)*t[k]
+        d += (I[k]-1)*strides[k]
     end
     d + 1
 end
 
-function view2data{T,n}(v::ArrayView{T,n}, i::Int)
+function view2data{n}(d::Int, strides::NTuple{n,Int}, size::NTuple{n,Int}, i::Int)
     i -= 1
-    d = v.offset
-    z = v.size
-    t = v.strides
     @inbounds for k = 1:n
-        d += rem(i,z[k])*t[k]
-        i =  div(i,z[k])
+        d += rem(i,strides[k])*size[k]
+        i =  div(i,strides[k])
     end
     d + 1
 end
+
+view2data{T,n}(v::ArrayView{T,n}, I::NTuple{n,Int}) = view2data(v.offset, v.strides, I)
+view2data{T,n}(v::ArrayView{T,n}, i::Int) = view2data(v.offset, v.strides, v.size, i)
 
 getindex(v::ArrayView, i::Int) = @inbounds return v.data[view2data(v,i)]
 getindex(v::ArrayView, I::Int...) = @inbounds return v.data[view2data(v,I)]
