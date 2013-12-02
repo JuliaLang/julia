a335ce18298ac1b93bde57000f98d0615d56e522
Stefan Karpinski <stefan@karpinski.org>
2013-12-02 05:53:02 +0000

    base/array/view.jl: view construction with ranges a la slicing.
    Also support views into multidimensional arrays. This wasn't all that
    annoying, actually; I realized I could just add an `m` parameter.

--- base/array/view.jl
+++ base/array/view.jl
@@ -1,37 +1,50 @@
 importall Base
 
-isdefined(:ArrayView) || immutable ArrayView{T,n} <: AbstractArray
-    data::Vector{T}
-    offset::Int
-    strides::NTuple{n,Int}
+isdefined(:ArrayView) || immutable ArrayView{T,n,m} <: AbstractArray
+    array::Array{T,m}
     size::NTuple{n,Int}
+    strides::NTuple{n,Int}
+    offset::Int
 end
 
 size(v::ArrayView) = v.size
 size(v::ArrayView, i::Int) = v.size[i]
 ndims{T,n}(v::ArrayView{T,n}) = n
+eltype(v::ArrayView) = eltype(v.array)
 show(io::IO, v::ArrayView) = invoke(show,(IO,Any),io,v)
 
-function view2data{n}(d::Int, strides::NTuple{n,Int}, I::NTuple{n,Int})
+function v2a{n}(strides::NTuple{n,Int}, o::Int, I::NTuple{n,Int})
     @inbounds for k = 1:n
-        d += (I[k]-1)*strides[k]
+        o += (I[k]-1)*strides[k]
     end
-    d + 1
+    o + 1
 end
 
-function view2data{n}(d::Int, strides::NTuple{n,Int}, size::NTuple{n,Int}, i::Int)
+function v2a{n}(size::NTuple{n,Int}, strides::NTuple{n,Int}, o::Int, i::Int)
     i -= 1
     @inbounds for k = 1:n
-        d += rem(i,strides[k])*size[k]
-        i =  div(i,strides[k])
+        o += rem(i,size[k])*strides[k]
+        i =  div(i,size[k])
     end
-    d + 1
+    o + 1
 end
 
-view2data{T,n}(v::ArrayView{T,n}, I::NTuple{n,Int}) = view2data(v.offset, v.strides, I)
-view2data{T,n}(v::ArrayView{T,n}, i::Int) = view2data(v.offset, v.strides, v.size, i)
+v2a{T,n}(v::ArrayView{T,n}, I::NTuple{n,Int}) = v2a(v.strides, v.offset, I)
+v2a{T,n}(v::ArrayView{T,n}, i::Int) = v2a(v.size, v.strides, v.offset, i)
 
-getindex(v::ArrayView, i::Int) = @inbounds return v.data[view2data(v,i)]
-getindex(v::ArrayView, I::Int...) = @inbounds return v.data[view2data(v,I)]
-setindex!(v::ArrayView, x, i::Int) = (@inbounds v.data[view2data(v,i)] = x)
-setindex!(v::ArrayView, x, I::Int...) = (@inbounds v.data[view2data(v,I)] = x)
+getindex(v::ArrayView, i::Int) = @inbounds return v.array[v2a(v,i)]
+getindex(v::ArrayView, I::Int...) = @inbounds return v.array[v2a(v,I)]
+setindex!(v::ArrayView, x, i::Int) = (@inbounds v.array[v2a(v,i)] = x)
+setindex!(v::ArrayView, x, I::Int...) = (@inbounds v.array[v2a(v,I)] = x)
+
+function ArrayView{T,n,m}(a::Array{T,m}, R::NTuple{n,Ranges})
+    sz = ntuple(n,k->length(R[k]))
+    offset, prod = 0, 1
+    strides = ntuple(n) do k
+        offset += prod*(first(R[k])-1)
+        stride = prod*step(R[k])
+        prod *= size(a,k)
+        return stride
+    end
+    ArrayView(a,sz,strides,offset)
+end
