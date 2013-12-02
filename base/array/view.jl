7806513302f7fa4604588327350faca9caba7255
Stefan Karpinski <stefan@karpinski.org>
2013-12-02 06:06:32 +0000

    base/array/view.jl: use 1-based origin instead of 1-based offset.

--- base/array/view.jl
+++ base/array/view.jl
@@ -4,7 +4,7 @@ isdefined(:ArrayView) || immutable ArrayView{T,n,m} <: AbstractArray
     array::Array{T,m}
     size::NTuple{n,Int}
     strides::NTuple{n,Int}
-    offset::Int
+    origin::Int
 end
 
 size(v::ArrayView) = v.size
@@ -17,7 +17,7 @@ function v2a{n}(strides::NTuple{n,Int}, o::Int, I::NTuple{n,Int})
     @inbounds for k = 1:n
         o += (I[k]-1)*strides[k]
     end
-    o + 1
+    return o
 end
 
 function v2a{n}(size::NTuple{n,Int}, strides::NTuple{n,Int}, o::Int, i::Int)
@@ -26,11 +26,11 @@ function v2a{n}(size::NTuple{n,Int}, strides::NTuple{n,Int}, o::Int, i::Int)
         o += rem(i,size[k])*strides[k]
         i =  div(i,size[k])
     end
-    o + 1
+    return o
 end
 
-v2a{T,n}(v::ArrayView{T,n}, I::NTuple{n,Int}) = v2a(v.strides, v.offset, I)
-v2a{T,n}(v::ArrayView{T,n}, i::Int) = v2a(v.size, v.strides, v.offset, i)
+v2a{T,n}(v::ArrayView{T,n}, I::NTuple{n,Int}) = v2a(v.strides, v.origin, I)
+v2a{T,n}(v::ArrayView{T,n}, i::Int) = v2a(v.size, v.strides, v.origin, i)
 
 getindex(v::ArrayView, i::Int) = @inbounds return v.array[v2a(v,i)]
 getindex(v::ArrayView, I::Int...) = @inbounds return v.array[v2a(v,I)]
@@ -39,12 +39,12 @@ setindex!(v::ArrayView, x, I::Int...) = (@inbounds v.array[v2a(v,I)] = x)
 
 function ArrayView{T,n,m}(a::Array{T,m}, R::NTuple{n,Ranges})
     sz = ntuple(n,k->length(R[k]))
-    offset, prod = 0, 1
+    origin = prod = 1
     strides = ntuple(n) do k
-        offset += prod*(first(R[k])-1)
+        origin += prod*(first(R[k])-1)
         stride = prod*step(R[k])
         prod *= size(a,k)
         return stride
     end
-    ArrayView(a,sz,strides,offset)
+    ArrayView(a,sz,strides,origin)
 end
