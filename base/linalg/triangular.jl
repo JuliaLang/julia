0356cf3c1e5272b91ec7a4ef682038d66df8d1de
Jiahao Chen <jiahao@mit.edu>
2013-12-28 14:47:42 -0500

    Generic linear solver for upper triangular matrices
    - Generic inverse replaced with generic solve on identity matrix
    - Specialized transpose, ctranspose, istril, istriu

--- base/linalg/triangular.jl
+++ base/linalg/triangular.jl
@@ -15,13 +15,9 @@ function Triangular(A::Matrix)
     throw(ArgumentError("matrix is not triangular"))
 end
 
-size(A::Triangular, args...) = size(A.UL, args...)
-full(A::Triangular) = (istril(A) ? tril! : triu!)(A.UL)
-
-getindex{T}(A::Triangular{T}, i::Integer, j::Integer) = i == j ? A.UL[i,j] : ((A.uplo == 'U') == (i < j) ? getindex(A.UL, i, j) : zero(T))
-
-istril(A::Triangular) = A.uplo == 'L'
-istriu(A::Triangular) = A.uplo == 'U'
+######################
+# BlasFloat routines #
+######################
 
 # Vector multiplication
 *{T<:BlasFloat}(A::Triangular{T}, b::Vector{T}) = BLAS.trmv(A.uplo, 'N', A.unitdiag, A.UL, b)
@@ -44,7 +40,64 @@ Ac_ldiv_B{T<:BlasComplex}(A::Triangular{T}, B::StridedVecOrMat{T}) = LAPACK.trtr
 A_rdiv_Bc{T<:BlasReal}(A::StridedVecOrMat{T}, B::Triangular{T}) = BLAS.trsm!('R', B.uplo, 'T', B.unitdiag, one(T), B.UL, copy(A))
 A_rdiv_Bc{T<:BlasComplex}(A::StridedVecOrMat{T}, B::Triangular{T}) = BLAS.trsm!('R', B.uplo, 'C', B.unitdiag, one(T), B.UL, copy(A))
 
-det(A::Triangular) = prod(diag(A.UL))
-
 inv{T<:BlasFloat}(A::Triangular{T}) = LAPACK.trtri!(A.uplo, A.unitdiag, copy(A.UL))
-inv(A::Triangular) = inv(Triangular(float(A.UL), A.uplo, A.unitdiag))
+
+####################
+# Generic routines #
+####################
+
+full(A::Triangular) = (istril(A) ? tril! : triu!)(A.UL)
+getindex{T}(A::Triangular{T}, i::Integer, j::Integer) = i == j ? A.UL[i,j] : ((A.uplo == 'U') == (i < j) ? getindex(A.UL, i, j) : zero(T))
+
+istril(A::Triangular) = A.uplo == 'L' || istriu(A.UL)
+istriu(A::Triangular) = A.uplo == 'U' || istril(A.UL)
+
+size(A::Triangular, args...) = size(A.UL, args...)
+
+transpose(A::Triangular) = Triangular(A.UL, A.uplo=='U':'L':'U', A.unitdiag)
+ctranspose(A::Triangular) = conj(transpose(A))
+
+#Generic solver using naive substitution
+function naivesub!(A::Triangular, b::AbstractVector, x::AbstractVector=b)
+    N = size(A, 2)
+    N==length(b)==length(x) || throw(DimensionMismatch(""))
+
+	if A.uplo == 'L' #do forward substitution
+	    for j = 1:N
+	    	x[j] = b[j]
+	        for k = 1:j-1
+	        	x[j] -= A[j,k] * x[k]
+	        end
+	        if A.unitdiag=='N'
+	        	x[j]/= A[j,j]==0 ? throw(SingularException(j)) : A[j,j]
+	        end
+	    end
+    elseif A.uplo == 'U' #do backward substitution
+	    for j = N:-1:1
+	        x[j] = b[j]
+	        for k = j+1:1:N
+	            x[j] -= A[j,k] * x[k]
+	        end
+	        if A.unitdiag=='N'
+	        	x[j]/= A[j,j]==0 ? throw(SingularException(j)) : A[j,j]
+	        end
+	    end
+	else
+		throw(ArgumentError("Unknown uplo=$(A.uplo)"))
+	end
+    x
+end
+\{T<:Number}(A::Triangular{T}, b::AbstractVector{T}) = naivesub!(A, b, similar(b))
+\{T<:Number}(A::Triangular{T}, B::AbstractMatrix{T}) = hcat([naivesub!(A, B[:,i], similar(B[:,i])) for i=1:size(B,2)]...)
+
+function inv(A::Triangular)
+	B = eye(eltype(A), size(A, 1))
+	for i=1:size(B,2)
+		naivesub!(A, B[:,i])
+	end
+	B
+end
+
+#Generic eigensystems
+det(A::Triangular) = prod(eigvals(A))
+
