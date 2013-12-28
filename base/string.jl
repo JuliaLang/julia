102b2c2380c189275a0c9e6b3619832437a07733
Jon Malmaud <malmaud@gmail.com>
2013-12-28 11:02:51 -0600

    Added convert(::Type{Symbol}, ::String) method

--- base/string.jl
+++ base/string.jl
@@ -47,6 +47,7 @@ convert(::Type{Array{Uint8,1}}, s::String) = bytestring(s).data
 convert(::Type{Array{Uint8}}, s::String) = bytestring(s).data
 convert(::Type{ByteString}, s::String) = bytestring(s)
 convert(::Type{Array{Char,1}}, s::String) = collect(s)
+convert(::Type{Symbol}, s::String) = symbol(s)
 
 ## generic supplied functions ##
 
