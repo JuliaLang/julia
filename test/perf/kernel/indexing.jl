3151217cec5ca7166cb3b9173a6dc6d99cdca770
Jeff Bezanson <jeff.bezanson@gmail.com>
2013-12-29 16:13:13 -0500

    rename devec_add_one benchmarks; the names were too long for perfcomp.jl

--- test/perf/kernel/indexing.jl
+++ test/perf/kernel/indexing.jl
@@ -1,14 +1,14 @@
-function add_one!(x,y)
+function add1!(x,y)
     x[y] += 1
 end
 
-function devec_add_one!(x,y)
+function devec_add1!(x,y)
 	for i=1:length(y)
         x[y[i]] += 1
     end
 end
 
-function devec_add_one_logical!(x,y)
+function devec_add1_logical!(x,y)
 	for i=1:length(y)
         if y[i]
             x[i] += 1
