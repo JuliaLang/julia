fea00fb79a4c320cbdc4bc87039107ee6dbf1719
Elliot Saba <staticfloat@gmail.com>
2014-01-28 21:09:00 -0800

    Add maxlen parameter to stringpatch to avoid corrupting binaries with extremely long paths

--- Makefile
+++ Makefile
@@ -228,7 +228,7 @@ endif
 
 	# Overwrite JL_SYSTEM_IMAGE_PATH in julia binaries:
 	for julia in $(DESTDIR)$(bindir)/julia-* ; do \
-		$(build_bindir)/stringpatch $$(strings -t x - $$julia | grep "sys.ji$$" | awk '{print $$1;}' ) "$(private_libdir_rel)/sys.ji" $$julia; \
+		$(build_bindir)/stringpatch $$(strings -t x - $$julia | grep "sys.ji$$" | awk '{print $$1;}' ) "$(private_libdir_rel)/sys.ji" 256 $$julia; \
 	done
 
 	mkdir -p $(DESTDIR)$(sysconfdir)
--- contrib/stringpatch.c
+++ contrib/stringpatch.c
@@ -3,18 +3,24 @@
 #include <string.h>
 
 int main( int argc, char ** argv ) {
-	if( argc < 4 ) {
+	if( argc < 5 ) {
 		printf("Usage:\n");
-		printf("  %s <hex offset> <string to write> <file>\n", argv[0] );
+		printf("  %s <hex offset> <string to write> <maxlen> <file>\n", argv[0] );
 		return -1;
 	}
 
 	unsigned long offset = strtoul(argv[1], NULL, 16);
 	char * replacement = argv[2];
+	unsigned long maxlen = strtoul(argv[3], NULL, 10);
 
-	FILE * f = fopen( argv[3], "r+" );
+	FILE * f = fopen( argv[4], "r+" );
 	if( !f ) {
 		printf( "ERROR: Could not open %s for writing!\n", argv[3] );
+		return -1;
+	}
+
+	if( strlen(replacement) > maxlen ) {
+		printf( "ERROR: Replacement string length (%d) is greater than maxlen! (%d)\n", strlen(replacement), maxlen );
 		return -1;
 	}
 	
