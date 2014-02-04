b3b09ae1d52ebfe6ab37b828812610bfcab7ce6a
Steven G. Johnson <stevenj@mit.edu>
2014-02-04 11:03:47 -0500

    repl_command should not use bash -i (interactive) unless STDIN is a TTY (fix JuliaLang/IJulia.jl#120)

--- base/client.jl
+++ base/client.jl
@@ -60,7 +60,7 @@ function repl_cmd(cmd)
         end
         println(pwd())
     else
-        run(ignorestatus(@windows? cmd : `$shell -i -c "($(shell_escape(cmd))) && true"`))
+        run(ignorestatus(@windows? cmd : (isa(STDIN, TTY) ? `$shell -i -c "($(shell_escape(cmd))) && true"` : `$shell -c "($(shell_escape(cmd))) && true"`)))
     end
     nothing
 end
