fed6a6a3ef86a4cc8cb4f6852adda0ee40b193d5
Bassem Youssef <bassem@outlook.com>
2014-02-04 11:16:34 -0800

    Add Julia functions for manipulating process resources as provided by libc and specified in getrlimit(2) and setrlimit(2)

--- base/libc.jl
+++ base/libc.jl
@@ -66,6 +66,128 @@ time(tm::TmStruct) = float64(ccall(:mktime, Int, (Ptr{Void},), &tm))
 
 getpid() = ccall(:jl_getpid, Int32, ())
 
+
+# provides Julia functions to manipulate process resources provided by libc and
+# as specified in getrlimit(2) and setrlimit(2)
+
+@linux_only immutable Resources
+    RLIMIT_CPU::Int # 0 cpu time per process
+    RLIMIT_FSIZE::Int # 1 largest file size in bytes
+    RLIMIT_DATA::Int # 2 data segment size in bytes
+    RLIMIT_STACK::Int # 3 stack size in bytes
+    RLIMIT_CORE::Int # 4 core file size in bytes
+    RLIMIT_RSS::Int # 5 resident set size affects swapping processes that
+                    # are exceeding their resident set size will be more
+                    # likely to have physical memory taken from them
+    RLIMIT_NPROC::Int # 6 number of processes
+    RLIMIT_NOFILE::Int # 7 number of open files
+    RLIMIT_MEMLOCK::Int # 8 locked-in-memory address space
+    RLIMIT_AS::Int # 9 address space in bytes
+    RLIMIT_LOCKS::Int # 10 Maximum number of file locks
+    RLIMIT_SIGPENDING::Int # 11 Maximum number of pending signals
+    RLIMIT_MSGQUEUE::Int # 12 Maximum bytes in POSIX message queues
+    RLIMIT_NICE::Int # 13 Maximum nice priority allowed to raise to.
+                     # Nice levels 19 .. -20 correspond to 0 .. 39
+                     # values of this resource limit
+    RLIMIT_RTPRIO::Int # 14 Maximum realtime priority allowed for
+                        # non-priviledged processes
+    RLIMIT_RTTIME::Int # 15 Maximum CPU time in µs that a process scheduled
+                       # under a real-time scheduling policy may consume without
+                       # making a blocking system call before being forcibly descheduled
+    RLIM_NLIMITS::Int # 16 total number of resource limits
+    Resources() = new(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+end
+
+@osx_only immutable Resources
+    RLIMIT_CPU::Int # 0 cpu time per process
+    RLIMIT_FSIZE::Int # 1 file size
+    RLIMIT_DATA::Int # 2 data segment size
+    RLIMIT_STACK::Int # 3 stack size
+    RLIMIT_CORE::Int # 4 core file size
+    RLIMIT_AS::Int # 5 address space
+    RLIMIT_RSS::Int # 5 resident set size
+    RLIMIT_MEMLOCK::Int # 6 locked-in-memory address space
+    RLIMIT_NPROC::Int # 7 number of processes
+    RLIMIT_NOFILE::Int # 8 number of open files
+    RLIM_NLIMITS::Int # 9 total number of resource limits
+    Resources() = new(0,1,2,3,4,5,5,6,7,8,9)
+end
+
+type RLimit
+    rlim_cur::Clong
+    rlim_max::Clong
+    RLimit() = new(0,0)
+    RLimit(cur,max) = new(cur,max)
+end
+
+# perhaps an exception might be better
+function verify_resource(resource::Int)
+    if !in(resource,0:Resources().RLIM_NLIMITS)
+        err = names(Resources) |>
+              _->_[1:end-1] |>
+              _->enumerate(_) |>
+              _->[string(i[2]," = ",i[1] - 1) for i in _]
+        error("Invalid resource specified: should be one of the following:\n$err")
+    end
+end
+
+function getrlimit(resource::Int)
+    verify_resource(resource)
+    rlim = RLimit()
+    rc = ccall(:getrlimit, Int, (Int, Ptr{RLimit}), resource, &rlim)
+    if rc == 0
+        rlim
+    else
+        error("failed with errorcode:",rc)
+    end
+end
+
+function setrlimit(resource::Int,settings_tup::(Int,Int))
+    verify_resource(resource)
+    if !isa(settings_tup,(Int,Int))
+        error("Expected a tuple of two integers (current,maximum)")
+    end
+    rlim = RLimit(settings_tup[1],settings_tup[2])
+    rc = ccall(:setrlimit, Int, (Int, Ptr{RLimit}), resource, &rlim)
+    if rc == 0
+        rlim
+    else
+        error("failed with errorcode:",rc)
+    end
+end
+
+@linux_only prlimit(pid::Int32, resource::Int) = prlimit(pid, resource, (nothing, nothing))
+@linux_only function prlimit(pid::Int32, resource::Int, limits_tup)
+    verify_resource(resource)
+    rlim_old = RLimit()
+    if isa(limits_tup,(Nothing,Nothing))
+        rc = ccall(:prlimit, Int, (Int, Int, Ptr{RLimit}, Ptr{RLimit}),
+                                   pid, resource, C_NULL, &rlim_old)
+    elseif isa(limits_tup,(Int,Int))
+        rlim_new = RLimit(limits_tup[1],limits_tup[2])
+        rc = ccall(:prlimit, Int, (Int, Int, Ptr{RLimit}, Ptr{RLimit}),
+                                   pid, resource, &rlim_new, &rlim_old)
+    else
+        error("Expected a tuple of two integers (current,maximum)")
+    end
+
+    if rc == 0
+        (rlim_old.rlim_cur,rlim_old.rlim_max)
+    else
+        error("failed with errorcode:",rc)
+    end
+end
+
+# override default show for RLimit type
+import Base.show
+show(io::IO, limit::RLimit) = print(io, string("Resource Limits(Current: ",
+                                    limit.rlim_cur == typemax(Clong) ?
+                                        "unlimited" : limit.rlim_cur,
+                                    ", Maximum: ",
+                                    limit.rlim_max == typemax(Clong) ?
+                                        "unlimited" : limit.rlim_max,")"))
+
+
 ## network functions ##
 
 function gethostname()
