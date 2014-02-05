bc84af5ed8c80b3b2a2b451b43eb24ab90351bd9
Jeff Bezanson <jeff.bezanson@gmail.com>
2014-02-05 16:59:43 -0500

    lex dot-relational operators the same as other dot operators. fixes #5689

--- src/julia-parser.scm
+++ src/julia-parser.scm
@@ -128,7 +128,7 @@
 ;; characters that can be in an operator
 (define (opchar? c) (and (char? c) (string.find op-chars c)))
 ;; characters that can follow . in an operator
-(define (dot-opchar? c) (and (char? c) (string.find ".*^/\\+-'" c)))
+(define (dot-opchar? c) (and (char? c) (string.find ".*^/\\+-'<>!=%" c)))
 (define (operator? c) (memq c operators))
 
 (define (skip-to-eol port)
