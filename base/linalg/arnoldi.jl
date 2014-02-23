e239e338a3ab28f6a33948d4263aeeaeef6c31bd
acroy <alexcroy@googlemail.com>
2014-02-23 16:45:10 +0100

    Only non-empty sigma selects shift and invert mode (implies which=LM). Test updated.

--- base/linalg/arnoldi.jl
+++ base/linalg/arnoldi.jl
@@ -3,7 +3,7 @@ using .ARPACK
 ## eigs
 
 function eigs(A;nev::Integer=6, ncv::Integer=20, which::ASCIIString="LM",
-          tol=0.0, maxiter::Integer=1000, sigma=nothing, v0::Vector=zeros((0,)),
+          tol=0.0, maxiter::Integer=1000, sigma=(), v0::Vector=zeros((0,)),
           ritzvec::Bool=true, complexOP::Bool=false)
     n = chksquare(A)
     (n <= 6) && (nev = min(n-1, nev))
@@ -13,8 +13,6 @@ function eigs(A;nev::Integer=6, ncv::Integer=20, which::ASCIIString="LM",
     T     = eltype(A)
     cmplx = T<:Complex
     bmat  = "I"
-    const arpack_which_lm = "LM"   # always looking for largest EV
-    arpack_sigma = 0
     
     if !isempty(v0)
         length(v0)==n || throw(DimensionMismatch(""))
@@ -23,17 +21,19 @@ function eigs(A;nev::Integer=6, ncv::Integer=20, which::ASCIIString="LM",
         v0=zeros(T,(0,))
     end
 
-    if sigma == nothing && which == "LM"
+    if isempty(sigma)
         # normal iteration
         mode = 1
+        sigma = 0
         linop(x) = A * x
     else
-        if (sigma == nothing && which == "SM") || sigma == 0
-            # invert only
+        # always find ev closest in magnitude to sigma ...
+        which = "LM" 
+        if sigma == 0
+            # using invert only
             C = lufact(A)
         else
-            # shift and invert
-            arpack_sigma = sigma
+            # using shift and invert
             C = lufact(A - sigma*eye(A))
         end
         if cmplx
@@ -52,10 +52,10 @@ function eigs(A;nev::Integer=6, ncv::Integer=20, which::ASCIIString="LM",
         
     # Compute the Ritz values and Ritz vectors
     (resid, v, ldv, iparam, ipntr, workd, workl, lworkl, rwork, TOL) = 
-       ARPACK.aupd_wrapper(T, linop, n, sym, cmplx, bmat, nev, ncv, arpack_which_lm, tol, maxiter, mode, v0)
+       ARPACK.aupd_wrapper(T, linop, n, sym, cmplx, bmat, nev, ncv, which, tol, maxiter, mode, v0)
     
     # Postprocessing to get eigenvalues and eigenvectors
-    ARPACK.eupd_wrapper(T, n, sym, cmplx, bmat, nev, arpack_which_lm, ritzvec, TOL,
-        resid, ncv, v, ldv, arpack_sigma, iparam, ipntr, workd, workl, lworkl, rwork)
+    ARPACK.eupd_wrapper(T, n, sym, cmplx, bmat, nev, which, ritzvec, TOL,
+        resid, ncv, v, ldv, sigma, iparam, ipntr, workd, workl, lworkl, rwork)
 
 end
