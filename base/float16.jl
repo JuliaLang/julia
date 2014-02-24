aa724dcfa9326ba870a9bc6eb91a12afeff01aaa
Stefan Karpinski <stefan@karpinski.org>
2014-02-23 19:14:35 -0500

    Float16: convert to various number types; round, trunc, floor, ceil.
    These additional operations are needed to get the random and linalg
    tests closer to passing for the FloatRange change [#5636]. We really
    need to make a decision about how first-class we want Float16 to be.
    I'm starting to suspect that we should just bite the bullet and make
    all the things that work for other floating-point types work for the
    Float16 type also. It's just easier than having them be half-broken
    and try to explain that they're "just for storage".

--- base/float16.jl
+++ base/float16.jl
@@ -96,6 +96,16 @@ function convert(::Type{Float16}, val::Float32)
     reinterpret(Float16, h)
 end
 
+for T in (Bool, Int128, Uint128, Complex, Rational)
+    @eval convert(::Type{$T}, x::Float16) = convert($T,float32(x))
+end
+convert{T<:Number}(::Type{T}, x::Float16) = convert(T,float32(x))
+
+round(x::Float16) = float16(round(float32(x)))
+trunc(x::Float16) = float16(trunc(float32(x)))
+floor(x::Float16) = float16(floor(float32(x)))
+ ceil(x::Float16) = float16( ceil(float32(x)))
+
 isnan(x::Float16)    = reinterpret(Uint16,x)&0x7fff  > 0x7c00
 isinf(x::Float16)    = reinterpret(Uint16,x)&0x7fff == 0x7c00
 isfinite(x::Float16) = reinterpret(Uint16,x)&0x7c00 != 0x7c00
