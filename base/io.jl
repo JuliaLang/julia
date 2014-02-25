991cf2f6b3d8f8261d0f89220f2899ffabd94cbf
Keno Fischer <kfischer@college.harvard.edu>
2014-02-25 14:57:04 -0500

    Fix #5951

--- base/io.jl
+++ base/io.jl
@@ -381,7 +381,7 @@ end
 
 ## low-level calls ##
 
-write(s::IOStream, b::Uint8) = int(ccall(:jl_putc, Int32, (Uint8, Ptr{Void}), b, s.ios))
+write(s::IOStream, b::Uint8) = int(ccall(:ios_putc, Int32, (Uint8, Ptr{Void}), b, s.ios))
 
 function write{T}(s::IOStream, a::Array{T})
     if isbits(T)
