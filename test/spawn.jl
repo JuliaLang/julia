e615c9515c03610612232593178f7dbabe478e68
Jeff Bezanson <jeff.bezanson@gmail.com>
2014-03-01 16:40:29 -0500

    fix #5904, assertion failure running pipelines
    must be careful using functional constructs where side effects are needed

--- test/spawn.jl
+++ test/spawn.jl
@@ -122,3 +122,7 @@ yield()
 # issue #4535
 exename=joinpath(JULIA_HOME,(ccall(:jl_is_debugbuild,Cint,())==0?"julia-basic":"julia-debug-basic"))
 @test readall(`$exename -f -e 'println(STDERR,"Hello World")'` .> `cat`) == "Hello World\n"
+
+
+# issue #5904
+@test run(ignorestatus(`false`) |> `true`) === nothing
