616b17ef08fb2f06f0e24c069f5c358ec70508a9
Miles Gould <mgould1@inf.ed.ac.uk>
2014-03-05 20:33:18 +0000

    Make version_git.sh work on older versions of git.
    The --count option to git-rev-list was introduced in git version 1.7.x
    for 1 < x < 12. When base/version_git.sh is run on versions of git
    without this option, a long help message is generated (cluttering up
    make output) and generating an incorrect base/version_git.jl.

--- base/version_git.sh
+++ base/version_git.sh
@@ -33,7 +33,7 @@ if [ -n "$(git status --porcelain)" ]; then
     commit_short="$commit_short"*
 fi
 branch=$(git branch | sed -n '/\* /s///p')
-build_number=$(git rev-list --count HEAD ^$last_tag)
+build_number=$(git rev-list HEAD ^$last_tag | wc -l)
 
 date_string=$git_time
 if [ "$(uname)" = "Darwin" ] || [ "$(uname)" = "FreeBSD" ]; then
@@ -46,7 +46,7 @@ if [ $(git describe --tags --exact-match 2> /dev/null) ]; then
 else
     tagged_commit="false"
 fi
-fork_master_distance=$(git rev-list --count HEAD ^"$(echo $origin)master")
+fork_master_distance=$(git rev-list HEAD ^"$(echo $origin)master" | wc -l)
 fork_master_timestamp=$(git show -s $(git merge-base HEAD $(echo $origin)master) --format=format:"%ct")
 
 # Check for errrors and emit default value for missing numbers.
