f04411b6fbf31bbfad00d12cd8ff08c264af1520
Dahua Lin <lindahua@gmail.com>
2014-03-11 15:19:52 -0500

    add typealias for DenseVector & DenseMatrix

--- base/array.jl
+++ base/array.jl
@@ -4,6 +4,10 @@ typealias Vector{T} Array{T,1}
 typealias Matrix{T} Array{T,2}
 typealias VecOrMat{T} Union(Vector{T}, Matrix{T})
 
+typealias DenseVector{T} DenseArray{T,1}
+typealias DenseMatrix{T} DenseArray{T,2}
+typealias DenseVecOrMat{T} Union(DenseVector{T}, DenseMatrix{T})
+
 typealias StoredVector{T} StoredArray{T,1}
 typealias StridedArray{T,N,A<:DenseArray} Union(DenseArray{T,N}, SubArray{T,N,A})
 typealias StridedVector{T,A<:DenseArray}  Union(DenseArray{T,1}, SubArray{T,1,A})
