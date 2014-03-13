51f8484e5d5048d08e3ba420177613602d9cf1a5
Tony Kelman <tony@kelman.net>
2014-03-12 22:12:35 -0700

    Remove hard-coded Windows installation paths
    - For Git, shouldn't need to look in Program Files since Windows Julia bundles Git.
    - MinGW is a build-time dependency, not a user dependency. These path additions were the only reason test/spawn.jl could ever execute successfully outside of MSYS. This is a problem that can be addressed separately, and the solution should not rely on a hard-coded installation path of MSYS1.
    - Python should only be needed by packages, or at build time for LLVM. Wouldn't a better solution involve PYTHONPATH or PYTHONHOME, or manage this on a package level when Python is required?

--- contrib/windows/juliarc.jl
+++ contrib/windows/juliarc.jl
@@ -1,8 +1,5 @@
 let user_data_dir
-    ENV["PATH"] = JULIA_HOME*";"*joinpath(JULIA_HOME,"..","Git","bin")*";"*ENV["PATH"]*
-        ";C:\\Program Files\\Git\\bin;C:\\Program Files (x86)\\Git\\bin"*
-        ";C:\\MinGW\\bin;C:\\MinGW\\msys\\1.0\\bin"*
-        ";C:\\Python27;C:\\Python26;C:\\Python25"
+    ENV["PATH"] = JULIA_HOME*";"*joinpath(JULIA_HOME,"..","Git","bin")*";"*ENV["PATH"]
     haskey(ENV,"JULIA_EDITOR") || (ENV["JULIA_EDITOR"] = "start")
     user_data_dir = abspath(ENV["AppData"],"julia")
     isdir(user_data_dir) || mkdir(user_data_dir)
