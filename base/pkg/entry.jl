cc616e4db0d54ea14502a039fc4bc31f07c66345
Stefan Karpinski <stefan@karpinski.org>
2014-03-17 18:34:40 -0400

    Pkg.clone: allow leading [^\/] for auto package name detection.
    Closes #6141 and #6152.

--- base/pkg/entry.jl
+++ base/pkg/entry.jl
@@ -166,7 +166,7 @@ function clone(url_or_pkg::String)
         # TODO: Cache.prefetch(pkg,url)
     else
         url = url_or_pkg
-        m = match(r"/(\w+?)(?:\.jl)?(?:\.git)?$", url)
+        m = match(r"(?:^|[/\\])(\w+?)(?:\.jl)?(?:\.git)?$", url)
         m != nothing || error("can't determine package name from URL: $url")
         pkg = m.captures[1]
     end
