35423619628742359be0e6d8396d67cad17b4d99
Tony Kelman <tony@kelman.net>
2014-03-25 19:36:26 -0700

    Work to get basic REPL to compile with MSVC
    explicit casts and fix ifdefs (__WIN32__ => _WIN32)
    
    remove unistd header already included in repl.h
    
    remove DLLEXPORT from repl.h
    
    use hex code for escape
    
    put back getopt.h
    
    define dirname and PATH_MAX for repl
    
    fix LDFLAGS that MSVC doesn't understand
    
    skip julia_res.o with msvc
    
    extern C linkage on repl

--- ui/repl.h
+++ ui/repl.h
@@ -14,22 +14,33 @@
 #ifndef _MSC_VER
 #include <unistd.h>
 #include <libgen.h>
-#include <getopt.h>
+#else
+char * basename(char *);
+char * dirname(char *);
 #endif
 #include <limits.h>
 #include <errno.h>
 #include <math.h>
+#include <getopt.h>
 #include <ctype.h>
 
 #include "julia.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern int tab_width;
 extern DLLEXPORT char *julia_home;
 
 extern void handle_input(jl_value_t *ast, int end, int show_value);
 extern int ends_with_semicolon(const char *input);
-DLLEXPORT extern void repl_callback_enable(char *prompt);
-DLLEXPORT extern void jl_read_buffer(unsigned char *base, ssize_t nread);
-DLLEXPORT extern void jl_init_repl(int history);
+extern void repl_callback_enable(char *prompt);
+extern void jl_read_buffer(unsigned char *base, ssize_t nread);
+extern void jl_init_repl(int history);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // JL_REPL_H
