c3db624dc090d20c15ad42c987fdd139f3a421a8
Jameson Nash <vtjnash@gmail.com>
2014-03-29 03:51:29 -0400

    accelerate backtrace-to-function mapping
    this seems to make printing backtraces about 90% faster

--- src/debuginfo.cpp
+++ src/debuginfo.cpp
@@ -10,19 +10,24 @@ struct FuncInfo{
 extern "C" EXCEPTION_DISPOSITION _seh_exception_handler(PEXCEPTION_RECORD ExceptionRecord,void *EstablisherFrame, PCONTEXT ContextRecord, void *DispatcherContext);
 #endif
 
+struct revcomp {
+  bool operator() (const size_t& lhs, const size_t& rhs) const
+  {return lhs>rhs;}
+};
+
 class JuliaJITEventListener: public JITEventListener
 {
-    std::map<size_t, FuncInfo> info;
-    
+    std::map<size_t, FuncInfo, revcomp> info;
+
 public:	
     JuliaJITEventListener(){}
     virtual ~JuliaJITEventListener() {}
-    
+
     virtual void NotifyFunctionEmitted(const Function &F, void *Code,
                                        size_t Size, const EmittedFunctionDetails &Details)
     {
         FuncInfo tmp = {&F, Size, Details.LineStarts};
-        info[(size_t)(Code)] = tmp;
+        if (tmp.lines.size() != 0) info[(size_t)(Code)] = tmp;
 #if defined(_OS_WINDOWS_) && defined(_CPU_X86_64_)
         uintptr_t catchjmp = (uintptr_t)Code+Size;
         *(uint8_t*)(catchjmp+0) = 0x48;
@@ -46,8 +51,8 @@ public:
         RtlAddFunctionTable(tbl,1,(DWORD64)Code);
 #endif
     }
-    
-    std::map<size_t, FuncInfo>& getMap()
+
+    std::map<size_t, FuncInfo, revcomp>& getMap()
     {
         return info;
     }
@@ -59,53 +64,44 @@ extern "C" void getFunctionInfo(const char **name, int *line, const char **filen
 
 void getFunctionInfo(const char **name, int *line, const char **filename, size_t pointer)
 {
-    std::map<size_t, FuncInfo> &info = jl_jit_events->getMap();
+    std::map<size_t, FuncInfo, revcomp> &info = jl_jit_events->getMap();
     *name = NULL;
     *line = -1;
     *filename = "no file";
-    for (std::map<size_t, FuncInfo>::iterator it= info.begin(); it!= info.end(); it++) {
-        if ((*it).first <= pointer) {
-            if ((size_t)(*it).first + (*it).second.lengthAdr >= pointer) {
-                // commenting these lines out skips functions that don't
-                // have explicit debug info. this is useful for hiding
-                // the jlcall wrapper functions we generate.
+    std::map<size_t, FuncInfo, revcomp>::iterator it = info.lower_bound(pointer);
+    if (it != info.end() && (size_t)(*it).first + (*it).second.lengthAdr >= pointer) {
+        // commenting these lines out skips functions that don't
+        // have explicit debug info. this is useful for hiding
+        // the jlcall wrapper functions we generate.
 #if LLVM_VERSION_MAJOR == 3
 #if LLVM_VERSION_MINOR == 0
-                //*name = &(*(*it).second.func).getNameStr()[0];
+        //*name = &(*(*it).second.func).getNameStr()[0];
 #elif LLVM_VERSION_MINOR >= 1
-                //*name = (((*(*it).second.func).getName()).data());
+        //*name = (((*(*it).second.func).getName()).data());
 #endif
 #endif
-                if ((*it).second.lines.size() == 0) {
-                    continue;
-                }
-                
-                std::vector<JITEvent_EmittedFunctionDetails::LineStart>::iterator vit = (*it).second.lines.begin();
-                JITEvent_EmittedFunctionDetails::LineStart prev = *vit;
+        std::vector<JITEvent_EmittedFunctionDetails::LineStart>::iterator vit = (*it).second.lines.begin();
+        JITEvent_EmittedFunctionDetails::LineStart prev = *vit;
 
-                DISubprogram debugscope =
-                    DISubprogram(prev.Loc.getScope((*it).second.func->getContext()));
-                *filename = debugscope.getFilename().data();
-                // the DISubprogram has the un-mangled name, so use that if
-                // available.
-                *name = debugscope.getName().data();
-                
-                vit++;
-                
-                while (vit != (*it).second.lines.end()) {
-                    if (pointer <= (*vit).Address) {
-                        *line = prev.Loc.getLine();
-                        break;
-                    }
-                    prev = *vit;
-                    vit++;
-                }
-                if (*line == -1) {
-                    *line = prev.Loc.getLine();
-                }
-                
+        DISubprogram debugscope =
+            DISubprogram(prev.Loc.getScope((*it).second.func->getContext()));
+        *filename = debugscope.getFilename().data();
+        // the DISubprogram has the un-mangled name, so use that if
+        // available.
+        *name = debugscope.getName().data();
+
+        vit++;
+
+        while (vit != (*it).second.lines.end()) {
+            if (pointer <= (*vit).Address) {
+                *line = prev.Loc.getLine();
                 break;
             }
+            prev = *vit;
+            vit++;
+        }
+        if (*line == -1) {
+            *line = prev.Loc.getLine();
         }
     }
 }
