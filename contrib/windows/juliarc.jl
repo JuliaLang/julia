c7fc84a4092776087b448cd5082d6e9da1385f60
Jameson Nash <jameson@mit.edu>
2014-03-30 21:52:14 -0400

    partially fix using the repl on msys

--- contrib/windows/juliarc.jl
+++ contrib/windows/juliarc.jl
@@ -3,4 +3,16 @@ let user_data_dir
     haskey(ENV,"JULIA_EDITOR") || (ENV["JULIA_EDITOR"] = "start")
     user_data_dir = abspath(ENV["AppData"],"julia")
     isdir(user_data_dir) || mkdir(user_data_dir)
+    ispty(s::Base.AsyncStream) = bool(ccall(:jl_ispty, Cint, (Ptr{Void},), s))
+    ispty(s) = false
+    if ispty(STDIN)
+        function Base.Terminals.raw!(t::Base.Terminals.UnixTerminal,raw::Bool)
+            if raw
+                run(`stty raw -echo`,t.in_stream,t.out_stream,t.err_stream)
+            else
+                run(`stty cooked echo`,t.in_stream,t.out_stream,t.err_stream)
+            end
+            true
+        end
+    end
 end
