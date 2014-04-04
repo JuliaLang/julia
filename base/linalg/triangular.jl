2bcb7dcfbac65c95e080f16340dbd76019da75bf
Jiahao Chen <jiahao@mit.edu>
2014-04-04 00:54:18 -0400

    Remove unnecessary map from \{::Triangular} error check
    Cleanup of #6399.

--- base/linalg/triangular.jl
+++ base/linalg/triangular.jl
@@ -37,7 +37,7 @@ A_mul_Bc!{T<:BlasReal}(A::StridedMatrix{T}, B::Triangular{T}) = BLAS.trmm('R', B
 function \{T<:BlasFloat}(A::Triangular{T}, B::StridedVecOrMat{T})
     x = LAPACK.trtrs!(A.uplo, 'N', A.unitdiag, A.UL, copy(B))
     errors=LAPACK.trrfs!(A.uplo, 'N', A.unitdiag, A.UL, B, x)
-    all(map(isfinite, [errors...])) || all([errors...] .< one(T)/eps(T)) || warn("""Unreasonably large error in computed solution:
+    all(isfinite, [errors...]) || all([errors...] .< one(T)/eps(T)) || warn("""Unreasonably large error in computed solution:
 forward errors:
 $(errors[1])
 backward errors:
