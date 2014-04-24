62f3d76c1c602efa0a6daff9d52a6ee8de6d075c
Avik Sengupta <avik@sengupta.net>
2014-04-24 11:04:46 +0100

    Enable running java perf via Makefile

--- test/perf/micro/bin/table.pl
+++ test/perf/micro/bin/table.pl
@@ -30,6 +30,7 @@ our $lua_ver = `gsl-shell -v 2>&1 | grep Shell | cut -f3 -d" " | cut -f1 -d,`;
 our $javascript_ver = `node -e "console.log(process.versions.v8)"`;
 our $mathematica_ver = `echo quit | math -version | head -n 1 | cut -f2 -d" "`;
 our $stata_ver = `stata -q -b version && grep version stata.log | cut -f2 -d" " && rm stata.log`;
+our $java_ver = `java -version 2>&1 |grep "version" | cut -f 3 -d " " | cut -c 2-9`;
 
 our %systems = (
   "fortran"    => ["Fortran"     , "GCC $fortran_ver" ],
@@ -43,6 +44,7 @@ our %systems = (
   "mathematica"=> ["Mathematica" , $mathematica_ver ],
   "lua"	       => ["LuaJIT"      , "gsl-shell $lua_ver" ],
   "stata"      => ["Stata"       , $stata_ver ],
+  "java"       => ["Java"        , $java_ver ],
 );
 
 our @systems = qw(fortran julia python r matlab octave mathematica javascript go lua);
