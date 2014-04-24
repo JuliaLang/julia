f7910291097d192dc8e0618424fe10653d8545c0
Stefan Karpinski <stefan@karpinski.org>
2014-04-24 18:37:49 -0400

    move generic rational hashing definitions into hashing2.jl

--- base/hashing.jl
+++ base/hashing.jl
@@ -27,130 +27,18 @@ function hash_uint(n::Uint32)
     return a
 end
 
-## efficient value-based hashing of integers ##
-
-function hash_integer(n::Integer, h::Uint)
-    h = hash_uint(uint(n & typemax(Uint)) $ h) $ h
-    n = ifelse(n < 0, oftype(n,-n), n)
-    n >>>= sizeof(Uint) << 3
-    while n != 0
-        h = hash_uint(uint(n & typemax(Uint)) $ h) $ h
-        n >>>= sizeof(Uint) << 3
-    end
-    return h
-end
-
-## hashing rational values ##
-
-#=
-`decompose(x)`: non-canonical decomposition of rational values as `num*2^pow/den`.
-
-The decompose function is the point where rational-valued numeric types that support
-hashing hook into the hashing protocol. `decompose(x)` should return three integer
-values `num, pow, den`, such that the value of `x` is mathematically equal to
-
-    num*2^pow/den
-
-The decomposition need not be canonical in the sense that it just needs to be *some*
-way to express `x` in this form, not any particular way – with the restriction that
-`num` and `den` may not share any odd common factors. They may, however, have powers
-of two in common – the generic hashing code will normalize those as necessary.
-
-Special values:
-
- - `x` is zero: `num` should be zero and `den` should have the same sign as `x`
- - `x` is infinite: `den` should be zero and `num` should have the same sign as `x`
- - `x` is not a number: `num` and `den` should both be zero
-=#
-
-decompose(x::Integer) = x, 0, 1
-decompose(x::Rational) = num(x), 0, den(x)
-
-function decompose(x::Float32)
-    isnan(x) && return 0, 0, 0
-    isinf(x) && return ifelse(x < 0, -1, 1), 0, 0
-    n = reinterpret(Int32, x)
-    s = int32(n & 0x007fffff)
-    e = int32(n & 0x7f800000 >> 23)
-    s |= int32(e != 0) << 23
-    d = ifelse(signbit(n) == 1, -1, 1)
-    int(s), int(e - 150 + (e == 0)), d
-end
-
-function decompose(x::Float64)
-    isnan(x) && return 0, 0, 0
-    isinf(x) && return ifelse(x < 0, -1, 1), 0, 0
-    n = reinterpret(Int64, x)
-    s = int64(n & 0x000fffffffffffff)
-    e = int64(n & 0x7ff0000000000000 >> 52)
-    s |= int64(e != 0) << 52
-    d = ifelse(signbit(n) == 1, -1, 1)
-    int(s), int(e - 1075 + (e == 0)), d
-end
-
-# hashing methods for rational-valued types
+## hashing small, built-in numeric types ##
 
 hx(a::Uint64, b::Float64, h::Uint) = hash_uint((3a + reinterpret(Uint64,b)) - h)
 
 hash(x::Uint64,  h::Uint) = hx(x, float64(x), h)
 hash(x::Int64,   h::Uint) = hx(reinterpret(Uint64,x), float64(x), h)
-hash(x::Float64, h::Uint) = hx(box(Uint64,fptosi(unbox(Float64,x))), ifelse(x==x,x,NaN), h)
+hash(x::Float64, h::Uint) = hx(box(Uint64,fptosi(unbox(Float64,x))), ifelse(isnan(x), NaN, x), h)
 
 hash(x::Union(Int8,Uint8,Int16,Uint16,Int32,Uint32), h::Uint) = hash(int64(x), h)
 hash(x::Float32, h::Uint) = hash(float64(x), h)
 
-function hash(x::Real, h::Uint)
-    # decompose x as num*2^pow/den
-    num, pow, den = decompose(x)::(Integer,Integer,Integer)
-
-    # handle special values
-    num == 0 && den == 0 && return hash(NaN, h)
-    if num == 0
-        den > 0 && return hash(+0.0, h)
-        den < 0 && return hash(-0.0, h)
-    end
-    if den == 0
-        num > 0 && return hash(+Inf, h)
-        num < 0 && return hash(-Inf, h)
-    end
-
-    # normalize decomposition
-    if den < 0
-        num = -num
-        den = -den
-    end
-    z = trailing_zeros(num)
-    if z != 0
-        num >>= z
-        pow += z
-    end
-    z = trailing_zeros(den)
-    if z != 0
-        den >>= z
-        pow -= z
-    end
-
-    # handle values representable as Int64, Uint64, Float64
-    if den == 1
-        left = ndigits0z(num,2) + pow
-        right = trailing_zeros(num) + pow
-        if -1074 <= right
-            if 0 <= right && left <= 64
-                left <= 63                     && return hash(int64(num) << int(pow), h)
-                signbit(num) == signbit(den)   && return hash(uint64(num) << int(pow), h)
-            end
-            left <= 1024 && left - right <= 53 && return hash(float64(num) * 2.0^pow, h)
-        end
-    end
-
-    # handle "generic" real values
-    h = hash_integer(den, h)
-    h = hash_integer(pow, h)
-    h = hash_integer(num, h)
-    return h
-end
-
-## hashing complex values ##
+## hashing complex numbers ##
 
 const h_imag = 0x32a7a07f3e7cd1f9
 const hash_0_imag = hash(0, h_imag)
@@ -166,7 +54,7 @@ end
 hash(x::Bool, h::Uint) = hash(int(x), h + 0x4cd135a1755139a5)
 hash(x::Char, h::Uint) = hash(int(x), h + 0x10f989ff0f886f11)
 
-## expression hashing ##
+## symbol & expression hashing ##
 
 hash(x::Symbol, h::Uint) = hash(object_id(x), h)
 hash(x::Expr, h::Uint) = hash(x.args, hash(x.head, h + 0x83c7900696d26dc6))
--- base/hashing2.jl
+++ base/hashing2.jl
@@ -1,4 +1,15 @@
-## hashing BigInts, BigFloats, and Float16s ##
+## efficient value-based hashing of integers ##
+
+function hash_integer(n::Integer, h::Uint)
+    h = hash_uint(uint(n & typemax(Uint)) $ h) $ h
+    n = ifelse(n < 0, oftype(n,-n), n)
+    n >>>= sizeof(Uint) << 3
+    while n != 0
+        h = hash_uint(uint(n & typemax(Uint)) $ h) $ h
+        n >>>= sizeof(Uint) << 3
+    end
+    return h
+end
 
 function hash_integer(n::BigInt, h::Uint)
     s = n.size
@@ -12,6 +23,99 @@ function hash_integer(n::BigInt, h::Uint)
     return h
 end
 
+## generic hashing for rational values ##
+
+function hash(x::Real, h::Uint)
+    # decompose x as num*2^pow/den
+    num, pow, den = decompose(x)::(Integer,Integer,Integer)
+
+    # handle special values
+    num == 0 && den == 0 && return hash(NaN, h)
+    num == 0 && return hash(ifelse(den > 0, 0.0, -0.0), h)
+    den == 0 && return hash(ifelse(num > 0, Inf, -Inf), h)
+
+    # normalize decomposition
+    if den < 0
+        num = -num
+        den = -den
+    end
+    z = trailing_zeros(num)
+    if z != 0
+        num >>= z
+        pow += z
+    end
+    z = trailing_zeros(den)
+    if z != 0
+        den >>= z
+        pow -= z
+    end
+
+    # handle values representable as Int64, Uint64, Float64
+    if den == 1
+        left = ndigits0z(num,2) + pow
+        right = trailing_zeros(num) + pow
+        if -1074 <= right
+            if 0 <= right && left <= 64
+                left <= 63                     && return hash(int64(num) << int(pow), h)
+                signbit(num) == signbit(den)   && return hash(uint64(num) << int(pow), h)
+            end # typemin(Int64) handled by Float64 case
+            left <= 1024 && left - right <= 53 && return hash(float64(num) * 2.0^pow, h)
+        end
+    end
+
+    # handle generic rational values
+    h = hash_integer(den, h)
+    h = hash_integer(pow, h)
+    h = hash_integer(num, h)
+    return h
+end
+
+#=
+`decompose(x)`: non-canonical decomposition of rational values as `num*2^pow/den`.
+
+The decompose function is the point where rational-valued numeric types that support
+hashing hook into the hashing protocol. `decompose(x)` should return three integer
+values `num, pow, den`, such that the value of `x` is mathematically equal to
+
+    num*2^pow/den
+
+The decomposition need not be canonical in the sense that it just needs to be *some*
+way to express `x` in this form, not any particular way – with the restriction that
+`num` and `den` may not share any odd common factors. They may, however, have powers
+of two in common – the generic hashing code will normalize those as necessary.
+
+Special values:
+
+ - `x` is zero: `num` should be zero and `den` should have the same sign as `x`
+ - `x` is infinite: `den` should be zero and `num` should have the same sign as `x`
+ - `x` is not a number: `num` and `den` should both be zero
+=#
+
+decompose(x::Integer) = x, 0, 1
+decompose(x::Rational) = num(x), 0, den(x)
+
+function decompose(x::Float32)
+    isnan(x) && return 0, 0, 0
+    isinf(x) && return ifelse(x < 0, -1, 1), 0, 0
+    n = reinterpret(Int32, x)
+    s = int32(n & 0x007fffff)
+    e = int32(n & 0x7f800000 >> 23)
+    s |= int32(e != 0) << 23
+    d = ifelse(signbit(n) == 1, -1, 1)
+    int(s), int(e - 150 + (e == 0)), d
+end
+
+function decompose(x::Float64)
+    isnan(x) && return 0, 0, 0
+    isinf(x) && return ifelse(x < 0, -1, 1), 0, 0
+    n = reinterpret(Int64, x)
+    s = int64(n & 0x000fffffffffffff)
+    e = int64(n & 0x7ff0000000000000 >> 52)
+    s |= int64(e != 0) << 52
+    d = ifelse(signbit(n) == 1, -1, 1)
+    int(s), int(e - 1075 + (e == 0)), d
+end
+
 function decompose(x::BigFloat)
     isnan(x) && return big(0), 0, 0
     isinf(x) && return big(x.sign), 0, 0
@@ -22,6 +126,8 @@ function decompose(x::BigFloat)
     ccall(:memcpy, Ptr{Void}, (Ptr{Void}, Ptr{Void}, Csize_t), s.d, x.d, s.size*sizeof(Culong))
     s, int(x.exp - x.prec), int(x.sign)
 end
+
+## hashing Float16s ##
 
 hash(x::Float16, h::Uint) = hash(float64(x), h)
 
@@ -53,6 +159,6 @@ hash(a::AbstractArray{Bool}, h::Uint) = hash(bitpack(a), h)
 hash{T<:Range}(r::T, h::Uint) =
     hash(first(r), hash(step(r), hash(last(r), h + object_id(eltype(T)))))
 
-## hashing general objects and expressions ##
+## hashing general objects ##
 
 hash(x::ANY,  h::Uint) = hash(object_id(x), h)
