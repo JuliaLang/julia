8c388693d76af56dc13a2bafcdfab5bcf1ba4614
Cameron McBride <cameron.mcbride@gmail.com>
2014-06-02 04:41:45 -0400

    tweaks to trapz,simps to remove unnecessary ops

--- base/integrate.jl
+++ base/integrate.jl
@@ -1,88 +1,98 @@
 module Integrate
+    # some simple integration routines, mostly for tabulated data
     export trapz, simps
-    # simple integration routines, mostly for tabulated vector data
-    # There are notable interface differences with quadgk()
-    #  * these do does not return an error term
-    #  * these only return scalar values
-    
-    # trapezoidal function integration, probably not the most accurate 
-    # method for functions, but this method is included to aid testing 
+
+    # trapezoidal function integration, probably not the most accurate
+    # method for functions, but this method is included to aid testing
     # and provide comparisons with the vector versions
+    #
+    # unlike quadgk(), this does not return an error term, which
+    # is designed to mirror the vector integrations methods
     function trapz(f::Function, a, b, n=1)
         h = (b-a) / n
-        r = f(a) + f(b)
+        r = (f(a) + f(b))/2.0
         for i in 1:n-1
-            r += 2*f(i*h + a)
+            r += f(i*h + a)
         end
-        r * h / 2
+        r * h
     end
 
-    # vector integration for uniform points (in x)
-    function trapz{T<:Number}(y::Vector{T}, dx=one(T))
-        r = zero(zero(T)*zero(T))
-        for i in 2:length(y)
-            r += y[i] + y[i-1]
-        end
-        r * dx /2.0
+    # integration for uniform points (in x)
+    function trapz{T <: Number}(y::Vector{T}, dx=one(T))
+        r = zero(zero(eltype(y)) * zero(dx))
+        r = (y[1] + y[end])/2
+        r += sum(y[2:end-1])
+        r * dx
     end
 
-    # vector integration for non-uniform points (in x)
-    function trapz{T<:Number}(y::Vector{T}, x::Vector{T})
+    # integration for non-uniform points (in x)
+    function trapz{T <: Number}(y::Vector{T}, x::Vector{T})
         n = length(y)
         if n != length(x)
-            error("Vectors must be of same length")
+            error("Input x,y must be of same length")
         end
-        r = zero(zero(T)*zero(T))
+        r = zero(zero(eltype(x))*zero(eltype(y)))
         for i in 2:n
             r += (x[i] - x[i-1]) * (y[i] + y[i-1])
         end
         r/2.0
     end
 
-    # some simpson rules for more simple integration
+    # simpson rules for more simple integration
     function simps(f::Function, a, b, n=2)
         h = (b-a) / n
-        r = f(a) + f(b)
-        for i in 2:2:n-1
-            r += 2*f(i*h + a)
-        end
+        r = zero(zero(a)*zero(f(a)))
         for i in 1:2:n
-            r += 4*f(i*h + a)
+            r += f(i*h + a)
         end
-        r * h / 3
+        r *= 2
+        for i in 2:2:n-1
+            r += f(i*h + a)
+        end
+        r *= 2
+        r += f(a) + f(b)
+        r * h / 3.0
     end
 
-    function simps{T<:Number}(y::Vector{T}, dx=one(T))
-        n = length(y) 
-        if iseven(n) 
+    function simps{T <: Number}(y::Vector{T}, dx=one(T))
+        n = length(y)
+        if iseven(n)
             error("Simpson rule requires ODD length input (EVEN number of intervals)")
         end
-        r = zero(zero(T)*zero(T))
-        for i in 3:2:n
-            r += (y[i-2] + 4*y[i-1] + y[i])
+        r = zero(zero(eltype(y))*zero(dx))
+        for i in 2:2:n-1
+            r += y[i]
+        end
+        r *= 2.0
+        for i in 3:2:n-2
+            r += y[i]
+        end
+        r *= 2.0
+        for i in (1,n)
+            r += y[i]
         end
         r * dx / 3.0
     end
 
-    function simps{T<:Number}(y::Vector{T}, x::Vector{T})
+    function simps{T <: Number}(y::Vector{T}, x::Vector{T})
         n = length(y)
         if n != length(x)
-            error("Vectors must be of same length")
+            error("Input x,y must be of same length")
         end
-        if iseven(n) 
+        if iseven(n)
             error("Simpson rule requires ODD length input (EVEN number of intervals)")
         end
 
-        # this is a quick generalization of the simpson's rule for 
+        # this is a quick generalization of the simpson's rule for
         # arbitrary separations in x
-        r = zero(zero(T)*zero(T))
+        r = zero(zero(eltype(y))*zero(eltype(x)))
         for i in 3:2:n
-            d1 = x[i-1] - x[i-2] 
-            d2 = x[i] - x[i-1] 
+            d1 = x[i-1] - x[i-2]
+            d2 = x[i] - x[i-1]
             h = x[i] - x[i-2]
-            r += h * ( 
-                    y[i-2] * (2 - d2 / d1) + 
-                    y[i-1] * h * h / (d1 * d2) + 
+            r += h * (
+                    y[i-2] * (2 - d2 / d1) +
+                    y[i-1] * h * h / (d1 * d2) +
                     y[i] * (2 - d1 / d2)
                 )
         end
