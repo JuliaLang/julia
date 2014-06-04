e57c162ebdc8c2f11c06958734981b3d4678c8ad
Andy Hayden <andyhayden1@gmail.com>
2014-06-04 01:02:50 -0700

    use fileorlink

--- base/pkg/entry.jl
+++ base/pkg/entry.jl
@@ -150,7 +150,7 @@ function clone(url::String, pkg::String)
         Git.run(`clone -q $url $pkg`)
         Git.set_remote_url(url, dir=pkg)
     catch
-        run(`rm -rf $pkg`)
+        rmdir(pkg)
         rethrow()
     end
     isempty(Reqs.parse("$pkg/REQUIRE")) && return
