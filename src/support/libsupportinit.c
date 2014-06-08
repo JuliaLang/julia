6a07ad574d93833089ed5cb5b7b4b19c0b811062
Jeff Bezanson <jeff.bezanson@gmail.com>
2014-06-08 19:56:38 -0400

    set numeric locale to "C". helps #7167

--- src/support/libsupportinit.c
+++ src/support/libsupportinit.c
@@ -17,6 +17,7 @@ void libsupport_init(void)
 {
     if (!isInitialized) {
         setlocale(LC_ALL, ""); // set to user locale
+        setlocale(LC_NUMERIC, "C"); // use locale-independent numeric formats
 
         ios_init_stdstreams();
 
