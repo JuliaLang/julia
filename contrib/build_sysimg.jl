21cc5342ee4805fe2164323aed54c639acdd88b6
Elliot Saba <staticfloat@gmail.com>
2014-06-08 14:47:28 -0700

    use cd() do syntax in contrib/build_sysimg.jl

--- contrib/build_sysimg.jl
+++ contrib/build_sysimg.jl
@@ -11,41 +11,43 @@ function build_sysimg(sysimg_path; force=false, cpu_target="native")
     end
 
     # Enter base/ and setup some useful paths
-    cd(dirname(Base.find_source_file("sysimg.jl")))
-    julia = joinpath(JULIA_HOME, "julia")
-    julia_libdir = dirname(Sys.dlpath("libjulia"))
-    ld = find_system_linker()
+    base_dir = dirname(Base.find_source_file("sysimg.jl"))
+    cd(base_dir) do
+        julia = joinpath(JULIA_HOME, "julia")
+        julia_libdir = dirname(Sys.dlpath("libjulia"))
+        ld = find_system_linker()
 
-    # Ensure we have write-permissions to wherever we're trying to write to
-    if !success(`touch $sysimg_path.$(Sys.dlext)`)
-        error("$sysimg_path unwritable, ensure parent directory exists and is writable! (Do you need to run this with sudo?)")
+        # Ensure we have write-permissions to wherever we're trying to write to
+        if !success(`touch $sysimg_path.$(Sys.dlext)`)
+            error("$sysimg_path unwritable, ensure parent directory exists and is writable! (Do you need to run this with sudo?)")
+        end
+
+        # Start by building sys0.{ji,o}
+        sys0_path = joinpath(dirname(sysimg_path), "sys0")
+        println("Building sys0.o...")
+        println("$julia -C $cpu_target --build $sys0_path sysimg.jl")
+        run(`$julia -C $cpu_target --build $sys0_path sysimg.jl`)
+
+        # Bootstrap off of that to create sys.{ji,o}
+        println("Building sys.o...")
+        println("$julia -C $cpu_target --build $sysimg_path -J $sys0_path.ji -f sysimg.jl")
+        run(`$julia -C $cpu_target --build $sysimg_path -J $sys0_path.ji -f sysimg.jl`)
+
+        # Link sys.o into sys.$(dlext)
+        FLAGS = ["-L$julia_libdir"]
+        if OS_NAME == :Darwin
+            push!(FLAGS, "-dylib")
+            push!(FLAGS, "-undefined")
+            push!(FLAGS, "dynamic_lookup")
+        else
+            push!(FLAGS, "--unresolved-symbols")
+            push!(FLAGS, "ignore-all")
+        end
+        @windows_only append!(FLAGS, ["-L$JULIA_HOME", "-ljulia", "-lssp"])
+
+        println("Linking sys.$(Sys.dlext)")
+        run(`$ld $FLAGS -o $sysimg_path.$(Sys.dlext) $sysimg_path.o`)
     end
-
-    # Start by building sys0.{ji,o}
-    sys0_path = joinpath(dirname(sysimg_path), "sys0")
-    println("Building sys0.o...")
-    println("$julia -C $cpu_target --build $sys0_path sysimg.jl")
-    run(`$julia -C $cpu_target --build $sys0_path sysimg.jl`)
-
-    # Bootstrap off of that to create sys.{ji,o}
-    println("Building sys.o...")
-    println("$julia -C $cpu_target --build $sysimg_path -J $sys0_path.ji -f sysimg.jl")
-    run(`$julia -C $cpu_target --build $sysimg_path -J $sys0_path.ji -f sysimg.jl`)
-
-    # Link sys.o into sys.$(dlext)
-    FLAGS = ["-L$julia_libdir"]
-    if OS_NAME == :Darwin
-        push!(FLAGS, "-dylib")
-        push!(FLAGS, "-undefined")
-        push!(FLAGS, "dynamic_lookup")
-    else
-        push!(FLAGS, "--unresolved-symbols")
-        push!(FLAGS, "ignore-all")
-    end
-    @windows_only append!(FLAGS, ["-L$JULIA_HOME", "-ljulia", "-lssp"])
-
-    println("Linking sys.$(Sys.dlext)")
-    run(`$ld $FLAGS -o $sysimg_path.$(Sys.dlext) $sysimg_path.o`)
 end
 
 # Search for a linker to link sys.o into sys.dl_ext.  Honor LD environment variable, otherwise search for something we know works
