3ea4812786d63ea402c399d81732d431c3201d8d
Steve Kelly <kd2cca@gmail.com>
2014-06-09 00:46:38 -0400

    use mktempdir since it does the same thing

--- test/pkg.jl
+++ test/pkg.jl
@@ -1,6 +1,6 @@
 function temp_pkg_dir(fn::Function)
   # Used in tests below to setup and teardown a sandboxed package directory
-  const tmpdir = ENV["JULIA_PKGDIR"] = abspath(string("tmp.",randstring()))
+  const tmpdir = ENV["JULIA_PKGDIR"] = mktempdir()
   @test !isdir(Pkg.dir())
   try
     Pkg.init()
