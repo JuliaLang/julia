c3e219a8b031888b66dbfe07bdcec52717d1c3ba
Keno Fischer <kfischer@college.harvard.edu>
2014-06-11 15:33:04 -0400

    Small LLVM3.5 API change

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -3795,7 +3795,11 @@ static Function *emit_function(jl_lambda_info_t *lam, bool cstyle)
 static MDNode* tbaa_make_child( const char* name, MDNode* parent, bool isConstant=false )
 {
     MDNode* n = mbuilder->createTBAANode(name,parent,isConstant);
+#ifdef LLVM35
+    n->setValueName( ValueName::Create(name));
+#else
     n->setValueName( ValueName::Create(name, name+strlen(name)));
+#endif
     return n;
 }
 
