987eb73b2c3a20a64a3f5491716af786867deec6
Andreas Noack Jensen <andreasnoackjensen@gmail.com>
2014-06-17 21:24:39 +0200

    Add tests for transpose of block matrices (#7244)

--- test/arrayops.jl
+++ test/arrayops.jl
@@ -871,3 +871,9 @@ end
 
 # map with promotion (issue #6541)
 @test map(join, ["z", "я"]) == ["z", "я"]
+
+# Handle block matrices
+A = [randn(2,2) for i = 1:2, j = 1:2]
+@test issym(A.'A)
+A = [complex(randn(2,2), randn(2,2)) for i = 1:2, j = 1:2]
+@test ishermitian(A'A)
