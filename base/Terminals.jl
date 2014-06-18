8c5e198bdf0d9a52251b5cbab80d67abfaa21f94
Simon Kornblith <simon@simonster.com>
2014-06-17 21:32:29 -0400

    Move FakeTerminal to test/repl.jl

--- base/Terminals.jl
+++ base/Terminals.jl
@@ -106,20 +106,6 @@ type TerminalBuffer <: UnixTerminal
     out_stream::Base.IO
 end
 
-type FakeTerminal <: UnixTerminal
-    in_stream::Base.IO
-    out_stream::Base.IO
-    err_stream::Base.IO
-    hascolor::Bool
-    raw::Bool
-    FakeTerminal(stdin,stdout,stderr,hascolor=true) =
-        new(stdin,stdout,stderr,hascolor,false)
-end
-
-hascolor(t::FakeTerminal) = t.hascolor
-raw!(t::FakeTerminal, raw::Bool) = t.raw = raw
-size(t::FakeTerminal) = (24, 80)
-
 type TTYTerminal <: UnixTerminal
     term_type::ASCIIString
     in_stream::Base.TTY
