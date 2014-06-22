8f12e3541a53806f7f71659a1210223edb2c3d0b
Elliot Saba <staticfloat@gmail.com>
2014-06-21 23:59:13 -0700

    Don't use @timeit1, as that doesn't give us a standard deviation at all, even though it takes a long time.  The wait is worth it.

--- test/perf/perfutil.jl
+++ test/perf/perfutil.jl
@@ -39,6 +39,7 @@ function submit_to_codespeed(vals,name,desc,unit,test_group,lessisbetter=true)
 
     println( "$name: $(mean(vals))" )
     ret = post( "http://$codespeed_host/result/add/json/", {"json" => json([csdata])} )
+    println( json([csdata]) )
     if ret.http_code != 200 && ret.http_code != 202
         error("Error submitting $name [HTTP code $(ret.http_code)], dumping headers and text: $(ret.headers)\n$(bytestring(ret.body))\n\n")
         return false
