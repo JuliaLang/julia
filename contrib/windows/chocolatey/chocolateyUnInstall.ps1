3b70c1d965ecd6369772deb903384e5bf1bbacc0
David Anthoff <anthoff@berkeley.edu>
2014-06-24 10:53:09 -0700

    Add error handling to chocolatey package

--- contrib/windows/chocolatey/chocolateyUnInstall.ps1
+++ contrib/windows/chocolatey/chocolateyUnInstall.ps1
@@ -1,5 +1,18 @@
-$juliapath = (Join-Path (Get-BinRoot) "julia")
-$juliaexepath = (Join-Path (Join-Path $juliapath "bin") "julia.exe")
+try
+{
+    $juliapath = (Join-Path (Get-BinRoot) "julia")
+    $juliaexepath = (Join-Path (Join-Path $juliapath "bin") "julia.exe")
+    $startmenufolder = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::StartMenu)
+    $shortcutfilename = (Join-Path $startmenufolder "Julia.lnk")
 
-Remove-BinFile 'julia' $juliaexepath
-Remove-Item $juliapath -Force -Recurse
+    Remove-Item $shortcutfilename -Force -ErrorAction SilentlyContinue
+    Remove-BinFile 'julia' $juliaexepath
+    Remove-Item $juliapath -Force -Recurse
+
+    Write-ChocolateySuccess 'julia'
+}
+catch
+{
+    Write-ChocolateyFailure 'julia' $($_.Exception.Message)
+    throw
+}
