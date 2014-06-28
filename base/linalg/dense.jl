26e7b87709a0c589a6d32cedc63198e9fa34e912
mschauer <moritzschauer@web.de>
2014-06-28 02:09:33 +0200

    Avoid explicit transposes

--- base/linalg/dense.jl
+++ base/linalg/dense.jl
@@ -463,11 +463,11 @@ end
 # AX + XB + C = 0
 function sylvester{T<:BlasFloat}(A::StridedMatrix{T},B::StridedMatrix{T},C::StridedMatrix{T})
     RA, QA = schur(A)
-    RB, QB = schur(B')
+    RB, QB = schur(B)
 
-    D = -QA'*C*QB
-    Y, scale = LAPACK.trsyl!('N', T <: Complex ? 'C' : 'T', RA, RB, D)
-    scale!(QA*Y*QB', inv(scale))
+    D = -Ac_mul_B(QA,C*QB)
+    Y, scale = LAPACK.trsyl!('N','N', RA, RB, D)
+    scale!(QA*A_mul_Bc(Y,QB), inv(scale))
 end
 sylvester{T<:Integer}(A::StridedMatrix{T},B::StridedMatrix{T},C::StridedMatrix{T}) = sylvester(float(A), float(B), float(C))
 
@@ -476,9 +476,9 @@ function lyap{T<:BlasFloat}(A::StridedMatrix{T},C::StridedMatrix{T})
     chksquare(A, C)
     R, Q = schur(A)
 
-    D = -Q'*C*Q
+    D = -Ac_mul_B(Q,C*Q)
     Y, scale = LAPACK.trsyl!('N', T <: Complex ? 'C' : 'T', R, R, D)
-    scale!(Q*Y*Q', inv(scale))
+    scale!(Q*A_mul_Bc(Y,Q), inv(scale))
 end
 lyap{T<:Integer}(A::StridedMatrix{T},C::StridedMatrix{T}) = lyap(float(A), float(C))
 lyap{T<:Number}(a::T, c::T) = -c/(2a)
