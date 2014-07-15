5021c985aab7804248e3213c0cd8bca67070258d
Jiahao Chen <jiahao@mit.edu>
2014-07-14 20:52:01 -0700

    Add naive pmap benchmark for small matrix expms

---
+++ test/perf/jiahaobench/expm_pmap.jl
@@ -0,0 +1,12 @@
+#Simple parallel expm using pmap
+t = 10^5          #Number of trials
+@everywhere n = 3 #Size of matrices
+Ms = pmap(_->randn(n,n), 1:t)
+
+#Precompile
+expm(randn(n,n))
+map(identity, [randn(n,n)])
+pmap(identity, [randn(n,n)])
+
+@time expMs_ser = map(expm, Ms)
+@time expMs_par = pmap(expm, Ms)
