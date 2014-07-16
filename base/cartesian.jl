470b9d849474e5643912cc4bc62b509e1b810f86
Jutho Haegeman <jutho.haegeman@ugent.be>
2014-07-16 17:37:10 +0200

    restored default argument dims=1:CARTESIAN_DIMS

--- base/cartesian.jl
+++ base/cartesian.jl
@@ -97,7 +97,7 @@ generate1(itersym::Symbol, prototype, bodyfunc, N::Int, varname, T) =
 generate1{K}(itersyms::NTuple{K,Symbol}, prototype, bodyfunc, itervals::NTuple{K,Int}) =
      Expr(:function, spliceint!(sreplace!(copy(prototype), itersyms, itervals)), bodyfunc(itervals))
 
-function ngenerate(itersym::Symbol, returntypeexpr, prototype, bodyfunc, dims, makecached::Bool = true)
+function ngenerate(itersym::Symbol, returntypeexpr, prototype, bodyfunc, dims=1:CARTESIAN_DIMS, makecached::Bool = true)
     varname, T = get_splatinfo(prototype, itersym)
     # Generate versions for specific dimensions
     fdim = [generate1(itersym, prototype, bodyfunc, N, varname, T) for N in dims]
