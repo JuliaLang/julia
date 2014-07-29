f044bdab4633790145377462ad5a2c0c217091a5
Jeff Bezanson <jeff.bezanson@gmail.com>
2014-07-28 22:54:07 -0400

    fix #7764, endof and next for RepString

--- base/string.jl
+++ base/string.jl
@@ -679,15 +679,27 @@ immutable RepString <: String
     repeat::Integer
 end
 
-endof(s::RepString)  = endof(s.string)*s.repeat
+function endof(s::RepString)
+    e = endof(s.string)
+    (next(s.string,e)[2]-1) * (s.repeat-1) + e
+end
 length(s::RepString) = length(s.string)*s.repeat
 sizeof(s::RepString) = sizeof(s.string)*s.repeat
 
 function next(s::RepString, i::Int)
-    if i < 1 || i > endof(s)
-        error(BoundsError)
+    if i < 1
+        throw(BoundsError())
     end
-    j = mod1(i,length(s.string))
+    e = endof(s.string)
+    sz = next(s.string,e)[2]-1
+
+    r, j = divrem(i-1, sz)
+    j += 1
+
+    if r >= s.repeat || j > e
+        throw(BoundsError())
+    end
+
     c, k = next(s.string, j)
     c, k-j+i
 end
--- test/strings.jl
+++ test/strings.jl
@@ -1079,3 +1079,21 @@ let s = "|η(α)-ϕ(κ)| < ε"
     @test length(SubString(s,1,7))==length(s[1:7])
     @test length(SubString(s,4,11))==length(s[4:11])
 end
+
+# issue #7764
+let
+    srep = RepString("Σβ",2)
+    s="Σβ"
+    ss=SubString(s,1,endof(s))
+
+    @test ss^2 == "ΣβΣβ"
+    @test RepString(ss,2) == "ΣβΣβ"
+
+    @test endof(srep) == 7
+
+    @test next(srep, 3) == ('β',5)
+    @test next(srep, 7) == ('β',9)
+
+    @test srep[7] == 'β'
+    @test_throws BoundsError srep[8]
+end
