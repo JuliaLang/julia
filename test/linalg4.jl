445a7d3f6bd500696fd6097fd9d0d00ca4d7a927
Tony Kelman <tony@kelman.net>
2014-08-09 04:45:50 -0700

    fix #7933, full(Symmetric(A)) mutates A

--- test/linalg4.jl
+++ test/linalg4.jl
@@ -335,3 +335,9 @@ end
 x, r = LAPACK.gelsy!([0 1; 0 2; 0 3.], [2, 4, 6.])
 @test_approx_eq x [0,2]
 @test r == 1
+
+# Issue #7933
+A7933 = [1 2; 3 4]
+B7933 = copy(A7933)
+C7933 = full(Symmetric(A7933))
+@test A7933 == B7933
