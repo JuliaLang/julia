e03700a8b2b4b7afc3b33735c966c0227d688981
Andreas Noack Jensen <andreasnoackjensen@gmail.com>
2014-08-19 16:22:31 -0400

    Check arguments in wrapper for xheevr. Add tests for errors reported in #8057 and #8058

--- base/linalg/lapack.jl
+++ base/linalg/lapack.jl
@@ -2985,6 +2985,12 @@ for (syev, syevr, sygvd, elty, relty) in
         function syevr!(jobz::BlasChar, range::BlasChar, uplo::BlasChar, A::StridedMatrix{$elty}, vl::FloatingPoint, vu::FloatingPoint, il::Integer, iu::Integer, abstol::FloatingPoint)
             chkstride1(A)
             n = chksquare(A)
+            if range == 'I'
+                1 <= il <= iu <= n || throw(ArgumentError("illegal choise of eigenvalue indices"))
+            end
+            if range == 'V' 
+                vl < vu || throw(ArgumentError("lower boundary must be less than upper boundary"))
+            end
             lda = max(1,stride(A,2))
             m = Array(BlasInt, 1)
             w = similar(A, $relty, n)
