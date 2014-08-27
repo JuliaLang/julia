231d3e30990145ca10f001739404a3bd576a97fc
Jacob Quinn <quinn.jacobd@gmail.com>
2014-08-26 20:59:31 -0400

    Export nowutc() from Dates module

--- base/Dates.jl
+++ base/Dates.jl
@@ -26,7 +26,7 @@ export Period, DatePeriod, TimePeriod,
        July, August, September, October, November, December,
        Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec,
        # conversions.jl
-       unix2datetime, datetime2unix, now, today, 
+       unix2datetime, datetime2unix, now, nowutc, today, 
        rata2datetime, datetime2rata, julian2datetime, datetime2julian,
        # adjusters.jl
        firstdayofweek, lastdayofweek,
