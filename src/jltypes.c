b429303dd6824299801fcd6ded7a23c12794d72a
Jeff Bezanson <jeff.bezanson@gmail.com>
2014-08-29 12:57:58 -0400

    allow all bits values as type parameters. closes #6081

--- src/jltypes.c
+++ src/jltypes.c
@@ -1590,7 +1590,7 @@ static int valid_type_param(jl_value_t *v)
     }
     else {
         // TODO: maybe more things
-        return jl_is_type(v) || jl_is_long(v) || jl_is_symbol(v) || jl_is_typevar(v) || jl_is_bool(v);
+        return jl_is_type(v) || jl_is_typevar(v) || jl_is_symbol(v) || jl_isbits(jl_typeof(v));
     }
 }
 
