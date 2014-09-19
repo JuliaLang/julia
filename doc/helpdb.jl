9fa2c991a11768166b5dec7b2e7e11f375db4301
Erik Schnetter <schnetter@gmail.com>
2014-09-19 14:24:47 -0400

    Correct signbit documentation

--- doc/helpdb.jl
+++ doc/helpdb.jl
@@ -4516,8 +4516,8 @@ popdisplay(d::Display)
 
 ("Base","signbit","signbit(x)
 
-   Returns \"1\" if the value of the sign of \"x\" is negative,
-   otherwise \"0\".
+   Returns \"true\" if the value of the sign of \"x\" is negative,
+   otherwise \"false\".
 
 "),
 
