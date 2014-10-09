cc89285a9391f029582827d07ec36fc9f1566ed2
Simon Kornblith <simon@simonster.com>
2014-10-09 16:08:08 -0400

    @garborg's updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,14 @@
 using Compat
 using Base.Test
 
+v = 1
+
 d = Dict{Int,Int}()
 d[1] = 1
 @test Compat.@Dict(1 => 1) == d
+@test Compat.@Dict(1 => v) == d
 
 ad = Dict{Any,Any}()
 ad[1] = 1
 @test Compat.@AnyDict(1 => 1) == ad
+@test Compat.@AnyDict(1 => v) == ad
