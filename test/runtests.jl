fe5df00664e81427295a9abbda7c1f244813631a
Simon Kornblith <simon@simonster.com>
2014-10-15 18:08:45 -0400

    Add ＠compat as discussed in #8

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,35 @@ d[1] = 1
 @test Compat.@Dict(1 => 1) == d
 @test Compat.@Dict(1 => v) == d
 
+@test typeof(@compat(Dict(1 => 1))) == Dict{Int,Int}
+@test @compat(Dict(1 => 1)) == d
+@test @compat(Dict(1 => v)) == d
+
 ad = Dict{Any,Any}()
 ad[1] = 1
 @test Compat.@AnyDict(1 => 1) == ad
 @test Compat.@AnyDict(1 => v) == ad
+
+@test typeof(@compat(Dict{Any,Any}(1 => 1))) == Dict{Any,Any}
+@test @compat(Dict{Any,Any}(1 => 1)) == ad
+@test @compat(Dict{Any,Any}(1 => v)) == ad
+
+td = Dict{Int,Float64}()
+td[1] = 1.0
+
+@test typeof(@compat(Dict{Int,Float64}(1 => 1))) == Dict{Int,Float64}
+@test @compat(Dict{Int,Float64}(1 => 1)) == td
+@test @compat(Dict{Int,Float64}(1 => v)) == td
+
+@test @compat(Dict()) == Dict()
+@test @compat(Dict{Any,Any}()) == Dict{Any,Any}()
+@test @compat(Dict([(1, 1)])) == Dict([(1, 1)])
+
+@compat function f()
+	a = :a
+	b = :b
+	c = :c
+	d = :d
+	Dict(a => b, c => d)
+end
+@test f() == Dict([(:a, :b), (:c, :d)])
