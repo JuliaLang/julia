e3c0611579aab060d8b2163672e32b94eb9e6b40
Jeff Bezanson <jeff.bezanson@gmail.com>
2014-10-20 00:59:26 -0400

    slightly cut down lowered code for kwcall

--- src/julia-syntax.scm
+++ src/julia-syntax.scm
@@ -1514,10 +1514,13 @@
 			      `(block (= (tuple ,k ,v) ,rk)
 				      ,push-expr))))
 		      restkeys))
-	     (if (call (top isempty) ,container)
-		 (call ,f ,@pa)
-		 (call (top kwcall) call ,(length keys) ,@keyargs
-		       ,f ,container ,@pa))))))))
+	     ,(let ((kw-call `(call (top kwcall) call ,(length keys) ,@keyargs
+				    ,f ,container ,@pa)))
+		(if (not (null? keys))
+		    kw-call
+		    `(if (call (top isempty) ,container)
+			 (call ,f ,@pa)
+			 ,kw-call)))))))))
 
 (define (expand-transposed-op e ops)
   (let ((a (caddr e))
