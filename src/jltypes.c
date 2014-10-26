da878b3b891e7c350345b74bdec98f1711598ee4
Oscar Blumberg <oscar.blumberg@ens.fr>
2014-10-26 21:04:07 +0100

    Root a forgotten tuple
    (cherry picked from commit 0840a0e2a177e3a9f793535988d8295d89f1af4f)
    (cherry picked from commit 75cff0337de1aa287671cf72633e92b0a5f63035)
    (cherry picked from commit 36e11eda1aac918f76711e65e16ae70b08e9ce55)
    
    Backport of #8819

--- src/jltypes.c
+++ src/jltypes.c
@@ -245,7 +245,7 @@ jl_tuple_t *jl_compute_type_union(jl_tuple_t *types)
 {
     size_t n = count_union_components(types);
     jl_value_t **temp;
-    JL_GC_PUSHARGS(temp, n);
+    JL_GC_PUSHARGS(temp, n+1);
     size_t idx=0;
     flatten_type_union(types, temp, &idx);
     assert(idx == n);
@@ -267,6 +267,7 @@ jl_tuple_t *jl_compute_type_union(jl_tuple_t *types)
         }
     }
     jl_tuple_t *result = jl_alloc_tuple_uninit(n - ndel);
+    temp[n] = (jl_value_t*)result; // root result tuple while sorting
     j=0;
     for(i=0; i < n; i++) {
         if (temp[i] != NULL) {
