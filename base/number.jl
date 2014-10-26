3ad21b0d1411e1941a9908f36c6f3849886e411a
jake bolewski <jakebolewski@gmail.com>
2014-10-25 22:44:39 -0400

    fix bug for getindex(::Number, ::Real)
    (cherry picked from commit b718cbc72e90e28f8df514e1a320fc2eb4f9279c)

--- base/number.jl
+++ base/number.jl
@@ -14,7 +14,7 @@ endof(x::Number) = 1
 getindex(x::Number) = x
 getindex(x::Number, i::Integer) = i == 1 ? x : throw(BoundsError())
 getindex(x::Number, I::Integer...) = all([i == 1 for i in I]) ? x : throw(BoundsError())
-getindex(x::Number, I::Real...) = getindex(x, to_index(i)...)
+getindex(x::Number, I::Real...) = getindex(x, to_index(I)...)
 first(x::Number) = x
 last(x::Number) = x
 
