a39d6eb6b18f0258f66029aa24101318379108d0
Stefan Karpinski <stefan@karpinski.org>
2014-11-01 17:56:10 -0400

    wip: Str (Bytes-based string type)

--- base/bytes.jl
+++ base/bytes.jl
@@ -37,3 +37,49 @@ function cmp(x::Bytes, y::Bytes)
 end
 
 isless(x::Bytes, y::Bytes) = cmp(x, y) < 0
+
+## Bytes-based string type ##
+
+immutable Str <: AbstractString
+    data::Bytes
+end
+Str(s::AbstractString) = Str(Bytes(s))
+
+function endof(s::Str)
+    d = s.data
+    i = length(d)
+    i == 0 && return i
+    while !is_utf8_start(d[i])
+        i -= 1
+    end
+    i
+end
+
+function length(s::Str)
+    d = s.data
+    n = 0
+    for i = 1:length(s.data)
+        n += is_utf8_start(d[i])
+    end
+    return n
+end
+
+function next(s::Str, i::Int)
+    d = s.data
+    a::Uint32 = d[i]
+    a < 0x80 && return Char(a), i+1
+    # is_utf8_start(a) || error("invalid UTF-8 character index")
+    b::Uint32 = a << 6 + d[i+1]
+    a < 0xe0 && return Char(b - 0x00003080), i+2
+    c::Uint32 = b << 6 + d[i+2]
+    a < 0xf0 && return Char(c - 0x000e2080), i+3
+    return Char(c << 6 + d[i+3] - 0x03c82080), i+4
+end
+
+## overload methods for efficiency ##
+
+sizeof(s::Str) = length(s.data)
+
+isless(x::Str, y::Str) = isless(x.data, y.data)
+   cmp(x::Str, y::Str) =    cmp(x.data, y.data)
+
