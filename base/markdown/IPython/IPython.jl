0086b523502c2a4055c6043b47cc8bbf3d621389
Mike Innes <mike.j.innes@gmail.com>
2014-11-12 00:38:44 +0000

    fix a whoopsie

--- base/markdown/IPython/IPython.jl
+++ base/markdown/IPython/IPython.jl
@@ -23,5 +23,5 @@ end
 writemime(io::IO, ::MIME"text/plain", tex::LaTeX) =
   print(io, '$', tex.formula, '$')
 
-term(io::IO, tex::LaTeX, cols) = print_with_format(:magenta, io, tex.formula)
-terminline(io::IO, tex::LaTeX) = println_with_format(:magenta, io, tex.formula)
+term(io::IO, tex::LaTeX, cols) = println_with_format(:magenta, io, tex.formula)
+terminline(io::IO, tex::LaTeX) = print_with_format(:magenta, io, tex.formula)
