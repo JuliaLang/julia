JuliaLang/julia

commit adca441118137f48bd751dfafdc94280b54b2ac6

Fixes space completion in paths in the REPL like: cd C:\\Program\ Files\\

base/REPLCompletions.jl
@@ -130,7 +130,8 @@ function complete_path(path::AbstractString, pos)
             push!(matches, id ? file * (@windows? "\\\\" : "/") : file)
         end
     end
-    matches, (nextind(path, pos-sizeof(prefix))):pos, length(matches) > 0
+    matches = UTF8String[replace(s, r"\s", "\\ ") for s in matches]
+    return matches, nextind(path, pos - sizeof(prefix) - length(matchall(r" ", prefix))):pos, length(matches) > 0
 end
 
 function complete_methods(input::AbstractString)
@@ -148,7 +149,6 @@ end
 include("latex_symbols.jl")
 
 const non_identifier_chars = [" \t\n\r\"\\'`\$><=:;|&{}()[],+-*/?%^~"...]
-const non_filename_chars = [(" \t\n\r\"'`@\$><=;|&{(" * (@unix?"\\" : "")) ...]
 const whitespace_chars = [" \t\n\r"...]
 
 # Aux function to detect whether we're right after a
@@ -187,12 +187,13 @@ function completions(string, pos)
     partial = string[1:pos]
     inc_tag = Base.incomplete_tag(parse(partial , raise=false))
     if inc_tag in [:cmd, :string]
-        startpos = nextind(partial, rsearch(partial, non_filename_chars, pos))
+        m = match(r"[\t\n\r\"'`@\$><=;|&\{]| (?!\\)",reverse(partial))
+        startpos = length(partial)-(m == nothing ? 1 : m.offset) + 2
         r = startpos:pos
-        paths, r, success = complete_path(string[r], pos)
+        paths, r, success = complete_path(unescape_string(string[r]), pos)
         if inc_tag == :string &&
            length(paths) == 1 &&                              # Only close if there's a single choice,
-           !isdir(string[startpos:start(r)-1] * paths[1]) &&  # except if it's a directory
+           !isdir(unescape_string(string[startpos:start(r)-1] * paths[1])) &&  # except if it's a directory
            (length(string) <= pos || string[pos+1] != '"')    # or there's already a " at the cursor.
             paths[1] *= "\""
         end

test/replcompletions.jl
@@ -164,6 +164,28 @@ c,r = test_scomplete("\$a")
     @test s[r] == "Pk"
 end
 
+let #test that it can auto complete with spaces in file/path
+    path = tempdir()
+    space_folder = randstring() * " r"
+    dir = joinpath(path, space_folder)
+    dir_space = replace(space_folder, " ", "\\ ")
+    mkdir(dir)
+    cd(path) do
+        open(joinpath(space_folder, "space .file"),"w") do f
+            s = @windows? "rm $dir_space\\\\space" : "cd $dir_space/space"
+            c,r = test_scomplete(s)
+            @test r == endof(s)-4:endof(s)
+            @test "space\\ .file" in c
+
+            s = @windows? "cd(\"$dir_space\\\\space" : "cd(\"$dir_space/space"
+            c,r = test_complete(s)
+            @test r == endof(s)-4:endof(s)
+            @test "space\\ .file\"" in c
+        end
+    end
+    rm(dir, recursive=true)
+end
+
 @windows_only begin
     tmp = tempname()
     path = dirname(tmp)

base/string.jl
@@ -1075,7 +1075,24 @@ macro mstr(s...); triplequoted(s...); end
 ## shell-like command parsing ##
 
 function shell_parse(raw::AbstractString, interp::Bool)
-    s = strip(raw)
+    s = lstrip(raw)
+    #Strips the end but respects the space when the string endswith "\\ "
+    r = RevString(s)
+    i = start(r)
+    c_old = nothing
+    while !done(r,i)
+        c, j = next(r,i)
+        if c == '\\' && c_old == ' '
+            i -= 1
+            break
+        elseif !(c in _default_delims)
+            break
+        end
+        i = j
+        c_old = c
+    end
+    s = s[1:end-i+1]
+
     last_parse = 0:-1
     isempty(s) && return interp ? (Expr(:tuple,:()),last_parse) : ([],last_parse)
 

commit 6e0e9d638b9f1acee714c25812662f04a188a6fe

Fix error regarding unescape_string introduced by commit adca441118137f48bd751dfafdc94280b54b2ac6 and introduced some visual spaces.

base/REPLCompletions.jl
@@ -187,13 +187,13 @@ function completions(string, pos)
     partial = string[1:pos]
     inc_tag = Base.incomplete_tag(parse(partial , raise=false))
     if inc_tag in [:cmd, :string]
-        m = match(r"[\t\n\r\"'`@\$><=;|&\{]| (?!\\)",reverse(partial))
-        startpos = length(partial)-(m == nothing ? 1 : m.offset) + 2
+        m = match(r"[\t\n\r\"'`@\$><=;|&\{]| (?!\\)", reverse(partial))
+        startpos = length(partial) - (m == nothing ? 1 : m.offset) + 2
         r = startpos:pos
-        paths, r, success = complete_path(unescape_string(string[r]), pos)
+        paths, r, success = complete_path(replace(string[r], r"\\ ", " "), pos)
         if inc_tag == :string &&
            length(paths) == 1 &&                              # Only close if there's a single choice,
-           !isdir(unescape_string(string[startpos:start(r)-1] * paths[1])) &&  # except if it's a directory
+           !isdir(replace(string[startpos:start(r)-1] * paths[1], r"\\ ", " ")) &&  # except if it's a directory
            (length(string) <= pos || string[pos+1] != '"')    # or there's already a " at the cursor.
             paths[1] *= "\""
         end

test/replcompletions.jl
@@ -80,6 +80,10 @@ c,r,res = test_complete(s)
 @test r == 7:12
 @test length(c) == 1
 
+# `cd("C:\U should not make the repl crash due to escaping see comment #9137
+s = "cd(\"C:\\U"
+c,r,res = test_complete(s)
+
 ## Test completion of packages
 #mkp(p) = ((@assert !isdir(p)); mkdir(p))
 #temp_pkg_dir() do