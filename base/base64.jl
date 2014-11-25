9d4d0c35a7df02a605a379a7e49c4ba661fb24e0
amartinez <amartinez@daedalus.es>
2014-11-25 18:22:58 +0100

    adds base64 decoding (fixes #5656)

--- base/base64.jl
+++ base/base64.jl
@@ -1,30 +1,32 @@
 module Base64
-import Base: read, write, close
-export Base64Pipe, base64
+import Base: read, write, close, eof
+export Base64Pipe, Base64EncodePipe, Base64DecodePipe, base64, base64encode, base64decode
 
-# Base64Pipe is a pipe-like IO object, which converts writes (and
-# someday reads?) into base64 encoded (decoded) data send to a stream.
-# (You must close the pipe to complete the encode, separate from
-# closing the target stream).  We also have a function base64(f,
+# Base64EncodePipe is a pipe-like IO object, which converts into base64 data sent 
+# to a stream. (You must close the pipe to complete the encode, separate from
+# closing the target stream).  We also have a function base64encode(f,
 # args...) which works like sprint except that it produces
-# base64-encoded data, along with base64(args...)  which is equivalent
-# to base64(write, args...), to return base64 strings.
-
+# base64-encoded data, along with base64encode(args...)  which is equivalent
+# to base64encode(write, args...), to return base64 strings.
+# A Base64DecodePipe object can be used to decode base64-encoded data read from a stream
+# , while function base64decode is useful for decoding strings
 #############################################################################
 
-type Base64Pipe <: IO
+type Base64EncodePipe <: IO
     io::IO
     # writing works in groups of 3, so we need to cache last two bytes written
     b0::UInt8
     b1::UInt8
     nb::UInt8 # number of bytes in cache: 0, 1, or 2
 
-    function Base64Pipe(io::IO)
+    function Base64EncodePipe(io::IO)
         b = new(io,0,0,0)
         finalizer(b, close)
         return b
     end
 end
+
+Base64Pipe = Base64EncodePipe
 
 #############################################################################
 
@@ -32,6 +34,14 @@ end
 
 const b64chars = ['A':'Z','a':'z','0':'9','+','/']
 
+const revb64chars = Dict('A'=> 0, 'B'=> 1, 'C'=> 2, 'D'=> 3, 'E'=> 4, 'F'=> 5, 'G'=> 6, 'H'=> 7, 'I'=> 8, 'J'=> 9, 
+                         'K'=> 10, 'L'=> 11, 'M'=> 12, 'N'=> 13, 'O'=> 14, 'P'=> 15, 'Q'=> 16, 'R'=> 17, 'S'=> 18, 'T'=> 19, 
+                         'U'=> 20, 'V'=> 21, 'W'=> 22, 'X'=> 23, 'Y'=> 24, 'Z'=> 25, 'a'=> 26, 'b'=> 27, 'c'=> 28, 'd'=> 29, 
+                         'e'=> 30, 'f'=> 31, 'g'=> 32, 'h'=> 33, 'i'=> 34, 'j'=> 35, 'k'=> 36, 'l'=> 37, 'm'=> 38, 'n'=> 39, 
+                         'o'=> 40, 'p'=> 41, 'q'=> 42, 'r'=> 43, 's'=> 44, 't'=> 45, 'u'=> 46, 'v'=> 47, 'w'=> 48, 'x'=> 49, 
+                         'y'=> 50, 'z'=> 51, '0'=> 52, '1'=> 53, '2'=> 54, '3'=> 55, '4'=> 56, '5'=> 57, '6'=> 58, '7'=> 59,
+                         '8'=> 60, '9'=> 61, '+'=> 62, '/'=> 63)
+                         
 function b64(x::UInt8, y::UInt8, z::UInt8)
     n = int(x)<<16 | int(y)<<8 | int(z)
     b64chars[(n >> 18) + 1],
@@ -50,9 +60,30 @@ function b64(x::UInt8)
     a, b, '=', '='
 end
 
+function b64decode(encvec::Vector{UInt8})
+    if length(encvec) < 2 
+        error("Incorrect base64 format")
+    end
+    n = revb64chars[encvec[1]] << 18 | revb64chars[encvec[2]] << 12
+    if length(encvec) > 2
+        n =  n | revb64chars[encvec[3]] << 6
+    end
+    if length(encvec) > 3
+        n = n | revb64chars[encvec[4]]
+    end
+    decvec = [uint8(n >> 16)]
+    if length(encvec) > 2
+        push!(decvec, uint8(n >> 8 & 0b11111111))
+    end
+    if length(encvec) > 3
+        push!(decvec, uint8(n & 0b11111111))
+    end
+    decvec
+end
+
 #############################################################################
 
-function write(b::Base64Pipe, x::AbstractVector{UInt8})
+function write(b::Base64EncodePipe, x::AbstractVector{UInt8})
     n = length(x)
     s = 1 # starting index
     # finish any cached data to write:
@@ -93,7 +124,7 @@ function write(b::Base64Pipe, x::AbstractVector{UInt8})
     end
 end
 
-function write(b::Base64Pipe, x::UInt8)
+function write(b::Base64EncodePipe, x::UInt8)
     if b.nb == 0
         b.b0 = x
         b.nb = 1
@@ -106,7 +137,7 @@ function write(b::Base64Pipe, x::UInt8)
     end
 end
 
-function close(b::Base64Pipe)
+function close(b::Base64EncodePipe)
     if b.nb > 0
         # write leftover bytes + padding
         if b.nb == 1
@@ -119,19 +150,66 @@ function close(b::Base64Pipe)
 end
 
 # like sprint, but returns base64 string
-function base64(f::Function, args...)
+function base64encode(f::Function, args...)
     s = IOBuffer()
-    b = Base64Pipe(s)
+    b = Base64EncodePipe(s)
     f(b, args...)
     close(b)
     takebuf_string(s)
 end
-base64(x...) = base64(write, x...)
+base64encode(x...) = base64encode(write, x...)
+
+base64 = base64encode
 
 #############################################################################
 
 # read(b::Base64Pipe, ::Type{UInt8}) = # TODO: decode base64
 
 #############################################################################
+
+type Base64DecodePipe <: IO
+    io::IO
+    # reading works in blocks of 4 characters that are decoded into 3 bytes and 2 of them cached
+    cache::Vector{UInt8}
+
+    function Base64DecodePipe(io::IO)
+        b = new(io,[])
+        finalizer(b, close)
+        return b
+    end
+end
+
+function read(b::Base64DecodePipe, t::Type{UInt8})
+    if length(b.cache) > 0
+        val = shift!(b.cache)
+    else
+        encvec = Array(UInt8, 0)
+        while !eof(b.io) && length(encvec) < 4
+            c = read(b.io, t)
+            if haskey(revb64chars, c)
+                push!(encvec, c)
+            end
+        end
+        decvec = b64decode(encvec)
+        val = decvec[1]
+        b.cache = decvec[2:end]
+    end
+    val
+end
+
+function eof(b::Base64DecodePipe)
+    return length(b.cache) == 0 && eof(b.io)
+end
+
+function close(b::Base64DecodePipe)
+end
+
+# Decodes a base64-encoded string
+function base64decode(s)
+    b = IOBuffer(s)
+    decoded = readall(Base64DecodePipe(b))
+    close(b)
+    decoded
+end
 
 end # module
