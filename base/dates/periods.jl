ee0378f6c78707dfefcdcc1e83c666ae34482733
Steven G. Johnson <stevenj@alum.mit.edu>
2014-12-02 01:28:54 -0500

    fix #9216: more consistent units for Period arithmetic

--- base/dates/periods.jl
+++ base/dates/periods.jl
@@ -17,7 +17,7 @@ end
 # Now we're safe to define Period-Number conversions
 # Anything an Int64 can convert to, a Period can convert to
 Base.convert{T<:Number}(::Type{T},x::Period) = convert(T,value(x))
-Base.convert{T<:Period}(::Type{T},x::Real) = T(int64(x))
+Base.convert{T<:Period}(::Type{T},x::Real) = T(x)
 
 #Print/show/traits
 Base.string{P<:Period}(x::P) = string(value(x),_units(x))
@@ -33,29 +33,46 @@ default{T<:TimePeriod}(p::Union(T,Type{T})) = zero(p)
 
 (-){P<:Period}(x::P) = P(-value(x))
 Base.isless{P<:Period}(x::P,y::P) = isless(value(x),value(y))
-Base.isless{R<:Real}(x::Period,y::R) = isless(int64(y),value(x))
-Base.isless{R<:Real}(y::R,x::Period) = isless(int64(y),value(x))
-=={P<:Period}(x::P,y::P) = ===(value(x),value(y))
-=={R<:Real}(x::Period,y::R) = ==(int64(y),value(x))
-=={R<:Real}(y::R,x::Period) = ==(int64(y),value(x))
+=={P<:Period}(x::P,y::P) = value(x) == value(y)
 
-#Period Arithmetic:
-import Base.div, Base.mod, Base.gcd, Base.lcm
-let vec_ops = [:.+,:.-,:.*,:.%,:div]
-    for op in [:+,:-,:*,:%,:mod,:gcd,:lcm,vec_ops]
-        @eval begin
-        #Period-Period
-        ($op){P<:Period}(x::P,y::P) = P(($op)(value(x),value(y)))
-        #Period-Real
-        ($op){P<:Period}(x::P,y::Real) = P(($op)(value(x),convert(Int64,y)))
-        ($op){P<:Period}(x::Real,y::P) = P(($op)(convert(Int64,x),value(y)))
-        end
-        #Vectorized
-        if op in vec_ops
-            @eval begin
-            ($op){P<:Period}(x::AbstractArray{P}, y::P) = reshape([$op(i,y) for i in x], size(x))
-            ($op){P<:Period}(y::P, x::AbstractArray{P}) = reshape([$op(i,y) for i in x], size(x))
+# Period Arithmetic, grouped by dimensionality:
+import Base: div, mod, rem, gcd, lcm, +, -, *, /, %, .+, .-, .*, .%
+for op in (:+,:-,:lcm,:gcd)
+    @eval ($op){P<:Period}(x::P,y::P) = P(($op)(value(x),value(y)))
+end
+for op in (:.+, :.-)
+    op_ = symbol(string(op)[2:end])
+    @eval begin
+        ($op){P<:Period}(x::P,Y::StridedArray{P}) = ($op)(Y,x)
+        ($op_){P<:Period}(x::P,Y::StridedArray{P}) = ($op)(Y,x)
+        ($op_){P<:Period}(Y::StridedArray{P},x::P) = ($op)(Y,x)
+    end
+end
+for op in (:/,:%,:div,:mod)
+    @eval begin
+        ($op){P<:Period}(x::P,y::P) = ($op)(value(x),value(y))
+        ($op){P<:Period}(x::P,y::Real) = P(($op)(value(x),Int64(y)))
+    end
+end
+/{P<:Period}(X::StridedArray{P}, y::P) = X ./ y
+%{P<:Period}(X::StridedArray{P}, y::P) = X .% y
+*{P<:Period}(x::P,y::Real) = P(value(x) * Int64(y))
+*(y::Real,x::Period) = x * y
+.*{P<:Period}(y::Real, X::StridedArray{P}) = X .* y
+for (op,Ty,Tz) in ((:.+,:P,:P),(:.-,:P,:P), (:.*,Real,:P),
+                   (:./,:P,Float64), (:./,Real,:P),
+                   (:.%,:P,Int64), (:.%,Integer,:P),
+                   (:div,:P,Int64), (:div,Integer,:P),
+                   (:mod,:P,Int64), (:mod,Integer,:P))
+    sop = string(op)
+    op_ = sop[1] == '.' ? symbol(sop[2:end]) : op
+    @eval begin
+        function ($op){P<:Period}(X::StridedArray{P},y::$Ty)
+            Z = similar(X, $Tz)
+            for i = 1:length(X)
+                @inbounds Z[i] = ($op_)(X[i],y)
             end
+            return Z
         end
     end
 end
@@ -63,6 +80,9 @@ end
 # intfuncs
 Base.gcdx{T<:Period}(a::T,b::T) = ((g,x,y)=gcdx(value(a),value(b)); return T(g),x,y)
 Base.abs{T<:Period}(a::T) = T(abs(value(a)))
+
+# Like Base.steprem in range.jl, but returns the correct type for Periods
+Base.steprem(start::Period,stop::Period,step::Period) = (stop-start) % value(step)
 
 periodisless(::Period,::Year)        = true
 periodisless(::Period,::Month)       = true
