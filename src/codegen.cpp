JuliaLang/julia

commit 55e2ee0391a9e499ad92d5354fe1c0a9abae4f51

llvm 3.6 fixes + symbolic disassembly for llvm3.5/mcjit

src/codegen.cpp
@@ -24,6 +24,7 @@
 #include <llvm/ExecutionEngine/JITEventListener.h>
 #include <llvm/PassManager.h>
 #include <llvm/Target/TargetLibraryInfo.h>
+#include <llvm/Target/TargetSubtargetInfo.h>
 #include <llvm/Support/TargetRegistry.h>
 #include <llvm/Analysis/Passes.h>
 #include <llvm/Bitcode/ReaderWriter.h>
@@ -41,12 +42,12 @@
 #ifdef USE_MCJIT
 #include <llvm/ExecutionEngine/MCJIT.h>
 #include <llvm/ExecutionEngine/SectionMemoryManager.h>
-#include <llvm/ExecutionEngine/ObjectImage.h>
 #include <llvm/ADT/DenseMapInfo.h>
 #include <llvm/Object/ObjectFile.h>
 #else
 #include <llvm/ExecutionEngine/JIT.h>
 #include <llvm/ExecutionEngine/JITMemoryManager.h>
+#include <llvm/ExecutionEngine/Interpreter.h>
 #endif
 #ifdef LLVM33
 #include <llvm/IR/DerivedTypes.h>
@@ -751,7 +752,7 @@ extern void RegisterJuliaJITEventListener();
 
 extern int jl_get_llvmf_info(size_t fptr, uint64_t *symsize,
 #ifdef USE_MCJIT
-    object::ObjectFile **object);
+    const object::ObjectFile **object);
 #else
     std::vector<JITEvent_EmittedFunctionDetails::LineStart> *lines);
 #endif
@@ -759,7 +760,7 @@ extern int jl_get_llvmf_info(size_t fptr, uint64_t *symsize,
 extern "C"
 void jl_dump_function_asm(const char *Fptr, size_t Fsize,
 #ifdef USE_MCJIT
-                          object::ObjectFile *objectfile,
+                          const object::ObjectFile *objectfile,
 #else
                           std::vector<JITEvent_EmittedFunctionDetails::LineStart> lineinfo,
 #endif
@@ -778,7 +779,7 @@ const jl_value_t *jl_dump_llvmf(void *f, bool dumpasm)
         uint64_t symsize;
 #ifdef USE_MCJIT
         size_t fptr = (size_t)jl_ExecutionEngine->getFunctionAddress(llvmf->getName());
-        object::ObjectFile *object;
+        const object::ObjectFile *object;
 #else
         size_t fptr = (size_t)jl_ExecutionEngine->getPointerToFunction(llvmf);
         std::vector<JITEvent_EmittedFunctionDetails::LineStart> object;
@@ -4860,16 +4861,17 @@ extern "C" void jl_init_codegen(void)
     SmallVector<std::string, 4> MAttrs(mattr, mattr+2);
 #endif
 #ifdef LLVM36
-    EngineBuilder eb = EngineBuilder(std::unique_ptr<Module>(engine_module));
+    EngineBuilder *eb = new EngineBuilder(std::unique_ptr<Module>(engine_module));
 #else
-    EngineBuilder eb = EngineBuilder(engine_module);
+    EngineBuilder *eb = new EngineBuilder(engine_module);
 #endif
-    eb  .setEngineKind(EngineKind::JIT)
+    std::string ErrorStr;
+    eb  ->setEngineKind(EngineKind::JIT)
 #if defined(_OS_WINDOWS_) && defined(_CPU_X86_64_)
 #if defined(USE_MCJIT)
-        .setMCJITMemoryManager(createRTDyldMemoryManagerWin(new SectionMemoryManager()))
+        ->setMCJITMemoryManager(createRTDyldMemoryManagerWin(new SectionMemoryManager()))
 #else
-        .setJITMemoryManager(createJITMemoryManagerWin())
+        ->setJITMemoryManager(createJITMemoryManagerWin())
 #endif
 #endif
         .setTargetOptions(options)
@@ -4881,7 +4883,7 @@ extern "C" void jl_init_codegen(void)
 #if defined(_OS_WINDOWS_) && defined(USE_MCJIT)
     TheTriple.setObjectFormat(Triple::ELF);
 #endif
-    jl_TargetMachine = eb.selectTarget(
+    jl_TargetMachine = eb->selectTarget(
             TheTriple,
             "",
 #if LLVM35
@@ -4891,7 +4893,19 @@ extern "C" void jl_init_codegen(void)
 #endif
             MAttrs);
     assert(jl_TargetMachine);
-    jl_ExecutionEngine = eb.create(jl_TargetMachine);
+#ifdef LLVM36
+    engine_module->setDataLayout(jl_TargetMachine->getSubtargetImpl()->getDataLayout());
+#elif defined(LLVM35)
+    engine_module->setDataLayout(jl_TargetMachine->getDataLayout());
+#else
+    engine_module->setDataLayout(jl_TargetMachine->getDataLayout()->getStringRepresentation());
+#endif
+    jl_ExecutionEngine = eb->create(jl_TargetMachine);
+    //ios_printf(ios_stderr,"%s\n",jl_ExecutionEngine->getDataLayout()->getStringRepresentation().c_str());
+    if (!jl_ExecutionEngine) {
+        JL_PRINTF(JL_STDERR, "Critical error initializing llvm: ", ErrorStr.c_str());
+        exit(1);
+    }
 #ifdef LLVM35
     jl_ExecutionEngine->setProcessAllSections(true);
 #endif

src/debuginfo.cpp
@@ -19,7 +19,9 @@
 #include <llvm/Analysis/DebugInfo.h>
 #endif
 #ifdef USE_MCJIT
+#ifndef LLVM36
 #include <llvm/ExecutionEngine/ObjectImage.h>
+#endif
 #include <llvm/ExecutionEngine/RuntimeDyld.h>
 #else
 #include <llvm/ExecutionEngine/JITMemoryManager.h>
@@ -62,8 +64,7 @@ struct FuncInfo {
 };
 #else
 struct ObjectInfo {
-    object::ObjectFile* object;
-    object::SymbolRef symref;
+    const object::ObjectFile* object;
     size_t size;
 };
 #endif
@@ -183,37 +184,71 @@ public:
 #endif // ifndef USE_MCJIT
 
 #ifdef USE_MCJIT
+#ifdef LLVM36
+    virtual void NotifyObjectEmitted(const object::ObjectFile &obj,
+                                     const RuntimeDyld::LoadedObjectInfo &L)
+#else
     virtual void NotifyObjectEmitted(const ObjectImage &obj)
+#endif
     {
-        uint64_t Addr;
+        uint64_t SectAddr, Addr;
         uint64_t Size;
         object::SymbolRef::Type SymbolType;
+#ifndef _OS_LINUX_
+#ifdef LLVM36
+        object::section_iterator Section = obj.section_begin();
+        object::section_iterator EndSection = obj.section_end();
+        StringRef sName;
+#else
+        object::section_iterator Section = obj.begin_sections();
+        object::section_iterator EndSection = obj.end_sections();
+#endif
+#endif
 #ifdef _OS_WINDOWS_
         StringRef Name;
-        object::section_iterator Section = obj.begin_sections();
         uint64_t SectionAddr;
         uint64_t SectionSize;
 #endif
 
-        #ifdef LLVM35
+#ifdef LLVM35
         for (const object::SymbolRef &sym_iter : obj.symbols()) {
             sym_iter.getType(SymbolType);
             if (SymbolType != object::SymbolRef::ST_Function) continue;
-            sym_iter.getAddress(Addr);
             sym_iter.getSize(Size);
+            sym_iter.getAddress(SectAddr);
+#ifndef _OS_LINUX_
+            sym_iter.getSection(Section);
+            if (Section == EndSection) continue;
+            Section = Section->getRelocatedSection();
+            if (Section == EndSection) continue;
+#ifdef LLVM36
+            if (!Section->isText()) continue;
+            Section->getName(sName);
+            Addr = SectAddr + L.getSectionLoadAddress(sName);
+#else
+            Addr = SectAddr;
+#endif
+#else
+            Addr = SectAddr;
+#endif
 #ifdef _OS_WINDOWS_
             sym_iter.getName(Name);
-            sym_iter.getSection(Section);
             Section->getAddress(SectionAddr);
             Section->getSize(SectionSize);
             create_PRUNTIME_FUNCTION(
                    (uint8_t*)(intptr_t)Addr, (size_t)Size, Name,
                    (uint8_t*)(intptr_t)SectionAddr, (size_t)SectionSize);
 #endif
-            ObjectInfo tmp = {obj.getObjectFile(), sym_iter, (size_t)Size};
+            const object::ObjectFile *objfile =
+#ifdef LLVM36
+                &obj;
+#else
+                obj.getObjectFile();
+#endif
+            ObjectInfo tmp = {objfile, (size_t)Size};
             objectmap[Addr] = tmp;
         }
-        #else
+#else //LLVM34
         error_code itererr;
         object::symbol_iterator sym_iter = obj.begin_symbols();
         object::symbol_iterator sym_end = obj.end_symbols();
@@ -221,15 +256,17 @@ public:
             sym_iter->getType(SymbolType);
             if (SymbolType != object::SymbolRef::ST_Function) continue;
             sym_iter->getAddress(Addr);
+            sym_iter->getSize(Size);
 
-            ObjectInfo tmp = {obj.getObjectFile(), *sym_iter};
+            ObjectInfo tmp = {obj.getObjectFile(), (size_t)Size};
             objectmap[Addr] = tmp;
         }
-        #endif
+#endif
     }
 
     // must implement if we ever start freeing code
     // virtual void NotifyFreeingObject(const ObjectImage &obj) {}
+    // virtual void NotifyFreeingObject(const object::ObjectFile &Obj) {}
 
     std::map<size_t, ObjectInfo, revcomp>& getObjectMap()
     {
@@ -429,7 +466,7 @@ void jl_getDylibFunctionInfo(const char **name, size_t *line, const char **filen
 #endif
 #ifdef LLVM36
            std::unique_ptr<MemoryBuffer> membuf = MemoryBuffer::getMemBuffer(
-                    StringRef((const char *)fbase, msize)), "", false);
+                    StringRef((const char *)fbase, msize), "", false);
            auto origerrorobj = llvm::object::ObjectFile::createObjectFile(
                 membuf->getMemBufferRef(), sys::fs::file_magic::unknown);
 #elif defined(LLVM35)
@@ -571,7 +608,7 @@ void jl_getFunctionInfo(const char **name, size_t *line, const char **filename, 
 #ifdef LLVM36
     DIContext *context = DIContext::getDWARFContext(*it->second.object);
 #else
-    DIContext *context = DIContext::getDWARFContext(it->second.object);
+    DIContext *context = DIContext::getDWARFContext(const_cast<object::ObjectFile*>(it->second.object));
 #endif
     lookup_pointer(context, name, line, filename, pointer, 1, fromC);
 
@@ -637,7 +674,7 @@ void jl_getFunctionInfo(const char **name, size_t *line, const char **filename, 
 
 int jl_get_llvmf_info(size_t fptr, uint64_t *symsize,
 #ifdef USE_MCJIT
-    object::ObjectFile **object)
+    const object::ObjectFile **object)
 #else
     std::vector<JITEvent_EmittedFunctionDetails::LineStart> *lines)
 #endif
@@ -659,35 +696,9 @@ int jl_get_llvmf_info(size_t fptr, uint64_t *symsize,
     if (fit == objmap.end()) {
         return 0;
     }
-
-    object::SymbolRef::Type symtype;
-    uint64_t symaddr;
-
-#ifdef LLVM35
-    for (const object::SymbolRef &sym_iter : fit->second.object->symbols()) {
-        sym_iter.getType(symtype);
-        sym_iter.getAddress(symaddr);
-        if (symtype != object::SymbolRef::ST_Function || symaddr != fptr)
-            continue;
-        sym_iter.getSize(*symsize);
-        *object = fit->second.object;
-        return 1;
-    }
-#else
-    error_code itererr;
-    object::symbol_iterator sym_iter = fit->second.object->begin_symbols();
-    object::symbol_iterator sym_end = fit->second.object->end_symbols();
-    for (; sym_iter != sym_end; sym_iter.increment(itererr)) {
-        sym_iter->getType(symtype);
-        sym_iter->getAddress(symaddr);
-        if (symtype != object::SymbolRef::ST_Function || symaddr != fptr)
-            continue;
-        sym_iter->getSize(*symsize);
-        *object = fit->second.object;
-        return 1;
-    }
-#endif // LLVM35
-    return 0;
+    *symsize = fit->second.size;
+    *object = fit->second.object;
+    return 1;
 #endif
 }
 

src/disasm.cpp
@@ -46,6 +46,7 @@
 #include <llvm/MC/MCSymbol.h>
 #ifdef LLVM35
 #include <llvm/AsmParser/Parser.h>
+#include <llvm/MC/MCExternalSymbolizer.h>
 #else
 #include <llvm/Assembly/Parser.h>
 #include <llvm/ADT/OwningPtr.h>
@@ -77,34 +78,36 @@
 #include <llvm/Analysis/DebugInfo.h>
 #endif
 
-
 #include "julia.h"
 
 using namespace llvm;
 
 extern DLLEXPORT LLVMContext &jl_LLVMContext;
 
-#ifndef LLVM36
 namespace {
+#ifdef LLVM36
+#define FuncMCView ArrayRef<uint8_t>
+#else
 class FuncMCView : public MemoryObject {
 private:
     const char *Fptr;
-    size_t Fsize;
+    const size_t Fsize;
 public:
     FuncMCView(const void *fptr, size_t size) : Fptr((const char*)fptr), Fsize(size) {}
-
-    const char *operator[] (const size_t idx) { return (Fptr+idx); }
-
     uint64_t getBase() const { return 0; }
     uint64_t getExtent() const { return Fsize; }
-
     int readByte(uint64_t Addr, uint8_t *Byte) const {
         if (Addr >= getExtent())
             return -1;
         *Byte = Fptr[Addr];
         return 0;
     }
+
+    // ArrayRef-like accessors:
+    const char operator[] (const size_t idx) const { return Fptr[idx]; }
+    uint64_t size() const { return Fsize; }
 };
+#endif
 
 // Look up a symbol, and return a const char* to its name when the
 // address matches. We currently just use "L<address>" as name for the
@@ -161,7 +164,6 @@ const char *SymbolTable::lookupSymbol(uint64_t addr)
     return TempName.c_str();
 }
 
-#ifndef USE_MCJIT // otherwise unused
 const char *SymbolLookup(void *DisInfo,
                          uint64_t ReferenceValue,
                          uint64_t *ReferenceType,
@@ -195,8 +197,10 @@ int OpInfoLookup(void *DisInfo, uint64_t PC,
         return 0;               // Unknown data format
     LLVMOpInfo1 *info = (LLVMOpInfo1*)TagBuf;
     uint8_t *bytes = (uint8_t*) alloca(Size*sizeof(uint8_t));
+    if (PC+Offset+Size > SymTab->getMemoryObject().size())
+        return 0;              // Invalid memory location
     for (uint64_t i=0; i<Size; ++i)
-        SymTab->getMemoryObject().readByte(PC+Offset+i, &bytes[i]);
+        bytes[i] = SymTab->getMemoryObject()[PC+Offset+i];
     size_t pointer;
     switch (Size) {
     case 1: { uint8_t  val; std::memcpy(&val, bytes, 1); pointer = val; break; }
@@ -220,16 +224,14 @@ int OpInfoLookup(void *DisInfo, uint64_t PC,
     info->Value = 0;                 // offset
     return 1;                        // Success
 }
-#endif
 } // namespace
-#endif
 
 extern "C"
 void jl_dump_function_asm(const char *Fptr, size_t Fsize,
 #ifndef USE_MCJIT
                           std::vector<JITEvent_EmittedFunctionDetails::LineStart> lineinfo,
 #else
-                          object::ObjectFile *objectfile,
+                          const object::ObjectFile *objectfile,
 #endif
                           formatted_raw_ostream &stream)
 {
@@ -291,7 +293,7 @@ void jl_dump_function_asm(const char *Fptr, size_t Fsize,
 #ifdef LLVM35
     std::unique_ptr<MCSubtargetInfo>
         STI(TheTarget->createMCSubtargetInfo(TripleName, MCPU, Features.getString()));
-    std::unique_ptr<const MCDisassembler> DisAsm(TheTarget->createMCDisassembler(*STI, Ctx));
+    std::unique_ptr<MCDisassembler> DisAsm(TheTarget->createMCDisassembler(*STI, Ctx));
 #else
     OwningPtr<MCSubtargetInfo>
         STI(TheTarget->createMCSubtargetInfo(TripleName, MCPU, Features.getString()));
@@ -308,6 +310,8 @@ void jl_dump_function_asm(const char *Fptr, size_t Fsize,
 
 #ifdef LLVM35
     std::unique_ptr<MCInstrInfo> MCII(TheTarget->createMCInstrInfo());
+    std::unique_ptr<MCInstrAnalysis>
+        MCIA(TheTarget->createMCInstrAnalysis(MCII.get()));
 #else
     OwningPtr<MCInstrInfo> MCII(TheTarget->createMCInstrInfo());
     OwningPtr<MCInstrAnalysis>
@@ -339,21 +343,30 @@ void jl_dump_function_asm(const char *Fptr, size_t Fsize,
     Streamer->InitSections();
 #endif
 
-#ifndef USE_MCJIT // LLVM33 version
-
     // Make the MemoryObject wrapper
 #ifdef LLVM36
     ArrayRef<uint8_t> memoryObject((uint8_t*)Fptr,Fsize);
 #else
     FuncMCView memoryObject(Fptr, Fsize);
 #endif
-
     SymbolTable DisInfo(Ctx, memoryObject);
+
+#ifdef USE_MCJIT
+    if (!objectfile) return;
+#ifdef LLVM36
+    DIContext *di_ctx = DIContext::getDWARFContext(*objectfile);
+#else
+    DIContext *di_ctx = DIContext::getDWARFContext(const_cast<object::ObjectFile*>(objectfile));
+#endif
+    if (di_ctx == NULL) return;
+    DILineInfoTable lineinfo = di_ctx->getLineInfoForAddressRange((size_t)Fptr, Fsize);
+#else
+    typedef std::vector<JITEvent_EmittedFunctionDetails::LineStart> LInfoVec;
+#endif
 
     // Take two passes: In the first pass we record all branch labels,
     // in the second we actually perform the output
     for (int pass = 0; pass < 2; ++ pass) {
-
         DisInfo.setPass(pass);
         if (pass != 0) {
             // Switch to symbolic disassembly. We cannot do this
@@ -363,42 +376,74 @@ void jl_dump_function_asm(const char *Fptr, size_t Fsize,
             // MCIA->evaluateBranch. (It should be possible to rewrite
             // this routine to handle this case correctly as well.)
             // Could add OpInfoLookup here
-            DisAsm->setupForSymbolicDisassembly
-                (OpInfoLookup, SymbolLookup, &DisInfo, &Ctx);
+#ifdef LLVM35
+            DisAsm->setSymbolizer(std::unique_ptr<MCSymbolizer>(new MCExternalSymbolizer(
+                        Ctx,
+                        std::unique_ptr<MCRelocationInfo>(new MCRelocationInfo(Ctx)),
+                        OpInfoLookup,
+                        SymbolLookup,
+                        &DisInfo)));
+#else
+            DisAsm->setupForSymbolicDisassembly(
+                    OpInfoLookup, SymbolLookup, &DisInfo, &Ctx);
+#endif
         }
 
-        uint64_t Size = 0;
-        uint64_t Index = 0;
-        uint64_t absAddr = 0;
 
+        uint64_t nextLineAddr = -1;
+#ifdef USE_MCJIT
         // Set up the line info
-        typedef std::vector<JITEvent_EmittedFunctionDetails::LineStart>
-            LInfoVec;
+        DILineInfoTable::iterator lineIter = lineinfo.begin();
+        DILineInfoTable::iterator lineEnd = lineinfo.end();
+
+        if (lineIter != lineEnd) {
+            nextLineAddr = lineIter->first;
+            if (pass != 0) {
+#ifdef LLVM35
+                stream << "Filename: " << lineIter->second.FileName << "\n";
+#else
+                stream << "Filename: " << lineIter->second.getFileName() << "\n";
+#endif
+            }
+        }
+#else
+        // Set up the line info
         LInfoVec::iterator lineIter = lineinfo.begin();
         LInfoVec::iterator lineEnd  = lineinfo.end();
 
-        uint64_t nextLineAddr = -1;
-        DISubprogram debugscope;
-
         if (lineIter != lineEnd) {
             nextLineAddr = (*lineIter).Address;
-            debugscope = DISubprogram((*lineIter).Loc.getScope(jl_LLVMContext));
-
+            DISubprogram debugscope = DISubprogram((*lineIter).Loc.getScope(jl_LLVMContext));
             if (pass != 0) {
                 stream << "Filename: " << debugscope.getFilename() << "\n";
                 stream << "Source line: " << (*lineIter).Loc.getLine() << "\n";
             }
         }
+#endif
+
+        uint64_t Index = 0;
+        uint64_t absAddr = 0;
+        uint64_t insSize = 0;
 
         // Do the disassembly
         for (Index = 0, absAddr = (uint64_t)Fptr;
-             Index < memoryObject.getExtent(); Index += Size, absAddr += Size) {
+             Index < Fsize; Index += insSize, absAddr += insSize) {
 
             if (nextLineAddr != (uint64_t)-1 && absAddr == nextLineAddr) {
+#ifdef USE_MCJIT
+#ifdef LLVM35
                 if (pass != 0)
-                    stream << "Source line: "
-                           << (*lineIter).Loc.getLine() << "\n";
+                    stream << "Source line: " << lineIter->second.Line << "\n";
+#else
+                if (pass != 0)
+                    stream << "Source line: " << lineIter->second.getLine() << "\n";
+#endif
+                nextLineAddr = (++lineIter)->first;
+#else
+                if (pass != 0)
+                    stream << "Source line: " << (*lineIter).Loc.getLine() << "\n";
                 nextLineAddr = (*++lineIter).Address;
+#endif
             }
             if (pass != 0) {
                 // Uncomment this to output addresses for all instructions
@@ -409,127 +454,52 @@ void jl_dump_function_asm(const char *Fptr, size_t Fsize,
             }
 
             MCInst Inst;
-
             MCDisassembler::DecodeStatus S;
-            S = DisAsm->getInstruction(Inst, Size, memoryObject, Index,
+            S = DisAsm->getInstruction(Inst, insSize, memoryObject, Index,
                                       /*REMOVE*/ nulls(), nulls());
             switch (S) {
             case MCDisassembler::Fail:
-            if (pass != 0)
-                SrcMgr.PrintMessage(SMLoc::getFromPointer(memoryObject[Index]),
-                                    SourceMgr::DK_Warning,
-                                    "invalid instruction encoding");
-            if (Size == 0)
-                Size = 1; // skip illegible bytes
-            break;
+                if (pass != 0)
+                    SrcMgr.PrintMessage(SMLoc::getFromPointer(Fptr + Index),
+                                        SourceMgr::DK_Warning,
+                                        "invalid instruction encoding");
+                if (insSize == 0)
+                    insSize = 1; // skip illegible bytes
+                break;
 
             case MCDisassembler::SoftFail:
-            if (pass != 0)
-                SrcMgr.PrintMessage(SMLoc::getFromPointer(memoryObject[Index]),
-                                    SourceMgr::DK_Warning,
-                                    "potentially undefined instruction encoding");
-            // Fall through
+                if (pass != 0)
+                    SrcMgr.PrintMessage(SMLoc::getFromPointer(Fptr + Index),
+                                        SourceMgr::DK_Warning,
+                                        "potentially undefined instruction encoding");
+                // Fall through
 
             case MCDisassembler::Success:
-            #ifdef LLVM35
-                if (pass != 0)
-                    Streamer->EmitInstruction(Inst, *STI);
-            #else
                 if (pass == 0) {
                     // Pass 0: Record all branch targets
                     if (MCIA->isBranch(Inst)) {
-                        uint64_t addr = MCIA->evaluateBranch(Inst, Index, Size);
-                        if (addr != uint64_t(-1))
+                        uint64_t addr;
+#ifdef LLVM35
+                        if (MCIA->evaluateBranch(Inst, Index, insSize, addr))
+#else
+                        if ((addr = MCIA->evaluateBranch(Inst, Index, insSize)) != -1)
+#endif
                             DisInfo.insertAddress(addr);
                     }
-                } else {
-                    // Pass 1: Output instruction
-                    Streamer->EmitInstruction(Inst);
                 }
-            #endif
-            break;
+                else {
+                    // Pass 1: Output instruction
+#ifdef LLVM35
+                    Streamer->EmitInstruction(Inst, *STI);
+#else
+                    Streamer->EmitInstruction(Inst);
+#endif
+                }
+                break;
             }
         }
 
         if (pass == 0)
             DisInfo.createSymbols();
     }
-#else // MCJIT version
-#ifdef LLVM36
-    ArrayRef<uint8_t> memoryObject((uint8_t*)Fptr,Fsize);
-#else
-    FuncMCView memoryObject(Fptr, Fsize); // MemoryObject wrapper
-#endif
-
-
-    if (!objectfile) return;
-#ifdef LLVM36
-    DIContext *di_ctx = DIContext::getDWARFContext(*objectfile);
-#else
-    DIContext *di_ctx = DIContext::getDWARFContext(objectfile);
-#endif
-    if (di_ctx == NULL) return;
-    DILineInfoTable lineinfo = di_ctx->getLineInfoForAddressRange((size_t)Fptr, Fsize);
-
-    // Set up the line info
-    DILineInfoTable::iterator lineIter = lineinfo.begin();
-    DILineInfoTable::iterator lineEnd = lineinfo.end();
-
-    uint64_t nextLineAddr = -1;
-
-    if (lineIter != lineEnd) {
-        nextLineAddr = lineIter->first;
-        #ifdef LLVM35
-        stream << "Filename: " << lineIter->second.FileName << "\n";
-        #else
-        stream << "Filename: " << lineIter->second.getFileName() << "\n";
-        #endif
-    }
-
-    uint64_t Index = 0;
-    uint64_t absAddr = 0;
-    uint64_t insSize = 0;
-
-    // Do the disassembly
-    for (Index = 0, absAddr = (uint64_t)Fptr;
-         Index < Fsize; Index += insSize, absAddr += insSize) {
-
-        if (nextLineAddr != (uint64_t)-1 && absAddr == nextLineAddr) {
-            #ifdef LLVM35
-            stream << "Source line: " << lineIter->second.Line << "\n";
-            #else
-            stream << "Source line: " << lineIter->second.getLine() << "\n";
-            #endif
-            nextLineAddr = (++lineIter)->first;
-        }
-
-        MCInst Inst;
-        MCDisassembler::DecodeStatus S;
-        S = DisAsm->getInstruction(Inst, insSize, memoryObject, Index,
-                                  /*REMOVE*/ nulls(), nulls());
-        switch (S) {
-        case MCDisassembler::Fail:
-        SrcMgr.PrintMessage(SMLoc::getFromPointer(Fptr + Index),
-                            SourceMgr::DK_Warning,
-                            "invalid instruction encoding");
-        if (insSize == 0)
-            insSize = 1; // skip illegible bytes
-        break;
-
-        case MCDisassembler::SoftFail:
-        SrcMgr.PrintMessage(SMLoc::getFromPointer(Fptr + Index),
-                            SourceMgr::DK_Warning,
-                            "potentially undefined instruction encoding");
-        // Fall through
-        case MCDisassembler::Success:
-        #ifdef LLVM35
-            Streamer->EmitInstruction(Inst, *STI);
-        #else
-            Streamer->EmitInstruction(Inst);
-        #endif
-        break;
-        }
-    }
-
-#endif
 }

commit 7dd805feb0e18d08a8159d4b49415af685cf62dc

fix win64 build breakage from 55e2ee0391a9e499ad92d5354fe1c0a9abae4f51

src/codegen.cpp
@@ -4869,9 +4869,9 @@ extern "C" void jl_init_codegen(void)
     eb  ->setEngineKind(EngineKind::JIT)
 #if defined(_OS_WINDOWS_) && defined(_CPU_X86_64_)
 #if defined(USE_MCJIT)
-        ->setMCJITMemoryManager(createRTDyldMemoryManagerWin(new SectionMemoryManager()))
+        .setMCJITMemoryManager(createRTDyldMemoryManagerWin(new SectionMemoryManager()))
 #else
-        ->setJITMemoryManager(createJITMemoryManagerWin())
+        .setJITMemoryManager(createJITMemoryManagerWin())
 #endif
 #endif
         .setTargetOptions(options)