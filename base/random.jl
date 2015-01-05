c500e7f7226c3d996344dcf2a485290e4b5d12ca
jake bolewski <jakebolewski@gmail.com>
2015-01-05 13:54:51 -0500

    add DomainError message for make_seed

--- base/random.jl
+++ base/random.jl
@@ -131,7 +131,7 @@ function make_seed()
 end
 
 function make_seed(n::Integer)
-    n < 0 && throw(DomainError())
+    n < 0 && throw(DomainError("seed integer must be ≥ 0"))
     seed = UInt32[]
     while true
         push!(seed, n & 0xffffffff)
