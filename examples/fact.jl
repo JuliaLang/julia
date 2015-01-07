a3a5243c3d4c0b4057dd7fcdc229d9df2e8967b0
Melvin <melvin0008@gmail.com>
2015-01-08 01:24:56 +0530

    removed spaces to build. Added Factorial to examples

--- examples/fact.jl
+++ examples/fact.jl
@@ -1,14 +1,13 @@
 #Tail Recursion Factorial using Julia
 
-function fact(acc::BigInt,n)                                                                                                                                      
-    if n == 0                                                                                                                                         
-        acc                                                                                                                                             
-    else                                                                                                                                              
-        fact(acc* n,n-1)                                                                                                                                 
-    end                                                                                                                                               
+function fact(acc::BigInt,n)
+    if n == 0
+        acc
+    else
+        fact(acc* n,n-1)
+    end
 end
 
 function factorial(n)
-    fact(BigInt(1),n)                                                                                                                                                   
+    fact(BigInt(1),n)
 end
-
