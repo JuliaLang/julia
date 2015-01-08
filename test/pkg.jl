eee269f264f85be2130c5e1fb2c91608a9ea98be
Tim Holy <tim.holy@gmail.com>
2015-01-08 08:19:16 -0600

    test/pkg: restrict code-coverage to unix_only

--- test/pkg.jl
+++ test/pkg.jl
@@ -78,6 +78,7 @@ temp_pkg_dir() do
 end
 
 # Testing with code-coverage
+@unix_only begin                 # TODO: delete unix_only when #8911, #9654 are fixed
 temp_pkg_dir() do
   Pkg.generate("PackageWithCodeCoverage", "MIT", config=Dict("user.name"=>"Julia Test", "user.email"=>"test@julialang.org"))
 
@@ -117,4 +118,5 @@ end"""
       covline = (linetested[i] ? "        1 " : "        - ")*srclines[i]
       @test covlines[i] == covline
   end
+end
 end
