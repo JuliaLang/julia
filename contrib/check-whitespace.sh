3c21751066fd8c3ed41d5a9cd4fb2ed92c862a9f
jake bolewski <jakebolewski@gmail.com>
2015-01-15 14:37:54 -0500

    update travis to check whitespace with script

--- contrib/check-whitespace.sh
+++ contrib/check-whitespace.sh
@@ -21,9 +21,14 @@ file_patterns='
 *Makefile
 '
 
-# TODO: Look also for trailing empty lines, and missing '\n' after the
-# last line
+# TODO: Look also for trailing empty lines, and missing '\n' after the last line
 if git --no-pager grep --color -n --full-name -e ' $' -- $file_patterns; then
     echo "Error: trailing whitespace found in source file(s)"
+    echo ""
+    echo "This can often be fixed with:"
+    echo "    git rebase --whitespace=fix HEAD~1"
+    echo "or"
+    echo "    git rebase --whitespace=fix master"
+    echo "and then a forced push of the correct branch"
     exit 1
 fi
