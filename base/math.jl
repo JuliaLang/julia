74e28e6938c1fecb1a5efa15db41b83216bce5f6
Simon Byrne <simonbyrne@gmail.com>
2015-01-22 12:11:41 +0000

    use muladd in horner macro

--- base/math.jl
+++ base/math.jl
@@ -45,7 +45,7 @@ clamp{T}(x::AbstractArray{T}, lo, hi) =
 macro horner(x, p...)
     ex = esc(p[end])
     for i = length(p)-1:-1:1
-        ex = :($(esc(p[i])) + t * $ex)
+        ex = :(muladd(t, $ex, $(esc(p[i]))))
     end
     Expr(:block, :(t = $(esc(x))), ex)
 end
