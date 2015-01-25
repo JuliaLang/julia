da1242b01ff95effb04f579c33c40159cf8847b1
Andy Hayden <andyhayden1@gmail.com>
2015-01-25 13:07:18 -0800

    add tests for markdown headers

--- test/markdown.jl
+++ test/markdown.jl
@@ -9,6 +9,11 @@ import Base: writemime
 @test md"foo" == MD(Paragraph("foo"))
 @test md"foo *bar* baz" == MD(Paragraph(["foo ", Italic("bar"), " baz"]))
 
+@test md"#title" == Markdown.MD(Markdown.Header{1}("title"))
+@test md"## section" == Markdown.MD(Markdown.Header{2}("section"))
+@test md"# title *foo* `bar` **baz**" == Markdown.MD(Markdown.Header{1}(["title ",
+    Markdown.Italic("foo")," ",Markdown.Code("bar")," ",Markdown.Bold("baz")]))
+
 @test md"**foo *bar* baz**" == MD(Paragraph(Bold(["foo ", Italic("bar"), " baz"])))
 # @test md"**foo *bar* baz**" == MD(Paragraph(Italic(["foo ", Bold("bar"), " baz"])))
 
@@ -20,10 +25,15 @@ import Base: writemime
 
 @test md"foo" |> plain == "foo\n"
 @test md"foo *bar* baz" |> plain == "foo *bar* baz\n"
+@test md"#title" |> plain == "# title\n"
+@test md"## section" |> plain == "## section\n"
+@test md"## section `foo`" |> plain == "## section `foo`\n"
 
 # HTML output
 
 @test md"foo *bar* baz" |> html == "<p>foo <em>bar</em> baz</p>\n"
+@test md"# title *blah*" |> html == "<h1>title <em>blah</em></h1>\n"
+@test md"## title *blah*" |> html == "<h2>title <em>blah</em></h2>\n"
 
 # Interpolation / Custom types
 
