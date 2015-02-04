b00802c32a6218641936f74c05e7398dc1ad9b7f
waTeim <truthset@gmail.com>
2015-02-04 01:52:05 -0500

    Fix behavior on windows

--- contrib/julia-config.jl
+++ contrib/julia-config.jl
@@ -1,52 +1,54 @@
 const options =
 [
-   "--cflags",
-   "--ldflags"
+    "--cflags",
+    "--ldflags"
 ];
 
 function imagePath()
-   opts = Base.compileropts();
-   image_path = bytestring(opts.image_file);
+    opts = Base.compileropts();
+    bytestring(opts.image_file);
 end
 
 function libDir()
-   @windows_only dir = joinpath(Base.JULIA_HOME, "julia");
-   @unix_only dir = match(r"(.*)(sys.ji)",imagePath()).captures[1];
+    @unix_only return match(r"(.*)(sys.ji)",imagePath()).captures[1];
+    @windows_only return Base.JULIA_HOME;
 end
 
 function includeDir()
-   dir = match(r"(.*)(bin)",JULIA_HOME).captures[1]*"include/julia";
+    joinpath(match(r"(.*)(bin)",JULIA_HOME).captures[1],"include","julia");
 end
 
 function initDir()
-   @unix_only dir = match(r"(.*)(julia/sys.ji)",imagePath()).captures[1];
+    @unix_only return match(r"(.*)(julia/sys.ji)",imagePath()).captures[1];
+    @windows_only return match(r"(.*)(julia\sys.ji)",imagePath()).captures[1];
 end
 
 function ldflags()
-   @unix_only "-L$(libDir()) -Wl,-rpath $(libDir()) -ljulia";
+    @unix_only return "-L$(libDir()) -Wl,-rpath $(libDir()) -ljulia";
+    @windows_only return "-L$(libDir()) -ljulia";
 end
 
 function cflags()
-   @unix_only "-DJULIA_INIT_DIR=\"$(initDir())\" -I$(includeDir())";
+    "-DJULIA_INIT_DIR=\"$(initDir())\" -I$(includeDir())";
 end
 
 function check_args(args)
-   checked = intersect(args,options);
-   if length(checked) == 0 || length(checked) != length(args)
-      println(STDERR,"Usage: julia-config [",reduce((x,y)->"$x|$y",options),"]");
-      exit(1);
-   end
+    checked = intersect(args,options);
+    if length(checked) == 0 || length(checked) != length(args)
+        println(STDERR,"Usage: julia-config [",reduce((x,y)->"$x|$y",options),"]");
+        exit(1);
+    end
 end
 
 function main()
-   check_args(ARGS);
-   for args in ARGS
-      if args == "--ldflags"
-         println(ldflags());
-      elseif args == "--cflags"
-         println(cflags());
-      end
-   end
+    check_args(ARGS);
+    for args in ARGS
+        if args == "--ldflags"
+            println(ldflags());
+        elseif args == "--cflags"
+            println(cflags());
+        end
+    end
 end
 
 main();
