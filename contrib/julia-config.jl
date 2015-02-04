7b655356ddd43999df42c1926c3437ce70e578ca
waTeim <truthset@gmail.com>
2015-02-04 00:44:18 -0500

    Added define to set root of directory where julia searches for sysimage

--- contrib/julia-config.jl
+++ contrib/julia-config.jl
@@ -4,23 +4,30 @@ const options =
    "--ldflags"
 ];
 
-function libdir()
+function imagePath()
    opts = Base.compileropts();
    image_path = bytestring(opts.image_file);
-   @windows_only dir = joinpath(Base.JULIA_HOME, "julia");
-   @unix_only dir = match(r"(.*)(sys.ji)",image_path).captures[1];
 end
 
-function includedir()
+function libDir()
+   @windows_only dir = joinpath(Base.JULIA_HOME, "julia");
+   @unix_only dir = match(r"(.*)(sys.ji)",imagePath()).captures[1];
+end
+
+function includeDir()
    dir = match(r"(.*)(bin)",JULIA_HOME).captures[1]*"include/julia";
 end
 
+function initDir()
+   @unix_only dir = match(r"(.*)(julia/sys.ji)",imagePath()).captures[1];
+end
+
 function ldflags()
-   @unix_only "-L$(libdir()) -Wl,-rpath $(libdir()) -ljulia";
+   @unix_only "-L$(libDir()) -Wl,-rpath $(libDir()) -ljulia";
 end
 
 function cflags()
-   @unix_only "-I$(includedir())";
+   @unix_only "-DJULIA_INIT_DIR=\"$(initDir())\" -I$(includeDir())";
 end
 
 function check_args(args)
