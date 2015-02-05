e510f7c282fccde37fea14e0f7b881f10e27192f
Ariel Keselman <skariel@gmail.com>
2015-02-05 08:46:41 +0200

    a first general pool

--- base/gmp.jl
+++ base/gmp.jl
@@ -41,14 +41,31 @@ type BigInt <: Integer
     size::Cint
     d::Ptr{Limb}
     function BigInt()
+        global bigintpool
+        if length(bigintpool) > 0
+            b = pop!(bigintpool)
+            finalizer(b, poolingfinalizer)
+            return b
+        end
         b = new(zero(Cint), zero(Cint), C_NULL)
         ccall((:__gmpz_init,:libgmp), Void, (Ptr{BigInt},), &b)
-        finalizer(b, _gmp_clear_func)
+        finalizer(b, poolingfinalizer)
         return b
     end
 end
 
-_gmp_clear_func = C_NULL
+const bigintpool = BigInt[]
+const BIGINTPOOL_MAXSIZE = 100000
+function poolingfinalizer(b::BigInt)
+    global bigintpool
+    global BIGINTPOOL_MAXSIZE
+    if length(bigintpool) < BIGINTPOOL_MAXSIZE
+        push!(bigintpool, b)
+    else
+        ccall((:__gmpz_clear,:libgmp), Void, (Ptr{BigInt},), &b)
+    end
+end
+
 _mpfr_clear_func = C_NULL
 
 function __init__()
@@ -58,7 +75,6 @@ function __init__()
                      "Please rebuild Julia."))
     end
 
-    global _gmp_clear_func = cglobal((:__gmpz_clear, :libgmp))
     global _mpfr_clear_func = cglobal((:mpfr_clear, :libmpfr))
     ccall((:__gmp_set_memory_functions, :libgmp), Void,
           (Ptr{Void},Ptr{Void},Ptr{Void}),
