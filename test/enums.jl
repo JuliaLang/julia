0d81f1fc6b2b3b5480f5a66abdead8ced0840aaa
Jacob Quinn <quinn.jacobd@gmail.com>
2015-02-11 21:59:13 -0700

    Unqualify usage of @enum to test that export is working correctly

--- test/enums.jl
+++ test/enums.jl
@@ -4,7 +4,7 @@ using Base.Test
 
 @test_throws MethodError convert(Base.Enums.Enum, 1.0)
 
-Base.Enums.@enum Fruit apple orange kiwi
+@enum Fruit apple orange kiwi
 @test typeof(Fruit) == DataType
 @test isbits(Fruit)
 @test typeof(apple) <: Fruit <: Base.Enums.Enum
@@ -58,60 +58,60 @@ for (i,enum) in enumerate(Fruit)
     @test enum == vals[i]
 end
 
-Base.Enums.@enum(QualityofFrenchFood, ReallyGood)
+@enum(QualityofFrenchFood, ReallyGood)
 @test length(QualityofFrenchFood) == 1
 @test typeof(ReallyGood) <: QualityofFrenchFood <: Base.Enums.Enum
 @test int(ReallyGood) == 0
 
-Base.Enums.@enum Binary _zero=0 _one=1 _two=10 _three=11
+@enum Binary _zero=0 _one=1 _two=10 _three=11
 @test _zero.val === 0
 @test _one.val === 1
 @test _two.val === 10
 @test _three.val === 11
-Base.Enums.@enum Negative _neg1=-1 _neg2=-2
+@enum Negative _neg1=-1 _neg2=-2
 @test _neg1.val === -1
 @test _neg2.val === -2
-Base.Enums.@enum Negative2 _neg5=-5 _neg4 _neg3
+@enum Negative2 _neg5=-5 _neg4 _neg3
 @test _neg5.val === -5
 @test _neg4.val === -4
 @test _neg3.val === -3
 
 # currently allow silent overflow
-Base.Enums.@enum Uint8Overflow ff=0xff overflowed
+@enum Uint8Overflow ff=0xff overflowed
 @test ff.val === 0xff
 @test overflowed.val === 0x00
 
-@test_throws MethodError eval(macroexpand(:(Base.Enums.@enum Test1 _zerofp=0.0)))
+@test_throws MethodError eval(macroexpand(:(@enum Test1 _zerofp=0.0)))
 
-@test_throws InexactError eval(macroexpand(:(Base.Enums.@enum Test11 _zerofp2=0.5)))
+@test_throws InexactError eval(macroexpand(:(@enum Test11 _zerofp2=0.5)))
 # can't use non-identifiers as enum members
-@test_throws ErrorException eval(macroexpand(:(Base.Enums.@enum Test2 1=2)))
+@test_throws ErrorException eval(macroexpand(:(@enum Test2 1=2)))
 # other Integer types of enum members
-Base.Enums.@enum Test3 _one_Test3=0x01 _two_Test3=0x02 _three_Test3=0x03
+@enum Test3 _one_Test3=0x01 _two_Test3=0x02 _three_Test3=0x03
 @test typeof(_one_Test3.val) <: UInt8
 @test _one_Test3.val === 0x01
 @test length(Test3) == 3
 
-Base.Enums.@enum Test4 _one_Test4=0x01 _two_Test4=0x0002 _three_Test4=0x03
+@enum Test4 _one_Test4=0x01 _two_Test4=0x0002 _three_Test4=0x03
 @test typeof(_one_Test4.val) <: UInt16
 
-Base.Enums.@enum Test5 _one_Test5=0x01 _two_Test5=0x00000002 _three_Test5=0x00000003
+@enum Test5 _one_Test5=0x01 _two_Test5=0x00000002 _three_Test5=0x00000003
 @test typeof(_one_Test5.val) <: UInt32
 
-Base.Enums.@enum Test6 _one_Test6=0x00000000000000000000000000000001 _two_Test6=0x00000000000000000000000000000002
+@enum Test6 _one_Test6=0x00000000000000000000000000000001 _two_Test6=0x00000000000000000000000000000002
 @test typeof(_one_Test6.val) <: UInt128
 
-Base.Enums.@enum Test7 _zero_Test7=0b0 _one_Test7=0b1 _two_Test7=0b10
+@enum Test7 _zero_Test7=0b0 _one_Test7=0b1 _two_Test7=0b10
 @test typeof(_zero_Test7.val) <: UInt8
 
-@test_throws MethodError eval(macroexpand(:(Base.Enums.@enum Test8 _zero="zero")))
-@test_throws MethodError eval(macroexpand(:(Base.Enums.@enum Test9 _zero='0')))
+@test_throws MethodError eval(macroexpand(:(@enum Test8 _zero="zero")))
+@test_throws MethodError eval(macroexpand(:(@enum Test9 _zero='0')))
 
-Base.Enums.@enum Test8 _zero_Test8=zero(Int64)
+@enum Test8 _zero_Test8=zero(Int64)
 @test typeof(_zero_Test8.val) <: Int64
 @test _zero_Test8.val === Int64(0)
 
-Base.Enums.@enum Test9 _zero_Test9 _one_Test9=0x01 _two_Test9
+@enum Test9 _zero_Test9 _one_Test9=0x01 _two_Test9
 @test typeof(_zero_Test9.val) <: Int
 @test _zero_Test9.val === 0
 @test typeof(_one_Test9.val) <: Int
@@ -119,7 +119,7 @@ Base.Enums.@enum Test9 _zero_Test9 _one_Test9=0x01 _two_Test9
 @test typeof(_two_Test9.val) <: Int
 @test _two_Test9.val === 2
 
-Base.Enums.@enum Test10 _zero_Test10=0x00 _one_Test10 _two_Test10
+@enum Test10 _zero_Test10=0x00 _one_Test10 _two_Test10
 @test typeof(_zero_Test10.val) <: UInt8
 @test _zero_Test10.val === 0x00
 @test typeof(_one_Test10.val) <: UInt8
