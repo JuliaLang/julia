4e82814a1abcd2a12fe0e6e7e1920c19a08c6f59
Simon Kornblith <simon@simonster.com>
2015-02-18 13:48:52 -0500

    Don't throw BoundsError when calling deleteat! with an empty range
    (cherry picked from commit b67025bd73e164d23b35d30e92d0c6570221b3f2)
    
    Conflicts:
    	test/arrayops.jl
    
    Fix logic bug from b67025bd73e164d23b35d30e92d0c6570221b3f2
    
    (cherry picked from commit 9018da0498f4c085c05c7482143c826a8a9b753a)

--- test/arrayops.jl
+++ test/arrayops.jl
@@ -785,6 +785,7 @@ for idx in {1, 2, 5, 9, 10, 1:0, 2:1, 1:1, 2:2, 1:2, 2:4, 9:8, 10:9, 9:9, 10:10,
     @test deleteat!(a, idx) == [acopy[1:(first(idx)-1)], acopy[(last(idx)+1):end]]
 end
 a = [1:10]
+@test deleteat!(a, 11:10) == [1:10;]
 @test deleteat!(a, [1,3,5,7:10...]) == [2,4,6]
 @test_throws BoundsError deleteat!(a, 13)
 @test_throws BoundsError deleteat!(a, [1,13])
