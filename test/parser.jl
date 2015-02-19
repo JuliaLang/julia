JuliaLang/julia

commit 7bd9769cd8cc25539e4d31a8358c4b8a40e30559

remove _mstr macros. closes #10228

src/julia-syntax.scm
@@ -3303,7 +3303,12 @@ So far only the second case can actually occur.
         ((eq? (car e) 'macrocall)
          ;; expand macro
          (let ((form
-                (apply invoke-julia-macro (cadr e) (cddr e))))
+		(if (and (length> e 2) (pair? (caddr e)) (eq? (caaddr e) 'triple_quoted_string))
+		    ;; for a custom triple-quoted string literal, first invoke mstr
+		    ;; to handle unindenting
+		    (invoke-julia-macro (cadr e)
+					(julia-expand-macros `(macrocall @mstr ,(cadr (caddr e)))))
+		    (apply invoke-julia-macro (cadr e) (cddr e)))))
            (if (not form)
                (error (string "macro \"" (cadr e) "\" not defined")))
            (if (and (pair? form) (eq? (car form) 'error))

base/basedocs.jl
@@ -229,8 +229,6 @@ keywords[:...] = doc"""
       RegexMatch("angry,\nBad world")
   """ r""
 
-@doc (@doc r"") @r_mstr
-
 @doc doc"""
       push!(collection, items...) → collection
 

base/docs.jl
@@ -1,6 +1,6 @@
 module Docs
 
-import Base.Markdown: @doc_str, @doc_mstr, MD
+import Base.Markdown: @doc_str, MD
 
 export doc, @doc
 
@@ -149,9 +149,9 @@ namify(sy::Symbol) = sy
 
 function mdify(ex)
     if isa(ex, AbstractString)
-        :(@doc_str $(esc(ex)))
+        :(@doc_str $ex)
     elseif isexpr(ex, :macrocall) && namify(ex) == symbol("@mstr")
-        :(@doc_mstr $(esc(ex.args[2])))
+        :(@doc_str $(Expr(:triple_quoted_string, ex.args[2])))
     else
         esc(ex)
     end
@@ -258,7 +258,7 @@ end
 
 import Base: print, writemime
 
-export HTML, @html_str, @html_mstr
+export HTML, @html_str
 
 export HTML, Text
 
@@ -289,13 +289,8 @@ writemime(io::IO, ::MIME"text/html", h::HTML) = print(io, h.content)
 writemime(io::IO, ::MIME"text/html", h::HTML{Function}) = h.content(io)
 
 @doc "Create an `HTML` object from a literal string." ->
-macro html_str (s)
+macro html_str(s)
     :(HTML($s))
-end
-
-@doc (@doc html"") ->
-macro html_mstr (s)
-    :(HTML($(Base.triplequoted(s))))
 end
 
 function catdoc(xs::HTML...)
@@ -306,7 +301,7 @@ function catdoc(xs::HTML...)
     end
 end
 
-export Text, @text_str, @text_mstr
+export Text, @text_str
 
 # @doc """
 # `Text(s)`: Create an object that renders `s` as plain text.
@@ -330,11 +325,6 @@ writemime(io::IO, ::MIME"text/plain", t::Text) = print(io, t)
 @doc "Create a `Text` object from a literal string." ->
 macro text_str (s)
     :(Text($s))
-end
-
-@doc (@doc text"") ->
-macro text_mstr (s)
-    :(Text($(Base.triplequoted(s))))
 end
 
 function catdoc(xs::Text...)

base/exports.jl
@@ -1383,12 +1383,9 @@ export
     @__FILE__,
     @b_str,
     @r_str,
-    @r_mstr,
     @v_str,
     @text_str,
-    @text_mstr,
     @html_str,
-    @html_mstr,
     @int128_str,
     @uint128_str,
     @bigint_str,
@@ -1447,5 +1444,4 @@ export
     @inline,
     @noinline,
     @doc,
-    @doc_str,
-    @doc_mstr
+    @doc_str

base/markdown/Markdown.jl
@@ -19,7 +19,7 @@ include("render/html.jl")
 
 include("render/terminal/render.jl")
 
-export readme, license, @md_str, @md_mstr, @doc_str, @doc_mstr
+export readme, license, @md_str, @doc_str
 
 parse(markdown::String; flavor = julia) = parse(IOBuffer(markdown), flavor = flavor)
 parse_file(file::String; flavor = julia) = parse(readall(file), flavor = flavor)
@@ -49,17 +49,7 @@ macro md_str(s, t...)
     mdexpr(s, t...)
 end
 
-macro md_mstr(s, t...)
-    s = Base.triplequoted(s)
-    mdexpr(s, t...)
-end
-
 macro doc_str(s, t...)
-    docexpr(s, t...)
-end
-
-macro doc_mstr(s, t...)
-    s = Base.triplequoted(s)
     docexpr(s, t...)
 end
 

base/regex.jl
@@ -53,7 +53,6 @@ function compile(regex::Regex)
 end
 
 macro r_str(pattern, flags...) Regex(pattern, flags...) end
-macro r_mstr(pattern, flags...) Regex(pattern, flags...) end
 
 copy(r::Regex) = r
 

src/julia-parser.scm
@@ -998,15 +998,14 @@
                  (let* ((str (begin (take-token s)
                                     (parse-string-literal s #t)))
                         (nxt (peek-token s))
-                        (suffix (if (triplequote-string-literal? str) '_mstr '_str))
-                        (macname (symbol (string #\@ ex suffix)))
-                        (macstr (cdr str)))
+                        (macname (symbol (string #\@ ex '_str)))
+                        (macstr (if (triplequote-string-literal? str) str (cadr str))))
                    (if (and (symbol? nxt) (not (operator? nxt))
                             (not (ts:space? s)))
                        ;; string literal suffix, "s"x
-                       (loop `(macrocall ,macname ,@macstr
+                       (loop `(macrocall ,macname ,macstr
                                          ,(string (take-token s))))
-                       (loop `(macrocall ,macname ,@macstr))))
+                       (loop `(macrocall ,macname ,macstr))))
                  ex))
             (else ex))))))
 

commit 2a1ab562a26a7b521b87a7c10d8daea75237a2da

fix custom string literal suffix regression caused by 7bd9769cd8cc25539e4d31a8358c4b8a40e30559

src/julia-syntax.scm
@@ -3306,8 +3306,9 @@ So far only the second case can actually occur.
 		(if (and (length> e 2) (pair? (caddr e)) (eq? (caaddr e) 'triple_quoted_string))
 		    ;; for a custom triple-quoted string literal, first invoke mstr
 		    ;; to handle unindenting
-		    (invoke-julia-macro (cadr e)
-					(julia-expand-macros `(macrocall @mstr ,(cadr (caddr e)))))
+		    (apply invoke-julia-macro (cadr e)
+			   (julia-expand-macros `(macrocall @mstr ,(cadr (caddr e))))
+			   (cdddr e))
 		    (apply invoke-julia-macro (cadr e) (cddr e)))))
            (if (not form)
                (error (string "macro \"" (cadr e) "\" not defined")))

test/parser.jl
@@ -22,3 +22,16 @@ let a = :a
             end) == :(module a
                       end)
 end
+
+# string literals
+macro test999_str(args...); args; end
+@test test999"a"b == ("a","b")
+@test test999"""a"""b == ("a","b")
+@test test999"
+    a
+    b" == ("
+    a
+    b",)
+@test test999"""
+    a
+    b""" == ("a\nb",)