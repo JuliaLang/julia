b41b530c90072fcd85415bf82e61a62b3ecaf91c
Shashi Gowda <shashigowda91@gmail.com>
2015-02-23 18:46:47 +0530

    update to latest nullable.jl

--- src/nullable.jl
+++ src/nullable.jl
@@ -13,16 +13,18 @@ immutable NullException <: Exception
 end
 
 Nullable{T}(value::T) = Nullable{T}(value)
+Nullable() = Nullable{Union()}()
 
 eltype{T}(::Type{Nullable{T}}) = T
 
-eltype{T}(x::Nullable{T}) = T
-
 function convert{T}(::Type{Nullable{T}}, x::Nullable)
-    return isnull(x) ? Nullable{T}() : Nullable(convert(T, get(x)))
+    return isnull(x) ? Nullable{T}() : Nullable{T}(convert(T, get(x)))
 end
 
 convert{T}(::Type{Nullable{T}}, x::T) = Nullable{T}(x)
+
+convert{T}(::Type{Nullable{T}}, ::Void) = Nullable{T}()
+convert(   ::Type{Nullable   }, ::Void) = Nullable{Union()}()
 
 function show{T}(io::IO, x::Nullable{T})
     if x.isnull
