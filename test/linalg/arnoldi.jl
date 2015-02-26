014c2d7c1ce3f150c6b707d0fb93625401363c85
Jiahao Chen <jiahao@mit.edu>
2015-02-26 17:43:29 -0500

    Hacky minimal fix for #10329
    good jerb ARPACK

--- test/linalg/arnoldi.jl
+++ test/linalg/arnoldi.jl
@@ -17,6 +17,13 @@ let # svds test
     s1_right = sign(S1[3][3,1]) * S1[3][:,1]
     s2_right = sign(S2[3][3,1]) * S2[3][:,1]
     @test_approx_eq s1_right s2_right
+
+    #10329
+    B = sparse(diagm([1.0, 2.0, 34.0, 5.0, 6.0]))
+    S3 = svds(B, ritzvec=false, nsv=2)
+    @test_approx_eq S3[1] [34.0, 6.0]
+    S4 = svds(B, nsv=2)
+    @test_approx_eq S4[2] [34.0, 6.0]
 end
 
 let # complex svds test
