e303d6a76133b2b27c2dba038ba9cb6e4decf9da
jake bolewski <jakebolewski@gmail.com>
2015-03-04 11:39:07 -0500

    force push! and unshift! to need at least one item

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -1453,10 +1453,8 @@ end
 
 # multi-item push!, unshift! (built on top of type-specific 1-item version)
 # (note: must not cause a dispatch loop when 1-item case is not defined)
-push!(A) = A
 push!(A, a, b) = push!(push!(A, a), b)
 push!(A, a, b, c...) = push!(push!(A, a, b), c...)
-unshift!(A) = A
 unshift!(A, a, b) = unshift!(unshift!(A, b), a)
 unshift!(A, a, b, c...) = unshift!(unshift!(A, c...), a, b)
 
