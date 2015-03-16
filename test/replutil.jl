9851b29ee044bb4ef8ca90b2bfaf6a8efaf7882c
Jeff Bezanson <jeff.bezanson@gmail.com>
2015-03-16 14:00:32 -0400

    fix replutil test for extra newline in no-method error
    I had no idea there were so many tests for this.

--- test/replutil.jl
+++ test/replutil.jl
@@ -10,22 +10,22 @@ method_c1(x::Float64, s::AbstractString...) = true
 
 buf = IOBuffer()
 Base.show_method_candidates(buf, Base.MethodError(method_c1,(1, 1, "")))
-no_color = "\nClosest candidates are:\n  method_c1(!Matched::Float64, !Matched::AbstractString...)\n"
+no_color = "\nClosest candidates are:\n  method_c1(!Matched::Float64, !Matched::AbstractString...)"
 test_have_color(buf,
-                "\e[0m\nClosest candidates are:\n  method_c1(\e[1m\e[31m::Float64\e[0m, \e[1m\e[31m::AbstractString...\e[0m)\n\e[0m",
+                "\e[0m\nClosest candidates are:\n  method_c1(\e[1m\e[31m::Float64\e[0m, \e[1m\e[31m::AbstractString...\e[0m)\e[0m",
                 no_color)
 
-no_color = "\nClosest candidates are:\n  method_c1(!Matched::Float64, ::AbstractString...)\n"
+no_color = "\nClosest candidates are:\n  method_c1(!Matched::Float64, ::AbstractString...)"
 Base.show_method_candidates(buf, Base.MethodError(method_c1,(1, "", "")))
 test_have_color(buf,
-                "\e[0m\nClosest candidates are:\n  method_c1(\e[1m\e[31m::Float64\e[0m, ::AbstractString...)\n\e[0m",
+                "\e[0m\nClosest candidates are:\n  method_c1(\e[1m\e[31m::Float64\e[0m, ::AbstractString...)\e[0m",
                 no_color)
 
 # should match
-no_color = "\nClosest candidates are:\n  method_c1(::Float64, ::AbstractString...)\n"
+no_color = "\nClosest candidates are:\n  method_c1(::Float64, ::AbstractString...)"
 Base.show_method_candidates(buf, Base.MethodError(method_c1,(1., "", "")))
 test_have_color(buf,
-                "\e[0m\nClosest candidates are:\n  method_c1(::Float64, ::AbstractString...)\n\e[0m",
+                "\e[0m\nClosest candidates are:\n  method_c1(::Float64, ::AbstractString...)\e[0m",
                 no_color)
 
 # Have no matches so should return empty
@@ -38,14 +38,14 @@ method_c2(x::Int32, y::Float64) = true
 method_c2{T<:Real}(x::T, y::T, z::T) = true
 
 Base.show_method_candidates(buf, Base.MethodError(method_c2,(1., 1., 2)))
-color = "\e[0m\nClosest candidates are:\n  method_c2(\e[1m\e[31m::Int32\e[0m, ::Float64, ::Any...)\n  method_c2(\e[1m\e[31m::Int32\e[0m, ::Any...)\n  method_c2{T<:Real}(::T<:Real, ::T<:Real, \e[1m\e[31m::T<:Real\e[0m)\n  ...\n\e[0m"
-no_color = no_color = "\nClosest candidates are:\n  method_c2(!Matched::Int32, ::Float64, ::Any...)\n  method_c2(!Matched::Int32, ::Any...)\n  method_c2{T<:Real}(::T<:Real, ::T<:Real, !Matched::T<:Real)\n  ...\n"
+color = "\e[0m\nClosest candidates are:\n  method_c2(\e[1m\e[31m::Int32\e[0m, ::Float64, ::Any...)\n  method_c2(\e[1m\e[31m::Int32\e[0m, ::Any...)\n  method_c2{T<:Real}(::T<:Real, ::T<:Real, \e[1m\e[31m::T<:Real\e[0m)\n  ...\e[0m"
+no_color = no_color = "\nClosest candidates are:\n  method_c2(!Matched::Int32, ::Float64, ::Any...)\n  method_c2(!Matched::Int32, ::Any...)\n  method_c2{T<:Real}(::T<:Real, ::T<:Real, !Matched::T<:Real)\n  ..."
 test_have_color(buf, color, no_color)
 
 method_c3(x::Float64, y::Float64) = true
 Base.show_method_candidates(buf, Base.MethodError(method_c3,(1.,)))
-color = "\e[0m\nClosest candidates are:\n  method_c3(::Float64, \e[1m\e[31m::Float64\e[0m)\n\e[0m"
-no_color = no_color = "\nClosest candidates are:\n  method_c3(::Float64, !Matched::Float64)\n"
+color = "\e[0m\nClosest candidates are:\n  method_c3(::Float64, \e[1m\e[31m::Float64\e[0m)\e[0m"
+no_color = no_color = "\nClosest candidates are:\n  method_c3(::Float64, !Matched::Float64)"
 test_have_color(buf, color, no_color)
 
 # Test for the method error in issue #8651
@@ -54,8 +54,8 @@ test_have_color(buf, "", "")
 
 method_c4(::Type{Float64}) = true
 Base.show_method_candidates(buf, MethodError(method_c4,(Float64,)))
-test_have_color(buf, "\e[0m\nClosest candidates are:\n  method_c4(::Type{Float64})\n\e[0m",
-                "\nClosest candidates are:\n  method_c4(::Type{Float64})\n")
+test_have_color(buf, "\e[0m\nClosest candidates are:\n  method_c4(::Type{Float64})\e[0m",
+                "\nClosest candidates are:\n  method_c4(::Type{Float64})")
 
 Base.show_method_candidates(buf, MethodError(method_c4,(Int32,)))
 test_have_color(buf, "", "")
