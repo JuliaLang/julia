fc412d47efa8da8a4fb7333d780afd1f3e378552
Andy Hayden <andyhayden1@gmail.com>
2015-03-20 17:22:26 -0700

    fenced code blocks can use ~
    fix multiple ` can be inline

--- base/markdown/GitHub/GitHub.jl
+++ base/markdown/GitHub/GitHub.jl
@@ -2,17 +2,31 @@ include("table.jl")
 
 @breaking true ->
 function fencedcode(stream::IO, block::MD, config::Config)
-    startswith(stream, "```", padding = true) || return false
+    start = position(stream)
+    startswith(stream, "~~~", padding = true) || startswith(stream, "```", padding = true) || return false
+    skip(stream, -2)
+    ch = read(stream, Char)
     trailing = strip(readline(stream))
-    flavor = lstrip(trailing, '`')
+    flavor = lstrip(trailing, ch)
     n = 3 + length(trailing) - length(flavor)
+
+    # inline code block
+    if contains(flavor, string(ch) ^ n)
+        seek(stream, start)
+        return false
+    end
+
     buffer = IOBuffer()
     while !eof(stream)
-        startswith(stream, "`" ^ n) && break
-        write(buffer, readline(stream))
+        if startswith(stream, string(ch) ^ n)
+            push!(block, Code(flavor, takebuf_string(buffer) |> chomp))
+            return true
+        else
+            write(buffer, readline(stream))
+        end
     end
-    push!(block, Code(flavor, takebuf_string(buffer) |> chomp))
-    return true
+    seek(stream, start)
+    return false
 end
 
 function github_paragraph(stream::IO, md::MD, config::Config)
