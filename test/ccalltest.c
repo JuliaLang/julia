97b18707a8c04358980a819f5f3b0008b13d77c4
Ivar Nesje <ivarne@gmail.com>
2015-03-24 14:04:06 +0100

    Update ccalltest.c
    OOops!! 
    
    This should fix https://github.com/JuliaLang/julia/commit/03da3a3035519dcbb5cdc744704b7ee076dd3e7e#commitcomment-10366257

--- test/ccalltest.c
+++ test/ccalltest.c
@@ -118,7 +118,7 @@ complex_t ctest(complex_t a) {
 complex double cgtest(complex double a) {
     //Unpack a ComplexPair{Float64} struct
     if (verbose) fprintf(stderr,"%g + %g i\n", creal(a), cimag(a));
-    a += 1 - (2.0*i);
+    a += 1 - (2.0*I);
     return a;
 }
 
