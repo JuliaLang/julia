fbf9aaf683e15b2ae2442a3b0a88e067a8f4b5f8
Kevin Squire <kevin@secondspectrum.com>
2015-04-12 20:24:00 -0700

    Fix screwed up indentation accidentally added in 4593894a78

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,7 +31,7 @@ td[1] = 1.0
 @test @compat(Dict()) == Dict()
 @test @compat(Dict{Any,Any}()) == Dict{Any,Any}()
 if VERSION >= v"0.3.0-"
-@test @compat(Dict([(1, 1)])) == d
+	@test @compat(Dict([(1, 1)])) == d
 end
 
 d2 = Dict{Symbol,Dict{Symbol,Int}}()
@@ -43,9 +43,9 @@ d = Dict(zip([1, 2], [3, 4]))
 @test d == @compat Dict(1=>3, 2=>4)
 
 @compat function f()
-a = :a
-b = Dict(:b => 1)
-Dict(a => b)
+	a = :a
+	b = Dict(:b => 1)
+	Dict(a => b)
 end
 @test f() == d2
 
@@ -63,7 +63,7 @@ ns = length(d.slots)
 @test @compat rsplit("a,b,,c", ',', keep=true) == ["a", "b", "", "c"]
 
 if VERSION < v"0.4.0-dev+1387"
-@test isdefined(Main, :AbstractString)
+    @test isdefined(Main, :AbstractString)
 end
 
 @test round(Int, 3//4) == 1
@@ -110,10 +110,10 @@ end
 @test rand(Bool) in [false, true]
 
 module CartesianTest
-using Base.Cartesian, Compat
-@ngenerate N NTuple{N,Int} function f(X::NTuple{N,Int}...)
-    @ncall N tuple X
-end
+	using Base.Cartesian, Compat
+	@ngenerate N NTuple{N,Int} function f(X::NTuple{N,Int}...)
+		@ncall N tuple X
+	end
 end
 
 @test CartesianTest.f(1) == (1,)
@@ -126,63 +126,63 @@ end
 @test success(pipe(`true`, `true`))
 
 let convert_funcs_and_types =
-((integer, :Integer), (signed, :Signed), (unsigned, :Unsigned),
- (int, :Int), (int8, :Int8), (int16, :Int16), (int32, :Int32),
- (int64, :Int64), (int128, :Int128), (uint, :UInt),
- (uint8, :UInt8), (uint16, :UInt16), (uint32, :UInt32),
- (uint64, :UInt64), (uint128, :UInt128),
- (float16, :Float16), (float32, :Float32), (float64, :Float64),
- (complex32,:Complex32), (complex64,:Complex64),(complex128,:Complex128),
- (char,:Char))
+    ((integer, :Integer), (signed, :Signed), (unsigned, :Unsigned),
+     (int, :Int), (int8, :Int8), (int16, :Int16), (int32, :Int32),
+     (int64, :Int64), (int128, :Int128), (uint, :UInt),
+     (uint8, :UInt8), (uint16, :UInt16), (uint32, :UInt32),
+     (uint64, :UInt64), (uint128, :UInt128),
+     (float16, :Float16), (float32, :Float32), (float64, :Float64),
+     (complex32,:Complex32), (complex64,:Complex64),(complex128,:Complex128),
+     (char,:Char))
 
-for (df,t) in convert_funcs_and_types
-    x = @compat UInt8(10)
-    r1 = eval(:(@compat($t($x))))
-    ty = eval(t)
-    if ty.abstract
-        @test issubtype(typeof(r1),ty)
-    else
-        @test typeof(r1) === ty
-    end
-    if VERSION <  v"0.4.0-dev+3732"
-        r2 = df(x)
-        @test r1 === r2
-        if t === :Signed || t === :Complex32
-            continue
+    for (df,t) in convert_funcs_and_types
+        x = @compat UInt8(10)
+        r1 = eval(:(@compat($t($x))))
+        ty = eval(t)
+        if ty.abstract
+            @test issubtype(typeof(r1),ty)
+        else
+            @test typeof(r1) === ty
         end
-        x = fill(x, 10)
-        r1 = eval(:(@compat map($t, $x)))
-        r2 = df(x)
-        @test r1 == r2
-        @test typeof(r1) === typeof(r2)
+        if VERSION <  v"0.4.0-dev+3732"
+            r2 = df(x)
+            @test r1 === r2
+            if t === :Signed || t === :Complex32
+                continue
+            end
+            x = fill(x, 10)
+            r1 = eval(:(@compat map($t, $x)))
+            r2 = df(x)
+            @test r1 == r2
+            @test typeof(r1) === typeof(r2)
+        end
     end
-end
-@test (@compat Bool(1))
-@test !(@compat Bool(0))
+    @test (@compat Bool(1))
+    @test !(@compat Bool(0))
 
-# issue #54
-c = @compat Complex128(1)
-@test c.re == 1
-@test c.im == 0
+    # issue #54
+    c = @compat Complex128(1)
+    @test c.re == 1
+    @test c.im == 0
 
-c = @compat Complex128(1,2)
-@test c.re == 1
-@test c.im == 2
+    c = @compat Complex128(1,2)
+    @test c.re == 1
+    @test c.im == 2
 
-c = @compat Complex128(1+2im)
-@test c.re == 1
-@test c.im == 2
+    c = @compat Complex128(1+2im)
+    @test c.re == 1
+    @test c.im == 2
 end
 
 type Test3609
-a
-b
+    a
+    b
 end
 
 if VERSION < v"0.4.0-dev+3609"
-let v = Test3609(1,2)
-    @test fieldnames(Test3609) == fieldnames(v) == Symbol[:a, :b]
-end
+    let v = Test3609(1,2)
+        @test fieldnames(Test3609) == fieldnames(v) == Symbol[:a, :b]
+    end
 end
 
 @test parse(Int8, '9') == convert(Int8, 9)
@@ -194,10 +194,10 @@ end
 
 # Make sure exports from Libc and Libdl are defined
 for x in [:strftime,:systemsleep,:getpid,:FILE,:malloc,:MS_SYNC,:munmap,:flush_cstdio,:realloc,:strptime,:Libc,:errno,:msync,:TmStruct,:calloc,:MS_INVALIDATE,:MS_ASYNC,:time,:strerror,:gethostname,:free,:mmap]
-Libc.(x)
+    Libc.(x)
 end
 for x in [:RTLD_LOCAL,:RTLD_GLOBAL,:find_library,:dlsym,:RTLD_LAZY,:RTLD_NODELETE,:DL_LOAD_PATH,:RTLD_NOW,:Libdl,:dlext,:dlsym_e,:RTLD_FIRST,:dlopen,:dllist,:dlpath,:RTLD_NOLOAD,:dlclose,:dlopen_e,:RTLD_DEEPBIND]
-Libdl.(x)
+    Libdl.(x)
 end
 
 # Test unsafe_convert
