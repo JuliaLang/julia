e7fbd4ef81bacf3a0ea1c19acd979534877fe4b2
Fabian R Lischka <github@fabian-lischka.de>
2015-04-14 22:53:07 +0800

    remove mod2pi from list of tests

--- test/choosetests.jl
+++ test/choosetests.jl
@@ -23,7 +23,7 @@ function choosetests(choices = [])
         "priorityqueue", "file", "version", "resolve",
         "pollfd", "mpfr", "broadcast", "complex", "socket",
         "floatapprox", "readdlm", "reflection", "regex", "float16",
-        "combinatorics", "sysinfo", "rounding", "ranges", "mod2pi",
+        "combinatorics", "sysinfo", "rounding", "ranges",
         "euler", "show", "lineedit", "replcompletions", "repl",
         "replutil", "sets", "test", "goto", "llvmcall", "grisu",
         "nullable", "meta", "profile", "libgit2", "docs", "markdown",
