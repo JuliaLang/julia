ddd1b02cfffc26fab35ca5090ba4166bdbf5583f
Isaiah Norton <isaiah.norton@gmail.com>
2015-04-15 12:12:03 -0400

    Don't bail out on test errors by default
    Adds check for JL_TESTFAILURE_STOP environment variable to control fail-on-error.

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,8 +10,15 @@ cd(dirname(@__FILE__)) do
 
     @everywhere include("testdefs.jl")
 
-    reduce(propagate_errors, nothing, pmap(runtests, tests; err_retry=false, err_stop=true))
+    err_stop = haskey(ENV, "JL_TESTFAILURE_STOP")
+    results = reduce(propagate_errors, nothing, pmap(runtests, tests; err_retry=false, err_stop=err_stop))
 
     @unix_only n > 1 && rmprocs(workers(), waitfor=5.0)
+
+    # exit(1) if any test raised an error so that travis knows the build failed.
+    if any(x -> x==true, results)
+        exit(1)
+    end
+
     println("    \033[32;1mSUCCESS\033[0m")
 end
