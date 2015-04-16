a54e9d5d90cb1764e62c2dead5d0ca7edc3f8ea0
Dahua Lin <lindahua@gmail.com>
2015-04-16 10:10:55 +0800

    reduce with `|` and `&` passed for arrays longer than 16.

--- test/reduce.jl
+++ test/reduce.jl
@@ -195,10 +195,10 @@ prod2(itr) = invoke(prod, (Any,), itr)
 @test all(x->x>0, [4]) == true
 @test all(x->x>0, [-3, 4, 5]) == false
 
-@test reduce(|, fill(trues(5), 3))  == trues(5)
-@test reduce(|, fill(falses(5), 3)) == falses(5)
-@test reduce(&, fill(trues(5), 3))  == trues(5)
-@test reduce(&, fill(falses(5), 3)) == falses(5)
+@test reduce(|, fill(trues(5), 24))  == trues(5)
+@test reduce(|, fill(falses(5), 24)) == falses(5)
+@test reduce(&, fill(trues(5), 24))  == trues(5)
+@test reduce(&, fill(falses(5), 24)) == falses(5)
 
 # in
 
