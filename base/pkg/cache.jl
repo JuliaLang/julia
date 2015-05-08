f836a71a6854c70a55cd6f342212cbc81e3fb29e
Art Wild <wildart@gmail.com>
2015-05-07 21:53:37 -0400

    added `clone` & `fetch`, updated 'Dir' and `Cache`

--- base/pkg/cache.jl
+++ base/pkg/cache.jl
@@ -2,7 +2,7 @@
 
 module Cache
 
-import ..Git, ..Dir
+import ..LibGit2, ..Dir
 using ..Types
 
 path(pkg::AbstractString) = abspath(".cache", pkg)
@@ -28,27 +28,26 @@ function mkcachedir()
     mkdir(cache)
 end
 
-
 function prefetch(pkg::AbstractString, url::AbstractString, sha1s::Vector)
     isdir(".cache") || mkcachedir()
     cache = path(pkg)
     if !isdir(cache)
         info("Cloning cache of $pkg from $url")
-        try Git.run(`clone -q --mirror $url $cache`)
-        catch
+        repo = LibGit2.clone(url, path, bare = true, remote_cb = Pkg.LibGit2.mirror_cb)
+        if repo == noting
             rm(cache, recursive=true)
-            rethrow()
+            error("Cannot clone $pkg from $url")
         end
+    else
+        repo = LibGit2.GitRepo(cache)
     end
-    Git.set_remote_url(url, dir=cache)
-    in_cache = Git.iscommit(sha1s, dir=cache)
-    if !all(in_cache)
+    LibGit2.set_remote_url(repo, url)
+    if !all(sha1->LibGit2.iscommit(sha1, repo), sha1s)
         info("Updating cache of $pkg...")
-        Git.success(`remote update`, dir=cache) ||
-        error("couldn't update $cache using `git remote update`")
-        in_cache = Git.iscommit(sha1s, dir=cache)
+        isa(LibGit2.fetch(repo), LibGit2.GitError) &&
+            error("couldn't update $cache using `git remote update`")
     end
-    sha1s[!in_cache]
+    filter(sha1->!LibGit2.iscommit(sha1, repo), sha1s)
 end
 prefetch(pkg::AbstractString, url::AbstractString, sha1::AbstractString...) = prefetch(pkg, url, AbstractString[sha1...])
 
