563e1697c877cc1d1e0ea27fb37a7272cfbf7f4b
Art Wild <wildart@gmail.com>
2015-05-07 21:50:37 -0400

    write to repo configuration value with any accepted type

--- base/pkg/libgit2/config.jl
+++ base/pkg/libgit2/config.jl
@@ -27,7 +27,7 @@ end
 GitConfig(r::GitRepo) = begin
     cfg_ptr = Ptr{Void}[C_NULL]
     err = ccall((:git_repository_config, :libgit2), Cint,
-                 (Ptr{Ptr{Void}}, Ptr{Void}), cfg_ptr, r)
+                 (Ptr{Ptr{Void}}, Ptr{Void}), cfg_ptr, r.ptr)
     err !=0 && return nothing
     return GitConfig(cfg_ptr[1])
 end
@@ -49,8 +49,23 @@ function lookup{T<:AbstractString}(::Type{T}, c::GitConfig, name::AbstractString
     end
 end
 
-function set!{T<:AbstractString}(::Type{T}, c::GitConfig, name::AbstractString, value::AbstractString)
-    err = ccall((:git_config_set_string, :libgit2), Cint,
+function set!{T}(c::GitConfig, name::AbstractString, value::T)
+    err = if T<:AbstractString
+        ccall((:git_config_set_string, :libgit2), Cint,
                  (Ptr{Void}, Ptr{Uint8}, Ptr{Uint8}), c.ptr, name, value)
+    elseif is(T, Bool)
+        bval = Int32(value)
+        ccall((:git_config_set_bool, :libgit2), Cint,
+                 (Ptr{Void}, Ptr{Uint8}, Cint), c.ptr, name, bval)
+    elseif is(T, Int32)
+        ccall((:git_config_set_int32, :libgit2), Cint,
+                 (Ptr{Void}, Ptr{Uint8}, Cint), c.ptr, name, value)
+    elseif is(T, Int64)
+        ccall((:git_config_set_int64, :libgit2), Cint,
+                 (Ptr{Void}, Ptr{Uint8}, Cintmax_t), c.ptr, name, value)
+    else
+        warn("Type $T is not supported")
+    end
     return err
 end
+
