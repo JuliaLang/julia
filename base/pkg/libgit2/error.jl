8fbf905c0cf18e0ea90923faa5c9906bcbd7f1a0
Art Wild <wildart@gmail.com>
2015-05-12 21:02:31 -0400

    refactored error handling

--- base/pkg/libgit2/error.jl
+++ base/pkg/libgit2/error.jl
@@ -1,106 +1,100 @@
-module GitErrorConst #TODO: use @enum & rid of git_error_code
-    const GIT_OK          = Cint(0)
-    const ERROR           = Cint(-01)
-    const ENOTFOUND       = Cint(-03)
-    const EEXISTS         = Cint(-04)
-    const EAMBIGUOUS      = Cint(-05)
-    const EBUFS           = Cint(-06)
-    const EUSER           = Cint(-07)
-    const EBAREREPO       = Cint(-08)
-    const EUNBORNBRANCH   = Cint(-09)
-    const EUNMERGED       = Cint(-10)
-    const ENONFASTFORWARD = Cint(-11)
-    const EINVALIDSPEC    = Cint(-12)
-    const EMERGECONFLICT  = Cint(-13)
-    const ELOCKED         = Cint(-14)
-    const PASSTHROUGH     = Cint(-30)
-    const ITEROVER        = Cint(-31)
-end
+module Error
 
-const git_error_code = Dict{Int,Symbol}(
-     00 => :OK,             # no error
-    -01 => :Error,          # generic error
-    -03 => :NotFound,       # requested object could not be found
-    -04 => :Exists,         # object exits preventing op
-    -05 => :Ambiguous,      # more than one object matches
-    -06 => :Bufs,           # output buffer too small to hold data
-    -07 => :User,           # user callback generated error
-    -08 => :BareRepo,       # operation not allowed on bare repo
-    -09 => :UnbornBranch,   # HEAD refers to branch with 0 commits
-    -10 => :Unmerged,       # merge in progress prevented op
-    -11 => :NonFastForward, # ref not fast-forwardable
-    -12 => :InvalidSpec,    # name / ref not in valid format
-    -13 => :MergeConflict,  # merge conflict prevented op
-    -14 => :Locked,         # lock file prevented op
-    -15 => :Modified,       # ref value does not match expected
-    -31 => :Iterover        # signals end of iteration
-)
+@enum(Code, GIT_OK          = Cint(0),   # no error
+            ERROR           = Cint(-01), # generic error
+            ENOTFOUND       = Cint(-03), # requested object could not be found
+            EEXISTS         = Cint(-04), # object exits preventing op
+            EAMBIGUOUS      = Cint(-05), # more than one object matches
+            EBUFS           = Cint(-06), # output buffer too small to hold data
+            EUSER           = Cint(-07), # user callback generated error
+            EBAREREPO       = Cint(-08), # operation not allowed on bare repo
+            EUNBORNBRANCH   = Cint(-09), # HEAD refers to branch with 0 commits
+            EUNMERGED       = Cint(-10), # merge in progress prevented op
+            ENONFASTFORWARD = Cint(-11), # ref not fast-forwardable
+            EINVALIDSPEC    = Cint(-12), # name / ref not in valid format
+            EMERGECONFLICT  = Cint(-13), # merge conflict prevented op
+            ELOCKED         = Cint(-14), # lock file prevented op
+            EMODIFIED       = Cint(-15), # ref value does not match expected
+            EAUTH           = Cint(-16), # authentication error
+            ECERTIFICATE    = Cint(-17), # server certificate is invalid
+            EAPPLIED        = Cint(-18), # patch/merge has already been applied
+            EPEEL           = Cint(-19), # the requested peel operation is not possible
+            PASSTHROUGH     = Cint(-30), # internal only
+            ITEROVER        = Cint(-31)) # signals end of iteration
 
-const git_error_class = Dict{Int,Symbol}(
-     0 => :None,
-     1 => :NoMemory,
-     2 => :OS,
-     3 => :Invalid,
-     4 => :Ref,
-     5 => :Zlib,
-     6 => :Repo,
-     7 => :Config,
-     8 => :Regex,
-     9 => :Odb,
-    10 => :Index,
-    11 => :Object,
-    12 => :Net,
-    13 => :Tag,
-    14 => :Tree,
-    15 => :Indexer,
-    16 => :SSL,
-    17 => :Submodule,
-    18 => :Thread,
-    19 => :Stash,
-    20 => :Checkout,
-    21 => :FetchHead,
-    22 => :Merge,
-    23 => :SSH,
-    24 => :Filter,
-    25 => :Revert,
-    26 => :Callback,
-    27 => :CherryPick
-)
+Base.(:(==))(c::Code, e::Integer) = c.val == e
+Base.(:(==))(e::Integer, c::Code) = c.val == e
+
+@enum(Class, None,
+             NoMemory,
+             OS,
+             Invalid,
+             Reference,
+             Zlib,
+             Repository,
+             Config,
+             Regex,
+             Odb,
+             Index,
+             Object,
+             Net,
+             Tag,
+             Tree,
+             Indexer,
+             SSL,
+             Submodule,
+             Thread,
+             Stash,
+             Checkout,
+             FetchHead,
+             Merge,
+             SSH,
+             Filter,
+             Revert,
+             Callback,
+             CherryPick,
+             Describe,
+             Rebase)
 
 immutable ErrorStruct
     message::Ptr{UInt8}
     class::Cint
 end
 
-immutable GitError{Class, Code} <: Exception
+immutable GitError <: Exception
+    class::Class
+    code::Code
     msg::AbstractString
 end
+Base.show(io::IO, err::GitError) = print(io, "[Code:$(err.code), Class:$(err.class)]: $(err.msg)")
 
 function last_error()
     err = ccall((:giterr_last, :libgit2), Ptr{ErrorStruct}, ())
     if err != C_NULL
         err_obj   = unsafe_load(err)
-        err_class = git_error_class[Int(err_obj.class)]
+        err_class = Class[err_obj.class][]
         err_msg   = bytestring(err_obj.message)
     else
-        err_class = git_error_class[0]
+        err_class = Class[0][]
         err_msg = "No errors"
     end
     return (err_class, err_msg)
 end
 
 function GitError(code::Integer)
-    err_code = git_error_code[Int(code)]
+    err_code = Code[code][]
     err_class, err_msg = last_error()
-    return GitError{err_class, err_code}(err_msg)
+    return GitError(err_class, err_code, err_msg)
 end
+
+end # Error module
 
 macro check(git_func)
     quote
         local err::Cint
         err = $(esc(git_func::Expr))
         if err < 0
-            throw(GitError(err))
+            throw(Error.GitError(err))
         end
         err
     end
--- base/pkg/libgit2/walker.jl
+++ base/pkg/libgit2/walker.jl
@@ -1,8 +1,7 @@
 function GitRevWalker(r::GitRepo)
     w_ptr = Ref{Ptr{Void}}(C_NULL)
-    err = ccall((:git_revwalk_new, :libgit2), Cint,
+    @check ccall((:git_revwalk_new, :libgit2), Cint,
                   (Ptr{Ptr{Void}}, Ptr{Void}), w_ptr, r.ptr)
-    err != GitErrorConst.GIT_OK && return nothing
     return GitRevWalker(w_ptr[])
 end
 
@@ -10,7 +9,7 @@ function Base.start(w::GitRevWalker)
     id_ptr = Ref(Oid())
     err = ccall((:git_revwalk_next, :libgit2), Cint,
                 (Ptr{Oid}, Ptr{Void}), id_ptr, w.ptr)
-    err != GitErrorConst.GIT_OK && return (nothing, true)
+    err != Error.GIT_OK && return (nothing, true)
     return (id_ptr[], false)
 end
 
@@ -20,19 +19,17 @@ function Base.next(w::GitRevWalker, state)
     id_ptr = Ref(Oid())
     err = ccall((:git_revwalk_next, :libgit2), Cint,
                 (Ptr{Oid}, Ptr{Void}), id_ptr, w.ptr)
-    err != GitErrorConst.GIT_OK && return (state[1], (nothing, true))
+    err != Error.GIT_OK && return (state[1], (nothing, true))
     return (state[1], (id_ptr[], false))
 end
 
 function push_head!(w::GitRevWalker)
-    err = ccall((:git_revwalk_push_head, :libgit2), Cint, (Ptr{Void},), w.ptr)
-    err != GitErrorConst.GIT_OK && return GitError(err)
+    @check ccall((:git_revwalk_push_head, :libgit2), Cint, (Ptr{Void},), w.ptr)
     return w
 end
 
 function Base.push!(w::GitRevWalker, cid::Oid)
-    err = ccall((:git_revwalk_push, :libgit2), Cint, (Ptr{Void}, Ptr{Oid}), w.ptr, Ref(cid))
-    err != GitErrorConst.GIT_OK && return GitError(err)
+    @check ccall((:git_revwalk_push, :libgit2), Cint, (Ptr{Void}, Ptr{Oid}), w.ptr, Ref(cid))
     return w
 end
 
@@ -44,21 +41,26 @@ end
 
 function Base.map(f::Function, repo::GitRepo; oid::Oid=Oid(), by::Cint = GitConst.SORT_NONE, rev::Bool=false)
     walker = GitRevWalker(repo)
-    sort!(walker, by=by, rev=rev)
-    if iszero(oid)
-        push_head!(walker)
-    else
-        push!(walker, oid)
-    end
-    s = start(walker)
     res = nothing
-    while !done(walker, s)
-        val = f(s[1], repo)
-        if res == nothing
-            res = Array(typeof(val),0)
+    try
+        sort!(walker, by=by, rev=rev)
+        if iszero(oid)
+            push_head!(walker)
+        else
+            push!(walker, oid)
         end
-        push!(res, val)
-        val, s = next(walker, s)
+        s = start(walker)
+
+        while !done(walker, s)
+            val = f(s[1], repo)
+            if res == nothing
+                res = Array(typeof(val),0)
+            end
+            push!(res, val)
+            val, s = next(walker, s)
+        end
+    finally
+        finalize(walker)
     end
     return res
 end
