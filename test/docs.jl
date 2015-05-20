1dc5472b2c2bbc1ed8d853a7279b95afd87f4687
fcard <ficarde@gmail.com>
2015-05-20 14:42:01 -0300

    Add tests to macro documentation fix

--- test/docs.jl
+++ test/docs.jl
@@ -3,3 +3,8 @@
 @doc "Doc abstract type" ->
 abstract C74685 <: AbstractArray
 @test stringmime("text/plain", Docs.doc(C74685))=="Doc abstract type\n"
+
+macro macro_doctest() end
+@doc "Helps test if macros can be documented with `@doc \"...\" -> @...`." ->
+@macro_doctest
+@test (@doc @macro_doctest) != nothing
