3ac5abf33be6dd77478e4d243ac74a6e49c5acd8
pabloferz <pabloferz@yahoo.com.mx>
2015-05-31 14:52:11 -0500

    Avoid the printing of e+00

--- base/mpfr.jl
+++ base/mpfr.jl
@@ -768,11 +768,9 @@ function string(x::BigFloat)
     lng::Int32 = 127
     buf = Array(UInt8, lng + 1)
     lng = ccall((:mpfr_snprintf,:libmpfr), Int32, (Ptr{UInt8}, Culong, Ptr{UInt8}, Ptr{BigFloat}...), buf, lng + 1, "%.Re", &x)
-    if 84 <= lng <= 127
-        return bytestring(pointer(buf), 1 <= x < 10 || x == 0 ? lng - 4 : lng)
-    elseif lng < 84 # print at least 78 decimal places
+    if lng < 84 # print at least 78 decimal places
         lng = ccall((:mpfr_sprintf,:libmpfr), Int32, (Ptr{UInt8}, Ptr{UInt8}, Ptr{BigFloat}...), buf, "%.78Re", &x)
-    else
+    elseif lng > 127
         buf = Array(UInt8, lng + 1)
         lng = ccall((:mpfr_snprintf,:libmpfr), Int32, (Ptr{UInt8}, Culong, Ptr{UInt8}, Ptr{BigFloat}...), buf, lng + 1, "%.Re", &x)
     end
