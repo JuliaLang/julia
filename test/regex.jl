cf8c62a02a0aa4fcde48d1c90ab311799dc99f7e
Jon Malmaud <malmaud@gmail.com>
2015-06-03 15:11:37 -0600

    define getindex on regex matches to return captures.

--- test/regex.jl
+++ test/regex.jl
@@ -37,3 +37,7 @@ show(buf, r"")
 # regex match / search string must be a ByteString
 @test_throws ArgumentError match(r"test", utf32("this is a test"))
 @test_throws ArgumentError search(utf32("this is a test"), r"test")
+
+# Named subpatterns
+m = match(r"(?<a>.)(.)(?<b>.)", "xyz")
+@test (m["a"], m[2], m["b"]) == ("x", "y", "z")
