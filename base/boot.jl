7e6f5e604d78dc769f35a0bdfa5f0db208fc6458
Jacob Quinn <quinn.jacobd@gmail.com>
2015-06-03 01:11:10 -0600

    Partially address #11351 by catching writes to read-only memory on windows and creating a new ReadOnlyMemoryError type

--- base/boot.jl
+++ base/boot.jl
@@ -134,8 +134,8 @@ export
     # string types
     Char, ASCIIString, ByteString, DirectIndexString, AbstractString, UTF8String,
     # errors
-    BoundsError, DivideError, DomainError, Exception,
-    InexactError, InterruptException, OutOfMemoryError, OverflowError,
+    BoundsError, DivideError, DomainError, Exception, InexactError,
+    InterruptException, OutOfMemoryError, ReadOnlyMemoryError, OverflowError,
     StackOverflowError, SegmentationFault, UndefRefError, UndefVarError,
     # AST representation
     Expr, GotoNode, LabelNode, LineNumberNode, QuoteNode, SymbolNode, TopNode,
@@ -218,6 +218,7 @@ immutable DomainError        <: Exception end
 immutable OverflowError      <: Exception end
 immutable InexactError       <: Exception end
 immutable OutOfMemoryError   <: Exception end
+immutable ReadOnlyMemoryError<: Exception end
 immutable SegmentationFault  <: Exception end
 immutable StackOverflowError <: Exception end
 immutable UndefRefError      <: Exception end
