fae0b8c803e69ae839a201096f4a7f728768fcf8
Simon Byrne <simonbyrne@gmail.com>
2015-06-04 17:50:20 +0100

    add 2 argument Nullable constructor

--- test/nullable.jl
+++ test/nullable.jl
@@ -40,6 +40,23 @@ for T in types
     @test eltype(x) === T
 end
 
+# Nullable{T}(value::T, isnull::Bool) = new(isnull, value)
+for T in types
+    x = Nullable{T}(zero(T),false)
+    @test x.isnull === false
+    @test isa(x.value, T)
+    @test x.value === zero(T)
+    @test eltype(x) === T
+
+    x = Nullable{T}(zero(T),true)
+    @test x.isnull === true
+    @test isa(x.value, T)
+    @test eltype(Nullable{T}) === T
+    @test eltype(x) === T
+end
+
+
+
 # immutable NullException <: Exception
 @test isa(NullException(), NullException)
 @test_throws NullException throw(NullException())
