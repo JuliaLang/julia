262f413d495a7b745a02e8c54a137147872beaf6
Mike Innes <mike.j.innes@gmail.com>
2015-06-24 00:45:31 -0400

    new doc syntax

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -353,15 +353,16 @@ if Base.USE_GPL_LIBS
 
 end # USE_GPL_LIBS
 
-@doc doc"""
-      include("file.jl")
+"""
+    include("file.jl")
 
-  Evaluate the contents of a source file in the current context.
-  During including, a task-local include path is set to the directory
-  containing the file. Nested calls to `include` will search
-  relative to that path. All paths refer to files on node 1 when
-  running in parallel, and files will be fetched from node 1. This
-  function is typically used to load source interactively, or to
-  combine files in packages that are broken into multiple source
-  files.
-  """ include_from_node1
+Evaluate the contents of a source file in the current context.
+During including, a task-local include path is set to the directory
+containing the file. Nested calls to `include` will search
+relative to that path. All paths refer to files on node 1 when
+running in parallel, and files will be fetched from node 1. This
+function is typically used to load source interactively, or to
+combine files in packages that are broken into multiple source
+files.
+"""
+include_from_node1
