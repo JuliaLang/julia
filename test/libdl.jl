baa547664dbed00b3d5dccfc36fa36a746488231
Jake Bolewski <jakebolewski@gmail.com>
2015-07-05 14:15:07 -0400

    add more dlload tests adapted from @ihnorton

--- test/libdl.jl
+++ test/libdl.jl
@@ -18,41 +18,124 @@ end
 @test_throws ArgumentError Libdl.dlsym(C_NULL, :foo)
 @test_throws ArgumentError Libdl.dlsym_e(C_NULL, :foo)
 
-@test !isempty(Libdl.find_library(["libccalltest"], [dirname(@__FILE__)]))
+# @test !isempty(Libdl.find_library(["libccalltest"], [dirname(@__FILE__)]))
 
 # dlopen should be able to handle absolute and relative paths, with and without dlext
-let dl = Libdl.dlopen_e(abspath("./libccalltest"))
-    @test dl != C_NULL
-    Libdl.dlclose(dl)
+let dl = C_NULL
+    try
+        dl = Libdl.dlopen_e(abspath("./libccalltest"))
+        @test dl != C_NULL
+    finally
+        Libdl.dlclose(dl)
+    end
 end
 
-let dl = Libdl.dlopen_e(join((abspath("./libccalltest"), string(".", Libdl.dlext))))
-    @test dl != C_NULL
-    Libdl.dlclose(dl)
+let dl = C_NULL
+    try
+        dl = Libdl.dlopen_e(join((abspath("./libccalltest"), string(".", Libdl.dlext))))
+        @test dl != C_NULL
+    finally
+        Libdl.dlclose(dl)
+    end
 end
 
-let dl = Libdl.dlopen_e("./libccalltest")
-    @test dl != C_NULL
-    Libdl.dlclose(dl)
+let dl = C_NULL
+    try
+        dl = Libdl.dlopen_e("./libccalltest")
+        @test dl != C_NULL
+    finally
+        Libdl.dlclose(dl)
+    end
 end
 
-let dl = Libdl.dlopen_e(join(("./libccalltest", string(".", Libdl.dlext))))
-    @test dl != C_NULL
-    Libdl.dlclose(dl)
+let dl = C_NULL
+    try
+        dl = Libdl.dlopen_e(join(("./libccalltest", string(".", Libdl.dlext))))
+        @test dl != C_NULL
+    finally
+        Libdl.dlclose(dl)
+    end
 end
 
-let dl = Libdl.dlopen_e("./foo")
-    @test dl == C_NULL
+let dl = C_NULL
+    try
+        dl = Libdl.dlopen_e("./foo")
+        @test dl == C_NULL
+    finally
+        Libdl.dlclose(dl)
+    end
 end
+
+# unqualified names present in DL_LOAD_PATH
+let dl = C_NULL
+    push!(Libdl.DL_LOAD_PATH, pwd())
+    try
+        dl = Libdl.dlopen_e("libccalltest")
+        @test dl != C_NULL
+    finally
+        Libdl.dlclose(dl)
+        pop!(Libdl.DL_LOAD_PATH)
+    end
+end
+
+let dl = C_NULL
+    push!(Libdl.DL_LOAD_PATH, pwd())
+    try
+        dl = Libdl.dlopen_e(string("libccalltest",".",Libdl.dlext))
+        @test dl != C_NULL
+    finally
+        Libdl.dlclose(dl)
+        pop!(Libdl.DL_LOAD_PATH)
+    end
+end
+
+# path with dlopen-able file first in load path
+#=
+let dl = C_NULL,
+    tmpdir = mktempdir(),
+    fpath = joinpath(tmpdir,"libccalltest")
+    try
+        write(open(fpath,"w"))
+        push!(Libdl.DL_LOAD_PATH, dirname(@__FILE__))
+        push!(Libdl.DL_LOAD_PATH, dirname(fpath))
+        dl = Libdl.dlopen_e("libccalltest")
+        @test dl != C_NULL
+    finally
+        pop!(Libdl.DL_LOAD_PATH)
+        pop!(Libdl.DL_LOAD_PATH)
+        rm(tmpdir, recursive=true)
+    end
+end
+=#
+
+# path with dlopen-able file second in load path
+#=
+let dl = C_NULL,
+    tmpdir = mktempdir(),
+    fpath = joinpath(tmpdir,"libccalltest")
+    try
+        write(open(fpath,"w"))
+        push!(Libdl.DL_LOAD_PATH, dirname(fpath))
+        push!(Libdl.DL_LOAD_PATH, dirname(@__FILE__))
+        dl = Libdl.dlopen_e("libccalltest")
+        @test dl != C_NULL
+    finally
+        pop!(Libdl.DL_LOAD_PATH)
+        pop!(Libdl.DL_LOAD_PATH)
+        rm(tmpdir, recursive=true)
+    end
+end
+=#
 
 # test dlpath
-let dl = Libdl.dlopen("./libccalltest")
+let dl = C_NULL
     try
+        dl = Libdl.dlopen("./libccalltest")
         @test dl != C_NULL
         @test Base.samefile(abspath(Libdl.dlpath(dl)),
                             abspath(Libdl.dlpath("./libccalltest")))
         @test Base.samefile(abspath(Libdl.dlpath(dl)),
-                            abspath(join(("./libccalltest",Libdl.dlext), '.')))
+                            abspath(string("./libccalltest",".", Libdl.dlext)))
     finally
         Libdl.dlclose(dl)
     end
@@ -62,8 +145,9 @@ end
 @test_throws ErrorException Libdl.dlopen("./foo")
 
 # test dlsym
-let dl = Libdl.dlopen("./libccalltest")
+let dl = C_NULL
     try
+        dl = Libdl.dlopen("./libccalltest")
         fptr = Libdl.dlsym(dl, :set_verbose)
         @test fptr != C_NULL
         @test_throws ErrorException Libdl.dlsym(dl, :foo)
