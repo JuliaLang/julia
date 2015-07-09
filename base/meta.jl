2fb06bb5ff3464a8ef0d1ced827501788e84f67d
Mike Innes <mike.j.innes@gmail.com>
2015-07-10 00:26:22 +0100

    implement bindings as values

--- base/meta.jl
+++ base/meta.jl
@@ -16,7 +16,8 @@ export quot,
        isdef,
        subs,
        @expand,
-       @match
+       @match,
+       @var
 
 quot(ex) = Expr(:quote, ex)
 
@@ -249,5 +250,48 @@ macro match(ex, lines)
     push!(result.args, body)
     return result
 end
+
+# Variable bindings as values
+
+moduleusings(mod) = ccall(:jl_module_usings, Any, (Any,), mod)
+
+function findsource(mod::Module, var::Symbol, seen = Set{Module}())
+    mod in seen && return
+    var in names(mod, true) && return mod
+    push!(seen, mod)
+    sources = filter(m -> m ≠ nothing && !(m in seen),
+                     map(m -> findsource(m, var, seen),
+                         moduleusings(mod)))
+    isempty(sources) && return
+    return collect(sources)[1]
+end
+
+immutable Binding
+    mod::Module
+    var::Symbol
+
+    function Binding(mod::Module, var::Symbol)
+        mod′ = findsource(mod, var)
+        mod′ == nothing && error("$mod.$var not found")
+        return new(mod′, var)
+    end
+end
+
+macro var(x)
+    (mod, x) = @match x begin
+        mod_.x_    -> (mod, x)
+        (mod_.@x_) -> (mod, x)
+        (@x_)      -> (nothing, x)
+        x_""       -> (nothing, x)
+        _          -> isa(x, Symbol) ? (nothing, x) :
+                        error("Invalid @var syntax `$x`")
+    end
+    mod == nothing && (mod = module_name(current_module()))
+    :(Binding($(esc(mod)), $(Expr(:quote, x))))
+end
+
+Base.show(io::IO, x::Binding) = println(io, "•$(x.mod).$(x.var)")
+
+Base.getindex(x::Binding) = x.mod.(x.var)
 
 end # module
