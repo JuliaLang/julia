85c6bcb4d623b01bd99d372000265d461e77e090
Massimiliano Fasi <massimiliano.fasi@gmail.com>
2015-07-20 23:34:21 +0100

    Redirected the standard error for some of the tests related to logm, removed a faulty test

--- test/linalg3.jl
+++ test/linalg3.jl
@@ -162,20 +162,26 @@ for elty in (Float32, Float64, Complex64, Complex128)
 end
 
 for elty in (Float64, Complex{Float64})
+
     A4  = convert(Matrix{elty}, [1/2 1/3 1/4 1/5+eps();
                                  1/3 1/4 1/5 1/6;
                                  1/4 1/5 1/6 1/7;
                                  1/5 1/6 1/7 1/8])
     @test_approx_eq expm(logm(A4)) A4
 
+    OLD_STDERR = STDERR
+    rd,wr = redirect_stderr()
     A5  = convert(Matrix{elty}, [1 1 0 1; 0 1 1 0; 0 0 1 1; 1 0 0 1])
     @test_approx_eq expm(logm(A5)) A5
+    s = readline(rd)
+    @test contains(s, "WARNING: Matrix with nonpositive real eigenvalues, a nonprimary matrix logarithm will be returned.")
 
     A6  = convert(Matrix{elty}, [-5 2 0 0 ; 1/2 -7 3 0; 0 1/3 -9 4; 0 0 1/4 -11])
     @test_approx_eq expm(logm(A6)) A6
+    s = readline(rd)
+    @test contains(s, "WARNING: Matrix with nonpositive real eigenvalues, a nonprimary matrix logarithm will be returned.")
+    redirect_stderr(OLD_STDERR)
 
-    A7  = convert(Matrix{elty}, [1 0 0 1e-8; 0 1 0 0; 0 0 1 0; 0 0 0 1])
-    @test_approx_eq expm(logm(A7)) A7
 end
 
 A8 = 100 * [-1+1im 0 0 1e-8; 0 1 0 0; 0 0 1 0; 0 0 0 1];
