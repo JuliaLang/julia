e7dc997e296ba08575279b54eccc0549d53fcf06
Kenta Sato <bicycle1885@gmail.com>
2015-07-24 15:40:25 +0900

    read n-byte integer from ios

--- base/iostream.jl
+++ base/iostream.jl
@@ -165,11 +165,11 @@ function read(s::IOStream, ::Type{UInt8})
     b % UInt8
 end
 
-for (fname, U, S) in (("jl_ios_get_uint16", :UInt16, :Int16),
-                      ("jl_ios_get_uint32", :UInt32, :Int32),
-                      ("jl_ios_get_uint64", :UInt64, :Int64))
+for (U, S) in ((:UInt16, :Int16),
+               (:UInt32, :Int32),
+               (:UInt64, :Int64))
     @eval function read{T<:Union{$U,$S}}(s::IOStream, ::Type{T})
-        ccall($fname, $U, (Ptr{Void},), s.ios) % T
+        ccall(:jl_ios_get_nbytes, UInt64, (Ptr{Void}, Csize_t), s.ios, sizeof(T)) % T
     end
 end
 
