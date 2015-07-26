6d785921367e0d89cc903604a0ce4109057ccadf
kshyatt <kshyatt@physics.ucsb.edu>
2015-07-26 15:10:08 -0700

    Little bite-sized tests in reduce and test.jl

--- test/test.jl
+++ test/test.jl
@@ -90,7 +90,8 @@ end
 @test isapprox(.1+.1+.1, .3)
 @test !isapprox(.1+.1+.1, .4)
 
+@test_throws ErrorException Test.test_approx_eq(ones(10),ones(11),1e-8,"a","b")
+@test_throws ErrorException Test.test_approx_eq(ones(10),zeros(10),1e-8,"a","b")
 
 # Test @test_approx_eq_eps
 # TODO
-
