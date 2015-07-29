4a0b506e34b37415fa6a1e737fb3b02643af105a
Jiahao Chen <jiahao@mit.edu>
2015-07-29 03:27:08 -0400

    Update tab completion doc generator
    Creating a sorted list of (key, value) pairs now requires specifying
    sort by=first, since iterating through a dictionary now returns Pairs
    rather than Tuples, and Pairs do not have a defined lexical order.
    
    Ref: cc61506ba35c4339cbdab405e643b695399d6d3f #12327

--- doc/tabcomplete.jl
+++ doc/tabcomplete.jl
@@ -35,7 +35,7 @@ end
 entries = Any[("Code point(s)", "Character(s)", "Tab completion sequence(s)", "Unicode name(s)")]
 maxlen = [map(length, entries[1])...]
 
-for (chars, inputs) in sort!([x for x in vals])
+for (chars, inputs) in sort!([x for x in vals], by=first)
     #Find all keys with this value
     entry = (
             join(map(c->"U+"*uppercase(hex(c, 5)), collect(chars)), " + "),
