66ef1e7965e74b120dad26d472388687d9e89493
Jesse Johnson <holocronweavers@gmail.com>
2015-07-29 15:07:03 -0400

    Update julia-mode.el for UInt -> Uint.

--- contrib/julia-mode.el
+++ contrib/julia-mode.el
@@ -207,7 +207,7 @@ This function provides equivalent functionality, but makes no efforts to optimis
 (defconst julia-builtin-types-regex
   (julia--regexp-opt
    '("Number" "Real" "BigInt" "Integer"
-     "UInt" "UInt8" "UInt16" "UInt32" "UInt64" "UInt128"
+     "Uint" "Uint8" "Uint16" "Uint32" "Uint64" "Uint128"
      "Int" "Int8" "Int16" "Int32" "Int64" "Int128"
      "BigFloat" "FloatingPoint" "Float16" "Float32" "Float64"
      "Complex128" "Complex64"
