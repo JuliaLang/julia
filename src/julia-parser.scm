03e5920a959e0ebd5622cc73f70b987777e5e711
Yichao Yu <yyc1992@gmail.com>
2015-08-07 07:48:33 -0400

    Only treat true string literals as doc string instead of looking for quote. This should be fine now after we change how tripple quote strings are parsed. Fix #12501

--- src/julia-parser.scm
+++ src/julia-parser.scm
@@ -2072,16 +2072,14 @@
 (define (doc-string-literal? e)
   (or (simple-string-literal? e)
       (and (length= e 3) (eq? (car e) 'macrocall)
-	   (simple-string-literal? (caddr e))
-	   (eq? (cadr e) '@doc_str))))
+           (simple-string-literal? (caddr e))
+           (eq? (cadr e) '@doc_str))))
 
 (define (parse-docstring s production)
-  (let* ((isstr (eqv? (peek-token s) #\"))
-	 (ex    (production s)))
-    (if (and (or isstr (doc-string-literal? ex))
-	     (not (closing-token? (peek-token s))))
-	`(macrocall (|.| Base (quote @doc)) ,ex ,(production s))
-	ex)))
+  (let* ((ex    (production s)))
+    (if (and (doc-string-literal? ex) (not (closing-token? (peek-token s))))
+        `(macrocall (|.| Base (quote @doc)) ,ex ,(production s))
+        ex)))
 
 ; --- main entry point ---
 
