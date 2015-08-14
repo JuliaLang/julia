247f9e3890aee5ad91768bb794b02f86b68a8beb
Jameson Nash <vtjnash@gmail.com>
2015-08-14 12:42:13 -0400

    make int.jl more --compile=all friendly

--- base/printf.jl
+++ base/printf.jl
@@ -6,7 +6,7 @@ export @printf, @sprintf
 
 ### printf formatter generation ###
 const SmallFloatingPoint = Union{Float64,Float32,Float16}
-const SmallNumber = Union{SmallFloatingPoint,Base.Signed64,Base.Unsigned64,UInt128,Int128}
+const SmallNumber = Union{SmallFloatingPoint,Base.Integer64,UInt128,Int128}
 
 function gen(s::AbstractString)
     args = []
