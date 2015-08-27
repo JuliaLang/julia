ac19678cd3be80269491d980bab4e2f7761fe6fd
Stefan Karpinski <stefan@karpinski.org>
2015-08-27 17:24:34 -0400

    use jl_type_error in jl_array_to_string

--- src/array.c
+++ src/array.c
@@ -351,8 +351,8 @@ jl_array_t *jl_pchar_to_array(const char *str, size_t len)
 
 jl_value_t *jl_array_to_string(jl_array_t *a)
 {
-    if (!(jl_typeis(a, jl_array_uint8_type) && jl_array_ndims(a) == 1))
-        jl_error("jl_array_to_string: argument not byte vector");
+    if (!jl_typeis(a, jl_array_uint8_type))
+        jl_type_error("jl_array_to_string", (jl_value_t*)jl_array_uint8_type, (jl_value_t*)a);
     jl_datatype_t *string_type = u8_isvalid((char*)a->data, jl_array_len(a)) == 1 ? // ASCII
         jl_ascii_string_type : jl_utf8_string_type;
     jl_value_t *s = (jl_value_t*)jl_gc_alloc_1w();
