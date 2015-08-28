2ebcd8f4aabded72f59eacb6b9059db9e7a88e17
Jake Bolewski <jakebolewski@gmail.com>
2015-08-28 18:47:15 -0400

    fix manage docstring

--- base/docs/helpdb.jl
+++ base/docs/helpdb.jl
@@ -6006,17 +6006,18 @@ doc"""
 ```rst
 ::
 
-           manage(manager::FooManager, pid::Int, config::WorkerConfig. op::Symbol)
+          manage(manager::FooManager, pid::Int, config::WorkerConfig. op::Symbol)
 
- Implemented by cluster managers. It is called on the master process, during a worker's lifetime,
- with appropriate ``op`` values:
+Implemented by cluster managers. It is called on the master process, during a worker's lifetime,
+with appropriate ``op`` values:
 
-   - with ``:register``/``:deregister`` when a worker is added / removed
-     from the Julia worker pool.
-   - with ``:interrupt`` when ``interrupt(workers)`` is called. The
-     :class:`ClusterManager` should signal the appropriate worker with an
-     interrupt signal.
-   - with ``:finalize`` for cleanup purposes.
+    - with ``:register``/``:deregister`` when a worker is added / removed
+      from the Julia worker pool.
+    - with ``:interrupt`` when ``interrupt(workers)`` is called. The
+      :class:`ClusterManager` should signal the appropriate worker with an
+      interrupt signal.
+    - with ``:finalize`` for cleanup purposes.
+
 ```
 """
 manage
