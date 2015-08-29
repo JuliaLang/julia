ff20985e6970b04dfd30e6f17ca682602ae19524
Jake Bolewski <jakebolewski@gmail.com>
2015-08-28 23:57:03 -0400

    add arguemnts to Pkg methods in helpdb

--- base/docs/helpdb.jl
+++ base/docs/helpdb.jl
@@ -13057,77 +13057,77 @@ doc"""
 
 Run the build scripts for all installed packages in depth-first recursive order.
 """
-Pkg.build
+Pkg.build()
 
 doc"""
     build(pkgs...)
 
 Run the build script in "deps/build.jl" for each package in `pkgs` and all of their dependencies in depth-first recursive order. This is called automatically by `Pkg.resolve()` on all installed or updated packages.
 """
-Pkg.build
+Pkg.build(pkgs...)
 
 doc"""
     init(meta::AbstractString=DEFAULT_META, branch::AbstractString=META_BRANCH)
 
 Initialize `Pkg.dir()` as a package directory. This will be done automatically when the `JULIA_PKGDIR` is not set and `Pkg.dir()` uses its default value. As part of this process, clones a local METADATA git repository from the site and branch specified by its arguments, which are typically not provided. Explicit (non-default) arguments can be used to support a custom METADATA setup.
 """
-Pkg.init
+Pkg.init()
 
 doc"""
     publish()
 
 For each new package version tagged in `METADATA` not already published, make sure that the tagged package commits have been pushed to the repo at the registered URL for the package and if they all have, open a pull request to `METADATA`.
 """
-Pkg.publish
+Pkg.publish()
 
 doc"""
     pin(pkg)
 
 Pin `pkg` at the current version. To go back to using the newest compatible released version, use `Pkg.free(pkg)`
 """
-Pkg.pin
+Pkg.pin(pkg)
 
 doc"""
     pin(pkg, version)
 
 Pin `pkg` at registered version `version`.
 """
-Pkg.pin
+Pkg.pin(pkg, version)
 
 doc"""
     resolve()
 
 Determines an optimal, consistent set of package versions to install or upgrade to. The optimal set of package versions is based on the contents of `Pkg.dir("REQUIRE")` and the state of installed packages in `Pkg.dir()`, Packages that are no longer required are moved into `Pkg.dir(".trash")`.
 """
-Pkg.resolve
+Pkg.resolve()
 
 doc"""
     available() -> Vector{ASCIIString}
 
 Returns the names of available packages.
 """
-Pkg.available
+Pkg.available()
 
 doc"""
     available(pkg) -> Vector{VersionNumber}
 
 Returns the version numbers available for package `pkg`.
 """
-Pkg.available
+Pkg.available(pkg)
 
 doc"""
     register(pkg, [url])
 
 Register `pkg` at the git URL `url`, defaulting to the configured origin URL of the git repo `Pkg.dir(pkg)`.
 """
-Pkg.register
+Pkg.register(pkg)
 
 doc"""
     rm(pkg)
 
 Remove all requirement entries for `pkg` from `Pkg.dir("REQUIRE")` and call `Pkg.resolve()`.
 """
-Pkg.rm
+Pkg.rm(pkg)
 
 doc"""
     free(pkg)
@@ -13136,7 +13136,7 @@ Free the package `pkg` to be managed by the package manager again. It calls `Pkg
 
 You can also supply an iterable collection of package names, e.g., `Pkg.free(("Pkg1", "Pkg2"))` to free multiple packages at once.
 """
-Pkg.free
+Pkg.free()
 
 doc"""
     status()
@@ -13150,28 +13150,28 @@ doc"""
 
 Opens `Pkg.dir("REQUIRE")` in the editor specified by the `VISUAL` or `EDITOR` environment variables; when the editor command returns, it runs `Pkg.resolve()` to determine and install a new optimal set of installed package versions.
 """
-Pkg.edit
+Pkg.edit()
 
 doc"""
     clone(url, [pkg])
 
 Clone a package directly from the git URL `url`. The package does not need to be a registered in `Pkg.dir("METADATA")`. The package repo is cloned by the name `pkg` if provided; if not provided, `pkg` is determined automatically from `url`.
 """
-Pkg.clone
+Pkg.clone(url)
 
 doc"""
     clone(pkg)
 
 If `pkg` has a URL registered in `Pkg.dir("METADATA")`, clone it from that URL on the default branch. The package does not need to have any registered versions.
 """
-Pkg.clone
+Pkg.clone(pkg)
 
 doc"""
     checkout(pkg, [branch="master"])
 
 Checkout the `Pkg.dir(pkg)` repo to the branch `branch`. Defaults to checking out the "master" branch. To go back to using the newest compatible released version, use `Pkg.free(pkg)`
 """
-Pkg.checkout
+Pkg.checkout(pkg)
 
 doc"""
     update()
@@ -13185,63 +13185,63 @@ doc"""
 
 Add a requirement entry for `pkg` to `Pkg.dir("REQUIRE")` and call `Pkg.resolve()`. If `vers` are given, they must be `VersionNumber` objects and they specify acceptable version intervals for `pkg`.
 """
-Pkg.add
+Pkg.add(pkg, vers...)
 
 doc"""
     tag(pkg, [ver, [commit]])
 
 Tag `commit` as version `ver` of package `pkg` and create a version entry in `METADATA`. If not provided, `commit` defaults to the current commit of the `pkg` repo. If `ver` is one of the symbols `:patch`, `:minor`, `:major` the next patch, minor or major version is used. If `ver` is not provided, it defaults to `:patch`.
 """
-Pkg.tag
+Pkg.tag(pkg)
 
 doc"""
     test()
 
 Run the tests for all installed packages ensuring that each package's test dependencies are installed for the duration of the test. A package is tested by running its `test/runtests.jl` file and test dependencies are specified in `test/REQUIRE`.
 """
-Pkg.test
+Pkg.test()
 
 doc"""
     test(pkgs...)
 
 Run the tests for each package in `pkgs` ensuring that each package's test dependencies are installed for the duration of the test. A package is tested by running its `test/runtests.jl` file and test dependencies are specified in `test/REQUIRE`.
 """
-Pkg.test
+Pkg.test(pkgs...)
 
 doc"""
     generate(pkg,license)
 
 Generate a new package named `pkg` with one of these license keys: `"MIT"`, `"BSD"` or `"ASL"`. If you want to make a package with a different license, you can edit it afterwards. Generate creates a git repo at `Pkg.dir(pkg)` for the package and inside it `LICENSE.md`, `README.md`, `REQUIRE`, the julia entrypoint `\$pkg/src/\$pkg.jl`, and Travis and AppVeyor CI configuration files `.travis.yml` and `appveyor.yml`.
 """
-Pkg.generate
+Pkg.generate(pkg,license)
 
 doc"""
     dir() -> AbstractString
 
 Returns the absolute path of the package directory. This defaults to `joinpath(homedir(),".julia","v\$(VERSION.major).\$(VERSION.minor)")` on all platforms (i.e. `~/.julia/v0.4` in UNIX shell syntax). If the `JULIA_PKGDIR` environment variable is set, then that path is used in the returned value as `joinpath(ENV["JULIA_PKGDIR"],"v\$(VERSION.major).\$(VERSION.minor)")`. If `JULIA_PKGDIR` is a relative path, it is interpreted relative to whatever the current working directory is.
 """
-Pkg.dir
+Pkg.dir()
 
 doc"""
     dir(names...) -> AbstractString
 
 Equivalent to `normpath(Pkg.dir(),names...)` – i.e. it appends path components to the package directory and normalizes the resulting path. In particular, `Pkg.dir(pkg)` returns the path to the package `pkg`.
 """
-Pkg.dir
+Pkg.dir(names...)
 
 doc"""
     installed() -> Dict{ASCIIString,VersionNumber}
 
 Returns a dictionary mapping installed package names to the installed version number of each package.
 """
-Pkg.installed
+Pkg.installed()
 
 doc"""
     installed(pkg) -> Void | VersionNumber
 
 If `pkg` is installed, return the installed version number, otherwise return `nothing`.
 """
-Pkg.installed
+Pkg.installed(pkg)
 
 doc"""
     randjump(r::MersenneTwister, jumps, [jumppoly]) -> Vector{MersenneTwister}
