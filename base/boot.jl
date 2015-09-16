JuliaLang/julia

commit 11009e152383de6f5ce665d537200b2a43f36a07

TypeError is needed by type-intersection during --compile=all

base/boot.jl
@@ -141,7 +141,7 @@ export
     # errors
     BoundsError, DivideError, DomainError, Exception, InexactError,
     InterruptException, OutOfMemoryError, ReadOnlyMemoryError, OverflowError,
-    StackOverflowError, SegmentationFault, UndefRefError, UndefVarError,
+    StackOverflowError, SegmentationFault, UndefRefError, UndefVarError, TypeError,
     # AST representation
     Expr, GotoNode, LabelNode, LineNumberNode, QuoteNode, SymbolNode, TopNode,
     GlobalRef, NewvarNode, GenSym,
@@ -209,8 +209,20 @@ else
     typealias UInt UInt32
 end
 
-abstract Exception
+abstract AbstractString
+abstract DirectIndexString <: AbstractString
 
+immutable ASCIIString <: DirectIndexString
+    data::Array{UInt8,1}
+end
+
+immutable UTF8String <: AbstractString
+    data::Array{UInt8,1}
+end
+
+typealias ByteString Union{ASCIIString,UTF8String}
+
+abstract Exception
 immutable BoundsError        <: Exception
     a::Any
     i::Any
@@ -231,25 +243,18 @@ immutable UndefVarError      <: Exception
     var::Symbol
 end
 immutable InterruptException <: Exception end
-
-abstract AbstractString
-abstract DirectIndexString <: AbstractString
+type TypeError <: Exception
+    func::Symbol
+    context::AbstractString
+    expected::Type
+    got
+end
 
 type SymbolNode
     name::Symbol
     typ
     SymbolNode(name::Symbol, t::ANY) = new(name, t)
 end
-
-immutable ASCIIString <: DirectIndexString
-    data::Array{UInt8,1}
-end
-
-immutable UTF8String <: AbstractString
-    data::Array{UInt8,1}
-end
-
-typealias ByteString Union{ASCIIString,UTF8String}
 
 include(fname::ByteString) = ccall(:jl_load_, Any, (Any,), fname)
 

base/base.jl
@@ -11,13 +11,6 @@ type SystemError <: Exception
     SystemError(p::AbstractString) = new(p, Libc.errno())
 end
 
-type TypeError <: Exception
-    func::Symbol
-    context::AbstractString
-    expected::Type
-    got
-end
-
 type ParseError <: Exception
     msg::AbstractString
 end

base/essentials.jl
@@ -32,6 +32,7 @@ call(T::Type{SegmentationFault}) = Core.call(T)
 call(T::Type{UndefRefError}) = Core.call(T)
 call(T::Type{UndefVarError}, var::Symbol) = Core.call(T, var)
 call(T::Type{InterruptException}) = Core.call(T)
+call(T::Type{TypeError}, func::Symbol, context::AbstractString, expected::Type, got) = Core.call(T, func, context, expected, got)
 call(T::Type{SymbolNode}, name::Symbol, t::ANY) = Core.call(T, name, t)
 call(T::Type{GlobalRef}, modu, name::Symbol) = Core.call(T, modu, name)
 call(T::Type{ASCIIString}, d::Array{UInt8,1}) = Core.call(T, d)

src/init.c
@@ -711,6 +711,7 @@ void jl_get_builtin_hooks(void)
     jl_boundserror_type    = (jl_datatype_t*)core("BoundsError");
     jl_memory_exception    = jl_new_struct_uninit((jl_datatype_t*)core("OutOfMemoryError"));
     jl_readonlymemory_exception = jl_new_struct_uninit((jl_datatype_t*)core("ReadOnlyMemoryError"));
+    jl_typeerror_type = (jl_datatype_t*)core("TypeError");
 
 #ifdef SEGV_EXCEPTION
     jl_segv_exception      = jl_new_struct_uninit((jl_datatype_t*)core("SegmentationFault"));
@@ -731,7 +732,6 @@ DLLEXPORT void jl_get_system_hooks(void)
 
     jl_errorexception_type = (jl_datatype_t*)basemod("ErrorException");
     jl_argumenterror_type = (jl_datatype_t*)basemod("ArgumentError");
-    jl_typeerror_type = (jl_datatype_t*)basemod("TypeError");
     jl_methoderror_type = (jl_datatype_t*)basemod("MethodError");
     jl_loaderror_type = (jl_datatype_t*)basemod("LoadError");
     jl_complex_type = (jl_datatype_t*)basemod("Complex");

src/toplevel.c
@@ -138,7 +138,6 @@ jl_value_t *jl_eval_module_expr(jl_expr_t *ex)
         // to pick up new types from Base
         jl_errorexception_type = NULL;
         jl_argumenterror_type = NULL;
-        jl_typeerror_type = NULL;
         jl_methoderror_type = NULL;
         jl_loaderror_type = NULL;
         jl_current_task->tls = jl_nothing; // may contain an entry for :SOURCE_FILE that is not valid in the new base

commit 633141f950b61bc6b905724d8ef72ff62057463e

fix mysterious compiler performance regression
11009e152383de6f5ce665d537200b2a43f36a07 caused a large compiler
performance regression for some code. By isolating each piece of the
change, @vtjnash and I determined that this seemingly meaningless
movement of declarations in boot.jl was the cause. After poking at it
for a couple hours we still have no idea why. It's possibly related
to the order of methods in Core.call.

base/boot.jl
@@ -211,17 +211,6 @@ else
 end
 
 abstract AbstractString
-abstract DirectIndexString <: AbstractString
-
-immutable ASCIIString <: DirectIndexString
-    data::Array{UInt8,1}
-end
-
-immutable UTF8String <: AbstractString
-    data::Array{UInt8,1}
-end
-
-typealias ByteString Union{ASCIIString,UTF8String}
 
 abstract Exception
 immutable BoundsError        <: Exception
@@ -256,6 +245,18 @@ type SymbolNode
     typ
     SymbolNode(name::Symbol, t::ANY) = new(name, t)
 end
+
+abstract DirectIndexString <: AbstractString
+
+immutable ASCIIString <: DirectIndexString
+    data::Array{UInt8,1}
+end
+
+immutable UTF8String <: AbstractString
+    data::Array{UInt8,1}
+end
+
+typealias ByteString Union{ASCIIString,UTF8String}
 
 include(fname::ByteString) = ccall(:jl_load_, Any, (Any,), fname)
 