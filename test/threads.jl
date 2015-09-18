5658b35380afa895e93ee10f63c0b393f3efd680
Kiran Pamnany <kiran.pamnany@intel.com>
2015-09-18 14:20:34 -0700

    Refactor threading stuff into Base.Threads
    The Threads module now includes the threading constructs (@threads),
    the thread management functions (threadid(), maxthreads(), nthreads()),
    atomics (Atomic, atomic_cas!, atomic_xchg!, etc.), and locks (SpinLock,
    RecursiveSpinLock, Mutex, lock! and unlock!).
    
    Some test cases are in test/threads.jl. Performance tests are updated
    in test/perf/threads/.

--- test/threads.jl
+++ test/threads.jl
@@ -1,47 +1,107 @@
 using Base.Test
-using Base.Threading
+using Base.Threads
 
-expected = [1:nthreads();]
+# threading constructs
 
-# test 1
-arr = zeros(Int16, nthreads())
-
-function foo(A)
+# parallel call form
+function threaded_call(A)
     tid = threadid()
     A[tid] = tid
 end
 
-@time @threads all foo(arr)
+function test_threaded_call()
+    expected = collect(1:nthreads())
+    arr = zeros(Int16, nthreads())
+    @threads all threaded_call(arr)
+    @test arr == expected
+end
 
-@test arr == expected
+test_threaded_call()
 
-
-# test 2
-arr = zeros(Int16, nthreads())
-
-function bar(A)
+# parallel loop form
+function threaded_loop(A)
     @threads all for i = 1:nthreads()
         tid = threadid()
         A[i] = tid
     end
 end
 
-@time bar(arr)
+function test_threaded_loop()
+    expected = collect(1:nthreads())
+    arr = zeros(Int16, nthreads())
+    threaded_loop(arr)
+    @test arr == expected
+end
 
-@test arr == expected
+test_threaded_loop()
 
-
-# test 3
-arr = zeros(Int16, nthreads())
-
-function baz(A)
+# parallel block form
+function threaded_block(A)
     @threads all begin
         tid = threadid()
         A[tid] = tid
     end
 end
 
-@time baz(arr)
+function test_threaded_block()
+    expected = collect(1:nthreads())
+    arr = zeros(Int16, nthreads())
+    threaded_block(arr)
+    @test arr == expected
+end
 
-@test arr == expected
+test_threaded_block()
+
+# parallel atomic addition
+function threaded_atomic_add(x, n)
+    @threads all for i = 1:n
+        atomic_add!(x, 1)
+    end
+end
+
+function test_threaded_atomic_add()
+    x = Atomic()
+    threaded_atomic_add(x, 10000)
+    @test x[] == 10000
+end
+
+test_threaded_atomic_add()
+
+# spin locks
+function threaded_add_using_spinlock(s, x, n)
+    @threads all for i = 1:n
+        lock!(s)
+        x = x + 1
+        unlock!(s)
+    end
+    return x
+end
+
+function test_spinlock()
+    s = SpinLock()
+    x = 0
+    x = threaded_add_using_spinlock(s, x, 10000)
+    @test x == 10000
+end
+
+test_spinlock()
+
+# mutexes
+function threaded_add_using_mutex(m, x, n)
+    @threads all for i = 1:n
+        lock!(m)
+        x = x + 1
+        unlock!(m)
+    end
+    return x
+end
+
+function test_mutex()
+    m = Mutex()
+    x = 0
+    x = threaded_add_using_mutex(m, x, 10000)
+    @test x == 10000
+end
+
+test_mutex()
 
