08061be840f05865215ed1e9b7d06c15f7197edc
Jeff Bezanson <jeff.bezanson@gmail.com>
2015-09-18 14:03:57 -0400

    remove jl_alloca; now unused

--- base/inference.jl
+++ base/inference.jl
@@ -1989,8 +1989,7 @@ function is_pure_builtin(f)
         if !(f === Intrinsics.pointerref || # this one is volatile
              f === Intrinsics.pointerset || # this one is never effect-free
              f === Intrinsics.ccall ||      # this one is never effect-free
-             f === Intrinsics.llvmcall ||   # this one is never effect-free
-             f === Intrinsics.jl_alloca)
+             f === Intrinsics.llvmcall)     # this one is never effect-free
             return true
         end
     end
