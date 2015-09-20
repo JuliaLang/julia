ce93cfe975f2e5da9be61e496bbac981e0ad1f65
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-09-20 13:39:07 +0200

    move some sparse docs from helpdb.jl
    (cherry picked from commit 0a44ce8177def25b3003490171a1c19a49156640)
    ref #13236

--- base/sparse/abstractsparse.jl
+++ base/sparse/abstractsparse.jl
@@ -5,6 +5,11 @@ abstract AbstractSparseArray{Tv,Ti,N} <: AbstractArray{Tv,N}
 typealias AbstractSparseVector{Tv,Ti} AbstractSparseArray{Tv,Ti,1}
 typealias AbstractSparseMatrix{Tv,Ti} AbstractSparseArray{Tv,Ti,2}
 
+"""
+    issparse(S)
+
+Returns `true` if `S` is sparse, and `false` otherwise.
+"""
 issparse(A::AbstractArray) = false
 issparse(S::AbstractSparseArray) = true
 
--- base/sparse/csparse.jl
+++ base/sparse/csparse.jl
@@ -12,6 +12,16 @@
 # Based on Direct Methods for Sparse Linear Systems, T. A. Davis, SIAM, Philadelphia, Sept. 2006.
 # Section 2.4: Triplet form
 # http://www.cise.ufl.edu/research/sparse/CSparse/
+
+"""
+    sparse(I,J,V,[m,n,combine])
+
+Create a sparse matrix `S` of dimensions `m x n` such that `S[I[k], J[k]] = V[k]`.
+The `combine` function is used to combine duplicates. If `m` and `n` are not
+specified, they are set to `maximum(I)` and `maximum(J)` respectively. If the
+`combine` function is not supplied, duplicates are added by default. All elements
+of `I` must satisfy `1 <= I[k] <= m`, and all elements of `J` must satisfy `1 <= J[k] <= n`.
+"""
 function sparse{Tv,Ti<:Integer}(I::AbstractVector{Ti},
                                 J::AbstractVector{Ti},
                                 V::AbstractVector{Tv},
@@ -195,6 +205,13 @@ end
 # A root node is indicated by 0. This tree may actually be a forest in that
 # there may be more than one root, indicating complete separability.
 # A trivial example is speye(n, n) in which every node is a root.
+
+"""
+    etree(A[, post])
+
+Compute the elimination tree of a symmetric sparse matrix `A` from `triu(A)`
+and, optionally, its post-ordering permutation.
+"""
 function etree{Tv,Ti}(A::SparseMatrixCSC{Tv,Ti}, postorder::Bool)
     m,n = size(A)
     Ap = A.colptr
