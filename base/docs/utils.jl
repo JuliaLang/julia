035c10ddbcb6485279863817ff3accd8af7ac460
Michael Hatherly <michaelhatherly@gmail.com>
2015-09-27 10:15:53 +0200

    Simplify `@repl` implementation.
    Enable `apropos` search via help mode by passing a string or regex:
    
        help?> "foo"
        help?> r"foo"
    
    This shouldn't interfere with any docstring lookup since we don't encourage
    documenting specific strings or regex objects. Docs for `@r_str` can still
    be found by searching for either an empty regex `r""` or `@r_str` directly.
    
    Enable specifying method signatures using
    
        help?> f(::Float64, ::Int)
    
    as well as the current behaviour
    
        help?> f(x, y)
    
    where `x` and `y` are already defined. This is quite useful when you don't
    have an instance of the required type. Prior to this an error was thrown
    in `gen_call_with_extracted_types` when using the `::` syntax.
    
    (cherry picked from commit 8c236e96d243b8396a5901ee22388dd75bf78c1d)

--- base/docs/utils.jl
+++ base/docs/utils.jl
@@ -110,21 +110,40 @@ end
 
 repl_corrections(s) = repl_corrections(STDOUT, s)
 
-macro repl(ex)
+macro repl(ex) repl(ex) end
+
+function repl(s::Symbol)
     quote
-        # Fuzzy Searching
-        $(isexpr(ex, Symbol)) && repl_search($(string(ex)))
-        if $(isa(ex, Symbol)) &&
-                !(isdefined($(current_module()), $(Expr(:quote, ex))) ||
-                  haskey(keywords, $(Expr(:quote, ex))))
-            repl_corrections($(string(ex)))
-        else
-            if $(isfield(ex) ? :(isa($(esc(ex.args[1])), DataType)) : false)
-                $(isfield(ex) ? :(fielddoc($(esc(ex.args[1])), $(ex.args[2]))) : nothing)
+        repl_search($(string(s)))
+        ($(isdefined(s) || haskey(keywords, s))) || repl_corrections($(string(s)))
+        $(_repl(s))
+    end
+end
+
+isregex(x) = isexpr(x, :macrocall, 2) && x.args[1] == symbol("@r_str") && !isempty(x.args[2])
+
+repl(ex::Expr) = isregex(ex) ? :(apropos($ex)) : _repl(ex)
+
+repl(str::AbstractString) = :(apropos($str))
+
+repl(other) = nothing
+
+function _repl(x)
+    docs = :(@doc $(esc(x)))
+    try
+        # Handles function call syntax where each argument is a symbol.
+        isexpr(x, :call) && (docs = Base.gen_call_with_extracted_types(doc, x))
+    end
+    if isfield(x)
+        quote
+            if isa($(esc(x.args[1])), DataType)
+                fielddoc($(esc(x.args[1])), $(esc(x.args[2])))
             else
-                $((isa(ex,Symbol) || isfield(ex) || isexpr(ex,:macrocall)) ? :(@doc ($(esc(ex)))) : Base.gen_call_with_extracted_types(doc, ex))
+                $docs
             end
         end
+    else
+        docs
     end
 end
 
