fb032d893bcebafaf6a67b7cecaef6290436caa0
Tony Kelman <tony@kelman.net>
2015-10-02 02:38:04 -0400

    Actually throw PkgErrors, add Pkg error tests
    Add an unsatisfiable REQUIRES test
    
    Add tests for Pkg.pin error cases
    
    Adjust comment for issue number in a test
    
    (cherry picked from commit 9d368d4fdb3b5634a638ed388ebea982d371dd92)
    ref #13420

--- test/pkg.jl
+++ test/pkg.jl
@@ -1,3 +1,5 @@
+# This file is a part of Julia. License is MIT: http://julialang.org/license
+
 function temp_pkg_dir(fn::Function)
     # Used in tests below to setup and teardown a sandboxed package directory
     const tmpdir = ENV["JULIA_PKGDIR"] = joinpath(tempdir(),randstring())
@@ -20,6 +22,36 @@ temp_pkg_dir() do
     @test [keys(Pkg.installed())...] == ["Example"]
     Pkg.rm("Example")
     @test isempty(Pkg.installed())
+
+    # adding a package with unsatisfiable julia version requirements (REPL.jl) errors
+    try
+        Pkg.add("REPL")
+        error("unexpected")
+    catch err
+        @test err.msg == "REPL can't be installed because " *
+            "it has no versions that support $VERSION of julia. You may " *
+            "need to update METADATA by running `Pkg.update()`"
+    end
+
+    # trying to add, check availability, or pin a nonexistent package errors
+    try
+        Pkg.add("NonexistentPackage")
+        error("unexpected")
+    catch err
+        @test err.msg == "unknown package NonexistentPackage"
+    end
+    try
+        Pkg.available("NonexistentPackage")
+        error("unexpected")
+    catch err
+        @test err.msg == "NonexistentPackage is not a package (not registered or installed)"
+    end
+    try
+        Pkg.pin("NonexistentPackage", v"1.0.0")
+        error("unexpected")
+    catch err
+        @test err.msg == "NonexistentPackage is not a git repo"
+    end
 end
 
 # testing a package with test dependencies causes them to be installed for the duration of the test
@@ -55,6 +87,7 @@ temp_pkg_dir() do
 
     try
         Pkg.test("PackageWithNoTests")
+        error("unexpected")
     catch err
         @test err.msg == "PackageWithNoTests did not provide a test/runtests.jl file"
     end
@@ -72,8 +105,15 @@ temp_pkg_dir() do
 
     try
         Pkg.test("PackageWithFailingTests")
+        error("unexpected")
     catch err
         @test err.msg == "PackageWithFailingTests had test errors"
     end
 end
 
+# issue #13374
+temp_pkg_dir() do
+    Pkg.generate("Foo", "MIT")
+    Pkg.tag("Foo")
+    Pkg.tag("Foo")
+end
