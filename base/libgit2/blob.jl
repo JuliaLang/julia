b2fc048652b24141896ddb8cf030d77ed6db33a2
Jake Bolewski <jakebolewski@gmail.com>
2015-10-15 15:41:59 -0400

    fix some issues with Pkg.pin, add some tests
    closes #13520

--- base/libgit2/blob.jl
+++ base/libgit2/blob.jl
@@ -4,10 +4,6 @@ function content(blob::GitBlob)
     return ccall((:git_blob_rawcontent, :libgit2), Ptr{Void}, (Ptr{Void},), blob.ptr)
 end
 
-function isbinary(blob::GitBlob)
-    return ccall((:git_blob_is_binary, :libgit2), CInt, (Ptr{Void},), blob.ptr) == 1
-end
-
 function Base.length(blob::GitBlob)
     return ccall((:git_blob_rawsize, :libgit2), Coff_t, (Ptr{Void},), blob.ptr)
 end
