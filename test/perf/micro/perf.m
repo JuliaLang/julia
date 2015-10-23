de2e1449c894abf391bcc0a5fbbe81ade0bab930
Aslak Grinsted <ag@glaciology.net>
2015-10-23 11:08:38 +0200

    Vectorize matlab parseint to improve performance
    But I realize that all other parseint tests are written using loops, so I am not sure this edit is appropriate. But if not then I think you should add comments in the code to explain why it is written as it is, because it really stands out in the performance comparison table.

--- test/perf/micro/perf.m
+++ test/perf/micro/perf.m
@@ -90,12 +90,10 @@ end
 %% parse int %%
 
 function n = parseintperf(t)
-    for i = 1:t
-        n = randi([0,2^32-1],1,'uint32');
-        s = dec2hex(n);
-        m = hex2dec(s);
-        assert(m == n);
-    end
+    n = randi([0,2^32-1],t,1,'uint32');
+    s = dec2hex(n);
+    m = hex2dec(s);
+    assert(m == n);
 end
 
 %% matmul and transpose %%
