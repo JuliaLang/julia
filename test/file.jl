1a45e82ac1f0d1fd53452b15ec4152b22d9f82a1
Kenta Sato <bicycle1885@gmail.com>
2015-10-25 15:57:54 +0900

    fix the read and write methods of DevNull

--- test/file.jl
+++ test/file.jl
@@ -1091,3 +1091,16 @@ function test_read_nbyte()
     rm(fn)
 end
 test_read_nbyte()
+
+# DevNull
+@test !isreadable(DevNull)
+@test iswritable(DevNull)
+@test isopen(DevNull)
+@test write(DevNull, 0xff) == 0
+@test write(DevNull, 1234) == 0
+@test_throws EOFError read(DevNull, UInt8)
+@test close(DevNull) === nothing
+@test flush(DevNull) === nothing
+@test copy(DevNull) === DevNull
+@test eof(DevNull)
+@test print(DevNull, "go to /dev/null") === nothing
