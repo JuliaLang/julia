a3b010922b46f7358cb214db1b9d75d6a03119b1
Lachlan Gunn <lachlan@twopif.net>
2015-11-03 23:52:23 +0100

    Added URL rewriting to Pkg.

--- base/pkg/cache.jl
+++ base/pkg/cache.jl
@@ -5,6 +5,11 @@ module Cache
 import ...LibGit2, ..Dir, ...Pkg.PkgError
 using ..Types
 
+rewrite_url_to = "https"
+
+const GITHUB_REGEX =
+    r"^(?:git@|git://|https://(?:[\w\.\+\-]+@)?)github.com[:/](([^/].+)/(.+?))(?:\.git)?$"i
+
 path(pkg::AbstractString) = abspath(".cache", pkg)
 
 function mkcachedir()
@@ -30,23 +35,24 @@ end
 
 function prefetch(pkg::AbstractString, url::AbstractString, sha1s::Vector)
     isdir(".cache") || mkcachedir()
-    #TODO: force switch to https
-    #url = LibGit2.normalize_url(url)
+
     cache = path(pkg)
+    normalized_url = normalize_url(url)
+
     repo = if isdir(cache)
         LibGit2.GitRepo(cache) # open repo, free it at the end
     else
-        info("Cloning cache of $pkg from $url")
+        info("Cloning cache of $pkg from $normalized_url")
         try
             # clone repo, free it at the end
-            LibGit2.clone(url, cache, isbare = true, remote_cb = LibGit2.mirror_cb())
+            LibGit2.clone(normalized_url, cache, isbare = true, remote_cb = LibGit2.mirror_cb())
         catch err
             isdir(cache) && rm(cache, recursive=true)
-            throw(PkgError("Cannot clone $pkg from $url. $(err.msg)"))
+            throw(PkgError("Cannot clone $pkg from $normalized_url. $(err.msg)"))
         end
     end
     try
-        LibGit2.set_remote_url(repo, url)
+        LibGit2.set_remote_url(repo, normalized_url)
         in_cache = BitArray(map(sha1->LibGit2.iscommit(sha1, repo), sha1s))
         if !all(in_cache)
             info("Updating cache of $pkg...")
@@ -60,5 +66,24 @@ function prefetch(pkg::AbstractString, url::AbstractString, sha1s::Vector)
 end
 prefetch(pkg::AbstractString, url::AbstractString, sha1::AbstractString...) =
     prefetch(pkg, url, AbstractString[sha1...])
+
+function setprotocol!(proto::AbstractString)
+    global rewrite_url_to
+
+    if length(proto) == 0
+        rewrite_url_to = nothing
+    else
+        rewrite_url_to = proto
+    end
+end
+
+function normalize_url(url::AbstractString)
+    global rewrite_url_to
+
+    m = match(GITHUB_REGEX,url)
+    (m === nothing || rewrite_url_to === nothing) ?
+        url : "$rewrite_url_to://github.com/$(m.captures[1]).git"
+
+end
 
 end # module
