84e808118e8b5dc257624d1d62178a67814cd502
Andreas Noack <andreasnoackjensen@gmail.com>
2015-11-04 13:51:53 -0500

    Remove a LaTeXism in documentation for ordschur(GeneralizedSchur)

--- base/linalg/schur.jl
+++ base/linalg/schur.jl
@@ -128,7 +128,7 @@ end
 doc"""
     ordschur(F::GeneralizedSchur, select::Union{Vector{Bool},BitVector}) -> F::GeneralizedSchur
 
-Reorders the Generalized Schur factorization `F` of a matrix pair `(A, B) = (Q*S*Z^{H}, Q*T*Z^{H})` according to the logical array `select` and returns a GeneralizedSchur object `F`.  The selected eigenvalues appear in the leading diagonal of both `F[:S]` and F[:T]`, and the left and right orthogonal/unitary Schur vectors are also reordered such that `(A, B) = F[:Q]*(F[:S], F[:T])*F[:Z]^{H}` still holds and the generalized eigenvalues of `A` and `B` can still be obtained with `F[:alpha]./F[:beta]`.
+Reorders the Generalized Schur factorization `F` of a matrix pair `(A, B) = (Q*S*Z', Q*T*Z')` according to the logical array `select` and returns a GeneralizedSchur object `F`.  The selected eigenvalues appear in the leading diagonal of both `F[:S]` and `F[:T]`, and the left and right orthogonal/unitary Schur vectors are also reordered such that `(A, B) = F[:Q]*(F[:S], F[:T])*F[:Z]'` still holds and the generalized eigenvalues of `A` and `B` can still be obtained with `F[:alpha]./F[:beta]`.
 """
 ordschur{Ty<:BlasFloat}(gschur::GeneralizedSchur{Ty}, select::Union{Vector{Bool},BitVector}) = GeneralizedSchur(ordschur(gschur.S, gschur.T, gschur.Q, gschur.Z, select)...)
 
