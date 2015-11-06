JuliaLang/julia

commit 62027cd50a1f67b5a81be4c1ecf4a829d278e589

Add qr(::AbstractVector), qr!(::AbstractVector)
Compute polar decomposition of vector

`qr[!]` is equivalent to `v->(normalize[!](v), norm(v))` but is
convenient for reusing the norm calculation, e.g. for Krylov subspace
algorithms.

Also refactors the in place normalization code to a separate routine,
`__normalize!()`, so that it can be reused by `qr!`

base/linalg/generic.jl
@@ -547,19 +547,22 @@ Normalize the vector `v` in-place with respect to the `p`-norm.
 
 # See also
 
-`normalize`
+`normalize`, `qr`
 
 """
 function normalize!(v::AbstractVector, p::Real=2)
     nrm = norm(v, p)
+    __normalize!(v, nrm)
+end
 
+@inline function __normalize!{T<:AbstractFloat}(v::AbstractVector{T}, nrm::T)
     #The largest positive floating point number whose inverse is less than
     #infinity
-    const δ = inv(prevfloat(typemax(float(nrm))))
+    const δ = inv(prevfloat(typemax(T)))
 
     if nrm ≥ δ #Safe to multiply with inverse
         invnrm = inv(nrm)
-        scale!(v, invrm)
+        scale!(v, invnrm)
 
     else #Divide by norm; slower but more correct
         #Note 2015-10-19: As of Julia 0.4, @simd does not vectorize floating
@@ -590,7 +593,7 @@ Normalize the vector `v` with respect to the `p`-norm.
 
 # See also
 
-`normalize!`
+`normalize!`, `qr`
 """
 normalize(v::AbstractVector, p::Real=2) = v/norm(v, p)
 

base/linalg/qr.jl
@@ -108,6 +108,49 @@ function _qr(A::Union{Number, AbstractMatrix}, ::Type{Val{true}}; thin::Bool=tru
     full(getq(F), thin=thin), F[:R]::Matrix{eltype(F)}, F[:p]::Vector{BlasInt}
 end
 
+"""
+    qr(v::AbstractVector)
+
+Computes the polar decomposition of a vector.
+
+# Input
+- `v::AbstractVector` - vector to normalize
+
+# Outputs
+- `w` - A unit vector in the direction of `v`
+- `r` - The norm of `v`
+
+# See also
+
+`normalize`, `normalize!`, `qr!`
+"""
+function qr(v::AbstractVector)
+    nrm = norm(v)
+    v/nrm, nrm
+end
+
+"""
+    qr!(v::AbstractVector)
+
+Computes the polar decomposition of a vector.
+
+# Input
+- `v::AbstractVector` - vector to normalize
+
+# Outputs
+- `w` - A unit vector in the direction of `v`
+- `r` - The norm of `v`
+
+# See also
+
+`normalize`, `normalize!`, `qr`
+"""
+function qr!(v::AbstractVector)
+    nrm = norm(v)
+    __normalize!(v, nrm), nrm
+end
+
+
 convert{T}(::Type{QR{T}},A::QR) = QR(convert(AbstractMatrix{T}, A.factors), convert(Vector{T}, A.τ))
 convert{T}(::Type{Factorization{T}}, A::QR) = convert(QR{T}, A)
 convert{T}(::Type{QRCompactWY{T}},A::QRCompactWY) = QRCompactWY(convert(AbstractMatrix{T}, A.factors), convert(AbstractMatrix{T}, A.T))

test/linalg/generic.jl
@@ -157,17 +157,17 @@ let
     w = normalize(v)
     @test w == [0.6, 0.8]
     @test norm(w) === 1.0
-    @test normalize!(v) == w
+    @test norm(normalize!(v) - w, Inf) < eps()
 end
 
 #Test potential overflow in normalize!
 let
-    δ = inv(prevfloat(typemax(float(nrm))))
+    δ = inv(prevfloat(typemax(Float64)))
     v = [δ, -δ]
 
     @test norm(v) === 7.866824069956793e-309
     w = normalize(v)
-    @test norm(w) === 1.0
     @test w ≈ [1/√2, -1/√2]
-    @test normalize!(v) == w
+    @test norm(w) === 1.0
+    @test norm(normalize!(v) - w, Inf) < eps()
 end

test/linalg/qr.jl
@@ -162,3 +162,11 @@ let
     Q=full(qrfact(A)[:Q])
     @test vecnorm(A-Q) < eps()
 end
+
+let
+    debug && println("qr on AbstractVector")
+
+    v = [3.0, 4.0]
+    @test qr(v) == ([0.6, 0.8], 5.0)
+end
+

commit 3160e54edc761ba4bc844e28beb26094141da0a3

You can't have titles in function descs in RST – it's a SEVERE error
Introduced in 62027cd50a1f67b5a81be4c1ecf4a829d278e589

base/linalg/generic.jl
@@ -542,17 +542,17 @@ end
 
 Normalize the vector `v` in-place with respect to the `p`-norm.
 
-# Inputs
+Inputs:
 
 - `v::AbstractVector` - vector to be normalized
 - `p::Real` - The `p`-norm to normalize with respect to. Default: 2
 
-# Output
+Output:
 
 - `v` - A unit vector being the input vector, rescaled to have norm 1.
         The input vector is modified in-place.
 
-# See also
+See also:
 
 `normalize`, `qr`
 
@@ -585,16 +585,16 @@ end
 
 Normalize the vector `v` with respect to the `p`-norm.
 
-# Inputs
+Inputs:
 
 - `v::AbstractVector` - vector to be normalized
 - `p::Real` - The `p`-norm to normalize with respect to. Default: 2
 
-# Output
+Output:
 
 - `v` - A unit vector being a copy of the input vector, scaled to have norm 1
 
-# See also
+See also:
 
 `normalize!`, `qr`
 """

base/linalg/qr.jl
@@ -113,14 +113,16 @@ end
 
 Computes the polar decomposition of a vector.
 
-# Input
+Input:
+
 - `v::AbstractVector` - vector to normalize
 
-# Outputs
+Outputs:
+
 - `w` - A unit vector in the direction of `v`
 - `r` - The norm of `v`
 
-# See also
+See also:
 
 `normalize`, `normalize!`, `LinAlg.qr!`
 """
@@ -141,14 +143,16 @@ end
 Computes the polar decomposition of a vector. Instead of returning a new vector
 as `qr(v::AbstractVector)`, this function mutates the input vector `v` in place.
 
-# Input
+Input:
+
 - `v::AbstractVector` - vector to normalize
 
-# Outputs
+Outputs:
+
 - `w` - A unit vector in the direction of `v` (This is a mutation of `v`).
 - `r` - The norm of `v`
 
-# See also
+See also:
 
 `normalize`, `normalize!`, `qr`
 """