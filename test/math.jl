cc31d44afe5e713c8b72c92a0b9a94a191984a65
Katie Hyatt <kshyatt@physics.ucsb.edu>
2015-11-13 18:33:38 -0800

    Few more math tests
    (cherry picked from commit 3a9e9941fa460a4e0fcb142e7c0939c5d07a2f84)
    ref #13985

--- test/math.jl
+++ test/math.jl
@@ -166,12 +166,15 @@ for T in (Float32, Float64)
     @test isnan(log1p(convert(T,NaN)))
     @test_throws DomainError log1p(convert(T,-2.0))
 end
+@test_approx_eq exp10(5) exp10(5.0)
 
 for T in (Int, Float64, BigFloat)
     @test_approx_eq deg2rad(T(180)) 1pi
     @test_approx_eq deg2rad(T[45, 60]) [pi/T(4), pi/T(3)]
     @test_approx_eq rad2deg([pi/T(4), pi/T(3)]) [45, 60]
     @test_approx_eq rad2deg(T(1)*pi) 180
+    @test_approx_eq rad2deg(T(1)) rad2deg(true)
+    @test_approx_eq deg2rad(T(1)) deg2rad(true)
 end
 
 # degree-based trig functions
