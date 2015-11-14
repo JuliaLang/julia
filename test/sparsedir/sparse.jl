10e09f341a02b2e6daea5e7ccba73acf0910a935
kshyatt <kshyatt@physics.ucsb.edu>
2015-11-14 13:51:22 -0800

    Tests for issparse and indtype

--- test/sparsedir/sparse.jl
+++ test/sparsedir/sparse.jl
@@ -2,6 +2,10 @@
 
 using Base.Test
 
+@test issparse(sparse(ones(5,5)))
+@test !issparse(ones(5,5))
+@test Base.SparseArrays.indtype(sparse(ones(Int8,2),ones(Int8,2),rand(2))) == Int8
+
 # check sparse matrix construction
 @test isequal(full(sparse(complex(ones(5,5),ones(5,5)))), complex(ones(5,5),ones(5,5)))
 
