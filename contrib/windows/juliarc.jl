c225fb0d05f5a6011cfc4c7d7b0ac23cc6f84dac
Tony Kelman <tony@kelman.net>
2015-11-30 23:38:38 -0800

    Take Git/usr/bin off the path on Windows
    to hopefully fix starting the binaries in Cygwin or MSYS2
    ref https://groups.google.com/forum/#!msg/julia-users/tCfrhMWBEwk/K4-nde_1BQAJ
    
    adjust paths in spawn test so it still passes

--- contrib/windows/juliarc.jl
+++ contrib/windows/juliarc.jl
@@ -1,4 +1,3 @@
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
-ENV["PATH"] = JULIA_HOME*";"*joinpath(JULIA_HOME,"..","Git","bin")*";"*
-    joinpath(JULIA_HOME,"..","Git","usr","bin")*";"*ENV["PATH"]
+ENV["PATH"] = JULIA_HOME*";"*joinpath(JULIA_HOME,"..","Git","bin")*";"*ENV["PATH"]
