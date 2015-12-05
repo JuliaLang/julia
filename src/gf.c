df9875c7376f75a87ec805d61c2bc857e0597d2e
Keno Fischer <kfischer+github@college.harvard.edu>
2015-12-05 13:17:31 -0500

    Fix incorrect write barrier

--- src/gf.c
+++ src/gf.c
@@ -1577,7 +1577,7 @@ static jl_value_t *all_p2c(jl_value_t *ast, jl_svec_t *tvars)
         jl_gc_wb(unspec->linfo, unspec->linfo->ast);
         parameters_to_closureenv(unspec->linfo->ast, tvars); // move sparams to closure env
         unspec->linfo->ast = all_p2c(unspec->linfo->ast, tvars);
-        jl_gc_wb(unspec, unspec->linfo->ast);
+        jl_gc_wb(unspec->linfo, unspec->linfo->ast);
         if (jl_array_len(jl_lam_staticparams((jl_expr_t*)unspec->linfo->ast)) == 0)
             // mark this as compilable; otherwise, would need to make an unspecialized version of
             // this unspecialized function to handle all of the static parameters
