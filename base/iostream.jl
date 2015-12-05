JuliaLang/julia

commit cb52f15feec9052e68c9102e7e12e93a49367def

make integer `write` and `read` only apply to types where we know they work
fixes #14274

base/iostream.jl
@@ -166,7 +166,7 @@ function read(s::IOStream, ::Type{UInt8})
     b % UInt8
 end
 
-function read{T<:Union{UInt16, Int16, UInt32, Int32, UInt64, Int64}}(s::IOStream, ::Type{T})
+function read{T<:Union{UInt16, Int16, UInt32, Int32, UInt64, Int64, UInt128, Int128}}(s::IOStream, ::Type{T})
     ccall(:jl_ios_get_nbyte_int, UInt64, (Ptr{Void}, Csize_t), s.ios, sizeof(T)) % T
 end
 

base/io.jl
@@ -59,7 +59,7 @@ function write(io::IO, xs...)
 end
 
 if ENDIAN_BOM == 0x01020304
-    function write(s::IO, x::Integer)
+    function write(s::IO, x::Union{Int8,Int16,UInt16,Int32,UInt32,Int64,UInt64,Int128,UInt128})
         sz = sizeof(x)
         local written::Int = 0
         for n = sz:-1:1
@@ -68,7 +68,7 @@ if ENDIAN_BOM == 0x01020304
         return written
     end
 else
-    function write(s::IO, x::Integer)
+    function write(s::IO, x::Union{Int8,Int16,UInt16,Int32,UInt32,Int64,UInt64,Int128,UInt128})
         sz = sizeof(x)
         local written::Int = 0
         for n = 1:sz
@@ -129,7 +129,7 @@ end
 
 read(s::IO, ::Type{Int8}) = reinterpret(Int8, read(s,UInt8))
 
-function read{T <: Integer}(s::IO, ::Type{T})
+function read{T <: Union{Int16,UInt16,Int32,UInt32,Int64,UInt64,Int128,UInt128}}(s::IO, ::Type{T})
     x = zero(T)
     for n = 1:sizeof(x)
         x |= (convert(T,read(s,UInt8))<<((n-1)<<3))

commit ada3ef155aefbd546bc96894fb0757ceb99f0b1e

hopefully fix assertion failure in random test
introduced by https://github.com/JuliaLang/julia/commit/cb52f15feec9052e68c9102e7e12e93a49367def
jl_ios_get_nbyte_int does not support int128 right now

base/iostream.jl
@@ -166,7 +166,7 @@ function read(s::IOStream, ::Type{UInt8})
     b % UInt8
 end
 
-function read{T<:Union{UInt16, Int16, UInt32, Int32, UInt64, Int64, UInt128, Int128}}(s::IOStream, ::Type{T})
+function read{T<:Union{UInt16, Int16, UInt32, Int32, UInt64, Int64}}(s::IOStream, ::Type{T})
     ccall(:jl_ios_get_nbyte_int, UInt64, (Ptr{Void}, Csize_t), s.ios, sizeof(T)) % T
 end
 