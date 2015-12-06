8954dbd8f49b1bbe28b19916f08edf54624a63ce
Erik Schnetter <schnetter@gmail.com>
2015-12-06 12:57:35 -0500

    Re-enable non-division unchecked_* type tests

--- test/numbers.jl
+++ test/numbers.jl
@@ -1624,7 +1624,6 @@ for T in (Int8,Int16,Int32,Int64,Int128, UInt8,UInt16,UInt32,UInt64,UInt128)
     @test typeof(Base.checked_mod(z,o)) === T
     @test typeof(Base.checked_cld(z,o)) === T
 
-    #=
     @test typeof(Base.unchecked_add(z)) === T
     @test typeof(Base.unchecked_neg(z)) === T
     @test typeof(Base.unchecked_abs(z)) === T
@@ -1632,6 +1631,7 @@ for T in (Int8,Int16,Int32,Int64,Int128, UInt8,UInt16,UInt32,UInt64,UInt128)
     @test typeof(Base.unchecked_add(z,z)) === T
     @test typeof(Base.unchecked_sub(z,z)) === T
     @test typeof(Base.unchecked_mul(z,z)) === T
+    #=
     @test typeof(Base.unchecked_div(z,o)) === T
     @test typeof(Base.unchecked_rem(z,o)) === T
     @test typeof(Base.unchecked_fld(z,o)) === T
