c93a2727343c35079d758cda1e32f15c9ad767a8
Stefan Karpinski <stefan@karpinski.org>
2015-12-09 18:26:54 -0500

    hex2bytes: generalize code to work with any string argument

--- base/strings/util.jl
+++ base/strings/util.jl
@@ -212,27 +212,26 @@ replace(s::AbstractString, pat, r) = replace(s, pat, r, 0)
 
 # hex <-> bytes conversion
 
-function hex2bytes(s::ASCIIString)
-    len = length(s)
-    iseven(len) || throw(ArgumentError("string length must be even: length($(repr(s))) == $len"))
-    arr = zeros(UInt8, div(len,2))
-    i = j = 0
-    while i < len
-        n = 0
-        c = s[i+=1]
+function hex2bytes(s::AbstractString)
+    a = zeros(UInt8, div(endof(s), 2))
+    i, j = start(s), 0
+    while !done(s, i)
+        c, i = next(s, i)
         n = '0' <= c <= '9' ? c - '0' :
             'a' <= c <= 'f' ? c - 'a' + 10 :
             'A' <= c <= 'F' ? c - 'A' + 10 :
-                throw(ArgumentError("not a hexadecimal string: $(repr(s))"))
-        c = s[i+=1]
+            throw(ArgumentError("not a hexadecimal string: $(repr(s))"))
+        done(s, i) &&
+            throw(ArgumentError("string length must be even: length($(repr(s))) == $(length(s))"))
+        c, i = next(s, i)
         n = '0' <= c <= '9' ? n << 4 + c - '0' :
             'a' <= c <= 'f' ? n << 4 + c - 'a' + 10 :
             'A' <= c <= 'F' ? n << 4 + c - 'A' + 10 :
-                throw(ArgumentError("not a hexadecimal string: $(repr(s))"))
-        arr[j+=1] = n
+            throw(ArgumentError("not a hexadecimal string: $(repr(s))"))
+        a[j += 1] = n
     end
-    return arr
+    resize!(a, j)
+    return a
 end
-hex2bytes(s::AbstractString) = hex2bytes(ascii(s))
 
-bytes2hex{T<:UInt8}(arr::Vector{T}) = join([hex(i,2) for i in arr])
+bytes2hex(arr::Vector{UInt8}) = join([hex(i,2) for i in arr])
