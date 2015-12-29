a1d4e5f52dc603eb1d1fd89d5c9f62bf09e7fce2
Yichao Yu <yyc1992@gmail.com>
2015-12-29 01:32:14 -0500

    Fix compilation on AArch64

--- src/disasm.cpp
+++ src/disasm.cpp
@@ -583,7 +583,7 @@ void jl_dump_asm_internal(uintptr_t Fptr, size_t Fsize, size_t slide,
             switch (S) {
             case MCDisassembler::Fail:
                 if (insSize == 0) // skip illegible bytes
-#if defined(_CPU_PPC_) || defined(_CPU_PPC64_) || defined(_CPU_ARM_)
+#if defined(_CPU_PPC_) || defined(_CPU_PPC64_) || defined(_CPU_ARM_) || defined(_CPU_AARCH64_)
                     insSize = 4; // instructions are always 4 bytes
 #else
                     insSize = 1; // attempt to slide 1 byte forward
