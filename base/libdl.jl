f94f97d36faeb76b771352e713a05b563e2af67f
Andy Hayden <andyhayden1@gmail.com>
2016-01-02 12:46:59 -0800

    Move libc and libdl docstrings inline, out of helpdb.

--- base/libdl.jl
+++ base/libdl.jl
@@ -20,15 +20,45 @@ const RTLD_NOLOAD    = 0x00000020
 const RTLD_DEEPBIND  = 0x00000040
 const RTLD_FIRST     = 0x00000080
 
+"""
+    dlsym(handle, sym)
+
+Look up a symbol from a shared library handle, return callable function pointer on success.
+"""
 function dlsym(hnd::Ptr, s::Union{Symbol,AbstractString})
     hnd == C_NULL && throw(ArgumentError("NULL library handle"))
     ccall(:jl_dlsym, Ptr{Void}, (Ptr{Void}, Cstring), hnd, s)
 end
 
+"""
+    dlsym_e(handle, sym)
+
+Look up a symbol from a shared library handle, silently return `NULL` pointer on lookup failure.
+"""
 function dlsym_e(hnd::Ptr, s::Union{Symbol,AbstractString})
     hnd == C_NULL && throw(ArgumentError("NULL library handle"))
     ccall(:jl_dlsym_e, Ptr{Void}, (Ptr{Void}, Cstring), hnd, s)
 end
+
+"""
+    dlopen(libfile::AbstractString [, flags::Integer])
+
+Load a shared library, returning an opaque handle.
+
+The optional flags argument is a bitwise-or of zero or more of `RTLD_LOCAL`, `RTLD_GLOBAL`,
+`RTLD_LAZY`, `RTLD_NOW`, `RTLD_NODELETE`, `RTLD_NOLOAD`, `RTLD_DEEPBIND`, and `RTLD_FIRST`.
+These are converted to the corresponding flags of the POSIX (and/or GNU libc and/or MacOS)
+dlopen command, if possible, or are ignored if the specified functionality is not available
+on the current platform. The default flags are platform specific. On MacOS the default
+`dlopen` flags are `RTLD_LAZY|RTLD_DEEPBIND|RTLD_GLOBAL` while on other platforms the
+defaults are `RTLD_LAZY|RTLD_DEEPBIND|RTLD_LOCAL`. An important usage of these flags is to
+specify non default behavior for when the dynamic library loader binds library references to
+exported symbols and if the bound references are put into process local or global scope. For
+instance `RTLD_LAZY|RTLD_DEEPBIND|RTLD_GLOBAL` allows the library's symbols to be available
+for usage in other shared libraries, addressing situations where there are dependencies
+between shared libraries.
+"""
+function dlopen end
 
 dlopen(s::Symbol, flags::Integer = RTLD_LAZY | RTLD_DEEPBIND) =
     dlopen(string(s), flags)
@@ -36,16 +66,37 @@ dlopen(s::Symbol, flags::Integer = RTLD_LAZY | RTLD_DEEPBIND) =
 dlopen(s::AbstractString, flags::Integer = RTLD_LAZY | RTLD_DEEPBIND) =
     ccall(:jl_load_dynamic_library, Ptr{Void}, (Cstring,UInt32), s, flags)
 
+"""
+    dlopen_e(libfile::AbstractString [, flags::Integer])
+
+Similar to [`dlopen`](:func:`dlopen`), except returns a `NULL` pointer instead of raising errors.
+"""
+function dlopen_e end
+
 dlopen_e(s::Symbol, flags::Integer = RTLD_LAZY | RTLD_DEEPBIND) =
     dlopen_e(string(s), flags)
 
 dlopen_e(s::AbstractString, flags::Integer = RTLD_LAZY | RTLD_DEEPBIND) =
     ccall(:jl_load_dynamic_library_e, Ptr{Void}, (Cstring,UInt32), s, flags)
 
+"""
+    dlclose(handle)
+
+Close shared library referenced by handle.
+"""
 function dlclose(p::Ptr)
     0 == ccall(:jl_dlclose, Cint, (Ptr{Void},), p)
 end
 
+"""
+    find_library(names, locations)
+
+Searches for the first library in `names` in the paths in the `locations` list,
+`DL_LOAD_PATH`, or system library paths (in that order) which can successfully be dlopen'd.
+On success, the return value will be one of the names (potentially prefixed by one of the
+paths in locations). This string can be assigned to a `global const` and used as the library
+name in future `ccall`'s. On failure, it returns the empty string.
+"""
 function find_library(libnames, extrapaths=ASCIIString[])
     for lib in libnames
         for path in extrapaths
@@ -89,6 +140,13 @@ else
     #assume OS_NAME === :Linux, or similar
     const dlext = "so"
 end
+
+"""
+    dlext
+
+File extension for dynamic libraries (e.g. dll, dylib, so) on the current platform.
+"""
+dlext
 
 @linux_only begin
     immutable dl_phdr_info
