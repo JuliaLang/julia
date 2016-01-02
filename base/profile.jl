6713ab484e7eb29f1732b4392f92336103ff00aa
Andy Hayden <andyhayden1@gmail.com>
2016-01-02 12:48:44 -0800

    Move Profile docstrings inline, out of helpdb.

--- base/profile.jl
+++ base/profile.jl
@@ -23,6 +23,16 @@ end
 ####
 #### User-level functions
 ####
+
+"""
+    init(; n::Integer, delay::Float64)
+
+Configure the `delay` between backtraces (measured in seconds), and the number `n` of
+instruction pointers that may be stored. Each instruction pointer corresponds to a single
+line of code; backtraces generally consist of a long list of instruction pointers. Default
+settings can be obtained by calling this function with no arguments, and each can be set
+independently using keywords or in the order `(n, delay)`.
+"""
 function init(; n::Union{Void,Integer} = nothing, delay::Union{Void,Float64} = nothing)
     n_cur = ccall(:jl_profile_maxlen_data, Csize_t, ())
     delay_cur = ccall(:jl_profile_delay_nsec, UInt64, ())/10^9
@@ -45,8 +55,26 @@ end
 # Use a max size of 1M profile samples, and fire timer every 1ms
 @windows? (__init__() = init(1_000_000, 0.01)) : (__init__() = init(1_000_000, 0.001))
 
+"""
+    clear()
+
+Clear any existing backtraces from the internal buffer.
+"""
 clear() = ccall(:jl_profile_clear_data, Void, ())
 
+"""
+    print([io::IO = STDOUT,] [data::Vector]; format = :tree, C = false, combine = true, maxdepth = typemax(Int), sortedby = :filefuncline)
+
+Prints profiling results to `io` (by default, `STDOUT`). If you do not
+supply a `data` vector, the internal buffer of accumulated backtraces
+will be used. `format` can be `:tree` or `:flat`. If `C==true`,
+backtraces from C and Fortran code are shown. `combine==true` merges
+instruction pointers that correspond to the same line of
+code. `maxdepth` can be used to limit the depth of printing in `:tree`
+format, while `sortedby` can be used to control the order in `:flat`
+format (`:filefuncline` sorts by the source line, whereas `:count`
+sorts in order of number of collected samples).
+"""
 function print{T<:Unsigned}(io::IO, data::Vector{T} = fetch(), lidict::Dict = getdict(data);
         format = :tree,
         C = false,
@@ -62,8 +90,26 @@ function print{T<:Unsigned}(io::IO, data::Vector{T} = fetch(), lidict::Dict = ge
         throw(ArgumentError("output format $(repr(format)) not recognized"))
     end
 end
+
+"""
+    print([io::IO = STDOUT,] data::Vector, lidict::Dict; kwargs)
+
+Prints profiling results to `io`. This variant is used to examine results exported by a
+previous call to [`retrieve`](:func:`retrieve`). Supply the vector `data` of backtraces and
+a dictionary `lidict` of line information.
+
+See `Profile.print([io], data)` for an explanation of the valid keyword arguments.
+"""
 print{T<:Unsigned}(data::Vector{T} = fetch(), lidict::Dict = getdict(data); kwargs...) = print(STDOUT, data, lidict; kwargs...)
 
+"""
+    retrieve() -> data, lidict
+
+"Exports" profiling results in a portable format, returning the set of all backtraces
+(`data`) and a dictionary that maps the (session-specific) instruction pointers in `data` to
+`LineInfo` values that store the file name, function name, and line number. This function
+allows you to save profiling results for future analysis.
+"""
 function retrieve()
     data = fetch()
     copy(data), getdict(data)
@@ -73,6 +119,19 @@ function getdict(data::Vector{UInt})
     uip = unique(data)
     Dict{UInt, LineInfo}([ip=>lookup(ip) for ip in uip])
 end
+
+# TODO update signature in docstring.
+"""
+    callers(funcname, [data, lidict], [filename=<filename>], [linerange=<start:stop>]) -> Vector{Tuple{count, linfo}}
+
+Given a previous profiling run, determine who called a particular function. Supplying the
+filename (and optionally, range of line numbers over which the function is defined) allows
+you to disambiguate an overloaded method. The returned value is a vector containing a count
+of the number of calls and line information about the caller. One can optionally supply
+backtrace data obtained from [`retrieve`](:func:`retrieve`); otherwise, the current internal
+profile buffer is used.
+"""
+function callers end
 
 function callers(funcname::ByteString, bt::Vector{UInt}, lidict; filename = nothing, linerange = nothing)
     if filename === nothing && linerange === nothing
@@ -95,6 +154,15 @@ callers(func::Function; kwargs...) = callers(string(func), retrieve()...; kwargs
 ##
 # Reset the malloc log. Used to avoid counting memory allocated during
 # compilation.
+
+"""
+    clear_malloc_data()
+
+Clears any stored memory allocation data when running julia with `--track-allocation`.
+Execute the command(s) you want to test (to force JIT-compilation), then call
+[`clear_malloc_data`](:func:`clear_malloc_data`). Then execute your command(s) again, quit
+Julia, and examine the resulting `*.mem` files.
+"""
 clear_malloc_data() = ccall(:jl_clear_malloc_data, Void, ())
 
 
@@ -156,6 +224,16 @@ error_codes = Dict{Int,ASCIIString}(
     -3=>"cannot start the timer for profiling",
     -4=>"cannot unblock SIGUSR1")
 
+
+"""
+    fetch() -> data
+
+Returns a reference to the internal buffer of backtraces. Note that subsequent operations,
+like [`clear`](:func:`clear`), can affect `data` unless you first make a copy. Note that the
+values in `data` have meaning only on this machine in the current session, because it
+depends on the exact memory addresses used in JIT-compiling. This function is primarily for
+internal use; [`retrieve`](:func:`retrieve`) may be a better choice for most users.
+"""
 function fetch()
     len = len_data()
     maxlen = maxlen_data()
