c1125accd82798c4173fa7ad7ad28c8fc7cb6156
Andy Hayden <andyhayden1@gmail.com>
2016-01-02 17:28:56 -0800

    Move mysnc docstring out of unix_only block.

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -575,6 +575,21 @@ export @math_const
 # 11280, mmap
 
 export msync
+
+"""
+    msync(ptr, len, [flags])
+
+Forces synchronization of the [`mmap`](:func:`mmap`)ped memory region from `ptr` to
+`ptr+len`. Flags defaults to `MS_SYNC`, but can be a combination of `MS_ASYNC`, `MS_SYNC`,
+or `MS_INVALIDATE`. See your platform man page for specifics. The flags argument is not
+valid on Windows.
+
+You may not need to call `msync`, because synchronization is performed at intervals
+automatically by the operating system. However, you can call this directly if, for example,
+you are concerned about losing the result of a long-running calculation.
+"""
+function msync end
+
 msync{T}(A::Array{T}) = msync(pointer(A), length(A)*sizeof(T))
 msync(B::BitArray) = msync(pointer(B.chunks), length(B.chunks)*sizeof(UInt64))
 
@@ -608,18 +623,6 @@ end
 const MS_ASYNC = 1
 const MS_INVALIDATE = 2
 const MS_SYNC = 4
-"""
-    msync(ptr, len, [flags])
-
-Forces synchronization of the [`mmap`](:func:`mmap`)ped memory region from `ptr` to
-`ptr+len`. Flags defaults to `MS_SYNC`, but can be a combination of `MS_ASYNC`, `MS_SYNC`,
-or `MS_INVALIDATE`. See your platform man page for specifics. The flags argument is not
-valid on Windows.
-
-You may not need to call `msync`, because synchronization is performed at intervals
-automatically by the operating system. However, you can call this directly if, for example,
-you are concerned about losing the result of a long-running calculation.
-"""
 @noinline function msync(p::Ptr, len::Integer, flags::Integer=MS_SYNC)
     depwarn("`msync` is deprecated, use `Mmap.sync!(array)` instead", :msync)
     systemerror("msync", ccall(:msync, Cint, (Ptr{Void}, Csize_t, Cint), p, len, flags) != 0)
