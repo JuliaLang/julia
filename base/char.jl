0999c5b598ac57f3e87aab15e2996df2063fbbbb
Curtis Vogt <curtis.vogt@gmail.com>
2016-01-06 15:39:29 -0600

    Fix unintentional promotion of Char/Int array math
    Fixes issue #14573.

--- base/char.jl
+++ base/char.jl
@@ -42,6 +42,10 @@ isless(x::Integer, y::Char) = isless(x, UInt32(y))
 +(x::Char, y::Integer) = Char(Int32(x) + Int32(y))
 +(x::Integer, y::Char) = y + x
 
+Base.promote_op{I<:Integer}(::Base.SubFun, ::Type{Char}, ::Type{I}) = Char
+Base.promote_op{I<:Integer}(::Base.AddFun, ::Type{Char}, ::Type{I}) = Char
+Base.promote_op{I<:Integer}(::Base.AddFun, ::Type{I}, ::Type{Char}) = Char
+
 bswap(x::Char) = Char(bswap(UInt32(x)))
 
 print(io::IO, c::Char) = (write(io, c); nothing)
