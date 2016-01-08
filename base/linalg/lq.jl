a0573d56834f7b902b00ba879a05f66893f39a7d
Katie Hyatt <kshyatt@physics.ucsb.edu>
2016-01-07 17:35:00 -0800

    Add ldiv solvers and improve error messages

--- base/linalg/lq.jl
+++ base/linalg/lq.jl
@@ -19,13 +19,15 @@ lqfact!{T<:BlasFloat}(A::StridedMatrix{T}) = LQ(LAPACK.gelqf!(A)...)
 lqfact{T<:BlasFloat}(A::StridedMatrix{T})  = lqfact!(copy(A))
 lqfact(x::Number) = lqfact(fill(x,1,1))
 
-function lq(A::Union(Number, AbstractMatrix); thin::Bool=true)
+function lq(A::Union{Number, AbstractMatrix}; thin::Bool=true)
     F = lqfact(A)
     F[:L], full(F[:Q], thin=thin)
 end
 
+copy(A::LQ) = LQ(copy(A.factors), copy(A.τ))
 convert{T}(::Type{LQ{T}},A::LQ) = LQ(convert(AbstractMatrix{T}, A.factors), convert(Vector{T}, A.τ))
 convert{T}(::Type{Factorization{T}}, A::LQ) = convert(LQ{T}, A)
+ctranspose{T}(A::LQ{T}) = QR{T,typeof(A.factors)}(A.factors', A.τ)
 
 function getindex(A::LQ, d::Symbol)
     m, n = size(A)
@@ -38,7 +40,13 @@ function getindex(A::LQ, d::Symbol)
     end
 end
 
-getindex(A::LQPackedQ, i::Integer, j::Integer) = (x = zeros(eltype(A), size(A, 1)); x[i] = 1; y = zeros(eltype(A), size(A, 2)); y[j] = 1; dot(x, A*y))
+function getindex(A::LQPackedQ, i::Integer, j::Integer)
+    x = zeros(eltype(A), size(A, 1))
+    x[i] = 1
+    y = zeros(eltype(A), size(A, 2))
+    y[j] = 1
+    return dot(x, A*y)
+end
 
 getq(A::LQ) = LQPackedQ(A.factors, A.tau)
 
@@ -47,16 +55,41 @@ convert{T}(::Type{AbstractMatrix{T}}, Q::LQPackedQ) = convert(LQPackedQ{T}, Q)
 
 size(A::LQ, dim::Integer) = size(A.factors, dim)
 size(A::LQ) = size(A.factors)
-size(A::LQPackedQ, dim::Integer) = 0 < dim ? (dim <= 2 ? size(A.factors, 1) : 1) : throw(BoundsError())
-size(A::LQPackedQ) = size(A, 1), size(A, 2)
+size(A::LQPackedQ, dim::Integer) = 0 < dim ? (dim <= 2 ? size(A.factors, dim) : 1) : throw(BoundsError())
+size(A::LQPackedQ) = size(A.factors)
 
 full(A::LQ) = A[:L]*A[:Q]
+#=
+We construct the full eye here, even though it seems ineffecient, because
+every element in the output matrix is a function of all the elements of
+the input matrix. The eye is modified by the elementary reflectors held
+in A, so this is not just an indexing operation. Note that in general
+explicitly constructing Q, rather than using the ldiv or mult methods,
+may be a wasteful allocation.
+=#
 function full{T}(A::LQPackedQ{T}; thin::Bool=true)
     if thin
-        A_mul_B!(eye(T, minimum(size(A.factors)), size(A.factors,2)), A)
+        LAPACK.orglq!(copy(A.factors),A.τ)
     else
-        A_mul_B!(eye(T, size(A.factors,2)), A)
+        A_mul_B!(A, eye(T, size(A.factors,2), size(A.factors,1)))
     end
+end
+
+## Multiplication by LQ
+A_mul_B!{T<:BlasFloat}(A::LQ{T}, B::StridedVecOrMat{T}) = A[:L]*LAPACK.ormlq!('L','N',A.factors,A.τ,B)
+A_mul_B!{T<:BlasFloat}(A::LQ{T}, B::QR{T}) = L*LAPACK.ormlq!('L','N',A.factors,A.τ,full(B))
+A_mul_B!{T<:BlasFloat}(A::QR{T}, B::LQ{T}) = A_mul_B!(zeros(full(A)), full(A), full(B))
+function *{TA,TB}(A::LQ{TA},B::StridedVecOrMat{TB})
+    TAB = promote_type(TA, TB)
+    A_mul_B!(convert(Factorization{TAB}, A), TB==TAB ? copy(B) : copy_oftype(B, TAB))
+end
+function *{TA,TB}(A::LQ{TA},B::QR{TB})
+    TAB = promote_type(TA, TB)
+    A_mul_B!(convert(Factorization{TAB}, A), TB==TAB ? copy(B) : convert(Factorization{TAB}, B))
+end
+function *{TA,TB}(A::QR{TA},B::LQ{TB})
+    TAB = promote_type(TA, TB)
+    A_mul_B!(convert(Factorization{TAB}, A), TB==TAB ? copy(B) : convert(Factorization{TAB}, B))
 end
 
 ## Multiplication by Q
@@ -64,7 +97,7 @@ end
 A_mul_B!{T<:BlasFloat}(A::LQPackedQ{T}, B::StridedVecOrMat{T})   = LAPACK.ormlq!('L','N',A.factors,A.τ,B)
 function *{TA,TB}(A::LQPackedQ{TA},B::StridedVecOrMat{TB})
     TAB = promote_type(TA, TB)
-    A_mul_B!(convert(AbstractMatrix{TAB}, A), TB==TAB ? copy(B) : convert(AbstractArray{TAB}, B))
+    A_mul_B!(convert(AbstractMatrix{TAB}, A), TB==TAB ? copy(B) : copy_oftype(B, TAB))
 end
 
 ### QcB
@@ -76,7 +109,7 @@ function Ac_mul_B{TA,TB}(A::LQPackedQ{TA}, B::StridedVecOrMat{TB})
         if TB == TAB
             Ac_mul_B!(convert(AbstractMatrix{TAB}, A), copy(B))
         else
-            Ac_mul_B!(convert(AbstractMatrix{TAB}, A), convert(AbstractMatrix{TAB}, B))
+            Ac_mul_B!(convert(AbstractMatrix{TAB}, A), copy_oftype(B, TAB))
         end
     elseif size(B,1) == size(A.factors,1)
         Ac_mul_B!(convert(AbstractMatrix{TAB}, A), [B; zeros(TAB, size(A.factors, 2) - size(A.factors, 1), size(B, 2))])
@@ -110,6 +143,28 @@ function A_mul_Bc{TA<:Number,TB<:Number}( A::StridedVecOrMat{TA}, B::LQPackedQ{T
     if TA == TAB
         A_mul_Bc!(copy(A), convert(AbstractMatrix{TAB},B))
     else
-        A_mul_Bc!(convert(AbstractArray{TAB}, A), convert(AbstractMatrix{TAB},B))
+        A_mul_Bc!(copy_oftype(A, TAB), convert(AbstractMatrix{TAB},(B)))
     end
+end
+
+function \{TA,Tb}(A::LQ{TA}, b::StridedVector{Tb})
+    S = promote_type(TA,Tb)
+    m = checksquare(A)
+    m == length(b) || throw(DimensionMismatch("left hand side has $m rows, but right hand side has length $(length(b))"))
+    AA = convert(Factorization{S}, A)
+    x = A_ldiv_B!(AA, copy_oftype(b, S))
+    return x
+end
+function \{TA,TB}(A::LQ{TA},B::StridedMatrix{TB})
+    S = promote_type(TA,TB)
+    m = checksquare(A)
+    m == size(B,1) || throw(DimensionMismatch("left hand side has $m rows, but right hand side has $(size(B,1)) rows"))
+    AA = convert(Factorization{S}, A)
+    X = A_ldiv_B!(AA, copy_oftype(B, S))
+    return X
+end
+
+function A_ldiv_B!{T}(A::LQ{T}, B::StridedVecOrMat{T})
+    Ac_mul_B!(A[:Q], A_ldiv_B!(LowerTriangular(A[:L]),B))
+    return B
 end
