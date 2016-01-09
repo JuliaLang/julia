c9587a7ce1579642271874bf3bfc6628ce868b49
Blake Johnson <blakejohnson04@gmail.com>
2016-01-08 22:39:10 -0500

    Add tests for bounds check elimination.

---
+++ test/boundscheck.jl
@@ -0,0 +1,61 @@
+
+module TestBoundsCheck
+
+using Base.Test
+
+# test for boundscheck block eliminated at same level
+function A1()
+    r = 0
+    @boundscheck r += 1
+    return r
+end
+
+function A1_inbounds()
+    r = 0
+    @inbounds begin
+        @boundscheck r += 1
+    end
+    return r
+end
+
+@test A1() == 1
+@test A1_inbounds() == 0
+
+# test for boundscheck block eliminated one layer deep
+function A2()
+    r = A1()+1
+    return r
+end
+
+function A2_inbounds()
+    @inbounds r = A1()+1
+    return r
+end
+
+@test A2() == 2
+@test A2_inbounds() == 1
+
+# test boundscheck NOT eliminated two layers deep
+
+function A3()
+    r = A2()+1
+    return r
+end
+
+function A3_inbounds()
+    @inbounds r = A2()+1
+    return r
+end
+
+@test A3() == 3
+@test A3_inbounds() == 3
+
+@inline B() = A1() + 1
+function A3_inbounds2()
+    @inbounds r = B()+1
+    return r
+end
+
+# @test A3_inbounds2() == 3
+
+end
