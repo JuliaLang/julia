98802a70fafaabb85de4cf41f302e4cdfd33009d
Jeff Bezanson <jeff.bezanson@gmail.com>
2016-01-18 16:15:38 -0500

    clean up after rebase

--- test/arrayops.jl
+++ test/arrayops.jl
@@ -1454,7 +1454,7 @@ end
 
 # make sure @inbounds isn't used too much
 type OOB_Functor{T}; a::T; end
-call(f::OOB_Functor, i::Int) = f.a[i]
+(f::OOB_Functor)(i::Int) = f.a[i]
 let f = OOB_Functor([1,2])
     @test_throws BoundsError map(f, [1,2,3,4,5])
 end
