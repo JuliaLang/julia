c5e346d4c4054d030a72df7507c1e706ec227e6b
CarloLucibello <carlo.lucibello@gmail.com>
2016-01-24 12:50:43 +0100

    fix typewarns for real types. Still to address complexes.

--- base/special/beta.jl
+++ base/special/beta.jl
@@ -17,14 +17,16 @@ const ASYMP_FACTOR = 1e6
 fastabs(x::Real) = abs(x)
 fastabs(x::Complex) = abs(real(x)) + abs(imag(x))
 
-function beta(a::Number, b::Number)
+beta(a::Number, b::Number) = beta(promote(float(a), float(b))...)
+
+function beta{T<:Number}(a::T, b::T)
     real(a) <= 0.0 && isinteger(a) && return beta_negint(a, b)
     real(b) <= 0.0 && isinteger(b) && return beta_negint(b, a)
-
+    #
     if fastabs(a) < fastabs(b)
         a, b = b, a
     end
-
+    #
     if fastabs(a) > ASYMP_FACTOR * fastabs(b) && a > ASYMP_FACTOR
         # Avoid loss of precision in lgamma(a + b) - lgamma(a)
         y, s = lbeta_asymp(a, b)
@@ -47,7 +49,7 @@ function beta(a::Number, b::Number)
     y = gamma(y)
     a = gamma(a)
     b = gamma(b)
-    y == 0.0 && return Inf
+    y == 0.0 && return convert(T, Inf)
 
     if fastabs(fastabs(a) - fastabs(y)) > fastabs(fastabs(b) - fastabs(y))
         y = b / y
@@ -60,7 +62,9 @@ function beta(a::Number, b::Number)
     return y
 end
 
-function lbeta(a::Number, b::Number)
+lbeta(a::Number, b::Number) = lbeta(promote(float(a), float(b))...)
+
+function lbeta{T<:Number}(a::T, b::T)
     real(a) <= 0.0 && isinteger(a) && return lbeta_negint(a, b)
     real(b) <= 0.0 && isinteger(b) && return lbeta_negint(b, a)
 
@@ -87,7 +91,7 @@ function lbeta(a::Number, b::Number)
     y = gamma(y)
     a = gamma(a)
     b = gamma(b)
-    y == 0.0 && return Inf
+    y == 0.0 && return inf(T)
 
     if fastabs(fastabs(a) - fastabs(y)) > fastabs(fastabs(b) - fastabs(y))
         y = b / y
@@ -101,33 +105,34 @@ function lbeta(a::Number, b::Number)
 end
 
 # assuming isinteger(x) and x < 0.
-function beta_negint(x::Number, w::Number)
+function beta_negint{T}(x::T, w::T)
     if isinteger(w) && 1 - x - w > 0
-        s = ifelse(w % 2 == 0, 1., -1.)
+        s = ifelse(Int(w) % 2 == 0, T(1), T(-1))
         return s * beta(1 - x - w, w)
     else
-        return Inf
+        return convert(T, Inf)
     end
 end
 
 # assuming isinteger(x) and x < 0.
-function lbeta_negint(x::Number, w::Number)
+function lbeta_negint{T}(x::T, w::T)
     if isinteger(w) && 1 - x - w > 0
-        s = ifelse(w % 2 == 0 , 1., -1.)
+        s = ifelse(Int(w) % 2 == 0 , T(1), T(-1))
+        return s
         return s * lbeta(1 - x - w, w)
     else
-        return Inf
+        return convert(T, Inf)
     end
 end
 
  # Asymptotic expansion for  ln(|B(a, b)|) for a > ASYMP_FACTOR*max(|b|, 1).
-function lbeta_asymp(a::Number, b::Number)
+function lbeta_asymp{T}(a::T, b::T)
     r, s = lgamma_r(b)
     r -= b * log(a)
     r += b*(1-b)/(2*a);
     r += b*(1-b)*(1-2*b)/(12*a*a)
     r += - b*b*(1-b)*(1-b)/(12*a*a*a)
-    return r, s
+    return r, T(s)
 end
 
 @vectorize_2arg Number beta
