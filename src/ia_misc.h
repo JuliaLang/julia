866a505ff255fa1fbe1ae8ad04acdc1b1a201270
Yichao Yu <yyc1992@gmail.com>
2016-01-28 00:59:17 -0500

    Use uv_hrtime instead of assembly to get current CPU time
    This is more portable and predictable (since it's independent of CPU frequency).

--- src/ia_misc.h
+++ src/ia_misc.h
@@ -6,49 +6,12 @@
 #include <stdint.h>
 #if defined(_CPU_X86_64_) || defined(_CPU_X86_)
 #  include <immintrin.h>
-#elif defined(__linux__)
-#  include <linux/perf_event.h>
-#  include <linux/hw_breakpoint.h>
-#  include <asm/unistd.h>
 #endif
 #include "support/dtypes.h"
 
 #if defined(__i386__) && defined(__GNUC__) && !defined(__SSE2__)
 #error Julia can only be built for architectures above Pentium 4. Pass -march=pentium4, or set MARCH=pentium4 and ensure that -march is not passed separately with an older architecture.
 #endif
-
-#if defined(__i386__)
-
-STATIC_INLINE unsigned long long rdtsc(void)
-{
-    unsigned long long int x;
-    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
-    return x;
-}
-
-#elif defined(__x86_64__)
-
-STATIC_INLINE uint64_t rdtsc(void)
-{
-    unsigned hi, lo;
-    __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
-    return ((uint64_t)lo) | (((uint64_t)hi) << 32);
-}
-
-#elif defined(_COMPILER_MICROSOFT_)
-
-#include <intrin.h>
-
-STATIC_INLINE uint64_t rdtsc(void)
-{
-    return __rdtsc();
-}
-
-#elif defined(__linux__)
-
-long long rdtsc(void);
-
-#endif  /* __i386__ */
 
 #ifdef __MIC__
 
