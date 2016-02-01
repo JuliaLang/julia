41684655a6016503f21b91a8af34e4264064c3e9
Michael Hatherly <michaelhatherly@gmail.com>
2016-02-01 09:50:58 +0200

    Correctly render `:data:` xrefs in RST output
    Fixes issue mentioned in #14864.

--- base/markdown/render/rst.jl
+++ base/markdown/render/rst.jl
@@ -83,7 +83,7 @@ rstinline(io::IO, md::Vector) = !isempty(md) && rstinline(io, md...)
 # rstinline(io::IO, md::Image) = rstinline(io, ".. image:: ", md.url)
 
 function rstinline(io::IO, md::Link)
-    if ismatch(r":(func|obj|ref|exc|class|const):`\.*", md.url)
+    if ismatch(r":(func|obj|ref|exc|class|const|data):`\.*", md.url)
         rstinline(io, md.url)
     else
         rstinline(io, "`", md.text, " <", md.url, ">`_")
