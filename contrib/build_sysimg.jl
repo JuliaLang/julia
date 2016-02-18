874853c261045c3e11d6dbd95bcb4fe7613a99f6
Tony Kelman <tony@kelman.net>
2016-02-17 23:14:55 -0800

    Remove no longer accurate Windows warning in build_sysimg.jl
    The default Windows build currently skips loading precompiled code from sys.dll
    
    Also remove a duplicate comment in the Windows-specific juliarc.jl
    
    And fix usage of default_sysimg_path

--- contrib/build_sysimg.jl
+++ contrib/build_sysimg.jl
@@ -82,7 +82,7 @@ function build_sysimg(sysimg_path=nothing, cpu_target="native", userimg_path=not
                 info("System image successfully built at $sysimg_path.ji")
             end
 
-            if !Base.samefile("$default_sysimg_path.ji", "$sysimg_path.ji")
+            if !Base.samefile("$(default_sysimg_path(debug)).ji", "$sysimg_path.ji")
                 if Base.isfile("$sysimg_path.$(Libdl.dlext)")
                     info("To run Julia with this image loaded, run: julia -J $sysimg_path.$(Libdl.dlext)")
                 else
@@ -151,13 +151,6 @@ function link_sysimg(sysimg_path=nothing, cc=find_system_compiler(), debug=false
     run(`$cc $FLAGS -o $sysimg_path.$(Libdl.dlext) $sysimg_path.o`)
 
     info("System image successfully built at $sysimg_path.$(Libdl.dlext)")
-    @windows_only begin
-        if convert(VersionNumber, Base.libllvm_version) < v"3.5.0"
-            LLVM_msg = "Building sys.dll on Windows against LLVM < 3.5.0 can cause incorrect backtraces!"
-            LLVM_msg *= " Delete generated sys.dll to avoid these problems"
-            warn( LLVM_msg )
-        end
-    end
 end
 
 # When running this file as a script, try to do so with default values.  If arguments are passed
@@ -176,7 +169,7 @@ if !isinteractive()
         println("   build_sysimg.jl /usr/local/lib/julia/sys core2 ~/my_usrimg.jl --force")
         println()
         println(" Running this script with no arguments is equivalent to:")
-        println("   build_sysimg.jl $(default_sysimg_path) native")
+        println("   build_sysimg.jl $(default_sysimg_path()) native")
         return 0
     end
 
