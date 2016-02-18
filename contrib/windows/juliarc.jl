345d011e4dc600f8d72355f109127a60fdcca7d5
Tony Kelman <tony@kelman.net>
2016-02-17 23:14:55 -0800

    Remove no longer accurate Windows warning in build_sysimg.jl
    The default Windows build currently skips loading precompiled code from sys.dll
    
    Also remove a duplicate comment in the Windows-specific juliarc.jl
    
    And fix usage of default_sysimg_path

--- contrib/windows/juliarc.jl
+++ contrib/windows/juliarc.jl
@@ -1,4 +1,2 @@
-# This file should contain site-specific commands to be executed on Julia startup
-# Users should store their own personal commands in homedir(), in a file named .juliarc.jl
-
+# Set up environment for Julia Windows binary distribution
 ENV["PATH"] = JULIA_HOME*";"*joinpath(JULIA_HOME,"..","Git","bin")*";"*ENV["PATH"]
