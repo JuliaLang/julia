23f96e0fbfdb55cbc27a2f0518364ba5ac28bcf5
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2016-02-22 10:23:24 +0100

    Add issparse methods for specialized matrix types.

--- base/sparse/abstractsparse.jl
+++ base/sparse/abstractsparse.jl
@@ -13,4 +13,11 @@ Returns `true` if `S` is sparse, and `false` otherwise.
 issparse(A::AbstractArray) = false
 issparse(S::AbstractSparseArray) = true
 
+issparse{T, A<:AbstractSparseMatrix}(S::Symmetric{T, A}) = true
+issparse{T, A<:AbstractSparseMatrix}(S::Hermitian{T, A}) = true
+issparse{T, A<:AbstractSparseMatrix}(S::LowerTriangular{T, A}) = true
+issparse{T, A<:AbstractSparseMatrix}(S::LinAlg.UnitLowerTriangular{T, A}) = true
+issparse{T, A<:AbstractSparseMatrix}(S::UpperTriangular{T, A}) = true
+issparse{T, A<:AbstractSparseMatrix}(S::LinAlg.UnitUpperTriangular{T, A}) = true
+
 indtype{Tv,Ti}(S::AbstractSparseArray{Tv,Ti}) = Ti
