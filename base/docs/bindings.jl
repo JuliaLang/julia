6ef643475ab394dcac0382fa07f5b06f9604d718
Michael Hatherly <michaelhatherly@gmail.com>
2016-02-27 18:36:53 +0200

    Refactor docsystem internals
    Structure of a module's `#meta` dict now has the following layout in
    all cases (with the exception of keywords):
    
    - each module's `#meta` `ObjectIdDict` has keys of type `Binding` and
      values of type `MultiDoc`.
    - each `MultiDoc` stores a collection of `DocStr` objects representing a
      single docstring each. These are ordered based on their order of
      definition rather than the current `type_morespecific` approach.
    - `DocStr`s store the raw text of a docstring and lazily parse this to a
      `Markdown.MD` object when requested. By not parsing every docstring we
      also make some space savings in the `sys.*` files.
    
    By keying `#meta` by `Binding` in every case the rest of the logic in
    `doc!` and `doc` becomes a lot more straightforward. `at-doc` expression
    building has also been simplified.
    
    Additionally, the "summaries" displayed when no documentation can be
    found have been refactored and simplified.

--- base/docs/bindings.jl
+++ base/docs/bindings.jl
@@ -5,30 +5,36 @@ export @var
 immutable Binding
     mod::Module
     var::Symbol
-    defined::Bool
 
-    function Binding(m, v)
-        if !isdefined(m, v)
-            new(m, v, false)
-        else
-            new(Base.binding_module(m, v), v, true)
-        end
+    function Binding(m::Module, v::Symbol)
+        # Normalise the binding module for module symbols so that:
+        #   Binding(Base, :Base) === Binding(Main, :Base)
+        m = module_name(m) === v ? module_parent(m) : m
+        new(Base.binding_module(m, v), v)
     end
 end
 
+bindingexpr(x) = Expr(:call, Binding, splitexpr(x)...)
+
+defined(b::Binding) = isdefined(b.mod, b.var)
+resolve(b::Binding) = getfield(b.mod, b.var)
+
 function splitexpr(x::Expr)
     isexpr(x, :macrocall) ? splitexpr(x.args[1]) :
-    isexpr(x, :.)         ? (esc(x.args[1]), x.args[2]) :
+    isexpr(x, :.)         ? (x.args[1], x.args[2]) :
     error("Invalid @var syntax `$x`.")
 end
-splitexpr(s::Symbol) = :(current_module()), quot(s)
+splitexpr(s::Symbol) = Expr(:call, current_module), quot(s)
 splitexpr(other)     = error("Invalid @var syntax `$other`.")
 
-isvar(x) = isexpr(x, [:macrocall, :.])
-isvar(::Symbol) = true
-
 macro var(x)
-    :(Binding($(splitexpr(x)...)))
+    esc(bindingexpr(x))
 end
 
-Base.show(io::IO, x::Binding) = print(io, "$(x.mod).$(x.var)")
+Base.show(io::IO, b::Binding) = b.mod === Main ? print(io, b.var) : print(io, b.mod, '.', b.var)
+
+aliasof(b::Binding)     = defined(b) ? (a = aliasof(resolve(b), b); defined(a) ? a : b) : b
+aliasof(d::DataType, b) = Binding(d.name.module, d.name.name)
+aliasof(λ::Function, b) = (m = typeof(λ).name.mt; Binding(m.module, m.name))
+aliasof(m::Module,   b) = Binding(m, module_name(m))
+aliasof(other,       b) = b
