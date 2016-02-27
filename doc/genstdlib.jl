8ed5ee39482b8a2d76ead5eeee1b69c561eb0a11
Michael Hatherly <michaelhatherly@gmail.com>
2016-02-27 18:39:01 +0200

    Rewrite genstdlib.jl
    This takes advantage of the simplifications in the previous commit to reduce
    the complexity in this script. Outwardly the only difference should be
    the formatting of the summary that is displayed and speed (where timings drop
    from ~18 to ~9 seconds.)

--- doc/genstdlib.jl
+++ doc/genstdlib.jl
@@ -1,337 +1,198 @@
-using .Markdown
-using Base.Markdown: MD
+module GenStdLib
 
-cd(dirname(@__FILE__))
+import Base.Docs: Binding, DocStr
 
-isop(func) = ismatch(r"[^\w@!.]|^!$", func)
+# Constants.
+const DOCSTRING_DIRECTIVE = r"^(.. (function):: ).*"
 
-ident(mod, x) = "$mod.$(isop(x) ? "(:($x))" : x)"
+# Types.
 
-all_docs = ObjectIdDict()
+typealias Signature Tuple{Binding, Type}
 
-function add_all_docs(it)
-    for (k, v) in it
-        all_docs[v] = k
-    end
+type State
+    files      :: Dict{UTF8String, Vector{UTF8String}}
+    validdocs  :: Dict{UTF8String, Tuple{Module, Signature, DocStr}}
+    baddocs    :: Dict{Signature, Tuple{Module, DocStr}}
+    documented :: Dict{Signature, Tuple{Module, DocStr, UTF8String}}
+    errorlevel :: Int
+    debug      :: Bool
+    State() = new(Dict(), Dict(), Dict(), Dict(), 0, "JULIA_GENSTDLIB_DEBUG" in keys(ENV))
 end
 
-function add_all_docs(it, k::ANY)
-    for (_, v) in it
-        all_docs[v] = k
-    end
-end
+# Documentation Translator.
 
-function sym_exported(obj::ANY, m, exports)
-    if isa(obj, Function)
-        try
-            return typeof(obj).name.mt.name in exports
+function translate(dirs::Vector; root = dirname(@__FILE__))
+    cd(root) do
+        state = State()
+        println("\n# GENSTDLIB\n")
+        # Find all available docstrings within `Base` and its submodules.
+        info("loading docstrings from modules.")
+        loaddocs!(state)
+        # Read in rst pages and update all docstrings.
+        info("parsing external documentation.")
+        for dir in dirs, file in readdir(dir)
+            translate(state, joinpath(dir, file))
         end
-    end
-    if isa(obj, Union{Function,DataType,Module})
-        return symbol(string(obj)) in exports
-    elseif isa(obj, IntrinsicFunction)
-        # Only example seems to be cglobal for now
-        return true
-    elseif isa(obj, Docs.Binding)
-        return (obj::Docs.Binding).var in exports
-    else
-        return false
-    end
-end
-
-function add_all_docs_meta(m::Module,meta)
-    exports = names(m)
-    for (obj, d) in meta
-        isexported = sym_exported(obj, m, exports)
-        if isa(d, Base.Docs.MultiDoc)
-            add_all_docs(d.docs, (obj, isexported))
-        else
-            all_docs[d] = (obj, isexported)
-        end
-    end
-end
-
-mod_added = ObjectIdDict()
-
-function add_all_docs_mod(m::Module)
-    mod_added[m] = m
-    try
-        add_all_docs_meta(m,Docs.meta(m))
-    end
-    for name in names(m, true)
-        try
-            sub_m = getfield(m,name)
-            isa(sub_m, Module) || continue
-            if !(sub_m in keys(mod_added))
-                add_all_docs_mod(sub_m)
-            end
-        end
-    end
-end
-
-add_all_docs_mod(Base)
-# Most of the keywords are not functions and they are easy to check by hand
-# add_all_docs(Docs.keywords)
-
-println("Collect $(length(all_docs)) Docs")
-
-function find_docs(v)
-    docs = []
-    for mod in keys(mod_added)
-        try
-            m = Docs.meta(mod)
-            if haskey(m, v)
-                meta = m[v]
-            elseif isa(v, Docs.Binding)
-                obj = getfield(v.mod, v.var)
-                meta = m[obj]
-            end
-            if isa(meta, Base.Docs.MultiDoc)
-                append!(docs, collect(values(meta.docs)))
-            else
-                push!(docs, meta)
-            end
-        end
-    end
-    if isempty(docs)
-        error("Cannot find doc for $v")
-    end
-    return docs
-end
-
-function getdoc(mod, x)
-    try
-        x = unescape_string(x)
-        if symbol(x) in keys(Docs.keywords)
-            return Any[Docs.keywords[symbol(x)]]
-        end
-        if x[1] != '@'
-            v = eval(parse(ident(mod, x)))
-            isa(v, Colon) && (v = Base.colon)
-            return find_docs(v)
-        else
-            for m in [eval(parse(mod)); collect(keys(mod_added));]
-                try
-                    v = Docs.Binding(m, symbol(x))
-                    return find_docs(v)
-                end
-            end
-            error("Cannot find doc for $x")
-        end
-    catch e
-        println(e)
-        warn("Mod $mod $x")
-    end
-    []
-end
-
-flat_content(md) = md
-flat_content(xs::Vector) = reduce((xs, x) -> vcat(xs,flat_content(x)), [], xs)
-flat_content(md::MD) = flat_content(md.content)
-
-flatten(md::MD) = MD(flat_content(md))
-
-isrst(md) =
-    length(flatten(md).content) == 1 &&
-    isa(flatten(md).content[1], Markdown.Code) &&
-    flatten(md).content[1].language == "rst"
-
-function tryrst(md, remove_decl)
-    try
-        if remove_decl && isa(md.content[1], Markdown.Code) && md.content[1].language == ""
-            shift!(md.content)
-        end
-        return Markdown.rst(md)
-    catch e
-        warn("Error converting docstring:")
-        # display(md)
-        println(e)
-        return
-    end
-end
-
-torst(md,remove_decl) = isrst(md) ? flatten(md).content[1].code : tryrst(md, remove_decl)
-
-function split_decl_rst(md)
-    if isrst(md)
-        rst_text = flatten(md).content[1].code
-        ls = split(rst_text, "\n")
-        body_start = 1
-        if startswith(ls[1], ".. ") && !endswith(ls[1], "::")
-            decl = ".. function:: " * replace(ls[1], r"^.. *", "")
-            body_start += 1
-            while startswith(ls[body_start], "   ")
-                decl *= replace(ls[body_start], r"^ *", "\n              ")
-                body_start += 1
-            end
-            while ls[body_start] == ""
-                body_start += 1
-            end
-            return decl, join(ls[body_start:end], "\n")
-        end
-    else
-        if isa(md.content[1], Markdown.Code) && md.content[1].language == ""
-            sigs = shift!(md.content)
-            decl = ".. function:: " * replace(sigs.code, "\n",
-                                              "\n              ")
-            body = Markdown.rst(md)
-            unshift!(md.content, sigs)
-            return decl, body
-        end
-    end
-    return "", ""
-end
-
-# Disable by default since it is hard to eliminate false positives
-const warn_doc_between_func = "JULIA_WARN_DOC_BETWEEN_FUNC" in keys(ENV)
-
-function translate(file)
-    @assert(isfile(file))
-    ls = split(readstring(file), "\n")[1:end-1]
-    doccing = false
-    func = nothing
-    mod = "Base"
-    modidx = -1
-    open(file, "w+") do io
-        has_func_doc = false
-        missing_func_doc = []
-        cur_func = ""
-        function warn_missing_func_doc()
-            if (warn_doc_between_func && has_func_doc &&
-                !isempty(missing_func_doc))
-                doc_str = join(missing_func_doc, "\n")
-                warn("Possible missing document for `$cur_func` in `$file`:")
-                info(doc_str)
-            end
-            missing_func_doc = []
-        end
-        function start_new_section()
-            warn_missing_func_doc()
-            has_func_doc = false
-        end
-        function start_func_doc(func_line)
-            warn_missing_func_doc()
-            cur_func = func_line
-            has_func_doc = true
-        end
-        function push_non_func_line(l)
-            has_func_doc || return
-            if (startswith(l, ".. data:: ") ||
-                startswith(l, ".. _") ||
-                ismatch(r"^[A-Z][a-z]* implemented", l))
-                start_new_section()
-            elseif (startswith(l, "----") ||
-                    startswith(l, "====") ||
-                    startswith(l, "~~~~"))
-                if !isempty(missing_func_doc)
-                    pop!(missing_func_doc)
-                    start_new_section()
-                end
-            end
-            has_func_doc || return
-            push!(missing_func_doc, l)
-        end
-        for (i,l) in enumerate(ls)
-            if ismatch(r"^\.\. (current)?module::", l)
-                start_new_section()
-                mod = match(r"^\.\. (current)?module:: ([\w\.]+)", l).captures[2]
-                modidx = i
-                println(io, l)
-            elseif startswith(l, ".. function::")
-                func = match(r"^\.\. function:: (@?[^\(\s\{]+)(.*)", l)
-                func == nothing && (warn("bad function $l"); continue)
-                funcname = func.captures[1]
-                full = funcname * func.captures[2]
-                if !('(' in full || '@' in full)
-                    ex = parse(full)
-                    if isa(ex, Expr)
-                        if (ex.head == :(||) || ex.head == :(&&))
-                            funcname = string(ex.head)
-                        elseif ex.head == :macrocall
-                            funcname = string(ex.args[1])
-                        end
+        # Write the newly updated rst docs back to their files -- replaces original content.
+        if state.errorlevel < 2
+            info("writing documentation back to file.")
+            for (file, contents) in state.files
+                open(file, "w") do io
+                    for line in contents
+                        println(io, line)
                     end
                 end
-                doc = nothing
-                decl = nothing
-                body = nothing
-                for mdoc in getdoc(mod, funcname)
-                    mdecl, mbody = split_decl_rst(mdoc)
-                    if contains(replace(mdecl, r"[\n ][\n ]+", " "),
-                                replace(".. function:: " * unescape_string(full),
-                                        r"[\n ][\n ]+", " "))
-                        if doc != nothing
-                            error("duplicate $full $l")
-                        end
-                        doc = mdoc
-                        decl = replace(mdecl, '\\', "\\\\")
-                        body = mbody
-                    end
-                end
-                if doc == nothing || torst(doc, false) == nothing
-                    info("$file: no docs for $full in $mod")
-                    println(io, l)
-                    doccing = false
-                    start_new_section()
-                    continue
-                end
-                delete!(all_docs, doc)
-                doccing = true
-                start_func_doc(full)
-                println(io, decl)
-                println(io)
-                println(io, "   .. Docstring generated from Julia source\n")
-                for l in split(body, "\n")
-                    ismatch(r"^\s*$", l) ? println(io) : println(io, "   ", l)
-                end
-                isrst(doc) && println(io)
-            elseif doccing && (ismatch(r"^\s+", l) || ismatch(r"^\s*$", l))
-                modidx == i-1 && println(io)
-            else
-                doccing = false
-                push_non_func_line(l)
-                println(io, l)
+            end
+        else
+            warn("errors found while generating documentation. Aborting file writing.")
+        end
+        # Report statistics about 'Base' docs.
+        summarise(state)
+    end
+end
+function translate(state::State, file::AbstractString)
+    input, output = split(readstring(file), '\n')[1:end-1], []
+    while !isempty(input)
+        if ismatch(DOCSTRING_DIRECTIVE, first(input))
+            append!(output, getdoc(state, file, input))
+        else
+            push!(output, shift!(input))
+        end
+    end
+    state.files[file] = output
+    return state
+end
+
+# Documentation Summary Report.
+
+function summarise(state::State)
+    println("\n# SUMMARY\n")
+    println("   ", length(state.validdocs),  " docstrings found in modules.")
+    println("   ", length(state.documented), " docstrings found in external docs.")
+
+    # Of the missing docstrings, which are public and which are private?
+    public, private = [], []
+    for (sig, (mod, signature, docstr)) in state.validdocs
+        # The docstring is valid, but not found in the external docs.
+        if !haskey(state.documented, signature)
+            binding, typesig = signature
+            exported = binding.var in names(binding.mod)
+            push!(exported ? public : private, (binding, typesig))
+        end
+    end
+    println("   ", length(public), " public docstrings missing from external docs.")
+    state.debug && (println(); foreach(printmissing, public); println())
+    println("   ", length(private), " private docstrings missing from external docs.")
+    state.debug && (println(); foreach(printmissing, private); println())
+
+    # Expected format is: first element in docstring should be a code block.
+    println("   ", length(state.baddocs),    " docstrings not matching expected format. Skipped.")
+    println("   ", length(state.files),      " files parsed.\n")
+    state.debug || println("Set 'JULIA_GENSTDLIB_DEBUG' ENV for additional debug info.\n")
+end
+printmissing(x) = println("     X ", x[1], " :: ", x[2])
+
+# Documentation Loader.
+
+function loaddocs!(state::State)
+    for mod in Base.Docs.modules
+        for (binding, multidoc) in Base.Docs.meta(mod)
+            for (typesig, docstr) in multidoc.docs
+                loaddocs!(state, mod, binding, typesig, docstr)
             end
         end
     end
+    for (keyword, docstr) in Base.Docs.keywords
+        loaddocs!(state, Main, keyword, Union{}, docstr)
+    end
+    return state
 end
-
-for folder in ["stdlib", "manual", "devdocs"]
-    println("\nConverting rst files in $folder/\n")
-    for file in readdir("$folder")
-        if endswith(file, ".rst")
-            translate("$folder/$file")
+function loaddocs!(state::State, mod, binding::Binding, typesig, docstr)
+    markdown = Base.Docs.parsedoc(docstr)
+    if validdocstr(markdown)
+        code = rstrip(markdown.content[1].code)
+        if haskey(state.validdocs, code)
+            code = indent(code)
+            warn("duplicate signature found for '$binding' in module '$mod':\n\n$code\n")
+            state.errorlevel = 2
+        else
+            state.validdocs[code] = (mod, (binding, typesig), docstr)
+        end
+    else
+        if haskey(state.baddocs, (binding, typesig))
+            warn("duplicate binding '$binding :: $typesig' found in module '$mod'.")
+            state.errorlevel = 2
+        else
+            state.baddocs[(binding, typesig)] = (mod, docstr)
         end
     end
+    return state
+end
+function loaddocs!(state::State, mod, keyword::Symbol, typesig, docstr)
+    binding = Base.Docs.Binding(mod, keyword)
+    loaddocs!(state, mod, binding, typesig, docstr)
 end
 
-println()
+# Retrieve and format docstrings.
 
-exported_missing_count = 0
-unexported_missing_count = 0
-
-for (d, v) in all_docs
-    if length(v) == 2
-        val,isexported = v
-    else
-        val,isexported = v,true
+function getdoc(state::State, file::AbstractString, input::Vector)
+    # Capture the lines containing the docstring signature.
+    output = []
+    while !isempty(input)
+        line = shift!(input)
+        push!(output, line)
+        ismatch(r"^$", line) && break
     end
-    isa(val, ObjectIdDict) && continue # No idea what these are
-    isa(val, Int) && continue # We don't document `0` as a function
-    if isexported
-        warn("Exported method missing doc for $val")
-        exported_missing_count += 1
-    else
-        # info("Unexported method missing doc for $val")
-        unexported_missing_count += 1
+    # Recover the unindented version of the signature.
+    n = length(match(DOCSTRING_DIRECTIVE, first(output))[1])
+    b = IOBuffer()
+    for line in output[1:end-1]
+        println(b, line[(n + 1):end])
     end
-    # println(tryrst(d, false))
-    # # Generate todo list ;-p
-    # println("- [ ] `$v`")
+    # The signature may contain `\` characaters, which must be unescaped.
+    signature = unescape_string(rstrip(takebuf_string(b)))
+    # Splice the correct docstring into the output after the signature.
+    if haskey(state.validdocs, signature)
+        # Push the rst text for the docstring into the output.
+        mod, (binding, typesig), docstr = state.validdocs[signature]
+        md  = Markdown.MD(Base.Docs.parsedoc(docstr).content[2:end])
+        rst = Base.Markdown.rst(md)
+        push!(output, "   .. Docstring generated from Julia source", "")
+        for line in split(rst, '\n')
+            line = isempty(line) ? "" : string(" "^3, line)
+            push!(output, rstrip(line))
+        end
+        # Consume all indented lines from the current docstring.
+        while !isempty(input)
+            line = first(input)
+            ismatch(r"^[^\s]", line) ? break : shift!(input)
+        end
+        # Track which docstrings have been found in the external docs.
+        state.documented[(binding, typesig)] = (mod, docstr, signature)
+    else
+        signature = indent(signature)
+        warn("missing docs for signature:\n\n$signature\n")
+        state.errorlevel = 1
+    end
+    return output
 end
 
-if (exported_missing_count + unexported_missing_count) > 0
-    println()
-    warn("Missing $exported_missing_count exported doc strings")
-    info("Missing $unexported_missing_count unexported doc strings")
+# Utilities.
+
+function indent(str::AbstractString, indent = 4)
+    buf = IOBuffer()
+    for line in split(str, '\n')
+        println(buf, " "^indent, line)
+    end
+    takebuf_string(buf)
 end
+
+function validdocstr(markdown::Base.Markdown.MD)
+    content = markdown.content
+    !isempty(content) && isa(first(content), Base.Markdown.Code)
+end
+validdocstr(other) = false
+
+end
+
+GenStdLib.translate(["manual", "stdlib", "devdocs"])
