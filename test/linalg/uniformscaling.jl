bb752da6636d9e370890ff15f159ec212bf7ebaf
Andreas Noack <andreasnoackjensen@gmail.com>
2016-03-07 16:08:48 -0500

    Promote in *(SparseMatrixCSC, Diagonal). Fixes #15380

--- test/linalg/uniformscaling.jl
+++ test/linalg/uniformscaling.jl
@@ -36,86 +36,88 @@ B = bitrand(2,2)
 @test B + I == B + eye(B)
 @test I + B == B + eye(B)
 
-let AA = randn(2,2), SS = sprandn(3,3,0.5)
-    for atype in ("Array", "SubArray")
-        if atype == "Array"
-            A = AA
-            S = SS
-        else
-            A = sub(AA, 1:2, 1:2)
-            S = sub(SS, 1:3, 1:3)
+let AA = randn(2, 2)
+    for SS in (sprandn(3,3, 0.5), speye(Int, 3))
+        for atype in ("Array", "SubArray")
+            if atype == "Array"
+                A = AA
+                S = SS
+            else
+                A = sub(AA, 1:2, 1:2)
+                S = sub(SS, 1:3, 1:3)
+            end
+
+            @test A + I == A + eye(A)
+            @test I + A == A + eye(A)
+            @test I - I === UniformScaling(0)
+            @test B - I == B - eye(B)
+            @test I - B == eye(B) - B
+            @test A - I == A - eye(A)
+            @test I - A == eye(A) - A
+            @test I*J === UniformScaling(λ)
+            @test B*J == B*λ
+            @test J*B == B*λ
+
+            @test S*J == S*λ
+            @test J*S == S*λ
+            @test A*J == A*λ
+            @test J*A == A*λ
+            @test J*ones(3) == ones(3)*λ
+            @test λ*J === UniformScaling(λ*J.λ)
+            @test J*λ === UniformScaling(λ*J.λ)
+            @test J/I === J
+            @test I/A == inv(A)
+            @test A/I == A
+            @test I/λ === UniformScaling(1/λ)
+            @test I\J === J
+
+            if atype == "Array"
+                T = LowerTriangular(randn(3,3))
+            else
+                T = LowerTriangular(sub(randn(3,3), 1:3, 1:3))
+            end
+            @test T + J == full(T) + J
+            @test J + T == J + full(T)
+            @test T - J == full(T) - J
+            @test J - T == J - full(T)
+            @test T\I == inv(T)
+
+            if atype == "Array"
+                T = LinAlg.UnitLowerTriangular(randn(3,3))
+            else
+                T = LinAlg.UnitLowerTriangular(sub(randn(3,3), 1:3, 1:3))
+            end
+            @test T + J == full(T) + J
+            @test J + T == J + full(T)
+            @test T - J == full(T) - J
+            @test J - T == J - full(T)
+            @test T\I == inv(T)
+
+            if atype == "Array"
+                T = UpperTriangular(randn(3,3))
+            else
+                T = UpperTriangular(sub(randn(3,3), 1:3, 1:3))
+            end
+            @test T + J == full(T) + J
+            @test J + T == J + full(T)
+            @test T - J == full(T) - J
+            @test J - T == J - full(T)
+            @test T\I == inv(T)
+
+            if atype == "Array"
+                T = LinAlg.UnitUpperTriangular(randn(3,3))
+            else
+                T = LinAlg.UnitUpperTriangular(sub(randn(3,3), 1:3, 1:3))
+            end
+            @test T + J == full(T) + J
+            @test J + T == J + full(T)
+            @test T - J == full(T) - J
+            @test J - T == J - full(T)
+            @test T\I == inv(T)
+
+            @test I\A == A
+            @test A\I == inv(A)
+            @test λ\I === UniformScaling(1/λ)
         end
-
-        @test A + I == A + eye(A)
-        @test I + A == A + eye(A)
-        @test I - I === UniformScaling(0)
-        @test B - I == B - eye(B)
-        @test I - B == eye(B) - B
-        @test A - I == A - eye(A)
-        @test I - A == eye(A) - A
-        @test I*J === UniformScaling(λ)
-        @test B*J == B*λ
-        @test J*B == B*λ
-
-        @test S*J == S*λ
-        @test J*S == S*λ
-        @test A*J == A*λ
-        @test J*A == A*λ
-        @test J*ones(3) == ones(3)*λ
-        @test λ*J === UniformScaling(λ*J.λ)
-        @test J*λ === UniformScaling(λ*J.λ)
-        @test J/I === J
-        @test I/A == inv(A)
-        @test A/I == A
-        @test I/λ === UniformScaling(1/λ)
-        @test I\J === J
-
-        if atype == "Array"
-            T = LowerTriangular(randn(3,3))
-        else
-            T = LowerTriangular(sub(randn(3,3), 1:3, 1:3))
-        end
-        @test T + J == full(T) + J
-        @test J + T == J + full(T)
-        @test T - J == full(T) - J
-        @test J - T == J - full(T)
-        @test T\I == inv(T)
-
-        if atype == "Array"
-            T = LinAlg.UnitLowerTriangular(randn(3,3))
-        else
-            T = LinAlg.UnitLowerTriangular(sub(randn(3,3), 1:3, 1:3))
-        end
-        @test T + J == full(T) + J
-        @test J + T == J + full(T)
-        @test T - J == full(T) - J
-        @test J - T == J - full(T)
-        @test T\I == inv(T)
-
-        if atype == "Array"
-            T = UpperTriangular(randn(3,3))
-        else
-            T = UpperTriangular(sub(randn(3,3), 1:3, 1:3))
-        end
-        @test T + J == full(T) + J
-        @test J + T == J + full(T)
-        @test T - J == full(T) - J
-        @test J - T == J - full(T)
-        @test T\I == inv(T)
-
-        if atype == "Array"
-            T = LinAlg.UnitUpperTriangular(randn(3,3))
-        else
-            T = LinAlg.UnitUpperTriangular(sub(randn(3,3), 1:3, 1:3))
-        end
-        @test T + J == full(T) + J
-        @test J + T == J + full(T)
-        @test T - J == full(T) - J
-        @test J - T == J - full(T)
-        @test T\I == inv(T)
-
-        @test I\A == A
-        @test A\I == inv(A)
-        @test λ\I === UniformScaling(1/λ)
     end
 end
