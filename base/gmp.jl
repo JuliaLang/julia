2f5c0831e0947771488d27eeea1a4a4368076694
Anthony Clays <anthonyclays@gmail.com>
2016-03-10 17:25:43 +0100

    Implemented powermod for negative exponents

--- base/gmp.jl
+++ base/gmp.jl
@@ -432,7 +432,7 @@ end
 ^(x::Integer, y::BigInt ) = bigint_pow(BigInt(x), y)
 
 function powermod(x::BigInt, p::BigInt, m::BigInt)
-    p < 0 && throw(DomainError())
+    p < 0 && return powermod(invmod(x, m), -p, m)
     r = BigInt()
     ccall((:__gmpz_powm, :libgmp), Void,
           (Ptr{BigInt}, Ptr{BigInt}, Ptr{BigInt}, Ptr{BigInt}),
