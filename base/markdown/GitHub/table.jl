14a4feda57839182244881da0cebebf80126a37a
Michael Hatherly <michaelhatherly@gmail.com>
2016-03-16 12:10:42 +0200

    Use "sidebars" for markdown tables.
    Adds `|` columns on each side of markdown tables to avoid trailing
    whitespace problems in rendered documents. This is still valid markdown.
    
    This does not change the parsing of markdown tables, which can still
    be written without the `|` columns on each side.

--- base/markdown/GitHub/table.jl
+++ base/markdown/GitHub/table.jl
@@ -97,20 +97,22 @@ function padcells!(rows, align; len = length, min = 0)
 end
 
 _dash(width, align) =
-    align == :l ? ":" * "-"^(width-1) :
-    align == :r ? "-"^(width-1) * ":" :
-    align == :c ? ":" * "-"^(width-2) * ":" :
+    align == :l ? ":" * "-"^width * " " :
+    align == :r ? " " * "-"^width * ":" :
+    align == :c ? ":" * "-"^width * ":" :
     throw(ArgumentError("Invalid alignment $align"))
 
 function plain(io::IO, md::Table)
     cells = mapmap(plaininline, md.rows)
     padcells!(cells, md.align, len = length, min = 3)
     for i = 1:length(cells)
+        print(io, "| ")
         print_joined(io, cells[i], " | ")
-        println(io)
+        println(io, " |")
         if i == 1
-            print_joined(io, [_dash(length(cells[i][j]), md.align[j]) for j = 1:length(cells[1])], " | ")
-            println(io)
+            print(io, "|")
+            print_joined(io, [_dash(length(cells[i][j]), md.align[j]) for j = 1:length(cells[1])], "|")
+            println(io, "|")
         end
     end
 end
