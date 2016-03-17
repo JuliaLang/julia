3837e903e0921b0f82ac410ed2d7e2182cb5805d
Stefan Karpinski <stefan@karpinski.org>
2016-03-17 17:00:25 -0400

    wip

--- test/dict.jl
+++ test/dict.jl
@@ -450,7 +450,7 @@ end
 # issue #15077
 
 immutable MyString <: AbstractString
-    str::ASCIIString
+    str::String
 end
 import Base.==
 
@@ -462,19 +462,21 @@ Base.next(s::MyString, v::Int) = next(s.str, v)
 Base.isequal(a::MyString, b::MyString) = isequal(a.str, b.str)
 ==(a::MyString, b::MyString) = (a.str == b.str)
 
-let badKeys = ASCIIString["FINO_emv5.0","FINO_ema0.1","RATE_ema1.0","NIBPM_ema1.0",
-                          "SAO2_emv5.0","O2FLOW_ema5.0","preop_Neuro/Psych_","gender_",
-                          "FIO2_ema0.1","PEAK_ema5.0","preop_Reproductive_denies","O2FLOW_ema0.1",
-                          "preop_Endocrine_denies","preop_Respiratory_",
-                          "NIBPM_ema0.1","PROPOFOL_MCG/KG/MIN_decay5.0","NIBPD_ema1.0","NIBPS_ema5.0",
-                          "anesthesiaStartTime","NIBPS_ema1.0","RESPRATE_ema1.0","PEAK_ema0.1",
-                          "preop_GU_denies","preop_Cardiovascular_","PIP_ema5.0","preop_ENT_denies",
-                          "preop_Skin_denies","preop_Renal_denies","asaCode_IIIE","N2OFLOW_emv5.0",
-                          "NIBPD_emv5.0", # <--- here is the key that we later can't find
-                          "NIBPM_ema5.0","preop_Respiratory_complete","ETCO2_ema5.0",
-                          "RESPRATE_ema0.1","preop_Functional Status_<2","preop_Renal_symptoms",
-                          "ECGRATE_ema5.0","FIO2_emv5.0","RESPRATE_emv5.0","7wu3ty0a4fs","BVO",
-                          "4UrCWXUsaT"]
+let badKeys = [
+    "FINO_emv5.0","FINO_ema0.1","RATE_ema1.0","NIBPM_ema1.0",
+    "SAO2_emv5.0","O2FLOW_ema5.0","preop_Neuro/Psych_","gender_",
+    "FIO2_ema0.1","PEAK_ema5.0","preop_Reproductive_denies","O2FLOW_ema0.1",
+    "preop_Endocrine_denies","preop_Respiratory_",
+    "NIBPM_ema0.1","PROPOFOL_MCG/KG/MIN_decay5.0","NIBPD_ema1.0","NIBPS_ema5.0",
+    "anesthesiaStartTime","NIBPS_ema1.0","RESPRATE_ema1.0","PEAK_ema0.1",
+    "preop_GU_denies","preop_Cardiovascular_","PIP_ema5.0","preop_ENT_denies",
+    "preop_Skin_denies","preop_Renal_denies","asaCode_IIIE","N2OFLOW_emv5.0",
+    "NIBPD_emv5.0", # <--- here is the key that we later can't find
+    "NIBPM_ema5.0","preop_Respiratory_complete","ETCO2_ema5.0",
+    "RESPRATE_ema0.1","preop_Functional Status_<2","preop_Renal_symptoms",
+    "ECGRATE_ema5.0","FIO2_emv5.0","RESPRATE_emv5.0","7wu3ty0a4fs","BVO",
+    "4UrCWXUsaT"
+]
     d = Dict{AbstractString,Int}()
     for i = 1:length(badKeys)
         d[badKeys[i]] = i
