6c7451b299f14c0eaf6da49902fc29658885cb5f
Jameson Nash <vtjnash@gmail.com>
2016-04-18 00:23:00 -0400

    eliminate stage0 inference
    all it did was ensure that everything in specializations was inferred
    and require a lot of complex infrastructure to make it acceptable to
    replace a module during compilation
    
    but instead we can just infer everything in specializations directly

--- src/toplevel.c
+++ src/toplevel.c
@@ -118,15 +118,12 @@ jl_value_t *jl_eval_module_expr(jl_expr_t *ex)
             jl_errorf("invalid redefinition of constant %s",
                       jl_symbol_name(name));
         }
-        if (jl_generating_output() && jl_options.incremental) {
-            jl_errorf("cannot replace module %s during incremental compile",
+        if (jl_generating_output()) {
+            jl_errorf("cannot replace module %s during compilation",
                       jl_symbol_name(name));
         }
-        if (!jl_generating_output()) {
-            // suppress warning "replacing module Core.Inference" during bootstrapping
-            jl_printf(JL_STDERR, "WARNING: replacing module %s\n",
-                      jl_symbol_name(name));
-        }
+        jl_printf(JL_STDERR, "WARNING: replacing module %s\n",
+                  jl_symbol_name(name));
     }
     jl_module_t *newm = jl_new_module(name);
     newm->parent = parent_module;
@@ -544,7 +541,7 @@ jl_value_t *jl_toplevel_eval_flex(jl_value_t *e, int fast)
 
     thk->specTypes = (jl_tupletype_t*)jl_typeof(jl_emptytuple); // no gc_wb needed
     if (ewc) {
-        jl_type_infer(thk);
+        jl_type_infer(thk, 0);
         jl_value_t *dummy_f_arg=NULL;
         result = jl_call_method_internal(thk, &dummy_f_arg, 1);
     }
