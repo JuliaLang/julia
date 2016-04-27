e1fadf8b496c1d3f32ad0f619274d5de695dfba8
Curtis Vogt <curtis.vogt@gmail.com>
2016-04-27 13:13:36 -0500

    Add missing promote_op method for Nullable

--- base/nullable.jl
+++ base/nullable.jl
@@ -25,6 +25,7 @@ convert(   ::Type{Nullable   }, ::Void) = Nullable{Union{}}()
 
 promote_rule{S,T}(::Type{Nullable{S}}, ::Type{T}) = Nullable{promote_type(S, T)}
 promote_rule{S,T}(::Type{Nullable{S}}, ::Type{Nullable{T}}) = Nullable{promote_type(S, T)}
+promote_op{S,T}(op::Any, ::Type{Nullable{S}}, ::Type{Nullable{T}}) = Nullable{promote_op(op, S, T)}
 
 function show{T}(io::IO, x::Nullable{T})
     if get(io, :compact, false)
