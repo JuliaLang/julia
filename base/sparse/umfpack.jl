c9292a06707ca7c8fb1ad39fc18acc8724a3f202
Katie Hyatt <kshyatt@physics.ucsb.edu>
2016-05-05 20:24:29 -0700

    Unwind horrible getindex and add a test

--- base/sparse/umfpack.jl
+++ base/sparse/umfpack.jl
@@ -415,13 +415,21 @@ end
 
 function getindex(lu::UmfpackLU, d::Symbol)
     L,U,p,q,Rs = umf_extract(lu)
-    d == :L ? L :
-    (d == :U ? U :
-     (d == :p ? p :
-      (d == :q ? q :
-       (d == :Rs ? Rs :
-        (d == :(:) ? (L,U,p,q,Rs) :
-         throw(KeyError(d)))))))
+    if d == :L
+        return L
+    elseif d == :U
+        return U
+    elseif d == :p
+        return p
+    elseif d == :q
+        return q
+    elseif d == :Rs
+        return Rs
+    elseif d == :(:)
+        return (L,U,p,q,Rs)
+    else
+        throw(KeyError(d))
+    end
 end
 
 for Tv in (:Float64, :Complex128), Ti in UmfpackIndexTypes
