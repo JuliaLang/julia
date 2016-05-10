1ddfa8e2cd5c62c9b6918b3773546ffa8d1dcd7f
Art <wildart@users.noreply.github.com>
2016-05-10 15:37:24 -0400

    fixed error in `LibGit2.merge!` when a branch does not have a remote (#15937)
    * fixed merge error when branch does not have remote
    
    fixed error in an exception message creation when exception isn't GitError
    (Found it while looked at #14616)
    
    testing LibGit2.merge! exceptions
    
    removed changes in original message
    
    fixed #13991, rethrowing uknown errors
    
    defer exceptions until update end
    
    moved statement to avoid contamination of backtrace
    
    * fixed similar error in `rebase!`, fixed typos
    
    * Separate internal API and Pkg UI commands by using `PkgError`
    `PkgError` becomes an information error that would keep underlying exceptions
    `CapturedException` should have supertype `Exception`
    
    * Put off `Cache.prefetch` exceptions in `Pkg.update`
    
    * enable `fetch` exceptions, nice `PkgError` output
    
    * fix possible undefined behavior (ref pull/14146)
    
    * fixed indentations

--- base/pkg.jl
+++ base/pkg.jl
@@ -11,6 +11,23 @@ const META_BRANCH = "metadata-v2"
 
 type PkgError <: Exception
     msg::AbstractString
+    ex::Nullable{Exception}
+end
+PkgError(msg::AbstractString) = PkgError(msg, Nullable{Exception}())
+function Base.showerror(io::IO, pkgerr::PkgError)
+    print(io, pkgerr.msg)
+    if !isnull(pkgerr.ex)
+        pkgex = get(pkgerr.ex)
+        if isa(pkgex, CompositeException)
+            for cex in pkgex
+                print(io, "\n=> ")
+                showerror(io, cex)
+            end
+        else
+            print(io, "\n")
+            showerror(io, pkgex)
+        end
+    end
 end
 
 for file in split("dir types reqs cache read query resolve write entry git")
