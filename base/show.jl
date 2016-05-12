88d1749d2633b54237bc231c7a97d33c8c68ca78
Jeff Bezanson <jeff.bezanson@gmail.com>
2016-05-12 17:57:27 -0400

    keep track of filenames in IR with metadata push/pop
    - add location info to empty blocks; fixes #15280
    - remove file names from non-initial `line` nodes
    - emit `(meta push_loc filename)` and `(meta pop_loc)` around nested
      blocks from different files, to handle macros
    - fix line number of `catch` blocks

--- base/show.jl
+++ base/show.jl
@@ -531,7 +531,7 @@ end
 ## AST printing ##
 
 show_unquoted(io::IO, sym::Symbol, ::Int, ::Int)        = print(io, sym)
-show_unquoted(io::IO, ex::LineNumberNode, ::Int, ::Int) = show_linenumber(io, ex.line, ex.file)
+show_unquoted(io::IO, ex::LineNumberNode, ::Int, ::Int) = show_linenumber(io, ex.line)
 show_unquoted(io::IO, ex::LabelNode, ::Int, ::Int)      = print(io, ex.label, ": ")
 show_unquoted(io::IO, ex::GotoNode, ::Int, ::Int)       = print(io, "goto ", ex.label)
 show_unquoted(io::IO, ex::GlobalRef, ::Int, ::Int)      = print(io, ex.mod, '.', ex.name)
@@ -597,7 +597,7 @@ function show_unquoted(io::IO, ex::Expr, indent::Int, prec::Int)
     head, args, nargs = ex.head, ex.args, length(ex.args)
     emphstate = typeemphasize(io)
     show_type = true
-    if (ex.head == :(=) ||
+    if (ex.head == :(=) || ex.head == :line ||
         ex.head == :boundscheck ||
         ex.head == :gotoifnot ||
         ex.head == :return)
