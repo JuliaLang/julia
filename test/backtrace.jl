06d4b2aded9e11c3e234e816c6096451bec6ff8b
Jeff Bezanson <jeff.bezanson@gmail.com>
2016-05-16 17:19:35 -0400

    add back backtrace tests for inlined functions

--- test/backtrace.jl
+++ test/backtrace.jl
@@ -13,6 +13,61 @@ end
 
 @test have_backtrace
 
+# Test location information for inlined code (ref issues #1334 #12544)
+module test_inline_bt
+using Base.Test
+
+function get_bt_frames(functionname, bt)
+    for i = 1:length(bt)
+        lkup = Base.StackTraces.lookup(bt[i])
+        lkup[end].func == functionname && (return lkup)
+    end
+end
+
+# same-file inline
+eval(Expr(:function, Expr(:call, :test_inline_1),
+          Expr(:block, Expr(:line, 42, Symbol("backtrace.jl")),
+                       Expr(:block, Expr(:meta, :push_loc, Symbol("backtrace.jl"), :inlfunc),
+                                    Expr(:line, 37),
+                                    Expr(:call, :throw, "foo"),
+                                    Expr(:meta, :pop_loc)))))
+
+try
+    test_inline_1()
+    error("unexpected")
+catch err
+    lkup = get_bt_frames(:test_inline_1, catch_backtrace())
+    @test length(lkup) == 2
+    @test endswith(string(lkup[2].file), "backtrace.jl")
+    @test lkup[2].line == 42
+    @test lkup[1].func == :inlfunc
+    @test endswith(string(lkup[1].file), "backtrace.jl")
+    @test lkup[1].line == 37
+end
+
+# different-file inline
+const absfilepath = OS_NAME == :Windows ? "C:\\foo\\bar\\baz.jl" : "/foo/bar/baz.jl"
+eval(Expr(:function, Expr(:call, :test_inline_2),
+          Expr(:block, Expr(:line, 99, Symbol("backtrace.jl")),
+                       Expr(:block, Expr(:meta, :push_loc, Symbol(absfilepath)),
+                                    Expr(:line, 111),
+                                    Expr(:call, :throw, "foo"),
+                                    Expr(:meta, :pop_loc)))))
+
+try
+    test_inline_2()
+    error("unexpected")
+catch err
+    lkup = get_bt_frames(:test_inline_2, catch_backtrace())
+    @test length(lkup) == 2
+    @test endswith(string(lkup[2].file), "backtrace.jl")
+    @test lkup[2].line == 99
+    @test string(lkup[1].file) == absfilepath
+    @test lkup[1].line == 111
+end
+
+end # module
+
 #issue 12977: line numbers for kwarg methods.
 linenum = @__LINE__; f12977(; args...) = ()
 loc = functionloc(f12977)
