e87176823f62a5283dec169ebbdb0dc08528e3b4
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2016-05-18 17:54:35 +0200

    allow A_ldiv_B!(x, LUFact, b)

--- test/sparsedir/umfpack.jl
+++ test/sparsedir/umfpack.jl
@@ -20,38 +20,47 @@ for Tv in (Float64, Complex128)
         @test nnz(lua) == 18
         @test_throws KeyError lua[:Z]
         L,U,p,q,Rs = lua[:(:)]
-        @test_approx_eq (Diagonal(Rs) * A)[p,q] L * U
+        @test (Diagonal(Rs) * A)[p,q] ≈ L * U
 
-        @test_approx_eq det(lua) det(full(A))
+        det(lua) ≈ det(full(A))
 
         b = [8., 45., -3., 3., 19.]
         x = lua\b
-        @test_approx_eq x float([1:5;])
+        @test x ≈ float([1:5;])
 
         @test norm(A*x-b,1) < eps(1e4)
-        x = Base.SparseArrays.A_ldiv_B!(lua,complex(b,zeros(b)))
-        @test_approx_eq x float([1:5;])
+        z = complex(b,zeros(b))
+        x = Base.SparseArrays.A_ldiv_B!(lua, z)
+        @test x ≈ float([1:5;])
+        y = similar(z)
+        A_ldiv_B!(y, lua,z)
+        @test y ≈ x
 
         @test norm(A*x-b,1) < eps(1e4)
 
         b = [8., 20., 13., 6., 17.]
         x = lua'\b
-        @test_approx_eq x float([1:5;])
+        @test x ≈ float([1:5;])
 
         @test norm(A'*x-b,1) < eps(1e4)
         x = Base.SparseArrays.Ac_ldiv_B!(lua,complex(b,zeros(b)))
-        @test_approx_eq x float([1:5;])
+        @test x ≈ float([1:5;])
+        y = similar(x)
+        Base.SparseArrays.Ac_ldiv_B!(y, lua,complex(b,zeros(b)))
+        @test y ≈ x
 
         @test norm(A'*x-b,1) < eps(1e4)
         x = lua.'\b
-        @test_approx_eq x float([1:5;])
+        @test x ≈ float([1:5;])
 
         @test norm(A.'*x-b,1) < eps(1e4)
         x = Base.SparseArrays.At_ldiv_B!(lua,complex(b,zeros(b)))
-        @test_approx_eq x float([1:5;])
+        @test x ≈ float([1:5;])
+        y = similar(x)
+        Base.SparseArrays.At_ldiv_B!(y, lua,complex(b,zeros(b)))
+        @test y ≈ x
 
         @test norm(A.'*x-b,1) < eps(1e4)
-
 
         # Element promotion and type inference
         @inferred lua\ones(Int, size(A, 2))
@@ -63,7 +72,7 @@ for Ti in Base.SparseArrays.UMFPACK.UMFITypes.types
     Ac = convert(SparseMatrixCSC{Complex128,Ti}, Ac0)
     lua = lufact(Ac)
     L,U,p,q,Rs = lua[:(:)]
-    @test_approx_eq (Diagonal(Rs) * Ac)[p,q] L * U
+    @test (Diagonal(Rs) * Ac)[p,q] ≈ L * U
 end
 
 for elty in (Float64, Complex128)
@@ -71,13 +80,13 @@ for elty in (Float64, Complex128)
         A = sparse([1:min(m,n); rand(1:m, 10)], [1:min(m,n); rand(1:n, 10)], elty == Float64 ? randn(min(m, n) + 10) : complex(randn(min(m, n) + 10), randn(min(m, n) + 10)))
         F = lufact(A)
         L, U, p, q, Rs = F[:(:)]
-        @test_approx_eq (Diagonal(Rs) * A)[p,q] L * U
+        @test (Diagonal(Rs) * A)[p,q] ≈ L * U
     end
 end
 
 #4523 - complex sparse \
 x = speye(2) + im * speye(2)
-@test_approx_eq (x*(lufact(x) \ ones(2))) ones(2)
+@test (x*(lufact(x) \ ones(2))) ≈ ones(2)
 
 @test det(sparse([1,3,3,1], [1,1,3,3], [1,1,1,1])) == 0
 
