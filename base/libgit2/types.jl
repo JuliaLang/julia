1b994b4234663492411cb2066c5b3711d1f51a76
Kevin Squire <kevin.squire@gmail.com>
2016-05-20 21:25:44 -0700

    Add speculative MergeOptions change for libgit2 v0.25.0
    * on libgit2 master, git_merge_options has an additional field,
      default_driver.  Presumably this modification will be released
      with v0.25.0, so test for that now.

--- base/libgit2/types.jl
+++ base/libgit2/types.jl
@@ -357,7 +357,8 @@ immutable DiffDelta
 end
 DiffDelta() = DiffDelta(Cint(0), UInt32(0), UInt16(0), UInt16(0), DiffFile(), DiffFile())
 
-if LibGit2.version() >= v"0.24.0"
+# TODO: double check this when libgit2 v0.25.0 is released
+if LibGit2.version() >= v"0.25.0"
     immutable MergeOptions
         version::Cuint
         flags::Cint
@@ -365,7 +366,7 @@ if LibGit2.version() >= v"0.24.0"
         target_limit::Cuint
         metric::Ptr{Void}
         recursion_limit::Cuint
-        # default_driver::Cstring  # TODO: to be added in v"0.25.0" (?)
+        default_driver::Cstring
         file_favor::Cint
         file_flags::Cuint
     end
@@ -374,7 +375,7 @@ if LibGit2.version() >= v"0.24.0"
                    target_limit::Cuint = Cuint(200),
                    metric::Ptr{Void} = C_NULL,
                    recursion_limit::Cuint = Cuint(0),
-                   # default_driver::Cstring = Cstring_NULL,
+                   default_driver::Cstring = Cstring_NULL,
                    file_favor::Cint = Cint(Consts.MERGE_FILE_FAVOR_NORMAL),
                    file_flags::Cuint = Cuint(Consts.MERGE_FILE_DEFAULT)) =
         MergeOptions(one(Cuint),
@@ -383,7 +384,33 @@ if LibGit2.version() >= v"0.24.0"
                      target_limit,
                      metric,
                      recursion_limit,
-                     # default_driver,
+                     default_driver,
+                     file_favor,
+                     file_flags)
+elseif LibGit2.version() >= v"0.24.0"
+    immutable MergeOptions
+        version::Cuint
+        flags::Cint
+        rename_threshold::Cuint
+        target_limit::Cuint
+        metric::Ptr{Void}
+        recursion_limit::Cuint
+        file_favor::Cint
+        file_flags::Cuint
+    end
+    MergeOptions(; flags::Cint = Cint(0),
+                   rename_threshold::Cuint = Cuint(50),
+                   target_limit::Cuint = Cuint(200),
+                   metric::Ptr{Void} = C_NULL,
+                   recursion_limit::Cuint = Cuint(0),
+                   file_favor::Cint = Cint(Consts.MERGE_FILE_FAVOR_NORMAL),
+                   file_flags::Cuint = Cuint(Consts.MERGE_FILE_DEFAULT)) =
+        MergeOptions(one(Cuint),
+                     flags,
+                     rename_threshold,
+                     target_limit,
+                     metric,
+                     recursion_limit,
                      file_favor,
                      file_flags)
 else
