922fdbf9ee100e587a82bb3d368600c8815a1e78
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2016-05-21 13:18:12 +0200

    Replace deprecated Array(T..., dims...) with Array{T...}(dims...) in base/ for files starting with p-r.

--- base/range.jl
+++ base/range.jl
@@ -761,7 +761,7 @@ function vcat{T}(rs::Range{T}...)
     for ra in rs
         n += length(ra)
     end
-    a = Array(T,n)
+    a = Array{T}(n)
     i = 1
     for ra in rs, x in ra
         @inbounds a[i] = x
