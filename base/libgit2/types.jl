93a25e0ef8e2137ee70ba54522e82654facf3fdd
Kevin Squire <kevin.squire@gmail.com>
2016-05-20 21:29:02 -0700

    Add inmemory keyword option to RebaseOptions for libgit2 v0.24.0
    * #16370 set this option to a constant value

--- base/libgit2/types.jl
+++ base/libgit2/types.jl
@@ -515,10 +515,11 @@ if LibGit2.version() >= v"0.24.0"
         checkout_opts::CheckoutOptions
     end
     RebaseOptions(; quiet::Cint = Cint(1),
+                    inmemory::Cint = Cint(0),
                     rewrite_notes_ref::Cstring = Cstring_NULL,
                     merge_opts::MergeOptions = MergeOptions(),
                     checkout_opts::CheckoutOptions = CheckoutOptions()) =
-        RebaseOptions(one(Cuint), quiet, Cint(0), rewrite_notes_ref, merge_opts, checkout_opts)
+        RebaseOptions(one(Cuint), quiet, inmemory, rewrite_notes_ref, merge_opts, checkout_opts)
 else
     immutable RebaseOptions
         version::Cuint
