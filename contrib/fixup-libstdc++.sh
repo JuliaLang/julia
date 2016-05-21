21b635fe5a4b3935e1eb242432a305a5528075fb
Tony Kelman <tony@kelman.net>
2016-05-20 22:56:22 -0400

    Update fixup-libstdc++.sh for libjulia being in libdir instead of private_libdir
    remove "major" from semver description

--- contrib/fixup-libstdc++.sh
+++ contrib/fixup-libstdc++.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
-# Run as: fixup-libstdc++.sh <$private_libdir>
+# Run as: fixup-libstdc++.sh <$libdir>
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <private_libdir>"
+    echo "Usage: $0 <libdir>"
     exit 1
 fi
 
-private_libdir=$1
+libdir=$1
 
-if [ ! -f "$private_libdir/libjulia.so" ]; then
-    echo "ERROR: Could not open $private_libdir/libjulia.so" >&2
+if [ ! -f "$libdir/libjulia.so" ]; then
+    echo "ERROR: Could not open $libdir/libjulia.so" >&2
     exit 2
 fi
 
 find_shlib ()
 {
-    if [ -f "$private_libdir/lib$1.so" ]; then
-        ldd "$private_libdir/lib$1.so" | grep $2 | cut -d' ' -f3 | xargs
+    if [ -f "$libdir/lib$1.so" ]; then
+        ldd "$libdir/lib$1.so" | grep $2 | cut -d' ' -f3 | xargs
     fi
 }
 
@@ -27,7 +27,7 @@ LIBSTD=$(find_shlib "julia" "libstdc++.so")
 LIBSTD_NAME=$(basename $LIBSTD)
 LIBSTD_DIR=$(dirname $LIBSTD)
 
-if [ ! -f "$private_libdir/$LIBSTD_NAME" ] && [ -f "$LIBSTD_DIR/$LIBSTD_NAME" ]; then
-    cp -v "$LIBSTD_DIR/$LIBSTD_NAME" "$private_libdir"
-    chmod 755 "$private_libdir/$LIBSTD_NAME"
+if [ ! -f "$libdir/$LIBSTD_NAME" ] && [ -f "$LIBSTD_DIR/$LIBSTD_NAME" ]; then
+    cp -v "$LIBSTD_DIR/$LIBSTD_NAME" "$libdir"
+    chmod 755 "$libdir/$LIBSTD_NAME"
 fi
