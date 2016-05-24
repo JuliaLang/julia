a2090be645b2572dae9d3508f55ac66517972233
Andreas Noack <andreasnoackjensen@gmail.com>
2016-05-23 23:33:55 -0400

    Loosen \(QRX,StridedMatOrVec) to \(QRX,AbstractMatOrVec) and fix a ton of ambiguity issues caused by this. This also cleans up real-complex solves. Fixes 16520.

--- base/linalg/factorization.jl
+++ base/linalg/factorization.jl
@@ -23,25 +23,18 @@ inv{T}(F::Factorization{T}) = A_ldiv_B!(F, eye(T, size(F,1)))
 
 # With a real lhs and complex rhs with the same precision, we can reinterpret
 # the complex rhs as a real rhs with twice the number of columns
-function (\){T<:BlasReal}(F::Factorization{T}, B::AbstractVector{Complex{T}})
+function (\){T<:BlasReal}(F::Factorization{T}, B::VecOrMat{Complex{T}})
     c2r = reshape(transpose(reinterpret(T, B, (2, length(B)))), size(B, 1), 2*size(B, 2))
     x = A_ldiv_B!(F, c2r)
-    return reinterpret(Complex{T}, transpose(reshape(x, div(length(x), 2), 2)), (size(F,2),))
+    return reinterpret(Complex{T}, transpose(reshape(x, div(length(x), 2), 2)), _ret_size(F, B))
 end
-function (\){T<:BlasReal}(F::Factorization{T}, B::AbstractMatrix{Complex{T}})
-    c2r = reshape(transpose(reinterpret(T, B, (2, length(B)))), size(B, 1), 2*size(B, 2))
-    x = A_ldiv_B!(F, c2r)
-    return reinterpret(Complex{T}, transpose(reshape(x, div(length(x), 2), 2)), (size(F,2), size(B,2)))
-end
-
-(\){T<:BlasReal}(F::Factorization{T}, B::SubArray) = F\copy(B)
 
 for (f1, f2) in ((:\, :A_ldiv_B!),
                  (:Ac_ldiv_B, :Ac_ldiv_B!),
                  (:At_ldiv_B, :At_ldiv_B!))
     @eval begin
-        function $f1{TF<:Number,TB<:Number,N}(F::Factorization{TF}, B::AbstractArray{TB,N})
-            TFB = typeof(one(TF)/one(TB))
+        function $f1(F::Factorization, B::AbstractVecOrMat)
+            TFB = typeof(one(eltype(F)) / one(eltype(B)))
             BB = similar(B, TFB, size(B))
             copy!(BB, B)
             $f2(convert(Factorization{TFB}, F), BB)
