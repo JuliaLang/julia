36d57fe9102df4c0b5bfac2d4a67c11d5b6d8ace
Curtis Vogt <curtis.vogt@gmail.com>
2016-05-25 09:10:19 -0500

    Fix Pkg.build when run on a non-installed package

--- base/pkg/entry.jl
+++ base/pkg/entry.jl
@@ -523,9 +523,9 @@ end
 function build!(pkgs::Vector, buildstream::IO, seen::Set)
     for pkg in pkgs
         pkg == "julia" && continue
-        pkg in seen && continue
-        build!(Read.requires_list(pkg),buildstream,push!(seen,pkg))
+        pkg in seen ? continue : push!(seen,pkg)
         Read.isinstalled(pkg) || throw(PkgError("$pkg is not an installed package"))
+        build!(Read.requires_list(pkg),buildstream,seen)
         path = abspath(pkg,"deps","build.jl")
         isfile(path) || continue
         println(buildstream, path) # send to build process for evalfile
