7cf11639c53c36ae22dd513c8bd948d32ac074a7
Tim Holy <tim.holy@gmail.com>
2016-05-25 11:53:26 -0500

    Speed up `in` for UnitRange

--- base/range.jl
+++ base/range.jl
@@ -820,5 +820,6 @@ function in(x, r::Range)
     n >= 1 && n <= length(r) && r[n] == x
 end
 
+in{T<:Integer}(x::Integer, r::UnitRange{T}) = (first(r) <= x) & (x <= last(r))
 in{T<:Integer}(x, r::Range{T}) = isinteger(x) && !isempty(r) && x>=minimum(r) && x<=maximum(r) && (mod(convert(T,x),step(r))-mod(first(r),step(r)) == 0)
 in(x::Char, r::Range{Char}) = !isempty(r) && x >= minimum(r) && x <= maximum(r) && (mod(Int(x) - Int(first(r)), step(r)) == 0)
