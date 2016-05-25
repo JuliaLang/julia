32052ed352a5c4da1412f8b763a29b596cdf9c9f
Jeff Bezanson <jeff.bezanson@gmail.com>
2016-05-25 12:34:59 -0400

    add deprecation for showcompact_lim

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -1254,6 +1254,7 @@ end
 
 @deprecate_binding WORD_SIZE Sys.WORD_SIZE
 
+@deprecate showcompact_lim show
 
 # During the 0.5 development cycle, do not add any deprecations below this line
 # To be deprecated in 0.6
