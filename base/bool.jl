ef687a3963f626eccd9b1d031cc5035034018064
Milan Bouchet-Valat <nalimilan@club.fr>
2016-06-17 15:33:45 +0200

    Implement operators for Nullable with lifting semantics
    Use fast path without a branch for types with unchecked arithmetic
    (for which the operation can be computed even when value is missing)
    and a slow path for other types. The new null_safe_op() function
    allows custom types to opt-in to the fast path when possible.
    Also use this strategy in isequal(), which keeps its current
    (non-lifting) behavior.

--- base/bool.jl
+++ base/bool.jl
@@ -64,6 +64,8 @@ cld(x::Bool, y::Bool) = div(x,y)
 rem(x::Bool, y::Bool) = y ? false : throw(DivideError())
 mod(x::Bool, y::Bool) = rem(x,y)
 
+promote_op{T}(op::Type{T}, ::Type{Bool}) = T # to fix ambiguity
+promote_op(op, ::Type{Bool}) = typeof(op(true))
 promote_op(op, ::Type{Bool}, ::Type{Bool}) = typeof(op(true, true))
 promote_op(::typeof(^), ::Type{Bool}, ::Type{Bool}) = Bool
 promote_op{T<:Integer}(::typeof(^), ::Type{Bool}, ::Type{T}) = Bool
