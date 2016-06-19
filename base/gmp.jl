9e611a7e276fc7a4a11ae4ccb1231a8ba1d0a705
Yichao Yu <yyc1992@gmail.com>
2016-06-18 21:48:25 -0400

    Lazy initialize gmp and mpfr finalizers

--- base/gmp.jl
+++ base/gmp.jl
@@ -44,13 +44,10 @@ type BigInt <: Integer
     function BigInt()
         b = new(zero(Cint), zero(Cint), C_NULL)
         ccall((:__gmpz_init,:libgmp), Void, (Ptr{BigInt},), &b)
-        finalizer(b, _gmp_clear_func)
+        finalizer(b, cglobal((:__gmpz_clear, :libgmp)))
         return b
     end
 end
-
-_gmp_clear_func = C_NULL
-_mpfr_clear_func = C_NULL
 
 function __init__()
     try
@@ -60,8 +57,6 @@ function __init__()
                          "Please rebuild Julia."))
         end
 
-        global _gmp_clear_func = cglobal((:__gmpz_clear, :libgmp))
-        global _mpfr_clear_func = cglobal((:mpfr_clear, :libmpfr))
         ccall((:__gmp_set_memory_functions, :libgmp), Void,
               (Ptr{Void},Ptr{Void},Ptr{Void}),
               cglobal(:jl_gc_counted_malloc),
