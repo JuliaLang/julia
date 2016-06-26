5d84340c4473467689bb931e56eddebaa17ee633
Carlo Baldassi <carlobaldassi@gmail.com>
2016-06-26 10:40:50 -0400

    Emit a message in case partial Pkg.update fails

--- base/pkg/query.jl
+++ base/pkg/query.jl
@@ -90,6 +90,62 @@ function partial_update_mask(instd::Dict{String,Tuple{VersionNumber,Bool}}, avai
     return dont_update
 end
 
+# Try to produce some helpful message in case of a partial update which does not go all the way
+# (Does not do a full analysis, it only checks requirements and direct dependents.)
+function check_partial_updates(reqs::Requires, deps::Dict{String,Dict{VersionNumber,Available}}, want::Dict{String,VersionNumber}, fixed::Dict{String,Fixed}, upkgs::Set{String})
+    for p in upkgs
+        if !haskey(want, p)
+            if !haskey(fixed, p)
+                warn("Something went wrong with the update of package $p, please submit a bug report")
+                continue
+            end
+            v = fixed[p].version
+        else
+            v = want[p]
+            if haskey(fixed, p) && v != fixed[p].version
+                warn("Something went wrong with the update of package $p, please submit a bug report")
+                continue
+            end
+        end
+        haskey(deps, p) || continue
+        vers = sort!(collect(keys(deps[p])))
+        higher_vers = vers[vers .> v]
+        isempty(higher_vers) && continue # package p has been set to the highest available version
+
+        # Determine if there are packages which depend on `p` and somehow prevent its update to
+        # the latest version
+        blocking_parents = Set{String}()
+        for (p1,d1) in deps
+            p1 in upkgs && continue # package `p1` is among the ones to be updated, skip the check
+            haskey(fixed, p1) || continue # if package `p1` is not fixed, it can't be blocking
+            r1 = fixed[p1].requires # get `p1` requirements
+            haskey(r1, p) || continue # check if package `p1` requires `p`
+            vs1 = r1[p] # get the versions of `p` allowed by `p1` requirements
+            any(hv in vs1 for hv in higher_vers) && continue # package `p1` would allow some of the higher versions,
+                                                             # therefore it's not responsible for blocking `p`
+            push!(blocking_parents, p1) # package `p1` is blocking the update of `p`
+        end
+
+        # Determine if the update of `p` is prevented by explicit user-provided requirements
+        blocking_reqs = (haskey(reqs, p) && all(hv ∉ reqs[p] for hv in higher_vers))
+
+        # Determine if the update of `p` is prevented by it being fixed (e.g. it's dirty, or pinned...)
+        isfixed = haskey(fixed, p)
+
+        msg = "Package $p was set to version $v, but a higher version $(vers[end]) exists.\n"
+        if isfixed
+            msg *= "      The package is fixed. You can try using `Pkg.free(\"$p\")` to update it."
+        elseif blocking_reqs
+            msg *= "      The update is prevented by explicit requirements constraints. Edit your REQUIRE file to change this."
+        elseif !isempty(blocking_parents)
+            msg *= string("      To install the latest version, you could try updating these packages as well: ", join(blocking_parents, ", ", " and "), ".")
+        else
+            msg *= "      To install the latest version, you could try doing a full update with `Pkg.update()`."
+        end
+        info(msg)
+    end
+end
+
 typealias PackageState Union{Void,VersionNumber}
 
 function diff(have::Dict, want::Dict, avail::Dict, fixed::Dict)
