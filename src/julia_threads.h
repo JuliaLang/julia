09a0be0b9b4fe566edded466ba7a41b3c826e492
Yichao Yu <yyc1992@gmail.com>
2016-06-27 21:52:55 -0400

    Try to implement our own TLS on windows too since it does not seem to have a static TLS model either......

--- src/julia_threads.h
+++ src/julia_threads.h
@@ -366,7 +366,7 @@ STATIC_INLINE int8_t jl_gc_state_set(jl_tls_states_t *ptls,
 }
 #else // ifndef JULIA_ENABLE_THREADING
 typedef jl_tls_states_t *(*jl_get_ptls_states_func)(void);
-#ifndef _OS_DARWIN_
+#if !defined(_OS_DARWIN_) && !defined(_OS_WINDOWS_)
 JL_DLLEXPORT void jl_set_ptls_states_getter(jl_get_ptls_states_func f);
 #endif
 // Make sure jl_gc_state() is always a rvalue
