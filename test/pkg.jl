5ccae12e4b99d2e2a0afc0852d95e7141845380f
Carlo Baldassi <carlobaldassi@gmail.com>
2016-06-29 22:38:52 +0200

    More Pkg.pin() tests

--- test/pkg.jl
+++ test/pkg.jl
@@ -16,6 +16,44 @@ function temp_pkg_dir(fn::Function, remove_tmp_dir::Bool=true)
     end
 end
 
+macro grab_outputs(ex)
+    quote
+        local err::String, out::String
+        local ferrname = ""
+        local foutname = ""
+        local ret
+        try
+            OLD_STDERR = STDERR
+            ferrname = tempname()
+            ferr = open(ferrname, "w")
+            try
+                OLD_STDOUT = STDOUT
+                foutname = tempname()
+                fout = open(foutname, "w")
+                try
+                    redirect_stderr(ferr)
+                    try
+                        redirect_stdout(fout)
+                        ret = $(esc(ex))
+                    finally
+                        redirect_stdout(OLD_STDOUT)
+                        close(fout)
+                    end
+                finally
+                    redirect_stderr(OLD_STDERR)
+                    close(ferr)
+                end
+                out = readstring(foutname)
+                err = readstring(ferrname)
+            finally
+                isfile(foutname) && rm(foutname)
+            end
+        finally
+            isfile(ferrname) && rm(ferrname)
+        end
+        ret, out, err
+    end
+end
 
 # Test basic operations: adding or removing a package, status, free
 #Also test for the existence of REQUIRE and META_Branch
@@ -178,14 +216,77 @@ temp_pkg_dir() do
         end
     end
 
+    # Various pin/free/re-pin/change-pin patterns (issue #17176)
     begin
-        Pkg.pin("Example")
-        Pkg.free("Example")
+        ret, out, err = @grab_outputs Pkg.free("Example")
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Freeing Example")
 
-        Pkg.pin("Example", v"0.4.0")
-        @test Pkg.update() == nothing
-        @test Pkg.installed()["Example"] == v"0.4.0"
+        ret, out, err = @grab_outputs Pkg.pin("Example")
+        @test ret == nothing && out == ""
+        @test ismatch(r"INFO: Creating Example branch pinned\.[0-9a-f]{8}\.tmp", err)
+        @test !contains(err, "INFO: No packages to install, update or remove")
+        branchid = replace(err, r".*pinned\.([0-9a-f]{8})\.tmp.*"s, s"\1")
+        vers = Pkg.installed("Example")
 
+        ret, out, err = @grab_outputs Pkg.free("Example")
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Freeing Example")
+
+        ret, out, err = @grab_outputs Pkg.pin("Example", v"0.4.0")
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Creating Example branch pinned.b1990792.tmp")
+        @test contains(err, "INFO: No packages to install, update or remove")
+        @test Pkg.installed("Example") == v"0.4.0"
+
+        ret, out, err = @grab_outputs Pkg.pin("Example", v"0.4.0")
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Package Example is already pinned to the selected commit")
+        @test !contains(err, "INFO: No packages to install, update or remove")
+        @test Pkg.installed("Example") == v"0.4.0"
+
+        ret, out, err = @grab_outputs Pkg.pin("Example")
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Package Example is already pinned")
+        @test !contains(err, "INFO: No packages to install, update or remove")
+        @test Pkg.installed("Example") == v"0.4.0"
+
+        ret, out, err = @grab_outputs Pkg.update()
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Package Example: skipping update (pinned)...")
+        @test Pkg.installed("Example") == v"0.4.0"
+
+        ret, out, err = @grab_outputs Pkg.pin("Example", v"0.3.1")
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Creating Example branch pinned.d1ef7b00.tmp")
+        @test contains(err, "INFO: No packages to install, update or remove")
+        @test Pkg.installed("Example") == v"0.3.1"
+
+        ret, out, err = @grab_outputs Pkg.pin("Example", v"0.4.0")
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Package Example: checking out existing branch pinned.b1990792.tmp")
+        @test contains(err, "INFO: No packages to install, update or remove")
+        @test Pkg.installed("Example") == v"0.4.0"
+
+        ret, out, err = @grab_outputs Pkg.free("Example")
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Freeing Example")
+        @test contains(err, "INFO: No packages to install, update or remove")
+        @test Pkg.installed("Example") == vers
+
+        ret, out, err = @grab_outputs Pkg.pin("Example")
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Package Example: checking out existing branch pinned.$branchid.tmp")
+        @test !contains(err, "INFO: No packages to install, update or remove")
+        @test Pkg.installed("Example") == vers
+
+        ret, out, err = @grab_outputs Pkg.free("Example")
+        @test ret == nothing && out == ""
+        @test contains(err, "INFO: Freeing Example")
+        @test Pkg.installed("Example") == vers
+    end
+
+    begin
         # bug identified in #16850, Base.url \ vs / for non-Base methods
         include(Pkg.dir("Example","src","Example.jl"))
         meth = first(methods(Example.domath))
