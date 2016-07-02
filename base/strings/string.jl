33be76907eeba40a51c2e80fb074057aadf26ece
Milan Bouchet-Valat <nalimilan@club.fr>
2016-07-02 23:12:39 +0200

    Add reverseind() for AbstractString, re-enable tests with GenericString
    reverse() for GenericString/AbstractString returns a RevString, whose
    indexing behavior is very different from a reverse()'d String which
    is returned for String. Thus, calling reverseind() on the underlying String
    object is not correct for GenericString. Add a generic but O(n) method for
    AbstractString and use it for GenericString.

--- base/strings/string.jl
+++ base/strings/string.jl
@@ -104,7 +104,7 @@ function first_utf8_byte(ch::Char)
 end
 
 function reverseind(s::String, i::Integer)
-    j = lastidx(s) + 1 - i
+    j = length(s.data) + 1 - i
     d = s.data
     while is_valid_continuation(d[j])
         j -= 1
@@ -115,8 +115,6 @@ end
 ## overload methods for efficiency ##
 
 sizeof(s::String) = sizeof(s.data)
-
-lastidx(s::String) = length(s.data)
 
 isvalid(s::String, i::Integer) =
     (1 <= i <= endof(s.data)) && !is_valid_continuation(s.data[i])
