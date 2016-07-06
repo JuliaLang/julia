d4d507b10c0a9dd1d9812991fd483b57690e32cb
Martin Holters <martin.holters@hsu-hh.de>
2016-07-06 15:55:02 +0200

    Use promote_op in SparseMatrixCSC operators

--- base/sparse/sparse.jl
+++ base/sparse/sparse.jl
@@ -2,7 +2,7 @@
 
 module SparseArrays
 
-using Base: ReshapedArray, setindex_shape_check, to_shape
+using Base: ReshapedArray, promote_op, setindex_shape_check, to_shape
 using Base.Sort: Forward
 using Base.LinAlg: AbstractTriangular, PosDefException
 
@@ -26,7 +26,7 @@ import Base: @get!, acos, acosd, acot, acotd, acsch, asech, asin, asind, asinh,
     rotl90, rotr90, round, scale!, setindex!, similar, size, transpose, tril,
     triu, vcat, vec, permute!
 
-import Base.Broadcast: eltype_plus, broadcast_shape
+import Base.Broadcast: broadcast_shape
 
 export AbstractSparseArray, AbstractSparseMatrix, AbstractSparseVector,
     SparseMatrixCSC, SparseVector, blkdiag, dense, droptol!, dropzeros!, dropzeros, etree,
