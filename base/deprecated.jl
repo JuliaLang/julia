7bf226de9bb92fecc63ca709180846dcbeca0958
Sacha Verweij <sacha@stanford.edu>
2016-07-07 21:04:24 -0700

    Clean up @vectorize_(1|2)arg deprecations.

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -872,8 +872,6 @@ end
 macro dep_vectorize_2arg(S, f)
     S = esc(S)
     f = esc(f)
-    T1 = esc(:T1)
-    T2 = esc(:T2)
     quote
         @deprecate $f{T<:$S}(x::$S, y::AbstractArray{T}) $f.(x,y)
         @deprecate $f{T<:$S}(x::AbstractArray{T}, y::$S) $f.(x,y)
@@ -883,25 +881,41 @@ end
 
 # Deprecate @vectorize_1arg-vectorized functions from...
 for f in (
-        :sinpi, :cospi, :sinc, :cosc, # base/special/trig.jl
-        :log, :log1p, # base/special/log.jl
-        :gamma, :lfact, :digamma, :trigamma, :zeta, :eta,# base/special/gamma.jl
-        :erfcx, :erfi, :dawson, # base/special/erf.jl
-        :airyprime, :airyai, :airyaiprime, :airybi, :airybiprime, :airy, :airyx, :besselj0, :besselj1, :bessely0, :bessely1, # base/special/bessel.jl
-        :cbrt, :sinh, :cosh, :tanh, :atan, :asinh, :exp, :erf, :erfc, :exp2, :expm1, :exp10, :sin, :cos, :tan, :asin, :acos, :acosh, :atanh, #=:log,=# :log2, :log10, :lgamma, #=:log1p,=# :sqrt, # base/math.jl
-        :abs, :abs2, :angle, :isnan, :isinf, :isfinite, # base/floatfuncs.jl
-        :acos_fast, :acosh_fast, :angle_fast, :asin_fast, :asinh_fast, :atan_fast, :atanh_fast, :cbrt_fast, :cis_fast, :cos_fast, :cosh_fast, :exp10_fast, :exp2_fast, :exp_fast, :expm1_fast, :lgamma_fast, :log10_fast, :log1p_fast, :log2_fast, :log_fast, :sin_fast, :sinh_fast, :sqrt_fast, :tan_fast, :tanh_fast, # base/fastmath.jl
-        :cis, # base/complex.jl
+        # base/special/trig.jl
+        :sinpi, :cospi, :sinc, :cosc,
+        # base/special/log.jl
+        :log, :log1p,
+        # base/special/gamma.jl
+        :gamma, :lfact, :digamma, :trigamma, :zeta, :eta,
+        # base/special/erf.jl
+        :erfcx, :erfi, :dawson,
+        # base/special/bessel.jl
+        :airyprime, :airyai, :airyaiprime, :airybi, :airybiprime,
+        :airy, :airyx, :besselj0, :besselj1, :bessely0, :bessely1,
+        # base/math.jl
+        :cbrt, :sinh, :cosh, :tanh, :atan, :asinh, :exp, :erf, :erfc, :exp2,
+        :expm1, :exp10, :sin, :cos, :tan, :asin, :acos, :acosh, :atanh,
+        #=:log,=# :log2, :log10, :lgamma, #=:log1p,=# :sqrt,
+        # base/floatfuncs.jl
+        :abs, :abs2, :angle, :isnan, :isinf, :isfinite,
+        # base/fastmath.jl
+        :acos_fast, :acosh_fast, :angle_fast, :asin_fast, :asinh_fast,
+        :atan_fast, :atanh_fast, :cbrt_fast, :cis_fast, :cos_fast,
+        :cosh_fast, :exp10_fast, :exp2_fast, :exp_fast, :expm1_fast,
+        :lgamma_fast, :log10_fast, :log1p_fast, :log2_fast, :log_fast,
+        :sin_fast, :sinh_fast, :sqrt_fast, :tan_fast, :tanh_fast,
+        # base/complex.jl
+        :cis,
         )
     @eval @dep_vectorize_1arg Number $f
 end
 for f in (
-        :sind, :cosd, :tand, :asind, :acosd, :atand, :asecd, :acscd, :acotd, # base/special/trig.jl
         :invdigamma, # base/special/gamma.jl
         :erfinc, :erfcinv, # base/special/erf.jl
-        :rad2deg, :deg2rad, :exponent, :significand, # base/math.jl
         :trunc, :floor, :ceil, :round, # base/floatfuncs.jl
+        :rad2deg, :deg2rad, :exponent, :significand, # base/math.jl
         :unix2datetime, :rata2datetime, :julian2datetime, # base/dates/conversions.jl
+        :sind, :cosd, :tand, :asind, :acosd, :atand, :asecd, :acscd, :acotd, # base/special/trig.jl
         )
     @eval @dep_vectorize_1arg Real $f
 end
@@ -909,9 +923,16 @@ end
 @dep_vectorize_1arg Complex round
 @dep_vectorize_1arg Complex float
 for f in (
-        :year, :month, :day, :week, :dayofmonth, :yearmonth, :monthday, :yearmonthday, # base/dates/accessors.jl
-        :firstdayofweek, :lastdayofweek, :firstdayofmonth, :lastdayofmonth, :firstdayofyear, :lastdayofyear, :firstdayofquarter, :lastdayofquarter, # base/dates/adjusters.jl
-        :dayname, :dayabbr, :dayofweek, :dayofweekofmonth, :daysofweekinmonth, :monthname, :monthabbr, :daysinmonth, :isleapyear, :dayofyear, :daysinyear, :quarterofyear, :dayofquarter, # base/dates/query.jl
+        # base/dates/accessors.jl
+        :year, :month, :day, :week, :dayofmonth, :yearmonth, :monthday, :yearmonthday,
+        # base/dates/adjusters.jl
+        :firstdayofweek, :lastdayofweek, :firstdayofmonth,
+        :lastdayofmonth, :firstdayofyear, :lastdayofyear,
+        :firstdayofquarter, :lastdayofquarter,
+        # base/dates/query.jl
+        :dayname, :dayabbr, :dayofweek, :dayofweekofmonth,
+        :daysofweekinmonth, :monthname, :monthabbr, :daysinmonth,
+        :isleapyear, :dayofyear, :daysinyear, :quarterofyear, :dayofquarter,
     )
     @eval @dep_vectorize_1arg Dates.TimeType $f
 end
@@ -925,10 +946,16 @@ end
 
 # Deprecate @vectorize_2arg-vectorized functions from...
 for f in (
-        :polygamma, :zeta, :beta, :lbeta, # base/special/gamma.jl
-        :airy, :airyx, :besseli, :besselix, :besselj, :besseljx, :besselk, :besselkx, :bessely, :besselyx, :besselh, :besselhx, :hankelh1, :hankelh2, :hankelh1x, :hankelh2x, # base/special/bessel.jl
-        :log, :hypot, :atan2, # base/math.jl
-        :pow_fast, :atan2_fast, :hypot_fast, :max_fast, :min_fast, :minmax_fast, # base/fastmath.jl
+        # base/special/gamma.jl
+        :polygamma, :zeta, :beta, :lbeta,
+        # base/special/bessel.jl
+        :airy, :airyx, :besseli, :besselix, :besselj, :besseljx,
+        :besselk, :besselkx, :bessely, :besselyx, :besselh,
+        :besselhx, :hankelh1, :hankelh2, :hankelh1x, :hankelh2x,
+        # base/math.jl
+        :log, :hypot, :atan2,
+        # base/fastmath.jl
+        :pow_fast, :atan2_fast, :hypot_fast, :max_fast, :min_fast, :minmax_fast,
     )
     @eval @dep_vectorize_2arg Number $f
 end
