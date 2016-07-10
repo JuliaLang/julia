JuliaLang/julia

commit c779caf0ee823128d8410f8aaf3d2255231a6acb

remove some unnecessary typevars as parameters of Tuple types

src/jltypes.c
@@ -2380,10 +2380,12 @@ static jl_value_t *inst_tuple_w_(jl_value_t *t, jl_value_t **env, size_t n,
     int i;
     for(i=0; i < ntp; i++) {
         jl_value_t *elt = jl_svecref(tp, i);
-        iparams[i] = (jl_value_t*)inst_type_w_(elt, env, n, stack, 0);
+        jl_value_t *pi = (jl_value_t*)inst_type_w_(elt, env, n, stack, 0);
+        if (jl_is_typevar(pi) && !((jl_tvar_t*)pi)->bound)
+            pi = ((jl_tvar_t*)pi)->ub;
+        iparams[i] = pi;
         if (ip_heap)
-            jl_gc_wb(ip_heap, iparams[i]);
-        jl_value_t *pi = iparams[i];
+            jl_gc_wb(ip_heap, pi);
         check_tuple_parameter(pi, i, ntp);
         if (cacheable && !jl_is_leaf_type(pi)) {
             cacheable = 0;

commit 0a46cbbf08435c861877959f1de1676e5f7d4bfb

fix #17350, regression in ambig. detection caused by c779caf0ee823128d8410f8aaf3d2255231a6acb

src/jltypes.c
@@ -2395,8 +2395,6 @@ static jl_value_t *inst_tuple_w_(jl_value_t *t, jl_value_t **env, size_t n,
     for(i=0; i < ntp; i++) {
         jl_value_t *elt = jl_svecref(tp, i);
         jl_value_t *pi = (jl_value_t*)inst_type_w_(elt, env, n, stack, 0);
-        if (jl_is_typevar(pi) && !((jl_tvar_t*)pi)->bound)
-            pi = ((jl_tvar_t*)pi)->ub;
         iparams[i] = pi;
         if (ip_heap)
             jl_gc_wb(ip_heap, pi);

test/ambiguous.jl
@@ -169,4 +169,13 @@ let ms = methods(g16493, (Complex, Any))
     @test first(ms).sig == Tuple{typeof(g16493), Complex{TypeVar(:T, Any, true)}, Any}
 end
 
+# issue #17350
+module Ambig6
+immutable ScaleMinMax{To,From} end
+map1{To<:Union{Float32,Float64},From<:Real}(mapi::ScaleMinMax{To,From}, val::From) = 1
+map1{To<:Union{Float32,Float64},From<:Real}(mapi::ScaleMinMax{To,From}, val::Union{Real,Complex}) = 2
+end
+
+@test isempty(detect_ambiguities(Ambig6))
+
 nothing