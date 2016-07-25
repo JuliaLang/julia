ae9f5a934e193c175086028eb2c6b8505a187f33
Andreas Noack <andreasnoackjensen@gmail.com>
2016-07-25 09:53:11 -0400

    Fix documentation for \ and move it to linalg/generic.jl

--- base/linalg/generic.jl
+++ base/linalg/generic.jl
@@ -329,6 +329,22 @@ function inv{T}(A::AbstractMatrix{T})
     A_ldiv_B!(factorize(convert(AbstractMatrix{S}, A)), eye(S, checksquare(A)))
 end
 
+"""
+    \\(A, B)
+
+Matrix division using a polyalgorithm. For input matrices `A` and `B`, the result `X` is
+such that `A*X == B` when `A` is square. The solver that is used depends upon the structure
+of `A`.  If `A` is upper or lower triangular (or diagonal), no factorization of `A` is
+required and the system is solved with either forward or backward substitution.
+For non-triangular square matrices, an LU factorization is used.
+
+For rectangular `A` the result is the minimum-norm least squares solution computed by a
+pivoted QR factorization of `A` and a rank estimate of `A` based on the R factor.
+
+When `A` is sparse, a similar polyalgorithm is used. For indefinite matrices, the `LDLt`
+factorization does not use pivoting during the numerical factorization and therefore the
+procedure can fail even for invertible matrices.
+"""
 function (\)(A::AbstractMatrix, B::AbstractVecOrMat)
     m, n = size(A)
     if m == n
