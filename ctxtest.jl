b0f3fb11ff248690a4117143780b5dd816dc655b
Tim Besard <tim.besard@gmail.com>
2016-07-27 10:57:57 -0400

    Fix CTX test.

--- ctxtest.jl
+++ ctxtest.jl
@@ -8,25 +8,32 @@ immutable GPU; end
 @context GPU foobar() = 123456789
 foobar() = 987654321
 
-@test contains(string(code_lowered(foobar, Tuple{})), string(987654321))
-@test contains(string(code_lowered(foobar, Tuple{}, ctx=GPU)), string(123456789))
-@test contains(string(code_lowered(foobar, Tuple{GPU}, ctx=Base.Bottom)), string(123456789))
+codestr(linfos) = sprint(io->show(io, MIME"text/plain"(), linfos[1]))
 
-@test contains(string(code_typed(foobar, Tuple{})), string(987654321))
-@test contains(string(code_typed(foobar, Tuple{}, ctx=GPU)), string(123456789))
-@test contains(string(code_typed(foobar, Tuple{GPU}, ctx=Base.Bottom)), string(123456789))
+@test contains(codestr(code_lowered(foobar, Tuple{})),
+               string(987654321))
+@test contains(codestr(code_lowered(foobar, Tuple{}, ctx=GPU)),
+               string(123456789))
+@test contains(codestr(code_lowered(foobar, Tuple{GPU}, ctx=Base.Bottom)),
+               string(123456789))
 
-buf = IOBuffer()
-code_llvm(buf, foobar, Tuple{})
-@test contains(takebuf_string(buf), string(987654321))
-code_llvm(buf, foobar, Tuple{}, true, false, GPU)
-@test contains(takebuf_string(buf), string(123456789))
-code_llvm(buf, foobar, Tuple{GPU}, true, false, Base.Bottom)
-@test contains(takebuf_string(buf), string(123456789))
+@test contains(codestr(code_typed(foobar, Tuple{})),
+               string(987654321))
+@test contains(codestr(code_typed(foobar, Tuple{}, ctx=GPU)),
+               string(123456789))
+@test contains(codestr(code_typed(foobar, Tuple{GPU}, ctx=Base.Bottom)),
+               string(123456789))
 
-code_native(buf, foobar, Tuple{})
-@test contains(takebuf_string(buf), string(987654321))
-code_native(buf, foobar, Tuple{}, GPU)
-@test contains(takebuf_string(buf), string(123456789))
-code_native(buf, foobar, Tuple{GPU}, Base.Bottom)
-@test contains(takebuf_string(buf), string(123456789))
+@test contains(sprint(io->code_llvm(io, foobar, Tuple{})),
+               string(987654321))
+@test contains(sprint(io->code_llvm(io, foobar, Tuple{}, true, false, GPU)),
+               string(123456789))
+@test contains(sprint(io->code_llvm(io, foobar, Tuple{GPU}, true, false, Base.Bottom)),
+               string(123456789))
+
+@test contains(sprint(io->code_native(io, foobar, Tuple{})),
+               string(987654321))
+@test contains(sprint(io->code_native(io, foobar, Tuple{}, GPU)),
+               string(123456789))
+@test contains(sprint(io->code_native(io, foobar, Tuple{GPU}, Base.Bottom)),
+               string(123456789))
