bff3f22d42d69d309a844b74da841aa54bf71b71
Jeff Bezanson <jeff.bezanson@gmail.com>
2016-08-02 11:21:47 -0400

    fix #17712, `show` of generator exprs with >1 range

--- test/show.jl
+++ test/show.jl
@@ -557,10 +557,11 @@ end
 @test repr(Core.svec(1,2)) == "svec(1,2)"
 
 # showing generator and comprehension expressions
-@test repr(:(x for x in y for z in w)) == ":(x for x = y for z = w)"
-@test repr(:(x for x in y if aa for z in w if bb)) == ":(x for x = y if aa for z = w if bb)"
+@test repr(:(x for x in y for z in w)) == ":((x for x = y for z = w))"
+@test repr(:(x for x in y if aa for z in w if bb)) == ":((x for x = y if aa for z = w if bb))"
 @test repr(:([x for x = y])) == ":([x for x = y])"
 @test repr(:([x for x = y if z])) == ":([x for x = y if z])"
+@test repr(:(z for z = 1:5, y = 1:5)) == ":((z for z = 1:5, y = 1:5))"
 
 for op in (:(.=), :(.+=), :(.&=))
     @test repr(parse("x $op y")) == ":(x $op y)"
