046c6eb7f896d806de3fc38da2e8c9153b2dba39
Viral B. Shah <viral@mayin.org>
2016-08-03 19:07:09 +0530

    Build curl to provide proxy support for Pkg. The curl build depends on mbedtls and libssh2 Patch for mbedtls to allow curl to build with mbedtls support Add curl to LICENSE.md, README.md and all the other various locations

---
+++ deps/curl.mk
@@ -0,0 +1,46 @@
+## CURL ##
+
+CURL_SRC_TARGET := $(BUILDDIR)/curl-$(CURL_VER)/.libs/libcurl.$(SHLIB_EXT)
+CURL_OBJ_TARGET := $(build_shlibdir)/libcurl.$(SHLIB_EXT)
+
+ifneq ($(OS),WINNT)
+CURL_LDFLAGS := $(RPATH_ESCAPED_ORIGIN)
+endif
+
+$(SRCDIR)/srccache/curl-$(CURL_VER).tar.bz2: | $(SRCDIR)/srccache
+	$(JLDOWNLOAD) $@ https://curl.haxx.se/download/curl-$(CURL_VER).tar.bz2
+$(SRCDIR)/srccache/curl-$(CURL_VER)/configure: $(SRCDIR)/srccache/curl-$(CURL_VER).tar.bz2 $(MBEDTLS_OBJ_TARGET) $(LIBSSH2_OBJ_TARGET)
+	$(JLCHECKSUM) $<
+	cd $(dir $<) && $(TAR) jxf $(notdir $<)
+	touch -c $@
+$(BUILDDIR)/curl-$(CURL_VER)/config.status: $(SRCDIR)/srccache/curl-$(CURL_VER)/configure
+	mkdir -p $(dir $@)
+	cd $(dir $@) && \
+	$< $(CONFIGURE_COMMON) --includedir=$(build_includedir) --with-mbedtls=$(build_shlibdir)/.. CFLAGS="$(CFLAGS) $(CURL_CFLAGS)" LDFLAGS="$(LDFLAGS) $(CURL_LDFLAGS)"
+	touch -c $@
+$(CURL_SRC_TARGET): $(BUILDDIR)/curl-$(CURL_VER)/config.status
+	$(MAKE) -C $(dir $<) $(LIBTOOL_CCLD)
+	touch -c $@
+$(BUILDDIR)/curl-$(CURL_VER)/checked: $(CURL_SRC_TARGET)
+ifeq ($(OS),$(BUILD_OS))
+ifneq ($(OS),WINNT)
+	$(MAKE) -C $(dir $@) check -j1
+endif
+endif
+	echo 1 > $@
+$(CURL_OBJ_TARGET): $(CURL_SRC_TARGET)
+	$(call make-install,curl-$(CURL_VER),$(LIBTOOL_CCLD))
+	$(INSTALL_NAME_CMD)libcurl.$(SHLIB_EXT) $@
+	touch -c $@
+
+clean-curl:
+	-$(MAKE) -C $(BUILDDIR)/curl-$(CURL_VER) clean
+	-rm -f $(build_shlibdir)/libcurl*
+distclean-curl:
+	-rm -rf $(SRCDIR)/srccache/curl-$(CURL_VER).tar.bz2 $(SRCDIR)/srccache/curl-$(CURL_VER) $(BUILDDIR)/curl-$(CURL_VER)
+
+get-curl: $(SRCDIR)/srccache/curl-$(CURL_VER).tar.bz2
+configure-curl: $(BUILDDIR)/curl-$(CURL_VER)/config.status
+compile-curl: $(CURL_SRC_TARGET)
+check-curl: $(BUILDDIR)/curl-$(CURL_VER)/checked
+install-curl: $(CURL_OBJ_TARGET)
