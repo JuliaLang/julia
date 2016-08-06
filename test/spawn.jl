08ac1467c390ba16221de3eabe3368bf560e7bfb
Jameson Nash <vtjnash@gmail.com>
2016-08-06 12:54:57 -0400

    only run Pipe exhaustion test on unix
    windows doesn't stop you from allocating millions of pipes, until Julia runs out of memory

--- test/spawn.jl
+++ test/spawn.jl
@@ -394,18 +394,20 @@ end
 @test reduce(&, [`$echo abc`, `$echo def`, `$echo hij`]) == `$echo abc` & `$echo def` & `$echo hij`
 
 # test for proper handling of FD exhaustion
-let ps = Pipe[]
-    try
-        for i = 1:100_000
-            p = Pipe()
-            Base.link_pipe(p)
-            push!(ps, p)
+if is_unix()
+    let ps = Pipe[]
+        try
+            for i = 1:100_000
+                p = Pipe()
+                Base.link_pipe(p)
+                push!(ps, p)
+            end
+            @test false
+        catch ex
+            for p in ps
+                close(p)
+            end
+            @test (ex::Base.UVError).code == Base.UV_EMFILE
         end
-        @test false
-    catch ex
-        for p in ps
-            close(p)
-        end
-        @test (ex::Base.UVError).code == Base.UV_EMFILE
     end
 end
