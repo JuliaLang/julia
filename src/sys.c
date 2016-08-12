362f877d2471fad5c9f5006e39aa9c309b3881bc
albap <albapompeo@gmail.com>
2016-08-12 18:45:13 -0400

    Remove nonstandard header sys/sysctl.h when unneeded
    This header prevents me from building Julia on musl libc.
    
    ```
    /home/alba/julia/src/sys.c:16:24: fatal error: sys/sysctl.h: No such file or directory
    compilation terminated.
    Makefile:94: recipe for target sys.o failed
    make[1]: *** [sys.o] Error 1
    ```

--- src/sys.c
+++ src/sys.c
@@ -10,19 +10,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#ifdef _OS_WINDOWS_
-#include <psapi.h>
-#else
-#include <sys/sysctl.h>
-#include <sys/wait.h>
-#include <sys/ptrace.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <dlfcn.h>
-#endif
 #include <errno.h>
 #include <signal.h>
 #include <fcntl.h>
+
+#ifdef _OS_WINDOWS_
+#include <psapi.h>
+#else
+#include <unistd.h>
+#if !defined(_SC_NPROCESSORS_ONLN) || defined(_OS_FREEBSD_) || defined(_OS_DARWIN_)
+// try secondary location for _SC_NPROCESSORS_ONLN, or for HW_AVAILCPU on BSDs
+#include <sys/sysctl.h>
+#endif
+#include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <sys/mman.h>
+#include <dlfcn.h>
+#endif
 
 #ifndef _OS_WINDOWS_
 // for getrusage
@@ -355,7 +359,10 @@ JL_DLLEXPORT int jl_cpu_cores(void)
     }
     return count;
 #elif defined(_SC_NPROCESSORS_ONLN)
-    return sysconf(_SC_NPROCESSORS_ONLN);
+    long count = sysconf(_SC_NPROCESSORS_ONLN);
+    if (count < 1)
+        return 1;
+    return count;
 #elif defined(_OS_WINDOWS_)
     //Try to get WIN7 API method
     GAPC gapc = (GAPC) jl_dlsym_e(
@@ -372,6 +379,7 @@ JL_DLLEXPORT int jl_cpu_cores(void)
         return info.dwNumberOfProcessors;
     }
 #else
+#warning "cpu core detection not defined for this platform"
     return 1;
 #endif
 }
