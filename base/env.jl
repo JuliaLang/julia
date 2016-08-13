96edeac61ea1e992fc020117585a1206b8093f66
Daniel Høegh <dhoegh91@gmail.com>
2016-08-13 13:50:12 +0200

    move doc for `withenv`

--- base/env.jl
+++ base/env.jl
@@ -138,7 +138,15 @@ function show(io::IO, ::EnvHash)
     end
 end
 
-# temporarily set and then restore an environment value
+"""
+    withenv(f::Function, kv::Pair...)
+
+Execute `f()` in an environment that is temporarily modified (not replaced as in `setenv`)
+by zero or more `"var"=>val` arguments `kv`. `withenv` is generally used via the
+`withenv(kv...) do ... end` syntax. A value of `nothing` can be used to temporarily unset an
+environment variable (if it is set). When `withenv` returns, the original environment has
+been restored.
+"""
 function withenv{T<:AbstractString}(f::Function, keyvals::Pair{T}...)
     old = Dict{T,Any}()
     for (key,val) in keyvals
