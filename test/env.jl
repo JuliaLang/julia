e1fab8366d026b533d0c3fb2af07555290f4bb4a
Daniel Høegh <dhoegh91@gmail.com>
2016-08-13 13:20:09 +0200

    Fix #17956 and add test. The bug was introduced in 103db50aa6c67d8e732e8e2bd7f5743bae17e369
    (cherry picked from commit b1e82163e259305ed989b005a736f941bf183f5a)
    ref #18012

--- test/env.jl
+++ test/env.jl
@@ -30,3 +30,36 @@ key = randstring(25)
 @test !haskey(ENV,key)
 @test_throws KeyError ENV[key]
 @test get(ENV,key,"default") == "default"
+
+# Test for #17956
+@test length(ENV) > 1
+k1, k2 = "__test__", "__test1__"
+withenv(k1=>k1, k2=>k2) do
+    b_k1, b_k2 = false, false
+    for (k, v) in ENV
+        if k==k1
+            b_k1=true
+        elseif k==k2
+            b_k2=true
+        end
+    end
+    @test b_k1 && b_k2
+    io = IOBuffer()
+    show(io, ENV)
+    s = takebuf_string(io)
+    @test contains(s, "$k1=$k1")
+    @test contains(s, "$k2=$k2")
+
+    @test pop!(ENV, k1) == k1
+    @test !haskey(ENV, k1)
+    ENV[k1] = k1
+    @test pop!(ENV, k1) == k1
+    @test pop!(ENV, k1, "not_there") == "not_there"
+
+    ENV[k1] = k1
+    @test delete!(ENV, k1) == ENV
+    @test !haskey(ENV, k1)
+end
+
+# Test for #10853
+@test withenv(Dict{Any,Any}()...) do; true; end
