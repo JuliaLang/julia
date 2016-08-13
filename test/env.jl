ede72312f4a052049f520570c4ec6cee6aa48389
Daniel Høegh <dhoegh91@gmail.com>
2016-08-13 13:17:43 +0200

    Move env.jl test to separate test file
    (cherry picked from commit 30afe08b5d4af597588551e132c67a51974557a4)
    ref #18012

---
+++ test/env.jl
@@ -0,0 +1,32 @@
+# This file is a part of Julia. License is MIT: http://julialang.org/license
+
+@test !("f=a=k=e=n=a=m=e" ∈ keys(ENV))
+
+# issue #10994
+@test_throws ArgumentError ENV["bad\0name"] = "ok"
+@test_throws ArgumentError ENV["okname"] = "bad\0val"
+@test_throws ArgumentError Sys.set_process_title("bad\0title")
+
+withenv("bad"=>"dog") do
+    @test_throws ArgumentError ENV["bad\0cat"]
+end
+
+# issue #11170
+withenv("TEST"=>"nonempty") do
+    @test ENV["TEST"] == "nonempty"
+end
+withenv("TEST"=>"") do
+    @test ENV["TEST"] == ""
+end
+
+let c = collect(ENV)
+    @test isa(c, Vector)
+    @test length(ENV) == length(c)
+    @test isempty(ENV) || first(ENV) in c
+end
+
+# test for non-existent keys
+key = randstring(25)
+@test !haskey(ENV,key)
+@test_throws KeyError ENV[key]
+@test get(ENV,key,"default") == "default"
