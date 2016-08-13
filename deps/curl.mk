6404e5406f8897bc252d11ebb96cf7067a809279
Jameson Nash <vtjnash@gmail.com>
2016-08-13 12:10:46 -0400

    stop curl from rebuilding incessantly

--- deps/curl.mk
+++ deps/curl.mk
@@ -1,6 +1,6 @@
 ## CURL ##
 
-CURL_SRC_TARGET := $(BUILDDIR)/curl-$(CURL_VER)/.libs/libcurl.$(SHLIB_EXT)
+CURL_SRC_TARGET := $(BUILDDIR)/curl-$(CURL_VER)/lib/.libs/libcurl.$(SHLIB_EXT)
 CURL_OBJ_TARGET := $(build_shlibdir)/libcurl.$(SHLIB_EXT)
 
 ifneq ($(OS),WINNT)
@@ -9,18 +9,22 @@ endif
 
 $(SRCDIR)/srccache/curl-$(CURL_VER).tar.bz2: | $(SRCDIR)/srccache
 	$(JLDOWNLOAD) $@ https://curl.haxx.se/download/curl-$(CURL_VER).tar.bz2
+
 $(SRCDIR)/srccache/curl-$(CURL_VER)/configure: $(SRCDIR)/srccache/curl-$(CURL_VER).tar.bz2 $(MBEDTLS_OBJ_TARGET) $(LIBSSH2_OBJ_TARGET)
 	$(JLCHECKSUM) $<
 	cd $(dir $<) && $(TAR) jxf $(notdir $<)
 	touch -c $@
+
 $(BUILDDIR)/curl-$(CURL_VER)/config.status: $(SRCDIR)/srccache/curl-$(CURL_VER)/configure
 	mkdir -p $(dir $@)
 	cd $(dir $@) && \
 	$< $(CONFIGURE_COMMON) --includedir=$(build_includedir) --without-ssl --with-mbedtls=$(build_prefix) CFLAGS="$(CFLAGS) $(CURL_CFLAGS)" LDFLAGS="$(LDFLAGS) $(CURL_LDFLAGS)"
 	touch -c $@
+
 $(CURL_SRC_TARGET): $(BUILDDIR)/curl-$(CURL_VER)/config.status
 	$(MAKE) -C $(dir $<) $(LIBTOOL_CCLD)
 	touch -c $@
+
 $(BUILDDIR)/curl-$(CURL_VER)/checked: $(CURL_SRC_TARGET)
 ifeq ($(OS),$(BUILD_OS))
 ifneq ($(OS),WINNT)
@@ -28,6 +32,7 @@ ifneq ($(OS),WINNT)
 endif
 endif
 	echo 1 > $@
+
 $(CURL_OBJ_TARGET): $(CURL_SRC_TARGET)
 	$(call make-install,curl-$(CURL_VER),$(LIBTOOL_CCLD))
 	$(INSTALL_NAME_CMD)libcurl.$(SHLIB_EXT) $@
