fd156ad5fa3b2fd02a4a8d1f0dac152306eb67ea
Curtis Vogt <curtis.vogt@gmail.com>
2016-08-17 13:24:57 -0500

    Removed SSH test from libgit2-online
    Test was only working for environments which had GitHub SSH keys setup.

--- test/libgit2-online.jl
+++ test/libgit2-online.jl
@@ -7,12 +7,11 @@
 #########
 # init & clone
 mktempdir() do dir
-    repo_url_https = "https://github.com/JuliaLang/Example.jl"
-    repo_url_git = "git@github.com:JuliaLang/Example.jl"
+    repo_url = "https://github.com/JuliaLang/Example.jl"
     #@testset "Cloning repository" begin
         #@testset "with 'https' protocol" begin
             repo_path = joinpath(dir, "Example1")
-            repo = LibGit2.clone(repo_url_https, repo_path)
+            repo = LibGit2.clone(repo_url, repo_path)
             try
                 @test isdir(repo_path)
                 @test isdir(joinpath(repo_path, ".git"))
@@ -26,23 +25,11 @@ mktempdir() do dir
                 repo_path = joinpath(dir, "Example2")
                 # credentials are required because github tries to authenticate on unknown repo
                 cred = LibGit2.UserPasswordCredentials("","") # empty credentials cause authentication error
-                LibGit2.clone(repo_url_https*randstring(10), repo_path, payload=Nullable(cred))
+                LibGit2.clone(repo_url*randstring(10), repo_path, payload=Nullable(cred))
                 error("unexpected")
             catch ex
                 @test isa(ex, LibGit2.Error.GitError)
                 @test ex.code == LibGit2.Error.EAUTH
-            end
-        #end
-
-        #TODO: remove or condition on libgit2 features this test when ssh protocol will be supported
-        #@testset "with 'ssh' protocol (by default is not supported)" begin
-            repo_path = joinpath(dir, "Example3")
-            repo = LibGit2.clone(repo_url_git, repo_path)
-            try
-                @test isdir(repo_path)
-                @test isdir(joinpath(repo_path, ".git"))
-            finally
-                finalize(repo)
             end
         #end
     #end
