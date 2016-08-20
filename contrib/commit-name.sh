87b9b67cdcc230e7b72df9549fb76ca2085b531c
LOAS2 <yoxr@o.cnew.ir>
2016-08-20 09:09:24 -0300

    echo -> printf

--- contrib/commit-name.sh
+++ contrib/commit-name.sh
@@ -7,23 +7,23 @@
 gitref=${1:-HEAD}
 
 ver=$(git show "$gitref:VERSION")
-major=$(echo $ver | cut -f 1 -d .)
-minor=$(echo $ver | cut -f 2 -d .)
+major=$(printf "$ver\n" | cut -f 1 -d .)
+minor=$(printf "$ver\n" | cut -f 2 -d .)
 
 if [ $major = 0 -a $minor -lt 5 ]; then
     # use tag based build number prior to 0.5.0-
     last_tag=$(git describe --tags --abbrev=0 "$gitref")
     nb=$(git rev-list --count "$gitref" "^$last_tag")
     if [ $nb = 0 ]; then
-        echo $ver
+        printf "$ver\n"
     else
-        echo "$ver+$nb"
+        printf "$ver+$nb\n"
     fi
 else
     topdir=$(git rev-parse --show-toplevel)
     verchanged=$(git blame -L ,1 -sl $gitref -- "$topdir/VERSION" | cut -f 1 -d " ")
     nb=$(git rev-list --count "$gitref" "^$verchanged")
-    pre=$(echo $ver | cut -s -f 2 -d "-")
+    pre=$(printf "$ver\n" | cut -s -f 2 -d "-")
     if [ $ver = "0.5.0-dev" ]; then
         # bump to 0.5.0-dev was one commit after tag during 0.5.0-dev
         nb=$(expr $nb + 1)
@@ -33,11 +33,11 @@ else
     fi
     if [ -n "$pre" ]; then
         if [ $major = 0 -a $minor -le 5 ]; then
-            echo "$ver+$nb"
+            printf "$ver+$nb\n"
         else
-            echo "$ver.$nb"
+            printf "$ver.$nb\n"
         fi
     else
-        echo $ver
+        printf "$ver\n"
     fi
 fi
