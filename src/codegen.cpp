ffa44f8043d6da5a0a79f2605958bf1cd3f4c632
Jameson Nash <vtjnash@gmail.com>
2016-08-22 17:14:29 -0400

    incremental deserialize: handle LambdaInfo identity uniquing
    this works to avoid having `Expr(:invoke)` creating
    unintentional copies of LambdaInfo objects when they show
    up in the system image
    
    fix #18184

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -1176,7 +1176,7 @@ void *jl_get_llvmf(jl_tupletype_t *tt, bool getwrapper, bool getdeclarations)
         linfo = jl_get_specialization1(tt);
         if (linfo == NULL) {
             linfo = jl_method_lookup_by_type(
-                ((jl_datatype_t*)jl_tparam0(tt))->name->mt, tt, 0, 0);
+                ((jl_datatype_t*)jl_tparam0(tt))->name->mt, tt, 0, 0, 1);
             if (linfo == NULL || jl_has_call_ambiguities(tt, linfo->def)) {
                 JL_GC_POP();
                 return NULL;
