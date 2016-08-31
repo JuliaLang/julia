a5105cb786ea1f3ea953b53e0f13e339d2304185
Milan Bouchet-Valat <nalimilan@club.fr>
2016-08-31 11:38:51 +0200

    Make isequal(::Nullable, ::Nullable) faster for safe types
    Introduce the null_safe_op() function to allow declaring which combinations
    of operators and types are safe (i.e. can be computed without branching
    even when null). This function will be used for other operators in the future.

--- base/nullable.jl
+++ base/nullable.jl
@@ -63,15 +63,52 @@ get(x::Nullable) = x.isnull ? throw(NullException()) : x.value
 
 isnull(x::Nullable) = x.isnull
 
-function isequal(x::Nullable, y::Nullable)
-    if x.isnull && y.isnull
-        return true
-    elseif x.isnull || y.isnull
-        return false
+
+## Operators
+
+"""
+    null_safe_op(f::Any, ::Type, ::Type...)::Bool
+
+Returns whether an operation `f` can safely be applied to any value of the passed type(s).
+Returns `false` by default.
+
+Custom types should implement methods for some or all operations `f` when applicable:
+returning `true` means that the operation may be called on any bit pattern without
+throwing an error (though returning invalid or nonsensical results is not a problem).
+In particular, this means that the operation can be applied on the whole domain of the
+type *and on uninitialized objects*. As a general rule, these properties are only true for
+safe operations on `isbits` types.
+
+Types declared as safe can benefit from higher performance for operations on nullable: by
+always computing the result even for null values, a branch is avoided, which helps
+vectorization.
+"""
+null_safe_op(f::Any, ::Type, ::Type...) = false
+
+typealias NullSafeSignedInts Union{Int128, Int16, Int32, Int64, Int8}
+typealias NullSafeUnsignedInts Union{Bool, UInt128, UInt16, UInt32, UInt64, UInt8}
+typealias NullSafeInts Union{NullSafeSignedInts, NullSafeUnsignedInts}
+typealias NullSafeFloats Union{Float16, Float32, Float64}
+typealias NullSafeTypes Union{NullSafeInts, NullSafeFloats}
+
+null_safe_op{S<:NullSafeTypes,
+             T<:NullSafeTypes}(::typeof(isequal), ::Type{S}, ::Type{T}) = true
+null_safe_op{S<:NullSafeTypes,
+             T<:NullSafeTypes}(::typeof(isequal), ::Type{Complex{S}}, ::Type{Complex{T}}) = true
+null_safe_op{S<:NullSafeTypes,
+             T<:NullSafeTypes}(::typeof(isequal), ::Type{Rational{S}}, ::Type{Rational{T}}) = true
+
+function isequal{S,T}(x::Nullable{S}, y::Nullable{T})
+    if null_safe_op(isequal, S, T)
+        (x.isnull & y.isnull) | (!x.isnull & !y.isnull & isequal(x.value, y.value))
     else
-        return isequal(x.value, y.value)
+        (x.isnull & y.isnull) || (!x.isnull & !y.isnull && isequal(x.value, y.value))
     end
 end
+
+isequal(x::Nullable{Union{}}, y::Nullable{Union{}}) = true
+isequal(x::Nullable{Union{}}, y::Nullable) = y.isnull
+isequal(x::Nullable, y::Nullable{Union{}}) = x.isnull
 
 ==(x::Nullable, y::Nullable) = throw(NullException())
 
