d5521ebae8afd35c7a68e03e25375709d9f627fa
Yichao Yu <yyc1992@gmail.com>
2016-09-04 04:36:56 -0400

    Do not disable inlining when coverage is enabled.
    Fix #17476

--- base/pkg/entry.jl
+++ base/pkg/entry.jl
@@ -703,7 +703,7 @@ function test!(pkg::AbstractString,
         cd(dirname(test_path)) do
             try
                 color = Base.have_color? "--color=yes" : "--color=no"
-                codecov = coverage? ["--code-coverage=user", "--inline=no"] : ["--code-coverage=none"]
+                codecov = coverage? ["--code-coverage=user"] : ["--code-coverage=none"]
                 compilecache = "--compilecache=" * (Bool(Base.JLOptions().use_compilecache) ? "yes" : "no")
                 julia_exe = Base.julia_cmd()
                 run(`$julia_exe --check-bounds=yes $codecov $color $compilecache $test_path`)
