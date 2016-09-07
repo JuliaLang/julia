5f74d16553197ae78b55cf9ff222cd7215e0390b
Simon Byrne <simonbyrne@gmail.com>
2016-09-07 12:17:48 +0100

    Add accuracy note for 2-arg log (#18360)
    * Add accuracy note for 2-arg log
    
    Addresses #18298.
    
    * update based on comments

--- base/math.jl
+++ base/math.jl
@@ -161,6 +161,18 @@ julia> log(4,8)
 julia> log(4,2)
 0.5
 ```
+
+!!! note
+    If `b` is a power of 2 or 10, `log2` or `log10` should be used, as these will
+    typically be faster and more accurate. For example,
+
+    ```jldoctest
+    julia> log(100,1000000)
+    2.9999999999999996
+
+    julia> log10(1000000)/2
+    3.0
+    ```
 """
 log(b::Number, x::Number) = log(promote(b,x)...)
 
