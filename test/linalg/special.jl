19b68d2416262f7ba567cab4fcdf5df58c1745d4
Sacha Verweij <Sacha0@users.noreply.github.com>
2016-09-13 20:16:20 -0700

    Make concatenations involving annotated sparse/special matrices yield sparse arrays. Test thoroughly. (#17900)

--- test/linalg/special.jl
+++ test/linalg/special.jl
@@ -162,3 +162,89 @@ let
         end
     end
 end
+
+# Test that concatenations of annotated sparse/special matrix types with other matrix
+# types yield sparse arrays, and that the code which effects that does not make concatenations
+# strictly involving un/annotated dense matrices yield sparse arrays
+#
+# TODO: As with the associated code, these tests should be moved to a more appropriate
+# location, particularly some future equivalent of base/linalg/special.jl dedicated to
+# intereactions between a broader set of matrix types
+let
+    N = 4
+    # The tested annotation types
+    testfull = Bool(parse(Int,(get(ENV, "JULIA_TESTFULL", "0"))))
+    utriannotations = (UpperTriangular, Base.LinAlg.UnitUpperTriangular)
+    ltriannotations = (LowerTriangular, Base.LinAlg.UnitLowerTriangular)
+    triannotations = (utriannotations..., ltriannotations...)
+    symannotations = (Symmetric, Hermitian)
+    annotations = testfull ? (triannotations..., symannotations...) : (LowerTriangular, Symmetric)
+    # Concatenations involving these types, un/annotated, should yield sparse arrays
+    spvec = spzeros(N)
+    spmat = speye(N)
+    diagmat = Diagonal(ones(N))
+    bidiagmat = Bidiagonal(ones(N), ones(N-1), true)
+    tridiagmat = Tridiagonal(ones(N-1), ones(N), ones(N-1))
+    symtridiagmat = SymTridiagonal(ones(N), ones(N-1))
+    sparseconcatmats = testfull ? (spmat, diagmat, bidiagmat, tridiagmat, symtridiagmat) : (spmat, diagmat)
+    # Concatenations involving strictly these types, un/annotated, should yield dense arrays
+    densevec = ones(N)
+    densemat = ones(N, N)
+    # Annotated collections
+    annodmats = [annot(densemat) for annot in annotations]
+    annospcmats = [annot(spcmat) for annot in annotations, spcmat in sparseconcatmats]
+    # Test that concatenations of pairwise combinations of annotated sparse/special
+    # yield sparse matrices
+    for annospcmata in annospcmats, annospcmatb in annospcmats
+        @test issparse(vcat(annospcmata, annospcmatb))
+        @test issparse(hcat(annospcmata, annospcmatb))
+        @test issparse(hvcat((2,), annospcmata, annospcmatb))
+        @test issparse(cat((1,2), annospcmata, annospcmatb))
+    end
+    # Test that concatenations of pairwise combinations of annotated sparse/special
+    # matrices and other matrix/vector types yield sparse matrices
+    for annospcmat in annospcmats
+        # --> Tests applicable to pairs including only matrices
+        for othermat in (densemat, annodmats..., sparseconcatmats...)
+            @test issparse(vcat(annospcmat, othermat))
+            @test issparse(vcat(othermat, annospcmat))
+        end
+        # --> Tests applicable to pairs including other vectors or matrices
+        for other in (spvec, densevec, densemat, annodmats..., sparseconcatmats...)
+            @test issparse(hcat(annospcmat, other))
+            @test issparse(hcat(other, annospcmat))
+            @test issparse(hvcat((2,), annospcmat, other))
+            @test issparse(hvcat((2,), other, annospcmat))
+            @test issparse(cat((1,2), annospcmat, other))
+            @test issparse(cat((1,2), other, annospcmat))
+        end
+    end
+    # The preceding tests should cover multi-way combinations of those types, but for good
+    # measure test a few multi-way combinations involving those types
+    @test issparse(vcat(spmat, densemat, annospcmats[1], annodmats[2]))
+    @test issparse(vcat(densemat, spmat, annodmats[1], annospcmats[2]))
+    @test issparse(hcat(spvec, annodmats[1], annospcmats[3], densevec, diagmat))
+    @test issparse(hcat(annodmats[2], annospcmats[4], spvec, densevec, diagmat))
+    @test issparse(hvcat((5,), diagmat, densevec, spvec, annodmats[1], annospcmats[1]))
+    @test issparse(hvcat((5,), spvec, annodmats[2], diagmat, densevec, annospcmats[2]))
+    @test issparse(cat((1,2), annodmats[1], diagmat, annospcmats[3], densevec, spvec))
+    @test issparse(cat((1,2), spvec, diagmat, densevec, annospcmats[4], annodmats[2]))
+    # Test that concatenations strictly involving un/annotated dense matrices/vectors
+    # yield dense arrays
+    for densemata in (densemat, annodmats...)
+        # --> Tests applicable to pairs including only matrices
+        for densematb in (densemat, annodmats...)
+            @test !issparse(vcat(densemata, densematb))
+            @test !issparse(vcat(densematb, densemata))
+        end
+        # --> Tests applicable to pairs including vectors or matrices
+        for otherdense in (densevec, densemat, annodmats...)
+            @test !issparse(hcat(densemata, otherdense))
+            @test !issparse(hcat(otherdense, densemata))
+            @test !issparse(hvcat((2,), densemata, otherdense))
+            @test !issparse(hvcat((2,), otherdense, densemata))
+            @test !issparse(cat((1,2), densemata, otherdense))
+            @test !issparse(cat((1,2), otherdense, densemata))
+        end
+    end
+end
