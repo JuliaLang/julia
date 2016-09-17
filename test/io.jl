a9dada2e5262e894cd073bdb87a5878db9dd0d9f
Jake Rosoman <jkroso@gmail.com>
2016-09-18 00:16:42 +1200

    tidy

--- test/io.jl
+++ test/io.jl
@@ -1,8 +1,5 @@
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
-type Buf <: IO b::Vector end
-Base.write(b::Buf, v::Vector{UInt8}) = (b.b = v; length(v))
-
-b = Buf([])
-@test write(b, IOBuffer("abc")) == 3
-@test b.b == b"abc"
+b = IOBuffer()
+@test invoke(write,(IO,IO), b, IOBuffer("abc")) == 3
+@test takebuf_string(b) == "abc"
