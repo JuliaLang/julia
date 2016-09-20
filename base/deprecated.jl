cdfc9cd26e3814af7e2b11ddc16ec7d9294b6a90
Sacha Verweij <sacha@stanford.edu>
2016-09-20 14:49:48 -0700

    Deprecate manually vectorized clamp methods in favor of compact broadcast syntax.

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -1360,4 +1360,7 @@ for f in (:sec, :sech, :secd, :asec, :asech,
     @eval @deprecate $f{T<:Number}(A::AbstractArray{T}) $f.(A)
 end
 
+# Deprecate manually vectorized clamp methods in favor of compact broadcast syntax
+@deprecate clamp(A::AbstractArray, lo, hi) clamp.(A, lo, hi)
+
 # End deprecations scheduled for 0.6
