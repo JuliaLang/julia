60290e24878433b89c3f5b4ede528d8ed177a87b
Valentin Churavy <v.churavy@gmail.com>
2016-09-23 05:47:02 +0900

    reenable partword atomics

--- deps/llvm.mk
+++ deps/llvm.mk
@@ -460,6 +460,7 @@ else ifeq ($(LLVM_VER_SHORT),3.9)
 # fix lowering for atomics on ppc
 $(eval $(call LLVM_PATCH,llvm-rL279933-ppc-atomicrmw-lowering)) # Remove for 4.0
 $(eval $(call LLVM_PATCH,llvm-PR22923)) # Remove for 4.0
+$(eval $(call LLVM_PATCH,llvm-r282182)) # Remove for 4.0
 endif # LLVM_VER
 
 ifeq ($(LLVM_VER),3.7.1)
