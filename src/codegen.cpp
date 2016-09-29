db6c417e037ddee6a5426024e69a0350a260dd8f
Tony Kelman <tony@kelman.net>
2016-09-28 19:32:43 -0700

    four space indent the metaprogramming doc examples
    and macroexpand doctests, and several places in base/ and src/ and test/

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -1307,7 +1307,7 @@ void *jl_get_llvmf_defn(jl_method_instance_t *linfo, size_t world, bool getwrapp
     jl_llvm_functions_t declarations;
     std::unique_ptr<Module> m;
     JL_TRY {
-         m = emit_function(linfo, src, world, &declarations, &params);
+        m = emit_function(linfo, src, world, &declarations, &params);
     }
     JL_CATCH {
         // something failed!
