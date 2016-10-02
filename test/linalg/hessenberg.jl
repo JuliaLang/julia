1c9cdbe5e922ccc7d745656a8aaea5e087140a45
kshyatt <kshyatt@physics.ucsb.edu>
2016-10-01 22:27:01 -0700

    Convert hessenberg tests to use testsets

--- test/linalg/hessenberg.jl
+++ test/linalg/hessenberg.jl
@@ -1,6 +1,5 @@
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
-debug = false
 using Base.Test
 
 using Base.LinAlg: BlasComplex, BlasFloat, BlasReal, QRPivoted
@@ -11,14 +10,12 @@ let n = 10
     Areal  = randn(n,n)/2
     Aimg   = randn(n,n)/2
 
-    for eltya in (Float32, Float64, Complex64, Complex128, BigFloat, Int)
+    @testset for eltya in (Float32, Float64, Complex64, Complex128, BigFloat, Int)
         A = eltya == Int ?
                 rand(1:7, n, n) :
                 convert(Matrix{eltya}, eltya <: Complex ?
                     complex(Areal, Aimg) :
                     Areal)
-
-debug && println("\ntype of a: ", eltya, " type of b: ", eltyb, "\n")
 
         if eltya != BigFloat
             H = hessfact(A)
