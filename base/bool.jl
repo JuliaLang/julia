3f12f9f8874e29c35e6992a90733f442e4c9324a
Yichao Yu <yyc1992@gmail.com>
2016-10-04 18:43:51 -0400

    Do more constant propagation at type inference time.

--- base/bool.jl
+++ base/bool.jl
@@ -14,7 +14,11 @@ typemax(::Type{Bool}) = true
 
 ## boolean operations ##
 
-!(x::Bool) = box(Bool,not_int(unbox(Bool,x)))
+function !(x::Bool)
+    ## We need a better heuristic to detect this automatically
+    @_pure_meta
+    return box(Bool,not_int(unbox(Bool,x)))
+end
 
 (~)(x::Bool) = !x
 (&)(x::Bool, y::Bool) = box(Bool,and_int(unbox(Bool,x),unbox(Bool,y)))
