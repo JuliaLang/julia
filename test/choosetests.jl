e265a5e8f302782cc3e44d67e842f0aa0a44f36d
Amit Murthy <amit.murthy@gmail.com>
2016-10-07 14:42:04 +0530

    Channel constructor requires an explict size. Move channel tests into its own file. Implement 0-sized channels.

--- test/choosetests.jl
+++ test/choosetests.jl
@@ -33,7 +33,8 @@ function choosetests(choices = [])
         "markdown", "base64", "serialize", "misc", "threads",
         "enums", "cmdlineargs", "i18n", "workspace", "libdl", "int",
         "checked", "intset", "floatfuncs", "compile", "parallel", "inline",
-        "boundscheck", "error", "ambiguous", "cartesian", "asmvariant"
+        "boundscheck", "error", "ambiguous", "cartesian", "asmvariant",
+        "channels"
     ]
     profile_skipped = false
     if startswith(string(Sys.ARCH), "arm")
