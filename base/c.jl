05c3a448c3d3b820554a91251d1d4749b217f1e5
Tim Besard <tim.besard@gmail.com>
2016-10-11 16:11:35 +0200

    Move simple typealiases from c.jl to an earlier-loaded ctypes.jl.
    This allows using said aliases for ccalls early in core and sysimg generation.

--- base/c.jl
+++ base/c.jl
@@ -18,36 +18,6 @@ Equivalent to the native `char` c-type.
 """
 Cchar
 
-"""
-    Cuchar
-
-Equivalent to the native `unsigned char` c-type (`UInt8`).
-"""
-typealias Cuchar UInt8
-"""
-    Cshort
-
-Equivalent to the native `signed short` c-type (`Int16`).
-"""
-typealias Cshort Int16
-"""
-    Cushort
-
-Equivalent to the native `unsigned short` c-type (`UInt16`).
-"""
-typealias Cushort UInt16
-"""
-    Cint
-
-Equivalent to the native `signed int` c-type (`Int32`).
-"""
-typealias Cint Int32
-"""
-    Cuint
-
-Equivalent to the native `unsigned int` c-type (`UInt32`).
-"""
-typealias Cuint UInt32
 if is_windows()
     typealias Clong Int32
     typealias Culong UInt32
@@ -75,61 +45,6 @@ Culong
 Equivalent to the native `wchar_t` c-type (`Int32`).
 """
 Cwchar_t
-
-"""
-    Cptrdiff_t
-
-Equivalent to the native `ptrdiff_t` c-type (`Int`).
-"""
-typealias Cptrdiff_t Int
-"""
-    Csize_t
-
-Equivalent to the native `size_t` c-type (`UInt`).
-"""
-typealias Csize_t UInt
-"""
-    Cssize_t
-
-Equivalent to the native `ssize_t` c-type.
-"""
-typealias Cssize_t Int
-"""
-    Cintmax_t
-
-Equivalent to the native `intmax_t` c-type (`Int64`).
-"""
-typealias Cintmax_t Int64
-"""
-    Cuintmax_t
-
-Equivalent to the native `uintmax_t` c-type (`UInt64`).
-"""
-typealias Cuintmax_t UInt64
-"""
-    Clonglong
-
-Equivalent to the native `signed long long` c-type (`Int64`).
-"""
-typealias Clonglong Int64
-"""
-    Culonglong
-
-Equivalent to the native `unsigned long long` c-type (`UInt64`).
-"""
-typealias Culonglong UInt64
-"""
-    Cfloat
-
-Equivalent to the native `float` c-type (`Float32`).
-"""
-typealias Cfloat Float32
-"""
-    Cdouble
-
-Equivalent to the native `double` c-type (`Float64`).
-"""
-typealias Cdouble Float64
 
 if !is_windows()
     const sizeof_mode_t = ccall(:jl_sizeof_mode_t, Cint, ())
