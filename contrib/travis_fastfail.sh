baeeb18925be9e7d1e98153ff1cf6d13fa24c1e3
albap <albapompeo@gmail.com>
2016-10-15 11:37:27 -0300

    Improve travis_fastfail.sh
    Used https://www.shellcheck.net/ to check for mistakes and fixed them.

--- contrib/travis_fastfail.sh
+++ contrib/travis_fastfail.sh
@@ -6,9 +6,9 @@ endpoint="https://api.travis-ci.org/repos/$TRAVIS_REPO_SLUG"
 
 # Fail fast for superseded builds to PR's
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  newestbuildforthisPR=$(curl -H "$curlhdr" $endpoint/builds?event_type=pull_request | \
+  newestbuildforthisPR=$(curl -H "$curlhdr" "$endpoint"/builds?event_type=pull_request | \
       jq ".builds | map(select(.pull_request_number == $TRAVIS_PULL_REQUEST))[0].number")
-  if [ $newestbuildforthisPR != null -a $newestbuildforthisPR != \"$TRAVIS_BUILD_NUMBER\" ]; then
+  if [ "$newestbuildforthisPR" != null ] && [ "$newestbuildforthisPR" != \""$TRAVIS_BUILD_NUMBER"\" ]; then
     echo "There are newer queued builds for this pull request, failing early."
     exit 1
   fi
@@ -18,8 +18,8 @@ else
     master | release*)
       ;;
     *)
-      if [ \"$TRAVIS_BUILD_NUMBER\" != $(curl -H "$curlhdr" \
-          $endpoint/branches/$TRAVIS_BRANCH | jq ".branch.number") ]; then
+      if [ \""$TRAVIS_BUILD_NUMBER"\" != "$(curl -H "$curlhdr" \
+          "$endpoint"/branches/"$TRAVIS_BRANCH" | jq ".branch.number")" ]; then
         echo "There are newer queued builds for this branch, failing early."
         exit 1
       fi
