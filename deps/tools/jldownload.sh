4d6d77bb9ca019e60880e24cdfe46469b56c4c3e
albap <albapompeo@gmail.com>
2016-10-15 11:55:55 -0300

    Rename jldownload to jldownload.sh

--- deps/tools/jldownload.sh
+++ deps/tools/jldownload.sh
@@ -44,4 +44,4 @@ fi
 # forward to the original URL if it has not cached this download yet, or
 # if the URL is not cacheable.  We fallback to directly querying the
 # uncached URL to protect against cache service downtime
-$GETURL $CACHE_URL || $GETURL $URL
+$GETURL "$CACHE_URL" || $GETURL "$URL"
