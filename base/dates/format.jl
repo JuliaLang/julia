59c79911dc124cb1d1a256c3f0b9d324c7122721
bramtayl <brandon.taylor221@gmail.com>
2016-10-16 16:36:37 -0400

    Update format.jl

--- base/dates/format.jl
+++ base/dates/format.jl
@@ -1,15 +1,15 @@
 
 """
     strftime([format], time)
-Convert time, given as a number of seconds since the epoch or a `TmStruct`, to a formatted
+Convert time, given as a number of seconds since the epoch or a `HumanTime`, to a formatted
 string using the given format. Supported formats are the same as those in the standard C
 library.
 """
 strftime(t) = strftime("%c", t)
-strftime(fmt::AbstractString, t::Real) = strftime(fmt, TmStruct(t))
-function strftime(fmt::AbstractString, tm::TmStruct)
+strftime(fmt::AbstractString, t::Real) = strftime(fmt, HumanTime(t))
+function strftime(fmt::AbstractString, tm::HumanTime)
     timestr = Array{UInt8}(128)
-    n = ccall(:strftime, Int, (Ptr{UInt8}, Int, Cstring, Ptr{TmStruct}),
+    n = ccall(:strftime, Int, (Ptr{UInt8}, Int, Cstring, Ptr{HumanTime}),
               timestr, length(timestr), fmt, &tm)
     if n == 0
         return ""
@@ -19,7 +19,7 @@ end
 
 """
     strptime([format], timestr)
-Parse a formatted time string into a `TmStruct` giving the seconds, minute, hour, date, etc.
+Parse a formatted time string into a `HumanTime` giving the seconds, minute, hour, date, etc.
 Supported formats are the same as those in the standard C library. On some platforms,
 timezones will not be parsed correctly. If the result of this function will be passed to
 `time` to convert it to seconds since the epoch, the `isdst` field should be filled in
@@ -28,8 +28,8 @@ determine the timezone.
 """
 strptime(timestr::AbstractString) = strptime("%c", timestr)
 function strptime(fmt::AbstractString, timestr::AbstractString)
-    tm = TmStruct()
-    r = ccall(:strptime, Cstring, (Cstring, Cstring, Ptr{TmStruct}),
+    tm = HumanTime()
+    r = ccall(:strptime, Cstring, (Cstring, Cstring, Ptr{HumanTime}),
               timestr, fmt, &tm)
     # the following would tell mktime() that this is a local time, and that
     # it should try to guess the timezone. not sure if/how this should be
@@ -43,7 +43,7 @@ function strptime(fmt::AbstractString, timestr::AbstractString)
         # if we didn't explicitly parse the weekday or year day, use mktime
         # to fill them in automatically.
         if !ismatch(r"([^%]|^)%(a|A|j|w|Ow)", fmt)
-            ccall(:mktime, Int, (Ptr{TmStruct},), &tm)
+            ccall(:mktime, Int, (Ptr{HumanTime},), &tm)
         end
     end
     return convert(DateTime, tm)
