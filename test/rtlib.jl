475cd37e2d63b081c7eb5e73a9c5814ce711ce7e
Valentin Churavy <v.churavy@gmail.com>
2016-10-16 10:58:47 +0900

    Compiler-rt adapt test extendhfsf2
    As of compiler-rt 3.9.0 the testsuite uses compareResultH to check for
    correctness. That function uses uint16_t under the hood instead of
    checking the full 32bit.

--- test/rtlib.jl
+++ test/rtlib.jl
@@ -77,6 +77,13 @@ end
 end
 
 @testset "extendhfsf2" begin
+    ##
+    # Note:
+    # These tests are taken fromt the compiler-rt testsuite. Were as of 3.9.0
+    # the test are done with compareResultH (so with after casting to UInt16)
+    # Tests that are commented out fail as === Float32 comparisons.
+    # Some tests succedd with ≈ (and are consistent with Julia v0.5 convert)
+    ##
     # NaN
     @test RTLIB.extendhfsf2(reinterpret(Float16, 0x7e00)) === NaN32
     # inf
@@ -85,9 +92,9 @@ end
     # zero
     @test RTLIB.extendhfsf2(reinterpret(Float16, 0x0000)) === 0.0f0
     @test RTLIB.extendhfsf2(reinterpret(Float16, 0x8000)) === -0.0f0
-    @test RTLIB.extendhfsf2(reinterpret(Float16, 0x4248)) === 3.1415926535f0
-    @test RTLIB.extendhfsf2(reinterpret(Float16, 0xc248)) === -3.1415926535f0
-    @test RTLIB.extendhfsf2(reinterpret(Float16, 0x7c00)) === Float32(0x1.987124876876324p+100)
+    @test RTLIB.extendhfsf2(reinterpret(Float16, 0x4248)) ≈ Float32(π)
+    @test RTLIB.extendhfsf2(reinterpret(Float16, 0xc248)) ≈ Float32(-π)
+    # @test RTLIB.extendhfsf2(reinterpret(Float16, 0x7c00)) === Float32(0x1.987124876876324p+100)
     @test RTLIB.extendhfsf2(reinterpret(Float16, 0x6e62)) === Float32(0x1.988p+12)
     @test RTLIB.extendhfsf2(reinterpret(Float16, 0x3c00)) === Float32(0x1.0p+0)
     @test RTLIB.extendhfsf2(reinterpret(Float16, 0x0400)) === Float32(0x1.0p-14)
@@ -95,10 +102,10 @@ end
     @test RTLIB.extendhfsf2(reinterpret(Float16, 0x0010)) === Float32(0x1.0p-20)
     @test RTLIB.extendhfsf2(reinterpret(Float16, 0x0001)) === Float32(0x1.0p-24)
     @test RTLIB.extendhfsf2(reinterpret(Float16, 0x8001)) === Float32(-0x1.0p-24)
-    @test RTLIB.extendhfsf2(reinterpret(Float16, 0x0001)) === Float32(0x1.5p-25)
+    #@test RTLIB.extendhfsf2(reinterpret(Float16, 0x0001)) === Float32(0x1.5p-25)
     # and back to zero
-    @test RTLIB.extendhfsf2(reinterpret(Float16, 0x0000)) === Float32(0x1.0p-25)
-    @test RTLIB.extendhfsf2(reinterpret(Float16, 0x8000)) === Float32(-0x1.0p-25)
+    # @test RTLIB.extendhfsf2(reinterpret(Float16, 0x0000)) === Float32(0x1.0p-25)
+    # @test RTLIB.extendhfsf2(reinterpret(Float16, 0x8000)) === Float32(-0x1.0p-25)
     # max (precise)
     @test RTLIB.extendhfsf2(reinterpret(Float16, 0x7bff)) === 65504.0f0
     # max (rounded)
