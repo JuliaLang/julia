b730778bd92fd4c531c382555d7889a0131fa940
Andreas Noack <andreasnoackjensen@gmail.com>
2016-10-18 16:49:55 -0400

    Fix BLAS.syr/her! by checking stride of x (#18919)
    (cherry picked from commit d0914f87c808fa234d91030c789d6f33a5330bc9)

--- test/blas.jl
+++ test/blas.jl
@@ -299,3 +299,19 @@ for elty in [Float32, Float64, Complex64, Complex128]
         @test_throws DimensionMismatch BLAS.syrk!('L','N',one(elty),eye(elty,5),one(elty),eye(elty,6))
     end
 end
+
+@testset "syr for eltype $elty" for elty in (Float32, Float64, Complex{Float32}, Complex{Float64})
+    A = rand(elty, 5, 5)
+    @test triu(A[1,:] * A[1,:].') ≈ BLAS.syr!('U', one(elty), A[1,:], zeros(elty, 5, 5))
+    @test tril(A[1,:] * A[1,:].') ≈ BLAS.syr!('L', one(elty), A[1,:], zeros(elty, 5, 5))
+    @test triu(A[1,:] * A[1,:].') ≈ BLAS.syr!('U', one(elty), view(A, 1, :), zeros(elty, 5, 5))
+    @test tril(A[1,:] * A[1,:].') ≈ BLAS.syr!('L', one(elty), view(A, 1, :), zeros(elty, 5, 5))
+end
+
+@testset "her for eltype $elty" for elty in (Complex{Float32}, Complex{Float64})
+    A = rand(elty, 5, 5)
+    @test triu(A[1,:] * A[1,:]') ≈ BLAS.her!('U', one(real(elty)), A[1,:], zeros(elty, 5, 5))
+    @test tril(A[1,:] * A[1,:]') ≈ BLAS.her!('L', one(real(elty)), A[1,:], zeros(elty, 5, 5))
+    @test triu(A[1,:] * A[1,:]') ≈ BLAS.her!('U', one(real(elty)), view(A, 1, :), zeros(elty, 5, 5))
+    @test tril(A[1,:] * A[1,:]') ≈ BLAS.her!('L', one(real(elty)), view(A, 1, :), zeros(elty, 5, 5))
+end
