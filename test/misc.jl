28630dfd3f2725efe9c6162059c02a8d04344394
colinfang <colinfang@live.com>
2016-10-21 23:58:44 +0100

    Chance test location

--- test/misc.jl
+++ test/misc.jl
@@ -474,3 +474,14 @@ let
     end
     @test c_18711 == 1
 end
+
+# Test prettyprint_getunits
+@test Base.prettyprint_getunits(0, 6, 1024) == (0, 1)
+@test Base.prettyprint_getunits(1, 6, 1024) == (1, 1)
+@test Base.prettyprint_getunits(1023, 6, 1024)[2] == 1
+@test Base.prettyprint_getunits(1024, 6, 1024) == (1, 2)
+@test Base.prettyprint_getunits(1025, 6, 1024)[2] == 2
+@test Base.prettyprint_getunits(1024^5 - 1, 6, 1024)[2] == 5
+@test Base.prettyprint_getunits(1024^5, 6, 1024) == (1, 6)
+@test Base.prettyprint_getunits(1024^5 + 1, 6, 1024)[2] == 6
+@test Base.prettyprint_getunits(1024^5, 5, 1024) == (1024, 5)
