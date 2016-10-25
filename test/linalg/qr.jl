5f48f4c1f1eb41c208a40aa46bcc680f5439a897
Katie Hyatt <kshyatt@physics.ucsb.edu>
2016-10-25 10:48:23 -0700

    Conversion tests & switch to testset

--- test/linalg/qr.jl
+++ test/linalg/qr.jl
@@ -1,6 +1,5 @@
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
-debug = false
 using Base.Test
 
 using Base.LinAlg: BlasComplex, BlasFloat, BlasReal, QRPivoted
@@ -20,14 +19,14 @@ a2img  = randn(n,n)/2
 breal = randn(n,2)/2
 bimg  = randn(n,2)/2
 
-for eltya in (Float32, Float64, Complex64, Complex128, BigFloat, Int)
+@testset for eltya in (Float32, Float64, Complex64, Complex128, BigFloat, Int)
     a = eltya == Int ? rand(1:7, n, n) : convert(Matrix{eltya}, eltya <: Complex ? complex(areal, aimg) : areal)
     a2 = eltya == Int ? rand(1:7, n, n) : convert(Matrix{eltya}, eltya <: Complex ? complex(a2real, a2img) : a2real)
     asym = a'+a                  # symmetric indefinite
     apd  = a'*a                 # symmetric positive-definite
     ε = εa = eps(abs(float(one(eltya))))
 
-    for eltyb in (Float32, Float64, Complex64, Complex128, Int)
+    @testset for eltyb in (Float32, Float64, Complex64, Complex128, Int)
         b = eltyb == Int ? rand(1:5, n, 2) : convert(Matrix{eltyb}, eltyb <: Complex ? complex(breal, bimg) : breal)
         εb = eps(abs(float(one(eltyb))))
         ε = max(εa,εb)
@@ -38,107 +37,108 @@ for eltya in (Float32, Float64, Complex64, Complex128, BigFloat, Int)
         @test abs(qrfact(α)[:Q][1,1]) ≈ one(eltyb)
         tab = promote_type(eltya,eltyb)
 
-debug && println("\ntype of a: ", eltya, " type of b: ", eltyb, "\n")
-debug && println("QR decomposition (without pivoting)")
         for i = 1:2
             let a = i == 1 ? a : view(a, 1:n - 1, 1:n - 1), b = i == 1 ? b : view(b, 1:n - 1), n = i == 1 ? n : n - 1
-                qra   = @inferred qrfact(a)
-                @inferred qr(a)
-                q, r  = qra[:Q], qra[:R]
-                @test_throws KeyError qra[:Z]
-                @test q'*full(q, thin=false) ≈ eye(n)
-                @test q*full(q, thin=false)' ≈ eye(n)
-                @test q'*eye(n)' ≈ full(q, thin=false)'
-                @test full(q, thin=false)'q ≈ eye(n)
-                @test eye(n)'q' ≈ full(q, thin=false)'
-                @test q*r ≈ a
-                @test_approx_eq_eps a*(qra\b) b 3000ε
-                @test full(qra) ≈ a
-                @test_approx_eq_eps A_mul_Bc(eye(eltyb,size(q.factors,2)),q)*full(q, thin=false) eye(n) 5000ε
-                if eltya != Int
-                    @test eye(eltyb,n)*q ≈ convert(AbstractMatrix{tab},q)
-                    ac = copy(a)
-                    @test qrfact!(a[:, 1:5])\b == qrfact!(view(ac, :, 1:5))\b
+                @testset "QR decomposition (without pivoting)" begin
+                    qra   = @inferred qrfact(a)
+                    @inferred qr(a)
+                    q, r  = qra[:Q], qra[:R]
+                    @test_throws KeyError qra[:Z]
+                    @test q'*full(q, thin=false) ≈ eye(n)
+                    @test q*full(q, thin=false)' ≈ eye(n)
+                    @test q'*eye(n)' ≈ full(q, thin=false)'
+                    @test full(q, thin=false)'q ≈ eye(n)
+                    @test eye(n)'q' ≈ full(q, thin=false)'
+                    @test q*r ≈ a
+                    @test_approx_eq_eps a*(qra\b) b 3000ε
+                    @test Array(qra) ≈ a
+                    @test_approx_eq_eps A_mul_Bc(eye(eltyb,size(q.factors,2)),q)*full(q, thin=false) eye(n) 5000ε
+                    if eltya != Int
+                        @test eye(eltyb,n)*q ≈ convert(AbstractMatrix{tab},q)
+                        ac = copy(a)
+                        @test qrfact!(a[:, 1:5])\b == qrfact!(view(ac, :, 1:5))\b
+                    end
                 end
-
-debug && println("Thin QR decomposition (without pivoting)")
-                qra   = @inferred qrfact(a[:,1:n1], Val{false})
-                @inferred qr(a[:,1:n1], Val{false})
-                q,r   = qra[:Q], qra[:R]
-                @test_throws KeyError qra[:Z]
-                @test q'*full(q, thin=false) ≈ eye(n)
-                @test q'*full(q) ≈ eye(n,n1)
-                @test q*r ≈ a[:,1:n1]
-                @test_approx_eq_eps q*b[1:n1] full(q)*b[1:n1] 100ε
-                @test_approx_eq_eps q*b full(q, thin=false)*b 100ε
-                @test_throws DimensionMismatch q*b[1:n1 + 1]
-                @test_throws DimensionMismatch b[1:n1 + 1]*q'
-                @test_approx_eq_eps A_mul_Bc(UpperTriangular(eye(eltyb,size(q.factors,2))),q)*full(q, thin=false) eye(n1,n) 5000ε
-                if eltya != Int
-                    @test eye(eltyb,n)*q ≈ convert(AbstractMatrix{tab},q)
+                @testset "Thin QR decomposition" begin
+                    qra   = @inferred qrfact(a[:,1:n1], Val{false})
+                    @inferred qr(a[:,1:n1], Val{false})
+                    q,r   = qra[:Q], qra[:R]
+                    @test_throws KeyError qra[:Z]
+                    @test q'*full(q, thin=false) ≈ eye(n)
+                    @test q'*full(q) ≈ eye(n,n1)
+                    @test q*r ≈ a[:,1:n1]
+                    @test_approx_eq_eps q*b[1:n1] full(q)*b[1:n1] 100ε
+                    @test_approx_eq_eps q*b full(q, thin=false)*b 100ε
+                    @test_throws DimensionMismatch q*b[1:n1 + 1]
+                    @test_throws DimensionMismatch b[1:n1 + 1]*q'
+                    @test_approx_eq_eps A_mul_Bc(UpperTriangular(eye(eltyb,size(q.factors,2))),q)*full(q, thin=false) eye(n1,n) 5000ε
+                    if eltya != Int
+                        @test eye(eltyb,n)*q ≈ convert(AbstractMatrix{tab},q)
+                    end
                 end
+                @testset "(Automatic) Fat (pivoted) QR decomposition" begin
+                    @inferred qrfact(a, Val{true})
+                    @inferred qr(a, Val{true})
 
-debug && println("(Automatic) Fat (pivoted) QR decomposition")
-                @inferred qrfact(a, Val{true})
-                @inferred qr(a, Val{true})
-
-                qrpa  = factorize(a[1:n1,:])
-                q,r = qrpa[:Q], qrpa[:R]
-                @test_throws KeyError qrpa[:Z]
-                p = qrpa[:p]
-                @test q'*full(q, thin=false) ≈ eye(n1)
-                @test q*full(q, thin=false)' ≈ eye(n1)
-                @test (UpperTriangular(eye(eltya,size(q,2)))*q')*full(q, thin=false) ≈ eye(n1)
-                @test q*r ≈ (isa(qrpa,QRPivoted) ? a[1:n1,p] : a[1:n1,:])
-                @test q*r[:,invperm(p)] ≈ a[1:n1,:]
-                @test q*r*qrpa[:P].' ≈ a[1:n1,:]
-                @test_approx_eq_eps a[1:n1,:]*(qrpa\b[1:n1]) b[1:n1] 5000ε
-                @test full(qrpa) ≈ a[1:5,:]
-                @test_throws DimensionMismatch q*b[1:n1+1]
-                @test_throws DimensionMismatch b[1:n1+1]*q'
-                if eltya != Int
-                    @test eye(eltyb,n1)*q ≈ convert(AbstractMatrix{tab},q)
+                    qrpa  = factorize(a[1:n1,:])
+                    q,r = qrpa[:Q], qrpa[:R]
+                    @test_throws KeyError qrpa[:Z]
+                    p = qrpa[:p]
+                    @test q'*full(q, thin=false) ≈ eye(n1)
+                    @test q*full(q, thin=false)' ≈ eye(n1)
+                    @test (UpperTriangular(eye(eltya,size(q,2)))*q')*full(q, thin=false) ≈ eye(n1)
+                    @test q*r ≈ (isa(qrpa,QRPivoted) ? a[1:n1,p] : a[1:n1,:])
+                    @test q*r[:,invperm(p)] ≈ a[1:n1,:]
+                    @test q*r*qrpa[:P].' ≈ a[1:n1,:]
+                    @test_approx_eq_eps a[1:n1,:]*(qrpa\b[1:n1]) b[1:n1] 5000ε
+                    @test Array(qrpa) ≈ a[1:5,:]
+                    @test_throws DimensionMismatch q*b[1:n1+1]
+                    @test_throws DimensionMismatch b[1:n1+1]*q'
+                    if eltya != Int
+                        @test eye(eltyb,n1)*q ≈ convert(AbstractMatrix{tab},q)
+                    end
                 end
-
-debug && println("(Automatic) Thin (pivoted) QR decomposition")
-                qrpa  = factorize(a[:,1:n1])
-                q,r = qrpa[:Q], qrpa[:R]
-                @test_throws KeyError qrpa[:Z]
-                p = qrpa[:p]
-                @test q'*full(q, thin=false) ≈ eye(n)
-                @test q*full(q, thin=false)' ≈ eye(n)
-                @test q*r ≈ a[:,p]
-                @test q*r[:,invperm(p)] ≈ a[:,1:n1]
-                @test full(qrpa) ≈ a[:,1:5]
-                @test_throws DimensionMismatch q*b[1:n1+1]
-                @test_throws DimensionMismatch b[1:n1+1]*q'
-                @test_approx_eq_eps A_mul_Bc(UpperTriangular(eye(eltyb,size(q.factors,2))),q)*full(q, thin=false) eye(n1,n) 5000ε
-                if eltya != Int
-                    @test eye(eltyb,n)*q ≈ convert(AbstractMatrix{tab},q)
+                @testset "(Automatic) Thin (pivoted) QR decomposition" begin
+                    qrpa  = factorize(a[:,1:n1])
+                    q,r = qrpa[:Q], qrpa[:R]
+                    @test_throws KeyError qrpa[:Z]
+                    p = qrpa[:p]
+                    @test q'*full(q, thin=false) ≈ eye(n)
+                    @test q*full(q, thin=false)' ≈ eye(n)
+                    @test q*r ≈ a[:,p]
+                    @test q*r[:,invperm(p)] ≈ a[:,1:n1]
+                    @test Array(qrpa) ≈ a[:,1:5]
+                    @test_throws DimensionMismatch q*b[1:n1+1]
+                    @test_throws DimensionMismatch b[1:n1+1]*q'
+                    @test_approx_eq_eps A_mul_Bc(UpperTriangular(eye(eltyb,size(q.factors,2))),q)*full(q, thin=false) eye(n1,n) 5000ε
+                    if eltya != Int
+                        @test eye(eltyb,n)*q ≈ convert(AbstractMatrix{tab},q)
+                    end
                 end
             end
         end
 
-debug && println("Matmul with QR factorizations")
-        if eltya != Int
-            qrpa = factorize(a[:,1:n1])
-            q, r = qrpa[:Q], qrpa[:R]
-            @test A_mul_B!(full(q, thin=false)',q) ≈ eye(n)
-            @test_throws DimensionMismatch A_mul_B!(eye(eltya,n+1),q)
-            @test A_mul_Bc!(full(q, thin=false),q) ≈ eye(n)
-            @test_throws DimensionMismatch A_mul_Bc!(eye(eltya,n+1),q)
-            @test_throws BoundsError size(q,-1)
-            @test_throws DimensionMismatch Base.LinAlg.A_mul_B!(q,zeros(eltya,n1+1))
-            @test_throws DimensionMismatch Base.LinAlg.Ac_mul_B!(q,zeros(eltya,n1+1))
+        @testset "Matmul with QR factorizations" begin
+            if eltya != Int
+                qrpa = factorize(a[:,1:n1])
+                q, r = qrpa[:Q], qrpa[:R]
+                @test A_mul_B!(full(q, thin=false)',q) ≈ eye(n)
+                @test_throws DimensionMismatch A_mul_B!(eye(eltya,n+1),q)
+                @test A_mul_Bc!(full(q, thin=false),q) ≈ eye(n)
+                @test_throws DimensionMismatch A_mul_Bc!(eye(eltya,n+1),q)
+                @test_throws BoundsError size(q,-1)
+                @test_throws DimensionMismatch Base.LinAlg.A_mul_B!(q,zeros(eltya,n1+1))
+                @test_throws DimensionMismatch Base.LinAlg.Ac_mul_B!(q,zeros(eltya,n1+1))
 
-            qra = qrfact(a[:,1:n1], Val{false})
-            q, r = qra[:Q], qra[:R]
-            @test A_mul_B!(full(q, thin=false)',q) ≈ eye(n)
-            @test_throws DimensionMismatch A_mul_B!(eye(eltya,n+1),q)
-            @test A_mul_Bc!(full(q, thin=false),q) ≈ eye(n)
-            @test_throws DimensionMismatch A_mul_Bc!(eye(eltya,n+1),q)
-            @test_throws BoundsError size(q,-1)
-            @test_throws DimensionMismatch q * eye(Int8,n+4)
+                qra = qrfact(a[:,1:n1], Val{false})
+                q, r = qra[:Q], qra[:R]
+                @test A_mul_B!(full(q, thin=false)',q) ≈ eye(n)
+                @test_throws DimensionMismatch A_mul_B!(eye(eltya,n+1),q)
+                @test A_mul_Bc!(full(q, thin=false),q) ≈ eye(n)
+                @test_throws DimensionMismatch A_mul_Bc!(eye(eltya,n+1),q)
+                @test_throws BoundsError size(q,-1)
+                @test_throws DimensionMismatch q * eye(Int8,n+4)
+            end
         end
     end
 end
@@ -154,20 +154,20 @@ end
 # Issue 7304
 let
     A = [-√.5 -√.5; -√.5 √.5]
-    Q = full(qrfact(A)[:Q])
+    Q = Array(qrfact(A)[:Q])
     @test vecnorm(A-Q) < eps()
 end
 
-let
-    debug && println("qr on AbstractVector")
-
-    vr = [3.0, 4.0]
-    for Tr in (Float32, Float64)
-        for T in (Tr, Complex{Tr})
-            v = convert(Vector{T}, vr)
-            nv, nm = qr(v)
-            @test norm(nv - [0.6, 0.8], Inf) < eps(Tr)
-            @test nm == 5.0
+@testset "QR on AbstractVector" begin
+    let
+        vr = [3.0, 4.0]
+        for Tr in (Float32, Float64)
+            for T in (Tr, Complex{Tr})
+                v = convert(Vector{T}, vr)
+                nv, nm = qr(v)
+                @test norm(nv - [0.6, 0.8], Inf) < eps(Tr)
+                @test nm == 5.0
+            end
         end
     end
 end
