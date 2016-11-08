a464afe6b0bbde5e871c245735450fd8da2b699b
Curtis Vogt <curtis.vogt@gmail.com>
2016-11-08 12:31:41 -0600

    Update docstrings

--- base/dates/periods.jl
+++ base/dates/periods.jl
@@ -190,36 +190,52 @@ end
 """
     CompoundPeriod(periods) -> CompoundPeriod
 
-Construct a `CompoundPeriod` from a `Vector` of `Period`s. The constructor will
-automatically simplify the periods into a canonical form according to the following rules:
-
-* All `Period`s of the same type will be added together
-* Any `Period` large enough be partially representable by a coarser `Period` will be broken
-  into multiple `Period`s (eg. `Hour(30)` becomes `Day(1) + Hour(6)`)
-* `Period`s with opposite signs will be combined when possible
-  (eg. `Hour(1) - Day(1)` becomes `-Hour(23)`)
-
-Due to the canonicalization, `CompoundPeriod` is also useful for converting time periods
-into more human-comprehensible forms.
+Construct a `CompoundPeriod` from a `Vector` of `Period`s. All `Period`s of the same type
+will be added together.
 
 # Examples
 ```julia
 julia> Dates.CompoundPeriod([Dates.Hour(12), Dates.Hour(13)])
-1 day, 1 hour
+25 hours
 
 julia> Dates.CompoundPeriod([Dates.Hour(-1), Dates.Minute(1)])
--59 minutes
+-1 hour, 1 minute
 
 julia> Dates.CompoundPeriod([Dates.Month(1), Dates.Week(-2)])
 1 month, -2 weeks
 
-julia> Dates.CompoundPeriod(Dates.Minute(50000)))
-4 weeks, 6 days, 17 hours, 20 minutes
+julia> Dates.CompoundPeriod(Dates.Minute(50000))
+50000 minutes
 ```
 """
 CompoundPeriod{P<:Period}(p::Vector{P}) = CompoundPeriod(Array{Period}(p))
 
 
+"""
+    canonicalize(::CompoundPeriod) -> CompoundPeriod
+
+Reduces the `CompoundPeriod` into its canonical form by applying the following rules:
+
+* Any `Period` large enough be partially representable by a coarser `Period` will be broken
+  into multiple `Period`s (eg. `Hour(30)` becomes `Day(1) + Hour(6)`)
+* `Period`s with opposite signs will be combined when possible
+  (eg. `Hour(1) - Day(1)` becomes `-Hour(23)`)
+
+# Examples
+```julia
+julia> Dates.canonicalize(Dates.CompoundPeriod([Dates.Hour(12), Dates.Hour(13)]))
+1 day, 1 hour
+
+julia> Dates.canonicalize(Dates.CompoundPeriod([Dates.Hour(-1), Dates.Minute(1)]))
+-59 minutes
+
+julia> Dates.canonicalize(Dates.CompoundPeriod([Dates.Month(1), Dates.Week(-2)]))
+1 month, -2 weeks
+
+julia> Dates.canonicalize(Dates.CompoundPeriod(Dates.Minute(50000)))
+4 weeks, 6 days, 17 hours, 20 minutes
+```
+"""
 function canonicalize(x::CompoundPeriod)
     # canonicalize Periods by pushing "overflow" into a coarser period.
     p = x.periods
