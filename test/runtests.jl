9b20fb96e758decf443a979a219090185a0bc34e
Elliot Saba <staticfloat@gmail.com>
2016-11-21 11:44:34 -0800

    Integrate test segfaults with the Testset scaffolding
    Fixes #19376
    
    Also add utility tests `segfault` and `throw_error_exception` that do
    exactly what they sound like.

--- test/runtests.jl
+++ test/runtests.jl
@@ -103,6 +103,12 @@ cd(dirname(@__FILE__)) do
     and Brokens from the worker and the full information about all errors and
     failures encountered running the tests. This information will be displayed
     as a summary at the end of the test run.
+
+    If a test failed, returning an `Exception` that is not a `RemoteException`,
+    it is likely the julia process running the test has encountered some kind
+    of internal error, such as a segfault.  The entire testset is marked as
+    Errored, and execution continues until the summary at the end of the test
+    run, where the test file is printed out as the "failed expression".
     =#
     o_ts = Base.Test.DefaultTestSet("Overall")
     Base.Test.push_testset(o_ts)
@@ -141,6 +147,16 @@ cd(dirname(@__FILE__)) do
                 Base.Test.record(o_ts, fake)
                 Base.Test.pop_testset()
             end
+        elseif isa(res[2][1], Exception)
+            # If this test raised an exception that is not a RemoteException, that means
+            # the test runner itself had some problem, so we may have hit a segfault
+            # or something similar.  Record this testset as Errored.
+            o_ts.anynonpass = true
+            fake = Base.Test.DefaultTestSet(res[1])
+            Base.Test.record(fake, Base.Test.Error(:test_error, res[1], res[2][1], []))
+            Base.Test.push_testset(fake)
+            Base.Test.record(o_ts, fake)
+            Base.Test.pop_testset()
         end
     end
     println()
---
+++ test/util/throw_error_exception.jl
@@ -0,0 +1,1 @@
+error("This purposefully dies")
