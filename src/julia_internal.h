a6a74636e61a169d8112394dd0e1d5c86f6a2f70
Jameson Nash <vtjnash@gmail.com>
2016-11-23 21:42:54 -0500

    remove jl_has_intrinsics bandaid

--- src/julia_internal.h
+++ src/julia_internal.h
@@ -370,7 +370,7 @@ jl_method_instance_t *jl_method_lookup(jl_methtable_t *mt, jl_value_t **args, si
 jl_value_t *jl_gf_invoke(jl_tupletype_t *types, jl_value_t **args, size_t nargs);
 
 JL_DLLEXPORT jl_datatype_t *jl_first_argument_datatype(jl_value_t *argtypes);
-int jl_has_intrinsics(jl_method_instance_t *li, jl_value_t *v, jl_module_t *m);
+int jl_has_intrinsics(jl_value_t *v);
 
 jl_value_t *jl_nth_slot_type(jl_value_t *sig, size_t i);
 void jl_compute_field_offsets(jl_datatype_t *st);
