7a3fd4e63c21f3e5b57ecfb6222db65070fb820e
kshyatt <kshyatt@physics.ucsb.edu>
2016-11-25 15:01:14 -0700

    Add doctest example for fill!

--- base/docs/helpdb/Base.jl
+++ base/docs/helpdb/Base.jl
@@ -15,6 +15,18 @@ systemerror
 Fill array `A` with the value `x`. If `x` is an object reference, all elements will refer to
 the same object. `fill!(A, Foo())` will return `A` filled with the result of evaluating
 `Foo()` once.
+
+```jldoctest
+julia> A = zeros(2,3)
+2×3 Array{Float64,2}:
+ 0.0  0.0  0.0
+ 0.0  0.0  0.0
+
+julia> fill!(A, 2.)
+2×3 Array{Float64,2}:
+ 2.0  2.0  2.0
+ 2.0  2.0  2.0
+```
 """
 fill!
 
