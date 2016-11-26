01339aa6973e0a674010aabfcd14646232931b12
Valentin Churavy <v.churavy@gmail.com>
2016-11-26 13:36:35 +0900

    LLVM debug builds creates big objects

--- deps/llvm.mk
+++ deps/llvm.mk
@@ -99,6 +99,9 @@ LLVM_FLAGS += --disable-assertions
 endif # LLVM_ASSERTIONS
 ifeq ($(LLVM_DEBUG), 1)
 LLVM_FLAGS += --disable-optimized --enable-debug-symbols --enable-keep-symbols
+ifeq ($(OS), WINNT)
+LLVM_CXXFLAGS += -Wa,-mbig-obj
+endif # OS == WINNT
 else
 LLVM_FLAGS += --enable-optimized
 endif # LLVM_DEBUG
