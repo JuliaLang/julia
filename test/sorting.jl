b7ac7ecd801973671c1eb95c1636049bf25f2859
Simon Byrne <simonbyrne@gmail.com>
2016-11-29 12:09:11 +0000

    Fix overflow in searchsorted (#19432)
    Fixes #19005.

--- test/sorting.jl
+++ test/sorting.jl
@@ -352,3 +352,7 @@ end
 # PR #18791
 @test sort([typemax(Int),typemin(Int)]) == [typemin(Int),typemax(Int)]
 @test sort([typemax(UInt),0]) == [0,typemax(UInt)]
+
+# issue #19005
+@test searchsortedfirst(0:256, 0x80) == 129
+@test searchsortedlast(0:256, 0x80) == 129
