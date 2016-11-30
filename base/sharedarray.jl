105af26176d78ad2d770e485889615c6854d1d3f
Amit Murthy <amit.murthy@gmail.com>
2016-11-30 21:41:24 +0530

    retrieve from node mapping the array

--- base/sharedarray.jl
+++ base/sharedarray.jl
@@ -344,7 +344,7 @@ end
 
 sub_1dim(S::SharedArray, pidx) = view(S.s, range_1dim(S, pidx))
 
-function init_loc_flds{T,N}(S::SharedArray{T,N})
+function init_loc_flds{T,N}(S::SharedArray{T,N}, empty_local=false)
     if myid() in S.pids
         S.pidx = findfirst(S.pids, myid())
         if isa(S.refs[1], Future)
@@ -357,8 +357,10 @@ function init_loc_flds{T,N}(S::SharedArray{T,N})
         S.loc_subarr_1d = sub_1dim(S, S.pidx)
     else
         S.pidx = 0
-        S.s = Array{T}(ntuple(d->0,N))
-        S.loc_subarr_1d = view(S.s, 1:0)
+        if empty_local
+            S.s = Array{T}(ntuple(d->0,N))
+        end
+        S.loc_subarr_1d = view(Array{T}(ntuple(d->0,N)), 1:0)
     end
 end
 
@@ -388,37 +390,25 @@ end
 
 function deserialize{T,N}(s::AbstractSerializer, t::Type{SharedArray{T,N}})
     S = invoke(deserialize, Tuple{AbstractSerializer, DataType}, s, t)
-    init_loc_flds(S)
+    init_loc_flds(S, true)
     S
 end
 
-function display_str(S::SharedArray)
-    if length(size(S)) == 1
-        size_str = string(size(S)[1], "-element")
-    else
-        size_str = join([string(x) for x in size(S)], "x")
-    end
-
-    string(size_str, " ", typeof(S))
-end
-
-unmapped_display_str(S::SharedArray) = string(display_str(S), ": <unmapped on node ", myid(), ">")
-
 function show(io::IO, S::SharedArray)
     if length(S.s) > 0
-        invoke(show, (IO, DenseArray,), io, S.s)
+        invoke(show, (IO, DenseArray), io, S)
     else
-        println(io, unmapped_display_str(S))
+        show(io, remotecall_fetch(sharr->sharr.s, S.pids[1], S))
     end
 end
 
 function show(io::IO, mime::MIME"text/plain", S::SharedArray)
     if length(S.s) > 0
-        invoke(show, (IO, MIME"text/plain", DenseArray,), io, mime, S.s)
-        println(io)
-        println(io, "mapped as a ", display_str(S))
+        invoke(show, (IO, MIME"text/plain", DenseArray), io, MIME"text/plain"(), S)
     else
-        println(io, unmapped_display_str(S))
+        # retrieve from the first worker mapping the array.
+        println(io, summary(S), ":")
+        showarray(io, remotecall_fetch(sharr->sharr.s, S.pids[1], S), false; header=false)
     end
 end
 
