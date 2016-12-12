02596f945a5bf392bbe4ce4e8f900868605f7122
Pablo Zubieta <pabloferz@yahoo.com.mx>
2016-12-11 23:18:58 -0600

    Fix the return type in broadcast

--- test/broadcast.jl
+++ test/broadcast.jl
@@ -341,14 +341,17 @@ end
 immutable StrangeType18623 end
 StrangeType18623(x) = x
 StrangeType18623(x,y) = (x,y)
-@test @inferred broadcast(StrangeType18623, 1:3) == [1,2,3]
-@test @inferred broadcast(StrangeType18623, 1:3, 4:6) == [(1,4),(2,5),(3,6)]
+@test @inferred(broadcast(StrangeType18623, 1:3)) == [1,2,3]
+@test @inferred(broadcast(StrangeType18623, 1:3, 4:6)) == [(1,4),(2,5),(3,6)]
 
 @test typeof(Int.(Number[1, 2, 3])) === typeof((x->Int(x)).(Number[1, 2, 3]))
 
-@test @inferred broadcast(CartesianIndex, 1:2) == [CartesianIndex(1), CartesianIndex(2)]
-@test @inferred broadcast(CartesianIndex, 1:2, 3:4) == [CartesianIndex(1,3), CartesianIndex(2,4)]
+@test @inferred(broadcast(CartesianIndex, 1:2)) == [CartesianIndex(1), CartesianIndex(2)]
+@test @inferred(broadcast(CartesianIndex, 1:2, 3:4)) == [CartesianIndex(1,3), CartesianIndex(2,4)]
 
 # Issue 18622
-@test @inferred muladd.([1.0], [2.0], [3.0])::Vector{Float64} == [5.0]
-@test @inferred tuple.(1:3, 4:6, 7:9)::Vector{Tuple{Int,Int,Int}} == [(1,4,7), (2,5,8), (3,6,9)]
+@test @inferred(broadcast(muladd, [1.0], [2.0], [3.0])) == [5.0]
+@test @inferred(broadcast(tuple, 1:3, 4:6, 7:9)) == [(1,4,7), (2,5,8), (3,6,9)]
+
+# 19419
+@test @inferred(broadcast(round, Int, [1])) == [1]
