9cd2f6b307c61ae29a76bb445a01a0415ee7e574
felixrehren <felixrehren@gmail.com>
2016-12-15 17:22:33 +0000

    vcat -> cat for rng

--- base/LineEdit.jl
+++ base/LineEdit.jl
@@ -918,7 +918,7 @@ function keymap{D<:Dict}(keymaps::Array{D})
 end
 
 const escape_defaults = merge!(
-    AnyDict(Char(i) => nothing for i=vcat(1:26, 28:31)), # Ignore control characters by default
+    AnyDict(Char(i) => nothing for i=cat(1:26, 28:31)), # Ignore control characters by default
     AnyDict( # And ignore other escape sequences by default
         "\e*" => nothing,
         "\e[*" => nothing,
