5f710b83c4cc9de84f68f47490800d3d2fd5c858
Alex Arslan <ararslan@comcast.net>
2016-12-15 21:26:12 -0800

    Fix specification of kwargs in deprecation

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -1135,17 +1135,23 @@ end)
 @deprecate cummax(A, dim=1) accumulate(max, A, dim=1)
 
 # #19598
-@deprecate sumabs(x)                 sum(abs, x)
-@deprecate sumabs(A, region)         sum(abs, A, region)
-@deprecate sumabs2(x)                sum(abs2, x)
-@deprecate sumabs2(A, region)        sum(abs2, A, region)
-@deprecate minabs(x)                 minimum(abs, x)
-@deprecate minabs(A, region)         minimum(abs, A, region)
-@deprecate maxabs(x)                 maximum(abs, x)
-@deprecate maxabs(A, region)         maximum(abs, A, region)
-@deprecate sumabs!(r, A; init=init)  sum!(abs, r, A; init=init)
-@deprecate sumabs2!(r, A; init=init) sum!(abs2, r, A; init=init)
-@deprecate minabs!(r, A; init=init)  minimum!(abs, r, A; init=init)
-@deprecate maxabs!(r, A; init=init)  maximum!(abs, r, A; init=init)
+@deprecate sumabs(x)          sum(abs, x)
+@deprecate sumabs(A, region)  sum(abs, A, region)
+@deprecate sumabs2(x)         sum(abs2, x)
+@deprecate sumabs2(A, region) sum(abs2, A, region)
+@deprecate minabs(x)          minimum(abs, x)
+@deprecate minabs(A, region)  minimum(abs, A, region)
+@deprecate maxabs(x)          maximum(abs, x)
+@deprecate maxabs(A, region)  maximum(abs, A, region)
+
+for (dep, f, op) in [(:sumabs!, :sum!, :abs),
+                     (:sumabs2!, :sum!, :abs2),
+                     (:minabs!, :minimum!, :abs),
+                     (:maxabs!, :maximum!, :abs)]
+    @eval function ($dep)(r, A; init=true)
+        Base.depwarn("$dep(r, A; init=$init) is deprecated, use $f($op, r, A; init=$init) instead.", Symbol($dep))
+        ($f)($op, r, A; init=init)
+    end
+end
 
 # End deprecations scheduled for 0.6
