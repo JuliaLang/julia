b71e9e0ad280ba9fcd0e7ba530f329d7e5312bd8
Jan Weidner <jw3126@gmail.com>
2016-12-21 21:40:20 +0100

    fix

--- base/array.jl
+++ base/array.jl
@@ -207,12 +207,13 @@ fill(v, dims::Integer...) = fill!(Array{typeof(v)}(dims...), v)
 
 for (fname, felt) in ((:zeros,:zero), (:ones,:one))
     @eval begin
+        # allow signature of similar
         $fname(a::AbstractArray, T::Type, dims::Tuple) = fill!(similar(a, T, dims), $felt(T))
+        $fname(a::AbstractArray, T::Type, dims...) = fill!(similar(a,T,dims...), $felt(T))
         $fname(a::AbstractArray, T::Type=eltype(a)) = fill!(similar(a,T), $felt(T))
+
         $fname(T::Type, dims::Tuple) = fill!(Array{T}(Dims(dims)...), $felt(T))
         $fname(dims::Tuple) = ($fname)(Float64, dims)
-
-        $fname(a::AbstractArray, T::Type, dims...) = fill!(similar(a,T,dims...), $felt(T))
         $fname(T::Type, dims...) = $fname(T, dims)
         $fname(dims...) = $fname(dims)
     end
