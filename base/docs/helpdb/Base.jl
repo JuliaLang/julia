fefbd3e66d30e3905941e63f19851be3b7515533
kshyatt <kshyatt@physics.ucsb.edu>
2016-12-24 12:03:05 -0800

    Add more doctests

--- base/docs/helpdb/Base.jl
+++ base/docs/helpdb/Base.jl
@@ -26,6 +26,17 @@ julia> fill!(A, 2.)
 2×3 Array{Float64,2}:
  2.0  2.0  2.0
  2.0  2.0  2.0
+
+julia> a = [1, 1, 1]; A = fill!(Vector{Vector{Int}}(3), a); a[1] = 2; A
+3-element Array{Array{Int64,1},1}:
+ [2,1,1]
+ [2,1,1]
+ [2,1,1]
+
+julia> x = 0; f() = (global x += 1; x); fill!(Vector{Int}(3), f())
+  1
+  1
+  1
 ```
 """
 fill!
