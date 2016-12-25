07dea4dcbb532d1bc1abefcf325f7c5a29cf8e74
Katharine Hyatt <kshyatt@users.noreply.github.com>
2016-12-24 23:38:16 -0800

    Add doctest example for fill! (#19422)
    * Add doctest example for fill!
    
    * Add more doctests

--- base/docs/helpdb/Base.jl
+++ base/docs/helpdb/Base.jl
@@ -15,6 +15,29 @@ systemerror
 Fill array `A` with the value `x`. If `x` is an object reference, all elements will refer to
 the same object. `fill!(A, Foo())` will return `A` filled with the result of evaluating
 `Foo()` once.
+
+```jldoctest
+julia> A = zeros(2,3)
+2×3 Array{Float64,2}:
+ 0.0  0.0  0.0
+ 0.0  0.0  0.0
+
+julia> fill!(A, 2.)
+2×3 Array{Float64,2}:
+ 2.0  2.0  2.0
+ 2.0  2.0  2.0
+
+julia> a = [1, 1, 1]; A = fill!(Vector{Vector{Int}}(3), a); a[1] = 2; A
+3-element Array{Array{Int64,1},1}:
+ [2,1,1]
+ [2,1,1]
+ [2,1,1]
+
+julia> x = 0; f() = (global x += 1; x); fill!(Vector{Int}(3), f())
+  1
+  1
+  1
+```
 """
 fill!
 
