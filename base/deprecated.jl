11d181b6300b72dc700f51021e2c06e49fcea975
Alex Arslan <ararslan@comcast.net>
2016-12-29 11:16:07 -0800

    Remove quadgk and friends from Base (#19741)
    * Remove quadgk and friends from Base
    
    * Remove trailing whitespace in NEWS.md
    
    [ci skip]

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -1175,4 +1175,11 @@ end
 @deprecate xor(A::AbstractArray, b::Number)         xor.(A, b)
 @deprecate xor(A::AbstractArray, B::AbstractArray)  xor.(A, B)
 
+# QuadGK moved to a package (#19741)
+function quadgk(args...)
+    error(string(quadgk, args, " has been moved to the package QuadGK.jl.\n",
+                 "Run Pkg.add(\"QuadGK\") to install QuadGK on Julia v0.6 and later, and then run `using QuadGK`."))
+end
+export quadgk
+
 # End deprecations scheduled for 0.6
--- test/math.jl
+++ test/math.jl
@@ -748,45 +748,6 @@ end
     @test isnan(zeta(complex(-Inf,0)))
 end
 
-@testset "quadgk" begin
-    @test quadgk(cos, 0,0.7,1)[1] ≈ sin(1)
-    @test quadgk(x -> exp(im*x), 0,0.7,1)[1] ≈ (exp(1im)-1)/im
-    @test quadgk(x -> exp(im*x), 0,1im)[1] ≈ -1im*expm1(-1)
-    @test_approx_eq_eps quadgk(cos, 0,BigFloat(1),order=40)[1] sin(BigFloat(1)) 1000*eps(BigFloat)
-    @test quadgk(x -> exp(-x), 0,0.7,Inf)[1] ≈ 1.0
-    @test quadgk(x -> exp(x), -Inf,0)[1] ≈ 1.0
-    @test quadgk(x -> exp(-x^2), -Inf,Inf)[1] ≈ sqrt(pi)
-    @test quadgk(x -> [exp(-x), exp(-2x)], 0, Inf)[1] ≈ [1,0.5]
-    @test quadgk(cos, 0,0.7,1, norm=abs)[1] ≈ sin(1)
-end
-
-module Test19626
-    using Base.Test
-
-    # Define a mock physical quantity type
-    immutable MockQuantity <: Number
-        val::Float64
-    end
-
-    # Following definitions needed for quadgk to work with MockQuantity
-    import Base: +, -, *, abs, isnan, isinf, isless
-    +(a::MockQuantity, b::MockQuantity) = MockQuantity(a.val+b.val)
-    -(a::MockQuantity, b::MockQuantity) = MockQuantity(a.val-b.val)
-    *(a::MockQuantity, b::Number) = MockQuantity(a.val*b)
-    *(a::MockQuantity, b::Bool) = MockQuantity(a.val*b) # resolve ambiguity
-    abs(a::MockQuantity) = MockQuantity(abs(a.val))
-    isnan(a::MockQuantity) = isnan(a.val)
-    isinf(a::MockQuantity) = isinf(a.val)
-    isless(a::MockQuantity, b::MockQuantity) = isless(a.val, b.val)
-
-    # isapprox only needed for test purposes
-    Base.isapprox(a::MockQuantity, b::MockQuantity) = isapprox(a.val, b.val)
-
-    # Test physical quantity-valued functions
-    @test quadgk(x->MockQuantity(x), 0.0, 1.0, abstol=MockQuantity(0.0))[1] ≈
-        MockQuantity(0.5)
-end
-
 @testset "subnormal flags" begin
     # Ensure subnormal flags functions don't segfault
     @test any(set_zero_subnormals(true) .== [false,true])
