05669736b9ded0a281058a33761ff57c9c7ae715
Simon Byrne <simonbyrne@gmail.com>
2016-12-30 15:04:20 +0000

    Fix deprecation error for 2 arg airy
    cf #18050

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -1284,9 +1284,9 @@ function _airy(k::Integer, z::Complex128)
     depwarn("`airy(k,x)` is deprecated, use `airyai(x)`, `airyaiprime(x)`, `airybi(x)` or `airybiprime(x)` instead.",:airy)
     id = Int32(k==1 || k==3)
     if k == 0 || k == 1
-        return _airy(z, id, Int32(1))
+        return Base.Math._airy(z, id, Int32(1))
     elseif k == 2 || k == 3
-        return _biry(z, id, Int32(1))
+        return Base.Math._biry(z, id, Int32(1))
     else
         throw(ArgumentError("k must be between 0 and 3"))
     end
@@ -1295,9 +1295,9 @@ function _airyx(k::Integer, z::Complex128)
     depwarn("`airyx(k,x)` is deprecated, use `airyaix(x)`, `airyaiprimex(x)`, `airybix(x)` or `airybiprimex(x)` instead.",:airyx)
     id = Int32(k==1 || k==3)
     if k == 0 || k == 1
-        return _airy(z, id, Int32(2))
+        return Base.Math._airy(z, id, Int32(2))
     elseif k == 2 || k == 3
-        return _biry(z, id, Int32(2))
+        return Base.Math._biry(z, id, Int32(2))
     else
         throw(ArgumentError("k must be between 0 and 3"))
     end
@@ -1308,6 +1308,8 @@ for afn in (:airy,:airyx)
     suf  = string(afn)[5:end]
     @eval begin
         function $afn(k::Integer, z::Complex128)
+            afn = $(QuoteNode(afn))
+            suf = $(QuoteNode(suf))
             depwarn("`$afn(k,x)` is deprecated, use `airyai$suf(x)`, `airyaiprime$suf(x)`, `airybi$suf(x)` or `airybiprime$suf(x)` instead.",$(QuoteNode(afn)))
             $_afn(k,z)
         end
@@ -1319,16 +1321,13 @@ for afn in (:airy,:airyx)
         $afn(k::Integer, x::AbstractFloat) = real($afn(k, complex(x)))
 
         function $afn{T<:Number}(k::Number, x::AbstractArray{T})
-            depwarn("`$afn(k::Number,x::AbstractArray)` is deprecated, use `airyai$suf.(x)`, `airyaiprime$suf.(x)`, `airybi$suf.(x)` or `airybiprime$suf.(x)` instead.",$(QuoteNode(afn)))
-            $_afn.(k,x)
+            $afn.(k,x)
         end
         function $afn{S<:Number}(k::AbstractArray{S}, x::Number)
-            depwarn("`$afn(k::AbstractArray,x::AbstractArray)` is deprecated, use `airyai$suf.(x)`, `airyaiprime$suf.(x)`, `airybi$suf.(x)` or `airybiprime$suf.(x)` instead.",$(QuoteNode(afn)))
-            $_afn.(k,x)
+            $afn.(k,x)
         end
         function $afn{S<:Number,T<:Number}(k::AbstractArray{S}, x::AbstractArray{T})
-            depwarn("`$afn(k::AbstractArray,x::AbstractArray)` is deprecated, use `airyai$suf.(x)`, `airyaiprime$suf.(x)`, `airybi$suf.(x)` or `airybiprime$suf.(x)` instead.",$(QuoteNode(afn)))
-            $_afn.(k,x)
+            $afn.(k,x)
         end
     end
 end
