26a2e9d3de046f7d5e80ae4f73a7a0c036814fd4
Steven G. Johnson <stevenj@mit.edu>
2017-01-03 19:05:39 -0500

    UTF8proc is not defined yet

--- base/char.jl
+++ base/char.jl
@@ -78,10 +78,9 @@ function show(io::IO, c::Char)
     return
 end
 
-using UTF8proc: category_abbrev, category_string
 function show(io::IO, ::MIME"text/plain", c::Char)
     show(io, c)
     u = UInt32(c)
     print(io, ": ", isascii(c) ? "ASCII/" : "", "Unicode U+", hex(u, u > 0xffff ? 6 : 4))
-    print(io, " (category ", category_abbrev(c), ": ", category_string(c), ")")
+    print(io, " (category ", UTF8proc.category_abbrev(c), ": ", UTF8proc.category_string(c), ")")
 end
