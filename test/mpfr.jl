0a5d624b8197e1c4083b5360c644529d285dcd2e
David Sanders <dpsanders@gmail.com>
2017-01-09 16:56:48 -0500

    Another find-replace error

--- test/mpfr.jl
+++ test/mpfr.jl
@@ -1,4 +1,4 @@
-# This file is a part of Julia. License is MIT: http://julialang.org/license" begin
+# This file is a part of Julia. License is MIT: http://julialang.org/license
 
 import Base.MPFR
 
