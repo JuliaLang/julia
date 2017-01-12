eb7302ac4aaa251520e7dedf8afcda509eb4468a
kshyatt <kshyatt@physics.ucsb.edu>
2017-01-12 12:56:58 -0800

    Add doc for newbase option

--- base/libgit2/libgit2.jl
+++ base/libgit2/libgit2.jl
@@ -465,6 +465,7 @@ end
 
 Attempt an automatic merge rebase of the current branch, from `upstream` if provided, or
 otherwise from the upstream tracking branch.
+`newbase` is the branch to rebase onto. By default this is `upstream`.
 
 If any conflicts arise which cannot be automatically resolved, the rebase will abort,
 leaving the repository and working tree in its original state, and the function will throw
