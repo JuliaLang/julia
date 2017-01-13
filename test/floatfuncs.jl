bc14714228d73d68010ab879f7f4f9917a26a42d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-01-13 15:59:46 -0500

    test isapprox with nans

--- test/floatfuncs.jl
+++ test/floatfuncs.jl
@@ -68,3 +68,10 @@ for elty in (Float32,Float64)
         @test round.(elty2,A) == fill(round(elty2,x),(10,10,10))
     end
 end
+
+# isapprox
+for nan in (Float16,Float32,Float64)
+    nan = elty(NaN)
+    @test isapprox(nan, nan) == false
+    @test isapprox(nan, nan, nans=true) == true7
+end
