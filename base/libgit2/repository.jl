57843d65160afff1c79b0c6a7159c74413c08546
Simon Byrne <simonbyrne@gmail.com>
2017-01-17 14:13:47 +0000

    Improves path handling in LibGit2, also defines show method for GitRepo. (#19664)
    * Improves path handling in LibGit2, also defines show method for GitRepo.
    
    Defines a `workdir` function, uses this to define `path` in a more principled way, and a show method.
    
    * Define LibGit2.path not in terms of working tree.
    
    * add posixpath function
    
    * spelling

--- base/libgit2/repository.jl
+++ base/libgit2/repository.jl
@@ -1,5 +1,10 @@
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
+"""
+    LibGit2.GitRepo(path::AbstractString)
+
+Opens a git repository at `path`.
+"""
 function GitRepo(path::AbstractString)
     repo_ptr_ptr = Ref{Ptr{Void}}(C_NULL)
     err = ccall((:git_repository_open, :libgit2), Cint,
@@ -115,14 +120,65 @@ function get{T <: GitObject}(::Type{T}, repo::GitRepo, oid::AbstractString)
     return get(T, repo, GitHash(oid), length(oid))
 end
 
+"""
+    LibGit2.gitdir(repo::GitRepo)
+
+Returns the location of the "git" files of `repo`:
+
+ - for normal repositories, this is the location of the `.git` folder.
+ - for bare repositories, this is the location of the repository itself.
+
+See also `workdir`, `path`
+"""
 function gitdir(repo::GitRepo)
     return unsafe_string(ccall((:git_repository_path, :libgit2), Cstring,
                         (Ptr{Void},), repo.ptr))
 end
 
+"""
+    LibGit2.workdir(repo::GitRepo)
+
+The location of the working directory of `repo`. This will throw an error for bare
+repositories.
+
+!!! note
+
+    This will typically be the parent directory of `gitdir(repo)`, but can be different in
+    some cases: e.g. if either the `core.worktree` configuration variable or the
+    `GIT_WORK_TREE` environment variable is set.
+
+See also `gitdir`, `path`
+"""
+function workdir(repo::GitRepo)
+    sptr = ccall((:git_repository_workdir, :libgit2), Cstring,
+                (Ptr{Void},), repo.ptr)
+    sptr == C_NULL && throw(GitError(Error.Object, Error.ERROR, "No working directory found."))
+    return unsafe_string(sptr)
+end
+
+"""
+    LibGit2.path(repo::GitRepo)
+
+The base file path of the repository `repo`.
+
+ - for normal repositories, this will typically be the parent directory of the ".git"
+   directory (note: this may be different than the working directory, see `workdir` for
+   more details).
+ - for bare repositories, this is the location of the "git" files.
+
+See also `gitdir`, `workdir`.
+"""
 function path(repo::GitRepo)
-    rpath = gitdir(repo)
-    return isbare(repo) ? rpath : splitdir(rpath[1:end-1])[1]*"/" # remove '.git' part
+    d = gitdir(repo)
+    if isdirpath(d)
+        d = dirname(d) # strip trailing separator
+    end
+    if isbare(repo)
+        return d
+    else
+        parent, base = splitdir(d)
+        return base == ".git" ? parent : d
+    end
 end
 
 function peel(obj::GitObject, obj_type::Cint)
@@ -208,4 +264,10 @@ function remotes(repo::GitRepo)
     @check ccall((:git_remote_list, :libgit2), Cint,
                   (Ptr{Void}, Ptr{Void}), out, repo.ptr)
     return convert(Vector{AbstractString}, out[])
+end
+
+function Base.show(io::IO, repo::GitRepo)
+    print(io, "LibGit2.GitRepo(")
+    show(io, path(repo))
+    print(io, ")")
 end
