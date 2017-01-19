6e056265a8cd3a67df49ff04f17a36a2b7102442
Stefan Karpinski <stefan@karpinski.org>
2017-01-19 13:43:15 -1000

    test find_in_path(name, nothing) since that's what using calls

--- test/loading.jl
+++ test/loading.jl
@@ -44,29 +44,25 @@ SAVED_LOAD_PATH = copy(LOAD_PATH)
 empty!(LOAD_PATH)
 dir = abspath(@__DIR__)
 push!(LOAD_PATH, dir)
-cd("..")
 
-let paddedname = "Atest_sourcepathZ"
-    filename = SubString(paddedname, 2, length(paddedname)-1)
-    @test Base.find_in_path(filename) == joinpath(dir, "test_sourcepath.jl")
-    LOAD_PATH[end] = GenericString(LOAD_PATH[end])
-    @test Base.find_in_path(filename) == joinpath(dir, "test_sourcepath.jl")
-end
+@test Base.find_in_path("test_sourcepath") == joinpath(dir, "test_sourcepath.jl")
+@test Base.find_in_path(GenericString("test_sourcepath")) == joinpath(dir, "test_sourcepath.jl")
+LOAD_PATH[end] = GenericString(LOAD_PATH[end])
+@test Base.find_in_path("test_sourcepath") == joinpath(dir, "test_sourcepath.jl")
 
 immutable CustomLoader
     path::String
 end
 push!(LOAD_PATH, CustomLoader("abc"))
 let name = randstring(20)
-    @test_throws ArgumentError Base.find_in_path(name)
+    @test_throws ArgumentError Base.find_in_path(name, nothing)
     Base.load_hook(prefix::CustomLoader, name::String, found) = joinpath(prefix.path, name)
-    @test Base.find_in_path(name) == joinpath("abc", name)
+    @test Base.find_in_path(name, nothing) == joinpath("abc", name)
 end
-@test Base.find_in_path("test_sourcepath") == joinpath("abc", "test_sourcepath")
+@test Base.find_in_path("test_sourcepath", nothing) == joinpath("abc", "test_sourcepath")
 Base.load_hook(prefix::CustomLoader, name::String, found::String) = found
-@test Base.find_in_path("test_sourcepath") == joinpath(dir, "test_sourcepath.jl")
+@test Base.find_in_path("test_sourcepath", nothing) == joinpath(dir, "test_sourcepath.jl")
 
-pop!(LOAD_PATH)
-cd(pop!(LOAD_PATH))
+empty!(LOAD_PATH)
 append!(LOAD_PATH, SAVED_LOAD_PATH)
 @test LOAD_PATH == SAVED_LOAD_PATH
