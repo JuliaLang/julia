325b1525db19f5c68717bcb936da30f4f4aa629a
Keno Fischer <keno@juliacomputing.com>
2017-01-24 14:13:01 -0500

    Improve inference for common reflection operations
    Make inference be able to infer the type constructor. This is a little
    tricky, since we don't really have a good way to represent this. It's
    not quite `Const(TypeVar(:T,lb,ub))`, because a) the bounds may only
    be accurate up to type equality and b) TypeVar is not `isbits`, so
    it's not actually egal to the value we'll have at runtime. Additionally
    Type{T} already has meaning as a partially constructed type (e.g. an
    unwrapped UnionAll), so using T::Type{T} runs the risk of confusing
    this with an unwrapped type. Instead, introduce a new Const-like type,
    `PartialTypeVar`, which carries the type var and also keeps track of
    whether the bounds were egal or only typequal (we don't take advantage
    of that yet, but we could in the future).
    
    Additionally, improve the inference of `typename` and allow constant folding
    of field accesses on TypeName and SimpleVectors (to be able to constant
    fold T.parameters[1]).

--- test/inference.jl
+++ test/inference.jl
@@ -588,3 +588,14 @@ f11015(a::AT11015) = g11015(Base.fieldtype(typeof(a), :f), true)
 g11015(::Type{Bool}, ::Bool) = 2.0
 @test Int <: Base.return_types(f11015, (AT11015,))[1]
 @test f11015(AT11015(true)) === 1
+
+# Inference for some type-level computation
+fUnionAll{T}(::Type{T}) = Type{S} where S <: T
+@inferred fUnionAll(Real) == Type{T} where T <: Real
+@inferred fUnionAll(Rational{T} where T <: AbstractFloat) == Type{T} where T<:(Rational{S} where S <: AbstractFloat)
+
+fComplicatedUnionAll{T}(::Type{T}) = Type{Tuple{S,rand() >= 0.5 ? Int : Float64}} where S <: T
+let pub = Base.parameter_upper_bound, x = fComplicatedUnionAll(Real)
+    @test pub(pub(x, 1), 1) == Real
+    @test pub(pub(x, 1), 2) == Int || pub(pub(x, 1), 2) == Float64
+end
