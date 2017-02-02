99de685c7d3079d9ea68d6061d643facb08b9b06
Jorge Fernández de Cossío Díaz <cossio@users.noreply.github.com>
2017-02-02 14:27:27 -0500

    Add count(itr) to count true elements
    Fix https://github.com/JuliaLang/julia/issues/20403.

--- base/reduce.jl
+++ base/reduce.jl
@@ -659,6 +659,19 @@ function count(pred, itr)
 end
 
 """
+    count(itr) -> Integer
+
+Count the number of `true` elements in `itr`.
+
+```jldoctest
+julia> count(4<=i<=6 for i in [2,3,4,5,6])
+3
+```
+"""
+count(itr) = count(identity, itr)
+
+
+"""
     countnz(A) -> Integer
 
 Counts the number of nonzero values in array `A` (dense or sparse). Note that this is not a constant-time operation.
