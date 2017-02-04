327ae99780ddb3b28e9d17ac0968cfd99017087b
Pablo Zubieta <pabloferz@yahoo.com.mx>
2017-02-04 15:39:28 -0600

    Use compact parametric syntax in base/i*.jl

--- base/iostream.jl
+++ base/iostream.jl
@@ -166,7 +166,7 @@ function unsafe_write(s::IOStream, p::Ptr{UInt8}, nb::UInt)
     return Int(ccall(:ios_write, Csize_t, (Ptr{Void}, Ptr{Void}, Csize_t), s.ios, p, nb))
 end
 
-function write{T,N,A<:Array}(s::IOStream, a::SubArray{T,N,A})
+function write{T,N}(s::IOStream, a::SubArray{T,N,<:Array})
     if !isbits(T) || stride(a,1)!=1
         return invoke(write, Tuple{Any, AbstractArray}, s, a)
     end
