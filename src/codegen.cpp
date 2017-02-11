1ac48a57fa453c67eb3a78b22827fb21a9509456
JKrehl <JKrehl@users.noreply.github.com>
2017-02-11 03:21:19 +0100

    parse environment arg JULIA_POLLY_ARGS into polly optimizers (#20432)
    create make flag USE_POLLY_OPENMP for openmp support
    
    replaced JULIA_POLLY_ARGS with preexisting JULIA_LLVM_ARGS
    
    moved env arg parsing

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -6418,7 +6418,7 @@ extern "C" void jl_init_codegen(void)
     const char *const argv_copyprop[] = {"", "-disable-copyprop"}; // llvm bug 21743
     cl::ParseCommandLineOptions(sizeof(argv_copyprop)/sizeof(argv_copyprop[0]), argv_copyprop, "disable-copyprop\n");
 #endif
-#ifdef JL_DEBUG_BUILD
+#if defined(JL_DEBUG_BUILD) || defined(USE_POLLY)
     cl::ParseEnvironmentOptions("Julia", "JULIA_LLVM_ARGS");
 #endif
 
