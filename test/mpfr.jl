9dbbe8b6eaede3b3ab25d92118edf32e661ab59c
David P. Sanders <dpsanders@gmail.com>
2017-02-13 15:13:47 -0600

    Change string output for BigFloat Inf and NaN (#19939)

--- test/mpfr.jl
+++ test/mpfr.jl
@@ -879,3 +879,9 @@ for prec in (10, 100, 1000)
         end
     end
 end
+
+setprecision(256) do
+    @test string(big(Inf)) == "BigFloat(Inf, 256)"
+    @test string(big(-Inf)) == "BigFloat(-Inf, 256)"
+    @test string(big(NaN)) == "BigFloat(NaN, 256)"
+end
