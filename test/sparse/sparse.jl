769d37b1def1920311900ac0ae0e84d6422aa72a
Andreas Noack <andreasnoackjensen@gmail.com>
2017-02-14 10:25:43 -0500

    Check that sparse matrix is valid before constructing a CHOLMOD.Sparse (#20464)
    Don't use length of underlying arrays to query sizes
    
    Rename variables in zero based constructors

--- test/sparse/sparse.jl
+++ test/sparse/sparse.jl
@@ -1738,3 +1738,16 @@ end
     @test length(c.rowval) == 9
     @test length(c.nzval) == 9
 end
+
+@testset "check buffers" for n in 1:3
+    colptr = [1,2,3,4]
+    rowval = [1,2,3]
+    nzval1  = ones(0)
+    nzval2  = ones(3)
+    A = SparseMatrixCSC(n, n, colptr, rowval, nzval1)
+    @test nnz(A) == n
+    @test_throws BoundsError A[n,n]
+    A = SparseMatrixCSC(n, n, colptr, rowval, nzval2)
+    @test nnz(A) == n
+    @test A      == eye(n)
+end
