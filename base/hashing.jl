d1398c3dbb0f43e371a70bd0652fe3f29bfe9cd5
Stefan Karpinski <stefan@karpinski.org>
2017-02-23 15:03:01 -0500

    hash_uint(3h - object_id(x)) is a somewhat better hash
    If we'd used `3h - object_id(x)` previously, the original issue
    would never have come up since `hash(a, hash(b, hash(c, h)))`
    wouldn't have produced `3A - 3B + 3C - h` but would instead have given
    the value `27h -9A - 3B - C`, which is asymmetrical in `a` and `c`.
    Passing the result to `hash_uint` fixes that issue since that function
    is non-linear, but defense in depth, right? We may as well do the
    same operations in such a way that the passed-through value gets the
    most operations done on it.

--- base/hashing.jl
+++ base/hashing.jl
@@ -7,7 +7,7 @@ hash(w::WeakRef, h::UInt) = hash(w.value, h)
 
 ## hashing general objects ##
 
-hash(x::ANY, h::UInt) = hash_uint(3*object_id(x) - h)
+hash(x::ANY, h::UInt) = hash_uint(3h - object_id(x))
 
 ## core data hashing functions ##
 
