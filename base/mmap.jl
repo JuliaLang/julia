f1adcc8c53d81d096e4f93878467857c954e54ae
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-02-23 17:01:27 +0800

    mmap: fix MAP_ANONYMOUS flags value for bsd family

--- base/mmap.jl
+++ base/mmap.jl
@@ -27,7 +27,7 @@ const PROT_READ     = Cint(1)
 const PROT_WRITE    = Cint(2)
 const MAP_SHARED    = Cint(1)
 const MAP_PRIVATE   = Cint(2)
-const MAP_ANONYMOUS = Cint(is_apple() ? 0x1000 : 0x20)
+const MAP_ANONYMOUS = Cint(is_bsd() ? 0x1000 : 0x20)
 const F_GETFL       = Cint(3)
 
 gethandle(io::IO) = fd(io)
