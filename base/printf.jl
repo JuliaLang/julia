078835c1c1a10164e376a9bcd4b7df1bd72070a8
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-02-24 09:15:05 -0500

    Document rounding away from zero (#20775)

--- base/printf.jl
+++ base/printf.jl
@@ -1196,7 +1196,9 @@ end
 
 Print `args` using C `printf()` style format specification string, with some caveats:
 `Inf` and `NaN` are printed consistently as `Inf` and `NaN` for flags `%a`, `%A`,
-`%e`, `%E`, `%f`, `%F`, `%g`, and `%G`.
+`%e`, `%E`, `%f`, `%F`, `%g`, and `%G`. Furthermore, if a floating point number is
+equally close to the numeric values of two possible output strings, the output
+string further away from zero is chosen.
 
 Optionally, an `IOStream`
 may be passed as the first argument to redirect output.
@@ -1206,6 +1208,9 @@ may be passed as the first argument to redirect output.
 ```jldoctest
 julia> @printf("%f %F %f %F\\n", Inf, Inf, NaN, NaN)
 Inf Inf NaN NaN\n
+
+julia> @printf "%.0f %.1f %f\\n" 0.5 0.025 -0.0078125
+1 0.0 -0.007813
 ```
 """
 macro printf(args...)
