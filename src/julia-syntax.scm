09e5b1b85ed1b4034b7f22c1778a1aca1bc66de3
Andrew Keller <andrew.keller.09@gmail.com>
2017-03-04 21:59:05 -0800

    Use top instead of Base.

--- src/julia-syntax.scm
+++ src/julia-syntax.scm
@@ -2057,7 +2057,7 @@
 
                  ((and (eq? f '^) (length= e 4) (integer? (cadddr e)))
                   (expand-forms
-                   `(call (|.| Base (quote literal_pow)) ,(caddr e) (call (core apply_type) (top Val) ,(cadddr e)) ^)))
+                   `(call (top literal_pow) ,(caddr e) (call (core apply_type) (top Val) ,(cadddr e)) ^)))
 
                  ((and (eq? f '*) (length= e 4))
                   (expand-transposed-op
