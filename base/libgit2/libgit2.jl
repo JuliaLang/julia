5555ca6ad7fd5193c657f7d0f1da101f8d4afcbf
kshyatt <kshyatt@physics.ucsb.edu>
2017-03-07 15:04:55 -0800

    [docathon] Add examples for isdiff/isdirty

--- base/libgit2/libgit2.jl
+++ base/libgit2/libgit2.jl
@@ -95,6 +95,17 @@ Checks if there have been any changes to tracked files in the working tree (if
 `cached=false`) or the index (if `cached=true`).
 `pathspecs` are the specifications for options for the diff.
 
+# Example
+```julia
+repo = LibGit2.GitRepo(repo_path)
+LibGit2.isdirty(repo) #should be false
+open(joinpath(repo_path, new_file), "a") do f
+    println(f, "here's my cool new file")
+end
+LibGit2.isdirty(repo) #now true
+LibGit2.isdirty(repo, new_file) #now true
+```
+
 Equivalent to `git diff-index HEAD [-- <pathspecs>]`.
 """
 isdirty(repo::GitRepo, paths::AbstractString=""; cached::Bool=false) =
@@ -106,6 +117,16 @@ isdirty(repo::GitRepo, paths::AbstractString=""; cached::Bool=false) =
 Checks if there are any differences between the tree specified by `treeish` and the
 tracked files in the working tree (if `cached=false`) or the index (if `cached=true`).
 `pathspecs` are the specifications for options for the diff.
+
+# Example
+```julia
+repo = LibGit2.GitRepo(repo_path)
+LibGit2.isdiff(repo, "HEAD") #should be false
+open(joinpath(repo_path, new_file), "a") do f
+    println(f, "here's my cool new file")
+end
+LibGit2.isdiff(repo, "HEAD") #now true
+```
 
 Equivalent to `git diff-index <treeish> [-- <pathspecs>]`.
 """
