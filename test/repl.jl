JuliaLang/julia

commit dcb529b0cd79af9631b4e4943ed6359ec7fee860

fix more UnicodeErrors in REPL search
fix #13819

base/REPL.jl
@@ -507,12 +507,14 @@ function history_move_prefix(s::LineEdit.PrefixSearchState,
         if (idx == max_idx) || (startswith(hist.history[idx], prefix) && (hist.history[idx] != cur_response || hist.modes[idx] != LineEdit.mode(s)))
             m = history_move(s, hist, idx)
             if m == :ok
-                if isempty(prefix)
+                if idx == max_idx
+                    # on resuming the in-progress edit, leave the cursor where the user last had it
+                elseif isempty(prefix)
                     # on empty prefix search, move cursor to the end
                     LineEdit.move_input_end(s)
                 else
                     # otherwise, keep cursor at the prefix position as a visual cue
-                    seek(LineEdit.buffer(s), length(prefix))
+                    seek(LineEdit.buffer(s), sizeof(prefix))
                 end
                 LineEdit.refresh_line(s)
                 return :ok
@@ -551,7 +553,7 @@ function history_search(hist::REPLHistoryProvider, query_buffer::IOBuffer, respo
     if 1 <= searchstart <= endof(response_str)
         match = searchfunc(response_str, searchdata, searchstart)
         if match != 0:-1
-            seek(response_buffer, first(match)-1)
+            seek(response_buffer, prevind(response_str, first(match)))
             return true
         end
     end
@@ -564,7 +566,7 @@ function history_search(hist::REPLHistoryProvider, query_buffer::IOBuffer, respo
         if match != 0:-1 && h != response_str && haskey(hist.mode_mapping, hist.modes[idx])
             truncate(response_buffer, 0)
             write(response_buffer, h)
-            seek(response_buffer, first(match)-1)
+            seek(response_buffer, prevind(response_str, first(match)))
             hist.cur_idx = idx
             return true
         end

test/repl.jl
@@ -158,7 +158,7 @@ end
 
 function AddCustomMode(repl)
     # Custom REPL mode tests
-    foobar_mode = LineEdit.Prompt("Test";
+    foobar_mode = LineEdit.Prompt("TestΠ";
         prompt_prefix="\e[38;5;166m",
         prompt_suffix=Base.text_colors[:white],
         on_enter = s->true,
@@ -286,15 +286,33 @@ begin
     @test ps.prefix == ""
     @test ps.parent == repl_mode
     @test LineEdit.input_string(ps) == "2 + 2"
+    @test position(LineEdit.buffer(s)) == 5
     LineEdit.history_prev_prefix(ps, hp, "")
     @test ps.parent == shell_mode
     @test LineEdit.input_string(ps) == "ls"
+    @test position(LineEdit.buffer(s)) == 2
     LineEdit.history_prev_prefix(ps, hp, "sh")
     @test ps.parent == repl_mode
     @test LineEdit.input_string(ps) == "shell"
+    @test position(LineEdit.buffer(s)) == 2
     LineEdit.history_next_prefix(ps, hp, "sh")
     @test ps.parent == repl_mode
     @test LineEdit.input_string(ps) == "wip"
+    @test position(LineEdit.buffer(s)) == 0
+    LineEdit.move_input_end(s)
+    LineEdit.history_prev_prefix(ps, hp, "é")
+    @test ps.parent == repl_mode
+    @test LineEdit.input_string(ps) == "éé"
+    @test position(LineEdit.buffer(s)) == sizeof("é") > 1
+    LineEdit.history_prev_prefix(ps, hp, "é")
+    @test ps.parent == repl_mode
+    @test LineEdit.input_string(ps) == "é"
+    @test position(LineEdit.buffer(s)) == sizeof("é")
+    LineEdit.history_next_prefix(ps, hp, "zzz")
+    @test ps.parent == repl_mode
+    @test LineEdit.input_string(ps) == "wip"
+    @test position(LineEdit.buffer(s)) == 3
+    LineEdit.accept_result(s, prefix_mode)
 
     # Test that searching backwards puts you into the correct mode and
     # skips invalid modes.
@@ -305,6 +323,7 @@ begin
     LineEdit.accept_result(s, histp)
     @test LineEdit.mode(s) == shell_mode
     @test buffercontents(LineEdit.buffer(s)) == "ls"
+    @test position(LineEdit.buffer(s)) == 0
 
     # Test that searching for `ll` actually matches `ll` after
     # both letters are types rather than jumping to `shell`
@@ -317,6 +336,7 @@ begin
     LineEdit.accept_result(s, histp)
     @test LineEdit.mode(s) == shell_mode
     @test buffercontents(LineEdit.buffer(s)) == "ll"
+    @test position(LineEdit.buffer(s)) == 0
 
     # Test that searching backwards with a one-letter query doesn't
     # return indefinitely the same match (#9352)
@@ -328,16 +348,19 @@ begin
     LineEdit.accept_result(s, histp)
     @test LineEdit.mode(s) == repl_mode
     @test buffercontents(LineEdit.buffer(s)) == "shell"
+    @test position(LineEdit.buffer(s)) == 4
 
     # Test that searching backwards doesn't skip matches (#9352)
     # (for a search with multiple one-byte characters, or UTF-8 characters)
     LineEdit.enter_search(s, histp, true)
     write(ss.query_buffer, "é") # matches right-most "é" in "éé"
     LineEdit.update_display_buffer(ss, ss)
+    @test position(ss.query_buffer) == sizeof("é")
     LineEdit.history_next_result(s, ss) # matches left-most "é" in "éé"
     LineEdit.update_display_buffer(ss, ss)
     LineEdit.accept_result(s, histp)
     @test buffercontents(LineEdit.buffer(s)) == "éé"
+    @test position(LineEdit.buffer(s)) == 0
 
     # Issue #7551
     # Enter search mode and try accepting an empty result
@@ -348,6 +371,7 @@ begin
     LineEdit.accept_result(s, histp)
     @test LineEdit.mode(s) == cur_mode
     @test buffercontents(LineEdit.buffer(s)) == ""
+    @test position(LineEdit.buffer(s)) == 0
 
     # Test that new modes can be dynamically added to the REPL and will
     # integrate nicely
@@ -361,12 +385,14 @@ begin
     LineEdit.accept_result(s, histp)
     @test LineEdit.mode(s) == foobar_mode
     @test buffercontents(LineEdit.buffer(s)) == "ls"
+    @test position(LineEdit.buffer(s)) == 0
 
     # Try the same for prefix search
     LineEdit.history_next(s, hp)
     LineEdit.history_prev_prefix(ps, hp, "l")
     @test ps.parent == foobar_mode
     @test LineEdit.input_string(ps) == "ls"
+    @test position(LineEdit.buffer(s)) == 1
 
     # Try entering search mode while in custom repl mode
     LineEdit.enter_search(s, custom_histp, true)
@@ -374,7 +400,7 @@ end
 # Simple non-standard REPL tests
 if @unix? true : (Base.windows_version() >= Base.WINDOWS_VISTA_VER)
     stdin_write, stdout_read, stdout_read, repl = fake_repl()
-    panel = LineEdit.Prompt("test";
+    panel = LineEdit.Prompt("testπ";
         prompt_prefix="\e[38;5;166m",
         prompt_suffix=Base.text_colors[:white],
         on_enter = s->true)

base/LineEdit.jl
@@ -35,7 +35,6 @@ end
 
 type Prompt <: TextInterface
     prompt
-    first_prompt
     # A string or function to be printed before the prompt. May not change the length of the prompt.
     # This may be used for changing the color, issuing other terminal escape codes, etc.
     prompt_prefix
@@ -215,14 +214,13 @@ function refresh_multi_line(termbuf::TerminalBuffer, terminal::UnixTerminal, buf
 
     miscountnl = @windows ? (isa(Terminals.pipe_reader(terminal), Base.TTY) && !Base.ispty(Terminals.pipe_reader(terminal))) : false
     plength = strwidth(prompt)
-    pslength = length(prompt.data)
     # Now go through the buffer line by line
     while cur_row == 0 || endswith(l, '\n')
         l = readline(buf)
         cur_row += 1
         # We need to deal with UTF8 characters. Since the IOBuffer is a bytearray, we just count bytes
         llength = strwidth(l)
-        slength = length(l.data)
+        slength = sizeof(l)
         if cur_row == 1 # First line
             if line_pos < slength
                 num_chars = strwidth(l[1:line_pos])
@@ -492,7 +490,7 @@ function edit_insert(s::PromptState, c)
     str = string(c)
     edit_insert(s.input_buffer, str)
     if !('\n' in str) && eof(s.input_buffer) &&
-        ((position(s.input_buffer) + length(s.p.prompt) + sizeof(str) - 1) < width(terminal(s)))
+        ((position(s.input_buffer) + sizeof(s.p.prompt) + sizeof(str) - 1) < width(terminal(s)))
         #Avoid full update
         write(terminal(s), str)
     else
@@ -1573,7 +1571,6 @@ end
 const default_keymap_dict = keymap([default_keymap, escape_defaults])
 
 function Prompt(prompt;
-    first_prompt = prompt,
     prompt_prefix = "",
     prompt_suffix = "",
     keymap_dict = default_keymap_dict,
@@ -1584,13 +1581,13 @@ function Prompt(prompt;
     hist = EmptyHistoryProvider(),
     sticky = false)
 
-    Prompt(prompt, first_prompt, prompt_prefix, prompt_suffix, keymap_dict, keymap_func_data,
+    Prompt(prompt, prompt_prefix, prompt_suffix, keymap_dict, keymap_func_data,
         complete, on_enter, on_done, hist, sticky)
 end
 
 run_interface(::Prompt) = nothing
 
-init_state(terminal, prompt::Prompt) = PromptState(terminal, prompt, IOBuffer(), InputAreaState(1, 1), length(prompt.prompt))
+init_state(terminal, prompt::Prompt) = PromptState(terminal, prompt, IOBuffer(), InputAreaState(1, 1), #=indent(spaces)=#strwidth(prompt.prompt))
 
 function init_state(terminal, m::ModalInterface)
     s = MIState(m, m.modes[1], false, Dict{Any,Any}())

commit 6a24a567117b1d8346842bcd7b1fea1f76cf6404

fix over-eager Unicode fixes
this is converting between zero-based file-position and one-based string indexign
not adjusting Unicode codepoints (bug introduced by dcb529b0cd7)

fix #19153

base/REPL.jl
@@ -551,8 +551,8 @@ function history_search(hist::REPLHistoryProvider, query_buffer::IOBuffer, respo
     response_str = String(response_buffer)
 
     # Alright, first try to see if the current match still works
-    a = position(response_buffer) + 1
-    b = min(endof(response_str), prevind(response_str, a + sizeof(searchdata)))
+    a = position(response_buffer) + 1 # position is zero-indexed
+    b = min(endof(response_str), prevind(response_str, a + sizeof(searchdata))) # ensure that b is valid
 
     !skip_current && searchdata == response_str[a:b] && return true
 
@@ -565,7 +565,7 @@ function history_search(hist::REPLHistoryProvider, query_buffer::IOBuffer, respo
     if 1 <= searchstart <= endof(response_str)
         match = searchfunc(response_str, searchdata, searchstart)
         if match != 0:-1
-            seek(response_buffer, prevind(response_str, first(match)))
+            seek(response_buffer, first(match) - 1)
             return true
         end
     end
@@ -578,7 +578,7 @@ function history_search(hist::REPLHistoryProvider, query_buffer::IOBuffer, respo
         if match != 0:-1 && h != response_str && haskey(hist.mode_mapping, hist.modes[idx])
             truncate(response_buffer, 0)
             write(response_buffer, h)
-            seek(response_buffer, prevind(h, first(match)))
+            seek(response_buffer, first(match) - 1)
             hist.cur_idx = idx
             return true
         end

test/repl.jl
@@ -242,6 +242,9 @@ fakehistory = """
 # time: 2014-06-30 17:32:59 EDT
 # mode: shell
 \tll
+# time: 2014-06-30 99:99:99 EDT
+# mode: julia
+\tx ΔxΔ
 # time: 2014-06-30 17:32:49 EDT
 # mode: julia
 \t1 + 1
@@ -363,6 +366,8 @@ begin
     LineEdit.enter_search(s, histp, true)
     write(ss.query_buffer, "l")
     LineEdit.update_display_buffer(ss, ss)
+    @test buffercontents(ss.response_buffer) == "ll"
+    @test position(ss.response_buffer) == 1
     write(ss.query_buffer, "l")
     LineEdit.update_display_buffer(ss, ss)
     LineEdit.accept_result(s, histp)
@@ -425,6 +430,20 @@ begin
     @test ps.parent == foobar_mode
     @test LineEdit.input_string(ps) == "ls"
     @test position(LineEdit.buffer(s)) == 1
+
+    # Some Unicode handling testing
+    LineEdit.history_prev(s, hp)
+    LineEdit.enter_search(s, histp, true)
+    write(ss.query_buffer, "x")
+    LineEdit.update_display_buffer(ss, ss)
+    @test buffercontents(ss.response_buffer) == "x ΔxΔ"
+    @test position(ss.response_buffer) == 4
+    write(ss.query_buffer, " ")
+    LineEdit.update_display_buffer(ss, ss)
+    LineEdit.accept_result(s, histp)
+    @test LineEdit.mode(s) == repl_mode
+    @test buffercontents(LineEdit.buffer(s)) == "x ΔxΔ"
+    @test position(LineEdit.buffer(s)) == 0
 
     # Try entering search mode while in custom repl mode
     LineEdit.enter_search(s, custom_histp, true)