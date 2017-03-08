e8d2a6c464c3c55d84b0d3e3a058496606d078f9
Sacha Verweij <Sacha0@users.noreply.github.com>
2017-03-08 04:00:00 -0800

    Remove unused import from sparse/sparse.jl. (#20938)
    Base.Broadcast.broadcast_indices is no longer used in SparseArrays outside of SparseArrays.HigherOrderFns, so remove the unused import.

--- base/sparse/sparse.jl
+++ base/sparse/sparse.jl
@@ -26,8 +26,6 @@ import Base: @get!, acos, acosd, acot, acotd, acsch, asech, asin, asind, asinh,
     rotl90, rotr90, round, scale!, setindex!, similar, size, transpose, tril,
     triu, vec, permute!, map, map!
 
-import Base.Broadcast: broadcast_indices
-
 export AbstractSparseArray, AbstractSparseMatrix, AbstractSparseVector,
     SparseMatrixCSC, SparseVector, blkdiag, dense, droptol!, dropzeros!, dropzeros,
     issparse, nonzeros, nzrange, rowvals, sparse, sparsevec, spdiagm, speye, spones,
