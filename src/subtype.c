JuliaLang/julia

commit 6fb2c67e9e4461d48cbfe57bd30849a6615b06a1

fix #20998
This was a case where an unknown typevar was incorrectly inferred to have the
value `Any`.

src/subtype.c
@@ -1286,9 +1286,9 @@ static jl_value_t *finish_unionall(jl_value_t *res, jl_varbinding_t *vb, jl_sten
 
     if (vb->right && e->envidx < e->envsz) {
         jl_value_t *oldval = e->envout[e->envidx];
-        if (!varval)
+        if (!varval || (!is_leaf_bound(varval) && !var_occurs_invariant(res, vb->var, 0)))
             e->envout[e->envidx] = (jl_value_t*)vb->var;
-        else if (!oldval || !jl_is_typevar(oldval) || !jl_is_long(varval))
+        else if (!(oldval && jl_is_typevar(oldval) && jl_is_long(varval)))
             e->envout[e->envidx] = varval;
     }
 

test/subtype.jl
@@ -853,6 +853,10 @@ function test_intersection()
     @testintersect(Tuple{Ref{Pair{p2,T2}}, Pair{p1,Pair}} where T2 where p2 where p1,
                    Tuple{Ref{Pair{p3,T3}}, Pair{p3}} where T3 where p3,
                    Tuple{Ref{Pair{p1,T2}}, Pair{p1,Pair}} where T2 where p1)
+
+    # issue #20998
+    _, E = intersection_env(Tuple{Int,Any,Any}, Tuple{T,T,S} where {T,S})
+    @test length(E) == 2 && E[1] == Int && isa(E[2], TypeVar)
 end
 
 function test_intersection_properties()

commit 08182714e09e3b2243f6f82fa578f54876e2d331

fix #21028, static parameter value incorrectly set to typevar
This issue was caused by 6fb2c67e9e4461d48cbfe57bd30849a6615b06a1.

src/subtype.c
@@ -1292,7 +1292,7 @@ static jl_value_t *finish_unionall(jl_value_t *res, jl_varbinding_t *vb, jl_sten
 
     if (vb->right && e->envidx < e->envsz) {
         jl_value_t *oldval = e->envout[e->envidx];
-        if (!varval || (!is_leaf_bound(varval) && !var_occurs_invariant(res, vb->var, 0)))
+        if (!varval || (!is_leaf_bound(varval) && !vb->occurs_inv))
             e->envout[e->envidx] = (jl_value_t*)vb->var;
         else if (!(oldval && jl_is_typevar(oldval) && jl_is_long(varval)))
             e->envout[e->envidx] = varval;

test/subtype.jl
@@ -857,6 +857,9 @@ function test_intersection()
     # issue #20998
     _, E = intersection_env(Tuple{Int,Any,Any}, Tuple{T,T,S} where {T,S})
     @test length(E) == 2 && E[1] == Int && isa(E[2], TypeVar)
+    _, E = intersection_env(Tuple{Dict{Int,Type}, Type, Any},
+                            Tuple{Dict{K,V}, Any, Int} where {K,V})
+    @test E[2] == Type
 end
 
 function test_intersection_properties()