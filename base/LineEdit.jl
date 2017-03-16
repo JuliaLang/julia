cfcbdc92050c78a89b72431959a3dc4b4dac3ae5
Randy Lai <randy.cs.lai@gmail.com>
2017-03-16 19:14:04 -0400

    use QuoteNode to prevent parameters being expanded

--- base/LineEdit.jl
+++ base/LineEdit.jl
@@ -1577,7 +1577,14 @@ function run_interface(terminal, m::ModalInterface)
             @static if is_unix(); ccall(:jl_repl_raise_sigtstp, Cint, ()); end
             buf, ok, suspend = prompt!(terminal, m, s)
         end
-        eval(Expr(:call, mode(state(s, s.current_mode)).on_done, s, buf, ok))
+        eval(Main,
+            Expr(:body,
+                Expr(:return,
+                     Expr(:call,
+                          mode(state(s, s.current_mode)).on_done,
+                          QuoteNode(s),
+                          QuoteNode(buf),
+                          QuoteNode(ok)))))
     end
 end
 
