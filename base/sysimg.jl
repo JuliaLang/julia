f012092cd7b01b00bc1e90263abf71babc65b0fb
Curtis Vogt <curtis.vogt@gmail.com>
2017-03-23 16:08:13 -0500

    Remove need for readuntil file

--- base/sysimg.jl
+++ base/sysimg.jl
@@ -412,7 +412,6 @@ import .Dates: Date, DateTime, DateFormat, @dateformat_str, now
 include("sparse/sparse.jl")
 using .SparseArrays
 
-include("readuntil.jl")
 include("asyncmap.jl")
 
 include("distributed/Distributed.jl")
