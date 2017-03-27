86d56239bb3a9cc6e6687958c578578b957dd8a2
Tony Kelman <tony@kelman.net>
2017-03-27 09:41:00 -0400

    Fix doctest line numbers

--- base/channels.jl
+++ base/channels.jl
@@ -190,8 +190,8 @@ julia> take!(c)
 julia> put!(c,1);
 ERROR: foo
 Stacktrace:
- [1] check_channel_state(::Channel{Any}) at ./channels.jl:125
- [2] put!(::Channel{Any}, ::Int64) at ./channels.jl:256
+ [1] check_channel_state(::Channel{Any}) at ./channels.jl:127
+ [2] put!(::Channel{Any}, ::Int64) at ./channels.jl:258
 ```
 """
 function bind(c::Channel, task::Task)
