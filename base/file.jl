1295b72fc15ad236cc13994d0fcd0f10c21cea31
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-04-11 17:13:44 -0400

    Refer to `mkpath` from `mkdir` docs (#21343)

--- base/file.jl
+++ base/file.jl
@@ -85,7 +85,10 @@ cd(f::Function) = cd(f, homedir())
     mkdir(path::AbstractString, mode::Unsigned=0o777)
 
 Make a new directory with name `path` and permissions `mode`. `mode` defaults to `0o777`,
-modified by the current file creation mask.
+modified by the current file creation mask. This function never creates more than one
+directory. If the directory already exists, or some intermediate directories do not exist,
+this function throws an error. See [`mkpath`](@ref) for a function which creates all
+required intermediate directories.
 """
 function mkdir(path::AbstractString, mode::Unsigned=0o777)
     @static if is_windows()
