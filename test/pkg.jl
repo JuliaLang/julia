7bb938e9fc56f8252af253650f731fb263986bdd
Curtis Vogt <curtis.vogt@gmail.com>
2017-04-14 11:04:42 -0500

    Remove need for temp_rel_pkg_dir

--- test/pkg.jl
+++ test/pkg.jl
@@ -18,10 +18,11 @@ function capture_stdout(f::Function)
 end
 
 
-function temp_pkg_dir(fn::Function, remove_tmp_dir::Bool=true)
+function temp_pkg_dir(fn::Function, tmp_dir=joinpath(tempdir(), randstring()),
+        remove_tmp_dir::Bool=true)
+
     # Used in tests below to set up and tear down a sandboxed package directory
-    const tmpdir = joinpath(tempdir(),randstring())
-    withenv("JULIA_PKGDIR" => tmpdir) do
+    withenv("JULIA_PKGDIR" => tmp_dir) do
         @test !isdir(Pkg.dir())
         try
             Pkg.init()
@@ -29,7 +30,7 @@ function temp_pkg_dir(fn::Function, remove_tmp_dir::Bool=true)
             Pkg.resolve()
             fn()
         finally
-            remove_tmp_dir && rm(tmpdir, recursive=true)
+            remove_tmp_dir && rm(tmp_dir, recursive=true)
         end
     end
 end
@@ -569,28 +570,11 @@ let io = IOBuffer()
     @test !contains(String(take!(io)), "backtrace()")
 end
 
-
-function temp_rel_pkg_dir(fn::Function, remove_tmp_dir::Bool=true)
-    # Used in tests below to set up and tear down a sandboxed package directory
-    cd(tempdir()) do
-        const tmpdir = randstring()
-        withenv("JULIA_PKGDIR" => tmpdir) do
-            @test !isdir(Pkg.dir())
-            try
-                Pkg.init()
-                @test isdir(Pkg.dir())
-                Pkg.resolve()
-                fn()
-            finally
-                remove_tmp_dir && rm(tmpdir, recursive=true)
-            end
-        end
-    end
-end
-
 @testset "Relative path operations" begin
-    temp_rel_pkg_dir() do
-        Pkg.add("Example")
-        @test [keys(Pkg.installed())...] == ["Example"]
+    cd(tempdir()) do
+        temp_pkg_dir(randstring()) do
+            Pkg.add("Example")
+            @test [keys(Pkg.installed())...] == ["Example"]
+        end
     end
 end
