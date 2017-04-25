5cdff93ebafdb87d6f7a447e4ad22d5d0fa75667
Mosè Giordano <giordano@users.noreply.github.com>
2017-04-25 05:33:02 +0200

    Fix typo: promt → prompt (#21538)

--- base/REPL.jl
+++ base/REPL.jl
@@ -685,8 +685,8 @@ end
 repl_filename(repl, hp::REPLHistoryProvider) = "REPL[$(length(hp.history)-hp.start_idx)]"
 repl_filename(repl, hp) = "REPL"
 
-const JL_PROMT_PASTE = Ref(true)
-enable_promtpaste(v::Bool) = JL_PROMT_PASTE[] = v
+const JL_PROMPT_PASTE = Ref(true)
+enable_promptpaste(v::Bool) = JL_PROMPT_PASTE[] = v
 
 function setup_interface(repl::LineEditREPL; hascolor = repl.hascolor, extra_repl_keymap = Dict{Any,Any}[])
     ###
@@ -836,7 +836,7 @@ function setup_interface(repl::LineEditREPL; hascolor = repl.hascolor, extra_rep
             firstline = true
             isprompt_paste = false
             while !done(input, oldpos) # loop until all lines have been executed
-                if JL_PROMT_PASTE[]
+                if JL_PROMPT_PASTE[]
                     # Check if the next statement starts with "julia> ", in that case
                     # skip it. But first skip whitespace
                     while input[oldpos] in ('\n', ' ', '\t')
