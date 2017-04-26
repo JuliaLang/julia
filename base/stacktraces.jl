829c4380c97817096236b40ac4bb1054857cf37a
Tony Kelman <tony@kelman.net>
2017-04-26 19:18:09 -0400

    Add a test for profile deserialization
    xref #21025 which is fixed on master, and a one-character fix on release-0.5,
    but should be tested on both branches

--- base/stacktraces.jl
+++ base/stacktraces.jl
@@ -37,7 +37,7 @@ Stack information representing execution context, with the following fields:
 
   True if the code is from an inlined frame.
 
-- `pointer::Int64`
+- `pointer::UInt64`
 
   Representation of the pointer to the execution context as returned by `backtrace`.
 
