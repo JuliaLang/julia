a9fd67d21ed1816cfc108390cf3853377f2b915c
kshyatt <kshyatt@physics.ucsb.edu>
2017-05-02 16:02:06 -0700

    More examples, clarification for git docs

--- base/libgit2/libgit2.jl
+++ base/libgit2/libgit2.jl
@@ -204,6 +204,23 @@ end
 
 Returns `true` if `a`, a [`GitHash`](@ref) in string form, is an ancestor of
 `b`, a [`GitHash`](@ref) in string form.
+
+# Example
+
+```julia
+julia> repo = LibGit2.GitRepo(repo_path);
+
+julia> LibGit2.add!(repo, test_file1);
+
+julia> commit_oid1 = LibGit2.commit(repo, "commit1");
+
+julia> LibGit2.add!(repo, test_file2);
+
+julia> commit_oid2 = LibGit2.commit(repo, "commit2");
+
+julia> LibGit2.is_ancestor_of(string(commit_oid1), string(commit_oid2), repo)
+true
+```
 """
 function is_ancestor_of(a::AbstractString, b::AbstractString, repo::GitRepo)
     A = revparseid(repo, a)
@@ -336,6 +353,7 @@ end
 
 Checkout a new git branch in the `repo` repository. `commit` is the [`GitHash`](@ref),
 in string form, which will be the start of the new branch.
+If `commit` is an empty string, the current HEAD will be used.
 
 The keyword arguments are:
   * `track::AbstractString=""`: the name of the
@@ -348,6 +366,13 @@ The keyword arguments are:
     finishes the branch head will be set as the HEAD of `repo`.
 
 Equivalent to `git checkout [-b|-B] <branch_name> [<commit>] [--track <track>]`.
+
+# Example
+
+```julia
+repo = LibGit2.GitRepo(repo_path)
+LibGit2.branch!(repo, "new_branch", set_head=false)
+```
 """
 function branch!(repo::GitRepo, branch_name::AbstractString,
                  commit::AbstractString = ""; # start point
