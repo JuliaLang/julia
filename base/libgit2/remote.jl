0914c8991593ea24819eb8b3891199cbf5b81aec
kshyatt <kshyatt@physics.ucsb.edu>
2017-05-02 15:55:15 -0700

    Add examples for name and for url

--- base/libgit2/remote.jl
+++ base/libgit2/remote.jl
@@ -75,6 +75,19 @@ end
     url(rmt::GitRemote)
 
 Get the URL of a remote git repository.
+
+# Example
+
+```julia
+julia> repo_url = "https://github.com/JuliaLang/Example.jl";
+
+julia> repo = LibGit2.clone(cache_repo, "test_directory");
+
+julia> remote = LibGit2.GitRemote(repo, "origin", repo_url);
+
+julia> url(remote)
+"https://github.com/JuliaLang/Example.jl"
+```
 """
 function url(rmt::GitRemote)
     url_ptr = ccall((:git_remote_url, :libgit2), Cstring, (Ptr{Void},), rmt.ptr)
@@ -88,6 +101,19 @@ end
 Get the name of a remote repository, for instance `"origin"`.
 If the remote is anonymous (see [`GitRemoteAnon`](@ref))
 the name will be an empty string `""`.
+
+# Example
+
+```julia
+julia> repo_url = "https://github.com/JuliaLang/Example.jl";
+
+julia> repo = LibGit2.clone(cache_repo, "test_directory");
+
+julia> remote = LibGit2.GitRemote(repo, "origin", repo_url);
+
+julia> name(remote)
+"origin"
+```
 """
 function name(rmt::GitRemote)
     name_ptr = ccall((:git_remote_name, :libgit2), Cstring, (Ptr{Void},), rmt.ptr)
