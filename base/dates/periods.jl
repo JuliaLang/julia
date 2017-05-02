c36310a812187b2824a300aece18364f2110a365
Tony Kelman <tony@kelman.net>
2017-05-02 14:55:26 -0400

    change a few example code blocks to doctests
    and fix quoted output of a Cmd object

--- base/dates/periods.jl
+++ base/dates/periods.jl
@@ -199,7 +199,7 @@ Construct a `CompoundPeriod` from a `Vector` of `Period`s. All `Period`s of the 
 will be added together.
 
 # Examples
-```julia
+```jldoctest
 julia> Dates.CompoundPeriod(Dates.Hour(12), Dates.Hour(13))
 25 hours
 
@@ -232,7 +232,7 @@ Reduces the `CompoundPeriod` into its canonical form by applying the following r
   (eg. `Hour(1) - Day(1)` becomes `-Hour(23)`)
 
 # Examples
-```julia
+```jldoctest
 julia> Dates.canonicalize(Dates.CompoundPeriod(Dates.Hour(12), Dates.Hour(13)))
 1 day, 1 hour
 
