1e22552c5a454a15c86e5d519fb352065fe34757
Alexey Stukalov <astukalov@gmail.com>
2017-05-04 21:19:07 +0200

    ntuple(f, -1) throws ArgumentError

--- base/tuple.jl
+++ base/tuple.jl
@@ -105,7 +105,7 @@ julia> ntuple(i -> 2*i, 4)
 ```
 """
 function ntuple(f::F, n::Integer) where F
-    t = n <= 0  ? () :
+    t = n == 0  ? () :
         n == 1  ? (f(1),) :
         n == 2  ? (f(1), f(2)) :
         n == 3  ? (f(1), f(2), f(3)) :
@@ -120,7 +120,11 @@ function ntuple(f::F, n::Integer) where F
     return t
 end
 
-_ntuple(f, n) = (@_noinline_meta; ([f(i) for i = 1:n]...))
+function _ntuple(f, n)
+    @_noinline_meta
+    (n >= 0) || throw(ArgumentError(string("tuple length should be ≥0, got ", n)))
+    ([f(i) for i = 1:n]...)
+end
 
 # inferrable ntuple
 ntuple(f, ::Type{Val{0}}) = (@_inline_meta; ())
