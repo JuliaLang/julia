3465b4f7a2f78ca1a33e86fa4763a747bf938ce6
Jan Weidner <jw3126@gmail.com>
2017-05-04 22:38:17 +0200

    fix

--- base/multidimensional.jl
+++ base/multidimensional.jl
@@ -581,8 +581,8 @@ end
 function _cumsum!(out, v, axis, ::ArithmeticRounds)
     axis == 1 ? accumulate_pairwise!(+, out, v) : copy!(out, v)
 end
-function _cumsum(out, v, axis, ::ArithmeticUnknown)
-    _cumsum(out, v, axis, ArithmeticRounds())
+function _cumsum!(out, v, axis, ::ArithmeticUnknown)
+    _cumsum!(out, v, axis, ArithmeticRounds())
 end
 function _cumsum!(out, v, axis, ::TypeArithmetic)
     axis == 1 ? accumulate!(+, out, v) : copy!(out, v)
