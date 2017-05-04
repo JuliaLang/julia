ecae3a726f5e75a8d972c5fc4674e2cc6046772a
Curtis Vogt <curtis.vogt@gmail.com>
2017-05-04 15:32:08 -0500

    Word wrap at 92-characters
    (cherry picked from commit b1d0897dddd3ecbf19a55958d26cdc260ad707f8)
    ref #21702

--- base/socket.jl
+++ base/socket.jl
@@ -451,8 +451,8 @@ Set UDP socket options.
 
 * `multicast_loop`: loopback for multicast packets (default: `true`).
 * `multicast_ttl`: TTL for multicast packets (default: `nothing`).
-* `enable_broadcast`: flag must be set to `true` if socket will be used for broadcast messages,
-  or else the UDP system will return an access error (default: `false`).
+* `enable_broadcast`: flag must be set to `true` if socket will be used for broadcast
+  messages, or else the UDP system will return an access error (default: `false`).
 * `ttl`: Time-to-live of packets sent on the socket (default: `nothing`).
 """
 function setopt(sock::UDPSocket; multicast_loop = nothing, multicast_ttl=nothing, enable_broadcast=nothing, ttl=nothing)
