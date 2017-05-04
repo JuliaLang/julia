9a7d44024166acb4806444c39eab15ed85ace047
GregPlowman <GregPlowman@users.noreply.github.com>
2017-05-04 21:11:52 +1000

    Update compile.jl
    Test for Issue #19960

--- test/compile.jl
+++ test/compile.jl
@@ -462,6 +462,56 @@ let dir = mktempdir()
     end
 end
 
+let dir = mktempdir()
+    try
+        insert!(LOAD_PATH, 1, dir)
+        insert!(Base.LOAD_CACHE_PATH, 1, dir)
+
+        loaded_modules = Channel{Symbol}(32)
+        callback = (mod::Symbol) -> put!(loaded_modules, mod)
+        push!(Base.package_callbacks, callback)
+
+        Test1_module = :Teste4095a81
+        Test2_module = :Teste4095a82
+        Test3_module = :Teste4095a83
+
+        write(joinpath(dir, "$(Test1_module).jl"),
+              """
+              module $(Test1_module)
+                  __precompile__(true)
+              end
+              """)
+
+        Base.compilecache("$(Test1_module)")
+        write(joinpath(dir, "$(Test2_module).jl"),
+              """
+              module $(Test2_module)
+                  __precompile__(true)
+                  using $(Test1_module)
+              end
+              """)
+        Base.compilecache("$(Test2_module)")
+        @test !Base.isbindingresolved(Main, Test2_module)
+        Base.require(Test2_module)
+        @test Base.isbindingresolved(Main, Test2_module)
+        @test take!(loaded_modules) == Test1_module
+        @test take!(loaded_modules) == Test2_module
+        write(joinpath(dir, "$(Test3_module).jl"),
+              """
+              module $(Test3_module)
+                  using $(Test3_module)
+              end
+              """)
+        Base.require(Test3_module)
+        @test take!(loaded_modules) == Test3_module
+    finally
+        pop!(Base.package_callbacks)
+        splice!(Base.LOAD_CACHE_PATH, 1)
+        splice!(LOAD_PATH, 1)
+        rm(dir, recursive=true)
+    end
+end
+
 let module_name = string("a",randstring())
     insert!(LOAD_PATH, 1, pwd())
     file_name = string(module_name, ".jl")
@@ -472,6 +522,63 @@ let module_name = string("a",randstring())
     @test isa(eval(Main, Symbol(module_name)), Module)
     deleteat!(LOAD_PATH,1)
     rm(file_name)
+end
+
+# Issue #19960
+let
+    # ideally this would test with workers on a remote host that does not have access to the master node filesystem for loading
+    # can simulate this for local workers by using relative load paths on master node that are not valid on workers
+    # so addprocs before changing directory to temp directory, otherwise workers will inherit temp working directory
+
+    test_workers = addprocs(1)
+
+    temp_path = mktempdir()
+    cd(temp_path)
+    load_path = mktempdir(temp_path)
+    load_cache_path = mktempdir(temp_path)
+    unshift!(LOAD_PATH, basename(load_path))
+    unshift!(Base.LOAD_CACHE_PATH, basename(load_cache_path))
+
+    ModuleA = :Issue19960A
+    ModuleB = :Issue19960B
+
+    write(joinpath(load_path, "$ModuleA.jl"),
+        """
+        __precompile__(true)
+        module $ModuleA
+            export f
+            f() = myid()
+        end
+        """)
+
+    write(joinpath(load_path, "$ModuleB.jl"),
+        """
+        __precompile__(true)
+        module $ModuleB
+            using $ModuleA
+            export g
+            g() = f()
+        end
+        """)
+
+    try
+        @eval using $ModuleB
+
+        @test g() == 1
+        for wid in test_workers
+            @test remotecall_fetch(g, wid) == wid
+        end
+
+    catch
+        @test false
+
+    finally
+        shift!(LOAD_PATH)
+        shift!(Base.LOAD_CACHE_PATH)
+        cd()
+        rm(temp_path, recursive=true)
+        rmprocs(test_workers)
+    end
 end
 
 end # !withenv
