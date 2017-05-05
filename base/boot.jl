01d202a085feb485b829820889292602578e8545
Jameson Nash <vtjnash@gmail.com>
2017-05-05 10:35:33 -0400

    better static printing of typemap entries

--- base/boot.jl
+++ base/boot.jl
@@ -370,10 +370,10 @@ end
 
 show(io::IO, x::ANY) = ccall(:jl_static_show, Void, (Ptr{Void}, Any), io_pointer(io), x)
 print(io::IO, x::Char) = ccall(:jl_uv_putc, Void, (Ptr{Void}, Char), io_pointer(io), x)
-print(io::IO, x::String) = write(io, x)
+print(io::IO, x::String) = (write(io, x); nothing)
 print(io::IO, x::ANY) = show(io, x)
 print(io::IO, x::ANY, a::ANY...) = (print(io, x); print(io, a...))
-println(io::IO) = write(io, 0x0a) # 0x0a = '\n'
+println(io::IO) = (write(io, 0x0a); nothing) # 0x0a = '\n'
 println(io::IO, x::ANY...) = (print(io, x...); println(io))
 
 show(a::ANY) = show(STDOUT, a)
