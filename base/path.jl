a54bbc857e88d0b8d9136099a8c4e7221df916bc
Sacha Verweij <Sacha0@users.noreply.github.com>
2017-05-12 16:04:42 -0700

    Add link to libuv's uv_os_homedir documentation to homedir's docstring. (#21806)

--- base/path.jl
+++ base/path.jl
@@ -53,6 +53,11 @@ splitdrive(path::AbstractString)
     homedir() -> AbstractString
 
 Return the current user's home directory.
+
+!!! note
+    `homedir` determines the home directory via `libuv`'s `uv_os_homedir`. For details
+    (for example on how to specify the home directory via environment variables), see the
+    [`uv_os_homedir` documentation](http://docs.libuv.org/en/v1.x/misc.html#c.uv_os_homedir).
 """
 function homedir()
     path_max = 1024
