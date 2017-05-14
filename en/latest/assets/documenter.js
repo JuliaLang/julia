121bc7656f0067a6d97549717dd23b6de52ca95c
autodocs <autodocs>
2017-05-14 13:41:03 +0000

    build based on 7a00007

--- en/latest/assets/documenter.js
+++ en/latest/assets/documenter.js
@@ -7,17 +7,15 @@
 
 requirejs.config({
     paths: {
-        'jquery': 'https://code.jquery.com/jquery-3.1.0.js?',
+        'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min',
         'jqueryui': 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min',
-        'mathjax': 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML',
-        'highlight': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/highlight.min',
-        'highlight-julia': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/languages/julia.min',
+        'mathjax': 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_HTML',
+        'highlight': 'highlightjs/highlight',
     },
     shim: {
         'mathjax' : {
             exports: "MathJax"
-        },
-        'highlight-julia': ['highlight']
+        }
     }
 });
 
@@ -50,7 +48,7 @@ require(['mathjax'], function(MathJax) {
     });
 })
 
-require(['jquery', 'highlight', 'highlight-julia'], function($, hljs) {
+require(['jquery', 'highlight'], function($, hljs) {
     $(document).ready(function() {
         hljs.initHighlighting();
     })
