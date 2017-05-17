49abbf3b24b9658d53f0be9203795f7043b3e475
Gollor <UristMcGollor@gmail.com>
2017-05-17 23:56:43 +0300

    Added @vtjnash code except for relative path in dependency tracker

--- base/loading.jl
+++ base/loading.jl
@@ -263,10 +263,15 @@ const _require_dependencies = Any[] # a list of (path, mtime) tuples that are th
 const _track_dependencies = Ref(false) # set this to true to track the list of file dependencies
 function _include_dependency(_path::AbstractString)
     prev = source_path(nothing)
-    if myid() != 1 && prev === nothing
-        prev = remotecall_fetch(abspath, 1, ".")
+    if prev === nothing
+        if myid() == 1
+            path = abspath(_path)
+        else
+            path = joinpath(remotecall_fetch(abspath, 1, "."), _path)
+        end
+    else
+        path = joinpath(dirname(prev), _path)
     end
-    path = (prev === nothing) ? abspath(_path) : joinpath(dirname(prev), _path)
     if myid() == 1 && _track_dependencies[]
         apath = abspath(path)
         push!(_require_dependencies, (apath, mtime(apath)))
