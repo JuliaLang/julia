f8b85191799d07e9551499eb3c827e8a4f353bfa
Fredrik Ekre <fredrik.ekre@chalmers.se>
2017-05-28 14:41:59 +0200

    Document number types (#22067)
    * document π, e, γ and φ
    
    * add type-documentation for number types
    
    - replace the uninformative list in doc/src/stdlib/numbers.md with these new docstrings
    - move constructor docstring for BigInt from helpdb/Base.jl to base/gmp.jl
    - add a doctest for BigInt constructor
    
    * add supertypes to number type docs
    
    * move irrational to concrete types
    
    (cherry picked from commit f5ed68fbad3ffcd4ef9710dff21e28d623bd9d8b)

--- base/irrationals.jl
+++ base/irrationals.jl
@@ -2,6 +2,11 @@
 
 ## general machinery for irrational mathematical constants
 
+"""
+    Irrational <: Real
+
+Irrational number type.
+"""
 struct Irrational{sym} <: Real end
 
 show(io::IO, x::Irrational{sym}) where {sym} = print(io, "$sym = $(string(float(x))[1:15])...")
@@ -152,7 +157,7 @@ julia> pi
 π = 3.1415926535897...
 ```
 """
-const pi = π
+π, const pi = π
 
 """
     e
@@ -165,7 +170,7 @@ julia> e
 e = 2.7182818284590...
 ```
 """
-const eu = e
+e, const eu = e
 
 """
     γ
@@ -178,7 +183,7 @@ julia> eulergamma
 γ = 0.5772156649015...
 ```
 """
-const eulergamma = γ
+γ, const eulergamma = γ
 
 """
     φ
@@ -191,7 +196,7 @@ julia> golden
 φ = 1.6180339887498...
 ```
 """
-const golden = φ
+φ, const golden = φ
 
 """
     catalan
