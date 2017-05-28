4a2c275dfd22714ea92b0cde9009ccd1f2abc8b9
Rafael Fourquet <fourquet.rafael@gmail.com>
2017-05-28 09:32:15 +0200

    printf.jl: add missing import of GMP

--- base/printf.jl
+++ base/printf.jl
@@ -1,7 +1,7 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 module Printf
-using Base.Grisu
+using Base: Grisu, GMP
 export @printf, @sprintf
 
 ### printf formatter generation ###
