7bd4a5ff928eaccdf4a5730965ee4e1ac4248608
Simon Byrne <simonbyrne@gmail.com>
2017-05-30 09:13:55 -0500

    make note

--- base/libgit2/reference.jl
+++ base/libgit2/reference.jl
@@ -162,13 +162,14 @@ end
     peel([T,] ref::GitReference)
 
 Recursively peel `ref` until an object of type `T` is obtained. If no `T` is provided,
-then `ref` will be peeled until an object other than a `GitTag` is obtained.
+then `ref` will be peeled until an object other than a [`GitTag`](@ref) is obtained.
 
 - A `GitTag` will be peeled to the object it references.
-- A `GitCommit` will be peeled to a `GitTree`.
+- A [`GitCommit`](@ref) will be peeled to a [`GitTree`](@ref).
 
-Note that only annotated tags can be peeled to `GitTag` objects. Lightweight tags (the
-default) are references under `refs/tags/` which point directly to `GitCommit` objects.
+!!! note
+    Only annotated tags can be peeled to `GitTag` objects. Lightweight tags (the default)
+    are references under `refs/tags/` which point directly to `GitCommit` objects.
 """
 function peel{T<:GitObject}(::Type{T}, ref::GitReference)
     obj_ptr_ptr = Ref{Ptr{Void}}(C_NULL)
