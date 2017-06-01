f7a23d9c63dafc873a4ad0a77b0464a771b2fd0b
Simon Byrne <simonbyrne@gmail.com>
2017-06-01 15:55:29 -0500

    Add note on lightweight tags. (#22117)
    * Add note on lightweight tags.
    
    Address #22102.
    
    * make note

--- base/libgit2/reference.jl
+++ base/libgit2/reference.jl
@@ -162,10 +162,14 @@ end
     peel([T,] ref::GitReference)
 
 Recursively peel `ref` until an object of type `T` is obtained. If no `T` is provided,
-then `ref` will be peeled until an object other than a `GitTag` is obtained.
+then `ref` will be peeled until an object other than a [`GitTag`](@ref) is obtained.
 
 - A `GitTag` will be peeled to the object it references.
-- A `GitCommit` will be peeled to a `GitTree`.
+- A [`GitCommit`](@ref) will be peeled to a [`GitTree`](@ref).
+
+!!! note
+    Only annotated tags can be peeled to `GitTag` objects. Lightweight tags (the default)
+    are references under `refs/tags/` which point directly to `GitCommit` objects.
 """
 function peel{T<:GitObject}(::Type{T}, ref::GitReference)
     obj_ptr_ptr = Ref{Ptr{Void}}(C_NULL)
