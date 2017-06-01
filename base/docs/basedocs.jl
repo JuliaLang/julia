9e8f3ae7a1687d5a884ba25fac4080dca0e260f9
Jeff Bezanson <jeff.bezanson@gmail.com>
2017-06-01 15:39:58 -0400

    add help text for the `where` keyword (#22177)
    (cherry picked from commit ab771ef3b4cfecf4bd4c899b87d613dab45f5040)

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -652,6 +652,36 @@ kw"mutable struct"
 kw"@__LINE__"
 
 """
+The `where` keyword creates a type that is an iterated union of other types, over all
+values of some variable. For example `Vector{T} where T<:Real` includes all `Vector`s
+where the element type is some kind of `Real` number.
+
+The variable bound defaults to `Any` if it is omitted:
+
+    Vector{T} where T    # short for `where T<:Any`
+
+Variables can also have lower bounds:
+
+    Vector{T} where T>:Int
+    Vector{T} where Int<:T<:Real
+
+There is also a concise syntax for nested `where` expressions. For example, this:
+
+    Pair{T, S} where S<:Array{T} where T<:Number
+
+can be shortened to:
+
+    Pair{T, S} where {T<:Number, S<:Array{T}}
+
+This form is often found on method signatures.
+
+Note that in this form, the variables are listed outermost-first. This matches the
+order in which variables are substituted when a type is "applied" to parameter values
+using the syntax `T{p1, p2, ...}`.
+"""
+kw"where"
+
+"""
     ans
 
 A variable referring to the last computed value, automatically set at the interactive prompt.
