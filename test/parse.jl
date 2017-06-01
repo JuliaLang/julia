9e537207cdb79b5c59c1bc72979a1fd308c1aef5
Steven G. Johnson <stevenj@alum.mit.edu>
2017-06-01 09:47:39 -0400

    add missing subscripts

--- test/parse.jl
+++ test/parse.jl
@@ -1232,6 +1232,7 @@ end === (3, String)
 @test parse("3 +̂ 4") == Expr(:call, :+̂, 3, 4)
 @test parse("3 +̂′ 4") == Expr(:call, :+̂′, 3, 4)
 @test parse("3 +⁽¹⁾ 4") == Expr(:call, :+⁽¹⁾, 3, 4)
+@test parse("3 +₍₀₎ 4") == Expr(:call, :+₍₀₎, 3, 4)
 @test Base.operator_precedence(:+̂) == Base.operator_precedence(:+)
 
 # issue #19351
