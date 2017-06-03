2c16404a1fa02c17230fa43d3aa35c1d0e0a156c
Dominique Orban <dominique.orban@gmail.com>
2017-06-03 17:44:00 -0400

    fix formatting

--- base/sparse/symmetric.jl
+++ base/sparse/symmetric.jl
@@ -8,10 +8,8 @@ end
 (*)(A::Symmetric{TA,SparseMatrixCSC{TA,S}}, x::StridedVecOrMat{Tx}) where {TA,S,Tx} = A_mul_B(A, x)
 
 function A_mul_B!(α::Number, A::Symmetric{TA,SparseMatrixCSC{TA,S}}, B::StridedVecOrMat, β::Number, C::StridedVecOrMat) where {TA,S}
-
     A.data.n == size(B, 1) || throw(DimensionMismatch())
     A.data.m == size(C, 1) || throw(DimensionMismatch())
-
     A.uplo == 'U' ? A_mul_B_U_kernel!(α, A, B, β, C) : A_mul_B_L_kernel!(α, A, B, β, C)
 end
 
@@ -26,7 +24,6 @@ function A_mul_B(A::Symmetric{TA,SparseMatrixCSC{TA,S}}, B::StridedMatrix{Tx}) w
 end
 
 function A_mul_B_U_kernel!(α::Number, A::Symmetric{TA,SparseMatrixCSC{TA,S}}, B::StridedVecOrMat, β::Number, C::StridedVecOrMat) where {TA,S}
-
     colptr = A.data.colptr
     rowval = A.data.rowval
     nzval = A.data.nzval
@@ -51,26 +48,25 @@ function A_mul_B_U_kernel!(α::Number, A::Symmetric{TA,SparseMatrixCSC{TA,S}}, B
 end
 
 function A_mul_B_L_kernel!(α::Number, A::Symmetric{TA,SparseMatrixCSC{TA,S}}, B::StridedVecOrMat, β::Number, C::StridedVecOrMat) where {TA,S}
-
-  colptr = A.data.colptr
-  rowval = A.data.rowval
-  nzval = A.data.nzval
-  if β != 1
-      β != 0 ? scale!(C, β) : fill!(C, zero(eltype(C)))
-  end
-  @inbounds for k = 1 : size(C, 2)
-      @inbounds for col = 1 : A.data.n
-          αxj = α * B[col, k]
-          tmp = TA(0)
-          @inbounds for j = (colptr[col + 1] - 1) : -1 : colptr[col]
-              row = rowval[j]
-              row < col && break
-              a = nzval[j]
-              C[row, k] += a * αxj
-              row == col || (tmp += a * B[row, k])
-          end
-          C[col, k] += α * tmp
-      end
-  end
-  C
+    colptr = A.data.colptr
+    rowval = A.data.rowval
+    nzval = A.data.nzval
+    if β != 1
+        β != 0 ? scale!(C, β) : fill!(C, zero(eltype(C)))
+    end
+    @inbounds for k = 1 : size(C, 2)
+        @inbounds for col = 1 : A.data.n
+            αxj = α * B[col, k]
+            tmp = TA(0)
+            @inbounds for j = (colptr[col + 1] - 1) : -1 : colptr[col]
+                row = rowval[j]
+                row < col && break
+                a = nzval[j]
+                C[row, k] += a * αxj
+                row == col || (tmp += a * B[row, k])
+            end
+            C[col, k] += α * tmp
+        end
+    end
+    C
 end
