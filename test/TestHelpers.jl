4238f54f16fca937df520c10828b4c77549a2cba
Jack Devine <devja964@student.otago.ac.nz>
2017-06-09 15:19:56 +1200

    Added resize! and done methods for OffsetArray

--- test/TestHelpers.jl
+++ test/TestHelpers.jl
@@ -216,6 +216,8 @@ end
     @inbounds deleteat!(parent(A), first_idx:last_idx)
 end
 
+Base.resize!(A::OffsetVector, nl::Integer) = resize!(A.parent, nl)
+Base.done(A::OffsetVector, ind::Integer) = done(A.parent, ind - A.offsets[1])
 # Computing a shifted index (subtracting the offset)
 offset{N}(offsets::NTuple{N,Int}, inds::NTuple{N,Int}) = _offset((), offsets, inds)
 _offset(out, ::Tuple{}, ::Tuple{}) = out
