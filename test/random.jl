08ad6e90dd40037bb17759a8e9eab3a1da05c0d5
Rafael Fourquet <fourquet.rafael@gmail.com>
2017-06-17 12:19:44 +0200

    adjust indentation in for loop, take 2 (tkelman)

--- test/random.jl
+++ test/random.jl
@@ -551,14 +551,14 @@ let b = ['0':'9';'A':'Z';'a':'z']
         @test length(randstring(rng..., 20)) == 20
         @test issubset(randstring(rng...), b)
         for c = ['a':'z', "qwèrtï", Set(Vector{UInt8}("gcat"))],
-            len = [8, 20]
-                s = len == 8 ? randstring(rng..., c) : randstring(rng..., c, len)
-                @test length(s) == len
-                if eltype(c) == Char
-                    @test issubset(s, c)
-                else # UInt8
-                    @test issubset(s, map(Char, c))
-                end
+                len = [8, 20]
+            s = len == 8 ? randstring(rng..., c) : randstring(rng..., c, len)
+            @test length(s) == len
+            if eltype(c) == Char
+                @test issubset(s, c)
+            else # UInt8
+                @test issubset(s, map(Char, c))
+            end
         end
     end
     @test randstring(MersenneTwister(0)) == randstring(MersenneTwister(0), b)
