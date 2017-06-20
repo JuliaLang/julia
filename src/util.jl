4817607e55fe204d98cab25c159605ccbf62481e
Nick Paul <npaul@ltu.edu>
2017-06-20 00:55:33 -0400

    Remove c lib in favor of pure julia

--- src/util.jl
+++ src/util.jl
@@ -1,9 +1,3 @@
-const LIB_MENUJL = string(splitdir(@__FILE__)[1], "/../lib/libmenujl.so")
-
-enableRawMode() = ccall( (:enableRawMode, LIB_MENUJL), Void, ())
-disableRawMode() = ccall( (:disableRawMode, LIB_MENUJL), Void, ())
-readKey() = ccall( (:editorReadKey, LIB_MENUJL), Int, ())
-
 @enum(Key,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
@@ -15,7 +9,79 @@ readKey() = ccall( (:editorReadKey, LIB_MENUJL), Int, ())
     PAGE_UP,
     PAGE_DOWN)
 
+terminal = Base.Terminals.TTYTerminal(get(ENV, "TERM", @static is_windows() ? "" : "dumb"), STDIN, STDOUT, STDERR)
+enableRawMode() = Base.Terminals.raw!(terminal, true)
+disableRawMode() = Base.Terminals.raw!(terminal, false)
+
 
 function clean(str::AbstractString)
     replace(str, "\n", "\\n")
+end
+
+readNextChar() = Char(read(STDIN,1)[1])
+
+function readKey() ::UInt32
+    c = readNextChar()
+
+	# Escape characters
+	if c == '\x1b'
+        STDIN.buffer.size < 3 && return '\x1b'
+        esc_a = readNextChar()
+        esc_b = readNextChar()
+
+		if esc_a == '['
+			if esc_b >= '0' && esc_b <= '9'
+				STDIN.buffer.size < 4 && return '\x1b'
+                esc_c = readNextChar()
+
+				if esc_c == '~'
+					if esc_b == '1'
+                        return HOME_KEY
+					elseif esc_b == '4'
+                        return END_KEY
+					elseif esc_b == '3'
+                        return DEL_KEY
+					elseif esc_b == '5'
+                        return PAGE_UP
+					elseif esc_b == '6'
+                        return PAGE_DOWN
+					elseif esc_b == '7'
+                        return HOME_KEY
+					elseif esc_b == '8'
+                        return END_KEY
+                    else
+                        return '\x1b'
+                    end
+                end
+
+			else
+				# Arrow keys
+				if esc_b == 'A'
+                    return ARROW_UP
+				elseif esc_b == 'B'
+                    return ARROW_DOWN
+				elseif esc_b == 'C'
+                    return ARROW_RIGHT
+				elseif esc_b == 'D'
+                    return ARROW_LEFT
+				elseif esc_b == 'H'
+                    return HOME_KEY
+				elseif esc_b == 'F'
+                    return END_KEY
+                else
+                    return '\x1b'
+                end
+			end
+		elseif esc_a == 'O'
+            if esc_a == 'H'
+                return HOME_KEY
+            elseif esc_a == 'F'
+                return END_KEY
+            end
+		end
+
+		return '\x1b'
+    else
+		return c;
+	end
 end
