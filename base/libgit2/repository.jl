4a88effb68f5065b8ef2111029c81b22574766fe
kshyatt <kshyatt@physics.ucsb.edu>
2017-06-21 17:05:39 -0700

    Fix the docs

--- base/libgit2/repository.jl
+++ base/libgit2/repository.jl
@@ -317,12 +317,11 @@ end
     LibGit2.cherrypick(repo::GitRepo, commit::GitCommit; options::CherrypickOptions = CherrypickOptions())
 
 Cherrypick the commit `commit` and apply the changes in it to the current state of `repo`.
-The keyword argument `options` sets checkout and merge options for the cherrypick. If `null`,
-the default options are used.
+The keyword argument `options` sets checkout and merge options for the cherrypick.
 
 !!! note
     `cherrypick` will *apply* the changes in `commit` but not *commit* them, so `repo` will
-    be left in a dirty state. If you want to apply and commit the changes in `commit` you must
+    be left in a dirty state. If you want to also commit the changes in `commit` you must
     call [`commit`](@ref) yourself.
 """
 function cherrypick(repo::GitRepo, commit::GitCommit; options::CherrypickOptions = CherrypickOptions())
