ec117affc3999405161c8529871b80e980a058e0
kshyatt <kshyatt@physics.ucsb.edu>
2017-06-22 21:23:14 -0700

    Fix typo

--- test/linalg/generic.jl
+++ test/linalg/generic.jl
@@ -343,5 +343,5 @@ Base.:<(a::ModInt{n}, b::ModInt{n}) where {n} = a.k < b.k
 
 @test A*(lufact(A, Val{true})\b) == b
 
-@test_throws ErrorException ctranpose(rand(2,2,2,2))
-@test_throws ErrorException tranpose(rand(2,2,2,2))
+@test_throws ErrorException ctranspose(rand(2,2,2,2))
+@test_throws ErrorException transpose(rand(2,2,2,2))
