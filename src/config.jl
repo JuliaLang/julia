01b6b734fbd52a483db5e9325e9347f7f72c305d
Nick Paul <npaul@ltu.edu>
2017-06-22 21:51:08 -0400

    Add config file, currently used for default characters

---
+++ src/config.jl
@@ -0,0 +1,35 @@
+CONFIG = Dict()
+
+function config(;charset::Symbol = :na,
+                cursor::Char = '\0',
+                up_arrow::Char = '\0',
+                down_arrow::Char = '\0',
+                checked::String = "",
+                unchecked::String = "")
+
+    if charset == :ascii
+        cursor = '>'
+        up_arrow = '^'
+        down_arrow = 'v'
+        checked = "[X]"
+        unchecked = "[ ]"
+    elseif charset == :unicode
+        cursor = '→'
+        scroll_up_arrow = '↑'
+        scroll_down_arrow = '↓'
+        checked = "✓"
+        unchecked = "⬚"
+    end
+
+    cursor != '\0'      && CONFIG[:cursor] = cursor
+    up_arrow != '\0'    && CONFIG[:up_arrow] = up_arrow
+    down_arrow != '\0'  && CONFIG[:down_arrow] = down_arrow
+    checked != ""       && CONFIG[:checked] = checked
+    unchecked != ""     && CONFIG[:unchecked] = unchecked
+
+    # Don't return anything
+    nothing
+end
+
+# Set up defaults
+config(charset=:ascii)
