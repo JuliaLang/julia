943fd2b7ab4f3a859575c206f6ebab752f10da9a
kshyatt <kshyatt@physics.ucsb.edu>
2017-06-25 18:20:56 -0700

    Reordering

--- base/libgit2/types.jl
+++ base/libgit2/types.jl
@@ -304,7 +304,7 @@ The fields represent:
   * `version`: version of the struct in use, in case this changes later. For now, always `1`.
   * `abbreviated_size`: lower bound on the size of the abbreviated `GitHash` to use, defaulting to `7`.
   * `always_use_long_format`: set to `1` to use the long format for strings even if a short format can be used.
-  * `dirty_suffix`: if set, if the [`workdir`](@ref) is dirty this will be appended to the description string.
+  * `dirty_suffix`: if set, this will be appended to the end of the description string if the [`workdir`](@ref) is dirty.
 """
 @kwdef struct DescribeFormatOptions
     version::Cuint          = 1
