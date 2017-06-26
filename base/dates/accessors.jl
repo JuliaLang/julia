da68c52008440477fe7acfdf099b6643177e1b06
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-06-26 21:45:06 +0200

    add some doctests to Dates (#22535)
    * add some doctests to Dates
    
    * updates

--- base/dates/accessors.jl
+++ base/dates/accessors.jl
@@ -84,9 +84,21 @@ end
 
 Return the [ISO week date](https://en.wikipedia.org/wiki/ISO_week_date) of a `Date` or
 `DateTime` as an [`Int64`](@ref). Note that the first week of a year is the week that
-contains the first Thursday of the year which can result in dates prior to January 4th
-being in the last week of the previous year. For example `week(Date(2005,1,1))` is the 53rd
+contains the first Thursday of the year, which can result in dates prior to January 4th
+being in the last week of the previous year. For example, `week(Date(2005, 1, 1))` is the 53rd
 week of 2004.
+
+# Examples
+```jldoctest
+julia> Dates.week(Date(1989, 6, 22))
+25
+
+julia> Dates.week(Date(2005, 1, 1))
+53
+
+julia> Dates.week(Date(2004, 12, 31))
+53
+```
 """
 week(dt::TimeType)
 
