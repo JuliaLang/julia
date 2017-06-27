7ac475fe3fadb158da4dc1e14df048cba579388b
kshyatt <kshyatt@physics.ucsb.edu>
2017-06-27 10:47:46 -0500

    [ci skip] fix wording

--- base/libgit2/blob.jl
+++ base/libgit2/blob.jl
@@ -7,8 +7,9 @@ end
 """
     rawcontent(blob::GitBlob) -> Array
 
-Fetch the *raw* contents of the [`GitBlob`](@ref) `blob`. This is a read-only
+Fetch the *raw* contents of the [`GitBlob`](@ref) `blob`. This is an
 `Array` containing the contents of the blob, which may be binary or may be Unicode.
+If you write to this `Array` the blob on disk will not be updated.
 `rawcontent` will allow the user to load the raw binary data into
 the output `Array` and will not check to ensure it is a valid Unicode, so errors
 may occur if the result is passed to functions which expect valid Unicode data.
