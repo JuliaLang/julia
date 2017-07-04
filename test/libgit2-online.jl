e482c6cd06d2bd9f2eaeb0ffb63962c98abf73b4
Jameson Nash <vtjnash@gmail.com>
2017-07-04 13:57:14 -0400

    libgit2 test: use a valid user/pass parameter to test EAUTH instead of OS:ERROR

--- test/libgit2-online.jl
+++ test/libgit2-online.jl
@@ -22,7 +22,7 @@ mktempdir() do dir
             try
                 repo_path = joinpath(dir, "Example2")
                 # credentials are required because github tries to authenticate on unknown repo
-                cred = LibGit2.UserPasswordCredentials("","") # empty credentials cause authentication error
+                cred = LibGit2.UserPasswordCredentials("JeffBezanson", "hunter2") # make sure Jeff is using a good password :)
                 LibGit2.clone(repo_url*randstring(10), repo_path, payload=Nullable(cred))
                 error("unexpected")
             catch ex
