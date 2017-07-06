f03b2d7a55afb4a3f9f4f29de411d9f1309315ed
Fredrik Ekre <fredrik.ekre@chalmers.se>
2017-07-06 09:48:14 +0200

    improve the documentation for [Abstract]Vector/[Abstract]Matrix

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -5,8 +5,9 @@
 """
     AbstractArray{T,N}
 
-Abstract `N`-dimensional array supertype containing elements of type `T`.
-[`Array`](@ref)s and some array-like objects inherit from this type.
+Supertype for `N`-dimensional arrays (or array-like types) with elements of type `T`.
+[`Array`](@ref) and other types are subtypes of this. See the manual section on the
+[`AbstractArray` interface](@ref man-interface-array).
 """
 AbstractArray
 
