30eed5db3e76418f6531708689b1c6f7e8646fff
Katharine Hyatt <kshyatt@users.noreply.github.com>
2017-07-14 13:44:41 -0700

    Take out the XDG ref

--- base/libgit2/config.jl
+++ base/libgit2/config.jl
@@ -40,7 +40,7 @@ end
 """
     GitConfig(level::Consts.GIT_CONFIG = Consts.CONFIG_LEVEL_DEFAULT)
 
-Get the default git configuration by loading the global, system, and XDG configuration
+Get the default git configuration by loading the global and system configuration
 files into a prioritized configuration. This can be used to access default configuration
 options outside a specific git repository.
 """
