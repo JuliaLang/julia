20abd15ba2121f1c8e470d3d4cbddb6c976c5dfe
Simon Byrne <simonbyrne@gmail.com>
2017-07-19 08:27:52 -0700

    use get_version as a function

--- base/mpfr.jl
+++ base/mpfr.jl
@@ -26,8 +26,8 @@ import Base.Math.lgamma_r
 
 import Base.FastMath.sincos_fast
 
-const MPFR_VERSION = VersionNumber(unsafe_string(ccall((:mpfr_get_version,:libmpfr), Ptr{Cchar}, ())))
-const MPFR_PATCHES = split(unsafe_string(ccall((:mpfr_get_patches,:libmpfr), Ptr{Cchar}, ())),' ')
+get_version() = VersionNumber(unsafe_string(ccall((:mpfr_get_version,:libmpfr), Ptr{Cchar}, ())))
+get_patches() = split(unsafe_string(ccall((:mpfr_get_patches,:libmpfr), Ptr{Cchar}, ())),' ')
 
 function __init__()
     try
--- test/mpfr.jl
+++ test/mpfr.jl
@@ -887,7 +887,7 @@ setprecision(256) do
 end
 
 # issue #22758
-if MPFR.MPFR_VERSION > v"3.1.5" || "r11590" in MPFR.MPFR_PATCHES
+if MPFR.get_version() > v"3.1.5" || "r11590" in MPFR.get_patches()
     setprecision(2_000_000) do
         @test abs(sin(big(pi)/6) - 0.5) < ldexp(big(1.0),-1_999_000)
     end
