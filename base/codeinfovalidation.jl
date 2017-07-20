481def5fd905ab2b7ab2ede948404137b1de5a23
Jarrett Revels <jarrettrevels@gmail.com>
2017-07-20 16:50:17 -0400

    mock out naive IR validator

---
+++ base/codeinfovalidation.jl
@@ -0,0 +1,23 @@
+# This file is a part of Julia. License is MIT: https://julialang.org/license
+
+const VALID_EXPR_HEADS = Symbol[:call, :invoke, :static_parameter, :line, :gotoifnot, :(=),
+                                :method, :const, :null, :new, :return, :the_exception,
+                                :enter, :leave, :inbounds, :boundscheck, :copyast, :meta]
+"""
+    is_valid_code_info(code_info::CodeInfo)
+
+Returns `true` if `code_info` is valid, and returns `false` otherwise.
+
+`code_info` is considered valid if the following properties hold true (where `nslotnums` is
+the number of `SlotNumber`s in `code_info.code` and `nssavals` is the number of `SSAValue`s
+in `code_info.code`):
+
+- `length(code_info.slotflags) == length(code_info.slotnames) == nslotnums`
+- `code_info.slottypes === nothing || length(code_info.slottypes) == nslotnums`
+- `code_info.ssatypes === nssavals || length(code_info.ssatypes) == nssavals`
+- all `Expr` heads are contained in `Base.Core.VALID_EXPR_HEADS`
+- call arguments are not `:gotoifnot` nodes
+
+"""
+function is_valid_code_info(code_info::CodeInfo)
+end
