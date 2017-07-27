8c71affd52a13aa00627f5076351c4838c6c6726
Yichao Yu <yyc1992@gmail.com>
2017-07-26 21:59:41 -0400

    Add a simple pass to copy `Expr`s that appears multiple times in the AST
    These objects make it really hard to mutate the AST correctly
    since one mutation can be accidentally done at places where it is invalid.

--- base/inference.jl
+++ base/inference.jl
@@ -3750,6 +3750,7 @@ function optimize(me::InferenceState)
         # and elide unnecessary allocations
         alloc_elim_pass!(opt)
         getfield_elim_pass!(opt)
+        copy_duplicated_expr_pass!(opt)
         # Clean up for `alloc_elim_pass!` and `getfield_elim_pass!`
         void_use_elim_pass!(opt)
         # Pop metadata before label reindexing
@@ -5886,14 +5887,18 @@ function meta_elim_pass!(code::Array{Any,1}, do_coverage::Bool)
         if isa(ex, Expr) && ex.head === :meta && length(ex.args) > 0 && ex.args[1] == :pop_loc
             npops += (length(ex.args) > 1 ? ex.args[2]::Int : 1)
             if lastpop === nothing
-                lastpop = ex
+                lastpop = i
             else
                 code[i] = nothing
             end
         elseif ex !== nothing && lastpop !== nothing
             if npops > 1
-                resize!(lastpop.args, 2)
-                lastpop.args[2] = npops
+                popex = code[lastpop]
+                if length(popex.args) == 1
+                    code[lastpop] = Expr(:meta, :pop_loc, npops)
+                else
+                    popex.args[2] = npops
+                end
             end
             lastpop = nothing
             npops = 0
@@ -6272,6 +6277,45 @@ function replace_getfield!(e::Expr, tupname, vals, field_names, sv::Optimization
             replace_getfield!(a, tupname, vals, field_names, sv)
         end
     end
+end
+
+const meta_pop_loc = Expr(:meta, :pop_loc)
+
+function copy_expr_in_array!(ary, seen)
+    for i in 1:length(ary)
+        ex = ary[i]
+        isa(ex, Expr) || continue
+        ex = ex::Expr
+        if ex.head === :meta
+            # Try to save some memory by using the same object for all `:pop_loc` meta node
+            if ex !== meta_pop_loc && length(ex.args) == 1 && ex.args[1] === :pop_loc
+                ary[i] = meta_pop_loc
+            end
+            continue # No need to copy meta expressions
+        end
+        if haskey(seen, ex)
+            newex = Expr(ex.head)
+            append!(newex.args, ex.args)
+            newex.typ = ex.typ
+            ary[i] = ex = newex
+            # No need to add to `seen`, there's no way there can be another one of the copied
+            # version in the AST....
+        else
+            seen[ex] = nothing
+            if haskey(seen, ex.args)
+                # Haven't actually seen this happen but it's pretty easy to check
+                ex.args = copy(ex.args)
+            else
+                seen[ex.args] = nothing
+            end
+        end
+        copy_expr_in_array!(ex.args, seen)
+    end
+end
+
+# Clone expressions that appears multiple times in the code
+function copy_duplicated_expr_pass!(sv::OptimizationState)
+    copy_expr_in_array!(sv.src.code, ObjectIdDict())
 end
 
 # fix label numbers to always equal the statement index of the label
