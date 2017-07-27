d45c20a97a0302cfdb5e15ad5c9b7338c845052f
Jarrett Revels <jarrettrevels@gmail.com>
2017-07-27 10:40:47 -0400

    add optional argument to code_lowered to enable generator expansion

--- base/reflection.jl
+++ base/reflection.jl
@@ -561,13 +561,32 @@ end
 tt_cons(@nospecialize(t), @nospecialize(tup)) = (@_pure_meta; Tuple{t, (isa(tup, Type) ? tup.parameters : tup)...})
 
 """
-    code_lowered(f, types)
+    code_lowered(f, types, expand_generated = false)
 
 Returns an array of lowered ASTs for the methods matching the given generic function and type signature.
+
+If `expand_generated` is `false`, then the `CodeInfo` instances returned for `@generated`
+methods will correspond to the generators' lowered ASTs. If `expand_generated` is `true`,
+these `CodeInfo` instances will correspond to the lowered ASTs of the method bodies yielded
+by expanding the generators.
 """
-function code_lowered(@nospecialize(f), @nospecialize t = Tuple)
-    asts = map(methods(f, t)) do m
-        return uncompressed_ast(m::Method)
+function code_lowered(@nospecialize(f), @nospecialize(t = Tuple), expand_generated::Bool = false)
+    if expand_generated
+        ft = isa(f,Type) ? Type{f} : typeof(f)
+        tt = isa(t,Type) ? Tuple{ft, t.parameters...} : Tuple{ft, t...}
+        asts = map(_methods_by_ftype(tt, -1, typemax(UInt))) do method_data
+            mtypes, msp, m = method_data
+            if isdefined(m, :generator)
+                instance = Core.Inference.code_for_method(m, mtypes, msp, typemax(UInt), false)
+                return Core.Inference.get_staged(instance::Core.MethodInstance)
+            else
+                return uncompressed_ast(m::Method)
+            end
+        end
+    else
+        asts = map(methods(f, t)) do m
+            return uncompressed_ast(m::Method)
+        end
     end
     return asts
 end
