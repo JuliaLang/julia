5961643ae835e8a6fe63cc9739887c9c8c12d9d4
Tim Besard <tim.besard@gmail.com>
2017-07-29 09:11:21 +0200

    Add a test for #22582/#22022.
    Ref #23021
    (cherry picked from commit e55b1dbcb9a073d0e35e6c0aa62600437647d404)

--- test/codegen.jl
+++ test/codegen.jl
@@ -7,3 +7,17 @@ function egal_svecs()
 end
 @test egal_svecs()
 @test Core.svec(:a, :b) === Core.svec(:a, :b)
+
+# issue #22582
+function issue22582!(a::AbstractArray, b)
+    len = length(a)
+    if b
+        ccall(:jl_array_grow_end, Void, (Any, Csize_t), a, 1)
+    end
+    return len
+end
+let c = [1,2,3]
+    len1 = length(c)
+    len2 = issue22582!(c, true)
+    @test len1 == len2
+end
