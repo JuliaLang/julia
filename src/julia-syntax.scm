16cf6d8f30a1309327d33d5b58720243ec73b5b8
Jeff Bezanson <jeff.bezanson@gmail.com>
2017-07-31 10:53:02 -0400

    preserve more syntax in parsing `?`, `do`, and `elseif`
    part of #21774

--- src/julia-syntax.scm
+++ src/julia-syntax.scm
@@ -1656,15 +1656,6 @@
                             (if ,g ,g
                                 ,(loop (cdr tail)))))))))))
 
-(define (expand-forms e)
-  (if (or (atom? e) (memq (car e) '(quote inert top core globalref outerref line module toplevel ssavalue null meta)))
-      e
-      (let ((ex (get expand-table (car e) #f)))
-        (if ex
-            (ex e)
-            (cons (car e)
-                  (map expand-forms (cdr e)))))))
-
 (define (expand-for while lhs X body)
   ;; (for (= lhs X) body)
   (let ((coll  (make-ssavalue))
@@ -1871,12 +1862,22 @@
               (extract (cdr params) (cons p newparams) whereparams)))))
   (extract (cddr e) '() '()))
 
+(define (expand-forms e)
+  (if (or (atom? e) (memq (car e) '(quote inert top core globalref outerref line module toplevel ssavalue null meta)))
+      e
+      (let ((ex (get expand-table (car e) #f)))
+        (if ex
+            (ex e)
+            (cons (car e)
+                  (map expand-forms (cdr e)))))))
+
 ;; table mapping expression head to a function expanding that form
 (define expand-table
   (table
    'function       expand-function-def
    'stagedfunction expand-function-def
    '->             expand-arrow
+   '?              (lambda (e) (expand-forms (cons 'if (cdr e))))
    'let            expand-let
    'macro          expand-macro-def
    'type           expand-type-def
@@ -2178,6 +2179,17 @@
                  (else
                   (map expand-forms e))))
          (map expand-forms e)))
+
+   'do
+   (lambda (e)
+     (let* ((call (cadr e))
+            (f    (cadr call))
+            (argl (cddr call))
+            (af   (caddr e)))
+       (expand-forms
+        (if (has-parameters? argl)
+            `(call ,f ,(car argl) ,af ,@(cdr argl))
+            `(call ,f ,af ,@argl)))))
 
    'tuple
    (lambda (e)
@@ -3493,7 +3505,7 @@ f(x) = yt(x)
              (if value
                  (compile (cadr e) break-labels value tail)
                  #f))
-            ((if)
+            ((if elseif)
              (let ((test `(gotoifnot ,(compile-cond (cadr e) break-labels) _))
                    (end-jump `(goto _))
                    (val (if (and value (not tail)) (new-mutable-var) #f)))
