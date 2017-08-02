0b0fffccf3fcb18377518b14ca12f01723bfc72b
Curtis Vogt <curtis.vogt@gmail.com>
2017-08-02 13:48:39 -0500

    [ci skip] Add docstring for git_url

--- base/libgit2/utils.jl
+++ base/libgit2/utils.jl
@@ -70,6 +70,37 @@ else Sys.isunix()
     posixpath(path) = path
 end
 
+"""
+    LibGit2.git_url(; kwargs...) -> String
+
+Create a string based upon the URL components provided. When the `scheme` keyword is not
+provided the URL produced will use the alternative [scp-like syntax](https://git-scm.com/docs/git-clone#_git_urls_a_id_urls_a).
+
+# Keywords
+
+  * `scheme::AbstractString=""`: the URL scheme which identifies the protocol to be used.
+    For HTTP use "http", SSH use "ssh", etc. When `scheme` is not provided the output format
+    will be "ssh" but using the scp-like syntax.
+  * `username::AbstractString=""`: the username to use in the output if provided.
+  * `password::AbstractString=""`: the password to use in the output if provided.
+  * `host::AbstractString=""`: the hostname to use in the output. A hostname is required to
+    be specified.
+  * `port::Union{AbstractString,Integer}=""`: the port number to use in the output if
+    provided. Cannot be specified when using the scp-like syntax.
+  * `path::AbstractString=""`: the path to use in the output if provided.
+
+# Examples
+```jldoctest
+julia> LibGit2.git_url(username="git", host="github.com", path="JuliaLang/julia.git")
+"git@github.com:JuliaLang/julia.git"
+
+julia> LibGit2.git_url(scheme="https", host="github.com", path="/JuliaLang/julia.git")
+"https://github.com/JuliaLang/julia.git"
+
+julia> LibGit2.git_url(scheme="ssh", username="git", host="github.com", port=2222, path="JuliaLang/julia.git")
+"ssh://git@github.com:2222/JuliaLang/julia.git"
+```
+"""
 function git_url(;
         scheme::AbstractString="",
         username::AbstractString="",
