21ce7bef98c9fb6f5a4ad476d55f145bcb8a933e
Mus M <mus-m@outlook.com>
2017-08-05 19:50:02 -0400

    update ccall docstring (#23085)

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -555,20 +555,21 @@ Short-circuiting boolean OR.
 kw"||"
 
 """
-    ccall((symbol, library) or function_pointer, ReturnType, (ArgumentType1, ...), ArgumentValue1, ...)
+    ccall((function_name, library), returntype, (argtype1, ...), argvalue1, ...)
+    ccall(function_pointer, returntype, (argtype1, ...), argvalue1, ...)
 
-Call function in C-exported shared library, specified by `(function name, library)`
-tuple, where each component is a string or symbol.
+Call a function in a C-exported shared library, specified by the tuple `(function_name, library)`,
+where each component is either a string or symbol. Alternatively, `ccall` may
+also be used to call a function pointer `function_pointer`, such as one returned by `dlsym`.
 
 Note that the argument type tuple must be a literal tuple, and not a tuple-valued
-variable or expression. Alternatively, `ccall` may also be used to call a function
-pointer, such as one returned by `dlsym`.
+variable or expression.
 
-Each `ArgumentValue` to the `ccall` will be converted to the corresponding
-`ArgumentType`, by automatic insertion of calls to `unsafe_convert(ArgumentType,
-cconvert(ArgumentType, ArgumentValue))`. (See also the documentation for each of these
+Each `argvalue` to the `ccall` will be converted to the corresponding
+`argtype`, by automatic insertion of calls to `unsafe_convert(argtype,
+cconvert(argtype, argvalue))`. (See also the documentation for each of these
 functions for further details.) In most cases, this simply results in a call to
-`convert(ArgumentType, ArgumentValue)`.
+`convert(argtype, argvalue)`.
 """
 kw"ccall"
 
