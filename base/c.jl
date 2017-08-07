9363b4970c951a48f425b50bdec62e5dc94cda27
Mus M <mus-m@outlook.com>
2017-08-07 00:44:24 -0400

    Update cfunction docstring (#23084)
    * Update cfunction docstring
    
    * Update c.jl
    
    * Add return type

--- base/c.jl
+++ base/c.jl
@@ -5,9 +5,9 @@
 import Core.Intrinsics: cglobal, bitcast
 
 """
-    cfunction(function::Function, ReturnType::Type, ArgumentTypes::Type)
+    cfunction(f::Function, returntype::Type, argtypes::Type) -> Ptr{Void}
 
-Generate C-callable function pointer from Julia function. Type annotation of the return
+Generate C-callable function pointer from the Julia function `f`. Type annotation of the return
 value in the callback function is a must for situations where Julia cannot infer the return
 type automatically.
 
