9950d82fc67fd07b34be025b027b84697821f9e9
Fredrik Ekre <fredrik.ekre@chalmers.se>
2017-08-07 21:44:36 +0200

    fix math in condskeel docstring (#23083)
    (cherry picked from commit 0f4229d2e744375255fa1b27cb180f678ebf3e76)

--- base/linalg/generic.jl
+++ base/linalg/generic.jl
@@ -849,13 +849,15 @@ condskeel(A::AbstractMatrix, p::Real=Inf) = norm(abs.(inv(A))*abs.(A), p)
     condskeel(M, [x, p::Real=Inf])
 
 ```math
-\\kappa_S(M, p) & = \\left\\Vert \\left\\vert M \\right\\vert \\left\\vert M^{-1} \\right\\vert  \\right\\Vert_p \\\\
-\\kappa_S(M, x, p) & = \\left\\Vert \\left\\vert M \\right\\vert \\left\\vert M^{-1} \\right\\vert \\left\\vert x \\right\\vert \\right\\Vert_p
+\\kappa_S(M, p) = \\left\\Vert \\left\\vert M \\right\\vert \\left\\vert M^{-1} \\right\\vert \\right\\Vert_p \\\\
+\\kappa_S(M, x, p) = \\left\\Vert \\left\\vert M \\right\\vert \\left\\vert M^{-1} \\right\\vert \\left\\vert x \\right\\vert \\right\\Vert_p
 ```
 
 Skeel condition number ``\\kappa_S`` of the matrix `M`, optionally with respect to the
-vector `x`, as computed using the operator `p`-norm.
-`p` is `Inf` by default, if not provided. Valid values for `p` are `1`, `2`, or `Inf`.
+vector `x`, as computed using the operator `p`-norm. ``\\left\\vert M \\right\\vert``
+denotes the matrix of (entry wise) absolute values of ``M``;
+``\\left\\vert M \\right\\vert_{ij} = \\left\\vert M_{ij} \\right\\vert``.
+Valid values for `p` are `1`, `2` and `Inf` (default).
 
 This quantity is also known in the literature as the Bauer condition number, relative
 condition number, or componentwise relative condition number.
