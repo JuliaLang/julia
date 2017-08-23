b690968daf887cf98f305a8f6737d62728e9275e
Alex Arslan <ararslan@comcast.net>
2017-08-23 13:45:36 -0700

    Change note to warning

--- base/libgit2/repository.jl
+++ base/libgit2/repository.jl
@@ -343,7 +343,7 @@ end
 Update the index and working tree of `repo` to match the commit pointed to by HEAD.
 `options` controls how the checkout will be performed. See [`CheckoutOptions`](@ref) for more information.
 
-!!! note
+!!! warning
     *Do not* use this function to switch branches! Doing so will cause checkout
     conflicts.
 """
