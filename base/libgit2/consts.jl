59eb9d15d12f969086e4e6f16b92b3a681d6d7cb
kshyatt <kshyatt@physics.ucsb.edu>
2017-08-24 11:18:56 -0700

    Doc rebase stuff

--- base/libgit2/consts.jl
+++ base/libgit2/consts.jl
@@ -176,7 +176,20 @@ module Consts
     const RESET_MIXED = Cint(2) # SOFT plus reset index to the commit
     const RESET_HARD  = Cint(3) # MIXED plus changes in working tree discarded
 
-    #rebase
+    # rebase
+    """ Options for what rebase operation is currently being performed on a commit.
+    * `REBASE_OPERATION_PICK`: cherry-pick the commit in question.
+    * `REBASE_OPERATION_REWORD`: cherry-pick the commit in question, but rewrite its
+      message using the prompt.
+    * `REBASE_OPERATION_EDIT`: cherry-pick the commit in question, but allow the user
+      to edit the commit's contents and its message.
+    * `REBASE_OPERATION_SQUASH`: squash the commit in question into the previous commit.
+      The commit messages of the two commits will be merged.
+    * `REBASE_OPERATION_FIXUP`: squash the commit in question into the previous commit.
+      Only the commit message of the previous commit will be used.
+    * `REBASE_OPERATION_EXEC`: do not cherry-pick a commit. Run a command and continue if
+      the command exits successfully.
+    """
     @enum(GIT_REBASE_OPERATION, REBASE_OPERATION_PICK   = Cint(0),
                                 REBASE_OPERATION_REWORD = Cint(1),
                                 REBASE_OPERATION_EDIT   = Cint(2),
