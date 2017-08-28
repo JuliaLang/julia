ff01d99c9155b28838e00c8b599e19d1071fcb19
kshyatt <kshyatt@physics.ucsb.edu>
2017-08-28 16:11:21 -0700

    Fix wording etc

--- base/libgit2/types.jl
+++ base/libgit2/types.jl
@@ -854,10 +854,10 @@ end
 with(f::Function, ::Type{T}, args...) where {T} = with(f, T(args...))
 
 """
-    with_warn(f::Function, ::Type{T}, args..)
+    with_warn(f::Function, ::Type{T}, args...)
 
 Resource management helper function. Apply `f` to `args`, first constructing
-an instance of type `T` out of them. Makes sure to call `close` on the resulting
+an instance of type `T` from `args`. Makes sure to call `close` on the resulting
 object after `f` successfully returns or throws an error. Ensures that
 allocated git resources are finalized as soon as they are no longer needed. If an
 error is thrown by `f`, a warning is shown containing the error.
