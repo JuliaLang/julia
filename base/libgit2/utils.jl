5dce447a32b5536330c0328ffdfc6ba9b87f9392
Curtis Vogt <curtis.vogt@gmail.com>
2017-09-03 18:06:35 -0500

    Exclude starting slash from path in URL_REGEX
    Keeps URL parsing consistent between the scp-like syntax and the scheme
    based parsing.

--- base/libgit2/utils.jl
+++ base/libgit2/utils.jl
@@ -10,12 +10,15 @@ const URL_REGEX = r"""
 )?
 (?<host>[A-Za-z0-9\-\.]+)
 (?(<scheme>)
-    (?:\:(?<port>\d+))?  # only parse port when not using scp-like syntax
+    # Only parse port when not using scp-like syntax
+    (?:\:(?<port>\d+))?
+    /?
     |
     :?
 )
 (?<path>
-    (?(<scheme>)/|(?<=:))  # scp-like syntax must be preceeded by a colon
+    # Require path to be preceeded by '/'. Alternatively, ':' when using scp-like syntax.
+    (?<=(?(<scheme>)/|:))
     .*
 )?
 $
