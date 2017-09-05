eb05225db1f5ae6b472fb2c3ab2137ddb68ebbac
Magnus Lie Hetland <magnus@hetland.org>
2017-09-05 10:24:17 +0200

    Correct default of writable in doc for IOBuffer
    Changed default for writable to false in the docs. Also removed superfluous comma in doc, and adjusted brackets for optionals.

--- base/iobuffer.jl
+++ base/iobuffer.jl
@@ -32,7 +32,7 @@ StringVector(n::Integer) = Vector{UInt8}(_string_n(n))
 # IOBuffers behave like Files. They are typically readable and writable. They are seekable. (They can be appendable).
 
 """
-    IOBuffer([data,],[readable::Bool=true, writable::Bool=true, [maxsize::Int=typemax(Int)]])
+    IOBuffer([data, ][readable::Bool=true, writable::Bool=false[, maxsize::Int=typemax(Int)]])
 
 Create an `IOBuffer`, which may optionally operate on a pre-existing array. If the
 readable/writable arguments are given, they restrict whether or not the buffer may be read
