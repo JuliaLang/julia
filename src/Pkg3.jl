e65a21fb1d5cc8d8310b8a1ddf43238c11c9f084
Stefan Karpinski <stefan@karpinski.org>
2017-09-08 15:33:29 -0400

    different approach to monkey patching loading

--- src/Pkg3.jl
+++ src/Pkg3.jl
@@ -1,17 +1,41 @@
-include("monkey_patch.jl")
+@eval Base module Loading
+    const DEPOTS = [joinpath(homedir(), ".julia")]
+end
 
 module Pkg3
-
-@eval Base module Loading; DEPOTS = []; end
-
-include("Types.jl")
-include("Operations.jl")
-include("Loading.jl")
-include("REPLMode.jl")
-REPLMode.repl_init(Base.active_repl)
-
-import .Types: VersionRange, VersionSpec, EnvCache
-import .Loading: Loader, LoadInstalled
-import .Operations: add, rm, up
-
+    include("Types.jl")
+    include("Operations.jl")
+    include("REPLMode.jl")
+    REPLMode.repl_init(Base.active_repl)
 end # module
+
+@eval Base.Loading begin
+    eval(Expr(:toplevel, :(import Pkg3)))
+    function _find_in_path(name::String, wd::Union{Void,String})
+        isabspath(name) && return name
+        base = name
+        if endswith(name, ".jl")
+            base = name[1:end-3]
+        else
+            name = string(base, ".jl")
+        end
+        if wd !== nothing
+            path = joinpath(wd, name)
+            isfile_casesensitive(path) && return path
+        end
+        info = Pkg3.Operations.package_env_info(base, verb = "use")
+        info == nothing && return nothing
+        haskey(info, "uuid") || return nothing
+        haskey(info, "hash-sha1") || return nothing
+        uuid = String(Base.Random.UUID(info["uuid"]))
+        hash = String(Pkg3.Types.SHA1(info["hash-sha1"]))
+        path = joinpath("packages", uuid, hash, "src", name)
+        for depot in DEPOTS
+            file = joinpath(depot, path)
+            ispath(file) && return file
+        end
+        return nothing
+    end
+    Base.find_in_path(name::String, wd::Void) = _find_in_path(name, wd)
+    Base.find_in_path(name::String, wd::String) = _find_in_path(name, wd)
+end
