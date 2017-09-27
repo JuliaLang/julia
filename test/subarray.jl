9e2cc24b96ebaedba9defda8ff8286cd71bad735
Martin Holters <martin.holters@hsu-hh.de>
2017-09-27 11:34:03 +0200

    Workaround #15276 in `flipdim`
    ...by wrapping a closure in a `let`.

--- test/subarray.jl
+++ test/subarray.jl
@@ -575,3 +575,6 @@ let
     @test Base.IndexStyle(view(a, :, :)) == Base.IndexLinear()
     @test isbits(view(a, :, :))
 end
+
+# ref issue #17351
+@test @inferred(flipdim(view([1 2; 3 4], :, 1), 1)) == [3, 1]
