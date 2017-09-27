15d2b35809ce66cf8f265c270c27c30da8bb6bb3
John F. Gibson <johnfgibson@gmail.com>
2017-09-27 14:58:39 -0400

    Updated paths to BLAS, dsfmt, Rmath in Makefile & sscanf syntax in perf.m

--- test/perf/micro/perf.m
+++ test/perf/micro/perf.m
@@ -92,8 +92,8 @@ end
 function n = parseintperf(t)
     for i = 1:t
         n = randi([0,2^32-1],1,'uint32');
-        s = sprintf('%08X',n);
-        m = sscanf(s,'%X');
+        s = sprintf('%08x',n);
+        m = sscanf(s,'%x');
         assert(m == n);
     end
 end
