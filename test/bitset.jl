99168f05f3ffcc6127bdc6a5c5651077616d880a
Matt Bauman <mbauman@gmail.com>
2017-10-27 15:37:11 -0500

    s/IntSet/BitSet/g (SQUASH ME)

--- test/bitset.jl
+++ test/bitset.jl
@@ -299,10 +299,10 @@ end
 end
 
 @testset "unsigned overflow" begin
-    @test IntSet(UInt8(2^8-1)) == IntSet(2^8-1)
-    @test [x for x in IntSet(UInt8(2^8-1))] == [UInt8(2^8-1)]
-    @test IntSet(UInt16(2^16-1)) == IntSet(2^16-1)
-    @test [x for x in IntSet(UInt16(2^16-1))] == [UInt16(2^16-1)]
+    @test BitSet(UInt8(2^8-1)) == BitSet(2^8-1)
+    @test [x for x in BitSet(UInt8(2^8-1))] == [UInt8(2^8-1)]
+    @test BitSet(UInt16(2^16-1)) == BitSet(2^16-1)
+    @test [x for x in BitSet(UInt16(2^16-1))] == [UInt16(2^16-1)]
 end
 
 @testset "order" begin
