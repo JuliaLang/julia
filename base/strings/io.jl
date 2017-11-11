806441e6270415673d73f8cedf42b2eaeef294c3
Stefan Karpinski <stefan@karpinski.org>
2017-11-11 10:59:19 -0500

    convert(String, ::Vector{Char}): don't normalize surrogate pairs
    we're in a new era where Julia tries as hard as it can to pass text
    data through intact rather than trying to guess what you meant. to
    that end, when converting a vector of characters to a `String` we
    no longer detect and combine surrogate pairs. there can be a utility
    function to do that kind of normalization on strings, but it's not
    the job of the basic string code.

--- base/strings/io.jl
+++ base/strings/io.jl
@@ -504,18 +504,7 @@ end
 
 function convert(::Type{String}, chars::AbstractVector{Char})
     sprint(length(chars), io->begin
-        state = start(chars)
-        while !done(chars, state)
-            c, state = next(chars, state)
-            if '\ud7ff' < c && c + 1024 < '\ue000'
-                d, state = next(chars, state)
-                if '\ud7ff' < d - 1024 && d < '\ue000'
-                    c = Char(0x10000 + ((UInt32(c) & 0x03ff) << 10) | (UInt32(d) & 0x03ff))
-                else
-                    write(io, c)
-                    c = d
-                end
-            end
+        for c in chars
             write(io, c)
         end
     end)
