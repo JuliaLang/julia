81e245c850b18d871dbbfc6eebf21d129030ae53
Jeff Bezanson <jeff.bezanson@gmail.com>
2017-11-16 17:43:17 -0500

    fix #22926, raw string escaping rules that allow writing all strings (#24621)

--- base/strings/io.jl
+++ base/strings/io.jl
@@ -371,15 +371,25 @@ macro b_str(s); :(Vector{UInt8}($(unescape_string(s)))); end
     @raw_str -> String
 
 Create a raw string without interpolation and unescaping.
-The exception is that quotation marks still must be escaped.
+The exception is that quotation marks still must be escaped. Backslashes
+escape both quotation marks and other backslashes, but only when a sequence
+of backslashes precedes a quote character. Thus, 2n backslashes followed by
+a quote encodes n backslashes and the end of the literal while 2n+1 backslashes
+followed by a quote encodes n backslashes followed by a quote character.
 
 # Examples
 ```jldoctest
-julia> raw"\\""
-"\\""
+julia> println(raw"\\ \$x")
+\\ \$x
 
-julia> raw""\" " ""\"
-" \\" "
+julia> println(raw"\\"")
+"
+
+julia> println(raw"\\\\\\"")
+\\"
+
+julia> println(raw"\\\\x \\\\\\"")
+\\\\x \\"
 ```
 """
 macro raw_str(s); s; end
