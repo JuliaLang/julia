b552904487a479dfbe6283374d05a8729fdbd56d
Kristoffer Carlsson <kcarlsson89@gmail.com>
2017-11-26 16:39:34 +0100

    use default depot for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,19 +3,13 @@ using Base.Test
 using Pkg3.Types
 
 function temp_pkg_dir(fn::Function)
-    local depot
     local project_path
     try
-        depot = joinpath(tempdir(), randstring())
-        empty!(Pkg3.DEPOTS)
-        push!(Pkg3.DEPOTS, depot)
         project_path = joinpath(tempdir(), randstring())
-
         withenv("JULIA_ENV" => project_path) do
             fn()
         end
     finally
-        rm(depot, recursive=true, force=true)
         rm(project_path, recursive=true, force=true)
     end
 end
