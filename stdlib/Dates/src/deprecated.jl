c33a7353cd8dc3fa30578e219f3bf81856c9ec04
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-11-27 16:01:52 -0500

    Unexport Dates deprecations

--- stdlib/Dates/src/deprecated.jl
+++ stdlib/Dates/src/deprecated.jl
@@ -23,19 +23,19 @@ import Base.range
     format(Y::AbstractArray{T}, df::DateFormat=default_format(T)) where {T<:TimeType},
     format.(Y, df) )
 
-@deprecate +(a::GeneralPeriod, b::StridedArray{<:GeneralPeriod}) broadcast(+, a, b)
-@deprecate +(a::StridedArray{<:GeneralPeriod}, b::GeneralPeriod) broadcast(+, a, b)
-@deprecate -(a::GeneralPeriod, b::StridedArray{<:GeneralPeriod}) broadcast(-, a, b)
-@deprecate -(a::StridedArray{<:GeneralPeriod}, b::GeneralPeriod) broadcast(-, a, b)
+@deprecate +(a::GeneralPeriod, b::StridedArray{<:GeneralPeriod}) broadcast(+, a, b) false
+@deprecate +(a::StridedArray{<:GeneralPeriod}, b::GeneralPeriod) broadcast(+, a, b) false
+@deprecate -(a::GeneralPeriod, b::StridedArray{<:GeneralPeriod}) broadcast(-, a, b) false
+@deprecate -(a::StridedArray{<:GeneralPeriod}, b::GeneralPeriod) broadcast(-, a, b) false
 
 # #24258
 # Physical units define an equivalence class: there is no such thing as a step of "1" (is
 # it one day or one second or one nanosecond?). So require the user to specify the step
 # (in physical units).
-@deprecate colon(start::T, stop::T) where {T<:DateTime}   start:Day(1):stop
-@deprecate colon(start::T, stop::T) where {T<:Date}       start:Day(1):stop
-@deprecate colon(start::T, stop::T) where {T<:Time}       start:Second(1):stop
+@deprecate colon(start::T, stop::T) where {T<:DateTime}   start:Day(1):stop    false
+@deprecate colon(start::T, stop::T) where {T<:Date}       start:Day(1):stop    false
+@deprecate colon(start::T, stop::T) where {T<:Time}       start:Second(1):stop false
 
-@deprecate range(start::DateTime, len::Integer)  range(start, Day(1), len)
-@deprecate range(start::Date, len::Integer)      range(start, Day(1), len)
+@deprecate range(start::DateTime, len::Integer)  range(start, Day(1), len) false
+@deprecate range(start::Date, len::Integer)      range(start, Day(1), len) false
 
