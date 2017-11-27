2d225253d7d4eb653dfd193cd7d0122e8caf4db6
Helge Eichhorn <git@helgeeichhorn.de>
2017-11-27 11:05:23 +0100

    Render URL and name of Markdown links in terminal

--- test/markdown.jl
+++ test/markdown.jl
@@ -224,7 +224,7 @@ World""" |> plain == "Hello\n\n---\n\nWorld\n"
 
 # multiple whitespace is ignored
 @test sprint(term, md"a  b") == "  a b\n"
-@test sprint(term, md"[x](https://julialang.org)") == "  x\n"
+@test sprint(term, md"[x](https://julialang.org)") == "  x (https://julialang.org)\n"
 @test sprint(term, md"![x](https://julialang.org)") == "  (Image: x)\n"
 
 # enumeration is normalized
