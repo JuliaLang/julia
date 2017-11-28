675884196bb6d16ac1a4810ae9a05eef78f05953
kshyatt <kshyatt@physics.ucsb.edu>
2017-11-28 12:15:59 -0800

    Examples for RowVector

--- base/linalg/rowvector.jl
+++ base/linalg/rowvector.jl
@@ -12,6 +12,48 @@ By convention, a vector can be multiplied by a matrix on its left (`A * v`) wher
 vector can be multiplied by a matrix on its right (such that `v.' * A = (A.' * v).'`). It
 differs from a `1×n`-sized matrix by the facts that its transpose returns a vector and the
 inner product `v1.' * v2` returns a scalar, but will otherwise behave similarly.
+
+# Examples
+```jldoctest
+julia> a = [1; 2; 3; 4]
+4-element Array{Int64,1}:
+ 1
+ 2
+ 3
+ 4
+
+julia> RowVector(a)
+1×4 RowVector{Int64,Array{Int64,1}}:
+ 1  2  3  4
+
+julia> a.'
+1×4 RowVector{Int64,Array{Int64,1}}:
+ 1  2  3  4
+
+julia> a.'[3]
+3
+
+julia> a.'[1,3]
+3
+
+julia> a.'[3,1]
+ERROR: BoundsError: attempt to access 1×4 RowVector{Int64,Array{Int64,1}} at index [3, 1]
+[...]
+
+julia> a.'*a
+30
+
+julia> B = [1 2; 3 4; 5 6; 7 8]
+4×2 Array{Int64,2}:
+ 1  2
+ 3  4
+ 5  6
+ 7  8
+
+julia> a.'*B
+1×2 RowVector{Int64,Array{Int64,1}}:
+ 50  60
+```
 """
 struct RowVector{T,V<:AbstractVector} <: AbstractMatrix{T}
     vec::V
