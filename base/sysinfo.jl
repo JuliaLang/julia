b31954fba41210cc8e6e4bcc8ec0f5f6ea37a9c1
Alex Arslan <ararslan@comcast.net>
2017-11-28 23:30:52 -0800

    Document that Sys.isbsd() is true on macOS (#24830)
    Fixes #24813

--- base/sysinfo.jl
+++ base/sysinfo.jl
@@ -235,6 +235,11 @@ islinux(os::Symbol) = (os == :Linux)
 
 Predicate for testing if the OS is a derivative of BSD.
 See documentation in [Handling Operating System Variation](@ref).
+
+!!! note
+    The Darwin kernel descends from BSD, which means that `Sys.isbsd()` is
+    `true` on macOS systems. To exclude macOS from a predicate, use
+    `Sys.isbsd() && !Sys.isapple()`.
 """
 isbsd(os::Symbol) = (os == :FreeBSD || os == :OpenBSD || os == :NetBSD || os == :DragonFly || os == :Darwin || os == :Apple)
 
