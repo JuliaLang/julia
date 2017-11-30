01c2bf2dfe60b502ff24a82ff0a12298b2a53fe1
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-11-30 09:58:19 +0800

    LLVM 6 api changes about 'UnsafeAlgebra'
    Ref: https://reviews.llvm.org/rL317488

--- src/llvm-muladd.cpp
+++ src/llvm-muladd.cpp
@@ -65,15 +65,27 @@ static bool checkCombine(Module *m, Instruction *addOp, Value *maybeMul, Value *
         auto newaddend = builder.CreateFNeg(addend);
         // Might be a const
         if (auto neginst = dyn_cast<Instruction>(newaddend))
+#if JL_LLVM_VERSION >= 60000
+            neginst->setFast(true);
+#else
             neginst->setHasUnsafeAlgebra(true);
+#endif
         addend = newaddend;
     }
     Instruction *newv = builder.CreateCall(muladdf, {mul1, mul2, addend});
+#if JL_LLVM_VERSION >= 60000
+    newv->setFast(true);
+#else
     newv->setHasUnsafeAlgebra(true);
+#endif
     if (negres) {
         // Shouldn't be a constant
         newv = cast<Instruction>(builder.CreateFNeg(newv));
+#if JL_LLVM_VERSION >= 60000
+        newv->setFast(true);
+#else
         newv->setHasUnsafeAlgebra(true);
+#endif
     }
     addOp->replaceAllUsesWith(newv);
     addOp->eraseFromParent();
@@ -91,14 +103,22 @@ bool CombineMulAdd::runOnFunction(Function &F)
             it++;
             switch (I.getOpcode()) {
             case Instruction::FAdd: {
+#if JL_LLVM_VERSION >= 60000
+                if (!I.isFast())
+#else
                 if (!I.hasUnsafeAlgebra())
+#endif
                     continue;
                 checkCombine(m, &I, I.getOperand(0), I.getOperand(1), false, false) ||
                     checkCombine(m, &I, I.getOperand(1), I.getOperand(0), false, false);
                 break;
             }
             case Instruction::FSub: {
+#if JL_LLVM_VERSION >= 60000
+                if (!I.isFast())
+#else
                 if (!I.hasUnsafeAlgebra())
+#endif
                     continue;
                 checkCombine(m, &I, I.getOperand(0), I.getOperand(1), true, false) ||
                     checkCombine(m, &I, I.getOperand(1), I.getOperand(0), true, true);
