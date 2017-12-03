39460485f4a325832eab3a3731c930eef5552f76
Andy Ferris <andy.ferris@roames.com.au>
2017-12-03 21:09:56 +1000

    Make `keys(::Tuple)` return a `Tuple`.

--- base/tuple.jl
+++ base/tuple.jl
@@ -38,7 +38,7 @@ start(t::Tuple) = 1
 done(t::Tuple, i::Int) = (length(t) < i)
 next(t::Tuple, i::Int) = (t[i], i+1)
 
-keys(t::Tuple) = 1:length(t)
+keys(t::Tuple) = ntuple(identity, Val(length(t)))
 
 prevind(t::Tuple, i::Integer) = Int(i)-1
 nextind(t::Tuple, i::Integer) = Int(i)+1
