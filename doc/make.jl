b386592b2d9afc989f5505798dfaeaff8d1ea275
Elliot Saba <staticfloat@gmail.com>
2017-12-09 15:05:09 -0800

    Set `JULIA_PKGDIR` before Julia ever starts up when building docs
    Attempting to set `ENV["JULIA_PKGDIR"]` from within Julia doesn't work,
    as the `LOAD_CACHE_PATH` is initialized within an `__init__()` function
    within `Pkg`, causing compile cache files to be saved to the default
    location (within the user's `$HOME`) rather than the override location.
    This change ensures that the docs pkg environment is properly confined.

--- doc/make.jl
+++ doc/make.jl
@@ -1,5 +1,10 @@
 # Install dependencies needed to build the documentation.
-ENV["JULIA_PKGDIR"] = joinpath(@__DIR__, "deps")
+if ENV["JULIA_PKGDIR"] != joinpath(@__DIR__, "deps")
+    # We clobber package repositories here, so don't let the user hurt themselves
+    error("Must set JULIA_PKGDIR to $(joinpath(@__DIR__, "deps"))")
+else
+    info(ENV["JULIA_PKGDIR"])
+end
 Pkg.init()
 cp(joinpath(@__DIR__, "REQUIRE"), Pkg.dir("REQUIRE"); remove_destination = true)
 Pkg.update()
