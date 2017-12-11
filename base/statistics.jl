be73121da006c359a4e1a92f1f5cb70c1e2e246f
Sacha Verweij <sacha@stanford.edu>
2017-12-10 19:56:54 -0800

    Rewrite A[ct]_(mul|ldiv|rdiv)_B[ct][!] calls in base/statistics.jl as *, /, \, mul!, ldiv!, or rdiv!.

--- base/statistics.jl
+++ base/statistics.jl
@@ -327,11 +327,11 @@ unscaled_covzm(x::AbstractMatrix, vardim::Int) = (vardim == 1 ? _conj(x'x) : x *
 
 unscaled_covzm(x::AbstractVector, y::AbstractVector) = dot(y, x)
 unscaled_covzm(x::AbstractVector, y::AbstractMatrix, vardim::Int) =
-    (vardim == 1 ? At_mul_B(x, _conj(y)) : At_mul_Bt(x, _conj(y)))
+    (vardim == 1 ? *(Transpose(x), _conj(y)) : *(Transpose(x), Transpose(_conj(y))))
 unscaled_covzm(x::AbstractMatrix, y::AbstractVector, vardim::Int) =
-    (c = vardim == 1 ? At_mul_B(x, _conj(y)) :  x * _conj(y); reshape(c, length(c), 1))
+    (c = vardim == 1 ? *(Transpose(x), _conj(y)) :  x * _conj(y); reshape(c, length(c), 1))
 unscaled_covzm(x::AbstractMatrix, y::AbstractMatrix, vardim::Int) =
-    (vardim == 1 ? At_mul_B(x, _conj(y)) : A_mul_Bc(x, y))
+    (vardim == 1 ? *(Transpose(x), _conj(y)) : *(x, Adjoint(y)))
 
 # covzm (with centered data)
 
