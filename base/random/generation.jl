dd6a94ab21dac82b2157730bbf3399f499050fc7
Rafael Fourquet <fourquet.rafael@gmail.com>
2017-12-12 11:33:00 +0100

    re-implement rand(::BitSet)

--- base/random/generation.jl
+++ base/random/generation.jl
@@ -340,6 +340,20 @@ Sampler(rng::AbstractRNG, t::Set, n::Repetition) = SamplerTag{Set}(Sampler(rng, 
 
 rand(rng::AbstractRNG, sp::SamplerTag{Set,<:Sampler}) = rand(rng, sp.data).first
 
+## random values from BitSet
+
+function Sampler(rng::AbstractRNG, t::BitSet, n::Repetition)
+    isempty(t) && throw(ArgumentError("collection must be non-empty"))
+    SamplerSimple(t, Sampler(rng, minimum(t):maximum(t), Val(Inf)))
+end
+
+function rand(rng::AbstractRNG, sp::SamplerSimple{BitSet,<:Sampler})
+    while true
+        n = rand(rng, sp.data)
+        n in sp[] && return n
+    end
+end
+
 ## random values from AbstractDict/AbstractSet
 
 # we defer to _Sampler to avoid ambiguities with a call like Sampler(rng, Set(1), Val(1))
