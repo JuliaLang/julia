a1fe16a58d59da5bccc49b0e093677c5235c942f
Tamas K. Papp <tkpapp@gmail.com>
2017-12-28 13:40:46 +0100

    Removed transpose from second permutation matrix.
    Change requested by @andreasnoack.
    
    Also mention properties of permutation matrix in docstring.

--- base/linalg/bunchkaufman.jl
+++ base/linalg/bunchkaufman.jl
@@ -43,11 +43,13 @@ end
 """
     bkfact(A, rook::Bool=false) -> BunchKaufman
 
-Compute the Bunch-Kaufman [^Bunch1977] factorization of a symmetric or Hermitian matrix `A` as ``PUDU'P'`` or ``PLDL'P'``, depending on which triangle is stored in `A`, and return a `BunchKaufman` object.
+Compute the Bunch-Kaufman [^Bunch1977] factorization of a symmetric or Hermitian matrix `A` as ``PUDU'P`` or ``PLDL'P``, depending on which triangle is stored in `A`, and return a `BunchKaufman` object.
 
 If `rook` is `true`, rook pivoting is used. If `rook` is false, rook pivoting is not used.
 
 The following functions are available for `BunchKaufman` objects: [`size`](@ref), `\\`, [`inv`](@ref), [`issymmetric`](@ref), [`ishermitian`](@ref), [`getindex`](@ref).
+
+Note that  `P` is symmetric, so ``P=P⁻¹=P'``.
 
 [^Bunch1977]: J R Bunch and L Kaufman, Some stable methods for calculating inertia and solving symmetric linear systems, Mathematics of Computation 31:137 (1977), 163-179. [url](http://www.ams.org/journals/mcom/1977-31-137/S0025-5718-1977-0428694-0/).
 
@@ -115,8 +117,8 @@ end
     getproperty(B::BunchKaufman, d::Symbol)
 
 Extract the factors of the Bunch-Kaufman factorization `B`. The factorization can take the
-two forms `P*L*D*L'*P'` or `P*U*D*U'*P'` (or `L*D*Transpose(L)` in the complex symmetric case)
-where `P` is a permutation matrix, `L` is a `UnitLowerTriangular` matrix, `U` is a
+two forms `P*L*D*L'*P` or `P*U*D*U'*P` (or `L*D*Transpose(L)` in the complex symmetric case)
+where `P` is a (symmetric) permutation matrix, `L` is a `UnitLowerTriangular` matrix, `U` is a
 `UnitUpperTriangular`, and `D` is a block diagonal symmetric or Hermitian matrix with
 1x1 or 2x2 blocks. The argument `d` can be
 
