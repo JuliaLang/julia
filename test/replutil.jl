0a98aa9db759d0b9718ccbc16e27f4031d1444e3
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2018-01-07 00:38:14 +0100

    make print_with_color emit starting and terminating ANSI sequences on each line (#25289)

--- test/replutil.jl
+++ test/replutil.jl
@@ -12,8 +12,8 @@ Base.show_method_candidates(buf, Base.MethodError(method_c1,(1, 1, "")))
 @test String(take!(buf)) == "\nClosest candidates are:\n  method_c1(!Matched::Float64, !Matched::AbstractString...)$cfile$c1line"
 @test length(methods(method_c1)) <= 3 # because of '...' in candidate printing
 Base.show_method_candidates(IOContext(buf, :color => true), Base.MethodError(method_c1,(1, 1, "")))
-@test String(take!(buf)) == "\e[0m\nClosest candidates are:\n  method_c1(\e[91m::Float64\e[39m, \e[91m::AbstractString...\e[39m)$cfile$c1line"
 
+@test String(take!(buf)) == "\n\e[0mClosest candidates are:\n\e[0m  method_c1(\e[91m::Float64\e[39m, \e[91m::AbstractString...\e[39m)$cfile$c1line"
 Base.show_method_candidates(buf, Base.MethodError(method_c1,(1, "", "")))
 @test String(take!(buf)) == "\nClosest candidates are:\n  method_c1(!Matched::Float64, ::AbstractString...)$cfile$c1line"
 
