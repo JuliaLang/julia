df91458f823cb6f7c952ac41e164ce4a18106c8c
Rafael Fourquet <fourquet.rafael@gmail.com>
2018-01-09 03:47:55 +0700

    deprecate randjump(rng, length, poly) in favor of randjump(rng, steps, length) (#25320)

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -205,10 +205,18 @@ import .LinAlg: cond
 @deprecate cond(F::LinAlg.LU, p::Integer) cond(convert(AbstractArray, F), p)
 
 # PR #21359
-import .Random: srand
+import .Random: srand, randjump
+
 @deprecate srand(r::MersenneTwister, filename::AbstractString, n::Integer=4) srand(r, read!(filename, Vector{UInt32}(uninitialized, Int(n))))
 @deprecate srand(filename::AbstractString, n::Integer=4) srand(read!(filename, Vector{UInt32}(uninitialized, Int(n))))
 @deprecate MersenneTwister(filename::AbstractString)  srand(MersenneTwister(0), read!(filename, Vector{UInt32}(uninitialized, Int(4))))
+
+function randjump(mt::MersenneTwister, jumps::Integer, jumppoly::AbstractString)
+    depwarn("`randjump(rng, jumps, jumppoly::AbstractString)` is deprecated; use `randjump(rng, steps, jumps)` instead", :randjump)
+    Base.Random._randjump(mt, dSFMT.GF2X(jumppoly), jumps)
+end
+
+@deprecate randjump(mt::MersenneTwister, jumps::Integer)  randjump(mt, big(10)^20, jumps)
 
 # PR #21974
 @deprecate versioninfo(verbose::Bool) versioninfo(verbose=verbose)
