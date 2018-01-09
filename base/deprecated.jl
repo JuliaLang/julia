f07eda663df7a57a95e44da3372d1da4d9b6e104
Sacha Verweij <sacha@stanford.edu>
2018-01-08 19:35:44 -0800

    Add a TODO to 0.7 deps.

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -188,6 +188,8 @@ end
 
 # BEGIN 0.7 deprecations
 
+# TODO: remove warning for using `_` in parse_input_line in base/client.jl
+
 @deprecate issubtype (<:)
 
 @deprecate union() Set()
@@ -2907,8 +2909,6 @@ end
 
 @deprecate substrides(s, parent, dim, I::Tuple) substrides(parent, strides(parent), I)
 
-# END 0.7 deprecations
-
 @deprecate lexcmp(x::AbstractArray, y::AbstractArray) cmp(x, y)
 @deprecate lexcmp(x::Real, y::Real)                   cmp(isless, x, y)
 @deprecate lexcmp(x::Complex, y::Complex)             cmp((real(x),imag(x)), (real(y),imag(y)))
@@ -2972,6 +2972,7 @@ end
 
 
 # END 0.7 deprecations
+
 # BEGIN 1.0 deprecations
 
 # END 1.0 deprecations
