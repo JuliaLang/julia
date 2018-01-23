d8f6033090d1f242a33e42d20c67cc918246e548
Alex Arslan <ararslan@comcast.net>
2018-01-23 13:25:45 -0800

    Change keyword arguments to regular NamedTuples
    Revert "undo breaking change to kwargs iteration order" (#25290)
    This reverts commit b90274e3a575519a46d7b87312a8d47f81ec3f1c.

--- test/keywordargs.jl
+++ test/keywordargs.jl
@@ -188,7 +188,7 @@ function test4974(;kwargs...)
     end
 end
 
-@test test4974(a=1) == (2, pairs((a=1,)))
+@test test4974(a=1) == (2, (a=1,))
 
 @testset "issue #7704, computed keywords" begin
     @test kwf1(1; :tens => 2) == 21
@@ -300,11 +300,11 @@ end
         counter += 1
         return counter
     end
-    f(args...; kws...) = (args, values(kws))
+    f(args...; kws...) = (args, kws)
     @test f(get_next(), a=get_next(), get_next(),
             b=get_next(), get_next(),
             [get_next(), get_next()]...; c=get_next(),
             (d = get_next(), f = get_next())...) ==
-                ((1, 3, 5, 6, 7),
+                ((1,3,5,6,7),
                  (a = 2, b = 4, c = 8, d = 9, f = 10))
 end
