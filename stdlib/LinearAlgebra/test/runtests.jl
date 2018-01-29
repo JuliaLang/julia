d722f2dd18b19a0fae3765fdea8ea0cead15d9d9
Kristoffer Carlsson <kcarlsson89@gmail.com>
2018-01-29 09:50:38 +0100

    fix runtest files for stdlibs [ci skip]

--- stdlib/LinearAlgebra/test/runtests.jl
+++ stdlib/LinearAlgebra/test/runtests.jl
@@ -1,5 +1,5 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
-for file in readlines(joinpath(@__DIR__, "testgroups")
+for file in readlines(joinpath(@__DIR__, "testgroups"))
     include(file * ".jl")
 end
