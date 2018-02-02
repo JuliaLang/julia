5d1480f780fe75bf10acd36899bd93a7ac6b9ece
Martin Holters <martin.holters@hsu-hh.de>
2018-02-02 14:13:22 +0100

    Improve inference of `collect` with unstable eltype
    Ensure that `promote_typejoin(::Any, ::Any)` is at worst inferred as
    `Type`, since inference of `similar(dest, ::Any)` is very vague as the
    second parameter could not only be the new element type, but also e.g.
    new size. Also ensure that `_array_for(::Type, itr, ::HasShape{N})` is
    inferable as an `Array{T,N}` with the correct `N`.

--- base/promotion.jl
+++ base/promotion.jl
@@ -125,19 +125,20 @@ Compute a type that contains both `T` and `S`, which could be
 either a parent of both types, or a `Union` if appropriate.
 Falls back to [`typejoin`](@ref).
 """
-promote_typejoin(@nospecialize(a), @nospecialize(b)) = typejoin(a, b)
-promote_typejoin(::Type{Nothing}, ::Type{T}) where {T} =
+promote_typejoin(@nospecialize(a), @nospecialize(b)) = _promote_typejoin(a, b)::Type
+_promote_typejoin(@nospecialize(a), @nospecialize(b)) = typejoin(a, b)
+_promote_typejoin(::Type{Nothing}, ::Type{T}) where {T} =
     isconcretetype(T) || T === Union{} ? Union{T, Nothing} : Any
-promote_typejoin(::Type{T}, ::Type{Nothing}) where {T} =
+_promote_typejoin(::Type{T}, ::Type{Nothing}) where {T} =
     isconcretetype(T) || T === Union{} ? Union{T, Nothing} : Any
-promote_typejoin(::Type{Missing}, ::Type{T}) where {T} =
+_promote_typejoin(::Type{Missing}, ::Type{T}) where {T} =
     isconcretetype(T) || T === Union{} ? Union{T, Missing} : Any
-promote_typejoin(::Type{T}, ::Type{Missing}) where {T} =
+_promote_typejoin(::Type{T}, ::Type{Missing}) where {T} =
     isconcretetype(T) || T === Union{} ? Union{T, Missing} : Any
-promote_typejoin(::Type{Nothing}, ::Type{Missing}) = Union{Nothing, Missing}
-promote_typejoin(::Type{Missing}, ::Type{Nothing}) = Union{Nothing, Missing}
-promote_typejoin(::Type{Nothing}, ::Type{Nothing}) = Nothing
-promote_typejoin(::Type{Missing}, ::Type{Missing}) = Missing
+_promote_typejoin(::Type{Nothing}, ::Type{Missing}) = Union{Nothing, Missing}
+_promote_typejoin(::Type{Missing}, ::Type{Nothing}) = Union{Nothing, Missing}
+_promote_typejoin(::Type{Nothing}, ::Type{Nothing}) = Nothing
+_promote_typejoin(::Type{Missing}, ::Type{Missing}) = Missing
 
 # Returns length, isfixed
 function full_va_len(p)
