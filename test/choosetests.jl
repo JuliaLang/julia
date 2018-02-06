98bddc8769cd031f18414011798c15fa92308a6f
Kristoffer Carlsson <kcarlsson89@gmail.com>
2018-02-06 15:55:29 +0100

    remove i18n

--- test/choosetests.jl
+++ test/choosetests.jl
@@ -50,7 +50,7 @@ function choosetests(choices = [])
         "errorshow", "sets", "goto", "llvmcall", "llvmcall2", "grisu",
         "some", "meta", "stacktraces", "docs",
         "misc", "threads",
-        "enums", "cmdlineargs", "i18n", "int",
+        "enums", "cmdlineargs", "int",
         "checked", "bitset", "floatfuncs", "compile", "inline",
         "boundscheck", "error", "ambiguous", "cartesian", "osutils",
         "channels", "iostream", "specificity", "codegen",
