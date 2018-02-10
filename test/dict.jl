ea96757cde444ab11527901e450d02f462550915
Milan Bouchet-Valat <nalimilan@club.fr>
2018-02-10 17:47:58 +0100

    Fix promote_type(Union{}, Missing/Nothing) and add tests
    Without this Dict(1=>missing, 2=>2) returns a Dict{Int,Any}.

--- test/dict.jl
+++ test/dict.jl
@@ -165,10 +165,12 @@ end
     @test Dict(1=>1, 2.0=>2) isa Dict{Real,Int}
     @test Dict(1=>1.0, 2.0=>2) isa Dict{Real,Real}
 
-    @test Dict(1=>1, 2=>missing) isa Dict{Int,Union{Int,Missing}}
-    @test Dict(1=>1, 2=>nothing) isa Dict{Int,Union{Int,Nothing}}
-    @test Dict(1=>1, missing=>2) isa Dict{Union{Int,Missing},Int}
-    @test Dict(1=>1, nothing=>2) isa Dict{Union{Int,Nothing},Int}
+    for T in (Nothing, Missing)
+        @test Dict(1=>1, 2=>T()) isa Dict{Int,Union{Int,T}}
+        @test Dict(1=>T(), 2=>2) isa Dict{Int,Union{Int,T}}
+        @test Dict(1=>1, T()=>2) isa Dict{Union{Int,T},Int}
+        @test Dict(T()=>1, 2=>2) isa Dict{Union{Int,T},Int}
+    end
 end
 
 @test_throws KeyError Dict("a"=>2)[Base.secret_table_token]
