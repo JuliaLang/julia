ec18ebdcc1b7ae3766b49f347dbc8125d62f71ec
Curtis Vogt <curtis.vogt@gmail.com>
2018-02-13 08:37:30 -0600

    Correct return type issue

--- base/rational.jl
+++ base/rational.jl
@@ -19,9 +19,14 @@ Rational(n::T, d::T) where {T<:Integer} = Rational{T}(n,d)
 Rational(n::Integer, d::Integer) = Rational(promote(n,d)...)
 Rational(n::Integer) = Rational(n,one(n))
 
-function divgcd(x::Integer,y::Integer)
-    g = gcd(x,y)
-    return iszero(g) ? (g, g) : (div(x,g), div(y,g))
+function divgcd(x::Integer, y::Integer)
+    g = gcd(x, y)
+    if iszero(g)
+        Tx, Ty, Tg = typeof(x), typeof(y), typeof(g)
+        return zero(promote_op(div, Tx, Tg)), zero(promote_op(div, Ty, Tg))
+    else
+        return div(x, g), div(y, g)
+    end
 end
 
 """
