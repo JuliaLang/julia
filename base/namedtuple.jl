45e9b82ae66dbdd68082d320c00028e6821f5a56
Andy Ferris <ferris.andy@gmail.com>
2018-02-15 02:30:02 +1000

    Faster conversion from `NamedTuple` to `Tuple` (#26025)

--- base/namedtuple.jl
+++ base/namedtuple.jl
@@ -113,6 +113,15 @@ function convert(::Type{NamedTuple{names,T}}, nt::NamedTuple{names}) where {name
     NamedTuple{names,T}(T(nt))
 end
 
+function Tuple(nt::NamedTuple{names}) where {names}
+    if @generated
+        return Expr(:tuple, Any[:(getfield(nt, $(QuoteNode(n)))) for n in names]...)
+    else
+        return tuple(nt...)
+    end
+end
+(::Type{T})(nt::NamedTuple) where {T <: Tuple} = convert(T, Tuple(nt))
+
 function show(io::IO, t::NamedTuple)
     n = nfields(t)
     for i = 1:n
