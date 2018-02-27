dc96ed4dd06012b1cda8337d6bdf32e0ef3c5bd5
Simon Byrne <simonbyrne@gmail.com>
2018-02-27 10:22:33 -0800

    Update abstractarray.jl

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -1,4 +1,4 @@
-ssi# This file is a part of Julia. License is MIT: https://julialang.org/license
+# This file is a part of Julia. License is MIT: https://julialang.org/license
 
 ## Basic functions ##
 
