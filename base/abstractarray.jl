e5de7b6914e899fd492d2069c26eda9c9ee321cd
Matt Bauman <mbauman@gmail.com>
2018-02-27 13:44:38 -0600

    Optimize dataids(::AbstractArray)
    I was previously calling objectid -- I should have been using pointer_from_objref.  The former is a hash, the latter is much quicker and easier to compute.

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -1529,7 +1529,7 @@ parts can specialize this method to return the concatenation of the `dataids` of
 their component parts.  A typical definition for an array that wraps a parent is
 `Base.dataids(C::CustomArray) = dataids(C.parent)`.
 """
-dataids(A::AbstractArray) = (UInt(objectid(A)),)
+dataids(A::AbstractArray) = (UInt(pointer_from_objref(A)),)
 dataids(A::Array) = (UInt(pointer(A)),)
 dataids(::AbstractRange) = ()
 dataids(x) = ()
