cde00cf7536455cf0216606e127a9da5a5835ccb
Steven G. Johnson <stevenj@mit.edu>
2018-03-01 12:22:20 -0500

    bugfixes for rsplit (for unicode and empty strings) (#26238)
    * bugfixes for rsplit (for unicode and empty strings)
    * replace isequal in tests with ==, equivalent for strings

--- base/strings/util.jl
+++ base/strings/util.jl
@@ -275,9 +275,9 @@ split(str::T, splitter::Char;
     _split(str, equalto(splitter), limit, keep, T <: SubString ? T[] : SubString{T}[])
 
 function _split(str::AbstractString, splitter, limit::Integer, keep_empty::Bool, strs::Array)
-    i = start(str)
+    @assert (i = firstindex(str)) == 1
     n = lastindex(str)
-    r = coalesce(findfirst(splitter,str), i - 1)
+    r = coalesce(findfirst(splitter,str), 0)
     if r != 0:-1
         j, k = first(r), nextind(str,last(r))
         while 0 < j <= n && length(strs) != limit-1
@@ -342,22 +342,16 @@ rsplit(str::T, splitter::Char;
   _rsplit(str, equalto(splitter), limit, keep, T <: SubString ? T[] : SubString{T}[])
 
 function _rsplit(str::AbstractString, splitter, limit::Integer, keep_empty::Bool, strs::Array)
-    i = start(str)
     n = lastindex(str)
-    r = coalesce(findlast(splitter, str), i - 1)
-    j = first(r)-1
-    k = last(r)
-    while((0 <= j < n) && (length(strs) != limit-1))
-        if i <= k
-            (keep_empty || (k < n)) && pushfirst!(strs, SubString(str,k+1,n))
-            n = j
-        end
-        (k <= j) && (j = prevind(str,j))
-        r = coalesce(findprev(splitter,str,j), 0)
-        j = first(r)-1
-        k = last(r)
+    r = coalesce(findlast(splitter, str), 0)
+    j, k = first(r), last(r)
+    while j > 0 && k > 0 && length(strs) != limit-1
+        (keep_empty || k < n) && pushfirst!(strs, SubString(str,nextind(str,k),n))
+        n = prevind(str, j)
+        r = coalesce(findprev(splitter,str,n), 0)
+        j, k = first(r), last(r)
     end
-    (keep_empty || (n > 0)) && pushfirst!(strs, SubString(str,1,n))
+    (keep_empty || n > 0) && pushfirst!(strs, SubString(str,1,n))
     return strs
 end
 #rsplit(str::AbstractString) = rsplit(str, _default_delims, 0, false)
