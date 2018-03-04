2119c2c0cadcbe705b1c07e097e599e6429fa251
Jim Garrison <jim@garrison.cc>
2018-03-04 10:48:52 -0500

    Add `@inbounds` to `cross`

--- stdlib/LinearAlgebra/src/generic.jl
+++ stdlib/LinearAlgebra/src/generic.jl
@@ -113,7 +113,7 @@ function cross(a::AbstractVector, b::AbstractVector)
     if !(length(a) == length(b) == 3)
         throw(DimensionMismatch("cross product is only defined for vectors of length 3"))
     end
-    [a[2]*b[3]-a[3]*b[2], a[3]*b[1]-a[1]*b[3], a[1]*b[2]-a[2]*b[1]]
+    @inbounds [a[2]*b[3]-a[3]*b[2], a[3]*b[1]-a[1]*b[3], a[1]*b[2]-a[2]*b[1]]
 end
 
 """
