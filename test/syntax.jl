0a188565081fc0964e7b5281994919abb1359d8c
Jeff Bezanson <jeff.bezanson@gmail.com>
2018-03-09 13:50:50 -0500

    make syntax that expands to calls always call functions in Base (#26380)
    fixes #25947

--- test/syntax.jl
+++ test/syntax.jl
@@ -1281,6 +1281,18 @@ let args = (Int, Any)
     @test >:(reverse(args)...)
 end
 
+# issue #25947
+let getindex = 0, setindex! = 1, colon = 2, vcat = 3, hcat = 4, hvcat = 5
+    a = [10,9,8]
+    @test a[2] == 9
+    @test 1:2 isa AbstractRange
+    a[1] = 1
+    @test a[1] == 1
+    @test length([1; 2]) == 2
+    @test size([0 0]) == (1, 2)
+    @test size([1 2; 3 4]) == (2, 2)
+end
+
 # issue #25020
 @test_throws ParseError Meta.parse("using Colors()")
 
