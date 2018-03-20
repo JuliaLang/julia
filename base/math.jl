43ab230fe82120d8804ade50a51efd14427a68c3
Jan Weidner <jw3126@gmail.com>
2018-03-20 21:54:49 +0100

    improve clamp documentation, fix #26520

--- base/math.jl
+++ base/math.jl
@@ -52,7 +52,7 @@ end
 """
     clamp(x, lo, hi)
 
-Return `x` if `lo <= x <= hi`. If `x < lo`, return `lo`. If `x > hi`, return `hi`. Arguments
+Return `x` if `lo <= x <= hi`. If `x > hi`, return `hi`. If `x < lo`, return `lo`. Arguments
 are promoted to a common type.
 
 ```jldoctest
@@ -61,6 +61,12 @@ julia> clamp.([pi, 1.0, big(10.)], 2., 9.)
  3.141592653589793238462643383279502884197169399375105820974944592307816406286198
  2.0
  9.0
+
+julia> clamp.([11,8,5],10,6) # an example were lo > hi
+3-element Array{Int64,1}:
+  6
+  6
+ 10
 ```
 """
 clamp(x::X, lo::L, hi::H) where {X,L,H} =
