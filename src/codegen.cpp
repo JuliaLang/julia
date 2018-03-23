45e1bd82bf6f2c29bb4fd9cadce96f189f8f0d47
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2018-03-22 17:12:32 -0700

    Add soname lookup back to codebase
    Partially reverts PR #22828

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -6956,6 +6956,7 @@ extern "C" void *jl_init_llvm(void)
     jl_page_size = jl_getpagesize();
     imaging_mode = jl_generating_output();
     jl_init_debuginfo();
+    jl_init_runtime_ccall();
 
 #ifdef USE_POLLY
     PassRegistry &Registry = *PassRegistry::getPassRegistry();
