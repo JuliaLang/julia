3a818e3afdab4cf31e3c3cd7988a22697352cd88
Tim Besard <tim.besard@gmail.com>
2018-04-04 15:48:08 +0200

    Enable logging after exit with atexit hook.
    Fixes #26687

--- stdlib/Logging/src/Logging.jl
+++ stdlib/Logging/src/Logging.jl
@@ -54,6 +54,12 @@ include("ConsoleLogger.jl")
 
 function __init__()
     global_logger(ConsoleLogger(stderr))
+    atexit() do
+        logger = global_logger()
+        if isa(logger, ConsoleLogger)
+            global_logger(ConsoleLogger(Core.stderr, min_enabled_level(logger)))
+        end
+    end
 end
 
 end
