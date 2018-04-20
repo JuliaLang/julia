367bc9688e6b5b3b9c680d49a9af0b13603da316
Jeff Bezanson <jeff.bezanson@gmail.com>
2018-04-20 14:29:06 -0400

    widen `Int8` and `Int16` to `Int` instead of `Int32` (#26859)
    This is consistent with `sum` and less arbitrary.

--- test/int.jl
+++ test/int.jl
@@ -188,15 +188,15 @@ end
     end
 end
 @testset "widen/widemul" begin
-    @test widen(UInt8(3)) === UInt32(3)
-    @test widen(UInt16(3)) === UInt32(3)
+    @test widen(UInt8(3)) === UInt(3)
+    @test widen(UInt16(3)) === UInt(3)
     @test widen(UInt32(3)) === UInt64(3)
     @test widen(UInt64(3)) === UInt128(3)
     @test widen(UInt128(3)) == 3
     @test typeof(widen(UInt128(3))) == BigInt
 
-    @test widen(Int8(-3)) === Int32(-3)
-    @test widen(Int16(-3)) === Int32(-3)
+    @test widen(Int8(-3)) === Int(-3)
+    @test widen(Int16(-3)) === Int(-3)
     @test widen(Int32(-3)) === Int64(-3)
     @test widen(Int64(-3)) === Int128(-3)
     @test widen(Int128(-3)) == -3
