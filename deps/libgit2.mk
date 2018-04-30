8770821e88b7033c631cbd8cf0598f6d121e143b
Valentin Churavy <vchuravy@users.noreply.github.com>
2018-04-30 17:25:44 -0400

    Remove indirect dependency on system ZLIB (#26888)
    * Remove indirect dependency on system ZLIB
    
    - build LLVM without ZLIB support
    - use the bundled ZLIB for libgit
    
    * backport bundled zlib patch

--- deps/libgit2.mk
+++ deps/libgit2.mk
@@ -18,7 +18,7 @@ $(BUILDDIR)/$(LIBGIT2_SRC_DIR)/build-configured: | $(build_prefix)/manifest/curl
 endif
 endif
 
-LIBGIT2_OPTS := $(CMAKE_COMMON) -DCMAKE_BUILD_TYPE=Release -DTHREADSAFE=ON
+LIBGIT2_OPTS := $(CMAKE_COMMON) -DCMAKE_BUILD_TYPE=Release -DTHREADSAFE=ON -DUSE_BUNDLED_ZLIB=ON
 ifeq ($(OS),WINNT)
 LIBGIT2_OPTS += -DWIN32=ON -DMINGW=ON
 ifneq ($(ARCH),x86_64)
@@ -71,6 +71,10 @@ $(LIBGIT2_SRC_PATH)/libgit2-ssh-loop.patch-applied: $(LIBGIT2_SRC_PATH)/source-e
 	cd $(LIBGIT2_SRC_PATH) && \
 		patch -p1 -f < $(SRCDIR)/patches/libgit2-ssh-loop.patch
 	echo 1 > $@
+$(LIBGIT2_SRC_PATH)/libgit2-bundled_zlib.patch-applied: $(LIBGIT2_SRC_PATH)/source-extracted | $(LIBGIT2_SRC_PATH)/libgit2-ssh-loop.patch-applied
+	cd $(LIBGIT2_SRC_PATH) && \
+		patch -p1 -f < $(SRCDIR)/patches/libgit2-bundled_zlib.patch
+	echo 1 > $@
 
 $(build_datarootdir)/julia/cert.pem:
 	$(JLDOWNLOAD) $(shell pwd)/cacert-2018-01-17.pem https://curl.haxx.se/ca/cacert-2018-01-17.pem
@@ -85,6 +89,7 @@ $(BUILDDIR)/$(LIBGIT2_SRC_DIR)/build-configured: \
 	$(LIBGIT2_SRC_PATH)/libgit2-mbedtls-verify.patch-applied \
 	$(LIBGIT2_SRC_PATH)/libgit2-mbedtls-fixup.patch-applied \
 	$(LIBGIT2_SRC_PATH)/libgit2-ssh-loop.patch-applied \
+	$(LIBGIT2_SRC_PATH)/libgit2-bundled_zlib.patch-applied \
 
 $(BUILDDIR)/$(LIBGIT2_SRC_DIR)/build-configured: $(LIBGIT2_SRC_PATH)/source-extracted $(build_datarootdir)/julia/cert.pem
 	mkdir -p $(dir $@)
