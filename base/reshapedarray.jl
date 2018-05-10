c1ce85a918ae6ec8d31fb6c21dcf8dc179d8ec65
Matt Bauman <mbauman@gmail.com>
2018-05-10 12:01:37 -0400

    Use the same extension system for `reshape` as `similar`.

--- base/reshapedarray.jl
+++ base/reshapedarray.jl
@@ -94,7 +94,7 @@ julia> reshape(A, 2, :)
 reshape
 
 reshape(parent::AbstractArray, dims::IntOrInd...) = reshape(parent, dims)
-reshape(parent::AbstractArray, shp::NeedsShaping) = reshape(parent, to_shape(shp))
+reshape(parent::AbstractArray, shp::Tuple{Union{Integer,OneTo}, Vararg{Union{Integer,OneTo}}}) = reshape(parent, to_shape(shp))
 reshape(parent::AbstractArray, dims::Dims)        = _reshape(parent, dims)
 
 # Allow missing dimensions with Colon():
