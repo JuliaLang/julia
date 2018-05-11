c8d78d931a582e8e6ca073af049ca32f65b37aff
Matt Bauman <mbauman@gmail.com>
2018-05-11 12:06:52 -0400

    Add `GC.-at-preserve` to the manual, explicit ref to ref Ref

--- base/c.jl
+++ base/c.jl
@@ -143,7 +143,7 @@ This function is "unsafe". Be careful to ensure that a Julia reference to
 macro should be used to protect the `array` argument from garbage collection
 within a given block of code.
 
-Calling [`Ref(array[, index])`](@ref) is generally preferable to this function as it guarantees validity.
+Calling [`Ref(array[, index])`](@ref Ref) is generally preferable to this function as it guarantees validity.
 """
 function pointer end
 
