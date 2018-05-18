336908969b6d297974f8ee4bc0b8c94d7c258c90
Harmen Stoppels <harmenstoppels@gmail.com>
2018-05-18 11:29:44 +0300

    Test if we can iterate the full range of the largest bits integer type

--- test/ranges.jl
+++ test/ranges.jl
@@ -418,12 +418,19 @@ end
     end
     @test s == 2
 
-    # loops covering the full range of smaller integer types
+    # loops covering the full range of integers
     s = 0
     for i = typemin(UInt8):typemax(UInt8)
         s += 1
     end
     @test s == 256
+
+    s = 0
+    for i = typemin(UInt):typemax(UInt)
+        i == 10 && break
+        s += 1
+    end
+    @test s == 10
 
     s = 0
     for i = typemin(UInt8):one(UInt8):typemax(UInt8)
