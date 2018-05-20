1db39efbd76a8e4b50f4a23f93d6db5a7a3c96e4
mikhail-j <mikhail.jin@gmail.com>
2018-05-20 08:29:59 -0400

    Rewrite makefile rule for $(build_datarootdir)/julia/cert.pem target as 3 targets.

--- deps/libgit2.mk
+++ deps/libgit2.mk
@@ -76,11 +76,15 @@ $(LIBGIT2_SRC_PATH)/libgit2-bundled_zlib.patch-applied: $(LIBGIT2_SRC_PATH)/sour
 		patch -p1 -f < $(SRCDIR)/patches/libgit2-bundled_zlib.patch
 	echo 1 > $@
 
-$(build_datarootdir)/julia/cert.pem:
-	$(JLDOWNLOAD) $(shell pwd)/cacert-$(MOZILLA_CACERT_VERSION).pem https://curl.haxx.se/ca/cacert-$(MOZILLA_CACERT_VERSION).pem
-	$(JLCHECKSUM) $(shell pwd)/cacert-$(MOZILLA_CACERT_VERSION).pem
+$(shell pwd)/cacert-$(MOZILLA_CACERT_VERSION).pem:
+	$(JLDOWNLOAD) $@ https://curl.haxx.se/ca/cacert-$(MOZILLA_CACERT_VERSION).pem
+	$(JLCHECKSUM) $@
+
+libgit2-install-mozilla-cacert: $(shell pwd)/cacert-$(MOZILLA_CACERT_VERSION).pem
 	mkdir -p $(build_datarootdir)/julia
-	mv $(shell pwd)/cacert-$(MOZILLA_CACERT_VERSION).pem $@
+	cp $< $(build_datarootdir)/julia/cert.pem
+
+$(build_datarootdir)/julia/cert.pem: libgit2-install-mozilla-cacert
 
 $(BUILDDIR)/$(LIBGIT2_SRC_DIR)/build-configured: \
 	$(LIBGIT2_SRC_PATH)/libgit2-mbedtls.patch-applied \
