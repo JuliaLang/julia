09c48e2e130955ce7492919e1ae783eb8bbe0a85
Simon Byrne <simonbyrne@gmail.com>
2018-05-24 15:39:21 -0700

    add docs back in

--- base/rounding.jl
+++ base/rounding.jl
@@ -110,6 +110,19 @@ function from_fenv(r::Integer)
 end
 
 """
+    setrounding(T, mode)
+
+Set the rounding mode of floating point type `T`, controlling the rounding of basic
+arithmetic functions ([`+`](@ref), [`-`](@ref), [`*`](@ref),
+[`/`](@ref) and [`sqrt`](@ref)) and type conversion. Other numerical
+functions may give incorrect or invalid values when using rounding modes other than the
+default `RoundNearest`.
+
+Note that this is currently only supported for `T == BigFloat`.
+"""
+setrounding(T::Type, mode)
+
+"""
     rounding(T)
 
 Get the current floating point rounding mode for type `T`, controlling the rounding of basic
