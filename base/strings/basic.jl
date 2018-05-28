c1e2798504c2e43bb29af497d612c00d48b78c3a
Yuri <yurivish@gmail.com>
2018-05-27 20:05:45 -0400

    fix a typo; protocoal -> protocol (#27281)

--- base/strings/basic.jl
+++ base/strings/basic.jl
@@ -128,7 +128,7 @@ Return a tuple of the character in `s` at index `i` with the index of the start
 of the following character in `s`. This is the key method that allows strings to
 be iterated, yielding a sequences of characters. If `i` is out of bounds in `s`
 then a bounds error is raised. The `iterate` function, as part of the iteration
-protocoal may assume that `i` is the start of a character in `s`.
+protocol may assume that `i` is the start of a character in `s`.
 
 See also: [`getindex`](@ref), [`checkbounds`](@ref)
 """
