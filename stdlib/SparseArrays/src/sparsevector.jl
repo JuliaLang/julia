b2866ab53f2a8b13a33b74ca0c3f8770a58738ed
Lars Ruthotto <lruthotto@users.noreply.github.com>
2018-06-11 21:45:13 -0400

    Update sparsevector.jl
    in response to issue #19426

--- stdlib/SparseArrays/src/sparsevector.jl
+++ stdlib/SparseArrays/src/sparsevector.jl
@@ -1952,3 +1952,12 @@ function fill!(A::Union{SparseVector, SparseMatrixCSC}, x)
     end
     return A
 end
+
+
+kron(v::SparseVector,A::SparseMatrixCSC) = kron(SparseMatrixCSC(v),A)
+kron(A::SparseMatrixCSC,v::SparseVector) = kron(A,SparseMatrixCSC(v))
+
+function kron(v1::SparseVector,v2::SparseVector)  
+   v = kron(SparseMatrixCSC(v1),SparseMatrixCSC(v2))
+   return SparseVector(v.n,v.nzind,v.nzval)
+end
