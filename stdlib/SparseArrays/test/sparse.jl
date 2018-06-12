4420717206c87551ad680fe2ae7add692162a0ff
Jeremy <jebej@users.noreply.github.com>
2018-06-12 16:26:47 -0400

    added sparse matrix inner product (#27470)

--- stdlib/SparseArrays/test/sparse.jl
+++ stdlib/SparseArrays/test/sparse.jl
@@ -327,6 +327,15 @@ end
     end
 end
 
+@testset "sparse Frobenius inner product" begin
+    for i = 1:5
+        A = sprand(ComplexF64,10,15,0.4)
+        B = sprand(ComplexF64,10,15,0.5)
+        @test vecdot(A,B) ≈ vecdot(Matrix(A),Matrix(B))
+    end
+    @test_throws DimensionMismatch vecdot(sprand(5,5,0.2),sprand(5,6,0.2))
+end
+
 sA = sprandn(3, 7, 0.5)
 sC = similar(sA)
 dA = Array(sA)
