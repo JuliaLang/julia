27981e01f4bd59d8cf3196f1125e2e893b3825bd
Jarrett Revels <jarrettrevels@gmail.com>
2018-06-13 16:31:10 -0400

    test REPL construction of Expr objects containing IR nodes

--- stdlib/REPL/test/repl.jl
+++ stdlib/REPL/test/repl.jl
@@ -961,3 +961,19 @@ for (line, expr) in Pair[
     buf = IOBuffer()
     @test Base.eval(REPL._helpmode(buf, line)) isa Union{Markdown.MD,Nothing}
 end
+
+# PR #27562
+fake_repl() do stdin_write, stdout_read, repl
+    repltask = @async begin
+        REPL.run_repl(repl)
+    end
+    write(stdin_write, "Expr(:call, GlobalRef(Base.Math, :float), Core.SlotNumber(1))\n")
+    readline(stdout_read)
+    @test readline(stdout_read) == "\e[0m:((Base.Math.float)(_1))"
+    write(stdin_write, "ans\n")
+    readline(stdout_read)
+    readline(stdout_read)
+    @test readline(stdout_read) == "\e[0m:((Base.Math.float)(_1))"
+    write(stdin_write, '\x04')
+    Base._wait(repltask)
+end
