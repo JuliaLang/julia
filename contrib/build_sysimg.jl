5b1ec9c2c8534353cd9e74503f7e748d3b42089d
staticfloat@gmail.com <staticfloat@gmail.com>
2018-06-17 19:08:41 -0700

    Incorporate `RalphAS`'s fix for translations units being dropped from custom system images.

--- contrib/build_sysimg.jl
+++ contrib/build_sysimg.jl
@@ -166,16 +166,20 @@ function link_sysimg(sysimg_path=nothing, cc=find_system_compiler(), debug=false
         push!(FLAGS, "-lssp")
     end
 
-    sysimg_file = "$sysimg_path.$(Libdl.dlext)"
-    info("Linking sys.$(Libdl.dlext)")
-    info("$cc $(join(FLAGS, ' ')) -o $sysimg_file $sysimg_path.o")
-    # Windows has difficulties overwriting a file in use so we first link to a temp file
-    if Sys.iswindows() && isfile(sysimg_file)
-        if success(pipeline(`$cc $FLAGS -o $sysimg_path.tmp $sysimg_path.o`; stdout=stdout, stderr=stderr))
-            mv("$sysimg_path.tmp", sysimg_file; force=true)
-        end
+    if Sys.isapple()
+        whole_archive = "-Wl,-all_load"
+        no_whole_archive = ""
     else
-        run(`$cc $FLAGS -o $sysimg_file $sysimg_path.o`)
+        whole_archive = "-Wl,--whole-archive"
+        no_whole_archive = "-Wl,--no-whole-archive"
+    end
+
+    sysimg_file = "$sysimg_path.$(Libdl.dlext)"
+    cc_cmd = `$cc $FLAGS -o $sysimg_path.tmp $whole_archive $sysimg_path.o $no_whole_archive`
+    @info("Linking sys.$(Libdl.dlext) with $cc_cmd")
+    # Windows has difficulties overwriting a file in use so we first link to a temp file
+    if success(pipeline(cc_cmd; stdout=stdout, stderr=stderr))
+        mv("$sysimg_path.tmp", sysimg_file; force=true)
     end
     @info("System image successfully built at $sysimg_path.$(Libdl.dlext)")
     return
