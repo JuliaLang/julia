0bc6e803cc8013d36620808b8e4d35723a9aa17e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-22 13:05:42 -0400

    add tests for `delete(::NamedTuple, ::Symbol)`

--- test/namedtuple.jl
+++ test/namedtuple.jl
@@ -206,6 +206,8 @@ abstr_nt_22194_3()
 @test Base.delete((a=1, b=2, z=20), (:b,)) == (a=1, z=20)
 @test Base.delete((a=1, b=2, z=20), (:b, :q, :z)) == (a=1,)
 @test Base.delete((a=1, b=2, z=20), (:b, :q, :z, :a)) == NamedTuple()
+@test Base.delete((a=1, b=2), :b) == (a=1,)
+@test Base.delete((a=1, b=2), :a) == (b=2,)
 
 @test Base.structdiff((a=1, b=2), (b=3,)) == (a=1,)
 @test Base.structdiff((a=1, b=2, z=20), (b=3,)) == (a=1, z=20)
