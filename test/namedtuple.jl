14dbfd270ae15fea33efbaf78e3f0b54790f6f17
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-23 09:51:52 -0400

    use recommended  @inferred test

--- test/namedtuple.jl
+++ test/namedtuple.jl
@@ -209,9 +209,8 @@ abstr_nt_22194_3()
 @test Base.delete((a=1, b=2), :b) == (a=1,)
 @test Base.delete((a=1, b=2), :a) == (b=2,)
 @test Base.delete((a=1, b=2), :z) == (a=1, b=2)
-@inferrable Base.delete((a=1, b=2), (:b,))
-@inferrable Base.delete((a=1, b=2), :b,)
-
+f(a) = Base.delete(a, (:b,))
+@inferred f((a=1, b=2))
 
 @test Base.structdiff((a=1, b=2), (b=3,)) == (a=1,)
 @test Base.structdiff((a=1, b=2, z=20), (b=3,)) == (a=1, z=20)
