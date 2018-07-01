7cf81b9e51b27e1450d0c757147b3223a52ef7c9
Jeff Bezanson <jeff.bezanson@gmail.com>
2018-07-01 13:28:05 -0400

    remove subarray.jl from compiler image; it's unused

--- base/sort.jl
+++ base/sort.jl
@@ -5,8 +5,8 @@ module Sort
 import ..@__MODULE__, ..parentmodule
 const Base = parentmodule(@__MODULE__)
 using .Base.Order
-using .Base: copymutable, LinearIndices, IndexStyle, viewindexing, IndexLinear, _length, (:),
-    eachindex, axes, first, last, similar, start, next, done, zip, @views, OrdinalRange,
+using .Base: copymutable, LinearIndices, _length, (:),
+    eachindex, axes, first, last, similar, start, next, done, zip, OrdinalRange,
     AbstractVector, @inbounds, AbstractRange, @eval, @inline, Vector, @noinline,
     AbstractMatrix, AbstractUnitRange, isless, identity, eltype, >, <, <=, >=, |, +, -, *, !,
     extrema, sub_with_overflow, add_with_overflow, oneunit, div, getindex, setindex!,
