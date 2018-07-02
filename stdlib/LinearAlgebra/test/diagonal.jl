80db579cf938a75cbb1edf0dbe90fc27d85210ac
Tamas K. Papp <tkpapp@gmail.com>
2018-07-02 16:02:45 +0200

    Add eigmin(::Diagonal). (#27863)
    Fixes #27847 by extenting `eigmin` to `AbstractMatrix`.
    
    This requires that `eigvals` accepts `permute` and `scale`, which are
    then ignored.

--- stdlib/LinearAlgebra/test/diagonal.jl
+++ stdlib/LinearAlgebra/test/diagonal.jl
@@ -272,12 +272,22 @@ srand(1)
         @test svdvals(D) == s
         @test svd(D).V == V
     end
+
 end
 
 @testset "svdvals and eigvals (#11120/#11247)" begin
     D = Diagonal(Matrix{Float64}[randn(3,3), randn(2,2)])
     @test sort([svdvals(D)...;], rev = true) ≈ svdvals([D.diag[1] zeros(3,2); zeros(2,3) D.diag[2]])
     @test [eigvals(D)...;] ≈ eigvals([D.diag[1] zeros(3,2); zeros(2,3) D.diag[2]])
+
+end
+
+@testset "eigmin (#27847)" begin
+    for _ in 1:100
+        d = randn(rand(1:10))
+        D = Diagonal(d)
+        @test eigmin(D) == minimum(d)
+    end
 end
 
 @testset "isposdef" begin
