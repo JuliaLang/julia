a9da9f57e216446341082ae806449f4a5309d2ff
Keno Fischer <keno@alumni.harvard.edu>
2018-07-06 15:57:13 -0400

    Restore a couple small cases in stmt_effect_free
    As noted in #27912

--- base/compiler/ssair/queries.jl
+++ base/compiler/ssair/queries.jl
@@ -19,13 +19,7 @@ function stmt_effect_free(@nospecialize(stmt), src, spvals::SimpleVector)
         ea = e.args
         if head === :call
             f = argextype(ea[1], src, spvals)
-            if isa(f, Const)
-                f = f.val
-            elseif isType(f)
-                f = f.parameters[1]
-            else
-                return false
-            end
+            f = isa(f, Const) ? f.val : isType(f) ? f.parameters[1] : return false
             f === return_type && return true
             # TODO: This needs significant refinement
             contains_is(_PURE_BUILTINS, f) || return false
