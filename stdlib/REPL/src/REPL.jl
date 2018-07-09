67c19eb9e21ee8444e4ea38dcc5138a6aac75bf0
Jamie Brandon <jamie@scattered-thoughts.net>
2018-07-09 14:53:26 +0100

    Support context module in replcompletions (#26930)
    * Support context module in replcompletions
    
    * Add more useful info to completions
    
    * Make a Completion subtype for packages

--- stdlib/REPL/src/REPL.jl
+++ stdlib/REPL/src/REPL.jl
@@ -341,7 +341,7 @@ function complete_line(c::REPLCompletionProvider, s)
     partial = beforecursor(s.input_buffer)
     full = LineEdit.input_string(s)
     ret, range, should_complete = completions(full, lastindex(partial))
-    return ret, partial[range], should_complete
+    return map(completion_text, ret), partial[range], should_complete
 end
 
 function complete_line(c::ShellCompletionProvider, s)
@@ -349,14 +349,14 @@ function complete_line(c::ShellCompletionProvider, s)
     partial = beforecursor(s.input_buffer)
     full = LineEdit.input_string(s)
     ret, range, should_complete = shell_completions(full, lastindex(partial))
-    return ret, partial[range], should_complete
+    return map(completion_text, ret), partial[range], should_complete
 end
 
 function complete_line(c::LatexCompletions, s)
     partial = beforecursor(LineEdit.buffer(s))
     full = LineEdit.input_string(s)
     ret, range, should_complete = bslash_completions(full, lastindex(partial))[2]
-    return ret, partial[range], should_complete
+    return map(completion_text, ret), partial[range], should_complete
 end
 
 mutable struct REPLHistoryProvider <: HistoryProvider
