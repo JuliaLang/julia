830bbcec2b1acba952b9da53b0d46d87cecd9d20
Kristoffer Carlsson <kcarlsson89@gmail.com>
2018-07-10 15:48:56 +0200

    fixup testing to be more isolated from env variables
    (cherry picked from commit 84b7e726357d2f212599eb9cc66a37112696da72)
    (cherry picked from commit b0c9df9aaea2f03e4469001a6c12dd16492178a7)

--- stdlib/Pkg/test/utils.jl
+++ stdlib/Pkg/test/utils.jl
@@ -15,11 +15,13 @@ function temp_pkg_dir(fn::Function)
         empty!(DEPOT_PATH)
         Base.HOME_PROJECT[] = nothing
         Base.ACTIVE_PROJECT[] = nothing
-        mktempdir() do env_dir
-            mktempdir() do depot_dir
-                push!(LOAD_PATH, "@", "@v#.#", "@stdlib")
-                push!(DEPOT_PATH, depot_dir)
-                fn(env_dir)
+        withenv("JULIA_PROJECT" => nothing, "JULIA_LOAD_PATH" => nothing) do
+            mktempdir() do env_dir
+                mktempdir() do depot_dir
+                    push!(LOAD_PATH, "@", "@v#.#", "@stdlib")
+                    push!(DEPOT_PATH, depot_dir)
+                    fn(env_dir)
+                end
             end
         end
     finally
