df8d519fc0c9eecdad8c00e1c73635d138304864
Katharine Hyatt <khyatt@flatironinstitute.org>
2018-07-11 13:37:53 -0400

    Document CompositeException and add more xrefs

--- base/task.jl
+++ base/task.jl
@@ -26,6 +26,14 @@ function showerror(io::IO, ce::CapturedException)
     showerror(io, ce.ex, ce.processed_bt, backtrace=true)
 end
 
+"""
+    CompositeException
+
+Wrap a `Vector` of exceptions thrown by a [`Task`](@ref) (e.g. generated from a remote worker over a channel
+or an asynchronously executing local I/O write or a remote worker under `pmap`) with information about the series of exceptions.
+For example, if a group of workers are executing several tasks, and multiple workers fail, the resulting `CompositeException` will
+contain a "bundle" of information from each worker indicating where and why the exception(s) occurred.
+"""
 struct CompositeException <: Exception
     exceptions::Vector{Any}
     CompositeException() = new(Any[])
