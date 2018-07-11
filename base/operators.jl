ab5fd17f4460ac1cb0beaaae4b3b4d101b93803c
Seth Bromberger <sbromberger@users.noreply.github.com>
2018-07-11 10:56:58 -0700

    widen: always double fixed-size integers < 128-bit (#28045)

--- base/operators.jl
+++ base/operators.jl
@@ -780,6 +780,9 @@ If `x` is a type, return a "larger" type, defined so that arithmetic operations
 `+` and `-` are guaranteed not to overflow nor lose precision for any combination
 of values that type `x` can hold.
 
+For fixed-size integer types less than 128 bits, `widen` will return a type with
+twice the number of bits.
+
 If `x` is a value, it is converted to `widen(typeof(x))`.
 
 # Examples
