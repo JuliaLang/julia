b57a2a08eccaa389da2c74b1f470e91c67a5c31f
Matt Bauman <mbauman@gmail.com>
2018-07-20 14:22:08 -0400

    slightly better abstract definition
    but not reliably inferable for nested structures

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -1529,7 +1529,14 @@ parts can specialize this method to return the concatenation of the `dataids` of
 their component parts.  A typical definition for an array that wraps a parent is
 `Base.dataids(C::CustomArray) = dataids(C.parent)`.
 """
-dataids(A::AbstractArray) = (UInt(pointer_from_objref(A)),)
+function dataids(A::AbstractArray)
+    @inline
+    ids = _splatmap(dataids, ntuple(i -> getfield(A, i), Val(nfields(A))))
+    if !isimmutable(A)
+        ids = (UInt(pointer_from_objref(A)), ids...)
+    end
+    return ids
+end
 dataids(A::Array) = (UInt(pointer(A)),)
 dataids(::AbstractRange) = ()
 dataids(x) = ()
