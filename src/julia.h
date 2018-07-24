956b0853aa19034c7857ad0c91836e65084b796f
Nathan Daly <NHDaly@gmail.com>
2018-07-24 16:04:58 -0400

    Add comments to julia.h documenting preconditions
    Add comments to both jl_set_global and jl_define_const documenting their effects and preconditions.

--- src/julia.h
+++ src/julia.h
@@ -1325,9 +1325,11 @@ JL_DLLEXPORT int jl_defines_or_exports_p(jl_module_t *m, jl_sym_t *var);
 JL_DLLEXPORT int jl_binding_resolved_p(jl_module_t *m, jl_sym_t *var);
 JL_DLLEXPORT int jl_is_const(jl_module_t *m, jl_sym_t *var);
 JL_DLLEXPORT jl_value_t *jl_get_global(jl_module_t *m JL_PROPAGATES_ROOT, jl_sym_t *var);
+// Set val on global var. NOTE: var must not be a const variable.
 JL_DLLEXPORT void jl_set_global(jl_module_t *m JL_ROOTING_ARGUMENT,
                                 jl_sym_t *var,
                                 jl_value_t *val JL_ROOTED_ARGUMENT);
+// Define a new const global variable. NOTE: var must not already exist.
 JL_DLLEXPORT void jl_define_const(jl_module_t *m JL_ROOTING_ARGUMENT,
                                jl_sym_t *var,
                                jl_value_t *val JL_ROOTED_ARGUMENT);
