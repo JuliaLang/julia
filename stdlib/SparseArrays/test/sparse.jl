b0b36886d7723de18e9c4f35bf67e5f0a34b3551
Klaus Crusius <klaus.crusius@web.de>
2018-08-07 18:22:12 +0200

    merge with master 2

--- stdlib/SparseArrays/test/sparse.jl
+++ stdlib/SparseArrays/test/sparse.jl
@@ -1553,10 +1553,10 @@ end
     @test Array(tril(A,1)) == tril(AF,1)
     @test Array(triu!(copy(A), 2)) == triu(AF,2)
     @test Array(tril!(copy(A), 2)) == tril(AF,2)
-    @test ArgumentError tril(A, -n - 2) == zero(A)
-    @test ArgumentError tril(A, n) == A
-    @test ArgumentError triu(A, -n) == A
-    @test ArgumentError triu(A, n + 2) == zero(A)
+    @test tril(A, -n - 2) == zero(A)
+    @test tril(A, n) == A
+    @test triu(A, -n) == A
+    @test triu(A, n + 2) == zero(A)
 
     # fkeep trim option
     @test isequal(length(tril!(sparse([1,2,3], [1,2,3], [1,2,3], 3, 4), -1).rowval), 0)
