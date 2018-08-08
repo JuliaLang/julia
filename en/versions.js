576098574bdf119c8cb12b3e85a4f6165405f8dd
autodocs <autodocs>
2018-08-08 08:32:20 +0000

    build based on a4cb80f

--- en/versions.js
+++ en/versions.js
@@ -1,10 +1,12 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.7",
   "release-0.6",
   "release-0.5",
   "release-0.4",
   "release-0.3",
+  "v0.7.0",
   "v0.6.4",
   "v0.6.3",
   "v0.6.2",
