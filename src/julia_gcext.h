151dc7d7de48479d5fb98228fb66a98109c205b6
Reimer Behrends <behrends@gmail.com>
2018-08-23 08:59:06 +0200

    Improve comments for the GC extension functionality.

--- src/julia_gcext.h
+++ src/julia_gcext.h
@@ -3,9 +3,7 @@
 
 // requires including "julia.h" beforehand.
 
-// Kinds of callbacks. For each kind of callback, there must be
-// a corresponding type with the same name, but in all lowercase,
-// and with a "_t" suffix.
+// Callbacks that allow C code to hook into the GC.
 
 typedef void (*jl_gc_cb_root_scanner_t)(int full);
 typedef void (*jl_gc_cb_task_scanner_t)(jl_task_t *task, int full);
@@ -23,9 +21,7 @@ JL_DLLEXPORT void jl_gc_set_cb_notify_external_alloc(jl_gc_cb_notify_external_al
 JL_DLLEXPORT void jl_gc_set_cb_notify_external_free(jl_gc_cb_notify_external_free_t cb,
         int enable);
 
-// Types for mark and sweep functions.
-// We make the cache and sp parameters opaque so that the internals
-// do not get exposed.
+// Types for custom mark and sweep functions.
 typedef uintptr_t (*jl_markfunc_t)(jl_ptls_t, jl_value_t *obj);
 typedef void (*jl_sweepfunc_t)(jl_value_t *obj);
 
@@ -52,8 +48,9 @@ typedef struct {
     jl_sweepfunc_t sweepfunc;
 } jl_fielddescdyn_t;
 
-// Allocate an object with a foreign type.
+// Allocate an object of a foreign type.
 JL_DLLEXPORT void *jl_gc_alloc_typed(jl_ptls_t ptls, size_t sz, void *ty);
+
 // Queue an object or array of objects for scanning by the garbage collector.
 // These functions must only be called from within a root scanner callback
 // or from within a custom mark function.
@@ -61,22 +58,25 @@ JL_DLLEXPORT int jl_gc_mark_queue_obj(jl_ptls_t ptls, jl_value_t *obj);
 JL_DLLEXPORT void jl_gc_mark_queue_objarray(jl_ptls_t ptls, jl_value_t *parent,
     jl_value_t **objs, size_t nobjs);
 
-// Calling this function on an object of a foreign type will cause the
-// sweep function to be called during garbage collection. This function
-// must be called at most once per object. If not enabled for an object,
-// the sweep function will not be called. Normally, this should happen
-// right after allocating such an object.
+// Sweep functions will not automatically be called for objects of
+// foreign types, as that may not always be desired. Only calling
+// jl_gc_schedule_foreign_sweepfunc() on an object of a foreign type
+// will result in the custome sweep function actually being called.
+// This must be done at most once per object and should usually be
+// done right after allocating the object.
 JL_DLLEXPORT void jl_gc_schedule_foreign_sweepfunc(jl_ptls_t ptls, jl_value_t * bj);
 
-// CAUTION: The following functions enable support for conservative
-// marking. Conservative marking will treat any machine word that points
-// to an object (potentially including its interior) as a valid
-// reference, regardless of whether it actually is one or just an
-// integer that happens to match an actual address.
-//
-// This is a sharp tool and should only be used as a measure of last
-// resort. The user should be familiar with the risk of memory leaks
-// (especially on 32-bit machines) if used inappropriately and how
+// The following functions enable support for conservative marking. This
+// functionality allows the user to determine if a machine word can be
+// interpreted as a pointer to an object (including the interior of an
+// object). It can be used to, for example, scan foreign stack frames or
+// data structures with an unknown layout. It is called conservative
+// marking, because it can lead to false positives, as non-pointer data
+// can mistakenly be interpreted as a pointer to a Julia object.
+
+// CAUTION: This is a sharp tool and should only be used as a measure of
+// last resort. The user should be familiar with the risk of memory
+// leaks (especially on 32-bit machines) if used inappropriately and how
 // optimizing compilers can hide references from conservative stack
 // scanning. In particular, arrays must be kept explicitly visible to
 // the GC (by using JL_GC_PUSH1(), storing them in a location marked by
@@ -84,7 +84,7 @@ JL_DLLEXPORT void jl_gc_schedule_foreign_sweepfunc(jl_ptls_t ptls, jl_value_t * 
 // reason is that array contents aren't marked separately, so if the
 // object itself is not visible to the GC, neither are the contents.
 
-// Enable support for conservative scanning. The function returns
+// Enable support for conservative marking. The function returns
 // whether support was already enabled. The function may implicitly
 // trigger a full garbage collection to properly update all internal
 // data structures.
@@ -96,7 +96,8 @@ JL_DLLEXPORT int jl_gc_enable_conservative_gc_support(void);
 JL_DLLEXPORT int jl_gc_conservative_gc_support_enabled(void);
 
 // Returns the base address of a memory block, assuming it is stored in
-// a julia memory pool. Return NULL otherwise.
+// a julia memory pool. Return NULL otherwise. Conservative support
+// *must* have been enabled for this to work reliably.
 //
 // NOTE: This will only work for internal pool allocations. For external
 // allocations, the user must track allocations using the notification
@@ -105,6 +106,5 @@ JL_DLLEXPORT int jl_gc_conservative_gc_support_enabled(void);
 // the user *must* validate that they have a proper type, i.e. that
 // jl_typeof(obj) is an actual type object.
 JL_DLLEXPORT jl_value_t *jl_gc_internal_obj_base_ptr(void *p);
-
 
 #endif // _JULIA_GCEXT_H
