75b052f1091993753a7886f97a647b051c58ff3a
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-08-24 21:12:14 +0200

    Absolutify project path specified with --project and JULIA_PROJECT. (#28625)
    (cherry picked from commit eb8a9333b040b8dc86b2ed7a93bb23e70830cf14)

--- src/init.c
+++ src/init.c
@@ -592,6 +592,8 @@ static void jl_resolve_sysimg_location(JL_IMAGE_SEARCH rel)
         jl_options.outputbc = abspath(jl_options.outputbc, 0);
     if (jl_options.machine_file)
         jl_options.machine_file = abspath(jl_options.machine_file, 0);
+    if (jl_options.project && strncmp(jl_options.project, "@.", strlen(jl_options.project)) != 0)
+        jl_options.project = abspath(jl_options.project, 0);
 
     const char **cmdp = jl_options.cmds;
     if (cmdp) {
