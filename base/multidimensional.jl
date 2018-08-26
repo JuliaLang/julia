49e90cb2fce43aaafce3599a516a03e89e5215a9
des2 <des2@cisoc.com>
2018-08-26 11:18:36 +0530

    Updated files for Multidimensioanl.jl

--- base/multidimensional.jl
+++ base/multidimensional.jl
@@ -652,12 +652,20 @@ end
     end
 end
 
+function diff(a::AbstractVector)
+    @assert !has_offset_axes(a)
+    [ a[i+1] - a[i] for i=1:length(a)-1 ]
+end
+
 function diff(a::AbstractArray)
     @assert !has_offset_axes(a)
     [ a[i+1] - a[i] for i=1:length(a)-1 ]
 end
 
 """
+    diff(A::AbstractVector)
+    diff(A::AbstractMatrix; dims::Integer)
+
     diff(A::AbstractArray)
     diff(A::AbstractArray; dims::Integer)
 
@@ -683,6 +691,16 @@ julia> diff(vec(a))
  12
 ```
 """
+function diff(A::AbstractMatrix; dims::Integer)
+    if dims == 1
+        [A[i+1,j] - A[i,j] for i=1:size(A,1)-1, j=1:size(A,2)]
+    elseif dims == 2
+        [A[i,j+1] - A[i,j] for i=1:size(A,1), j=1:size(A,2)-1]
+    else
+        throw(ArgumentError("dimension must be 1 or 2, got $dims"))
+    end
+end
+
 function diff(A::AbstractArray; dims::Integer)
     if dims == 1
         [A[i+1,j] - A[i,j] for i=1:size(A,1)-1, j=1:size(A,2)]
