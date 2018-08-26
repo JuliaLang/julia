4a5de482b0fd5b52883769f0e217d92e79832b5e
staticfloat <staticfloat@gmail.com>
2018-08-26 15:49:30 -0700

    Add `dlclose(::Nothing)` that does nothing

--- stdlib/Libdl/src/Libdl.jl
+++ stdlib/Libdl/src/Libdl.jl
@@ -114,7 +114,8 @@ end
 """
     dlopen_e(libfile::AbstractString [, flags::Integer])
 
-Similar to [`dlopen`](@ref), except returns a `NULL` pointer instead of raising errors.  It is preferred to directly call dlopen(libfile, flags; throw_error=false)`
+Similar to [`dlopen`](@ref), except returns a `NULL` pointer instead of raising errors.
+It is preferred to directly call dlopen(libfile, flags; throw_error=false)`
 """
 dlopen_e(args...) = dlopen(args...; throw_error=false)
 
@@ -125,6 +126,25 @@ Close shared library referenced by handle.
 """
 function dlclose(p::Ptr)
     0 == ccall(:jl_dlclose, Cint, (Ptr{Cvoid},), p)
+end
+
+"""
+    dlclose(::Nothing)
+
+For the very common pattern usage pattern of
+
+    try
+        hdl = dlopen(library_name)
+        ... do something
+    finally
+        dlclose(hdl)
+    end
+
+We define a `dlclose()` method that accepts a parameter of type `Nothing`, so
+that user code does not have to change its behavior for the case that `library_name`
+was not found.
+"""
+function dlclose(p::Nothing)
 end
 
 """
