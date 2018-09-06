059e36dd430b934d446aafaa455e1c8b56dcc072
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2018-09-06 14:03:05 -0400

    work around a splatting penalty in twiceprecision (#29060)
    * work around a splatting penalty in twiceprecision
    
    * add allocation test
    
    (cherry picked from commit 88d536a1ea8903dfdfd78f86a5be75eedcfddf5a)

--- test/ranges.jl
+++ test/ranges.jl
@@ -1371,3 +1371,10 @@ end # module NonStandardIntegerRangeTest
         end
     end
 end
+
+@testset "allocation of TwicePrecision call" begin
+    0:286.493442:360
+    0:286:360
+    @test @allocated(0:286.493442:360) == 0
+    @test @allocated(0:286:360) == 0
+end
