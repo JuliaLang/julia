3b74735923bdb4429125936abd6c03e92ac015dd
Rafael Fourquet <fourquet.rafael@gmail.com>
2018-09-26 14:56:28 +0200

    REPL: make paste work in search mode (fix #29347)

--- stdlib/REPL/src/REPL.jl
+++ stdlib/REPL/src/REPL.jl
@@ -620,7 +620,7 @@ function history_search(hist::REPLHistoryProvider, query_buffer::IOBuffer, respo
     # FIXME: I'm pretty sure this is broken since it uses an index
     # into the search data to index into the response string
     b = a + sizeof(searchdata)
-    b = b ≤ ncodeunits(response_str) ? prevind(response_str, b) : b-1
+    b = b ≤ ncodeunits(response_str) ? prevind(response_str, b) : b-1
     b = min(lastindex(response_str), b) # ensure that b is valid
 
     !skip_current && searchdata == response_str[a:b] && return true
