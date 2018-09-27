4e378ba66d2a38fa976714b3d5e84d6e9bc08e2d
Luke Adams <adamslc@whitman.edu>
2018-09-27 10:42:55 -0600

    update docs

--- stdlib/Logging/src/ConsoleLogger.jl
+++ stdlib/Logging/src/ConsoleLogger.jl
@@ -12,9 +12,9 @@ Log levels less than `min_level` are filtered out.
 Message formatting can be controlled by setting keyword arguments:
 
 * `meta_formatter` is a function which takes the log event metadata
-  `(level, _module, group, id, file, line)` and returns a color (as would be
-  passed to printstyled), prefix and suffix for the log message.  The
-  default is to prefix with the log level and a suffix containing the module,
+  `(level, _module, group, id, file, line)` and returns a prefix color (as would
+  be passed to printstyled), prefix, suffix color, and suffix for the log message.
+  The default is to prefix with the log level and a suffix containing the module,
   file and line location.
 * `show_limited` limits the printing of large data structures to something
   which can fit on the screen by setting the `:limit` `IOContext` key during
