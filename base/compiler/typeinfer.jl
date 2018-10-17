abc6654ce57673efe2eda3d57f6bf62cfd1ba630
Jameson Nash <vtjnash@gmail.com>
2018-10-17 17:49:10 -0400

    interpreter: in phi node, `val` may be explicitly null
    previously, we only handled the case where it was implicitly null

--- base/compiler/typeinfer.jl
+++ base/compiler/typeinfer.jl
@@ -529,6 +529,7 @@ end
 
 # compute (and cache) an inferred AST and return type
 function typeinf_ext(linfo::MethodInstance, params::Params)
+    method = linfo.def::Method
     for i = 1:2 # test-and-lock-and-test
         i == 2 && ccall(:jl_typeinf_begin, Cvoid, ())
         if isdefined(linfo, :inferred)
@@ -538,7 +539,6 @@ function typeinf_ext(linfo::MethodInstance, params::Params)
             if min_world(linfo) <= params.world <= max_world(linfo)
                 inf = linfo.inferred
                 if invoke_api(linfo) == 2
-                    method = linfo.def::Method
                     tree = ccall(:jl_new_code_info_uninit, Ref{CodeInfo}, ())
                     tree.code = Any[ Expr(:return, quoted(linfo.inferred_const)) ]
                     tree.method_for_inference_limit_heuristics = nothing
@@ -610,17 +610,22 @@ end
         # method lambda - infer this specialization via the method cache
         return typeinf_ext(linfo, Params(world))
     else
-        # toplevel lambda - infer directly
-        ccall(:jl_typeinf_begin, Cvoid, ())
-        result = InferenceResult(linfo)
-        frame = InferenceState(result, linfo.inferred::CodeInfo,
-                               #=cached=#true, Params(world))
-        typeinf(frame)
-        ccall(:jl_typeinf_end, Cvoid, ())
-        @assert frame.inferred # TODO: deal with this better
-        @assert frame.linfo === linfo
-        linfo.rettype = widenconst(frame.bestguess)
-        return svec(linfo, frame.src)
+        src = linfo.inferred::CodeInfo
+        if !src.inferred
+            # toplevel lambda - infer directly
+            ccall(:jl_typeinf_begin, Cvoid, ())
+            if !src.inferred
+                result = InferenceResult(linfo)
+                frame = InferenceState(result, src, #=cached=#true, Params(world))
+                typeinf(frame)
+                @assert frame.inferred # TODO: deal with this better
+                @assert frame.linfo === linfo
+                linfo.rettype = widenconst(frame.bestguess)
+                src = frame.src
+            end
+            ccall(:jl_typeinf_end, Cvoid, ())
+        end
+        return svec(linfo, src)
     end
 end
 
