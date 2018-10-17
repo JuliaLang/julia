03ecf8195bf73201056abb1ae0bfded442d37e97
Andreas Noack <andreas@noack.dk>
2018-10-17 10:41:58 +0200

    Use a stable inverse for Tridiagonal and SymTridiagonal (#29667)
    * Use a stable inverse for Tridiagonal and SymTridiagonal
    
    Fixes #29630
    
    * Fix ldlt in the complex case
    
    (cherry picked from commit 00bb4f2b8591168470be76d8b544ef6812076356)

--- stdlib/LinearAlgebra/test/tridiag.jl
+++ stdlib/LinearAlgebra/test/tridiag.jl
@@ -391,4 +391,24 @@ end
           "LinearAlgebra.LU{Float64,LinearAlgebra.Tridiagonal{Float64,SparseArrays.SparseVector")
 end
 
+@testset "Issue 29630" begin
+    function central_difference_discretization(N; dfunc = x -> 12x^2 - 2N^2,
+                                               dufunc = x -> N^2 + 4N*x,
+                                               dlfunc = x -> N^2 - 4N*x,
+                                               bfunc = x -> 114ℯ^-x * (1 + 3x),
+                                               b0 = 0, bf = 57/ℯ,
+                                               x0 = 0, xf = 1)
+        h = 1/N
+        d, du, dl, b = map(dfunc, (x0+h):h:(xf-h)), map(dufunc, (x0+h):h:(xf-2h)),
+                       map(dlfunc, (x0+2h):h:(xf-h)), map(bfunc, (x0+h):h:(xf-h))
+        b[1] -= dlfunc(x0)*b0     # subtract the boundary term
+        b[end] -= dufunc(xf)*bf   # subtract the boundary term
+        Tridiagonal(dl, d, du), b
+    end
+
+    A90, b90 = central_difference_discretization(90)
+
+    @test A90\b90 ≈ inv(A90)*b90
+end
+
 end # module TestTridiagonal
