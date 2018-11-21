72fdb2991309cb1956130f8bb728e8aef9f75af9
chethega <chethega@gmail.com>
2018-11-21 22:42:36 +0100

    also skip jl_arrayunset for isbitsunion

--- base/dict.jl
+++ base/dict.jl
@@ -621,8 +621,8 @@ end
 
 function _delete!(h::Dict{K,V}, index) where {K,V}
     @inbounds h.slots[index] = 0x2
-    isbitstype(K) || ccall(:jl_arrayunset, Cvoid, (Any, UInt), h.keys, index-1)
-    isbitstype(V) || ccall(:jl_arrayunset, Cvoid, (Any, UInt), h.vals, index-1)
+    isbitstype(K) || || isbitsunion(K) || ccall(:jl_arrayunset, Cvoid, (Any, UInt), h.keys, index-1)
+    isbitstype(V) || isbitsunion(V) || ccall(:jl_arrayunset, Cvoid, (Any, UInt), h.vals, index-1)
     h.ndel += 1
     h.count -= 1
     h.age += 1
