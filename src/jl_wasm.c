fcd87387b20a68b1f732a4e82bce7941732bb601
SimonDanisch <sdanisch@gmail.com>
2018-11-30 17:18:12 +0100

    fix jl_vprintf

--- src/jl_wasm.c
+++ src/jl_wasm.c
@@ -1,4 +1,4 @@
-#include "julia.h" 
+#include "julia.h"
 #include <stdio.h>
 
 JL_DLLEXPORT int jl_printf(JL_STREAM *s, const char *format, ...)
@@ -7,10 +7,20 @@ JL_DLLEXPORT int jl_printf(JL_STREAM *s, const char *format, ...)
     int c;
 
     va_start(args, format);
-	vprintf(format, args);
+	c = vprintf(format, args);
     va_end(args);
     return c;
 }
+JL_DLLEXPORT int jl_vprintf(JL_STREAM *s, const char *format, va_list args)
+{
+    int c;
+	va_list _args;
+	va_copy(_args, args);
+	c = vprintf(format, _args);
+    va_end(_args);
+    return c;
+}
+
 
 JL_DLLEXPORT void JL_NORETURN jl_exit(int status)
 {
