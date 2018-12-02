6959465cb8befc07780b722051d70d7d38f8dc81
Milan Bouchet-Valat <nalimilan@club.fr>
2018-12-02 14:33:07 +0100

    Add missing backticks and improve message
    [ci skip]

--- base/abstractarraymath.jl
+++ base/abstractarraymath.jl
@@ -420,7 +420,7 @@ eachrow(A::AbstractVecOrMat) = (view(A, i, :) for i in axes(A, 1))
 """
     eachcol(A::AbstractVecOrMat)
 
-Create a generator that iterates over the second dimension of matrix A, returning the
+Create a generator that iterates over the second dimension of matrix `A`, returning the
 columns as views.
 See also [`eachrow`](@ref) and [`eachslice`](@ref).
 """
@@ -429,17 +429,17 @@ eachcol(A::AbstractVecOrMat) = (view(A, :, i) for i in axes(A, 2))
 """
     eachslice(A::AbstractArray; dims)
 
-Create a generator that iterates over the given dims of A, returning views that select all
-the data from the other dimensions in A.
+Create a generator that iterates over dimensions `dims` of `A`, returning views that select all
+the data from the other dimensions in `A`.
 
-Only a single dimension in dims is currently supported. Equivalent to (view(A,:,:,...,i,:,:
-...)) for i in axes(A, dims)), where i is in position dims.
+Only a single dimension in `dims` is currently supported. Equivalent to `(view(A,:,:,...,i,:,:
+...)) for i in axes(A, dims))`, where `i` is in position `dims`.
 See also [`eachrow`](@ref), [`eachcol`](@ref), and [`selectdim`](@ref).
 """
 @inline function eachslice(A::AbstractArray; dims)
     length(dims) == 1 || throw(ArgumentError("only single dimensions are supported"))
     dim = first(dims)
-    dim <= ndims(A) || throw(DimensionMismatch("A doesn't have that many dimensions"))
+    dim <= ndims(A) || throw(DimensionMismatch("A doesn't have $dim dimensions"))
     idx1, idx2 = ntuple(d->(:), dim-1), ntuple(d->(:), ndims(A)-dim)
     return (view(A, idx1..., i, idx2...) for i in axes(A, dim))
 end
