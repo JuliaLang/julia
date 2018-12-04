c1bd17540010cfa49537d214d46823c4333fcf65
sam0410 <samikshya.chand.ece15@iitbhu.ac.in>
2018-12-05 03:32:47 +0530

    readability

--- stdlib/Dates/src/io.jl
+++ stdlib/Dates/src/io.jl
@@ -532,13 +532,13 @@ end
 function Base.show(io::IO, dt::DateTime)
     y,m,d = yearmonthday(dt)
     h = hour(dt)
-    min = minute(dt)
+    mi = minute(dt)
     s = second(dt)
     ms = millisecond(dt)
     if ms == 0
-        print(io, "DateTime(",y,", ",m,", ",d,", ",h,", ",min,", ",s,")")
+        print(io, "DateTime($y, $m, $d, $h, $mi, $s)")
     else
-        print(io, "DateTime(",y,", ",m,", ",d,", ",h,", ",min,", ",s,", ",ms,")")
+        print(io, "DateTime($y, $m, $d, $h, $mi, $s, $ms)")
     end
 end
 
@@ -551,8 +551,8 @@ function Base.show(io::IO, ::MIME"text/plain", dt::Date)
 end
 
 function Base.show(io::IO, dt::Date)
-    y,m,d = yearmonthday(value(dt))
-    print(io, "Date(",y,", ",m,", ",d,")")
+    y,m,d = yearmonthday(dt)
+    print(io, "Date($y, $m, $d)")
 end
 
 function Base.print(io::IO, dt::DateTime)
