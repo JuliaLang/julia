1bf1881606679f0f48aca8beca7efcb767c65c65
Elliot Saba <staticfloat@gmail.com>
2018-12-04 07:44:47 -0800

    Add closing parens

--- src/processor_arm.cpp
+++ src/processor_arm.cpp
@@ -523,7 +523,7 @@ static inline unsigned long jl_getauxval(unsigned long type)
     // First, try resolving getauxval in libc
     auto libc = jl_dlopen(nullptr, JL_RTLD_LOCAL);
     static (unsigned long (*)(unsigned long) getauxval_p;
-    if (jl_dlsym(libc, "getauxval", &getauxval_p, 0) {
+    if (jl_dlsym(libc, "getauxval", &getauxval_p, 0)) {
         return getauxval_p(type);
     }
 
