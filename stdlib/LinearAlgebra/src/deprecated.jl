c9ba74de0fc6fd2ed5a731d64da5c1588d758e03
Simon Byrne <simonbyrne@gmail.com>
2018-12-05 12:06:59 -0800

    remove depwarn

--- stdlib/LinearAlgebra/src/deprecated.jl
+++ stdlib/LinearAlgebra/src/deprecated.jl
@@ -1,10 +1,4 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
-using Base: @deprecate, depwarn
-
-# 2.0 deprecations
-function rank(A::AbstractMatrix, tol::Real)
-    depwarn(string("`rank` without any keywords has been deprecated. Consider using ",
-        "the keywords `rtol` or `atol` instead."), :rank)
-    rank(A,rtol=tol)
-end
+# To be deprecated in 2.0
+rank(A::AbstractMatrix, tol::Real) = rank(A,rtol=tol)
