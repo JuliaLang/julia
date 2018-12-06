03ba013e2b178e706dd0fa2a60824e7cbb1872aa
Matt Bauman <mbauman@gmail.com>
2018-12-06 18:10:26 -0500

    Fix 29545: Implement unaliascopy for ReinterpretArray

--- test/reinterpretarray.jl
+++ test/reinterpretarray.jl
@@ -160,3 +160,14 @@ let a = [0.1 0.2; 0.3 0.4], at = reshape([(i,i+1) for i = 1:2:8], 2, 2)
     r = reinterpret(Int, vt)
     @test r == OffsetArray(reshape(1:8, 2, 2, 2), (0, offsetvt...))
 end
+
+@testset "potentially aliased copies" begin
+    buffer = UInt8[1,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0]
+    mid = length(buffer) ÷ 2
+    x1 = reinterpret(Int, @view buffer[1:mid])
+    x2 = reinterpret(Int, @view buffer[mid+1:end])
+    x1 .= x2
+    @test x1 == x2 == [2]
+    @test x1[] === x2[] === 2
+end
+
