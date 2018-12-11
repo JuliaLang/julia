a03c0d0caa3baebba1cc70c672f2e43abe25020f
Fredrik Ekre <fredrik.ekre@outlook.com>
2018-12-11 15:51:04 +0100

    Update base/channels.jl
    Add the name at the beginning
    
    Co-Authored-By: matbesancon <mathieu.besancon@gmail.com>

--- base/channels.jl
+++ base/channels.jl
@@ -1,6 +1,7 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 """
+    AbstractChannel{T}
 Representation of a channel passing objects of type `T`.
 """
 abstract type AbstractChannel{T} end
