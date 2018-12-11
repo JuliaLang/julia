97de602901f3d8736c42f4f900379a24399df198
Alfredo Braunstein <alfredo.braunstein@polito.it>
2018-12-11 08:15:07 +0100

    avoid splatting single integer in circshift!

--- base/multidimensional.jl
+++ base/multidimensional.jl
@@ -932,7 +932,10 @@ See also [`circshift`](@ref).
     axes(dest) == inds || throw(ArgumentError("indices of src and dest must match (got $inds and $(axes(dest)))"))
     _circshift!(dest, (), src, (), inds, fill_to_length(shiftamt, 0, Val(N)))
 end
-circshift!(dest::AbstractArray, src, shiftamt) = circshift!(dest, src, (shiftamt...,))
+
+_circshift_helper!(dest::AbstractArray, src, shiftamt) = circshift!(dest, src, (shiftamt...,))
+_circshift_helper!(dest::AbstractArray, src, shiftamt::Integer) = circshift!(dest, src, (shiftamt,))
+circshift!(dest::AbstractArray, src, shiftamt) = _circshift_helper!(dest, src, shiftamt)
 
 # For each dimension, we copy the first half of src to the second half
 # of dest, and the second half of src to the first half of dest. This
