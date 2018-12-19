4ad0633788312c70d5daf88aeef75bf1b8887942
raghav9-97 <raghu2288661@gmail.com>
2018-12-19 13:36:59 +0530

    Added tests

--- stdlib/LinearAlgebra/test/generic.jl
+++ stdlib/LinearAlgebra/test/generic.jl
@@ -299,6 +299,14 @@ LinearAlgebra.Transpose(a::ModInt{n}) where {n} = transpose(a)
     @test A*(lu(A, Val(true))\b) == b
 end
 
+@testset "Issue 18742" begin
+    @test_throws DimensionMismatch rand(4, 2)/rand(3, 5)
+    @test_throws DimensionMismatch [1 2 3; 4 5 6]/[2 3 4 5; 1 2 4 6]
+    @test_throws DimensionMismatch ones(4,5)/zeros(3,6)
+    @test_throws DimensionMismatch rand(4, 2)\rand(3, 5)
+    @test_throws DimensionMismatch [1 2 3]\[2 3 4 5; 1 2 4 6]
+    @test_throws DimensionMismatch ones(4,5)\zeros(3,6)
+end
 @testset "fallback throws properly for AbstractArrays with dimension > 2" begin
     @test_throws ErrorException adjoint(rand(2,2,2,2))
     @test_throws ErrorException transpose(rand(2,2,2,2))
