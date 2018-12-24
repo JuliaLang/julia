6187a12457e4b0f9aceb62d058228f3cdffd7d63
sam0410 <samikshya.chand.ece15@iitbhu.ac.in>
2018-12-24 13:48:01 +0530

    Add changes

--- base/reflection.jl
+++ base/reflection.jl
@@ -177,12 +177,12 @@ fieldnames(::Core.TypeofBottom) =
 fieldnames(t::Type{<:Tuple}) = ntuple(identity, fieldcount(t))
 
 """
-    hasfield(T::DataType, name::Symbol)
+    hasfield(x, name::Symbol)
 
-Returns a boolean indicating whether the DataType has the specified field as one of
+Returns a boolean indicating whether the object 'x' has the specified field as one of
 its own fields.
 """
-hasfield(T::DataType, name::Symbol) = fieldindex(T, name, false) > 0
+hasfield(x, s::Symbol) = hasproperty(x, s)
 
 """
     nameof(t::DataType) -> Symbol
@@ -1218,4 +1218,4 @@ propertynames(x, private) = propertynames(x) # ignore private flag by default
 Returns a boolean indicating whether the object `x` has the specified property as one of
 its own properties.
 """
-hasproperty(x, s::Symbol) = isdefined(x,s)
+hasproperty(x, s::Symbol) = s in propertynames(x)
