dec05b862aa495a1fc546a13220df5f91dbdf401
Keno Fischer <keno@juliacomputing.com>
2018-12-30 18:20:23 -0500

    Emscripten build fixes

--- src/julia_threads.h
+++ src/julia_threads.h
@@ -35,6 +35,12 @@ typedef win32_ucontext_t jl_ucontext_t;
 #define JL_HAVE_UNW_CONTEXT
 #elif defined(_OS_LINUX_)
 #define JL_HAVE_UCONTEXT
+#elif defined(_OS_EMSCRIPTEN_)
+// This is a lie, but we don't have anything else either,
+// and this makes it build for now
+// // This is a lie, but we don't have anything else either,
+// and this makes it build for now
+#define JL_HAVE_ASM
 #else
 #define JL_HAVE_UNW_CONTEXT
 #endif
