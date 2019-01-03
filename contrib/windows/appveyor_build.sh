05a8951398d94708e3f4b6917da3dca5b2383eb2
Elliot Saba <staticfloat@gmail.com>
2019-01-03 14:03:38 -0800

    Fix `bb-install` naming conventions, add hashes (#30535)
    * Fix `bb-install` naming conventions, add hashes
    
    * Set `DEP_LIBS` to include `openblas` on Appveyor
    
    * When guessing BB libc, default to `glibc` on Linux
    
    * Fix bb-install bash parsing failure
    
    Quote to avoid bash freaking out from spurious `)` from compilers that
    have fancy version strings such as `(Red Hat 7.3.1-5)`, which gets
    `lastword`'ed down to `7.3.1-5)`.
    
    * Add `contrib/refresh_bb_tarballs.sh` to aid in batch-grabbing BB hashes
    
    (cherry picked from commit 8b189ec494479d9e2ffda0549f066ba2c6389182)

--- contrib/windows/appveyor_build.sh
+++ contrib/windows/appveyor_build.sh
@@ -198,7 +198,7 @@ else
   echo 'USE_BINARYBUILDER_LLVM = 1' >> Make.user
   echo 'USE_BINARYBUILDER_OPENBLAS = 1' >> Make.user
   echo 'BINARYBUILDER_LLVM_ASSERTS = 1' >> Make.user
-  echo 'override DEP_LIBS += llvm openlibm' >> Make.user
+  echo 'override DEP_LIBS += llvm openlibm openblas' >> Make.user
   export CCACHE_DIR=/cygdrive/c/ccache
   echo 'USECCACHE=1' >> Make.user
   make check-whitespace
