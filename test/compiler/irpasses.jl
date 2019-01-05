1a6f60cc2491d0c41c9d19036df4da6e61b24192
Keno Fischer <keno@juliacomputing.com>
2019-01-04 19:35:28 -0500

    Fix use counts for mutable struct SROA
    PR #28478 moved the computation of the use counts before the finish call.
    to fix #28444. However, the early parts of the finish call fixes up phi
    node arguments, which fail to get counted if we look at use counts before
    that fixup is performed. This causes #30594 where the only non-trivial use
    is on the backedge of the phi and would thus incorrectly fail to get accounted
    for. Fix that by taking the use count after phi fixup but before dce.

--- test/compiler/irpasses.jl
+++ test/compiler/irpasses.jl
@@ -37,3 +37,29 @@ let m = Meta.@lower 1 + 1
     Core.Compiler.verify_ir(ir)
     @test isa(ir.stmts[3], Core.PhiNode) && length(ir.stmts[3].edges) == 1
 end
+
+# Tests for SROA
+
+mutable struct Foo30594; x::Float64; end
+Base.copy(x::Foo30594) = Foo30594(x.x)
+function add!(p::Foo30594, off::Foo30594)
+    p.x += off.x
+    return p
+end
+Base.:(+)(a::Foo30594, b::Foo30594) = add!(copy(a), b)
+
+let results = Float64[]
+    @noinline use30594(x) = push!(results, x.x); nothing
+    function foo30594(cnt::Int, dx::Int)
+        step = Foo30594(dx)
+        curr = step + Foo30594(1)
+        for i in 1:cnt
+            use30594(curr)
+            curr = curr + step
+        end
+        nothing
+    end
+
+    foo30594(4, -1)
+    @test results == [0.0, -1.0, -2.0, -3.0]
+end
