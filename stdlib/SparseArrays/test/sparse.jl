a4b32610cf484b99f69e12d0ac1e192ef5c688a9
Alfredo Braunstein <abraunst+github@gmail.com>
2019-01-05 17:38:58 +0100

    Faster and simpler sprand for SparseMatrixCSC (#30494)
    * simpler and faster sprand
    * doctests
    * shaving about half the time in _sprand
    * avoid problems with T<:Integer, T!=Int
    * add tests clarifying the output distribution of sprand

--- stdlib/SparseArrays/test/sparse.jl
+++ stdlib/SparseArrays/test/sparse.jl
@@ -1501,7 +1501,7 @@ end
     local A = guardseed(1234321) do
         triu(sprand(10, 10, 0.2))
     end
-    @test SparseArrays.droptol!(A, 0.01).colptr == [1,1,1,2,2,3,4,6,6,7,9]
+    @test SparseArrays.droptol!(A, 0.01).colptr ==  [1, 2, 2, 3, 4, 5, 5, 6, 8, 10, 13]
     @test isequal(SparseArrays.droptol!(sparse([1], [1], [1]), 1), SparseMatrixCSC(1, 1, Int[1, 1], Int[], Int[]))
 end
 
@@ -2394,6 +2394,21 @@ end
     m2 = @which mul!(C,D,A)
     @test m1.module == SparseArrays
     @test m2.module == SparseArrays
+end
+
+@testset "sprand" begin
+    p=0.3; m=1000; n=2000;
+    for s in 1:10
+        # build a (dense) random matrix with randsubset + rand
+        Random.seed!(s);
+        v = randsubseq(1:m*n,p);
+        x = zeros(m,n);
+        x[v] .= rand(length(v));
+        # redo the same with sprand
+        Random.seed!(s);
+        a = sprand(m,n,p);
+        @test x == a
+    end
 end
 
 @testset "sprandn with type $T" for T in (Float64, Float32, Float16, ComplexF64, ComplexF32, ComplexF16)
