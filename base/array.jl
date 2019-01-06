94abdc1c169e3baf930cb48c89d820578dbc9f2f
Peter Shin <petershintech@gmail.com>
2019-01-06 11:47:59 +1100

    Make sure to call jl_islayout_inline() even in the case @aasert statement is deactivated

--- base/array.jl
+++ base/array.jl
@@ -192,7 +192,8 @@ julia> Base.bitsunionsize(Union{Float64, UInt8, Int128})
 function bitsunionsize(u::Union)
     sz = Ref{Csize_t}(0)
     algn = Ref{Csize_t}(0)
-    @assert ccall(:jl_islayout_inline, Cint, (Any, Ptr{Csize_t}, Ptr{Csize_t}), u, sz, algn) != Cint(0)
+    isunboxed = ccall(:jl_islayout_inline, Cint, (Any, Ptr{Csize_t}, Ptr{Csize_t}), u, sz, algn)
+    @assert isunboxed != Cint(0)
     return sz[]
 end
 
