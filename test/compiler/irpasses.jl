ddcee0d75a4974321d69e7e7b69c29b01fa2e17c
Keno Fischer <keno@juliacomputing.com>
2019-01-07 13:30:25 -0500

    Fix SROA confusing new and old nodes
    SROA was accidentally treating a pending node as old and thus
    getting the wrong type when querying the predecessor. As a result
    it thought one of the paths was unreachable causing undefined data
    to be introduced on that path (generally the `1.0` that happened to
    already be in register).
    
    Fix #29983

--- test/compiler/irpasses.jl
+++ test/compiler/irpasses.jl
@@ -23,3 +23,29 @@ let results = Float64[]
     foo30594(4, -1)
     @test results == [0.0, -1.0, -2.0, -3.0]
 end
+
+# Issue #29983
+# This one is a bit hard to trigger, but the key is to create a case
+# where SROA needs to introduce an intermediate type-unstable phi node
+struct Foo29983{T}
+    x::Tuple{T}
+end
+struct Bar29983{S}
+    x::S
+end
+Base.:+(a::T, b::Bar29983{S}) where {T, S} = Bar29983(a + b.x)
+Base.:+(a::Bar29983{S}, b::T) where {T, S} = b + a
+Base.:+(a::Bar29983{S}, b::Bar29983{T}) where {T, S} = Bar29983(a.x + b.x)
+Base.:+(a::Foo29983, b::Foo29983) = Foo29983((a.x[1] + b.x[1],))
+
+function f(x::Vector{T}) where {T}
+    x1 = Foo29983((x[1],))
+    la1 = Foo29983((x[1],))
+    f1 = Foo29983((0,))
+    for _ in 1:2
+        f1 += la1
+    end
+    return f1
+end
+
+@test f([Bar29983(1.0)]).x[1].x == 2.0
