0a6935f9c48fd0679a05f72eff64ab708fe7e164
Alfredo Braunstein <abraunst+github@gmail.com>
2019-01-13 18:37:37 +0100

    improve docstring of SparseMatrixCSC (#30689)

--- stdlib/SparseArrays/src/sparsematrix.jl
+++ stdlib/SparseArrays/src/sparsematrix.jl
@@ -9,7 +9,9 @@
     SparseMatrixCSC{Tv,Ti<:Integer} <: AbstractSparseMatrix{Tv,Ti}
 
 Matrix type for storing sparse matrices in the
-[Compressed Sparse Column](@ref man-csc) format.
+[Compressed Sparse Column](@ref man-csc) format. The standard way
+of constructing SparseMatrixCSC is through the [`sparse`](@ref) function.
+See also [`spzeros`](@ref), [`spdiagm`](@ref) and [`sprand`](@ref).
 """
 struct SparseMatrixCSC{Tv,Ti<:Integer} <: AbstractSparseMatrix{Tv,Ti}
     m::Int                  # Number of rows
