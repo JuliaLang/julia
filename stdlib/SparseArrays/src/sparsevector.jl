e3f1de5bca788403f40bb9dc352dee9a2425920f
Alfredo Braunstein <alfredo.braunstein@polito.it>
2019-01-20 14:13:32 +0100

    more correct copyto!

--- stdlib/SparseArrays/src/sparsevector.jl
+++ stdlib/SparseArrays/src/sparsevector.jl
@@ -470,12 +470,7 @@ function copyto!(A::SparseVector, B::SparseVector)
     return A
 end
 
-function copyto!(A::SparseVector, B::AbstractArray)
-    prep_sparsevec_copy_dest!(A::SparseVector, length(B), length(B))
-    copyto!(A.nzind, 1:length(B))
-    copyto!(A.nzval, B)
-    return A
-end
+copyto!(A::SparseVector, B::AbstractArray) = copyto!(A, sparse(B))
 
 function copyto!(A::SparseVector, B::SparseMatrixCSC)
     prep_sparsevec_copy_dest!(A, length(B), nnz(B))
