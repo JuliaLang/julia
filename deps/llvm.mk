39cde43f7f605d2453d9f9b6e5ff0731271d659b
Keno Fischer <keno@juliacomputing.com>
2019-01-23 20:16:02 -0500

    Add patch to fix LLVM build on ppc64le

--- deps/llvm.mk
+++ deps/llvm.mk
@@ -429,6 +429,7 @@ ifeq ($(LLVM_VER_PATCH), 0)
 $(eval $(call LLVM_PATCH,llvm-windows-race))
 endif
 $(eval $(call LLVM_PATCH,llvm-D51842-win64-byval-cc))
+$(eval $(call LLVM_PATCH,llvm-D57118-powerpc))
 endif # LLVM_VER
 
 # Independent to the llvm version add a JL prefix to the version map
