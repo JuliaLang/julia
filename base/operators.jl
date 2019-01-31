2a293c2a9c80c09c58bed2cf9c9073d651c18133
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-01-31 05:47:27 -0500

    add !=(x) <=(x) >=(x) <(x) >(x)

--- base/operators.jl
+++ base/operators.jl
@@ -928,6 +928,56 @@ used to implement specialized methods.
 ==(x) = Fix2(==, x)
 
 """
+    !=(x)
+
+Create a function that compares its argument to `x` using [`!=`](@ref), i.e.
+a function equivalent to `y -> y != x`.
+The returned function is of type `Base.Fix2{typeof(!=)}`, which can be
+used to implement specialized methods.
+"""
+!=(x) = Fix2(!=, x)
+
+"""
+    >=(x)
+
+Create a function that compares its argument to `x` using [`>=`](@ref), i.e.
+a function equivalent to `y -> y >= x`.
+The returned function is of type `Base.Fix2{typeof(>=)}`, which can be
+used to implement specialized methods.
+"""
+>=(x) = Fix2(>=, x)
+
+"""
+    <=(x)
+
+Create a function that compares its argument to `x` using [`<=`](@ref), i.e.
+a function equivalent to `y -> y <= x`.
+The returned function is of type `Base.Fix2{typeof(<=)}`, which can be
+used to implement specialized methods.
+"""
+<=(x) = Fix2(<=, x)
+
+"""
+    >(x)
+
+Create a function that compares its argument to `x` using [`>`](@ref), i.e.
+a function equivalent to `y -> y > x`.
+The returned function is of type `Base.Fix2{typeof(>)}`, which can be
+used to implement specialized methods.
+"""
+>(x) = Fix2(>, x)
+
+"""
+    <(x)
+
+Create a function that compares its argument to `x` using [`<`](@ref), i.e.
+a function equivalent to `y -> y < x`.
+The returned function is of type `Base.Fix2{typeof(<)}`, which can be
+used to implement specialized methods.
+"""
+<(x) = Fix2(<, x)
+
+"""
     splat(f)
 
 Defined as
