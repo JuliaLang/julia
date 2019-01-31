b3ca3b81f3f647880e3378ab798656379714a560
EternalLearner42 <46832510+EternalLearner42@users.noreply.github.com>
2019-01-31 01:47:42 +0000

    Update winrpm.sh

--- contrib/windows/winrpm.sh
+++ contrib/windows/winrpm.sh
@@ -2,7 +2,7 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 # build-time mini version of WinRPM, usage:
-# ./winrpm.sh http://download.opensuse.org/repositories/windows:/mingw:/win64/openSUSE_13.2/ mingw64-zlib1
+# ./winrpm.sh https://download.opensuse.org/repositories/windows:/mingw:/win64/openSUSE_13.2/ mingw64-zlib1     (This link no longer works)
 # depends on wget/curl, xmllint, gunzip, sort -V, sha256sum, and p7zip
 
 set -e
