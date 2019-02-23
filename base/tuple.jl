eca2ee86434e6814d5d9067602b8a6e97ff7593e
Jeff Bezanson <jeff.bezanson@gmail.com>
2019-02-23 11:32:11 -0700

    fix method signature for one of the Tuple constructors
    from #31112

--- base/tuple.jl
+++ base/tuple.jl
@@ -203,18 +203,6 @@ if nameof(@__MODULE__) === :Base
 
 (::Type{T})(x::Tuple) where {T<:Tuple} = convert(T, x)  # still use `convert` for tuples
 
-# resolve ambiguity between preceding and following methods
-All16{E,N}(x::Tuple) where {E,N} = convert(All16{E,N}, x)
-
-function (T::All16{E,N})(itr) where {E,N}
-    len = N+16
-    elts = collect(E, Iterators.take(itr,len))
-    if length(elts) != len
-        _totuple_err(T)
-    end
-    (elts...,)
-end
-
 (::Type{T})(itr) where {T<:Tuple} = _totuple(T, itr)
 
 _totuple(::Type{Tuple{}}, itr, s...) = ()
@@ -229,6 +217,16 @@ function _totuple(T, itr, s...)
     y = iterate(itr, s...)
     y === nothing && _totuple_err(T)
     (convert(tuple_type_head(T), y[1]), _totuple(tuple_type_tail(T), itr, y[2])...)
+end
+
+# use iterative algorithm for long tuples
+function _totuple(T::Type{All16{E,N}}, itr) where {E,N}
+    len = N+16
+    elts = collect(E, Iterators.take(itr,len))
+    if length(elts) != len
+        _totuple_err(T)
+    end
+    (elts...,)
 end
 
 _totuple(::Type{Tuple{Vararg{E}}}, itr, s...) where {E} = (collect(E, Iterators.rest(itr,s...))...,)
