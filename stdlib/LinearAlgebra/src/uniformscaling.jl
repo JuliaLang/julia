4ead61a0400d3bfc6d916caab049cb4766daa9a2
Sakse Dalum <dalum@users.noreply.github.com>
2019-02-26 13:24:42 +0100

    Add `ldiv!`, `rdiv!` for matrices and UniformScaling

--- stdlib/LinearAlgebra/src/uniformscaling.jl
+++ stdlib/LinearAlgebra/src/uniformscaling.jl
@@ -219,6 +219,8 @@ mul!(C::AbstractMatrix, A::AbstractMatrix, J::UniformScaling) = mul!(C, A, J.λ)
 mul!(C::AbstractVecOrMat, J::UniformScaling, B::AbstractVecOrMat) = mul!(C, J.λ, B)
 rmul!(A::AbstractMatrix, J::UniformScaling) = rmul!(A, J.λ)
 lmul!(J::UniformScaling, B::AbstractVecOrMat) = lmul!(J.λ, B)
+rdiv!(A::AbstractMatrix, J::UniformScaling) = rdiv!(A, J.λ)
+ldiv!(J::UniformScaling, B::AbstractVecOrMat) = ldiv!(J.λ, B)
 
 Broadcast.broadcasted(::typeof(*), x::Number,J::UniformScaling) = UniformScaling(x*J.λ)
 Broadcast.broadcasted(::typeof(*), J::UniformScaling,x::Number) = UniformScaling(J.λ*x)
