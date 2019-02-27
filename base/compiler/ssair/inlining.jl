20069cb7c2cb0686f93af046446331ea183215ab
Keno Fischer <keno@juliacomputing.com>
2019-02-27 18:05:39 -0500

    Allow constant-folding intrinsics that are non-pure for inference only
    Any constants produced during inference must be *exact* (in the === to
    what would have been produced at runtime sense). As a result, we disallow
    constant folding the sqrt intrinsic, since we can't guarantee that this
    will be the case (depending on what LLVM decides to do). However, this does
    not apply to the optimizer (and in fact we do it all the time by inlining
    pure functions here). Thus, for code size, inlining heuristics and non-LLVM
    backends, enable the optimizer to constant fold sqrt.
    
    Before:
    ```
    julia> f() = sqrt(2)
    f (generic function with 1 method)
    
    julia> @code_typed f()
    CodeInfo(
    1 ─ %1 = Base.Math.sqrt_llvm(2.0)::Float64
    └──      return %1
    ) => Float64
    
    julia> @code_typed optimize=false f()
    CodeInfo(
    1 ─ %1 = Main.sqrt(2)::Float64
    └──      return %1
    ) => Float64
    ```
    
    After
    ```
    julia> @code_typed f()
    CodeInfo(
    1 ─     return 1.4142135623730951
    ) => Float64
    
    julia> @code_typed optimize=false f()
    CodeInfo(
    1 ─ %1 = Main.sqrt(2)::Float64
    └──      return %1
    ) => Float64
    ```
    
    Note that we are not able to infer `Const`, but still inline the
    constant in the optimized version of the IR.

--- base/compiler/ssair/inlining.jl
+++ base/compiler/ssair/inlining.jl
@@ -1077,6 +1077,21 @@ function ispuretopfunction(@nospecialize(f))
         istopfunction(f, :promote_type)
 end
 
+function intrinsic_tfunc_optimize(f::IntrinsicFunction, atypes::Vector{Any})
+    is_pure_intrinsic_optimize(f) || return nothing
+    args = atypes[2:end]
+    _all(@nospecialize(a) -> isa(a, Const), args) || return nothing
+    iidx = Int(reinterpret(Int32, f::IntrinsicFunction)) + 1
+    if iidx < 0 || iidx > length(T_IFUNC)
+        # invalid intrinsic
+        return nothing
+    end
+    (min, max, _) = T_IFUNC[iidx]
+    min <= length(atypes) - 1 <= max || return nothing
+    argvals = anymap(a::Const -> a.val, atypes[2:end])
+    return f(argvals...)
+end
+
 function early_inline_special_case(ir::IRCode, @nospecialize(f), @nospecialize(ft), e::Expr, atypes::Vector{Any}, sv::OptimizationState,
                                    @nospecialize(etype))
     if (f === typeassert || ft ⊑ typeof(typeassert)) && length(atypes) == 3
@@ -1106,6 +1121,12 @@ function early_inline_special_case(ir::IRCode, @nospecialize(f), @nospecialize(f
                     return quoted(val)
                 end
             end
+        elseif isa(f, IntrinsicFunction) && is_pure_intrinsic_optimize_only(f)
+            # If this intrinsic was not eligible for constant propagation during
+            # inference, but is now, try to do that here.
+            val = intrinsic_tfunc_optimize(f, atypes)
+            val === nothing && return nothing
+            return quoted(val)
         end
     end
 
