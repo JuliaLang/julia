9656c44a79a8bbc8c16ccbc44ad9cd4e0b9209ad
Katharine Hyatt <kshyatt@users.noreply.github.com>
2019-03-05 04:27:01 -0600

    Docs for PosDefException and SingularException (#31251)

--- stdlib/LinearAlgebra/src/exceptions.jl
+++ stdlib/LinearAlgebra/src/exceptions.jl
@@ -29,10 +29,24 @@ function Base.showerror(io::IO, ex::ARPACKException)
     end
 end
 
+"""
+    SingularException
+
+Exception thrown when the input matrix has one or more zero-valued eigenvalues, and is not invertible.
+A linear solve involving such a matrix cannot be computed.
+The `info` field indicates the location of (one of) the singular value(s).
+"""
 struct SingularException <: Exception
     info::BlasInt
 end
 
+"""
+    PosDefException
+
+Exception thrown when the input matrix was not [positive definite](https://en.wikipedia.org/wiki/Definiteness_of_a_matrix).
+Some linear algebra functions and factorizations are only applicable to positive definite matrices.
+The `info` field indicates the location of (one of) the eigenvalue(s) which is (are) less than/equal to 0.
+"""
 struct PosDefException <: Exception
     info::BlasInt
 end
