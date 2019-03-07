0f281f9db6fc3546356824631f381fc9aa71ba6b
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2019-03-07 11:21:36 +0100

    doc: add factorizations docstrings
    docs for BunchKaufman
    
    docs for Cholesky(Pivoted)
    
    docs for (Generalized)Eigen
    
    minor edits
    
    merge hessenberg
    
    docs for LDLt
    
    docs for LQ
    
    docs for LU
    
    merge new hessenberg
    
    add components to LQ docstring
    
    add components to LU docstring
    
    add components to Eigen docstring
    
    add components to GeneralizedEigen
    
    add components to Cholesky(Pivoted)
    
    add components to BunchKaufman
    
    docs for (Generalized)Schur
    
    docs for (Generalized)SVD
    
    fix bunchkaufman as per review
    
    list factorization types in index.md
    
    fix docstring x-refs

--- stdlib/LinearAlgebra/src/factorization.jl
+++ stdlib/LinearAlgebra/src/factorization.jl
@@ -1,7 +1,14 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 ## Matrix factorizations and decompositions
+"""
+    LinearAlgebra.Factorization
 
+Abstract type for [matrix factorizations](https://en.wikipedia.org/wiki/Matrix_decomposition)
+a.k.a. matrix decompositions.
+See [online documentation](@ref man-linalg-factorizations) for a list of available
+matrix factorizations.
+"""
 abstract type Factorization{T} end
 
 eltype(::Type{<:Factorization{T}}) where {T} = T
--- stdlib/LinearAlgebra/src/lq.jl
+++ stdlib/LinearAlgebra/src/lq.jl
@@ -1,7 +1,41 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 # LQ Factorizations
+"""
+    LQ <: Factorization
 
+Matrix factorization type of the `LQ` factorization of a matrix `A`. The `LQ`
+decomposition is the `QR` decomposition of `transpose(A)`. This is the return
+type of [`lq`](@ref), the corresponding matrix factorization function.
+
+If `S::LQ` is the factorization object, the lower triangular component can be
+obtained via `S.L`, and the orthogonal/unitary component via `S.Q`, such that
+`A ≈ S.L*S.Q`.
+
+Iterating the decomposition produces the components `S.L` and `S.Q`.
+
+# Examples
+```jldoctest
+julia> A = [5. 7.; -2. -4.]
+2×2 Array{Float64,2}:
+  5.0   7.0
+ -2.0  -4.0
+
+julia> S = lq(A)
+LQ{Float64,Array{Float64,2}} with factors L and Q:
+[-8.60233 0.0; 4.41741 -0.697486]
+[-0.581238 -0.813733; -0.813733 0.581238]
+
+julia> S.L * S.Q
+2×2 Array{Float64,2}:
+  5.0   7.0
+ -2.0  -4.0
+
+julia> l, q = S; # destructuring via iteration
+
+julia> l == S.L &&  q == S.Q
+true
+"""
 struct LQ{T,S<:AbstractMatrix{T}} <: Factorization{T}
     factors::S
     τ::Vector{T}
