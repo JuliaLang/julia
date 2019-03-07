ec481ca4b3b1ee3a964ea3ce5cb9799ed0e5794e
zsoerenm <zsoerenm@hotmail.de>
2019-03-07 13:40:31 +0100

    Fix white space in test

--- stdlib/LinearAlgebra/test/lu.jl
+++ stdlib/LinearAlgebra/test/lu.jl
@@ -128,7 +128,7 @@ dimg  = randn(n)/2
                 ldiv!(c_dest, adjoint(lua), c)
                 @test norm(b_dest - lua' \ b, 1) < ε*κ*2n
                 @test norm(c_dest - lua' \ c, 1) < ε*κ*n
-                
+
                 rdiv!(b_dest, b, lua)
                 rdiv!(c_dest, c, lua)
                 @test norm(b_dest - b / lua, 1) < ε*κ*2n
