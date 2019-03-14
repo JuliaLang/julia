eee352b4c98a807021e78dd6fdef5af35b17d9de
eulerkochy <kc99.kol@gmail.com>
2019-03-14 20:58:34 +0530

    Modify prevind and nextind, revert code of chop

--- base/strings/basic.jl
+++ base/strings/basic.jl
@@ -458,7 +458,8 @@ prevind(s::AbstractString, i::Int)                 = prevind(s, i, 1)
 function prevind(s::AbstractString, i::Int, n::Int)
     n < 0 && throw(ArgumentError("n cannot be negative: $n"))
     z = ncodeunits(s) + 1
-    @boundscheck 0 ≤ i ≤ z || n == 0 && return thisind(s, i) == i ? i : string_index_err(s, i) || throw(BoundsError(s, i))
+    @boundscheck (n != 0) ≤ i ≤ z || throw(BoundsError(s, i))
+    n == 0 && return thisind(s, i) == i ? i : string_index_err(s, i)
     while n > 0 && 1 < i
         @inbounds n -= isvalid(s, i -= 1)
     end
@@ -517,7 +518,8 @@ nextind(s::AbstractString, i::Int)                 = nextind(s, i, 1)
 function nextind(s::AbstractString, i::Int, n::Int)
     n < 0 && throw(ArgumentError("n cannot be negative: $n"))
     z = ncodeunits(s)
-    @boundscheck 0 ≤ i ≤ z || n == 0 && return thisind(s, i) == i ? i : string_index_err(s, i) || throw(BoundsError(s, i))
+    @boundscheck 0 ≤ i ≤ z + (n == 0) || throw(BoundsError(s, i))
+    n == 0 && return thisind(s, i) == i ? i : string_index_err(s, i)
     while n > 0 && i < z
         @inbounds n -= isvalid(s, i += 1)
     end
