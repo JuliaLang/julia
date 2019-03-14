49541cdffe5cb9ee6028a04c0cff2ca9f8eefa75
narendrakpatel <narpatelk@gmail.com>
2019-03-15 02:50:04 +0530

    add tests for new implementation of nextfloat/prevfloat

--- test/mpfr.jl
+++ test/mpfr.jl
@@ -451,8 +451,15 @@ end
         @test BigFloat(nextfloat(12.12)) == nextfloat(x)
         @test BigFloat(prevfloat(12.12)) == prevfloat(x)
     end
+    x = BigFloat(12.12, 100)
+    @test nextfloat(x).prec == x.prec
+    @test prevfloat(x).prec == x.prec
+    @test nextfloat(x, -1) == prevfloat(x, 1)
+    @test prevfloat(x, -1) == nextfloat(x, 1)
     @test isnan(nextfloat(BigFloat(NaN)))
     @test isnan(prevfloat(BigFloat(NaN)))
+    @test isnan(nextfloat(BigFloat(NaN), 1))
+    @test isnan(prevfloat(BigFloat(NaN), 1))
 end
 # sqrt DomainError
 @test_throws DomainError sqrt(BigFloat(-1))
