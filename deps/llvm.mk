8c0b550b61bcb196e1c8a9b03b38f0e6eb8a9dd7
Nicolau Werneck <nwerneck@gmail.com>
2019-03-17 18:35:24 +0100

    Backporting LLVM patch r355582 to release/6.x (fixes AVX512 max issue) (#31292)
    * Backporting LLVM patch r355582 to release/6.x (fixes AVX512 max issue

--- deps/llvm.mk
+++ deps/llvm.mk
@@ -430,6 +430,7 @@ $(eval $(call LLVM_PATCH,llvm-windows-race))
 endif
 $(eval $(call LLVM_PATCH,llvm-D51842-win64-byval-cc))
 $(eval $(call LLVM_PATCH,llvm-D57118-powerpc))
+$(eval $(call LLVM_PATCH,llvm-r355582-avxminmax)) # remove for 8.0
 endif # LLVM_VER
 
 # Independent to the llvm version add a JL prefix to the version map
