4b147155caea155b94c18ddbcc8ac7edb4c555c3
Mus <mus-m@outlook.com>
2019-03-20 16:09:53 -0400

    Return current directory without the trailing slash

--- base/path.jl
+++ base/path.jl
@@ -145,7 +145,8 @@ end
 """
     dirname(path::AbstractString) -> AbstractString
 
-Get the directory part of a path.
+Get the directory part of a path. Trailing '/' characters in the path are
+counted as part of the path.
 
 # Examples
 ```jldoctest
