1690120810d75e0e490650cf8cd003f043843bb3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-22 00:16:06 -0400

    correct ovferror logic (&& signbit)

--- base/rational.jl
+++ base/rational.jl
@@ -19,7 +19,7 @@ struct Rational{T<:Integer} <: Real
 
     function Rational{T}(num::Integer, den::Integer) where T<:BitSigned
         num == den == zero(T) && __throw_rational_argerror(T)
-        if (num == typemin(T) && isodd(den)) || (den == typemin(T) && isodd(num))
+        if (num == typemin(T) && signbit(den) && isodd(den)) || (den == typemin(T) && signbit(num) && isodd(num))
             __throw_rational_ovferror(num, den)
         end
         num2, den2 = divgcd(num, den)
