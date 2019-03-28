0ade63d1985cb4f4c1deef46bb620b512ec2ec21
Stefan Karpinski <stefan@karpinski.org>
2019-03-28 12:50:29 -0400

    Update base/libc.jl
    Co-Authored-By: kshyatt <kshyatt@users.noreply.github.com>

--- base/libc.jl
+++ base/libc.jl
@@ -126,6 +126,10 @@ end
     systemsleep(s::Real)
 
 Suspends execution for `s` seconds.
+This function does not yield to Julia's scheduler and therefore blocks
+the Julia thread that it is running on for the duration of the sleep time.
+
+See also: [`sleep`](@ref)
 """
 systemsleep
 
