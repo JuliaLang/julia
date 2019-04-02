17854926b48362d8cf8719f1b058a0c373236223
Zepp <luzepu678@gmail.com>
2019-04-02 16:40:32 +0800

    fix test

--- stdlib/LinearAlgebra/test/uniformscaling.jl
+++ stdlib/LinearAlgebra/test/uniformscaling.jl
@@ -170,14 +170,15 @@ let
 
     @testset "binary ops with vectors" begin
         v = complex.(randn(3), randn(3))
-        @test v  * J == v  * λ
-        @test v' * J == v' * λ
-        @test J * v  == λ * v
-        @test J * v' == λ * v'
-        @test v  / J == v  / λ
-        @test v' / J == v' / λ
-        @test J \ v  == λ \ v
-        @test J \ v' == λ \ v'
+        # As shown in #20423@GitHub, vector acts like x1 matrix when participating in linear algebra
+        @test all(v  * J == v  * λ)
+        @test all(v' * J == v' * λ)
+        @test all(J * v  == λ * v )
+        @test all(J * v' == λ * v')
+        @test all(v  / J == v  / λ)
+        @test all(v' / J == v' / λ)
+        @test all(J \ v  == λ \ v )
+        @test all(J \ v' == λ \ v')
     end
 
     @testset "binary ops with matrices" begin
