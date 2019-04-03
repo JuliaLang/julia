JuliaLang/julia

commit 4eb63085f136868a2296ca28654e3f40cdd068e6

fix #31404, lowering `where` inside tuple in kwcall (#31409)

src/julia-syntax.scm
@@ -1398,9 +1398,9 @@
                            (begin (set! a (cons `(= ,g ,(caddr x)) a))
                                   `(kw ,(cadr x) ,g)))))
                     ((eq? (car x) 'tuple)
-                     (let ((tmp (map remove-argument-side-effects (cdr x))))
-                       (set! a (revappend (apply append (map cdr tmp)) a))
-                       `(tuple ,@(map car tmp))))
+                     (let ((tmp (remove-argument-side-effects x)))
+                       (set! a (revappend (cdr tmp) a))
+                       (car tmp)))
                     (else
                      (let ((g (make-ssavalue)))
                        (begin (set! a (cons `(= ,g ,x) a))

test/syntax.jl
@@ -1835,3 +1835,7 @@ end
 
 @test_throws UndefVarError eval(Symbol(""))
 @test_throws UndefVarError eval(:(1+$(Symbol(""))))
+
+# issue #31404
+f31404(a, b; kws...) = (a, b, kws.data)
+@test f31404(+, (Type{T} where T,); optimize=false) === (+, (Type,), (optimize=false,))

commit 0814a6746e0975764e1e5f558a99649ea892c74b

fix #31596, named tuple syntax in kw arg call
fixes regression caused by 4eb63085f136868a2296ca28654e3f40cdd068e6

src/julia-syntax.scm
@@ -1375,7 +1375,7 @@
 ;; for example a[f(x)] => (temp=f(x); a[temp])
 ;; returns a pair (expr . assignments)
 ;; where 'assignments' is a list of needed assignment statements
-(define (remove-argument-side-effects e)
+(define (remove-argument-side-effects e (tup #f))
   (if
    (not (pair? e))
    (cons e '())
@@ -1391,14 +1391,14 @@
                          (let ((g (make-ssavalue)))
                            (begin (set! a (cons `(= ,g ,(cadr x)) a))
                                   `(,(car x) ,g)))))
-                    ((eq? (car x) 'kw)
+                    ((or (eq? (car x) 'kw) (and tup (eq? (car x) '=)))
                      (if (effect-free? (caddr x))
                          x
                          (let ((g (make-ssavalue)))
                            (begin (set! a (cons `(= ,g ,(caddr x)) a))
-                                  `(kw ,(cadr x) ,g)))))
+                                  `(,(car x) ,(cadr x) ,g)))))
                     ((eq? (car x) 'tuple)
-                     (let ((tmp (remove-argument-side-effects x)))
+                     (let ((tmp (remove-argument-side-effects x #t)))
                        (set! a (revappend (cdr tmp) a))
                        (car tmp)))
                     (else

test/syntax.jl
@@ -1839,3 +1839,7 @@ end
 # issue #31404
 f31404(a, b; kws...) = (a, b, kws.data)
 @test f31404(+, (Type{T} where T,); optimize=false) === (+, (Type,), (optimize=false,))
+
+# issue #31596
+f31596(x; kw...) = x
+@test f31596((a=1,), b = 1.0) === (a=1,)