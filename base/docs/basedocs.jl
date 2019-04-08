c7a2fd985c7731dae8f3372f6cc302b8e79cf0f5
六月初六 <2660994113@qq.com>
2019-04-08 13:15:20 +0800

    Update basedocs.jl

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -164,8 +164,7 @@ kw"__module__"
 
 The argument `__source__` is only visible inside the macro, and it provides information
 (in the form of a `LineNumberNode` object) about the parser location of the `@` sign from
-the macro invocation. (in the form of a `Module` object) about the expansion context of the
-macro invocation. See the manual section on [Macro invocation](@ref) for more information.
+the macro invocation. See the manual section on [Macro invocation](@ref) for more information.
 """
 kw"__source__"
 
