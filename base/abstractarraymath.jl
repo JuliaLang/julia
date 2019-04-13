7beb9571beeff173b0cf1e82ec46dc3d0bc9e660
hdictus <h.t.dictus@gmail.com>
2019-04-13 17:08:38 +0200

    Fix various silly errors

--- base/abstractarraymath.jl
+++ base/abstractarraymath.jl
@@ -148,33 +148,16 @@ julia> reverse(b, dims=1,2)
 function reverse(A::AbstractArray; dims)
     # dims can in theory be any iterable, how do we type that?
     # TODO: add tests for duplicate dims, too many dims, empty dims
-    if length(dims) == 1:
-        return reverse(A, dims[1])
+    if length(dims) == 1
+        print("one dim")
+        return _reverse(A, dims[1])
     else
-        return reverse(reverse(A, dims[1]), dims[2:end])
+        return reverse(_reverse(A, dims[1]), dims[2:end])
     end
 end
-
-"""
-    reverse(A; dims::Integer)
-
-Reverse `A` in dimension `dims`.
-
-# Examples
-```jldoctest
-julia> b = [1 2; 3 4]
-2×2 Array{Int64,2}:
- 1  2
- 3  4
-
-julia> reverse(b, dims=2)
-2×2 Array{Int64,2}:
- 2  1
- 4  3
-```
-"""
-function reverse(A::AbstractArray; dims::Integer)
-    nd = ndims(A); d = dims
+ 
+function _reverse(A::AbstractArray, d::Integer)
+    nd = ndims(A)
     1 ≤ d ≤ nd || throw(ArgumentError("dimension $d is not 1 ≤ $d ≤ $nd"))
     if isempty(A)
         return copy(A)
