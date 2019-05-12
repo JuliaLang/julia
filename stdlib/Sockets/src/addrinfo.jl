f6018aa0efc567fa90a828efe85a27b841c8059b
lbilli <lbilli@users.noreply.github.com>
2019-05-12 08:55:14 -0400

    Fix function name in docstring

--- stdlib/Sockets/src/addrinfo.jl
+++ stdlib/Sockets/src/addrinfo.jl
@@ -106,7 +106,7 @@ end
 getalladdrinfo(host::AbstractString) = getalladdrinfo(String(host))
 
 """
-    getalladdrinfo(host::AbstractString, IPAddr=IPv4) -> IPAddr
+    getaddrinfo(host::AbstractString, IPAddr=IPv4) -> IPAddr
 
 Gets the first IP address of the `host` of the specified `IPAddr` type.
 Uses the operating system's underlying getaddrinfo implementation, which may do a DNS lookup.
