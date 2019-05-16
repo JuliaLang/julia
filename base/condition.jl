7bd363a2fc89d8b34f73476e793b7dd11decf09f
Jeff Bezanson <jeff.bezanson@gmail.com>
2019-05-15 20:21:09 -0400

    move export of `Event` from Base back to Threads

--- base/condition.jl
+++ base/condition.jl
@@ -153,7 +153,7 @@ Create an edge-triggered event source that tasks can wait for. Tasks that call [
 `Condition` are suspended and queued. Tasks are woken up when [`notify`](@ref) is later called on
 the `Condition`. Edge triggering means that only tasks waiting at the time [`notify`](@ref) is
 called can be woken up. For level-triggered notifications, you must keep extra state to keep
-track of whether a notification has happened. The [`Channel`](@ref) and [`Event`](@ref) types do
+track of whether a notification has happened. The [`Channel`](@ref) and [`Threads.Event`](@ref) types do
 this, and can be used for level-triggered events.
 
 This object is NOT thread-safe. See [`Threads.Condition`](@ref) for a thread-safe version.
