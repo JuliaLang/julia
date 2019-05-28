d8a5e3c72e0acc5e0851640f07ad50f35fec6d45
Chris Foster <chris42f@gmail.com>
2019-05-28 22:16:04 +1000

    Two minor doctest fixes

--- stdlib/LinearAlgebra/src/hessenberg.jl
+++ stdlib/LinearAlgebra/src/hessenberg.jl
@@ -25,10 +25,10 @@ julia> A = [1 2 3 4; 5 6 7 8; 9 10 11 12; 13 14 15 16]
 
 julia> UpperHessenberg(A)
 4×4 UpperHessenberg{Int64,Array{Int64,2}}:
-  1   2   3   4
-  5   6   7   8
-  ⋅  10  11  12
-  ⋅   ⋅  15  16
+ 1   2   3   4
+ 5   6   7   8
+ ⋅  10  11  12
+ ⋅   ⋅  15  16
 ```
 """
 struct UpperHessenberg{T,S<:AbstractMatrix{T}} <: AbstractMatrix{T}
