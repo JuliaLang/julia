bbbff75d4a70233fcfc76be5f38cc94f967c513b
Carsten Bauer <crstnbr@gmail.com>
2019-06-24 18:05:53 +0200

    SVDAlgorithms -> Algorithms

--- stdlib/LinearAlgebra/src/svd.jl
+++ stdlib/LinearAlgebra/src/svd.jl
@@ -55,11 +55,6 @@ function SVD{T}(U::AbstractArray, S::AbstractVector{Tr}, Vt::AbstractArray) wher
 end
 
 
-abstract type SVDAlgorithm end
-struct DivideAndConquer <: SVDAlgorithm end
-struct Simple <: SVDAlgorithm end
-
-
 # iteration for destructuring into components
 Base.iterate(S::SVD) = (S.U, Val(:S))
 Base.iterate(S::SVD, ::Val{:S}) = (S.S, Val(:V))
@@ -67,7 +62,7 @@ Base.iterate(S::SVD, ::Val{:V}) = (S.V, Val(:done))
 Base.iterate(S::SVD, ::Val{:done}) = nothing
 
 """
-    svd!(A; full::Bool = false, alg::SVDAlgorithm = DivideAndConquer()) -> SVD
+    svd!(A; full::Bool = false, alg::Algorithm = DivideAndConquer()) -> SVD
 
 `svd!` is the same as [`svd`](@ref), but saves space by
 overwriting the input `A`, instead of creating a copy.
@@ -98,14 +93,14 @@ julia> A
   0.0       0.0  -2.0  0.0  0.0
 ```
 """
-function svd!(A::StridedMatrix{T}; full::Bool = false, alg::SVDAlgorithm = DivideAndConquer()) where T<:BlasFloat
+function svd!(A::StridedMatrix{T}; full::Bool = false, alg::Algorithm = DivideAndConquer()) where T<:BlasFloat
     m,n = size(A)
     if m == 0 || n == 0
         u,s,vt = (Matrix{T}(I, m, full ? m : n), real(zeros(T,0)), Matrix{T}(I, n, n))
     else
         if typeof(alg) == DivideAndConquer
             u,s,vt = LAPACK.gesdd!(full ? 'A' : 'S', A)
-        elseif typeof(alg) == Simple
+        elseif typeof(alg) == GolubReinsch
             c = full ? 'A' : 'S'
             u,s,vt = LAPACK.gesvd!(c, c, A)
         else
@@ -116,7 +111,7 @@ function svd!(A::StridedMatrix{T}; full::Bool = false, alg::SVDAlgorithm = Divid
 end
 
 """
-    svd(A; full::Bool = false, alg::SVDAlgorithm = DivideAndConquer()) -> SVD
+    svd(A; full::Bool = false, alg::Algorithm = DivideAndConquer()) -> SVD
 
 Compute the singular value decomposition (SVD) of `A` and return an `SVD` object.
 
@@ -134,7 +129,7 @@ and `V` is `N \\times N`, while in the thin factorization `U` is `M
 number of singular values.
 
 If `alg = DivideAndConquer()` (default) a divide-and-conquer algorithm is used to calculate the SVD.
-One can set `alg = Simple()` to use a simple (typically slower) algorithm instead.
+Another (typically slower) option is `alg = GolubReinsch()`.
 
 # Examples
 ```jldoctest
@@ -160,20 +155,20 @@ julia> u == F.U && s == F.S && v == F.V
 true
 ```
 """
-function svd(A::StridedVecOrMat{T}; full::Bool = false, alg::SVDAlgorithm = DivideAndConquer()) where T
+function svd(A::StridedVecOrMat{T}; full::Bool = false, alg::Algorithm = DivideAndConquer()) where T
     svd!(copy_oftype(A, eigtype(T)), full = full, alg = alg)
 end
-function svd(x::Number; full::Bool = false, alg::SVDAlgorithm = DivideAndConquer())
+function svd(x::Number; full::Bool = false, alg::Algorithm = DivideAndConquer())
     SVD(x == 0 ? fill(one(x), 1, 1) : fill(x/abs(x), 1, 1), [abs(x)], fill(one(x), 1, 1))
 end
-function svd(x::Integer; full::Bool = false, alg::SVDAlgorithm = DivideAndConquer())
+function svd(x::Integer; full::Bool = false, alg::Algorithm = DivideAndConquer())
     svd(float(x), full = full, alg = alg)
 end
-function svd(A::Adjoint; full::Bool = false, alg::SVDAlgorithm = DivideAndConquer())
+function svd(A::Adjoint; full::Bool = false, alg::Algorithm = DivideAndConquer())
     s = svd(A.parent, full = full, alg = alg)
     return SVD(s.Vt', s.S, s.U')
 end
-function svd(A::Transpose; full::Bool = false, alg::SVDAlgorithm = DivideAndConquer())
+function svd(A::Transpose; full::Bool = false, alg::Algorithm = DivideAndConquer())
     s = svd(A.parent, full = full, alg = alg)
     return SVD(transpose(s.Vt), s.S, transpose(s.U))
 end
