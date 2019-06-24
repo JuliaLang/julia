64224de6b46d3dbefe23b340eca11fe311186d8c
Benoît Legat <benoit.legat@gmail.com>
2019-06-24 11:18:06 +0200

    Improve tests

--- stdlib/LinearAlgebra/test/symmetric.jl
+++ stdlib/LinearAlgebra/test/symmetric.jl
@@ -88,8 +88,8 @@ end
             @testset "Unary minus for Symmetric/Hermitian matrices" begin
                 @test (-Symmetric(asym))::typeof(Symmetric(asym)) == -asym
                 @test (-Hermitian(aherm))::typeof(Hermitian(aherm)) == -aherm
-                @test (-Symmetric(Symmetric(ones(Bool, 1, 1)))) == Symmetric(-ones(Bool, 1, 1))
-                @test (-Hermitian(Symmetric(ones(Bool, 1, 1)))) == Hermitian(-ones(Bool, 1, 1))
+                @test (-Symmetric([true true; false false]))::Symmetric{Int,Matrix{Int}} == [-1 -1; 0 0]
+                @test (-Symmetric([true false; true false]))::Symmetric{Int,Matrix{Int}} == [-1 0; -1 0]
             end
 
             @testset "Addition and subtraction for Symmetric/Hermitian matrices" begin
