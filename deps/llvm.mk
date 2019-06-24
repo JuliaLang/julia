bc06a36cfbefc6fc966fcd528cc7909e61bdc84c
Keno Fischer <keno@juliacomputing.com>
2019-06-23 21:37:42 -0400

    Carry LLVM patch that limits llvm-config flags
    This is an LLVM 6.0/7.0 backport of a patch that changes which flags
    llvm-config passes. This shouldn't matter much for most uses cases,
    but it's useful for two reasons:
    1) It makes the behavior of llvm-config consistent between all the LLVM
       versions we support, making it easier for find any problems with our
       Makefile.
    2) It prevents any flags meant for LLVM only from creeping into the build
       of our .cpp files (there isn't usually a need for this, but I needed
       to be able to do this when trying to target wasm)

--- deps/llvm.mk
+++ deps/llvm.mk
@@ -431,6 +431,7 @@ endif
 $(eval $(call LLVM_PATCH,llvm-D51842-win64-byval-cc))
 $(eval $(call LLVM_PATCH,llvm-D57118-powerpc))
 $(eval $(call LLVM_PATCH,llvm-r355582-avxminmax)) # remove for 8.0
+$(eval $(call LLVM_PATCH,llvm-rL349068-llvm-config)) # remove for 8.0
 endif # LLVM_VER 6.0
 
 ifeq ($(LLVM_VER_SHORT),7.0)
@@ -445,6 +446,7 @@ $(eval $(call LLVM_PATCH,llvm-7.0-D50167-scev-umin))
 $(eval $(call LLVM_PATCH,llvm7-windows-race))
 $(eval $(call LLVM_PATCH,llvm7-D51842-win64-byval-cc)) # remove for 8.0
 $(eval $(call LLVM_PATCH,llvm-D57118-powerpc))
+$(eval $(call LLVM_PATCH,llvm-rL349068-llvm-config)) # remove for 8.0
 endif # LLVM_VER 7.0
 
 ifeq ($(LLVM_VER_SHORT),8.0)
