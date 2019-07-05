a9da24d16723186800bf074a7e0bad144fad8486
Christian Peel <chris.peel@ieee.org>
2019-07-05 11:02:58 -0700

    remove "function" from dlpath docstring

--- stdlib/Libdl/src/Libdl.jl
+++ stdlib/Libdl/src/Libdl.jl
@@ -207,7 +207,7 @@ find_library(libname::Union{Symbol,AbstractString}, extrapaths=String[]) =
     find_library([string(libname)], extrapaths)
 
 """
-    function dlpath(handle::Ptr{Cvoid})
+    dlpath(handle::Ptr{Cvoid})
 
 Given a library `handle` from `dlopen`, return the full path.
 """
