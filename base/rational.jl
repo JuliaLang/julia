f3dcb6e91c8fd8173dc138df862fcec59d54924e
Simeon Schaub <simeondavidschaub99@gmail.com>
2019-07-13 10:38:50 +0200

    check if `typemin` is defined before calling it

--- base/rational.jl
+++ base/rational.jl
@@ -12,7 +12,9 @@ struct Rational{T<:Integer} <: Real
 
     function Rational{T}(num::Integer, den::Integer) where T<:Integer
         num == den == zero(T) && __throw_rational_argerror_zero(T)
-        den == typemin(T) &&  __throw_rational_argerror_typemin(T)
+        if T <: Signed && hasmethod(typemin, Tuple{Type{T}})
+            den == typemin(T) &&  __throw_rational_argerror_typemin(T)
+        end
         num2, den2 = signbit(den) ? divgcd(-num, -den) : divgcd(num, den)
         new(num2, den2)
     end
