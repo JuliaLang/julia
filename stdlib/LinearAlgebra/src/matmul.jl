5f7cab91bcb81570d71644faf223c11cf6b936a5
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2019-07-16 11:49:50 +0200

    add comment to explain reason for dispatching to BlasFloats
    Co-Authored-By: Fredrik Ekre <ekrefredrik@gmail.com>

--- stdlib/LinearAlgebra/src/matmul.jl
+++ stdlib/LinearAlgebra/src/matmul.jl
@@ -152,6 +152,7 @@ function (*)(A::AbstractMatrix, B::AbstractMatrix)
     TS = promote_op(matprod, eltype(A), eltype(B))
     mul!(similar(B, TS, (size(A,1), size(B,2))), A, B)
 end
+# optimization for dispatching to BLAS, e.g. *(::Matrix{Float32}, ::Matrix{Float64})
 function (*)(A::StridedMatrix{<:BlasFloat}, B::StridedMatrix{<:BlasFloat})
     TS = promote_type(eltype(A), eltype(B))
     mul!(similar(B, TS, (size(A,1), size(B,2))), convert(AbstractArray{TS}, A), convert(AbstractArray{TS}, B))
