1a6016745d89e366ece525b7405a6f15cf2b4c5b
Jeff Bezanson <jeff.bezanson@gmail.com>
2019-07-18 17:02:57 -0400

    fix #32620, crash when assigning to static parameter

--- test/syntax.jl
+++ test/syntax.jl
@@ -1563,6 +1563,23 @@ end
         return convert(B, b)
     end
 end) == Expr(:error, "local variable name \"B\" conflicts with a static parameter")
+# issue #32620
+@test Meta.lower(@__MODULE__, quote
+    function foo(a::T) where {T}
+        for i = 1:1
+            T = 0
+        end
+    end
+end) == Expr(:error, "local variable name \"T\" conflicts with a static parameter")
+function f32620(x::T) where T
+    local y
+    let T = 3
+        T = 2
+        y = T
+    end
+    return (T, y)
+end
+@test f32620(0) === (Int, 2)
 
 # issue #28044
 code28044(x) = 10x
