cb15864cf66a7360243155dc3e6efd48b45a759b
Jim Garrison <jim@garrison.cc>
2019-07-26 12:04:30 -0400

    Implement isdiag(::Bidiagonal), add tests

--- stdlib/LinearAlgebra/src/bidiag.jl
+++ stdlib/LinearAlgebra/src/bidiag.jl
@@ -250,6 +250,7 @@ iszero(M::Bidiagonal) = iszero(M.dv) && iszero(M.ev)
 isone(M::Bidiagonal) = all(isone, M.dv) && iszero(M.ev)
 istriu(M::Bidiagonal) = M.uplo == 'U' || iszero(M.ev)
 istril(M::Bidiagonal) = M.uplo == 'L' || iszero(M.ev)
+isdiag(M::Bidiagonal) = iszero(M.ev)
 
 function tril!(M::Bidiagonal, k::Integer=0)
     n = length(M.dv)
