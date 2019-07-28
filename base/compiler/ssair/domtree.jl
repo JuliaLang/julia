cffe93e132386046ae89d409d4eedc6b964f250b
Lyndon White <oxinabox@ucc.asn.au>
2019-07-28 16:50:09 -0400

    Update base/compiler/ssair/domtree.jl

--- base/compiler/ssair/domtree.jl
+++ base/compiler/ssair/domtree.jl
@@ -21,7 +21,9 @@ end
 
 """
     Checks if bb1 dominates bb2.
-    bb1 and bb2 are indexes into the 
+    bb1 and bb2 are indexes into the CFG blocks.
+    bb1 dominates bb2 if the only way to enter bb2 is via bb1. 
+    (Other blocks may be in between, e.g bb1->bbX->bb2).
 """
 function dominates(domtree::DomTree, bb1::Int, bb2::Int)
     bb1 == bb2 && return true
