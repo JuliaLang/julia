bd2afe306ba9cb23796830c4bf918c3467c53a5e
Alexander Seiler <seileralex@gmail.com>
2019-08-11 04:27:56 +0200

    Avoid accesing A.data and B.data directly
    My previous code produced wrong results when A and B were
    symmetric block matrices, because they accessed the raw
    data in a way that only works if A and B are symmetric
    matrices of numbers.

--- stdlib/LinearAlgebra/src/symmetric.jl
+++ stdlib/LinearAlgebra/src/symmetric.jl
@@ -424,15 +424,15 @@ function dot(A::Symmetric, B::Symmetric)
     @inbounds if A.uplo == 'U' && B.uplo == 'U'
         for j in 1:n
             for i in 1:(j - 1)
-                dotprod += 2 * dot(A.data[i, j], B.data[i, j])
+                dotprod += 2 * dot(A[i, j], B[i, j])
             end
-            dotprod += dot(A.data[j, j], B.data[j, j])
+            dotprod += dot(A[j, j], B[j, j])
         end
     elseif A.uplo == 'L' && B.uplo == 'L'
         for j in 1:n
-            dotprod += dot(A.data[j, j], B.data[j, j])
+            dotprod += dot(A[j, j], B[j, j])
             for i in (j + 1):n
-                dotprod += 2 * dot(A.data[i, j], B.data[i, j])
+                dotprod += 2 * dot(A[i, j], B[i, j])
             end
         end
     else
@@ -441,9 +441,9 @@ function dot(A::Symmetric, B::Symmetric)
         end
         for j in 1:n
             for i in 1:(j - 1)
-                dotprod += 2 * dot(A.data[i, j], B.data[j, i])
+                dotprod += 2 * dot(A[i, j], B[j, i])
             end
-            dotprod += dot(A.data[j, j], B.data[j, j])
+            dotprod += dot(A[j, j], B[j, j])
         end
     end
     return dotprod
