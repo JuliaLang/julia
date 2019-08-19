8248a9188670021ac560f22b63fffcacfac3fb7f
Stefan Karpinski <stefan@karpinski.org>
2019-08-19 18:12:12 -0400

    Base.operator_precedence: fix doc test

--- base/show.jl
+++ base/show.jl
@@ -833,7 +833,7 @@ operators. Return `0` if `s` is not a valid operator.
 # Examples
 ```jldoctest
 julia> Base.operator_precedence(:+), Base.operator_precedence(:*), Base.operator_precedence(:.)
-(11, 13, 17)
+(11, 12, 17)
 
 julia> Base.operator_precedence(:sin), Base.operator_precedence(:+=), Base.operator_precedence(:(=))  # (Note the necessary parens on `:(=)`)
 (0, 1, 1)
