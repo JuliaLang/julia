56fdb19aaad442d68fc9ea0c4759793a2265eb75
Kristoffer Carlsson <kcarlsson89@gmail.com>
2019-08-26 20:03:37 +0100

    fix performance regression in broadcasting in some cases (#33079)

--- base/broadcast.jl
+++ base/broadcast.jl
@@ -798,13 +798,13 @@ Like [`broadcast`](@ref), except in the case of a 0-dimensional result where it 
 Broadcast automatically unwraps zero-dimensional results to be just the element itself,
 but in some cases it is necessary to always return a container — even in the 0-dimensional case.
 """
-function broadcast_preserving_zero_d(f, As...)
+@inline function broadcast_preserving_zero_d(f, As...)
     bc = broadcasted(f, As...)
     r = materialize(bc)
     return length(axes(bc)) == 0 ? fill!(similar(bc, typeof(r)), r) : r
 end
-broadcast_preserving_zero_d(f) = fill(f())
-broadcast_preserving_zero_d(f, as::Number...) = fill(f(as...))
+@inline broadcast_preserving_zero_d(f) = fill(f())
+@inline broadcast_preserving_zero_d(f, as::Number...) = fill(f(as...))
 
 """
     Broadcast.materialize(bc)
