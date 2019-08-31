fdebdcc766397e7fcc5838efcb9ebb5a05065bb2
Ian <i.r.butterworth@gmail.com>
2019-08-31 11:09:15 -0400

    remove whitespace

--- test/project/deps/Foo1/src/Foo.jl
+++ test/project/deps/Foo1/src/Foo.jl
@@ -1,7 +1,7 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 module Foo
-include("SubFoo.jl") 
+include("SubFoo.jl")
 import Bar, Baz, Qux
 this = "Foo1"
 which = "path"
