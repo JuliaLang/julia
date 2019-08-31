48874e22cbc9565a137d71520aab70ae6abffc8d
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2019-08-31 11:54:49 +0200

    add compat note and jldoctest

--- stdlib/LinearAlgebra/src/generic.jl
+++ stdlib/LinearAlgebra/src/generic.jl
@@ -881,6 +881,21 @@ Compute the generalized dot product `dot(x, A*y)` between two vectors `x` and `y
 without storing the intermediate result of `A*y`. As for the two-argument
 [`dot(_,_)`](@ref), this acts recursively. Moreover, for complex vectors, the
 first vector is conjugated.
+
+!!! compat "Julia 1.4"
+    Three-argument `dot` requires at least Julia 1.4.
+
+# Examples
+```jldoctest
+julia> dot([1; 1], [1 2; 3 4], [2; 3])
+26
+
+julia> dot(1:5, reshape(1:25, 5, 5), 2:6)
+4850
+
+julia> ⋅(1:5, reshape(1:25, 5, 5), 2:6) == dot(1:5, reshape(1:25, 5, 5), 2:6)
+true
+```
 """
 dot(x, A, y) = dot(x, A*y) # generic fallback for cases that are not covered by specialized methods
 
