0849742f73aa19ff9ffa7a53c27141981a78355b
Jeff Bezanson <jeff.bezanson@gmail.com>
2019-09-04 14:28:20 -0400

    add `GC.safepoint()` for compute-bound threads (#33092)

--- base/gcutils.jl
+++ base/gcutils.jl
@@ -94,4 +94,19 @@ macro preserve(args...)
     end)
 end
 
+"""
+    GC.safepoint()
+
+Inserts a point in the program where garbage collection may run.
+This can be useful in rare cases in multi-threaded programs where some threads
+are allocating memory (and hence may need to run GC) but other threads are doing
+only simple operations (no allocation, task switches, or I/O).
+Calling this function periodically in non-allocating threads allows garbage
+collection to run.
+
+!!! compat "Julia 1.4"
+    This function is available as of Julia 1.4.
+"""
+safepoint() = ccall(:jl_gc_safepoint, Cvoid, ())
+
 end # module GC
