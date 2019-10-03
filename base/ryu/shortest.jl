f0301e52ffc72272685dcd09d95a558a3e92927f
Simon Byrne <simonbyrne@gmail.com>
2019-10-02 17:07:34 -0700

    Ryu: split out reduce short routine

--- base/ryu/shortest.jl
+++ base/ryu/shortest.jl
@@ -1,80 +1,9 @@
-@inline function writeshortest(buf::Vector{UInt8}, pos, x::T,
-    plus=false, space=false, hash=true,
-    precision=-1, expchar=UInt8('e'), padexp=false, decchar=UInt8('.'), typed=false, compact=false) where {T}
-    @assert 0 < pos <= length(buf)
-    neg = signbit(x)
-    # special cases
-    if x == 0
-        if neg
-            buf[pos] = UInt8('-')
-            pos += 1
-        elseif plus
-            buf[pos] = UInt8('+')
-            pos += 1
-        elseif space
-            buf[pos] = UInt8(' ')
-            pos += 1
-        end
-        buf[pos] = UInt8('0')
-        pos += 1
-        if hash
-            buf[pos] = decchar
-            pos += 1
-        end
-        if precision == -1
-            buf[pos] = UInt8('0')
-            pos += 1
-            if typed && x isa Float32
-                buf[pos] = UInt8('f')
-                buf[pos + 1] = UInt8('0')
-                pos += 2
-            end
-            return pos
-        end
-        while precision > 1
-            buf[pos] = UInt8('0')
-            pos += 1
-            precision -= 1
-        end
-        if typed && x isa Float32
-            buf[pos] = UInt8('f')
-            buf[pos + 1] = UInt8('0')
-            pos += 2
-        end
-        return pos
-    elseif isnan(x)
-        buf[pos] = UInt8('N')
-        buf[pos + 1] = UInt8('a')
-        buf[pos + 2] = UInt8('N')
-        if typed
-            if x isa Float32
-                buf[pos + 3] = UInt8('3')
-                buf[pos + 4] = UInt8('2')
-            elseif x isa Float16
-                buf[pos + 3] = UInt8('1')
-                buf[pos + 4] = UInt8('6')
-            end
-        end
-        return pos + 3 + (typed && x isa Union{Float32, Float16} ? 2 : 0)
-    elseif !isfinite(x)
-        if neg
-            buf[pos] = UInt8('-')
-        end
-        buf[pos + neg] = UInt8('I')
-        buf[pos + neg + 1] = UInt8('n')
-        buf[pos + neg + 2] = UInt8('f')
-        if typed
-            if x isa Float32
-                buf[pos + neg + 3] = UInt8('3')
-                buf[pos + neg + 4] = UInt8('2')
-            elseif x isa Float16
-                buf[pos + neg + 3] = UInt8('1')
-                buf[pos + neg + 4] = UInt8('6')
-            end
-        end
-        return pos + neg + 3 + (typed && x isa Union{Float32, Float16} ? 2 : 0)
-    end
+"""
+    output, nexp = reduce_shortest(x)
 
+Reduce to shortest decimal representation where `x = output * 10^nexp`
+"""
+@inline function reduce_shortest(x::T) where {T}
     bits = reinterpret(Unsigned, x)
     mant = bits & (oftype(bits, 1) << significand_bits(T) - oftype(bits, 1))
     exp = Int((bits >> significand_bits(T)) & ((Int64(1) << exponent_bits(T)) - 1))
@@ -219,6 +148,88 @@
             nexp += 1
         end
     end
+    return output, nexp
+end
+
+
+@inline function writeshortest(buf::Vector{UInt8}, pos, x::T,
+                               plus=false, space=false, hash=true,
+                               precision=-1, expchar=UInt8('e'), padexp=false, decchar=UInt8('.'), typed=false, compact=false) where {T}
+    @assert 0 < pos <= length(buf)
+    neg = signbit(x)
+    # special cases
+    if x == 0
+        if neg
+            buf[pos] = UInt8('-')
+            pos += 1
+        elseif plus
+            buf[pos] = UInt8('+')
+            pos += 1
+        elseif space
+            buf[pos] = UInt8(' ')
+            pos += 1
+        end
+        buf[pos] = UInt8('0')
+        pos += 1
+        if hash
+            buf[pos] = decchar
+            pos += 1
+        end
+        if precision == -1
+            buf[pos] = UInt8('0')
+            pos += 1
+            if typed && x isa Float32
+                buf[pos] = UInt8('f')
+                buf[pos + 1] = UInt8('0')
+                pos += 2
+            end
+            return pos
+        end
+        while precision > 1
+            buf[pos] = UInt8('0')
+            pos += 1
+            precision -= 1
+        end
+        if typed && x isa Float32
+            buf[pos] = UInt8('f')
+            buf[pos + 1] = UInt8('0')
+            pos += 2
+        end
+        return pos
+    elseif isnan(x)
+        buf[pos] = UInt8('N')
+        buf[pos + 1] = UInt8('a')
+        buf[pos + 2] = UInt8('N')
+        if typed
+            if x isa Float32
+                buf[pos + 3] = UInt8('3')
+                buf[pos + 4] = UInt8('2')
+            elseif x isa Float16
+                buf[pos + 3] = UInt8('1')
+                buf[pos + 4] = UInt8('6')
+            end
+        end
+        return pos + 3 + (typed && x isa Union{Float32, Float16} ? 2 : 0)
+    elseif !isfinite(x)
+        if neg
+            buf[pos] = UInt8('-')
+        end
+        buf[pos + neg] = UInt8('I')
+        buf[pos + neg + 1] = UInt8('n')
+        buf[pos + neg + 2] = UInt8('f')
+        if typed
+            if x isa Float32
+                buf[pos + neg + 3] = UInt8('3')
+                buf[pos + neg + 4] = UInt8('2')
+            elseif x isa Float16
+                buf[pos + neg + 3] = UInt8('1')
+                buf[pos + neg + 4] = UInt8('6')
+            end
+        end
+        return pos + neg + 3 + (typed && x isa Union{Float32, Float16} ? 2 : 0)
+    end
+
+    output, nexp = reduce_shortest(x)
 
     if typed && x isa Float16
         buf[pos] = UInt8('F')
@@ -286,9 +297,9 @@
                 buf[pos] = UInt8('0')
                 pos += 1
             end
-        # elseif pt >= olength
+            # elseif pt >= olength
             # nothing to do at this point
-        # else
+            # else
             # nothing to do at this point
         end
     else
@@ -325,116 +336,116 @@
         memcpy(ptr, pos + olength - i - 4, ptr2, c1 + 1, 2)
         i += 4
     end
-    if output2 >= 100
-        c = (output2 % UInt32(100)) << 1
-        output2 = div(output2, UInt32(100))
-        memcpy(ptr, pos + olength - i - 2, ptr2, c + 1, 2)
-        i += 2
-    end
-    if output2 >= 10
-        c = output2 << 1
-        buf[pos + 1] = DIGIT_TABLE[c + 2]
-        buf[pos - exp_form] = DIGIT_TABLE[c + 1]
-    else
-        buf[pos - exp_form] = UInt8('0') + (output2 % UInt8)
-    end
+if output2 >= 100
+    c = (output2 % UInt32(100)) << 1
+    output2 = div(output2, UInt32(100))
+    memcpy(ptr, pos + olength - i - 2, ptr2, c + 1, 2)
+    i += 2
+end
+if output2 >= 10
+    c = output2 << 1
+    buf[pos + 1] = DIGIT_TABLE[c + 2]
+    buf[pos - exp_form] = DIGIT_TABLE[c + 1]
+else
+    buf[pos - exp_form] = UInt8('0') + (output2 % UInt8)
+end
 
-    if !exp_form
-        if pt <= 0
-            pos += olength
-            precision -= olength
-            while hash && precision > 0
-                buf[pos] = UInt8('0')
-                pos += 1
-                precision -= 1
-            end
-        elseif pt >= olength
-            pos += olength
-            precision -= olength
-            for _ = 1:nexp
-                buf[pos] = UInt8('0')
-                pos += 1
-                precision -= 1
-            end
-            if hash
-                buf[pos] = decchar
-                pos += 1
-                if precision < 0
-                    buf[pos] = UInt8('0')
-                    pos += 1
-                end
-                while precision > 0
-                    buf[pos] = UInt8('0')
-                    pos += 1
-                    precision -= 1
-                end
-            end
-        else
-            pointoff = olength - abs(nexp)
-            memmove(ptr, pos + pointoff + 1, ptr, pos + pointoff, olength - pointoff + 1)
-            buf[pos + pointoff] = decchar
-            pos += olength + 1
-            precision -= olength
-            while hash && precision > 0
-                buf[pos] = UInt8('0')
-                pos += 1
-                precision -= 1
-            end
-        end
-        if typed && x isa Float32
-            buf[pos] = UInt8('f')
-            buf[pos + 1] = UInt8('0')
-            pos += 2
-        end
-    else
-        if olength > 1 || hash
-            buf[pos] = decchar
-            pos += olength
-            precision -= olength
-        end
-        if hash && olength == 1
-            buf[pos] = UInt8('0')
-            pos += 1
-        end
+if !exp_form
+    if pt <= 0
+        pos += olength
+        precision -= olength
         while hash && precision > 0
             buf[pos] = UInt8('0')
             pos += 1
             precision -= 1
         end
-
-        buf[pos] = expchar
-        pos += 1
-        exp2 = nexp + olength - 1
-        if exp2 < 0
-            buf[pos] = UInt8('-')
+    elseif pt >= olength
+        pos += olength
+        precision -= olength
+        for _ = 1:nexp
+            buf[pos] = UInt8('0')
             pos += 1
-            exp2 = -exp2
-        elseif padexp
-            buf[pos] = UInt8('+')
-            pos += 1
+            precision -= 1
         end
-
-        if exp2 >= 100
-            c = exp2 % 10
-            memcpy(ptr, pos, ptr2, 2 * div(exp2, 10) + 1, 2)
-            buf[pos + 2] = UInt8('0') + (c % UInt8)
-            pos += 3
-        elseif exp2 >= 10
-            memcpy(ptr, pos, ptr2, 2 * exp2 + 1, 2)
-            pos += 2
-        else
-            if padexp
+        if hash
+            buf[pos] = decchar
+            pos += 1
+            if precision < 0
                 buf[pos] = UInt8('0')
                 pos += 1
             end
-            buf[pos] = UInt8('0') + (exp2 % UInt8)
+            while precision > 0
+                buf[pos] = UInt8('0')
+                pos += 1
+                precision -= 1
+            end
+        end
+    else
+        pointoff = olength - abs(nexp)
+        memmove(ptr, pos + pointoff + 1, ptr, pos + pointoff, olength - pointoff + 1)
+        buf[pos + pointoff] = decchar
+        pos += olength + 1
+        precision -= olength
+        while hash && precision > 0
+            buf[pos] = UInt8('0')
             pos += 1
+            precision -= 1
         end
     end
-    if typed && x isa Float16
-        buf[pos] = UInt8(')')
+    if typed && x isa Float32
+        buf[pos] = UInt8('f')
+        buf[pos + 1] = UInt8('0')
+        pos += 2
+    end
+else
+    if olength > 1 || hash
+        buf[pos] = decchar
+        pos += olength
+        precision -= olength
+    end
+    if hash && olength == 1
+        buf[pos] = UInt8('0')
+        pos += 1
+    end
+    while hash && precision > 0
+        buf[pos] = UInt8('0')
+        pos += 1
+        precision -= 1
+    end
+
+    buf[pos] = expchar
+    pos += 1
+    exp2 = nexp + olength - 1
+    if exp2 < 0
+        buf[pos] = UInt8('-')
+        pos += 1
+        exp2 = -exp2
+    elseif padexp
+        buf[pos] = UInt8('+')
         pos += 1
     end
 
-    return pos
+    if exp2 >= 100
+        c = exp2 % 10
+        memcpy(ptr, pos, ptr2, 2 * div(exp2, 10) + 1, 2)
+        buf[pos + 2] = UInt8('0') + (c % UInt8)
+        pos += 3
+    elseif exp2 >= 10
+        memcpy(ptr, pos, ptr2, 2 * exp2 + 1, 2)
+        pos += 2
+    else
+        if padexp
+            buf[pos] = UInt8('0')
+            pos += 1
+        end
+        buf[pos] = UInt8('0') + (exp2 % UInt8)
+        pos += 1
+    end
+end
+if typed && x isa Float16
+    buf[pos] = UInt8(')')
+    pos += 1
+end
+
+return pos
 end
