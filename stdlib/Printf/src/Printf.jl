bfd4b4cf15875531b404ad2ae014657ae770b96c
Jacob Quinn <quinn.jacobd@gmail.com>
2019-10-09 13:31:55 -0600

    Fix docs

--- stdlib/Printf/src/Printf.jl
+++ stdlib/Printf/src/Printf.jl
@@ -702,6 +702,24 @@ function format(f::Format, args...) # => String
     return String(resize!(buf, pos - 1))
 end
 
+"""
+    @printf([io::IOStream], "%Fmt", args...)
+Print `args` using C `printf` style format specification string, with some caveats:
+`Inf` and `NaN` are printed consistently as `Inf` and `NaN` for flags `%a`, `%A`,
+`%e`, `%E`, `%f`, `%F`, `%g`, and `%G`. Furthermore, if a floating point number is
+equally close to the numeric values of two possible output strings, the output
+string further away from zero is chosen.
+Optionally, an [`IOStream`](@ref)
+may be passed as the first argument to redirect output.
+See also: [`@sprintf`](@ref)
+# Examples
+```jldoctest
+julia> @printf("%f %F %f %F\\n", Inf, Inf, NaN, NaN)
+Inf Inf NaN NaN\n
+julia> @printf "%.0f %.1f %f\\n" 0.5 0.025 -0.0078125
+1 0.0 -0.007813
+```
+"""
 macro printf(io_or_fmt, args...)
     if io_or_fmt isa String
         io = stdout
@@ -715,6 +733,16 @@ macro printf(io_or_fmt, args...)
     end
 end
 
+"""
+    @sprintf("%Fmt", args...)
+Return `@printf` formatted output as string.
+# Examples
+```jldoctest
+julia> s = @sprintf "this is a %s %15.1f" "test" 34.567;
+julia> println(s)
+this is a test            34.6
+```
+"""
 macro sprintf(fmt, args...)
     f = Format(fmt)
     return esc(:($Printf.format($f, $(args...))))
