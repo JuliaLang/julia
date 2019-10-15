e3b476769fb6267b31cb9f97235aa40fe541e04e
Antoine Levitt <antoine.levitt@gmail.com>
2019-10-15 12:28:18 +0200

    Move test

--- stdlib/LinearAlgebra/test/dense.jl
+++ stdlib/LinearAlgebra/test/dense.jl
@@ -35,6 +35,11 @@ Random.seed!(1234321)
         @test cond([0 0; 1 1], p) == Inf
         @test cond([0. 0.; 1. 1.], p) == Inf
     end
+    
+    @testset "Issue #33547, condition number of 2x2 matrix" begin
+        M = [1.0 -2.0; -2.0 -1.5]
+        @test cond(M, 1) ≈ 2.227272727272727
+    end
 end
 
 areal = randn(n,n)/2
