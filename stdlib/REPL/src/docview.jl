ed1ba12051c9fad857898922565a3e4197e8565b
lhnguyen-vn <lhnguyen.vn@gmail.com>
2019-10-23 23:02:12 -0400

    Help for UnionAll now prints out underlying data type

--- stdlib/REPL/src/docview.jl
+++ stdlib/REPL/src/docview.jl
@@ -214,8 +214,14 @@ function summarize(io::IO, m::Module, binding)
 end
 
 function summarize(io::IO, @nospecialize(T), binding)
-    T = typeof(T)
-    println(io, "`", binding, "` is of type `", T, "`.\n")
+    println(io, "`", binding, "` is of type `", typeof(T), "`.\n")
+    if isa(T, UnionAll)
+        while hasproperty(T, :body)
+            T = T.body
+        end
+    else
+        T = typeof(T)
+    end
     summarize(io, T, binding)
 end
 
