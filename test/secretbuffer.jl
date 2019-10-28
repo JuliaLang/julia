d4fc26bc82ce9136f6a44dd34ed263acaff7bf6f
Chris Foster <chris42f@gmail.com>
2019-10-28 16:50:52 +1000

    Make finalizer warning for shredded SecretBuffer async
    This avoids IO in the finalizer where it's invalid to task switch.

--- test/secretbuffer.jl
+++ test/secretbuffer.jl
@@ -28,7 +28,15 @@ using Test
         @test !isshredded(secret_b)
 
         # TODO: ideally we'd test that the finalizer warns from GC.gc(), but that is harder
-        @test_logs (:warn, r".*SecretBuffer was `shred!`ed by the GC.*") finalize(secret_b)
+        @test_logs (:warn, r".*SecretBuffer was `shred!`ed by the GC.*") begin
+            finalize(secret_b)
+            # Allow the async task which produces the SecretBuffer warning to run.
+            # This is a hack, but we don't have a way to get a handle to that
+            # task in order to `wait` on it.
+            for i=1:1000
+                yield()
+            end
+        end
         @test isshredded(secret_b)
         secret_b = nothing
         GC.gc()
