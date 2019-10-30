a92f56651d5182ebbe3e5cbe1a3fd47ed3a51372
Ian Butterworth <i.r.butterworth@gmail.com>
2019-10-30 12:20:00 -0400

    Add pkgdir(m::Module) (#33128)

--- base/loading.jl
+++ base/loading.jl
@@ -265,7 +265,7 @@ end
 """
     pathof(m::Module)
 
-Return the path of `m.jl` file that was used to `import` module `m`,
+Return the path of the `m.jl` file that was used to `import` module `m`,
 or `nothing` if `m` was not imported from a package.
 
 Use [`dirname`](@ref) to get the directory part and [`basename`](@ref)
@@ -275,6 +275,19 @@ function pathof(m::Module)
     pkgid = get(Base.module_keys, m, nothing)
     pkgid === nothing && return nothing
     return Base.locate_package(pkgid)
+end
+
+"""
+    pkgdir(m::Module)
+
+ Return the root directory of the package that imported module `m`,
+ or `nothing` if `m` was not imported from a package.
+ """
+function pkgdir(m::Module)
+    rootmodule = Base.moduleroot(m)
+    path = pathof(rootmodule)
+    path === nothing && return nothing
+    return dirname(dirname(path))
 end
 
 ## generic project & manifest API ##
--- test/project/deps/Foo1/src/SubFoo.jl
+++ test/project/deps/Foo1/src/SubFoo.jl
@@ -1,8 +1,5 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
-module Foo
-include("SubFoo.jl")
-import Bar, Baz, Qux
-this = "Foo1"
-which = "path"
+module SubFoo
+thissub = "Foo1.SubFoo"
 end
