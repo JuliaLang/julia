e0dc086c371cba9c69aa414596af2befddb3ae82
Matt Bauman <mbauman@juliacomputing.com>
2019-10-31 15:58:35 -0500

    Update base/subarray.jl
    Co-Authored-By: Tim Holy <tim.holy@gmail.com>

--- base/subarray.jl
+++ base/subarray.jl
@@ -7,7 +7,7 @@ const ScalarIndex = Real
 """
     SubArray{T,N,P,I,L} <: AbstractArray{T,N}
 
-`N`-dimensional view into a parent array (of type `P`) with an element type `T`, restricted by a tuple of indices (of type `I`).
+`N`-dimensional view into a parent array (of type `P`) with an element type `T`, restricted by a tuple of indices (of type `I`). `L` is true for types that support fast linear indexing, and `false` otherwise.
 
 Construct `SubArray`s using the [`view`](@ref) function.
 """
