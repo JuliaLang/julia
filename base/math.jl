9613615c956dd4ebaf18dcf501ef937278e3fcf0
Alex Arslan <ararslan@comcast.net>
2019-11-21 13:08:34 -0800

    Move algorithm reference to footnote
    Co-Authored-By: Fredrik Ekre <ekrefredrik@gmail.com>

--- base/math.jl
+++ base/math.jl
@@ -90,9 +90,9 @@ that is, the coefficients are given in ascending order by power of `x`.
 Loops are unrolled at compile time if the number of coefficients is statically known, i.e.
 when `p` is a `Tuple`.
 This function generates efficient code using Horner's method if `x` is real, or using
-a Goertzel-like algorithm if `x` is complex.
-The Goertzel-like algorthim is described in Knuth's _Art of Computer Programming_,
-Volume 2: Seminumerical Algorithms, Sec. 4.6.4.
+a Goertzel-like [^DK62] algorithm if `x` is complex.
+
+[^DK62]: Donald Knuth, Art of Computer Programming, Volume 2: Seminumerical Algorithms, Sec. 4.6.4.
 
 !!! compat "Julia 1.4"
     This function requires Julia 1.4 or later.
