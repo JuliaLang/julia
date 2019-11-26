3585a6e43b169a07b7e7e5397270c869cc05a248
Chris Foster <chris42f@gmail.com>
2019-11-26 17:25:23 +1000

    Clearer API for custom log levels
    Add AbstractLogLevel as the supertype of all log level types.
    
    Introduce "log level severity" including a `severity` function which
    maps any user-defined log level to an integer log level severity. This
    more cleanly separates out the ordinal notion of log severity from the
    categorical aspects of the log level.
    
    Log levels can be ordered by comparing their severities. This gives a
    certian type of pre-order which is reflexive, transitive and total, but
    not antisymmetric. This is the same as the ordering of complex numbers
    by their magnitude.
    
    Change to using `print` rather than `show` for formatting log levels for
    display. This seems to resolve the old conundrum of "do we print Warn as
    it appears in the program as "Warn" or in human-friendly form as
    "Warning""?
    
    The early (global) decision about log level filtering within the macro
    is now lowered to `shouldlog(level)` which is more natural and more
    customizable.

--- stdlib/Logging/src/Logging.jl
+++ stdlib/Logging/src/Logging.jl
@@ -12,10 +12,10 @@ module Logging
 # Doing it this way (rather than with import) makes these symbols accessible to
 # tab completion.
 for sym in [
-    :LogLevel, :BelowMinLevel, :Debug, :Info, :Warn, :Error, :AboveMaxLevel,
+    :AbstractLogLevel, :LogLevel, :BelowMinLevel, :Debug, :Info, :Warn, :Error, :AboveMaxLevel,
     :AbstractLogger,
     :NullLogger,
-    :handle_message, :shouldlog, :min_enabled_level, :catch_exceptions,
+    :handle_message, :severity, :shouldlog, :min_enabled_level, :catch_exceptions,
     Symbol("@debug"),
     Symbol("@info"),
     Symbol("@warn"),
@@ -58,5 +58,23 @@ include("ConsoleLogger.jl")
 function __init__()
     global_logger(ConsoleLogger(stderr))
 end
+
+
+#--------------------------------------------------
+# DEPRECATIONS
+#
+# In julia 1.4 the `severity` function was added to clarify the ordinal vs
+# categorical nature of log levels. The following functions are deprecated,
+# but left here until stdlibs can be versioned independently from Base.
+Base.isless(a::LogLevel, b::LogLevel) = isless(severity(a), severity(b))
+Base.:+(level::LogLevel, inc::Integer) = LogLevel(severity(level)+inc)
+Base.:-(level::LogLevel, inc::Integer) = LogLevel(severity(level)-inc)
+Base.convert(::Type{LogLevel}, level::Integer) = LogLevel(level)
+
+# Backward compatible fallbacks for levels.
+# `convert` used to be called by the macro-based lowering to support custom levels.
+severity(level) = severity(convert(LogLevel, level))
+# New lowering for global level override - fallback to use of convert
+shouldlog(level) = shouldlog(convert(LogLevel, level))
 
 end
