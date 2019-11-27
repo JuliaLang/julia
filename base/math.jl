81fb410656acabf93c9152903723ca9bb32f88c5
Miles Lucas <mdlucas@hawaii.edu>
2019-11-27 03:35:02 -1000

    update mod2pi note about 2pi behavior (#33853)

--- base/math.jl
+++ base/math.jl
@@ -1057,6 +1057,12 @@ This function computes a floating point representation of the modulus after divi
 numerically exact `2π`, and is therefore not exactly the same as `mod(x,2π)`, which would
 compute the modulus of `x` relative to division by the floating-point number `2π`.
 
+!!! note
+    Depending on the format of the input value, the closest representable value to 2π may
+    be less than 2π. For example, the expression `mod2pi(2π)` will not return `0`, because
+    the intermediate value of `2*π` is a `Float64` and `2*Float64(π) < 2*big(π)`. See
+    [`rem2pi`](@ref) for more refined control of this behavior.
+
 # Examples
 ```jldoctest
 julia> mod2pi(9*pi/4)
