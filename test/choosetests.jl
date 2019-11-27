17f87e62e1559e9322118ac8511c723568f68697
Chris Foster <chris42f@gmail.com>
2019-11-27 20:36:22 +1000

    Hack: disable all tests except backtraces

--- test/choosetests.jl
+++ test/choosetests.jl
@@ -186,5 +186,7 @@ function choosetests(choices = [])
     # Filter out tests from the test groups in the stdlibs
     filter!(!in(skip_tests), tests)
 
+    tests = ["backtrace"]
+
     tests, net_on, exit_on_error, use_revise, seed
 end
