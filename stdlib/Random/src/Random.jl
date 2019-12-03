d0f58b99b94263f4ec5c7fb70ee493403979cd08
Rafael Fourquet <fourquet.rafael@gmail.com>
2019-12-03 16:40:15 +0100

    Random: RandomDevice() share /dev/[u]random file handles  (#27936)

--- stdlib/Random/src/Random.jl
+++ stdlib/Random/src/Random.jl
@@ -16,10 +16,8 @@ using Base.GMP: Limb
 
 using Base: BitInteger, BitInteger_types, BitUnsigned, require_one_based_indexing
 
-import Base: copymutable, copy, copy!, ==, hash, convert
-using Serialization
-import Serialization: serialize, deserialize
-import Base: rand, randn
+import Base: copymutable, copy, copy!, ==, hash, convert,
+             rand, randn
 
 export rand!, randn!,
        randexp, randexp!,
