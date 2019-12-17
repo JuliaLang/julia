08345abd45a1ce09543d2abfc2f2ed37f85d4b19
Alex Arslan <ararslan@comcast.net>
2019-12-17 11:17:45 -0800

    Replace erroneous inbounds function annotation with inline
    See https://github.com/JuliaLang/julia/pull/33886#discussion_r358975658

--- base/array.jl
+++ base/array.jl
@@ -177,7 +177,7 @@ false
 isbitsunion(u::Union) = allocatedinline(u)
 isbitsunion(x) = false
 
-@inbounds function _unsetindex!(A::Array{T}, i::Int) where {T}
+@inline function _unsetindex!(A::Array{T}, i::Int) where {T}
     @boundscheck checkbounds(A, i)
     t = @_gc_preserve_begin A
     p = Ptr{Ptr{Cvoid}}(pointer(A, i))
