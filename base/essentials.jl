ce260dc47dd918f7c1a239198de678615b3b8abb
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-12-19 22:21:12 +0100

    improve AbstractDict doc (#34110)

--- base/essentials.jl
+++ base/essentials.jl
@@ -19,6 +19,7 @@ abstract type AbstractSet{T} end
 
 Supertype for dictionary-like types with keys of type `K` and values of type `V`.
 [`Dict`](@ref), [`IdDict`](@ref) and other types are subtypes of this.
+An `AbstractDict{K, V}` should be an iterator of `Pair{K, V}`.
 """
 abstract type AbstractDict{K,V} end
 
