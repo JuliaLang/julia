d25e6d177a0eb4f7e09f619a6e14f3f87694d4ec
Nathan <nhdaly@gmail.com>
2019-12-19 19:37:12 +0530

    Use same mechanism for quotes & macrocalls: subtract one level of `$`.
    The problem is this breaks if the macro _doesn't_ use the `$`, then
    the `$` is incorrectly never used.

--- base/task.jl
+++ base/task.jl
@@ -386,10 +386,8 @@ function _lift_one_interp_helper(expr::Expr, in_quote_context, letargs)
             push!(letargs, :($(esc(newarg)) = $(esc(expr.args[1]))))
             return newarg  # Don't recurse into the lifted $() exprs
         end
-    elseif expr.head == :quote
+    elseif expr.head == :quote || expr.head == :macrocall
         in_quote_context = true   # Don't try to lift $ directly out of quotes
-    elseif expr.head == :macrocall
-        return expr
     end
     for (i,e) in enumerate(expr.args)
         expr.args[i] = _lift_one_interp_helper(e, in_quote_context, letargs)
