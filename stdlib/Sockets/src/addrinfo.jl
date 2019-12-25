b61b35a9b6656f1ddb6ee25429bbb9e5480f897d
Atsushi Sakai <asakai.amsl+github@gmail.com>
2019-12-25 20:12:37 +0900

    removed tail space

--- stdlib/Sockets/src/addrinfo.jl
+++ stdlib/Sockets/src/addrinfo.jl
@@ -3,7 +3,7 @@
 """
     DNSError
 
-This exception thrown when an error occurs in DNS lookup. 
+This exception thrown when an error occurs in DNS lookup.
 The `host` field indicates the host URL string.
 The `code` field indicates the error code based on libuv.
 """
