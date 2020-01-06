40630a0f4d28ab10f54019fe1bccc8cb3165af04
Elliot Saba <staticfloat@gmail.com>
2020-01-05 19:04:24 -0800

    Bump OpenBLAS BB release to include win32 threading patch

--- deps/blas.mk
+++ deps/blas.mk
@@ -209,7 +209,7 @@ check-lapack: $(BUILDDIR)/lapack-$(LAPACK_VER)/build-checked
 else # USE_BINARYBUILDER_OPENBLAS
 
 
-OPENBLAS_BB_URL_BASE := https://github.com/JuliaPackaging/Yggdrasil/releases/download/OpenBLAS-v$(OPENBLAS_VER)-$(OPENBLAS_BB_REL)
+OPENBLAS_BB_URL_BASE := https://github.com/JuliaBinaryWrappers/OpenBLAS_jll.jl/releases/download/OpenBLAS-v$(OPENBLAS_VER)+$(OPENBLAS_BB_REL)
 OPENBLAS_BB_NAME := OpenBLAS.v$(OPENBLAS_VER)
 
 $(eval $(call bb-install,openblas,OPENBLAS,true))
