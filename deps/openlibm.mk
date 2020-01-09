cf605a0ffc91336ccac6980f76c0574fa29e9d35
Viral B. Shah <viral@mayin.org>
2020-01-09 10:46:49 -0500

    Update openlibm to 0.7. Use artifact binaries. Disable gcc platform expansion.

--- deps/openlibm.mk
+++ deps/openlibm.mk
@@ -1,6 +1,5 @@
 ## openlibm ##
 
-
 OPENLIBM_GIT_URL := git://github.com/JuliaMath/openlibm.git
 OPENLIBM_TAR_URL = https://api.github.com/repos/JuliaMath/openlibm/tarball/$1
 $(eval $(call git-external,openlibm,OPENLIBM,,,$(BUILDDIR)))
@@ -31,8 +30,9 @@ check-openlibm: compile-openlibm
 
 else # USE_BINARYBUILDER_OPENLIBM
 
-OPENLIBM_BB_URL_BASE := https://github.com/JuliaPackaging/Yggdrasil/releases/download/OpenLibm-v$(OPENLIBM_VER)-$(OPENLIBM_BB_REL)
+OPENLIBM_BB_URL_BASE := https://github.com/JuliaBinaryWrappers/OpenLibm_jll.jl/releases/download/OpenLibm-v$(OPENLIBM_VER)+$(OPENLIBM_BB_REL)
 OPENLIBM_BB_NAME := OpenLibm.v$(OPENLIBM_VER)
 
-$(eval $(call bb-install,openlibm,OPENLIBM,true))
+$(eval $(call bb-install,openlibm,OPENLIBM,false))
+
 endif
