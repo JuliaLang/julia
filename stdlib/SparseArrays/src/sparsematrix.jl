413a8912358f7ae8f76748f315202edaa44945ca
Pablo San-Jose <lekand@gmail.com>
2020-01-15 11:42:55 +0100

    fix: deal with empty matrices in _sparse_copyto!

--- stdlib/SparseArrays/src/sparsematrix.jl
+++ stdlib/SparseArrays/src/sparsematrix.jl
@@ -360,12 +360,13 @@ copyto!(A::PermutedDimsArray, B::AbstractSparseMatrixCSC) = _sparse_copyto!(A, B
 
 function _sparse_copyto!(dest::AbstractMatrix, src::AbstractSparseMatrixCSC)
     dest === src && return dest
+    z = convert(eltype(dest), zero(eltype(src))) # should throw if not possible
+    isempty(dest) && isempty(src) && return dest
     isrc = LinearIndices(src)
     checkbounds(dest, first(isrc))
     checkbounds(dest, last(isrc))
     # If src is not dense, zero out the portion of dest spanned by isrc
     if length(src) > nnz(src)
-        z = convert(eltype(dest), zero(eltype(src))) # should throw if not possible
         for i in isrc
             @inbounds dest[i] = z
         end
