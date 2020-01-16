94249dab6ac90d98e67dd4018448728e2cf8d4d1
Jameson Nash <vtjnash@gmail.com>
2020-01-15 19:00:03 -0500

    coverage: fix name of the new code_coverage_effect Expr head (#34392)

--- base/compiler/ssair/driver.jl
+++ base/compiler/ssair/driver.jl
@@ -61,7 +61,7 @@ function convert_to_ircode(ci::CodeInfo, code::Vector{Any}, coverage::Bool, narg
         codeloc = ci.codelocs[idx]
         if coverage && codeloc != prevloc && codeloc != 0
             # insert a side-effect instruction before the current instruction in the same basic block
-            insert!(code, idx, Expr(:code_coverage_marker))
+            insert!(code, idx, Expr(:code_coverage_effect))
             insert!(ci.codelocs, idx, codeloc)
             insert!(ci.ssavaluetypes, idx, Nothing)
             changemap[oldidx] += 1
