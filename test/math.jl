5aed97ea335dddb6e81d88154cf4578c1e8611d0
Keno Fischer <keno@alumni.harvard.edu>
2020-01-17 18:17:03 -0500

    Add clamp(x, T)
    This is #22235, but with the reversed argument order.
    ```
    julia> clamp( 260, UInt8 )
    0xff
    ```
    
    Co-authored-by: "arghhhh <david.hossack@gmail.com>"

--- test/math.jl
+++ test/math.jl
@@ -22,6 +22,11 @@ end
 
     @test clamp.([0, 1, 2, 3, 4], 1.0, 3.0) == [1.0, 1.0, 2.0, 3.0, 3.0]
     @test clamp.([0 1; 2 3], 1.0, 3.0) == [1.0 1.0; 2.0 3.0]
+
+    @test clamp(-200, Int8) === typemin(Int8)
+    @test clamp(100, Int8) === Int8(100)
+    @test clamp(200, Int8) === typemax(Int8)
+
     begin
         x = [0.0, 1.0, 2.0, 3.0, 4.0]
         clamp!(x, 1, 3)
