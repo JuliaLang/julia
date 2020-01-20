f543debd04d9155066c579ecb7f3f0fe20035ec1
Shan Sikdar <ssikdar1@bu.edu>
2020-01-20 11:17:36 -0500

    fix unit test to import func correctly

--- stdlib/Distributed/test/managers.jl
+++ stdlib/Distributed/test/managers.jl
@@ -3,8 +3,7 @@ using Distributed
 using Base: Semaphore
 using Sockets
 
-const BASE_TEST_PATH = joinpath(Sys.BINDIR, "..", "share", "julia", "stdlib", "v1.5", "Distributed", "src")
-@eval Main include(joinpath($(BASE_TEST_PATH), "managers.jl"))
+using Distributed: parse_machine
 
 @test parse_machine("127.0.0.1") == ("127.0.0.1", "")
 @test parse_machine("127.0.0.1:80") == ("127.0.0.1", "80")
