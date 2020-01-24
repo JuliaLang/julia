9806b42d663a7e48917619b13f63ddadc16092d8
Shan Sikdar <ssikdar1@bu.edu>
2020-01-23 22:33:28 -0500

    Add managers to runstests

--- stdlib/Distributed/test/runtests.jl
+++ stdlib/Distributed/test/runtests.jl
@@ -3,9 +3,11 @@
 # Run the distributed test outside of the main driver since it needs its own
 # set of dedicated workers.
 include(joinpath(Sys.BINDIR, "..", "share", "julia", "test", "testenv.jl"))
-disttestfile = joinpath(@__DIR__, "distributed_exec.jl")
 
-cmd = `$test_exename $test_exeflags $disttestfile`
+disttestfile = joinpath(@__DIR__, "distributed_exec.jl")
+managerstestfile = joinpath(@__DIR__, "managers.jl")
+
+cmd = `$test_exename $test_exeflags $disttestfile $managerstestfile`
 
 if !success(pipeline(cmd; stdout=stdout, stderr=stderr)) && ccall(:jl_running_on_valgrind,Cint,()) == 0
     error("Distributed test failed, cmd : $cmd")
