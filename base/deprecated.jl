a77d03af3a7c8090edada5863e6ccaaee47d9003
Shan Sikdar <ssikdar1@bu.edu>
2020-02-04 18:52:29 -0500

    add warning message to isimmutable documentation

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -194,7 +194,8 @@ end
 # BEGIN 1.5 deprecations
 """
     isimmutable(v) -> Bool
-
+!!! warning
+    Consider using `!ismutable(v)` instead, as `isimmutable(v)` will be replaced by `!ismutable(v)` in a future release.
 Return `true` iff value `v` is immutable.  See [Mutable Composite Types](@ref)
 for a discussion of immutability. Note that this function works on values, so if you give it
 a type, it will tell you that a value of `DataType` is mutable.
