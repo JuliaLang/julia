a252f5e64f9a9f07551238567b7d76eafc23d634
Shan Sikdar <ssikdar1@bu.edu>
2020-02-05 10:00:57 -0500

    add Julia version to deprecation warning in docstring

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -195,7 +195,7 @@ end
 """
     isimmutable(v) -> Bool
 !!! warning
-    Consider using `!ismutable(v)` instead, as `isimmutable(v)` will be replaced by `!ismutable(v)` in a future release.
+    Consider using `!ismutable(v)` instead, as `isimmutable(v)` will be replaced by `!ismutable(v)` in a future release. (Since Julia 1.5)
 Return `true` iff value `v` is immutable.  See [Mutable Composite Types](@ref)
 for a discussion of immutability. Note that this function works on values, so if you give it
 a type, it will tell you that a value of `DataType` is mutable.
