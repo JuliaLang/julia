ff82469491081498a093417b4a3c51bc0602812f
Oscar Smith <oscardssmith@gmail.com>
2020-02-08 13:53:43 -0600

    shorter, sweeter error message
    also gives direct advice in the case of splatting.

--- src/julia-parser.scm
+++ src/julia-parser.scm
@@ -234,7 +234,7 @@
 
 (define (read-operator port c)
   (if (and (eqv? c #\*) (eqv? (peek-char port) #\*))
-      (error "If you were trying to raise a number to a power, you should use \"^\" instead of \"**\".\n If you used \"**\" in some other context, you did something wrong. This is invalid syntax."))
+      (error "For exponentiation, use \"^\" instead of \"**\".\n For splatting, use \"x...\" instead of \"**x\"."))
   (if (or (eof-object? (peek-char port)) (not (op-or-sufchar? (peek-char port))))
       (symbol (string c)) ; 1-char operator
       (let ((str (let loop ((str (string c))
