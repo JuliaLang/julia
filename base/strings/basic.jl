487e660babd4ec4116aecd2551374c504e6f6e44
Logan Kilpatrick <lok026@g.harvard.edu>
2020-02-17 12:43:55 -0500

    Add an example to the codeunits docstring (#34607)
    Added usage example for `codeunits()`

--- base/strings/basic.jl
+++ base/strings/basic.jl
@@ -715,5 +715,17 @@ unsafe_convert(::Type{Ptr{Int8}}, s::CodeUnits{UInt8}) = unsafe_convert(Ptr{Int8
 Obtain a vector-like object containing the code units of a string.
 Returns a `CodeUnits` wrapper by default, but `codeunits` may optionally be defined
 for new string types if necessary.
+
+# Examples
+```jldoctest
+julia> codeunits("Juλia")
+6-element Base.CodeUnits{UInt8,String}:
+ 0x4a
+ 0x75
+ 0xce
+ 0xbb
+ 0x69
+ 0x61
+```
 """
 codeunits(s::AbstractString) = CodeUnits(s)
