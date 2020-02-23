8d95971a2a05d734a544a0d87e44e2e3e9da2545
Brandon Taylor <brandon.taylor221@gmail.com>
2020-02-23 12:24:43 -0500

    rerun CI

--- test/osutils.jl
+++ test/osutils.jl
@@ -44,7 +44,7 @@ end
     @test (@static if false 1 elseif false 2 else 3 end) === 3
     @test (@static if false 1 elseif false 2 elseif true && false 3 else 4 end) === 4
     @test (@static if false 1 elseif false 2 elseif true && false 3 end) === nothing
-    @test_throws ArgumentError("invalid @static macro") var"@static"(LineNumberNode(@__LINE__), @__MODULE__, 1)
+    @test_throws ArgumentError("invalid @static macro") var"@static"(LineNumberNode(@__LINE__), @__MODULE__, 0)
 end
 
 if Sys.iswindows()
