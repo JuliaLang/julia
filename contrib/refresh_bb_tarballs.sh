544d25fed5302808eccce47f8fa31869d1a50add
Viral B. Shah <viral@mayin.org>
2020-03-02 09:46:33 -0500

    arm -> armv7l in refresh-bb-tarballs.sh (#34935)
    Bump SuiteSparse from BB
    Add the armv7l binaries for openblas and remove checksums for older version

--- contrib/refresh_bb_tarballs.sh
+++ contrib/refresh_bb_tarballs.sh
@@ -9,7 +9,7 @@
 # Get this list via:
 #    using BinaryBuilder
 #    print("TRIPLETS=\"$(join(triplet.(BinaryBuilder.supported_platforms()), " "))\"")
-TRIPLETS="i686-linux-gnu x86_64-linux-gnu aarch64-linux-gnu arm-linux-gnueabihf powerpc64le-linux-gnu i686-linux-musl x86_64-linux-musl aarch64-linux-musl arm-linux-musleabihf x86_64-apple-darwin14 x86_64-unknown-freebsd11.1 i686-w64-mingw32 x86_64-w64-mingw32"
+TRIPLETS="i686-linux-gnu x86_64-linux-gnu aarch64-linux-gnu armv7l-linux-gnueabihf powerpc64le-linux-gnu i686-linux-musl x86_64-linux-musl aarch64-linux-musl armv7l-linux-musleabihf x86_64-apple-darwin14 x86_64-unknown-freebsd11.1 i686-w64-mingw32 x86_64-w64-mingw32"
 
 # These are the projects currently using BinaryBuilder; both GCC-expanded and non-GCC-expanded:
 BB_PROJECTS="gmp mbedtls libssh2 mpfr curl libgit2 pcre libuv unwind osxunwind dsfmt objconv p7zip zlib suitesparse openlibm"
