2f8cdd045cc6b7e77db14393e39cfee8ef4d4a66
Carsten Bauer <crstnbr@gmail.com>
2020-03-02 21:40:40 +0100

    Fix Base.show for SSHManager and LocalManager (#34955)

--- stdlib/Distributed/test/managers.jl
+++ stdlib/Distributed/test/managers.jl
@@ -1,7 +1,7 @@
 using Test
 using Distributed
 using Sockets
-using Distributed: parse_machine, bind_client_port
+using Distributed: parse_machine, bind_client_port, SSHManager, LocalManager
 
 @test parse_machine("127.0.0.1") == ("127.0.0.1", nothing)
 @test parse_machine("127.0.0.1:80") == ("127.0.0.1", 80)
@@ -22,3 +22,7 @@ addr, port = getsockname(sock)
 sock = bind_client_port(TCPSocket(), typeof(IPv6(0)))
 addr, port = getsockname(sock)
 @test addr == ip"::"
+
+@test occursin(r"^SSHManager\(machines=.*\)$",
+               sprint((t,x) -> show(t, "text/plain", x), SSHManager("127.0.0.1")))
+@test sprint((t,x) -> show(t, "text/plain", x), LocalManager(1, true)) == "LocalManager()"
