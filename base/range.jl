0a529c341de95c6749d1fa39d65d5dcaafe43dba
Mason Protter <mason.protter@icloud.com>
2020-03-06 13:45:17 -0700

    add comparison to range in LinRange docstring

--- base/range.jl
+++ base/range.jl
@@ -369,6 +369,26 @@ julia> LinRange(1.5, 5.5, 9)
 9-element LinRange{Float64}:
  1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5
 ```
+
+Compared to [`range`](@ref), `LinRange` should have less overhead but deal with floating point errors
+less cleanly:
+```julia
+julia> collect(-0.1:0.1:0.3)
+5-element Array{Float64,1}:
+ -0.1
+  0.0
+  0.1
+  0.2
+  0.3
+
+julia> collect(LinRange(-0.1, 0.3, 5))
+5-element Array{Float64,1}:
+ -0.1                   
+ -1.3877787807814457e-17
+  0.09999999999999999   
+  0.19999999999999998   
+  0.3    
+```
 """
 struct LinRange{T} <: AbstractRange{T}
     start::T
