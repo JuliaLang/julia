2d6fd60e1768e7d5ce58de1a8d1604c6cace1c5a
Carsten Bauer <crstnbr@gmail.com>
2020-03-11 20:34:52 +0100

    Curried versions of endswith and startswith (#35052)

--- base/strings/util.jl
+++ base/strings/util.jl
@@ -71,6 +71,36 @@ function endswith(a::Union{String, SubString{String}},
 end
 
 """
+    endswith(suffix)
+
+Create a function that checks whether its argument ends with `suffix`, i.e.
+a function equivalent to `y -> endswith(y, suffix)`.
+
+The returned function is of type `Base.Fix2{typeof(endswith)}`, which can be
+used to implement specialized methods.
+
+!!! compat "Julia 1.5"
+    The single argument `endswith(suffix)` requires at least Julia 1.5.
+
+"""
+endswith(s) = Base.Fix2(endswith, s)
+
+"""
+    startswith(prefix)
+
+Create a function that checks whether its argument starts with `prefix`, i.e.
+a function equivalent to `y -> startswith(y, prefix)`.
+
+The returned function is of type `Base.Fix2{typeof(startswith)}`, which can be
+used to implement specialized methods.
+
+!!! compat "Julia 1.5"
+    The single argument `startswith(prefix)` requires at least Julia 1.5.
+
+"""
+startswith(s) = Base.Fix2(startswith, s)
+
+"""
     chop(s::AbstractString; head::Integer = 0, tail::Integer = 1)
 
 Remove the first `head` and the last `tail` characters from `s`.
