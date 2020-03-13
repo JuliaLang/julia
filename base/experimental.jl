558eec94c0c28f0c7b7c513a293e7f1d4fe5f6d4
Keno Fischer <keno@juliacomputing.com>
2020-03-12 22:19:31 -0400

    A version of the sync macro that throws earlier (#34198)
    I've been looking at what causes deadlocks in our test suite in an effort to cut down on the number of failed tests on CI that result in hangs (since those are hard to diagnose and resolve). I found that by playing with various resource limits, it is easy to create hangs in the test suite. The reason we get a hang rather than a more easily diagnosable error is two fold. We either:
    
    1. Aren't watching for the error (e.g. a socket remote end closing)
    2. We aren't propagating the error to the top level
    
    A very common situation for case 2) is that the test is wrapped in @sync which doesn't return until all tasks have finished or error'ed. However, in many cases one of the tasks produces data for the others, so if that task errors, the remaining tasks will wait forever. This PR aims to address that situation by introducing a new `Experimental.@sync` macro that immediately rethrows any errors thrown by a contained task rather than waiting for all of them to finish. The implementation isn't super performant (it allocates a new task per object being waited on), but should be sufficient for use in the test suite. A better implementation would create a new scheduler object that can be inserted into multiple wait queues.
    
    Example usage of the new macro:
    
    ```
    @sync begin
        @async error("Hello")
        @async sleep(1000)
    end # Waits 1000s
    
    Experimental.@sync begin
        @async error("Hello")
        @async sleep(1000)
    end # Throws immediately
    ```
    
    The macro doesn't do any sort of cleanup for the tasks that do not finish, and just lets them run. In the future, we may want to automatically cancel those tasks, but that seemed like a bigger design problem than the simple thing that I wanted (something that propagates error messages more readily, so we see them in the logs).

--- base/experimental.jl
+++ base/experimental.jl
@@ -9,6 +9,8 @@
 """
 module Experimental
 
+using Base: Threads, sync_varname
+
 """
     Const(A::Array)
 
@@ -48,5 +50,55 @@ macro aliasscope(body)
         $sym
     end
 end
+
+
+function sync_end(refs)
+    local c_ex
+    defined = false
+    t = current_task()
+    cond = Threads.Condition()
+    lock(cond)
+    nremaining = length(refs)
+    for r in refs
+        schedule(Task(()->begin
+            try
+                wait(r)
+                lock(cond)
+                nremaining -= 1
+                nremaining == 0 && notify(cond)
+                unlock(cond)
+            catch e
+                lock(cond)
+                notify(cond, e; error=true)
+                unlock(cond)
+            end
+        end))
+    end
+    wait(cond)
+    unlock(cond)
+end
+
+"""
+    Experimental.@sync
+
+Wait until all lexically-enclosed uses of `@async`, `@spawn`, `@spawnat` and `@distributed`
+are complete, or at least one of them has errored. The first exception is immediately
+rethrown. It is the responsibility of the user to cancel any still-running operations
+during error handling.
+
+!!! Note
+    This interface is experimental and subject to change or removal without notice.
+"""
+macro sync(block)
+    var = esc(sync_varname)
+    quote
+        let $var = Any[]
+            v = $(esc(block))
+            sync_end($var)
+            v
+        end
+    end
+end
+
 
 end
