e7bb3a896c51a9bacfce0d8bd0c068a8da2c99b5
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2020-03-13 09:51:34 +0100

    Update stdlib/LinearAlgebra/test/triangular.jl

--- stdlib/LinearAlgebra/test/triangular.jl
+++ stdlib/LinearAlgebra/test/triangular.jl
@@ -30,7 +30,7 @@ for elty1 in (Float32, Float64, BigFloat, ComplexF32, ComplexF64, Complex{BigFlo
         @test t1(A1) === A1
         @test t1{elty1}(A1) === A1
         # test the ctor works for AbstractMatrix
-        symm =Symmetric(rand(Int8, n,n))
+        symm = Symmetric(rand(Int8, n, n))
         t1s = t1{elty1}(symm)
         @test typeof(t1s) == t1{elty1, Symmetric{elty1, Matrix{elty1}}}
         t1t = t1{elty1}(t1(rand(Int8, n,n)))
