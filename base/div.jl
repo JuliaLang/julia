e4c2ed10f2fb45c2b058edc8cb988e2b46a9d400
Shriprajwal K <40351128+ShriprajwalK@users.noreply.github.com>
2020-03-13 08:31:57 +0530

    Update base/div.jl
    Co-Authored-By: Simon Byrne <simonbyrne@gmail.com>

--- base/div.jl
+++ base/div.jl
@@ -67,7 +67,7 @@ rem(x, y, r::RoundingMode)
 rem(x, y, ::RoundingMode{:ToZero}) = rem(x,y)
 rem(x, y, ::RoundingMode{:Down}) = mod(x,y)
 rem(x, y, ::RoundingMode{:Up}) = mod(x,-y)
-rem(x, y, r::RoundingMode{:Nearest}) = x - y*div(x, y, r)
+rem(x, y, r::RoundingMode{:Nearest}) = divrem(x, y, r)[2]
 
 """
     fld(x, y)
