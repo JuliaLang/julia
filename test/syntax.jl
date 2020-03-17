36241a90bab7256f83392dc302808f04954a1f3b
Chris Foster <chris42f@gmail.com>
2020-03-17 14:28:13 +1000

    Add file and line number reporting for some lowering errors (#35105)
    * Add file and line number reporting for desugaring errors
    
    * Cleanup: use globals jl_filename jl_lineno less
      The various code paths that these globals can be set on are quite
      confusing and the globals can leak between unrelated evaluation
      contexts. Reduce this by using them less.

--- test/syntax.jl
+++ test/syntax.jl
@@ -1459,7 +1459,10 @@ end
 @test c27964(8) == (8, 2)
 
 # issue #26739
-@test_throws ErrorException("syntax: invalid syntax \"sin.[1]\"") Core.eval(@__MODULE__, :(sin.[1]))
+let exc = try Core.eval(@__MODULE__, :(sin.[1])) catch exc ; exc end
+    @test exc isa ErrorException
+    @test startswith(exc.msg, "syntax: invalid syntax \"sin.[1]\"")
+end
 
 # issue #26873
 f26873 = 0
@@ -2180,6 +2183,16 @@ end
         ex = Meta.parse("$imprt A: x, B: y", raise=false)
         @test ex == err
     end
+end
+
+# Syntax desugaring pass errors contain line numbers
+@test Meta.lower(@__MODULE__, Expr(:block, LineNumberNode(101, :some_file), :(f(x,x)=1))) ==
+    Expr(:error, "function argument names not unique around some_file:101")
+
+# Ensure file names don't leak between `eval`s
+eval(LineNumberNode(11, :incorrect_file))
+let exc = try eval(:(f(x,x)=1)) catch e ; e ; end
+    @test !occursin("incorrect_file", exc.msg)
 end
 
 # issue #34967
