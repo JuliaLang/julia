0a9f631c68b5ac317675d0a0dd1e1a3cd2528542
harryscholes <harryscholes@gmail.com>
2020-03-24 15:35:10 +0000

    Tidy up ImmutableDict docstring

--- base/dict.jl
+++ base/dict.jl
@@ -723,14 +723,14 @@ end
 """
     ImmutableDict
 
-ImmutableDict is a Dictionary implemented as an immutable linked list,
-which is optimal for small dictionaries that are constructed over many individual insertions
+`ImmutableDict` is a dictionary implemented as an immutable linked list,
+which is optimal for small dictionaries that are constructed over many individual insertions.
 Note that it is not possible to remove a value, although it can be partially overridden and hidden
-by inserting a new value with the same key
+by inserting a new value with the same key.
 
     ImmutableDict(KV::Pair)
 
-Create a new entry in the Immutable Dictionary for the key => value pair
+Create a new entry in the `ImmutableDict` for a `key => value` pair
 
  - use `(key => value) in dict` to see if this particular combination is in the properties set
  - use `get(dict, key, default)` to retrieve the most recent value for a particular key
