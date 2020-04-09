8a4693ed9ec9caa1a9249e013c864a6f9b9abfef
Ravishankar Joshi <21024229+ravibitsgoa@users.noreply.github.com>
2020-04-10 00:09:07 +0530

    Added tests for gcd and lcm for other integer datatypes.

--- base/intfuncs.jl
+++ base/intfuncs.jl
@@ -18,6 +18,21 @@ julia> gcd(6,9)
 
 julia> gcd(6,-9)
 3
+
+julia> gcd(6,0)
+6
+
+julia> gcd(0,0)
+0
+
+julia> gcd(1//3,2//3)
+1//3
+
+julia> gcd(1//3,-2//3)
+1//3
+
+julia> gcd(1//3,2)
+1//3
 ```
 """
 function gcd(a::T, b::T) where T<:Integer
@@ -32,8 +47,8 @@ end
 # binary GCD (aka Stein's) algorithm
 # about 1.7x (2.1x) faster for random Int64s (Int128s)
 function gcd(a::T, b::T) where T<:Union{Int8,UInt8,Int16,UInt16,Int32,UInt32,Int64,UInt64,Int128,UInt128}
-    a == 0 && return abs(b)
-    b == 0 && return abs(a)
+    a == 0 && return checked_abs(b)
+    b == 0 && return checked_abs(a)
     za = trailing_zeros(a)
     zb = trailing_zeros(b)
     k = min(za, zb)
@@ -69,12 +84,28 @@ julia> lcm(2,3)
 
 julia> lcm(-2,3)
 6
+
+julia> lcm(0,3)
+0
+
+julia> lcm(0,0)
+0
+
+julia> lcm(1//3,2//3)
+2//3
+
+julia> lcm(1//3,-2//3)
+2//3
+
+julia> lcm(1//3,2)
+2//1
 ```
 """
 function lcm(a::T, b::T) where T<:Integer
     # explicit a==0 test is to handle case of lcm(0,0) correctly
-    if a == 0
-        return a
+    # explicit b==0 test is to handle case of lcm(typemin(T),0) correctly
+    if a == 0 || b == 0
+        return zero(a)
     else
         return checked_abs(checked_mul(a, div(b, gcd(b,a))))
     end
