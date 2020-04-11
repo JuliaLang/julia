JuliaLang/julia

commit 0c284839fef6c8c153edc01fddfa37a9f5ac6752

Add compat entry for @deprecated (#35427)
* Add compat entry for at-deprecated
* Mention that Pkg.test still prints deprecation warnings

base/deprecated.jl
@@ -21,6 +21,11 @@ The first argument `old` is the signature of the deprecated method, the second o
 `new` is the call which replaces it. `@deprecate` exports `old` unless the optional
 third argument is `false`.
 
+!!! compat "Julia 1.5"
+    As of Julia 1.5, functions defined by `@deprecate` do not print warning inside normal
+    `julia` program as the defualt value of `--depwarn` option is `no`.  The warnings
+    are printed from tests run by `Pkg.test()`.
+
 # Examples
 ```jldoctest
 julia> @deprecate old(x) new(x)

commit e0ccf27eda9c63e32b3baeee53c5f9c90c7d5580

Fix docstring typo introduced in #35427

base/deprecated.jl
@@ -22,9 +22,9 @@ The first argument `old` is the signature of the deprecated method, the second o
 third argument is `false`.
 
 !!! compat "Julia 1.5"
-    As of Julia 1.5, functions defined by `@deprecate` do not print warning inside normal
-    `julia` program as the defualt value of `--depwarn` option is `no`.  The warnings
-    are printed from tests run by `Pkg.test()`.
+    As of Julia 1.5, functions defined by `@deprecate` do not print warning when `julia`
+    run without the `--depwarn=yes` flag set, as the default value of `--depwarn` option
+    is `no`.  The warnings are printed from tests run by `Pkg.test()`.
 
 # Examples
 ```jldoctest