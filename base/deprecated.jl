269911ebbc1bde205864532f099eebdd9cd0b4a5
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-04-11 14:11:02 +0100

    Update base/deprecated.jl

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -23,7 +23,7 @@ third argument is `false`.
 
 !!! compat "Julia 1.5"
     As of Julia 1.5, functions defined by `@deprecate` do not print warning when `julia`
-    run without the `--depwarn=yes` flag set, as the default value of `--depwarn` option
+    is run without the `--depwarn=yes` flag set, as the default value of `--depwarn` option
     is `no`.  The warnings are printed from tests run by `Pkg.test()`.
 
 # Examples
