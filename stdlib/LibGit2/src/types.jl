9ffa0abbfd91a1dd237274cfad1bb9bcf7595b6d
Keno Fischer <keno@juliacomputing.com>
2020-04-13 19:27:36 -0400

    Minor LibGit2 API improvement
    Use the _bypath function in GitTree getindex rather than opencoding
    it using the tree walker.

--- stdlib/LibGit2/src/types.jl
+++ stdlib/LibGit2/src/types.jl
@@ -1007,6 +1007,7 @@ for (typ, owntyp, sup, cname) in [
                 return obj
             end
         end
+        @eval Base.unsafe_convert(::Type{Ptr{Cvoid}}, x::$typ) = x.ptr
     else
         @eval mutable struct $typ <: $sup
             owner::$owntyp
@@ -1021,6 +1022,7 @@ for (typ, owntyp, sup, cname) in [
                 return obj
             end
         end
+        @eval Base.unsafe_convert(::Type{Ptr{Cvoid}}, x::$typ) = x.ptr
         if isa(owntyp, Expr) && owntyp.args[1] === :Union && owntyp.args[3] === :Nothing
             @eval begin
                 $typ(ptr::Ptr{Cvoid}, fin::Bool=true) = $typ(nothing, ptr, fin)
