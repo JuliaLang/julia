5708f798581f5649bf0493924572b992a273f900
Keno Fischer <keno@juliacomputing.com>
2020-04-21 02:01:37 -0400

    threads: Pass env through to thread_exec
    We want to start running rr on CI, but at the moment the threads
    test is clearing ENV, causing it to lose the LD_PRELOAD that improves
    rr performance. Fix that by instead passing through the environment
    and setting the relevant JULIA_NUM_THREADS flag on top of it.

--- test/threads.jl
+++ test/threads.jl
@@ -1,8 +1,12 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
+using Test
+
 let cmd = `$(Base.julia_cmd()) --depwarn=error --startup-file=no threads_exec.jl`
     for test_nthreads in (1, 2, 4, 4) # run once to try single-threaded mode, then try a couple times to trigger bad races
-        run(pipeline(setenv(cmd, "JULIA_NUM_THREADS" => test_nthreads), stdout = stdout, stderr = stderr))
+        new_env = copy(ENV)
+        new_env["JULIA_NUM_THREADS"] = string(test_nthreads)
+        run(pipeline(setenv(cmd, new_env), stdout = stdout, stderr = stderr))
     end
 end
 
