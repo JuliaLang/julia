bb51ff68ea5d352c882445be2bb88d4f54493100
MasonProtter <mason.protter@icloud.com>
2020-04-23 00:23:42 -0600

    fix typo

--- stdlib/LinearAlgebra/test/diagonal.jl
+++ stdlib/LinearAlgebra/test/diagonal.jl
@@ -714,7 +714,7 @@ end
 @testset "Empty (#35424)" begin
     @test zeros(0)'*Diagonal(zeros(0))*zeros(0) === 0.0
     @test transpose(zeros(0))*Diagonal(zeros(Complex{Int}, 0))*zeros(0) === 0.0 + 0.0im
-    @test dot(zeros(Int32, 0), Digonal(zeros(Int, 0)), zeros(Int16, 0)) === 0
+    @test dot(zeros(Int32, 0), Diagonal(zeros(Int, 0)), zeros(Int16, 0)) === 0
 end
 
 end # module TestDiagonal
