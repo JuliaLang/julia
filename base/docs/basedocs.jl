4f1fd2d681ee4cf3a2e49c3e91d88c2e0897c9f7
Takafumi Arakaki <aka.tkf@gmail.com>
2020-05-01 05:29:35 -0700

    Add Base.exit_on_sigint to wrap jl_exit_on_sigint (#29411)

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -1445,6 +1445,18 @@ TypeError
     InterruptException()
 
 The process was stopped by a terminal interrupt (CTRL+C).
+
+Note that, in Julia script started without `-i` (interactive) option,
+`InterruptException` is not thrown by default.  Calling
+[`Base.exit_on_sigint(false)`](@ref Base.exit_on_sigint) in the script
+can recover the behavior of the REPL.  Alternatively, a Julia script
+can be started with
+
+```sh
+julia -e "include(popfirst!(ARGS))" script.jl
+```
+
+to let `InterruptException` be thrown by CTRL+C during the execution.
 """
 InterruptException
 
