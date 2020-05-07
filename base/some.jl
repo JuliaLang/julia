fdcdb35461291b73a6b0d2a41d8f5c98a8155478
MasonProtter <mason.protter@icloud.com>
2020-05-07 16:26:09 -0600

    it's -> its

--- base/some.jl
+++ base/some.jl
@@ -6,7 +6,7 @@
 A wrapper type used in `Union{Some{T}, Nothing}` to distinguish between the absence
 of a value ([`nothing`](@ref)) and the presence of a `nothing` value (i.e. `Some(nothing)`).
 
-`Some` is also used in broadcasting to treat it's enclosed value as a scalar.
+`Some` is also used in broadcasting to treat its enclosed value as a scalar.
 
 Use [`something`](@ref) to access the value wrapped by a `Some` object.
 """
