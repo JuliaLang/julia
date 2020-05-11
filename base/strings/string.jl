9b72e7fd1c0d3896d42607f8d9ec9d2a2d686289
Kristoffer <kcarlsson89@gmail.com>
2020-05-11 14:03:37 +0200

    miro-optimize iterate on string

--- base/strings/string.jl
+++ base/strings/string.jl
@@ -176,9 +176,9 @@ is_valid_continuation(c) = c & 0xc0 == 0x80
 
 ## required core functionality ##
 
-@propagate_inbounds function iterate(s::String, i::Int=firstindex(s))
-    i > ncodeunits(s) && return nothing
-    b = codeunit(s, i)
+@inline function iterate(s::String, i::Int=firstindex(s))
+    (i % UInt) - 1 < ncodeunits(s) || return nothing
+    b = @inbounds codeunit(s, i)
     u = UInt32(b) << 24
     between(b, 0x80, 0xf7) || return reinterpret(Char, u), i+1
     return iterate_continued(s, i, u)
