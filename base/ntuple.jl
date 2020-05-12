2bc98b8d64c5558ec933d2529965d7e42ff9e93d
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2020-05-11 22:45:11 -0400

    Update ntuple docstring to be more consistent with method signature

--- base/ntuple.jl
+++ base/ntuple.jl
@@ -49,8 +49,8 @@ Create a tuple of length `N`, computing each element as `f(i)`,
 where `i` is the index of the element. By taking a `Val(N)` 
 argument, it is possible that this version of ntuple may
 generate more efficient code than the version taking the 
-length as an integer. But `ntuple(fun, N)` is preferable
-to `ntuple(fun, Val(N))` in cases where `N` cannot be
+length as an integer. But `ntuple(f, N)` is preferable
+to `ntuple(f, Val(N))` in cases where `N` cannot be
 determined at compile time.
 
 # Examples
