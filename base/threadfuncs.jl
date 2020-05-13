ba972dde8cbbf69e911ffd27733681368cdadad5
Jarrett Revels <jarrettrevels@gmail.com>
2020-05-13 12:02:14 -0400

    add schedule argument to docs

--- base/threadfuncs.jl
+++ base/threadfuncs.jl
@@ -1,18 +1,35 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 """
-    Threads.foreach(f, channel::Channel; ntasks=Threads.nthreads())
+    Threads.foreach(f, channel::Channel; ntasks=Threads.nthreads(),
+                    schedule=:fair)
 
 Similar to `foreach(f, channel)`, but iteration over `channel` and calls to
-`f` are split across `ntasks` Tasks spawned by `Threads.@spawn`. This function
+`f` are split across `ntasks` tasks spawned by `Threads.@spawn`. This function
 will wait for all internally spawned tasks to complete before returning.
+
+If `schedule` is `:fair`, `Threads.foreach` will spawn a new task for each work
+item, allowing Julia's scheduler to more freely load-balance work items across
+threads. Technically, in `:fair` mode, `Threads.foreach` may execute up to
+`2 * ntasks` tasks concurrently.
+
+If `schedule` is `:static`, `Threads.foreach` will simply spawn `ntasks` tasks
+such that each task is locked to the thread it was spawned on. This approach has
+lower scheduling overhead and thus may be more suitable for more uniform,
+fine-grained workloads.
 
 !!! compat "Julia 1.6"
     This function requires Julia 1.6 or later.
 """
 function Threads.foreach(f, channel::Channel; ntasks=Threads.nthreads(),
                          schedule=:fair)
-    apply = schedule === :static ? (f, x) -> f(x) : _waitspawn
+    if schedule === :fair
+        apply = _waitspawn
+    elseif schedule === :static
+        apply = (f, x) -> f(x)
+    else
+        throw(ArgumentError("`schedule` must be either `:fair` or `:static`"))
+    end
     stop = Threads.Atomic{Bool}(false)
     @sync for _ in 1:ntasks
         Threads.@spawn try
@@ -33,5 +50,5 @@ function Threads.foreach(f, channel::Channel; ntasks=Threads.nthreads(),
     return nothing
 end
 
-# Avoid registering the task in outer @sync:
+# avoid registering the task within `Threads.foreach`'s outer `@sync`
 _waitspawn(f, x) = wait(Threads.@spawn f(x))
