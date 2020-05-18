47de7a6602037ce6ad6eb9a3cbe4ef49f102f7d4
Sheehan Olver <solver@mac.com>
2020-05-18 19:10:17 +0100

    Remove unnecessary restriction to `StridedVecOrMat`
    The "Strided array interface" https://docs.julialang.org/en/v1/manual/interfaces/#man-interface-strided-arrays-1 means that this is useful beyond these types

--- stdlib/LinearAlgebra/src/adjtrans.jl
+++ stdlib/LinearAlgebra/src/adjtrans.jl
@@ -205,8 +205,8 @@ Base.strides(A::Transpose{<:Any, <:StridedVector}) = (stride(A.parent, 2), strid
 Base.strides(A::Adjoint{<:Real, <:StridedMatrix}) = reverse(strides(A.parent))
 Base.strides(A::Transpose{<:Any, <:StridedMatrix}) = reverse(strides(A.parent))
 
-Base.unsafe_convert(::Type{Ptr{T}}, A::Adjoint{<:Real, <:StridedVecOrMat}) where {T} = Base.unsafe_convert(Ptr{T}, A.parent)
-Base.unsafe_convert(::Type{Ptr{T}}, A::Transpose{<:Any, <:StridedVecOrMat}) where {T} = Base.unsafe_convert(Ptr{T}, A.parent)
+Base.unsafe_convert(::Type{Ptr{T}}, A::Adjoint{<:Real, <:AbstractVecOrMat}) where {T} = Base.unsafe_convert(Ptr{T}, A.parent)
+Base.unsafe_convert(::Type{Ptr{T}}, A::Transpose{<:Any, <:AbstractVecOrMat}) where {T} = Base.unsafe_convert(Ptr{T}, A.parent)
 
 # for vectors, the semantics of the wrapped and unwrapped types differ
 # so attempt to maintain both the parent and wrapper type insofar as possible
