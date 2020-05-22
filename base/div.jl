968fc685b0e6e46e91a84139999a791fdd8c64d6
Stefan Karpinski <stefan@karpinski.org>
2020-05-22 15:42:52 -0400

    Update base/div.jl

--- base/div.jl
+++ base/div.jl
@@ -89,7 +89,7 @@ julia> 6.0/0.1
 julia> 6.0/big(0.1)
 59.99999999999999666933092612453056361837965690217069245739573412231113406246995
 ```
-
+What is happening here is that the true value of the floating-point number written as `0.1` is slightly larger than the numerical value 1/10 while `6.0` represents the number 6 precisely. Therefore the true value of `6.0 / 0.1` is slightly less than 60. When doing division, this is rounded to precisely `60.0`, but `fld(6.0, 0.1)` always takes the floor or the true value, so the result is `59.0`.
 """
 fld(a, b) = div(a, b, RoundDown)
 
