5a3a6afa1a9cf63f80c94ff1c7cf587fc02b4bbc
Shan Sikdar <ssikdar1@bu.edu>
2020-05-31 10:37:48 -0400

    add documentation for findnz w/ SparseVector

--- stdlib/SparseArrays/src/sparsevector.jl
+++ stdlib/SparseArrays/src/sparsevector.jl
@@ -732,6 +732,25 @@ end
 findall(p::Base.Fix2{typeof(in)}, x::SparseVector{<:Any,Ti}) where {Ti} =
     invoke(findall, Tuple{Base.Fix2{typeof(in)}, AbstractArray}, p, x)
 
+"""
+    findnz(x)
+
+Return a tuple `(I, V)`  where `I` is the indices of the stored ("structurally non-zero")
+values in sparse vector `x` and `V` is a vector of the values.
+
+# Examples
+```jldoctest
+julia> x = sparsevec([1 2 0; 0 0 3; 0 4 0])
+9-element SparseVector{Int64,Int64} with 4 stored entries:
+  [1]  =  1
+  [4]  =  2
+  [6]  =  4
+  [8]  =  3
+
+julia> findnz(x)
+([1, 4, 6, 8], [1, 2, 4, 3])
+```
+"""
 function findnz(x::SparseVector{Tv,Ti}) where {Tv,Ti}
     numnz = nnz(x)
 
