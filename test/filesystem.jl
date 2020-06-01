697a6df5a1a633117b45807f32fc2d3fa2844298
jaakkor2 <jaakkor2@users.noreply.github.com>
2020-06-01 09:43:06 +0200

    Test copying a large file
    Test issues
    * https://github.com/JuliaLang/julia/issues/14574 (fixed)
    * https://github.com/JuliaLang/julia/issues/30723 (copy failure on Windows when filesize is >2 GB).
    
    https://github.com/JuliaLang/julia/pull/30989 warns that CI might not like big files, let's see.

--- test/filesystem.jl
+++ test/filesystem.jl
@@ -32,4 +32,14 @@ mktempdir() do dir
   @test res == UInt8[text..., (i > 20 for i in (length(text) + 1):length(res))...]
   close(file)
 
+  # test copying a big file, issues #14574, #30723
+  filename_big = tempname(dir)
+  filename_big_copy = tempname(dir)
+  len = 2^31 + 1
+  data = Array{UInt8, 1}(undef, len)
+  write(filename_big, data)
+  @test Base.Filesystem.cp(filename_big, filename_big_copy) == filename_big_copy
+  Base.Filesystem.rm(filename_big)
+  Base.Filesystem.rm(filename_big_copy)
+    
 end
