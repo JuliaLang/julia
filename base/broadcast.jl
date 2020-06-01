fe18cd56c5030604fe4dcf69a20e4dd05831e0f1
Takafumi Arakaki <aka.tkf@gmail.com>
2020-06-01 13:46:43 -0700

    ReservedStyle should win to Unknown

--- base/broadcast.jl
+++ base/broadcast.jl
@@ -1300,7 +1300,7 @@ julia> aspairs.(tuple.(Dict(:a => 1), Dict(:b => 2)))
 struct ReservedStyle <: BroadcastStyle end
 
 BroadcastStyle(s::ReservedStyle, ::BroadcastStyle) = s
-BroadcastStyle(::ReservedStyle, s::Unknown) = s
+BroadcastStyle(s::ReservedStyle, ::Unknown) = s
 instantiate(bc::Broadcasted{ReservedStyle}) = bc
 
 _reserved_style_error() =
