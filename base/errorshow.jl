99959620f9a3276a7c8c929b9382b6109c4d7796
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-06-03 18:49:26 +0200

    remove unnecessary [1]

--- base/errorshow.jl
+++ base/errorshow.jl
@@ -620,7 +620,7 @@ function show_reduced_backtrace(io::IO, t::Vector)
 
         modul = getmodule(frame)
         if !haskey(modulecolordict, modul)
-            modulecolordict[modul] = popfirst!(modulecolorcycler)[1]
+            modulecolordict[modul] = popfirst!(modulecolorcycler)
         end
         modulecolor = modulecolordict[modul]
         
