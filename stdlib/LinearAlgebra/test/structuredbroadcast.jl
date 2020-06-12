e7a1b99c129c2e76a684754fb709593c71d7ad44
Benoît Legat <benoit.legat@gmail.com>
2020-06-12 09:52:13 +0200

    Replace LinearAlgebra._iszero by Base.iszero (#36194)

--- stdlib/LinearAlgebra/test/structuredbroadcast.jl
+++ stdlib/LinearAlgebra/test/structuredbroadcast.jl
@@ -205,4 +205,22 @@ end
     @test typeof(tmp) <: Tridiagonal
 
 end
+
+struct Zero end
+Base.iszero(::Zero) = true
+Base.zero(::Type{Zero}) = Zero()
+@testset "PR #36193" begin
+    z = Zero()
+    Z = [z z
+         z z]
+    zz = [z, z]
+    U = UpperTriangular(Z)
+    L = LowerTriangular(Z)
+    D = Diagonal(zz)
+    for a in [U, L, D]
+        @test identity.(a) isa typeof(a)
+        @test map(identity, a) isa typeof(a)
+    end
+end
+
 end
