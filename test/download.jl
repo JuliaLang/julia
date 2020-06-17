940c504bbdf6231c2c603dd157dbd9065a678766
Elliot Saba <staticfloat@gmail.com>
2020-06-17 15:10:33 -0700

    Switch httpbin tests over to JuliaLang-hosted `httpbin` mock server

--- test/download.jl
+++ test/download.jl
@@ -1,6 +1,6 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
-let urls = ["http://httpbin.org/ip", "https://httpbin.org/ip"]
+let urls = ["http://httpbin.julialang.org/ip", "https://httpbin.julialang.org/ip"]
     for url in urls
         @test Base.download_url(url) == url
     end
@@ -17,14 +17,14 @@ end
 mktempdir() do temp_dir
     # Download a file
     file = joinpath(temp_dir, "ip")
-    @test download("http://httpbin.org/ip", file) == file
+    @test download("http://httpbin.julialang.org/ip", file) == file
     @test isfile(file)
     @test !isempty(read(file))
     ip = read(file, String)
 
     # Test download rewrite hook
     push!(Base.DOWNLOAD_HOOKS, url->replace(url, r"/status/404$" => "/ip"))
-    @test download("http://httpbin.org/status/404", file) == file
+    @test download("http://httpbin.julialang.org/status/404", file) == file
     @test isfile(file)
     @test !isempty(read(file))
     @test ip == read(file, String)
@@ -32,22 +32,24 @@ mktempdir() do temp_dir
 
     # Download an empty file
     empty_file = joinpath(temp_dir, "empty")
-    @test download("http://httpbin.org/status/200", empty_file) == empty_file
+    @test download("http://httpbin.julialang.org/status/200", empty_file) == empty_file
 
     # Windows and older versions of curl do not create the empty file (https://github.com/curl/curl/issues/183)
     @test !isfile(empty_file) || isempty(read(empty_file))
 
     # Make sure that failed downloads do not leave files around
     missing_file = joinpath(temp_dir, "missing")
-    @test_throws ProcessFailedException download("http://httpbin.org/status/404", missing_file)
+    @test_throws ProcessFailedException download("http://httpbin.julialang.org/status/404", missing_file)
     @test !isfile(missing_file)
 
-    # Make sure we properly handle metachar '
-    metachar_file = joinpath(temp_dir, "metachar")
-    download("https://httpbin.org/get?test='^'", metachar_file)
-    metachar_string = read(metachar_file, String)
-    m = match(r"\"url\"\s*:\s*\"(.*)\"", metachar_string)
-    @test m.captures[1] == "https://httpbin.org/get?test='^'"
+    # Make sure we properly handle metachar ' on windows with ^ escaping
+    if Sys.iswindows()
+        metachar_file = joinpath(temp_dir, "metachar")
+        download_powershell("https://httpbin.julialang.org/get?test='^'", metachar_file)
+        metachar_string = read(metachar_file, String)
+        m = match(r"\"test\"\s*:\s*\"(.*)\"", metachar_string)
+        @test m.captures[1] == "'^'"
+    end
 
     # Use a TEST-NET (192.0.2.0/24) address which shouldn't be bound
     invalid_host_file = joinpath(temp_dir, "invalid_host")
