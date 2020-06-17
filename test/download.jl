9b689c4b34dcca66f0b771e316f02f0144435df7
Elliot Saba <staticfloat@gmail.com>
2020-06-17 15:17:49 -0700

    Use `https` instead of `http` for `httpbin` tests

--- test/download.jl
+++ test/download.jl
@@ -1,5 +1,6 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
+# Test that `Base.download_url()` is altered by `Base.DOWNLOAD_HOOKS`.
 let urls = ["http://httpbin.julialang.org/ip", "https://httpbin.julialang.org/ip"]
     for url in urls
         @test Base.download_url(url) == url
@@ -17,14 +18,14 @@ end
 mktempdir() do temp_dir
     # Download a file
     file = joinpath(temp_dir, "ip")
-    @test download("http://httpbin.julialang.org/ip", file) == file
+    @test download("https://httpbin.julialang.org/ip", file) == file
     @test isfile(file)
     @test !isempty(read(file))
     ip = read(file, String)
 
     # Test download rewrite hook
     push!(Base.DOWNLOAD_HOOKS, url->replace(url, r"/status/404$" => "/ip"))
-    @test download("http://httpbin.julialang.org/status/404", file) == file
+    @test download("https://httpbin.julialang.org/status/404", file) == file
     @test isfile(file)
     @test !isempty(read(file))
     @test ip == read(file, String)
@@ -32,20 +33,20 @@ mktempdir() do temp_dir
 
     # Download an empty file
     empty_file = joinpath(temp_dir, "empty")
-    @test download("http://httpbin.julialang.org/status/200", empty_file) == empty_file
+    @test download("https://httpbin.julialang.org/status/200", empty_file) == empty_file
 
     # Windows and older versions of curl do not create the empty file (https://github.com/curl/curl/issues/183)
     @test !isfile(empty_file) || isempty(read(empty_file))
 
     # Make sure that failed downloads do not leave files around
     missing_file = joinpath(temp_dir, "missing")
-    @test_throws ProcessFailedException download("http://httpbin.julialang.org/status/404", missing_file)
+    @test_throws ProcessFailedException download("https://httpbin.julialang.org/status/404", missing_file)
     @test !isfile(missing_file)
 
     # Make sure we properly handle metachar ' on windows with ^ escaping
     if Sys.iswindows()
         metachar_file = joinpath(temp_dir, "metachar")
-        download_powershell("https://httpbin.julialang.org/get?test='^'", metachar_file)
+        Base.download_powershell("https://httpbin.julialang.org/get?test='^'", metachar_file)
         metachar_string = read(metachar_file, String)
         m = match(r"\"test\"\s*:\s*\"(.*)\"", metachar_string)
         @test m.captures[1] == "'^'"
