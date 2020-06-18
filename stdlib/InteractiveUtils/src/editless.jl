638510733757efd066b578582997af7cbf3b4fa9
Adam B <adam_gpg@thebeckmeyers.xyz>
2020-06-18 15:41:27 -0400

    Consider terminal emacs in EDITOR_CALLBACKS before graphical (#36346)
    Since the regex r"\bemacs" matches the cmd `emacs -nw`, the regex
    r"\bemacs\b.*\s(-nw|--no-window-system)\b" should be considered in the
    EDITOR_CALLBACKS list before it.
    
    Without this change, when JULIA_EDITOR="emacs -nw", julia tries to
    open the editor with run(...; wait=false) which causes the error:
    
    emacs: standard input is not a tty
    
    or for emacsclient:
    
    emacsclient: could not get terminal name
    emacsclient: error executing alternate editor ""

--- stdlib/InteractiveUtils/src/editless.jl
+++ stdlib/InteractiveUtils/src/editless.jl
@@ -58,6 +58,7 @@ Note that many editors are already defined. All of the following commands should
 already work:
 
 - emacs
+- emacsclient
 - vim
 - nvim
 - nano
@@ -113,13 +114,14 @@ function define_default_editors()
     define_editor(r".*") do cmd, path, line
         `$cmd $path`
     end
+    define_editor([r"\bemacs", "gedit", r"\bgvim"]) do cmd, path, line
+        `$cmd +$line $path`
+    end
+    # Must check that emacs not running in -t/-nw before regex match for general emacs
     define_editor([
         "vim", "vi", "nvim", "mvim", "nano",
         r"\bemacs\b.*\s(-nw|--no-window-system)\b",
         r"\bemacsclient\b.\s*-(-?nw|t|-?tty)\b"], wait=true) do cmd, path, line
-        `$cmd +$line $path`
-    end
-    define_editor([r"\bemacs", "gedit", r"\bgvim"]) do cmd, path, line
         `$cmd +$line $path`
     end
     define_editor(["textmate", "mate", "kate"]) do cmd, path, line
