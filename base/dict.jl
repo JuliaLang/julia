fe4be9a3b30335d4624fae27323aa3c133888579
harryscholes <harryscholes@gmail.com>
2020-06-19 08:48:21 +0100

    Use haslength to check for HasShape and HasLength traits

--- base/dict.jl
+++ base/dict.jl
@@ -99,10 +99,7 @@ mutable struct Dict{K,V} <: AbstractDict{K,V}
 end
 function Dict{K,V}(kv) where V where K
     h = Dict{K,V}()
-    chklen = IteratorSize(kv)
-    if (chklen isa HasLength) || (chklen isa HasShape)
-        sizehint!(h, length(kv))
-    end
+    haslength(kv) && sizehint!(h, length(kv))
     for (k,v) in kv
         h[k] = v
     end
