040916efea157fe4b7c398b942596f1c9e05e3c4
binarygcd <63384853+binarygcd@users.noreply.github.com>
2020-06-20 13:03:48 +0800

    Update basic.jl

--- base/strings/basic.jl
+++ base/strings/basic.jl
@@ -72,7 +72,7 @@ ncodeunits(s::AbstractString)
 
 Return the code unit type of the given string object. For ASCII, Latin-1, or
 UTF-8 encoded strings, this would be `UInt8`; for UCS-2 and UTF-16 it would be
-`UInt16`; for UTF-32 it would be `UInt32`. The unit code type need not be
+`UInt16`; for UTF-32 it would be `UInt32`. The code unit type need not be
 limited to these three types, but it's hard to think of widely used string
 encodings that don't use one of these units. `codeunit(s)` is the same as
 `typeof(codeunit(s,1))` when `s` is a non-empty string.
