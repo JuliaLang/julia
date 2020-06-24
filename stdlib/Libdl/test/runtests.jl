befc4c7adfe2b00602af7863aed28262ac0a37a9
Elliot Saba <staticfloat@gmail.com>
2020-06-24 20:17:41 +0000

    [Libdl]: Add `@executable_path` expansion test
    This exercises the replacement of `DL_LOAD_PATH` entries that start
    with `@executable_path`.

--- stdlib/Libdl/test/runtests.jl
+++ stdlib/Libdl/test/runtests.jl
@@ -222,4 +222,27 @@ let dl = C_NULL
     @test_skip !Libdl.dlclose(dl)   # Syscall doesn't fail on Win32
 end
 
+# test @executable_path expansion
+mktempdir() do dir
+    # Create a `libdcalltest` in a directory that is not on our load path
+    src_path = joinpath(private_libdir, "libccalltest.$(Libdl.dlext)")
+    dst_path = joinpath(dir, "libdcalltest.$(Libdl.dlext)")
+    cp(src_path, dst_path)
+
+    # Add this temporary directory to our load path, using `@executable_path` to do so.
+    push!(Base.DL_LOAD_PATH, joinpath("@executable_path", relpath(dir, Sys.BINDIR)))
+
+    # Test that we can now open that file
+    Libdl.dlopen("libdcalltest") do dl
+        fptr = Libdl.dlsym(dl, :set_verbose)
+        @test fptr !== nothing
+        @test_throws ErrorException Libdl.dlsym(dl, :foo)
+
+        fptr = Libdl.dlsym_e(dl, :set_verbose)
+        @test fptr != C_NULL
+        fptr = Libdl.dlsym_e(dl, :foo)
+        @test fptr == C_NULL
+    end
+end
+
 end
