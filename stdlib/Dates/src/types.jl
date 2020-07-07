7927e153191541ddc93092a027e9010675490dd4
Jacob Quinn <quinn.jacobd@gmail.com>
2020-07-07 17:57:09 -0600

    Update stdlib/Dates/src/types.jl

--- stdlib/Dates/src/types.jl
+++ stdlib/Dates/src/types.jl
@@ -90,7 +90,7 @@ struct ISOCalendar <: Calendar end
 
 Geographic zone generally based on longitude determining what the time is at a certain location.
 Some time zones observe daylight savings (eg EST -> EDT).
-For implementations and more support, see the `TimeZones` package
+For implementations and more support, see the [`TimeZones.jl`](https://github.com/JuliaTime/TimeZones.jl) package
 """
 abstract type TimeZone end
 
