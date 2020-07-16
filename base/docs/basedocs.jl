dc933d50922dc55813919a639e675ecb383b537e
Eric Forgy <Eric.Forgy@cavalre.com>
2020-07-16 07:44:42 -0700

    Improve docstring for `baremodule`. (#36630)
    Co-authored-by: Simeon Schaub <simeondavidschaub99@gmail.com>
    Co-authored-by: Michael Abbott <32575566+mcabbott@users.noreply.github.com>

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -126,8 +126,30 @@ kw"__init__"
 """
     baremodule
 
-`baremodule` declares a module that does not contain `using Base`
-or a definition of [`eval`](@ref Base.eval). It does still import `Core`.
+`baremodule` declares a module that does not contain `using Base` or local definitions of
+[`eval`](@ref Base.eval) and [`include`](@ref Base.include). It does still import `Core`. In other words,
+
+```julia
+module Mod
+
+...
+
+end
+```
+
+is equivalent to
+
+```julia
+baremodule Mod
+
+using Base
+
+eval(x) = Core.eval(Mod, x)
+include(p) = Base.include(Mod, p)
+
+...
+
+end
 """
 kw"baremodule"
 
