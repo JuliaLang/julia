27a09c87eb16d00a6fa7848677d5690d88705ff8
Mason Protter <mason.protter@icloud.com>
2020-07-16 17:06:03 -0600

    revert to ifelse for length(u) == 0

--- stdlib/LinearAlgebra/src/adjtrans.jl
+++ stdlib/LinearAlgebra/src/adjtrans.jl
@@ -268,12 +268,16 @@ Broadcast.broadcast_preserving_zero_d(f, tvs::Union{Number,TransposeAbsVec}...) 
 ## multiplication *
 
 function _dot_nonrecursive(u, v)
-    if length(u) != length(v)
+    lu = length(u)
+    if lu != length(v)
         throw(DimensionMismatch("first array has length $(lu) which does not match the length of the second, $(length(v))."))
     end
-    sum(uu*vv for (uu, vv) in zip(u, v); init=(zero(eltype(u)) * zero(eltype(v))))
+    if lu == 0
+        zero(eltype(u)) * zero(eltype(v))
+    else
+        sum(uu*vv for (uu, vv) in zip(u, v))
+    end
 end
-
 
 # Adjoint/Transpose-vector * vector
 *(u::AdjointAbsVec{T}, v::AbstractVector{T}) where {T<:Number} = dot(u.parent, v)
