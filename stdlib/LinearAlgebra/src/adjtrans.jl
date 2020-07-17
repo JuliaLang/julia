5851198703a3dac545ac01ebd8626ad63b7d7184
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2020-07-17 11:48:51 +0200

    merge the two dot versions

--- stdlib/LinearAlgebra/src/adjtrans.jl
+++ stdlib/LinearAlgebra/src/adjtrans.jl
@@ -280,8 +280,7 @@ function _dot_nonrecursive(u, v)
 end
 
 # Adjoint/Transpose-vector * vector
-*(u::AdjointAbsVec{<:Number}, v::AbstractVector{<:Number}) = dot(u.parent, v)
-*(u::TransposeAbsVec{T}, v::AbstractVector{T}) where {T<:Real} = dot(u.parent, v)
+*(u::AdjOrTransAbsVec{<:Number}, v::AbstractVector{<:Number}) = dot(u.parent, v)
 *(u::AdjOrTransAbsVec, v::AbstractVector) = _dot_nonrecursive(u, v)
 
 
