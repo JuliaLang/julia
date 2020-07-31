fb152de1370edd839a6780d6d9713ed6f8668e41
Fredrik Ekre <ekrefredrik@gmail.com>
2020-07-31 21:05:57 +0200

    Do not include commit-offset number in VERSION if we are building for a tag. (#36867)

--- base/version.jl
+++ base/version.jl
@@ -221,7 +221,7 @@ A `VersionNumber` object describing which version of Julia is in use. For detail
 """
 const VERSION = try
     ver = VersionNumber(VERSION_STRING)
-    if !isempty(ver.prerelease)
+    if !isempty(ver.prerelease) && !GIT_VERSION_INFO.tagged_commit
         if GIT_VERSION_INFO.build_number >= 0
             ver = VersionNumber(ver.major, ver.minor, ver.patch, (ver.prerelease..., GIT_VERSION_INFO.build_number), ver.build)
         else
