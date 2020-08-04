665c6c7c970c8659a49e1fe8db7ae55022f0b8d0
kimikage <kimikage.ceo@gmail.com>
2020-08-04 13:02:27 +0900

    Fix docstring for `Val(c)` in order that `c` accepts a `Symbol`

--- base/essentials.jl
+++ base/essentials.jl
@@ -683,8 +683,8 @@ const (:) = Colon()
 
 Return `Val{c}()`, which contains no run-time data. Types like this can be used to
 pass the information between functions through the value `c`, which must be an `isbits`
-value. The intent of this construct is to be able to dispatch on constants directly (at
-compile time) without having to test the value of the constant at run time.
+value or a `Symbol`. The intent of this construct is to be able to dispatch on constants
+directly (at compile time) without having to test the value of the constant at run time.
 
 # Examples
 ```jldoctest
