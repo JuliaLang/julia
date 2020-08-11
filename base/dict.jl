894ee043940402c1c78dc18ed1e1d50ecf9e0718
Cameron Bieganek <8310743+CameronBieganek@users.noreply.github.com>
2020-08-11 03:47:10 -0500

    doc: provide a better example for `get!(f::Function, collection, key)` (#36964)

--- base/dict.jl
+++ base/dict.jl
@@ -424,12 +424,25 @@ get!(collection, key, default)
 Return the value stored for the given key, or if no mapping for the key is present, store
 `key => f()`, and return `f()`.
 
-This is intended to be called using `do` block syntax:
-```julia
-get!(dict, key) do
-    # default value calculated here
-    time()
-end
+This is intended to be called using `do` block syntax.
+
+# Examples
+```jldoctest
+julia> squares = Dict{Int,Int}();
+
+julia> function get_square!(d, i)
+           get!(d, i) do
+               i^2
+           end
+       end
+get_square! (generic function with 1 method)
+
+julia> get_square!(squares, 2)
+4
+
+julia> squares
+Dict{Int64,Int64} with 1 entry:
+  2 => 4
 ```
 """
 get!(f::Function, collection, key)
