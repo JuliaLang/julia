06b7f0ba173a52b816044388729766c9a73daa0d
Simeon Schaub <simeondavidschaub99@gmail.com>
2020-08-13 01:54:17 +0200

    fix #28188: filename lost in cmd interpolation (#37007)
    * fix 28188: filename lost in cmd interpolation
    
    * move test to test/misc.jl

--- base/shell.jl
+++ base/shell.jl
@@ -17,7 +17,7 @@ function rstrip_shell(s::AbstractString)
 end
 
 function shell_parse(str::AbstractString, interpolate::Bool=true;
-                     special::AbstractString="")
+                     special::AbstractString="", filename="none")
     s = SubString(str, firstindex(str))
     s = rstrip_shell(lstrip(s))
 
@@ -71,7 +71,8 @@ function shell_parse(str::AbstractString, interpolate::Bool=true;
                 # string interpolation syntax (see #3150)
                 ex, j = :var, stpos+3
             else
-                ex, j = Meta.parse(s,stpos,greedy=false)
+                # use parseatom instead of parse to respect filename (#28188)
+                ex, j = Meta.parseatom(s, stpos, filename=filename)
             end
             last_parse = (stpos:prevind(s, j)) .+ s.offset
             update_arg(ex);
