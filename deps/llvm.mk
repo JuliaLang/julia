ef0189c15f1ceef8c6101e5c065e01d31038cde5
Valentin Churavy <v.churavy@gmail.com>
2020-08-22 14:08:38 -0400

    [LLVM] add D80101 & D84031

--- deps/llvm.mk
+++ deps/llvm.mk
@@ -430,6 +430,8 @@ $(eval $(call LLVM_PATCH,llvm9-D71443-PPC-MC-redef-symbol)) # remove for 10.0
 $(eval $(call LLVM_PATCH,llvm-9.0-D78196)) # remove for 11.0
 $(eval $(call LLVM_PATCH,llvm-julia-tsan-custom-as))
 $(eval $(call LLVM_PATCH,llvm-9.0-D85499))
+$(eval $(call LLVM_PATCH,llvm-D80101))
+$(eval $(call LLVM_PATCH,llvm-D84031))
 endif # LLVM_VER 9.0
 
 ifeq ($(LLVM_VER_SHORT),10.0)
@@ -445,6 +447,8 @@ $(eval $(call LLVM_PATCH,llvm-D75072-SCEV-add-type))
 $(eval $(call LLVM_PATCH,llvm-10.0-PPC_SELECT_CC)) # delete for LLVM 11
 $(eval $(call LLVM_PATCH,llvm-10.0-PPC-LI-Elimination)) # delete for LLVM 11
 $(eval $(call LLVM_PATCH,llvm-julia-tsan-custom-as))
+$(eval $(call LLVM_PATCH,llvm-D80101))
+$(eval $(call LLVM_PATCH,llvm-D84031))
 endif # LLVM_VER 10.0
 
 ifeq ($(LLVM_VER_SHORT),11.0)
