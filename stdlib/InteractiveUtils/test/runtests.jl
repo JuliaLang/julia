493a17ad8a6f4b159d290b992752843dae98cec5
Kristoffer <kcarlsson89@gmail.com>
2020-09-06 12:10:25 +0200

    fix some issues with buildbot path not updating in stacktraces / method errors

--- stdlib/InteractiveUtils/test/runtests.jl
+++ stdlib/InteractiveUtils/test/runtests.jl
@@ -568,6 +568,37 @@ file, ln = functionloc(versioninfo, Tuple{})
 @test isfile(pathof(InteractiveUtils))
 @test isdir(pkgdir(InteractiveUtils))
 
+@testset "buildbot path updating" begin
+    file, ln = functionloc(versioninfo, Tuple{})
+    @test isfile(file)
+
+    e = try versioninfo("wat")
+    catch e
+        e
+    end
+    @test e isa MethodError
+    s = sprint(showerror, e)
+    m = match(r"at (.*?):[0-9]*", s)
+    @info "DEBUG START"
+    println(s)
+    println(m)
+    println(m.captures[1])
+    println(expandusers(m.captures[1]))
+    @info "DEBUG END"
+
+    @test isfile(expanduser(m.captures[1]))
+
+    g() = x
+    e, bt = try code_llvm(g, Tuple{Int})
+    catch e
+        e, catch_backtrace()
+    end
+    @test e isa Exception
+    s = sprint(showerror, e, bt)
+    m = match(r"(\S*InteractiveUtils[\/\\]src\S*):", s)
+    @test isfile(expanduser(m.captures[1]))
+end
+
 @testset "Issue #34434" begin
     io = IOBuffer()
     code_native(io, eltype, Tuple{Int})
