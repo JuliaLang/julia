745f2870f1d57f0c560811a8672c125d10397863
Stefan Karpinski <stefan@karpinski.org>
2020-09-15 19:05:51 -0400

    upgrade libcurl & mbedtls; add nghttp2

--- stdlib/LibCURL_jll/src/LibCURL_jll.jl
+++ stdlib/LibCURL_jll/src/LibCURL_jll.jl
@@ -13,14 +13,18 @@ libcurl_handle = C_NULL
 libcurl_path = ""
 
 if Sys.iswindows()
+    const libnghttp2 = "libnghttp2-14.dll"
     const libcurl = "libcurl-4.dll"
 elseif Sys.isapple()
+    const libnghttp2 = "libnghttp2.14.dylib"
     const libcurl = "libcurl.4.dylib"
 else
+    const libnghttp2 = "libnghttp2.so"
     const libcurl = "libcurl.so"
 end
 
 function __init__()
+    dlopen(libnghttp2)
     global libcurl_handle = dlopen(libcurl)
     global libcurl_path = dlpath(libcurl_handle)
 end
