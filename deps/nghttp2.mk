f45c799985d24ee79e051bceac1884e6868d0c93
Yichao Yu <yyc1992@gmail.com>
2020-09-18 19:18:29 -0400

    Allow system libnghttp2 (#37618)

--- deps/nghttp2.mk
+++ deps/nghttp2.mk
@@ -1,11 +1,9 @@
 ## nghttp2
 
 ifneq ($(USE_BINARYBUILDER_NGHTTP2), 1)
-$(error "Non-BinaryBuilder nghttp2 is not supported.")
+ifneq ($(USE_SYSTEM_NGHTTP2), 1)
+$(error "FIXME: Source build nghttp2 is not supported.")
 endif
-
-ifneq ($(USE_SYSTEM_NGHTTP2), 0)
-$(error "Non-BinaryBuilder nghttp2 is not supported.")
 endif
 
 NGHTTP2_BB_URL_BASE := https://github.com/JuliaBinaryWrappers/nghttp2_jll.jl/releases/download/nghttp2-v$(NGHTTP2_VER)+$(NGHTTP2_BB_REL)
