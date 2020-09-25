0e3e68b75e41215fc1c89dd33f033240dc803ac4
Rémi Garcia <47191601+remi-garcia@users.noreply.github.com>
2020-09-25 08:53:50 +0200

    Better english
    Co-authored-by: Daniel Karrasch <daniel.karrasch@posteo.de>

--- base/operators.jl
+++ base/operators.jl
@@ -1064,7 +1064,7 @@ splat(f) = args->f(args...)
 
 Create a function that checks whether its argument is [`in`](@ref) `x`, i.e.
 a function equivalent to `y -> y in x`. See also [`insorted`](@ref) for the use
-in sorted collections.
+with sorted collections.
 
 The returned function is of type `Base.Fix2{typeof(in)}`, which can be
 used to implement specialized methods.
