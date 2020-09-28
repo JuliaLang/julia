edcd3a3bb042ae5c9a2171f3f7b9405deff56b1a
Ian <i.r.butterworth@gmail.com>
2020-09-28 17:46:55 -0400

    update timev example

--- base/timing.jl
+++ base/timing.jl
@@ -205,12 +205,24 @@ See also [`@time`](@ref), [`@timed`](@ref), [`@elapsed`](@ref), and
 [`@allocated`](@ref).
 
 ```julia-repl
-julia> @timev rand(10^6);
-  0.001006 seconds (7 allocations: 7.630 MiB)
-elapsed time (ns): 1005567
-bytes allocated:   8000256
-pool allocs:       6
-malloc() calls:    1
+julia> x = rand(10,10);
+
+julia> @timev x * x;
+  0.546770 seconds (2.20 M allocations: 116.632 MiB, 4.23% gc time, 99.94% compilation time)
+elapsed time (ns): 546769547
+gc time (ns):      23115606
+bytes allocated:   122297811
+pool allocs:       2197930
+non-pool GC allocs:1327
+malloc() calls:    36
+realloc() calls:   5
+GC pauses:         3
+
+julia> @timev x * x;
+  0.000010 seconds (1 allocation: 896 bytes)
+elapsed time (ns): 9848
+bytes allocated:   896
+pool allocs:       1
 ```
 """
 macro timev(ex)
