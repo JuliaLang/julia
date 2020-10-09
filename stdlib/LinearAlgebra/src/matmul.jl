6456bf563007aaf1ae3bcacfbfa7c8cd1e883c5c
Michael Abbott <me@escbook>
2020-10-09 18:40:59 +0200

    better wording

--- stdlib/LinearAlgebra/src/matmul.jl
+++ stdlib/LinearAlgebra/src/matmul.jl
@@ -186,16 +186,16 @@ end
     muladd(A, y, z)
 
 Combined multiply-add, `A*y .+ z`, for matrix-matrix or matrix-vector multiplication.
-The result is always the size of `A*y`, but `z` may be smaller, or a scalar.
+The result is always the same size as `A*y`, but `z` may be smaller, or a scalar.
 
 !!! compat "Julia 1.6"
      These methods require Julia 1.6 or later.
 
 # Examples
 ```jldoctest
-julia> A=[1.0 2.0; 3.0 4.0]; B=[1.0 1.0; 1.0 1.0]; C=[0, 100];
+julia> A=[1.0 2.0; 3.0 4.0]; B=[1.0 1.0; 1.0 1.0]; z=[0, 100];
 
-julia> muladd(A, B, C)
+julia> muladd(A, B, z)
 2×2 Matrix{Float64}:
    3.0    3.0
  107.0  107.0
