780faaf182fd46befbc74a1d70ee285804788734
Valentin Churavy <vchuravy@users.noreply.github.com>
2020-10-22 13:31:27 -0400

    Make Distributed.Worker threadsafe  (#38134)
    Co-authored-by: Jonas Schulze <jonas.schulze@st.ovgu.de>

--- stdlib/Distributed/test/distributed_exec.jl
+++ stdlib/Distributed/test/distributed_exec.jl
@@ -1694,4 +1694,5 @@ include("splitrange.jl")
 # Run topology tests last after removing all workers, since a given
 # cluster at any time only supports a single topology.
 rmprocs(workers())
+include("threads.jl")
 include("topology.jl")
