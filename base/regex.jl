d363fd2822298c0de7056bda89a633efd1c02a16
melonedo <44501064+melonedo@users.noreply.github.com>
2020-10-29 11:41:14 +0800

    Fix #36550 (showing SubstitutionString) with the same solution for Regex (#38161)

--- base/regex.jl
+++ base/regex.jl
@@ -428,10 +428,10 @@ substitutions. Most commonly constructed using the [`@s_str`](@ref) macro.
 
 ```jldoctest
 julia> SubstitutionString("Hello \\\\g<name>, it's \\\\1")
-s"Hello \\\\g<name>, it's \\\\1"
+s"Hello \\g<name>, it's \\1"
 
 julia> subst = s"Hello \\g<name>, it's \\1"
-s"Hello \\\\g<name>, it's \\\\1"
+s"Hello \\g<name>, it's \\1"
 
 julia> typeof(subst)
 SubstitutionString{String}
@@ -451,7 +451,7 @@ iterate(s::SubstitutionString, i::Integer...) = iterate(s.string, i...)::Union{N
 
 function show(io::IO, s::SubstitutionString)
     print(io, "s")
-    show(io, s.string)
+    print_quoted_literal(io, s.string)
 end
 
 """
