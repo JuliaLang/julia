30b3d790d01f5bb07a2e19bb578fc278136bc36e
Tim Holy <tim.holy@gmail.com>
2020-11-07 05:02:24 -0600

    Improve inference in Method display and others

--- base/methodshow.jl
+++ base/methodshow.jl
@@ -90,7 +90,7 @@ function kwarg_decl(m::Method, kwtype = nothing)
         kwli = ccall(:jl_methtable_lookup, Any, (Any, Any, UInt), kwtype.name.mt, sig, get_world_counter())
         if kwli !== nothing
             kwli = kwli::Method
-            slotnames = ccall(:jl_uncompress_argnames, Vector{Any}, (Any,), kwli.slot_syms)
+            slotnames = ccall(:jl_uncompress_argnames, Vector{Symbol}, (Any,), kwli.slot_syms)
             kws = filter(x -> !(x === empty_sym || '#' in string(x)), slotnames[(kwli.nargs + 1):end])
             # ensure the kwarg... is always printed last. The order of the arguments are not
             # necessarily the same as defined in the function
@@ -102,7 +102,7 @@ function kwarg_decl(m::Method, kwtype = nothing)
             return kws
         end
     end
-    return Any[]
+    return Symbol[]
 end
 
 function show_method_params(io::IO, tv)
@@ -152,7 +152,7 @@ function updated_methodloc(m::Method)::Tuple{String, Int32}
         end
     end
     file = fixup_stdlib_path(string(file))
-    return file, line
+    return file, Int32(line)
 end
 
 functionloc(m::Core.MethodInstance) = functionloc(m.def)
