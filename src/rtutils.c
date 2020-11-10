8f54d34c02206f6d8976e6e7916b51055a0f7a19
Nathan Daly <nhdaly@gmail.com>
2020-11-10 17:45:53 -0500

    Fix segfault in static_show, by using correct `vt` instead of `typeof(v)`
    This fixes a segfault introduced in #38049.
    
    Co-Authored-By: Sacha Verweij <sacha.verweij@alumni.stanford.edu>

--- src/rtutils.c
+++ src/rtutils.c
@@ -999,7 +999,7 @@ static size_t jl_static_show_x_(JL_STREAM *out, jl_value_t *v, jl_datatype_t *vt
         n += jl_static_show_x(out, *(jl_value_t**)v, depth);
         n += jl_printf(out, ")");
     }
-    else if (jl_function_type && jl_isa(v, (jl_value_t*)jl_function_type)) {
+    else if (jl_function_type && jl_subtype(vt, (jl_value_t*)jl_function_type)) {
         // v is function instance (an instance of a Function type).
         jl_datatype_t *dv = (jl_datatype_t*)vt;
         jl_sym_t *sym = dv->name->mt->name;
