ae86600988a1fa9e2d91defb7a7b0b7aa3c05076
Thomas Marks <marksta@umich.edu>
2020-11-16 15:25:48 -0500

    Update stdlib/Unicode/src/Unicode.jl
    Co-authored-by: Stefan Karpinski <stefan@karpinski.org>

--- stdlib/Unicode/src/Unicode.jl
+++ stdlib/Unicode/src/Unicode.jl
@@ -93,9 +93,9 @@ const EMOJI_RANGES = [
     0x1F600:0x1F64F,  # Emoticons
     0x1F300:0x1F5FF,  # Misc Symbols and Pictographs
     0x1F680:0x1F6FF,  # Transport and Map
-    0x2600:0x26FF,    # Misc symbols
-    0x2700:0x27BF,    # Dingbats
-    0xFE00:0xFE0F,    # Variation Selectors
+    0x02600:0x026FF,  # Misc symbols
+    0x02700:0x027BF,  # Dingbats
+    0x0FE00:0x0FE0F,  # Variation Selectors
     0x1F900:0x1F9FF,   # Supplemental Symbols and Pictographs
 ];
 
