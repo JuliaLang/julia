4a6385118478eda672055748b91407b1fa6d4712
Elliot Saba <staticfloat@gmail.com>
2020-11-17 23:28:14 +0000

    Add 'fake' JLLs for all binary dependencies
    JLLs provide the binary interaction interface that both packages and the
    Pkg resolver require in order to properly version and interact with the
    binary dependencies shipped with Julia.  This PR changes all binary
    dependencies to carry along a JLL package that dlopen()'s the binary
    that ships along with Julia.  These JLL packages are not the true JLL
    packages that are generated by BinaryBuilder, but are instead "fake"
    JLLs that simply `dlopen(filename)` and rely upon the default dlopen
    path to find the appropriate libraries, which affords the opportunity
    for system libraries and bundled-with-julia libraries alike to both be
    represented through the JLL interface.

--- deps/zlib.mk
+++ deps/zlib.mk
@@ -31,8 +31,6 @@ check-zlib: compile-zlib
 
 else # USE_BINARYBUILDER_ZLIB
 
-ZLIB_BB_URL_BASE := https://github.com/JuliaBinaryWrappers/Zlib_jll.jl/releases/download/Zlib-v$(ZLIB_VER)+$(ZLIB_BB_REL)
-ZLIB_BB_NAME := Zlib.v$(ZLIB_VER)
 $(eval $(call bb-install,zlib,ZLIB,false))
 
 endif # USE_BINARYBUILDER_ZLIB
--- stdlib/MozillaCACerts_jll/src/MozillaCACerts_jll.jl
+++ stdlib/MozillaCACerts_jll/src/MozillaCACerts_jll.jl
@@ -4,8 +4,18 @@
 
 module MozillaCACerts_jll
 
+# These get calculated in __init__()
+PATH = Ref("")
+LIBPATH = Ref("")
+artifact_dir = ""
+cacert = ""
+
 function __init__()
+	global artifact_dir = dirname(Sys.BINDIR)
+	global LIBPATH[] = joinpath(Sys.BINDIR, Base.LIBDIR, "julia")
 	global cacert = normpath(Sys.BINDIR::String, Base.DATAROOTDIR, "julia", "cert.pem")
 end
+
+is_available() = true
 
 end # module
