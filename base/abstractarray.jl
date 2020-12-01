454a71ec997afa85c64978b867865b5b4cc0824c
Bogumił Kamiński <bkamins@sgh.waw.pl>
2020-12-01 07:46:40 +0100

    Update base/abstractarray.jl

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -1132,8 +1132,8 @@ end
 """
     getindex(A, inds...)
 
-Return a subset of array `A` as specified by `inds`, where each `ind` may be in
-particular an `Int`, an [`AbstractRange`](@ref), or a [`Vector`](@ref).
+Return a subset of array `A` as specified by `inds`, where each `ind` may be,
+for example, an `Int`, an [`AbstractRange`](@ref), or a [`Vector`](@ref).
 See the manual section on [array indexing](@ref man-array-indexing) for details.
 
 # Examples
