cee7e883e565e19ebffb4a4836016cbc05760823
Leandro Martínez <31046348+lmiq@users.noreply.github.com>
2020-12-03 13:18:01 -0300

    added test with non-ascii chars

--- test/regex.jl
+++ test/regex.jl
@@ -71,10 +71,11 @@
     @test count(r"\w*", "foo bar") == 4
     @test count(r"\b", "foo bar") == 4
     # count with char as argument
-    @test count('a',"batman") == 2
-    @test count('a',"aaa",overlap=true) == 3
-    @test count('a',"") == 0
-
+    @test count('a', "batman") == 2
+    @test count('a', "aaa",overlap=true) == 3
+    @test count('a', "") == 0
+    @test count('→', "OH⁻ + H₃CBr →  (HOH₃CBr⁻)† → HOCH₃ + Br⁻") == 2
+    
     # Unnamed subpatterns
     let m = match(r"(.)(.)(.)", "xyz")
         @test haskey(m, 1)
