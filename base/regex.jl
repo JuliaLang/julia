bdd70713d0f95b5b34f2bbb7c591fb66a834ff6b
Leandro Martínez <31046348+lmiq@users.noreply.github.com>
2020-12-03 13:49:01 -0300

    added method findall(::Char,::String)

--- base/regex.jl
+++ base/regex.jl
@@ -397,6 +397,27 @@ function findall(t::Union{AbstractString,AbstractPattern}, s::AbstractString; ov
 end
 
 """
+    findall(c :: AbstractChar, s :: AbstractString)
+  
+Return a vector `I` of the indices of `s` where `s[i] == c`. If there are no such 
+elements in `s`, return an empty array.
+
+# Examples
+```jldoctest
+julia> findall('a', "batman")
+2-element Vector{Int64}:
+ 2
+ 5
+```
+
+!!! compat "Julia 1.6"
+     This method requires at least Julia 1.6.
+
+"""
+findall(c :: AbstractChar, s :: AbstractString) = findall(isequal(c),s)
+    
+
+"""
     count(
         pattern::Union{AbstractChar,AbstractString,AbstractPattern},
         string::AbstractString;
