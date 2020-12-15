fa6077e17aaf328dabc805a3000fa959c0359cf1
Ian Butterworth <i.r.butterworth@gmail.com>
2020-12-15 14:38:51 -0500

    measure compile time only when using time macros (fix #38877) (#38885)

--- src/julia_internal.h
+++ src/julia_internal.h
@@ -122,6 +122,7 @@ static inline uint64_t cycleclock(void)
 
 #include "timing.h"
 
+extern uint8_t jl_measure_compile_time;
 extern uint64_t jl_cumulative_compile_time;
 
 #ifdef _COMPILER_MICROSOFT_
