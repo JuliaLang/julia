fbaf7a8d5ad05500380b3d1b659e92a686c9928e
Keno Fischer <keno@juliacomputing.com>
2020-12-16 00:13:27 -0500

    Allow DCE of pure functions that are not inlined (#38130)
    E.g. because the return value is too large.

--- test/compiler/inline.jl
+++ test/compiler/inline.jl
@@ -350,3 +350,15 @@ f_apply_type_typeof(x) = (Ref{typeof(x)}; nothing)
 struct RealConstrained{T <: Real}; end
 @test !fully_eliminated(x->(RealConstrained{x}; nothing), Tuple{Int})
 @test !fully_eliminated(x->(RealConstrained{x}; nothing), Tuple{Type{Vector{T}} where T})
+
+# Check that pure functions with non-inlineable results still get deleted
+struct Big
+    x::NTuple{1024, Int}
+end
+@Base.pure Big() = Big(ntuple(identity, 1024))
+function pure_elim_full()
+    Big()
+    nothing
+end
+
+@test fully_eliminated(pure_elim_full, Tuple{})
