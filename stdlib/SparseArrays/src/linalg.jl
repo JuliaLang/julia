ae921bfda0ef46f5caf27a03453756d71bcaee65
Seth Axen <seth.axen@gmail.com>
2020-12-30 15:44:08 -0800

    Move opnormest1 to LinearAlgebra

--- stdlib/SparseArrays/src/linalg.jl
+++ stdlib/SparseArrays/src/linalg.jl
@@ -1,7 +1,6 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 import LinearAlgebra: checksquare, sym_uplo
-using Random: rand!
 
 # In matrix-vector multiplication, the correct orientation of the vector is assumed.
 const StridedOrTriangularMatrix{T} = Union{StridedMatrix{T}, LowerTriangular{T}, UnitLowerTriangular{T}, UpperTriangular{T}, UnitUpperTriangular{T}}
@@ -1162,169 +1161,17 @@ function cond(A::AbstractSparseMatrixCSC, p::Real=2)
     end
 end
 
-function opnormestinv(A::AbstractSparseMatrixCSC{T}, t::Integer = min(2,maximum(size(A)))) where T
-    maxiter = 5
-    # Check the input
-    n = checksquare(A)
-    F = factorize(A)
-    if t <= 0
-        throw(ArgumentError("number of blocks must be a positive integer"))
-    end
-    if t > n
-        throw(ArgumentError("number of blocks must not be greater than $n"))
-    end
-    ind = Vector{Int64}(undef, n)
-    ind_hist = Vector{Int64}(undef, maxiter * t)
+# utility type for opnormestinv
+struct InvMat{TF<:Factorization}
+    F::TF
+end
+InvMat(A::AbstractMatrix) = InvMat(factorize(A))
+Base.eltype(M::InvMat) = eltype(M.F)
+Base.size(M::InvMat) = size(M.F)
+Base.:*(M::InvMat, X) = M.F \ X
 
-    Ti = typeof(float(zero(T)))
-
-    S = zeros(T <: Real ? Int : Ti, n, t)
-
-    function _any_abs_eq(v,n::Int)
-        for vv in v
-            if abs(vv)==n
-                return true
-            end
-        end
-        return false
-    end
-
-    # Generate the block matrix
-    X = Matrix{Ti}(undef, n, t)
-    X[1:n,1] .= 1
-    for j = 2:t
-        while true
-            rand!(view(X,1:n,j), (-1, 1))
-            yaux = X[1:n,j]' * X[1:n,1:j-1]
-            if !_any_abs_eq(yaux,n)
-                break
-            end
-        end
-    end
-    rmul!(X, inv(n))
-
-    iter = 0
-    local est
-    local est_old
-    est_ind = 0
-    while iter < maxiter
-        iter += 1
-        Y = F \ X
-        est = zero(real(eltype(Y)))
-        est_ind = 0
-        for i = 1:t
-            y = norm(Y[1:n,i], 1)
-            if y > est
-                est = y
-                est_ind = i
-            end
-        end
-        if iter == 1
-            est_old = est
-        end
-        if est > est_old || iter == 2
-            ind_best = est_ind
-        end
-        if iter >= 2 && est <= est_old
-            est = est_old
-            break
-        end
-        est_old = est
-        S_old = copy(S)
-        for j = 1:t
-            for i = 1:n
-                S[i,j] = Y[i,j]==0 ? one(Y[i,j]) : sign(Y[i,j])
-            end
-        end
-
-        if T <: Real
-            # Check whether cols of S are parallel to cols of S or S_old
-            for j = 1:t
-                while true
-                    repeated = false
-                    if j > 1
-                        saux = S[1:n,j]' * S[1:n,1:j-1]
-                        if _any_abs_eq(saux,n)
-                            repeated = true
-                        end
-                    end
-                    if !repeated
-                        saux2 = S[1:n,j]' * S_old[1:n,1:t]
-                        if _any_abs_eq(saux2,n)
-                            repeated = true
-                        end
-                    end
-                    if repeated
-                        rand!(view(S,1:n,j), (-1, 1))
-                    else
-                        break
-                    end
-                end
-            end
-        end
-
-        # Use the conjugate transpose
-        Z = F' \ S
-        h_max = zero(real(eltype(Z)))
-        h = zeros(real(eltype(Z)), n)
-        h_ind = 0
-        for i = 1:n
-            h[i] = norm(Z[i,1:t], Inf)
-            if h[i] > h_max
-                h_max = h[i]
-                h_ind = i
-            end
-            ind[i] = i
-        end
-        if iter >=2 && ind_best == h_ind
-            break
-        end
-        p = sortperm(h, rev=true)
-        h = h[p]
-        permute!(ind, p)
-        if t > 1
-            addcounter = t
-            elemcounter = 0
-            while addcounter > 0 && elemcounter < n
-                elemcounter = elemcounter + 1
-                current_element = ind[elemcounter]
-                found = false
-                for i = 1:t * (iter - 1)
-                    if current_element == ind_hist[i]
-                        found = true
-                        break
-                    end
-                end
-                if !found
-                    addcounter = addcounter - 1
-                    for i = 1:current_element - 1
-                        X[i,t-addcounter] = 0
-                    end
-                    X[current_element,t-addcounter] = 1
-                    for i = current_element + 1:n
-                        X[i,t-addcounter] = 0
-                    end
-                    ind_hist[iter * t - addcounter] = current_element
-                else
-                    if elemcounter == t && addcounter == t
-                        break
-                    end
-                end
-            end
-        else
-            ind_hist[1:t] = ind[1:t]
-            for j = 1:t
-                for i = 1:ind[j] - 1
-                    X[i,j] = 0
-                end
-                X[ind[j],j] = 1
-                for i = ind[j] + 1:n
-                    X[i,j] = 0
-                end
-            end
-        end
-    end
-    return est
+function opnormestinv(A::AbstractSparseMatrixCSC, t::Integer = min(2,maximum(size(A))))
+    return LinearAlgebra.opnormest1(InvMat(A), t)
 end
 
 ## kron
