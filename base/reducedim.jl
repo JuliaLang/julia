b686f0bc12bceca296d5eefbfd0c5c1782a8020b
Oscar Smith <oscardssmith@gmail.com>
2020-12-30 23:01:47 -0600

    switch to use Generator which has the same performance and is cleaner.

--- base/reducedim.jl
+++ base/reducedim.jl
@@ -322,8 +322,7 @@ julia> mapreduce(isodd, |, a, dims=1)
 mapreduce(f, op, A::AbstractArrayOrBroadcasted; dims=:, init=_InitialValue()) =
     _mapreduce_dim(f, op, init, A, dims)
 mapreduce(f, op, A::AbstractArrayOrBroadcasted...; kw...) =
-    reduce(op, map(f, A...); kw...)
-mapreduce(f, op, A::AbstractArrayOrBroadcasted...) = mapreduce(A->f(A...),op,zip(A...))
+    reduce(op, Generator(f, itrs...); kw...)
 
 _mapreduce_dim(f, op, nt, A::AbstractArrayOrBroadcasted, ::Colon) =
     mapfoldl_impl(f, op, nt, A)
