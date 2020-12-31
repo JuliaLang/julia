6335ebb729d1af8ab296f012afdec88e6d86e05b
Seth Axen <seth.axen@gmail.com>
2020-12-31 01:56:18 -0800

    Indent to match

--- stdlib/LinearAlgebra/src/generic.jl
+++ stdlib/LinearAlgebra/src/generic.jl
@@ -942,7 +942,7 @@ function opnormest1(
             # Randomly permute any cols of S that are parallel to cols of S or S_old
             for j = 1:t
                 while (j > 1 && _any_cols_are_parallel(S, j, S, 1:(j - 1))) ||
-                    (iter >= 2 && _any_cols_are_parallel(S, j, S_old, 1:t))
+                      (iter >= 2 && _any_cols_are_parallel(S, j, S_old, 1:t))
                     rand!(view(S, 1:n, j), (-1, 1))
                 end
             end
