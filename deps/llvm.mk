81701f1fdfc6c4510c10369696779636103340ca
Valentin Churavy <vchuravy@csail.mit.edu>
2021-01-12 17:54:31 -0500

    move LLVM assert subversion to llvm.mk

--- deps/llvm.mk
+++ deps/llvm.mk
@@ -623,6 +623,12 @@ update-llvm:
 endif
 else # USE_BINARYBUILDER_LLVM
 
+# We provide a way to subversively swap out which LLVM JLL we pull artifacts from
+ifeq ($(BINARYBUILDER_LLVM_ASSERTS), 1)
+LLVM_JLL_DOWNLOAD_NAME := libLLVM_assert
+LLVM_JLL_VER := $(LLVM_ASSERT_JLL_VER)
+endif
+
 $(eval $(call bb-install,llvm,LLVM,false,true))
 $(eval $(call bb-install,clang,CLANG,false,true))
 $(eval $(call bb-install,llvm-tools,LLVM_TOOLS,false,true))
