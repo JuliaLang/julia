eb03717a1949ae4cfbe6a04b4987254d4b28ec0c
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2021-01-13 10:14:28 +0100

    fix stupid typos, test type stability

--- stdlib/LinearAlgebra/test/svd.jl
+++ stdlib/LinearAlgebra/test/svd.jl
@@ -11,15 +11,15 @@ using LinearAlgebra: BlasComplex, BlasFloat, BlasReal, QRPivoted
     m = [2, 0]
     @test @inferred(svdvals(m)) ≊ [2]
     @test @inferred(svdvals!(float(m))) ≊ [2]
-    for sf in (svd(m), svd!(float(m)))
+    for sf in (@inferred(svd(m)), @inferred(svd!(float(m))))
         @test sf.S ≊ [2]
         @test sf.U'sf.U ≊ [1]
         @test sf.Vt'sf.Vt ≊ [1]
         @test sf.U*Diagonal(sf.S)*sf.Vt' ≊ m
     end
-    F = svd(m, full=true)
-    @test size(F.Q) == (2, 2)
-    @test F.S ≊ [2, 0]
+    F = @inferred svd(m, full=true)
+    @test size(F.U) == (2, 2)
+    @test F.S ≊ [2]
     @test F.U'F.U ≊ Matrix(I, 2, 2)
     @test F.Vt'*F.Vt ≊ [1]
     @test @inferred(svdvals(3:4)) ≊ [5]
@@ -31,8 +31,8 @@ using LinearAlgebra: BlasComplex, BlasFloat, BlasReal, QRPivoted
     @test @inferred(svdvals(m2))  ≊ [2, 1]
     @test @inferred(svdvals(m2c)) ≊ [2, 1]
 
-    sf1 = svd(m1)
-    sf2 = svd(m2)
+    sf1 = @inferred svd(m1)
+    sf2 = @inferred svd(m2)
     @test sf1.S ≊ [2, 0]
     @test sf2.S ≊ [2, 1]
     # U & Vt are unitary
