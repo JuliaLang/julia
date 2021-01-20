56413bd781ceed8d5155c5b4ec2c2178572ce214
Oscar Smith <oscardssmith@gmail.com>
2021-01-19 22:17:13 -0600

    revert to first version

--- base/reducedim.jl
+++ base/reducedim.jl
@@ -322,7 +322,8 @@ julia> mapreduce(isodd, |, a, dims=1)
 mapreduce(f, op, A::AbstractArrayOrBroadcasted; dims=:, init=_InitialValue()) =
     _mapreduce_dim(f, op, init, A, dims)
 mapreduce(f, op, A::AbstractArrayOrBroadcasted...; kw...) =
-    reduce(op, Generator(f, A...); kw...)
+    reduce(op, map(f, A...); kw...)
+mapreduce(f, op, A::AbstractArrayOrBroadcasted...) = mapreduce(A->f(A...),op,zip(A...))
 
 _mapreduce_dim(f, op, nt, A::AbstractArrayOrBroadcasted, ::Colon) =
     mapfoldl_impl(f, op, nt, A)
