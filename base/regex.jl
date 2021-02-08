e9e6ecaef2addf8bb87eb14301d8cf627420ff25
Curtis Vogt <curtis.vogt@gmail.com>
2021-02-08 09:05:15 -0600

    Mention `\Q...\E` in `Regex` docstring and manual (#38941)

--- base/regex.jl
+++ base/regex.jl
@@ -23,6 +23,9 @@ with [`match`](@ref).
 `Regex` objects can be created using the [`@r_str`](@ref) string macro. The
 `Regex(pattern[, flags])` constructor is usually used if the `pattern` string needs
 to be interpolated. See the documentation of the string macro for details on flags.
+
+!!! note
+    To escape interpolated variables use `\\Q` and `\\E` (e.g. `Regex("\\\\Q\$x\\\\E")`)
 """
 mutable struct Regex <: AbstractPattern
     pattern::String
