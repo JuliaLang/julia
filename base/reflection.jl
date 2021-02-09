ec71f85e5d77cfbe88f2431e5a650ad54977b3b4
Jeff Bezanson <jeff.bezanson@gmail.com>
2021-02-09 15:09:42 -0500

    add missing io arg to `print_statement_costs` (#39579)

--- base/reflection.jl
+++ base/reflection.jl
@@ -1260,7 +1260,7 @@ function print_statement_costs(io::IO, @nospecialize(tt::Type);
         nd = ndigits(maxcost)
         println(io, meth)
         IRShow.show_ir(io, code, (io, linestart, idx) -> (print(io, idx > 0 ? lpad(cst[idx], nd+1) : " "^(nd+1), " "); return ""))
-        println()
+        println(io)
     end
 end
 
