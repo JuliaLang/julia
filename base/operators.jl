7fb0434d9dc2629416a567790b2af0d5030bdf27
Sebastian Stock <42280794+sostock@users.noreply.github.com>
2021-02-15 18:40:39 +0100

    Add compat notes for Julia 1.6 (#39671)
    (cherry picked from commit 715e6264e33a408a576966dc9ca2a75ddc4cc160)

--- base/operators.jl
+++ base/operators.jl
@@ -1157,6 +1157,9 @@ Avoid adding methods to this function; define `in` instead.
 
 Create a function that checks whether its argument contains the given `item`, i.e.
 a function equivalent to `y -> item in y`.
+
+!!! compat "Julia 1.6"
+    This method requires Julia 1.6 or later.
 """
 ∋(x) = Fix2(∋, x)
 
