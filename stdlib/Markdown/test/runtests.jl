5b80086c8872515c8c151db8c872d5e651517897
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2021-02-27 18:36:13 +0100

    Refactor / clean up tests

--- stdlib/Markdown/test/runtests.jl
+++ stdlib/Markdown/test/runtests.jl
@@ -1215,39 +1215,43 @@ end
 end
 
 @testset "issue #25992: status quo of parsing of interpolated Julia expressions" begin
-    str_ex = raw" $(1 + 1)$(1 + 1)"
-    @test Markdown.parse(str_ex).content[1].content[1].formula == Markdown.LaTeX("(1 + 1)").formula
-    @test Markdown.parse(str_ex).content[1].content[2] == "(1 + 1)"
+    # Status quo
+    @test Markdown.parse(raw" $(1 + 1)$(1 + 1)") == MD(Paragraph([LaTeX("(1 + 1)"), "(1 + 1)"]))
+    @test Markdown.parse(raw" $(1 + 1) $(1 + 1)") == MD(Paragraph([:(1 + 1), " ", :(1 + 1)]))
+    @test Markdown.parse(raw"$(1 + 1) $(1 + 1)") == MD([:(1 + 1), Paragraph([:(1 + 1)])])
+    @test Markdown.parse(raw"$(1 + 1)$(1 + 1)") == MD([LaTeX("(1 + 1)"), Paragraph(["(1 + 1)"])])
 
-
-    str_ex = raw" $(1 + 1) $(1 + 1)"
-    @test Markdown.parse(str_ex).content[1].content[1] == :(1 + 1)
-    @test Markdown.parse(str_ex).content[1].content[2] == " "
-    @test Markdown.parse(str_ex).content[1].content[3] == :(1 + 1)
-
-    str_ex = raw"$(1 + 1) $(1 + 1)"
-    @test Markdown.parse(str_ex).content[1] == :(1 + 1)
-    @test Markdown.parse(str_ex).content[2].content[1] == :(1 + 1)
-
-    str_ex = raw"$(1 + 1)$(1 + 1)"
-    @test Markdown.parse(str_ex).content[1].formula == Markdown.LaTeX("(1 + 1)").formula
-    @test Markdown.parse(str_ex).content[2].content[1] == "(1 + 1)"
+    # Potential future 'consistent' results? (e.g. if parsing was consistent with string interpolation behavior)
+    @test_broken Markdown.parse(raw" $(1 + 1)$(1 + 1)") == MD(Paragraph([:(1 + 1), :(1 + 1)]))
+    @test Markdown.parse(raw" $(1 + 1) $(1 + 1)") == MD(Paragraph([:(1 + 1), " ", :(1 + 1)]))
+    @test_broken Markdown.parse(raw"$(1 + 1) $(1 + 1)") == MD(Paragraph([:(1 + 1), " ", :(1 + 1)]))
+    @test_broken Markdown.parse(raw"$(1 + 1)$(1 + 1)") == MD(Paragraph([:(1 + 1), :(1 + 1)]))
 
 end
 
-# Document how combining interpolation and Latex leads to puzzling results
+# Document how combining interpolation and Latex leads to puzzling results, inconsistent with backtick syntax
 # (especially relevant in context of Pluto.jl)
 @testset "Latex + Interpolation Intrigues" begin
-    function parse_md_test(txt)
-        Markdown.parse(txt).content[1]
-    end
+    # Status quo
+    var_a_epsilon_dollar_1 = raw" $(a) $ \epsilon $"
+    var_a_epsilon_tick_1 = raw" $(a) `` \epsilon ``"
 
-    @test parse_md_test(raw" $(a) $ \epsilon $").content == Markdown.Paragraph(Any[:a, raw" $ \epsilon $"]).content
-    @test parse_md_test(raw"$(a) $\epsilon$").formula == raw"(a) $\epsilon"
+    var_a_epsilon_dollar_2 = raw"$(a) $\epsilon$"
+    var_a_epsilon_tick_2 = raw"$(a) ``\epsilon``"
 
-    test_string = raw" $(a) $\epsilon$ $(a)"
-    @test parse_md_test(test_string).content[1].formula == Markdown.LaTeX(raw"(a) $\epsilon").formula
-    @test parse_md_test(test_string).content[2] == " "
-    @test parse_md_test(test_string).content[3] == Symbol("a")
+    var_a_epsilon_dollar_3 = raw" $(a) $\epsilon$ $(a)"
+    var_a_epsilon_tick_3 = raw" $(a) ``\epsilon`` $(a)"
 
+    @test Markdown.parse(var_a_epsilon_dollar_1) == MD([Paragraph([:a, raw" $ \epsilon $"])]) # current behavior
+    @test Markdown.parse(var_a_epsilon_tick_1) == MD([Paragraph([:a, " ", LaTeX(raw"\epsilon")])])  # result using backtick
+    @test_broken Markdown.parse(var_a_epsilon_dollar_1) == MD([Paragraph([:a, " ", LaTeX(raw"\epsilon")])]) # consistent with backtick
+
+    @test Markdown.parse(var_a_epsilon_dollar_2) == MD([LaTeX(raw"(a) $\epsilon")]) # current behavior
+    @test Markdown.parse(var_a_epsilon_tick_2) == MD([:a, Paragraph([LaTeX(raw"\epsilon")])]) # result using backtick
+    @test_broken Markdown.parse(var_a_epsilon_dollar_2) == MD([:a, Paragraph([LaTeX(raw"\epsilon")])]) # consistent with backtick
+
+
+    @test Markdown.parse(var_a_epsilon_dollar_3) == MD(Paragraph([LaTeX(raw"(a) $\epsilon"), " ", :a])) # current behavior
+    @test Markdown.parse(var_a_epsilon_tick_3) == MD([Paragraph([:a, " ", LaTeX(raw"\epsilon"), " ", :a])]) # result using backtick
+    @test_broken Markdown.parse(var_a_epsilon_dollar_3) == MD([Paragraph([:a, " ", LaTeX(raw"\epsilon"), " ", :a])]) # consistent with backtick
 end
