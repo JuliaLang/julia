3ccda49648ae472cf8a3aaa1206f5800cc841de0
Steven G. Johnson <stevenj@alum.mit.edu>
2021-02-27 20:26:07 -0500

    no need for export, since deprecation does it

--- test/testhelpers/Furlongs.jl
+++ test/testhelpers/Furlongs.jl
@@ -1,7 +1,6 @@
 # retain this module for backwards compatibility with packages that use
 # it for tests.
 module Furlongs
-export Furlong
 import Test
 Base.@deprecate_binding Furlong Test.GenericDimensionful
 end
