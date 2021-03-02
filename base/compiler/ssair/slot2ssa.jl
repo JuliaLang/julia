eac0188fc2b1a7ce3962ceb076046402dbbe5939
Jameson Nash <vtjnash@gmail.com>
2021-03-02 16:00:48 -0500

    Canonicalize IR to disallow mutable GlobalRef in value position
    Generally we assume parameters can be duplicated without seeing
    side-effects. That is not entirely true of mutable globals and
    multi-threading.
    
    Refs: #36450
    Fixes: #39508

--- base/compiler/ssair/slot2ssa.jl
+++ base/compiler/ssair/slot2ssa.jl
@@ -162,7 +162,7 @@ function fixemup!(cond, rename, ir::IRCode, ci::CodeInfo, idx::Int, @nospecializ
                 return nothing
             end
             op[] = x
-        elseif isa(val, GlobalRef) && !isdefined(val.mod, val.name)
+        elseif isa(val, GlobalRef) && !(isdefined(val.mod, val.name) && isconst(val.mod, val.name))
             op[] = NewSSAValue(insert_node!(ir, idx, Any, val).id - length(ir.stmts))
         end
     end
