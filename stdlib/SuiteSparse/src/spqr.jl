9b3897f5c9b46f21f874e6a9e56cb1c055edf969
SomTambe <tambesom@gmail.com>
2021-03-03 18:08:16 +0000

    Add methods for `qr(::AdjOrTrans)`

--- stdlib/SuiteSparse/src/spqr.jl
+++ stdlib/SuiteSparse/src/spqr.jl
@@ -227,6 +227,8 @@ LinearAlgebra.qr(A::Union{SparseMatrixCSC{T},SparseMatrixCSC{Complex{T}}};
     "sparse floating point QR using SPQR or qr(Array(A)) for generic ",
     "dense QR.")))
 LinearAlgebra.qr(A::SparseMatrixCSC; tol=_default_tol(A)) = qr(float(A); tol=tol)
+LinearAlgebra.qr(A::Adjoint{T,<:SparseMatrixCSC}) where T =  qr(SparseMatrixCSC(A))
+LinearAlgebra.qr(A::Transpose{T,<:SparseMatrixCSC}) where T = qr(SparseMatrixCSC(A))
 
 function LinearAlgebra.lmul!(Q::QRSparseQ, A::StridedVecOrMat)
     if size(A, 1) != size(Q, 1)
