50f111809cfdc5a20365dde805197b5b4f3cb0cc
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2021-03-06 20:06:17 +0900

    improve inferrabilities in Test module (#39761)
    * improve inferrabilities in `Test` module

--- base/show.jl
+++ base/show.jl
@@ -2298,7 +2298,7 @@ end
 function print_type_stacktrace(io, type; color=:normal)
     str = sprint(show, type, context=io)
     i = findfirst('{', str)
-    if isnothing(i) || !get(io, :backtrace, false)::Bool
+    if i === nothing || !get(io, :backtrace, false)::Bool
         printstyled(io, str; color=color)
     else
         printstyled(io, str[1:prevind(str,i)]; color=color)
