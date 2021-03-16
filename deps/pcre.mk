2e9532bb99106cc59993bc5519d1c505c1e78980
Sacha Verweij <sacha.verweij@alumni.stanford.edu>
2021-03-16 12:35:56 -0400

    Prevent PCRE from needing a rebuild/reconf.
    This commit reorders a couple touches in deps/pcre.mkl. Those touches are
    intended to prevent patch application from triggering rebuild/reconf.
    The present ordering doesn't quite succeed in that objective; patch
    application triggers rebuild. On systems with compatible autotools,
    the rebuild succeeds and the build is nonetheless happy. On systems
    with incompatible versions of autotools, however, the build fails for
    need of a reconf. The reordering in this commit prevents the rebuild.
    
    Co-Authored-By: Rob Vermaas <rob.vermaas@gmail.com>
    (cherry picked from commit 068420b86564d0729ef8762302f9eec4a264f853)

--- deps/pcre.mk
+++ deps/pcre.mk
@@ -14,9 +14,9 @@ $(SRCCACHE)/pcre2-$(PCRE_VER)/source-extracted: $(SRCCACHE)/pcre2-$(PCRE_VER).ta
 	cp $(SRCDIR)/patches/config.sub $(SRCCACHE)/pcre2-$(PCRE_VER)/config.sub
 	cd $(SRCCACHE)/pcre2-$(PCRE_VER) && patch -p1 -f < $(SRCDIR)/patches/pcre2-cet-flags.patch
 	# Fix some old targets modified by the patching
+	touch -c $(SRCCACHE)/pcre2-$(PCRE_VER)/aclocal.m4
 	touch -c $(SRCCACHE)/pcre2-$(PCRE_VER)/Makefile.am
 	touch -c $(SRCCACHE)/pcre2-$(PCRE_VER)/Makefile.in
-	touch -c $(SRCCACHE)/pcre2-$(PCRE_VER)/aclocal.m4
 	touch -c $(SRCCACHE)/pcre2-$(PCRE_VER)/configure
 	echo $1 > $@
 
