4d06c905461101b4572b28da3b98c625621a00c2
KristofferC <kcarlsson89@gmail.com>
2021-04-02 16:21:12 +0200

    add another test

--- test/abstractarray.jl
+++ test/abstractarray.jl
@@ -1284,4 +1284,5 @@ end
 @testset "reduce(vcat, ...) inferrence #40277" begin
     x_vecs = ([5, ], [1.0, 2.0, 3.0])
     @test @inferred(reduce(vcat, x_vecs)) == [5.0, 1.0, 2.0, 3.0]
+    @test @inferred(reduce(vcat, ([10.0], [20.0], Bool[]))) == [10.0, 20.0]
 end
