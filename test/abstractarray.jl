233566be61ce40419ced3e09756e990ff9b08485
Sanjiban Sengupta <b518041@iiit-bh.ac.in>
2021-04-05 18:11:57 +0530

    Update test/abstractarray.jl
    Co-authored-by: Sebastian Stock <42280794+sostock@users.noreply.github.com>

--- test/abstractarray.jl
+++ test/abstractarray.jl
@@ -1293,7 +1293,6 @@ end
     @test_throws ArgumentError Int[t...; 3 4 5]
 end
 
-                    
 @testset "issue #39896, modified getindex " begin
     for arr = ([1:10;], reshape([1.0:16.0;],4,4), reshape(['a':'h';],2,2,2))
         for inds = (2:5, Base.OneTo(5), BigInt(3):BigInt(5), UInt(4):UInt(3))
