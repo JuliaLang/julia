b2e181fe77c067e35c2e765a5851a107e1093a36
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2021-04-16 10:52:36 -0700

    Added example for multiple arguments.

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -610,9 +610,20 @@ Create an anonymous function mapping argument(s) `x` to the function body `y`.
 ```jldoctest
 julia> f = x -> x^2 + 2x - 1
 #1 (generic function with 1 method)
-    x^2 + 2x - 1
-end
+ 
+julia> f(2)
+7
 ```
+
+Anonymous functions can also be defined for multiple argumets.
+```jldoctest
+julia> g = (x,y) -> x^2 + y^2
+#2 (generic function with 1 method)
+ 
+julia> g(2,3)
+13
+```
+
 See the manual section on [anonymous functions](@ref man-anonymous-functions) for more details.
 """
 kw"->"
