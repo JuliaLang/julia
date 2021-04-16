6126b84642b5d6097d5a67d5e2916c35b37acc49
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2021-04-16 08:47:49 -0700

    Update base/docs/basedocs.jl
    Added `julia>` to the beginning of the example
    
    Co-authored-by: Fredrik Ekre <ekrefredrik@gmail.com>

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -607,9 +607,12 @@ kw"function"
 
 Create an anonymous function mapping argument(s) `x` to the function body `y`.
 
-```julia
-x -> x^2 + 2x - 1
-```
+```jldoctest
+julia> f = x -> x^2 + 2x - 1
+#1 (generic function with 1 method)
+
+julia> f(2)
+7
 A similar function can be defined with the short form notation:
 
 ```julia
