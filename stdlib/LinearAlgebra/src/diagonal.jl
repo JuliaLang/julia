6aa30a7530fe90e3c877ef88c492d485d12af046
Sheehan Olver <solver@mac.com>
2021-04-18 00:05:06 -0400

    Overload permutedims for Diagonal (#39447)
    Optimization for performance

--- stdlib/LinearAlgebra/src/diagonal.jl
+++ stdlib/LinearAlgebra/src/diagonal.jl
@@ -552,6 +552,8 @@ transpose(D::Diagonal{<:Number}) = D
 transpose(D::Diagonal) = Diagonal(transpose.(D.diag))
 adjoint(D::Diagonal{<:Number}) = conj(D)
 adjoint(D::Diagonal) = Diagonal(adjoint.(D.diag))
+Base.permutedims(D::Diagonal) = D
+Base.permutedims(D::Diagonal, perm) = (Base.checkdims_perm(D, D, perm); D)
 
 function diag(D::Diagonal, k::Integer=0)
     # every branch call similar(..., ::Int) to make sure the
