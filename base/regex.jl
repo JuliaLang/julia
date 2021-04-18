b391dfc741fa9ce1f9d0ae0308932db2cf08791d
Ryan Regier <regier21@up.edu>
2021-04-17 23:54:17 -0700

    Minors edits from code review

--- base/regex.jl
+++ base/regex.jl
@@ -146,6 +146,8 @@ in a string using an `AbstractPattern`.
 abstract type AbstractMatch end
 
 """
+    RegexMatch
+
 A type representing a single match to a `Regex` found in a string.
 Typically created from the [`match`](@ref) function.
 
@@ -172,8 +174,7 @@ See [`keys`](@ref keys(::RegexMatch)) for more information.
 julia> m = match(r"(?<hour>\\d+):(?<minute>\\d+)(am|pm)?", "11:30 in the morning")
 RegexMatch("11:30", hour="11", minute="30", 3=nothing)
 
-julia> hr, min, ampm = m
-RegexMatch("11:30", hour="11", minute="30", 3=nothing)
+julia> hr, min, ampm = m;
 
 julia> hr
 "11"
@@ -196,15 +197,15 @@ end
 """
     keys(m::RegexMatch) -> Vector
 
-Returns a vector of keys for all capture groups of the underlying regex.
+Return a vector of keys for all capture groups of the underlying regex.
 A key is included even if the capture group fails to match.
-That is, `idx` will be in the return value even if `m.captures[idx] == nothing`.
+That is, `idx` will be in the return value even if `m[idx] == nothing`.
 
 Unnamed capture groups will have integer keys corresponding to their index.
 Named capture groups will have string keys.
 
-!!! compat "Julia 1.7"
-    This method was added in Julia 1.7
+!!! compat "Julia 1.6"
+    This method was added in Julia 1.6
 
 # Examples
 ```jldoctest
