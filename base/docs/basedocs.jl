c874fbe10ebf69c7470fde0e3270bd18bf707dac
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2021-04-19 02:49:02 -0700

    Added docstring for -> (#40470)
    Co-authored-by: Fredrik Ekre <ekrefredrik@gmail.com>

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -603,6 +603,32 @@ the last expression in the function body.
 kw"function"
 
 """
+    x -> y
+
+Create an anonymous function mapping argument(s) `x` to the function body `y`.
+
+```jldoctest
+julia> f = x -> x^2 + 2x - 1
+#1 (generic function with 1 method)
+
+julia> f(2)
+7
+```
+
+Anonymous functions can also be defined for multiple argumets.
+```jldoctest
+julia> g = (x,y) -> x^2 + y^2
+#2 (generic function with 1 method)
+
+julia> g(2,3)
+13
+```
+
+See the manual section on [anonymous functions](@ref man-anonymous-functions) for more details.
+"""
+kw"->"
+
+"""
     return
 
 `return x` causes the enclosing function to exit early, passing the given value `x`
