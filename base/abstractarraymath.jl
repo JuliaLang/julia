cd43564a11e0b57881926134cb84ca5b3e9be1a0
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2021-04-22 08:42:38 +0200

    Doc: Mention that dropdims'ed array shares memory (#40544)
    Co-authored-by: Jameson Nash <vtjnash@gmail.com>
    Co-authored-by: Michael Abbott <32575566+mcabbott@users.noreply.github.com>

--- base/abstractarraymath.jl
+++ base/abstractarraymath.jl
@@ -48,9 +48,13 @@ _sub(t::Tuple, s::Tuple) = _sub(tail(t), tail(s))
 """
     dropdims(A; dims)
 
-Remove the dimensions specified by `dims` from array `A`.
-Elements of `dims` must be unique and within the range `1:ndims(A)`.
-`size(A,i)` must equal 1 for all `i` in `dims`.
+Return an array with the same data as `A`, but with the dimensions specified by
+`dims` removed. `size(A,d)` must equal 1 for every `d` in `dims`,
+and repeated dimensions or numbers outside `1:ndims(A)` are forbidden.
+
+The result shares the same underlying data as `A`, such that the
+result is mutable if and only if `A` is mutable, and setting elements of one
+alters the values of the other.
 
 See also: [`reshape`](@ref), [`vec`](@ref).
 
@@ -62,10 +66,16 @@ julia> a = reshape(Vector(1:4),(2,2,1,1))
  1  3
  2  4
 
-julia> dropdims(a; dims=3)
+julia> b = dropdims(a; dims=3)
 2×2×1 Array{Int64, 3}:
 [:, :, 1] =
  1  3
+ 2  4
+
+julia> b[1,1,1] = 5; a
+2×2×1×1 Array{Int64, 4}:
+[:, :, 1, 1] =
+ 5  3
  2  4
 ```
 """
