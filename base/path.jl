93656ec86eddc42897cbcc75a352c0b7b93635e8
Mustafa M <mus-m@outlook.com>
2021-04-22 12:39:37 -0400

    Update base/path.jl
    Co-authored-by: Jameson Nash <vtjnash@gmail.com>

--- base/path.jl
+++ base/path.jl
@@ -325,27 +325,9 @@ end
 end # os-test
 
 """
-    joinpath(parts) -> String
-
-Join a non-empty collection of path components into a full path. If some argument is an
-absolute path or (on Windows) has a drive specification that doesn't match the drive computed for
-the join of the preceding paths, then prior components are dropped.
-
-Note on Windows since there is a current directory for each drive, `joinpath("c:", "foo")`
-represents a path relative to the current directory on drive "c:" so this is equal to "c:foo",
-not "c:\\foo". Furthermore, `joinpath` treats this as a non-absolute path and ignores the drive
-letter casing, hence `joinpath("C:\\A","c:b") = "C:\\A\\b"`.
-
-# Examples
-```jldoctest
-julia> joinpath(["/home/myuser", "example.jl"])
-"/home/myuser/example.jl"
-```
-"""
-joinpath
-
-"""
     joinpath(parts::AbstractString...) -> String
+    joinpath(parts::Vector{AbstractString}) -> String
+    joinpath(parts::Tuple{AbstractString}) -> String
 
 Join path components into a full path. If some argument is an absolute path or
 (on Windows) has a drive specification that doesn't match the drive computed for
