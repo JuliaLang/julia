bfbfd825f5dfe012df83116dd6f4ba0f521515be
Iagoba Apellaniz <iagoba.apellaniz@gmail.com>
2021-04-23 14:17:38 +0200

    make \cdot, \interpunct, and \cdotp equivalent (#25157)
    Co-authored-by: Steven G. Johnson <stevenj@alum.mit.edu>
    
    Co-authored-by: Steven G. Johnson <stevenj@alum.mit.edu>

--- src/flisp/julia_charmap.h
+++ src/flisp/julia_charmap.h
@@ -4,4 +4,6 @@
 static const uint32_t charmap[][2] = {
     { 0x025B, 0x03B5 }, // latin small letter open e -> greek small letter epsilon
     { 0x00B5, 0x03BC }, // micro sign -> greek small letter mu
+    { 0x00B7, 0x22C5 }, // middot char -> dot operator (#25098)
+    { 0x0387, 0x22C5 }, // Greek interpunct -> dot operator (#25098)
 };
