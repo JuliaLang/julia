bacd5f6a387d54a5e56d2284a956224c3c1cfb3c
Oscar Smith <oscardssmith@gmail.com>
2021-04-27 15:15:49 -0500

    make Float64 correct for 0^negative, 0^NaN

--- base/math.jl
+++ base/math.jl
@@ -945,8 +945,12 @@ function modf(x::Float64)
 end
 
 @inline function ^(x::Float64, y::Float64)
+<<<<<<< HEAD
     yint = unsafe_trunc(Int, y) # Note, this is actually safe since julia freezes the result
     y == yint && return x^yint
+=======
+    x==0 && return Float64(y==0)
+>>>>>>> make Float64 correct for 0^negative, 0^NaN
     z = ccall("llvm.pow.f64", llvmcall, Float64, (Float64, Float64), x, y)
     if isnan(z) & !isnan(x+y)
         throw_exp_domainerror(x)
