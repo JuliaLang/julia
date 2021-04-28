d72457b4fe3a76cc0f1486206f27712753395371
Kenta Sato <bicycle1885@gmail.com>
2021-04-28 21:11:29 +0900

    add docstring for VecElement (#40019)
    Co-authored-by: Jameson Nash <vtjnash@gmail.com>

--- base/baseext.jl
+++ base/baseext.jl
@@ -2,6 +2,17 @@
 
 # extensions to Core types to add features in Base
 
+"""
+    VecElement{T}
+
+A wrapper type that holds a single value of type `T`. When used in the context of an
+`NTuple{N, VecElement{T}} where {T, N}` object, it provides a hint to the runtime
+system to align that struct to be more amenable to vectorization optimization
+opportunities. In `ccall`, such an NTuple in the type signature will also use the
+vector register ABI, rather than the usual struct ABI.
+"""
+VecElement
+
 # hook up VecElement constructor to Base.convert
 VecElement{T}(arg) where {T} = VecElement{T}(convert(T, arg))
 convert(::Type{T}, arg::T) where {T<:VecElement} = arg
