249c86357178bf5fa2194af6f59933cb8d2f353f
Dilum Aluthge <dilum@aluthge.com>
2021-04-28 23:28:07 -0400

    Add a comment explaining why we have these tests

--- test/loading.jl
+++ test/loading.jl
@@ -720,6 +720,7 @@ import .Foo.Libdl; import Libdl
 end
 
 @testset "`Base.project_names` and friends" begin
+    # Some functions in Pkg assumes that these tuples have the same length
     n = length(Base.project_names)
     @test length(Base.manifest_names) == n
     @test length(Base.preferences_names) == n
