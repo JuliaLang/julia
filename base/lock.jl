d99f622328a62f34b61cfbeeb6f6e96c1a9c0204
Viktor G. Gumennyy <20798349+sairus7@users.noreply.github.com>
2021-04-29 11:20:26 +0300

    Update lock.jl

--- base/lock.jl
+++ base/lock.jl
@@ -204,7 +204,16 @@ end
 """
     @lock l expr
 
-Macro version for `lock(f, l::AbstractLock)`, but with `expr` instead of `f` function.
+Macro version of `lock(f, l::AbstractLock)` but with `expr` instead of `f` function.
+Expands to:
+```julia
+lock(l)
+try
+    expr
+finally
+    unlock(l)
+end
+```
 This is similar to using [`lock`](@ref) with a `do` block, but avoids creating a closure
 and thus can improve the performance.
 """
