88e9c1dd5d57a1582909392d83fcc18c6085f6ff
Alexander Stevenson <alex.stevenson1@me.com>
2021-05-03 14:55:17 -0400

    merged and updated

--- src/llvm-version.h
+++ src/llvm-version.h
@@ -20,10 +20,6 @@
 #define LLVM_DISABLE_ABI_BREAKING_CHECKS_ENFORCING 0
 #endif
 
-#ifndef LLVM_ENABLE_STATS
-#define LLVM_ENABLE_STATS 0
-#endif
-
 #if defined(__GNUC__) && (__GNUC__ >= 9)
 // Added in GCC 9, this warning is annoying
 #pragma GCC diagnostic ignored "-Winit-list-lifetime"
