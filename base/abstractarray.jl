613d76a959bb0dbf7b283c3ea530848cb72dccad
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-05-22 11:10:07 -0400

    document map on multi-dimensional arrays

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -2733,7 +2733,6 @@ map(f, A::AbstractArray) = collect_similar(A, Generator(f,A))
 mapany(f, A::AbstractArray) = map!(f, Vector{Any}(undef, length(A)), A)
 mapany(f, itr) = Any[f(x) for x in itr]
 
-# default to returning an Array for `map` on general iterators
 """
     map(f, c...) -> collection
 
@@ -2757,6 +2756,36 @@ julia> map(+, [1, 2, 3], [10, 20, 30, 400, 5000])
  33
 ```
 """
+map
+
+"""
+    map(f, A::AbstractArray...) -> N-array
+
+When acting on multi-dimensional arrays of the same [`ndims`](@ref),
+they must all have the same [`axes`](@ref), and the answer will too.
+
+See also [`broadcast`](@ref), which allows mismatched sizes.
+
+# Examples
+```
+julia> map(//, [1 2; 3 4], [4 3; 2 1])
+2×2 Matrix{Rational{$Int}}:
+ 1//4  2//3
+ 3//2  4//1
+
+julia> map(+, [1 2; 3 4], zeros(2,1))
+ERROR: DimensionMismatch
+
+julia> map(+, [1 2; 3 4], [1,10,100,1000], zeros(3,1))  # iterates until 3rd is exhausted
+3-element Vector{Float64}:
+   2.0
+  13.0
+ 102.0
+```
+"""
+map
+
+# default to returning an Array for `map` on general iterators
 map(f, A) = collect(Generator(f,A))
 
 map(f, ::AbstractDict) = error("map is not defined on dictionaries")
