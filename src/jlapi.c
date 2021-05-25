829d7e090f2cadfe1b1d506394f416b2938b3e7e
Max Horn <max@quendi.de>
2021-05-25 21:05:37 +0200

    Restore some ABI compatibility with Julia 1.6 (#40851)
    In Julia <= 1.6, the C API function `jl_init` and `jl_init_with_image` were
    exported on the ABI level under the names `jl_init__threading` and
    `jl_init_with_image__threading`. This was changed during development of Julia
    1.7, but unfortunately that broke ABI compatibility with binaries build
    against Julia <= 1.6.
    
    Add back and export two simple wrapper functions to restore this aspect of ABI
    compatibility.

--- src/jlapi.c
+++ src/jlapi.c
@@ -91,6 +91,19 @@ JL_DLLEXPORT void jl_init(void)
     free(libbindir);
 }
 
+// HACK: remove this for Julia 1.8 (see <https://github.com/JuliaLang/julia/issues/40730>)
+JL_DLLEXPORT void jl_init__threading(void)
+{
+    jl_init();
+}
+
+// HACK: remove this for Julia 1.8 (see <https://github.com/JuliaLang/julia/issues/40730>)
+JL_DLLEXPORT void jl_init_with_image__threading(const char *julia_bindir,
+                                     const char *image_relative_path)
+{
+    jl_init_with_image(julia_bindir, image_relative_path);
+}
+
 JL_DLLEXPORT jl_value_t *jl_eval_string(const char *str)
 {
     jl_value_t *r;
