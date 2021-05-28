5a5e06e9dfe93239eccb10c86117b55e2f94659e
Rik Huijzer <rikhuijzer@pm.me>
2021-05-28 11:59:19 +0200

    Fix documentation for SimpleLogger

--- base/logging.jl
+++ base/logging.jl
@@ -617,11 +617,16 @@ close(closed_stream)
 #-------------------------------------------------------------------------------
 # SimpleLogger
 """
-    SimpleLogger([stream,] min_level=Info)
+    SimpleLogger([stream,] level=Info)
 
 Simplistic logger for logging all messages with level greater than or equal to
 `min_level` to `stream`. If stream is closed then messages with log level
 greater or equal to `Warn` will be logged to `stderr` and below to `stdout`.
+                
+# Example
+```jldoctest
+julia> Logging.SimpleLogger(level=Logging.Error);
+```
 """
 struct SimpleLogger <: AbstractLogger
     stream::IO
