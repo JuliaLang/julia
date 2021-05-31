3c5411bf64c75085fdff3bae5c06cffdf9cfc774
KristofferC <kcarlsson89@gmail.com>
2021-05-31 17:18:16 +0200

    remove deprecation of getproperty on Pair

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -244,12 +244,6 @@ cat_shape(dims, shape::Tuple{}) = () # make sure `cat_shape(dims, ())` do not re
 
 # BEGIN 1.7 deprecations
 
-# the plan is to eventually overload getproperty to access entries of the dict
-@noinline function getproperty(x::Pairs, s::Symbol)
-    depwarn("use values(kwargs) and keys(kwargs) instead of kwargs.data and kwargs.itr", :getproperty, force=true)
-    return getfield(x, s)
-end
-
 # This function was marked as experimental and not exported.
 @deprecate catch_stack(task=current_task(); include_bt=true) current_exceptions(task; backtrace=include_bt) false
 
