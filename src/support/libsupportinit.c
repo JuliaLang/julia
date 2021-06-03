cd0811c6a809a3e08a79221ff07991f86d0b2494
Arun sanganal <74652697+ArunSanganal@users.noreply.github.com>
2021-06-03 22:02:00 +0530

    Update libsupportinit.c

--- src/support/libsupportinit.c
+++ src/support/libsupportinit.c
@@ -25,32 +25,6 @@ void libsupport_init(void)
     if (!isInitialized) {
         ios_init_stdstreams();
         isInitialized = 1;
-
-        // adopt the user's locale for most formatting
-        setlocale(LC_ALL, "");
-        // but use locale-independent numeric formats (for parsing)
-        setlocale(LC_NUMERIC, "C");
-        // and try to specify ASCII or UTF-8 (preferred) for our Libc and Cstring functions
-        char *ctype = setlocale(LC_CTYPE, NULL);
-        if (ctype) {
-            size_t codeset = jl_strchrnul(ctype, '.') - ctype;
-            if (strncmp(ctype + codeset, ".UTF-8", strlen(".UTF-8")) == 0 ||
-                strncmp(ctype + codeset, ".utf-8", strlen(".utf-8")) == 0 ||
-                strncmp(ctype + codeset, ".utf8", strlen(".utf8")) == 0)
-                return; // already UTF-8
-            ctype = (char*)memcpy(malloc_s(codeset + sizeof(".UTF-8")), ctype, codeset);
-            strcpy(ctype + codeset, ".UTF-8");
-        }
-        setlocale(LC_CTYPE, "C"); // ASCII
-#ifndef _OS_WINDOWS_
-        if (setlocale(LC_CTYPE, "C.UTF-8") == NULL && // Linux/FreeBSD name
-            setlocale(LC_CTYPE, "en_US.UTF-8") == NULL && // Common name
-            setlocale(LC_CTYPE, "UTF-8") == NULL && // Apple name
-            (ctype == NULL || setlocale(LC_CTYPE, ctype) == NULL)) { // attempt to form it manually
-            ios_puts("WARNING: failed to select UTF-8 encoding, using ASCII\n", ios_stderr);
-        }
-#endif
-
 #if defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 200112L
         // Raise the open file descriptor limit.
         {
@@ -73,6 +47,30 @@ void libsupport_init(void)
                     }
                 } while (min + 1 < max);
             }
+        }
+#endif
+        // adopt the user's locale for most formatting
+        setlocale(LC_ALL, "");
+        // but use locale-independent numeric formats (for parsing)
+        setlocale(LC_NUMERIC, "C");
+        // and try to specify ASCII or UTF-8 (preferred) for our Libc and Cstring functions
+        char *ctype = setlocale(LC_CTYPE, NULL);
+        if (ctype) {
+            size_t codeset = jl_strchrnul(ctype, '.') - ctype;
+            if (strncmp(ctype + codeset, ".UTF-8", strlen(".UTF-8")) == 0 ||
+                strncmp(ctype + codeset, ".utf-8", strlen(".utf-8")) == 0 ||
+                strncmp(ctype + codeset, ".utf8", strlen(".utf8")) == 0)
+                return; // already UTF-8
+            ctype = (char*)memcpy(malloc_s(codeset + sizeof(".UTF-8")), ctype, codeset);
+            strcpy(ctype + codeset, ".UTF-8");
+        }
+        setlocale(LC_CTYPE, "C"); // ASCII
+#ifndef _OS_WINDOWS_
+        if (setlocale(LC_CTYPE, "C.UTF-8") == NULL && // Linux/FreeBSD name
+            setlocale(LC_CTYPE, "en_US.UTF-8") == NULL && // Common name
+            setlocale(LC_CTYPE, "UTF-8") == NULL && // Apple name
+            (ctype == NULL || setlocale(LC_CTYPE, ctype) == NULL)) { // attempt to form it manually
+            ios_puts("WARNING: failed to select UTF-8 encoding, using ASCII\n", ios_stderr);
         }
 #endif
         if (ctype)
