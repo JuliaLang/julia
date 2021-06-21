d0bcdfe50dfbf857c5c66f1d55513ba4c98b3ff2
Marcell <havlikmarcell@gmail.com>
2021-06-21 17:26:05 +0200

    doctest fix, dimension print are cleaner in case of Array!

--- base/indices.jl
+++ base/indices.jl
@@ -142,7 +142,7 @@ julia> a = fill(1, (3,4,1,1,1));
 julia> b = fill(1, (3,4));
 
 julia> promote_shape(a,b)
-(Base.OneTo(3), Base.OneTo(4), Base.OneTo(1), Base.OneTo(1), Base.OneTo(1))
+(3, 4, 1, 1, 1)
 
 julia> promote_shape((2,3,1,4), (2, 3, 1, 4, 1))
 (2, 3, 1, 4, 1)
