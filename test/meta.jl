26bfefa69047fdef26334f3a8d0c40329e979233
Simeon Schaub <simeondavidschaub99@gmail.com>
2021-07-02 01:59:25 +0200

    fix `partially_inline!` with `isdefined` (#40628)
    ref https://github.com/JuliaLang/julia/pull/40562#issuecomment-824912109
    
    (cherry picked from commit 10b010f6011996558c56a5c46d97e9a201286ea0)

--- test/meta.jl
+++ test/meta.jl
@@ -241,7 +241,29 @@ ci = code_lowered(f, Tuple{Int})[1]
 
 g(::Val{x}) where {x} = x ? 1 : 0
 ci = code_lowered(g, Tuple{Val{true}})[1]
-@test Meta.partially_inline!(ci.code, [], Tuple{typeof(g),Val{true}}, Any[Val{true}], 0, 0, :propagate)[1] ==
-   Core.GotoIfNot(QuoteNode(Val{true}), 3)
-@test Meta.partially_inline!(ci.code, [], Tuple{typeof(g),Val{true}}, Any[Val{true}], 0, 2, :propagate)[1] ==
-   Core.GotoIfNot(QuoteNode(Val{true}), 5)
+@test Meta.partially_inline!(ci.code, [], Tuple{typeof(g),Val{true}}, Any[true], 0, 0, :propagate)[1] ==
+   Core.GotoIfNot(QuoteNode(true), 3)
+@test Meta.partially_inline!(ci.code, [], Tuple{typeof(g),Val{true}}, Any[true], 0, 2, :propagate)[1] ==
+   Core.GotoIfNot(QuoteNode(true), 5)
+
+@testset "inlining with isdefined" begin
+    isdefined_slot(x) = @isdefined(x)
+    ci = code_lowered(isdefined_slot, Tuple{Int})[1]
+    @test Meta.partially_inline!(copy(ci.code), [], Tuple{typeof(isdefined_slot), Int},
+                                 [], 0, 0, :propagate)[1] == Expr(:isdefined, Core.SlotNumber(2))
+    @test Meta.partially_inline!(copy(ci.code), [isdefined_slot, 1], Tuple{typeof(isdefined_slot), Int},
+                                 [], 0, 0, :propagate)[1] == true
+
+    isdefined_sparam(::T) where {T} = @isdefined(T)
+    ci = code_lowered(isdefined_sparam, Tuple{Int})[1]
+    @test Meta.partially_inline!(copy(ci.code), [], Tuple{typeof(isdefined_sparam), Int},
+                                 Any[Int], 0, 0, :propagate)[1] == true
+    @test Meta.partially_inline!(copy(ci.code), [], Tuple{typeof(isdefined_sparam), Int},
+                                 [], 0, 0, :propagate)[1] == Expr(:isdefined, Expr(:static_parameter, 1))
+
+    @eval isdefined_globalref(x) = $(Expr(:isdefined, GlobalRef(Base, :foo)))
+    ci = code_lowered(isdefined_globalref, Tuple{Int})[1]
+    @test Meta.partially_inline!(copy(ci.code), Any[isdefined_globalref, 1], Tuple{typeof(isdefined_globalref), Int},
+                                 [], 0, 0, :propagate)[1] == Expr(:isdefined, GlobalRef(Base, :foo))
+
+end
