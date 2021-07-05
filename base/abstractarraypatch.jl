0e237cf32d12e2534605043d37b524f5879d7c72
N5N3 <2642243996@qq.com>
2021-07-05 22:38:49 +0800

    Update abstractarraypatch.jl
    Fix for L2Ccopyto!

--- base/abstractarraypatch.jl
+++ base/abstractarraypatch.jl
@@ -20,47 +20,38 @@ function copyto_unaliased!(deststyle::IndexStyle, dest::AbstractArray, srcstyle:
     isempty(src) && return dest
     length(dest) < length(src) && throw(BoundsError(dest, LinearIndices(src)))
     if deststyle isa IndexLinear
-        # Cartesian --> Linear
-        srcstyle isa IndexCartesian && return C2Lcopyto!(dest, src)
-        # Linear --> Linear
-        Δi = firstindex(dest) - firstindex(src)
-        for i in eachindex(src)
-            @inbounds dest[i + Δi] = src[i]
-        end
-    else
-        # Linear --> Cartesian
-        srcstyle isa IndexLinear && return L2Ccopyto!(dest, src)
-        # Cartesian --> Cartesian
-        iterdest, itersrc = eachindex(dest), eachindex(src)
-        if iterdest == itersrc #shared iterator
-            @inbounds @simd for I in iterdest
-                dest[I] = src[I]
+        if srcstyle isa IndexLinear
+            Δi = firstindex(dest) - firstindex(src)
+            for i in eachindex(src)
+                @inbounds dest[i + Δi] = src[i]
             end
         else
-            @inbounds for (J, I) in zip(iterdest, itersrc)
-                dest[J] = src[I]
+            j = firstindex(dest) - 1
+            @inbounds @simd for I in eachindex(src)
+                dest[j += 1] = src[I]
+            end
+        end
+    else
+        if srcstyle isa IndexLinear
+            L2Ccopyto!(dest, src) # this manually expanded version seems faster
+            #i = firstindex(src) - 1
+            #@inbounds @simd for J in view(eachindex(dest), 1:length(src))
+            #    dest[J] = src[i += 1]
+            #end
+        else
+            iterdest, itersrc = eachindex(dest), eachindex(src)
+            if iterdest == itersrc #shared iterator
+                @inbounds @simd for I in iterdest
+                    dest[I] = src[I]
+                end
+            else
+                @inbounds for (J, I) in zip(iterdest, itersrc)
+                    dest[J] = src[I]
+                end
             end
         end
     end
     return dest
-end
-
-# Manually expanded SIMD kernal for Cartesian to Linear copy
-@eval @inline function C2Lcopyto!(dest::AbstractArray, src::AbstractArray)
-    iter = eachindex(src)
-    Δj = firstindex(dest)
-    @inbounds for I in simd_outer_range(iter)
-        n = simd_inner_length(iter, I)
-        j = zero(n)
-        while j < n
-            I′ = simd_index(iter, I, j)
-            dest[Δj + j] = src[I′]
-            j += 1
-            $(Expr(:loopinfo, Symbol("julia.simdloop"), nothing))
-        end
-        Δj += n
-    end
-    dest
 end
 
 # Manually expanded SIMD kernal for Linear to Cartesian copy
@@ -71,12 +62,10 @@ end
         n = min(final - Δi, simd_inner_length(iter, J))
         i = zero(n)
         while i < n
-            J′ = simd_index(iter, J, i)
-            dest[J′] = src[i + Δi]
+            dest[simd_index(iter, J, i)] = src[i + Δi]
             i += 1
             $(Expr(:loopinfo, Symbol("julia.simdloop"), nothing))
         end
-        Δi += n >= final && break
+        (Δi += n) >= final && break
     end
-    dest
 end
