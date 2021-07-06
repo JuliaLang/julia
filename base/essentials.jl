5bf4a34915cbdfb283daba624a3f5eb82c331a89
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-07-06 14:53:04 -0400

    Update base/essentials.jl
    Co-authored-by: Jeff Bezanson <jeff.bezanson@gmail.com>

--- base/essentials.jl
+++ base/essentials.jl
@@ -447,7 +447,7 @@ reinterpret(::Type{T}, x) where {T} = bitcast(T, x)
     sizeof(obj)
 
 Size, in bytes, of the canonical binary representation of the given `DataType` `T`, if any.
-Or the size, in bytes, of object `obj` if it is not `DataType`.
+Or the size, in bytes, of object `obj` if it is not a `DataType`.
 
 See also [`summarysize`](@ref).
 
