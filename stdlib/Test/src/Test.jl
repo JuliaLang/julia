83abe70afa0e93a58cfa7ee727d81e4c10b30d3d
Nicholas Bauer <nicholasbauer@outlook.com>
2021-07-08 02:01:30 -0400

    bt is a CapturedException?

--- stdlib/Test/src/Test.jl
+++ stdlib/Test/src/Test.jl
@@ -167,7 +167,7 @@ struct Error <: Result
             bt = scrub_exc_stack(bt)
         end
         if test_type === :test_error || test_type === :nontest_error
-            bt_str = sprint(Base.show_exception_stack, Base.ExceptionStack([(exception = Nothing, backtrace = bt)]); context=stdout)
+            bt_str = sprint(Base.show_exception_stack, Base.ExceptionStack([(exception = bt.ex, backtrace = bt.processed_bt)]); context=stdout)
         else
             bt_str = ""
         end
