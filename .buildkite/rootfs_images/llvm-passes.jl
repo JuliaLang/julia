a8a505dd1083892916bee9b52a04c5ffe8c3c087
Dilum Aluthge <dilum@aluthge.com>
2021-07-13 17:49:06 -0400

    CI (rootfs_utils): Require that the user explicitly specify the `tag_name`

--- .buildkite/rootfs_images/llvm-passes.jl
+++ .buildkite/rootfs_images/llvm-passes.jl
@@ -4,6 +4,11 @@
 ## Eventually, this image will probably be replaced with the actual builder image,
 ## as that will have the necessary toolchains as well, but that image is not built yet.
 
+if length(ARGS) != 1
+    throw(ArgumentError("Usage: llvm-passes.jl [tag_name]"))
+end
+const tag_name = convert(String, strip(ARGS[1]))::String
+
 include("rootfs_utils.jl")
 
 # Build debian-based image with the following extra packages:
@@ -26,4 +31,4 @@ packages = [
 tarball_path = debootstrap("llvm-passes"; packages)
 
 # Upload it
-upload_rootfs_image(tarball_path; tag_name = "v1")
+upload_rootfs_image(tarball_path; tag_name)
