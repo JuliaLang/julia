9fcc1fc44e4ef886d91a9dbcabb743f2d967c5e7
fre-hu <81446677+fre-hu@users.noreply.github.com>
2021-07-19 23:51:33 +0200

    optimize comparison for AbstractUnitRange and OneTo (#40767)

--- base/range.jl
+++ base/range.jl
@@ -1017,6 +1017,11 @@ function ==(r::OrdinalRange, s::OrdinalRange)
     (first(r) == first(s)) & (step(r) == step(s)) & (last(r) == last(s))
 end
 
+==(r::AbstractUnitRange, s::AbstractUnitRange) =
+    (isempty(r) & isempty(s)) | ((first(r) == first(s)) & (last(r) == last(s)))
+
+==(r::OneTo, s::OneTo) = last(r) == last(s)
+
 ==(r::T, s::T) where {T<:Union{StepRangeLen,LinRange}} =
     (isempty(r) & isempty(s)) | ((first(r) == first(s)) & (length(r) == length(s)) & (last(r) == last(s)))
 
