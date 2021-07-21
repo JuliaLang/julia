aaf1ebe5cac5242c2ae3718d83e9eec219f3ac63
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2021-07-21 23:32:17 +0200

    Switch from typeassert to

--- base/path.jl
+++ base/path.jl
@@ -36,7 +36,7 @@ elseif Sys.iswindows()
 
     function splitdrive(path::String)
         m = match(r"^([^\\]+:|\\\\[^\\]+\\[^\\]+|\\\\\?\\UNC\\[^\\]+\\[^\\]+|\\\\\?\\[^\\]+:|)(.*)$"s, path)
-        String(m.captures[1]::SubString), String(m.captures[2]::SubString)
+        String(something(m.captures[1])), String(something(m.captures[2]))
     end
 else
     error("path primitives for this OS need to be defined")
@@ -208,7 +208,7 @@ function splitext(path::String)
     a, b = splitdrive(path)
     m = match(path_ext_splitter, b)
     m === nothing && return (path,"")
-    (a*m.captures[1]::SubString), String(m.captures[2]::SubString)
+    (a*something(m.captures[1])), String(something(m.captures[2]))
 end
 
 # NOTE: deprecated in 1.4
