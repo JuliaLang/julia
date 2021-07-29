84e6bb9b61c9459a525cbc49c32d8fea85eb9e20
Jameson Nash <vtjnash@gmail.com>
2021-07-29 12:07:17 -0400

    Revert "Merge pull request #38405 from JuliaLang/vc/distributed_ts" (#41722)
    Also reverts "fixup to pull request #38405 (#41641)"
    
    Seems to be causing hanging in CI testing.
    
    This reverts commit 5af1cf0faf10308857f319b17432b007e318516b and this
    reverts commit 5a1680533b461471f537f5f5d4ee3c2866b21e1f, reversing
    changes made to 02807b279a5e6d5acaeb7095e4c0527e2a5c190e.
    
    (cherry picked from commit 66f9b55e3bf40f03e49cd95008ba1d7c709ca127)

--- stdlib/Distributed/test/distributed_exec.jl
+++ stdlib/Distributed/test/distributed_exec.jl
@@ -1689,5 +1689,4 @@ include("splitrange.jl")
 # Run topology tests last after removing all workers, since a given
 # cluster at any time only supports a single topology.
 rmprocs(workers())
-include("threads.jl")
 include("topology.jl")
