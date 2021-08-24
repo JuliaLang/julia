9bed4bdf5ecab627d5f7444a8746cb6cd64eeea3
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2021-08-25 00:42:41 +0200

    Fix collect on stateful generator (#41919)
    Previously this code would drop 1 from the length of some generators.
    
    Fixes #35530
    
    (cherry picked from commit 8364a4ccd8885fa8d8c78094c7653c58e33d9f0d)

--- test/iterators.jl
+++ test/iterators.jl
@@ -292,6 +292,15 @@ let (a, b) = (1:3, [4 6;
     end
 end
 
+# collect stateful iterator
+let
+    itr = (i+1 for i in Base.Stateful([1,2,3]))
+    @test collect(itr) == [2, 3, 4]
+    A = zeros(Int, 0, 0)
+    itr = (i-1 for i in Base.Stateful(A))
+    @test collect(itr) == Int[] # Stateful do not preserve shape
+end
+
 # with 1D inputs
 let a = 1:2,
     b = 1.0:10.0,
