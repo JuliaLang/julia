c5fb880d0f3aae4746bf71721b8e8ddbd18df044
Kristoffer Carlsson <kcarlsson89@gmail.com>
2021-08-24 09:27:50 +0200

    change optimization level to 1 (JuliaLang/JuliaSyntax.jl#187)

--- JuliaSyntax/src/Tokenize.jl
+++ JuliaSyntax/src/Tokenize.jl
@@ -1,5 +1,9 @@
 module Tokenize
 
+if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
+    @eval Base.Experimental.@optlevel 1
+end
+
 include("token.jl")
 include("lexer.jl")
 
