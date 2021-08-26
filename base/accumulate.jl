33eaf7228f05bbbf79c36072d7078b30395c65b7
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-08-26 19:41:34 -0400

    tweak + whitespace

--- base/accumulate.jl
+++ base/accumulate.jl
@@ -228,10 +228,10 @@ cumprod(itr) = accumulate(mul_prod, itr)
 Cumulative operation `op` along the dimension `dims` of `A` (providing `dims` is optional
 for vectors). An initial value `init` may optionally be provided by a keyword argument. See
 also [`accumulate!`](@ref) to use a preallocated output array, both for performance and
-to control the precision of the output (e.g. to avoid overflow). 
+to control the precision of the output (e.g. to avoid overflow).
 
-For common operations there are specialized variants of `accumulate`, 
-see [`cumsum`](@ref), [`cumprod`](@ref). For a lazy version, see 
+For common operations there are specialized variants of `accumulate`,
+see [`cumsum`](@ref), [`cumprod`](@ref). For a lazy version, see
 [`Iterators.accumulate`](@ref).
 
 !!! compat "Julia 1.5"
@@ -248,14 +248,14 @@ julia> accumulate(+, Int8[1,2,3])
 julia> accumulate(min, (1, -2, 3, -4, 5), init=0)
 (0, -2, -2, -4, -4)
 
-julia> accumulate(/, (1, 2, 4, 8))
-(1, 0.5, 0.125, 0.015625)
+julia> accumulate(/, (2, 4, Inf), init=100)
+(50.0, 12.5, 0.0)
 
-julia> accumulate(+, i^2 for i in 1:3; init=100.0)
-3-element Vector{Float64}:
- 101.0
- 105.0
- 114.0
+julia> accumulate(=>, i^2 for i in 1:3)
+3-element Vector{Any}:
+          1
+        1 => 4
+ (1 => 4) => 9
 
 julia> accumulate(+, fill(1, 3, 4))
 3×4 Matrix{Int64}:
@@ -263,10 +263,10 @@ julia> accumulate(+, fill(1, 3, 4))
  2  5  8  11
  3  6  9  12
 
-julia> accumulate(+, fill(1, 2, 5), dims=2, init=100)
-2×5 Matrix{Int64}:
- 101  102  103  104  105
- 101  102  103  104  105
+julia> accumulate(+, fill(1, 2, 5), dims=2, init=100.0)
+2×5 Matrix{Float64}:
+ 101.0  102.0  103.0  104.0  105.0
+ 101.0  102.0  103.0  104.0  105.0
 ```
 """
 function accumulate(op, A; dims::Union{Nothing,Integer}=nothing, kw...)
@@ -299,7 +299,7 @@ end
 
 Cumulative operation `op` on `A` along the dimension `dims`, storing the result in `B`.
 Providing `dims` is optional for vectors.  If the keyword argument `init` is given, its
-value is used to instantiate the accumulation. 
+value is used to instantiate the accumulation.
 
 See also [`accumulate`](@ref), [`cumsum!`](@ref), [`cumprod!`](@ref).
 
