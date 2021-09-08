6007f315b8a274800158c17d05af4bdd54dc12af
Johnny Chen <johnnychen94@hotmail.com>
2021-09-08 21:25:58 +0800

    Eagerly do boundscheck when indexing into CartesianIndices (#42119)
    (cherry picked from commit 15b9851c64de5f18aad28421634b3b9b5b72356b)

--- base/multidimensional.jl
+++ base/multidimensional.jl
@@ -349,8 +349,14 @@ module IteratorsMD
     # AbstractArray implementation
     Base.axes(iter::CartesianIndices{N,R}) where {N,R} = map(Base.axes1, iter.indices)
     Base.IndexStyle(::Type{CartesianIndices{N,R}}) where {N,R} = IndexCartesian()
-    @propagate_inbounds function Base.getindex(iter::CartesianIndices{N,R}, I::Vararg{Int, N}) where {N,R}
-        CartesianIndex(getindex.(iter.indices, I))
+    @inline function Base.getindex(iter::CartesianIndices{N,R}, I::Vararg{Int, N}) where {N,R}
+        # Eagerly do boundscheck before calculating each item of the CartesianIndex so that
+        # we can pass `@inbounds` hint to inside the map and generates more efficient SIMD codes (#42115)
+        @boundscheck checkbounds(iter, I...)
+        index = map(iter.indices, I) do r, i
+            @inbounds getindex(r, i)
+        end
+        CartesianIndex(index)
     end
 
     ndims(R::CartesianIndices) = ndims(typeof(R))
