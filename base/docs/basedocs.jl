dff88cfe85546213f7cc797e050f47eca658a69d
Colin Caine <cmcaine@gmail.com>
2021-09-10 18:01:41 +0100

    Document Module constructor

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -2812,6 +2812,13 @@ StridedVecOrMat
     Module
 
 A `Module` is a separate global variable workspace. See [`module`](@ref) and the [manual section about modules](@ref modules) for details.
+
+    Module(name::Symbol=:anonymous, std_imports=true, default_names=true)
+
+Return a module with the specified name. A `baremodule` corresponds to `Module(:ModuleName, false)`
+
+An empty module containing no names at all can be created with `Module(:ModuleName, false, false)`.
+This module will not import `Base` or `Core` and does not contain a reference to itself.
 """
 Module
 
