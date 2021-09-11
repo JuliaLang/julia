f14af43fd7f83b06afbe31705064442209a82820
jishnub <jishnub@users.noreply.github.com>
2021-09-11 13:40:17 +0400

    fix imported function name

--- stdlib/LinearAlgebra/src/LinearAlgebra.jl
+++ stdlib/LinearAlgebra/src/LinearAlgebra.jl
@@ -14,7 +14,7 @@ import Base: USE_BLAS64, abs, acos, acosh, acot, acoth, acsc, acsch, adjoint, as
     getproperty, imag, inv, isapprox, isequal, isone, iszero, IndexStyle, kron, kron!, length, log, map, ndims,
     one, oneunit, parent, power_by_squaring, print_matrix, promote_rule, real, round, sec, sech,
     setindex!, show, similar, sin, sincos, sinh, size, sqrt,
-    strides, stride, tan, tanh, transpose, trunc, typed_hcat, vec, zero, nonzeroindex
+    strides, stride, tan, tanh, transpose, trunc, typed_hcat, vec, zero, isnonzeroindex
 using Base: IndexLinear, promote_eltype, promote_op, promote_typeof,
     @propagate_inbounds, @pure, reduce, typed_hvcat, typed_vcat, require_one_based_indexing,
     splat
