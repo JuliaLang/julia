10a2eeef45f355efd1c5f662ab30c42e64736a74
Steven G. Johnson <stevenj@alum.mit.edu>
2021-09-12 22:38:38 -0400

    add cross-ref from readlines to eachline

--- base/io.jl
+++ base/io.jl
@@ -548,7 +548,8 @@ end
 
 Read all lines of an I/O stream or a file as a vector of strings. Behavior is
 equivalent to saving the result of reading [`readline`](@ref) repeatedly with the same
-arguments and saving the resulting lines as a vector of strings.
+arguments and saving the resulting lines as a vector of strings.  See also
+[`eachline`](@ref) to iterate over the lines without reading them all at once.
 
 # Examples
 ```jldoctest
