e126fcdeaaa8e5732abaecc1af73ed387245b0ff
Valentin Churavy <vchuravy@users.noreply.github.com>
2021-09-14 20:09:26 -0700

    [LLVM][PM] Make LowerSIMDLoop NewPM compatible (#40884)
    * [LLVM][PM] Make LowerSIMDLoop NewPM compatible
    * [LLVM][PM] Make DemoteFloat16 NewPM compatible
    * [LLVM][PM] Make MulAdd NewPM compatible
    * [LLVM][PM] Make RemoveNI NewPM compatible

--- src/llvm-remove-ni.cpp
+++ src/llvm-remove-ni.cpp
@@ -3,6 +3,7 @@
 #include "llvm-version.h"
 
 #include <llvm/IR/Module.h>
+#include <llvm/IR/PassManager.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Support/Debug.h>
 
@@ -14,31 +15,48 @@ using namespace llvm;
 
 namespace {
 
-struct RemoveNIPass : public ModulePass {
+static bool removeNI(Module &M)
+{
+    auto dlstr = M.getDataLayoutStr();
+    auto nistart = dlstr.find("-ni:");
+    if (nistart == std::string::npos)
+        return false;
+    auto len = dlstr.size();
+    auto niend = nistart + 1;
+    for (; niend < len; niend++) {
+        if (dlstr[niend] == '-') {
+            break;
+        }
+    }
+    dlstr.erase(nistart, niend - nistart);
+    M.setDataLayout(dlstr);
+    return true;
+}
+}
+
+struct RemoveNI : PassInfoMixin<RemoveNI> {
+    PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+};
+
+PreservedAnalyses RemoveNI::run(Module &M, ModuleAnalysisManager &AM)
+{
+    removeNI(M);
+    return PreservedAnalyses::all();
+}
+
+namespace {
+struct RemoveNILegacy : public ModulePass {
     static char ID;
-    RemoveNIPass() : ModulePass(ID) {};
+    RemoveNILegacy() : ModulePass(ID) {};
 
     bool runOnModule(Module &M)
     {
-        auto dlstr = M.getDataLayoutStr();
-        auto nistart = dlstr.find("-ni:");
-        if (nistart == std::string::npos)
-            return false;
-        auto len = dlstr.size();
-        auto niend = nistart + 1;
-        for (; niend < len; niend++) {
-            if (dlstr[niend] == '-') {
-                break;
-            }
-        }
-        dlstr.erase(nistart, niend - nistart);
-        M.setDataLayout(dlstr);
-        return true;
+        return removeNI(M);
     }
 };
 
-char RemoveNIPass::ID = 0;
-static RegisterPass<RemoveNIPass>
+char RemoveNILegacy::ID = 0;
+static RegisterPass<RemoveNILegacy>
         Y("RemoveNI",
           "Remove non-integral address space.",
           false,
@@ -47,7 +65,7 @@ static RegisterPass<RemoveNIPass>
 
 Pass *createRemoveNIPass()
 {
-    return new RemoveNIPass();
+    return new RemoveNILegacy();
 }
 
 extern "C" JL_DLLEXPORT void LLVMExtraAddRemoveNIPass(LLVMPassManagerRef PM)
