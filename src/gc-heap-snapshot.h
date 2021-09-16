6db1ea5a1664aa349041a3726464e95f72545e9a
Pete Vilter <7341+vilterp@users.noreply.github.com>
2021-09-16 19:27:02 -0400

    direct output to a stream

--- src/gc-heap-snapshot.h
+++ src/gc-heap-snapshot.h
@@ -7,10 +7,6 @@
 extern "C" {
 #endif
 
-struct HeapSnapshot;
-
-void serialize_heap_snapshot(JL_STREAM *stream, struct HeapSnapshot *snapshot);
-
 // ---------------------------------------------------------------------
 // Functions to call from GC when heap snapshot is enabled
 // ---------------------------------------------------------------------
@@ -21,7 +17,7 @@ JL_DLLEXPORT void record_edge_to_gc_snapshot(jl_value_t *a, jl_value_t *b);
 // Functions to call from Julia to start heap snapshot
 // ---------------------------------------------------------------------
 // ...
-JL_DLLEXPORT void take_gc_snapshot(void);
+JL_DLLEXPORT void take_gc_snapshot(JL_STREAM *stream);
 
 
 #ifdef __cplusplus
