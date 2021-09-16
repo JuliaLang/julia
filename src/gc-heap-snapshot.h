e207adfea19b6e90d361e6e256fb586c3194f978
Nathan Daly <nhdaly@gmail.com>
2021-09-16 16:08:57 -0400

    Initial dummy implementation

---
+++ src/gc-heap-snapshot.h
@@ -0,0 +1,27 @@
+#ifndef JL_GC_HEAP_SNAPSHOT_H
+#define JL_GC_HEAP_SNAPSHOT_H
+
+#include "julia.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ---------------------------------------------------------------------
+// Functions to call from GC when heap snapshot is enabled
+// ---------------------------------------------------------------------
+void record_edge_to_gc_snapshot(jl_value_t *a, jl_value_t *b);
+
+// ---------------------------------------------------------------------
+// Functions to call from Julia to start heap snapshot
+// ---------------------------------------------------------------------
+// ...
+JL_DLLEXPORT void take_gc_snapshot();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif  // JL_GC_HEAP_SNAPSHOT_H
