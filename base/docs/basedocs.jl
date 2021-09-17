918b9536f188f11d125c7b7a019d01dc26c1dac5
Jeff Bezanson <jeff.bezanson@gmail.com>
2021-09-17 16:38:14 -0400

    Update base/docs/basedocs.jl

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -1188,7 +1188,7 @@ kw"mutable struct"
     new, or new{A,B,...}
 
 Special function available to inner constructors which creates a new object
-of the type. The form new{A,B,...} calls out the explicit types needed in a parametric constructor.
+of the type. The form new{A,B,...} explicitly specifies values of parameters for parametric types.
 See the manual section on [Inner Constructor Methods](@ref man-inner-constructor-methods)
 for more information.
 """
