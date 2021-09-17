8ed849351fb2340c89b333644d70cc07b11b1da1
Daniel Lakeland <dlakelan@street-artists.org>
2021-09-17 13:41:32 -0700

    Update basedocs.jl (#42113)
    add the new{A,B,...} syntax to the description of the new keyword/special function

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -1188,10 +1188,10 @@ fields of the type to be set after construction. See the manual section on
 kw"mutable struct"
 
 """
-    new
+    new, or new{A,B,...}
 
-Special function available to inner constructors which created a new object
-of the type.
+Special function available to inner constructors which creates a new object
+of the type. The form new{A,B,...} explicitly specifies values of parameters for parametric types.
 See the manual section on [Inner Constructor Methods](@ref man-inner-constructor-methods)
 for more information.
 """
