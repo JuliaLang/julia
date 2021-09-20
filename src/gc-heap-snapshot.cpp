02603d25948a90fbed674fdf9dbedc52d8973658
Nathan Daly <nhdaly@gmail.com>
2021-09-20 15:49:11 -0400

    Change to the real format now that we understand it:
    - The edges array is *ordered* according to the parent of each edge in
      the Nodes table, such that the edges for each Node are contiguous.

--- src/gc-heap-snapshot.cpp
+++ src/gc-heap-snapshot.cpp
@@ -41,7 +41,18 @@ void print_str_escape_json(JL_STREAM *stream, const std::string &s) {
 struct HeapSnapshot;
 void serialize_heap_snapshot(JL_STREAM *stream, HeapSnapshot &snapshot);
 
-// Dump format:
+// Edges
+// "edge_fields":
+//   [ "type", "name_or_index", "to_node" ]
+
+struct Edge {
+    string type; // These *must* match the Enums on the JS side; control interpretation of name_or_index.
+    size_t name_or_index; // name of the field (for objects/modules) or index of array
+    size_t to_node;
+
+    // Book-keeping fields (not used for serialization)
+};
+
 // Nodes
 // "node_fields":
 //   [ "type", "name", "id", "self_size", "edge_count", "trace_node_id", "detachedness" ]
@@ -54,30 +65,15 @@ struct Node {
     size_t self_size;
     size_t edge_count;
     size_t trace_node_id;  // This is ALWAYS 0 in Javascript heap-snapshots.
-    // whether the node is attached or dettached from the main application state
+    // whether the from_node is attached or dettached from the main application state
     // TODO: .... meaning not yet understood.
-    // https://github.com/nodejs/node/blob/5fd7a72e1c4fbaf37d3723c4c81dce35c149dc84/deps/v8/include/v8-profiler.h#L739-L745
+    // https://github.com/nodejs/from_node/blob/5fd7a72e1c4fbaf37d3723c4c81dce35c149dc84/deps/v8/include/v8-profiler.h#L739-L745
     int detachedness;  // 0 - unknown,  1 - attached;  2 - detached
-};
-
-// Edges
-// "edge_fields":
-//   [ "type", "name_or_index", "to_node" ]
-
-const int k_edge_number_of_fields = 3;
-
-struct Edge {
-    string type; // These *must* match the Enums on the JS side; control interpretation of name_or_index.
-    size_t name_or_index; // name of the field (for objects/modules) or index of array
-    size_t to_node;
 
     // Book-keeping fields (not used for serialization)
-    size_t from_node; // For asserting that we built the edges in the right order
+    vector<Edge> edges; // For asserting that we built the edges in the right order
 };
 
-//template<typename K, typename V>
-//auto find_or_insert_iter(unordered_map<K,V>& map, const K &key) {
-//}
 
 struct StringTable {
     typedef unordered_map<string, size_t> MapType;
@@ -126,17 +122,18 @@ public:
 
 // private:
     vector<Node> nodes;
-
-    vector<Edge> edges;
+    // edges are stored on each from_node
 
     StringTable names;
     StringTable node_types = {"object"};
     StringTable edge_types = {"property"};
     unordered_map<void*, size_t> node_ptr_to_index_map;
+
+    size_t num_edges = 0; // For metadata, updated as you add each edge. Needed because edges owned by nodes.
 };
 
 
-// TODO: Do we need to refer to nodes by their index in the node array?
+// TODO: Do we need to refer to nodes by their index in the from_node array?
 //size_t find_or_create_node_id(HeapSnapshot& snapshot, string key) {
 //    return find_or_insert_iter(snapshot.nodes_map, key)->second;
 //}
@@ -202,7 +199,7 @@ JL_DLLEXPORT void record_node_to_gc_snapshot(jl_value_t *a) {
             {a, g_snapshot->nodes.size()});
     count_nodes += 1;
 
-    Node node{
+    Node from_node{
         "object", // string type;
         name, // string name;
         (size_t)a, // size_t id;
@@ -210,9 +207,12 @@ JL_DLLEXPORT void record_node_to_gc_snapshot(jl_value_t *a) {
 
         0, // int edge_count, will be incremented on every outgoing edge
         0, // size_t trace_node_id (unused)
-        0  // int detachedness;  // 0 - unknown,  1 - attached;  2 - detached
+        0,  // int detachedness;  // 0 - unknown,  1 - attached;  2 - detached
+
+        // Book-keeping: todo
+        vector<Edge>(),
     };
-    g_snapshot->nodes.push_back(node);
+    g_snapshot->nodes.push_back(from_node);
 }
 
 // TODO: remove JL_DLLEXPORT
@@ -229,21 +229,23 @@ JL_DLLEXPORT void record_edge_to_gc_snapshot2(char *type_description, jl_value_t
 
     auto from_node_idx = g_snapshot->node_ptr_to_index_map[a];
 
-    g_snapshot->nodes[from_node_idx].type = type_description;
-    g_snapshot->nodes[from_node_idx].edge_count += 1;
+    auto &from_node = g_snapshot->nodes[from_node_idx];
+    from_node.type = type_description;
+    from_node.edge_count += 1;
 
-    g_snapshot->edges.push_back(Edge{"property",
-                                    g_snapshot->names.find_or_create_string_id(fieldname), // name or index
-                                    g_snapshot->node_ptr_to_index_map[b], // to
-                                    // book-keeping
-                                    g_snapshot->node_ptr_to_index_map[a],  // from
-                                    });
+    from_node.edges.push_back(Edge{
+        "property",
+        g_snapshot->names.find_or_create_string_id(fieldname), // name or index
+        g_snapshot->node_ptr_to_index_map[b], // to
+        // book-keeping
+    });
 
-    count_edges += 1;
+    g_snapshot->num_edges += 1;
+    count_edges += 1;  // debugging
 }
 
 void serialize_heap_snapshot(JL_STREAM *stream, HeapSnapshot &snapshot) {
-    // mimicking https://github.com/nodejs/node/blob/5fd7a72e1c4fbaf37d3723c4c81dce35c149dc84/deps/v8/src/profiler/heap-snapshot-generator.cc#L2567-L2567
+    // mimicking https://github.com/nodejs/from_node/blob/5fd7a72e1c4fbaf37d3723c4c81dce35c149dc84/deps/v8/src/profiler/heap-snapshot-generator.cc#L2567-L2567
     jl_printf(stream, "{\"snapshot\":{");
     jl_printf(stream, "\"meta\":{");
     jl_printf(stream, "\"node_fields\":[\"type\",\"name\",\"id\",\"self_size\",\"edge_count\",\"trace_node_id\",\"detachedness\"],");
@@ -255,47 +257,46 @@ void serialize_heap_snapshot(JL_STREAM *stream, HeapSnapshot &snapshot) {
     jl_printf(stream, "\"edge_types\":[");
     snapshot.edge_types.print_json_array(stream, false);
     jl_printf(stream, ",");
-    jl_printf(stream, "\"string_or_number\",\"node\"]");
+    jl_printf(stream, "\"string_or_number\",\"from_node\"]");
     jl_printf(stream, "},\n"); // end "meta"
     jl_printf(stream, "\"node_count\":%zu,", snapshot.nodes.size());
-    jl_printf(stream, "\"edge_count\":%zu", snapshot.edges.size());
+    jl_printf(stream, "\"edge_count\":%zu", snapshot.num_edges);
     jl_printf(stream, "},\n"); // end "snapshot"
 
     jl_printf(stream, "\"nodes\":[");
     bool first_node = true;
-    for (const auto &node : snapshot.nodes) {
+    for (const auto &from_node : snapshot.nodes) {
         if (first_node) {
             first_node = false;
         } else {
             jl_printf(stream, ",");
         }
         // ["type","name","id","self_size","edge_count","trace_node_id","detachedness"]
-        jl_printf(stream, "%zu", snapshot.names.find_or_create_string_id(node.type));
-        jl_printf(stream, ",%zu", snapshot.names.find_or_create_string_id(node.name));
-        jl_printf(stream, ",%zu", node.id);
-        jl_printf(stream, ",%zu", node.self_size);
-        jl_printf(stream, ",%zu", node.edge_count);
-        jl_printf(stream, ",%zu", node.trace_node_id);
-        jl_printf(stream, ",%d", node.detachedness);
+        jl_printf(stream, "%zu", snapshot.names.find_or_create_string_id(from_node.type));
+        jl_printf(stream, ",%zu", snapshot.names.find_or_create_string_id(from_node.name));
+        jl_printf(stream, ",%zu", from_node.id);
+        jl_printf(stream, ",%zu", from_node.self_size);
+        jl_printf(stream, ",%zu", from_node.edge_count);
+        jl_printf(stream, ",%zu", from_node.trace_node_id);
+        jl_printf(stream, ",%d", from_node.detachedness);
         jl_printf(stream, "\n");
     }
     jl_printf(stream, "],\n");
 
     jl_printf(stream, "\"edges\":[");
-    for (int i = 0; i < snapshot.edges.size(); ++i) {
-        // Check that we constructed our nodes & edges correctly.
-        assert(i == 0 ||
-           snapshot.edges[i - 1].from_node <= snapshot.edges[i].from_node);
-
-        const auto &edge = snapshot.edges[i];
-
-        if (i != 0) {
-            jl_printf(stream, ",");
+    bool first_edge = true;
+    for (const auto &from_node : snapshot.nodes) {
+        for (const auto &edge : from_node.edges) {
+            if (first_edge) {
+                first_edge = false;
+            } else {
+                jl_printf(stream, ",");
+            }
+            jl_printf(stream, "%zu", snapshot.names.find_or_create_string_id(edge.type));
+            jl_printf(stream, ",%zu", edge.name_or_index * k_node_number_of_fields);
+            jl_printf(stream, ",%zu", edge.to_node * k_node_number_of_fields);
+            jl_printf(stream, "\n");
         }
-        jl_printf(stream, "%zu", snapshot.names.find_or_create_string_id(edge.type));
-        jl_printf(stream, ",%zu", edge.name_or_index * k_node_number_of_fields);
-        jl_printf(stream, ",%zu", edge.to_node * k_node_number_of_fields);
-        jl_printf(stream, "\n");
     }
     jl_printf(stream, "],\n"); // end "edges"
 
