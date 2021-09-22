96321d528c91cc2e178ceae962c1f8d7e9931437
Nathan Daly <nhdaly@gmail.com>
2021-09-22 15:56:52 -0400

    fix annotations on new inline funcs

--- src/gc-heap-snapshot.h
+++ src/gc-heap-snapshot.h
@@ -26,27 +26,27 @@ JL_DLLEXPORT void _gc_heap_snapshot_record_hidden_edge(jl_value_t *from, size_t 
 
 extern int gc_heap_snapshot_enabled;
 
-static inline void gc_heap_snapshot_record_array_edge(jl_value_t *from, jl_value_t *to, size_t index) {
+static inline void gc_heap_snapshot_record_array_edge(jl_value_t *from, jl_value_t *to, size_t index) JL_NOTSAFEPOINT {
     if (__unlikely(gc_heap_snapshot_enabled)) {
         _gc_heap_snapshot_record_array_edge(from, to, index);
     }
 }
-static inline void gc_heap_snapshot_record_module_edge(jl_module_t *from, jl_value_t *to, char *name) {
+static inline void gc_heap_snapshot_record_module_edge(jl_module_t *from, jl_value_t *to, char *name) JL_NOTSAFEPOINT {
     if (__unlikely(gc_heap_snapshot_enabled)) {
         _gc_heap_snapshot_record_module_edge(from, to, name);
     }
 }
-static inline void gc_heap_snapshot_record_object_edge(jl_value_t *from, jl_value_t *to, size_t field_index) {
+static inline void gc_heap_snapshot_record_object_edge(jl_value_t *from, jl_value_t *to, size_t field_index) JL_NOTSAFEPOINT {
     if (__unlikely(gc_heap_snapshot_enabled)) {
         _gc_heap_snapshot_record_object_edge(from, to, field_index);
     }
 }
-static inline void gc_heap_snapshot_record_internal_edge(jl_value_t *from, jl_value_t *to) {
+static inline void gc_heap_snapshot_record_internal_edge(jl_value_t *from, jl_value_t *to) JL_NOTSAFEPOINT {
     if (__unlikely(gc_heap_snapshot_enabled)) {
         _gc_heap_snapshot_record_internal_edge(from, to);
     }
 }
-static inline void gc_heap_snapshot_record_hidden_edge(jl_value_t *from, size_t bytes) {
+static inline void gc_heap_snapshot_record_hidden_edge(jl_value_t *from, size_t bytes) JL_NOTSAFEPOINT {
     if (__unlikely(gc_heap_snapshot_enabled)) {
         _gc_heap_snapshot_record_hidden_edge(from, bytes);
     }
