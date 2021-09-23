91731e9da0ce81c8ccf24c49e91ce0da86b191c7
Pete Vilter <7341+vilterp@users.noreply.github.com>
2021-09-23 16:24:21 -0400

    make gc root recording conditional on heap snapshotting being on

--- src/gc-heap-snapshot.h
+++ src/gc-heap-snapshot.h
@@ -26,6 +26,11 @@ void _gc_heap_snapshot_record_hidden_edge(jl_value_t *from, size_t bytes) JL_NOT
 
 extern int gc_heap_snapshot_enabled;
 
+static inline void gc_heap_snapshot_record_root(jl_value_t *root) {
+    if (__unlikely(gc_heap_snapshot_enabled)) {
+        _gc_heap_snapshot_record_root(root);
+    }
+}
 static inline void gc_heap_snapshot_record_array_edge(jl_value_t *from, jl_value_t *to, size_t index) JL_NOTSAFEPOINT {
     if (__unlikely(gc_heap_snapshot_enabled)) {
         _gc_heap_snapshot_record_array_edge(from, to, index);
