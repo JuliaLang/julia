e2833cedf1787fc368aaa39613d0f99cea68e441
Katharine Hyatt <khyatt@flatironinstitute.org>
2021-09-24 10:20:05 -0400

    tests for converting QR eltype

--- stdlib/LinearAlgebra/test/qr.jl
+++ stdlib/LinearAlgebra/test/qr.jl
@@ -415,4 +415,15 @@ end
     @test A.Q' * B ≈ A.Q
 end
 
+@testset "convert between eltypes" begin
+    a = rand(Float64, 10, 5)
+    qra = qr(a)
+    @test Array(LinearAlgebra.QRCompactWY{Float32}(qra.factors, qra.T)) ≈ Array(qr(Float32.(a)))
+    qra = qr(a, ColumnNorm())
+    @test Array(QRPivoted{Float32}(qra.factors, qra.τ, qra.jpvt)) ≈ Array(qr(Float32.(a), ColumnNorm()))
+    a = rand(Float16, 10, 5)
+    qra = qr(a)
+    @test Array(QR{ComplexF16}(qra.factors, qra.τ)) ≈ Array(qr(ComplexF16.(a)))
+end
+
 end # module TestQR
