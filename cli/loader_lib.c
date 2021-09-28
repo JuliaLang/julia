50eebfb56d0b4af6aae27f65e74febaf3ad4b3c9
Jeff Bezanson <jeff.bezanson@gmail.com>
2021-09-28 14:44:33 -0400

    fix #42411, restore export of `jl_options`

--- cli/loader_lib.c
+++ cli/loader_lib.c
@@ -183,6 +183,10 @@ __attribute__((constructor)) void jl_load_libjulia_internal(void) {
         }
         (*jl_exported_func_addrs[symbol_idx]) = addr;
     }
+
+    // jl_options must be initialized very early, in case an embedder sets some
+    // values there before calling jl_init
+    ((void (*)())jl_init_options_addr)();
 }
 
 // Load libjulia and run the REPL with the given arguments (in UTF-8 format)
