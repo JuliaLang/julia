fb42d782cb2cc3968223ba9cca10debe2a99572d
Dilum Aluthge <dilum@aluthge.com>
2021-09-28 00:23:08 -0400

    CI (Buildkite): on pull request builds, build the merge commit (e.g. `refs/pull/1234/merge`)

---
+++ .buildkite/utilities/merge_commit/merge_commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ "${BUILDKITE_PULL_REQUEST:?}" != "false" ]]; then
+    git log -n 1
+    echo "This is a pull request build."
+    echo "Pull request: ${BUILDKITE_PULL_REQUEST:?}"
+    echo "Fetching the pull request merge commit..."
+    sleep 0.01 # Make sure GitHub has enough time to create the merge commit.
+    git fetch origin +refs/pull/${BUILDKITE_PULL_REQUEST:?}/merge
+    echo "Checking out the pull request merge commit..."
+    git checkout --force FETCH_HEAD
+else
+    echo "This is not a pull request build."
+fi
+
+git log -n 1
