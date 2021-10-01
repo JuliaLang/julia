965a2c3eb5bcfd2b3edcfe345040f4111f00bb52
Oscar Smith <oscardssmith@gmail.com>
2021-10-01 01:15:43 -0500

    fix Inf^-1

--- base/math.jl
+++ base/math.jl
@@ -937,7 +937,7 @@ end
     xnlo = ynlo = 0.0
     if n < 0
         rx = inv(x)
-        xnlo = fma(x, rx, -1.) * rx
+        isfinite(x) && (xnlo = fma(x, rx, -1.) * rx)
         x = rx
         n = -n
     end
