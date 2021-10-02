b8e63b9bd73bddbc6ef1362867f870cfaa9c9d80
woclass <git@wo-class.cn>
2021-10-02 23:12:26 +0800

    [linear algebra] Patch Openblas v0.3.10 to fix EXCEPTION_ACCESS_VIOLATION (#42397)
    * [OpenBLAS] cherry pick one patch to fix `zdot` crash
    
    xref issue: JuliaLang/julia#40963
    cherry pick: https://github.com/xianyi/OpenBLAS/commit/b2053239fc36f9ca8c29286d8fc553d0200907b0
    
    * [test/LinearAlgebra] test Issue #40963
    
    * [OpenBLAS] Update version
    
    * [OpenBLAS] Update checksums

--- deps/blas.mk
+++ deps/blas.mk
@@ -103,7 +103,12 @@ $(BUILDDIR)/$(OPENBLAS_SRC_DIR)/openblas-ofast-power.patch-applied: $(BUILDDIR)/
 		patch -p1 -f < $(SRCDIR)/patches/openblas-ofast-power.patch
 	echo 1 > $@
 
-$(BUILDDIR)/$(OPENBLAS_SRC_DIR)/build-configured: $(BUILDDIR)/$(OPENBLAS_SRC_DIR)/openblas-ofast-power.patch-applied
+$(BUILDDIR)/$(OPENBLAS_SRC_DIR)/openblas-julia40963-zdot.patch-applied: $(BUILDDIR)/$(OPENBLAS_SRC_DIR)/openblas-ofast-power.patch-applied
+	cd $(BUILDDIR)/$(OPENBLAS_SRC_DIR) && \
+		patch -p1 -f < $(SRCDIR)/patches/openblas-julia40963-zdot.patch
+	echo 1 > $@
+
+$(BUILDDIR)/$(OPENBLAS_SRC_DIR)/build-configured: $(BUILDDIR)/$(OPENBLAS_SRC_DIR)/openblas-julia40963-zdot.patch-applied
 	echo 1 > $@
 
 $(BUILDDIR)/$(OPENBLAS_SRC_DIR)/build-compiled: $(BUILDDIR)/$(OPENBLAS_SRC_DIR)/build-configured
