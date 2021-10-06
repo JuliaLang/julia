19134592a90330d5856452dbacd3ddb6cb5d6376
Valentin Churavy <v.churavy@gmail.com>
2021-10-05 20:34:07 -0400

    Clang is now installed into usr/tools

--- contrib/asan/build.sh
+++ contrib/asan/build.sh
@@ -44,7 +44,7 @@ make -C "$TOOLCHAIN/deps" install-clang install-llvm-tools
 
 # TODO: https://github.com/JuliaPackaging/Yggdrasil/issues/3359
 rm "$TOOLCHAIN/usr/tools/clang++"
-ln -s "$TOOLCHAIN/usr/bin/clang" "$TOOLCHAIN/usr/tools/clang++"
+ln -s "$TOOLCHAIN/usr/tools/clang" "$TOOLCHAIN/usr/tools/clang++"
 
 echo
 echo "Building Julia..."
