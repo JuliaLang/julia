56000b77ac3c72e36be71c4fd3074c891d2b7b34
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2021-10-08 11:22:46 -0400

    Fix typo with arg

--- base/multidimensional.jl
+++ base/multidimensional.jl
@@ -1958,5 +1958,5 @@ end
 
 getindex(b::Ref, ::CartesianIndex{0}) = getindex(b)
 setindex!(b::Ref, x, ::CartesianIndex{0}) = setindex!(b, x)
-keys(::Ref) = CartesianIndicies(axes(b))
+keys(b::Ref) = CartesianIndicies(axes(b))
 keytype(::Ref) = CartesianIndex{0}
