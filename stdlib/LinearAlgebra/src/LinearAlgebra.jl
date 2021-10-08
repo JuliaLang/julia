146de38b4004e3fbd4cacc1e502fd650473cb67f
Marco <mcognetta@users.noreply.github.com>
2021-10-08 10:48:21 -0500

    Fix edge cases in `SymTridiagonal` when `ev` has an extra element (`+`, `-`, `iszero`, `isone`, etc.) (#42472)

--- stdlib/LinearAlgebra/src/LinearAlgebra.jl
+++ stdlib/LinearAlgebra/src/LinearAlgebra.jl
@@ -264,7 +264,6 @@ end
 
 @noinline throw_uplo() = throw(ArgumentError("uplo argument must be either :U (upper) or :L (lower)"))
 
-
 """
     ldiv!(Y, A, B) -> Y
 
@@ -452,6 +451,12 @@ export ⋅, ×
 ## vectors to matrices.
 _cut_B(x::AbstractVector, r::UnitRange) = length(x)  > length(r) ? x[r]   : x
 _cut_B(X::AbstractMatrix, r::UnitRange) = size(X, 1) > length(r) ? X[r,:] : X
+
+# SymTridiagonal ev can be the same length as dv, but the last element is
+# ignored. However, some methods can fail if they read the entired ev
+# rather than just the meaningful elements. This is a helper function
+# for getting only the meaningful elements of ev. See #41089
+_evview(S::SymTridiagonal) = @view S.ev[begin:length(S.dv) - 1]
 
 ## append right hand side with zeros if necessary
 _zeros(::Type{T}, b::AbstractVector, n::Integer) where {T} = zeros(T, max(length(b), n))
