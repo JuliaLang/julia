a935c7ac5cebee411c9ea198177d0700fe89e55b
Takafumi Arakaki <aka.tkf@gmail.com>
2021-10-11 01:40:56 -0400

    Revert "Further simplify llvmcall"
    This reverts commit fcae973c339646fa7727be907b900977d65dc6fc.

--- test/backtrace.jl
+++ test/backtrace.jl
@@ -268,7 +268,8 @@ NOTE: `f` and all functions reachable from `f` must not contain a yield point.
 """
 function withalloca end
 
-function withalloca_wrapper(f, int)
+function withalloca_wrapper(fptr, int)
+    f = unsafe_pointer_to_objref(Ptr{Cvoid}(fptr))
     f(Ptr{Cvoid}(int))
     nothing
 end
@@ -281,8 +282,10 @@ end
             top:
                 %aptr = alloca i8, i$(Sys.WORD_SIZE) %2
                 %aint = ptrtoint i8* %aptr to i$(Sys.WORD_SIZE)
-                %wptr = inttoptr i$(Sys.WORD_SIZE) %0 to void ({}*, i$(Sys.WORD_SIZE))*
-                call void %wptr({}* %1, i$(Sys.WORD_SIZE) %aint)
+                %wptr = inttoptr i$(Sys.WORD_SIZE) %0 to void (i$(Sys.WORD_SIZE),
+                                                                i$(Sys.WORD_SIZE))*
+                %fptr = ptrtoint {}* %1 to i$(Sys.WORD_SIZE)
+                call void %wptr(i$(Sys.WORD_SIZE) %fptr, i$(Sys.WORD_SIZE) %aint)
                 ret void
             }
             """,
@@ -292,7 +295,7 @@ end
         Tuple{Ptr{Cvoid},Any,Int},
         Base.unsafe_convert(
             Ptr{Cvoid},
-            @cfunction(withalloca_wrapper, Cvoid, (Any,UInt)),
+            @cfunction(withalloca_wrapper, Cvoid, (UInt,UInt)),
         ),
         f,
         nbytes,
