39a08a16b13ec50b9d5193de2ac137fa415d5bb9
Steven G. Johnson <stevenj@alum.mit.edu>
2021-10-10 20:45:00 -0400

    put Unicode.julia_chartransform in the manual

--- stdlib/Unicode/src/Unicode.jl
+++ stdlib/Unicode/src/Unicode.jl
@@ -5,7 +5,7 @@ module Unicode
 export graphemes, isequal_normalized
 
 """
-    julia_chartransform(c::Union{Char,Integer})
+    Unicode.julia_chartransform(c::Union{Char,Integer})
 
 Map the Unicode character (`Char`) or codepoint (`Integer`) `c` to the corresponding
 "equivalent" character or codepoint, respectively, according to the custom equivalence
@@ -20,6 +20,24 @@ julia> julia_chartransform('\u00B5')
 
 julia> julia_chartransform('x')
 'x': ASCII/Unicode U+0078 (category Ll: Letter, lowercase)
+```
+
+The `julia_chartransform` is mainly useful for passing to the [`Unicode.normalize`](@ref)
+function in order to mimic the normalization used by the Julia parser:
+```jl
+julia> s = "\u00B5o\u0308"
+"µö"
+
+julia> s2 = Unicode.normalize(s, compose=true, stable=true, chartransform=Unicode.julia_chartransform)
+"μö"
+
+julia> collect(s2)
+2-element Vector{Char}:
+ 'μ': Unicode U+03BC (category Ll: Letter, lowercase)
+ 'ö': Unicode U+00F6 (category Ll: Letter, lowercase)
+
+ julia> s2 == string(Meta.parse(s))
+true
 ```
 
 !!! compat "Julia 1.8"
