c733cd6f16a7ff59a9ae93ea08a4f7f8d88565b5
Pete Vilter <7341+vilterp@users.noreply.github.com>
2021-10-23 11:36:24 -0400

    record freed values

--- src/gc-alloc-profiler.h
+++ src/gc-alloc-profiler.h
@@ -16,6 +16,7 @@ JL_DLLEXPORT void jl_start_alloc_profile(int skip_every);
 JL_DLLEXPORT void jl_stop_and_write_alloc_profile(ios_t *stream);
 
 void _record_allocated_value(jl_value_t *val, size_t size);
+void _record_freed_value(jl_taggedvalue_t *tagged_val);
 
 // ---------------------------------------------------------------------
 // functions to call from GC when alloc profiling is enabled
@@ -26,6 +27,12 @@ extern int g_alloc_profile_enabled;
 static inline void record_allocated_value(jl_value_t *val, size_t size) {
     if (__unlikely(g_alloc_profile_enabled)) {
         _record_allocated_value(val, size);
+    }
+}
+
+static inline void record_freed_value(jl_taggedvalue_t *tagged_val) {
+    if (__unlikely(g_alloc_profile_enabled != 0)) {
+        _record_freed_value(tagged_val);
     }
 }
 
