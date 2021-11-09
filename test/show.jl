648bcfd61c3c29852ec033d395d4e7a4d026a546
woclass <git@wo-class.cn>
2021-11-09 13:10:24 +0800

    [base/show] test if branch

--- test/show.jl
+++ test/show.jl
@@ -1348,9 +1348,15 @@ test_repr("(:).a")
 @test repr(Tuple{Float32, Float32, Float32}) == "Tuple{Float32, Float32, Float32}"
 
 @testset "issue #42931" begin
-    @test repr(NTuple{4, :sym}) == "NTuple{4, :sym}"
-    @test repr(NTuple{3, :sym}) == "Tuple{:sym, :sym, :sym}"
-    @test repr(Tuple{:A, :B}) == "Tuple{:A, :B}"
+    @test repr(NTuple{4, :A}) == "NTuple{4, :A}"
+    @test repr(NTuple{3, :A}) == "Tuple{:A, :A, :A}"
+    @test repr(NTuple{2, :A}) == "Tuple{:A, :A}"
+    @test repr(NTuple{1, :A}) == "Tuple{:A}"
+    @test repr(NTuple{0, :A}) == "Tuple{}"
+
+    @test repr(Tuple{:A, :A, :A, :B}) == "Tuple{:A, :A, :A, :B}"
+    @test repr(Tuple{:A, :A, :A, :A}) == "NTuple{4, :A}"
+    @test repr(Tuple{:A, :A, :A}) == "Tuple{:A, :A, :A}"
     @test repr(Tuple{:A}) == "Tuple{:A}"
     @test repr(Tuple{}) == "Tuple{}"
 end
