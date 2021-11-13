9d434693adc293929efcc20dff8340ec85fff5a2
Takafumi Arakaki <aka.tkf@gmail.com>
2021-11-12 20:56:17 -0500

    Mention that it is supported only in Unix

--- base/libc.jl
+++ base/libc.jl
@@ -407,6 +407,8 @@ srand(seed=floor(Int, time()) % Cuint) = ccall(:srand, Cvoid, (Cuint,), seed)
 
 Make a FIFO special file (a named pipe) at `path`.  Return `path` as-is on success.
 
+`mkfifo` is supported only in Unix platforms.
+
 !!! compat "Julia 1.8"
     `mkfifo` requires at least Julia 1.8.
 """
