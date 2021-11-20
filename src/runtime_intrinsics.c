06d53085f8dc5ddf35e2454841ba74db1555976a
Alex Arslan <ararslan@comcast.net>
2021-11-20 13:34:33 -0800

    Avoid `-Wunused-function` on macOS for some `Float16` intrinsics (#43174)
    We're defining `half_to_float` and `float_to_half` unconditionally but
    only ever calling them on non-Mac platforms, so Clang produces unused
    function warnings during the build on macOS. To fix this, we can just
    move the platform check to encompass the definitions in addition to the
    uses.

--- src/runtime_intrinsics.c
+++ src/runtime_intrinsics.c
@@ -15,7 +15,9 @@
 const unsigned int host_char_bit = 8;
 
 // float16 intrinsics
-// TODO: use LLVM's compiler-rt
+// TODO: use LLVM's compiler-rt on all platforms (Xcode already links compiler-rt)
+
+#if !defined(_OS_DARWIN_)
 
 static inline float half_to_float(uint16_t ival) JL_NOTSAFEPOINT
 {
@@ -185,8 +187,6 @@ static inline uint16_t float_to_half(float param) JL_NOTSAFEPOINT
     }
     return h;
 }
-
-#if !defined(_OS_DARWIN_)   // xcode already links compiler-rt
 
 JL_DLLEXPORT float __gnu_h2f_ieee(uint16_t param)
 {
