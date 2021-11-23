e7ced9b1acb090a0f2960beb2ed30c64939adaaf
Oscar Smith <oscardssmith@gmail.com>
2021-11-23 17:13:51 -0500

    make sure `norm` always returns a float.

--- stdlib/LinearAlgebra/src/generic.jl
+++ stdlib/LinearAlgebra/src/generic.jl
@@ -449,11 +449,11 @@ diag(A::AbstractVector) = throw(ArgumentError("use diagm instead of diag to cons
 # Dot products and norms
 
 # special cases of norm; note that they don't need to handle isempty(x)
-generic_normMinusInf(x) = mapreduce(norm, min, x)
+generic_normMinusInf(x) = float(mapreduce(norm, min, x))
 
-generic_normInf(x) = mapreduce(norm, max, x)
+generic_normInf(x) = float(mapreduce(norm, max, x))
 
-generic_norm1(x) = mapreduce(norm, +, x)
+generic_norm1(x) = float(mapreduce(norm, +, x))
 
 # faster computation of norm(x)^2, avoiding overflow for integers
 norm_sqr(x) = norm(x)^2
