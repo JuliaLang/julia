2bb36fb04f797174b16f0845efc88ef7921f4b6e
freemin7 <25376460+freemin7@users.noreply.github.com>
2021-11-29 22:27:37 +0100

    Add Module DocString for Base.Checked
    Thanks to Pramodh Gopalan for feedback.

--- base/checked.jl
+++ base/checked.jl
@@ -3,6 +3,13 @@
 # Support for checked integer arithmetic
 
 module Checked
+"""
+    Checked
+The Checked module provides arithmetic functions for the built-in signed and unsigned
+Integer types which throw an error when an overflow occurs. They are named like `checked_sub`,
+`checked_div`, etc. In addition, `add_with_overflow, sub_with_overflow, mul_with_overflow`
+return both the unchecked results and a boolean value denoting the presence of an overflow.
+"""
 
 export checked_neg, checked_abs, checked_add, checked_sub, checked_mul,
        checked_div, checked_rem, checked_fld, checked_mod, checked_cld,
