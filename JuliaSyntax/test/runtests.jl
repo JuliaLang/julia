bafb771ca3a69cb3165b50cdc73bb2b46e50ae92
Chris Foster <chris42f@gmail.com>
2021-11-30 23:03:40 +1000

    Use case: show how SyntaxNode works for macro expansion
    This shows that SyntaxNode works nicely for simple macros based on
    interpolating expressions into one another. In particular it shows how
    precise source information from multiple files can coexist within the
    same syntax tree.

--- JuliaSyntax/test/runtests.jl
+++ JuliaSyntax/test/runtests.jl
@@ -65,17 +65,95 @@ show(stdout, MIME"text/plain"(), t, code, show_trivia=true)
 println("\nSyntaxNode")
 
 # And the following AST
-s = SyntaxNode(source, t, 1)
+s = SyntaxNode(source, t)
 
 #code = "42"
 #SyntaxNode(N(K"Integer", 2), 1, code)
 
-# Simulate the following Undescores.jl - like macro:
+#-------------------------------------------------------------------------------
+# The following shows that SyntaxNode works nicely for simple macros based on
+# interpolating expressions into one another. In particular it shows how
+# precise source information from multiple files can coexist within the same
+# syntax tree.
 
-# @U f(x+_, y)
-#   ↦
-# f(_1 -> x+_1, y)
+# First, here's the functionality that we're going to implement as a normal
+# Julia macro. It's similar to the standard @show macro.
+macro show2(ex)
+    name = sprint(Base.show_unquoted, ex)
+    quote
+        value = $(esc(ex))
+        println($name, " = ", value)
+        value
+    end
+end
 
-# macro U(ex)
-# end
+# Now, how would this be implemented if we were to do it with SyntaxNode?
+# We don't have a parser which is capable of producing our tree structures yet,
+# so we need to hand construct all our trees.
+function at_show2(ex::SyntaxNode)
+    code = String(read(@__FILE__))
+    name = sprint(JuliaSyntax._show_syntax_node_compact, ex)
+    quote_begin = (@__LINE__) + 1
+    quote
+        value = $ex
+        println($name, " = ", value)
+        value
+    end
+    raw = N(K"block",
+            T(K"quote", 5),
+            T(K"\n", 9),
+            N(K"=",
+              N(K"Identifier", 5),
+              T(K" ", 1),
+              T(K"=", 1),
+              T(K" ", 1),
+              N(K"$",
+                T(K"$", 1),
+                N(K"Identifier", 2)),
+              T(K"\n", 9)),
+            N(K"call",
+              N(K"Identifier", 7),
+              T(K"(", 1),
+              N(K"$",
+                T(K"$", 1),
+                N(K"Identifier", 4)),
+              T(K",", 1),
+              T(K" ", 1),
+              N(K"String", 5),
+              T(K",", 1),
+              T(K" ", 1),
+              N(K"Identifier", 5),
+              T(K")", 1)),
+            T(K"\n", 9),
+            N(K"Identifier", 5),
+            T(K"\n", 5),
+            T(K"end", 3))
+    source = SourceFile(code, filename=@__FILE__)
+    block = SyntaxNode(source, raw, source.line_starts[quote_begin]+4)
+    # Now that we have the block, we need to interpolate into it.
 
+    # Inserting a SyntaxNode `ex` is simple:
+    block.val[1].val[2] = ex
+    # The interpolation of a Julia *value* should inherit the source location
+    # of the $ interpolation expression. This is different to the
+    # interpolation of a SyntaxNode, which should just be inserted as-is.
+    block.val[2].val[2] = JuliaSyntax.interpolate_literal(block.val[2].val[2], name)
+    block
+end
+
+# Usage of at_show2()
+
+# Let's have some simple expression to pass to at_show2. This will be
+# attributed to a different file foo.jl
+code2 = "foo + 42"
+source2 = SourceFile(code2, filename="foo.jl")
+s2 = SyntaxNode(source2, NI(K"call",
+                            N(K"Identifier", 3),
+                            T(K" ", 1),
+                            N(K"+", 1),
+                            T(K" ", 1),
+                            N(K"Integer", 2)))
+
+# Calling at_show2, we see that the precise source information is preserved for
+# both the surrounding expression and the interpolated fragments.
+show(stdout, MIME"text/plain"(), at_show2(s2))
