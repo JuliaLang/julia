c736dbc6a71c76876b794c68f3f2a8d4dc6d9f0d
Nathan Daly <nhdaly@gmail.com>
2021-12-10 17:44:54 -0500

    Mark the AllocProfile C code as `JL_NOTSAFEPOINT`
    TODO: is this correct!?

--- src/gc-alloc-profiler.h
+++ src/gc-alloc-profiler.h
@@ -28,14 +28,15 @@ struct RawAllocResults {
     size_t num_frees;
 };
 
-void _report_gc_started(void);
-void _report_gc_finished(uint64_t pause, uint64_t freed, uint64_t allocd);
+// TODO(PR): Is this correct? Are these JL_NOTSAFEPOINT?
+void _report_gc_started(void) JL_NOTSAFEPOINT;
+void _report_gc_finished(uint64_t pause, uint64_t freed, uint64_t allocd) JL_NOTSAFEPOINT;
 JL_DLLEXPORT void jl_start_alloc_profile(int skip_every);
 JL_DLLEXPORT struct RawAllocResults jl_stop_alloc_profile(void);
 JL_DLLEXPORT void jl_free_alloc_profile(void);
 
-void _record_allocated_value(jl_value_t *val, size_t size);
-void _record_freed_value(jl_taggedvalue_t *tagged_val);
+void _record_allocated_value(jl_value_t *val, size_t size) JL_NOTSAFEPOINT;
+void _record_freed_value(jl_taggedvalue_t *tagged_val) JL_NOTSAFEPOINT;
 
 // ---------------------------------------------------------------------
 // functions to call from GC when alloc profiling is enabled
@@ -43,13 +44,13 @@ void _record_freed_value(jl_taggedvalue_t *tagged_val);
 
 extern int g_alloc_profile_enabled;
 
-static inline void record_allocated_value(jl_value_t *val, size_t size) {
+static inline void record_allocated_value(jl_value_t *val, size_t size) JL_NOTSAFEPOINT {
     if (__unlikely(g_alloc_profile_enabled)) {
         _record_allocated_value(val, size);
     }
 }
 
-static inline void record_freed_value(jl_taggedvalue_t *tagged_val) {
+static inline void record_freed_value(jl_taggedvalue_t *tagged_val) JL_NOTSAFEPOINT {
     if (__unlikely(g_alloc_profile_enabled != 0)) {
         _record_freed_value(tagged_val);
     }
