b38a5d0f825be1dd61db283218620ea6de92b341
Pete Vilter <pete.vilter@gmail.com>
2021-12-17 21:37:08 -0500

    put test back in; get it passing

--- stdlib/AllocProfile/test/runtests.jl
+++ stdlib/AllocProfile/test/runtests.jl
@@ -1,8 +1,21 @@
+# TODO: register AllocProfile in the stdlib
+using Pkg; Pkg.activate("stdlib/AllocProfile")
+
+using Test
+
 using AllocProfile
 
-AllocProfile.start()
+@testset "alloc profiler doesn't segfault" begin
+    AllocProfile.start()
 
-using Base64
+    # test the allocations during compilation
+    using Base64
 
-raw_results = AllocProfile.stop()
+    results = AllocProfile.stop()
 
+    @test length(results.allocs) > 0
+    first_alloc = results.allocs[1]
+    @test first_alloc.size > 0
+    @test length(first_alloc.stacktrace) > 0
+    @test length(string(first_alloc.type)) > 0
+end
