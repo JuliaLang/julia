c06b479ac813c685ea1a426aec944c087e11b827
Tanay Sharma <190050122@iitb.ac.in>
2021-12-19 13:08:28 +0530

    Update base/rational.jl
    Co-authored-by: Jeff Bezanson <jeff.bezanson@gmail.com>

--- base/rational.jl
+++ base/rational.jl
@@ -221,7 +221,7 @@ rationalize(x::Complex; kvs...) = Complex(rationalize(Int, x.re; kvs...), ration
 rationalize(::Type{T}, x::Rational, tol::Real) where {T<:Integer} = rationalize(T, float(x), tol)::Rational{T}
 rationalize(::Type{T}, x::Rational; tol::Real = zero(x)) where {T<:Integer} = rationalize(T, float(x), tol)::Rational{T}
 rationalize(x::Rational{T}; kvs...) where{T} = rationalize(T, x; kvs...)
-rationalize(::Type{T}, x::Integer, tol::Real) where {T<:Integer} = rationalize(T, Rational(x, 1), tol)
+rationalize(::Type{T}, x::Integer, tol::Real) where {T<:Integer} = Rational{T}(x)
 rationalize(::Type{T}, x::Integer; tol::Real = 0) where {T<:Integer} = Rational(x, 1)
 rationalize(x::Integer; kvs...) = Rational(x)
 
