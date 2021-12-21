b5ab61185a450f01d082c1a8785d95a7802f72c1
Pete Vilter <pete.vilter@gmail.com>
2021-12-21 11:31:09 -0500

    test that we get a free

--- stdlib/AllocProfile/test/runtests.jl
+++ stdlib/AllocProfile/test/runtests.jl
@@ -10,13 +10,17 @@ using AllocProfile
 
     # test the allocations during compilation
     using Base64
+    # make sure some frees show up in the profile
+    GC.gc()
 
     results = AllocProfile.stop()
-    # AllocProfile.clear()
+    AllocProfile.clear()
 
     @test length(results.allocs) > 0
     first_alloc = results.allocs[1]
     @test first_alloc.size > 0
     @test length(first_alloc.stacktrace) > 0
     @test length(string(first_alloc.type)) > 0
+    
+    @test length(results.frees) > 0
 end
