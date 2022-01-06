2c1c226e56d50c392aeab778170a4069a04e7a6c
Claire Foster <aka.c42f@gmail.com>
2022-01-06 22:53:17 +1000

    Add self-parsing of test code as an additional test

--- JuliaSyntax/test/self_parse.jl
+++ JuliaSyntax/test/self_parse.jl
@@ -7,3 +7,12 @@
               JuliaSyntax.remove_linenums!(JuliaSyntax.flisp_parse_all(code))
     end
 end
+
+@testset "JuliaSyntax self-parsing tests" begin
+    testdir = @__DIR__
+    @testset "Parse $(joinpath("test",f))" for f in readdir(testdir)
+        code = read(joinpath(testdir, f), String)
+        @test JuliaSyntax.remove_linenums!(JuliaSyntax.parse_all(Expr, code)) == 
+              JuliaSyntax.remove_linenums!(JuliaSyntax.flisp_parse_all(code))
+    end
+end
