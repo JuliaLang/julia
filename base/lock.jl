628bf890d78235f318d8ee785e24ac36651df28b
Ian Butterworth <i.r.butterworth@gmail.com>
2022-01-09 20:59:33 -0500

    add do-block syntax for Semaphore acquire

--- base/lock.jl
+++ base/lock.jl
@@ -315,6 +315,36 @@ function acquire(s::Semaphore)
 end
 
 """
+    acquire(f::Function, s::Semaphore)
+
+Execute `f` after acquiring from Semaphore `s`,
+and `release` on completion or error.
+
+For example, a do-block form that ensures only 2
+calls of `foo` will be active at the same time:
+
+```julia
+s = Base.Semaphore(2)
+@sync for _ in 1:100
+    Threads.@spawn begin
+        Base.acquire(s) do
+            foo()
+        end
+    end
+end
+```
+"""
+function acquire(f::Function, s::Semaphore)
+    acquire(s)
+    try
+        f()
+    finally
+        release(s)
+    end
+    return
+end
+
+"""
     release(s::Semaphore)
 
 Return one permit to the pool,
