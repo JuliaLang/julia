5ddfd5987dfbaa4e8397378dec75d1aa129eb164
Dilum Aluthge <dilum@aluthge.com>
2022-01-17 03:49:31 -0500

    Update InteractiveUtils.jl

--- stdlib/InteractiveUtils/src/InteractiveUtils.jl
+++ stdlib/InteractiveUtils/src/InteractiveUtils.jl
@@ -85,10 +85,6 @@ controlled with boolean keyword arguments:
 
 - `verbose`: print all additional information
 
-!!! warning "Warning"
-    The output of this function may contain sensitive information. Before sharing the output,
-    please review the output and remove any data that should not be shared publicly.
-
 See also: [`VERSION`](@ref).
 """
 function versioninfo(io::IO=stdout; verbose::Bool=false)
