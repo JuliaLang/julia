99ea4b058b9e728b6695261974b5264fa9dcd09a
Bogumił Kamiński <bkamins@sgh.waw.pl>
2022-01-18 19:33:12 +0100

    Add defalg methods to resolve potential dispatch ambiguities (#43426)
    We could also consider adding `defalg(v::AbstractArray{<:Number}) = DEFAULT_UNSTABLE`,
    but it is unlikely that someone will want to do `Union` of `<:Number`
    and something other than `Missing` that still would support comparison.
    
    Relevant for https://github.com/JuliaStrings/InlineStrings.jl/issues/21
    (and other custom types that will want to add `defalg` support allowing
    for `Union` with `Missing`).
    
    (cherry picked from commit eb724e0065bdcc50da103d00f4a9e98757cdb5d7)

--- base/sort.jl
+++ base/sort.jl
@@ -651,6 +651,8 @@ end
 
 defalg(v::AbstractArray) = DEFAULT_STABLE
 defalg(v::AbstractArray{<:Union{Number, Missing}}) = DEFAULT_UNSTABLE
+defalg(v::AbstractArray{Missing}) = DEFAULT_UNSTABLE
+defalg(v::AbstractArray{Union{}}) = DEFAULT_UNSTABLE
 
 function sort!(v::AbstractVector, alg::Algorithm, order::Ordering)
     inds = axes(v,1)
