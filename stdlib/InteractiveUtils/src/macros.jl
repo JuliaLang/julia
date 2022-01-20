f6ea7cc5f2e6cb1403c5462bd9df9e4961c1e662
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2022-01-20 12:29:17 +0100

    Elaborate `code_native` funciton and macro docstring (#43860)

--- stdlib/InteractiveUtils/src/macros.jl
+++ stdlib/InteractiveUtils/src/macros.jl
@@ -336,11 +336,17 @@ by putting them and their value before the function call, like this:
 Evaluates the arguments to the function or macro call, determines their types, and calls
 [`code_native`](@ref) on the resulting expression.
 
-Set the optional keyword argument `debuginfo` by putting it before the function call, like this:
+Set any of the optional keyword arguments `syntax`, `debuginfo`, `binary` or `dump_module`
+by putting it before the function call, like this:
 
-    @code_native debuginfo=:default f(x)
+    @code_native syntax=:intel debuginfo=:default binary=true dump_module=false f(x)
 
-`debuginfo` may be one of `:source` (default) or `:none`, to specify the verbosity of code comments.
+* Set assembly syntax by setting `syntax` to `:att` (default) for AT&T syntax or `:intel` for Intel syntax.
+* Specify verbosity of code comments by setting `debuginfo` to `:source` (default) or `:none`.
+* If `binary` is `true`, also print the binary machine code for each instruction precedented by an abbreviated address.
+* If `dump_module` is `false`, do not print metadata such as rodata or directives.
+
+See also: [`code_native`](@ref), [`@code_llvm`](@ref), [`@code_typed`](@ref) and [`@code_lowered`](@ref)
 """
 :@code_native
 
