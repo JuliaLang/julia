42c052afafbe0245b666b4fc35f010f4281c99db
Claire Foster <aka.c42f@gmail.com>
2022-01-21 12:06:59 +1000

    Basic tools for reducing test cases
    This includes some simple tooling for test case reduction. This isn't
    very precise yet and in various circumstances still can't reduce the
    test case down from the whole file.

--- JuliaSyntax/test/parse_packages.jl
+++ JuliaSyntax/test/parse_packages.jl
@@ -6,16 +6,25 @@
     test_parse_all_in_path(joinpath(pkgdir, "test"))
 end
 
-@testset "Parse Base" begin
-    test_parse_all_in_path(joinpath(Sys.BINDIR, Base.DATAROOTDIR, "julia", "base"))
+base_path = joinpath(Sys.BINDIR, Base.DATAROOTDIR, "julia", "base") 
+@testset "Parse Base at $base_path" begin
+    test_parse_all_in_path(base_path)
 end
 
 #=
-@testset "Parse Base tests" begin
-    test_parse_all_in_path(joinpath(Sys.BINDIR, Base.DATAROOTDIR, "julia", "test"))
+base_tests_path = joinpath(Sys.BINDIR, Base.DATAROOTDIR, "julia", "test")
+@testset "Parse Base tests at $base_tests_path" begin
+    test_parse_all_in_path(base_tests_path)
+end
+
+@testset "Parse Julia stdlib at $(Sys.STDLIB)" begin
+    for stdlib in readdir(Sys.STDLIB)
+        fulldir = joinpath(Sys.STDLIB, stdlib)
+        if isdir(fulldir)
+            @testset "Parse $stdlib" begin
+                test_parse_all_in_path(joinpath(Sys.STDLIB, fulldir))
+            end
+        end
+    end
 end
 =#
-
-@testset "Parse Julia stdlib" begin
-    test_parse_all_in_path(joinpath(Sys.BINDIR, Base.DATAROOTDIR, "julia", "stdlib"))
-end
