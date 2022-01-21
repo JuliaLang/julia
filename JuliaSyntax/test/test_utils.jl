c44148075d03db0993b73adc25058b147d326567
Chris Foster <chris42f@gmail.com>
2022-01-21 12:06:59 +1000

    Basic tools for reducing test cases
    This includes some simple tooling for test case reduction. This isn't
    very precise yet and in various circumstances still can't reduce the
    test case down from the whole file.

--- JuliaSyntax/test/test_utils.jl
+++ JuliaSyntax/test/test_utils.jl
@@ -15,14 +15,32 @@ using JuliaSyntax:
     # Node inspection
     kind,
     flags,
+    is_trivia,
+    sourcetext,
     haschildren,
     children,
-    child
+    child,
+    flisp_parse_all
+
+function remove_macro_linenums!(ex)
+    if Meta.isexpr(ex, :macrocall)
+        ex.args[2] = nothing
+    end
+    if ex isa Expr
+        map!(remove_macro_linenums!, ex.args, ex.args)
+    end
+    return ex
+end
+
+function remove_all_linenums!(ex)
+    JuliaSyntax.remove_linenums!(ex)
+    remove_macro_linenums!(ex)
+end
 
 function parsers_agree_on_file(path)
     code = read(path, String)
-    JuliaSyntax.remove_linenums!(JuliaSyntax.parse_all(Expr, code)) == 
-    JuliaSyntax.remove_linenums!(JuliaSyntax.flisp_parse_all(code))
+    JuliaSyntax.remove_linenums!(parse_all(Expr, code)) == 
+    JuliaSyntax.remove_linenums!(flisp_parse_all(code))
 end
 
 function find_source_in_path(basedir)
@@ -41,7 +59,65 @@ function test_parse_all_in_path(basedir)
     end
 end
 
-# Version of test_parse for interactive exploration
+#-------------------------------------------------------------------------------
+# Test case reduction
+
+# Check whether a given SyntaxNode converts to the same Expr as the flisp
+# parser produces from the source text of the node.
+function equals_flisp_parse(tree)
+    node_text = sourcetext(tree)
+    fl_ex = kind(tree) == K"toplevel" ?
+        flisp_parse_all(node_text) :
+        Meta.parse(node_text, raise=false)
+    if Meta.isexpr(fl_ex, :error)
+        return true  # Something went wrong in reduction; ignore these cases 😬
+    end
+    remove_all_linenums!(Expr(tree)) == remove_all_linenums!(fl_ex)
+end
+
+"""
+Select a subtree of `tree` which is inconsistent between flisp and JuliaSyntax
+parsers. This isn't very precise yet!
+
+TODO:
+* For some syntax elements (eg, the `x in xs` inside `for x in xs`) the
+  children can't be parsed out of context. Fix this.
+* Replace good siblings of bad nodes with placeholders. For blocks, delete such
+  siblings.
+"""
+function reduce_test(tree)
+    if equals_flisp_parse(tree)
+        return nothing
+    end
+    if !haschildren(tree)
+        return tree
+    else
+        subtrees = []
+        for child in children(tree)
+            if is_trivia(child) || !haschildren(child)
+                continue
+            end
+            t = reduce_test(child)
+            if !isnothing(t)
+                push!(subtrees, t)
+            end
+        end
+        if length(subtrees) == 1
+            return only(subtrees)
+        end
+    end
+    return tree
+end
+
+
+#-------------------------------------------------------------------------------
+"""
+    itest_parse(production, code; julia_version::VersionNumber=v"1.6")
+
+Parse `code`, entering the recursive descent parser at the given function
+`production`. This function shows the various tree representations on stdout
+for debugging.
+"""
 function itest_parse(production, code; julia_version::VersionNumber=v"1.6")
     stream = ParseStream(code)
     production(JuliaSyntax.ParseState(stream, julia_version))
