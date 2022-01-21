e06e9dc710458ec22411f8a7123ed3cac806afc5
Gabriel Baraldi <baraldigabriel@gmail.com>
2022-01-21 14:56:47 -0300

    I have no idea why it fails whitespace

--- base/twiceprecision.jl
+++ base/twiceprecision.jl
@@ -249,11 +249,9 @@ function twiceprecision(val::TwicePrecision{T}, nb::Integer) where {T<:IEEEFloat
     TwicePrecision{T}(hi, (val.hi - hi) + val.lo)
 end
 
-
 nbitslen(r::StepRangeLen) = nbitslen(eltype(r), length(r), r.offset)
 nbitslen(::Type{T}, len, offset) where {T<:IEEEFloat} =
     min(cld(precision(T), 2), nbitslen(len, offset))
-
 # The +1 here is for safety, because the precision of the significand
 # is 1 bit higher than the number that are explicitly stored.
 nbitslen(len, offset) = len < 2 ? 0 : ceil(Int, log2(max(offset-1, len-offset))) + 1
