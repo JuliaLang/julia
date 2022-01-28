9749ec5779f33492a3b4c0ba990df3c67ea3c5cd
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2022-01-28 09:09:27 +0100

    Update base/strings/string.jl
    Co-authored-by: Takafumi Arakaki <takafumi.a@gmail.com>

--- base/strings/string.jl
+++ base/strings/string.jl
@@ -36,7 +36,8 @@ The default string type in Julia, used by e.g. string literals.
 
 `String`s are immutable sequences of `Char`s. A `String` is stored internally as
 a contiguous byte array, and while they are interpreted as being UTF-8 encoded,
-they can be composed of any byte sequence.
+they can be composed of any byte sequence. Use [`isvalid`](@ref) to validate
+that the underlying byte sequence is valid as UTF-8.
 This representation often makes `String` appropriate for passing strings to C.
 """
 String
