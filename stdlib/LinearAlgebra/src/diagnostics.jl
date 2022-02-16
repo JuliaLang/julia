20a7d1d7e7fce1091a2b3e36ac386dbf4ac9d772
Dilum Aluthge <dilum@aluthge.com>
2022-02-16 04:05:14 -0500

    Print the number of BLAS threads

--- stdlib/LinearAlgebra/src/diagnostics.jl
+++ stdlib/LinearAlgebra/src/diagnostics.jl
@@ -1,5 +1,7 @@
 # LinearAlgebra.__diagnostics__()
 function __diagnostics__(io::IO)
     versioninfo(io) # LinearAlgebra.versioninfo()
+    blas_threads = BLAS.get_num_threads()
+    println(io, "LinearAlgebra.BLAS.get_num_threads() = ", blas_threads)
     return nothing
 end
