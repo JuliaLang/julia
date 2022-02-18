5fd8e421bcf7dfd000b00a6d0b43b7e9a5b00754
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2022-02-17 20:14:17 -0600

    Try to fix build error (4)

--- base/sort.jl
+++ base/sort.jl
@@ -1195,8 +1195,8 @@ end
 ## sorting serialization to alow radix sorting primitives other than UInts ##
 module Serial
 using ...Order
-using ..Base: @inbounds, min, max, AbstractVector, nothing, something, signed, unsigned,
-    typemin, xor, reinterpret, isbitstype, Signed, Unsigned, Some, Type, eltype
+using ..Base: @inbounds, min, max, AbstractVector, nothing, signed, unsigned,
+    typemin, xor, reinterpret, isbitstype, Signed, Unsigned, Type, eltype
 
 """
     Serializable(T::Type, order::Ordering)
