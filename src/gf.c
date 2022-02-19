d6ebcab09163d9e48c6c5eb19b09098aefa14626
Ian Butterworth <i.r.butterworth@gmail.com>
2022-02-19 09:25:03 -0500

    only lock if threading is possible

--- src/gf.c
+++ src/gf.c
@@ -1982,7 +1982,8 @@ static void record_precompile_statement(jl_method_instance_t *mi)
     if (!jl_is_method(def))
         return;
 
-    JL_LOCK(&precomp_statement_out_lock);
+    if (jl_n_threads > 1)
+        JL_LOCK(&precomp_statement_out_lock);
     if (s_precompile == NULL) {
         const char *t = jl_options.trace_compile;
         if (!strncmp(t, "stderr", 6)) {
@@ -2001,7 +2002,8 @@ static void record_precompile_statement(jl_method_instance_t *mi)
         if (s_precompile != JL_STDERR)
             ios_flush(&f_precompile);
     }
-    JL_UNLOCK(&precomp_statement_out_lock);
+    if (jl_n_threads > 1)
+        JL_UNLOCK(&precomp_statement_out_lock);
 }
 
 jl_code_instance_t *jl_compile_method_internal(jl_method_instance_t *mi, size_t world)
