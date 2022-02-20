32e822dc5cf40e43dbe3dc7e6692a491774dd6ee
Chris Foster <chris42f@gmail.com>
2022-02-20 13:51:38 +1000

    Improvements to core parsing hooks (JuliaLang/JuliaSyntax.jl#12)
    * Make ParseError work with Meta.parse()
    * Make incremental parsing of statements work
    * Add some basic tests

--- JuliaSyntax/src/hooks.jl
+++ JuliaSyntax/src/hooks.jl
@@ -1,5 +1,5 @@
 # Adaptor for the API/ABI expected by the Julia runtime code.
-function core_parser_hook(code, filename, offset, options)
+function core_parser_hook(code, filename, lineno, offset, options)
     try
         # TODO: Check that we do all this input wrangling without copying the
         # code buffer
@@ -13,19 +13,32 @@ function core_parser_hook(code, filename, offset, options)
 
         stream = ParseStream(io)
         rule = options === :all ? :toplevel : options
+        if rule !== :toplevel
+            # To copy the flisp parser driver, we ignore leading trivia when
+            # parsing statements or atoms
+            bump_trivia(stream)
+        end
         JuliaSyntax.parse(stream; rule=rule)
 
-        ex = if any_error(stream)
+        if any_error(stream)
             e = Expr(:error, ParseError(SourceFile(code), stream.diagnostics))
-            options === :all ? Expr(:toplevel, e) : e
+            ex = options === :all ? Expr(:toplevel, e) : e
         else
-            build_tree(Expr, stream)
+            ex = build_tree(Expr, stream, wrap_toplevel_as_kind=K"None")
+            if Meta.isexpr(ex, :None)
+                # The None wrapping is only to give somewhere for trivia to be
+                # attached; unwrap!
+                ex = only(ex.args)
+            end
         end
 
-        pos = last_byte(stream) - 1
+        # Note the next byte in 1-based indexing is `last_byte(stream) + 1` but
+        # the Core hook must return an offset (ie, it's 0-based) so the factors
+        # of one cancel here.
+        last_offset = last_byte(stream)
 
         # Rewrap result in an svec for use by the C code
-        return Core.svec(ex, pos)
+        return Core.svec(ex, last_offset)
     catch exc
         @error("JuliaSyntax parser failed — falling back to flisp!",
                exception=(exc,catch_backtrace()),
@@ -34,6 +47,18 @@ function core_parser_hook(code, filename, offset, options)
     end
     return Core.Compiler.fl_parse(code, filename, offset, options)
 end
+
+# Core._parse gained a `lineno` argument in
+# https://github.com/JuliaLang/julia/pull/43876
+# Prior to this, the following signature was needed:
+function core_parser_hook(code, filename, offset, options)
+    core_parser_hook(code, filename, LineNumberNode(0), offset, options)
+end
+
+# Hack:
+# Meta.parse() attempts to construct a ParseError from a string if it receives
+# `Expr(:error)`.
+Base.Meta.ParseError(e::JuliaSyntax.ParseError) = e
 
 """
 Connect the JuliaSyntax parser to the Julia runtime so that it replaces the
