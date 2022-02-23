0d40342796cb8ac0d48eeb212c7b37e09ed2bc8a
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2022-02-23 10:30:09 -0600

    bisection 1
    Co-Authored-By: Dilum Aluthge <dilum@aluthge.com>

--- base/sort.jl
+++ base/sort.jl
@@ -751,10 +751,10 @@ end
 
 ## generic sorting methods ##
 
-defalg(v::AbstractArray) = DEFAULT_STABLE
-defalg(v::AbstractArray{<:Union{Number, Missing}}) = DEFAULT_UNSTABLE
-defalg(v::AbstractArray{Missing}) = DEFAULT_UNSTABLE
-defalg(v::AbstractArray{Union{}}) = DEFAULT_UNSTABLE
+defalg(v::AbstractArray) = MergeSort
+defalg(v::AbstractArray{<:Union{Number, Missing}}) = QuickSort
+defalg(v::AbstractArray{Missing}) = QuickSort
+defalg(v::AbstractArray{Union{}}) = QuickSort
 
 function sort!(v::AbstractVector, alg::Algorithm, order::Ordering)
     inds = axes(v,1)
@@ -1452,5 +1452,10 @@ sort!(v::AbstractVector{<:Integer}, a::Algorithm, o::Perm{<:DirectOrdering,<:FPS
     fpsort!(v, a, o)
 
 end # module Sort.Float
+
+defalg(v::AbstractArray) = DEFAULT_STABLE
+defalg(v::AbstractArray{<:Union{Number, Missing}}) = DEFAULT_UNSTABLE
+defalg(v::AbstractArray{Missing}) = DEFAULT_UNSTABLE
+defalg(v::AbstractArray{Union{}}) = DEFAULT_UNSTABLE
 
 end # module Sort
