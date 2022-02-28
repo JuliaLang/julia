9f797858dbc4e26a8cc89f640ea9b3073659c83c
Viral B. Shah <viral@mayin.org>
2022-02-28 15:04:00 -0500

    Remove BLAS.vendor() that was deprecated in 1.7

--- stdlib/LinearAlgebra/src/blas.jl
+++ stdlib/LinearAlgebra/src/blas.jl
@@ -77,17 +77,6 @@ Return an object representing the current `libblastrampoline` configuration.
 """
 get_config() = lbt_get_config()
 
-# We hard-lock `vendor()` to `openblas(64)` here to satisfy older code, but all new code should use
-# `get_config()` since it is now possible to have multiple vendors loaded at once.
-function vendor()
-    Base.depwarn("`vendor()` is deprecated, use `BLAS.get_config()` and inspect the output instead", :vendor; force=true)
-    if USE_BLAS64
-        return :openblas64
-    else
-        return :openblas
-    end
-end
-
 if USE_BLAS64
     macro blasfunc(x)
         return Expr(:quote, Symbol(x, "64_"))
