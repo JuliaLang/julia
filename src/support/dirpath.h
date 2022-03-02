17ccbedb9c7fd0e614bdded523b4d290e444e118
2005m <morgan.emailbox@gmail.com>
2022-03-02 18:49:09 +0000

    Update dirpath.h

--- src/support/dirpath.h
+++ src/support/dirpath.h
@@ -7,12 +7,12 @@
 #define PATHSEPSTRING "\\"
 #define PATHLISTSEPSTRING ";"
 #define JL_PATH_MAX PATH_MAX
-#if defined(_COMPILER_CLANG_)
-#define JL_PATH_MAX MAX_PATH
-#endif
 #else
 #define PATHSEPSTRING "/"
 #define PATHLISTSEPSTRING ":"
+#if defined(_COMPILER_CLANG_)
+#define JL_PATH_MAX MAX_PATH
+#endif
 #ifndef JL_PATH_MAX // many platforms don't have a max path, we define one anyways
 #define JL_PATH_MAX 1024
 #endif
