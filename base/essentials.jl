b10aa56b501dd9917921f05d3240cfdee40dd63e
Milan Bouchet-Valat <nalimilan@club.fr>
2022-03-02 18:48:33 +0100

    doc: add warning about stateful iterators to `isempty` (#43900)
    The manual now mentions that stateful iterators should implement `isdone`,
    so it appears `isempty` is not supposed to consume elements.
    This matters a lot for callers as if you can't rely on this you can't do
    `isempty(c) && return; for x in c` but instead have to call `iterate(c)` manually.

--- base/essentials.jl
+++ base/essentials.jl
@@ -773,6 +773,13 @@ inferencebarrier(@nospecialize(x)) = Ref{Any}(x)[]
 
 Determine whether a collection is empty (has no elements).
 
+!!! warning
+
+    `isempty(itr)` may consume the next element of a stateful iterator `itr`
+    unless an appropriate `Base.isdone(itr)` or `isempty` method is defined.
+    Use of `isempty` should therefore be avoided when writing generic
+    code which should support any iterator type.
+
 # Examples
 ```jldoctest
 julia> isempty([])
