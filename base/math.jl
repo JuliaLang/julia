2e65198ca95ae3eb5670fbb12f521320669492eb
Oscar Smith <oscardssmith@gmail.com>
2022-03-04 16:06:34 -0500

    fix

--- base/math.jl
+++ base/math.jl
@@ -999,7 +999,7 @@ end
 @constprop :aggressive function ^(x::Float64, y::Float64)
     yint = unsafe_trunc(Int, y) # Note, this is actually safe since julia freezes the result
     y == yint && return x^yint
-    x === -1.0 && y >= 2*inv(eps()) && return 1.0
+    y >= 2*inv(eps()) && return x^typemax(Int64)
     x<0 && y > -4e18 && throw_exp_domainerror(x) # |y| is small enough that y isn't an integer
     x == 1 && return 1.0
     return pow_body(x, y)
