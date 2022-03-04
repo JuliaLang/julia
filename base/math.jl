c5388908bf460691ad57dae1ac4a00c8fa236eb6
Oscar Smith <oscardssmith@gmail.com>
2022-03-04 16:27:31 -0500

    fix for float32,16

--- base/math.jl
+++ base/math.jl
@@ -1018,7 +1018,7 @@ end
 @constprop :aggressive function ^(x::T, y::T) where T <: Union{Float16, Float32}
     yint = unsafe_trunc(Int64, y) # Note, this is actually safe since julia freezes the result
     y == yint && return x^yint
-    x === T(-1) && y >= 2*inv(eps(T)) && return T(1)
+    y >= 2*inv(eps(T)) && return x^typemax(Int64)
     x < 0 && y > -4e18 && throw_exp_domainerror(x) # |y| is small enough that y isn't an integer
     return pow_body(x, y)
 end
