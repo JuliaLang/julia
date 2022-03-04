b4c74ae5d3733a2eef509e0b41c8e7e08d47d53c
Dherya <ranaDherya09@gmail.com>
2022-03-04 23:44:15 +0530

    Added few testcases for widen in int.jl (all the 524704 test cases passed)

--- test/int.jl
+++ test/int.jl
@@ -240,6 +240,14 @@ end
     @test typeof(widen(Int64(-3))) == Int128
     @test typeof(widen(Int128(-3))) == BigInt
 
+    @test typeof(widen(UInt16(65535))) == UInt32
+    @test typeof(widen(UInt32(4294967295))) == UInt64
+    @test typeof(widen(UInt64(1999999999999999990))) == UInt128
+
+    @test widen(UInt16(65535)) === UInt32(65535)
+    @test widen(UInt32(4294967295)) === UInt64(4294967295)
+    @test widen(UInt64(1999999999999999990)) === UInt128(1999999999999999990)
+
     @test widemul(false, false) == false
     @test widemul(false, 3) == 0
     @test widemul(3, true) == widemul(true, 3) == 3
