269dcfb5f198f0a426c23bbfc34f6f411de8cf22
Tim Holy <tim.holy@gmail.com>
2022-03-09 04:56:54 -0600

    Move lazy"" to its docstring

--- base/strings/lazy.jl
+++ base/strings/lazy.jl
@@ -14,13 +14,9 @@ as much work as possible to either the macro or later printing operations.
 ```jldoctest
 julia> n = 5; str = LazyString("n is ", n)
 "n is 5"
-
-julia> str2 = lazy"n is \$n"
-"n is 5"
-
-julia> typeof(str2)
-LazyString
 ```
+
+See also [`lazy"str"`](@ref).
 
 !!! compat "Julia 1.8"
     `LazyString` requires Julia 1.8 or later.
@@ -38,6 +34,16 @@ end
 Create a [`LazyString`](@ref) using regular string interpolation syntax.
 Note that interpolations are *evaluated* at LazyString construction time,
 but *printing* is delayed until the first access to the string.
+
+# Examples
+
+```
+julia> n = 5; str = lazy"n is \$n"
+"n is 5"
+
+julia> typeof(str)
+LazyString
+```
 
 !!! compat "Julia 1.8"
     `lazy"str"` requires Julia 1.8 or later.
