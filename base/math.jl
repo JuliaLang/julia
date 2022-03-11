0ac47534c52dcc6b198507712f5b9f9c97ee6c3f
Oscar Smith <oscardssmith@gmail.com>
2022-03-11 10:56:08 -0500

    typo

--- base/math.jl
+++ base/math.jl
@@ -1066,7 +1066,7 @@ end
 
 function ^(x::Float32, n::Integer)
     n == 3 && return x*x*x #keep compatibility with literal_pow
-    n < 0 && return Float32(Base.power_by_squaring(inv(Float64(x)),n))
+    n < 0 && return Float32(Base.power_by_squaring(inv(Float64(x)),-n))
     Float32(Base.power_by_squaring(Float64(x),n))
 end
 @inline ^(x::Float16, y::Integer) = Float16(Float32(x) ^ y)
