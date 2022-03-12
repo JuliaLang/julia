8e66acd0a1f5972e22cb3401d35f44b1111433c4
Kristoffer <kcarlsson89@gmail.com>
2022-03-12 09:44:43 +0100

    extend the API for `LazyString` a bit

--- test/strings/basic.jl
+++ test/strings/basic.jl
@@ -1100,4 +1100,8 @@ end
     let d = Dict(lazy"$(1+2) is 3" => 3)
         @test d["3 is 3"] == 3
     end
+    l = lazy"1+2"
+    @test codeunit(l) == UInt8
+    @test codeunit(l,2) == 0x2b
+    @test isvalid(l, 1)
 end
