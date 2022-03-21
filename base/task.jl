28f58e77f9e1e60b96c707daf1f53a071e564ae8
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-21 11:18:11 -0700

    doc: recommend `@spawn` over `@async` (#44667)

--- base/task.jl
+++ base/task.jl
@@ -467,6 +467,14 @@ Values can be interpolated into `@async` via `\$`, which copies the value direct
 constructed underlying closure. This allows you to insert the _value_ of a variable,
 isolating the asynchronous code from changes to the variable's value in the current task.
 
+!!! warning
+    It is strongly encouraged to favor `Threads.@spawn` over `@async` always **even when no
+    parallelism is required** especially in publicly distributed libraries.  This is
+    because a use of `@async` disables the migration of the *parent* task across worker
+    threads in the current implementation of Julia.  Thus, seemingly innocent use of
+    `@async` in a library function can have a large impact on the performance of very
+    different parts of user applications.
+
 !!! compat "Julia 1.4"
     Interpolating values via `\$` is available as of Julia 1.4.
 """
