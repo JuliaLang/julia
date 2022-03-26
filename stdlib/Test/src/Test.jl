ac44d7303d93ea10f5bbf509e5c2f2c1c828e2e6
Sebastian Pfitzner <pfitzseb@gmail.com>
2022-03-26 13:08:57 +0100

    Fix test stacktrace scrubbing

--- stdlib/Test/src/Test.jl
+++ stdlib/Test/src/Test.jl
@@ -53,7 +53,7 @@ function scrub_backtrace(bt)
     if do_test_ind !== nothing && length(bt) > do_test_ind
         bt = bt[do_test_ind + 1:end]
     end
-    name_ind = findfirst(ip -> ip_has_file_and_func(ip, @__FILE__, (Symbol("macro expansion"),)), bt)
+    name_ind = findfirst(ip -> ip_has_file_and_func(ip, @__FILE__, (Symbol("macro expansion"), Symbol("top-level scope"))), bt)
     if name_ind !== nothing && length(bt) != 0
         bt = bt[1:name_ind]
     end
