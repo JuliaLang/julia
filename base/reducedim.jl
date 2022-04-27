d4c0c9aa31be737f8e8f1273a303f7aa0a1ba0e7
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-04-26 19:08:48 -0700

    Update docstrings

--- base/reducedim.jl
+++ base/reducedim.jl
@@ -1128,6 +1128,9 @@ julia> A = [-1.0 1; -0.5 2]
  -1.0  1.0
  -0.5  2.0
 
+julia> findmin(abs2, A, dims=1)
+([0.25 1.0], CartesianIndex{2}[CartesianIndex(2, 1) CartesianIndex(1, 2)])
+
 julia> findmin(abs2, A, dims=2)
 ([1.0; 0.25;;], CartesianIndex{2}[CartesianIndex(1, 1); CartesianIndex(2, 1);;])
 ```
@@ -1195,6 +1198,9 @@ julia> A = [-1.0 1; -0.5 2]
 2×2 Matrix{Float64}:
  -1.0  1.0
  -0.5  2.0
+
+julia> findmax(abs2, A, dims=1)
+([1.0 4.0], CartesianIndex{2}[CartesianIndex(1, 1) CartesianIndex(2, 2)])
 
 julia> findmax(abs2, A, dims=2)
 ([1.0; 4.0;;], CartesianIndex{2}[CartesianIndex(1, 1); CartesianIndex(2, 2);;])
