c355c8a36234bb720a9d5103335bbeba6d0f5791
William S. Moses <gh@wsmoses.com>
2022-05-01 11:48:24 -0400

    Extend code_warntype to take existing code_typed output

--- stdlib/InteractiveUtils/src/codeview.jl
+++ stdlib/InteractiveUtils/src/codeview.jl
@@ -59,9 +59,13 @@ See [`@code_warntype`](@ref man-code-warntype) for more information.
 """
 function code_warntype(io::IO, @nospecialize(f), @nospecialize(t=Base.default_tt(f));
                        debuginfo::Symbol=:default, optimize::Bool=false, kwargs...)
+    code_warntype(io, code_typed(f, t; optimize, kwargs...))
+end
+
+function code_warntype(io::IO, code_typed_output::Vector{Any})
     debuginfo = Base.IRShow.debuginfo(debuginfo)
     lineprinter = Base.IRShow.__debuginfo[debuginfo]
-    for (src, rettype) in code_typed(f, t; optimize, kwargs...)
+    for (src, rettype) in code_typed_output
         if !(src isa Core.CodeInfo)
             println(io, src)
             println(io, "  failed to infer")
