cc42cc4754c08acfa2115397a00cb9334bbddd0d
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-05-01 12:02:22 -0400

    add a comment, remove a link

--- base/math.jl
+++ base/math.jl
@@ -412,7 +412,7 @@ See also [`tan`](@ref), [`atanh`](@ref).
 # Examples
 
 ```jldoctest
-julia> tanh.(-3:3f0)
+julia> tanh.(-3:3f0)  # Here 3f0 isa Float32
 7-element Vector{Float32}:
  -0.9950548
  -0.9640276
@@ -422,11 +422,11 @@ julia> tanh.(-3:3f0)
   0.9640276
   0.9950548
 
-julia> tan.(im .* (1:3f0))
-3-element Vector{ComplexF32}:
- 0.0f0 + 0.7615941f0im
- 0.0f0 + 0.96402764f0im
- 0.0f0 + 0.9950547f0im
+julia> tan.(im .* (1:3))
+3-element Vector{ComplexF64}:
+ 0.0 + 0.7615941559557649im
+ 0.0 + 0.9640275800758169im
+ 0.0 + 0.9950547536867306im
 ```
 """
 tanh(x::Number)
