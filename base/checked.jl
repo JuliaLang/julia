89463b3a2b14dcef6dd2bd09e2b84472fe98588c
Kristoffer Carlsson <kcarlsson89@gmail.com>
2022-05-06 11:57:26 +0200

    fix review comments

--- base/checked.jl
+++ base/checked.jl
@@ -4,9 +4,10 @@
 
 """
     Checked
+
 The Checked module provides arithmetic functions for the built-in signed and unsigned
 Integer types which throw an error when an overflow occurs. They are named like `checked_sub`,
-`checked_div`, etc. In addition, `add_with_overflow, sub_with_overflow, mul_with_overflow`
+`checked_div`, etc. In addition, `add_with_overflow`, `sub_with_overflow`, `mul_with_overflow`
 return both the unchecked results and a boolean value denoting the presence of an overflow.
 """
 module Checked
