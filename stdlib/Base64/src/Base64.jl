206698c129977dd88f30e97239f9b76ff466957a
Kristoffer Carlsson <kcarlsson89@gmail.com>
2022-05-06 14:26:59 +0200

    fix trailing whitespace (#45207)

--- stdlib/Base64/src/Base64.jl
+++ stdlib/Base64/src/Base64.jl
@@ -3,7 +3,7 @@
 """
     Base64
 
-Functionality for [base64 encoding and decoding](https://en.wikipedia.org/wiki/Base64), 
+Functionality for [base64 encoding and decoding](https://en.wikipedia.org/wiki/Base64),
 a method to represent binary data using text, common on the web.
 """
 module Base64
