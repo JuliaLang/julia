42d168f2cb2cd06d607a953dbac5195be8630da6
Takafumi Arakaki <takafumi.a@gmail.com>
2022-05-06 23:08:31 +0900

    Revert "Clarify that `modifyproperty!` does not call `convert` (#45178)"
    This reverts commit 350376b2baef2e60baa78efa42d4a590ac8a47da.

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -2758,11 +2758,6 @@ The syntax `@atomic max(a().b, c)` returns `modifyproperty!(a(), :b,
 max, c, :sequentially_consistent))`, where the first argument must be a
 `getfield` expression and is modified atomically.
 
-Unlike [`setproperty!`](@ref Base.setproperty!), the default implementation of
-`modifyproperty!` does not call `convert` automatically.  Thus, `op` must return a value
-that can be stored in the field `f` directly when invoking the default `modifyproperty!`
-implementation.
-
 See also [`modifyfield!`](@ref Core.modifyfield!)
 and [`setproperty!`](@ref Base.setproperty!).
 """
