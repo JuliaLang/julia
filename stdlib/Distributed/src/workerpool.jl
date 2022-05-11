452a992a80afb80bd0562055679578057a4486bb
Kristoffer Carlsson <kcarlsson89@gmail.com>
2022-05-11 16:29:51 +0200

    Update stdlib/Distributed/src/workerpool.jl

--- stdlib/Distributed/src/workerpool.jl
+++ stdlib/Distributed/src/workerpool.jl
@@ -43,6 +43,7 @@ Create a `WorkerPool` from a vector or range of worker ids.
 
 julia> WorkerPool([2, 3])
 WorkerPool(Channel{Int64}(sz_max:9223372036854775807,sz_curr:2), Set([2, 3]), RemoteChannel{Channel{Any}}(1, 1, 6))
+
 julia> WorkerPool(2:4)
 WorkerPool(Channel{Int64}(sz_max:9223372036854775807,sz_curr:2), Set([4, 2, 3]), RemoteChannel{Channel{Any}}(1, 1, 7))
 ```
