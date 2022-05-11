beeba1d2f62a9f853cdddbff70f327f57ec2cfc4
Kristoffer Carlsson <kcarlsson89@gmail.com>
2022-05-11 14:05:22 +0200

    fix whitespace

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -806,7 +806,7 @@ kw"?", kw"?:"
 iterating over a sequence of values.
 
 The iteration variable is always a new variable, even if a variable of the same name
-exists in the enclosing scope.  
+exists in the enclosing scope.
 Use [`outer`](@ref) to reuse an existing local variable for iteration.
 
 # Examples
