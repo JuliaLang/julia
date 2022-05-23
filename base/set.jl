093176c20a71e5634cef865ebabb95fd98276408
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2022-05-23 11:23:40 +0200

    Fix doctest mistake

--- base/set.jl
+++ base/set.jl
@@ -14,14 +14,14 @@ See also: [`AbstractSet`](@ref), [`BitSet`](@ref), [`Dict`](@ref),
 [`push!`](@ref), [`empty!`](@ref), [`union!`](@ref), [`in`](@ref)
 
 # Examples
-```jldoctest filter = r"^Set{[A-Za-z0-9]+} with [0-9]+ element(s)?:"
+```jldoctest filter = r"^(true)|(Set{[A-Za-z0-9]+} with [0-9]+ element(s)?:)"
 julia> s = Set(Any[0, -0.0, 0.0])
 Set{Any} with 2 elements:
   0.0
   -0.0
 
 julia> push!(s, 'a'); push!(s, 0.0)
-Set{Any} with 2 elements:
+Set{Any} with 3 elements:
   0.0
   'a'
   -0.0
