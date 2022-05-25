bc1c7bb97d2c8b0a6de151bfab971ddb7a654460
KristofferC <kristoffer.carlsson@juliacomputing.com>
2022-05-25 09:21:28 +0200

    move writing to REPL history file to behind a PID lock

--- stdlib/REPL/src/REPL.jl
+++ stdlib/REPL/src/REPL.jl
@@ -19,6 +19,7 @@ Base.Experimental.@max_methods 1
 
 using Base.Meta, Sockets
 import InteractiveUtils
+import FileWatching
 
 export
     AbstractREPL,
@@ -662,7 +663,6 @@ function add_history(hist::REPLHistoryProvider, s::PromptState)
     # mode: $mode
     $(replace(str, r"^"ms => "\t"))
     """
-    # TODO: write-lock history file
     try
         seekend(hist.history_file)
     catch err
@@ -671,8 +671,10 @@ function add_history(hist::REPLHistoryProvider, s::PromptState)
         # If this doesn't fix it (e.g. when file is deleted), we'll end up rethrowing anyway
         hist_open_file(hist)
     end
-    print(hist.history_file, entry)
-    flush(hist.history_file)
+    FileWatching.mkpidlock(hist.file_path  * ".pid", stale_age=3) do
+        print(hist.history_file, entry)
+        flush(hist.history_file)
+    end
     nothing
 end
 
