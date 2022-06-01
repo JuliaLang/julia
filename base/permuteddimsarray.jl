1c70288447327b9a7f7e5e318d0dc53b66a3079d
Johnny Chen <johnnychen94@hotmail.com>
2022-06-02 00:45:49 +0800

    docs: a few more words on permutation
    For users new to numerical computation, the meaning of `perm` isn't
    always self-explained.

--- base/permuteddimsarray.jl
+++ base/permuteddimsarray.jl
@@ -100,26 +100,37 @@ julia> A = reshape(Vector(1:8), (2,2,2))
  5  7
  6  8
 
-julia> permutedims(A, (3, 2, 1))
+julia> perm = (3, 1, 2); # put the last dimension first
+
+julia> B = permutedims(A, perm)
 2×2×2 Array{Int64, 3}:
 [:, :, 1] =
- 1  3
- 5  7
+ 1  2
+ 5  6
 
 [:, :, 2] =
- 2  4
- 6  8
+ 3  4
+ 7  8
 
-julia> B = randn(5, 7, 11, 13);
-
-julia> perm = [4,1,3,2];
-
-julia> size(permutedims(B, perm))
-(13, 5, 11, 7)
-
-julia> size(B)[perm] == ans
+julia> A == permutedims(B, invperm(perm)) # the inverse permutation
 true
 ```
+
+For each dimension `i` of `B = permutedims(A, perm)`, its corresponding dimension of `A`
+will be `perm[i]`. This means the equality `size(B, i) == size(A, perm[i])` holds.
+
+```jldoctest
+julia> A = randn(5, 7, 11, 13);
+
+julia> perm = [4, 1, 3, 2];
+
+julia> B = permutedims(A, perm);
+
+julia> size(B)
+(13, 5, 11, 7)
+
+julia> size(A)[perm] == ans
+true
 """
 function permutedims(A::AbstractArray, perm)
     dest = similar(A, genperm(axes(A), perm))
