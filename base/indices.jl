d6c5ecae82a0dde4e9d45e51bf29a27006711e15
Jan Weidner <jw3126@gmail.com>
2022-06-02 07:13:45 +0200

    improve docstring of to_indices

--- base/indices.jl
+++ base/indices.jl
@@ -306,7 +306,7 @@ to_index(i) = throw(ArgumentError("invalid index: $(limitrepr(i)) of type $(type
 
 Convert the tuple `I` to a tuple of indices for use in indexing into array `A`.
 
-The returned tuple must only contain either `Int`s or `AbstractArray`s of
+The returned tuple must only contain either `Int`s, `CartesianIndex`s or `AbstractArray`s of
 scalar indices that are supported by array `A`. It will error upon encountering
 a novel index type that it does not know how to process.
 
@@ -320,6 +320,27 @@ which they index. To support those cases, `to_indices(A, I)` calls
 `to_indices(A, axes(A), I)`, which then recursively walks through both the
 given tuple of indices and the dimensional indices of `A` in tandem. As such,
 not all index types are guaranteed to propagate to `Base.to_index`.
+
+# Examples
+```jldoctest
+
+julia> A = zeros(1,2,3,4);
+
+julia> to_indices(A, (1,1,2,2))
+(1, 1, 2, 2)
+
+julia> to_indices(A, (1,1,2,20)) # no bounds checking
+(1, 1, 2, 20)
+
+julia> to_indices(A, (CartesianIndex((1,)), 2, CartesianIndex((3,4)))) # exotic index
+(1, 2, 3, 4)
+
+julia> to_indices(A, ([1,1], 1:2, 3, 4))
+([1, 1], 1:2, 3, 4)
+
+julia> to_indices(A, (1,2)) # no shape checking
+(1, 2)
+```
 """
 to_indices(A, I::Tuple) = (@inline; to_indices(A, axes(A), I))
 to_indices(A, I::Tuple{Any}) = (@inline; to_indices(A, (eachindex(IndexLinear(), A),), I))
