71d062c83e388dd945b691d96195dbe64c699959
Valentin Churavy <v.churavy@gmail.com>
2022-06-11 17:26:34 -0400

    Define aliases to FP16 crt in the OJIT
    (cherry picked from commit ff360153dd70491c6ec7812a486c860c54a3f55c)

--- src/jitlayers.h
+++ src/jitlayers.h
@@ -204,6 +204,7 @@ public:
     void RegisterJITEventListener(JITEventListener *L);
 #endif
 
+    orc::SymbolStringPtr mangle(StringRef Name);
     void addGlobalMapping(StringRef Name, uint64_t Addr);
     void addModule(std::unique_ptr<Module> M);
 
