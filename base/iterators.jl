00a12f865032ac217845982d450493df12667bc1
Vincent Yu <v@v-yu.com>
2022-06-15 06:59:02 +0000

    Fix `length(::Iterators.Drop)` for unsigned lengths

--- base/iterators.jl
+++ base/iterators.jl
@@ -55,7 +55,10 @@ _min_length(a, b, A, B) = min(length(a),length(b))
 _diff_length(a, b, A, ::IsInfinite) = 0
 _diff_length(a, b, ::IsInfinite, ::IsInfinite) = 0
 _diff_length(a, b, ::IsInfinite, B) = length(a) # inherit behaviour, error
-_diff_length(a, b, A, B) = max(length(a)-length(b), 0)
+function _diff_length(a, b, A, B)
+    (m, n) = length.((a, b))
+    return m > n ? m - n : oftype(m - n, 0)
+end
 
 and_iteratorsize(isz::T, ::T) where {T} = isz
 and_iteratorsize(::HasLength, ::HasShape) = HasLength()
