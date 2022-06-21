bc9fdcef490f32c48e92fe93c373b2aa7076e55b
N5N3 <2642243996@qq.com>
2022-06-22 00:44:50 +0800

    Improve the inferability of some index-related self-recursive functions in `Base` (#45672)
    * Better inferred `to_indices`
    * Better inferred `CartedianIndex(x::Union{Integer,CartedianIndex}...)`
    
    Co-authored-by: Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>

--- base/indices.jl
+++ base/indices.jl
@@ -349,11 +349,15 @@ to_indices(A, I::Tuple{}) = ()
 to_indices(A, I::Tuple{Vararg{Int}}) = I
 to_indices(A, I::Tuple{Vararg{Integer}}) = (@inline; to_indices(A, (), I))
 to_indices(A, inds, ::Tuple{}) = ()
-to_indices(A, inds, I::Tuple{Any, Vararg{Any}}) =
-    (@inline; (to_index(A, I[1]), to_indices(A, _maybetail(inds), tail(I))...))
+function to_indices(A, inds, I::Tuple{Any, Vararg{Any}})
+    @inline
+    head = _to_indices1(A, inds, I[1])
+    rest = to_indices(A, _cutdim(inds, I[1]), tail(I))
+    (head..., rest...)
+end
 
-_maybetail(::Tuple{}) = ()
-_maybetail(t::Tuple) = tail(t)
+_to_indices1(A, inds, I1) = (to_index(A, I1),)
+_cutdim(inds, I1) = safe_tail(inds)
 
 """
     Slice(indices)
