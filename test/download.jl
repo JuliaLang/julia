18a15b2d3c1efb935cebaa6187d2e5ce4ac5e6eb
Páll Haraldsson <Pall.Haraldsson@gmail.com>
2022-06-29 13:46:53 +0000

    Delete since download_exec.jl is deleted

--- test/download.jl
+++ test/download.jl
@@ -1,6 +1,1 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
-
-cmd = `$(Base.julia_cmd()) --depwarn=no --startup-file=no download_exec.jl`
-if !success(pipeline(cmd; stdout=stdout, stderr=stderr))
-    error("download test failed, cmd : $cmd")
-end
