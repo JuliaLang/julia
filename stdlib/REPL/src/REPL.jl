6262e0801710b3316faca34ba1679795efdf4648
Steve Kelly <kd2cca@gmail.com>
2022-07-05 15:04:28 -0400

    Update stdlib/REPL/src/REPL.jl
    Co-authored-by: Jameson Nash <vtjnash@gmail.com>

--- stdlib/REPL/src/REPL.jl
+++ stdlib/REPL/src/REPL.jl
@@ -180,7 +180,7 @@ end
 
 function modules_to_be_loaded(ast::Expr, mods::Vector{Symbol} = Symbol[])
     ast.head === :quote && return mods # don't search if it's not going to be run during this eval
-    if ast.head in [:using, :import]
+    if ast.head === :using || ast.head === :import
         for arg in ast.args
             arg = arg::Expr
             arg1 = first(arg.args)
