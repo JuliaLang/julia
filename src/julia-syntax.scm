12d48b25a7ce0e45dffa69fbe3b009cc9fc6d32c
Shuhei Kadowaki <aviatesk@gmail.com>
2022-07-05 14:22:57 +0900

    propagate other method metadata as well

--- src/julia-syntax.scm
+++ src/julia-syntax.scm
@@ -562,7 +562,9 @@
              ,(if (any kwarg? pargl) (gensy) UNUSED)
              (call (core kwftype) ,ftype)) ,kw ,@pargl ,@vararg)
           `(block
-            ,@(filter inline-meta? prologue)
+            ;; propagate method metadata to keyword sorter
+            ,@(map propagate-method-meta (filter meta? prologue))
+            ,@(filter argwide-nospecialize-meta? prologue)
             ,@(let ((lnns (filter linenum? prologue)))
                 (if (pair? lnns)
                     (list (car lnns))
