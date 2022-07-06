38c3e479f29aabf9cf7a7995813b0104bde464b1
Elliot Saba <staticfloat@gmail.com>
2022-07-06 15:30:20 -0700

    [DO NOT MERGE] Testing commit for JuliaCon talk

--- test/runtests.jl
+++ test/runtests.jl
@@ -89,6 +89,11 @@ linalg_tests = tests[linalg_test_ids]
 deleteat!(tests, linalg_test_ids)
 prepend!(tests, linalg_tests)
 
+
+# Only run 3 tests
+tests = tests[1:3]
+
+
 import LinearAlgebra
 cd(@__DIR__) do
     # `net_on` implies that we have access to the loopback interface which is
