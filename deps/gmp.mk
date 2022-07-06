db9461913efa3343fd3ebddc466ff3fdf95d5bee
woclass <git@wo-class.cn>
2022-07-06 19:33:35 +0800

    deps/gmp: add function to `Make.inc`

--- deps/gmp.mk
+++ deps/gmp.mk
@@ -73,16 +73,10 @@ ifeq ($(OS),$(BUILD_OS))
 endif
 	echo 1 > $@
 
-# Windows doesn't like soft link, use hard link
-ifeq ($(OS), WINNT)
-WIN_ADD_HARD_LINK_CMD := cp -f --link $(build_bindir)/libgmp-*.dll $(build_bindir)/libgmp.dll
-else
-WIN_ADD_HARD_LINK_CMD := true -ignore
-endif
 $(eval $(call staged-install, \
 	gmp,gmp-$(GMP_VER), \
 	MAKE_INSTALL,,, \
-	$$(WIN_ADD_HARD_LINK_CMD) && \
+	$$(WIN_MAKE_HARD_LINK) $(build_bindir)/libgmp-*.dll $(build_bindir)/libgmp.dll && \
 		$$(INSTALL_NAME_CMD)libgmp.$$(SHLIB_EXT) $$(build_shlibdir)/libgmp.$$(SHLIB_EXT)))
 
 clean-gmp:
