36dbd8306d0d358320b3e2692e558ed62986d904
Katharine Hyatt <kshyatt@users.noreply.github.com>
2022-07-11 18:41:12 -0400

    Update base/task.jl
    Co-authored-by: Fredrik Ekre <ekrefredrik@gmail.com>

--- base/task.jl
+++ base/task.jl
@@ -460,8 +460,8 @@ julia> Threads.nthreads()
 4
 
 julia> @sync begin
-           Threads.@spawn println("Thread-id $(Threads.threadid()), task 1")
-           Threads.@spawn println("Thread-id $(Threads.threadid()), task 2")
+           Threads.@spawn println("Thread-id \$(Threads.threadid()), task 1")
+           Threads.@spawn println("Thread-id \$(Threads.threadid()), task 2")
        end;
 Thread-id 3, task 1
 Thread-id 1, task 2
