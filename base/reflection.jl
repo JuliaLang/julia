c72625f18935d653d6f2447f526e4c98b3d43e87
Lilith Orion Hafner <lilithhafner@gmail.com>
2022-07-11 23:54:48 -0400

    Expand on `objectid` docstring (#45987)

--- base/reflection.jl
+++ base/reflection.jl
@@ -330,7 +330,9 @@ end
 """
     objectid(x) -> UInt
 
-Get a hash value for `x` based on object identity. `objectid(x)==objectid(y)` if `x === y`.
+Get a hash value for `x` based on object identity.
+
+If `x === y` then `objectid(x) == objectid(y)`, and usually when `x !== y`, `objectid(x) != objectid(y)`.
 
 See also [`hash`](@ref), [`IdDict`](@ref).
 """
