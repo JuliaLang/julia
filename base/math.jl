fbf9c2e3c7e80a53685f09819eead364b377a003
oscarddssmith <oscar.smith@juliacomputing.com>
2022-07-22 09:02:56 -0400

    use weaker effect

--- base/math.jl
+++ base/math.jl
@@ -293,10 +293,10 @@ end
 
 # polynomial evaluation using compensated summation.
 # much more accurate, especially when lo can be combined with other rounding errors
-Base.@assume_effects :total  @inline function exthorner(x::Union{Float16, Float32, Float64}, p::Tuple)
+Base.@assume_effects :terminates_locally  @inline function exthorner(x::Union{Float16, Float32, Float64}, p::Tuple)
     hi, lo = p[end], zero(x)
     for i in length(p)-1:-1:1
-        pi = p[i]
+        pi = getfield(p, i)
         prod, err = two_mul(hi,x)
         hi = pi+prod
         lo = fma(lo, x, prod - (hi - pi) + err)
