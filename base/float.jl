5b713139386610116e15dc0eb46399961c65fa3c
Oscar Smith <oscardssmith@gmail.com>
2022-07-25 10:01:06 -0400

    faster `isfinite`

--- base/float.jl
+++ base/float.jl
@@ -494,7 +494,7 @@ See also: [`iszero`](@ref), [`isone`](@ref), [`isinf`](@ref), [`ismissing`](@ref
 isnan(x::AbstractFloat) = (x != x)::Bool
 isnan(x::Number) = false
 
-isfinite(x::AbstractFloat) = x - x == 0
+isfinite(x::AbstractFloat) = x - x === zero(x)
 isfinite(x::Real) = decompose(x)[3] != 0
 isfinite(x::Integer) = true
 
