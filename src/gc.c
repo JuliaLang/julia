6fe18f4400d6ec6b27522f34dff318d8d90ab4f9
Diogo Netto <dcn@Diogos-MacBook-Air.local>
2022-07-27 14:14:46 -0400

    timing

--- src/gc.c
+++ src/gc.c
@@ -3116,16 +3116,6 @@ static int _jl_gc_collect(jl_ptls_t ptls, jl_gc_collection_t collection)
     }
     gc_mark_loop(ptls, sp);
     gc_mark_sp_init(gc_cache, &sp);
-    gc_num.since_sweep += gc_num.allocd;
-    JL_PROBE_GC_MARK_END(scanned_bytes, perm_scanned_bytes);
-    gc_settime_premark_end();
-    gc_time_mark_pause(gc_start_time, scanned_bytes, perm_scanned_bytes);
-    uint64_t end_mark_time = jl_hrtime();
-    uint64_t mark_time = end_mark_time - start_mark_time;
-    gc_num.mark_time = mark_time;
-    gc_num.total_mark_time += mark_time;
-    int64_t actual_allocd = gc_num.since_sweep;
-    // marking is over
 
     // 4. check for objects to finalize
     clear_weak_refs();
@@ -3160,7 +3150,18 @@ static int _jl_gc_collect(jl_ptls_t ptls, jl_gc_collection_t collection)
     gc_mark_queue_finlist(gc_cache, &sp, &to_finalize, 0);
     gc_mark_loop(ptls, sp);
     mark_reset_age = 0;
+
+    gc_num.since_sweep += gc_num.allocd;
+    JL_PROBE_GC_MARK_END(scanned_bytes, perm_scanned_bytes);
+    gc_settime_premark_end();
+    gc_time_mark_pause(gc_start_time, scanned_bytes, perm_scanned_bytes);
+    uint64_t end_mark_time = jl_hrtime();
+    uint64_t mark_time = end_mark_time - start_mark_time;
+    gc_num.mark_time = mark_time;
+    gc_num.total_mark_time += mark_time;
+    int64_t actual_allocd = gc_num.since_sweep;
     gc_settime_postmark_end();
+    // marking is over
 
     // Flush everything in mark cache
     gc_sync_all_caches_nolock(ptls);
