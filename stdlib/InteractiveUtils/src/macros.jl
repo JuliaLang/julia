731cb753becf2bd400074d448511e95ae3c94dfb
hyrodium <hyrodium@gmail.com>
2022-08-09 11:50:30 +0900

    update docstring of `functionloc`

--- stdlib/InteractiveUtils/src/macros.jl
+++ stdlib/InteractiveUtils/src/macros.jl
@@ -251,7 +251,7 @@ end
 
 Applied to a function or macro call, it evaluates the arguments to the specified call, and
 returns a tuple `(filename,line)` giving the location for the method that would be called for those arguments.
-It calls out to the `functionloc` function.
+It calls out to the [`functionloc`](@ref) function.
 """
 :@functionloc
 
