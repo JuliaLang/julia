bd1cf73c87cedaef4770744fc4ab50358b240ce9
hyrodium <hyrodium@gmail.com>
2022-08-09 11:27:08 +0900

    update docstrings of `less` and `edit`

--- stdlib/InteractiveUtils/src/macros.jl
+++ stdlib/InteractiveUtils/src/macros.jl
@@ -270,7 +270,7 @@ See also: [`@less`](@ref), [`@edit`](@ref).
 """
     @less
 
-Evaluates the arguments to the function or macro call, determines their types, and calls the `less`
+Evaluates the arguments to the function or macro call, determines their types, and calls the [`less`](@ref)
 function on the resulting expression.
 
 See also: [`@edit`](@ref), [`@which`](@ref), [`@code_lowered`](@ref).
@@ -280,7 +280,7 @@ See also: [`@edit`](@ref), [`@which`](@ref), [`@code_lowered`](@ref).
 """
     @edit
 
-Evaluates the arguments to the function or macro call, determines their types, and calls the `edit`
+Evaluates the arguments to the function or macro call, determines their types, and calls the [`edit`](@ref)
 function on the resulting expression.
 
 See also: [`@less`](@ref), [`@which`](@ref).
