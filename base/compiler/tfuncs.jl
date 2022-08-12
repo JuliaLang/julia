1a29a83350bb3f62dcc20db2efdb798ea371ce6b
Shuhei Kadowaki <aviatesk@gmail.com>
2022-08-12 18:37:10 +0900

    effects: improve effect analysis on `isdefined`
    When it's applied to non-immutable object, we can taint `:consistent`-cy
    by `CONSISTENT_IF_INACCESSIBLEMEMONLY` instead of `ALWAYS_FALSE`.

--- base/compiler/tfuncs.jl
+++ base/compiler/tfuncs.jl
@@ -1911,7 +1911,7 @@ function isdefined_effects(argtypes::Vector{Any})
     isempty(argtypes) && return EFFECTS_THROWS
     obj = argtypes[1]
     isvarargtype(obj) && return Effects(EFFECTS_THROWS; consistent=ALWAYS_FALSE)
-    consistent = is_immutable_argtype(obj) ? ALWAYS_TRUE : ALWAYS_FALSE
+    consistent = is_immutable_argtype(obj) ? ALWAYS_TRUE : CONSISTENT_IF_INACCESSIBLEMEMONLY
     nothrow = isdefined_nothrow(argtypes)
     return Effects(EFFECTS_TOTAL; consistent, nothrow)
 end
