c565a58b0adedb3ed0a4890e407c3c04f4fe6703
Claire Foster <aka.c42f@gmail.com>
2022-08-12 18:13:35 +1000

    Sysimage / precompile fixes
    * Fix for sysimage compilation
    * Disable info logging so stdlib build is simpler

--- JuliaSyntax/sysimage/precompile_exec.jl
+++ JuliaSyntax/sysimage/precompile_exec.jl
@@ -2,4 +2,4 @@ import JuliaSyntax
 Base.include(@__MODULE__(), joinpath(pkgdir(JuliaSyntax), "test", "test_utils.jl"))
 Base.include(@__MODULE__(), joinpath(pkgdir(JuliaSyntax), "test", "parser.jl"))
 JuliaSyntax.enable_in_core!()
-@info "Some parsing" Meta.parse("x+y+z-w .+ [a b c]")
+Meta.parse("x+y+z-w .+ [a b c]")
