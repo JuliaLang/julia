151d07a02c56495705d387dbb8534afc2c4348f4
Martin Holters <martin.holters@hsu-hh.de>
2022-08-12 11:45:31 +0200

    Allow elimination of `isbits` for non-const argument
    It should be sufficient for the _type_ of `x` to be known at compile
    time to fully eliminate `isbits(x)`. However, the pevious code relied on
    `isbits` being called on its argument during inference (by declaring it
    `:total`) for eliminating it. But if the value of `x` was not known,
    only its type, this would not work. The new implementation has the added
    benefit of being simpler and DRYer (wrt. `isbitstype`).

--- base/reflection.jl
+++ base/reflection.jl
@@ -588,7 +588,7 @@ isbitstype(@nospecialize t) = (@_total_meta; isa(t, DataType) && (t.flags & 0x8)
 
 Return `true` if `x` is an instance of an [`isbitstype`](@ref) type.
 """
-isbits(@nospecialize x) = (@_total_meta; typeof(x).flags & 0x8 == 0x8)
+isbits(@nospecialize x) = isbitstype(typeof(x))
 
 """
     isdispatchtuple(T)
