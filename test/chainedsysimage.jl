3ee200f42bc3cd9633050eba91f6c1ff987cd6f9
Petr Vana <petvana@centrum.cz>
2022-08-14 17:20:03 +0200

    Improve CI by using LLVM_full_jll

--- test/chainedsysimage.jl
+++ test/chainedsysimage.jl
@@ -2,28 +2,47 @@
 
 using Test, Libdl
 
+try
+    # TODO - This is not perfect because the test depends on an external package
+    using LLVM_full_jll
+catch
+    using Pkg
+    Pkg.add("LLVM_full_jll")
+    using LLVM_full_jll
+end
+
 @testset "empty chained sysimage" begin
+    function get_llvm_cmd(exe) # Not all binaries are exported from LLVM_full_jll
+        a = LLVM_full_jll.clang()
+        name_replace(path) = joinpath(dirname(path), replace(basename(path), "clang" => exe))
+        return Cmd(Cmd(name_replace.(a.exec)); env = a.env)
+    end
+
+    ar = get_llvm_cmd("llvm-ar")
+    objcopy = get_llvm_cmd("llvm-objcopy")
+    clang = LLVM_full_jll.clang()
+
     binariesavailable = false
     try
-        # TODO - consider using LLVM_full_jll to make this test multiplatform
-        success(`ar --version`)
-        success(`llvm-objcopy --version`)
-        success(`ld --version`)
+        @assert success(`$ar --version`)
+        @assert success(`$objcopy --version`)
+        @assert success(`$clang --version`)
         binariesavailable = true
     catch
     end
 
     if !binariesavailable
-        @test_broken false # Not supported for this OS yet
+        @test_broken false # This test is not yet supported by this OS
     else
         sysimg = Base.unsafe_string(Base.JLOptions().image_file)
         cd(mktempdir()) do
+            # TODO - `sys-o.a` file is not distributed
             sysoa = replace(sysimg, ".$(Libdl.dlext)" => "-o.a")
             cp("$sysoa", "sys-o.a", force=true)
             @test success(`ar x sys-o.a`)
             rm("data.o")
             mv("text.o", "text-old.o")
-            @test success(`llvm-objcopy --remove-section .data.jl.sysimg_link text-old.o`)
+            @test success(`$objcopy --remove-section .data.jl.sysimg_link text-old.o`)
 
             source_txt = """
 Base.__init_build();
@@ -35,8 +54,8 @@ println(0.1, 1, 0x2)
 """
 
             @test success(`$(Base.julia_cmd()) --sysimage-native-code=chained --startup-file=no --sysimage=$sysimg --output-o chained.o.a -e $source_txt`)
-            @test success(`ar x chained.o.a`) # Extract new sysimage files
-            @test success(`ld -shared -o chained.$(Libdl.dlext) text.o data.o text-old.o`)
+            @test success(`$ar x chained.o.a`) # Extract new sysimage files
+            @test success(`$clang -shared -o chained.$(Libdl.dlext) text.o data.o text-old.o`)
 
             # Test if "println(0.1, 1, 0x2)" is precompiled
             source_txt2 = """
