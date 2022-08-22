39453f9e2a8aa72334f46dbf99f443fd2fc3551e
Petr Vana <petvana@centrum.cz>
2022-08-22 09:05:49 +0200

    Show error for CI

--- test/chainedsysimage.jl
+++ test/chainedsysimage.jl
@@ -39,7 +39,7 @@ end
     else
         sysimage_path = Base.unsafe_string(Base.JLOptions().image_file)
         object_file = replace(sysimage_path, ".$(Libdl.dlext)" => "-o.a")
-        if true || !isfile(object_file)
+        if !isfile(object_file)
             # Compile julia sysimage because `sys-o.a` file is not distributed
             object_file = tempname() * ".o"
             # Use the default values from PackageCompiler for incremental sysimage build (without re-using native code
@@ -66,40 +66,40 @@ end
                     -L$(PackageCompiler.julia_private_libdir()) \
                     -o $sysimage_path $o_file_flags \
                     $(Base.shell_split(PackageCompiler.ldlibs())) $extra`
-            @show cmd
-            @test success(cmd)
+            run(cmd)
         end
         dir = mktempdir()
         dir = mkpath("chained")
         cd(dir) do
             cp("$object_file", "sys-o.a", force=true)
-            @test success(`$ar x sys-o.a`)
+            run(`$ar x sys-o.a`)
             rm("data.o")
             mv("text.o", "text-old.o", force = true)
-            @test success(`$objcopy --remove-section .data.jl.sysimg_link text-old.o`)
+            run(`$objcopy --remove-section .data.jl.sysimg_link text-old.o`)
 
             source_txt = """
 Base.__init_build();
 module PrecompileStagingArea;
-    using Printf
+    using Printf;
+    @ccall jl_precompiles_for_sysimage(1::Cuchar)::Cvoid;
+    println(0.1, 1, 0x2);
 end;
-@ccall jl_precompiles_for_sysimage(1::Cuchar)::Cvoid;
-println(0.1, 1, 0x2)
 """
 
-            @test success(`$(Base.julia_cmd()) --sysimage-native-code=chained --startup-file=no --sysimage=$sysimage_path --output-o chained.o.a -e $source_txt`)
-            @test success(`$ar x chained.o.a`) # Extract new sysimage files
+            run(`$(Base.julia_cmd()) --sysimage-native-code=chained --startup-file=no --sysimage=$sysimage_path --output-o chained.o.a -e $source_txt`)
+            run(`$ar x chained.o.a`) # Extract new sysimage files
             #@show `$clang -shared -o chained.$(Libdl.dlext) text.o data.o text-old.o`
-            @test success(`$clang -shared -o chained.$(Libdl.dlext) text.o data.o text-old.o`)
+            run(`$clang -shared -o chained.$(Libdl.dlext) text.o data.o text-old.o`)
 
             # Test if "println(0.1, 1, 0x2)" is precompiled
             source_txt2 = """
 a = @allocated println(0.1, 1, 0x2);
 b = @allocated println(0.1, 1, 0x2);
-@assert a == b;
+@show a, b
+@assert a + 1000 > b;
 """
 
-            @test success(`$(Base.julia_cmd()) --sysimage=chained.$(Libdl.dlext) -e $source_txt2`)
+            @test run(`$(Base.julia_cmd()) --sysimage=chained.$(Libdl.dlext) -e $source_txt2`).exitcode == 0
         end
     end
 end
