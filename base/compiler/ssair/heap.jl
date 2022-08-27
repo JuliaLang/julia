b95d8a5dce86e8fdc26cd89bf84e99422e59c3c2
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-08-27 15:35:57 -0500

    remove unused parts

--- base/compiler/ssair/heap.jl
+++ base/compiler/ssair/heap.jl
@@ -1,11 +1,7 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
-using Base.Order: Forward, Ordering, lt
-
-const DefaultReverseOrdering = Base.ReverseOrdering{Base.ForwardOrdering}
-
-# Heap operations on flat arrays
-# ------------------------------
+# Heap operations on flat vectors
+# -------------------------------
 
 
 # Binary heap indexing
@@ -15,7 +11,7 @@ heapparent(i::Integer) = div(i, 2)
 
 
 # Binary min-heap percolate down.
-function percolate_down!(xs::AbstractArray, i::Integer, x=xs[i], o::Ordering=Forward, len::Integer=length(xs))
+function percolate_down!(xs::Vector, i::Integer, x, o::Ordering, len::Integer=length(xs))
     @inbounds while (l = heapleft(i)) <= len
         r = heapright(i)
         j = r > len || lt(o, xs[l], xs[r]) ? l : r
@@ -26,11 +22,8 @@ function percolate_down!(xs::AbstractArray, i::Integer, x=xs[i], o::Ordering=For
     xs[i] = x
 end
 
-percolate_down!(xs::AbstractArray, i::Integer, o::Ordering, len::Integer=length(xs)) = percolate_down!(xs, i, xs[i], o, len)
-
-
 # Binary min-heap percolate up.
-function percolate_up!(xs::AbstractArray, i::Integer, x=xs[i], o::Ordering=Forward)
+function percolate_up!(xs::Vector, i::Integer, x, o::Ordering)
     @inbounds while (j = heapparent(i)) >= 1
         lt(o, x, xs[j]) || break
         xs[i] = xs[j]
@@ -39,15 +32,13 @@ function percolate_up!(xs::AbstractArray, i::Integer, x=xs[i], o::Ordering=Forwa
     xs[i] = x
 end
 
-@inline percolate_up!(xs::AbstractArray, i::Integer, o::Ordering) = percolate_up!(xs, i, xs[i], o)
-
 """
-    heappop!(v, [ord])
+    heappop!(v, ord)
 
 Given a binary heap-ordered array, remove and return the lowest ordered element.
 For efficiency, this function does not check that the array is indeed heap-ordered.
 """
-function heappop!(xs::AbstractArray, o::Ordering=Forward)
+function heappop!(xs::Vector, o::Ordering)
     x = xs[1]
     y = pop!(xs)
     if !isempty(xs)
@@ -57,83 +48,27 @@ function heappop!(xs::AbstractArray, o::Ordering=Forward)
 end
 
 """
-    heappush!(v, x, [ord])
+    heappush!(v, x, ord)
 
 Given a binary heap-ordered array, push a new element `x`, preserving the heap property.
 For efficiency, this function does not check that the array is indeed heap-ordered.
 """
-@inline function heappush!(xs::AbstractArray, x, o::Ordering=Forward)
+function heappush!(xs::Vector, x, o::Ordering)
     push!(xs, x)
-    percolate_up!(xs, length(xs), o)
+    i = lastindex(xs)
+    percolate_up!(xs, i, @inbounds(xs[i]), o)
     return xs
 end
 
 
-# Turn an arbitrary array into a binary min-heap (by default) in linear time.
 """
-    heapify!(v, ord::Ordering=Forward)
+    heapify!(v, ord::Ordering)
 
-In-place [`heapify`](@ref).
+Turn an arbitrary vector into a binary min-heap in linear time.
 """
-function heapify!(xs::AbstractArray, o::Ordering=Forward)
-    for i in heapparent(length(xs)):-1:1
-        percolate_down!(xs, i, o)
+function heapify!(xs::Vector, o::Ordering)
+    for i in heapparent(lastindex(xs)):-1:1
+        percolate_down!(xs, i, @inbounds(xs[i]), o)
     end
     return xs
-end
-
-"""
-    heapify(v, ord::Ordering=Forward)
-
-Returns a new vector in binary heap order, optionally using the given ordering.
-```jldoctest
-julia> a = [1,3,4,5,2];
-
-julia> heapify(a)
-5-element Array{Int64,1}:
- 1
- 2
- 4
- 5
- 3
-
-julia> heapify(a, Base.Order.Reverse)
-5-element Array{Int64,1}:
- 5
- 3
- 4
- 1
- 2
-```
-"""
-# Todo, benchmarking shows copy(xs) outperforms copyto!(similar(xs), xs) for 10^6 Float64
-heapify(xs::AbstractArray, o::Ordering=Forward) = heapify!(copyto!(similar(xs), xs), o)
-
-"""
-    isheap(v, ord::Ordering=Forward)
-
-Return `true` if an array is heap-ordered according to the given order.
-
-```jldoctest
-julia> a = [1,2,3]
-3-element Vector{Int64}:
- 1
- 2
- 3
-
-julia> isheap(a,Base.Order.Forward)
-true
-
-julia> isheap(a,Base.Order.Reverse)
-false
-```
-"""
-function isheap(xs::AbstractArray, o::Ordering=Forward)
-    for i in 1:div(length(xs), 2)
-        if lt(o, xs[heapleft(i)], xs[i]) ||
-           (heapright(i) <= length(xs) && lt(o, xs[heapright(i)], xs[i]))
-            return false
-        end
-    end
-    return true
 end
