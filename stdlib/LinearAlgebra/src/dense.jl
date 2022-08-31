24854560e933ca2021f1ae46512de6139ec8f99c
Daniel Karrasch <Daniel.Karrasch@posteo.de>
2022-08-31 14:15:23 +0200

    fix typo

--- stdlib/LinearAlgebra/src/dense.jl
+++ stdlib/LinearAlgebra/src/dense.jl
@@ -358,7 +358,7 @@ _kronsize(A::AbstractVector, B::AbstractMatrix) = (length(A)*size(B, 1), size(B,
     kron!(C, A, B)
 
 Computes the Kronecker product of `A` and `B` and stores the result in `C`,
-overwriting the existing content of `C`. This is the in-place version of [`kron``](@ref).
+overwriting the existing content of `C`. This is the in-place version of [`kron`](@ref).
 
 !!! compat "Julia 1.6"
     This function requires Julia 1.6 or later.
