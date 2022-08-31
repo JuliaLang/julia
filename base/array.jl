71ba31d4dd05695256caf198ee81c302439434a2
Hendrik Ranocha <mail@ranocha.de>
2022-08-31 15:45:09 +0200

    improve type stability of `findall(testf::Function, A::AbstractArray)`
    This prevents some invalidations in `mightalias(A::AbstractArray, B::AbstractArray)` in abstractarray.jl when loading Static.jl.
    Here we specialize on the function instead of using map since broadcasting returns a BitArray, map a Vector{Bool}.

--- base/array.jl
+++ base/array.jl
@@ -2325,7 +2325,7 @@ findall(testf::Function, A) = collect(first(p) for p in pairs(A) if testf(last(p
 
 # Broadcasting is much faster for small testf, and computing
 # integer indices from logical index using findall has a negligible cost
-findall(testf::Function, A::AbstractArray) = findall(map(testf, A))
+findall(testf::F, A::AbstractArray) where {F<:Function} = findall(testf.(A))
 
 """
     findall(A)
