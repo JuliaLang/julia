b3b621bed73d2d9bfe61511c4cf4cd26d00d1f88
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-09-08 18:02:08 -0400

    change to collect_similar

--- base/iterators.jl
+++ base/iterators.jl
@@ -322,7 +322,7 @@ setindex!(v::Pairs, value, key) = (getfield(v, :data)[key] = value; v)
 get(v::Pairs, key, default) = get(getfield(v, :data), key, default)
 get(f::Base.Callable, v::Pairs, key) = get(f, getfield(v, :data), key)
 
-Base.map(f, A::Pairs) = collect(Base.Generator(f,A))
+Base.map(f, v::Pairs) = Base.collect_similar(values(v), Base.generator(f, v))
 
 # zip
 
