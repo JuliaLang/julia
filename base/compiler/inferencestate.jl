df955b81b13f70502fee3c0ef2ed5d6d4c9db360
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2022-09-13 21:01:26 +0900

    compiler: add missing `@nospecialize` annotations (#46734)

--- base/compiler/inferencestate.jl
+++ base/compiler/inferencestate.jl
@@ -487,7 +487,7 @@ function add_cycle_backedge!(frame::InferenceState, caller::InferenceState, curr
 end
 
 # temporarily accumulate our edges to later add as backedges in the callee
-function add_backedge!(li::MethodInstance, caller::InferenceState, invokesig::Union{Nothing,Type}=nothing)
+function add_backedge!(li::MethodInstance, caller::InferenceState, @nospecialize(invokesig=nothing))
     isa(caller.linfo.def, Method) || return # don't add backedges to toplevel exprs
     edges = caller.stmt_edges[caller.currpc]
     if edges === nothing
