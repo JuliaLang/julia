892da7b925c29257ce89b26578eb83343091f62c
Udoh Jeremiah <udohjeremiah@icloud.com>
2022-09-17 17:31:24 +0100

    add example to exponent

--- base/math.jl
+++ base/math.jl
@@ -938,11 +938,20 @@ Returns the largest integer `y` such that `2^y ≤ abs(x)`.
 
 # Examples
 ```jldoctest
+julia> exponent(8)
+3
+
+julia> exponent(64//1)
+6
+
 julia> exponent(6.5)
 2
 
 julia> exponent(16.0)
 4
+
+julia> exponent(3.142e-4)
+-12
 ```
 """
 function exponent(x::T) where T<:IEEEFloat
