e8dba44868ab091b879b061a9db4905358965255
Elliot Saba <staticfloat@gmail.com>
2022-09-20 10:37:44 -0700

    Support older names

--- stdlib/OpenBLAS_jll/src/OpenBLAS_jll.jl
+++ stdlib/OpenBLAS_jll/src/OpenBLAS_jll.jl
@@ -40,7 +40,11 @@ function __init__()
     # Ensure that OpenBLAS does not grab a huge amount of memory at first,
     # since it instantly allocates scratch buffer space for the number of
     # threads it thinks it needs to use.
-    if !haskey(ENV, "OPENBLAS_NUM_THREADS")
+    # X-ref: https://github.com/xianyi/OpenBLAS/blob/c43ec53bdd00d9423fc609d7b7ecb35e7bf41b85/README.md#setting-the-number-of-threads-using-environment-variables
+    # X-ref: https://github.com/JuliaLang/julia/issues/45434
+    if !haskey(ENV, "OPENBLAS_NUM_THREADS") &&
+       !haskey(ENV, "GOTO_NUM_THREADS") &&
+       !haskey(ENV, "OMP_NUM_THREADS")
         # We set this to `1` here, and then LinearAlgebra will update
         # to the true value in its `__init__()` function.
         ENV["OPENBLAS_NUM_THREADS"] = "1"
