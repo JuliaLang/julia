a33ef133d54c30af721f8bcd3ce0e1a303b690f7
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2022-09-21 10:34:32 +0200

    better error handling

--- stdlib/LinearAlgebra/src/diagonal.jl
+++ stdlib/LinearAlgebra/src/diagonal.jl
@@ -763,14 +763,17 @@ function eigen(D::Diagonal; permute::Bool=true, scale::Bool=true, sortby::Union{
     Eigen(λ, evecs)
 end
 function eigen(Da::Diagonal, Db::Diagonal; sortby::Union{Function,Nothing}=nothing)
-    if any(!isfinite, Da.diag) || any(!isfinite, Db.diag) || any(iszero, Db.diag)
-        throw(ArgumentError("matrix contains Infs or NaNs"))
+    if any(!isfinite, Da.diag) || any(!isfinite, Db.diag)
+        throw(ArgumentError("matrices contain Infs or NaNs"))
+    end
+    if !isposdef(Db)
+        throw(ArgumentError("right-hand side diagonal matrix is not positive definit"))
     end
     return GeneralizedEigen(eigen(Db \ Da; sortby)...)
 end
 function eigen(A::AbstractMatrix, D::Diagonal; sortby::Union{Function,Nothing}=nothing)
-    if any(!isfinite, D.diag) || any(iszero, D.diag)
-        throw(ArgumentError("right-hand side diagonal matrix contains Infs or NaNs or is singular"))
+    if !isposdef(D)
+        throw(ArgumentError("right-hand side diagonal matrix is not positive definit"))
     end
     if size(A, 1) == size(A, 2) && isdiag(A)
         return eigen(Diagonal(A), D; sortby)
@@ -778,9 +781,8 @@ function eigen(A::AbstractMatrix, D::Diagonal; sortby::Union{Function,Nothing}=n
         S = promote_type(eigtype(eltype(A)), eltype(D))
         return eigen!(eigencopy_oftype(Hermitian(A), S), Diagonal{S}(D); sortby)
     else
-        DA = D \ A
-        E = DA isa Matrix ? eigen!(DA; sortby) : eigen(DA; sortby)
-        return GeneralizedEigen(E...)
+        S = promote_type(eigtype(eltype(A)), eltype(D))
+        return eigen!(eigencopy_oftype(A, S), Diagonal{S}(D); sortby)
     end
 end
 
