adc9e59b6c28e35ecc8a8a73e6da95aebde725bf
apaz <aarpazdera@gmail.com>
2022-10-03 11:41:27 -0500

    Update style
    Co-authored-by: Jameson Nash <vtjnash@gmail.com>

--- src/gc.c
+++ src/gc.c
@@ -2878,7 +2878,8 @@ mark: {
 }
 
 static void jl_gc_queue_thread_local(jl_gc_mark_cache_t *gc_cache, jl_gc_mark_sp_t *sp,
-                                     jl_ptls_t ptls2) {
+                                     jl_ptls_t ptls2)
+{
     jl_value_t* current_task = jl_atomic_load_relaxed(&ptls2->current_task);
     gc_mark_queue_obj(gc_cache, sp, current_task);
     gc_heap_snapshot_record_root(current_task, "current task");
