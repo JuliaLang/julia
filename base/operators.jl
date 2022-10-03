1855583131a5a04d40fe5748e76d8fa50263855c
Udoh Jeremiah <udohjeremiah@icloud.com>
2022-10-03 11:23:32 +0100

    complete info on the |> operator (#46890)
    Co-authored-by: Michael Abbott <32575566+mcabbott@users.noreply.github.com>
    Co-authored-by: Lilith Orion Hafner <lilithhafner@gmail.com>

--- base/operators.jl
+++ base/operators.jl
@@ -887,13 +887,21 @@ widen(x::Type{T}) where {T} = throw(MethodError(widen, (T,)))
 """
     |>(x, f)
 
-Applies a function to the preceding argument. This allows for easy function chaining.
-When used with anonymous functions, parentheses are typically required around the definition to get the intended chain.
+Infix operator which applies function `f` to the argument `x`.
+This allows `f(g(x))` to be written `x |> g |> f`.
+When used with anonymous functions, parentheses are typically required around
+the definition to get the intended chain.
 
 # Examples
 ```jldoctest
-julia> [1:5;] .|> (x -> x^2) |> sum |> inv
-0.01818181818181818
+julia> 4 |> inv
+0.25
+
+julia> [2, 3, 5] |> sum |> inv
+0.1
+
+julia> [0 1; 2 3] .|> (x -> x^2) |> sum
+14
 ```
 """
 |>(x, f) = f(x)
