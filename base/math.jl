a8db53dd82013c68bebfeabcc3bd3101fe165fa1
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-10-06 08:44:38 -0400

    tweak

--- base/math.jl
+++ base/math.jl
@@ -935,20 +935,20 @@ ldexp(x::Float16, q::Integer) = Float16(ldexp(Float32(x), q))
 Returns the largest integer `y` such that `2^y ≤ abs(x)`.
 
 Throws a `DomainError` when `x` is zero, infinite, or [`NaN`](@ref).
-For any other non-subnormal floating-point number `x`, this reads the exponent bits of `x`.
+For any other non-subnormal floating-point number `x`, this corresponds to the exponent bits of `x`.
 
-See also [`significand`](@ref), [`frexp`](@ref), [`issubnormal`](@ref), [`log2`](@ref).
+See also [`signbit`](@ref), [`significand`](@ref), [`frexp`](@ref), [`issubnormal`](@ref), [`log2`](@ref).
 
 # Examples
 ```jldoctest
 julia> exponent(8)
 3
 
-julia> exponent(-6.5)
+julia> exponent(6.5)
 2
 
-julia> exponent(3.142e-4)
--12
+julia> exponent(-1//4)
+-2
 
 julia> exponent(floatmin(Float32)), exponent(nextfloat(0.0f0))
 (-126, -149)
