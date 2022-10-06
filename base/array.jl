c1c2a69b824e7e4be91bab9bf53f9a54f7152515
Katharine Hyatt <kshyatt@users.noreply.github.com>
2022-10-06 10:23:59 -0400

    Doctest for setindex (#47072)

--- base/array.jl
+++ base/array.jl
@@ -951,6 +951,18 @@ end
 
 Store the given value at the given key or index within a collection. The syntax `a[i,j,...] =
 x` is converted by the compiler to `(setindex!(a, x, i, j, ...); x)`.
+
+# Examples
+```jldoctest
+julia> a = Dict("a"=>1)
+Dict{String, Int64} with 1 entry:
+  "a" => 1
+
+julia> setindex!(a, 2, "b")
+Dict{String, Int64} with 2 entries:
+  "b" => 2
+  "a" => 1
+```
 """
 function setindex! end
 
