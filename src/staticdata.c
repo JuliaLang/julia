66d84ced052f1f864a867b96158d4a18985ff8e2
Jameson Nash <vtjnash@gmail.com>
2022-10-12 13:37:13 -0400

    remove null roots

--- src/staticdata.c
+++ src/staticdata.c
@@ -1866,6 +1866,7 @@ static void jl_collect_new_roots(jl_array_t *roots, htable_t *mset, uint64_t key
             if (nwithkey) {
                 jl_array_ptr_1d_push(roots, (jl_value_t*)m);
                 newroots = jl_alloc_vec_any(nwithkey);
+                jl_array_del_end(newroots, nwithkey);
                 jl_array_ptr_1d_push(roots, (jl_value_t*)newroots);
                 rle_iter_state rootiter = rle_iter_init(0);
                 uint64_t *rletable = NULL;
@@ -1877,6 +1878,7 @@ static void jl_collect_new_roots(jl_array_t *roots, htable_t *mset, uint64_t key
                 while (rle_iter_increment(&rootiter, nroots, rletable, nblocks2))
                     if (rootiter.key == key)
                         jl_array_ptr_1d_push(newroots, jl_array_ptr_ref(m->roots, rootiter.i));
+                assert(jl_array_len(newroots) == nwithkey);
             }
         }
     }
