b6e894d83ca1f0947a34b9215acd99a995242a1a
Oscar Smith <oscardssmith@gmail.com>
2022-10-20 11:52:43 -0400

    faster `inv` for normal sized `ComplexF64`
    ```
    julia> xc64 = rand(ComplexF64, 1024); yc64=similar(xc64);
    #Old
    julia> @benchmark @. $yc64 = inv($xc64)
    BenchmarkTools.Trial: 10000 samples with 6 evaluations.
     Range (min … max):  5.467 μs …  10.603 μs  ┊ GC (min … max): 0.00% … 0.00%
     Time  (median):     5.983 μs               ┊ GC (median):    0.00%
     Time  (mean ± σ):   5.838 μs ± 368.716 ns  ┊ GC (mean ± σ):  0.00% ± 0.00%
    
      █▂                   ██▁                                    ▂
      ██▄▅▄▄▃▁▅▆▇▇▆▇▆▆▆▇▇▇▇███▇▇▇█▇███▇▆▆▆▅▆▅▇▆▆▇▇▅▄▅▃▅▄▅▃▅▄▁▃▃▁▃ █
      5.47 μs      Histogram: log(frequency) by time      6.86 μs <
    
     Memory estimate: 0 bytes, allocs estimate: 0.
    #new
    julia> @benchmark @. $yc64 = inv($xc64)
    BenchmarkTools.Trial: 10000 samples with 9 evaluations.
     Range (min … max):  2.008 μs …   6.809 μs  ┊ GC (min … max): 0.00% … 0.00%
     Time  (median):     2.118 μs               ┊ GC (median):    0.00%
     Time  (mean ± σ):   2.209 μs ± 301.481 ns  ┊ GC (mean ± σ):  0.00% ± 0.00%
    
      ▆▆▄█▂▄█▅▂▂▃▁ ▁▂ ▁                                           ▂
      ██████████████████▇███▇▆▆▆▆▆▆▅▆▅▆▆▅▅▆▆▆▅▅▆▅▆▆▅▄▅▅▅▄▅▂▄▅▅▃▂▄ █
      2.01 μs      Histogram: log(frequency) by time      3.73 μs <
    
     Memory estimate: 0 bytes, allocs estimate: 0.
    julia> xc64 = 1e200.+rand(ComplexF64, 1024); yc64=similar(xc64);
    #old
    julia> @benchmark @. $yc64 = inv($xc64)
    @b^[[ABenchmarkTools.Trial: 10000 samples with 6 evaluations.
     Range (min … max):  5.465 μs …  10.222 μs  ┊ GC (min … max): 0.00% … 0.00%
     Time  (median):     5.482 μs               ┊ GC (median):    0.00%
     Time  (mean ± σ):   5.749 μs ± 360.455 ns  ┊ GC (mean ± σ):  0.00% ± 0.00%
    
      █                          ▇▂                               ▁
      █▆▃▄▄▅▃▄▄▅▅▅▆▇▅▆▆▆▆▇▆▇▄▅▆▆▆██▇▆▇▇▇▇█▆▇▇▇▆▆▇▇▇▆▆▆▅▆▆▆▆▆▄▅▅▄▄ █
      5.46 μs      Histogram: log(frequency) by time      6.57 μs <
    
     Memory estimate: 0 bytes, allocs estimate: 0.
    #new
    julia> @benchmark @. $yc64 = inv($xc64)
    BenchmarkTools.Trial: 10000 samples with 6 evaluations.
     Range (min … max):  5.683 μs …  18.619 μs  ┊ GC (min … max): 0.00% … 0.00%
     Time  (median):     6.214 μs               ┊ GC (median):    0.00%
     Time  (mean ± σ):   6.033 μs ± 479.948 ns  ┊ GC (mean ± σ):  0.00% ± 0.00%
    
      █           ▅█▁▁        ▁                                   ▂
      █▅▄▃▄▆▇▇▇█▇▇████████▇▇███▇▅▆▅▅▆▅▄▁▄▃▄▅▃▄▄▃▄▅▄▄▁▁▃▁▁▁▄▁▁▁▁▅▆ █
      5.68 μs      Histogram: log(frequency) by time       8.1 μs <
    
     Memory estimate: 0 bytes, allocs estimate: 0.
    ```
    So for most values that will appear in practice this is 2x faster (but it's 20% slower for values that risk overflow.

--- base/complex.jl
+++ base/complex.jl
@@ -472,10 +472,13 @@ function inv(z::Complex{T}) where T<:Union{Float16,Float32}
 end
 function inv(w::ComplexF64)
     c, d = reim(w)
-    (isinf(c) | isinf(d)) && return complex(copysign(0.0, c), flipsign(-0.0, d))
     absc, absd = abs(c), abs(d)
-    cd = ifelse(absc>absd, absc, absd) # cheap `max`: don't need sign- and nan-checks here
-
+    cd = ifelse(absc>absd, absc, absd)
+    if sqrt(floatmin(Float64)/2) <= cd <= sqrt(floatmax(Float64)/2)
+        return conj(w) / abs2(w)
+    end
+    (isinf(c) | isinf(d)) && return complex(copysign(0.0, c), flipsign(-0.0, d))
+    
     ϵ  = eps(Float64)
     bs = 2/(ϵ*ϵ)
 
@@ -493,7 +496,7 @@ function inv(w::ComplexF64)
     else
         q, p = robust_cinv(-d, -c)
     end
-    return ComplexF64(p*s, q*s) # undo scaling
+    return ComplexF64(p*s, q*s)
 end
 function robust_cinv(c::Float64, d::Float64)
     r = d/c
