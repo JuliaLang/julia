57b1994ec89ee86f06f5a6cd1905148765a2b7e6
Simeon David Schaub <schaub@mit.edu>
2022-10-26 14:31:53 +0200

    fix inference of `split_rest`
    fixes #47326

--- test/tuple.jl
+++ test/tuple.jl
@@ -757,3 +757,16 @@ g42457(a, b) = Base.isequal(a, b) ? 1 : 2.0
 
 # issue #46049: setindex(::Tuple) regression
 @inferred Base.setindex((1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16), 42, 1)
+
+# issue #47326
+function fun1_47326(args...)
+    head..., tail = args
+    head
+end
+
+function fun2_47326(args...)
+    head, tail... = args
+    tail
+end
+@test @inferred(fun1_47326(1,2,3)) === (1, 2)
+@test @inferred(fun2_47326(1,2,3)) === (2, 3)
