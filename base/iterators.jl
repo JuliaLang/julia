39c790fb01e775e486198f2d9c8f05e722f84bbe
Jerry Ling <proton@jling.dev>
2022-10-27 10:50:06 -0400

    0 element

--- base/iterators.jl
+++ base/iterators.jl
@@ -399,8 +399,8 @@ eltype(::Type{Zip{Is}}) where {Is<:Tuple} = Tuple{map(eltype, fieldtypes(Is))...
 end
 @inline _zip_any_isdone(::Tuple{}, ::Tuple{}) = false
 
-iterate(::Zip{Tuple{}}) = ((), nothing)
-iterate(::Zip{Tuple{}}, nothing) = nothing
+length(::Zip{Tuple{}}) = 0
+iterate(::Zip{Tuple{}}) = nothing
 @propagate_inbounds iterate(z::Zip) = _zip_iterate_all(z.is, Base.map(_ -> (), z.is))
 @propagate_inbounds iterate(z::Zip, ss) = _zip_iterate_all(z.is, Base.map(tuple, ss))
 
