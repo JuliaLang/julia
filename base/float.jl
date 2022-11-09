63019978a45e67102b4762248d75f2fcee7c6df2
Gabriel Baraldi <baraldigabriel@gmail.com>
2022-11-09 17:01:54 -0300

    Also check if x is equal to 0

--- base/float.jl
+++ base/float.jl
@@ -524,7 +524,7 @@ function fmod_internal(x::T, y::T) where {T<:IEEEFloat}
 end
 
 function fmod(x::T, y::T) where {T<:IEEEFloat}
-    if (!isnan(x) && isfinite(x) && !iszero(y) && !isnan(y) && isfinite(y) && !iszero(y))
+    if (!isnan(x) && isfinite(x) && !iszero(x) && !isnan(y) && isfinite(y) && !iszero(y))
         xabs = abs(x)
         yabs = abs(y)
         res = fmod_internal(xabs, yabs)
