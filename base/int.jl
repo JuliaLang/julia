3fbbafeb0b2c75f5341d98e7c3013496e07cc80b
oscarddssmith <oscar.smith@juliacomputing.com>
2022-11-14 18:07:31 -0500

    remove redundant comment

--- base/int.jl
+++ base/int.jl
@@ -504,7 +504,6 @@ trailing_ones(x::Integer) = trailing_zeros(~x)
 >>>(x::BitInteger, y::BitUnsigned) = lshr_int(x, y)
 # signed shift counts can shift in either direction
 # note: this early during bootstrap, `>=` is not yet available
-# note: we only define Int shift counts here; the generic case is handled later
 >>(x::BitInteger, y::Int) =
     ifelse(0 <= y, x >> unsigned(y), x << unsigned(-y))
 >>>(x::BitInteger, y::Int) =
