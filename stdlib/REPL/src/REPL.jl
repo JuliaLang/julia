d12c9cb6803613c3dc439449710de4394f51670c
Rashid Rafeek <rashidrafeek@gmail.com>
2022-11-28 12:14:05 +0530

    Fix REPL keybinding CTRL-Q for stdlib methods (#47637)
    (cherry picked from commit 7514bcf0bda547012f19a071daa132c3e8e97613)

--- stdlib/REPL/src/REPL.jl
+++ stdlib/REPL/src/REPL.jl
@@ -1085,7 +1085,7 @@ function setup_interface(
             if n <= 0 || n > length(linfos) || startswith(linfos[n][1], "REPL[")
                 @goto writeback
             end
-            InteractiveUtils.edit(linfos[n][1], linfos[n][2])
+            InteractiveUtils.edit(Base.fixup_stdlib_path(linfos[n][1]), linfos[n][2])
             LineEdit.refresh_line(s)
             return
             @label writeback
