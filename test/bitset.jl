43194259c819cd92ed15a4e089db329c573ca930
Lilith Orion Hafner <lilithhafner@gmail.com>
2022-12-02 06:41:56 +0600

    Change expected error type in tests

--- test/bitset.jl
+++ test/bitset.jl
@@ -348,8 +348,8 @@ end
     x = BitSet(rand(-1000:1000, 500))
     y = copy(x)
     @test union!(x, BitSet(a:b)) == union!(y, BitSet(a:1:b))
-    @test_throws ArgumentError BitSet(Int128(typemin(Int))-1:typemin(Int))
-    @test_throws ArgumentError BitSet(typemax(Int):Int128(typemax(Int))+1)
+    @test_throws InexactError BitSet(Int128(typemin(Int))-1:typemin(Int))
+    @test_throws InexactError BitSet(typemax(Int):Int128(typemax(Int))+1)
     # union! with an empty range doesn't modify the BitSet
     @test union!(x, b:a) == y
 end
