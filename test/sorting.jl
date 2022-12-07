f37e6c49a1107c7593430b6d01520e6f62a41cb4
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-12-07 09:23:47 +0600

    add test demonstrating overflow in countsort

--- test/sorting.jl
+++ test/sorting.jl
@@ -897,6 +897,7 @@ end
 @testset "Count sort near the edge of its range" begin
     @test issorted(sort(rand(typemin(Int):typemin(Int)+100, 1000)))
     @test issorted(sort(rand(typemax(Int)-100:typemax(Int), 1000)))
+    @test issorted(sort(rand(Int8, 600)))
 end
 
 # This testset is at the end of the file because it is slow.
