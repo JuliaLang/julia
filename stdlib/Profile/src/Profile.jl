7c539fe4c0b74edfb843db5ef8d6fdafc181efc7
K Pamnany <kpamnany@users.noreply.github.com>
2022-12-09 18:35:17 -0500

    47320.patch
    A couple of tidy ups on 47300.

--- stdlib/Profile/src/Profile.jl
+++ stdlib/Profile/src/Profile.jl
@@ -1262,7 +1262,7 @@ function take_heap_snapshot(filepath::String, all_one::Bool=false)
     return filepath
 end
 function take_heap_snapshot(all_one::Bool=false)
-    f = joinpath(pwd(), "$(getpid())_$(time_ns()).heapsnapshot")
+    f = abspath("$(getpid())_$(time_ns()).heapsnapshot")
     return take_heap_snapshot(f, all_one)
 end
 
