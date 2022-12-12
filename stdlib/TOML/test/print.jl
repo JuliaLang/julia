808d6ad98b64e8b6620a31d80b3a5c2807ad7bcb
Fons van der Plas <fonsvdplas@gmail.com>
2022-12-12 15:35:42 +0100

    testset

--- stdlib/TOML/test/print.jl
+++ stdlib/TOML/test/print.jl
@@ -80,21 +80,21 @@ loaders = ["gzip", { driver = "csv", args = {delim = "\t"}}]
 @test roundtrip(str)
 
 
-# Array of dictionaries and non-dictionaries
-# https://github.com/JuliaLang/julia/issues/45340
-d =  Dict("b" => Any[111, Dict("a" =>  222, "d" => 333)])
-@test toml_str(d) == "b = [111, {a = 222, d = 333}]\n"
+@testset "vec with dicts and non-dicts" begin
+    # https://github.com/JuliaLang/julia/issues/45340
+    d =  Dict("b" => Any[111, Dict("a" =>  222, "d" => 333)])
+    @test toml_str(d) == "b = [111, {a = 222, d = 333}]\n"
 
-d =  Dict("b" => Any[Dict("a" =>  222, "d" => 333), 111])
-@test toml_str(d) == "b = [{a = 222, d = 333}, 111]\n"
+    d =  Dict("b" => Any[Dict("a" =>  222, "d" => 333), 111])
+    @test toml_str(d) == "b = [{a = 222, d = 333}, 111]\n"
 
-d =  Dict("b" => Any[Dict("a" =>  222, "d" => 333)])
-@test toml_str(d) == """
-[[b]]
-a = 222
-d = 333
-"""
-
+    d =  Dict("b" => Any[Dict("a" =>  222, "d" => 333)])
+    @test toml_str(d) == """
+    [[b]]
+    a = 222
+    d = 333
+    """
+end
 
 struct Foo
     a::Int64
