6764e91a3e498fbee62c7ec3464d748e25f3ba1a
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2023-01-03 16:03:18 -0500

    Simplify multiversioning
    (cherry picked from commit 615b142c88a074399bac08a0e8fd8f48b491c1fd)
    
    Refactor aotcompile
    
    (cherry picked from commit 27808e136757c19c9d7accfafae3958f3f48b7f1)
    
    Timing print statements
    
    (cherry picked from commit 4524987a384f444e02c0e21afacfc3c4f4d68a4e)
    
    Move image init to processor.cpp
    
    (cherry picked from commit 094269c8c1e506e36f0b4bd7ddc6ec38f279bb3c)
    
    Annotate information before running optimization
    
    (cherry picked from commit 2c7375cbb0c5ab7d331829d7a55d97881cd33255)
    
    Table-based dlsym
    
    (cherry picked from commit 6ab1862106bc7f48afa54bac792cb7909df35cd7)
    
    Allow loader to deal with multiple shards
    
    (cherry picked from commit 798ee2245b6aae597a99d25f27aa3ed96cf3c2aa)
    
    Multithreaded image builder
    
    (cherry picked from commit 3915101dc65d3d0844cf8e0f5d5a1e39ddf97407)
    
    Don't try to extract indexes during partitioning
    
    (cherry picked from commit 4ad943da621f5d696ebfdf853ac03aa742edec65)
    
    Fix whitespace
    
    (cherry picked from commit d717fa7023ab104290a136b955511c38e1416a4c)
    
    Fix warnings
    
    (cherry picked from commit fe0600d2dc74e1381ac3018fe12835fafc25d529)
    
    Set reloc slot to be external linkage
    
    (cherry picked from commit bdf65f4b4e8a1c8f1a058fe8fc69a6b4c56acf80)
    
    Formalize printing more, correct module weight estimation with multiversioning
    
    (cherry picked from commit 4fc5bed6b5fa58f056ee5ee87730bea2ac17fa8c)
    
    Alter naming, sort partitions
    
    (cherry picked from commit 0c68e4af4d80e4dccfb682274b3130a699be2309)
    
    Fix whitespace
    
    (cherry picked from commit f9da0e261abf505af4a5841d7114efd7f499c755)
    
    Avoid unused function warning
    
    (cherry picked from commit 9a72be669b5cac14cd9a2228563e12494d6d7717)
    
    Check relocations for generic target as well
    
    (cherry picked from commit 1f07ea51faecfdcad80aeafa99e93bb65249f369)
    
    Debug macos linker
    
    (cherry picked from commit 83b196758b9fabbe64b36750f56d50e083f87020)
    
    Respect JULIA_CPU_THREADS
    
    (cherry picked from commit c98ff304ab697f0eb492a150833c12baa499f29e)
    
    Don't inject CRT aliases on macos
    
    (cherry picked from commit 8cf48f2369a4197c46858e0bc6c166ad69d3e8d4)
    
    Clean up timers and prints, link to JULIA_IMAGE_TIMINGS
    
    (cherry picked from commit 4e35f416bb71f0a33ad3605742bfa8f6bc82a85b)
    
    Fix whitespace
    
    (cherry picked from commit a723211c3106d6eebfbbbb680615269995cab0ec)
    
    Don't leave aliases to extern global objects
    
    (cherry picked from commit 7cf839aaeb636e27b75c9857fe52913477ac1734)
    
    Break multiversioning's dependency on jl_get_llvm_clone_targets
    
    (cherry picked from commit fa208d43a95e1336c6e793795aad8134cb72883b)
    
    Add multiversioning annotation test
    
    (cherry picked from commit 3dcd1a23ac16748daa6770d44bf0825fa3981767)
    
    Couple more tests for multiversioning
    
    (cherry picked from commit b3d3ffbc3384451819aa9d1886f9c7230969411e)
    
    Inject CRT aliases with internal linkage within every shard
    
    (cherry picked from commit e75e362dc936b2bf98028e887ec075f50c928c6b)
    
    Expand on the multiversioning tests
    
    (cherry picked from commit 65e6de2a6265243cb750b3551d5ac86029e7ffad)
    
    Remove stray debug prints
    
    (cherry picked from commit 556122393ab3762f6d19fc3f19c83739065b8c28)
    
    Track gvar count
    
    (cherry picked from commit fef319cf11394caf3526460758d4e57196bd2322)
    
    Add more assertions
    
    (cherry picked from commit acc54d9e93567de0077ba248206f1a02f4d7b0cd)
    
    Move dbgs under LLVM_DEBUG
    
    (cherry picked from commit 27f1ccdf55078670d4f4f0a7407e53df580134d5)
    
    Add some documentation
    
    (cherry picked from commit 6b8ec27dbc582ba67f717e400b1bcff8f886c6d3)
    
    Update documentation
    
    (cherry picked from commit 5108b4036d7610fff1ef6e56c80f760a05d2c4d0)

--- doc/src/devdocs/sysimg.md
+++ doc/src/devdocs/sysimg.md
@@ -8,6 +8,9 @@ as many platforms as possible, so as to give vastly improved startup times.  On 
 not ship with a precompiled system image file, one can be generated from the source files shipped
 in Julia's `DATAROOTDIR/julia/base` folder.
 
+Julia will by default generate its system image on half of the available system threads. This
+may be controlled by the [`JULIA_IMAGE_THREADS`](@ref env-image-threads) environment variable.
+
 This operation is useful for multiple reasons.  A user may:
 
   * Build a precompiled shared library system image on a platform that did not ship with one, thereby
--- src/processor.h
+++ src/processor.h
@@ -14,82 +14,9 @@
 extern "C" {
 #endif
 
-/**
- * Related sysimg exported symbols
- *
- * In the following text, function refers to an abstract entity.
- * It corresponds to a `Function` that we emit in the codegen, and there might be multiple copies
- * of it in the system image. Only one of those copies will be used in a given session.
- * Function pointers refer to a real piece of code in the system image.
- * Each function might have multiple function pointers in the system image
- * and each function pointer will correspond to only one function.
- *
- * # Global function and base pointers
- * `jl_sysimg_gvars_base`:
- *     The address of this symbol is the base data pointer
- *     (all other data pointers are stored as offsets to this address)
- * `jl_sysimg_fvars_base`:
- *     The address of this symbol is the base function pointer
- *     (all other function pointers are stored as offsets to this address)
- * `jl_sysimg_fvars_offsets`: [static data]
- *     The array of function pointer offsets (`int32_t`) from the base pointer.
- *     This includes all julia functions in sysimg as well as all other functions that are cloned.
- *     The default function pointer is used if the function is cloned.
- *     The first element is the size of the array, which should **NOT** be used as the number
- *     of julia functions in the sysimg.
- *     Each entry in this array uniquely identifies a function we are interested in
- *     (the function may have multiple function pointers corresponding to different versions).
- *     In other sysimg info, all references to functions are stored as their `uint32_t` index
- *     in this array.
- *
- * # Target data and dispatch slots (Only needed by runtime during loading)
- * `jl_dispatch_target_ids`: [static data] serialize target data.
- *     This contains the number of targets which is needed to decode `jl_dispatch_fvars_idxs`
- *     in addition to the name and feature set of each target.
- * `jl_dispatch_reloc_slots`: [static data] location and index of relocation slots.
- *     Stored as pairs of function indices and `int32_t` offsets from `jl_sysimg_gvars_base`.
- *     The first element is an `uint32_t` giving the number of relocations.
- *     This is needed for functions whose address is used in a way that requires dispatch.
- *     We currently only support one type of relocation (i.e. absolute pointer) which is enough
- *     for all use in functions as well as GOT slot (for "PLT" callback).
- *     Note that not all functions being cloned are assigned a slot.
- *     This array is sorted by the function indices.
- *     There can be more than one slot per-function,
- *     i.e. there can be duplicated function indices.
- *
- * # Target functions
- * `jl_dispatch_fvars_idxs`: [static data] Target-specific function indices.
- *     For each target, this includes a tagged `uint32_t` length, an optional `uint32_t` index
- *     of the base target followed by an array of tagged function indices.
- *     The base target index is required to be smaller than the index of the current target
- *     and must be the default (`0`) or a `clone_all` target.
- *     If it's not `0`, the function pointer array for the `clone_all` target will be used as
- *     the base function pointer offsets instead.
- *     The tag bits for both the length and the indices are the top bit.
- *     A tagged length indicates that all of the functions are cloned and the indices follows
- *     are the ones that requires relocation. The base target index is omitted in this case.
- *     Otherwise, the length is the total number of functions that we are interested in
- *     for this target, which includes all cloned julia functions and
- *     all other cloned functions that requires relocation.
- *     A tagged index means that the function pointer should be filled into the GOT slots
- *     identified by `jl_dispatch_reloc_slots`. There could be more than one slot per function.
- *     (Note that a tagged index could corresponds to a functions pointer that's the same as
- *     the base one since this is the only way we currently represent relocations.)
- *     A tagged length implicitly tags all the indices and the indices will not have the tag bit
- *     set. The lengths in this variable is needed to decode `jl_dispatch_fvars_offsets`.
- * `jl_dispatch_fvars_offsets`: [static data] Target-specific function pointer offsets.
- *     This contains all the cloned functions that we are interested in and it needs to be decoded
- *     and used along with `jl_dispatch_fvars_idxs`.
- *     For the default target, there's no entries in this variable, if there's any relocations
- *     needed for the default target, the function pointers are taken from the global offset
- *     arrays directly.
- *     For a `clone_all` target (i.e. with the length in `jl_dispatch_fvars_idxs` tagged), this
- *     variable contains an offset array of the same length as the global one. Only the indices
- *     appearing in `jl_dispatch_fvars_idxs` need relocation and the dispatch code should return
- *     this array as the original/base function offsets.
- *     For other targets, this variable contains an offset array with the length defined in
- *     `jl_dispatch_fvars_idxs`. Tagged indices need relocations.
- */
+// Image metadata
+// Every image exports a `jl_image_pointers_t` as a global symbol `jl_image_pointers`.
+// This symbol acts as a root for all other code-related symbols in the image.
 
 enum {
     JL_TARGET_VEC_CALL = 1 << 0,
@@ -155,6 +82,127 @@ typedef struct _jl_image_fptrs_t {
     const uint32_t *clone_idxs;
 } jl_image_fptrs_t;
 
+typedef struct {
+    uint64_t base;
+    uintptr_t *gvars_base;
+    const int32_t *gvars_offsets;
+    uint32_t ngvars;
+    jl_image_fptrs_t fptrs;
+} jl_image_t;
+
+// The header for each image
+// Details important counts about the image
+typedef struct {
+    // The version of the image format
+    // Most up-to-date version is 1
+    uint32_t version;
+    // The number of shards in this image
+    uint32_t nshards;
+    // The total number of fvars in this image among all shards
+    uint32_t nfvars;
+    // The total number of gvars in this image among all shards
+    uint32_t ngvars;
+} jl_image_header_t;
+
+// Per-shard data for image shards. Each image contains header->nshards of these.
+typedef struct {
+
+    // This is the base function pointer
+    // (all other function pointers are stored as offsets to this address)
+    const char *fvar_base;
+    // The array of function pointer offsets (`int32_t`) from the base pointer.
+    // This includes all julia functions in sysimg as well as all other functions that are cloned.
+    // The default function pointer is used if the function is cloned.
+    // The first element is the size of the array, which should **NOT** be used as the number
+    // of julia functions in the sysimg.
+    // Each entry in this array uniquely identifies a function we are interested in
+    // (the function may have multiple function pointers corresponding to different versions).
+    // In other sysimg info, all references to functions are stored as their `uint32_t` index
+    // in this array.
+    const int32_t *fvar_offsets;
+    // This is the mapping of shard function index -> global function index
+    // staticdata.c relies on the same order of functions in the global function array being
+    // the same as what it saw when serializing the global function array. However, partitioning
+    // into multiple shards will cause functions to be reordered. This array is used to map
+    // back to the original function array for loading.
+    const uint32_t *fvar_idxs;
+    // This is the base data pointer
+    // (all other data pointers in this shard are stored as offsets to this address)
+    uintptr_t *gvar_base;
+    // This array of global variable offsets (`int32_t`) from the base pointer.
+    // Similar to fvar_offsets, but for gvars
+    const int32_t *gvar_offsets;
+    // This is the mapping of shard global variable index -> global global variable index
+    // Similar to fvar_idxs, but for gvars
+    const uint32_t *gvar_idxs;
+    // location and index of relocation slots.
+    // Stored as pairs of function indices and `int32_t` offsets from `jl_sysimg_gvars_base`.
+    // The first element is an `uint32_t` giving the number of relocations.
+    // This is needed for functions whose address is used in a way that requires dispatch.
+    // We currently only support one type of relocation (i.e. absolute pointer) which is enough
+    // for all use in functions as well as GOT slot (for "PLT" callback).
+    // Note that not all functions being cloned are assigned a slot.
+    // This array is sorted by the function indices.
+    // There can be more than one slot per-function,
+    // i.e. there can be duplicated function indices.
+    const int32_t *clone_slots;
+    //  Target-specific function pointer offsets.
+    //  This contains all the cloned functions that we are interested in and it needs to be decoded
+    //  and used along with `jl_dispatch_fvars_idxs`.
+    //  For the default target, there's no entries in this variable, if there's any relocations
+    //  needed for the default target, the function pointers are taken from the global offset
+    //  arrays directly.
+    //  For a `clone_all` target (i.e. with the length in `jl_dispatch_fvars_idxs` tagged), this
+    //  variable contains an offset array of the same length as the global one. Only the indices
+    //  appearing in `jl_dispatch_fvars_idxs` need relocation and the dispatch code should return
+    //  this array as the original/base function offsets.
+    //  For other targets, this variable contains an offset array with the length defined in
+    //  `jl_dispatch_fvars_idxs`. Tagged indices need relocations.
+    const int32_t *clone_offsets;
+    //  Target-specific function indices.
+    //  For each target, this includes a tagged `uint32_t` length, an optional `uint32_t` index
+    //  of the base target followed by an array of tagged function indices.
+    //  The base target index is required to be smaller than the index of the current target
+    //  and must be the default (`0`) or a `clone_all` target.
+    //  If it's not `0`, the function pointer array for the `clone_all` target will be used as
+    //  the base function pointer offsets instead.
+    //  The tag bits for both the length and the indices are the top bit.
+    //  A tagged length indicates that all of the functions are cloned and the indices follows
+    //  are the ones that requires relocation. The base target index is omitted in this case.
+    //  Otherwise, the length is the total number of functions that we are interested in
+    //  for this target, which includes all cloned julia functions and
+    //  all other cloned functions that requires relocation.
+    //  A tagged index means that the function pointer should be filled into the GOT slots
+    //  identified by `jl_dispatch_reloc_slots`. There could be more than one slot per function.
+    //  (Note that a tagged index could corresponds to a functions pointer that's the same as
+    //  the base one since this is the only way we currently represent relocations.)
+    //  A tagged length implicitly tags all the indices and the indices will not have the tag bit
+    //  set. The lengths in this variable is needed to decode `jl_dispatch_fvars_offsets`.
+    const uint32_t *clone_idxs;
+} jl_image_shard_t;
+
+// The TLS data for each image
+typedef struct {
+    void *pgcstack_func_slot;
+    void *pgcstack_key_slot;
+    size_t *tls_offset;
+} jl_image_ptls_t;
+
+//The root struct for images, points to all the other globals
+typedef struct {
+    // The image header, contains numerical global data
+    const jl_image_header_t *header;
+    // The shard table, contains per-shard data
+    const jl_image_shard_t *shards; // points to header->nshards length array
+    // The TLS data
+    const jl_image_ptls_t *ptls;
+
+    //  serialized target data
+    //  This contains the number of targets
+    //  in addition to the name and feature set of each target.
+    const void *target_data;
+} jl_image_pointers_t;
+
 /**
  * Initialize the processor dispatch system with sysimg `hdl` (also initialize the sysimg itself).
  * The dispatch system will find the best implementation to be used in this session.
@@ -165,8 +213,8 @@ typedef struct _jl_image_fptrs_t {
  *
  * Return the data about the function pointers selected.
  */
-jl_image_fptrs_t jl_init_processor_sysimg(void *hdl);
-jl_image_fptrs_t jl_init_processor_pkgimg(void *hdl);
+jl_image_t jl_init_processor_sysimg(void *hdl);
+jl_image_t jl_init_processor_pkgimg(void *hdl);
 
 // Return the name of the host CPU as a julia string.
 JL_DLLEXPORT jl_value_t *jl_get_cpu_name(void);
