15018b6ce52281f0ec3f6544ce105ee25f0eaf92
Benny <mail2benny+github@gmail.com>
2023-01-03 06:29:20 +0200

    One less potential allocation for BigInt binomial implementation

--- base/gmp.jl
+++ base/gmp.jl
@@ -685,7 +685,7 @@ factorial(x::BigInt) = isneg(x) ? BigInt(0) : MPZ.fac_ui(x)
 function binomial(n::BigInt, k::Integer)
     k < 0 && return BigInt(0)
     k <= typemax(UInt) && return binomial(n, UInt(k))
-    n < 0 && return (isodd(k) ? -one(n) : one(n)) * binomial(k - n - 1, k)
+    n < 0 && return isodd(k) ? -binomial(k - n - 1, k) : binomial(k - n - 1, k)
     κ = n - k
     κ < 0 && return BigInt(0)
     κ <= typemax(UInt) && return binomial(n, UInt(κ))
