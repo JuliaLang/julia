463e5f0fef2665497438b06bd5ad0e326015530e
Kristoffer Carlsson <kcarlsson89@gmail.com>
2023-01-06 04:43:23 +0100

    add a note to `ismutable` docstring about `String` and `Symbol` (#48149)

--- base/reflection.jl
+++ base/reflection.jl
@@ -527,6 +527,10 @@ Return `true` if and only if value `v` is mutable.  See [Mutable Composite Types
 for a discussion of immutability. Note that this function works on values, so if you
 give it a `DataType`, it will tell you that a value of the type is mutable.
 
+!!! note
+    For technical reasons, `ismutable` returns `true` for values of certain special types
+    (for example `String` and `Symbol`) even though they cannot be mutated in a permissible way.
+
 See also [`isbits`](@ref), [`isstructtype`](@ref).
 
 # Examples
