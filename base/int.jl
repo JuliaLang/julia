896f0d4c3a2d5666b86ba9e77f35cddaf95e7ab5
Mosè Giordano <mose@gnu.org>
2023-01-09 22:27:58 +0100

    Fix `top_set_bit` doctest now that it's actually running

--- base/int.jl
+++ base/int.jl
@@ -496,13 +496,13 @@ See also: [`ndigits0z`](@ref), [`ndigits`](@ref).
 
 # Examples
 ```jldoctest
-julia> top_set_bit(4)
+julia> Base.top_set_bit(4)
 3
 
-julia> top_set_bit(0)
+julia> Base.top_set_bit(0)
 0
 
-julia> top_set_bit(-1)
+julia> Base.top_set_bit(-1)
 64
 ```
 """
