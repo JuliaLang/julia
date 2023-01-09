a48ad5304993c62112f435ebed6834c07d9ce4f4
Mosè Giordano <mose@gnu.org>
2023-01-09 21:19:02 +0100

    Revert "[CompilerSupportLibraries_jll] Add libssp for more platforms (#48027)"
    This reverts commit 0f2665f2ee66483afd7ec3ca94ab994efc93833c.

--- stdlib/CompilerSupportLibraries_jll/src/CompilerSupportLibraries_jll.jl
+++ stdlib/CompilerSupportLibraries_jll/src/CompilerSupportLibraries_jll.jl
@@ -43,15 +43,11 @@ elseif Sys.isapple()
     const libgfortran = string("@rpath/", "libgfortran.", libgfortran_version(HostPlatform()).major, ".dylib")
     const libstdcxx = "@rpath/libstdc++.6.dylib"
     const libgomp = "@rpath/libgomp.1.dylib"
-    const libssp = "@rpath/libssp.0.dylib"
 else
     const libgcc_s = "libgcc_s.so.1"
     const libgfortran = string("libgfortran.so.", libgfortran_version(HostPlatform()).major)
     const libstdcxx = "libstdc++.so.6"
     const libgomp = "libgomp.so.1"
-    if libc(HostPlatform()) != "musl"
-        const libssp = "libssp.so.0"
-    end
 end
 
 function __init__()
@@ -63,7 +59,7 @@ function __init__()
     global libstdcxx_path = dlpath(libstdcxx_handle)
     global libgomp_handle = dlopen(libgomp)
     global libgomp_path = dlpath(libgomp_handle)
-    @static if libc(HostPlatform()) != "musl"
+    if Sys.iswindows()
         global libssp_handle = dlopen(libssp)
         global libssp_path = dlpath(libssp_handle)
     end
