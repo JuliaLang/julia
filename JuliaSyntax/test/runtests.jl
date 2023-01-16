7c3ebe9795225386907d54c831437b1672a63c74
Claire Foster <aka.c42f@gmail.com>
2023-01-16 12:17:01 +1000

    Test the test_utils from runtests.jl

--- JuliaSyntax/test/runtests.jl
+++ JuliaSyntax/test/runtests.jl
@@ -7,11 +7,28 @@ using JuliaSyntax: GreenNode, SyntaxNode,
     flags, EMPTY_FLAGS, TRIVIA_FLAG, INFIX_FLAG,
     children, child, setchild!, SyntaxHead
 
+include("test_utils.jl")
+# Tests for the test_utils go here to allow the utils to be included on their
+# own without invoking the tests.
+@testset "Test tools" begin
+    @test exprs_roughly_equal(Expr(:global, :x, :y),
+                              Expr(:global, Expr(:tuple, :x, :y)))
+    @test exprs_roughly_equal(Expr(:local, :x, :y),
+                              Expr(:local, Expr(:tuple, :x, :y)))
+    @test exprs_roughly_equal(1.5,
+                              Expr(:call, :*, 1.5, :f))
+    @test exprs_roughly_equal(1.5,
+                              Expr(:call, :*, 1.5, :f0))
+    @test exprs_roughly_equal(Expr(:do, Expr(:macrocall, Symbol("@f"), LineNumberNode(1), Expr(:kw, :a, 1)),
+                                   Expr(:->, Expr(:tuple), Expr(:block, LineNumberNode(1)))),
+                              Expr(:do, Expr(:macrocall, Symbol("@f"), LineNumberNode(1), Expr(:(=), :a, 1)),
+                                   Expr(:->, Expr(:tuple), Expr(:block, LineNumberNode(1)))))
+end
+
 @testset "Tokenize" begin
     include("tokenize.jl")
 end
 
-include("test_utils.jl")
 include("parse_stream.jl")
 include("parser.jl")
 include("diagnostics.jl")
