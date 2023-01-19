b8ca7cfa2c996ff12ed0de6cc61f7fad4d3dfa88
Keno Fischer <keno@juliacomputing.com>
2023-01-19 08:36:28 -0500

    Improve effects for pairs(::NamedTuple) (#48344)
    The `new` syntax as well as the implicit inner constructor
    introduces an implicit `convert(fieldtype(T, n), x)` for every
    field. This convert is generally a no-op (because the fieldtype
    comes from the type parameter, which in turn comes from the
    type of `x`). However, the compiler cannot prove this and a
    `convert` call with unknown types taints all effects. Avoid
    that by manually avoiding the `convert` call. It is a bit
    ugly, but there isn't really another way to write it at
    the moment. In the future we may want to have some nicer
    way to disable the implicit generation of `convert` calls.
    
    Co-authored-by: Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>

--- base/iterators.jl
+++ base/iterators.jl
@@ -290,8 +290,8 @@ length(v::Pairs) = length(getfield(v, :itr))
 axes(v::Pairs) = axes(getfield(v, :itr))
 size(v::Pairs) = size(getfield(v, :itr))
 
-@propagate_inbounds function _pairs_elt(p::Pairs{K, V}, idx) where {K, V}
-    return Pair{K, V}(idx, getfield(p, :data)[idx])
+Base.@eval @propagate_inbounds function _pairs_elt(p::Pairs{K, V}, idx) where {K, V}
+    return $(Expr(:new, :(Pair{K, V}), :idx, :(getfield(p, :data)[idx])))
 end
 
 @propagate_inbounds function iterate(p::Pairs{K, V}, state...) where {K, V}
