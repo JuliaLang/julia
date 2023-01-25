6c3808bab35dce245937809bb7d1b4be8c9ddc1d
Tim Besard <tim.besard@gmail.com>
2023-01-25 08:31:11 +0100

    Add a test verifying codegen of non-concrete varargs (#48395)
    This was issue #34459, fixed by #46953.

--- test/compiler/codegen.jl
+++ test/compiler/codegen.jl
@@ -797,3 +797,10 @@ f48085(@nospecialize x...) = length(x)
 
 # Make sure that the bounds check is elided in tuple iteration
 @test !occursin("call void @", get_llvm(iterate, Tuple{NTuple{4, Float64}, Int}))
+
+# issue #34459
+function f34459(args...)
+    Base.pointerset(args[1], 1, 1, 1)
+    return
+end
+@test !occursin("jl_f_tuple", get_llvm(f34459, Tuple{Ptr{Int}, Type{Int}}, true, false, false))
