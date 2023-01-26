bbe8b29e966ef6da9a2ea979f90454bb96249e6e
Pete Vilter <pete.vilter@gmail.com>
2023-01-26 12:15:37 -0500

    add start time

--- src/task.c
+++ src/task.c
@@ -312,6 +312,7 @@ static _Atomic(jl_function_t*) task_done_hook_func JL_GLOBALLY_ROOTED = NULL;
 void JL_NORETURN jl_finish_task(jl_task_t *ct)
 {
     JL_PROBE_RT_FINISH_TASK(ct);
+    ct->cpu_time_ns += jl_hrtime() - ct->last_scheduled_time_ns;
     JL_SIGATOMIC_BEGIN();
     if (jl_atomic_load_relaxed(&ct->_isexception))
         jl_atomic_store_release(&ct->_state, JL_TASK_STATE_FAILED);
@@ -1251,6 +1252,7 @@ CFI_NORETURN
 
     ct->ctx.started = 1;
     JL_PROBE_RT_START_TASK(ct);
+    ct->last_scheduled_ns = jl_hrtime();
     jl_timing_block_task_enter(ct, ptls, NULL);
     if (jl_atomic_load_relaxed(&ct->_isexception)) {
         record_backtrace(ptls, 0);
