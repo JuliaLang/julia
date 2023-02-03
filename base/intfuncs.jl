JuliaLang/julia

commit fec8304a8c6e2157759b76d20339b426201a5f61

fixed the overflow problem of powermod(x::Integer, p::Integer, m::T) (#48192)
* fixed the overflow problem of `powermod(x::Integer, p::Integer, m::T)`

Co-authored-by: Oscar Smith <oscardssmith@gmail.com>

base/intfuncs.jl
@@ -369,8 +369,19 @@ julia> powermod(5, 3, 19)
 ```
 """
 function powermod(x::Integer, p::Integer, m::T) where T<:Integer
-    p < 0 && return powermod(invmod(x, m), -p, m)
     p == 0 && return mod(one(m),m)
+    # When the concrete type of p is signed and has the lowest value,
+    # `p != 0 && p == -p` is equivalent to `p == typemin(typeof(p))` for 2's complement representation.
+    # but will work for integer types like `BigInt` that don't have `typemin` defined
+    # It needs special handling otherwise will cause overflow problem.
+    if p == -p
+        t = powermod(invmod(x, m), -(p÷2), m)
+        t = mod(widemul(t, t), m)
+        iseven(p) && return t
+        #else odd
+        return mod(widemul(t, invmod(x, m)), m)
+    end
+    p < 0 && return powermod(invmod(x, m), -p, m)
     (m == 1 || m == -1) && return zero(m)
     b = oftype(m,mod(x,m))  # this also checks for divide by zero
 

test/intfuncs.jl
@@ -267,6 +267,12 @@ end
     @test powermod(2, -2, 5) == 4
     @test powermod(2, -1, -5) == -2
     @test powermod(2, -2, -5) == -1
+
+    @test powermod(2, typemin(Int128), 5) == 1
+    @test powermod(2, typemin(Int128), -5) == -4
+
+    @test powermod(2, big(3), 5) == 3
+    @test powermod(2, big(3), -5) == -2
 end
 
 @testset "nextpow/prevpow" begin

commit bebff47519759c752e49c25f6a4ac5431d2b97d3

fix type stability of `powermod` (#48367)
* type stability of powermod regressed in https://github.com/JuliaLang/julia/pull/48192

base/intfuncs.jl
@@ -375,18 +375,20 @@ function powermod(x::Integer, p::Integer, m::T) where T<:Integer
     # but will work for integer types like `BigInt` that don't have `typemin` defined
     # It needs special handling otherwise will cause overflow problem.
     if p == -p
-        t = powermod(invmod(x, m), -(p÷2), m)
-        t = mod(widemul(t, t), m)
-        iseven(p) && return t
+        imod = invmod(x, m)
+        rhalf = powermod(imod, -(p÷2), m)
+        r::T = mod(widemul(rhalf, rhalf), m)
+        isodd(p) && (r = mod(widemul(r, imod), m))
         #else odd
-        return mod(widemul(t, invmod(x, m)), m)
+        return r
+    elseif p < 0
+        return powermod(invmod(x, m), -p, m)
     end
-    p < 0 && return powermod(invmod(x, m), -p, m)
     (m == 1 || m == -1) && return zero(m)
     b = oftype(m,mod(x,m))  # this also checks for divide by zero
 
     t = prevpow(2, p)
-    r::T = 1
+    r = 1
     while true
         if p >= t
             r = mod(widemul(r,b),m)

test/intfuncs.jl
@@ -273,6 +273,8 @@ end
 
     @test powermod(2, big(3), 5) == 3
     @test powermod(2, big(3), -5) == -2
+    @inferred  powermod(2, -2, -5)
+    @inferred  powermod(big(2), -2, UInt(5))
 end
 
 @testset "nextpow/prevpow" begin