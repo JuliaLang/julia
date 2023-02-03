a7f04b82fd34a68d18b6cb94c074a0f199b784c2
Mosè Giordano <mose@gnu.org>
2023-02-03 16:38:53 +0000

    [LinearAlgebra] Fix `libblas_name` test
    Use `Int` as type for integer arguments, instead of `Cint`.
    
    (cherry picked from commit cdc6eafb3681840ddef395aa7ffad45db3064e71)

--- stdlib/LinearAlgebra/test/blas.jl
+++ stdlib/LinearAlgebra/test/blas.jl
@@ -718,7 +718,7 @@ end
 # https://github.com/JuliaLang/julia/issues/48427 again.
 @testset "libblas_name" begin
     dot_sym = dlsym(dlopen(Base.libblas_name), "cblas_ddot" * (Sys.WORD_SIZE == 64 ? "64_" : ""))
-    @test 23.0 === @ccall $(dot_sym)(2::Cint, [2.0, 3.0]::Ref{Cdouble}, 1::Cint, [4.0, 5.0]::Ref{Cdouble}, 1::Cint)::Cdouble
+    @test 23.0 === @ccall $(dot_sym)(2::Int, [2.0, 3.0]::Ref{Cdouble}, 1::Int, [4.0, 5.0]::Ref{Cdouble}, 1::Int)::Cdouble
 end
 
 end # module TestBLAS
