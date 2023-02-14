b6cbfd1f348ee9e76b65ee2bb28394de3d967cab
Tim Holy <tim.holy@gmail.com>
2023-02-14 09:08:35 -0600

    Add lineno to SourceFile (JuliaLang/JuliaSyntax.jl#191)
    Co-authored-by: c42f <chris42f@gmail.com>

--- JuliaSyntax/test/source_files.jl
+++ JuliaSyntax/test/source_files.jl
@@ -9,4 +9,21 @@
     @test source_location(SourceFile("a\nb\n"), 3) == (2,1)
     @test source_location(SourceFile("a\nb\n"), 4) == (2,2)
     @test source_location(SourceFile("a\nb\n"), 5) == (2,3)
+
+    @test source_location(SourceFile("a"; first_line=7), 1) == (7,1)
+    @test source_location(SourceFile("a"; first_line=7), 2) == (7,2)
+
+    @test source_location(SourceFile("a\n"; first_line=7), 2) == (7,2)
+    @test source_location(SourceFile("a\n"; first_line=7), 3) == (7,3)
+
+    @test source_location(SourceFile("a\nb\n"; first_line=7), 2) == (7,2)
+    @test source_location(SourceFile("a\nb\n"; first_line=7), 3) == (8,1)
+    @test source_location(SourceFile("a\nb\n"; first_line=7), 4) == (8,2)
+    @test source_location(SourceFile("a\nb\n"; first_line=7), 5) == (8,3)
+
+    mktemp() do path, io
+        write(io, "a\n")
+        @test source_location(SourceFile(; filename=path), 1) == (1,1)
+        @test source_location(SourceFile(; filename=path, first_line=7), 1) == (7,1)
+    end
 end
