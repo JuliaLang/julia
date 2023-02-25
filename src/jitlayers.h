c136b7e787aee1dffefcea6afce51d0bdd6d5dd1
Jameson Nash <vtjnash@gmail.com>
2023-02-25 15:31:33 -0700

    enable re-using external code in pkgimages (#48723)
    * enable using external code in pkgimages
    
    This was unintentionally disabled (and incomplete) in the original PR
    for pkgimages.
    
    Co-authored-by: Valentin Churavy <v.churavy@gmail.com>

--- src/jitlayers.h
+++ src/jitlayers.h
@@ -190,7 +190,7 @@ typedef struct _jl_codegen_params_t {
     // outputs
     std::vector<std::pair<jl_code_instance_t*, jl_codegen_call_target_t>> workqueue;
     std::map<void*, GlobalVariable*> globals;
-    std::map<std::tuple<jl_code_instance_t*,bool>, Function*> external_fns;
+    std::map<std::tuple<jl_code_instance_t*,bool>, GlobalVariable*> external_fns;
     std::map<jl_datatype_t*, DIType*> ditypes;
     std::map<jl_datatype_t*, Type*> llvmtypes;
     DenseMap<Constant*, GlobalVariable*> mergedConstants;
