c95d3fbf11bb227975663dfb49ca6a69ed85b131
Tim Stahlhut <stahta01@gmail.com>
2023-03-01 08:55:09 -0500

    deps/blastrampoline: Add and apply patch file
    blastrampoline-cygwin_build_fix.patch
    Edit BLASTRAMPOLINE_INSTALL to copy DLL without version number.

--- deps/blastrampoline.mk
+++ deps/blastrampoline.mk
@@ -9,23 +9,33 @@ $(eval $(call git-external,blastrampoline,BLASTRAMPOLINE,,,$(BUILDDIR)))
 BLASTRAMPOLINE_BUILD_OPTS := $(MAKE_COMMON) CC="$(CC) $(SANITIZE_OPTS)" CFLAGS="$(CFLAGS)" LDFLAGS="$(LDFLAGS)"
 BLASTRAMPOLINE_BUILD_OPTS += ARCH="$(ARCH)" OS="$(OS)"
 
-$(BUILDDIR)/$(BLASTRAMPOLINE_SRC_DIR)/build-configured: $(BUILDDIR)/$(BLASTRAMPOLINE_SRC_DIR)/source-extracted
+BLASTRAMPOLINE_BUILDDIR := $(BUILDDIR)/$(BLASTRAMPOLINE_SRC_DIR)
+
+$(BLASTRAMPOLINE_BUILDDIR)/blastrampoline-cygwin_build_fix.patch-applied: $(BLASTRAMPOLINE_BUILDDIR)/source-extracted
+	cd $(BLASTRAMPOLINE_BUILDDIR) && \
+		patch -p1 -f < $(SRCDIR)/patches/blastrampoline-cygwin_build_fix.patch
+	echo 1 > $@
+
+$(BLASTRAMPOLINE_BUILDDIR)/source-patched: $(BLASTRAMPOLINE_BUILDDIR)/blastrampoline-cygwin_build_fix.patch-applied
+	echo 1 > $@
+
+$(BUILDDIR)/$(BLASTRAMPOLINE_SRC_DIR)/build-configured: $(BLASTRAMPOLINE_BUILDDIR)/source-patched
 	mkdir -p $(dir $@)
 	echo 1 > $@
 
 BLASTRAMPOLINE_BUILD_ROOT := $(BUILDDIR)/$(BLASTRAMPOLINE_SRC_DIR)/src
 $(BUILDDIR)/$(BLASTRAMPOLINE_SRC_DIR)/build-compiled: $(BUILDDIR)/$(BLASTRAMPOLINE_SRC_DIR)/build-configured
 	cd $(dir $@)/src && $(MAKE) $(BLASTRAMPOLINE_BUILD_OPTS)
-ifeq ($(OS), WINNT)
-	# Windows doesn't like soft link, use hard link
-	cd $(BLASTRAMPOLINE_BUILD_ROOT)/build/ && \
-		cp -f --dereference --link libblastrampoline.dll libblastrampoline.dll
-endif
 	echo 1 > $@
 
 define BLASTRAMPOLINE_INSTALL
 	$(MAKE) -C $(BLASTRAMPOLINE_BUILD_ROOT) install $(BLASTRAMPOLINE_BUILD_OPTS) DESTDIR="$2"
 endef
+ifeq ($(OS), WINNT)
+# Windows doesn't like soft link, use hard link to copy file without version suffix
+BLASTRAMPOLINE_INSTALL += && cd $2$$(build_prefix)/bin && \
+$$(WIN_MAKE_HARD_LINK) libblastrampoline-*.dll libblastrampoline.dll
+endif
 $(eval $(call staged-install, \
 	blastrampoline,$(BLASTRAMPOLINE_SRC_DIR), \
 	BLASTRAMPOLINE_INSTALL,, \
@@ -38,7 +48,7 @@ clean-blastrampoline:
 		$(BUILDDIR)/$(BLASTRAMPOLINE_SRC_DIR)/build-configured
 
 get-blastrampoline: $(BLASTRAMPOLINE_SRC_FILE)
-extract-blastrampoline: $(BUILDDIR)/$(BLASTRAMPOLINE_SRC_DIR)/source-extracted
+extract-blastrampoline: $(BLASTRAMPOLINE_BUILDDIR)/source-extracted
 configure-blastrampoline: extract-blastrampoline
 compile-blastrampoline: $(BUILDDIR)/$(BLASTRAMPOLINE_SRC_DIR)/build-compiled
 fastcheck-blastrampoline: check-blastrampoline
