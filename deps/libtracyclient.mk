acf9566f31e012fab10e045d855865f0bfaa5812
Cody Tapscott <topolarity@tapscott.me>
2023-03-01 20:36:12 -0700

    Use BUILDDIR instead of SRCCACHE for libTracyCore
    This is the right strategy according to #31530, so that the patching
    process doesn't hit any races between multiple builds running from the
    same SRCCACHE.

--- deps/libtracyclient.mk
+++ deps/libtracyclient.mk
@@ -2,7 +2,7 @@
 ifneq ($(USE_BINARYBUILDER_LIBTRACYCLIENT),1)
 LIBTRACYCLIENT_GIT_URL:=https://github.com/wolfpld/tracy.git
 LIBTRACYCLIENT_TAR_URL=https://api.github.com/repos/wolfpld/tracy/tarball/$1
-$(eval $(call git-external,libtracyclient,LIBTRACYCLIENT,,,$(SRCCACHE)))
+$(eval $(call git-external,libtracyclient,LIBTRACYCLIENT,,,$(BUILDDIR)))
 
 LIBTRACYCLIENT_BUILDDIR := $(BUILDDIR)/$(LIBTRACYCLIENT_SRC_DIR)
 LIBTRACYCLIENT_SRCCACHE := $(SRCCACHE)/$(LIBTRACYCLIENT_SRC_DIR)
@@ -17,28 +17,28 @@ LIBTRACYCLIENT_CMAKE += -DTRACY_NO_FRAME_IMAGE=ON
 LIBTRACYCLIENT_CMAKE += -DTRACY_NO_CRASH_HANDLER=ON
 LIBTRACYCLIENT_CMAKE += -DTRACY_ON_DEMAND=ON
 
-$(LIBTRACYCLIENT_SRCCACHE)/cmake-patch-applied: $(LIBTRACYCLIENT_SRCCACHE)/source-extracted
+$(LIBTRACYCLIENT_BUILDDIR)/cmake-patch-applied: $(LIBTRACYCLIENT_BUILDDIR)/source-extracted
 ifneq ($(OS),WINNT)
-	echo "target_compile_definitions(TracyClient PUBLIC __STDC_FORMAT_MACROS)" >> $(LIBTRACYCLIENT_SRCCACHE)/CMakeLists.txt
+	echo "target_compile_definitions(TracyClient PUBLIC __STDC_FORMAT_MACROS)" >> $(LIBTRACYCLIENT_BUILDDIR)/CMakeLists.txt
 else
-	echo "target_compile_definitions(TracyClient PUBLIC WINVER=0x0602 _WIN32_WINNT=0x0602)" >> $(LIBTRACYCLIENT_SRCCACHE)/CMakeLists.txt
+	echo "target_compile_definitions(TracyClient PUBLIC WINVER=0x0602 _WIN32_WINNT=0x0602)" >> $(LIBTRACYCLIENT_BUILDDIR)/CMakeLists.txt
 endif
 	echo 1 > $@
 
-$(LIBTRACYCLIENT_SRCCACHE)/libTracyClient-freebsd-elfw.patch-applied: $(LIBTRACYCLIENT_SRCCACHE)/cmake-patch-applied
-	cd $(LIBTRACYCLIENT_SRCCACHE) && \
+$(LIBTRACYCLIENT_BUILDDIR)/libTracyClient-freebsd-elfw.patch-applied: $(LIBTRACYCLIENT_BUILDDIR)/cmake-patch-applied
+	cd $(LIBTRACYCLIENT_BUILDDIR) && \
 		patch -p1 -f < $(SRCDIR)/patches/libTracyClient-freebsd-elfw.patch
 	echo 1 > $@
 
-$(LIBTRACYCLIENT_SRCCACHE)/libTracyClient-no-crash-handler.patch-applied: $(LIBTRACYCLIENT_SRCCACHE)/libTracyClient-freebsd-elfw.patch-applied
-	cd $(LIBTRACYCLIENT_SRCCACHE) && \
+$(LIBTRACYCLIENT_BUILDDIR)/libTracyClient-no-crash-handler.patch-applied: $(LIBTRACYCLIENT_BUILDDIR)/libTracyClient-freebsd-elfw.patch-applied
+	cd $(LIBTRACYCLIENT_BUILDDIR) && \
 		patch -p1 -f < $(SRCDIR)/patches/libTracyClient-no-crash-handler.patch
 	echo 1 > $@
 
-$(LIBTRACYCLIENT_BUILDDIR)/build-configured: $(LIBTRACYCLIENT_SRCCACHE)/libTracyClient-no-crash-handler.patch-applied
+$(LIBTRACYCLIENT_BUILDDIR)/build-configured: $(LIBTRACYCLIENT_BUILDDIR)/libTracyClient-no-crash-handler.patch-applied
 	mkdir -p $(dir $@)
 	cd $(dir $@) && \
-		$(CMAKE) $(LIBTRACYCLIENT_SRCCACHE) $(CMAKE_GENERATOR_COMMAND) $(CMAKE_COMMON) $(LIBTRACYCLIENT_CMAKE) \
+		$(CMAKE) . $(CMAKE_GENERATOR_COMMAND) $(CMAKE_COMMON) $(LIBTRACYCLIENT_CMAKE) \
 		|| { echo '*** To install a newer version of cmake, run contrib/download_cmake.sh ***' && false; }
 	echo 1 > $@
 
@@ -59,7 +59,7 @@ clean-libtracyclient:
 	-$(MAKE) -C $(LIBTRACYCLIENT_BUILDDIR) clean
 
 get-libtracyclient: $(LIBTRACYCLIENT_SRC_FILE)
-extract-libtracyclient: $(LIBTRACYCLIENT_SRCCACHE)/source-extracted
+extract-libtracyclient: $(LIBTRACYCLIENT_BUILDDIR)/source-extracted
 configure-libtracyclient: $(LIBTRACYCLIENT_BUILDDIR)/build-configured
 compile-libtracyclient: $(LIBTRACYCLIENT_BUILDDIR)/build-compiled
 fastcheck-libtracyclient: check-libtracyclient
