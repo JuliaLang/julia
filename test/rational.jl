024b45405ac94ead7e4c15bf71715fd1a85a5c26
Marek Kaluba <kalmar@amu.edu.pl>
2023-03-05 12:06:28 +0100

    add test: show matrix of Union{Int, Rational{Int}}

--- test/rational.jl
+++ test/rational.jl
@@ -255,6 +255,8 @@ end
     @test sprint(show, rational2) == "-1//2"
     @test sprint(show, -2//2) == "-1//1"
     @test sprint(show, [-2//2,]) == "Rational{Int64}[-1]"
+    @test sprint(show, MIME"text/plain"(), Union{Int, Rational{Int}}[7 3//6; 6//3 2]) ==
+        "2×2 Matrix{Union{Rational{Int64}, Int64}}:\n  7    1//2\n 2//1   2"
     let
         io1 = IOBuffer()
         write(io1, rational1)
