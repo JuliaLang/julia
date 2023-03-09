e8fade7b3ff68580374e2e57f9705e42c2ff1d9f
Tomas Chor <tomaschor@gmail.com>
2023-03-08 18:33:22 -0800

    Add a `fourthroot` function with unicode symbol ∜ (#48899)
    * adds fourthroot function

--- base/Base.jl
+++ base/Base.jl
@@ -342,6 +342,7 @@ include("math.jl")
 using .Math
 const (√)=sqrt
 const (∛)=cbrt
+const (∜)=fourthroot
 
 # now switch to a simple, race-y TLS, relative include for the rest of Base
 delete_method(which(include, (Module, String)))
