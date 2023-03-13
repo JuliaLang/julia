5b041b54534b560c4a51567d1d0b453527d2fd65
Zachary P. Christensen <zchristensen7@gmail.com>
2023-03-13 07:00:57 -0400

    try fixing more codegen

--- base/buffers.jl
+++ base/buffers.jl
@@ -13,25 +13,33 @@ if !isdefined(Main, :Base)
         end
         return b
     end
-    function isassigned(b::Buffer, i::Int)
-        @inline
-        @boundscheck 1 <= i <= length(b) || return false
-        ccall(:jl_buffer_isassigned, Cint, (Any, UInt), b, i) == 1
-    end
 end
+
+
+eltype(::Type{<:Buffer{T}}) where {T} = T
+
+elsize(@nospecialize T::Type{<:Buffer}) = aligned_sizeof(eltype(T))
+
+length(b::Buffer) = Core.Intrinsics.bufferlen(b)
+
+firstindex(b::Buffer) = 1
+
+lastindex(b::Buffer) = length(b)
+
+keys(b::Buffer) = OneTo(length(b))
+
+axes(b::Buffer) = (OneTo(length(b)),)
+axes(b::Buffer, d::Integer) = d <= 1 ? OneTo(length(b)) : OneTo(1)
+
+iterate(v::Buffer, i=1) = (length(v) < i ? nothing : (v[i], i + 1))
+
+isempty(b::Buffer) = (length(b) == 0)
 
 @eval getindex(b::Buffer{T}, i::Int) where {T} = Core.bufref($(Expr(:boundscheck)), b, i)
 @eval setindex!(b::Buffer{T}, x, i::Int) where {T} =
     Core.bufset($(Expr(:boundscheck)), b, convert(T, x)::T, i)
-elsize(@nospecialize T::Type{<:Buffer}) = aligned_sizeof(eltype(T))
-length(b::Buffer) = Core.Intrinsics.bufferlen(b)
-firstindex(b::Buffer) = 1
-lastindex(b::Buffer) = length(b)
-keys(b::Buffer) = OneTo(length(b))
-axes(b::Buffer) = (OneTo(length(b)),)
-axes(b::Buffer, d::Integer) = d <= 1 ? OneTo(length(b)) : OneTo(1)
-iterate(v::Buffer, i=1) = (length(v) < i ? nothing : (v[i], i + 1))
-isempty(b::Buffer) = (length(b) == 0)
+
+
 function ==(v1::Buffer, v2::Buffer)
     length(v1)==length(v2) || return false
     for i in 1:length(v1)
@@ -41,3 +49,9 @@ function ==(v1::Buffer, v2::Buffer)
 end
 
 sizeof(b::Buffer) = Core.sizeof(b)
+
+function isassigned(b::Buffer, i::Int)
+    @inline
+    @boundscheck 1 <= i <= length(b) || return false
+    ccall(:jl_buffer_isassigned, Cint, (Any, UInt), b, i) == 1
+end
