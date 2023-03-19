301e4615bd8f96227ec4d8937f8e82b3b36f3d3a
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-03-18 21:26:14 -0400

    Add SparseArrays to cache_stdlibs.jl

--- contrib/cache_stdlibs.jl
+++ contrib/cache_stdlibs.jl
@@ -23,13 +23,14 @@ stdlibs = [
     # 3-depth packages
     :LibGit2_jll,
 
+    # 4-depth packages
+    :SparseArrays,
+
     # 7-depth packages
     :LLD_jll,
-    :SuiteSparse_jll,
 
     # 9-depth packages
     :Statistics,
-    :SuiteSparse,
 ]
 
 depot = abspath(Sys.BINDIR, "..", "share", "julia")
