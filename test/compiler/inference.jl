fe2274a219fa641e2adbeb096b68f2c5694cd719
Shuhei Kadowaki <aviatesk@gmail.com>
2023-03-23 15:18:33 +0900

    rework on `src.slottypes` and `ir.argtypes`
    This commit reworks our handling of `(src::CodeInfo).slottypes` and
    `(ir::IRCode).argtypes`. Currently `ir.argtypes` contains types for call
    arguments and local slots even after the SSA conversion (`slot2reg`),
    which can be quite confusing. Similarly, `src.slot[names|flags|types]`
    contains information about local slots regardless of whether `src` is
    optimized or not, even though local slots never appear within `src`.
    
    With this commit, we resize `ir.argtypes` so that it only contains
    information about call arguments after `slot2reg`, as well as resize
    `src.slot[names|flags|types]` after optimization.
    
    This commit removes unnecessary information upon appropriate timings and
    allows us to use `Core.OpaqueClosure(ir::IRCode)` constructor for such
    `ir` that is retrieved by `Base.code_ircode`:
    
    ```julia
    let ir = first(only(Base.code_ircode(sin, (Int,))))
        @test OpaqueClosure(ir)(42) == sin(42)
        ir = first(only(Base.code_ircode(sin, (Float64,))))
        @test OpaqueClosure(ir)(42.) == sin(42.)
    end
    ```

--- test/compiler/inference.jl
+++ test/compiler/inference.jl
@@ -4099,18 +4099,18 @@ let # Test the presence of PhiNodes in lowered IR by taking the above function,
     mi = Core.Compiler.specialize_method(first(methods(f_convert_me_to_ir)),
         Tuple{Bool, Float64}, Core.svec())
     ci = Base.uncompressed_ast(mi.def)
+    ci.slottypes = Any[ Any for i = 1:length(ci.slotflags) ]
     ci.ssavaluetypes = Any[Any for i = 1:ci.ssavaluetypes]
     sv = Core.Compiler.OptimizationState(mi, Core.Compiler.NativeInterpreter())
     ir = Core.Compiler.convert_to_ircode(ci, sv)
     ir = Core.Compiler.slot2reg(ir, ci, sv)
     ir = Core.Compiler.compact!(ir)
-    Core.Compiler.replace_code_newstyle!(ci, ir, 4)
-    ci.ssavaluetypes = length(ci.code)
+    Core.Compiler.replace_code_newstyle!(ci, ir)
+    ci.ssavaluetypes = length(ci.ssavaluetypes)
     @test any(x->isa(x, Core.PhiNode), ci.code)
     oc = @eval b->$(Expr(:new_opaque_closure, Tuple{Bool, Float64}, Any, Any,
         Expr(:opaque_closure_method, nothing, 2, false, LineNumberNode(0, nothing), ci)))(b, 1.0)
     @test Base.return_types(oc, Tuple{Bool}) == Any[Float64]
-
     oc = @eval ()->$(Expr(:new_opaque_closure, Tuple{Bool, Float64}, Any, Any,
         Expr(:opaque_closure_method, nothing, 2, false, LineNumberNode(0, nothing), ci)))(true, 1.0)
     @test Base.return_types(oc, Tuple{}) == Any[Float64]
