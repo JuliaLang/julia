364b3b4692f1ec195ce9f010cf12ee78da8b4fe7
c42f <chris42f@gmail.com>
2023-03-24 07:13:02 +1000

    Automatically reduce test failures General registry (JuliaLang/JuliaSyntax.jl#229)

--- JuliaSyntax/tools/check_all_packages.jl
+++ JuliaSyntax/tools/check_all_packages.jl
@@ -6,6 +6,7 @@
 using JuliaSyntax, Logging, TerminalLoggers, ProgressLogging, Serialization
 
 include("../test/test_utils.jl")
+include("../test/fuzz_test.jl")
 
 pkgspath = joinpath(@__DIR__, "pkgs")
 source_paths = find_source_in_path(pkgspath)
@@ -15,6 +16,24 @@ exception_count = 0
 mismatch_count = 0
 t0 = time()
 exceptions = []
+
+function parsers_disagree(text::AbstractString)
+    fl_ex = fl_parseall(text, filename="none")
+    if Meta.isexpr(fl_ex, (:error,:incomplete)) ||
+            (Meta.isexpr(fl_ex, :toplevel) && length(fl_ex.args) >= 1 &&
+             Meta.isexpr(fl_ex.args[end], (:error,:incomplete)))
+        return false
+    end
+    try
+        ex = parseall(Expr, text, filename="none", ignore_errors=true)
+        return !exprs_roughly_equal(fl_ex, ex)
+    catch
+        @error "Reduction failed" text
+        return false
+    end
+end
+
+all_reduced_failures = String[]
 
 Logging.with_logger(TerminalLogger()) do
     global exception_count, mismatch_count, t0
@@ -29,16 +48,19 @@ Logging.with_logger(TerminalLogger()) do
             e1 = JuliaSyntax.parseall(Expr, text, filename=fpath, ignore_warnings=true)
             if !exprs_roughly_equal(e2, e1)
                 mismatch_count += 1
-                reduced_chunks = sprint(context=:color=>true) do io
+                failing_source = sprint(context=:color=>true) do io
                     for c in reduce_test(text)
                         JuliaSyntax.highlight(io, c.source, range(c), context_lines_inner=5)
                         println(io, "\n")
                     end
                 end
+                reduced_failures = rand_reduce.(sourcetext.(reduce_test(text)),
+                                                   parsers_disagree)
+                append!(all_reduced_failures, reduced_failures)
                 @error("Parsers succeed but disagree",
                        fpath,
-                       reduced_chunks=Text(reduced_chunks),
-                       # diff=Text(sprint(show_expr_text_diff, show, e1, e2)),
+                       failing_source=Text(failing_source),
+                       reduced_failures,
                        )
             end
         catch err
@@ -75,3 +97,14 @@ println()
         $(exception_count) failures compared to reference parser
         $(mismatch_count) Expr mismatches
         $(t_avg)ms per file"""
+
+open(joinpath(@__DIR__, "reduced_failures.jl"), write=true) do io
+    for str in all_reduced_failures
+        println(io, repr(str))
+    end
+    for str in all_reduced_failures
+        println(io, "#------------------------------")
+        println(io, str)
+        println(io)
+    end
+end
