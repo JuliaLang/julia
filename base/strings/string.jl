de2b56f08b95d7812cbecdf3159f3fd9ac145413
Shuhei Kadowaki <aviatesk@gmail.com>
2023-03-24 13:38:29 +0900

    minor follow up on #48996
    - removed unnecessary `Union`-signature
    - use one-liner definition when applicable
    - add type assertion to make it more robust against invalidation

--- base/strings/string.jl
+++ base/strings/string.jl
@@ -331,9 +331,8 @@ isvalid(s::String, i::Int) = checkbounds(Bool, s, i) && thisind(s, i) == i
 isascii(s::String) = isascii(codeunits(s))
 
 # don't assume effects for general integers since we cannot know their implementation
-@assume_effects :foldable function repeat(c::Char, r::BitInteger)
-    @invoke repeat(c, r::Integer)
-end
+@assume_effects :foldable repeat(c::Char, r::BitInteger) = @invoke repeat(c, r::Integer)
+
 """
     repeat(c::AbstractChar, r::Integer) -> String
 
@@ -347,7 +346,7 @@ julia> repeat('A', 3)
 ```
 """
 function repeat(c::AbstractChar, r::Integer)
-    c = Char(c)
+    c = Char(c)::Char
     r == 0 && return ""
     r < 0 && throw(ArgumentError("can't repeat a character $r times"))
     u = bswap(reinterpret(UInt32, c))
