282c7bccb1e9c4babb4dd47fcd71e963ef6972e5
Nicholas R Dinsmore <nicholas.dinsmore@gmail.com>
2023-03-28 17:49:52 -0400

    make length_continued foldable

--- base/strings/string.jl
+++ base/strings/string.jl
@@ -680,7 +680,7 @@ function _length_continued_nonascii(
     return c
 end
 
-@assume_effects :terminates_locally @inline @propagate_inbounds  function length_continued(s::String, first::Int, last::Int, c::Int)
+@assume_effects :foldable @inline @propagate_inbounds  function length_continued(s::String, first::Int, last::Int, c::Int)
     cu = codeunits(s)
     chunk_size = _STRING_LENGTH_CHUNKING_SIZE
     first < last || return c
