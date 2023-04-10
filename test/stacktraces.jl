5435ac53c7953ad744607847891e68a2d44001e9
Tim Holy <tim.holy@gmail.com>
2023-04-10 10:12:02 -0500

    Add tests

--- test/stacktraces.jl
+++ test/stacktraces.jl
@@ -191,3 +191,88 @@ let bt
     end
     @test any(s->startswith(string(s), "f33065(x::Float32, y::Float32; b::Float64, a::String, c::"), bt)
 end
+
+@testset "TypeTreeIO" begin
+    ttio = Base.TypeTreesIO.TypeTreeIO()
+    obj = view([1, 2, 3], 1:2)
+    print(ttio, typeof(obj))
+    @test sprint(print, ttio.tree) == sprint(print, typeof(obj))
+    @test sprint((io, node) -> print(IOContext(io, :type_maxdepth=>1), node), ttio.tree) == "SubArray{…}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxdepth=>2), node), ttio.tree) == "SubArray{$Int, 1, Vector{…}, Tuple{…}, true}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxdepth=>3), node), ttio.tree) == "SubArray{$Int, 1, Vector{$Int}, Tuple{UnitRange{…}}, true}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxdepth=>4), node), ttio.tree) == "SubArray{$Int, 1, Vector{$Int}, Tuple{UnitRange{$Int}}, true}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxwidth=>3), node), ttio.tree) == "SubArray{…}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxwidth=>44), node), ttio.tree) == "SubArray{…}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxwidth=>45), node), ttio.tree) == "SubArray{$Int, 1, Vector{…}, Tuple{…}, true}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxwidth=>59), node), ttio.tree) == "SubArray{$Int, 1, Vector{…}, Tuple{…}, true}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxwidth=>60), node), ttio.tree) == "SubArray{Int64, 1, Vector{Int64}, Tuple{UnitRange{…}}, true}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxwidth=>100), node), ttio.tree) == "SubArray{$Int, 1, Vector{$Int}, Tuple{UnitRange{$Int}}, true}"
+    @test String(take!(ttio)) == sprint(print, typeof(obj))
+
+    # ttio can be reused (after `take!`)
+    typ = Vector{V} where V<:AbstractVector{T} where T<:Real
+    print(ttio, typ)
+    @test sprint(print, ttio.tree) == sprint(print, typ)
+    @test sprint((io, node) -> print(IOContext(io, :type_maxdepth=>1), node), ttio.tree) == "Vector{…} where {…}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxdepth=>2), node), ttio.tree) == "Vector{V} where {T<:Real, V<:AbstractVector{…}}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxdepth=>3), node), ttio.tree) == "Vector{V} where {T<:Real, V<:AbstractVector{T}}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxwidth=>20), node), ttio.tree) == "Vector{…} where {…}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxwidth=>46), node), ttio.tree) == "Vector{…} where {…}"
+    @test sprint((io, node) -> print(IOContext(io, :type_maxwidth=>47), node), ttio.tree) == "Vector{V} where {T<:Real, V<:AbstractVector{T}}"
+    @test String(take!(ttio)) == sprint(print, typ)
+
+    typ = Vector{T} where T<:Real
+    print(ttio, typ)
+    @test String(take!(ttio)) == sprint(print, typ)
+    # IOContext
+    print(IOContext(ttio, :color=>true), typ)
+    @test String(take!(ttio)) == sprint(print, typ)
+
+    typ = Union{Int32, T} where T
+    print(ttio, typ)
+    @test String(take!(ttio)) == sprint(print, typ)
+
+    print(ttio, Tuple{})
+    @test String(take!(ttio)) == sprint(print, Tuple{})
+
+    # Stacktrace
+    a = UInt8(81):UInt8(160)
+    b = view(a, 1:64)
+    c = reshape(b, (8, 8))
+    d = reinterpret(reshape, Float64, c)
+    sqrteach(a) = [sqrt(x) for x in a]
+    st = try
+        sqrteach(d)
+    catch e
+        stacktrace(catch_backtrace())
+    end
+    nmi = 0
+    for sf in st
+        mi = sf.linfo
+        if mi !== nothing
+            if isa(mi, Core.MethodInstance)
+                nmi += 1
+                print(ttio, mi.specTypes)
+                @test String(take!(ttio)) == sprint(print, mi.specTypes)
+            end
+        end
+        iobuf = IOBuffer()
+        ioctx = IOContext(iobuf, :limit=>true,  :color=>true, :displaysize=>(50,1000), :backtrace=>true)
+        Base.StackTraces.show_spec_linfo(ioctx, sf)
+        str = String(take!(iobuf))
+        ioctx = IOContext(iobuf, :limit=>false, :color=>true, :displaysize=>(50,1000), :backtrace=>true)
+        Base.StackTraces.show_spec_linfo(ioctx, sf)
+        @test str == String(take!(iobuf))
+    end
+    @test nmi > 0
+
+    # Whole signatures
+    m = which(show, (IO, String))
+    print(ttio, m.sig)
+    @test String(take!(ttio)) == sprint(print, m.sig)
+    print(ttio, "show(io::IO, x::String)")
+    @test String(take!(ttio)) == "show(io::IO, x::String)"
+    T = TypeVar(:T)
+    print(ttio, "show(io::IO, x::", T, ')', " where ", T)
+    @test String(take!(ttio)) == "show(io::IO, x::T) where T"
+end
