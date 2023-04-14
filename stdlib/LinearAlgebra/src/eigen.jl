0ac185624b11c31f32ae1f0b0f1475cf2f14f2bf
KronosTheLate <61620837+KronosTheLate@users.noreply.github.com>
2023-04-14 15:58:12 +0200

    Update stdlib/LinearAlgebra/src/eigen.jl
    Co-authored-by: Daniel Karrasch <daniel.karrasch@posteo.de>

--- stdlib/LinearAlgebra/src/eigen.jl
+++ stdlib/LinearAlgebra/src/eigen.jl
@@ -182,7 +182,7 @@ end
 
 Compute the eigenvalue decomposition of `A`, returning an [`Eigen`](@ref) factorization object `F`
 which contains the eigenvalues in `F.values` and the eigenvectors in the columns of the
-matrix `F.vectors`. This corresponds to solving a generalized eigenvalue problem of the form
+matrix `F.vectors`. This corresponds to solving a eigenvalue problem of the form
 `Ax =  λx`, where `A` is a matrix, `x` is an eigenvector, and  `λ` is an eigenvalue.
 (The `k`th eigenvector can be obtained from the slice `F.vectors[:, k]`.)
 
