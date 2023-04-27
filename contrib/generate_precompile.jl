09a0f34831a6fff5f9358b7034b7ef9073f3053e
Kristoffer Carlsson <kcarlsson89@gmail.com>
2023-04-27 20:44:32 +0200

    add two more explicit precompile statements (#49537)

--- contrib/generate_precompile.jl
+++ contrib/generate_precompile.jl
@@ -45,6 +45,8 @@ precompile(Tuple{typeof(push!), Vector{Function}, Function})
 # miscellaneous
 precompile(Tuple{typeof(Base.require), Base.PkgId})
 precompile(Tuple{typeof(Base.recursive_prefs_merge), Base.Dict{String, Any}})
+precompile(Tuple{typeof(Base.hashindex), Tuple{Base.PkgId, Nothing}, Int64})
+precompile(Tuple{typeof(Base.hashindex), Tuple{Base.PkgId, String}, Int64})
 precompile(Tuple{typeof(isassigned), Core.SimpleVector, Int})
 precompile(Tuple{typeof(getindex), Core.SimpleVector, Int})
 precompile(Tuple{typeof(Base.Experimental.register_error_hint), Any, Type})
