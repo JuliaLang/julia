5ee5a05cb7543183af6a563c70f377d2922aba9e
Keno Fischer <keno@juliacomputing.com>
2023-05-05 00:14:28 -0400

    Update test/compiler/inference.jl
    Co-authored-by: Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>

--- test/compiler/inference.jl
+++ test/compiler/inference.jl
@@ -4862,13 +4862,13 @@ end |> only == Type{Int}
 
 # Test that NamedTuple abstract iteration works for PartialStruct/Const
 function nt_splat_const()
-    nt = (; x=1, y = 2)
+    nt = (; x=1, y=2)
     Val{tuple(nt...)[2]}()
 end
-@test @inferred(nt_splat_const) == Val{2}()
+@test @inferred(nt_splat_const()) == Val{2}()
 
-function nt_splat_partial()
-    nt = (; x=1, y = 2)
+function nt_splat_partial(x::Int)
+    nt = (; x, y=2)
     Val{tuple(nt...)[2]}()
 end
-@test @inferred(nt_splat_partial) == Val{2}()
+@test @inferred(nt_splat_partial(42)) == Val{2}()
