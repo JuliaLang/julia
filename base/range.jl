aedadd80764fc1960810b4957e7c6213cec3e1c8
Sukera <Seelengrab@users.noreply.github.com>
2023-05-06 09:59:22 +0200

    Remove mention of TwicePrecision from the docs

--- base/range.jl
+++ base/range.jl
@@ -478,8 +478,6 @@ value `r[1]`, but alternatively you can supply it as the value of
 `r[offset]` for some other index `1 <= offset <= len`. The syntax `a:b`
 or `a:b:c`, where any of `a`, `b`, or `c` are floating-point numbers, creates a
 `StepRangeLen`.
-In conjunction with `TwicePrecision` this can be used to implement ranges that
-are free of roundoff error.
 
 !!! compat "Julia 1.7"
     The 4th type parameter `L` requires at least Julia 1.7.
