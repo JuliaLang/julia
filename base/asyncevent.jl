580fbeaa13f34ad28582e41215b74e341317eb0b
Katharine Hyatt <khyatt@flatironinstitute.org>
2023-05-08 16:08:48 -0400

    Add a doctest for timedwait

--- base/asyncevent.jl
+++ base/asyncevent.jl
@@ -331,7 +331,20 @@ Waits until `testcb()` returns `true` or `timeout` seconds have passed, whicheve
 The test function is polled every `pollint` seconds. The minimum value for `pollint` is 0.001 seconds,
 that is, 1 millisecond.
 
-Return `:ok` or `:timed_out`.
+Returns `:ok` or `:timed_out`.
+
+# Examples
+```jldoctest
+julia> cb() = (sleep(5); return);
+
+julia> t = @async cb();
+
+julia> timedwait(()->istaskdone(t), 1)
+:timed_out
+
+julia> timedwait(()->istaskdone(t), 6.5)
+:ok
+```
 """
 function timedwait(testcb, timeout::Real; pollint::Real=0.1)
     pollint >= 1e-3 || throw(ArgumentError("pollint must be ≥ 1 millisecond"))
