d17456c75e67696081bbb1aefd05ade4477ec806
Vasily Ilin <vasilin97@gmail.com>
2023-05-13 15:36:36 -0700

    add rand! to rand's docstring

--- stdlib/Random/src/Random.jl
+++ stdlib/Random/src/Random.jl
@@ -325,6 +325,7 @@ Pick a random element or array of random elements from the set of values specifi
 When only one argument is passed besides the optional `rng` and is a `Tuple`, it is interpreted
 as a collection of values (`S`) and not as `dims`.
 
+Also see the `rand!` function.
 
 !!! compat "Julia 1.1"
     Support for `S` as a tuple requires at least Julia 1.1.
