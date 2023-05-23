3112960a995090fc7d514d638d078becdfb291c0
Christopher Doris <github.com/cjdoris>
2023-05-23 19:12:04 +0100

    update the example code and make it a doctest

--- base/libc.jl
+++ base/libc.jl
@@ -82,14 +82,22 @@ It can be passed as a `Ptr{FILE}` argument to [`ccall`](@ref) and also supports
 [`seek`](@ref), [`position`](@ref) and [`close`](@ref).
 
 A `FILE` can be constructed from an ordinary `IO` object, provided it is an open file. It
-must be closed afterward. For example:
-```
-using Libc
-open("example.txt", "w") do io
-    file = FILE(io)
-    ccall(:fputs, Cint, (Cstring, Ptr{FILE}), "hello world", file)
-    close(file)
-end
+must be closed afterward.
+
+# Examples
+```jldoctest
+julia> using Base.Libc
+
+julia> mktemp() do _, io
+           # write to the temporary file using `puts(char*, FILE*)` from libc
+           file = FILE(io)
+           ccall(:fputs, Cint, (Cstring, Ptr{FILE}), "hello world", file)
+           close(file)
+           # read the file again
+           seek(io, 0)
+           read(io, String)
+       end
+"hello world"
 ```
 """
 struct FILE
