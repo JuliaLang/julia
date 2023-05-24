39faddbb8244233c3b06e70a874425a0fab871cd
Romain De Givry <romain.degivry@hotmail.fr>
2023-05-24 21:54:57 +0100

    abstractarray: implement review comments

--- test/abstractarray.jl
+++ test/abstractarray.jl
@@ -682,8 +682,13 @@ function test_cat(::Type{TestAbstractArray})
     @test Base.typed_hcat(Float64, B, B) == TSlow(b2hcat)
     @test Base.typed_hcat(Float64, B, B, B) == TSlow(b3hcat)
 
-    # issue #49676
-    @test_throws ArgumentError Base.typed_hcat([1 2 3], 1, +1)
+    @testset "issue #49676, bad error message on v[1 +1]" begin
+        # This is here because all these expressions are handled by Base.typed_hcat
+        v = [1 2 3]
+        @test_throws ArgumentError v[1 +1]
+        @test_throws ArgumentError v[1 1]
+        @test_throws ArgumentError v[[1 2] [2 3]]
+    end
 
     @test vcat(B1, B2) == TSlow(vcat([1:24...], [1:25...]))
     @test hcat(C1, C2) == TSlow([1 2 1 2 3; 3 4 4 5 6])
