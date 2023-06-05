c16891d03f46f06393bb3be360454db2317d2f3f
Zachary P Christensen <zchristensen7@gmail.com>
2023-06-05 19:20:47 -0400

    Atomic pointer operations APIs (#49811)
    This provides and interface to `Intrinsics.atomic_pointerref` and
    `Intrinsics.atomic_pointerset` through `unsafe_load` and
    `unsafe_store!`.
    
    Added the following atomic pointer operations:
        unsafe_modify!
        unsafe_replace!
        unsafe_swap!
        elsize(::Type{<:Ptr})
    
    Tests previously using the explicit intrinsic method here are replaced
    by the new user facing variants.
    
    Add notes about atomic implementation and refs to relevant field and
    property method docs.
    
    Move general atomic doc strings to kw doc and ref in multi-threading.
    
    Co-authored-by: Jameson Nash <vtjnash@gmail.com>
    Co-authored-by: Sukera <11753998+Seelengrab@users.noreply.github.com>

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -3228,6 +3228,15 @@ See also [`"`](@ref \")
 kw"\"\"\""
 
 """
+Unsafe pointer operations are compatible with loading and storing pointers declared with
+`_Atomic` and `std::atomic` type in C11 and C++23 respectively. An error may be thrown if
+there is not support for atomically loading the Julia type `T`.
+
+See also: [`unsafe_load`](@ref), [`unsafe_modify!`](@ref), [`unsafe_replace!`](@ref), [`unsafe_store!`](@ref), [`unsafe_swap!`](@ref)
+"""
+kw"atomic"
+
+"""
     Base.donotdelete(args...)
 
 This function prevents dead-code elimination (DCE) of itself and any arguments
