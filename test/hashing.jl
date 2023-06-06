JuliaLang/julia

commit 19610191c8b518385750d490472d8f5024465ade

Optimize top_set_bit(::BigInt) and hash(::Real) (#49996)
* implement top_set_bit in Julia for increased performance

* simplify and optimize hash(::Real)

---------

Co-authored-by: Lilith Hafner <Lilith.Hafner@gmail.com>

base/float.jl
@@ -674,34 +674,28 @@ function hash(x::Real, h::UInt)
         num = -num
         den = -den
     end
-    z = trailing_zeros(num)
-    if z != 0
-        num >>= z
-        pow += z
-    end
-    z = trailing_zeros(den)
-    if z != 0
-        den >>= z
-        pow -= z
-    end
+    num_z = trailing_zeros(num)
+    den_z = trailing_zeros(den)
+    den >>= den_z
+    pow += num_z - den_z
 
     # handle values representable as Int64, UInt64, Float64
     if den == 1
-        left = ndigits0z(num,2) + pow
-        right = trailing_zeros(num) + pow
+        left = top_set_bit(abs(num)) - den_z
+        right = pow
         if -1074 <= right
-            if 0 <= right && left <= 64
-                left <= 63                     && return hash(Int64(num) << Int(pow), h)
-                signbit(num) == signbit(den)   && return hash(UInt64(num) << Int(pow), h)
+            if 0 <= right
+                left <= 63 && return hash(Int64(num) << Int(pow-num_z), h)
+                left <= 64 && !signbit(num) && return hash(UInt64(num) << Int(pow-num_z), h)
             end # typemin(Int64) handled by Float64 case
-            left <= 1024 && left - right <= 53 && return hash(ldexp(Float64(num),pow), h)
+            left <= 1024 && left - right <= 53 && return hash(ldexp(Float64(num), pow-num_z), h)
         end
     end
 
     # handle generic rational values
     h = hash_integer(den, h)
     h = hash_integer(pow, h)
-    h = hash_integer(num, h)
+    h = hash_integer(num >> num_z, h)
     return h
 end
 

base/gmp.jl
@@ -606,9 +606,9 @@ Number of ones in the binary representation of abs(x).
 count_ones_abs(x::BigInt) = iszero(x) ? 0 : MPZ.mpn_popcount(x)
 
 function top_set_bit(x::BigInt)
-    x < 0 && throw(DomainError(x, "top_set_bit only supports negative arguments when they have type BitSigned."))
-    x == 0 && return 0
-    Int(ccall((:__gmpz_sizeinbase, :libgmp), Csize_t, (Base.GMP.MPZ.mpz_t, Cint), x, 2))
+    isneg(x) && throw(DomainError(x, "top_set_bit only supports negative arguments when they have type BitSigned."))
+    iszero(x) && return 0
+    x.size * sizeof(Limb) << 3 - leading_zeros(GC.@preserve x unsafe_load(x.d, x.size))
 end
 
 divrem(x::BigInt, y::BigInt) = MPZ.tdiv_qr(x, y)

commit 53bcb3987e64e72b6550b75c0d0c315cf15ad476

Fixup `hash(::Real)` broken by #49996 (#50067)
* fixup for 49996 and add test from 50065

---------

Co-authored-by: Lilith Hafner <Lilith.Hafner@gmail.com>

base/float.jl
@@ -675,27 +675,28 @@ function hash(x::Real, h::UInt)
         den = -den
     end
     num_z = trailing_zeros(num)
+    num >>= num_z
     den_z = trailing_zeros(den)
     den >>= den_z
     pow += num_z - den_z
 
     # handle values representable as Int64, UInt64, Float64
     if den == 1
-        left = top_set_bit(abs(num)) - den_z
-        right = pow
+        left = top_set_bit(abs(num)) + pow
+        right = pow + den_z
         if -1074 <= right
             if 0 <= right
-                left <= 63 && return hash(Int64(num) << Int(pow-num_z), h)
-                left <= 64 && !signbit(num) && return hash(UInt64(num) << Int(pow-num_z), h)
+                left <= 63 && return hash(Int64(num) << Int(pow), h)
+                left <= 64 && !signbit(num) && return hash(UInt64(num) << Int(pow), h)
             end # typemin(Int64) handled by Float64 case
-            left <= 1024 && left - right <= 53 && return hash(ldexp(Float64(num), pow-num_z), h)
+            left <= 1024 && left - right <= 53 && return hash(ldexp(Float64(num), pow), h)
         end
     end
 
     # handle generic rational values
     h = hash_integer(den, h)
     h = hash_integer(pow, h)
-    h = hash_integer(num >> num_z, h)
+    h = hash_integer(num, h)
     return h
 end
 

test/hashing.jl
@@ -8,8 +8,14 @@ types = Any[
     Bool,
     Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64, UInt64, Float32, Float64,
     Rational{Int8}, Rational{UInt8}, Rational{Int16}, Rational{UInt16},
-    Rational{Int32}, Rational{UInt32}, Rational{Int64}, Rational{UInt64}
+    Rational{Int32}, Rational{UInt32}, Rational{Int64}, Rational{UInt64},
+    BigFloat, #BigInt, # TODO: BigInt hashing is broken on 32-bit systems
 ]
+if Int === Int64
+    push!(types, BigInt)
+else
+    @test_broken hash(12345678901234) == hash(big(12345678901234))
+end
 vals = vcat(
     typemin(Int64),
     -Int64(maxintfloat(Float64)) .+ Int64[-4:1;],
@@ -51,8 +57,7 @@ let collides = 0
             collides += eq
         end
     end
-    # each pair of types has one collision for these values
-    @test collides <= (length(types) - 1)^2
+    @test collides <= 452
 end
 @test hash(0.0) != hash(-0.0)
 