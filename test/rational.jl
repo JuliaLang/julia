ff0e53df02ac82ce54aec0c4368f232e9815f6b1
Oscar Smith <oscardssmith@gmail.com>
2023-06-12 20:57:37 -0400

    Update rational.jl

--- test/rational.jl
+++ test/rational.jl
@@ -33,7 +33,7 @@ using Test
 
     @test @inferred(rationalize(Int, 3.0, 0.0)) === 3//1
     @test @inferred(rationalize(Int, 3.0, 0)) === 3//1
-    @test @inferred(rationalize(Int, 33//100; tol=0.1)) === 33//100
+    @test @inferred(rationalize(Int, 33//100; tol=0.1)) === 1//3 # because tol
     @test @inferred(rationalize(Int, 3; tol=0.0)) === 3//1
     @test @inferred(rationalize(Int8, 1000//333)) === Rational{Int8}(3//1)
     @test @inferred(rationalize(Int8, 1000//3)) === Rational{Int8}(1//0)
