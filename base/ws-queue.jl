54bbc609aad60a8ad6341ecb84514688901cd3ce
Valentin Churavy <v.churavy@gmail.com>
2023-06-19 17:06:27 -0400

    Implement WSQueue

---
+++ base/ws-queue.jl
@@ -0,0 +1,111 @@
+# This file is a part of Julia. License is MIT: https://julialang.org/license
+
+# Also see `work-stealing-queue.h` this is a pure Julia re-implementation
+
+# =======
+# Chase and Lev's work-stealing queue, optimized for
+# weak memory models by Le et al.
+#
+# * Chase D., Lev Y. Dynamic Circular Work-Stealing queue
+# * Le N. M. et al. Correct and Efficient Work-Stealing for
+#   Weak Memory Models
+# =======
+
+# mutable so that we don't get a mutex in WSQueue
+mutable struct WSBuffer{T}
+    const buffer::Vector{T}
+    const capacity::Int64
+    const mask::Int64
+    @noinline function WSBuffer{T}(capacity::Int64) where T
+        if nextpow(capacity, 2) != capacity
+            throw(ArgumentError("Capacity must be a power of two"))
+        end
+        if capacity == 0
+            throw(ArgumentError("Capacity can't be zero"))
+        end
+        buffer = Vector{T}(undef, capacity)
+        mask = capacity - 1
+        return new(buffer, capacity, mask)
+    end
+end
+
+function Base.getindex(buf::WSBuffer{T}, idx::Int64) where T
+    @inbounds buf.buffer[idx & buf.capacity]
+end
+
+function Base.setindex!(buf::WSBuffer{T}, val::T, idx::Int64) where T
+    @inbounds buf.buffer[idx & buf.capacity] = val
+end
+
+mutable struct WSQueue{T}
+    @atomic top::Int64
+    @atomic bottom::Int64
+    @atomic buffer::WSBuffer{T}
+    function WSQueue{T}(capacity = 64) where T 
+        new(1, 1, WSBuffer{T}(capacity))
+    end
+end
+
+function Base.push!(q::WSQueue{T}, v::T) where T
+    bottom = @atomic :monotonic q.bottom
+    top    = @atomic :acquire   q.top
+    buffer = @atomic :monotonic q.buffer
+
+    # add unlikely
+    if __unlikely(bottom - top > (buffer.capacity - 1))
+        # @debug "Growing WS buffer" bottom top capacity = buffer.capacity
+        new_buffer = WSBuffer{T}(2*buffer.capacity)
+        copyto!(new_buffer.buffer, buffer.buffer) # TODO only copy active range?
+        @atomic :release q.buffer = new_buffer
+        buffer = new_buffer
+    end
+    buffer[bottom] = v
+    Core.Intrinsics.atomic_fence(:release)
+    @atomic :monotonic q.bottom = bottom + 1
+    return nothing
+end
+
+function Base.pop!(q::WSQueue{T}) where T
+    bottom = (@atomic :monotonic q.bottom) - 1
+    buffer =  @atomic :monotonic q.buffer
+    @atomic :monotonic q.bottom = bottom
+
+    Core.Intrinsics.atomic_fence(:sequentially_consistent) # TODO slow on AMD
+
+    top = @atomic :monotonic q.top
+
+    # todo add likely
+    if __likely(top <= bottom)
+        v = buffer[bottom]
+        if top == bottom
+            _, success = @atomicreplace q.top top => top+1
+            @atomic :monotonic q.bottom = bottom + 1
+            if !success
+                return nothing # failed
+            end
+        end
+        return v
+    else
+        @atomic :monotonic q.bottom = bottom + 1
+        return nothing # failed
+    end
+end
+
+function steal!(q::WSQueue{T}) where T
+    top    = @atomic :acquire q.top
+    Core.Intrinsics.atomic_fence(:sequentially_consistent) # TODO slow on AMD
+    bottom = @atomic :acquire q.bottom
+    if top < bottom
+        buffer = @atomic :monotonic q.buffer
+        v = buffer[top]
+        _, success = @atomicreplace q.top top => top+1
+        if !success
+            return nothing # failed
+        end
+        return v
+    end
+    return nothing # failed 
+end
+
+@inline __likely(cond::Bool) = ccall("llvm.expect", llvmcall, Bool, (Bool, Bool), cond, true)
+@inline __unlikely(cond::Bool) = ccall("llvm.expect", llvmcall, Bool, (Bool, Bool), cond, false)
