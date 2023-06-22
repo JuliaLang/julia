680c7b8a396ce95a73b42c2fbf824352e2366fcd
Ujjwal Sarswat <ujjwalsarswat1@gmail.com>
2023-06-22 11:49:16 +0530

    fix(stdlib/Dates/periods.jl): conversion of empty CompoundPeriod to zero units

--- stdlib/Dates/src/periods.jl
+++ stdlib/Dates/src/periods.jl
@@ -325,7 +325,7 @@ end
 Base.show(io::IO,x::CompoundPeriod) = print(io, string(x))
 
 Base.convert(::Type{T}, x::CompoundPeriod) where T<:Period =
-    isconcretetype(T) ? sum(T, x.periods) : throw(MethodError(convert,(T,x)))
+    isconcretetype(T) ? sum(T, x.periods; init = zero(T)) : throw(MethodError(convert,(T,x)))
 
 # E.g. Year(1) + Day(1)
 (+)(x::Period,y::Period) = CompoundPeriod(Period[x, y])
