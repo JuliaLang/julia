ecaf48a5ee40ba67c61b60f7967503f3eb825f73
Gabriel Baraldi <baraldigabriel@gmail.com>
2023-06-29 11:07:18 -0300

    Move ccall tests to node 1 on 32 bit

--- test/runtests.jl
+++ test/runtests.jl
@@ -71,6 +71,9 @@ function move_to_node1(t)
 end
 
 # Base.compilecache only works from node 1, so precompile test is handled specially
+if Int == Int32
+    move_to_node1("ccall") # ccall seems to be test that most often OOMs on 32-bit
+end
 move_to_node1("precompile")
 move_to_node1("SharedArrays")
 move_to_node1("threads")
