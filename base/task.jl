e025877105630311b7f07d470d3cecda11a3e5d7
Valentin Churavy <vchuravy@users.noreply.github.com>
2023-07-05 12:11:13 +0200

    Remove dynamic dispatch from _wait/wait2 (#50202)
    Co-authored-by: Gabriel Baraldi <baraldigabriel@gmail.com>

--- base/task.jl
+++ base/task.jl
@@ -303,13 +303,14 @@ end
 # just wait for a task to be done, no error propagation
 function _wait(t::Task)
     if !istaskdone(t)
-        lock(t.donenotify)
+        donenotify = t.donenotify::ThreadSynchronizer
+        lock(donenotify)
         try
             while !istaskdone(t)
-                wait(t.donenotify)
+                wait(donenotify)
             end
         finally
-            unlock(t.donenotify)
+            unlock(donenotify)
         end
     end
     nothing
@@ -330,13 +331,14 @@ function _wait2(t::Task, waiter::Task)
             tid = Threads.threadid()
             ccall(:jl_set_task_tid, Cint, (Any, Cint), waiter, tid-1)
         end
-        lock(t.donenotify)
+        donenotify = t.donenotify::ThreadSynchronizer
+        lock(donenotify)
         if !istaskdone(t)
-            push!(t.donenotify.waitq, waiter)
-            unlock(t.donenotify)
+            push!(donenotify.waitq, waiter)
+            unlock(donenotify)
             return nothing
         else
-            unlock(t.donenotify)
+            unlock(donenotify)
         end
     end
     schedule(waiter)
