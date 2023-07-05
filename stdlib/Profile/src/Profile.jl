877b368d78f9fbb6bb698f40f6e9c725ef057bd0
Adnan Alhomssi <adnan.alhomssi@relational.ai>
2023-07-05 12:16:01 +0200

    Use tempdir() to store heap snapshot files instead of abspatch ~= rootdir (#50026)

--- stdlib/Profile/src/Profile.jl
+++ stdlib/Profile/src/Profile.jl
@@ -1234,7 +1234,7 @@ function take_heap_snapshot(filepath::String, all_one::Bool=false)
     return filepath
 end
 function take_heap_snapshot(all_one::Bool=false)
-    f = abspath("$(getpid())_$(time_ns()).heapsnapshot")
+    f = joinpath(tempdir(), "$(getpid())_$(time_ns()).heapsnapshot")
     return take_heap_snapshot(f, all_one)
 end
 
