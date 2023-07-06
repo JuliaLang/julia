c09efd934df375a291a2c9ca33e3bac7c811f4c6
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-07-06 15:42:14 -0500

    More consistent findall output type (take 2) (#48976)
    Fixup for #45538
    Helps with #45495

--- base/array.jl
+++ base/array.jl
@@ -2445,9 +2445,8 @@ julia> findall(x -> x >= 0, d)
 ```
 """
 function findall(testf::Function, A)
-    T = eltype(keys(A))
     gen = (first(p) for p in pairs(A) if testf(last(p)))
-    isconcretetype(T) ? collect(T, gen) : collect(gen)
+    @default_eltype(gen) === Union{} ? collect(@default_eltype(keys(A)), gen) : collect(gen)
 end
 
 # Broadcasting is much faster for small testf, and computing
