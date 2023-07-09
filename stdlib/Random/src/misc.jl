ea104e6bda96c256da7c5f7cc091dc7d1ea3aa63
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-07-09 10:34:56 -0500

    Clarify compat note in randcycle

--- stdlib/Random/src/misc.jl
+++ stdlib/Random/src/misc.jl
@@ -344,8 +344,8 @@ argument specifies a random number generator, see [Random Numbers](@ref).
 The element type of the result is the same as the type of `n`.
 
 !!! compat "Julia 1.1"
-    In Julia 1.1 `randcycle` returns a vector `v` with `eltype(v) == typeof(n)`
-    while in Julia 1.0 `eltype(v) == Int`.
+    In Julia 1.1 and above, `randcycle` returns a vector `v` with 
+    `eltype(v) == typeof(n)` while in Julia 1.0 `eltype(v) == Int`.
 
 # Examples
 ```jldoctest
