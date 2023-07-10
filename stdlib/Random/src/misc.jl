8dd58a48397d7a43e659e3e79fd7027107dbba15
Steven G. Johnson <stevenj@alum.mit.edu>
2023-07-10 12:45:27 -0400

    whitespace fix in randcycle (#50493)

--- stdlib/Random/src/misc.jl
+++ stdlib/Random/src/misc.jl
@@ -344,7 +344,7 @@ argument specifies a random number generator, see [Random Numbers](@ref).
 The element type of the result is the same as the type of `n`.
 
 !!! compat "Julia 1.1"
-    In Julia 1.1 and above, `randcycle` returns a vector `v` with 
+    In Julia 1.1 and above, `randcycle` returns a vector `v` with
     `eltype(v) == typeof(n)` while in Julia 1.0 `eltype(v) == Int`.
 
 # Examples
