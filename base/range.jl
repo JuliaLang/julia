c332e901869d6c5f9c212d3a5670b59f32609683
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-07-17 16:40:02 +0530

    Array(::AbstractRange) should return an Array

--- base/range.jl
+++ base/range.jl
@@ -1377,8 +1377,16 @@ function vcat(rs::AbstractRange{T}...) where T
     return a
 end
 
-Array{T,1}(r::AbstractRange{T}) where {T} = vcat(r)
-collect(r::AbstractRange) = vcat(r)
+function Array{T,1}(r::AbstractRange{T}) where {T}
+    a = Vector{T}(undef, length(r))
+    i = 1
+    for x in r
+        @inbounds a[i] = x
+        i += 1
+    end
+    return a
+end
+collect(r::AbstractRange) = Array(r)
 
 _reverse(r::OrdinalRange, ::Colon) = (:)(last(r), negate(step(r)), first(r))
 function _reverse(r::StepRangeLen, ::Colon)
