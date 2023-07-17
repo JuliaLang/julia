1825b616155d9741ddb78dbeeeeb77b5a4fac6f1
Ben Baumgold <4933671+baumgold@users.noreply.github.com>
2023-07-17 07:57:57 -0400

    fix stacktrace tests

--- test/stacktraces.jl
+++ test/stacktraces.jl
@@ -207,16 +207,16 @@ struct F49231{a,b,c,d,e,f,g} end
     tdl = Base.type_depth_limit
 
     str = repr(typeof(view([1, 2, 3], 1:2)))
-    @test tdl(str, 0, maxdepth = 1) == "SubArray{…}"
-    @test tdl(str, 0, maxdepth = 2) == "SubArray{$Int, 1, Vector{…}, Tuple{…}, true}"
-    @test tdl(str, 0, maxdepth = 3) == "SubArray{$Int, 1, Vector{$Int}, Tuple{UnitRange{…}}, true}"
-    @test tdl(str, 0, maxdepth = 4) == "SubArray{$Int, 1, Vector{$Int}, Tuple{UnitRange{$Int}}, true}"
-    @test tdl(str, 3) == "SubArray{…}"
-    @test tdl(str, 44) == "SubArray{…}"
-    @test tdl(str, 45) == "SubArray{$Int, 1, Vector{…}, Tuple{…}, true}"
-    @test tdl(str, 59) == "SubArray{$Int, 1, Vector{…}, Tuple{…}, true}"
-    @test tdl(str, 60) == "SubArray{$Int, 1, Vector{$Int}, Tuple{UnitRange{…}}, true}"
-    @test tdl(str, 100) == "SubArray{$Int, 1, Vector{$Int}, Tuple{UnitRange{$Int}}, true}"
+    @test tdl(str, 0, maxdepth = 1) == "SubVector{…}"
+    @test tdl(str, 0, maxdepth = 2) == "SubVector{$Int, Vector{…}, Tuple{…}, true}"
+    @test tdl(str, 0, maxdepth = 3) == "SubVector{$Int, Vector{$Int}, Tuple{UnitRange{…}}, true}"
+    @test tdl(str, 0, maxdepth = 4) == "SubVector{$Int, Vector{$Int}, Tuple{UnitRange{$Int}}, true}"
+    @test tdl(str, 3) == "SubVector{…}"
+    @test tdl(str, 44) == "SubVector{$Int, Vector{…}, Tuple{…}, true}"
+    @test tdl(str, 45) == "SubVector{$Int, Vector{…}, Tuple{…}, true}"
+    @test tdl(str, 59) == "SubVector{$Int, Vector{$Int}, Tuple{UnitRange{…}}, true}"
+    @test tdl(str, 60) == "SubVector{$Int, Vector{$Int}, Tuple{UnitRange{…}}, true}"
+    @test tdl(str, 100) == "SubVector{$Int, Vector{$Int}, Tuple{UnitRange{$Int}}, true}"
 
     str = repr(Vector{V} where V<:AbstractVector{T} where T<:Real)
     @test tdl(str, 0, maxdepth = 1) == "Vector{…} where {…}"
