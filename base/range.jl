1b7bf936febef3c7a022c8a837b60963c217df9c
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-07-20 13:15:20 +0530

    Add comment to Array method

--- base/range.jl
+++ base/range.jl
@@ -1380,6 +1380,11 @@ function vcat(rs::AbstractRange{T}...) where T
     return a
 end
 
+# This method differs from that for AbstractArrays as it
+# use iteration instead of indexing. This works even if certain
+# non-standard ranges don't support indexing.
+# See https://github.com/JuliaLang/julia/pull/27302
+# Similarly, collect(r::AbstractRange) uses iteration
 function Array{T,1}(r::AbstractRange{T}) where {T}
     a = Vector{T}(undef, length(r))
     i = 1
