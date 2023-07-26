5ab786539ef678ab1dcf86f22ffb4fa1cb92cf7c
Gabriel Baraldi <baraldigabriel@gmail.com>
2023-07-26 16:35:13 -0300

    Add fallback if we have make a weird GC decision.

--- src/gc.c
+++ src/gc.c
@@ -3331,9 +3331,9 @@ static int _jl_gc_collect(jl_ptls_t ptls, jl_gc_collection_t collection)
             target_allocs = sqrt(((double)heap_size/min_interval * alloc_rate)/(gc_rate * tuning_factor)); // work on multiples of min interval
         }
     }
-    if (target_allocs == 0.0 || thrashing) // If we are thrashing go back to default
+    int bad_result = target_allocs > 2 * jl_atomic_load_relaxed(&gc_heap_stats.heap_target); // Something went wrong
+    if (target_allocs == 0.0 || thrashing || bad_result) // If we are thrashing go back to default
         target_allocs = 2*sqrt((double)heap_size/min_interval);
-
     uint64_t target_heap = (uint64_t)target_allocs*min_interval + heap_size;
     if (target_heap > max_total_memory && !thrashing) // Allow it to go over if we are thrashing if we die we die
         target_heap = max_total_memory;
