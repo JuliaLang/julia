b809a9057879e552c167dc53a14c9452d7b0ada1
Katharine Hyatt <khyatt@flatironinstitute.org>
2023-08-06 13:09:22 -0400

    Some more basic doctests for Sockets

--- stdlib/Sockets/src/addrinfo.jl
+++ stdlib/Sockets/src/addrinfo.jl
@@ -344,7 +344,7 @@ are not guaranteed to be unique beyond their network segment,
 therefore routers do not forward them. Link-local addresses are from
 the address blocks `169.254.0.0/16` or `fe80::/10`.
 
-# Example
+# Examples
 ```julia
 filter(!islinklocaladdr, getipaddrs())
 ```
