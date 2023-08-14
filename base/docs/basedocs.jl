ad0712fdebcad2e2e93d64b9b4c15d51f5da31b5
matthias314 <56549971+matthias314@users.noreply.github.com>
2023-08-14 07:29:54 -0400

    various minor changes to the documentation (#50879)
    A small contribution to an improved documentation. I've bundled a few
    tiny, unrelated changes together instead of submitting many PRs. Let me
    know if you prefer it differently or if you want any other change.
    
    Some comments:
    
    `llvmcall`: The file `test/llvmcall.jl` was mentioned. I've added a link
    to GitHub. However, it works only for tagged version. Improvements
    welcome.
    
    `@sync`: I would like to add links to `Distributed.@spawnat` and
    `Distributed.@distributed`, but I don't know how to do it from one
    package (Base) to another. I once asked about it on
    [Discourse](https://discourse.julialang.org/t/documentation-how-to-add-cross-references-to-functions-in-other-modules/88504),
    but didn't get an answer.
    
    `hasfastin`: now mentioned in the documentation

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -1315,7 +1315,11 @@ a tuple of types. All types, as well as the LLVM code, should be specified as li
 not as variables or expressions (it may be necessary to use `@eval` to generate these
 literals).
 
-See `test/llvmcall.jl` for usage examples.
+[Opaque pointers](https://llvm.org/docs/OpaquePointers.html) (written as `ptr`) are not allowed in the LLVM code.
+
+See
+[`test/llvmcall.jl`](https://github.com/JuliaLang/julia/blob/v$VERSION/test/llvmcall.jl)
+for usage examples.
 """
 Core.Intrinsics.llvmcall
 
