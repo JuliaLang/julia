974a6df19f94716c270ee3daac555f52f2d038a3
Antoine Levitt <antoine.levitt@gmail.com>
2023-08-17 17:25:27 +0200

    Deprecate implicit pseudo-inverses of vectors

--- stdlib/LinearAlgebra/src/deprecated.jl
+++ stdlib/LinearAlgebra/src/deprecated.jl
@@ -5,3 +5,7 @@ rank(A::AbstractMatrix, tol::Real) = rank(A,rtol=tol)
 nullspace(A::AbstractVector, tol::Real) = nullspace(reshape(A, length(A), 1), rtol= tol)
 nullspace(A::AbstractMatrix, tol::Real) = nullspace(A, rtol=tol)
 pinv(A::AbstractMatrix{T}, tol::Real) where T = pinv(A, rtol=tol)
+
+# To be removed in 2.0
+Base.@deprecate /(x::Number, v::AbstractVector)  x*pinv(v)
+Base.@deprecate (\)(a::AbstractVector, b::AbstractArray) = pinv(a) * b
