b60f71a5e37b17f961193ddb45a7b6bfeed67fce
Denis Baručić <barucden@fel.cvut.cz>
2023-08-19 18:27:45 +0200

    Dates: Round micro- and nanoseconds
    When adding a `Microsecond` or a `Nanosecond` to a `DateTime`, the
    conversion to milliseconds rounds instead of truncates.

--- stdlib/Dates/src/types.jl
+++ stdlib/Dates/src/types.jl
@@ -146,7 +146,7 @@ abstract type AbstractDateTime <: TimeType end
 The finest resolution of the time is millisecond (i.e., microseconds or
 nanoseconds cannot be represented by this type). The type supports fixed-point
 arithmetic, and thus is prone to underflowing (and overflowing). A notable
-consequence is truncation when adding a `Microsecond` or a `Nanosecond`.
+consequence is rounding when adding a `Microsecond` or a `Nanosecond`:
 
 ```jldoctest
 julia> dt = DateTime(2023, 8, 19, 17, 45, 32, 900)
@@ -158,11 +158,11 @@ julia> dt + Millisecond(1)
 julia> dt + Microsecond(1000) # 1000us == 1ms
 2023-08-19T17:45:32.901
 
-julia> dt + Microsecond(999) # truncated
-2023-08-19T17:45:32.900
+julia> dt + Microsecond(999) # 999us rounded to 1000us
+2023-08-19T17:45:32.901
 
-julia> dt + Nanosecond(999999) # truncated
-2023-08-19T17:45:32.900
+julia> dt + Microsecond(1499) # 1499 rounded to 1000us
+2023-08-19T17:45:32.901
 ```
 """
 struct DateTime <: AbstractDateTime
