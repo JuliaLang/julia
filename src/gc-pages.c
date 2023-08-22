6468d9e203a408de6ef09b62aa7a602f405a9006
Bradley C. Kuszmaul <kuszmaul@gmail.com>
2023-08-22 14:27:07 -0400

    RAI: Never use MADV_FREE

--- src/gc-pages.c
+++ src/gc-pages.c
@@ -205,7 +205,7 @@ void jl_gc_free_page(jl_gc_pagemeta_t *pg) JL_NOTSAFEPOINT
     }
 #ifdef _OS_WINDOWS_
     VirtualFree(p, decommit_size, MEM_DECOMMIT);
-#elif defined(MADV_FREE)
+#elif 0
     static int supports_madv_free = 1;
     if (supports_madv_free) {
         if (madvise(p, decommit_size, MADV_FREE) == -1) {
