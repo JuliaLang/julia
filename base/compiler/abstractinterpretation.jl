9b7e90de8ec3ef6eb0ffe1f00b8e1bf94d6e45e6
Jeff Bezanson <jeff.bezanson@gmail.com>
2023-08-23 14:38:07 -0400

    fix #50709, type bound error due to free typevar in inferred ccall ret type

--- base/compiler/abstractinterpretation.jl
+++ base/compiler/abstractinterpretation.jl
@@ -2186,6 +2186,12 @@ function sp_type_rewrap(@nospecialize(T), linfo::MethodInstance, isreturn::Bool)
                         T = UnionAll(v, T)
                     end
                 end
+                if has_free_typevars(T)
+                    fv = ccall(:jl_find_free_typevars, Vector{Any}, (Any,), T)
+                    for v in fv
+                        T = UnionAll(v, T)
+                    end
+                end
             else
                 T = rewrap_unionall(T, spsig)
             end
