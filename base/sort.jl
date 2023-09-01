5b05071075e664e8ae9ff97c566bc2a1ab938226
mikmoore <95002244+mikmoore@users.noreply.github.com>
2023-09-01 10:26:44 -0600

    remove outdated language

--- base/sort.jl
+++ base/sort.jl
@@ -339,10 +339,9 @@ Return the index of the first value in `v` that is not ordered before `x`.
 If all values in `v` are ordered before `x`, return `lastindex(v) + 1`.
 
 The vector `v` must be sorted according to the order defined by the keywords.
-`insert!`ing `x` at the returned index will maintain the sorted order. Refer to
-[`sort!`](@ref) for the meaning of the keywords and the definition of
-"greater than" and equivalence. Note that the `by` function is applied to the
-searched value `x` as well as the values in `v`.
+Refer to [`sort!`](@ref) for the meaning and use of the keywords.
+Note that the `by` function is applied to the searched value `x` as well as the
+values in `v`.
 
 The index is generally found using binary search, but there are optimized
 implementations for some inputs.
@@ -378,9 +377,9 @@ Return the index of the last value in `v` that is not ordered after `x`.
 If all values in `v` are ordered after `x`, return `firstindex(v) - 1`.
 
 The vector `v` must be sorted according to the order defined by the keywords.
-Refer to [`sort!`](@ref) for the meaning of the keywords and the definition of
-"less than" and equivalence. Note that the `by` function is applied to the
-searched value `x` as well as the values in `v`.
+Refer to [`sort!`](@ref) for the meaning and use of the keywords.
+Note that the `by` function is applied to the searched value `x` as well as the
+values in `v`.
 
 The index is generally found using binary search, but there are optimized
 implementations for some inputs
