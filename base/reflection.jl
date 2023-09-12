4a24b857849f2797bbcae3ee962b11d901b6b12a
Shuhei Kadowaki <aviatesk@gmail.com>
2023-09-12 21:20:42 +0900

    elaborate `incremental` argument of `Base.generating_output`
    Follows up #51216

--- base/reflection.jl
+++ base/reflection.jl
@@ -2351,9 +2351,10 @@ end
     Base.generating_output([incremental::Bool])::Bool
 
 Return `true` if the current process is being used to pre-generate a
-code cache via any of the `--output-*` command line arguments. The
-optional argument `incremental` controls whether to only return `true`
-for that specific mode of compilation.
+code cache via any of the `--output-*` command line arguments. The optional
+`incremental` argument further specifies the precompilation mode: when set
+to `true`, the function will return `true` only for package precompilation;
+when set to `false`, it will return `true` only for system image generation.
 
 !!! compat "Julia 1.11"
     This function requires at least Julia 1.11.
