9895ee6d20fcdc18d357f0102f7d1805919e219d
Shuhei Kadowaki <aviatesk@gmail.com>
2023-09-13 22:02:44 +0900

    EA: some noncritical updates on EA

--- test/compiler/EscapeAnalysis/setup.jl
+++ test/compiler/EscapeAnalysis/setup.jl
@@ -1,37 +1,16 @@
-include(normpath(@__DIR__, "..", "irutils.jl"))
-include(normpath(@__DIR__, "EAUtils.jl"))
-using Test, Core.Compiler.EscapeAnalysis, .EAUtils
-import Core: Argument, SSAValue, ReturnNode
-const EA = Core.Compiler.EscapeAnalysis
-import .EA: ignore_argescape
+const use_core_compiler = true
 
-isT(T) = (@nospecialize x) -> x === T
-isreturn(@nospecialize x) = isa(x, Core.ReturnNode) && isdefined(x, :val)
-isthrow(@nospecialize x) = Meta.isexpr(x, :call) && Core.Compiler.is_throw_call(x)
-isϕ(@nospecialize x) = isa(x, Core.PhiNode)
-function with_normalized_name(@nospecialize(f), @nospecialize(x))
-    if Meta.isexpr(x, :foreigncall)
-        name = x.args[1]
-        nn = EA.normalize(name)
-        return isa(nn, Symbol) && f(nn)
-    end
-    return false
+if use_core_compiler
+    const EscapeAnalysis = Core.Compiler.EscapeAnalysis
+else
+    include(normpath(Sys.BINDIR, "..", "..", "base", "compiler", "ssair", "EscapeAnalysis", "EscapeAnalysis.jl"))
 end
-isarrayalloc(@nospecialize x) = with_normalized_name(nn->!isnothing(Core.Compiler.alloc_array_ndims(nn)), x)
-isarrayresize(@nospecialize x) = with_normalized_name(nn->!isnothing(EA.array_resize_info(nn)), x)
-isarraycopy(@nospecialize x) = with_normalized_name(nn->EA.is_array_copy(nn), x)
 
-"""
-    is_load_forwardable(x::EscapeInfo) -> Bool
+include("EAUtils.jl")
+include("../irutils.jl")
 
-Queries if `x` is elibigle for store-to-load forwarding optimization.
-"""
-function is_load_forwardable(x::EA.EscapeInfo)
-    AliasInfo = x.AliasInfo
-    # NOTE technically we also need to check `!has_thrown_escape(x)` here as well,
-    # but we can also do equivalent check during forwarding
-    return isa(AliasInfo, EA.IndexableFields) || isa(AliasInfo, EA.IndexableElements)
-end
+using Test, .EscapeAnalysis, .EAUtils
+using .EscapeAnalysis: ignore_argescape
 
 let setup_ex = quote
         mutable struct SafeRef{T}
