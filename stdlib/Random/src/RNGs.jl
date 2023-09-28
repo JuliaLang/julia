daeb1b4c6b8efe8abcb4553a626cc3ca5138f903
Rafael Fourquet <fourquet.rafael@gmail.com>
2023-09-28 14:49:20 +0200

    Random.default_rng: clarify and expand documentation (#51385)
    Co-authored-by: Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>

--- stdlib/Random/src/RNGs.jl
+++ stdlib/Random/src/RNGs.jl
@@ -331,14 +331,22 @@ end
 typeof_rng(::_GLOBAL_RNG) = TaskLocalRNG
 
 """
-    default_rng() -> rng
+    Random.default_rng() -> rng
 
-Return the default global random number generator (RNG).
+Return the default global random number generator (RNG), which is used by `rand`-related functions when
+no explicit RNG is provided.
+
+When the `Random` module is loaded, the default RNG is _randomly_ seeded, via [`Random.seed!()`](@ref):
+this means that each time a new julia session is started, the first call to `rand()` produces a different
+result, unless `seed!(seed)` is called first.
+
+It is thread-safe: distinct threads can safely call `rand`-related functions on `default_rng()` concurrently,
+e.g. `rand(default_rng())`.
 
 !!! note
-    The default RNG is an implementation detail. Across different versions of
-    Julia, you should not expect the default RNG to be always the same, nor that it will
-    return the same stream of random numbers for a given seed.
+    The type of the default RNG is an implementation detail. Across different versions of
+    Julia, you should not expect the default RNG to always have the same type, nor that it will
+    produce the same stream of random numbers for a given seed.
 
 !!! compat "Julia 1.3"
     This function was introduced in Julia 1.3.
