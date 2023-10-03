607b676cbfbde3f8f92626059b3224435eac02e4
Nathan Daly <nathan.daly@relational.ai>
2023-10-03 16:01:54 -0600

    Fix correctness mistake: The edges have to be reordered according to the node order. That's the whole reason this is tricky.
    But i'm not sure now whether the SoAs approach is actually an
    optimization.... It seems like we should probably prefer to inline the
    Edges right into the vector, rather than having to do another random
    lookup into the edges table?

--- stdlib/Profile/src/heapsnapshot_reassemble.jl
+++ stdlib/Profile/src/heapsnapshot_reassemble.jl
@@ -25,6 +25,12 @@ struct Nodes
     self_size::Vector{Int}     # size of the object itself, not including the size of its fields
     edge_count::Vector{UInt32} # number of outgoing edges
     edges::Edges               # outgoing edges
+    # This is the main complexity of the .heapsnapshot format, and it's the reason we need
+    # to read in all the data before writing it out. The edges vector contains all edges,
+    # but organized by which node they came from. First, it contains all the edges coming
+    # out of node 0, then all edges leaving node 1, etc. So we need to have visited all
+    # edges, and assigned them to their corresponding nodes, before we can emit the file.
+    edge_idxs::Vector{Vector{UInt}} # indexes into edges, keeping per-node outgoing edge ids
 end
 function init_nodes(n::Int, e::Int)
     Nodes(
@@ -34,6 +40,7 @@ function init_nodes(n::Int, e::Int)
         Vector{Int}(undef, n),
         Vector{UInt32}(undef, n),
         init_edges(e),
+        [Vector{UInt}() for _ in 1:n],  # Take care to construct n separate empty vectors
     )
 end
 Base.length(n::Nodes) = length(n.type)
@@ -81,9 +88,19 @@ function assemble_snapshot(in_prefix, io::IO)
 
     nodes = init_nodes(node_count, edge_count)
 
+    shouldlog(i) = false
+    # N = 100000
+    # shouldlog(i) = i % N == 0
+    #     N *= 10
+    #     return true
+    # else
+    #     return false
+    # end
+
     # Parse nodes with empty edge counts that we need to fill later
     # TODO: preallocate line buffer
     for (i, line) in enumerate(eachline(string(in_prefix, ".nodes")))
+        shouldlog(i) && println("Parsing node $i")
         iter = eachsplit(line, ',')
         x, s = iterate(iter)
         node_type = parse(Int8, x)
@@ -106,6 +123,15 @@ function assemble_snapshot(in_prefix, io::IO)
         nodes.id[i] = id
         nodes.self_size[i] = self_size
         nodes.edge_count[i] = edge_count
+
+        shouldlog(i) && begin
+            println("Parsed node $i")
+            @show node_type
+            @show node_name_idx
+            @show id
+            @show self_size
+            @show edge_count
+        end
     end
 
     # Parse the edges to fill in the edge counts for nodes and correct the to_node offsets
@@ -125,6 +151,7 @@ function assemble_snapshot(in_prefix, io::IO)
         nodes.edges.name_or_index[i] = edge_name_or_index
         nodes.edges.to_pos[i] = to_node * 7 # 7 fields per node, the streaming format doesn't multiply the offset by 7
         nodes.edge_count[from_node + 1] += UInt32(1)  # C and JSON use 0-based indexing
+        push!(nodes.edge_idxs[from_node + 1], i) # Index into nodes.edges
     end
 
     _digits_buf = zeros(UInt8, ndigits(typemax(UInt)))
@@ -143,18 +170,29 @@ function assemble_snapshot(in_prefix, io::IO)
         _write_decimal_number(io, nodes.edge_count[i], _digits_buf)
         print(io, ",0,0")
     end
-    println(io, "],\"edges\":[")
-    for i in 1:length(nodes.edges)
-        i > 1 && println(io, ",")
-        _write_decimal_number(io, nodes.edges.type[i], _digits_buf)
-        print(io, ",")
-        _write_decimal_number(io, nodes.edges.name_or_index[i], _digits_buf)
-        print(io, ",")
-        _write_decimal_number(io, nodes.edges.to_pos[i], _digits_buf)
+    print(io, "],\"edges\":[")
+    e = 1
+    for n in 1:length(nodes)
+        count = nodes.edge_count[n]
+        len_edges = length(nodes.edge_idxs[n])
+        @assert count == len_edges "For node $n: $count != $len_edges"
+        for i in nodes.edge_idxs[n]
+            e > 1 && print(io, ",")
+            println(io)
+            _write_decimal_number(io, nodes.edges.type[i], _digits_buf)
+            print(io, ",")
+            _write_decimal_number(io, nodes.edges.name_or_index[i], _digits_buf)
+            print(io, ",")
+            _write_decimal_number(io, nodes.edges.to_pos[i], _digits_buf)
+            if !(nodes.edges.to_pos[i] % 7 == 0)
+                @warn "Bug in to_pos for edge $i from node $n: $(nodes.edges.to_pos[i])"
+            end
+            e += 1
+        end
     end
+    println(io, "],")
     open(string(in_prefix, ".strings"), "r") do strings_io
         skip(strings_io, 2) # skip "{\n"
-        println(io, "],")
         write(io, strings_io) # strings contain the trailing "}" so we close out what we opened in preamble
     end
     return nothing
--- stdlib/Profile/test/heapsnapshot_reassemble.jl
+++ stdlib/Profile/test/heapsnapshot_reassemble.jl
@@ -12,7 +12,19 @@ using Test
     end
     @test test_write(0) == "0"
     @test test_write(99) == "99"
+
+    @test test_write(UInt8(0)) == "0"
+    @test test_write(UInt32(0)) == "0"
+    @test test_write(Int32(0)) == "0"
+
+    @test test_write(UInt8(99)) == "99"
+    @test test_write(UInt32(99)) == "99"
+    @test test_write(Int32(99)) == "99"
+
     # Sample among possible UInts we might print
+    for x in typemin(UInt8):typemax(UInt8)
+        @test test_write(x) == string(x)
+    end
     for x in typemin(UInt):typemax(UInt)÷10001:typemax(UInt)
         @test test_write(x) == string(x)
     end
