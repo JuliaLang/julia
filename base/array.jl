d5b41d937ce6b32b4bdf23481fc3de9635e08e37
Petr Vana <petvana@gmail.com>
2023-10-07 16:15:51 +0200

    Controll shrinkage at `sizehint!`

--- base/array.jl
+++ base/array.jl
@@ -1321,7 +1321,7 @@ function resize!(a::Vector, nl::Integer)
 end
 
 """
-    sizehint!(s, n) -> s
+    sizehint!(s, n; shrink = true) -> s
 
 Suggest that collection `s` reserve capacity for at least `n` elements. That is, if
 you expect that you're going to have to push a lot of values onto `s`, you can avoid
@@ -1342,13 +1342,19 @@ For types that support `sizehint!`,
    `Base`.
 
 3. `empty!` is nearly costless (and O(1)) for types that support this kind of preallocation.
+
+4. `shrink` controls if the collection can be shrunk.
 """
 function sizehint! end
 
 function sizehint!(a::Vector, sz::Integer)
+    # TODO - controll shrinkage
     ccall(:jl_array_sizehint, Cvoid, (Any, UInt), a, sz)
     a
 end
+
+# Fall-back implementation for non-shrinkable collections
+sizehint!(a, sz; shrink) = sizehint!(a, sz)
 
 """
     pop!(collection) -> item
