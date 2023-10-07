85b048abd816f8ab865c89264b0ee86c95d6c251
Valentin Churavy <v.churavy@gmail.com>
2023-10-07 11:46:04 -0400

    excise Test

--- stdlib/Test/test/nothrow_testset.jl
+++
@@ -1,8 +0,0 @@
-# This file is a part of Julia. License is MIT: https://julialang.org/license
-
-mutable struct NoThrowTestSet <: Test.AbstractTestSet
-    results::Vector
-    NoThrowTestSet(desc) = new([])
-end
-Test.record(ts::NoThrowTestSet, t::Test.Result) = (push!(ts.results, t); t)
-Test.finish(ts::NoThrowTestSet) = ts.results
--- stdlib/Test/test/test_pop_testset_exec.jl
+++
@@ -1,8 +0,0 @@
-# This file is a part of Julia. License is MIT: https://julialang.org/license
-
-using Test
-
-@testset begin
-    @test false
-    return
-end
