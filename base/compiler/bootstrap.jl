38bfb950d9f68a002118cdbb0547c1b31bbef2fc
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-10-17 22:46:04 +0530

    Update bootstrap.jl
    perform_type_inference function added to handle type inference and function signatures are collected in Signature array.

--- base/compiler/bootstrap.jl
+++ base/compiler/bootstrap.jl
@@ -5,48 +5,54 @@
 # especially try to make sure any recursive and leaf functions have concrete signatures,
 # since we won't be able to specialize & infer them at runtime
 
-let time() = ccall(:jl_clock_now, Float64, ())
+module Main
+    import Base: time
+    import JuliaInterpreter: NativeInterpreter
+    import Core.Compiler: typeinf_ext, typeinf, typeinf_edge, _methods_by_ftype, MethodMatch, typeinf_type
 
-    interp = NativeInterpreter()
+    # Define custom types here, if needed
 
-    # analyze_escapes_tt = Tuple{typeof(analyze_escapes), IRCode, Int, TODO}
-    optimize_tt = Tuple{typeof(optimize), NativeInterpreter, OptimizationState{NativeInterpreter}, InferenceResult}
-    fs = Any[
-        # we first create caches for the optimizer, because they contain many loop constructions
-        # and they're better to not run in interpreter even during bootstrapping
-        #=analyze_escapes_tt,=# optimize_tt,
-        # then we create caches for inference entries
-        typeinf_ext, typeinf, typeinf_edge,
+    # Function signatures
+    signatures = Any[
+        Tuple{typeof(optimize), NativeInterpreter, OptimizationState{NativeInterpreter}, InferenceResult},
+        # Add more function signatures as needed
     ]
-    # tfuncs can't be inferred from the inference entries above, so here we infer them manually
+
+    # Add function types manually
     for x in T_FFUNC_VAL
-        push!(fs, x[3])
+        push!(signatures, Tuple{typeof(x[3]), Vararg{Any}})
     end
+
     for i = 1:length(T_IFUNC)
         if isassigned(T_IFUNC, i)
             x = T_IFUNC[i]
-            push!(fs, x[3])
+            push!(signatures, Tuple{typeof(x[3]), Vararg{Any}})
         else
             println(stderr, "WARNING: tfunc missing for ", reinterpret(IntrinsicFunction, Int32(i)))
         end
     end
-    starttime = time()
-    for f in fs
-        if isa(f, DataType) && f.name === typename(Tuple)
-            tt = f
-        else
-            tt = Tuple{typeof(f), Vararg{Any}}
-        end
-        for m in _methods_by_ftype(tt, 10, get_world_counter())::Vector
-            # remove any TypeVars from the intersection
-            m = m::MethodMatch
-            typ = Any[m.spec_types.parameters...]
-            for i = 1:length(typ)
-                typ[i] = unwraptv(typ[i])
+
+    # A method to perform type inference
+    function perform_type_inference(interp::NativeInterpreter, signatures::Vector{T}) where T
+        starttime = time()
+        for sig in signatures
+            for m in _methods_by_ftype(sig, 10, get_world_counter())::Vector{MethodMatch}
+                # Remove TypeVars from the intersection
+                typ = Any[m.spec_types.parameters...]
+                for i = 1:length(typ)
+                    typ[i] = unwraptv(typ[i])
+                end
+                typeinf_type(interp, m.method, Tuple{typ...}, m.sparams)
             end
-            typeinf_type(interp, m.method, Tuple{typ...}, m.sparams)
         end
+        endtime = time()
+        println("Core.Compiler ──── ", sub_float(endtime, starttime), " seconds")
     end
-    endtime = time()
-    println("Core.Compiler ──── ", sub_float(endtime,starttime), " seconds")
+
+    # Initialize the NativeInterpreter
+    interp = NativeInterpreter()
+
+    # Perform type inference
+    perform_type_inference(interp, signatures)
 end
+
