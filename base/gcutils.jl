1ec506e3708725c7849f94c8745caa02f25895f1
d-netto <diogonetto.dcn@gmail.com>
2023-10-20 15:33:54 -0300

    Fix incorrect GC docstring

--- base/gcutils.jl
+++ base/gcutils.jl
@@ -119,9 +119,9 @@ const GC_INCREMENTAL = 2
     GC.gc([full=true])
 
 Perform garbage collection. The argument `full` determines the kind of
-collection: A full collection (default) sweeps all objects, which makes the
-next GC scan much slower, while an incremental collection may only sweep
-so-called young objects.
+collection: a full collection (default) traverses all live objects (e.g. full mark)
+and should reclaim memory from all unreachable objects. An incremental collection only
+reclaims memory from young objects which are not reachable.
 
 !!! warning
     Excessive use will likely lead to poor performance.
