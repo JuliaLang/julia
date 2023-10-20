467e2ffc3990c25bce8c9aee57b139133a06d66d
Simeon David Schaub <schaub@mit.edu>
2023-10-20 16:09:22 +0200

    cleanup

--- test/cmdlineargs.jl
+++ test/cmdlineargs.jl
@@ -997,7 +997,10 @@ end
 
 # test --bug-report=rr
 if Sys.islinux() && Sys.ARCH in (:i686, :x86_64, :aarch64) # rr is only available on these platforms
-    @test success(pipeline(setenv(`$(Base.julia_cmd()) --color=yes --bug-report=rr-local -e 'exit()'`,
-                                  "JULIA_RR_RECORD_ARGS" => "-n --nested=ignore");
-                           stdout, stderr))
+    mktempdir() do temp_trace_dir
+        @test success(pipeline(setenv(`$(Base.julia_cmd()) --bug-report=rr-local -e 'exit()'`,
+                                      "JULIA_RR_RECORD_ARGS" => "-n --nested=ignore",
+                                      "_RR_TRACE_DIR" => temp_trace_dir);
+                               #=stdout, stderr=#))
+    end
 end
