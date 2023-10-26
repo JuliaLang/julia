edb088c1496e16843c7eba4f3d9f561ca8bae24a
Valentin Churavy <v.churavy@gmail.com>
2023-10-26 17:27:40 -0400

    make build step sensitive to LLVM config

--- deps/llvmdialects.mk
+++ deps/llvmdialects.mk
@@ -1,29 +1,40 @@
 ## LLVMDialects
+include $(SRCDIR)/llvm.version
+include $(SRCDIR)/llvm-ver.make
+include $(SRCDIR)/llvm-options.mk
+
 # ifneq ($(USE_BINARYBUILDER_LLVMDIALECTS),1)
 
 LLVMDIALECTS_GIT_URL := https://github.com/GPUOpen-Drivers/llvm-dialects.git
 LLVMDIALECTS_TAR_URL = https://api.github.com/repos/GPUOpen-Drivers/llvm-dialects/tarball/$1
 $(eval $(call git-external,llvmdialects,LLVMDIALECTS,CMakeLists.txt,,$(SRCCACHE)))
 
+LLVMDIALECTS_BUILDDIR_withtype := $(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build_$(LLVM_BUILDTYPE)
+
 ifeq ($(USE_SYSTEM_LLVM), 0)
 $(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-configured: | $(build_prefix)/manifest/llvm
 endif
 
 # TODO: We are not shipping `lib/cmake/llvm`
-LLVMDIALECTS_OPTS := $(CMAKE_COMMON) -DCMAKE_BUILD_TYPE=Release -DLLVM_ROOT=$(build_libdir)/cmake/llvm
+LLVMDIALECTS_OPTS := $(CMAKE_COMMON) -DCMAKE_BUILD_TYPE=Release -DLLVM_ROOT="$(build_libdir)/cmake/llvm" \
+					 -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE="$(LLVM_CMAKE_BUILDTYPE)"
+ifeq ($(LLVM_ASSERTIONS), 1)
+LLVMDIALECTS_OPTS += -DLLVM_ENABLE_ASSERTIONS:BOOL=ON
+endif # LLVM_ASSERTIONS
+
 LLVMDIALECTS_SRC_PATH := $(SRCCACHE)/$(LLVMDIALECTS_SRC_DIR)
 
-$(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-configured: $(LLVMDIALECTS_SRC_PATH)/source-extracted
+$(LLVMDIALECTS_BUILDDIR_withtype)/build-configured: $(LLVMDIALECTS_SRC_PATH)/source-extracted
 	mkdir -p $(dir $@)
 	cd $(dir $@) && \
 	$(CMAKE) $(dir $<) $(LLVMDIALECTS_OPTS)
 	echo 1 > $@
 
-$(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-compiled: $(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-configured
+$(LLVMDIALECTS_BUILDDIR_withtype)/build-compiled: $(LLVMDIALECTS_BUILDDIR_withtype)/build-configured
 	$(MAKE) -C $(dir $<)
 	echo 1 > $@
 
-$(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-checked: $(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-compiled
+$(LLVMDIALECTS_BUILDDIR_withtype)/build-checked: $(LLVMDIALECTS_BUILDDIR_withtype)/build-compiled
 ifeq ($(OS),$(BUILD_OS))
 	$(MAKE) -C $(dir $@) test
 endif
@@ -39,19 +50,19 @@ define LLVMDIALECTS_INSTALL
 endef
 
 $(eval $(call staged-install, \
-	llvmdialects,$(LLVMDIALECTS_SRC_DIR), \
+	llvmdialects,$$(LLVMDIALECTS_SRC_DIR)/build_$$(LLVM_BUILDTYPE), \
 	LLVMDIALECTS_INSTALL,,,))
 
 clean-llvmdialects:
-	-rm -f $(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-configured $(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-compiled
-	-$(MAKE) -C $(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR) clean
+	-rm -f $(LLVMDIALECTS_BUILDDIR_withtype)/build-configured $(LLVMDIALECTS_BUILDDIR_withtype)/build-compiled
+	-$(MAKE) -C $(LLVMDIALECTS_BUILDDIR_withtype) clean
 
 get-llvmdialects: $(LLVMDIALECTS_SRC_FILE)
 extract-llvmdialects: $(SRCCACHE)/$(LLVMDIALECTS_SRC_DIR)/source-extracted
-configure-llvmdialects: $(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-configured
-compile-llvmdialects: $(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-compiled
+configure-llvmdialects: $(LLVMDIALECTS_BUILDDIR_withtype)/build-configured
+compile-llvmdialects: $(LLVMDIALECTS_BUILDDIR_withtype)/build-compiled
 fastcheck-llvmdialects: #none
-check-llvmdialects: $(BUILDDIR)/$(LLVMDIALECTS_SRC_DIR)/build-checked
+check-llvmdialects: $(LLVMDIALECTS_BUILDDIR_withtype)/build-checked
 
 #else # USE_BINARYBUILDER_LIBGIT2
 
