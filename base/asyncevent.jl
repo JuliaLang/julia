0e0d8e17c7b41ef2e43ce6e903b06a7f1541515d
Jameson Nash <vtjnash@gmail.com>
2023-10-30 16:27:17 -0400

    Update asyncevent.jl

--- base/asyncevent.jl
+++ base/asyncevent.jl
@@ -327,11 +327,11 @@ end
 """
     timedwait(testcb, timeout::Real; pollint::Real=0.1)
 
-Waits until `testcb()` returns `true` or `timeout` seconds have passed, whichever is earlier.
+Wait until `testcb()` returns `true` or `timeout` seconds have passed, whichever is earlier.
 The test function is polled every `pollint` seconds. The minimum value for `pollint` is 0.001 seconds,
 that is, 1 millisecond.
 
-Returns `:ok` or `:timed_out`.
+Return `:ok` or `:timed_out`.
 
 # Examples
 ```jldoctest
