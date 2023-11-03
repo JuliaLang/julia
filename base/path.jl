JuliaLang/julia

commit 34dfa72ad529c51769201befeb4423372fd9a556

splitdrive under windows (#19695)
* splitdrive under windows

* splitdrive under windows tests

* more test

* move tests outside loop over S

* changed test cases to S("...")

base/path.jl
@@ -34,7 +34,7 @@ elseif is_windows()
     const path_ext_splitter = r"^((?:.*[/\\])?(?:\.|[^/\\\.])[^/\\]*?)(\.[^/\\\.]*|)$"
 
     function splitdrive(path::String)
-        m = match(r"^(\w+:|\\\\\w+\\\w+|\\\\\?\\UNC\\\w+\\\w+|\\\\\?\\\w+:|)(.*)$", path)
+        m = match(r"^([^\\]+:|\\\\[^\\]+\\[^\\]+|\\\\\?\\UNC\\[^\\]+\\[^\\]+|\\\\\?\\[^\\]+:|)(.*)$", path)
         String(m.captures[1]), String(m.captures[2])
     end
 else

test/path.jl
@@ -68,6 +68,15 @@ for S in (String, GenericString)
     @test joinpath(splitdir(S(homedir()))...) == homedir()
     @test string(splitdrive(S(homedir()))...) == homedir()
 
+    if is_windows()
+        @test splitdrive(S("\\\\servername\\hello.world\\filename.ext")) ==
+            ("\\\\servername\\hello.world","\\filename.ext")
+        @test splitdrive(S("\\\\servername.com\\hello.world\\filename.ext")) ==
+            ("\\\\servername.com\\hello.world","\\filename.ext")
+        @test splitdrive(S("C:\\foo\\bar")) ==
+            ("C:","\\foo\\bar")
+    end
+
     @test splitext(S("")) == ("", "")
     @test splitext(S(".")) == (".", "")
     @test_broken splitext(S("..")) == ("..", "")

commit 09cbae8fe03315288c1bb53ba7ce24061bfb9f26

Revise Windows implementation of splitdrive (#42204)
This PR makes three improvements to the Windows `splitdrive`
implementation:

1. The matched regex is split into pieces and annotated.
2. Forward and backward slashes are considered equivalent. This fixes
   #38492.
3. The patterns in the regex are reordered so that long UNC paths and
   long drive letters are once more recognized. This has been broken
   since #19695 (Julia 0.5).

Co-authored-by: Jameson Nash <vtjnash@gmail.com>

base/path.jl
@@ -34,8 +34,25 @@ elseif Sys.iswindows()
     const path_dir_splitter = r"^(.*?)([/\\]+)([^/\\]*)$"sa
     const path_ext_splitter = r"^((?:.*[/\\])?(?:\.|[^/\\\.])[^/\\]*?)(\.[^/\\\.]*|)$"sa
 
+    const splitdrive_re = let
+        # Slash in either direction.
+        S = raw"[\\/]"
+        # Not a slash in either direction.
+        N = raw"[^\\/]"
+        # Drive letter, e.g. `C:`
+        drive = "$(N)+:"
+        # UNC path, e.g. `\\server\share`
+        unc = "$(S)$(S)$(N)+$(S)$(N)+"
+        # Long drive letter, e.g. `\\?\C:`
+        long_drive = "$(S)$(S)\\?$(S)$(drive)"
+        # Long UNC path, e.g. `\\?\UNC\server\share`
+        long_unc = "$(S)$(S)\\?$(S)UNC$(S)$(N)+$(S)$(N)+"
+        # Need to match the long patterns first so they get priority.
+        Regex("^($long_unc|$long_drive|$unc|$drive|)(.*)\$", "sa")
+    end
+
     function splitdrive(path::String)
-        m = match(r"^([^\\]+:|\\\\[^\\]+\\[^\\]+|\\\\\?\\UNC\\[^\\]+\\[^\\]+|\\\\\?\\[^\\]+:|)(.*)$"sa, path)::AbstractMatch
+        m = match(splitdrive_re, path)::AbstractMatch
         String(something(m.captures[1])), String(something(m.captures[2]))
     end
 else