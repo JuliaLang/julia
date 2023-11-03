5ea25eeffeb1ae82bc672935831fc22343ce4f26
Diogo Netto <61364108+d-netto@users.noreply.github.com>
2023-11-03 14:30:31 -0300

    Suggestion from review: GC may decide to trigger a full collection either way
    Co-authored-by: Kiran Pamnany <kpamnany@users.noreply.github.com>

--- base/gcutils.jl
+++ base/gcutils.jl
@@ -123,6 +123,9 @@ collection: a full collection (default) traverses all live objects (i.e. full ma
 and should reclaim memory from all unreachable objects. An incremental collection only
 reclaims memory from young objects which are not reachable.
 
+The GC may decide to perform a full collection even if an incremental collection was
+requested.
+
 !!! warning
     Excessive use will likely lead to poor performance.
 """
