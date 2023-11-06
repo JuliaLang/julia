069c453f88312ec6d3d4f0f98a2b235728d62930
Lilith Hafner <Lilith.Hafner@gmail.com>
2023-11-05 21:22:10 -0600

    accept that invalid lt continues to work

--- test/sorting.jl
+++ test/sorting.jl
@@ -752,9 +752,10 @@ end
     for alg in safe_algs
         @test sort(1:n, alg=alg, lt = (i,j) -> v[i]<=v[j]) == perm
     end
-    # Broken by the introduction of BracketedSort in #52006 which is unstable
-    @test_broken partialsort(1:n, 172, lt = (i,j) -> v[i]<=v[j]) == perm[172]
-    @test_broken partialsort(1:n, 315:415, lt = (i,j) -> v[i]<=v[j]) == perm[315:415]
+    # This could easily break with minor heuristic adjustments
+    # because partialsort is not even guaranteed to be stable:
+    @test partialsort(1:n, 172, lt = (i,j) -> v[i]<=v[j]) == perm[172]
+    @test partialsort(1:n, 315:415, lt = (i,j) -> v[i]<=v[j]) == perm[315:415]
 
     # lt can be very poorly behaved and sort will still permute its input in some way.
     for alg in safe_algs
