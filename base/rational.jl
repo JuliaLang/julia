3addfd69f08f44f76eaa79e5568b8ca3e2f9bf1b
hyrodium <hyrodium@gmail.com>
2023-11-07 01:37:35 +0900

    update docstring by code review

--- base/rational.jl
+++ base/rational.jl
@@ -46,8 +46,13 @@ end
 """
     //(num, den)
 
-Divide two numbers, giving a result with [`Rational`](@ref).
-Note that [`AbstractFloat`](@ref) arguments are not accepted.
+Divide two integers or rational numbers, giving a [`Rational`](@ref) result.
+More generally, `//` can be used for exact rational division of other numeric types
+with integer or rational components, such as complex numbers with integer components.
+
+Note that floating-point ([`AbstractFloat`](@ref)) arguments are not permitted by `//`
+(even if the values are rational).
+The arguments must be subtypes of [`Integer`](@ref), `Rational`, or composites thereof.
 
 # Examples
 ```jldoctest
@@ -57,8 +62,8 @@ julia> 3 // 5
 julia> (3 // 5) // (2 // 1)
 3//10
 
-julia> (2+im) // (1-2im)
-0//1 + 1//1*im
+julia> (1+2im) // (3+4im)
+11//25 + 2//25*im
 
 julia> 1.0 // 2
 ERROR: MethodError: no method matching //(::Float64, ::Int64)
