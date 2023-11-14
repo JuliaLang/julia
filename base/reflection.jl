c11ad3029aab25cd51868a98c977c756c0cbd819
Stefan Karpinski <stefan@karpinski.org>
2023-11-14 16:30:20 -0500

    @‌code_{llvm,native}: change default debuginfo to :none

--- base/reflection.jl
+++ base/reflection.jl
@@ -1107,7 +1107,7 @@ function code_lowered(@nospecialize(f), @nospecialize(t=Tuple); generated::Bool=
     if @isdefined(IRShow)
         debuginfo = IRShow.debuginfo(debuginfo)
     elseif debuginfo === :default
-        debuginfo = :source
+        debuginfo = :none
     end
     if debuginfo !== :source && debuginfo !== :none
         throw(ArgumentError("'debuginfo' must be either :source or :none"))
@@ -1584,7 +1584,7 @@ function code_typed_by_type(@nospecialize(tt::Type);
     if @isdefined(IRShow)
         debuginfo = IRShow.debuginfo(debuginfo)
     elseif debuginfo === :default
-        debuginfo = :source
+        debuginfo = :none
     end
     if debuginfo !== :source && debuginfo !== :none
         throw(ArgumentError("'debuginfo' must be either :source or :none"))
