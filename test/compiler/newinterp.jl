d1a01f3d7d984ea81339e8f9be77f905a6124e9c
Valentin Churavy <v.churavy@gmail.com>
2023-11-23 11:35:02 -0500

    fix newinterp

--- test/compiler/newinterp.jl
+++ test/compiler/newinterp.jl
@@ -14,23 +14,20 @@ macro newinterp(InterpName)
     C = Core
     CC = Core.Compiler
     quote
-        struct $InterpCacheName
-            dict::IdDict{$C.MethodInstance,$C.CodeInstance}
-        end
-        $InterpCacheName() = $InterpCacheName(IdDict{$C.MethodInstance,$C.CodeInstance}())
+        $InterpCacheName() = $CC.InternalCodeCache($InterpCacheName)
         struct $InterpName <: $CC.AbstractInterpreter
             meta # additional information
             world::UInt
             inf_params::$CC.InferenceParams
             opt_params::$CC.OptimizationParams
             inf_cache::Vector{$CC.InferenceResult}
-            code_cache::$InterpCacheName
+            code_cache::$CC.InternalCodeCache
             function $InterpName(meta = nothing;
                                  world::UInt = Base.get_world_counter(),
                                  inf_params::$CC.InferenceParams = $CC.InferenceParams(),
                                  opt_params::$CC.OptimizationParams = $CC.OptimizationParams(),
                                  inf_cache::Vector{$CC.InferenceResult} = $CC.InferenceResult[],
-                                 code_cache::$InterpCacheName = $InterpCacheName())
+                                 code_cache::$CC.InternalCodeCache = $InterpCacheName())
                 return new(meta, world, inf_params, opt_params, inf_cache, code_cache)
             end
         end
@@ -39,9 +36,6 @@ macro newinterp(InterpName)
         $CC.get_world_counter(interp::$InterpName) = interp.world
         $CC.get_inference_cache(interp::$InterpName) = interp.inf_cache
         $CC.code_cache(interp::$InterpName) = $CC.WorldView(interp.code_cache, $CC.WorldRange(interp.world))
-        $CC.get(wvc::$CC.WorldView{$InterpCacheName}, mi::$C.MethodInstance, default) = get(wvc.cache.dict, mi, default)
-        $CC.getindex(wvc::$CC.WorldView{$InterpCacheName}, mi::$C.MethodInstance) = getindex(wvc.cache.dict, mi)
-        $CC.haskey(wvc::$CC.WorldView{$InterpCacheName}, mi::$C.MethodInstance) = haskey(wvc.cache.dict, mi)
-        $CC.setindex!(wvc::$CC.WorldView{$InterpCacheName}, ci::$C.CodeInstance, mi::$C.MethodInstance) = setindex!(wvc.cache.dict, ci, mi)
+        $CC.cache_owner(::$InterpName) = $InterpCacheName
     end
 end
