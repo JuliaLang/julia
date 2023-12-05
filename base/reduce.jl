5cdca767ecd228a75ecba68435f4b1b0e4852044
Steven G. Johnson <stevenj@alum.mit.edu>
2023-12-04 23:06:11 -0500

    tweak

--- base/reduce.jl
+++ base/reduce.jl
@@ -288,7 +288,7 @@ function mapreduce_impl(f, op, nt, itr)
     a2, state = it
     v = op(nt isa _InitialValue ? f(a1) : op(nt, f(a1)), f(a2))
     n = pairwise_blocksize(f, op)
-    v, state = _mapreduce_impl(f, op, v, itr, state, n)
+    v, state = _mapreduce_impl(f, op, v, itr, state, n-2)
     while state !== nothing
         v, state = _mapreduce_impl(f, op, v, itr, state, n)
         n *= 2
