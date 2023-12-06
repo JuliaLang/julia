9aebb4cdd3eee2f13c79ae0daa7bc78b2567c82f
Sukera <Seelengrab@users.noreply.github.com>
2023-12-06 18:39:05 +0100

    Allow type hashing to be concretely evaluated

--- base/hashing.jl
+++ base/hashing.jl
@@ -29,7 +29,10 @@ See also: [`objectid`](@ref), [`Dict`](@ref), [`Set`](@ref).
 """
 hash(x::Any) = hash(x, zero(UInt))
 hash(w::WeakRef, h::UInt) = hash(w.value, h)
-hash(T::Type, h::UInt) = hash_uint(3h - ccall(:jl_type_hash, UInt, (Any,), T))
+
+# Types can't be deleted, so marking as total allows the compiler to look up the hash
+@assume_effects :total _typehash(T::Type) = ccall(:jl_type_hash, UInt, (Any,), T)
+hash(T::Type, h::UInt) = hash_uint(3h - _typehash(T))
 
 ## hashing general objects ##
 
