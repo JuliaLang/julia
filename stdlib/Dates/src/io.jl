0e0cd5cb6abd7ad4eda454cf4569be43e7cc3b10
Páll Haraldsson <Pall.Haraldsson@gmail.com>
2023-12-11 23:01:03 +0000

    Doc: upper case

--- stdlib/Dates/src/io.jl
+++ stdlib/Dates/src/io.jl
@@ -13,7 +13,7 @@ abstract type AbstractDateToken end
     tryparsenext(tok::AbstractDateToken, str::String, i::Int, len::Int, locale::DateLocale)
 
 `tryparsenext` parses for the `tok` token in `str` starting at index `i`.
-`len` is the length of the string.  parsing can be optionally based on the
+`len` is the length of the string.  Parsing can be optionally based on the
 `locale`. If a `tryparsenext` method does not need a locale, it can leave
 the argument out in the method definition.
 
