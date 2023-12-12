aea1a92f2658aae65b725a556240ef0f0800847c
Zentrik <Zentrik@users.noreply.github.com>
2023-12-12 19:40:41 +0000

    Switch to FxHasher for 32bit values

--- src/support/hashing.c
+++ src/support/hashing.c
@@ -16,13 +16,7 @@ extern "C" {
 
 uint32_t int32hash(uint32_t a)
 {
-    a = (a+0x7ed55d16) + (a<<12);
-    a = (a^0xc761c23c) ^ (a>>19);
-    a = (a+0x165667b1) + (a<<5);
-    a = (a+0xd3a2646c) ^ (a<<9);
-    a = (a+0xfd7046c5) + (a<<3);
-    a = (a^0xb55a4f09) ^ (a>>16);
-    return a;
+    return a * 0x9e3779b9;
 }
 
 // FxHasher
