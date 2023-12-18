cc6cd07fcf66bdfc8efc24a976b5580a929fd87c
Elliot Saba <staticfloat@gmail.com>
2023-12-18 10:17:51 -0800

    Delete trailing whitespace

--- stdlib/Sockets/src/Sockets.jl
+++ stdlib/Sockets/src/Sockets.jl
@@ -570,7 +570,7 @@ end
 Nagle's algorithm batches multiple small TCP packets into larger
 ones. This can improve throughput but worsen latency. Nagle's algorithm
 is enabled by default. This function sets whether Nagle's algorithm is
-active on a given TCP server or socket. The opposite option is called 
+active on a given TCP server or socket. The opposite option is called
 `TCP_NODELAY` in other languages.
 
 !!! compat "Julia 1.3"
