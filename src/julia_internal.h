cd4c9830ee82621671cdae043d1380be018e9ed9
Zentrik <Zentrik@users.noreply.github.com>
2023-12-20 15:31:26 +0000

    Revert "Switch to FxHasher for 32bit values"
    This reverts commit 80ee6228496b03ec98197f17e0f452145acf528f.
    We have a perf regression since first commit, I've had trouble getting reliable benchmarking results so reverting this in case this caused a regression.

--- src/julia_internal.h
+++ src/julia_internal.h
@@ -278,7 +278,13 @@ extern JL_DLLEXPORT _Atomic(uint64_t) jl_cumulative_recompile_time;
 
 STATIC_INLINE uint32_t jl_int32hash_fast(uint32_t a)
 {
-    return a * 0x9e3779b9;
+//    a = (a+0x7ed55d16) + (a<<12);
+//    a = (a^0xc761c23c) ^ (a>>19);
+//    a = (a+0x165667b1) + (a<<5);
+//    a = (a+0xd3a2646c) ^ (a<<9);
+//    a = (a+0xfd7046c5) + (a<<3);
+//    a = (a^0xb55a4f09) ^ (a>>16);
+    return a;  // identity hashing seems to work well enough here
 }
 
 
