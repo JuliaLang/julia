8549adfcdeb23fd5e72697418cdd322b9eeac2cd
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-12-22 18:11:30 +0000

    re-comment out test

--- test/sorting.jl
+++ test/sorting.jl
@@ -723,7 +723,7 @@ end
         @test sort(1:n, alg=alg, lt = (i,j) -> v[i]<=v[j]) == perm
     end
     # Broken by the introduction of BracketedSort in #52006 which is unstable
-    @test_broken partialsort(1:n, 172, lt = (i,j) -> v[i]<=v[j]) == perm[172]
+    # @test_broken partialsort(1:n, 172, lt = (i,j) -> v[i]<=v[j]) == perm[172] (sometimes passes due to RNG)
     @test_broken partialsort(1:n, 315:415, lt = (i,j) -> v[i]<=v[j]) == perm[315:415]
 
     # lt can be very poorly behaved and sort will still permute its input in some way.
