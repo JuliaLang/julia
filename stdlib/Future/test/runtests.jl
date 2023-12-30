fb76a004150f6f29f6345c4d36365fb1bfab9092
Aman Singh <E21CSEU0001@bennett.edu.in>
2023-12-30 23:11:39 +0530

    added tests for Base.copy!

--- stdlib/Future/test/runtests.jl
+++ stdlib/Future/test/runtests.jl
@@ -2,3 +2,73 @@
 
 using Test
 using Future
+
+# making positive tests
+
+@testset "Testing copy! for sets" begin
+@test begin
+dst = Set([])# Create a set using a collection
+src = Set([1, 24, 5, 6, 51, 62])
+copy!(dst, src)
+@assert dst == src# Compare sets using a collection
+println("destinition = ", dst)
+return true
+end
+end
+
+@testset "Testing copy! for Dictionaries" begin
+@test begin
+dst = Dict("a" => 1, "b" => 2)
+src = Dict("c" => 3, "d" => 4)
+copy!(dst, src)
+println("destinition = ", dst)
+@assert dst == src
+return true
+end
+end
+
+@testset "Testing copy! for Arrays" begin
+@test begin
+dst = [1, 2, 3]
+src = [4, 5, 6]
+copy!(dst, src)
+@assert dst == src
+println("destinition = ", dst)
+return true
+end
+end
+
+# making negative test where
+
+@testset "Negative Testing copy! for sets" begin
+@test begin
+dst = Set([])# Create a set using a collection
+src = Set([1, 24, 5, 6, 51, 62])
+copy!(dst, src)
+is_fake = dst != Set([1, 24, 5])# Compare sets using a collection
+println(is_fake)
+return true
+end
+end
+
+@testset "Negative Testing copy! for Dictionaries" begin
+@test begin
+dst = Dict("a" => 1, "b" => 2)
+src = Dict("c" => 3, "d" => 4)
+copy!(dst, src)
+is_fake = dst == Dict("a" => 1, "b" => 2)
+println(is_fake)
+return true
+end
+end
+
+@testset "Negative Testing copy! for Arrays" begin
+@test begin
+dst = [1, 2, 3]
+src = [4, 5, 6]
+copy!(dst, src)
+is_fake = dst == [1, 2, 3]
+println(is_fake)
+return true
+end
+end
