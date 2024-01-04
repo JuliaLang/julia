6166cdde5501546efa5d7ad624d9e936f2aedbd8
Michael Tiemann <github@michaeltiemann.net>
2024-01-05 00:48:25 +0100

    add jldoctest to @doc_str

--- stdlib/Markdown/src/Markdown.jl
+++ stdlib/Markdown/src/Markdown.jl
@@ -79,6 +79,16 @@ corresponding [`MD`](@ref) object.
 
 `@doc_str` can be used in conjunction with the [`Base.Docs`](@ref) module. Please also refer to
 the manual section on [documentation](@ref man-documentation) for more information.
+
+# Examples
+```
+julia> s = doc"f(x) = 2*x"
+  f(x) = 2*x
+
+julia> typeof(s)
+Markdown.MD
+
+```
 """
 macro doc_str(s::AbstractString, t...)
     docexpr(__source__, __module__, s, t...)
