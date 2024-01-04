cfb01116df6e7013b23d4d70785a447c8f18ecdf
Fons van der Plas <fonsvdplas@gmail.com>
2024-01-04 21:17:59 +0100

    Update html.jl
    Co-authored-by: Steven G. Johnson <stevenj@mit.edu>

--- stdlib/Markdown/src/render/html.jl
+++ stdlib/Markdown/src/render/html.jl
@@ -185,7 +185,11 @@ export html
 """
     html([io::IO], md)
 
-Render a Markdown object to its HTML representation. If no `io` is provided, the result will be returned as a `String`.
+Output the contents of the Markdown object `md` in HTML format, either
+writing to an (optional) `io` stream or returning a string.
+
+One can alternatively use `show(io, "text/html", md)` or `repr("text/html", md)`, which
+differ in that they wrap the output in a `<div class="markdown"> ... </div>` element.
 
 # Example
 ```jldoctest
