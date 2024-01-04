69adbde6c4efa69eb0f98509e224cbee7a81062c
Steven G. Johnson <stevenj@alum.mit.edu>
2024-01-04 11:39:18 -0500

    Update base/docs/Docs.jl
    Co-authored-by: Lilith Orion Hafner <lilithhafner@gmail.com>

--- base/docs/Docs.jl
+++ base/docs/Docs.jl
@@ -678,7 +678,7 @@ end
 """
     undocumented_names(mod::Module; private=false)
 
-Return a sorted array of undocumented symbols in `module` (that is, lacking docstrings).
+Return a sorted vector of undocumented symbols in `module` (that is, lacking docstrings).
 `private=false` (the default) returns only identifiers declared with `public` and/or
 `export`, whereas `private=true` returns all symbols in the module (excluding
 compiler-generated hidden symbols starting with `#`).
