534953abb2a4edcc14bbccffb009edea0e57cccf
Michael Tiemann <126827163+mitiemann@users.noreply.github.com>
2024-01-05 01:05:33 +0100

    add reviewer suggestion
    Co-authored-by: Steven G. Johnson <stevenj@mit.edu>

--- stdlib/Markdown/src/Markdown.jl
+++ stdlib/Markdown/src/Markdown.jl
@@ -3,7 +3,7 @@
 """
     Markdown
 
-Tools for working with the Markdown file format. Mainly for documentation.
+Tools for working with the Markdown markup language for formatted text, used within Julia for documentation.
 The `Markdown` module provides the (internal) [`MD`](@ref) type as well as the string
 literals `md"..."` and `doc"..."`.
 """
