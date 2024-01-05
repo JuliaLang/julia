b4f216a26f2167420843dd73377b4f669d4a07b7
Michael Tiemann <github@michaeltiemann.net>
2024-01-05 23:29:14 +0100

    fix whitespace

--- stdlib/Markdown/src/parse/parse.jl
+++ stdlib/Markdown/src/parse/parse.jl
@@ -4,8 +4,8 @@
     MD
 
 `MD` represents a Markdown document. Note that the `MD` constructor should not generally be
-used directly, since it constructs the internal data structures.   Instead, you can
-construct `MD` objects using the exported macros [`@md_str`](@ref) and [`@doc_str`](@ref).
+used directly, since it constructs the internal data structures. Instead, you can construct 
+`MD` objects using the exported macros [`@md_str`](@ref) and [`@doc_str`](@ref).
 """
 mutable struct MD
     content::Vector{Any}
