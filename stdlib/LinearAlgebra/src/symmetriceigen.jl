4daa3230c403d82bf9357c877c530181f8284188
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-01-08 13:00:03 +0530

    Change one to oneunit

--- stdlib/LinearAlgebra/src/symmetriceigen.jl
+++ stdlib/LinearAlgebra/src/symmetriceigen.jl
@@ -312,7 +312,7 @@ function eigen(A::Hermitian{Complex{T}, <:Tridiagonal}; kwargs...) where {T}
         S = Vector{Complex{eigtype(T)}}(undef, N)
         S[1] = 1
         for i ∈ 1:N-1
-            S[i+1] = iszero(Er[i]) ? one(eltype(S)) : S[i] * sign(E[i])
+            S[i+1] = iszero(Er[i]) ? oneunit(eltype(S)) : S[i] * sign(E[i])
         end
         B = SymTridiagonal(real(d), Er)
         Λ, Φ = eigen(B; kwargs...)
