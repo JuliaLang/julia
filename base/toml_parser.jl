7a293160cc58b6d3f9c4c639a689b02b57cad5fd
Steven G. Johnson <stevenj@mit.edu>
2024-01-10 08:38:03 -0500

    Update base/toml_parser.jl

--- base/toml_parser.jl
+++ base/toml_parser.jl
@@ -3,8 +3,7 @@
 """
 TOML.jl is a Julia standard library for parsing and writing TOML v1.0 files.
 This module provides functions to parse TOML strings and files into Julia data structures
-and to serialize Julia data structures to TOML format. The module is designed to
-handle exceptions, offers both regular and try-parse methods for development.
+and to serialize Julia data structures to TOML format.
 """
 module TOML
 
