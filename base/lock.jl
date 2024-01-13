a4f4a30771d63d3b67dd73f6caaa21cec495b64e
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-01-13 21:46:27 +0100

    Add Lockable
    Co-authored-by: Jacob Quinn <quinn.jacobd@gmail.com>

--- base/lock.jl
+++ base/lock.jl
@@ -293,6 +293,59 @@ macro lock_nofail(l, expr)
     end
 end
 
+"""
+  Lockable(value, lock = ReentrantLock())
+
+Creates a `Lockable` object that wraps `value` and
+associates it with the provided `lock`. This object
+supports [`@lock`](@ref), [`lock`](@ref), [`trylock`](@ref),
+[`unlock`](@ref), [`islocked`](@ref), and can be indexed to retrieve
+the value.
+
+## Example
+
+```jldoctest
+julia> locked_list = Lockable(Int[]);
+
+julia> @lock(locked_list, push!(locked_list[], 1))
+1-element Vector{Int64}:
+ 1
+
+julia> locked_list[]
+1-element Vector{Int64}:
+ 1
+```
+"""
+struct Lockable{T, L <: Base.AbstractLock}
+    value::T
+    lock::L
+end
+
+Lockable(value) = Lockable(value, ReentrantLock())
+Base.getindex(l::Lockable) = l.value
+
+"""
+  lock(f::Function, l::Lockable)
+
+Acquire the lock associated with `l`, execute `f` with the lock held,
+and release the lock when `f` returns. `f` will receive one positional
+argument: the value wrapped by `l`. If the lock is already locked by a
+different task/thread, wait for it to become available.
+When this function returns, the `lock` has been released, so the caller should
+not attempt to `unlock` it.
+"""
+function Base.lock(f, l::Lockable)
+    lock(l.lock) do
+        f(l.value)
+    end
+end
+
+# implement the rest of the Lock interface on Lockable
+Base.islocked(l::Lockable) = islocked(l.lock)
+Base.lock(l::Lockable) = lock(l.lock)
+Base.trylock(l::Lockable) = trylock(l.lock)
+Base.unlock(l::Lockable) = unlock(l.lock)
+
 @eval Threads begin
     """
         Threads.Condition([lock])
