c4edba46fc7db8ba9043bc1e54205c15fddb2ecf
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-01-14 01:27:00 +0100

    fix doctest, improve docs

--- base/lock.jl
+++ base/lock.jl
@@ -299,7 +299,8 @@ end
 Creates a `Lockable` object that wraps `value` and
 associates it with the provided `lock`. This object
 supports [`@lock`](@ref), [`lock`](@ref), [`trylock`](@ref),
-[`unlock`](@ref), and can be indexed to retrieve the value.
+[`unlock`](@ref). To access the value, index the lockable object while
+holding the lock.
 
 !!! compat "Julia 1.11"
     Requires at least Julia 1.11.
@@ -309,13 +310,12 @@ supports [`@lock`](@ref), [`lock`](@ref), [`trylock`](@ref),
 ```jldoctest
 julia> locked_list = Lockable(Int[]);
 
-julia> @lock(locked_list, push!(locked_list[], 1))
+julia> @lock(locked_list, push!(locked_list[], 1)) # must hold the lock to access the value
 1-element Vector{Int64}:
  1
 
-julia> locked_list[]
-1-element Vector{Int64}:
- 1
+julia> lock(summary, locked_list)
+"1-element Vector{Int64}"
 ```
 """
 struct Lockable{T, L <: AbstractLock}
