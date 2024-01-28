35a5711d4fa3c91265fa143dbfc4085171ac841f
jam-khan <jamkhan@connect.hku.hk>
2024-01-28 10:43:26 -0500

    Added reference to UnionAll manual for UnionAll docstrings. (#52838)

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -2893,7 +2893,7 @@ Union
     UnionAll
 
 A union of types over all values of a type parameter. `UnionAll` is used to describe parametric types
-where the values of some parameters are not known.
+where the values of some parameters are not known. See the manual section on [UnionAll Types](@ref).
 
 # Examples
 ```jldoctest
