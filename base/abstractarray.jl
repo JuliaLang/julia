06c7b0ebf7482ebdbc121c609dec95bb34458ea9
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-01-28 10:41:22 -0600

    Move docstrings from AbstractArray methods to functions

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -209,72 +209,7 @@ String
 """
 valtype(A::Type{<:AbstractArray}) = eltype(A)
 
-"""
-    prevind(A, i)
-
-Return the index before `i` in `A`. The returned index is often equivalent to `i
-- 1` for an integer `i`. This function can be useful for generic code.
-
-!!! warning
-    The returned index might be out of bounds. Consider using
-    [`checkbounds`](@ref).
-
-See also: [`nextind`](@ref).
-
-# Examples
-```jldoctest
-julia> x = [1 2; 3 4]
-2×2 Matrix{Int64}:
- 1  2
- 3  4
-
-julia> prevind(x, 4) # valid result
-3
-
-julia> prevind(x, 1) # invalid result
-0
-
-julia> prevind(x, CartesianIndex(2, 2)) # valid result
-CartesianIndex(1, 2)
-
-julia> prevind(x, CartesianIndex(1, 1)) # invalid result
-CartesianIndex(2, 0)
-```
-"""
 prevind(::AbstractArray, i::Integer) = Int(i)-1
-
-"""
-    nextind(A, i)
-
-Return the index after `i` in `A`. The returned index is often equivalent to `i
-+ 1` for an integer `i`. This function can be useful for generic code.
-
-!!! warning
-    The returned index might be out of bounds. Consider using
-    [`checkbounds`](@ref).
-
-See also: [`prevind`](@ref).
-
-# Examples
-```jldoctest
-julia> x = [1 2; 3 4]
-2×2 Matrix{Int64}:
- 1  2
- 3  4
-
-julia> nextind(x, 1) # valid result
-2
-
-julia> nextind(x, 4) # invalid result
-5
-
-julia> nextind(x, CartesianIndex(1, 1)) # valid result
-CartesianIndex(2, 1)
-
-julia> nextind(x, CartesianIndex(2, 2)) # invalid result
-CartesianIndex(1, 3)
-```
-"""
 nextind(::AbstractArray, i::Integer) = Int(i)+1
 
 
