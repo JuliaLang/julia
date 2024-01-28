b88f245c2c47b19d0a9f47b30d9f18548f8c3734
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-01-28 15:57:04 -0600

    qualify usage of Dates.adjust in docstring

--- stdlib/Dates/src/adjusters.jl
+++ stdlib/Dates/src/adjusters.jl
@@ -216,24 +216,24 @@ The default values for parameters `start` and `limit` are 1 Day and 10,000 respe
 
 # Examples
 ```jldoctest
-julia> adjust(date -> month(date) == 10, Date(2022, 1, 1), step=Month(3), limit=10)
+julia> Dates.adjust(date -> month(date) == 10, Date(2022, 1, 1), step=Month(3), limit=10)
 2022-10-01
 
-julia> adjust(date -> year(date) == 2025, Date(2022, 1, 1), step=Year(1), limit=4)
+julia> Dates.adjust(date -> year(date) == 2025, Date(2022, 1, 1), step=Year(1), limit=4)
 2025-01-01
 
-julia> adjust(date -> day(date) == 15, Date(2022, 1, 1), step=Year(1), limit=3)
+julia> Dates.adjust(date -> day(date) == 15, Date(2022, 1, 1), step=Year(1), limit=3)
 ERROR: ArgumentError: Adjustment limit reached: 3 iterations
 Stacktrace:
 [...]
 
-julia> adjust(date -> month(date) == 10, Date(2022, 1, 1))
+julia> Dates.adjust(date -> month(date) == 10, Date(2022, 1, 1))
 2022-10-01
 
-julia> adjust(date -> year(date) == 2025, Date(2022, 1, 1))
+julia> Dates.adjust(date -> year(date) == 2025, Date(2022, 1, 1))
 2025-01-01
 
-julia> adjust(date -> year(date) == 2224, Date(2022, 1, 1))
+julia> Dates.adjust(date -> year(date) == 2224, Date(2022, 1, 1))
 ERROR: ArgumentError: Adjustment limit reached: 10000 iterations
 Stacktrace:
 [...]
