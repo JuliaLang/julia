80174ab6f65fce03ae61647709bfd1856a117fe3
Pablo San-Jose <lekand@gmail.com>
2024-01-28 18:34:13 +0100

    Revert "move test to OpenBLAS_jll tests"
    This reverts commit f4ae41233ea00502af7ee3774c9cce5f3998bc9c.

--- stdlib/OpenBLAS_jll/test/runtests.jl
+++ stdlib/OpenBLAS_jll/test/runtests.jl
@@ -14,17 +14,4 @@ end
 
 @testset "OpenBLAS_jll" begin
     @test dlsym(OpenBLAS_jll.libopenblas_handle, @blasfunc(openblas_set_num_threads); throw_error=false) != nothing
-    @testset "deterministic mul!" begin
-        # mul! should be deterministic, see #53054
-        function tester_53054()
-            C = ComplexF32
-            mat = zeros(C, 1, 1)
-            for _ in 1:100
-                v = [C(1-0.2im) C(2+0.3im)]
-                mul!(mat, v, v', C(1+im), 1)
-            end
-            return mat
-        end
-        @test allequal(tester_53054() for _ in 1:10000)
-    end
 end
