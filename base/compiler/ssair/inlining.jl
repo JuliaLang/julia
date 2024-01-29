a215b3761bef3c25aaefc9a0687d8307dcd8f0fb
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2024-01-29 15:26:42 +0900

    1.10: inlining: fix `joint_effects` calculation (#53076)
    This particular fix was part of #50805, but it wasn't included in
    version 1.10, leading to situations where an incorrect `:nothrow` could
    occur in 1.10 (#53062). This commit implements a minimal correction in
    1.10 and also added some test cases.
    
    Fixes #53062.

--- base/compiler/ssair/inlining.jl
+++ base/compiler/ssair/inlining.jl
@@ -1408,7 +1408,7 @@ function compute_inlining_cases(@nospecialize(info::CallInfo), flag::UInt8, sig:
         fully_covered &= split_fully_covered
     end
 
-    fully_covered || (joint_effects = Effects(joint_effects; nothrow=false))
+    (handled_all_cases & fully_covered) || (joint_effects = Effects(joint_effects; nothrow=false))
 
     if handled_all_cases && revisit_idx !== nothing
         # we handled everything except one match with unmatched sparams,
