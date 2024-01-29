cc21e72972feeefa7fa8207741a45698093f9bb8
Pablo San-Jose <lekand@gmail.com>
2024-01-29 15:59:07 +0100

    update docstring for has_offset_axes

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -103,7 +103,7 @@ end
     has_offset_axes(A, B, ...)
 
 Return `true` if the indices of `A` start with something other than 1 along any axis.
-If multiple arguments are passed, equivalent to `has_offset_axes(A) | has_offset_axes(B) | ...`.
+If multiple arguments are passed, equivalent to `has_offset_axes(A) || has_offset_axes(B) || ...`.
 
 See also [`require_one_based_indexing`](@ref).
 """
