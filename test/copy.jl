4d04c44c9828ba31e5bfb188c69c635cb05f6f16
Aman Singh <E21CSEU0001@bennett.edu.in>
2024-02-11 17:49:58 +0530

    Added Unit Tests for Base.copy for Arrays, Sets and Dictionary

--- test/copy.jl
+++ test/copy.jl
@@ -268,3 +268,33 @@ end
     @test islocked(a.lock)
     @test !islocked(b.lock)
 end
+
+"""
+Basic tests for Base.copy!
+"""
+using Test
+using Future
+
+@testset "Testing copy! for sets" begin
+    dst = Set([])  # Create an empty set
+    src = Set([1, 24, 5, 6, 51, 62])
+    copy!(dst, src)
+    @test dst == src
+end
+
+@testset "Testing copy! for Dictionaries" begin
+    dst = Dict("a" => 1, "b" => 2)
+    src = Dict("c" => 3, "d" => 4)
+    copy!(dst, src)
+    @test dst == src
+end
+
+@testset "Testing copy! for Arrays" begin
+    dst = [1, 2, 3]
+    src = [4, 5, 6]
+    copy!(dst, src)
+    @test dst == src
+end
+
+
+
