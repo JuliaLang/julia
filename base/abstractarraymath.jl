8352a5a25035e8f9485e9fd2a19d6ec6c2468301
Daniel Karrasch <daniel.karrasch@posteo.de>
2024-02-12 19:59:17 +0100

    Update base/abstractarraymath.jl
    Co-authored-by: Matt Bauman <mbauman@juliahub.com>

--- base/abstractarraymath.jl
+++ base/abstractarraymath.jl
@@ -119,7 +119,7 @@ julia> A
 """
 conj!(A::AbstractArray{<:Number}) = (@inbounds broadcast!(conj, A, A); A)
 conj!(x::AbstractArray{<:Real}) = x
-function conj!(A::AbstractArray{<:AbstractArray})
+function conj!(A::AbstractArray)
     foreach(A) do ai
         conj!(ai)
     end
