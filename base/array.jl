e14e36c7ed2e41f02187cd1d5a41dfa00ae41689
Max Horn <max@quendi.de>
2024-02-14 00:09:09 +0100

    Apply suggestions from code review
    Co-authored-by: Jishnu Bhattacharya <jishnub.github@gmail.com>

--- base/array.jl
+++ base/array.jl
@@ -669,13 +669,13 @@ _array_for(::Type{T}, itr, isz) where {T} = _array_for(T, isz, _similar_shape(it
     collect(collection)
 
 Return an `Array` of all items in a collection or iterator. For dictionaries, returns
-a `Vector` of `key=>value` [pairs](@ref Pair). If the argument is array-like or is an iterator
+a `Vector` of `key=>value` [Pair](@ref Pair)s. If the argument is array-like or is an iterator
 with the [`HasShape`](@ref IteratorSize) trait, the result will have the same shape
 and number of dimensions as the argument.
 
 Used by [comprehensions](@ref man-comprehensions) to turn a [generator expression](@ref man-generators)
-into an `Array`. Thus, *on generators*, the bracket notation can be used instead of calling `collect`,
-see 2nd example.
+into an `Array`. Thus, *on generators*, the square-brackets notation may be used instead of calling `collect`,
+see second example.
 
 # Examples
 
