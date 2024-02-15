1b25d1e1e461dbd4b13d73dcc9ec8b38741fe5c8
Nathan Boyer <65452054+nathanrboyer@users.noreply.github.com>
2024-02-15 10:25:31 -0500

    Add CartesianIndex to getindex docstring (#51567)
    Co-authored-by: Matt Bauman <mbauman@gmail.com>
    Co-authored-by: Denis Barucic <barucic.d@gmail.com>
    Co-authored-by: Jishnu Bhattacharya <jishnub.github@gmail.com>
    Co-authored-by: Max Horn <max@quendi.de>

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -1274,8 +1274,11 @@ end
 """
     getindex(A, inds...)
 
-Return a subset of array `A` as specified by `inds`, where each `ind` may be,
-for example, an `Int`, an [`AbstractRange`](@ref), or a [`Vector`](@ref).
+Return a subset of array `A` as selected by the indices `inds`.
+
+Each index may be any [supported index type](@ref man-supported-index-types), such
+as an [`Integer`](@ref), [`CartesianIndex`](@ref), [range](@ref Base.AbstractRange), or [array](@ref man-multi-dim-arrays) of supported indices.
+A [:](@ref Base.Colon) may be used to select all elements along a specific dimension, and a boolean array (e.g. an `Array{Bool}` or a [`BitArray`](@ref)) may be used to filter for elements where the corresponding index is `true`.
 
 When `inds` selects multiple elements, this function returns a newly
 allocated array. To index multiple elements without making a copy,
@@ -1302,6 +1305,27 @@ julia> getindex(A, 2:4)
 3-element Vector{Int64}:
  3
  2
+ 4
+
+julia> getindex(A, 2, 1)
+3
+
+julia> getindex(A, CartesianIndex(2, 1))
+3
+
+julia> getindex(A, :, 2)
+2-element Vector{Int64}:
+ 2
+ 4
+
+julia> getindex(A, 2, :)
+2-element Vector{Int64}:
+ 3
+ 4
+
+julia> getindex(A, A .> 2)
+2-element Vector{Int64}:
+ 3
  4
 ```
 """
