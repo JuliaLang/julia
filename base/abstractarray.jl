8fc7ba7d1fcf8080cc6c99f70cd054cf8a68cd54
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-02-19 13:03:27 +0530

    Add _unsetindex! methods for SubArrays and CartesianIndexes

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -1472,7 +1472,10 @@ function _setindex!(::IndexCartesian, A::AbstractArray, v, I::Vararg{Int,M}) whe
     r
 end
 
-_unsetindex!(A::AbstractArray, i::Integer) = _unsetindex!(A, to_index(i))
+function _unsetindex!(A::AbstractArray, i::Integer...)
+    @_propagate_inbounds_meta
+    _unsetindex!(A, map(to_index, i)...)
+end
 
 """
     parent(A)
