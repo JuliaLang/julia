6af35966451d5f2620ae5f9cb0c94a421e91a0a4
Jeff Bezanson <jeff.bezanson@gmail.com>
2024-02-20 16:42:30 -0500

    fix code coverage bug in tail position and `else` (#53354)
    This was due to lowering keeping the same location info for the inserted
    `return` or `goto` statement, even though the last seen location might
    not have executed.
    
    Also fixes inliner handling of the sentinel `0` value for code
    locations.
    
    (cherry picked from commit 61fc907a225eb642fd180257a02e5951336dabe4)

--- base/compiler/ssair/passes.jl
+++ base/compiler/ssair/passes.jl
@@ -1519,8 +1519,12 @@ function try_inline_finalizer!(ir::IRCode, argexprs::Vector{Any}, idx::Int,
             ssa_rename[ssa.id]
         end
         stmt′ = ssa_substitute_op!(InsertBefore(ir, SSAValue(idx)), inst, stmt′, ssa_substitute)
+        newline = inst[:line]
+        if newline != 0
+            newline += ssa_substitute.linetable_offset
+        end
         ssa_rename[idx′] = insert_node!(ir, idx,
-            NewInstruction(inst; stmt=stmt′, line=inst[:line]+ssa_substitute.linetable_offset),
+            NewInstruction(inst; stmt=stmt′, line=newline),
             attach_after)
     end
 
