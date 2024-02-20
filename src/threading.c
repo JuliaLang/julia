303f23e1f36a53f81564d3e18370cfd25c6cfa69
Carsten Bauer <crstnbr@gmail.com>
2024-02-20 18:45:25 +0100

    convention

--- src/threading.c
+++ src/threading.c
@@ -989,7 +989,6 @@ JL_DLLEXPORT int jl_getaffinity(int16_t tid, char *mask, int cpumasksize) {
     if (tid < 0 || tid >= nthreads)
         jl_error("invalid tid");
 
-    // TODO: relaxed or acquire here?
     jl_ptls_t *ptls2 = jl_atomic_load_relaxed(&jl_all_tls_states)[tid];
     uv_thread_t uvtid = ptls2->system_id;
 
@@ -1002,9 +1001,8 @@ JL_DLLEXPORT int jl_setaffinity(int16_t tid, char *mask, int cpumasksize) {
     if (tid < 0 || tid >= nthreads)
         jl_error("invalid tid");
 
-    // TODO: relaxed or acquire here?
-    jl_ptls_t *tls = jl_atomic_load_relaxed(&jl_all_tls_states);
-    uv_thread_t uvtid = tls[tid]->system_id;
+    jl_ptls_t *ptls2 = jl_atomic_load_relaxed(&jl_all_tls_states)[tid];
+    uv_thread_t uvtid = ptls2->system_id;
 
     // returns 0 in case of success and a value != 0 otherwise
     return uv_thread_setaffinity(&uvtid, mask, NULL, cpumasksize);
