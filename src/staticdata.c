2d6c154f6493c4b9458ec0e001cab16582f6338f
Jameson Nash <vtjnash@gmail.com>
2024-02-20 23:07:24 -0500

    fix sysimage-native-code=yes option (#53407)
    Follow up to #53373, it seems this assert was broken for empty packages,
    causing CI issues. It is not necessary.
    
    Observed in CI here: https://github.com/JuliaLang/julia/pull/53395
    
    https://buildkite.com/julialang/julia-master/builds/33860#018dc4dc-a603-4ad1-90cf-574540a41720
    (cherry picked from commit 3742d33ecc9048bb925ad89020751702c45f18a2)

--- src/staticdata.c
+++ src/staticdata.c
@@ -3092,11 +3092,7 @@ static void jl_restore_system_image_from_stream_(ios_t *f, jl_image_t *image, jl
 
     // in --build mode only use sysimg data, not precompiled native code
     int imaging_mode = jl_generating_output() && !jl_options.incremental;
-    if (!imaging_mode && jl_options.use_sysimage_native_code == JL_OPTIONS_USE_SYSIMAGE_NATIVE_CODE_YES) {
-        if (image->gvars_base)
-            assert(image->fptrs.ptrs);
-    }
-    else {
+    if (imaging_mode || jl_options.use_sysimage_native_code != JL_OPTIONS_USE_SYSIMAGE_NATIVE_CODE_YES) {
         memset(&image->fptrs, 0, sizeof(image->fptrs));
         image->gvars_base = NULL;
     }
