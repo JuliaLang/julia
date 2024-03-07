0e194af5b8273323af1171520673b3db301f165e
Cameron Bieganek <8310743+CameronBieganek@users.noreply.github.com>
2024-03-07 07:03:11 -0600

    Slightly rearrange the `@with` docstring.

--- base/scopedvalues.jl
+++ base/scopedvalues.jl
@@ -185,10 +185,9 @@ end
     @with (var::ScopedValue{T} => val)... expr
 
 Macro version of `with`. The expression `@with var=>val expr` evaluates `expr` in a
-new dynamic scope with `var` set to `val`. `@with var=>val expr` is equivalent to
-`with(var=>val) do expr end`, but `@with` avoids creating a closure.
-
-`val` will be converted to type `T`.
+new dynamic scope with `var` set to `val`. `val` will be converted to type `T`.
+`@with var=>val expr` is equivalent to `with(var=>val) do expr end`, but `@with`
+avoids creating a closure.
 
 See also: [`ScopedValues.with`](@ref), [`ScopedValues.ScopedValue`](@ref), [`ScopedValues.get`](@ref).
 
