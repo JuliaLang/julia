bcccaf155ea56e398845912a4cf189b2456bfd75
Valentin Churavy <v.churavy@gmail.com>
2024-03-07 15:15:30 -0500

    use the right pre-requisite type

--- pkgimage.mk
+++ pkgimage.mk
@@ -24,13 +24,13 @@ $(JULIA_DEPOT_PATH)/compiled:
 print-depot-path:
 	@$(call PRINT_JULIA, $(call spawn,$(JULIA_EXECUTABLE)) --startup-file=no -e '@show Base.DEPOT_PATH')
 
-$(BUILDDIR)/stdlib/%.image: | $(JULIAHOME)/stdlib/Project.toml $(JULIAHOME)/stdlib/Manifest.toml $(INDEPENDENT_STDLIBS_SRCS) $(JULIA_DEPOT_PATH)/compiled
+$(BUILDDIR)/stdlib/%.image: $(JULIAHOME)/stdlib/Project.toml $(JULIAHOME)/stdlib/Manifest.toml $(INDEPENDENT_STDLIBS_SRCS) $(JULIA_DEPOT_PATH)/compiled
 	export JULIA_CPU_TARGET=$(JULIA_CPU_TARGET)
 	@$(call PRINT_JULIA, $(call spawn,$(JULIA_EXECUTABLE)) --startup-file=no -e 'Base.Precompilation.precompilepkgs(;configs=[``=>Base.CacheFlags(), `--check-bounds=yes`=>Base.CacheFlags(;check_bounds=1)])')
 	touch $@
 
-$(BUILDDIR)/stdlib/release.image: | $(build_private_libdir)/sys.$(SHLIB_EXT)
-$(BUILDDIR)/stdlib/debug.image: | $(build_private_libdir)/sys-debug.$(SHLIB_EXT)
+$(BUILDDIR)/stdlib/release.image: $(build_private_libdir)/sys.$(SHLIB_EXT)
+$(BUILDDIR)/stdlib/debug.image: $(build_private_libdir)/sys-debug.$(SHLIB_EXT)
 
 clean:
 	rm -rf $(JULIA_DEPOT_PATH)/compiled
