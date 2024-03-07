f5013887fa8089f04e095ea7420069b591fa7846
Valentin Churavy <vchuravy@users.noreply.github.com>
2024-03-07 12:44:43 -0500

    Update pkgimage.mk

--- pkgimage.mk
+++ pkgimage.mk
@@ -24,7 +24,7 @@ $(JULIA_DEPOT_PATH):
 print-depot-path:
 	@$(call PRINT_JULIA, $(call spawn,$(JULIA_EXECUTABLE)) --startup-file=no -e '@show Base.DEPOT_PATH')
 
-$(BUILDDIR)/stdlib/%.image: | $(JULIAHOME)/stdlib/Project.toml $(JULIAHOME)/stdlib/Manifest.toml $(INDEPENDENT_STDLIBS_SRCS) $(build_private_libdir)/sys-%.$(SHLIB_EXT)
+$(BUILDDIR)/stdlib/%.image: | $(JULIAHOME)/stdlib/Project.toml $(JULIAHOME)/stdlib/Manifest.toml $(INDEPENDENT_STDLIBS_SRCS)
 	export JULIA_CPU_TARGET=$(JULIA_CPU_TARGET)
 	@$(call PRINT_JULIA, $(call spawn,$(JULIA_EXECUTABLE)) --startup-file=no -e 'Base.Precompilation.precompilepkgs(;configs=[``=>Base.CacheFlags(), `--check-bounds=yes`=>Base.CacheFlags(;check_bounds=1)])')
 	touch $@
