501c40886bfa1cc7013945f9821a624dd05789c5
Keno Fischer <keno@juliahub.com>
2024-03-08 05:52:32 +0000

    Mark as broken on windows

--- test/compileall.jl
+++ test/compileall.jl
@@ -5,5 +5,7 @@
 mktempdir() do dir
     run(`$(Base.julia_cmd()) --compile=all --strip-ir --output-o $(dir)/sys.o.a -e 'exit()'`)
     Base.Linking.link_image(joinpath(dir, "sys.o.a"), joinpath(dir, "sys.so"))
-    @test success(`$(Base.julia_cmd()) -J $(dir)/sys.so -e 'Base.scrub_repl_backtrace(nothing); exit()'`)
+    # TODO: Broken on Windows due to
+    # https://github.com/llvm/llvm-project/issues/84424
+    @test success(`$(Base.julia_cmd()) -J $(dir)/sys.so -e 'Base.scrub_repl_backtrace(nothing); exit()'`) broken=Sys.iswindows()
 end
