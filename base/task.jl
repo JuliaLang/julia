0a382a3fa44c2ae64e32d3dce588704fbb6c39df
Kenta Murata <3959+mrkn@users.noreply.github.com>
2024-03-09 21:26:00 +0900

    Add usage note of waitall in docstring
    Co-authored-by: Jameson Nash <vtjnash@gmail.com>

--- base/task.jl
+++ base/task.jl
@@ -377,6 +377,12 @@ completed tasks completes with an exception.
 
 The return value consists of two task vectors. The first one consists of
 completed tasks, and the other consists of uncompleted tasks.
+
+!!! warning
+    This may scale poorly compared to writing code that uses multiple individual tasks that
+    each runs serially, since this needs to scan the list of `tasks` each time and
+    synchronize with each one every time this is called. Or consider using
+    [`waitall(tasks; failfast=true)`](@ref waitall) instead.
 """
 waitany(tasks; throw=true) = _wait_multiple(tasks, throw)
 
