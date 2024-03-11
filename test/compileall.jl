aaf46b57caf673d01d7ae38b0f107d80ae7a4a71
Keno Fischer <keno@juliacomputing.com>
2024-03-11 11:04:10 +0000

    Also mark OOM as broken

--- test/compileall.jl
+++ test/compileall.jl
@@ -3,9 +3,11 @@
 # with the rest of the tests.
 
 mktempdir() do dir
-    run(`$(Base.julia_cmd()) --compile=all --strip-ir --output-o $(dir)/sys.o.a -e 'exit()'`)
-    Base.Linking.link_image(joinpath(dir, "sys.o.a"), joinpath(dir, "sys.so"))
-    # TODO: Broken on Windows due to
-    # https://github.com/llvm/llvm-project/issues/84424
-    @test success(`$(Base.julia_cmd()) -J $(dir)/sys.so -e 'Base.scrub_repl_backtrace(nothing); exit()'`) broken=Sys.iswindows()
+    @test success(`$(Base.julia_cmd()) --compile=all --strip-ir --output-o $(dir)/sys.o.a -e 'exit()'`) broken=(Sys.iswindows() && Sys.WORD_SIZE == 32)
+    if isfile(joinpath(dir, "sys.o.a"))
+        Base.Linking.link_image(joinpath(dir, "sys.o.a"), joinpath(dir, "sys.so"))
+        # TODO: Broken on Windows due to
+        # https://github.com/llvm/llvm-project/issues/84424
+        @test success(`$(Base.julia_cmd()) -J $(dir)/sys.so -e 'Base.scrub_repl_backtrace(nothing); exit()'`) broken=Sys.iswindows()
+    end
 end
