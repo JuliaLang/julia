c4ab0d46a9c3a470794e5a441176bfdf8ff36e52
Ian Butterworth <i.r.butterworth@gmail.com>
2024-03-14 10:23:58 -0400

    Increase build precompilation (#53682)
    - Bake precompilepkgs into the sysimage
    - Enable compilation in stdlib JLLs @staticfloat I guess this is ok now
    we have pkgimages?
    - Add some hardcoded precompiles encountered locally
    - Disable a problematic llvm test, with a note
    
    With this there's no compilation loading Pkg in interactive mode
    ```
    % ./julia --trace-compile=stderr --start=no -q
    julia> ]
    (@v1.12) pkg> 
    ```
    
    Before
    ```
    % time julia +nightly --start=no --trace-compile=stderr -q -e "using Pkg"
    precompile(Tuple{typeof(Base.setindex!), Base.Dict{Symbol, Union{Nothing, Bool, Int64, String}}, Int64, Symbol})
    precompile(Tuple{typeof(Base.haskey), Base.Dict{String, Any}, String})
    precompile(Tuple{typeof(MbedTLS_jll.__init__)})
    precompile(Tuple{typeof(LibSSH2_jll.__init__)})
    precompile(Tuple{typeof(LibGit2_jll.__init__)})
    precompile(Tuple{typeof(nghttp2_jll.__init__)})
    precompile(Tuple{typeof(LibCURL_jll.__init__)})
    precompile(Tuple{typeof(MozillaCACerts_jll.__init__)})
    precompile(Tuple{typeof(Base.Filesystem.normpath), String, String, Vararg{String}})
    precompile(Tuple{typeof(p7zip_jll.__init__)})
    precompile(Tuple{typeof(p7zip_jll.init_p7zip_path)})
    precompile(Tuple{typeof(Base.append!), Array{String, 1}, Array{String, 1}})
    precompile(Tuple{typeof(Base.join), Array{String, 1}, Char})
    julia +nightly --start=no --trace-compile=stderr -q -e "using Pkg"  1.48s user 0.40s system 206% cpu 0.910 total
    ```
    PR
    ```
    % time ./julia --start=no --trace-compile=stderr -q -e "using Pkg"
    ./julia --start=no --trace-compile=stderr -q -e "using Pkg"  1.62s user 0.13s system 402% cpu 0.433 total
    ```

--- base/precompilation.jl
+++ base/precompilation.jl
@@ -354,12 +354,10 @@ function precompilepkgs(pkgs::Vector{String}=String[];
                         _from_loading::Bool=false,
                         configs::Union{Config,Vector{Config}}=(``=>Base.CacheFlags()),
                         io::IO=stderr,
-                        flags_cacheflags=nothing)
+                        # asking for timing disables fancy mode, as timing is shown in non-fancy mode
+                        fancyprint::Bool = can_fancyprint(io) && !timing
+                    )
 
-    if flags_cacheflags !== nothing
-        # TODO: Remove `flags_cacheflags` once Pkg moves to the renamed kwarg `configs`
-        configs = flags_cacheflags
-    end
     configs = configs isa Config ? [configs] : configs
 
     time_start = time_ns()
@@ -373,9 +371,6 @@ function precompilepkgs(pkgs::Vector{String}=String[];
 
     num_tasks = parse(Int, get(ENV, "JULIA_NUM_PRECOMPILE_TASKS", string(default_num_tasks)))
     parallel_limiter = Base.Semaphore(num_tasks)
-
-    # asking for timing disables fancy mode, as timing is shown in non-fancy mode
-    fancyprint = can_fancyprint(io) && !timing
 
     if _from_loading && !Sys.isinteractive() && Base.get_bool_env("JULIA_TESTS", false)
         # suppress passive loading printing in julia test suite. `JULIA_TESTS` is set in Base.runtests
@@ -434,7 +429,7 @@ function precompilepkgs(pkgs::Vector{String}=String[];
         end
     end
 
-    @debug "precompile: deps collected" _group=:precompile
+    @debug "precompile: deps collected"
     # this loop must be run after the full depsmap has been populated
     for (pkg, pkg_exts) in pkg_exts_map
         # find any packages that depend on the extension(s)'s deps and replace those deps in their deps list with the extension(s),
@@ -446,7 +441,7 @@ function precompilepkgs(pkgs::Vector{String}=String[];
             end
         end
     end
-    @debug "precompile: extensions collected" _group=:precompile
+    @debug "precompile: extensions collected"
 
     # return early if no deps
     if isempty(depsmap)
@@ -474,7 +469,7 @@ function precompilepkgs(pkgs::Vector{String}=String[];
             was_recompiled[dep_config] = false
         end
     end
-    @debug "precompile: signalling initialized"  _group=:precompile
+    @debug "precompile: signalling initialized"
 
 
     # find and guard against circular deps
@@ -515,7 +510,7 @@ function precompilepkgs(pkgs::Vector{String}=String[];
     if !isempty(circular_deps)
         @warn """Circular dependency detected. Precompilation will be skipped for:\n  $(join(string.(circular_deps), "\n  "))"""
     end
-    @debug "precompile: circular dep check done"  _group=:precompile
+    @debug "precompile: circular dep check done"
 
     # if a list of packages is given, restrict to dependencies of given packages
     if !isempty(pkgs)
@@ -562,7 +557,7 @@ function precompilepkgs(pkgs::Vector{String}=String[];
     else
         target *= "..."
     end
-    @debug "precompile: packages filtered"  _group=:precompile
+    @debug "precompile: packages filtered"
 
     pkg_queue = PkgConfig[]
     failed_deps = Dict{PkgConfig, String}()
@@ -737,7 +732,7 @@ function precompilepkgs(pkgs::Vector{String}=String[];
     if !_from_loading
         Base.LOADING_CACHE[] = Base.LoadingCache()
     end
-    @debug "precompile: starting precompilation loop"  _group=:precompile
+    @debug "precompile: starting precompilation loop" depsmap direct_deps
     ## precompilation loop
     for (pkg, deps) in depsmap
         cachepaths = Base.find_all_in_cache_path(pkg)
