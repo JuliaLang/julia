7a1e53f9e5dc9f8c90100f2577472e3ba911438a
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2024-03-19 11:27:20 +0000

    Update gmp.jl

--- base/gmp.jl
+++ base/gmp.jl
@@ -10,7 +10,8 @@ import .Base: *, +, -, /, <, <<, >>, >>>, <=, ==, >, >=, ^, (~), (&), (|), xor, 
              trailing_zeros, trailing_ones, count_ones, count_zeros, tryparse_internal,
              bin, oct, dec, hex, isequal, invmod, _prevpow2, _nextpow2, ndigits0zpb,
              widen, signed, unsafe_trunc, trunc, iszero, isone, big, flipsign, signbit,
-             sign, hastypemax, isodd, iseven, digits!, hash, hash_integer, top_set_bit
+             sign, hastypemax, isodd, iseven, digits!, hash, hash_integer, top_set_bit,
+             ispositive, isnegative
 
 if Clong == Int32
     const ClongMax = Union{Int8, Int16, Int32}
