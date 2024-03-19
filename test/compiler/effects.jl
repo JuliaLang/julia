678fe46f7648f4f5948ef0ffd170d30854367458
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2024-03-19 20:10:11 +0900

    post-opt: add more test cases for `visit_conditional_successors` (#53642)
    This commit fixes the first problem that was found while digging into
    JuliaLang/julia#53613. It turns out that the post-domtree constructed
    from regular `IRCode` doesn't work for visiting conditional successors
    for post-opt analysis in cases like:
    ```julia
    julia> let code = Any[
                   # block 1
                   GotoIfNot(Argument(2), 3),
                   # block 2
                   ReturnNode(Argument(3)),
                   # block 3 (we should visit this block)
                   Expr(:call, throw, "potential throw"),
                   ReturnNode(), # unreachable
               ]
               ir = make_ircode(code; slottypes=Any[Any,Bool,Bool])
               visited = BitSet()
               @test !Core.Compiler.visit_conditional_successors(CC.LazyPostDomtree(ir), ir, #=bb=#1) do succ::Int
                   push!(visited, succ)
                   return false
               end
               @test 2 ∉ visited
               @test 3 ∈ visited
           end
    Test Failed at REPL[14]:16
      Expression: 2 ∉ visited
       Evaluated: 2 ∉ BitSet([2])
    ```
    
    This might mean that we need to fix on the `postdominates` end, but for
    now, this commit tries to get around it by using the augmented post
    domtree in `visit_conditional_successors`. Since the augmented post
    domtree is enforced to have a single return, we can keep using the
    current `postdominates` to fix the issue.
    
    However, this commit isn't enough to fix the NeuralNetworkReachability
    segfault as reported in #53613, and we need to tackle the second issue
    reported there too
    (https://github.com/JuliaLang/julia/issues/53613#issuecomment-1983243419).

--- test/compiler/effects.jl
+++ test/compiler/effects.jl
@@ -1387,3 +1387,13 @@ let; Base.Experimental.@force_compile; func52843(); end
 # https://github.com/JuliaLang/julia/issues/53508
 @test !Core.Compiler.is_consistent(Base.infer_effects(getindex, (UnitRange{Int},Int)))
 @test !Core.Compiler.is_consistent(Base.infer_effects(getindex, (Base.OneTo{Int},Int)))
+
+@noinline f53613() = @assert isdefined(@__MODULE__, :v53613)
+g53613() = f53613()
+@test !Core.Compiler.is_consistent(Base.infer_effects(f53613))
+@test_broken !Core.Compiler.is_consistent(Base.infer_effects(g53613))
+@test_throws AssertionError f53613()
+@test_throws AssertionError g53613()
+global v53613 = nothing
+@test f53613() === nothing
+@test g53613() === nothing
