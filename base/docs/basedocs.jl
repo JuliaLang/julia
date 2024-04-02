4b8ed18bda951a974a0497c6c58fe30651473e2b
Steven G. Johnson <stevenj@alum.mit.edu>
2024-04-02 18:36:14 -0400

    Update basedocs.jl

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -1521,6 +1521,8 @@ Nothing
 The singleton instance of type [`Nothing`](@ref), used by convention when there is no value to return
 (as in a C `void` function) or when a variable or field holds no value.
 
+A return value of `nothing` is not displayed by the REPL and similar interactive environments.
+
 See also: [`isnothing`](@ref), [`something`](@ref), [`missing`](@ref).
 """
 nothing
