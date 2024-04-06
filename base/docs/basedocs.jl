597549bbc67d76571abc591775e533d1f412391a
Sukera <Seelengrab@users.noreply.github.com>
2024-04-06 09:57:47 +0200

    Add more known-unsafe behavior & mention the error in multithreading doc

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -3722,6 +3722,7 @@ An error thrown when a detectable violation of concurrent semantics has occurred
 A non-exhaustive list of examples of when this is used include:
 
  * Throwing when a deadlock has been detected (e.g. `wait(current_task())`)
+ * Known-unsafe behavior is attempted (e.g. `yield(current_task)`)
  * A known non-threadsafe datastructure is attempted to be modified from multiple concurrent tasks
  * A lock is being unlocked that wasn't locked by this task
 """
