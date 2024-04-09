d245384e2b8ba7d5d159ae7d9b7003e8ef96e731
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-04-09 10:55:04 +0200

    Fake dense array for codeunits of String

--- base/strings/substring.jl
+++ base/strings/substring.jl
@@ -72,6 +72,12 @@ convert(::Type{Union{String, SubString{String}}}, s::String) = s
 convert(::Type{Union{String, SubString{String}}}, s::SubString{String}) = s
 convert(::Type{Union{String, SubString{String}}}, s::AbstractString) = convert(String, s)::String
 
+# This allows CodeUnits of known, memory-backed String types to act like a dense array
+# See issue #53996
+function _memory_offset(x::CodeUnits{<:Any, <:Union{String, SubString{String}}})
+	(_to_linear_index(x, I...) - first(LinearIndices(x)))*elsize(x)
+end
+
 function String(s::SubString{String})
     parent = s.string
     copy = GC.@preserve parent unsafe_string(pointer(parent, s.offset+1), s.ncodeunits)
