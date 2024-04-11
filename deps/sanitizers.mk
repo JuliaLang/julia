9e7cc437bc39781706807d78c2b6b43183e61a2b
gbaraldi <baraldigabriel@gmail.com>
2024-04-11 16:56:31 -0300

    Simplify

--- deps/sanitizers.mk
+++ deps/sanitizers.mk
@@ -14,7 +14,7 @@ define copy_sanitizer_lib
 install-sanitizers: $$(addprefix $$(build_libdir)/, $$(notdir $$(call pathsearch_all,$(1),$$(SANITIZER_LIB_PATH)))) | $$(build_shlibdir)
 $$(addprefix $$(build_shlibdir)/,$(2)): $$(addprefix $$(SANITIZER_LIB_PATH)/,$(2)) | $$(build_shlibdir)
 	-cp $$< $$@
-	ifneq (,$(findstring $(OS),Linux))
+	ifeq ($(OS), Linux)
 	-$(PATCHELF) $(PATCHELF_SET_RPATH_ARG) '$$ORIGIN' $$@
 	endif
 endef
