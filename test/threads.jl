64b14bfd54491050efd84c6f7f9bc72d4a428afe
Carsten Bauer <crstnbr@gmail.com>
2024-04-17 11:56:49 +0200

    rebase fix

--- test/threads.jl
+++ test/threads.jl
@@ -354,3 +354,13 @@ end
         @test istaskfailed(t)
     end
 end
+
+@testset "jl_*affinity" begin
+    cpumasksize = @ccall uv_cpumask_size()::Cint
+    mask = zeros(Cchar, cpumasksize);
+    jl_getaffinity = (tid, mask, cpumasksize) -> ccall(:jl_getaffinity, Int32, (Int16, Ptr{Cchar}, Int32), tid, mask, cpumasksize)
+    jl_setaffinity = (tid, mask, cpumasksize) -> ccall(:jl_setaffinity, Int32, (Int16, Ptr{Cchar}, Int32), tid, mask, cpumasksize)
+    @test jl_getaffinity(1, mask, cpumasksize) == 0
+    fill!(mask, 1)
+    @test jl_setaffinity(1, mask, cpumasksize) == 0
+end
