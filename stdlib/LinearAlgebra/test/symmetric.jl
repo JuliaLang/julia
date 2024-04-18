c741bd3d7e29a29dfeb4fcf2144a006669543069
Mateus Araújo <maltusan@gmail.com>
2024-04-18 08:13:49 +0200

    stdlib: faster kronecker product between hermitian and symmetric matrices (#53186)
    The kronecker product between complex hermitian matrices is again
    hermitian, so it can be computed much faster by only doing the upper (or
    lower) triangular. As @andreasnoack will surely notice, this only true
    for types where `conj(a*b) == conj(a)*conj(b)`, so I'm restricting the
    function to act only on real and complex numbers. In the symmetric case,
    however, no additional assumption is needed, so I'm letting it act on
    anything.
    
    Benchmarking showed that the code is roughly 2 times as fast as the
    vanilla kronecker product, as expected. The fastest case was always the
    UU case, and the slowest the LU case. The code I used is below
    ```julia
    using LinearAlgebra
    using BenchmarkTools
    using Quaternions
    
    randrmatrix(d, uplo = :U) = Hermitian(randn(Float64, d, d), uplo)
    randcmatrix(d, uplo = :U) = Hermitian(randn(ComplexF64, d, d), uplo)
    randsmatrix(d, uplo = :U) = Symmetric(randn(ComplexF64, d, d), uplo)
    randqmatrix(d, uplo = :U) = Symmetric(randn(QuaternionF64, d, d), uplo)
    
    dima = 69
    dimb = 71
    for randmatrix in [randrmatrix, randcmatrix, randsmatrix, randqmatrix]
        for auplo in [:U, :L]
            for buplo in [:U, :L]
                a = randmatrix(dima, auplo)
                b = randmatrix(dimb, buplo)
                c = kron(a,b)
                therm = @belapsed kron!($c, $a, $b)
                C = Matrix(c)
                A = Matrix(a)
                B = Matrix(b)
                told = @belapsed kron!($C, $A, $B)
                @show told/therm
            end
        end
    end
    ```
    Weirdly enough, I got this expected speedup in one of my machines, but
    when running the benchmark in another I got roughly the same time. I
    guess that's a bug with `BechmarkTools`, because that's not consistent
    with the times I get running the functions individually, out of the
    loop.
    
    Another issue is that although I added a couple of tests, I couldn't get
    them to run. Perhaps someone here can tell me what's going on? I could
    run the tests from LinearAlgebra, it's just that editing the files made
    no difference to what was being run. I did get hundreds of errors from
    `triangular.jl`, but that's untouched by my code.
    
    ---------
    
    Co-authored-by: Oscar Smith <oscardssmith@gmail.com>

--- stdlib/LinearAlgebra/test/symmetric.jl
+++ stdlib/LinearAlgebra/test/symmetric.jl
@@ -467,6 +467,28 @@ end
                 @test dot(symblockml, symblockml) ≈ dot(msymblockml, msymblockml)
             end
         end
+
+        @testset "kronecker product of symmetric and Hermitian matrices" begin
+            for mtype in (Symmetric, Hermitian)
+                symau = mtype(a, :U)
+                symal = mtype(a, :L)
+                msymau = Matrix(symau)
+                msymal = Matrix(symal)
+                for eltyc in (Float32, Float64, ComplexF32, ComplexF64, BigFloat, Int)
+                    creal = randn(n, n)/2
+                    cimag = randn(n, n)/2
+                    c = eltya == Int ? rand(1:7, n, n) : convert(Matrix{eltya}, eltya <: Complex ? complex.(creal, cimag) : creal)
+                    symcu = mtype(c, :U)
+                    symcl = mtype(c, :L)
+                    msymcu = Matrix(symcu)
+                    msymcl = Matrix(symcl)
+                    @test kron(symau, symcu) ≈ kron(msymau, msymcu)
+                    @test kron(symau, symcl) ≈ kron(msymau, msymcl)
+                    @test kron(symal, symcu) ≈ kron(msymal, msymcu)
+                    @test kron(symal, symcl) ≈ kron(msymal, msymcl)
+                end
+            end
+        end
     end
 end
 
@@ -487,6 +509,7 @@ end
         @test S - S == MS - MS
         @test S*2 == 2*S == 2*MS
         @test S/2 == MS/2
+        @test kron(S,S) == kron(MS,MS)
     end
     @testset "mixed uplo" begin
         Mu = Matrix{Complex{BigFloat}}(undef,2,2)
@@ -502,6 +525,8 @@ end
             MSl = Matrix(Sl)
             @test Su + Sl == Sl + Su == MSu + MSl
             @test Su - Sl == -(Sl - Su) == MSu - MSl
+            @test kron(Su,Sl) == kron(MSu,MSl)
+            @test kron(Sl,Su) == kron(MSl,MSu)
         end
     end
 end
@@ -515,6 +540,16 @@ end
     A, B = [Quaternion.(randn(3,3), randn(3, 3), randn(3, 3), randn(3,3)) |> t -> t + transpose(t) for i in 1:2]
     @test A == Symmetric(A) && B == Symmetric(B)
     @test dot(A, B) ≈ dot(Symmetric(A), Symmetric(B))
+end
+
+# let's make sure the analogous bug will not show up with kronecker products
+@testset "kron Hermitian quaternion #52318" begin
+    A, B = [Quaternion.(randn(3,3), randn(3, 3), randn(3, 3), randn(3,3)) |> t -> t + t' for i in 1:2]
+    @test A == Hermitian(A) && B == Hermitian(B)
+    @test kron(A, B) ≈ kron(Hermitian(A), Hermitian(B))
+    A, B = [Quaternion.(randn(3,3), randn(3, 3), randn(3, 3), randn(3,3)) |> t -> t + transpose(t) for i in 1:2]
+    @test A == Symmetric(A) && B == Symmetric(B)
+    @test kron(A, B) ≈ kron(Symmetric(A), Symmetric(B))
 end
 
 #Issue #7647: test xsyevr, xheevr, xstevr drivers.
