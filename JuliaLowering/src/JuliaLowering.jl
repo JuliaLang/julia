b7e32642bee272c7bb13d9465ee02257b0009f90
Claire Foster <aka.c42f@gmail.com>
2024-04-21 06:43:14 +1000

    Move kinds into kinds.jl

--- JuliaLowering/src/JuliaLowering.jl
+++ JuliaLowering/src/JuliaLowering.jl
@@ -3,65 +3,12 @@ module JuliaLowering
 using JuliaSyntax
 
 using JuliaSyntax: SyntaxHead, highlight, Kind, GreenNode, @KSet_str
-using JuliaSyntax: haschildren, children, child, numchildren, head, kind, flags
+using JuliaSyntax: haschildren, children, child, numchildren, head, kind, flags, has_flags
 using JuliaSyntax: filename, first_byte, last_byte, source_location, span
 
 using JuliaSyntax: is_literal, is_number, is_operator, is_prec_assignment, is_infix_op_call, is_postfix_op_call, is_error
 
-# The following kinds are used in intermediate forms by lowering but are not
-# part of the surface syntax
-function _insert_kinds()
-    JuliaSyntax.insert_kinds!(JuliaLowering, 1, [
-        "BEGIN_LOWERING_KINDS"
-            # Compiler metadata hints
-            "meta"
-            "extension"
-            # A literal Julia value of any kind, as might be inserted by the AST
-            # during macro expansion
-            "Value"
-            # TODO: Use `meta` for inbounds and loopinfo etc?
-            "inbounds"
-            "inline"
-            "noinline"
-            "loopinfo"
-            # Identifier for a value which is only assigned once
-            "SSAValue"
-            # Scope expressions `(hygienic_scope ex s)` mean `ex` should be
-            # interpreted as being in scope `s`.
-            "hygienic_scope"
-            # Various heads harvested from flisp lowering.
-            # (TODO: May or may not need all these - assess later)
-            "break_block"
-            "scope_block"
-            "local_def"
-            "_while"
-            "_do_while"
-            "with_static_parameters"
-            "top"
-            "core"
-            "toplevel_butfirst"
-            "thunk"
-            "lambda"
-            "moved_local"
-            "the_exception"
-            "foreigncall"
-            "new"
-            "globalref"
-            "outerref"
-            "enter"
-            "leave"
-            "label"
-            "goto"
-            "gotoifnot"
-            "trycatchelse"
-            "tryfinally"
-            "method"
-            "slot"
-            "unnecessary"
-            "decl"
-        "END_LOWERING_KINDS"
-    ])
-end
+include("kinds.jl")
 _insert_kinds()
 
 include("syntax_graph.jl")
---
+++ JuliaLowering/src/kinds.jl
@@ -0,0 +1,58 @@
+
+# The following kinds are used in intermediate forms by lowering but are not
+# part of the surface syntax
+function _insert_kinds()
+    JuliaSyntax.insert_kinds!(JuliaLowering, 1, [
+        "BEGIN_LOWERING_KINDS"
+            # Compiler metadata hints
+            "meta"
+            "extension"
+            # A literal Julia value of any kind, as might be inserted by the AST
+            # during macro expansion
+            "Value"
+            # Quoted symbol. Used to distinguish Symbol literals from AST
+            # literals of kind K"Identifier"
+            "Symbol"
+            # TODO: Use `meta` for inbounds and loopinfo etc?
+            "inbounds"
+            "inline"
+            "noinline"
+            "loopinfo"
+            # Identifier for a value which is only assigned once
+            "SSAValue"
+            # Scope expressions `(hygienic_scope ex s)` mean `ex` should be
+            # interpreted as being in scope `s`.
+            "hygienic_scope"
+            # Various heads harvested from flisp lowering.
+            # (TODO: May or may not need all these - assess later)
+            "break_block"
+            "scope_block"
+            "local_def"
+            "_while"
+            "_do_while"
+            "with_static_parameters"
+            "top"
+            "core"
+            "toplevel_butfirst"
+            "thunk"
+            "lambda"
+            "moved_local"
+            "the_exception"
+            "foreigncall"
+            "new"
+            "globalref"
+            "outerref"
+            "enter"
+            "leave"
+            "label"
+            "goto"
+            "gotoifnot"
+            "trycatchelse"
+            "tryfinally"
+            "method"
+            "slot"
+            "unnecessary"
+            "decl"
+        "END_LOWERING_KINDS"
+    ])
+end
