3050998852fe7ae6425ec89c1fdffa24ba6472c4
gbaraldi <baraldigabriel@gmail.com>
2024-04-23 18:55:22 -0300

    Change jll example to use zstd because it has a simpler interface
    Co-authored-by: Cody Tapscott <topolarity@tapscott.me>

--- juliac/exe_examples/jll_minimal.jl
+++ juliac/exe_examples/jll_minimal.jl
@@ -3,12 +3,12 @@
 module Main2
 
 using Libdl
-using OpenSpecFun_jll
+using Zstd_jll
 Base.@ccallable function main()::Cint
-    println(Core.stdout,"Hello, world!")
-    a = rand(10)
-    openspecfun
-    println(Core.stdout, dlsym(libopenspecfun, :zairy_))
+    println(Core.stdout,"Julia! Hello, world!")
+    fptr = dlsym(Zstd_jll.libzstd_handle, :ZSTD_versionString)
+    println(Core.stdout, unsafe_string(ccall(fptr, Cstring, ())))
+    println(Core.stdout, unsafe_string(ccall((:ZSTD_versionString, libzstd), Cstring, ())))
     return 0
 end
 
