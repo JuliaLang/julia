315526e6baf256636b55681b9e5de4aa3e1870d8
Sahiti004 <99157468+Sahiti004@users.noreply.github.com>
2024-04-24 23:50:59 +0530

    Update stdlib/LinearAlgebra/src/symmetric.jl: efficient flipping of data
    Co-authored-by: Steven G. Johnson <stevenj@mit.edu>

--- stdlib/LinearAlgebra/src/symmetric.jl
+++ stdlib/LinearAlgebra/src/symmetric.jl
@@ -255,7 +255,7 @@ end
 end
 
 Base._reverse(A::Symmetric, dims::Integer) = (reverse((Matrix(A)); dims))
-Base._reverse(A::Symmetric, dims::Colon) = Symmetric(reverse(Matrix(A)))
+Base._reverse(A::Symmetric, dims::Colon) = Symmetric(reverse(A.data), A.uplo == 'U' ? :L : :U)
 Base._reverse!(A::Symmetric, dims::Colon) = (reverse!(A.data); A)
 
 @propagate_inbounds function setindex!(A::Symmetric, v, i::Integer, j::Integer)
