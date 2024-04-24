c1c2e46b10434ef591d49c35cb39c68e27059729
Sahiti004 <99157468+Sahiti004@users.noreply.github.com>
2024-04-24 23:54:34 +0530

    Update stdlib/LinearAlgebra/src/triangular.jl
    Co-authored-by: Steven G. Johnson <stevenj@mit.edu>

--- stdlib/LinearAlgebra/src/triangular.jl
+++ stdlib/LinearAlgebra/src/triangular.jl
@@ -301,7 +301,7 @@ end
 end
 
 Base._reverse(A::LowerTriangular, dims) = reverse!(Matrix(A); dims)
-Base._reverse(A::LowerTriangular, dims::Colon) = LowerTriangular(reverse(Matrix(A)))
+Base._reverse(A::LowerTriangular, dims::Colon) = UpperTriangular(reverse(A.data))
 Base._reverse!(A::LowerTriangular, dims::Colon) = (reverse!(A.data); A)
 
 @propagate_inbounds function setindex!(A::LowerTriangular, x, i::Integer, j::Integer)
