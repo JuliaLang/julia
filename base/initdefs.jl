acba89aa52402facbd9abaea5984a36e24b9e87e
Jeff Bezanson <jeff.bezanson@gmail.com>
2024-04-24 14:27:24 -0400

    fix handling of `nothing` in init_load_path
    upstreamable

--- base/initdefs.jl
+++ base/initdefs.jl
@@ -245,8 +245,15 @@ function init_load_path()
     if haskey(ENV, "JULIA_LOAD_PATH")
         paths = parse_load_path(ENV["JULIA_LOAD_PATH"])
     else
-        paths = filter!(env -> env !== nothing,
-            String[env == "@." ? current_project() : env for env in DEFAULT_LOAD_PATH])
+        paths = String[]
+        for env in DEFAULT_LOAD_PATH
+            if env == "@."
+                env = current_project()
+            end
+            if env !== nothing
+                push!(paths, env)
+            end
+        end
     end
     append!(empty!(LOAD_PATH), paths)
 end
