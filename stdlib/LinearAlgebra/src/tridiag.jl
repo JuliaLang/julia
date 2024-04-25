f7424e1056b643ab5428bc0349baa4526150ddb0
Sahiti004 <99157468+Sahiti004@users.noreply.github.com>
2024-04-25 20:24:24 +0530

    Update stdlib/LinearAlgebra/src/tridiag.jl
    Co-authored-by: Lilith Orion Hafner <lilithhafner@gmail.com>

--- stdlib/LinearAlgebra/src/tridiag.jl
+++ stdlib/LinearAlgebra/src/tridiag.jl
@@ -701,6 +701,8 @@ Base._reverse(A::Tridiagonal, dims) = reverse!(Matrix(A); dims)
 Base._reverse(A::Tridiagonal, dims::Colon) = Tridiagonal(reverse(A.du), reverse(A.d), reverse(A.dl))
 function Base._reverse!(A::Tridiagonal, dims::Colon)
     n = length(A.du) # == length(A.dl), & always 1-based
+    n == length(A.dl) || throw(DimensionMismatch("Diagonals have different lengths"))
+    Base.require_one_based_indexing(A.dl, A.du)
     # reverse and swap A.dl and A.du:
     for i in 1:(n >> 1)
         @inbounds A.dl[i], A.du[i], A.dl[n+1-i], A.du[n+1-i] = A.du[n+1-i], A.dl[n+1-i], A.du[i], A.dl[i]
