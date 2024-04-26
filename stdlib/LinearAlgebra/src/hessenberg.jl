13209b317343999424026f81a5ab5079227cd337
Sahiti004 <99157468+Sahiti004@users.noreply.github.com>
2024-04-26 22:24:38 +0530

    Update hessenberg.jl remove whitespace

--- stdlib/LinearAlgebra/src/hessenberg.jl
+++ stdlib/LinearAlgebra/src/hessenberg.jl
@@ -91,7 +91,7 @@ Base.@propagate_inbounds getindex(H::UpperHessenberg{T}, i::Integer, j::Integer)
     i <= j+1 ? convert(T, H.data[i,j]) : zero(T)
 
 Base._reverse(A::UpperHessenberg, dims) = reverse!(Matrix(A); dims)
-    
+
 Base.@propagate_inbounds function setindex!(A::UpperHessenberg, x, i::Integer, j::Integer)
     if i > j+1
         x == 0 || throw(ArgumentError("cannot set index in the lower triangular part " *
