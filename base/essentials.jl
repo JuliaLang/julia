f8a8fdde85c30ec076ca3c397c5e8802fb07922f
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-04-27 20:46:37 +0000

    Update base/essentials.jl
    Co-authored-by: Steven G. Johnson <stevenj@alum.mit.edu>

--- base/essentials.jl
+++ base/essentials.jl
@@ -852,8 +852,8 @@ end
     Using `@inbounds` may return incorrect results/crashes/corruption
     for out-of-bounds indices. The user is responsible for checking it manually.
     Only use `@inbounds` when you are certain that all accesses are in bounds (as 
-    undefined behavior, e.g. crashes, might occur if this assertion is violated). In
-    particular, using `1:length(A)` instead of `eachindex(A)` in a function like
+    undefined behavior, e.g. crashes, might occur if this assertion is violated). For
+    example, using `1:length(A)` instead of `eachindex(A)` in a function like
     the one above is _not_ safely inbounds because the first index of `A` may not
     be `1` for all user defined types that subtype `AbstractArray`.
 """
