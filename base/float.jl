1bfa3d331b54c4a32161651e11a6ed0b24b5d666
kimikage <kimikage.ceo@gmail.com>
2024-04-29 13:20:06 +0900

    Add note to `unsafe_trunc` docstring

--- base/float.jl
+++ base/float.jl
@@ -398,6 +398,8 @@ float(::Type{Union{}}, slurp...) = Union{}(0.0)
 Return the nearest integral value of type `T` whose absolute value is
 less than or equal to the absolute value of `x`. If the value is not representable by `T`,
 an arbitrary value will be returned.
+"Arbitrary" here means that it is not only dependent on the CPU architecture or
+the julia version, but also possibly different on every invocation.
 See also [`trunc`](@ref).
 
 # Examples
@@ -405,7 +407,7 @@ See also [`trunc`](@ref).
 julia> unsafe_trunc(Int, -2.2)
 -2
 
-julia> unsafe_trunc(Int, NaN)
+julia> unsafe_trunc(Int, NaN) # possibly different on every invocation
 -9223372036854775808
 ```
 """
