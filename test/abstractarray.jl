d6d70d36294e8183358855243435bb34287de969
Sahiti004 <99157468+Sahiti004@users.noreply.github.com>
2024-05-02 22:24:06 +0530

    Update test/abstractarray.jl
    Co-authored-by: Steven G. Johnson <stevenj@mit.edu>

--- test/abstractarray.jl
+++ test/abstractarray.jl
@@ -932,7 +932,7 @@ end
 end
 
 @testset "reverse dim on empty" begin
-    @test reverse(Diagonal(Int[]),dims=1) == Diagonal([])
+    @test reverse(Diagonal([]),dims=1) == Diagonal([])
 end
 
 @testset "ndims and friends" begin
