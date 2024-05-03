51d73ef3208c83378a59242aa19cc29067a7947b
Sahiti004 <99157468+Sahiti004@users.noreply.github.com>
2024-05-03 15:55:50 +0530

    Update stdlib/LinearAlgebra/test/tridiag.jl
    Co-authored-by: Steven G. Johnson <stevenj@mit.edu>

--- stdlib/LinearAlgebra/test/tridiag.jl
+++ stdlib/LinearAlgebra/test/tridiag.jl
@@ -867,7 +867,7 @@ end
         T = Tridiagonal(dl, d, du)
         @test reverse(T, dims=1) == reverse(Matrix(T), dims=1)
         @test reverse(T, dims=2) == reverse(Matrix(T), dims=2)
-        @test reverse(T) == reverse(Matrix(T)) == reverse!(copy(T))
+        @test reverse(T)::Tridiagonal == reverse(Matrix(T)) == reverse!(copy(T))
     end
 end
 
