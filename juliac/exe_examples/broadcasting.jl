c4c8a4dacbf161d1521d1a79266505cc11a7b2f1
gbaraldi <baraldigabriel@gmail.com>
2024-05-08 15:59:55 -0300

    Expand mapreduce tests and add overloads to the build script

--- juliac/exe_examples/broadcasting.jl
+++ juliac/exe_examples/broadcasting.jl
@@ -7,7 +7,10 @@ Base.@ccallable function main()::Cint
     println(Core.stdout, "Hello, world!")
     A = rand(10, 10)
     L = sin.(A)
+    B = rand(10)
+    L2 = sin.(B)
     ccall(:jl_, Cvoid, (Any,), L)
+    ccall(:jl_, Cvoid, (Any,), L2)
     return 0
 end
 
