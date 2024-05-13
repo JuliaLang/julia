37a177674cb3724ed5e2b6783d5b6cc152dd6e51
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-05-13 09:03:06 +0200

    Fix typo

--- base/int.jl
+++ base/int.jl
@@ -720,7 +720,7 @@ macro big_str(s::String)
             is_prev_dot = (c == '.')
         end
         print(bf, s[end])
-        s = unsafe_takestring(bf)
+        s = unsafe_takestring!(bf)
     end
     n = tryparse(BigInt, s)
     n === nothing || return n
