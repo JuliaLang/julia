a38e1ef6aafb28cfe29f7450d121798d2b527dfc
Nathan Daly <nathan.daly@relational.ai>
2024-05-15 16:24:08 -0600

    PR feedback

--- base/checked.jl
+++ base/checked.jl
@@ -366,14 +366,14 @@ Calculates `^(x,y)`, checking for overflow errors where applicable.
 The overflow protection may impose a perceptible performance penalty.
 """
 checked_pow(x::Integer, y::Integer) = checked_power_by_squaring(x, y)
+# BigInt can never overflow, so we don't need to check anything, and can fall back to the
+# default implementation of `^` (which calls into the C library).
+checked_pow(x::BigInt, p::Integer) = x^p
+checked_pow(x::Integer, p::BigInt) = x^p
 
 checked_power_by_squaring(x_, p::Integer) = Base.power_by_squaring(x_, p; mul = checked_mul)
 # For Booleans, the default implementation covers all cases.
 checked_power_by_squaring(x::Bool, p::Integer) = Base.power_by_squaring(x, p)
-# BigInt can never overflow, so we don't need to check anything, and can fall back to the
-# default implementation of `^` (which calls into the C library).
-checked_power_by_squaring(x::BigInt, p::Integer) = x^p
-checked_power_by_squaring(x::Integer, p::BigInt) = x^p
 
 """
     Base.checked_length(r)
