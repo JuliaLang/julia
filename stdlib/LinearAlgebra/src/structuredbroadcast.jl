cc1ef605037e27ea39f8e500a804a87535b777cc
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-05-16 16:10:45 -0500

    Use Some/Nothing instead of a sentinel for `fzero` (#54469)
    Avoid bug that destroys sparsity when missing occurs in input, which is misinterpreted as a sentinel.

--- stdlib/LinearAlgebra/src/structuredbroadcast.jl
+++ stdlib/LinearAlgebra/src/structuredbroadcast.jl
@@ -144,21 +144,26 @@ structured arguments.
 
 For trivial broadcasted values such as `bc::Number`, this reduces to `iszero(bc)`.
 """
-fzeropreserving(bc) = (v = fzero(bc); !ismissing(v) && (iszerodefined(typeof(v)) ? iszero(v) : v == 0))
+function fzeropreserving(bc)
+    v = fzero(bc)
+    isnothing(v) && return false
+    v2 = something(v)
+    iszerodefined(typeof(v2)) ? iszero(v2) : isequal(v2, 0)
+end
 
 # Like sparse matrices, we assume that the zero-preservation property of a broadcasted
 # expression is stable.  We can test the zero-preservability by applying the function
 # in cases where all other arguments are known scalars against a zero from the structured
 # matrix. If any non-structured matrix argument is not a known scalar, we give up.
-fzero(x::Number) = x
-fzero(::Type{T}) where T = T
-fzero(r::Ref) = r[]
-fzero(t::Tuple{Any}) = t[1]
-fzero(S::StructuredMatrix) = zero(eltype(S))
-fzero(x) = missing
+fzero(x::Number) = Some(x)
+fzero(::Type{T}) where T = Some(T)
+fzero(r::Ref) = Some(r[])
+fzero(t::Tuple{Any}) = Some(only(t))
+fzero(S::StructuredMatrix) = Some(zero(eltype(S)))
+fzero(x) = nothing
 function fzero(bc::Broadcast.Broadcasted)
     args = map(fzero, bc.args)
-    return any(ismissing, args) ? missing : bc.f(args...)
+    return any(isnothing, args) ? nothing : Some(bc.f(map(something, args)...))
 end
 
 function Base.similar(bc::Broadcasted{StructuredMatrixStyle{T}}, ::Type{ElType}) where {T,ElType}
