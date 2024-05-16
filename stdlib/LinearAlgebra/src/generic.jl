11bfa974deb3ad0e7df6de5366de7fb810131f4b
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-05-16 11:55:11 +0530

    Use lacpy! in copytrito! for strided matrices

--- stdlib/LinearAlgebra/src/generic.jl
+++ stdlib/LinearAlgebra/src/generic.jl
@@ -2028,3 +2028,7 @@ function copytrito!(B::AbstractMatrix, A::AbstractMatrix, uplo::AbstractChar)
     end
     return B
 end
+# Forward contiguous strided matrices to LAPACK to avail the faster method
+function copytrito!(B::StridedMatrixStride1{T}, A::StridedMatrixStride1{T}, uplo::AbstractChar) where {T<:BlasFloat}
+    LAPACK.lacpy!(B, A, uplo)
+end
