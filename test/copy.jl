c28a9de26dd467ad847b0f694419e061df205574
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-05-18 16:42:14 +0200

    Make `deepcopy_internal` inferrable for BigInt and BigFloat (#54496)
    Co-authored-by: Lilith Orion Hafner <lilithhafner@gmail.com>

--- test/copy.jl
+++ test/copy.jl
@@ -248,6 +248,11 @@ end
     @test (@inferred Base.deepcopy_internal(zeros(), IdDict())) == zeros()
 end
 
+@testset "deepcopy_internal big" begin
+    @inferred Base.deepcopy_internal(big(1), IdDict())
+    @inferred Base.deepcopy_internal(big(1.0), IdDict())
+end
+
 @testset "`copyto!`'s unaliasing" begin
     a = view([1:3;], :)
     @test copyto!(a, 2, a, 1, 2) == [1;1:2;]
