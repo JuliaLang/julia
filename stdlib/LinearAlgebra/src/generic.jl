3a8c099f4e5ef0b011bc3564b573b7b781c84ed4
James Foster <38274066+jd-foster@users.noreply.github.com>
2024-05-22 22:55:09 +1000

    Avoid StackOverflowError of generic `norm` on `AbstractChar` (#54122)

--- stdlib/LinearAlgebra/src/generic.jl
+++ stdlib/LinearAlgebra/src/generic.jl
@@ -668,6 +668,9 @@ true
 """
 Base.@constprop :aggressive function norm(itr, p::Real=2)
     isempty(itr) && return float(norm(zero(eltype(itr))))
+    v, s = iterate(itr)
+    !isnothing(s) && !ismissing(v) && v == itr && throw(ArgumentError(
+        "cannot evaluate norm recursively if the type of the initial element is identical to that of the container"))
     if p == 2
         return norm2(itr)
     elseif p == 1
