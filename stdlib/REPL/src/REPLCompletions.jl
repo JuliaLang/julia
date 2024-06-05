37f48b25ecfd9c8e1c61ea72a87347ad698f638d
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2024-06-05 09:13:10 +0900

    REPLInterpreter: discard sources aggressively (#54673)
    There's no need to hold any inferred sources in the `REPLInterpreter`s
    cache, so it's more space efficient to aggressively discard the inferred
    sources.

--- stdlib/REPL/src/REPLCompletions.jl
+++ stdlib/REPL/src/REPLCompletions.jl
@@ -566,6 +566,9 @@ CC.cache_owner(::REPLInterpreter) = REPLCacheToken()
 # REPLInterpreter is only used for type analysis, so it should disable optimization entirely
 CC.may_optimize(::REPLInterpreter) = false
 
+# REPLInterpreter doesn't need any sources to be cached, so discard them aggressively
+CC.transform_result_for_cache(::REPLInterpreter, ::Core.MethodInstance, ::CC.WorldRange, ::CC.InferenceResult) = nothing
+
 # REPLInterpreter analyzes a top-level frame, so better to not bail out from it
 CC.bail_out_toplevel_call(::REPLInterpreter, ::CC.InferenceLoopState, ::CC.InferenceState) = false
 
