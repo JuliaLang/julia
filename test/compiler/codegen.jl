5cb1107cce1f8361b44e8609f08d5e3b0e11c77d
Gabriel Baraldi <baraldigabriel@gmail.com>
2024-06-06 00:00:18 -0300

    Fix assertion/crash when optimizing function with dead basic block (#54690)
    AllocOpt probably needs to handle that in other places more smartly but
    this seems to at least stop it crashing. Fixes issue found in
    https://github.com/JuliaLang/julia/pull/54604#issuecomment-2136320508 by
    @topolarity.

--- test/compiler/codegen.jl
+++ test/compiler/codegen.jl
@@ -939,11 +939,20 @@ BigStructAnyInt() = BigStructAnyInt((Union{Base.inferencebarrier(Float64), Int}=
 @test egal_any54109(Torture2_54109(), Torture2_54109())
 @test !egal_any54109(Torture1_54109(), Torture1_54109((DefaultOr54109(2.0, false) for i = 1:897)...))
 
+bar54599() = Base.inferencebarrier(true) ? (Base.PkgId(Main),1) : nothing
+
 function foo54599()
-    pkgid = Base.identify_package("Test")
-    println(devnull,pkgid)
-    println(devnull, pkgid.uuid)
-    pkgid.uuid
+    pkginfo = @noinline bar54599()
+    pkgid = pkginfo !== nothing ? pkginfo[1] : nothing
+    @noinline println(devnull, pkgid)
+    pkgid.uuid !== nothing ? pkgid.uuid : false
 end
 
-@test foo54599() !== nothing
+#this function used to crash allocopt due to a no predecessors bug
+barnopreds() = Base.inferencebarrier(true) ? (Base.PkgId(Test),1) : nothing
+function foonopreds()
+    pkginfo = @noinline barnopreds()
+    pkgid = pkginfo !== nothing ? pkginfo[1] : nothing
+    pkgid.uuid !== nothing ? pkgid.uuid : false
+end
+@test foonopreds() !== nothing
