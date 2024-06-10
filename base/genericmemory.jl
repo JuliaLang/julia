9f7959f20162ad19e23bc439af9d0c97a3851c26
oscarddssmith <oscar.smith@juliacomputing.com>
2024-06-10 09:41:25 -0400

    adress review

--- base/genericmemory.jl
+++ base/genericmemory.jl
@@ -27,7 +27,7 @@ GenericMemory
 """
     Memory{T} == GenericMemory{:not_atomic, T, Core.CPU}
 
-One-dimensional, fixed-size, dense array with elements of type `T`.
+Fixed-size [`DenseVector{T}`](@ref DenseVector).
 
 !!! compat "Julia 1.11"
     This type requires Julia 1.11 or later.
@@ -37,9 +37,12 @@ Memory
 """
     AtomicMemory{T} == GenericMemory{:atomic, T, Core.CPU}
 
-!!! warning
 Fixed-size [`DenseVector{T}`](@ref DenseVector).
-Each element is independently atomic when accessed, and cannot be set non-atomically.
+Access to its any of its elements is performed atomically (with `:monotonic` ordering).
+Setting any of the elements must be accomplished using the `@atomic` macro and explicitly specifying ordering.
+
+!!! warning
+    Each element is independently atomic when accessed, and cannot be set non-atomically.
     Currently the `@atomic` macro and higher level interface have not been completed,
     but the building blocks for a future implementation are the internal intrinsics
     `Core.memoryrefget`, `Core.memoryrefset!`, `Core.memoryref_isassigned`, `Core.memoryrefswap!`,
