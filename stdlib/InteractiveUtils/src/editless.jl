c7bf7b8158a166f6631fde7edce427bd72189b7d
Phillip Alday <palday@users.noreply.github.com>
2024-06-12 03:13:31 +0000

    add missing compat entry to edit (#54769)
    (cherry picked from commit 3f8e1bd953ebff71cc430ec87977d06bd16e16f2)

--- stdlib/InteractiveUtils/src/editless.jl
+++ stdlib/InteractiveUtils/src/editless.jl
@@ -223,7 +223,10 @@ Edit a file or directory optionally providing a line number to edit the file at.
 Return to the `julia` prompt when you quit the editor. The editor can be changed
 by setting `JULIA_EDITOR`, `VISUAL` or `EDITOR` as an environment variable.
 
-See also [`define_editor`](@ref).
+!!! compat "Julia 1.9"
+    The `column` argument requires at least Julia 1.9.
+
+See also [`InteractiveUtils.define_editor`](@ref).
 """
 function edit(path::AbstractString, line::Integer=0, column::Integer=0)
     path isa String || (path = convert(String, path))
