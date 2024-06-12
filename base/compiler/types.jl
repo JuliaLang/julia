2a4353c122a9a37778d8cd7b0a50431515b4447f
Gabriel Baraldi <baraldigabriel@gmail.com>
2024-06-12 09:38:12 -0300

    Less invasive changes to base to make hello world work

--- base/compiler/types.jl
+++ base/compiler/types.jl
@@ -213,7 +213,7 @@ function InferenceParams(
         #=tuple_complexity_limit_depth::Int=# 3,
         #=ipo_constant_propagation::Bool=# true,
         #=aggressive_constant_propagation::Bool=# false,
-        #=unoptimize_throw_blocks::Bool=# true,
+        #=unoptimize_throw_blocks::Bool=# false,
         #=assume_bindings_static::Bool=# false,
         #=ignore_recursion_hardlimit::Bool=# false);
     max_methods::Int = params.max_methods,
