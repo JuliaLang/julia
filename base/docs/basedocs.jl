9232b7be439afcbd074e102c8e83c9b2dabb48b0
Ian Butterworth <i.r.butterworth@gmail.com>
2024-06-14 21:08:31 -0400

    add sticky task warning to `@task` and `schedule`

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -1786,7 +1786,8 @@ The task will run in the "world age" from the parent at construction when [`sche
 !!! warning
     By default tasks will have the sticky bit set to true `t.sticky`. This models the
     historic default for [`@async`](@ref). Sticky tasks can only be run on the worker thread
-    they are first scheduled on. To obtain the behavior of [`Threads.@spawn`](@ref) set the sticky
+    they are first scheduled on, and when scheduled will make the task that they were scheduled
+    from sticky. To obtain the behavior of [`Threads.@spawn`](@ref) set the sticky
     bit manually to `false`.
 
 # Examples
