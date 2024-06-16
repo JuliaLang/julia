4afe2cea5e85cd3eea0d6ec7d933381d5a38dcc6
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2024-06-15 23:22:03 -0400

    Update `Base.peek` docstring
    When looking at the help for `peek` in the repl, the current docstring looks like it applies to all `IO`, so I added `::ParseStream` to make it more specific. See also https://github.com/JuliaLang/julia/issues/54749

--- JuliaSyntax/src/parse_stream.jl
+++ JuliaSyntax/src/parse_stream.jl
@@ -475,7 +475,7 @@ end
 end
 
 """
-    peek(stream [, n=1]; skip_newlines=false)
+    peek(stream::ParseStream [, n=1]; skip_newlines=false)
 
 Look ahead in the stream `n` tokens, returning the token kind. Comments and
 non-newline whitespace are skipped automatically. Whitespace containing a
