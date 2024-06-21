3b84709d03e0075eb9b7c14c1569d27df9f737f4
Andrés Riedemann <38795484+longemen3000@users.noreply.github.com>
2024-06-21 13:47:43 -0400

    add similar for ReshapedArray, SubArray, ReinterpretArray types

--- base/reshapedarray.jl
+++ base/reshapedarray.jl
@@ -229,6 +229,7 @@ end
 size(A::ReshapedArray) = A.dims
 length(A::ReshapedArray) = length(parent(A))
 similar(A::ReshapedArray, eltype::Type, dims::Dims) = similar(parent(A), eltype, dims)
+similar(::Type{TA}, dims::Dims) where {T,N,P,TA<:ReshapedArray{T,N,P}} = similar(P, dims)
 IndexStyle(::Type{<:ReshapedArrayLF}) = IndexLinear()
 parent(A::ReshapedArray) = A.parent
 parentindices(A::ReshapedArray) = map(oneto, size(parent(A)))
