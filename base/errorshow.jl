4a657725ea2bc899d6e7005988b1160e7a95c5bd
Adrian Hill <adrian.hill@mailbox.org>
2024-06-21 16:51:15 +0200

    Highlight repeated function calls in stack traces (#54714)
    As pointed out in #52334, stack traces of `StackOverflowError`s
    currently don't highlight recursions enough. Spotting the light black
    `(repeats $n times)` note in a colorful stack trace is difficult.
    
    This PR highlights this note in bold warning color.

--- base/errorshow.jl
+++ base/errorshow.jl
@@ -768,7 +768,7 @@ function print_stackframe(io, i, frame::StackFrame, n::Int, ndigits_max, modulec
 
     StackTraces.show_spec_linfo(IOContext(io, :backtrace=>true), frame)
     if n > 1
-        printstyled(io, " (repeats $n times)"; color=:light_black)
+        printstyled(io, " (repeats $n times)"; color=Base.warn_color(), bold=true)
     end
     println(io)
 
