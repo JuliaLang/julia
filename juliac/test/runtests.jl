08ac7268179bd7856aa43f319de62e935a7069e1
Jeff Bezanson <jeff.bezanson@gmail.com>
2024-07-05 15:04:53 -0400

    rename option to --trim

--- juliac/test/runtests.jl
+++ juliac/test/runtests.jl
@@ -9,9 +9,9 @@ end
 root = @__DIR__
 tmp = mktempdir()
 @testset "juliac" begin
-    @test success(`$(cmd) --project=$(root)/../. $(root)/../juliac.jl --output-exe $(tmp)/hello --static-call-graph $(root)/../exe_examples/hello_world.jl`)
+    @test success(`$(cmd) --project=$(root)/../. $(root)/../juliac.jl --output-exe $(tmp)/hello --trim $(root)/../exe_examples/hello_world.jl`)
     @test readchomp(`$(tmp)/hello`) == "Hello, world!"
-    @test success(`$(cmd) --project=$(root)/../. $(root)/../juliac.jl --output-exe $(tmp)/lu --static-call-graph $(root)/../exe_examples/simple_lu.jl`)
+    @test success(`$(cmd) --project=$(root)/../. $(root)/../juliac.jl --output-exe $(tmp)/lu --trim $(root)/../exe_examples/simple_lu.jl`)
     @test success(`$(tmp)/lu`)
 end
 
