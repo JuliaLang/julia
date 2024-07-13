6e11ffcfad5a87e5d24e7b04a9b047e4363fc426
Jameson Nash <vtjnash@gmail.com>
2024-07-13 17:12:47 -0400

    make threaded exit and cache file loading safer by pausing other threads (#55105)

--- src/signals-mach.c
+++ src/signals-mach.c
@@ -45,7 +45,6 @@ static void attach_exception_port(thread_port_t thread, int segv_only);
 
 // low 16 bits are the thread id, the next 8 bits are the original gc_state
 static arraylist_t suspended_threads;
-extern uv_mutex_t safepoint_lock;
 extern uv_cond_t safepoint_cond_begin;
 
 #define GC_STATE_SHIFT 8*sizeof(int16_t)
