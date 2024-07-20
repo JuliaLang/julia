373bd8e992e9a4a8c5cd89e1efea16e2ffbb708b
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-07-20 15:40:34 +0530

    Fix whitespace

--- stdlib/LinearAlgebra/test/diagonal.jl
+++ stdlib/LinearAlgebra/test/diagonal.jl
@@ -1235,7 +1235,7 @@ end
     @test repr(Diagonal([1,2])) == "Diagonal([1, 2])"  # 2-arg show
     @test contains(repr(MIME"text/plain"(), Diagonal([1,2])), "⋅  2")  # 3-arg show
 end
-                            
+
 @testset "copyto! with UniformScaling" begin
     @testset "Fill" begin
         for len in (4, InfiniteArrays.Infinity())
