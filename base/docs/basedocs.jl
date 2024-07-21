042230a83a85c2a747b039854d085dd1c1adc21d
Ian Butterworth <i.r.butterworth@gmail.com>
2024-07-21 18:53:08 -0400

    document mutable struct const fields

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -1447,8 +1447,20 @@ kw"struct"
     mutable struct
 
 `mutable struct` is similar to [`struct`](@ref), but additionally allows the
-fields of the type to be set after construction. See the manual section on
-[Composite Types](@ref) for more information.
+fields of the type to be set after construction.
+
+Individual fields of a mutable struct can be marked as `const` to make them immutable:
+
+```julia
+mutable struct Baz
+    a::Int
+    const b::Float64
+end
+```
+!!! compat "Julia 1.8"
+    The `const` keyword for fields of mutable structs requires at least Julia 1.8.
+
+See the manual section on [Composite Types](@ref) for more information.
 """
 kw"mutable struct"
 
