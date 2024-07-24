c4d71d98ee376daeeadf3da33db5c6fc91232556
Andre Guedes <andre.guedes@relational.ai>
2024-07-24 13:41:02 -0300

    Only ensure tls when libunwind is present

--- src/threading.c
+++ src/threading.c
@@ -404,7 +404,7 @@ jl_ptls_t jl_init_threadtls(int16_t tid)
     jl_fence();
     uv_mutex_unlock(&tls_lock);
 
-#if !defined(JL_DISABLE_LIBUNWIND)
+#if !defined(_OS_WINDOWS_) && !defined(JL_DISABLE_LIBUNWIND) && !defined(LLVMLIBUNWIND)
     // ensures libunwind TLS space for this thread is allocated eagerly
     // to make unwinding async-signal-safe even when using thread local caches.
     unw_ensure_tls();
