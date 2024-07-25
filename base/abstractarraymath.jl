cf23193954d5cafea881a171408ebf84a7146652
Felix Wechsler <fxw+git@mailbox.org>
2024-07-25 18:24:59 +0200

    Update base/abstractarraymath.jl
    Co-authored-by: Neven Sajko <s@purelymail.com>

--- base/abstractarraymath.jl
+++ base/abstractarraymath.jl
@@ -160,7 +160,7 @@ function _insertdims(A::AbstractArray{T, N}, dims::Tuple{Vararg{Int64, M}}) wher
     # n is the amount of the dims already inserted
     ax_n = Base._foldoneto(((ds, n, dims), _) ->
                              dims != Tuple(()) && n == first(dims) ?
-                             ((ds..., Base.OneTo(1)), n, Base.tail(dims)) : 
+                             ((ds..., Base.OneTo(1)), n, Base.tail(dims)) :
                              ((ds..., axes(A,n)), n+1, dims),
                            ((), 1, dims), Val(ndims(A) + length(dims)))
            # we need only the new shape and not n
