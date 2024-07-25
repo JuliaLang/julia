0429e1edd5e085fa2eb997f6e08504efd9aa7db2
Mateus Araújo <maltusan@gmail.com>
2024-07-25 10:02:50 +0200

    [docs] change docstring to match code (#55013)
    The docstring of `LinearAlgebra.reflectorApply!` is incorrect. It says
    the function is applying `(I - τ*[1; x] * [1; x]')*A`, but in reality it
    is applying `(I - conj(τ)*[1; x[2:end]]*[1; x[2:end]]')*A`. You can
    check that by looking at the code, or running for example
    ```julia
    using LinearAlgebra
    T=ComplexF64;d=5;
    τ=randn(T);x=randn(T,d);A=randn(T,d,d);
    (I - conj(τ)*[1; x[2:end]]*[1; x[2:end]]')*A
    LinearAlgebra.reflectorApply!(x,τ,A)
    ```
    
    (cherry picked from commit 1ece299e4a1ebf9410099d2442c8dd2a19f9f615)

--- stdlib/LinearAlgebra/src/generic.jl
+++ stdlib/LinearAlgebra/src/generic.jl
@@ -1607,7 +1607,7 @@ end
 """
     reflectorApply!(x, τ, A)
 
-Multiplies `A` in-place by a Householder reflection on the left. It is equivalent to `A .= (I - τ*[1; x] * [1; x]')*A`.
+Multiplies `A` in-place by a Householder reflection on the left. It is equivalent to `A .= (I - conj(τ)*[1; x[2:end]]*[1; x[2:end]]')*A`.
 """
 @inline function reflectorApply!(x::AbstractVector, τ::Number, A::AbstractVecOrMat)
     require_one_based_indexing(x)
