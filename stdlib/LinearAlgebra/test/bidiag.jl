f2f188d57346a0163b82740ac0a758311c41004f
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-08-03 14:27:59 +0000

    Ensure bidiagonal setindex! does not read indices in error message (#55342)
    This fixes the error message if the matrix is uninitialized. This is
    because a `Bidiagonal` with `uplo == 'L'` may still be `istriu` if the
    subdiaognal is zero. We only care about the band index in the error
    message, and not the values.

--- stdlib/LinearAlgebra/test/bidiag.jl
+++ stdlib/LinearAlgebra/test/bidiag.jl
@@ -984,4 +984,9 @@ end
     @test Tridiagonal{Float64}(B) === Tridiagonal(evf, dvf, zf)
 end
 
+@testset "off-band indexing error" begin
+    B = Bidiagonal(Vector{BigInt}(undef, 4), Vector{BigInt}(undef,3), :L)
+    @test_throws "cannot set entry" B[1,2] = 4
+end
+
 end # module TestBidiagonal
