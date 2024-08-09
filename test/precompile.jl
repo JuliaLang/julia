3a4ccaa990c8a3d623b6f67d6b140a5fb202d2c2
Ian Butterworth <i.r.butterworth@gmail.com>
2024-08-09 19:47:20 +0200

    `stale_cachefile`: handle if the expected cache file is missing (#55419)
    Part of fixing https://github.com/JuliaLang/Pkg.jl/issues/3984

--- test/precompile.jl
+++ test/precompile.jl
@@ -522,6 +522,10 @@ precompile_test_harness(false) do dir
     @eval using Baz
     @test Base.invokelatest(Baz.baz) == 1
 
+    # should not throw if the cachefile does not exist
+    @test !isfile("DoesNotExist.ji")
+    @test Base.stale_cachefile("", "DoesNotExist.ji") === true
+
     # Issue #12720
     FooBar1_file = joinpath(dir, "FooBar1.jl")
     write(FooBar1_file,
