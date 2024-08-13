5e31a0784f0b9b92e33e06ed1b4b3a26fe424898
Jeff Bezanson <jeff.bezanson@gmail.com>
2024-08-13 19:53:48 -0400

    hook up CI for trimming

--- test/trimming/trimming.jl
+++ test/trimming/trimming.jl
@@ -1,31 +1,6 @@
 using Test
 
-tmpdir = mktempdir()
-img_path = joinpath(tmpdir, "img.a")
-init_path = joinpath(tmpdir, "init.a")
-exe_path = joinpath(tmpdir, "hello")
-
-cmd = Base.julia_cmd()
-cmd = `$cmd --startup-file=no --history-file=no`
-
-cflags = readchomp(`$(cmd) $(joinpath(Sys.BINDIR, Base.DATAROOTDIR,"julia", "julia-config.jl")) --cflags `)
-cflags = Base.shell_split(cflags)
-allflags = readchomp(`$(cmd) $(joinpath(Sys.BINDIR, Base.DATAROOTDIR,"julia", "julia-config.jl")) --allflags`)
-allflags = Base.shell_split(allflags)
-
-cmd = addenv(`$cmd --output-o $img_path --output-incremental=no --strip-ir --strip-metadata --trim $(joinpath(@__DIR__,"buildscript.jl")) hello.jl --output-exe true`, "OPENBLAS_NUM_THREADS" => 1, "JULIA_NUM_THREADS" => 1)
-
-@test success(cmd)
-
-cd(@__DIR__) do
-    global CC = chomp(read(`make`, String))
-end
-
-@test success(`$CC $(cflags) -g -c -o $init_path $(joinpath(@__DIR__, "init.c"))`)
-
-julia_libs = Base.shell_split(Base.isdebugbuild() ? "-ljulia-debug -ljulia-internal-debug" : "-ljulia -ljulia-internal")
-
-@test success(`$CC $(allflags) -o $exe_path -Wl,$(Base.Linking.WHOLE_ARCHIVE) $img_path -Wl,$(Base.Linking.NO_WHOLE_ARCHIVE) $init_path $(julia_libs)`)
+exe_path = ARGS[1]
 
 @test readchomp(`$exe_path`) == "Hello, world!"
 
