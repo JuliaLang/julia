c8b3a49f058bfaabe3c1931a79efeb8626f871d9
Devansh Ramgopal Agrawal <devansh@umich.edu>
2024-08-22 10:42:42 -0400

    fixed whitespace v2

--- stdlib/LinearAlgebra/src/cholesky.jl
+++ stdlib/LinearAlgebra/src/cholesky.jl
@@ -551,7 +551,7 @@ end
 # allow packages like SparseArrays.jl to hook into here and redirect to out-of-place `cholesky`
 _cholesky(A::AbstractMatrix, args...; kwargs...) = cholesky!(A, args...; kwargs...)
 
-# allow cholesky of cholesky 
+# allow cholesky of cholesky
 cholesky(A::M) where {M <: Cholesky} = A
 
 ## With pivoting
