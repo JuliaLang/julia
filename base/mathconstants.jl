fb97552f25dc3eadb84f4d748912f77f40229e14
Neven Sajko <s@purelymail.com>
2024-08-22 17:56:42 +0200

    `MathConstants`: return type domain integers when that makes sense

--- base/mathconstants.jl
+++ base/mathconstants.jl
@@ -8,6 +8,8 @@ See [`π`](@ref), [`ℯ`](@ref), [`γ`](@ref), [`φ`](@ref) and [`catalan`](@ref
 """
 module MathConstants
 
+using ..TypeDomainIntegers
+
 export π, pi, ℯ, e, γ, eulergamma, catalan, φ, golden
 
 Base.@irrational π        pi
@@ -120,13 +122,13 @@ for T in (AbstractIrrational, Rational, Integer, Number, Complex)
 end
 Base.literal_pow(::typeof(^), ::Irrational{:ℯ}, ::Val{p}) where {p} = exp(p)
 
-Base.log(::Irrational{:ℯ}) = 1 # use 1 to correctly promote expressions like log(x)/log(ℯ)
+Base.log(::Irrational{:ℯ}) = one(TypeDomainInteger)
 Base.log(::Irrational{:ℯ}, x::Number) = log(x)
 
-Base.sin(::Irrational{:π}) = 0.0
-Base.cos(::Irrational{:π}) = -1.0
-Base.sincos(::Irrational{:π}) = (0.0, -1.0)
-Base.tan(::Irrational{:π}) = 0.0
+Base.sin(::Irrational{:π}) = zero(TypeDomainInteger)
+Base.cos(::Irrational{:π}) = -one(TypeDomainInteger)
+Base.sincos(::Irrational{:π}) = (zero(TypeDomainInteger), -one(TypeDomainInteger))
+Base.tan(::Irrational{:π}) = zero(TypeDomainInteger)
 Base.cot(::Irrational{:π}) = -1/0
 
 end # module
