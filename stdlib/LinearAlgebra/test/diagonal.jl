272f1ba790aec52795b2b331a0b9b84dedf2bbb7
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-08-29 16:37:43 +0530

    fix diag index
    Co-authored-by: Daniel Karrasch <daniel.karrasch@posteo.de>

--- stdlib/LinearAlgebra/test/diagonal.jl
+++ stdlib/LinearAlgebra/test/diagonal.jl
@@ -782,7 +782,7 @@ end
     @test adjoint(Dsym) == Diagonal([[1 1-im; 1-im 1], [1 1-im; 1-im 1]])
     @test transpose(Dsym) == Dsym
     @test diag(D, 0) == diag(D) == [[1 2; 3 4], [1 2; 3 4]]
-    @test diag(D, 1) == diag(D, 1) == [zeros(Int,2,2)]
+    @test diag(D, 1) == diag(D, -1) == [zeros(Int,2,2)]
     @test diag(D, 2) == diag(D, -2) == []
 
     v = [[1, 2], [3, 4]]
