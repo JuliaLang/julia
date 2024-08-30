dcee8f5d518407b5926fd55443aa6f8b6d6fbc7b
Ian Butterworth <i.r.butterworth@gmail.com>
2024-08-30 13:26:54 -0400

    fall back to slower stat filesize if optimized filesize fails

--- base/iostream.jl
+++ base/iostream.jl
@@ -228,7 +228,13 @@ function position(s::IOStream)
 end
 
 function filesize(s::IOStream)
-    sz = @_lock_ios s ccall(:ios_filesize, Int64, (Ptr{Cvoid},), s.ios)
+    sz = try
+        @_lock_ios s ccall(:ios_filesize, Int64, (Ptr{Cvoid},), s.ios)
+    catch e
+        e isa IOError || rethrow()
+        # if `s` is not seekable `ios_filesize` can fail, so fall back to slower stat method
+        stat(s).filesize
+    end
     if sz == -1
         err = Libc.errno()
         throw(IOError(string("filesize: ", Libc.strerror(err), " for ", s.name), err))
