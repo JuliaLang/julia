373c860f8588f0a30b42920ef7e0dda01cc37a01
Zentrik <Zentrik@users.noreply.github.com>
2024-08-31 15:13:35 +0100

    Fix deprecation due to https://github.com/llvm/llvm-project/commit/867faeec054abb4c035673189c1169fef45f54c8

--- src/disasm.cpp
+++ src/disasm.cpp
@@ -920,11 +920,9 @@ static void jl_dump_asm_internal(
     // LLVM will destroy the formatted stream, and we keep the raw stream.
     std::unique_ptr<formatted_raw_ostream> ustream(new formatted_raw_ostream(rstream));
     std::unique_ptr<MCStreamer> Streamer(
-            TheTarget->createAsmStreamer(Ctx, std::move(ustream), /*asmverbose*/true,
-                                         /*useDwarfDirectory*/ true,
-                                         IP.release(),
-                                         std::move(CE), std::move(MAB),
-                                         /*ShowInst*/ false));
+        TheTarget->createAsmStreamer(Ctx, std::move(ustream),
+
+                                     IP.release(), std::move(CE), std::move(MAB)));
     Streamer->initSections(true, *STI);
 
     // Make the MemoryObject wrapper
@@ -1257,10 +1255,7 @@ jl_value_t *jl_dump_function_asm_impl(jl_llvmf_dump_t* dump, char emit_mc, const
             }
             auto FOut = std::make_unique<formatted_raw_ostream>(asmfile);
             std::unique_ptr<MCStreamer> S(TM->getTarget().createAsmStreamer(
-                *Context, std::move(FOut), true,
-                true, InstPrinter,
-                std::move(MCE), std::move(MAB),
-                false));
+                *Context, std::move(FOut), InstPrinter, std::move(MCE), std::move(MAB)));
             std::unique_ptr<AsmPrinter> Printer(
                 TM->getTarget().createAsmPrinter(*TM, std::move(S)));
             Printer->addAsmPrinterHandler(std::unique_ptr<AsmPrinterHandler>(
