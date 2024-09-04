8a0de1d4b1cf13a325005241cae85135709a5a8f
Ian Butterworth <i.r.butterworth@gmail.com>
2024-09-04 10:19:44 -0400

    fix lib dir handling
    Co-Authored-By: Jameson Nash <vtjnash+github@gmail.com>

--- stdlib/Profile/src/Profile.jl
+++ stdlib/Profile/src/Profile.jl
@@ -504,20 +504,21 @@ function flatten(data::Vector, lidict::LineInfoDict)
 end
 
 const SRC_DIR = normpath(joinpath(Sys.BUILD_ROOT_PATH, "src"))
-const LIB_DIR = normpath(joinpath(Sys.BUILD_ROOT_PATH, "usr", "lib"))
 
 # Take a file-system path and try to form a concise representation of it
 # based on the package ecosystem
 function short_path(spath::Symbol, filenamecache::Dict{Symbol, Tuple{String,String,String}})
     return get!(filenamecache, spath) do
         path = Base.fixup_stdlib_path(string(spath))
+        path_norm = normpath(path)
         possible_base_path = normpath(joinpath(Sys.BINDIR, Base.DATAROOTDIR, "julia", "base", path))
-        if startswith(path, SRC_DIR)
-            remainder = only(split(path, SRC_DIR, keepempty=false))
-            return (isfile(path) ? path : ""), "@src", remainder
-        elseif startswith(path, LIB_DIR)
-            remainder = only(split(path, LIB_DIR, keepempty=false))
-            return (isfile(path) ? path : ""), "@lib", remainder
+        lib_dir = abspath(Sys.BINDIR, Base.LIBDIR)
+        if startswith(path_norm, SRC_DIR)
+            remainder = only(split(path_norm, SRC_DIR, keepempty=false))
+            return (isfile(path_norm) ? path_norm : ""), "@juliasrc", remainder
+        elseif startswith(path_norm, lib_dir)
+            remainder = only(split(path_norm, lib_dir, keepempty=false))
+            return (isfile(path_norm) ? path_norm : ""), "@julialib", remainder
         elseif isabspath(path)
             if ispath(path)
                 # try to replace the file-system prefix with a short "@Module" one,
