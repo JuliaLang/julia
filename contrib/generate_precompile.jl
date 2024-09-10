d00891e6345ec5572122c7809f03f5b9ad1a42c5
Ian Butterworth <i.r.butterworth@gmail.com>
2024-09-10 16:21:28 -0400

    also precompile `@time` reports

--- contrib/generate_precompile.jl
+++ contrib/generate_precompile.jl
@@ -176,6 +176,11 @@ for match = Base._methods(+, (Int, Int), -1, Base.get_world_counter())
     # interactive startup uses this
     write(IOBuffer(), "")
 
+    # not critical, but helps hide unrelated compilation from @time when using --trace-compile
+    foo() = rand(2,2) * rand(2,2)
+    @time foo()
+    @time foo()
+
     break   # only actually need to do this once
 end
 """
