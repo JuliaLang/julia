4889804f247dca5695c85f17be98d410cb3736d0
nhz2 <nhz2@cornell.edu>
2024-09-14 21:22:23 -0400

    Fix `unsafe_read` for `IOBuffer` with non dense data

--- base/iobuffer.jl
+++ base/iobuffer.jl
@@ -184,12 +184,31 @@ end
 function unsafe_read(from::GenericIOBuffer, p::Ptr{UInt8}, nb::UInt)
     from.readable || _throw_not_readable()
     avail = bytesavailable(from)
-    adv = min(avail, nb)
-    GC.@preserve from unsafe_copyto!(p, pointer(from.data, from.ptr), adv)
+    adv = UInt(min(avail, nb))
+    unsafe_read!(p, from.data, from.ptr, adv)
     from.ptr += adv
     if nb > avail
         throw(EOFError())
     end
+    nothing
+end
+
+function unsafe_read!(dest::Ptr{UInt8}, src::AbstractVector{UInt8}, so::Integer, nbytes::UInt)
+    dest_array = unsafe_wrap(Array, dest, nbytes)
+    copyto!(dest_array, 1, src, so, nbytes)
+    nothing
+end
+
+# Note: Currently, CodeUnits <: DenseVector, which makes this union redundant w.r.t
+# DenseArrayType{UInt8}, but this is a bug, and may be removed in future versions
+# of Julia. See #54002
+const DenseBytes = Union{
+    <:DenseArrayType{UInt8},
+    CodeUnits{UInt8, <:Union{String, SubString{String}}},
+}
+
+function unsafe_read!(dest::Ptr{UInt8}, src::DenseBytes, so::Integer, nbytes::UInt)
+    GC.@preserve src unsafe_copyto!(dest, pointer(src, so), nbytes)
     nothing
 end
 
