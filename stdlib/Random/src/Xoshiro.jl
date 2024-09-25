5f46099ae4147d67da31326e4173b557dc89a009
Daniel Wennberg <daniel.wennberg@gmail.com>
2024-09-25 05:26:48 -0700

    Update TaskLocalRNG docstring according to #49110 (#55863)
    Since #49110, which is included in 1.10 and 1.11, spawning a task no
    longer advances the parent task's RNG state, so this statement in the
    docs was incorrect.
    
    (cherry picked from commit c601b113be8f81a00711455349a3dfab7755de9d)

--- stdlib/Random/src/Xoshiro.jl
+++ stdlib/Random/src/Xoshiro.jl
@@ -185,8 +185,8 @@ end
     TaskLocalRNG
 
 The `TaskLocalRNG` has state that is local to its task, not its thread.
-It is seeded upon task creation, from the state of its parent task.
-Therefore, task creation is an event that changes the parent's RNG state.
+It is seeded upon task creation, from the state of its parent task, but without
+advancing the state of the parent's RNG.
 
 As an upside, the `TaskLocalRNG` is pretty fast, and permits reproducible
 multithreaded simulations (barring race conditions), independent of scheduler
@@ -203,6 +203,9 @@ may be any integer.
 
 !!! compat "Julia 1.11"
     Seeding `TaskLocalRNG()` with a negative integer seed requires at least Julia 1.11.
+
+!!! compat "Julia 1.10"
+    Task creation no longer advances the parent task's RNG state as of Julia 1.10.
 """
 struct TaskLocalRNG <: AbstractRNG end
 TaskLocalRNG(::Nothing) = TaskLocalRNG()
