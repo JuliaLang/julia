e9970c61b1bf0ab9a1e79715555e75527099518f
Yi Lin <qinsoon@gmail.com>
2024-09-27 20:31:53 +1200

    Update to mmtk-core PR mmtk/mmtk-julia#1205 (mmtk/mmtk-julia#177)
    Co-authored-by: mmtkgc-bot <mmtkgc.bot@gmail.com>

--- src/gc-mmtk/mmtk_julia/src/api.rs
+++ src/gc-mmtk/mmtk_julia/src/api.rs
@@ -271,7 +271,7 @@ pub extern "C" fn mmtk_handle_user_collection_request(tls: VMMutatorThread, coll
         // incremental
         2 => SINGLETON.handle_user_collection_request(tls, true, false),
         _ => unreachable!(),
-    }
+    };
 }
 
 #[no_mangle]
