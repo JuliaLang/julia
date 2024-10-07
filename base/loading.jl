4cdd864e535b16e928c2eff43e6f1583bd77209d
Jameson Nash <vtjnash@gmail.com>
2024-10-07 19:54:51 -0400

    add require_lock call to maybe_loaded_precompile (#56027)
    If we expect this to be a public API
    (https://github.com/timholy/Revise.jl for some reason is trying to
    access this state), we should lock around it for consistency with the
    other similar functions.
    
    Needed for https://github.com/timholy/Revise.jl/pull/856

--- base/loading.jl
+++ base/loading.jl
@@ -2397,11 +2397,12 @@ const module_keys = IdDict{Module,PkgId}() # the reverse of loaded_modules
 root_module_key(m::Module) = @lock require_lock module_keys[m]
 
 function maybe_loaded_precompile(key::PkgId, buildid::UInt128)
-    assert_havelock(require_lock)
+    @lock require_lock begin
     mods = get(loaded_precompiles, key, nothing)
     mods === nothing && return
     for mod in mods
         module_build_id(mod) == buildid && return mod
+    end
     end
 end
 
