60b5bb5ed8eefeaa303d64af5253ec5e22887ec1
Ian Butterworth <i.r.butterworth@gmail.com>
2024-10-20 01:31:59 -0400

    don't complete string and command macros when the input matches the internal name like `r_` to `r"`

--- stdlib/REPL/test/replcompletions.jl
+++ stdlib/REPL/test/replcompletions.jl
@@ -1515,6 +1515,16 @@ end
     @test "testcmd`" in c
     c, r, res = test_complete("CompletionFoo.tϵsτc")
     @test "tϵsτcmδ`" in c
+
+    # Issue #56071: don't complete string and command macros when the input matches the internal name like `r_` to `r"`
+    c, r, res = test_complete("CompletionFoo.teststr_")
+    @test isempty(c)
+    c, r, res = test_complete("CompletionFoo.teststr_s")
+    @test isempty(c)
+    c, r, res = test_complete("CompletionFoo.testcmd_")
+    @test isempty(c)
+    c, r, res = test_complete("CompletionFoo.testcmd_c")
+    @test isempty(c)
 end
 
 @testset "Keyword-argument completion" begin
