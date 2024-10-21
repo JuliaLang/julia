3dd91a1b0e63e42b8247afd4223702d14ec4d188
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2024-10-22 05:23:04 +0900

    inference: fix inference error from constructing invalid `TypeVar` (#56264)
    - fixes JuliaLang/julia#56248
    
    (cherry picked from commit 08d11d041b22fe90380e56be4fb4d44aaf46ec85)

--- test/compiler/inference.jl
+++ test/compiler/inference.jl
@@ -5685,3 +5685,11 @@ t255751 = Array{Float32, 3}
 
 issue55882_nfields(x::Union{T,Nothing}) where T<:Number = nfields(x)
 @test Base.infer_return_type(issue55882_nfields) <: Int
+
+# JuliaLang/julia#56248
+@test Base.infer_return_type() do
+    TypeVar(:Issue56248, 1)
+end === Union{}
+@test Base.infer_return_type() do
+    TypeVar(:Issue56248, Any, 1)
+end === Union{}
