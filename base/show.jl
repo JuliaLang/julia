c94102bea4443c8cab8c78589a96cbcddbea283e
Ian Butterworth <i.r.butterworth@gmail.com>
2024-10-24 22:43:21 -0400

    add docstring note about `displaysize` and `IOContext` with `context` (#55510)

--- base/show.jl
+++ base/show.jl
@@ -339,6 +339,11 @@ end
     IOContext(io::IO, context::IOContext)
 
 Create an `IOContext` that wraps an alternate `IO` but inherits the properties of `context`.
+
+!!! note
+    Unless explicitly set in the wrapped `io` the `displaysize` of `io` will not be inherited.
+    This is because by default `displaysize` is not a property of IO objects themselves, but lazily inferred,
+    as the size of the terminal window can change during the lifetime of the IO object.
 """
 IOContext(io::IO, context::IO) = IOContext(io, ioproperties(context))
 
