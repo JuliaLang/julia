4f06ad09c7f266a43c35c3e10d01d91394e7d9a7
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-11-04 07:28:00 -0600

    restore `lcm(Rational{Int}[]) === 1//1`

--- test/intfuncs.jl
+++ test/intfuncs.jl
@@ -197,7 +197,7 @@ end
     @test gcd(Int[]) === 0
     @test lcm(Int[]) === 1
     @test gcd(Rational{Int}[]) === 0//1
-    @test_throws ArgumentError lcm(Rational{Int}[])
+    @test lcm(Rational{Int}[]) === 1//1
 end
 
 ⟷(a::Tuple{T, T, T}, b::Tuple{T, T, T}) where T <: Union{Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64, UInt64, Int128, UInt128} = a === b
