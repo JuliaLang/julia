93f40c4bb6bf729a3c580765e1a736086f3d6b0b
Micah Rufsvold <mrufsvold@mathematica-mpr.com>
2024-11-06 19:35:58 -0500

    set more reasonable resize bounds

--- base/channels.jl
+++ base/channels.jl
@@ -730,8 +730,9 @@ julia> take!(c, 3, zeros(Int, 3))
     Requires at least Julia 1.12.
 """
 function take!(c::Channel{T}, n::Integer, buffer::AbstractVector = Vector{T}()) where {T}
-    if n < typemax(n)
-        sizehint!(buffer, n)
+    # for small-ish n, we can avoid resizing the buffer as we push elements 
+    if n < 2^16
+        resize!(buffer, n)
     end
 
     buffered = isbuffered(c)
