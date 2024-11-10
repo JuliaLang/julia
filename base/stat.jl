d95e76473156dd8bfda4c188bb0136bb5adeb55c
Dennis Hoelgaard Bal <61620837+KronosTheLate@users.noreply.github.com>
2024-11-10 14:17:11 +0100

    Another trailing whitespace
    Co-authored-by: Lilith Orion Hafner <lilithhafner@gmail.com>

--- base/stat.jl
+++ base/stat.jl
@@ -351,7 +351,7 @@ mtime(st::StatStruct) = st.mtime
     ctime(path_elements...)
     ctime(stat_struct)
 
-Return the unix timestamp of when the metadata of the file at `path` was last modified, 
+Return the unix timestamp of when the metadata of the file at `path` was last modified,
 or the last modified metadata timestamp indicated by the file descriptor `stat_struct`.
 
 Equivalent to `stat(path).ctime` or `stat_struct.ctime`.
