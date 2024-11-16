ed7a9842fd8f26ae7d0a9e40b150dda8c0d1fffc
cschen <camillo.schenone@gmail.com>
2024-11-16 13:21:32 +0100

    removed useless module annotations added nth to export list

--- base/iterators.jl
+++ base/iterators.jl
@@ -32,7 +32,7 @@ import Base:
     getindex, setindex!, get, iterate,
     popfirst!, isdone, peek, intersect
 
-export enumerate, zip, rest, countfrom, take, drop, takewhile, dropwhile, cycle, repeated, product, flatten, flatmap, partition
+export enumerate, zip, rest, countfrom, take, drop, takewhile, dropwhile, cycle, repeated, product, flatten, flatmap, partition, nth
 public accumulate, filter, map, peel, reverse, Stateful
 
 """
@@ -1625,13 +1625,13 @@ _nth(::SizeUnknown, itr, n) = _fallback_nth(itr, n)
 _nth(::Union{HasShape,HasLength}, itr, n) = _withlength_nth(itr, n, length(itr))
 _nth(::IsInfinite, itr, n) = _inbounds_nth(itr, n)
 
-_inbounds_nth(itr, n) = iterate(Base.Iterators.drop(itr, n - 1))[1]
+_inbounds_nth(itr, n) = iterate(drop(itr, n - 1))[1]
 _inbounds_nth(itr::AbstractArray, n) = itr[n]
 
 _withlength_nth(itr, n, N) = n > N ? nothing : _inbounds_nth(itr, n)
 
 function _fallback_nth(itr, n)
-    y = iterate(Iterators.drop(itr, n - 1))
+    y = iterate(drop(itr, n - 1))
     y === nothing && return nothing
     y[1]
 end
@@ -1647,7 +1647,7 @@ nth(itr::Repeated, n::Integer) = itr.x
 nth(itr::Take{Repeated{O}}, n::Integer) where {O} = n > itr.n ? nothing : itr.xs.x
 
 # infinite cycle
-nth(itr::Iterators.Cycle{I}, n::Integer) where {I} = _nth_inf_cycle(IteratorSize(I), itr, n)
+nth(itr::Cycle{I}, n::Integer) where {I} = _nth_inf_cycle(IteratorSize(I), itr, n)
 _nth_inf_cycle(::IsInfinite, itr, n) = _inbounds_nth(itr.xs, n)
 _nth_inf_cycle(::SizeUnknown, itr, n) = _fallback_nth(itr.xs, n)
 _nth_inf_cycle(::Union{HasShape,HasLength}, itr, n) = _repeating_cycle_nth(itr.xs, n, length(itr.xs))
@@ -1664,10 +1664,7 @@ _nth_finite_cycle(::Union{HasShape,HasLength}, itr, n) = begin
     n > K * N && return nothing
     _repeating_cycle_nth(torepeat, n, K)
 end
-# O = [....] # K length
-# Repeat(O) = [ [....] [....] [....] ...
-# Take(Repeat(O), N) = [ [....] [....] [....] ] # N times
-# Flatten(...) = [....|....|....] # K*N elements
+
 
 _repeating_cycle_nth(inner_itr, n, inner_N) = _inbounds_nth(inner_itr, 1 + ((n - 1) % inner_N))
 
