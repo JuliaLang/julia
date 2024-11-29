117e7629e2dd048f879ba85b7456e27800ece4f1
Erik Schnetter <schnetter@gmail.com>
2024-11-28 20:42:37 -0500

    Update sha1 and tests

--- stdlib/LibSSH2_jll/test/runtests.jl
+++ stdlib/LibSSH2_jll/test/runtests.jl
@@ -3,7 +3,5 @@
 using Test, Libdl, LibSSH2_jll
 
 @testset "LibSSH2_jll" begin
-    #TODO # We use a `startswith()` here because when built from source, this returns "1.9.0_DEV"
-    #TODO vn = startswith(unsafe_string(ccall((:libssh2_version, libssh2), Cstring, (Cint,), 0)), "1.9.0")
-    @test VersionNumber(major[], minor[], patch[]) == v"1.11.1"
+    @test VersionNumber(unsafe_string(ccall((:libssh2_version, libssh2), Cstring, (Cint,), 0))) == v"1.11.1"
 end
