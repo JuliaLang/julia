f7af55f7ded12114247ae0d36d1b9459bd6a1302
Erik Schnetter <schnetter@gmail.com>
2024-12-02 12:28:54 -0500

    LibSSH2_jll: Correct version number check

--- stdlib/LibSSH2_jll/test/runtests.jl
+++ stdlib/LibSSH2_jll/test/runtests.jl
@@ -4,5 +4,8 @@ using Test, Libdl, LibSSH2_jll
 
 @testset "LibSSH2_jll" begin
     vn = unsafe_string(ccall((:libssh2_version, libssh2), Cstring, (Cint,), 0))
-    @test vn == "1.11.1_DEV"
+    # Depending on how LibSSH2_jll was installed (downloaded from
+    # BinaryBuilder or built from source here), the version number is
+    # either "1.11.1" or "1.11.1_DEV", respectively.
+    @test startswith(vn, "1.11.1")
 end
