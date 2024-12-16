f5700fcd2bf91d15aec2af770ede3207d02d91fd
Chengyu Han <cyhan.dev@outlook.com>
2024-12-16 21:14:34 +0800

    stdlib: remove OpenLibm_jll

--- stdlib/OpenLibm_jll/test/runtests.jl
+++
@@ -1,7 +0,0 @@
-# This file is a part of Julia. License is MIT: https://julialang.org/license
-
-using Test, Libdl, OpenLibm_jll
-
-@testset "OpenLibm_jll" begin
-    @test ccall((:isopenlibm, libopenlibm), Cint, ()) == 1
-end
