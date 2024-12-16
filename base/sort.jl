0d272b7da396552698e961fc6d67de249d4d8262
Aaron Matthis <rapus95@gmail.com>
2024-12-16 03:25:25 +0100

    fix example

--- base/sort.jl
+++ base/sort.jl
@@ -1700,7 +1700,7 @@ julia> v = [(1, "c"), (3, "a"), (2, "b")]; sort!(v, by = x -> x[2]); v
  (2, "b")
  (1, "c")
 
-julia> sort(0:3, by=x->x-2, order=Base.Order.By(abs)) # same as sort(0:3, by=abs(x->x-2))
+julia> sort(0:3, by=x->x-2, order=Base.Order.By(abs)) # same as sort(0:3, by=x->abs(x-2))
 4-element Vector{Int64}:
  2
  1
