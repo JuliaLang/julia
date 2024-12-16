646c18719deea35a6fc6226e61ce77eb7e347d1a
oscarddssmith <oscar.smith@juliacomputing.com>
2024-12-16 18:22:53 -0500

    optimize constant length Memory

--- src/llvm-alloc-opt.cpp
+++ src/llvm-alloc-opt.cpp
@@ -752,11 +752,10 @@ void Optimizer::moveToStack(CallInst *orig_inst, size_t sz, bool has_ref, AllocF
                 call->eraseFromParent();
                 return;
             }
-            //if (pass.gc_loaded_func == callee) {
-            //    call->replaceAllUsesWith(new_i);
-            //    call->eraseFromParent();
-            //    return;
-            //}
+            if (pass.gc_loaded_func == callee) {
+                user->replaceUsesOfWith(orig_i, Constant::getNullValue(orig_i->getType()));
+                return;
+            }
             if (pass.typeof_func == callee) {
                 ++RemovedTypeofs;
                 call->replaceAllUsesWith(tag);
