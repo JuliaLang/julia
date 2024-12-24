69808de5125775977c9f4ee851bd1177a84c9e0c
Neven Sajko <s@purelymail.com>
2024-12-24 04:30:43 +0100

    don't touch `IteratorEltype`, avoid breaking common assumptions

--- base/generator_eltype.jl
+++ base/generator_eltype.jl
@@ -2,24 +2,12 @@ function eltype(::Type{Generator{A, typeof(identity)}}) where {A}
     eltype(A)
 end
 
-function IteratorEltype(::Type{Generator{A, typeof(identity)}}) where {A}
-    IteratorEltype(A)
-end
-
 function eltype(::Type{Generator{A, Fix1{typeof(getindex), B}}} where {A}) where {B}
     if B <: Type
         # a user may overload `getindex(user_type)` to return a non-`Vector` `AbstractVector`
         Any
     else
         eltype(B)
-    end
-end
-
-function IteratorEltype(::Type{Generator{A, Fix1{typeof(getindex), B}}} where {A}) where {B}
-    if B <: Type
-        EltypeUnknown()
-    else
-        IteratorEltype(B)
     end
 end
 
