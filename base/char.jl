c717418d5d2c01bb0c0c993be33e36d3c29fc665
Steven G. Johnson <stevenj@alum.mit.edu>
2025-01-02 15:49:15 -0500

    not well-formed -> malformed

--- base/char.jl
+++ base/char.jl
@@ -79,8 +79,8 @@ end
     codepoint(c::AbstractChar) -> Integer
 
 Return the Unicode codepoint (an unsigned integer) corresponding
-to the character `c` (or throw an exception if `c` does not represent
-a well-formed character). For `Char`, this is a `UInt32` value, but
+to the character `c` (or throw an exception if `c` represents
+a malformed character). For `Char`, this is a `UInt32` value, but
 `AbstractChar` types that represent only a subset of Unicode may
 return a different-sized integer (e.g. `UInt8`).
 
