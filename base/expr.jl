f04724a369f916af0e3f97ef3c5ddede0352ee0b
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2025-01-05 12:14:52 +0100

    back to `LazyString`
    Co-authored-by: Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>

--- base/expr.jl
+++ base/expr.jl
@@ -408,7 +408,7 @@ function constprop_setting(@nospecialize setting)
     elseif setting === :none
         return :no_constprop
     end
-    throw(ArgumentError("`@constprop $s` not supported"))
+    throw(ArgumentError(LazyString("`Base.@constprop ", setting, "` not supported")))
 end
 
 """
