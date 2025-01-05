bd7d5d6f5019a952cbac166a93b58005444433c4
Mosè Giordano <mose@gnu.org>
2025-01-05 12:41:36 +0100

    [deps] Fix build systems of curl and openssl
    * Curl was missing a dependency on OpenSSL after #56708
    * openssl is missing configuration target for riscv64

--- deps/curl.mk
+++ deps/curl.mk
@@ -1,6 +1,10 @@
 ## CURL ##
 include $(SRCDIR)/curl.version
 
+ifeq ($(USE_SYSTEM_OPENSSL), 0)
+$(BUILDDIR)/curl-$(CURL_VER)/build-configured: | $(build_prefix)/manifest/openssl
+endif
+
 ifeq ($(USE_SYSTEM_LIBSSH2), 0)
 $(BUILDDIR)/curl-$(CURL_VER)/build-configured: | $(build_prefix)/manifest/libssh2
 endif
