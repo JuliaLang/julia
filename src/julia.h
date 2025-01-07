d0fd51d38a06a2bb66bc6d91d0726a66fdeed418
Jeff Bezanson <jeff.bezanson@gmail.com>
2025-01-07 14:54:43 -0500

    Revert "add fenv cache to task struct (#51288)"
    This reverts commit 12aa9dea03a8a6bbe2891d3ca6ce34a21ec84734.

--- src/julia.h
+++ src/julia.h
@@ -20,7 +20,6 @@
 #include "libsupport.h"
 #include <stdint.h>
 #include <string.h>
-#include <fenv.h>
 
 #include "htable.h"
 #include "arraylist.h"
@@ -2314,9 +2313,6 @@ typedef struct _jl_task_t {
     _Atomic(uint64_t) finished_at;
 
 // hidden state:
-    // cached floating point environment
-    // only updated at task switch
-    fenv_t fenv;
 
     // id of owning thread - does not need to be defined until the task runs
     _Atomic(int16_t) tid;
