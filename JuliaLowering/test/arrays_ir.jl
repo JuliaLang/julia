8409b93d0d33db3007ee24d0450201003ea9e095
Claire Foster <aka.c42f@gmail.com>
2025-01-09 13:11:52 +1000

    Simplify arrays_ir test cases using literals
    These don't need separate SSA values so result in more compact IR which
    is easier to read.

--- JuliaLowering/test/arrays_ir.jl
+++ JuliaLowering/test/arrays_ir.jl
@@ -1,151 +1,119 @@
 ########################################
 # vect syntax
-[a, b, c]
+[10, 20, 30]
 #---------------------
-1   TestMod.a
-2   TestMod.b
-3   TestMod.c
-4   (call top.vect %₁ %₂ %₃)
-5   (return %₄)
+1   (call top.vect 10 20 30)
+2   (return %₁)
 
 ########################################
 # Error: vect syntax with parameters
-[a, b; c]
+[10, 20; 30]
 #---------------------
 LoweringError:
-[a, b; c]
-└───────┘ ── unexpected semicolon in array expression
+[10, 20; 30]
+└──────────┘ ── unexpected semicolon in array expression
 
 ########################################
 # Error: vect syntax with embedded assignments
-[a=b, c]
+[a=20, 30]
 #---------------------
 LoweringError:
-[a=b, c]
-#└─┘ ── misplaced assignment statement in `[ ... ]`
+[a=20, 30]
+#└──┘ ── misplaced assignment statement in `[ ... ]`
 
 ########################################
 # hcat syntax
-[a b c]
+[10 20 30]
 #---------------------
-1   TestMod.a
-2   TestMod.b
-3   TestMod.c
-4   (call top.hcat %₁ %₂ %₃)
-5   (return %₄)
+1   (call top.hcat 10 20 30)
+2   (return %₁)
 
 ########################################
 # typed hcat syntax
-T[a b c]
+T[10 20 30]
 #---------------------
 1   TestMod.T
-2   TestMod.a
-3   TestMod.b
-4   TestMod.c
-5   (call top.typed_hcat %₁ %₂ %₃ %₄)
-6   (return %₅)
+2   (call top.typed_hcat %₁ 10 20 30)
+3   (return %₂)
 
 ########################################
 # Error: hcat syntax with embedded assignments
-[a b c=d]
+[10 20 a=40]
 #---------------------
 LoweringError:
-[a b c=d]
-#   └──┘ ── misplaced assignment statement in `[ ... ]`
+[10 20 a=40]
+#     └───┘ ── misplaced assignment statement in `[ ... ]`
 
 ########################################
 # vcat syntax
-[a; b; c]
+[10; 20; 30]
 #---------------------
-1   TestMod.a
-2   TestMod.b
-3   TestMod.c
-4   (call top.vcat %₁ %₂ %₃)
-5   (return %₄)
+1   (call top.vcat 10 20 30)
+2   (return %₁)
 
 ########################################
 # vcat with splats
-[a...; b; c]
+[a...; 20; 30]
 #---------------------
 1   TestMod.a
-2   TestMod.b
-3   TestMod.c
-4   (call core.tuple %₂ %₃)
-5   (call core._apply_iterate top.iterate top.vcat %₁ %₄)
-6   (return %₅)
+2   (call core.tuple 20 30)
+3   (call core._apply_iterate top.iterate top.vcat %₁ %₂)
+4   (return %₃)
 
 ########################################
 # hvcat syntax
-[a; b c; d e f]
+[10; 20 30; 40 e f]
 #---------------------
 1   (call core.tuple 1 2 3)
-2   TestMod.a
-3   TestMod.b
-4   TestMod.c
-5   TestMod.d
-6   TestMod.e
-7   TestMod.f
-8   (call top.hvcat %₁ %₂ %₃ %₄ %₅ %₆ %₇)
-9   (return %₈)
+2   TestMod.e
+3   TestMod.f
+4   (call top.hvcat %₁ 10 20 30 40 %₂ %₃)
+5   (return %₄)
 
 ########################################
 # hvcat with splats nested within rows
-[a; b c...]
+[10; 20 a...]
 #---------------------
-1   TestMod.a
-2   (call core.tuple %₁)
-3   TestMod.b
-4   (call core.tuple %₃)
-5   TestMod.c
-6   (call core._apply_iterate top.iterate core.tuple %₄ %₅)
-7   (call top.hvcat_rows %₂ %₆)
-8   (return %₇)
-
-########################################
-# Error: vcat syntax with assignments
-[a=b; c]
-#---------------------
-LoweringError:
-[a=b; c]
-#└─┘ ── misplaced assignment statement in `[ ... ]`
-
-########################################
-# typed_vcat syntax
-T[a; b; c]
-#---------------------
-1   TestMod.T
-2   TestMod.a
-3   TestMod.b
-4   TestMod.c
-5   (call top.typed_vcat %₁ %₂ %₃ %₄)
+1   (call core.tuple 10)
+2   (call core.tuple 20)
+3   TestMod.a
+4   (call core._apply_iterate top.iterate core.tuple %₂ %₃)
+5   (call top.hvcat_rows %₁ %₄)
 6   (return %₅)
 
 ########################################
+# Error: vcat syntax with assignments
+[a=20; 30]
+#---------------------
+LoweringError:
+[a=20; 30]
+#└──┘ ── misplaced assignment statement in `[ ... ]`
+
+########################################
+# typed_vcat syntax
+T[10; 20; 30]
+#---------------------
+1   TestMod.T
+2   (call top.typed_vcat %₁ 10 20 30)
+3   (return %₂)
+
+########################################
 # typed_hvcat syntax
-T[a; b c; d e f]
+T[10; 20 30; 40 50 60]
 #---------------------
 1   TestMod.T
 2   (call core.tuple 1 2 3)
-3   TestMod.a
-4   TestMod.b
-5   TestMod.c
-6   TestMod.d
-7   TestMod.e
-8   TestMod.f
-9   (call top.typed_hvcat %₁ %₂ %₃ %₄ %₅ %₆ %₇ %₈)
-10  (return %₉)
+3   (call top.typed_hvcat %₁ %₂ 10 20 30 40 50 60)
+4   (return %₃)
 
 ########################################
 # typed_hvcat with splats nested within rows
-T[a; b c...]
+T[10; 20 a...]
 #---------------------
 1   TestMod.T
-2   TestMod.a
-3   (call core.tuple %₂)
-4   TestMod.b
-5   (call core.tuple %₄)
-6   TestMod.c
-7   (call core._apply_iterate top.iterate core.tuple %₅ %₆)
-8   (call top.typed_hvcat_rows %₁ %₃ %₇)
-9   (return %₈)
-
+2   (call core.tuple 10)
+3   (call core.tuple 20)
+4   TestMod.a
+5   (call core._apply_iterate top.iterate core.tuple %₃ %₄)
+6   (call top.typed_hvcat_rows %₁ %₂ %₅)
+7   (return %₆)
