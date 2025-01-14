1f05953fab1e14e1e881a224d8719d2c7d637da3
Jameson Nash <vtjnash@gmail.com>
2025-01-14 10:33:09 -0500

    linux: workaround to avoid deadlock inside dl_iterate_phdr in glibc (#57035)
    Extend the fix for #43578 on Darwin to also cover the same bug in Glibc
    (and just assume other libc have the same bug). We cannot use the same
    atfork trick, since the atfork implementation of this in Glibc makes
    this unsafe to use this after fork, just like Darwin (though for
    different basic concurrency mistakes in each of their respective codes).
    
    Fix #57017

--- src/julia_internal.h
+++ src/julia_internal.h
@@ -208,8 +208,7 @@ JL_DLLEXPORT void jl_lock_profile(void) JL_NOTSAFEPOINT JL_NOTSAFEPOINT_ENTER;
 JL_DLLEXPORT void jl_unlock_profile(void) JL_NOTSAFEPOINT JL_NOTSAFEPOINT_LEAVE;
 JL_DLLEXPORT void jl_lock_profile_wr(void) JL_NOTSAFEPOINT JL_NOTSAFEPOINT_ENTER;
 JL_DLLEXPORT void jl_unlock_profile_wr(void) JL_NOTSAFEPOINT JL_NOTSAFEPOINT_LEAVE;
-int jl_lock_stackwalk(void) JL_NOTSAFEPOINT JL_NOTSAFEPOINT_ENTER;
-void jl_unlock_stackwalk(int lockret) JL_NOTSAFEPOINT JL_NOTSAFEPOINT_LEAVE;
+void jl_with_stackwalk_lock(void (*f)(void*) JL_NOTSAFEPOINT, void *ctx) JL_NOTSAFEPOINT;
 
 arraylist_t *jl_get_all_tasks_arraylist(void) JL_NOTSAFEPOINT;
 typedef struct {
