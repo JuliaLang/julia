87fc9ff239b7a6805a746957e6a78d5bbbc93421
Chengyu Han <cyhan.dev@outlook.com>
2025-01-18 15:49:42 +0800

    deps: use libmsvcrt.a fomr i686-12.2.0 toolchain

--- deps/csl.mk
+++ deps/csl.mk
@@ -133,6 +133,7 @@ ifeq ($(ARCH),x86_64)
 else
 # For i686 we are using niXman/mingw-builds-binaries, i686-12.2.0-release-posix-sjlj-rt_v10-rev0.7z
 #	https://github.com/JuliaCI/rootfs-images/blob/e7be8086ff1285b0f333ca72884977d01aed0e1f/windows/package-i686/Dockerfile#L28C107-L28C152
+	-cp -a /mingw32/i686-w64-mingw32/lib/libmsvcrt.a	$(build_private_libdir)/
 	-cp -a /mingw32/i686-w64-mingw32/lib/libmingw32.a	$(build_private_libdir)/
 	-cp -a /mingw32/i686-w64-mingw32/lib/libmingwex.a	$(build_private_libdir)/
 endif # ifeq ($(ARCH),x86_64)
