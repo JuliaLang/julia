36020d92d48c4ec63ca97198c8d92ee2459f699d
gbaraldi <baraldigabriel@gmail.com>
2025-01-22 16:58:46 -0300

    Add test for ccallable function called by ccallable function.

--- test/trimming/hello.jl
+++ test/trimming/hello.jl
@@ -1,6 +1,13 @@
 module MyApp
+
+Base.@ccallable function foo()::Cint
+    Cint(1)
+end
+
 Base.@ccallable function main()::Cint
     println(Core.stdout, "Hello, world!")
+    u = foo()
+    println(Core.stdout, u)
     return 0
 end
 end
