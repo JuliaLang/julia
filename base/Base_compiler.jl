780d041ebe5e3a556e5a66d08df698a7d0f0222c
Steven G. Johnson <stevenj@alum.mit.edu>
2025-01-22 08:46:04 -0500

    mod 2⁶⁴

--- base/Base_compiler.jl
+++ base/Base_compiler.jl
@@ -160,7 +160,7 @@ end
 
 Get the time in nanoseconds relative to some machine-specific arbitrary time in the past.
 The primary use is for measuring elapsed times during program execution.  The return value is guaranteed to
-be monotonic while the system is running, and is unaffected by clock drift or changes to local calendar time,
+be monotonic (mod 2⁶⁴) while the system is running, and is unaffected by clock drift or changes to local calendar time,
 but it may change arbitrarily across system reboots or suspensions.
 
 (Although the returned time is always in nanoseconds, the timing resolution is platform-dependent.)
