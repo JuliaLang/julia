ac622466d0292c64bd93a975039da92e0b5b244d
Steven G. Johnson <stevenj@alum.mit.edu>
2025-01-30 10:59:39 -0500

    Update base/Base_compiler.jl
    Co-authored-by: Kristoffer Carlsson <kcarlsson89@gmail.com>

--- base/Base_compiler.jl
+++ base/Base_compiler.jl
@@ -159,7 +159,7 @@ end
     time_ns() -> UInt64
 
 Get the time in nanoseconds relative to some machine-specific arbitrary time in the past.
-The primary use is for measuring elapsed times during program execution.  The return value is guaranteed to
+The primary use is for measuring elapsed times during program execution. The return value is guaranteed to
 be monotonic (mod 2⁶⁴) while the system is running, and is unaffected by clock drift or changes to local calendar time,
 but it may change arbitrarily across system reboots or suspensions.
 
