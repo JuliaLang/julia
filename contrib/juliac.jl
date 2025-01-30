23b45cf047dec77905612ea5f4c5fb731b0a471b
Cody Tapscott <topolarity@tapscott.me>
2025-01-29 20:36:25 -0500

    contrib: add `--relative-rpath` option to `juliac.jl`
    Also avoid calling Julia as a separate process just to query
    cflags, etc. preferring to import `julia-config.jl` as a module.
    
    Co-authored-by: Gabriel Baraldi <baraldigabriel@gmail.com>

--- contrib/juliac.jl
+++ contrib/juliac.jl
@@ -1,12 +1,17 @@
 # Julia compiler wrapper script
 # NOTE: The interface and location of this script are considered unstable/experimental
 
+module JuliaConfig
+    include(joinpath(@__DIR__, "julia-config.jl"))
+end
+
 cmd = Base.julia_cmd()
 cmd = `$cmd --startup-file=no --history-file=no`
 output_type = nothing  # exe, sharedlib, sysimage
 outname = nothing
 file = nothing
 add_ccallables = false
+relative_rpath = false
 verbose = false
 
 help = findfirst(x->x == "--help", ARGS)
@@ -16,6 +21,7 @@ if help !== nothing
         Usage: julia juliac.jl [--output-exe | --output-lib | --output-sysimage] <name> [options] <file.jl>
         --experimental --trim=<no,safe,unsafe,unsafe-warn>  Only output code statically determined to be reachable
         --compile-ccallable  Include all methods marked `@ccallable` in output
+        --relative-rpath     Configure the library / executable to lookup all required libraries in an adjacent "julia/" folder
         --verbose            Request verbose output
         """)
     exit(0)
@@ -37,6 +43,8 @@ let i = 1
             global add_ccallables = true
         elseif arg == "--verbose"
             global verbose = true
+        elseif arg == "--relative-rpath"
+            global relative_rpath = true
         elseif startswith(arg, "--trim") || arg == "--experimental"
             # forwarded args
             push!(julia_args, arg)
@@ -54,11 +62,27 @@ end
 isnothing(outname) && error("No output file specified")
 isnothing(file) && error("No input file specified")
 
+function get_rpath(; relative::Bool = false)
+    if relative
+        if Sys.isapple()
+            return "-Wl,-rpath,'@loader_path/julia/' -Wl,-rpath,'@loader_path/'"
+        elseif Sys.islinux()
+            return "-Wl,-rpath,'\$ORIGIN/julia/' -Wl,-rpath,'\$ORIGIN/'"
+        else
+            error("unimplemented")
+        end
+    else
+        return JuliaConfig.ldrpath()
+    end
+end
+
 absfile = abspath(file)
-cflags = readchomp(`$(cmd) $(joinpath(Sys.BINDIR, Base.DATAROOTDIR,"julia", "julia-config.jl")) --cflags `)
+cflags = JuliaConfig.cflags(; framework=false)
 cflags = Base.shell_split(cflags)
-allflags = readchomp(`$(cmd) $(joinpath(Sys.BINDIR, Base.DATAROOTDIR,"julia", "julia-config.jl")) --allflags`)
+allflags = JuliaConfig.allflags(; framework=false, rpath=false)
 allflags = Base.shell_split(allflags)
+rpath = get_rpath(; relative = relative_rpath)
+rpath = Base.shell_split(rpath)
 tmpdir = mktempdir(cleanup=false)
 initsrc_path = joinpath(tmpdir, "init.c")
 init_path = joinpath(tmpdir, "init.a")
@@ -96,13 +120,16 @@ end
 
 julia_libs = Base.shell_split(Base.isdebugbuild() ? "-ljulia-debug -ljulia-internal-debug" : "-ljulia -ljulia-internal")
 try
+    cmd2 = nothing
     if output_type == "--output-lib"
-        run(`cc $(allflags) -o $outname -shared -Wl,$(Base.Linking.WHOLE_ARCHIVE) $img_path  -Wl,$(Base.Linking.NO_WHOLE_ARCHIVE) $init_path  $(julia_libs)`)
+        cmd2 = `cc $(allflags) $(rpath) -o $outname -shared -Wl,$(Base.Linking.WHOLE_ARCHIVE) $img_path  -Wl,$(Base.Linking.NO_WHOLE_ARCHIVE) $init_path  $(julia_libs)`
     elseif output_type == "--output-sysimage"
-        run(`cc $(allflags) -o $outname -shared -Wl,$(Base.Linking.WHOLE_ARCHIVE) $img_path  -Wl,$(Base.Linking.NO_WHOLE_ARCHIVE)             $(julia_libs)`)
+        cmd2 = `cc $(allflags) $(rpath) -o $outname -shared -Wl,$(Base.Linking.WHOLE_ARCHIVE) $img_path  -Wl,$(Base.Linking.NO_WHOLE_ARCHIVE)             $(julia_libs)`
     else
-        run(`cc $(allflags) -o $outname -Wl,$(Base.Linking.WHOLE_ARCHIVE) $img_path -Wl,$(Base.Linking.NO_WHOLE_ARCHIVE) $init_path $(julia_libs)`)
+        cmd2 = `cc $(allflags) $(rpath) -o $outname -Wl,$(Base.Linking.WHOLE_ARCHIVE) $img_path -Wl,$(Base.Linking.NO_WHOLE_ARCHIVE) $init_path $(julia_libs)`
     end
+    verbose && println("Running: $cmd2")
+    run(cmd2)
 catch
     println("\nCompilation failed.")
     exit(1)
