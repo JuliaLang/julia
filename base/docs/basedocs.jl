28f72f924b5b4b267ed0750c8945c38086122ce8
Priynsh <119518987+Priynsh@users.noreply.github.com>
2025-02-06 18:30:39 +0530

    Update basedocs.jl

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -2849,6 +2849,10 @@ Construct a `GenericMemoryRef` from a memory object and an offset index (1-based
 can also be negative. This always returns an inbounds object, and will throw an
 error if that is not possible (because the index would result in a shift
 out-of-bounds of the underlying memory).
+
+# See also
+- [`Core.memoryrefoffset`](@ref) – Retrieves the offset of a `MemoryRef` within its parent memory.
+- [`length(::GenericMemoryRef)`](@ref) – Gets the total number of elements in a `MemoryRef`.
 """
 memoryref(::Union{GenericMemory,GenericMemoryRef}, ::Integer)
 
