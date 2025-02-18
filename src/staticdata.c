de19930e76013f5fbd686aeca4a8f6eab879a345
Keno Fischer <keno@juliahub.com>
2025-02-18 00:39:35 +0000

    bpart: Also partition ->deprecated
    This repeats the exercise in #57405. This is required for correctness,
    because the ->deprecated flag also affects `using` resolution (since
    it makes the tagged binding weaker for `using` purposes). That said,
    in general our `->deprecated` semantics have been somewhat underspecified
    with lots of `XXX` comments in the surrounding code. This tries to define
    the semantics to give a depwarn on *access* (read or write) when:
    
    1. Either the binding itself is deprecated; or
    2. The implicit imports pass through a deprecated binding.
    
    However, we do not give depwarns on access to bindings that were explicitly
    imported (although we do give those warnings on the import) - the reasoning
    being that it's the import that needs to be adjusted not the access.
    
    Additionally, this PR moves into the direction of making the depwarn a
    semantic part of the global access, by adjusting codegen and inference
    appropriately.

--- src/staticdata.c
+++ src/staticdata.c
@@ -3554,10 +3554,10 @@ static int jl_validate_binding_partition(jl_binding_t *b, jl_binding_partition_t
     if (jl_atomic_load_relaxed(&bpart->max_world) != ~(size_t)0)
         return 1;
     size_t raw_kind = bpart->kind;
-    enum jl_partition_kind kind = (enum jl_partition_kind)(raw_kind & 0x0f);
+    enum jl_partition_kind kind = (enum jl_partition_kind)(raw_kind & BINDING_KIND_MASK);
     if (!unchanged_implicit && jl_bkind_is_some_implicit(kind)) {
         jl_check_new_binding_implicit(bpart, b, NULL, jl_atomic_load_relaxed(&jl_world_counter));
-        bpart->kind |= (raw_kind & 0xf0);
+        bpart->kind |= (raw_kind & BINDING_FLAG_MASK);
         if (bpart->min_world > jl_require_world)
             goto invalidated;
     }
