b6ab310c85406f4b1f157021de98b0e583d958fc
Ajinkya Kokandakar <ajinkyakokandakar@gmail.com>
2025-02-18 10:27:35 -0600

    using mingw env variable for the lib path in csl.mk instead of  hardcoded mingw lib path

--- deps/csl.mk
+++ deps/csl.mk
@@ -125,11 +125,7 @@ install-csl:
 	cp -a $(build_libdir)/gcc/$(BB_TRIPLET)/$(GCC_VERSION)/libmsvcrt.a $(build_private_libdir)/
 	cp -a $(build_libdir)/gcc/$(BB_TRIPLET)/$(GCC_VERSION)/libssp.dll.a $(build_private_libdir)/
 	cp -a $(build_libdir)/gcc/$(BB_TRIPLET)/$(GCC_VERSION)/libssp.dll.a $(build_libdir)/
-ifeq ($(ARCH),x86_64)
-	-cp -a /mingw64/lib/libmsvcrt.a  $(build_private_libdir)/
-else
-	-cp -a /mingw32/i686-w64-mingw32/lib/libmsvcrt.a  $(build_private_libdir)/
-endif # ifeq ($(ARCH),x86_64)
+	-cp -a $(MINGW_PREFIX)/lib/libmsvcrt.a  $(build_private_libdir)/
 endif
 endif
 ifeq ($(OS),WINNT)
