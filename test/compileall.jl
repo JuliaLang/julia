4170a4a32f6a34f7a7491e9dc630742b2abb70b4
Gabriel Baraldi <baraldigabriel@gmail.com>
2025-02-19 13:19:00 -0300

    Change logic slightly

--- test/compileall.jl
+++ test/compileall.jl
@@ -4,9 +4,9 @@
 
 
 
-if !Sys.iswindows() #Windows doesn't support large images
+if !Sys.iswindows() && !(Sys.WORD_SIZE == 32) #Windows doesn't support large images
     mktempdir() do dir
-        @test success(pipeline(`$(Base.julia_cmd()) --compile=all --strip-ir --output-o $(dir)/sys.o.a -e 'const ballast = Memory{UInt8}(undef, 1 << 31); exit()'`, stderr=stderr)) skip=(Sys.WORD_SIZE == 32)
+        @test success(pipeline(`$(Base.julia_cmd()) --compile=all --strip-ir --output-o $(dir)/sys.o.a -e 'const ballast = Memory{UInt8}(undef, 1 << 31); exit()'`, stderr=stderr))
         if isfile(joinpath(dir, "sys.o.a"))
             Base.Linking.link_image(joinpath(dir, "sys.o.a"), joinpath(dir, "sys.so"))
             @test success(`$(Base.julia_cmd()) -J $(dir)/sys.so -e 'Base.scrub_repl_backtrace(nothing); exit()'`)
