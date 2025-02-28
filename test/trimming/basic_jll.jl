1bc82e24874b4e2efd01a17b8ffccaf893f69f76
Cody Tapscott <topolarity@tapscott.me>
2025-02-28 15:30:52 -0500

    trimming: Add basic JLL test
    This gets us JLL coverage in our trimming tests.
    
    This test project should be re-worked to actually use `juliac.jl`
    instead of re-implementing it out-of-place, and also to make it easier
    to add additional tests in the future.

---
+++ test/trimming/basic_jll.jl
@@ -0,0 +1,14 @@
+module MyApp
+
+using Libdl
+using Zstd_jll
+
+Base.@ccallable function main()::Cint
+    println(Core.stdout, "Julia! Hello, world!")
+    fptr = dlsym(Zstd_jll.libzstd_handle, :ZSTD_versionString)
+    println(Core.stdout, unsafe_string(ccall(fptr, Cstring, ())))
+    println(Core.stdout, unsafe_string(ccall((:ZSTD_versionString, libzstd), Cstring, ())))
+    return 0
+end
+
+end
