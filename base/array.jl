358337b72a88ad483e62eb65e9d01c53900abb53
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2025-03-11 12:17:35 +0100

    `Base`: `append!`, `resize!`: convert length to `Int` before passing it on (#57585)
    Reduces the number of invalidations from 512 to 505 on running this
    code:
    
    ```julia
    struct I <: Integer end
    Base.Int(::I) = 7
    ```
    
    (cherry picked from commit a97137ee079de308599c72e1ed04154dbd3ffc50)

--- base/array.jl
+++ base/array.jl
@@ -1320,7 +1320,7 @@ function append! end
 
 function append!(a::Vector{T}, items::Union{AbstractVector{<:T},Tuple}) where T
     items isa Tuple && (items = map(x -> convert(T, x), items))
-    n = length(items)
+    n = Int(length(items))::Int
     _growend!(a, n)
     copyto!(a, length(a)-n+1, items, firstindex(items), n)
     return a
@@ -1444,7 +1444,8 @@ julia> a[1:6]
  1
 ```
 """
-function resize!(a::Vector, nl::Integer)
+function resize!(a::Vector, nl_::Integer)
+    nl = Int(nl_)::Int
     l = length(a)
     if nl > l
         _growend!(a, nl-l)
