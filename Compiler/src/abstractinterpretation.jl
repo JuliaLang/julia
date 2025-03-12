ea40196a6f6ba92abad47532f00d9ade151c2146
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2025-03-12 09:42:15 +0100

    Update Compiler/src/abstractinterpretation.jl

--- Compiler/src/abstractinterpretation.jl
+++ Compiler/src/abstractinterpretation.jl
@@ -2622,7 +2622,7 @@ function abstract_call_known(interp::AbstractInterpreter, @nospecialize(f),
         arginfo::ArgInfo, si::StmtInfo, sv::AbsIntState,
         max_methods::Int = get_max_methods(interp, f, sv))
     (; fargs, argtypes) = arginfo
-    argtypes::Vector{Any} = arginfo.argtypes  # declare type because the closure below captures it
+    argtypes::Vector{Any} = arginfo.argtypes  # declare type because the closure below captures `argtypes`
     fargs = arginfo.fargs
     la = length(argtypes)
     𝕃ᵢ = typeinf_lattice(interp)
