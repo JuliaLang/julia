b4640b27c29cb22510c92ea1b02b151aa0374ee8
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2025-03-21 10:00:31 +0100

    Error when calling `write(x::IOBuffer, x)`
    Since it needs to write all its content, this would cause an infinite loop,
    exhausing all memory. Throw an error instead.

--- base/iobuffer.jl
+++ base/iobuffer.jl
@@ -743,25 +743,7 @@ function write(to::IO, from::GenericIOBuffer)
     # If they're the same buffer, we need to special case it since the buffer
     # is being mutated twice in the write call.
     if to === from
-        if from.append
-            # If from.append, we copy data to from.size + 1.
-            # Note that this ensureroom might switch the data buffer, or update
-            # the fields like from.size
-            ensureroom(from, available)
-            data = from.data
-            size = from.size
-            existing_space = lastindex(data) - min(from.maxsize, size)
-            to_write = min(existing_space, available)
-            iszero(to_write) && return 0
-            GC.@preserve from unsafe_copyto!(data, size + 1, data, from.ptr, to_write)
-            from.size = size + to_write
-            from.ptr += to_write
-            return to_write
-        else
-            # Else, we copy data to itself, meaning we don't need to copy the data at all.
-            from.ptr = from.size + 1
-            return available
-        end
+        throw(ArgumentError("Writing all content fron an IOBuffer into itself in invalid"))
     else
         written = GC.@preserve from unsafe_write(to, pointer(from.data, from.ptr), UInt(available))
         from.ptr += written
