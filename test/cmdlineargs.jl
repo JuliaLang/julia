7c1559a687b4f91fc7f0b83d5cce4cdc2640989a
prashantrahul141 <prashantrahul141@protonmail.com>
2025-03-27 17:00:24 +0530

    add: Base.julia_cmd test for --quiet flag
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- test/cmdlineargs.jl
+++ test/cmdlineargs.jl
@@ -140,6 +140,8 @@ end
                             ("--banner=no", false),
                             ("--banner=yes", false),
                             ("--banner=short", false),
+
+                            ("--quiet", false),
                         )
         @testset "$arg" begin
             str = arg isa Cmd ? join(arg.exec, ' ') : arg
