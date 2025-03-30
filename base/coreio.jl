93da645175da7551ee88b7261ca1b95f7cfa2de3
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2025-03-30 10:52:40 +0200

    Set `isreadable(::DevNull) = true`, expand `isreadable` docstring
    Devnull is clearly readable, since it's an open, readable but EOF IO object,
    so the previous implementation must have been buggy. Fix it.
    Also, expand on what `isreadable` means.

--- base/coreio.jl
+++ base/coreio.jl
@@ -20,15 +20,17 @@ closewrite(::DevNull) = nothing
 close(::DevNull) = nothing
 wait_close(::DevNull) = wait()
 bytesavailable(io::DevNull) = 0
+isreadable(::DevNull) = true
 
 let CoreIO = Union{Core.CoreSTDOUT, Core.CoreSTDERR}
     global write(io::CoreIO, x::UInt8) = Core.write(io, x)
     global unsafe_write(io::CoreIO, x::Ptr{UInt8}, nb::UInt) = Core.unsafe_write(io, x, nb)
+    global isreadable(::CoreIO) = false
+
 
     CoreIO = Union{CoreIO, DevNull}
     global read(::CoreIO, ::Type{UInt8}) = throw(EOFError())
     global isopen(::CoreIO) = true
-    global isreadable(::CoreIO) = false
     global iswritable(::CoreIO) = true
     global flush(::CoreIO) = nothing
     global eof(::CoreIO) = true
