843ab2f653eb3285e4ac99cd1b2c208d5a486e02
Kristoffer <kcarlsson89@gmail.com>
2025-04-03 15:27:12 +0200

    Revert "Profile.print: Make paths clickable"

--- stdlib/Profile/src/Profile.jl
+++ stdlib/Profile/src/Profile.jl
@@ -38,8 +38,6 @@ public clear,
     Allocs
 
 import Base.StackTraces: lookup, UNKNOWN, show_spec_linfo, StackFrame
-import Base: AnnotatedString
-using StyledStrings: @styled_str
 
 const nmeta = 4 # number of metadata fields per block (threadid, taskid, cpu_cycle_clock, thread_sleeping)
 
@@ -89,10 +87,10 @@ end
 
 # An internal function called to show the report after an information request (SIGINFO or SIGUSR1).
 function _peek_report()
-    iob = Base.AnnotatedIOBuffer()
+    iob = IOBuffer()
     ioc = IOContext(IOContext(iob, stderr), :displaysize=>displaysize(stderr))
     print(ioc, groupby = [:thread, :task])
-    Base.print(stderr, read(seekstart(iob), AnnotatedString))
+    Base.print(stderr, String(take!(iob)))
 end
 # This is a ref so that it can be overridden by other profile info consumers.
 const peek_report = Ref{Function}(_peek_report)
@@ -853,40 +851,6 @@ function flat(io::IO, data::Vector{UInt64}, lidict::Union{LineInfoDict, LineInfo
     return false
 end
 
-# make a terminal-clickable link to the file and linenum.
-# Similar to `define_default_editors` in `Base.Filesystem` but for creating URIs not commands
-function editor_link(path::String, linenum::Int)
-    # Note: the editor path can include spaces (if escaped) and flags.
-    editor = nothing
-    for var in ["JULIA_EDITOR", "VISUAL", "EDITOR"]
-        str = get(ENV, var, nothing)
-        str isa String || continue
-        editor = str
-        break
-    end
-    path_encoded = Base.Filesystem.encode_uri_component(path)
-    if editor !== nothing
-        if editor == "code"
-            return "vscode://file/$path_encoded:$linenum"
-        elseif editor == "subl" || editor == "sublime_text"
-            return "subl://open?url=file://$path_encoded&line=$linenum"
-        elseif editor == "idea" || occursin("idea", editor)
-            return "idea://open?file=$path_encoded&line=$linenum"
-        elseif editor == "pycharm"
-            return "pycharm://open?file=$path_encoded&line=$linenum"
-        elseif editor == "atom"
-            return "atom://core/open/file?filename=$path_encoded&line=$linenum"
-        elseif editor == "emacsclient" || editor == "emacs"
-            return "emacs://open?file=$path_encoded&line=$linenum"
-        elseif editor == "vim" || editor == "nvim"
-            # Note: Vim/Nvim may not support standard URI schemes without specific plugins
-            return "vim://open?file=$path_encoded&line=$linenum"
-        end
-    end
-    # fallback to generic URI, but line numbers are not supported by generic URI
-    return Base.Filesystem.uripath(path)
-end
-
 function print_flat(io::IO, lilist::Vector{StackFrame},
         n::Vector{Int}, m::Vector{Int},
         cols::Int, filenamemap::FileNameMap,
@@ -950,12 +914,7 @@ function print_flat(io::IO, lilist::Vector{StackFrame},
                 Base.print(io, slash)
                 wpad -= 1
             end
-            if isempty(path)
-                Base.print(io, rpad(file_trunc, wpad, " "))
-            else
-                link = editor_link(path, li.line)
-                Base.print(io, rpad(styled"{link=$link:$file_trunc}", wpad, " "))
-            end
+            Base.print(io, rpad(file_trunc, wpad, " "))
             Base.print(io, lpad(li.line > 0 ? string(li.line) : "?", wline, " "), " ")
             fname = funcnames[i]
             if !li.from_c && li.linfo !== nothing
@@ -1003,7 +962,6 @@ end
 
 # mimics Stacktraces
 const PACKAGE_FIXEDCOLORS = Dict{String, Any}("@Base" => :gray, "@Core" => :gray)
-
 function tree_format(frames::Vector{<:StackFrameTree}, level::Int, cols::Int, maxes, filenamemap::FileNameMap, showpointer::Bool)
     nindent = min(cols>>1, level)
     ndigoverhead = ndigits(maxes.overhead)
@@ -1011,7 +969,7 @@ function tree_format(frames::Vector{<:StackFrameTree}, level::Int, cols::Int, ma
     ndigline = ndigits(maximum(frame.frame.line for frame in frames)) + 6
     ntext = max(30, cols - ndigoverhead - nindent - ndigcounts - ndigline - 6)
     widthfile = 2*ntext÷5 # min 12
-    strs = Vector{AnnotatedString{String}}(undef, length(frames))
+    strs = Vector{String}(undef, length(frames))
     showextra = false
     if level > nindent
         nextra = level - nindent
@@ -1047,21 +1005,12 @@ function tree_format(frames::Vector{<:StackFrameTree}, level::Int, cols::Int, ma
                         " ",
                         fname)
                 end
-                pkgcolor = get!(() -> popfirst!(Base.STACKTRACE_MODULECOLORS), PACKAGE_FIXEDCOLORS, pkgname)
-                remaining_path = ltruncate(filename, max(1, widthfile - textwidth(pkgname) - 1))
-                linenum = li.line == -1 ? "?" : string(li.line)
-                _slash = (!isempty(pkgname) && !startswith(remaining_path, slash)) ? slash : ""
-                styled_path = styled"{$pkgcolor:$pkgname}$(_slash)$remaining_path:$linenum"
-                rich_file = if isempty(path)
-                    styled_path
-                else
-                    link = editor_link(path, li.line)
-                    styled"{link=$link:$styled_path}"
-                end
-                strs[i] = Base.annotatedstring(stroverhead, "╎", base, strcount, " ", rich_file, "  ", fname)
-                if frame.overhead > 0
-                    strs[i] = styled"{bold:$(strs[i])}"
-                end
+                strs[i] = string(stroverhead, "╎", base, strcount, " ",
+                    rtruncto(filename, widthfile),
+                    ":",
+                    li.line == -1 ? "?" : string(li.line),
+                    "; ",
+                    fname)
             end
         else
             strs[i] = string(stroverhead, "╎", base, strcount, " [unknown stackframe]")
@@ -1230,7 +1179,7 @@ end
 function print_tree(io::IO, bt::StackFrameTree{T}, cols::Int, fmt::ProfileFormat, is_subsection::Bool) where T
     maxes = maxstats(bt)
     filenamemap = FileNameMap()
-    worklist = [(bt, 0, 0, AnnotatedString(""))]
+    worklist = [(bt, 0, 0, "")]
     if !is_subsection
         Base.print(io, "Overhead ╎ [+additional indent] Count File:Line  Function\n")
         Base.print(io, "=========================================================\n")
@@ -1263,7 +1212,7 @@ function print_tree(io::IO, bt::StackFrameTree{T}, cols::Int, fmt::ProfileFormat
             count = down.count
             count < fmt.mincount && continue
             count < noisefloor && continue
-            str = strs[i]::AnnotatedString
+            str = strs[i]::String
             noisefloor_down = fmt.noisefloor > 0 ? floor(Int, fmt.noisefloor * sqrt(count)) : 0
             pushfirst!(worklist, (down, level + 1, noisefloor_down, str))
         end
