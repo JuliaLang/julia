57c5bee9efc0495e31ade830a5b98296da46bbb3
Kristoffer <kcarlsson89@gmail.com>
2025-04-03 15:30:03 +0200

    remove left over references to StyledStrings

--- contrib/juliac-buildscript.jl
+++ contrib/juliac-buildscript.jl
@@ -261,14 +261,6 @@ let
         end
     end
 
-    StyledStrings = find_loaded_root_module(Base.PkgId(
-        Base.UUID("f489334b-da3d-4c2e-b8f0-e476e12c162b"), "StyledStrings"))
-    if StyledStrings !== nothing
-        @eval StyledStrings begin
-            __init__() = rand()
-        end
-    end
-
     Markdown = find_loaded_root_module(Base.PkgId(
         Base.UUID("d6f4376e-aef5-505a-96c1-9c027394607a"), "Markdown"))
     if Markdown !== nothing
