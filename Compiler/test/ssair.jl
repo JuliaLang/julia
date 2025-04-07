b621f136c4f6e29f9ae033ba2d6f71328a71d8bc
Shuhei Kadowaki <aviatesk@gmail.com>
2025-04-07 11:20:34 -0400

    optimizer: fix various `optimize_until` misuses
    I fixed a bunch of places where the `optimize_until` path was specified
    incorrectly. Also, I changed `"Inlining"` to `"inlining"` to match the
    lowercase style of the other path names.

--- Compiler/test/ssair.jl
+++ Compiler/test/ssair.jl
@@ -601,6 +601,7 @@ import .Compiler: NewInstruction, insert_node!
 let ir = Base.code_ircode((Int,Int); optimize_until="inlining") do a, b
         a^b
     end |> only |> first
+    ir = Compiler.compact!(ir)
     nstmts = length(ir.stmts)
     invoke_idx = findfirst(@nospecialize(stmt)->Meta.isexpr(stmt, :invoke), ir.stmts.stmt)
     @test invoke !== nothing
@@ -662,6 +663,7 @@ end
 let ir = Base.code_ircode((Int,Int); optimize_until="inlining") do a, b
         a^b
     end |> only |> first
+    ir = Compiler.compact!(ir)
     invoke_idx = findfirst(@nospecialize(stmt)->Meta.isexpr(stmt, :invoke), ir.stmts.stmt)
     @test invoke_idx !== nothing
     invoke_expr = ir.stmts.stmt[invoke_idx]
