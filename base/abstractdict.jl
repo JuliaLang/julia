57f0ad1ddbc7dfff1c9628a0f81b0e1fbb83cf3f
Andy Dienes <andydienes@gmail.com>
2025-04-09 14:36:49 -0400

    fixup

--- base/abstractdict.jl
+++ base/abstractdict.jl
@@ -92,7 +92,7 @@ But `keys(a)`, `values(a)` and `pairs(a)` all iterate `a`
 and return the elements in the same order.
 
 # Examples
-```jldoctest; filter = r"^\\s+\\S+\\s+=>\\s+\\d\$"m
+```jldoctest; filter = r"^\\s+'\\S'.*\$"m
 julia> D = Dict('a'=>2, 'b'=>3)
 Dict{Char, Int64} with 2 entries:
   'a' => 2
@@ -118,7 +118,7 @@ But `keys(a)`, `values(a)` and `pairs(a)` all iterate `a`
 and return the elements in the same order.
 
 # Examples
-```jldoctest; filter = r"^\\s+\\S+\\s+=>\\s+\\d\$"m
+```jldoctest; filter = r"^\\s+\\S+(\\s+=>\\s+\\d)?\$"m
 julia> D = Dict('a'=>2, 'b'=>3)
 Dict{Char, Int64} with 2 entries:
   'a' => 2
