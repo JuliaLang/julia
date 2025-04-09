c85344fb67d9db2625adae493d1b5ca81f38074b
oscarddssmith <oscar.smith@juliacomputing.com>
2025-04-09 15:35:14 -0400

    add activate! support for JuliaSyntax and JuliaLowering

--- stdlib/InteractiveUtils/src/macros.jl
+++ stdlib/InteractiveUtils/src/macros.jl
@@ -440,7 +440,7 @@ in the current environment.
 When using `@activate`, additional options for a component may be specified in
 square brackets `@activate Compiler[:option1, :option]`
 
-Currently `@activate Compiler` is the only available component that may be
+Currently `Compiler`, `JuliaSyntax`, and `JuliaLowering` are the only available components that may be
 activatived.
 
 For `@activate Compiler`, the following options are available:
@@ -475,7 +475,7 @@ macro activate(what)
     if !isa(Component, Symbol)
         error("Usage Error: Component $Component is not a symbol")
     end
-    allowed_components = (:Compiler,)
+    allowed_components = (:Compiler, :JuliaSyntax, :JuliaLowering)
     if !(Component in allowed_components)
         error("Usage Error: Component $Component is not recognized. Expected one of $allowed_components")
     end
