fe618c3c85762b546fc78d34f0c55669188ea73b
Zentrik <llvm.zentrik@gmail.com>
2025-04-18 12:30:46 +0100

    Don't make the memcpy at all
    It seems like it was ub in the first place, ig we need to wait for https://developers.redhat.com/articles/2024/12/11/making-memcpynull-null-0-well-defined to land

--- src/stackwalk.c
+++ src/stackwalk.c
@@ -326,10 +326,11 @@ static void decode_backtrace(jl_bt_element_t *bt_data, size_t bt_size,
     bt = *btout = jl_alloc_array_1d(array_ptr_void_type, bt_size);
     static_assert(sizeof(jl_bt_element_t) == sizeof(void*),
                   "jl_bt_element_t is presented as Ptr{Cvoid} on julia side");
-    if (bt_data == NULL) {
-        assert(bt_size == 0); // stop static analyzer from complaining
+    if (bt_data != NULL) {
+        memcpy(jl_array_data(bt, jl_bt_element_t), bt_data, bt_size * sizeof(jl_bt_element_t));
+    } else {
+        assert(bt_size == 0);
     }
-    memcpy(jl_array_data(bt, jl_bt_element_t), bt_data, bt_size * sizeof(jl_bt_element_t));
     bt2 = *bt2out = jl_alloc_array_1d(jl_array_any_type, 0);
     // Scan the backtrace buffer for any gc-managed values
     for (size_t i = 0; i < bt_size; i += jl_bt_entry_size(bt_data + i)) {
