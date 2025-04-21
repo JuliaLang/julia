580bb5e710b543aded63a46555965a8e34c65ce8
Matt Bauman <mbauman@juliacomputing.com>
2025-04-21 14:29:11 -0400

    fix doctests

--- base/reducedim.jl
+++ base/reducedim.jl
@@ -887,13 +887,13 @@ julia> A = [true false; true false]
  1  0
  1  0
 
-julia> all!([1; 1], A)
-2-element Vector{Int64}:
+julia> all!(Bool[1; 1], A)
+2-element Vector{Bool}:
  0
  0
 
-julia> all!([1 1], A)
-1×2 Matrix{Int64}:
+julia> all!(Bool[1 1], A)
+1×2 Matrix{Bool}:
  1  0
 ```
 """
@@ -962,12 +962,12 @@ julia> A = [true false; true false]
  1  0
  1  0
 
-julia> any!([1; 1], A)
+julia> any!(Bool[1; 1], A)
 2-element Vector{Int64}:
  1
  1
 
-julia> any!([1 1], A)
+julia> any!(Bool[1 1], A)
 1×2 Matrix{Int64}:
  1  0
 ```
