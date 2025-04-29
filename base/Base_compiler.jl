b9a0497e96286fa3e43740294e2c5f6b17b70ae2
Steven G. Johnson <stevenj@alum.mit.edu>
2025-04-29 11:34:49 -0400

    clarify that time_ns is monotonic (#57129)

--- base/Base_compiler.jl
+++ base/Base_compiler.jl
@@ -188,8 +188,12 @@ end
 """
     time_ns()::UInt64
 
-Get the time in nanoseconds relative to some arbitrary time in the past. The primary use is for measuring the elapsed time
-between two moments in time.
+Get the time in nanoseconds relative to some machine-specific arbitrary time in the past.
+The primary use is for measuring elapsed times during program execution. The return value is guaranteed to
+be monotonic (mod 2⁶⁴) while the system is running, and is unaffected by clock drift or changes to local calendar time,
+but it may change arbitrarily across system reboots or suspensions.
+
+(Although the returned time is always in nanoseconds, the timing resolution is platform-dependent.)
 """
 time_ns() = ccall(:jl_hrtime, UInt64, ())
 
