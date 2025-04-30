fa4e1e0bd57228696dea39d650dd2e2f48209f61
Cody Tapscott <topolarity@tapscott.me>
2025-04-30 09:27:53 -0400

    Fix trimming test

--- test/trimming/init.c
+++ test/trimming/init.c
@@ -1,9 +1,11 @@
+#define _GNU_SOURCE
+#include <dlfcn.h>
 #include <julia.h>
 
 __attribute__((constructor)) void static_init(void)
 {
     if (jl_is_initialized())
         return;
-    julia_init(JL_IMAGE_IN_MEMORY);
+    jl_init_with_image_handle((void *)RTLD_DEFAULT);
     jl_exception_clear();
 }
