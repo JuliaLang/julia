7858427643c9c1ae7e29c4798c337423ad810a6a
Andy Dienes <andydienes@gmail.com>
2025-05-10 10:57:11 -0400

    remove Irrational from hash(::Real) fallback

--- base/irrationals.jl
+++ base/irrationals.jl
@@ -182,8 +182,6 @@ isinteger(::AbstractIrrational) = false
 iszero(::AbstractIrrational) = false
 isone(::AbstractIrrational) = false
 
-hash(x::Irrational, h::UInt) = 3*objectid(x) - h
-
 widen(::Type{T}) where {T<:Irrational} = T
 
 zero(::AbstractIrrational) = false
