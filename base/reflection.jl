2e097bd88ebfc0c493c440e4f108a3d79907cae0
Jeff Bezanson <jeff.bezanson@gmail.com>
2025-05-14 17:36:33 -0400

    fix `hasmethod` with kwargs to exclude positional arg names (#58410)
    (cherry picked from commit 7df60f480df8c6aed874b35eb7c8a26fc769a4cc)

--- base/reflection.jl
+++ base/reflection.jl
@@ -1851,11 +1851,11 @@ function hasmethod(f, t, kwnames::Tuple{Vararg{Symbol}}; world::UInt=get_world_c
     match = ccall(:jl_gf_invoke_lookup, Any, (Any, Any, UInt), tt, nothing, world)
     match === nothing && return false
     kws = ccall(:jl_uncompress_argnames, Array{Symbol,1}, (Any,), (match::Method).slot_syms)
+    kws = kws[((match::Method).nargs + 1):end] # remove positional arguments
     isempty(kws) && return true # some kwfuncs simply forward everything directly
     for kw in kws
         endswith(String(kw), "...") && return true
     end
-    kwnames = Symbol[kwnames[i] for i in 1:length(kwnames)]
     return issubset(kwnames, kws)
 end
 
