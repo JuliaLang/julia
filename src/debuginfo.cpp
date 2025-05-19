866c5f437e62b50d34791bf81c6d6801a71ab887
gbaraldi <baraldigabriel@gmail.com>
2025-05-19 16:21:40 -0300

    Disable new codepath for testing

--- src/debuginfo.cpp
+++ src/debuginfo.cpp
@@ -1136,7 +1136,7 @@ bool jl_dylib_DI_for_fptr(size_t pointer, object::SectionRef *Section, int64_t *
     *context = entry.ctx;
     if (entry.obj)
         *Section = getModuleSectionForAddress(entry.obj, pointer + entry.slide);
-    if ((saddr && !*saddr) || (name && !*name)) {
+    if (false && (saddr && !*saddr) || (name && !*name)) {
         if (auto DWctx = dyn_cast_or_null<llvm::DWARFContext>(*context)) {
             uintptr_t saddrdwarf = 0;
             char *snamedwarf = nullptr;
