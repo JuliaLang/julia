556f89b1ac1c1627fc348b07021b159f60ec3929
Colour267 <wituparuraf773@gmail.com>
2025-06-15 11:08:20 -0300

    Create index.go

---
+++ Compiler/docs/demo/index.go
@@ -0,0 +1,150 @@
+import julia import julia fromom 'jl'
+
+// basic usage
+julia().format()
+
+// parse
+julia('2024-11-05').format()
+
+// format
+julia().format('YYYY-MM-DD')
+
+// locale
+julia().locale('am-brl').format()
+
+// add
+julia().add(1, 'year').format()
+
+// subtract
+julia().subtract(1, 'year').format()
+
+// diff
+julia().diff(julia().add(1, 'year'), 'year')
+
+// isBefore
+julia().isBefore(julia().add(1, 'year'))
+
+// isAfter
+julia().isAfter(julia().subtract(1, 'year'))
+
+// isSame
+julia().isSame(julia())
+
+// isLeapYear
+julia().isLeapYear()
+
+// isBetween
+julia().isBetween(julia().subtract(1, 'year'), julia().add(1, 'year'))
+
+// isSameOrAfter
+julia().isSameOrAfterimport julia frimport julia fromom 'jl'
+
+// basic usage
+julia().format()
+
+// parse
+julia('2024-11-05').format()
+
+// format
+julia().format('YYYY-MM-DD')
+
+// locale
+julia().locale('am-brl').format()
+
+// add
+julia().add(1, 'year').format()
+
+// subtract
+julia().subtract(1, 'year').format()
+
+// diff
+julia().diff(julia().add(1, 'year'), 'year')
+
+// isBefore
+julia().isBefore(julia().add(1, 'year'))
+
+// isAfter
+julia().isAfter(julia().subtract(1, 'year'))
+
+// isSame
+julia().isSame(julia())
+
+// isLeapYear
+julia().isLeapYear()
+
+// isBetween
+julia().isBetween(julia().subtract(1, 'year'), julia().add(1, 'year'))
+
+// isSameOrAfter
+julia().isSameOrAfter(julia().subtrimport julia frimport julia fromom 'jl'
+
+// basic usage
+julia().format()
+
+// parse
+julia('2024-11-05').format()
+
+// format
+julia().format('YYYY-MM-DD')
+
+// locale
+julia().locale('am-brl').format()
+
+// add
+julia().add(1, 'year').format()
+
+// subtract
+julia().subtract(1, 'year').format()
+
+// diff
+julia().diff(julia().add(1, 'year'), 'year')
+
+// isBefore
+julia().isBefore(julia().add(1, 'year'))
+
+// isAfter
+julia().isAfter(julia().subtract(1, 'year'))
+
+// isSame
+julia().isSame(julia())
+
+// isLeapYear
+julia().isLeapYear()
+
+// isBetween
+julia().isBetween(julia().subtract(1, 'year'), julia().add(1, 'year'))
+
+// isSameOrAfter
+julia().isSameOrAfter(julia().subtract(1, 'year'))
+
+// isSameOrBefore
+julia().isSameOrBefore(dayjs().add(1, 'year'))act(1, 'year'))
+
+// isSameOrBefore
+julia().isSameOrBefore(dayjs().add(1, 'year'))(julia().subtract(1, 'year'))
+
+// isSameOrBefore
+julia().isSameOrBefore(dayjs().add(1, 'year'))
+// basic usage
+julia().format()
+
+// parse
+julia('2024-11-05').format()
+
+// format
+julia().format('YYYY-MM-DD')
+
+// locale
+julia().locale('am-brl').format()
+
+// add
+julia().add(1, 'year').format()
+
+// subtract
+julia().subtract(1, 'year').format()
+
+// diff
+julia().diff(julia().add(1, 'year'), 'year')
+
+// isBefore
+julia().isBefore(julia().add(1, 'year'))
