aba3834ece4b03855d5d074d9abbe184af09ad41
Jameson Nash <vtjnash@gmail.com>
2025-06-19 15:17:27 -0400

    fix showing types after removing using Core (#58773)
    PR #57357 changed the default using list, but only changed some of the
    places where the `show` code handled that. This led to duplicate
    (confusing) printing, since both Core. and Base. prefixes are dropped.
    
    Fix #58772
    
    (cherry picked from commit 089810cd490d0f040d1ea5d748243c56307d6201)

--- base/show.jl
+++ base/show.jl
@@ -531,7 +531,7 @@ function active_module()
     return invokelatest(active_module, active_repl)::Module
 end
 
-module UsesCoreAndBaseOnly
+module UsesBaseOnly
 end
 
 function show_function(io::IO, f::Function, compact::Bool, fallback::Function)
@@ -543,8 +543,8 @@ function show_function(io::IO, f::Function, compact::Bool, fallback::Function)
     elseif isdefined(fname, :module) && isdefinedglobal(fname.module, fname.singletonname) && isconst(fname.module, fname.singletonname) &&
             getglobal(fname.module, fname.singletonname) === f
         # this used to call the removed internal function `is_exported_from_stdlib`, which effectively
-        # just checked for exports from Core and Base.
-        mod = get(io, :module, UsesCoreAndBaseOnly)
+        # just checked for exports from Base.
+        mod = get(io, :module, UsesBaseOnly)
         if !(isvisible(fname.singletonname, fname.module, mod) || fname.module === mod)
             print(io, fname.module, ".")
         end
@@ -818,7 +818,7 @@ function make_typealiases(@nospecialize(x::Type))
     Any === x && return aliases, Union{}
     x <: Tuple && return aliases, Union{}
     mods = modulesof!(Set{Module}(), x)
-    Core in mods && push!(mods, Base)
+    replace!(mods, Core=>Base)
     vars = Dict{Symbol,TypeVar}()
     xenv = UnionAll[]
     each = Any[]
@@ -836,7 +836,7 @@ function make_typealiases(@nospecialize(x::Type))
                     ti === Union{} && continue
                     # make sure this alias wasn't from an unrelated part of the Union
                     mod2 = modulesof!(Set{Module}(), alias)
-                    mod in mod2 || (mod === Base && Core in mods) || continue
+                    mod in mod2 || (mod === Base && Core in mod2) || continue
                     env = env::SimpleVector
                     applied = alias
                     if !isempty(env)
