7bbbb2ce24053f373ba08ef5c0898caac3ec376a
Cédric Belmant <cedric.belmant@juliahub.com>
2025-06-23 22:04:22 +0000

    Add basic logging & filtering for sequential Compiler tests

--- Compiler/test/runtests.jl
+++ Compiler/test/runtests.jl
@@ -8,7 +8,9 @@ using Test, Compiler
 @testset "Compiler.jl" begin
     for file in readlines(joinpath(@__DIR__, "testgroups"))
         file == "special_loading" && continue # Only applicable to Base.Compiler
+        startswith(lstrip(file), "#") && continue # Comment out files to ignore them
         testfile = file * ".jl"
+        println("Testing $testfile...")
         @eval @testset $testfile include($testfile)
     end
 end
