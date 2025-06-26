ae388212adcf51109c609a4f209e2c6953b91b79
Cody Tapscott <84105208+topolarity@users.noreply.github.com>
2025-06-26 07:35:02 -0400

    Add `cfunction` support for `--trim` (#58812)

--- test/trimming/basic_jll.jl
+++ test/trimming/basic_jll.jl
@@ -3,11 +3,22 @@ module MyApp
 using Libdl
 using Zstd_jll
 
-Base.@ccallable function main()::Cint
-    println(Core.stdout, "Julia! Hello, world!")
-    fptr = dlsym(Zstd_jll.libzstd_handle, :ZSTD_versionString)
+function print_string(fptr::Ptr{Cvoid})
     println(Core.stdout, unsafe_string(ccall(fptr, Cstring, ())))
+end
+
+Base.@ccallable function main()::Cint
+    # Test the basic "Hello, world!"
+    println(Core.stdout, "Julia! Hello, world!")
+
+    # Make sure that JLL's are working as expected
     println(Core.stdout, unsafe_string(ccall((:ZSTD_versionString, libzstd), Cstring, ())))
+
+    # Add an indirection via `@cfunction`
+    cfunc = @cfunction(print_string, Cvoid, (Ptr{Cvoid},))
+    fptr = dlsym(Zstd_jll.libzstd_handle, :ZSTD_versionString)
+    ccall(cfunc, Cvoid, (Ptr{Cvoid},), fptr)
+
     return 0
 end
 
