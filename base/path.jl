efc923f7f95cf1f49becf4ae494164834c6dee91
Nicholas Bauer <nicholasbauer@outlook.com>
2025-07-09 17:10:39 -0400

    Fix `joinpath` docstring so Windows paths are printed as escaped.

--- base/path.jl
+++ base/path.jl
@@ -357,7 +357,7 @@ the join of the preceding paths, then prior components are dropped.
 Note on Windows since there is a current directory for each drive, `joinpath("c:", "foo")`
 represents a path relative to the current directory on drive "c:" so this is equal to "c:foo",
 not "c:\\foo". Furthermore, `joinpath` treats this as a non-absolute path and ignores the drive
-letter casing, hence `joinpath("C:\\A","c:b") = "C:\\A\\b"`.
+letter casing, hence `joinpath("C:\\\\A","c:b") = "C:\\\\A\\\\b"`.
 
 # Examples
 ```jldoctest
