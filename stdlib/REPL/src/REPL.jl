ghacc4813/ju-lia

Make the REPL banner resize to avoid wrapping
We also spritz it up a bit using the new StyledStrings library, namely:
- colouring the Help and Pkg key prompts
- making the docs link a terminal link, and Pkg a link to the Pkg docs
- using box drawing characters for the dividing line
- making branch status more colourful
- making the official version text more subdued

With these change the four banners (from smallest to largest) are:
1. A one-liner, for extreme circumstances (new)
2. The short banner
3. The large banner, with the description stacked vertically (new)
4. The large banner, with the description stacked horizontally

stdlib/REPL/src/REPL.jl
@@ -1516,11 +1516,21 @@ ends_with_semicolon(code::AbstractString) = ends_with_semicolon(String(code))
 ends_with_semicolon(code::Union{String,SubString{String}}) =
     contains(_rm_strings_and_comments(code), r";\s*$")
 
-function banner(io::IO = stdout; short = false)
-    if Base.GIT_VERSION_INFO.tagged_commit
-        commit_string = Base.TAGGED_RELEASE_BANNER
+"""
+    banner(io::IO = stdout, preferred::Symbol = :full)
+
+Print the "Julia" informative banner to `io`, using the `preferred` variant
+if reasonable and known.
+
+!!! warning
+    The particular banner selected by `preferred` is liable to being changed
+    without warning. The current variants are: `:tiny`, `:short`, `:narrow`, and `:full`.
+"""
+function banner(io::IO = stdout, preferred::Symbol = :full)
+    commit_string = if Base.GIT_VERSION_INFO.tagged_commit
+        Base.AnnotatedString(TAGGED_RELEASE_BANNER, :face => :shadow)
     elseif isempty(Base.GIT_VERSION_INFO.commit)
-        commit_string = ""
+         styled""
     else
         days = Int(floor((ccall(:jl_clock_now, Float64, ()) - Base.GIT_VERSION_INFO.fork_master_timestamp) / (60 * 60 * 24)))
         days = max(0, days)
@@ -1529,60 +1539,65 @@ function banner(io::IO = stdout; short = false)
         commit = Base.GIT_VERSION_INFO.commit_short
 
         if distance == 0
-            commit_string = "Commit $(commit) ($(days) $(unit) old master)"
+            styled"""Commit {grey:$commit} \
+                     ({warning:⌛ {italic:$days $unit}} old master)"""
         else
             branch = Base.GIT_VERSION_INFO.branch
-            commit_string = "$(branch)/$(commit) (fork: $(distance) commits, $(days) $(unit))"
+            styled"""{emphasis:$branch}/{grey:$commit} \
+                     ({italic:{success:{bold,(slant=normal):↑} $distance commits}, \
+                     {warning:{(slant=normal):⌛} $days $unit}})"""
         end
     end
 
-    commit_date = isempty(Base.GIT_VERSION_INFO.date_string) ? "" : " ($(split(Base.GIT_VERSION_INFO.date_string)[1]))"
+    commit_date = isempty(Base.GIT_VERSION_INFO.date_string) ? "" : styled" {light:($(split(Base.GIT_VERSION_INFO.date_string)[1]))}"
+    doclink = styled"{bold:Documentation:} {(underline=grey),link={https://docs.julialang.org}:https://docs.julialang.org}"
+    help = styled"Type {repl_prompt_help:?} for help, {repl_prompt_pkg:]?} for {(underline=grey),link={https://pkgdocs.julialang.org/}:Pkg} help."
 
-    if get(io, :color, false)::Bool
-        c = Base.text_colors
-        tx = c[:normal] # text
-        jl = c[:normal] # julia
-        d1 = c[:bold] * c[:blue]    # first dot
-        d2 = c[:bold] * c[:red]     # second dot
-        d3 = c[:bold] * c[:green]   # third dot
-        d4 = c[:bold] * c[:magenta] # fourth dot
+    sizenames = (:tiny, :short, :narrow, :full)
+    maxsize = something(findfirst(==(preferred), sizenames), lenght(sizenames))
+    size = min(if     all(displaysize(io) .>= (8, 70)); 4 # Full size
+               elseif all(displaysize(io) .>= (8, 45)); 3 # Narrower
+               elseif all(displaysize(io) .>= (3, 50)); 2 # Tiny
+               else 1 end,
+               max(0, maxsize))
 
-        if short
-            print(io,"""
-              $(d3)o$(tx)  | Version $(VERSION)$(commit_date)
-             $(d2)o$(tx) $(d4)o$(tx) | $(commit_string)
-            """)
-        else
-            print(io,"""               $(d3)_$(tx)
-               $(d1)_$(tx)       $(jl)_$(tx) $(d2)_$(d3)(_)$(d4)_$(tx)     |  Documentation: https://docs.julialang.org
-              $(d1)(_)$(jl)     | $(d2)(_)$(tx) $(d4)(_)$(tx)    |
-               $(jl)_ _   _| |_  __ _$(tx)   |  Type \"?\" for help, \"]?\" for Pkg help.
-              $(jl)| | | | | | |/ _` |$(tx)  |
-              $(jl)| | |_| | | | (_| |$(tx)  |  Version $(VERSION)$(commit_date)
-             $(jl)_/ |\\__'_|_|_|\\__'_|$(tx)  |  $(commit_string)
-            $(jl)|__/$(tx)                   |
+    if size == 4 # Full size
+        print(io, styled"""
+                                 {bold,green:_}
+                     {bold,blue:_}       _ {bold:{red:_}{green:(_)}{magenta:_}}     {shadow:│}  $doclink
+                    {bold,blue:(_)}     | {bold:{red:(_)} {magenta:(_)}}    {shadow:│}
+                     _ _   _| |_  __ _   {shadow:│}  $help
+                    | | | | | | |/ _` |  {shadow:│}
+                    | | |_| | | | (_| |  {shadow:│}  Version {bold:$VERSION}$commit_date
+                   _/ |\\__'_|_|_|\\__'_|  {shadow:│}  $commit_string
+                  |__/                   {shadow:│}
+                  \n""")
+    elseif size == 3 # Rotated
+        print(io, styled"""
+                                 {bold,green:_}
+                     {bold,blue:_}       _ {bold:{red:_}{green:(_)}{magenta:_}}
+                    {bold,blue:(_)}     | {bold:{red:(_)} {magenta:(_)}}
+                     _ _   _| |_  __ _
+                    | | | | | | |/ _` |
+                    | | |_| | | | (_| |
+                   _/ |\\__'_|_|_|\\__'_|
+                  |__/
 
-            """)
-        end
-    else
-        if short
-            print(io,"""
-              o  |  Version $(VERSION)$(commit_date)
-             o o |  $(commit_string)
-            """)
-        else
-            print(io,"""
-                           _
-               _       _ _(_)_     |  Documentation: https://docs.julialang.org
-              (_)     | (_) (_)    |
-               _ _   _| |_  __ _   |  Type \"?\" for help, \"]?\" for Pkg help.
-              | | | | | | |/ _` |  |
-              | | |_| | | | (_| |  |  Version $(VERSION)$(commit_date)
-             _/ |\\__'_|_|_|\\__'_|  |  $(commit_string)
-            |__/                   |
+                   $doclink
+                   $help
 
-            """)
-        end
+                   Version {bold:$VERSION}$commit_date
+                   $commit_string
+                  \n""")
+    elseif size == 2 # Tiny
+        print(io, styled"""
+                     {bold,green:o}  {shadow:│} Version {bold:$VERSION}$commit_date
+                    {bold:{red:o} {magenta:o}} {shadow:│} $commit_string
+                   """, ifelse(displaysize(io) > (12, 0), "\n", ""))
+    elseif size == 1 && Base.GIT_VERSION_INFO.tagged_commit # Text only
+        print(io, styled"""{bold:{blue:∴} {magenta:Julia} $VERSION}$commit_date\n""")
+    elseif size == 1 # Text only
+        print(io, styled"""{bold:{blue:∴} {magenta:Julia} $VERSION}$commit_date $commit_string\n""")
     end
 end
 

JuliaLang/julia

stdlib/REPL/src/REPL.jl
@@ -1724,11 +1724,21 @@ function ends_with_semicolon(code)
     return semi
 end
 
-function banner(io::IO = stdout; short = false)
-    if Base.GIT_VERSION_INFO.tagged_commit
-        commit_string = Base.TAGGED_RELEASE_BANNER
+"""
+    banner(io::IO = stdout, preferred::Symbol = :full)
+
+Print the "Julia" informative banner to `io`, using the `preferred` variant
+if reasonable and known.
+
+!!! warning
+    The particular banner selected by `preferred` is liable to being changed
+    without warning. The current variants are: `:tiny`, `:short`, `:narrow`, and `:full`.
+"""
+function banner(io::IO = stdout, preferred::Symbol = :full)
+    commit_string = if Base.GIT_VERSION_INFO.tagged_commit
+        Base.AnnotatedString(TAGGED_RELEASE_BANNER, :face => :shadow)
     elseif isempty(Base.GIT_VERSION_INFO.commit)
-        commit_string = ""
+         styled""
     else
         days = Int(floor((ccall(:jl_clock_now, Float64, ()) - Base.GIT_VERSION_INFO.fork_master_timestamp) / (60 * 60 * 24)))
         days = max(0, days)
@@ -1737,60 +1747,65 @@ function banner(io::IO = stdout; short = false)
         commit = Base.GIT_VERSION_INFO.commit_short
 
         if distance == 0
-            commit_string = "Commit $(commit) ($(days) $(unit) old master)"
+            styled"""Commit {grey:$commit} \
+                     ({warning:⌛ {italic:$days $unit}} old master)"""
         else
             branch = Base.GIT_VERSION_INFO.branch
-            commit_string = "$(branch)/$(commit) (fork: $(distance) commits, $(days) $(unit))"
+            styled"""{emphasis:$branch}/{grey:$commit} \
+                     ({italic:{success:{bold,(slant=normal):↑} $distance commits}, \
+                     {warning:{(slant=normal):⌛} $days $unit}})"""
         end
     end
 
-    commit_date = isempty(Base.GIT_VERSION_INFO.date_string) ? "" : " ($(split(Base.GIT_VERSION_INFO.date_string)[1]))"
+    commit_date = isempty(Base.GIT_VERSION_INFO.date_string) ? "" : styled" {light:($(split(Base.GIT_VERSION_INFO.date_string)[1]))}"
+    doclink = styled"{bold:Documentation:} {(underline=grey),link={https://docs.julialang.org}:https://docs.julialang.org}"
+    help = styled"Type {repl_prompt_help:?} for help, {repl_prompt_pkg:]?} for {(underline=grey),link={https://pkgdocs.julialang.org/}:Pkg} help."
 
-    if get(io, :color, false)::Bool
-        c = Base.text_colors
-        tx = c[:normal] # text
-        jl = c[:normal] # julia
-        d1 = c[:bold] * c[:blue]    # first dot
-        d2 = c[:bold] * c[:red]     # second dot
-        d3 = c[:bold] * c[:green]   # third dot
-        d4 = c[:bold] * c[:magenta] # fourth dot
+    sizenames = (:tiny, :short, :narrow, :full)
+    maxsize = something(findfirst(==(preferred), sizenames), length(sizenames))
+    size = min(if     all(displaysize(io) .>= (8, 70)); 4 # Full size
+               elseif all(displaysize(io) .>= (8, 45)); 3 # Narrower
+               elseif all(displaysize(io) .>= (3, 50)); 2 # Tiny
+               else 1 end,
+               max(0, maxsize))
 
-        if short
-            print(io,"""
-              $(d3)o$(tx)  | Version $(VERSION)$(commit_date)
-             $(d2)o$(tx) $(d4)o$(tx) | $(commit_string)
-            """)
-        else
-            print(io,"""               $(d3)_$(tx)
-               $(d1)_$(tx)       $(jl)_$(tx) $(d2)_$(d3)(_)$(d4)_$(tx)     |  Documentation: https://docs.julialang.org
-              $(d1)(_)$(jl)     | $(d2)(_)$(tx) $(d4)(_)$(tx)    |
-               $(jl)_ _   _| |_  __ _$(tx)   |  Type \"?\" for help, \"]?\" for Pkg help.
-              $(jl)| | | | | | |/ _` |$(tx)  |
-              $(jl)| | |_| | | | (_| |$(tx)  |  Version $(VERSION)$(commit_date)
-             $(jl)_/ |\\__'_|_|_|\\__'_|$(tx)  |  $(commit_string)
-            $(jl)|__/$(tx)                   |
+    if size == 4 # Full size
+        print(io, styled"""
+                                 {bold,green:_}
+                     {bold,blue:_}       _ {bold:{red:_}{green:(_)}{magenta:_}}     {shadow:│}  $doclink
+                    {bold,blue:(_)}     | {bold:{red:(_)} {magenta:(_)}}    {shadow:│}
+                     _ _   _| |_  __ _   {shadow:│}  $help
+                    | | | | | | |/ _` |  {shadow:│}
+                    | | |_| | | | (_| |  {shadow:│}  Version {bold:$VERSION}$commit_date
+                   _/ |\\__'_|_|_|\\__'_|  {shadow:│}  $commit_string
+                  |__/                   {shadow:│}
+                  \n""")
+    elseif size == 3 # Rotated
+        print(io, styled"""
+                                 {bold,green:_}
+                     {bold,blue:_}       _ {bold:{red:_}{green:(_)}{magenta:_}}
+                    {bold,blue:(_)}     | {bold:{red:(_)} {magenta:(_)}}
+                     _ _   _| |_  __ _
+                    | | | | | | |/ _` |
+                    | | |_| | | | (_| |
+                   _/ |\\__'_|_|_|\\__'_|
+                  |__/
 
-            """)
-        end
-    else
-        if short
-            print(io,"""
-              o  |  Version $(VERSION)$(commit_date)
-             o o |  $(commit_string)
-            """)
-        else
-            print(io,"""
-                           _
-               _       _ _(_)_     |  Documentation: https://docs.julialang.org
-              (_)     | (_) (_)    |
-               _ _   _| |_  __ _   |  Type \"?\" for help, \"]?\" for Pkg help.
-              | | | | | | |/ _` |  |
-              | | |_| | | | (_| |  |  Version $(VERSION)$(commit_date)
-             _/ |\\__'_|_|_|\\__'_|  |  $(commit_string)
-            |__/                   |
+                   $doclink
+                   $help
 
-            """)
-        end
+                   Version {bold:$VERSION}$commit_date
+                   $commit_string
+                  \n""")
+    elseif size == 2 # Tiny
+        print(io, styled"""
+                     {bold,green:o}  {shadow:│} Version {bold:$VERSION}$commit_date
+                    {bold:{red:o} {magenta:o}} {shadow:│} $commit_string
+                   """, ifelse(displaysize(io) > (12, 0), "\n", ""))
+    elseif size == 1 && Base.GIT_VERSION_INFO.tagged_commit # Text only
+        print(io, styled"""{bold:{blue:∴} {magenta:Julia} $VERSION}$commit_date\n""")
+    elseif size == 1 # Text only
+        print(io, styled"""{bold:{blue:∴} {magenta:Julia} $VERSION}$commit_date $commit_string\n""")
     end
 end
 