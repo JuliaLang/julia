1eae8d71b6083f251383efeb31b2587467cbcc5e
Neven Sajko <s@purelymail.com>
2025-07-26 14:20:46 +0200

    just forgo the doctest

--- base/complex.jl
+++ base/complex.jl
@@ -593,12 +593,12 @@ More accurate method for `cis(pi*x)` (especially for large `x`).
 See also [`cis`](@ref), [`sincospi`](@ref), [`exp`](@ref), [`angle`](@ref).
 
 # Examples
-```jldoctest
+```julia-repl
 julia> cispi(10000)
 1.0 + 0.0im
 
 julia> cispi(0.25 + 1im)
-[...]
+0.030556854645954562 + 0.03055685464595456im
 ```
 
 !!! compat "Julia 1.6"
