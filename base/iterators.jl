c7c6735ecb934425960f1fe74fa54813b016a057
Shuhei Kadowaki <aviatesk@gmail.com>
2025-07-30 02:25:17 +0900

    enforce `Tuple{Any,Any}` to be returned.

--- base/iterators.jl
+++ base/iterators.jl
@@ -536,11 +536,10 @@ function iterate(f::Filter, state...)
     while y !== nothing
         v, s = y
         if f.flt(v)
-            if y isa Tuple{Any,Any}
-                return (v, s) # incorporate type information that may be improved by user-provided `f.flt`
-            else
-                return y
-            end
+            # Incorporate type information that may be improved by user-provided `f.flt`:
+            # This forces the return value of `iterate(::Filter, state...)` to always be `Tuple{Any,Any}`,
+            # but since that return value is only used by `iterate(::Filter, state...)`, this should be fine.
+            return (v, s)
         end
         y = iterate(f.itr, s)
     end
