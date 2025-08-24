458934f559f9cc01523db009829db46984b6d78e
Keno Fischer <keno@juliahub.com>
2025-08-24 01:48:44 +0000

    spawn: Allow libuv IOServers to be passed as extra FDs
    Extracted from #59271, but I now have an independent use case.
    This allows libuv `IOServer`s (unix domain servers, tcp servers,
    etc) to be passed as extra fd arguments to child processes.

--- test/spawn.jl
+++ test/spawn.jl
@@ -1053,3 +1053,34 @@ end
     @assert eltype(args) != String
     @test Cmd(["ls", args...]) == `ls -l /tmp`
 end
+
+# Test passing a pipe server as an addition fd
+@testset "Pipe server as additional fd" begin
+    if !Sys.iswindows()
+        # Windows CRT does not support passing server sockets as stdio fds
+        mktempdir() do dir
+            path = joinpath(dir, "test.sock")
+            server = Sockets.PipeServer()
+            bind(server, path)
+            Base.errormonitor(@async begin
+                local client
+                while true
+                    try
+                        client = Sockets.connect(path)
+                        break
+                    catch e
+                        isa(e, Base.IOError) || rethrow(e)
+                    end
+                    sleep(1)
+                end
+                println(client, "Hello Socket!")
+                closewrite(client)
+            end)
+            buf = IOBuffer()
+            proc = run(`$(Base.julia_cmd()) -e 'using Sockets; s = listen(Sockets.PipeServer(RawFD(3))); c = accept(s); print(read(c, String))'`, devnull, buf, stderr, server)
+            close(server)
+            @test success(proc)
+            @test String(take!(buf)) == "Hello Socket!\n"
+        end
+    end
+end
