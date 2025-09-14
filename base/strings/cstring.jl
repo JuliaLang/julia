f862e93a22958bda9c3909a04d2d4f510252b3e1
Steven G. Johnson <stevenj@alum.mit.edu>
2025-09-14 13:14:33 -0400

    Update cstring.jl: doc Cwstring method of unsafe_string

--- base/strings/cstring.jl
+++ base/strings/cstring.jl
@@ -301,6 +301,19 @@ function transcode(::Type{UInt8}, src::AbstractVector{UInt16})
     return dst
 end
 
+"""
+    unsafe_string(p::Ptr{T}, [length::Integer]) where {T<:Union{UInt16,UInt32,Cwchar_t}}
+    unsafe_string(p::Cwstring)
+
+Transcode a string from the address of a C-style (NUL-terminated) string encoded as UTF-16
+(`T=UInt16`), UTF-32 (`T=UInt32`), or the system-dependent `wchar_t` (`T=Cwchar_t` or `Cwstring`),
+returning a `String` (UTF-8 encoding), similar to [`transcode`](@ref) but reading directly
+from a pointer.  (The pointer can be safely freed afterwards.) If `length` is specified
+(the length of the data in bytes), the string does not have to be NUL-terminated.
+
+This function is labeled "unsafe" because it will crash if `p` is not
+a valid memory address to data of the requested length (or NUL-terminated data).
+"""
 function unsafe_string(p::Ptr{T}, length::Integer) where {T<:Union{UInt16,UInt32,Cwchar_t}}
     transcode(String, unsafe_wrap(Array, p, length; own=false))
 end
