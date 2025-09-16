aecb173ae677b248e42d34f39164673e82ef0d52
Gabriel Baraldi <baraldigabriel@gmail.com>
2025-09-16 06:29:14 -0300

    Fix assume effects handling for gc_safe (#59557)
    Fixes https://github.com/JuliaLang/julia/issues/59552

--- test/ccall.jl
+++ test/ccall.jl
@@ -1970,7 +1970,7 @@ end
 
 function gc_safe_ccall()
     # jl_rand is marked as JL_NOTSAFEPOINT
-    @ccall gc_safe=true jl_rand()::UInt64
+    Base.@assume_effects :nothrow @ccall gc_safe=true jl_rand()::UInt64
 end
 
 let llvm = sprint(code_llvm, gc_safe_ccall, ())
@@ -1978,6 +1978,7 @@ let llvm = sprint(code_llvm, gc_safe_ccall, ())
     @test gc_safe_ccall() isa UInt64
     # check for the gc_safe store
     @test occursin("store atomic i8 2", llvm)
+    @test Base.infer_effects(gc_safe_ccall, Tuple{}).nothrow == true
 end
 
 @testset "jl_dlfind and dlsym" begin
