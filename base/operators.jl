bf95f5a9f77f09c74d514aed17da468595ee6ff1
Neven Sajko <s@purelymail.com>
2025-09-18 00:09:57 +0200

    `split_rest` for `Tuple`: slighly improve inference
    * Introduce function `_split_tuple` as the core of the implementation
      of `split_rest` for `Tuple`.
    
        * Improves inference for `split_rest` in some cases because
          `_split_tuple` only accepts `i::Int`.
    
        * Avoids a bit of code duplication in `split_rest`.
    
    * Apply `_split_tuple` within the generic method making `Fix` callable,
      where the same pattern is implemented inline.

--- base/operators.jl
+++ base/operators.jl
@@ -1194,7 +1194,8 @@ end
 
 function (f::Fix{N})(args::Vararg{Any,M}; kws...) where {N,M}
     M < N-1 && throw(ArgumentError(LazyString("expected at least ", N-1, " arguments to `Fix{", N, "}`, but got ", M)))
-    return f.f(args[begin:begin+(N-2)]..., f.x, args[begin+(N-1):end]...; kws...)
+    (left, right) = _split_tuple(args, N-1)
+    return f.f(left..., f.x, right...; kws...)
 end
 
 # Special cases for improved constant propagation
