af16e4e221511a5e2000a254f9d2cf2776d4190d
Jameson Nash <vtjnash@gmail.com>
2025-09-26 14:50:38 +0000

    make sched_task more robust
    If the sched_task ever dies, we need to recreate a new one. Creating a
    new one is very cheap, so it is unclear why we don't always do it this
    way. But at least do it this way if it ever dies (e.g. from an untimely
    InterruptException).

--- contrib/juliac/juliac-trim-base.jl
+++ contrib/juliac/juliac-trim-base.jl
@@ -12,6 +12,7 @@ end
     depwarn(msg, funcsym; force::Bool=false) = nothing
     _assert_tostring(msg) = ""
     reinit_stdio() = nothing
+    wait_forever() = while true; wait(); end
     JuliaSyntax.enable_in_core!() = nothing
     init_active_project() = ACTIVE_PROJECT[] = nothing
     set_active_project(projfile::Union{AbstractString,Nothing}) = ACTIVE_PROJECT[] = projfile
