11bd484736ec7bff5ae7d69febda23e00c739c75
Dilum Aluthge <dilum@aluthge.com>
2025-09-28 20:53:20 -0400

    Don't use `@something` (because we don't want to throw)

--- base/initdefs.jl
+++ base/initdefs.jl
@@ -382,7 +382,8 @@ options and their priority in package loading.
 See also [`Base.active_project`](@ref), [`Base.set_active_project`](@ref).
 """
 function active_manifest(project_file::Union{AbstractString,Nothing}=nothing, search_load_path::Bool=true)
-    project_file === @something project_file active_project(search_load_path)
+    # If `project_file` was specified, use that, otherwise get the active project:
+    project_file === !isnothing(project_file) ? project_file : active_project(search_load_path)
     project_file === nothing && return nothing
     return project_file_manifest_path(project_file)
 end
