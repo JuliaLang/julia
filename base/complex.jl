0e5284027d0d3b1f7e8b20e1888e2ba2dcf5f1ed
Andy Dienes <andydienes@gmail.com>
2025-09-29 10:06:05 -0400

    fix overflow in complex muladd

--- base/complex.jl
+++ base/complex.jl
@@ -290,8 +290,8 @@ inv(z::Complex{<:Integer}) = inv(float(z))
                                     real(z) * imag(w) + imag(z) * real(w))
 
 muladd(z::Complex, w::Complex, x::Complex) =
-    Complex(muladd(real(z), real(w), -muladd(imag(z), imag(w), -real(x))),
-            muladd(real(z), imag(w),  muladd(imag(z), real(w),  imag(x))))
+    Complex(muladd(real(z), real(w), real(x) - imag(z) * imag(w)),
+            muladd(real(z), imag(w), muladd(imag(z), real(w), imag(x))))
 
 # handle Bool and Complex{Bool}
 # avoid type signature ambiguity warnings
@@ -338,7 +338,7 @@ muladd(z::Complex, x::Real, w::Complex) =
     Complex(muladd(real(z),x,real(w)), muladd(imag(z),x,imag(w)))
 muladd(x::Real, y::Real, z::Complex) = Complex(muladd(x,y,real(z)), imag(z))
 muladd(z::Complex, w::Complex, x::Real) =
-    Complex(muladd(real(z), real(w), -muladd(imag(z), imag(w), -x)),
+    Complex(muladd(real(z), real(w), x - imag(z) * imag(w)),
             muladd(real(z), imag(w), imag(z) * real(w)))
 
 /(a::R, z::S) where {R<:Real,S<:Complex} = (T = promote_type(R,S); a*inv(T(z)))
