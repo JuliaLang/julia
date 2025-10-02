3d8b4612a5858cee2951a4a87c29e0c9bf9d7f63
Cody Tapscott <84105208+topolarity@users.noreply.github.com>
2025-10-02 10:53:08 -0400

    Add `:no_constprop` as expected `Expr(:meta, ...)` argument (JuliaLang/JuliaLowering.jl#88)

--- JuliaLowering/src/compat.jl
+++ JuliaLowering/src/compat.jl
@@ -472,7 +472,7 @@ function _insert_convert_expr(@nospecialize(e), graph::SyntaxGraph, src::SourceA
             end
         elseif e.args[1] in (:inline, :noinline, :generated, :generated_only,
                              :max_methods, :optlevel, :toplevel, :push_loc, :pop_loc,
-                             :aggressive_constprop, :specialize, :compile, :infer,
+                             :no_constprop, :aggressive_constprop, :specialize, :compile, :infer,
                              :nospecializeinfer, :force_compile, :doc)
             # TODO: Some need to be handled in lowering
             child_exprs[1] = Expr(:quoted_symbol, e.args[1])
