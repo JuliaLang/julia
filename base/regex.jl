4c14d62aaba57f1bf6b8ba9f67f43b773da20419
GianmarcoCuppari <cupparigianmarco@gmail.com>
2025-10-07 18:42:58 +0200

    Fix whitespace and finalize docstring format in regex.jl

--- base/regex.jl
+++ base/regex.jl
@@ -356,7 +356,7 @@ Return `true` if `s` ends with the regex pattern, `suffix`.
     `match_option` to PCRE. If compile time is amortized,
     `occursin(r"...\$", s)` is faster than `endswith(s, r"...")`.
 
-See also [`occursin`](@ref), [`startswith`](@ref), [`startswith`](@ref), [`occursin`](@ref), [`match`](@ref) 
+See also [`occursin`](@ref), [`startswith`](@ref), [`startswith`](@ref), [`occursin`](@ref), [`match`](@ref)
 
 !!! compat "Julia 1.2"
     This method requires at least Julia 1.2.
