0f9b2fc19fd201f18d92f823361a4bfd24047a29
Timothy <git@tecosaur.net>
2025-10-25 23:34:09 +0800

    Make banner size depend on terminal size (#51811)
    We also spritz it up a bit using the new StyledStrings library, namely:
    - colouring the Help and Pkg key prompts
    - making the docs link a terminal link, and Pkg a link to the Pkg docs
    - using box drawing characters for the dividing line
    - making branch status more colourful
    - making the official version text more subdued
    
    With these change the four banners (from smallest to largest) are:
    1. A one-liner, for extreme circumstances (new)
    2. The short banner
    3. The large banner, with the description stacked vertically (new)
    4. The large banner, with the description stacked horizontally
    
    Because precompilation is a little flakey at the moment, I add a bunch
    of precompile statements to prevent latency from regressing.

--- stdlib/REPL/test/repl.jl
+++ stdlib/REPL/test/repl.jl
@@ -1801,9 +1801,9 @@ let io = IOBuffer()
     seek(io, 0)
     @test countlines(io) == 9
     take!(io)
-    @test REPL.banner(io; short=true) === nothing
+    @test REPL.banner(io, :tiny) === nothing
     seek(io, 0)
-    @test countlines(io) == 2
+    @test countlines(io) == 1
 end
 
 @testset "Docstrings" begin
