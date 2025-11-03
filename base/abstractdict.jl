7cc2709bb0d9ecb588aaa4b2a1a0b914be333b5a
Andy Dienes <51664769+adienes@users.noreply.github.com>
2025-11-03 12:44:56 -0500

    finish deprecation of `merge(::Callable ..)` (#60027)
    fixup to https://github.com/JuliaLang/julia/pull/59775
    
    closes https://github.com/JuliaLang/julia/issues/59990
    
    (cherry picked from commit 946de3327d8cad8ffbb4b0c5b06633a318db21d9)

--- base/abstractdict.jl
+++ base/abstractdict.jl
@@ -365,16 +365,12 @@ typeof(merge).name.max_methods = UInt8(1)
 """
     mergewith(combine, d::AbstractDict, others::AbstractDict...)
     mergewith(combine)
-    merge(combine, d::AbstractDict, others::AbstractDict...)
 
 Construct a merged collection from the given collections. If necessary, the
 types of the resulting collection will be promoted to accommodate the types of
 the merged collections. Values with the same key will be combined using the
 combiner function.  The curried form `mergewith(combine)` returns the function
 `(args...) -> mergewith(combine, args...)`.
-
-Method `merge(combine::Union{Function,Type}, args...)` as an alias of
-`mergewith(combine, args...)` is still available for backward compatibility.
 
 !!! compat "Julia 1.5"
     `mergewith` requires Julia 1.5 or later.
@@ -409,8 +405,6 @@ mergewith(combine, d::AbstractDict, others::AbstractDict...) =
     mergewith!(combine, _typeddict(d, others...), others...)
 mergewith(combine) = (args...) -> mergewith(combine, args...)
 typeof(mergewith).name.max_methods = UInt8(1)
-merge(combine::Callable, d::AbstractDict, others::AbstractDict...) =
-    merge!(combine, _typeddict(d, others...), others...)
 
 promoteK(K) = K
 promoteV(V) = V
