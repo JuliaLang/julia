442e8d4efef37c29c8f6f19ceb645eca92ed429b
Cody Tapscott <84105208+topolarity@users.noreply.github.com>
2025-12-02 13:27:46 -0500

    Add `test-JuliaLowering_stdlibs` target for precompiling stdlibs with JuliaLowering (#60194)
    This adds a new test target: `make test-JuliaLowering_stdlibs`, which
    precompiles a supported set of standard libraries using JuliaLowering.
    
    To accomplish this, it also adds a basic incremental sysimage build
    target (`julia-sysimg-JL-release` / `julia-sysimg-JL-debug`) that
    modifies the base sysimage to include JuliaLowering.
    
    Currently 38 / 52 pkgimage-based stdlibs can be compiled with
    JuliaLowering:
    
    - Pre-compiling: ArgTools, Future, LibCURL_jll, Logging, OpenSSL_jll,
    Tar, dSFMT_jll, Base64, GMP_jll, LibGit2_jll, MPFR_jll, PCRE2_jll,
    UUIDs, libLLVM_jll, CRC32c, LLD_jll, LibSSH2_jll, MozillaCACerts_jll,
    Printf, Unicode, Mmap, nghttp2_jll, Distributed, LLVMLibUnwind_jll,
    LibUV_jll, NetworkOptions, Serialization, Zlib_jll, p7zip_jll, LibCURL,
    LibUnwind_jll, OpenLibm_jll, SuiteSparse_jll, Zstd_jll, Dates,
    DelimitedFiles, SharedArrays, Downloads
    - Failing: Statistics, StyledStrings, SparseArrays, SuiteSparse,
    Profile, JuliaSyntaxHighlighting, Markdown, LibGit2, InteractiveUtils,
    Test, REPL, TOML, Pkg, LazyArtifacts

---
+++ test/JuliaLowering_stdlibs.jl
@@ -0,0 +1,79 @@
+import Libdl
+
+### 38 / 52 (non-sysimage) stdlibs precompile successfully with JuliaLowering ###
+const INCOMPATIBLE_STDLIBS = String[
+    "Statistics", "JuliaSyntaxHighlighting", "Markdown", "LibGit2",
+    "InteractiveUtils", "Test", "REPL", "Pkg", "LazyArtifacts", "SparseArrays",
+    "TOML", "StyledStrings", "Profile", "SuiteSparse",
+]
+
+const JULIA_EXECUTABLE = Base.unsafe_string(Base.JLOptions().julia_bin)
+const JULIA_CPU_TARGET = get(ENV, "JULIA_CPU_TARGET", Base.unsafe_string(Base.JLOptions().cpu_target))
+const debug = get(ENV, "JULIA_BUILD_MODE", "release") == "debug" ? "-debug" : ""
+const JL_sysimage = joinpath(dirname(unsafe_string(Base.JLOptions().image_file)), "sys-JL$(debug).$(Libdl.dlext)")
+
+function compile_JL_sysimage(output_filepath)
+    sysimage = unsafe_string(Base.JLOptions().image_file)
+    output_sysimage = abspath(output_filepath)
+    output_object = "$(splitext(output_sysimage)[1])-o.a"
+
+    package_root = joinpath(Sys.STDLIB, "..", "..", "JuliaLowering")
+    cmd = `$(JULIA_EXECUTABLE) -C "$(JULIA_CPU_TARGET)" --output-o $(output_object)
+           --startup-file=no --warn-overwrite=yes --depwarn=error --sysimage $(sysimage)
+           -e "Core.include(Base, $(repr(joinpath(package_root, "src", "JuliaLowering.jl"))))"`
+    cmd = addenv(
+        Base.Cmd(cmd; dir = joinpath(package_root, "src")),
+        "JULIA_BINDIR" => unsafe_string(Base.JLOptions().julia_bindir),
+        "JULIA_LOAD_PATH" => "@stdlib",
+        "JULIA_PROJECT" => nothing,
+        "JULIA_DEPOT_PATH" => ":",
+        "JULIA_NUM_THREADS" => "1",
+    )
+    println("Compiling incremental sysimage with JuliaLowering...")
+    success(run(cmd))
+
+    cmd = Base.Linking.link_image_cmd(output_object, output_sysimage)
+    success(run(cmd))
+
+    return nothing
+end
+
+# ensure JL-inclusive sysimage is built / available
+if "BUILDROOT" in keys(ENV)
+    # Running via Makefile, use sysimage.mk with its built-in caching / file tracking
+    run(`$(ENV["MAKE"]) -C $(ENV["BUILDROOT"]) -f sysimage.mk sysimg-JL-$(ENV["JULIA_BUILD_MODE"])`)
+else
+    # Standalone test run (CI), compile every time
+    compile_JL_sysimage(JL_sysimage)
+end
+stdlibs_to_test = filter(name -> !in(name, INCOMPATIBLE_STDLIBS), readdir(Sys.STDLIB))
+
+configs = [
+    ``=>Base.CacheFlags(check_bounds=0, debug_level=2, opt_level=3),
+    ``=>Base.CacheFlags(check_bounds=1, debug_level=2, opt_level=3),
+]
+setupproject_command = "using Pkg; Pkg.add($(stdlibs_to_test))"
+compilecache_command = "using Base: CacheFlags; Base.Precompilation.precompilepkgs($(stdlibs_to_test); configs=$(configs))"
+
+# pre-compile stdlibs (into temporary depot)
+mktempdir() do tmp_depot
+    # first setup the project / environment
+    env_dir = joinpath(tmp_depot, "environments", "v$(VERSION.major).$(VERSION.minor)")
+    cmd = addenv(
+        `$(JULIA_EXECUTABLE) --startup-file=no --project=$env_dir -e $setupproject_command`,
+        ; inherit = true
+    )
+    success(run(cmd))
+
+    # now actually perform the precompilation
+    cmd = addenv(
+        `$(JULIA_EXECUTABLE) --sysimage $(JL_sysimage) --startup-file=no -e $compilecache_command`,
+        "JULIA_LOAD_PATH" => "@stdlib$(Base.Linking.pathsep)$(env_dir)",
+        "JULIA_CPU_TARGET" => "sysimage",
+        "JULIA_USE_FLISP_LOWERING" => "0",
+        "JULIA_USE_FALLBACK_REPL" => "0",
+        "JULIA_DEPOT_PATH" => tmp_depot,
+        ; inherit = true
+    )
+    success(run(cmd))
+end
