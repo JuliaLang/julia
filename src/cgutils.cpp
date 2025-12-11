a9acd43c9b0d3e229e7e4ac550a3076f756f79b5
Yichao Yu <yyc1992@gmail.com>
2025-12-11 13:50:21 -0500

    (Mostly) Trivial part of LLVM 21 support (#60356)
    Split out of https://github.com/JuliaLang/julia/pull/59946 to make it
    hopefully easier to review.

--- src/cgutils.cpp
+++ src/cgutils.cpp
@@ -1177,8 +1177,13 @@ static void emit_memcpy_llvm(jl_codectx_t &ctx, Value *dst, jl_aliasinfo_t const
     // above problem won't be as serious.
 
     auto merged_ai = dst_ai.merge(src_ai);
+#if JL_LLVM_VERSION < 210000
     ctx.builder.CreateMemCpy(dst, align_dst, src, align_src, sz, is_volatile,
                              merged_ai.tbaa, merged_ai.tbaa_struct, merged_ai.scope, merged_ai.noalias);
+#else
+    ctx.builder.CreateMemCpy(dst, align_dst, src, align_src, sz, is_volatile,
+                             merged_ai.toAAMDNodes());
+#endif
 }
 
 template<typename T1>
