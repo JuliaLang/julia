3dc170a366048c9f18ce33257a60eb6dd011947a
Andy Dienes <andydienes@gmail.com>
2025-12-24 09:07:51 -0500

    use pointer math for some int --> string conversions

--- base/uuid.jl
+++ base/uuid.jl
@@ -92,13 +92,19 @@ let groupings = [36:-1:25; 23:-1:20; 18:-1:15; 13:-1:10; 8:-1:1]
     global string
     function string(u::UUID)
         u = u.value
-        a = Base.StringMemory(36)
-        for i in groupings
-            @inbounds a[i] = hex_chars[1 + u & 0xf]
-            u >>= 4
+        str = Base._string_n(36)
+        GC.@preserve str begin
+            p = pointer(str)
+            for i in groupings
+                @inbounds unsafe_store!(p, hex_chars[1 + u & 0xf], i)
+                u >>= 4
+            end
+            @inbounds unsafe_store!(p, UInt8('-'), 9)
+            @inbounds unsafe_store!(p, UInt8('-'), 14)
+            @inbounds unsafe_store!(p, UInt8('-'), 19)
+            @inbounds unsafe_store!(p, UInt8('-'), 24)
         end
-        @inbounds a[24] = a[19] = a[14] = a[9] = '-'
-        return unsafe_takestring(a)
+        return str
     end
 end
 
