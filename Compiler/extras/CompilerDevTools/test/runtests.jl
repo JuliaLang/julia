4943888708cff561553a341474a643b3ab6525cf
Mason Protter <mason.protter@icloud.com>
2026-01-18 11:05:07 +0100

    fix invoke test

--- Compiler/extras/CompilerDevTools/test/runtests.jl
+++ Compiler/extras/CompilerDevTools/test/runtests.jl
@@ -12,17 +12,16 @@ using CompilerDevTools: lookup_method_instance, SplitCacheInterp
   mi = lookup_method_instance(f1)
   @test !haskey(cache, mi)
   @test with_new_compiler(f1, interp.owner) === 3
-  @test haskey(cache, mi)
-  # Here `do_work` is compiled at runtime, and so must have
-  # required extra work to be cached under the same cache owner.
-  mi = lookup_method_instance(do_work, 1, 2)
-  @test haskey(cache, mi)
+    @test haskey(cache, mi)
+    # Here `do_work` is compiled at runtime, and so must have
+    # required extra work to be cached under the same cache owner.
+    mi = lookup_method_instance(do_work, 1, 2)
+    @test haskey(cache, mi)
 
-  # Should not error with a builtin whose type we do not know
-  f_unknown_builtin() = Base.compilerbarrier(:type, isa)(1, Int)
-  with_new_compiler(f_unknown_builtin, interp.owner) === true
+    # Should not error with a builtin whose type we do not know
+    f_unknown_builtin() = Base.compilerbarrier(:type, isa)(1, Int)
+    with_new_compiler(f_unknown_builtin, interp.owner) === true
 end;
-
 
 const cinst = let world = get_world_counter()
     sig = Tuple{typeof(sin), Float64}
@@ -35,12 +34,19 @@ end
         @allocated(f(rand())) == 0
     end
 end
-@testset "Recompile null `invoke` pointer" begin
-    @atomic cinst.invoke = C_NULL
-    f(x) = invoke(sin, cinst, x)
-    f(1.0)
-    @test cinst.invoke != C_NULL
+
+const cinst2 = let world = get_world_counter()
+    sig = Tuple{typeof(sin), Float64}
+    mi = lookup_method_instance(sin, 1.0)
+    typeinf_ext_toplevel(SplitCacheInterp(; world), mi, SOURCE_MODE_ABI)
 end
+@testset "Recompile null `invoke` pointer" begin
+    @atomic cinst2.invoke = C_NULL
+    f(x) = invoke(sin, cinst2, x)
+    f(1.0)
+    @test cinst2.invoke != C_NULL
+end
+
 @testset "worldage checks" begin
     this_world = Base.get_world_counter()
     f(x) = invoke(sin, cinst, x)
