5376edf27bc29eb90a966f58e1d0c348ffa86586
Mason Protter <mason.protter@icloud.com>
2026-01-18 11:41:47 +0100

    untabify

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -4309,9 +4309,9 @@ static bool emit_builtin_call(jl_codectx_t &ctx, jl_cgval_t *ret, jl_value_t *f,
         if (func.constant && argtypes.constant && jl_is_code_instance(argtypes.constant)) {
             jl_code_instance_t *codeinst = (jl_code_instance_t*)argtypes.constant;
             auto invoke = jl_atomic_load_acquire(&codeinst->invoke);
-			auto codeinst_min_world = jl_atomic_load_acquire(&codeinst->min_world);
-			auto codeinst_max_world = jl_atomic_load_acquire(&codeinst->max_world);
-			if (invoke == NULL) {
+            auto codeinst_min_world = jl_atomic_load_acquire(&codeinst->min_world);
+            auto codeinst_max_world = jl_atomic_load_acquire(&codeinst->max_world);
+            if (invoke == NULL) {
                 jl_compile_codeinst(codeinst);
                 invoke = jl_atomic_load_acquire(&codeinst->invoke);
             }
