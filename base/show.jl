77f70ff1ca28f514272a0035b4a49048d058a848
rokke <66498307+rokke-git@users.noreply.github.com>
2026-01-25 11:37:16 +0000

    Fix improper allignment of matrices with elided strings

--- base/show.jl
+++ base/show.jl
@@ -3028,7 +3028,7 @@ end
 
 nocolor(io::IO) = IOContext(io, :color => false)
 alignment_from_show(io::IO, x::Any) =
-    textwidth(sprint(show, x, context=nocolor(io), sizehint=0))
+    textwidth(sprint(show, MIME"text/plain"(), x, context=nocolor(io), sizehint=0))
 
 """
 `alignment(io, X)` returns a tuple (left,right) showing how many characters are
