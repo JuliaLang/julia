6e6c47c70a886228d7ac11c87f3230e3c223ce3d
Neven Sajko <s@purelymail.com>
2026-01-27 23:24:18 +0100

    avoid unnecessarily introducing a global binding

--- base/pair.jl
+++ base/pair.jl
@@ -41,12 +41,10 @@ eltype(p::Type{Pair{A, B}}) where {A, B} = Union{A, B}
 iterate(p::Pair, i=1) = i > 2 ? nothing : (getfield(p, i), i + 1)
 indexed_iterate(p::Pair, i::Int, state=1) = (getfield(p, i), i + 1)
 
-const _pairhash_seed = if UInt === UInt64
-    0x94cb2bb20a28ce96
-else
-    0x1f60a087
+let seed = (UInt === UInt64) ? 0x94cb2bb20a28ce96 : 0x1f60a087
+    global hash
+    hash(p::Pair, h::UInt) = hash(p.second, hash(p.first, xor(seed, h)))
 end
-hash(p::Pair, h::UInt) = hash(p.second, hash(p.first, xor(_pairhash_seed, h)))
 
 ==(p::Pair, q::Pair) = (p.first==q.first) & (p.second==q.second)
 isequal(p::Pair, q::Pair) = isequal(p.first,q.first)::Bool & isequal(p.second,q.second)::Bool
