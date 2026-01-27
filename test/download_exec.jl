447ef3bde7bab4806bb798bfa5b514fa892b0b7e
rokke <66498307+rokke-git@users.noreply.github.com>
2026-01-27 10:39:10 +0000

    fall back on httpbin when julia's mirror is down

--- test/download_exec.jl
+++ test/download_exec.jl
@@ -5,22 +5,28 @@ module TestDownload
 using Test
 
 mktempdir() do temp_dir
+    try
+        download("https://httpbin.julialang.org")
+        global url = "https://httpbin.julialang.org"
+    catch
+        global url = "https://httpbin.org"
+    end
     # Download a file
     file = joinpath(temp_dir, "ip")
-    @test download("https://httpbin.julialang.org/ip", file) == file
+    @test download("$url/ip", file) == file
     @test isfile(file)
     @test !isempty(read(file))
     ip = read(file, String)
 
     # Download an empty file
     empty_file = joinpath(temp_dir, "empty")
-    @test download("https://httpbin.julialang.org/status/200", empty_file) == empty_file
+    @test download("$url/status/200", empty_file) == empty_file
     @test isfile(empty_file)
     @test isempty(read(empty_file))
 
     # Make sure that failed downloads do not leave files around
     missing_file = joinpath(temp_dir, "missing")
-    @test_throws Exception download("https://httpbin.julialang.org/status/404", missing_file)
+    @test_throws Exception download("$url/status/404", missing_file)
     @test !isfile(missing_file)
 
     # Use a TEST-NET (192.0.2.0/24) address which shouldn't be bound
