92d5d5c20e01fb564b9bfb39e0efed1bb8d5e2ba
deepeshraghav <165639247+deepeshraghav@users.noreply.github.com>
2026-02-01 04:53:48 +0530

    Reset key repeats in REPL history navigation

--- stdlib/REPL/src/REPL.jl
+++ stdlib/REPL/src/REPL.jl
@@ -974,7 +974,9 @@ function history_prev(s::LineEdit.MIState, hist::REPLHistoryProvider,
     m = history_move(s, hist, hist.cur_idx-num, save_idx)
     if m === :ok
         LineEdit.move_input_start(s)
+        LineEdit.reset_key_repeats(s) do
         LineEdit.move_line_end(s)
+    end
         return LineEdit.refresh_line(s)
     elseif m === :skip
         return history_prev(s, hist, num+1, save_idx)
