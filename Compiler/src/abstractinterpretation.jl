6cfdf95353dec6f7326038d5bb3b4b67977323ca
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2026-02-17 23:15:13 +0900

    inference: Widen slot wrappers in `update_exc_bestguess!` (#61048)
    Widen slot wrappers in `exct` before merging into `exc_bestguess`, so
    that the merged types are simple enough to be handled by the ipo lattice
    without needing complex lattice element merging.

--- Compiler/src/abstractinterpretation.jl
+++ Compiler/src/abstractinterpretation.jl
@@ -4174,6 +4174,7 @@ end
 function update_exc_bestguess!(interp::AbstractInterpreter, @nospecialize(exct), frame::InferenceState)
     𝕃ₚ = ipo_lattice(interp)
     handler = gethandler(frame)
+    exct = widenslotwrapper(exct)
     if handler === nothing
         if !⊑(𝕃ₚ, exct, frame.exc_bestguess)
             frame.exc_bestguess = tmerge(𝕃ₚ, frame.exc_bestguess, exct)
