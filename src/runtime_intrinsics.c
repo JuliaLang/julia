55843833ad7e7b8ac434fc94c936a88c372d77a2
Keno Fischer <keno@juliacomputing.com>
2026-02-20 11:47:43 -0500

    runtime_intrinsics: Fix f16 ABI fallback (#61094)
    The x86 psABI specifies that f16 goes on the stack (but is returned in
    xmm0). Our emulation for compilers that do not support _Float16 did not
    handle this properly. Fixes #61072.
    
    I think this fixes it at least - I'm too tired to actually go check at
    this point, so I'll just let CI try it ;). My remaining concern is that
    I don't quite understand why seeing this behavior is AVX512 dependent.
    
    (cherry picked from commit 2ba9b37b3f8074106e21b621fe58a9518ae07cd8)

--- src/runtime_intrinsics.c
+++ src/runtime_intrinsics.c
@@ -260,45 +260,55 @@ JL_DLLEXPORT float julia_half_to_float(uint16_t param) {
      (defined(__clang__) && __clang_major__ > 14)) && \
     !defined(_CPU_PPC64_) && !defined(_CPU_PPC_) && \
     !defined(_OS_WINDOWS_) && !defined(_CPU_RISCV64_)
-    #define FLOAT16_TYPE _Float16
-    #define FLOAT16_TO_UINT16(x) (*(uint16_t*)&(x))
-    #define FLOAT16_FROM_UINT16(x) (*(_Float16*)&(x))
+    #define FLOAT16_ARG_TYPE _Float16
+    #define FLOAT16_RET_TYPE _Float16
+    #define FLOAT16_ARG_TO_UINT16(x) (*(uint16_t*)&(x))
+    #define FLOAT16_RET_FROM_UINT16(x) (*(_Float16*)&(x))
 // on older compilers, we need to emulate the platform-specific ABI
-#elif defined(_CPU_X86_) || (defined(_CPU_X86_64_) && !defined(_OS_WINDOWS_))
-    // on x86, we can use __m128; except on Windows where x64 calling
-    // conventions expect to pass __m128 by reference.
-    #define FLOAT16_TYPE __m128
-    #define FLOAT16_TO_UINT16(x) take_from_xmm(x)
-    #define FLOAT16_FROM_UINT16(x) return_in_xmm(x)
+#elif defined(_CPU_X86_)
+    // On i686, LLVM's half return convention uses XMM0, so we return __m128.
+    // But arguments are passed on the stack like integers, so use uint16_t.
+    #define FLOAT16_ARG_TYPE uint16_t
+    #define FLOAT16_RET_TYPE __m128
+    #define FLOAT16_ARG_TO_UINT16(x) (x)
+    #define FLOAT16_RET_FROM_UINT16(x) return_in_xmm(x)
+#elif defined(_CPU_X86_64_) && !defined(_OS_WINDOWS_)
+    // On x86_64 SysV, both f16 args and __m128 args go in XMM registers.
+    #define FLOAT16_ARG_TYPE __m128
+    #define FLOAT16_RET_TYPE __m128
+    #define FLOAT16_ARG_TO_UINT16(x) take_from_xmm(x)
+    #define FLOAT16_RET_FROM_UINT16(x) return_in_xmm(x)
 #elif defined(_CPU_PPC64_) || defined(_CPU_PPC_)
     // on PPC, pass Float16 as if it were an integer, similar to the old x86 ABI
     // before _Float16
-    #define FLOAT16_TYPE uint16_t
-    #define FLOAT16_TO_UINT16(x) (x)
-    #define FLOAT16_FROM_UINT16(x) (x)
+    #define FLOAT16_ARG_TYPE uint16_t
+    #define FLOAT16_RET_TYPE uint16_t
+    #define FLOAT16_ARG_TO_UINT16(x) (x)
+    #define FLOAT16_RET_FROM_UINT16(x) (x)
 #else
     // otherwise, pass using floating-point calling conventions
-    #define FLOAT16_TYPE float
-    #define FLOAT16_TO_UINT16(x) ((uint16_t)*(uint32_t*)&(x))
-    #define FLOAT16_FROM_UINT16(x) ({ uint32_t tmp = (uint32_t)(x); *(float*)&tmp; })
+    #define FLOAT16_ARG_TYPE float
+    #define FLOAT16_RET_TYPE float
+    #define FLOAT16_ARG_TO_UINT16(x) ((uint16_t)*(uint32_t*)&(x))
+    #define FLOAT16_RET_FROM_UINT16(x) ({ uint32_t tmp = (uint32_t)(x); *(float*)&tmp; })
 #endif
 
-JL_DLLEXPORT float julia__gnu_h2f_ieee(FLOAT16_TYPE param)
+JL_DLLEXPORT float julia__gnu_h2f_ieee(FLOAT16_ARG_TYPE param)
 {
-    uint16_t param16 = FLOAT16_TO_UINT16(param);
+    uint16_t param16 = FLOAT16_ARG_TO_UINT16(param);
     return half_to_float(param16);
 }
 
-JL_DLLEXPORT FLOAT16_TYPE julia__gnu_f2h_ieee(float param)
+JL_DLLEXPORT FLOAT16_RET_TYPE julia__gnu_f2h_ieee(float param)
 {
     uint16_t res = float_to_half(param);
-    return FLOAT16_FROM_UINT16(res);
+    return FLOAT16_RET_FROM_UINT16(res);
 }
 
-JL_DLLEXPORT FLOAT16_TYPE julia__truncdfhf2(double param)
+JL_DLLEXPORT FLOAT16_RET_TYPE julia__truncdfhf2(double param)
 {
     uint16_t res = double_to_half(param);
-    return FLOAT16_FROM_UINT16(res);
+    return FLOAT16_RET_FROM_UINT16(res);
 }
 
 
