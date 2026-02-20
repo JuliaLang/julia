8fb69a8a548a69eefab23f22a720751f7a2d5f00
Nicholas Bauer <nicholasbauer@outlook.com>
2026-02-20 00:41:41 -0500

    Remove unnecessary inline

--- base/indices.jl
+++ base/indices.jl
@@ -215,7 +215,6 @@ end
 # those are the permutations that preserve the order of the non-singleton
 # dimensions.
 function setindex_shape_check(X::AbstractArray, I::Integer...)
-    @inline
     li = ndims(X)
     lj = length(I)
     i = j = 1
