a74538c4b9589eb0b0efe56f56bb98381009a505
Dilum Aluthge <dilum@aluthge.com>
2026-02-28 15:55:05 -0500

    Combine the two checks into a single `if` statement

--- stdlib/SharedArrays/src/SharedArrays.jl
+++ stdlib/SharedArrays/src/SharedArrays.jl
@@ -153,10 +153,8 @@ function SharedArray{T,N}(dims::Dims{N}; init=false, pids=Int[]) where {T,N}
         shm_seg_name = ""
 
     finally
-        if !isempty(shm_seg_name)
-            if @isdefined shmmem_create_pid
-                remotecall_fetch(shm_unlink, shmmem_create_pid, shm_seg_name)
-            end
+        if !isempty(shm_seg_name) && @isdefined shmmem_create_pid
+            remotecall_fetch(shm_unlink, shmmem_create_pid, shm_seg_name)
         end
     end
     S
