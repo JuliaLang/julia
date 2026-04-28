b54ab68b2dac4b8d1c10d69874113b8e49c9d28c
Nicholas Bauer <nicholasbauer@outlook.com>
2026-04-27 20:52:52 -0400

    whitespace

--- stdlib/Mmap/src/Mmap.jl
+++ stdlib/Mmap/src/Mmap.jl
@@ -560,12 +560,12 @@ mmap(::Type{BitArray{N}}, dims::NTuple{N, Integer}; kwargs...) where {N} =
     open(io -> mmap(io, BitArray{N}, dims; kwargs...), SharedMemory, "", Base.num_bit_chunks(prod(dims)) * sizeof(UInt64); readonly = false, create = true)
 
 
-"""                                                                                                                                                                                
+"""
     Mmap.munmap!(A)
 
 Eagerly release the memory mapping backing `A` (an `Array` or `BitArray` returned
-by `mmap`). After this call, `A` must not be accessed. Equivalent to what happens                                                                                                  
-when `A` is garbage-collected, but deterministic.                                                                                                                                  
+by `mmap`). After this call, `A` must not be accessed. Equivalent to what happens
+when `A` is garbage-collected, but deterministic.
 """
 munmap!(A::Array) = finalize(A.ref.mem)
 munmap!(B::BitArray) = finalize(B.chunks.ref.mem)
