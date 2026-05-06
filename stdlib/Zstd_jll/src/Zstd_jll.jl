c9e8301d8f288ef5ab3ba45abb3fecaa8cc9052e
Cody Tapscott <topolarity@tapscott.me>
2026-05-06 16:53:44 -0400

    Zstd_jll: Convert to non-LazyLibrary stub form
    Rewrite the Zstd_jll stdlib stub to the classic string-const + dlopen
    form used by the rest of the bundled JLLs. Preserves the public surface
    added since the LazyLibrary conversion (libzstd_path, zstd_path,
    zstdmt_path, the zstd()/zstdmt() command builders) and the conditional
    CompilerSupportLibraries_jll dependency on Windows i686 (where libzstd
    links against libgcc_s).
    
    With Zstd_jll no longer using LazyLibrary, the trimming test no longer
    needs its vendored copy of Zstd_jll, so drop test/trimming/Zstd_jll/
    and the [sources] override.

--- stdlib/Zstd_jll/src/Zstd_jll.jl
+++ stdlib/Zstd_jll/src/Zstd_jll.jl
@@ -1,41 +1,33 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
-## dummy stub for https://github.com/JuliaBinaryWrappers/Zstd_jll.j:
-#
+## dummy stub for https://github.com/JuliaBinaryWrappers/Zstd_jll.jl
 baremodule Zstd_jll
 using Base, Libdl
 if Sys.iswindows() && Sys.WORD_SIZE == 32
     using CompilerSupportLibraries_jll
 end
 
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
 export libzstd, zstd, zstdmt
 
 # These get calculated in __init__()
 const PATH = Ref("")
-const PATH_list = String[]
 const LIBPATH = Ref("")
-const LIBPATH_list = String[]
 artifact_dir::String = ""
-
+libzstd_handle::Ptr{Cvoid} = C_NULL
 libzstd_path::String = ""
 zstd_path::String = ""
 zstdmt_path::String = ""
-const libzstd = LazyLibrary(
-    if Sys.iswindows()
-        BundledLazyLibraryPath("libzstd-1.dll")
-    elseif Sys.isapple()
-        BundledLazyLibraryPath("libzstd.1.dylib")
-    elseif Sys.islinux() || Sys.isfreebsd()
-        BundledLazyLibraryPath("libzstd.so.1")
-    else
-        error("Zstd_jll: Library 'libzstd' is not available for $(Sys.KERNEL)")
-    end;
-    dependencies = if Sys.iswindows() && Sys.WORD_SIZE == 32
-        LazyLibrary[libgcc_s]
-    else
-        LazyLibrary[]
-    end
-)
+
+if Sys.iswindows()
+    const libzstd = "libzstd-1.dll"
+elseif Sys.isapple()
+    const libzstd = "@rpath/libzstd.1.dylib"
+else
+    const libzstd = "libzstd.so.1"
+end
 
 if Sys.iswindows()
     const zstd_exe = "zstd.exe"
@@ -47,8 +39,6 @@ end
 
 if Sys.iswindows()
     const pathsep = ';'
-elseif Sys.isapple()
-    const pathsep = ':'
 else
     const pathsep = ':'
 end
@@ -84,26 +74,23 @@ end
 zstd() = adjust_ENV(`$zstd_path`)
 zstdmt() = adjust_ENV(`$zstdmt_path`)
 
-# Function to eagerly dlopen our library and thus resolve all dependencies
-function eager_mode()
-    @static if @isdefined CompilerSupportLibraries_jll
-        CompilerSupportLibraries_jll.eager_mode()
-    end
-    dlopen(libzstd)
-end
-
-is_available() = true
-
 function __init__()
-    global libzstd_path = string(libzstd.path)
+    global libzstd_handle = dlopen(libzstd)
+    global libzstd_path = dlpath(libzstd_handle)
     global zstd_path = joinpath(Sys.BINDIR, Base.PRIVATE_LIBEXECDIR, zstd_exe)
     global zstdmt_path = joinpath(Sys.BINDIR, Base.PRIVATE_LIBEXECDIR, zstdmt_exe)
     global artifact_dir = dirname(Sys.BINDIR)
+    LIBPATH[] = dirname(libzstd_path)
+    push!(LIBPATH_list, LIBPATH[])
 end
 
-if Base.generating_output()
-    precompile(eager_mode, ())
-    precompile(is_available, ())
-end
+# JLLWrappers API compatibility shims.  Note that not all of these will really make sense.
+# For instance, `find_artifact_dir()` won't actually be the artifact directory, because
+# there isn't one.  It instead returns the overall Julia prefix.
+is_available() = true
+find_artifact_dir() = artifact_dir
+dev_jll() = error("stdlib JLLs cannot be dev'ed")
+best_wrapper = nothing
+get_libzstd_path() = libzstd_path
 
 end  # module Zstd_jll
