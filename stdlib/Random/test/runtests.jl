f70c0f19d13ba9b4e4f29399d228faa0fba79694
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-06-09 17:30:04 -0300

    [Random] Fix `rand!` for 0-dim complex arrays (#62006)
    https://github.com/JuliaLang/julia/pull/61762 reinterprets complex
    arrays as their underlying real type to take advantage of vectorization,
    but this fails for singleton arrays so we define a method for this case.
    
    @adienes @IanButterworth
    
    ---------
    
    Co-authored-by: Andy Dienes <51664769+adienes@users.noreply.github.com>

--- stdlib/Random/test/runtests.jl
+++ stdlib/Random/test/runtests.jl
@@ -511,7 +511,7 @@ end
     end
 end
 
-@testset "reproducility of methods for $RNG" for RNG=(MersenneTwister,Xoshiro)
+@testset "reproducibility of methods for $RNG" for RNG=(MersenneTwister,Xoshiro)
     mta, mtb = RNG(42), RNG(42)
 
     @test rand(mta) == rand(mtb)
@@ -529,6 +529,8 @@ end
     for T in Base.uniontypes(Base.HWReal)
         a, b = Vector{Complex{T}}(undef, 10), Vector{Complex{T}}(undef, 10)
         @test rand!(mta, a) == rand!(mtb, b)
+        c, d = Array{Complex{T},0}(undef), Array{Complex{T},0}(undef)
+        @test rand!(mta, c) == rand!(mtb, d)
     end
 
     @test randstring(mta) == randstring(mtb)
