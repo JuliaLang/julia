bb04830e207f679bcc24d9bc5d35fb06c67c267d
Jameson Nash <vtjnash@gmail.com>
2026-07-29 15:12:45 +0000

    codegen: add support for llvm-22

--- src/jitlayers.h
+++ src/jitlayers.h
@@ -410,7 +410,10 @@ private:
 
 struct jl_linker_info_t {
     DenseMap<jl_code_instance_t *, jl_codeinst_funcs_t<orc::SymbolStringPtr>> ci_funcs;
-    DenseMap<std::pair<jl_code_instance_t *, jl_invoke_api_t>, orc::SymbolStringPtr>
+    // Key on the enum's underlying integer type: LLVM's DenseMapInfo requires
+    // sentinel keys outside the key's value range, which an unfixed C enum
+    // (jl_invoke_api_t) cannot represent (see LLVM 22 DenseMapInfo<Enum>).
+    DenseMap<std::pair<jl_code_instance_t *, std::underlying_type_t<jl_invoke_api_t>>, orc::SymbolStringPtr>
         call_targets;
     DenseMap<void *, orc::SymbolStringPtr> global_targets;
 };
@@ -456,7 +459,9 @@ public:
 
 public:
     // outputs
-    DenseMap<std::pair<jl_code_instance_t *, jl_invoke_api_t>, jl_codegen_call_target_t>
+    // See the note on jl_linker_info_t::call_targets: keyed on the enum's
+    // underlying integer type so DenseMapInfo has valid sentinel keys.
+    DenseMap<std::pair<jl_code_instance_t *, std::underlying_type_t<jl_invoke_api_t>>, jl_codegen_call_target_t>
         call_targets;
     DenseMap<jl_code_instance_t *, jl_llvm_functions_t> ci_funcs;
     SmallVector<std::pair<jl_code_instance_t *, GlobalVariable *>, 0> external_fns;
--- src/julia_threads.h
+++ src/julia_threads.h
@@ -166,7 +166,7 @@ typedef struct _jl_tls_states_t {
     struct _jl_task_t *root_task;
     struct _jl_timing_block_t *timing_stack;
     // This is the location of our copy_stack
-    void *stackbase;
+    void *JL_NONNULL stackbase;
     size_t stacksize;
     // Temp storage for exception thrown in signal handler. Not rooted.
     struct _jl_value_t *sig_exception;
