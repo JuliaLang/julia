afc3bf8ad20e385c9e6399aa354111a188dd1ae7
Kristoffer Carlsson <kcarlsson89@gmail.com>
2026-08-02 20:04:40 +0200

    move some other macros out of loading.jl

--- base/package.jl
+++ base/package.jl
@@ -8,6 +8,55 @@ function find_source_file(path::AbstractString)
     return isfile(base_path) ? normpath(base_path) : nothing
 end
 
+"""
+    @__FILE__ -> String
+
+Expand to a string with the path to the file containing the
+macrocall, or an empty string if evaluated by `julia -e <expr>`.
+Return `nothing` if the macro was missing parser source information.
+Alternatively see [`PROGRAM_FILE`](@ref).
+"""
+macro __FILE__()
+    __source__.file === nothing && return nothing
+    return String(__source__.file::Symbol)
+end
+
+"""
+    @__DIR__ -> String
+
+Macro to obtain the absolute path of the current directory as a string.
+
+If in a script, returns the directory of the script containing the `@__DIR__` macrocall. If run from a
+REPL or if evaluated by `julia -e <expr>`, returns the current working directory.
+
+# Examples
+
+The example illustrates the difference in the behaviors of `@__DIR__` and `pwd()`, by creating
+a simple script in a different directory than the current working one and executing both commands:
+
+```julia-repl
+julia> cd("/home/JuliaUser") # working directory
+
+julia> # create script at /home/JuliaUser/Projects
+       open("/home/JuliaUser/Projects/test.jl","w") do io
+           print(io, \"\"\"
+               println("@__DIR__ = ", @__DIR__)
+               println("pwd() = ", pwd())
+           \"\"\")
+       end
+
+julia> # outputs script directory and current working directory
+       include("/home/JuliaUser/Projects/test.jl")
+@__DIR__ = /home/JuliaUser/Projects
+pwd() = /home/JuliaUser
+```
+"""
+macro __DIR__()
+    __source__.file === nothing && return nothing
+    _dirname = dirname(String(__source__.file::Symbol))
+    return isempty(_dirname) ? pwd() : abspath(_dirname)
+end
+
 function get_pkgversion_from_path(path)
     project_file = locate_project_file(path)
     project_file isa String || return nothing
