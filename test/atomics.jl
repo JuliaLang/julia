9608bbe2fa31b089082d98b878a1be2c59fbc4da
Jakob Nybo Andersen <jakobnybonissen@gmail.com>
2026-08-15 13:43:27 +0200

    Add unsetindex! for AtomicMemory and AtomicMemoryRef (#62590)

--- test/atomics.jl
+++ test/atomics.jl
@@ -1299,6 +1299,54 @@ test_memory_unset_multiptr(TwoInlinePtr,   AtomicMemory{TwoInlinePtr},   Val(:un
 # atomic memory with element size > MAX_POINTERATOMIC_SIZE: clears via lock + memset
 test_memory_unset_multiptr(ThreeInlinePtr, AtomicMemory{ThreeInlinePtr}, Val(:unordered))
 
+# Test the public atomic unsetindex! overloads for memory references and memory.
+@testset "unsetindex_atomic!" begin
+    for (T, value) in Any[(Any, "value"), (Float32, 1.0f0)]
+        mem = AtomicMemory{T}(undef, 3)
+        ref = memoryref(mem, 2)
+        for order in [:unordered, :monotonic, :release, :sequentially_consistent]
+            # Unset the ref
+            @atomic :monotonic mem[2] = value
+            @test isassigned(ref) == isassigned(mem, 2) == true
+            @test Base.unsetindex_atomic!(ref, order) === ref
+            if T === Any
+                @test isassigned(ref) == isassigned(mem, 2) == false
+                @test_throws UndefRefError ref[]
+                @test_throws UndefRefError @atomic :monotonic mem[2]
+            else
+                @test isassigned(ref) == isassigned(mem, 2) == true
+                @test ref[] isa T
+                @test((@atomic :monotonic mem[2]) isa T)
+            end
+
+            # Unset the mem
+            @atomic :monotonic mem[2] = value
+            @test isassigned(ref) == isassigned(mem, 2) == true
+            @test Base.unsetindex_atomic!(mem, order, 2) === mem
+            if T === Any
+                @test isassigned(ref) == isassigned(mem, 2) == false
+                @test_throws UndefRefError ref[]
+                @test_throws UndefRefError @atomic :monotonic mem[2]
+            else
+                @test isassigned(ref) == isassigned(mem, 2) == true
+                @test ref[] isa T
+                @test((@atomic :monotonic mem[2]) isa T)
+            end
+        end
+    end
+
+    mem = AtomicMemory{Any}(undef, 1)
+    @atomic :monotonic mem[1] = "value"
+    ref = memoryref(mem, 1)
+    @test_throws ConcurrencyViolationError Base.unsetindex_atomic!(ref, :acquire)
+    @test_throws ConcurrencyViolationError Base.unsetindex_atomic!(ref, :acquire_release)
+    @test_throws ConcurrencyViolationError Base.unsetindex_atomic!(ref, :not_atomic)
+    @test_throws ConcurrencyViolationError Base.unsetindex_atomic!(mem, :acquire, 1)
+    @test_throws ConcurrencyViolationError Base.unsetindex_atomic!(mem, :acquire_release, 1)
+    @test_throws ConcurrencyViolationError Base.unsetindex_atomic!(mem, :not_atomic, 1)
+    @test_throws BoundsError Base.unsetindex_atomic!(mem, :release, 0)
+end
+
 @noinline function _test_once_undef(r)
     r = r[]
     TT = eltype(r)
