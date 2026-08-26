3567fdd88e516217bd812acbcc9543f58f51ca28
adienes <andydienes@gmail.com>
2026-08-25 22:38:04 -0400

    remove flisp changes

--- test/broadcast.jl
+++ test/broadcast.jl
@@ -1258,15 +1258,6 @@ end
     end
 end
 
-@testset "issue #32956: dot-call broadcast marks f as called" begin
-    # gf.c's arg-despecialization heuristic strips a Function/Type slot's
-    # concrete type unless the slot is marked called. The flisp lowerer
-    # must mark `f` as called for both `f.(x)` and `f.(x; kw=v)`.
-    bcast(f, x)   = f.(x)
-    bcastkw(f, x) = f.(x; init=0)
-    @test only(methods(bcast)).called   & 0x1 == 0x1
-    @test only(methods(bcastkw)).called & 0x1 == 0x1
-end
 
 @testset "fused broadcast eltype through intermediate unions" begin
     f62564(c, x, y) = sqrt.(ifelse.(c, x, y))
