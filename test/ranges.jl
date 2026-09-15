27456a0c17ebe77f30412371fd1b9101edcaf2aa
Katharine Hyatt <kslimes@gmail.com>
2026-09-15 16:46:28 +0200

    Add a few missing tests for float converter

--- test/ranges.jl
+++ test/ranges.jl
@@ -1478,7 +1478,7 @@ end
 end
 
 @testset "PR 12200 and related" begin
-    for _r in (1:2:100, 1:100, 1f0:2f0:100f0, 1.0:2.0:100.0,
+    for _r in (1:2:100, 1:100, 1f0:2f0:100f0, 1.0:2.0:100.0, LinRange(1, 10, 10),
                range(1, stop=100, length=10), range(1f0, stop=100f0, length=10))
         float_r = float(_r)
         big_r = broadcast(big, _r)
