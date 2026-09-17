ed3c5c1e8fd53e67270d99dae2747f82a6ec234e
Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>
2026-09-16 23:38:46 -0400

    Revert "codegen: handle dynamic-index extractelement in late GC lowering (#62…"
    This reverts commit 9b0103b846bfd5c9b2830b33a36ae785149e49b7.

--- src/llvm-late-gc-lowering.cpp
+++ src/llvm-late-gc-lowering.cpp
@@ -141,12 +141,10 @@ static std::pair<Value*,int> FindBaseValue(const State &S, Value *V, bool UseCac
         }
         else if (auto EEI = dyn_cast<ExtractElementInst>(CurrentV)) {
             assert(CurrentV->getType()->isPointerTy() && fld_idx == -1);
-            if (auto IdxOp = dyn_cast<ConstantInt>(EEI->getIndexOperand())) {
-                fld_idx = IdxOp->getLimitedValue(INT_MAX);
-                CurrentV = EEI->getVectorOperand();
-            }
-            else
-                break;
+            // TODO: For now, only support constant index.
+            auto IdxOp = cast<ConstantInt>(EEI->getIndexOperand());
+            fld_idx = IdxOp->getLimitedValue(INT_MAX);
+            CurrentV = EEI->getVectorOperand();
         }
         else if (auto LI = dyn_cast<LoadInst>(CurrentV)) {
             if (hasLoadedTy(LI->getType())) {
@@ -258,7 +256,6 @@ static std::pair<Value*,int> FindBaseValue(const State &S, Value *V, bool UseCac
            isa<InsertValueInst>(CurrentV) ||
            isa<ExtractValueInst>(CurrentV) ||
            isa<InsertElementInst>(CurrentV) ||
-           isa<ExtractElementInst>(CurrentV) ||
            isa<ShuffleVectorInst>(CurrentV));
     return std::make_pair(CurrentV, fld_idx);
 }
@@ -415,28 +412,6 @@ void LateLowerGCFrame::LiftSelect(State &S, SelectInst *SI) {
         S.AllCompositeNumbering[SI] = Numbers;
 }
 
-void LateLowerGCFrame::LiftExtractElement(State &S, ExtractElementInst *EEI) {
-    if (S.AllPtrNumbering.count(EEI))
-        return; // already visited here--nothing to do
-    assert(!isTrackedValue(EEI) && !isa<ConstantInt>(EEI->getIndexOperand()));
-    Value *Base = MaybeExtractScalar(S, FindBaseValue(S, EEI->getVectorOperand(), false), EEI);
-    if (S.AllPtrNumbering.count(EEI))
-        return; // handled recursively for us
-    if (isa<PointerType>(Base->getType())) {
-        S.AllPtrNumbering[EEI] = Number(S, Base);
-        return;
-    }
-    SmallVector<Value*, 0> Bases = MaybeExtractVector(S, Base, EEI);
-    IRBuilder<> builder(EEI);
-    Value *BaseVec = PoisonValue::get(FixedVectorType::get(T_prjlvalue, Bases.size()));
-    for (unsigned i = 0; i < Bases.size(); ++i) {
-        assert(Bases[i]->getType() == T_prjlvalue);
-        BaseVec = builder.CreateInsertElement(BaseVec, Bases[i], i);
-    }
-    Value *lift = builder.CreateExtractElement(BaseVec, EEI->getIndexOperand(), "gclift");
-    S.AllPtrNumbering[EEI] = Number(S, lift);
-}
-
 void LateLowerGCFrame::LiftPhi(State &S, PHINode *Phi) {
     if (isa<PointerType>(Phi->getType()) ?
             S.AllPtrNumbering.count(Phi) :
@@ -516,10 +491,6 @@ int LateLowerGCFrame::NumberBase(State &S, Value *CurrentV)
         return Number;
     } else if (isa<PHINode>(CurrentV) && !isTrackedValue(CurrentV)) {
         LiftPhi(S, cast<PHINode>(CurrentV));
-        Number = S.AllPtrNumbering[CurrentV];
-        return Number;
-    } else if (isa<ExtractElementInst>(CurrentV) && !isTrackedValue(CurrentV)) {
-        LiftExtractElement(S, cast<ExtractElementInst>(CurrentV));
         Number = S.AllPtrNumbering[CurrentV];
         return Number;
     } else if (isa<ExtractValueInst>(CurrentV)) {
@@ -1461,14 +1432,6 @@ State LateLowerGCFrame::LocalScan(Function &F) {
                         }
                     }
                     MaybeNoteDef(S, BBS, ASCI, std::move(RefinedPtr));
-                }
-            } else if (auto *EEI = dyn_cast<ExtractElementInst>(&I)) {
-                if (isTrackedValue(EEI) && !isa<ConstantInt>(EEI->getIndexOperand())) {
-                    SmallVector<int, 0> Nums = NumberAll(S, EEI->getVectorOperand());
-                    SmallVector<int, 1> RefinedPtr(Nums.begin(), Nums.end());
-                    if (MaybeNoteDef(S, BBS, EEI, std::move(RefinedPtr)))
-                        BBS.FirstSafepointAfterFirstDef = BBS.FirstSafepoint;
-                    NoteOperandUses(S, BBS, I);
                 }
             } else if (auto *AI = dyn_cast<AllocaInst>(&I)) {
                 Type *ElT = AI->getAllocatedType();
