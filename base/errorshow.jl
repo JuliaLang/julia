4e475c0806a9a4228457e6df9887ae63f92d3843
Nicholas Bauer <nicholasbauer@outlook.com>
2026-09-18 10:11:35 -0400

    Use existing helper method
    Co-authored-by: Andy Dienes <51664769+adienes@users.noreply.github.com>

--- base/errorshow.jl
+++ base/errorshow.jl
@@ -1195,7 +1195,7 @@ end
 
 function _backtrace_remove_kwcall_frames!(trace)
     todelete = findall(trace) do (frame, _)
-        code = frame.linfo isa CodeInstance ? frame.linfo.def : frame.linfo
+        code = StackTraces.frame_mi(frame)
         if code isa MethodInstance
             def = code.def
             if def isa Method && def.name !== :kwcall && def.sig <: Tuple{typeof(Core.kwcall),NamedTuple,Any,Vararg}
